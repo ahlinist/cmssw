6179dfa75c66e1d3d66c7cd63d33447241cd5af2
wmtan <>
2005-06-08 21:15:55 +0000

    implement GroupSelector for selecting output branches

--- Framework/interface/OutputModule.h
+++ Framework/interface/OutputModule.h
@@ -10,16 +10,23 @@ $Id$
 
 ----------------------------------------------------------------------*/
 
+#include "FWCore/CoreFramework/interface/GroupSelector.h"
+
 namespace edm
 {
+  class ParameterSet;
   class EventPrincipal;
   class OutputModule
   {
   public:
     typedef OutputModule module_type;
 
+    explicit OutputModule(ParameterSet const& pset) : groupSelector_(pset) {}
     virtual ~OutputModule();
     virtual void write(const EventPrincipal& e) = 0;
+    bool selected(std::string const& label) const {return groupSelector_.selected(label);}
+  private:
+    GroupSelector groupSelector_;
   };
 }
 
