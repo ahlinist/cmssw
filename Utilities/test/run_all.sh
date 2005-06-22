4f0469c20b6ebdbe4842ddabe800cd9c9260d2d1
paterno <>
2005-06-22 22:22:30 +0000

    Functioning first version of basic exception classes and some tests

---
+++ Utilities/test/run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------
+# $Id$
+#----------------------------------------------------------------------
+
+# Pass in name and status
+function die { echo $1: status $2 ;  exit $2; }
+
+# Pass in name
+function do_or_die { echo ===== Running $1 ===== && $1 && echo ===== $1 OK ===== || die ">>>>> $1 failed <<<<<" $?; }
+
+do_or_die Simple_t
+do_or_die Exception_t
+do_or_die ExceptionDerived_t
+do_or_die CodedException_t
+
+
+
