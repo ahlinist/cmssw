d3699b93da95c8c9e5da062009fdb87acba9bd57
wmtan <>
2005-07-01 00:07:27 +0000

    add all the event setup tests to script

--- Framework/test/run_all.sh
+++ Framework/test/run_all.sh
@@ -17,5 +17,16 @@ do_or_die maker2_t
 do_or_die EventProcessor_t
 do_or_die EventProcessor2_t
 do_or_die ScheduleExecutorFromPSet_t
-
-
+do_or_die core_eventsetup_producer_t.exe
+do_or_die core_eventsetup_proxyfactoryproducer_t.exe
+do_or_die core_eventsetup_callback_t.exe
+do_or_die core_eventsetup_proxyfactoryproducer_t.exe
+do_or_die core_eventsetup_callback_t.exe
+do_or_die core_eventsetup_products_t.exe
+do_or_die eventsetup_t
+do_or_die eventsetuprecord_t
+do_or_die dependentrecord_t
+do_or_die interval_t
+do_or_die datakey_t
+do_or_die full_chain_t
+do_or_die eventsetup_plugin_t
