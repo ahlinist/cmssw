28749df2bbc27c3aceb93195db753349775c6fa1
jbk <>
2005-07-21 14:05:51 +0000

    pulled out all the static variable from the exception template. changed the API and put more responibility in the user code.

--- Utilities/test/run_all.sh
+++ Utilities/test/run_all.sh
@@ -4,11 +4,13 @@
 # $Id$
 #----------------------------------------------------------------------
 
+ARC=../../../../test/`scramv1 arch`
+
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
 # Pass in name
-function do_or_die { echo ===== Running $1 ===== && $1 && echo ===== $1 OK ===== || die ">>>>> $1 failed <<<<<" $?; }
+function do_or_die { echo ===== Running $1 ===== && ${ARC}/$1 && echo ===== $1 OK ===== || die ">>>>> $1 failed <<<<<" $?; }
 
 do_or_die Simple_t
 do_or_die Exception_t
