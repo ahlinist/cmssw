8beed4caabc545cf54bc6a7090650773c7de3ef8
argiro <>
2005-07-21 20:42:52 +0000

    Added product registration. Functions produces<Product>("label") registers to an internal list, that then is read by the Worker that wraps the EDProducter

--- Framework/interface/EDProducer.h
+++ Framework/interface/EDProducer.h
@@ -8,10 +8,13 @@ EDProducts into an Event.
 
 $Id$
 
+
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-
+#include "FWCore/Framework/src/TypeID.h"
+#include <string>
+#include <utility>
 namespace edm
 {
   class EDProducer
@@ -20,11 +23,34 @@ namespace edm
     typedef EDProducer ModuleType;
 
     virtual ~EDProducer();
-    virtual void produce(Event& e, EventSetup const& c) = 0;
+    virtual void produce(Event& e, EventSetup const& c)=0;
     virtual void beginJob( EventSetup const& ) ;
     virtual void endJob() ;
-    
+ 
+    /// declare what type of product will make and with which optional label 
+    /** the statement
+        \code
+           produces<ProductType>("optlabel");
+        \endcode
+        should be added to the producer ctor for every product */
+
+    template <class ProductType> 
+    void  produces( std::string label="" ){
+
+      ProductType aproduct;
+      productList_.push_back(std::make_pair(TypeID(aproduct), label));
+      
+    }
+
+    typedef std::list<std::pair<TypeID,std::string> > TypeLabelList;
+    /// used by the fwk to register the list of products of this module 
+    const TypeLabelList& getTypeLabelList() const;
+
+  private:
+    TypeLabelList productList_;
   };
+
+
 }
 
 #endif // EDM_EDPRODUCER_INCLUDED
