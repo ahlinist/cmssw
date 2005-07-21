87a6df2d6cfaf383e6731ddf6e829301e34bf77a
wmtan <>
2005-07-21 16:47:32 +0000

    Move event-independent Provenance to ProductDescription

--- Framework/src/Event.cc
+++ Framework/src/Event.cc
@@ -84,9 +84,9 @@ namespace edm {
 	// set parts of provenance
 	pv->cid = 0; // TODO: what is this supposed to be?
 	// TODO: what is this supposed to be? this is a disgusting string.
-	pv->full_product_type_name = TypeID(*pr).userClassName();
-	pv->friendly_product_type_name = TypeID(*pr).friendlyClassName();
-	pv->product_instance_name = pit->second;
+	pv->product.full_product_type_name = TypeID(*pr).userClassName();
+	pv->product.friendly_product_type_name = TypeID(*pr).friendlyClassName();
+	pv->product.product_instance_name = pit->second;
 	pv->status = Provenance::Success;
 	pv->parents = got_product_ids_;
 
