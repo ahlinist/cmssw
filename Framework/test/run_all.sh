57c80338f579ba16783372802219dd89073f2b5d
wmtan <>
2005-07-25 15:50:18 +0000

    ScheduleExecutorFromPSet_t works again

--- Framework/test/run_all.sh
+++ Framework/test/run_all.sh
@@ -20,7 +20,7 @@ do_or_die maker_t
 do_or_die maker2_t
 do_or_die EventProcessor_t
 do_or_die EventProcessor2_t
-#do_or_die ScheduleExecutorFromPSet_t
+do_or_die ScheduleExecutorFromPSet_t
 do_or_die core_eventsetup_producer_t.exe
 do_or_die core_eventsetup_proxyfactoryproducer_t.exe
 do_or_die core_eventsetup_callback_t.exe
