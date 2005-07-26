f123d923bae53aa5a5b7c90bb2c94794245e171a
chrjones <>
2005-07-26 16:03:14 +0000

    Tommaso's first working implementation of the source

---
+++ GeometryReaders/XMLIdealGeometryESSource/interface/XMLIdealGeometryESSource.h
@@ -0,0 +1,29 @@
+#ifndef GeometryReaders_XMLIdealGeometryESSource_XMLIdealGeometryESSource_H
+#define GeometryReaders_XMLIdealGeometryESSource_XMLIdealGeometryESSource_H
+
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/EventSetupRecordIntervalFinder.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DetectorDescription/DDCore/interface/DDCompactView.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+
+#include <string>
+
+class XMLIdealGeometryESSource : public edm::eventsetup::ESProducer, 
+                                  public edm::eventsetup::EventSetupRecordIntervalFinder
+{
+public:
+    XMLIdealGeometryESSource(const edm::ParameterSet & p);
+    virtual ~XMLIdealGeometryESSource(); 
+    const DDCompactView * produce(const IdealGeometryRecord &);
+protected:
+    virtual void setIntervalFor(const edm::eventsetup::EventSetupRecordKey &,
+				const edm::Timestamp &,edm::ValidityInterval &);
+private:
+    XMLIdealGeometryESSource(const XMLIdealGeometryESSource &);
+    const XMLIdealGeometryESSource & operator=(const XMLIdealGeometryESSource &);
+};
+
+
+#endif
---
+++ GeometryReaders/XMLIdealGeometryESSource/src/XMLIdealGeometryESSource.cc
@@ -0,0 +1,47 @@
+#include "GeometryReaders/XMLIdealGeometryESSource/interface/XMLIdealGeometryESSource.h"
+
+#include "DetectorDescription/DDCore/interface/DDdebug.h"
+#include "DetectorDescription/DDParser/interface/DDLParser.h"
+#include "DetectorDescription/DDCore/interface/DDCompactView.h"
+#include "DetectorDescription/DDCore/interface/DDSpecifics.h"
+#include "DetectorDescription/DDParser/interface/DDLConfiguration.h"
+#include "DetectorDescription/DDAlgorithm/src/AlgoInit.h"
+
+#include <memory>
+
+using namespace edm::eventsetup;
+
+XMLIdealGeometryESSource::XMLIdealGeometryESSource(const edm::ParameterSet & p) 
+{
+    DDLParser * parser = DDLParser::instance();
+    AlgoInit();
+    DDLConfiguration cf;
+    int result1 = cf.readConfig(p.getParameter<std::string>("GeometryConfiguration"));
+    if (result1 !=0) throw DDException("DDLConfiguration: readConfig failed !");
+    int result2 = parser->parse(cf);
+    if (result2 != 0) throw DDException("DDD-Parser: parsing failed!");
+    setWhatProduced(this);
+    findingRecord<IdealGeometryRecord>();
+}
+
+XMLIdealGeometryESSource::~XMLIdealGeometryESSource() {}
+
+const DDCompactView *
+XMLIdealGeometryESSource::produce(const IdealGeometryRecord &)
+{ return new DDCompactView(); }
+
+void XMLIdealGeometryESSource::setIntervalFor(const EventSetupRecordKey &,
+					       const edm::Timestamp &, 
+					       edm::ValidityInterval & oValidity)
+{
+   edm::ValidityInterval infinity(edm::Timestamp(1), edm::Timestamp::endOfTime());
+   oValidity = infinity;
+}
+
+
+#include "FWCore/Framework/interface/SourceFactory.h"
+
+
+DEFINE_FWK_EVENTSETUP_SOURCE(XMLIdealGeometryESSource)
+
+
