fb202165fa3ae9021292ea56a5db8f0c1d4b4ae8
wmtan <>
2005-07-27 04:39:58 +0000

    add vector of ProductDescription pointers

--- Framework/interface/OutputModule.h
+++ Framework/interface/OutputModule.h
@@ -11,24 +11,28 @@ $Id$
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/GroupSelector.h"
+#include "FWCore/Framework/interface/ProductDescription.h"
+#include <vector>
 
 namespace edm {
   class ParameterSet;
   class EventPrincipal;
+  class ProductDescription;
   class ProductRegistry;
   class OutputModule {
   public:
     typedef OutputModule ModuleType;
 
-    explicit OutputModule(ParameterSet const& pset) : groupSelector_(pset) {}
+    explicit OutputModule(ParameterSet const& pset) : preg_(), descVec_(), groupSelector_(pset) {}
     virtual ~OutputModule();
     virtual void write(EventPrincipal const& e) = 0;
     bool selected(ProductDescription const& desc) const {return groupSelector_.selected(desc);}
-    ProductRegistry const& productRegistry() const {return *preg_;}
-    void setProductRegistry(ProductRegistry const* reg_) {preg_ = reg_;}
+    void setProductRegistry(ProductRegistry & reg_);
+  protected:
+    ProductRegistry *preg_;
+    std::vector<ProductDescription const *> descVec_;
   private:
     GroupSelector groupSelector_;
-    ProductRegistry const* preg_;
   };
 }
 
--- Framework/src/OutputModule.cc
+++ Framework/src/OutputModule.cc
@@ -3,10 +3,21 @@ $Id$
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/OutputModule.h"
+#include "FWCore/Framework/interface/ProductRegistry.h"
 
-namespace edm
-{
-  OutputModule::~OutputModule() 
-  { }
+namespace edm {
+  OutputModule::~OutputModule() {
+  }
 
+  void
+  OutputModule::setProductRegistry(ProductRegistry & reg_) {
+    reg_.sort();
+    preg_ = &reg_;  
+    for (ProductRegistry::ProductList::const_iterator it = reg_.productList().begin();
+          it != reg_.productList().end(); ++it) {
+      if (selected(*it)) {
+        descVec_.push_back(&*it);
+      }
+    }
+  }
 }
