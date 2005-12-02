640d49aef083504feae755dde89e684088c39468
maya <>
2005-12-02 11:20:40 +0000

    added files

---
+++ IOMC/FlatEventVertexGenerator/src/module.cc
@@ -0,0 +1,8 @@
+#include "IOMC/EventVertexGenerators/interface/EventVertexGeneratorFactory.h"
+#include "IOMC/FlatEventVertexGenerator/interface/FlatEventVertexGenerator.h"
+
+#include "PluginManager/ModuleDef.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_EVENTVERTEXGENERATOR(FlatEventVertexGenerator);
+
