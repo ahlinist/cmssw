2ad72bb34f17863c2c3912ef7c10b415a6336206
maya <>
2005-12-02 11:20:40 +0000

    added files

---
+++ IOMC/GaussianEventVertexGenerator/src/module.cc
@@ -0,0 +1,8 @@
+#include "IOMC/EventVertexGenerators/interface/EventVertexGeneratorFactory.h"
+#include "IOMC/GaussianEventVertexGenerator/interface/GaussianEventVertexGenerator.h"
+
+#include "PluginManager/ModuleDef.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_EVENTVERTEXGENERATOR(GaussianEventVertexGenerator);
+
