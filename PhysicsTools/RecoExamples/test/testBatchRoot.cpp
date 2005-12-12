6bd96a2c962b781f97a15346b5241f773459c7bf
llista <>
2005-12-12 20:32:18 +0000

    simplified branch name

--- PhysicsTools/RecoExamples/test/testBatchRoot.cpp
+++ PhysicsTools/RecoExamples/test/testBatchRoot.cpp
@@ -17,7 +17,7 @@ int main() {
   TFile file("reco.root");
   TTree * events = dynamic_cast<TTree*>( file.Get( "Events" ) );
   assert( events != 0 );
-  TBranch * branch = events->GetBranch( "TrackExtra_AllTrackExtras__ORCA.obj" );
+  TBranch * branch = events->GetBranch( "TrackExtra_AllTrackExtras.obj" );
   assert( branch != 0 );
 
   TrackExtraCollection trkExtra;
