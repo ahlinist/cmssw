a00ff98bc6839ed6603852608e7136cc8cbb4aef
llista <>
2005-12-20 13:29:11 +0000

    added track plotter

--- PhysicsTools/RecoExamples/test/rootlogon.C
+++ PhysicsTools/RecoExamples/test/rootlogon.C
@@ -5,11 +5,17 @@ gSystem->Load("liblcg_Cintex");
 cout << "Loading TrackReco..." << endl;
 gSystem->Load("libDataFormatsTrackReco");
 
+cout << "Loading MuonReco..." << endl;
+gSystem->Load("libDataFormatsMuonReco");
+
 cout << "Loading VertexReco..." << endl;
 gSystem->Load("libDataFormatsVertexReco");
 
 cout << "Loading EGammaReco..." << endl;
 gSystem->Load("libDataFormatsEGammaReco");
+
+cout << "Loading Framework..." << endl;
+gSystem->Load("libFWCoreFramework");
 
 cout << "Cintex init..." << endl;
 Cintex::enable();
