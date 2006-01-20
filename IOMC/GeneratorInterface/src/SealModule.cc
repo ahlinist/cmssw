89db8669e05437403e8bf24ddea28a69b3b5c907
fmoortga <>
2006-01-20 13:58:55 +0000

    added HepMCProductAnalyzer

--- IOMC/GeneratorInterface/src/SealModule.cc
+++ IOMC/GeneratorInterface/src/SealModule.cc
@@ -2,7 +2,10 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOMC/GeneratorInterface/interface/PythiaSource.h"
+#include "IOMC/GeneratorInterface/interface/HepMCProductAnalyzer.h"
 
-  using edm::PythiaSource; 
+  using edm::PythiaSource;
+
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_INPUT_SOURCE(PythiaSource)
+  DEFINE_ANOTHER_FWK_MODULE(HepMCProductAnalyzer)
