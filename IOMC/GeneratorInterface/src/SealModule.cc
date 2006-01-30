9e9be8d4cc878b6081daf1e541896859e60674f3
fmoortga <>
2006-01-30 18:11:43 +0000

    added filter

--- IOMC/GeneratorInterface/src/SealModule.cc
+++ IOMC/GeneratorInterface/src/SealModule.cc
@@ -3,9 +3,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOMC/GeneratorInterface/interface/PythiaSource.h"
 #include "IOMC/GeneratorInterface/interface/HepMCProductAnalyzer.h"
+#include "IOMC/GeneratorInterface/interface/PythiaFilter.h"
 
   using edm::PythiaSource;
 
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_INPUT_SOURCE(PythiaSource)
   DEFINE_ANOTHER_FWK_MODULE(HepMCProductAnalyzer)
+  DEFINE_ANOTHER_FWK_MODULE(PythiaFilter)
