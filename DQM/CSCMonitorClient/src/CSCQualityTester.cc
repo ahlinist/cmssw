11b5172f1b055a7d6cdb19f835f3b7ae40d43364
segoni <>
2006-02-02 17:04:06 +0000

    improved file reading

--- DQM/CSCMonitorClient/src/CSCQualityTester.cc
+++ DQM/CSCMonitorClient/src/CSCQualityTester.cc
@@ -2,15 +2,15 @@
  *
  *  Implementation of CSCQualityTester
  *
- *  $Date: 2006/01/19 15:17:47 $
- *  $Revision: 1.3 $
+ *  $Date: 2006/01/25 16:28:39 $
+ *  $Revision: 1.1 $
  *  \author Ilaria Segoni
  */
 #include "DQM/CSCMonitorClient/interface/CSCQualityTester.h"
+#include "DQM/CSCMonitorClient/interface/CSCQualityTestTypes.h"
 #include "DQMServices/QualityTests/interface/QCriterionRoot.h"
 
 #include<iostream>
-#include <sstream>
 
 
 CSCQualityTester::CSCQualityTester()
@@ -19,7 +19,7 @@ CSCQualityTester::CSCQualityTester()
   printout=true;
   logFile.open("CSCDQMClient.log");
   
-  if(printout) logFile <<"A Quality Tester Is being Created"<<std::endl;
+  if(printout) logFile <<"A CSC Quality Tester Is being Created"<<std::endl;
 
 }
 
@@ -28,47 +28,52 @@ CSCQualityTester::~CSCQualityTester(){
   logFile.close();
 }
 
+//
+void CSCQualityTester::SetupTests(MonitorUserInterface * mui){
+    
+  if(printout) logFile<<"In CSCQualityTester::SetupTests(...)"<<std::endl;  
+  this->SetupTestsFromTextFile(mui);
+  this->LinkTeststoME();
+  this->AttachRunTests(mui);
+
+}
 
 
-void CSCQualityTester::GetTestsFromDB(MonitorUserInterface * mui){ 
 
+//
+void CSCQualityTester::SetupTestsFromTextFile(MonitorUserInterface * mui){ 
+
+
+  if(printout) logFile<<"In CSCQualityTester::SetupTestsFromTextFiles(...)"<<std::endl;
   FILE* TestsFile;
   char  fileName[128];
   sprintf(fileName,"QualityTests.db");
+  TestsFile= fopen(fileName,"r");
 
-
-  int TestType;
-  char TestName[20];
+  char  TestType[20];
+  char  TestName[20];
   float WarningLevel=0;
   float params[5];
   
   for(int ii=0; ii<5; ii++) params[ii]=0.0;
-  
-  TestsFile= fopen(fileName,"r");
-  
-  int counter =1;
+
   int file_status=1;
-  
-  do{
-    if(printout) logFile<< "File Status before= "<<file_status<<" Reading line: "<<counter<<std::endl;
-    
-    file_status=fscanf(TestsFile,"%d%s%f%f%f%f%f%f\n",
+ 
+  do{    
+     file_status=fscanf(TestsFile,"%s%s%f%f%f%f%f%f\n",
                          &TestType,&TestName,&WarningLevel,
 			 &params[0],&params[1],&params[2],&params[3],&params[4]);
 			 
     if(file_status != EOF){			 
-       if(printout)  logFile<<" Reading Quality Tests Configuration: "
-            <<TestType<<" "<<TestName<<" "<<WarningLevel<<" "
-	    <<params[0]<<" "<<params[1]<<" "<<params[2]
-	    <<" "<<params[3]<<" "<<params[4]<< std::endl;
+    
+           if(printout)  logFile<<" Reading Quality Tests Configuration: "
+                          <<TestType<<" "<<TestName <<" "<<WarningLevel<<" "
+	                  <<params[0]<<" "<<params[1]<<" "<<params[2]
+	                  <<" "<<params[3]<<" "<<params[4]<< std::endl;
 	
-    if(TestType==1) SetContentsXRangeROOTTest(mui,TestName,WarningLevel,params);    
-       	    
-    }	
-
-     counter++;
-    if(printout) logFile<< "File Status after= "<<file_status<<std::endl;
-
+	    
+           if(!std::strcmp(TestType,dqm::qTestType_csc::XRangeContent)) this->SetContentsXRangeROOTTest(mui,TestName,WarningLevel,params);    
+    }
 
   }while( file_status!=EOF ); 
 
@@ -77,81 +82,106 @@ void CSCQualityTester::GetTestsFromDB(MonitorUserInterface * mui){
 }
 
 
+void CSCQualityTester::LinkTeststoME(){ 
+
+
+  if(printout) logFile<<"in CSCQualityTester::LinkTeststoME"<<std::endl;
+  FILE* TestsMEsFile;
+  char  fileName[128];
+  sprintf(fileName,"TestsMEs.db");
+  TestsMEsFile= fopen(fileName,"r");
+  
+  
+  char TestName[20];
+  char MEName[20];
+  
+  int file_status=1;
+  
+  do{    
+      file_status=fscanf(TestsMEsFile,"%s%s\n",&TestName,&MEName);
+			 
+      if(file_status != EOF){			 
+            if(printout) logFile<<"Reading Quality Tests List: " <<TestName <<" "<<MEName << std::endl;
+	    
+	    std::vector<std::string>  MEList;
+	    if( qTestToMEMap.find(TestName) == qTestToMEMap.end()){
+ 	      MEList.push_back(MEName);
+	      qTestToMEMap[TestName]=MEList;
+	    }else{
+	      MEList=qTestToMEMap[TestName];
+	      MEList.push_back(MEName);
+	    }	    
+      
+     }	
+
+  }while( file_status!=EOF ); 
+
+
+
+}
 
 void CSCQualityTester::SetContentsXRangeROOTTest(MonitorUserInterface * mui, char  TestName[20], float  WarningLevel, float  params[5]){
 
+  if(printout) logFile<<"In CSCQualityTester::SetContentsXRangeROOTTest, configuring "<<TestName<<" test."<<std::endl;
 	
- /// X-axis content withing a given range [Xmin, Xmax]
- qTests.push_back(TestName);
- 
- 
- QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),TestName);
- MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
- me_qc1->setAllowedXRange(params[0],params[1]);
- me_qc1->setWarningProb(WarningLevel);
+  qTests.push_back(TestName);
+  QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),TestName);
+  MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
+  me_qc1->setAllowedXRange(params[0],params[1]);
+  me_qc1->setWarningProb(WarningLevel);
  
 }
 
 
 
-void CSCQualityTester::SetupTests(MonitorUserInterface * mui){    
+void CSCQualityTester::AttachRunTests(MonitorUserInterface * mui){
+
+if(printout) logFile<<"In CSCQualityTester::AttachRunTests(...)"<<std::endl;
+
+for (std::map<std::string, std::vector<std::string> >::iterator testsMap=qTestToMEMap.begin(); 
+			   testsMap!=qTestToMEMap.end();++testsMap)	   
+    {
+     
+       std::string testName=testsMap->first;
+       std::vector<std::string> MElist=testsMap->second;
     
- /// X-axis content withing a given range [Xmin, Xmax]
- qtest1 = "xRange";
- 
- 
- QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),qtest1);
- MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
- /// set allowed range in X-axis (default values: histogram's X-range)
- me_qc1->setAllowedXRange(1.0, 200.0);
- /// set probability limit for test warning (default: 90%)
- me_qc1->setWarningProb(0.90);
- 
- 
- 
- return;
-
-}
-
-void CSCQualityTester::AttachTests(MonitorUserInterface * mui){
-
- /// use test <qtest1> on all MEs matching string ""
- if(printout) logFile<<"... for DDU_L1A_Increment_0 ..."<<std::endl;
- mui->useQTest("DDU_L1A_Increment_*", qtest1);
- if(printout) logFile<<"...done."<<std::endl;
-
-
+      	     for(std::vector<std::string>::iterator list = MElist.begin(); list != MElist.end(); ++list){
+      	    	  std::string meName = *(list);
+      	    	  if(printout) logFile<<"Attaching Test "<< testName <<" to ME "<<meName<<std::endl;
+      	    	  mui->useQTest(meName, testName);
+      	     }
+    }
 }
 
 
 
 void CSCQualityTester::CheckTests(MonitorUserInterface * mui) 
 {
-  if(printout) logFile << "Checking Status of Quality Tests" << std::endl;
+  if(printout) logFile << "In CSCQualityTester::CheckTests(...)" << std::endl;
 
   
 	int status = 1980;
 	
 	status= mui->getSystemStatus();
-        if(printout) logFile << "The STATUS IS " << status<<std::endl;
-        if(printout) logFile << "Possible states: \nerror:  " 
+        if(printout) logFile << "Possible states: successful "<< dqm::qstatus::STATUS_OK<<", error:  " 
 	<< dqm::qstatus::ERROR<<
-	"\nwarning:  "<< dqm::qstatus::WARNING<<
-	"\n Other: "<< dqm::qstatus::OTHER<<std::endl;
+	", warning:  "<< dqm::qstatus::WARNING<<
+	", Other: "<< dqm::qstatus::OTHER<<std::endl;
+        if(printout) logFile << "The STATUS IS " << status<<std::endl;
 	
 	switch(status)
 	  {
 	  case dqm::qstatus::ERROR:
-	    std::cout << " Error(s)";
+	    logFile << " Error(s)";
 	    break;
 	  case dqm::qstatus::WARNING:
-	    std::cout << " Warning(s)";
+	    logFile << " Warning(s)";
 	    break;
 	  case dqm::qstatus::OTHER:
-	    std::cout << " Some tests did not run;";
+	    logFile << " Some tests did not run;";
 	    break; 
 	  default:
-	    std::cout << " No problems";
+	    logFile << " No problems";
 	  }
 
 	//MonitorElement * me4 = mui->get("Collector/FU0/C1/C2/histo4");
@@ -162,3 +192,5 @@ void CSCQualityTester::CheckTests(MonitorUserInterface * mui)
 }
 
   
+
+
