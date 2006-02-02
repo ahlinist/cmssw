9fffbba35444071b9a6456a524a56aeeee923ef0
segoni <>
2006-02-02 17:03:08 +0000

    quality test names to put in the cfg file

---
+++ DQM/CSCMonitorClient/interface/CSCQualityTestTypes.h
@@ -0,0 +1,18 @@
+#ifndef CSCQualityTestTypes_H
+#define CSCQualityTestTypes_H
+
+namespace dqm {
+
+  namespace qTestType_csc{
+
+    static const char XRangeContent[14]="XRangeContent";
+
+ }
+
+}
+
+
+
+
+
+#endif
