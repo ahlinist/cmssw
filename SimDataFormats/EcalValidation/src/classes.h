c9bd03bdcf0c24dd576e0cad7a65f67ab6040a03
xhuang <>
2006-02-03 20:10:15 +0000

    first version

---
+++ SimDataFormats/EcalValidation/src/classes.h
@@ -0,0 +1,11 @@
+#include "SimDataFormats/EcalValidation/interface/PEcalValidInfo.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+#include <CLHEP/Vector/LorentzVector.h>
+#include <vector>
+namespace {
+  namespace {
+    std::vector<float>  dumm1;
+    CLHEP::HepLorentzVector dummy1;
+    edm::Wrapper<PEcalValidInfo> theValidData;
+  }
+}
