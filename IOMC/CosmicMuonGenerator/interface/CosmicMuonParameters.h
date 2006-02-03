dd5b4e82519d5bd51ef9abeb55fd3d6f25aff0c0
droll <>
2006-02-03 16:13:47 +0000

    first version

---
+++ IOMC/CosmicMuonGenerator/interface/CosmicMuonParameters.h
@@ -0,0 +1,37 @@
+#ifndef CosmicMuonParameters_h
+#define CosmicMuonParameters_h
+//
+// Parameters for CosmicMuonGenerator by droll (05/12/2005)
+//
+#include "TMath.h"
+
+  // flags
+  const bool Debug = false; // debugging printout
+  const bool EventDisplay = true; // display single events (if ROOT_INTERACTIVE is defined as true)
+
+  // algorithmic constants
+  const double MinStepSize = 10.; // minimal propagation step size [mm] must be small compared to target size
+  // mathematical constants
+  const double Pi = acos(-1.); // [rad]
+  const double TwoPi = 2.0*Pi; // [rad]
+  const double Deg2Rad = Pi/180.; // [deg] -> [rad]
+  const double Rad2Deg = 180./Pi; // [rad] -> [deg]
+  // physical constants
+  const double SpeedOfLight = 299.792458; // [mm/ns]
+  const double MuonMass = 0.105658357; // [GeV/c^2]
+  const double ChargeFrac = 0.545454545; // n(mu+)/n(mu-) ~ 1.2
+  // geometry
+  const double SurfaceOfEarth = 88874.; // Y-distance to surface of earth [mm]
+  const double Z_PX56 = 14000.; // Z-distance to central axis of PX 56 [mm]
+  // densities of materials
+  const double RhoAir  = 0.00; // [g cm^-3]
+  const double RhoWall = 2.65; // [g cm^-3]
+  const double RhoRock = 2.50; // [g cm^-3]
+  // cylinder around CMS (with R, +-Z)
+  const double RadiusCMS =  8000.; // [mm]
+  const double Z_DistCMS = 15000.; // [mm]
+  // cylinder actually used in the code
+  const double RadiusTarget = RadiusCMS; // [mm]
+  const double Z_DistTarget = Z_DistCMS; // [mm]
+
+#endif
---
+++ IOMC/CosmicMuonGenerator/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "IOMC/CosmicMuonGenerator/interface/CosMuoGenSource.h"
+
+  DEFINE_SEAL_MODULE();
+  using edm::CosMuoGenSource;
+  DEFINE_ANOTHER_FWK_INPUT_SOURCE(CosMuoGenSource)
