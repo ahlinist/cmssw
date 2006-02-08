2824aa135c284c5e43b41b521fe6342fdfc0a3b0
llista <>
2006-02-08 11:31:32 +0000

    using automatic library loader

--- PhysicsTools/RecoExamples/test/rootlogon.C
+++ PhysicsTools/RecoExamples/test/rootlogon.C
@@ -1,25 +1,4 @@
 {
-cout << "Loading lcg_Cintex..." << endl;
-gSystem->Load("liblcg_Cintex");
-
-cout << "Loading TrackReco..." << endl;
-gSystem->Load("libDataFormatsTrackReco");
-
-cout << "Loading MuonReco..." << endl;
-gSystem->Load("libDataFormatsMuonReco");
-
-cout << "Loading VertexReco..." << endl;
-gSystem->Load("libDataFormatsVertexReco");
-
-cout << "Loading EGammaReco..." << endl;
-gSystem->Load("libDataFormatsEGammaReco");
-
-cout << "Loading Framework..." << endl;
-gSystem->Load("libFWCoreFramework");
-
-cout << "Cintex init..." << endl;
-Cintex::enable();
-
-cout << "Opening RECO file" << endl;
-TFile f("reco.root");
+  gSystem->Load("libPhysicsToolsFWLite");
+  AutoLibraryLoader::enable();
 }
