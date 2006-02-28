1e770758504f014e170e7f5e3e1bcca426d5ffb6
strang <>
2006-02-28 22:56:15 +0000

    first version

---
+++ SimDataFormats/GlobalValidation/src/classes.h
@@ -0,0 +1,14 @@
+#include "SimDataFormats/GlobalValidation/interface/PGlobalSimHit.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    std::vector<PGlobalSimHit::Vtx> dummy1;
+    std::vector<PGlobalSimHit::Trk> dummy2;
+    std::vector<PGlobalSimHit::CalHit> dummy3;
+    std::vector<PGlobalSimHit::FwdHit> dummy4;
+    std::vector<PGlobalSimHit::BrlHit> dummy5;
+    edm::Wrapper<PGlobalSimHit> theValidData1;
+  }
+}
