f25dd9d67e625edd07356459722560e0064f39c2
igorvrb <>
2006-03-06 13:58:38 +0000

    fixed columns names

--- CondTools/CSC/sql/CSCPedestals_select.sql
+++ CondTools/CSC/sql/CSCPedestals_select.sql
@@ -1,14 +1,34 @@
 REM For CSCPEDESTALS:
-select iov_sequence.NextVal,run_num,data_taking_time from PED_RECORD
-  where run_num=given_value;
+SELECT
+ iov_sequence.NextVal iov_value_id,
+ run_num time,
+ data_taking_time
+FROM PED_RECORD
+WHERE
+ run_num=given_value;
 
 REM For CSCPEDESTALS_MAP:
-select mapid_sequence.NextVal,iov_sequence.CurrVal,layer_id from PED_RECORD
-  where run_num=given_value order by layer_id;
+SELECT
+ mapid_sequence.NextVal map_id,
+ iov_sequence.CurrVal iov_value_id,
+ layer_id csc_int_id
+FROM PED_RECORD
+WHERE
+ run_num=given_value order by csc_int_id;
 
 REM For CSCPEDESTALS_DATA:
-select vector_index.PED_VALUES,map_id.CSCPEDESTALS_MAP,iov_value_id.CSCPEDESTALS_MAP,
-  ped.PED_VALUES,rms.PED_VALUES from PED_VALUES,PED_RECORD,CSCPEDESTALS_MAP
-  where record_id.PED_VALUES=record_id.PED_RECORD and
-    layer_id.CSCPEDESTALS_MAP=layer_id.PED_RECORD
-  order by iov_value_id.CSCPEDESTALS_MAP,map_id.CSCPEDESTALS_MAP,vector_index.PED_VALUES;
+SELECT
+ vector_index.PED_VALUES vec_index,
+ map_id.CSCPEDESTALS_MAP,
+ iov_value_id.CSCPEDESTALS_MAP,
+ ped.PED_VALUES,
+ rms.PED_VALUES
+FROM
+ PED_VALUES,PED_RECORD,CSCPEDESTALS_MAP
+WHERE
+ record_id.PED_VALUES=record_id.PED_RECORD and
+ layer_id.CSCPEDESTALS_MAP=layer_id.PED_RECORD
+ORDER BY
+ iov_value_id.CSCPEDESTALS_MAP,
+ map_id.CSCPEDESTALS_MAP,
+ vector_index.PED_VALUES;
