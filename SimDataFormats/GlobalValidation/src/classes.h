2798aee62c377fcc33e12878a51e7a2c3826c632
strang <>
2006-03-14 19:14:17 +0000

    chage FWCore/EDProduct -> DataFormats/Common

--- SimDataFormats/GlobalValidation/src/classes.h
+++ SimDataFormats/GlobalValidation/src/classes.h
@@ -1,5 +1,5 @@
 #include "SimDataFormats/GlobalValidation/interface/PGlobalSimHit.h"
-#include "FWCore/EDProduct/interface/Wrapper.h"
+#include "DataFormats/Common/interface/Wrapper.h"
 #include <vector>
 
 namespace {
