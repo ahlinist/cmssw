6ea2529f4260c699385cdaa745a7680cd17b7dd0
wmtan <>
2006-04-03 23:06:09 +0000

    Copy constructor and copy assignment operator for HepMCProduct

--- SimDataFormats/HepMCProduct/interface/HepMCProduct.h
+++ SimDataFormats/HepMCProduct/interface/HepMCProduct.h
@@ -16,15 +16,20 @@ namespace edm {
   public:
     HepMCProduct() : evt_( 0 ) { };
     explicit HepMCProduct( HepMC::GenEvent * evt );
-     virtual ~HepMCProduct();
-     void addHepMCData( HepMC::GenEvent  *evt);
+    virtual ~HepMCProduct();
+    void addHepMCData( HepMC::GenEvent  *evt);
     const HepMC::GenEvent& getHepMCData() const ;
  
     const HepMC::GenEvent * GetEvent() const {
       return evt_;
     }
-    private:
-     HepMC::GenEvent * evt_;
+
+    HepMCProduct(HepMCProduct const& x);
+    void swap(HepMCProduct & other);
+    HepMCProduct& operator=(HepMCProduct const& other);
+
+  private:
+    HepMC::GenEvent * evt_;
   };
 
 }
