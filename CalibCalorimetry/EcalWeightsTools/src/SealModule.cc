52cd2b1a580053f6b2336e6f2f6ed63c3fafd47f
rahatlou <>
2006-04-11 16:10:16 +0000

    added analyzer for pedestals. makes pedestals.txt in output as well as pedestals.root

--- CalibCalorimetry/EcalWeightsTools/src/SealModule.cc
+++ CalibCalorimetry/EcalWeightsTools/src/SealModule.cc
@@ -2,7 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "TestEcal/Analyzers/src/EcalPedestalAnalyzer.h"
 #include "CalibCalorimetry/EcalWeightsTools/interface/EcalWeightsBuilder.h"
+#include "CalibCalorimetry/EcalWeightsTools/interface/EcalPedestalAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(EcalWeightsBuilder);
+DEFINE_ANOTHER_FWK_MODULE(EcalPedestalAnalyzer);
