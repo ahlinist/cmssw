8786455c12f7106322241cf963cc11ef1c76c259
bainbrid <>
2006-04-18 20:54:38 +0000

    addition of new commissioning tasks, interface changes, impl changes

---
+++ DQM/SiStripCommissioningSources/interface/OptoScanTask.h
@@ -0,0 +1,40 @@
+#ifndef DQM_SiStripCommissioningSources_OptoScanTask_H
+#define DQM_SiStripCommissioningSources_OptoScanTask_H
+
+#include "DQM/SiStripCommissioningSources/interface/CommissioningTask.h"
+#include <vector>
+
+/**
+   @class OptoScanTask
+*/
+class OptoScanTask : public CommissioningTask {
+
+ public:
+  
+  OptoScanTask( DaqMonitorBEInterface*, const FedChannelConnection& );
+  virtual ~OptoScanTask();
+  
+ private: // ----- private methods -----
+
+  virtual void book();
+  virtual void fill( const SiStripEventSummary&,
+		     const edm::DetSet<SiStripRawDigi>& );
+  virtual void update();
+
+  void locateTicks( const edm::DetSet<SiStripRawDigi>& scope_mode_data,
+		    pair< uint16_t, uint16_t >& digital_range, 
+		    bool first_tick_only = false );
+  
+ private: // ----- data members -----
+
+  /** "Histo sets" for the various gain settings and digital 0 and 1
+      levels. First index is gain (0->3) and the second index is the
+      digital level ("0" or "1"). */
+  vector< vector<HistoSet> > opto_;
+
+  uint16_t nBins_;
+
+};
+
+#endif // DQM_SiStripCommissioningSources_OptoScanTask_H
+
