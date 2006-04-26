bde82e60ca0ba44a1743e13ac4a57b469469adbc
bellan <>
2006-04-26 07:02:57 +0000

    Producer moved from interface

--- RecoMuon/GlobalMuonProducer/src/SealModule.cc
+++ RecoMuon/GlobalMuonProducer/src/SealModule.cc
@@ -2,7 +2,7 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoMuon/GlobalMuonProducer/interface/GlobalMuonProducer.h"
+#include "RecoMuon/GlobalMuonProducer/src/GlobalMuonProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(GlobalMuonProducer)
