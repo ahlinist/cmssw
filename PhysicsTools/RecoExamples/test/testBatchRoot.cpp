b5c1d48594fa24a3cf6270e6d7aa1688ab4b5593
llista <>
2006-04-27 09:01:57 +0000

    added more plots; removed reduntand example

--- PhysicsTools/RecoExamples/test/testBatchRoot.cpp
+++ PhysicsTools/RecoExamples/test/testBatchRoot.cpp
@@ -27,25 +27,45 @@ int main() {
   TrackCollection tracks;
   branch->SetAddress( & tracks );
 
-  TH1F histo( "pt", "Transverse momentum", 100, 0, 20 );
+  TH1F h_pt( "pt", "p_{t}", 50, 0, 20 );
+  TH1F h_eta( "eta", "#eta", 50, -3, 3 );
+  TH1F h_phi( "phi", "#phi", 50, -M_PI, M_PI );
 
   int nev = events->GetEntries();
   for( int ev = 0; ev < nev; ++ ev ) {
     branch->GetEntry( ev );
     for( size_t i = 0; i < tracks.size(); ++i ) {
-      histo.Fill( tracks[ i ].pt() );
+      const Track & track = tracks[ i ];
+      h_pt.Fill( track.pt() );
+      h_eta.Fill( track.eta() );
+      h_phi.Fill( track.phi() );
     }
   }
   file.Close();
 
   TFile hFile( "histo.root", "RECREATE" );
-  histo.Write();
+  h_pt.Write();
+  h_eta.Write();
+  h_phi.Write();
 
   gROOT->SetBatch();
   gROOT->SetStyle("Plain");
   TCanvas c;
-  histo.Draw();
+  h_pt.GetXaxis()->SetTitle( "p_{t} (GeV/c^{2})" );
+  h_pt.SetFillColor( kRed );
+  h_pt.SetLineWidth( 2 );
+  h_pt.Draw();
   c.SaveAs( "pt.jpg" );
+  h_eta.GetXaxis()->SetTitle( "#eta" );
+  h_eta.SetFillColor( kRed );
+  h_eta.SetLineWidth( 2 );
+  h_eta.Draw();
+  c.SaveAs( "eta.jpg" );
+  h_phi.GetXaxis()->SetTitle( "#phi (rad)" );
+  h_phi.SetFillColor( kRed );
+  h_phi.SetLineWidth( 2 );
+  h_phi.Draw();
+  c.SaveAs( "phi.jpg" );
 
   return 0;
 }
