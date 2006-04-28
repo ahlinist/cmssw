ca451895bfb841ed432b5ba3af79bebb3236d79e
llista <>
2006-04-28 12:00:33 +0000

    added more plots

--- PhysicsTools/RecoExamples/test/trackPlots.C
+++ PhysicsTools/RecoExamples/test/trackPlots.C
@@ -1,5 +1,16 @@
 {
 TFile f("reco.root");
+
+// plot track chi-squared
+Events.Draw("tracks.normalizedChi2()", "tracks.chi2()<1000")
+
+// plot number of rec hits
+Events.Draw("trackExtras.recHitsSize()");
+
+// plot pt at vertex vs pt at outermost point
+Events.Draw("trackExtras.outerPt():tracks.pt()", "tracks.pt()>9", "box");
+
+// plot Delta_phi for positive and negative tracks
 TH1F dpplus ("dpplus",  "#Delta#phi", 200,-0.25, 0.25 );
 TH1F dpminus("dpminus", "#Delta#phi", 200,-0.25, 0.25 );
 Events->Project( "dpplus", "tracks.phi()-trackExtras.outerPhi()", "tracks.charge()>0" );
@@ -8,4 +19,12 @@ dpplus.SetFillColor( kBlue );
 dpminus.SetFillColor( kRed );
 dpplus.Draw();
 dpminus.Draw("same");
+
+// plot recHits local position
+Events.Draw("recHits.data_.localPosition().x()")
+
+// plot position of first hit
+Events.Draw( "recHits.data_[trackExtras.recHits_.refVector_.items_[0].index()].localPosition().x():recHits.data_[trackExtras.recHits_.refVector_.items_[1].index()].localPosition().x()", "trackExtras.recHitsSize()>1" );
+// should become:
+// Events.Draw("trackExtras.recHit(0).localPosition().x():trackExtras.recHit(1).localPosition().x()", "trackExtras.recHitsSize()>1");
 }
