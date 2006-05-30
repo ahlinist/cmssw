9aaa190d7db0a042ddab50fe97425db46575bf6b
xiezhen <>
2006-05-30 15:13:41 +0000

    added

---
+++ CondTools/CSCHACalibration/src/CSCRSensorsDataConverter.h
@@ -0,0 +1,22 @@
+#ifndef CondTools_CSCRSensorsDataConverter_h
+#define CondTools_CSCRSensorsDataConverter_h
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "CondTools/Utilities/interface/CSVFieldMap.h"
+#include <string>
+#include <vector>
+namespace edm{
+  class ParameterSet;
+  class Event;
+  class EventSetup;
+}
+class CSCRSensorsDataConverter : public edm::EDAnalyzer {
+ public:
+  explicit CSCRSensorsDataConverter(const edm::ParameterSet& iConfig );
+  ~CSCRSensorsDataConverter(){}
+  virtual void analyze( const edm::Event&, const edm::EventSetup& ){}
+  virtual void endJob();
+ private:
+  CSVFieldMap m_fieldMap;
+  std::string m_inFileName;
+};
+#endif
---
+++ CondTools/CSCHACalibration/src/SealModule.cc
@@ -0,0 +1,6 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CondTools/CSCHACalibration/src/CSCZSensorsDataConverter.h"
+#include "CondTools/CSCHACalibration/src/CSCRSensorsDataConverter.h"
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(CSCZSensorsDataConverter)
+DEFINE_ANOTHER_FWK_MODULE(CSCRSensorsDataConverter)
