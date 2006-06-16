405f0c93010f49bff940625bb25e3d9662855d09
ptc <>
2006-06-16 17:00:57 +0000

    find bin with max content

---
+++ RecoLocalMuon/CSCRecHit/src/CSCPeakBinOfSCAPulse.cc
@@ -0,0 +1,25 @@
+#include <RecoLocalMuon/CSCRecHit/src/CSCPeakBinOfSCAPulse.h>
+#include <DataFormats/CSCDigi/interface/CSCStripDigi.h>
+#include <vector>
+#include <algorithm>
+
+bool CSCPeakBinOfSCAPulse::peakAboveBaseline(const CSCStripDigi & digi, 
+                         const CSCChamberSpecs & specs,
+                         double & height, double & sigma) const {
+  
+    std::vector<int> sca = digi.getADCCounts();
+
+    if ( !sca.empty() ) {
+      int tmax = std::max_element( sca.begin(), sca.end() ) - sca.begin();
+      height = sca[tmax] - baseline(digi);
+    }
+    else {
+      height = 0.;
+    }
+
+    //@@ Assumes 3% calibration uncertainty, and the same noise as
+    //@@ was in ORCA_4_4_1.  Should probably tie these to configurables.
+
+    sigma  = sqrt( height*0.03 * height*0.03 + 4.*4. );
+    return true;
+}
