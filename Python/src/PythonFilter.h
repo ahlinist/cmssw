8ccc97f64ea8ee9c1bbe4ddee7cb38bb29731705
hegner <>
2006-07-22 13:17:19 +0000

    code rearrangement to allow easier extension with more EDClasses

---
+++ Python/src/PythonFilter.h
@@ -0,0 +1,45 @@
+// -*- C++ -*-
+//
+// Package:    PythonFilter
+// Class:      PythonFilter
+// 
+/**\class PythonFilter PythonFilter.cc FWCore/PythonFilter/src/PythonFilter.cc
+
+ Description: an EDFilter which uses python code to do the work
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Chris D Jones & Benedikt Hegner
+//         Created:  Sun Jul 22 10:50:17 EST 2006
+// $Id$
+//
+//
+#ifndef PythonFilter_h
+#define PythonFilter_h
+
+#include "boost/python.hpp"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Python/src/PythonManager.h"
+
+
+class PythonFilter : public edm::EDFilter {
+   public:
+      explicit PythonFilter(const edm::ParameterSet&);
+      ~PythonFilter();
+
+
+      virtual bool filter(edm::Event&, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+      std::string command_; 
+      //have this first to guarantee that Py_Finalize not called
+      // until the object's destructor is called
+      PythonManagerHandle handle_;
+      boost::python::object filter_;
+};
+
+#endif // PythonFilter_h
