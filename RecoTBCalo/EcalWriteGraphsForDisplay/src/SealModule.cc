5a71eaef8bb70938bbd918b2454d0b2cf46ed12f
ghezzi <>
2006-07-24 18:59:16 +0000

    *** empty log message ***

---
+++ RecoTBCalo/EcalWriteGraphsForDisplay/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoTBCalo/EcalWriteGraphsForDisplay/src/EcalWriteGraphsWindows.h"
+
+DEFINE_SEAL_MODULE();
+
+DEFINE_ANOTHER_FWK_MODULE( EcalWriteGraphsWindows );
