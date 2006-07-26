4ef9ec0c26d444d148ca22f18582e93777c2e4fc
ghezzi <>
2006-07-26 12:08:16 +0000

    Added a new module

--- RecoTBCalo/EcalWriteGraphsForDisplay/src/SealModule.cc
+++ RecoTBCalo/EcalWriteGraphsForDisplay/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTBCalo/EcalWriteGraphsForDisplay/src/EcalWriteGraphsWindows.h"
+#include "RecoTBCalo/EcalWriteGraphsForDisplay/src/FoundMaximumCry.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE( EcalWriteGraphsWindows );
+DEFINE_ANOTHER_FWK_MODULE( FoundMaximumCry );
