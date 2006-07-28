3b561326414a6c2cdae25c5d8c7969ec6dd9a421
giordano <>
2006-07-28 22:34:39 +0000

    tidy up

--- CalibTracker/SiStripPedestals/test/go_test_SiStripCondObj.sh
+++ CalibTracker/SiStripPedestals/test/go_test_SiStripCondObj.sh
@@ -4,11 +4,30 @@
 
 eval `scramv1 runtime -sh`
 
-rm -f prova.*
+export workdir=/tmp/${USER}/$$
+echo -e "workdir $workdir\n"
 
-cmsRun write_SiStripCondObj.cfg > out_write
-cmsRun read_SiStripCondObj.cfg > out_read
+[ -e ${workdir} ] && rm -rf ${workdir} 
+mkdir ${workdir}
 
+mapping=blobmappingfile_pednoise.xml
+dbfile=${workdir}/dummy.db
+
+echo	pool_build_object_relational_mapping -f ${mapping} -d CondFormatsSiStripObjects -c sqlite_file:${dbfile} -u me -p mypass  
+pool_build_object_relational_mapping -f ${mapping} -d CondFormatsSiStripObjects -c sqlite_file:${dbfile} -u me -p mypass  
+
+cat write_SiStripCondObj.cfg | sed -e "s#insert_dbfile#${workdir}/dummy.db#" -e "s#insert_dbcatalog#${workdir}/dummy.xml#" > ${workdir}/write_SiStripCondObj.cfg
+cat read_SiStripCondObj.cfg | sed -e "s#insert_dbfile#${workdir}/dummy.db#" -e "s#insert_dbcatalog#${workdir}/dummy.xml#" > ${workdir}/read_SiStripCondObj.cfg
+
+
+echo -e "\ncmsRun ${workdir}/write_SiStripCondObj.cfg > ${workdir}/out_write"
+cmsRun ${workdir}/write_SiStripCondObj.cfg > ${workdir}/out_write
+
+echo -e "\ncmsRun ${workdir}/read_SiStripCondObj.cfg > ${workdir}/out_read"
+cmsRun ${workdir}/read_SiStripCondObj.cfg > ${workdir}/out_read
+
+thisdir=`pwd`
+cd ${workdir}
 echo "------------"
 compare_conn
 echo "------------"
@@ -16,4 +35,4 @@ compare_ped
 echo "------------"
 compare_noise
 echo "------------"
-
+cd $thisdir
