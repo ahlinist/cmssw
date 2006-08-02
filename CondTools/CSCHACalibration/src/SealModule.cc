b6cbf57d2b08e3a4b6266c97fee6badf41eb3a79
kdziedzi <>
2006-08-02 14:22:50 +0000

    *** empty log message ***

--- CondTools/CSCHACalibration/src/SealModule.cc
+++ CondTools/CSCHACalibration/src/SealModule.cc
@@ -1,10 +1,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "CondTools/CSCHACalibration/src/CSCZSensorsDataConverter.h"
 #include "CondTools/CSCHACalibration/src/CSCRSensorsDataConverter.h"
-#include "CondTools/CSCHACalibration/src/PX_sensorsDataConverter.h"
+#include "CondTools/CSCHACalibration/src/PXsensorsDataConverter.h"
 #include "CondTools/CSCHACalibration/src/InclinometersDataConverter.h"
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCZSensorsDataConverter)
 DEFINE_ANOTHER_FWK_MODULE(CSCRSensorsDataConverter)
-DEFINE_ANOTHER_FWK_MODULE(PX_sensorsDataConverter)
+DEFINE_ANOTHER_FWK_MODULE(PXsensorsDataConverter)
 DEFINE_ANOTHER_FWK_MODULE(InclinometersDataConverter)
