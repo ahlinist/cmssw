3ddf81e1eb54d99b70499d3532e88633751808ef
giordano <>
2006-08-05 23:19:41 +0000

    added postscript file

--- CalibTracker/SiStripPedestals/test/SiStripCondObjDisplay.sh
+++ CalibTracker/SiStripPedestals/test/SiStripCondObjDisplay.sh
@@ -14,6 +14,7 @@ test_area=/tmp/$USER/Display
 [ ! -e ${test_area} ] && mkdir ${test_area}
 
 output_file_name=${test_area}/Display_PedNoise_RunNb_${runNb}.root 
+ps_file_name=${test_area}/Display_PedNoise_RunNb_${runNb}.ps 
 cfg_file=${test_area}/SiStripCondObjDisplay_RunNb_${runNb}.cfg
 
 echo ${cfg_file}
@@ -24,9 +25,13 @@ export CORAL_AUTH_PASSWORD=me
 
 eval `scramv1 runtime -sh`
 
-cat template_SiStripCondObjDisplay.cfg | sed -e "s#insert_DBfile#$DBfile#" -e "s#insert_DBcatalog#$DBcatalog#" -e "s#insert_output_filename#${output_file_name}#" -e "s#insert_runNb#${runNb}#" > ${cfg_file}
+cat template_SiStripCondObjDisplay.cfg | sed -e "s#insert_DBfile#$DBfile#" -e "s#insert_DBcatalog#$DBcatalog#" -e "s#insert_output_filename#${output_file_name}#" -e "s#insert_ps_filename#${ps_file_name}#" -e "s#insert_runNb#${runNb}#" > ${cfg_file}
 
 echo "cmsRun ${cfg_file}"
 cmsRun ${cfg_file} > ${test_area}/out_diplay_${runNb}
 
-
+echo "Empty Pedestal Histos" `grep "Empty Ped"  ${test_area}/out_diplay_${runNb} | wc -l`
+echo "Empty Noise Histos" `grep "Empty Nois"  ${test_area}/out_diplay_${runNb} | wc -l`
+echo "Pedestal Histos with all channel at zero" `grep "All channel at zero in Ped"  ${test_area}/out_diplay_${runNb} | wc -l`
+echo "Noise Histos with all channel at zero" `grep "All channel at zero in Nois"  ${test_area}/out_diplay_${runNb} | wc -l`
+grep "SiStripCondObjDisplay::geometry_Vs_Cabling"  ${test_area}/out_diplay_${runNb}
