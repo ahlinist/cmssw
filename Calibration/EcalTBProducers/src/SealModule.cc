265e96bba4f4972b338d440a4a175587b697e1f1
govoni <>
2006-08-10 07:52:26 +0000

    testing module for calibration coefficients effectiveness added

--- Calibration/EcalTBProducers/src/SealModule.cc
+++ Calibration/EcalTBProducers/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Calibration/EcalTBProducers/interface/DumpADC.h"
+#include "Calibration/EcalTBProducers/interface/TestCalib.h"
 
 DEFINE_SEAL_MODULE();
 
-DEFINE_ANOTHER_FWK_MODULE( DumpADC );                                                      
+DEFINE_ANOTHER_FWK_MODULE( DumpADC ) ;
+DEFINE_ANOTHER_FWK_MODULE( TestCalib ) ;
