3ee97418d9c903bd95364d41c7bda5aec2b31e9f
maya <>
2006-08-22 14:35:39 +0000

    added files

---
+++ SimG4CMS/Muon81/src/module.cc
@@ -0,0 +1,6 @@
+#include "SimG4CMS/Muon81/interface/MuonSensitiveDetector.h"
+#include "SimG4Core/SensitiveDetector/interface/SensitiveDetectorPluginFactory.h"
+#include "PluginManager/ModuleDef.h"
+  
+DEFINE_SEAL_MODULE ();
+DEFINE_SENSITIVEDETECTOR(MuonSensitiveDetector);
