6cd3591a17077cdeaba5d7be313d2e31feb5885d
maya <>
2006-08-22 14:38:30 +0000

    added processes, set G4BremsstrahlungThreshold

--- SimG4Core/Physics81/src/PhysicsList.cc
+++ SimG4Core/Physics81/src/PhysicsList.cc
@@ -1,36 +1,42 @@
 #include "SimG4Core/Physics81/interface/PhysicsList.h"
 #include "SimG4Core/Physics81/interface/DDG4ProductionCuts.h"
 
+#include "G4LossTableManager.hh"
+
 PhysicsList::PhysicsList(const edm::ParameterSet & p) 
-  : G4VModularPhysicsList(), m_pPhysics(p),  prodCuts(0)
-{
-    //if (m_pPhysics.getParameter<bool>("CutsPerRegion")) 
-      // prodCuts = new DDG4ProductionCuts();	
-}
+  : G4VModularPhysicsList(), m_pPhysics(p), prodCuts(0)
+{}
  
 PhysicsList::~PhysicsList() 
-{
-    if (prodCuts!=0) delete prodCuts;
+{ 
+    if (m_pPhysics.getUntrackedParameter<int>("Verbosity") > 1)
+	std::cout << " G4BremsstrahlungThreshold was " 
+		  << G4LossTableManager::Instance()->BremsstrahlungTh() 
+		  << " GeV " << std::endl;
+    if (prodCuts!=0) delete prodCuts; 
 }
 
 void PhysicsList::SetCuts() 
 { 
-
     SetDefaultCutValue(m_pPhysics.getParameter<double>("DefaultCutValue")*cm);
     SetCutsWithDefault();
     
-    if ( m_pPhysics.getParameter<bool>("CutsPerRegion") )
+    G4LossTableManager::Instance()->SetBremsstrahlungTh
+	(m_pPhysics.getParameter<double>("G4BremsstrahlungThreshold")*GeV);
+    
+    if (m_pPhysics.getParameter<bool>("CutsPerRegion"))
     {
-       std::cout << " Setting Production Cuts per Region" << std::endl ;
-       DDG4ProductionCuts prodCuts;
-       prodCuts.SetVerbosity( m_pPhysics.getUntrackedParameter<int>("Verbosity",0)) ;
-       prodCuts.update();
+	std::cout << " Setting Production Cuts per Region" << std::endl;
+	DDG4ProductionCuts prodCuts;
+	prodCuts.SetVerbosity(m_pPhysics.getUntrackedParameter<int>("Verbosity",0));
+	prodCuts.update();
     }
 
-    if ( m_pPhysics.getUntrackedParameter<int>("Verbosity") > 1 ) 
+    int v = m_pPhysics.getUntrackedParameter<int>("Verbosity");
+    if (v > 1)
+    {
+	G4LossTableManager::Instance()->SetVerbose(v-1);
 	G4VUserPhysicsList::DumpCutValuesTable();
-
-    return ;
-
+    }
 }
 
