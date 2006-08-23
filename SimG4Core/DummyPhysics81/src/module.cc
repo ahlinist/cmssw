bc192b84a252eb21c9dd234dfa97aaf03f33e679
maya <>
2006-08-23 13:34:27 +0000

    added files

---
+++ SimG4Core/DummyPhysics81/src/module.cc
@@ -0,0 +1,8 @@
+#include "SimG4Core/Physics81/interface/PhysicsListFactory.h"
+#include "SimG4Core/DummyPhysics81/interface/DummyPhysics.h"
+
+#include "PluginManager/ModuleDef.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_PHYSICSLIST (DummyPhysics);
+
