0164e08b6a7d2ef582bf51dd2646342e2e5f05dc
spitsky <>
2006-09-11 15:25:11 +0000

    *** empty log message ***

---
+++ GeneratorInterface/CommonInterface/interface/FindJets.h
@@ -0,0 +1,6 @@
+#define findjets findjets_
+extern "C" {
+      void findjets();
+      void call_findjets(){ findjets( ); }
+}
+ 
---
+++ GeneratorInterface/CommonInterface/interface/Txgive.h
@@ -0,0 +1,10 @@
+#define TXGIVE txgive_
+extern "C" {
+  void TXGIVE(const char*,int length);
+}
+
+#define TXGIVE_INIT txgive_init_
+extern "C" {
+  void TXGIVE_INIT();
+}
+ 
