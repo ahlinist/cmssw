2c00338067e6ed3b71d8e560d7f9dd5cceaed10b
mfhansen <>
2006-09-11 13:29:42 +0000

    latest version

---
+++ GeneratorInterface/MadGraphInterface/src/SealModule.cc
@@ -0,0 +1,12 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "GeneratorInterface/MadGraphInterface/interface/MadGraphSource.h"
+#include "GeneratorInterface/MadGraphInterface/interface/HepMGProductAnalyzer.h"
+
+using edm::MadGraphSource; 
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(MadGraphSource)
+DEFINE_ANOTHER_FWK_MODULE(HepMGProductAnalyzer)
+
