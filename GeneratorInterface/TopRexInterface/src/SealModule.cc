f444fb4bbcc8bd7e7d87c9187108f1ccd1b4f695
spitsky <>
2006-09-11 14:39:22 +0000

    *** empty log message ***

---
+++ GeneratorInterface/TopRexInterface/src/SealModule.cc
@@ -0,0 +1,11 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "GeneratorInterface/TopRexInterface/interface/ToprexSource.h"
+
+  using edm::ToprexSource;
+
+  DEFINE_SEAL_MODULE();
+  DEFINE_ANOTHER_FWK_INPUT_SOURCE(ToprexSource)
+  // DEFINE_ANOTHER_FWK_MODULE(HepMCProductAnalyzer)
+  // DEFINE_ANOTHER_FWK_MODULE(PythiaFilter)
