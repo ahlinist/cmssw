02eb5e9eb3a49ffe922e5dd34d85b6c0822c938e
dfortin <>
2006-09-14 20:45:37 +0000

    Added 3x3 Gatti option

--- RecoLocalMuon/CSCRecHitB/src/CSCFitXonStripWithGatti.cc
+++ RecoLocalMuon/CSCRecHitB/src/CSCFitXonStripWithGatti.cc
@@ -23,6 +23,7 @@
 CSCFitXonStripWithGatti::CSCFitXonStripWithGatti(const edm::ParameterSet& ps){
 
   debug                      = ps.getUntrackedParameter<bool>("CSCDebug");
+  use3TimeBins               = ps.getUntrackedParameter<bool>("CSCUse3x3Gatti");
 
 }
 
@@ -53,13 +54,16 @@ void CSCFitXonStripWithGatti::findXOnStrip( const CSCLayer* layer, const CSCStri
 
   // Loading in 3x3 matrix with corrected adcs... well eventually.
   int j = 0;
-  Q_tot = 0.;
+  Q_tot[0] = 0.;
+  Q_tot[1] = 0.;
+  Q_tot[2] = 0.;
+
   for ( int i=1; i <= nStrips; i++ ) {
     if ( i > (CenterStrip-2) && i < (CenterStrip+2) ) {
       for ( int t=0; t<3; t++ ) {
         int k = t + 3*(i-1);
         d[j][t] = adcs[k];
-        Q_tot += adcs[k];
+        Q_tot[t] += adcs[k];
       }
       j++;
     }
@@ -191,16 +195,26 @@ void CSCFitXonStripWithGatti::runGattiFit( int istrt ) {
  */
 float CSCFitXonStripWithGatti::chisqrFromGatti( float x ) {
 
-  // Compute Gatti function for 3 positions
-  getGatti( x );
+  // Compute Gatti function for 3 positions and 3 time bins
+  for (int t = 0; t < 3; t++ ) getGatti( x, t );
 
   float chi2 = 0.;
 
-  // What is the error on these adc counts ?  
-  // Assume poisson stats ...
+  // What is the error on these adc counts after applying all the corrections ?  
+  // Blightly assume poisson stats ...
 
-  for ( int i = 0; i < 3; i++) {
-	chi2 += (d[i][1] - q[i][1])*(d[i][1] - q[i][1]) / (d[i][1] + q[i][1]);
+  // Also, leave the option open to use either 3 x 3 Gatti or simply look at 3 strip for tmax
+  if (use3TimeBins) {
+    for ( int i = 0; i < 3; i++) {
+      for (int t = 0; t < 3; t++) {
+        chi2 += (d[i][t] - q[i][t])*(d[i][t] - q[i][t]) / (d[i][t] + q[i][t]);
+      } 
+    }
+  } else {
+    int t = 1;
+    for (int i = 0; i < 3; i++) {
+      chi2 += (d[i][t] - q[i][t])*(d[i][t] - q[i][t]) / (d[i][t] + q[i][t]);
+    } 
   }
   return chi2;
 }
@@ -239,20 +253,20 @@ void CSCFitXonStripWithGatti::initChamberSpecs() {
 /* getGatti
  *
  */
-void CSCFitXonStripWithGatti::getGatti( float x ) {
+void CSCFitXonStripWithGatti::getGatti( float x, int t ) {
 
   double r = h / stripWidth;
 
-  double g1 = Q_tot * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x - 1.5)/r ) );
-  double g2 = Q_tot * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x - 0.5)/r ) );
-  double g3 = Q_tot * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x + 0.5)/r ) );
-  double g4 = Q_tot * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x + 1.5)/r ) );
+  double g1 = Q_tot[t] * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x - 1.5)/r ) );
+  double g2 = Q_tot[t] * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x - 0.5)/r ) );
+  double g3 = Q_tot[t] * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x + 0.5)/r ) );
+  double g4 = Q_tot[t] * norm * r * atan( sqrt_k_3 * tanh( k_2 * (-x + 1.5)/r ) );
 
 
   if (debug) std::cout << "g3, g2, q[1][1] " << g3 << " " << g2 << " " << g3 - g2 << std::endl;
 
-  q[0][1] = g2 - g1;
-  q[1][1] = g3 - g2;
-  q[2][1] = g4 - g3;
+  q[0][t] = g2 - g1;
+  q[1][t] = g3 - g2;
+  q[2][t] = g4 - g3;
 
 }
