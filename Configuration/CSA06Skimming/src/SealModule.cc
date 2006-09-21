388dca84300cfd54aaddc155cbe4662ff61a22b8
gruen <>
2006-09-21 11:05:35 +0000

    Add filter on CSA06 HLT bits.

--- Configuration/CSA06Skimming/src/SealModule.cc
+++ Configuration/CSA06Skimming/src/SealModule.cc
@@ -3,9 +3,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Configuration/CSA06Skimming/interface/MCProcessFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCSingleParticleFilter.h"
+#include "Configuration/CSA06Skimming/interface/HLTCSA06Bits.h"
  
 
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_MODULE(MCProcessFilter);
   DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter)
-
+  DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits)
