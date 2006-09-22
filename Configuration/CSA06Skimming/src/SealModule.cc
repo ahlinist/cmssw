1e33eaaf858419391f65e3af86c88b3fe4576791
fmoortga <>
2006-09-22 14:03:49 +0000

    add MCParticlePairFilter

--- Configuration/CSA06Skimming/src/SealModule.cc
+++ Configuration/CSA06Skimming/src/SealModule.cc
@@ -4,9 +4,11 @@
 #include "Configuration/CSA06Skimming/interface/MCProcessFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCSingleParticleFilter.h"
 #include "Configuration/CSA06Skimming/interface/HLTCSA06Bits.h"
+#include "Configuration/CSA06Skimming/interface/MCParticlePairFilter.h"
  
 
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_MODULE(MCProcessFilter);
-  DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter)
-  DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits)
+  DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter);
+  DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits);
+  DEFINE_ANOTHER_FWK_MODULE(MCParticlePairFilter)
