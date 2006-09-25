caeb3d11c1f50953c1dd835c65d39c152a6025db
meridian <>
2006-09-25 17:29:42 +0000

    Adding MCZll

--- Configuration/CSA06Skimming/src/SealModule.cc
+++ Configuration/CSA06Skimming/src/SealModule.cc
@@ -3,14 +3,17 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Configuration/CSA06Skimming/interface/MCProcessFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCSingleParticleFilter.h"
+#include "Configuration/CSA06Skimming/interface/MCZll.h" 
 #include "Configuration/CSA06Skimming/interface/HLTCSA06Bits.h"
 #include "Configuration/CSA06Skimming/interface/MCParticlePairFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCProcessRangeFilter.h"
  
 
-  DEFINE_SEAL_MODULE();
-  DEFINE_ANOTHER_FWK_MODULE(MCProcessFilter);
-  DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter);
-  DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits);
-  DEFINE_ANOTHER_FWK_MODULE(MCParticlePairFilter);
-  DEFINE_ANOTHER_FWK_MODULE(MCProcessRangeFilter)
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MCProcessFilter);
+DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits);
+DEFINE_ANOTHER_FWK_MODULE(MCParticlePairFilter);
+DEFINE_ANOTHER_FWK_MODULE(MCProcessRangeFilter);
+DEFINE_ANOTHER_FWK_MODULE(MCZll)
+  
