bf64549c237f67b0b8aa7060e32b6979aec5e410
sunanda <>
2006-09-25 13:53:07 +0000

    Add flag for usage in G4.8.1

--- SimG4Core/LHEP/src/HadronPhysicsLHEP.hh
+++ SimG4Core/LHEP/src/HadronPhysicsLHEP.hh
@@ -1,8 +1,10 @@
 #ifndef HadronPhysicsLHEP_h
 #define HadronPhysicsLHEP_h 1
 
+#define G4v7
 #include "G4VPhysicsConstructor.hh"
 
+#ifdef G4v7
 #include "SimG4Core/Packaging/src/G4HadronQEDBuilder.hh"
 #include "SimG4Core/Packaging/src/G4StoppingHadronBuilder.hh"
 #include "SimG4Core/Packaging/src/G4MiscLHEPBuilder.hh"
@@ -15,6 +17,19 @@
 
 #include "SimG4Core/Packaging/src/G4NeutronBuilder.hh"
 #include "SimG4Core/Packaging/src/G4LHEPNeutronBuilder.hh"
+#else
+#include "G4StoppingHadronBuilder.hh"
+#include "G4MiscLHEPBuilder.hh"
+ 
+#include "G4LHEPPiKBuilder.hh"
+#include "G4PiKBuilder.hh"
+ 
+#include "G4ProtonBuilder.hh"
+#include "G4LHEPProtonBuilder.hh"
+ 
+#include "G4NeutronBuilder.hh"
+#include "G4LHEPNeutronBuilder.hh"
+#endif 
 
 class HadronPhysicsLHEP : public G4VPhysicsConstructor
 {
@@ -27,6 +42,7 @@ class HadronPhysicsLHEP : public G4VPhysicsConstructor
     virtual void ConstructProcess();
 
   private:
+#ifdef G4v7
     G4NeutronBuilder theNeutrons;
     G4LHEPNeutronBuilder theLHEPNeutron;
     
@@ -39,6 +55,20 @@ class HadronPhysicsLHEP : public G4VPhysicsConstructor
     G4MiscLHEPBuilder theMiscLHEP;
     G4StoppingHadronBuilder theStoppingHadron;
     G4HadronQEDBuilder theHadronQED;
+#else
+    void CreateModels();
+    G4NeutronBuilder * theNeutrons;
+    G4LHEPNeutronBuilder * theLHEPNeutron;
+     
+    G4PiKBuilder * thePiK;
+    G4LHEPPiKBuilder * theLHEPPiK;
+     
+    G4ProtonBuilder * thePro;
+    G4LHEPProtonBuilder * theLHEPPro;
+     
+    G4MiscLHEPBuilder * theMiscLHEP;
+    G4StoppingHadronBuilder * theStoppingHadron;
+#endif
 };
 
 // 2002 by J.P. Wellisch
--- SimG4Core/LHEP/src/LHEP.cc
+++ SimG4Core/LHEP/src/LHEP.cc
@@ -1,21 +1,40 @@
 #include "LHEP.hh"
 #include "HadronPhysicsLHEP.hh"
 
+#ifdef G4v7
 #include "SimG4Core/Packaging/src/GeneralPhysics.hh"
 #include "SimG4Core/Packaging/src/EMPhysics.hh"
 #include "SimG4Core/Packaging/src/MuonPhysics.hh"
 #include "SimG4Core/Packaging/src/IonPhysics.hh"
 #include "SimG4Core/Packaging/src/G4DataQuestionaire.hh"
+#else
+#endif
 
 LHEP::LHEP(const edm::ParameterSet & p) : PhysicsList(p)
 {
-    G4DataQuestionaire it(photon);
-    std::cout << "You are using the simulation engine: LHEP 3.7" << std::endl;
+  G4DataQuestionaire it(photon);
+#ifdef G4v7
+  std::cout << "You are using the simulation engine: LHEP 3.7" << std::endl;
   
-    RegisterPhysics(new GeneralPhysics("general"));
-    RegisterPhysics(new EMPhysics("EM"));
-    RegisterPhysics(new MuonPhysics("muon"));
-    RegisterPhysics(new HadronPhysicsLHEP("hadron"));
-    RegisterPhysics(new IonPhysics("ion"));
+  RegisterPhysics(new GeneralPhysics("general"));
+  RegisterPhysics(new EMPhysics("EM"));
+  RegisterPhysics(new MuonPhysics("muon"));
+  RegisterPhysics(new HadronPhysicsLHEP("hadron"));
+  RegisterPhysics(new IonPhysics("ion"));
+#else
+  std::cout << "You are using the simulation engine: LHEP 4.1" << std::endl;
+  // EM Physics
+  RegisterPhysics( new G4EmStandardPhysics("standard EM"));
+  // Synchroton Radiation & GN Physics
+  RegisterPhysics( new G4EmExtraPhysics("extra EM"));
+  // General Physics - i.e. decay
+  RegisterPhysics( new G4DecayPhysics("decay"));
+  // Hadron Elastic scattering
+  RegisterPhysics( new G4HadronElasticPhysics("LElastic"));
+  // Hadron Physics
+  RegisterPhysics(  new HadronPhysicsLHEP("hadron"));
+  // Ion Physics
+  RegisterPhysics( new G4IonPhysics("ion"));
+#endif
 }
 
