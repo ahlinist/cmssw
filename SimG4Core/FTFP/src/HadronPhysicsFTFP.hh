4cc854c2fa05b9fc0649e68440d3293fab535099
sunanda <>
2006-09-25 13:38:34 +0000

    Add flag for usage in G4.8.1

--- SimG4Core/FTFP/src/HadronPhysicsFTFP.hh
+++ SimG4Core/FTFP/src/HadronPhysicsFTFP.hh
@@ -1,8 +1,10 @@
 #ifndef SimG4Core_HadronPhysicsFTFP_H
 #define SimG4Core_HadronPhysicsFTFP_H
 
+#define G4v7
 #include "G4VPhysicsConstructor.hh"
 
+#ifdef G4v7
 #include "SimG4Core/Packaging/src/G4HadronQEDBuilder.hh"
 #include "SimG4Core/Packaging/src/G4StoppingHadronBuilder.hh"
 #include "SimG4Core/Packaging/src/G4MiscLHEPBuilder.hh"
@@ -18,6 +20,21 @@
 #include "SimG4Core/Packaging/src/G4NeutronBuilder.hh"
 #include "SimG4Core/Packaging/src/G4LEPNeutronBuilder.hh"
 #include "SimG4Core/Packaging/src/G4FTFPNeutronBuilder.hh"
+#else
+#include "G4MiscLHEPBuilder.hh"
+ 
+#include "G4PiKBuilder.hh"
+#include "G4LEPPiKBuilder.hh"
+#include "G4FTFPPiKBuilder.hh"
+ 
+#include "G4ProtonBuilder.hh"
+#include "G4LEPProtonBuilder.hh"
+#include "G4FTFPProtonBuilder.hh"
+ 
+#include "G4NeutronBuilder.hh"
+#include "G4LEPNeutronBuilder.hh"
+#include "G4FTFPNeutronBuilder.hh"
+#endif
 
 class HadronPhysicsFTFP : public G4VPhysicsConstructor
 {
@@ -30,6 +47,7 @@ class HadronPhysicsFTFP : public G4VPhysicsConstructor
     virtual void ConstructProcess();
 
   private:
+#ifdef G4v7
     G4NeutronBuilder theNeutrons;
     G4LEPNeutronBuilder theLEPNeutron;
     G4FTFPNeutronBuilder theFTFPNeutron;
@@ -45,6 +63,22 @@ class HadronPhysicsFTFP : public G4VPhysicsConstructor
     G4MiscLHEPBuilder theMiscLHEP;
     G4StoppingHadronBuilder theStoppingHadron;
     G4HadronQEDBuilder theHadronQED;
+#else
+    void CreateModels();
+    G4NeutronBuilder * theNeutrons;
+    G4LEPNeutronBuilder * theLEPNeutron;
+    G4FTFPNeutronBuilder * theFTFPNeutron;
+     
+    G4PiKBuilder * thePiK;
+    G4LEPPiKBuilder * theLEPPiK;
+    G4FTFPPiKBuilder * theFTFPPiK;
+     
+    G4ProtonBuilder * thePro;
+    G4LEPProtonBuilder * theLEPPro;
+    G4FTFPProtonBuilder * theFTFPPro;
+     
+    G4MiscLHEPBuilder * theMiscLHEP;
+#endif
 };
 
 // 2002 by J.P. Wellisch
