cdf65026dcc84c153b37ebc6c83625ca2628e120
mfhansen <>
2006-10-01 10:04:17 +0000

    bug fix

--- GeneratorInterface/MadGraphInterface/src/HepMGProductAnalyzer.cc
+++ GeneratorInterface/MadGraphInterface/src/HepMGProductAnalyzer.cc
@@ -1,29 +1,16 @@
 #include "GeneratorInterface/MadGraphInterface/interface/HepMGProductAnalyzer.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include <iostream>
-#include "TFile.h"
-#include "TH1.h"
-
-using namespace edm;
-using namespace std;
 
 HepMGProductAnalyzer::HepMGProductAnalyzer(const edm::ParameterSet& iConfig) :
 label_(iConfig.getUntrackedParameter("moduleLabel",std::string("source")))
 {
-  // Set up histograms and output file
-  fOutputFile   = new TFile( "GenLevelPlots.root", "RECREATE" ) ;
-  etaPlot  = new TH1D(  "eta", "Parton level eta", 50,  -5., 5. ) ;
-  etPlot  = new TH1D(  "et", "Parton level transverse energy", 10, 0, 500.) ;
-  phiPlot   = new TH1D(  "phi",  "Parton level phi",   30, -3., 3. ) ; 
-  nt = new TNtuple("GenTuple","Ntuple for generator stuff","HiggsEta:HiggsPhi:HiggsEt");
+
 }
 
 
 HepMGProductAnalyzer::~HepMGProductAnalyzer()
 {
-  // Write to file and close it
-  fOutputFile->Write();
-  fOutputFile->Close();
 }
 
 void HepMGProductAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
@@ -34,31 +21,22 @@ void HepMGProductAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
    iEvent.getByType(evt);
 
    HepMC::GenEvent * myGenEvent = new  HepMC::GenEvent(*(evt->GetEvent()));
+
+   //Print out the HepMCProduct
+   //evt->GetEvent()->print();
   
    //To print out status code, pdg, mass, etc table uncomment below
-   //   this->printTable(myGenEvent);
+      this->printTable(myGenEvent);
 
-   //To histogram phi, eta and transverse energy for the Higgs
-   for ( HepMC::GenEvent::particle_iterator p = myGenEvent->particles_begin();
-	 p != myGenEvent->particles_end(); ++p) { 
-     if((*p)->pdg_id()==25){
-       pdgPlot->Fill((*p)->pdg_id());
-       etaPlot->Fill((*p)->momentum().rapidity());
-       etPlot->Fill((*p)->momentum().et());
-       phiPlot->Fill((*p)->momentum().phi());
-       nt->Fill((*p)->momentum().rapidity(),(*p)->momentum().phi(),(*p)->momentum().et(),1);
-     }
-   }
-   delete myGenEvent;
 }
 
 void HepMGProductAnalyzer::printTable(HepMC::GenEvent * event)
 {
   HepMC::GenEvent * myGenEvent = event;
   std::cout<<std::endl;
-  std::cout<<"Pgd  status   Px   Py   Pz   E   mass"<<std::endl;   
+  std::cout<<"Barcode:  Pgd:     Status:  Mother:"<<std::endl;   
   for ( HepMC::GenEvent::particle_iterator p = myGenEvent->particles_begin();
 	p != myGenEvent->particles_end(); ++p) { 
-    std::cout<<(*p)->pdg_id()<<"   "<<(*p)->status()<<"   "<<(*p)->momentum().x()<<"   "<<(*p)->momentum().y()<<"   "<<(*p)->momentum().z()<<"     "<<(*p)->momentum().t()<<"   "<<(*p)->generatedMass()<<std::endl;
+    std::cout<<" "<<(*p)->barcode()<<"        "<<(*p)->pdg_id()<<"       "<<(*p)->status()<<"        "<<(*p)->Mother()<<std::endl;
   }
 }
