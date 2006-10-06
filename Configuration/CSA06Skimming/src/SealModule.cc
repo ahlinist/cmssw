60cde2421fb2b2400330471003a1dfed49aa9579
fambrogl <>
2006-10-06 08:10:42 +0000

    committed the MinimumBias and UnderlyingEvent filter

--- Configuration/CSA06Skimming/src/SealModule.cc
+++ Configuration/CSA06Skimming/src/SealModule.cc
@@ -6,6 +6,7 @@
 #include "Configuration/CSA06Skimming/interface/MCZll.h" 
 #include "Configuration/CSA06Skimming/interface/HLTCSA06Bits.h"
 #include "Configuration/CSA06Skimming/interface/MCParticlePairFilter.h"
+#include "Configuration/CSA06Skimming/interface/MinimumBiasFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCProcessRangeFilter.h"
 #include "Configuration/CSA06Skimming/interface/MCDijetResonance.h" 
 
@@ -16,6 +17,7 @@ DEFINE_ANOTHER_FWK_MODULE(MCProcessFilter);
 DEFINE_ANOTHER_FWK_MODULE(MCSingleParticleFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTCSA06Bits);
 DEFINE_ANOTHER_FWK_MODULE(MCParticlePairFilter);
+DEFINE_ANOTHER_FWK_MODULE(MinimumBiasFilter);
 DEFINE_ANOTHER_FWK_MODULE(MCProcessRangeFilter);
 DEFINE_ANOTHER_FWK_MODULE(MCZll)
 DEFINE_ANOTHER_FWK_MODULE(MCDijetResonance)
