e8ca96f9db4de33c3a15402b6dec8709e3b1d8e0
jbrooke <>
2006-10-10 23:31:20 +0000

    not sure records are registered

--- L1Trigger/L1Scales/src/ES_L1CaloEtScale.cc
+++ L1Trigger/L1Scales/src/ES_L1CaloEtScale.cc
@@ -1,5 +1,5 @@
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 #include "L1Trigger/L1Scales/interface/L1CaloEtScale.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 EVENTSETUP_DATA_REG(L1CaloEtScale);
