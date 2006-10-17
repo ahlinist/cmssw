ecb15af49572133996a6733e175932bab5e32f66
llista <>
2006-10-17 12:02:46 +0000

    updated interactive examples

--- PhysicsTools/RecoExamples/test/trackPlots.C
+++ PhysicsTools/RecoExamples/test/trackPlots.C
@@ -1,30 +1,28 @@
 {
-TFile f("reco.root");
+TFile f( "reco.root" );
 
 // plot track chi-squared
-Events.Draw("tracks.normalizedChi2()", "tracks.chi2()<1000")
+Events.Draw("ctfWithMaterialTracks.normalizedChi2()", "ctfWithMaterialTracks.chi2()<1000")
 
 // plot number of rec hits
-Events.Draw("trackExtras.recHitsSize()");
+Events.Draw("ctfWithMaterialTrackExtras.recHitsSize()");
 
 // plot pt at vertex vs pt at outermost point
-Events.Draw("trackExtras.outerPt():tracks.pt()", "tracks.pt()>9", "box");
+Events.Draw("ctfWithMaterialTrackExtras.outerPt():ctfWithMaterialTracks.pt()", "ctfWithMaterialTracks.pt()<100", "box");
 
 // plot Delta_phi for positive and negative tracks
-TH1F dpplus ("dpplus",  "#Delta#phi", 200,-0.25, 0.25 );
-TH1F dpminus("dpminus", "#Delta#phi", 200,-0.25, 0.25 );
-Events->Project( "dpplus", "tracks.phi()-trackExtras.outerPhi()", "tracks.charge()>0" );
-Events->Project( "dpminus", "tracks.phi()-trackExtras.outerPhi()", "tracks.charge()<0" );
+TH1F dpplus ("dpplus",  "#Delta#phi", 200,-1, 1 );
+TH1F dpminus("dpminus", "#Delta#phi", 200,-1, 1 );
+Events->Project( "dpplus", "ctfWithMaterialTracks.phi()-ctfWithMaterialTrackExtras.outerPhi()", "ctfWithMaterialTracks.charge()>0" );
+Events->Project( "dpminus", "ctfWithMaterialTracks.phi()-ctfWithMaterialTrackExtras.outerPhi()", "ctfWithMaterialTracks.charge()<0" );
 dpplus.SetFillColor( kBlue );
 dpminus.SetFillColor( kRed );
 dpplus.Draw();
 dpminus.Draw("same");
 
-// plot recHits local position
-Events.Draw("recHits.data_.localPosition().x()")
+// plot rec hits local position
+Events.Draw("ctfWithMaterialRecHits.data_.localPosition().x()")
 
-// plot position of first hit
-Events.Draw( "recHits.data_[trackExtras.recHits_.refVector_.items_[0].key()].localPosition().x():recHits.data_[trackExtras.recHits_.refVector_.items_[1].key()].localPosition().x()", "trackExtras.recHitsSize()>1" );
-// should become:
-// Events.Draw("trackExtras.recHit(0).localPosition().x():trackExtras.recHit(1).localPosition().x()", "trackExtras.recHitsSize()>1");
+// plot local position of first hit
+Events.Draw( "ctfWithMaterialRecHits.data_[ctfWithMaterialTrackExtras.recHits_.refVector_.items_[0].key()].localPosition().x()", "ctfWithMaterialTrackExtras.recHitsSize()>1" );
 }
