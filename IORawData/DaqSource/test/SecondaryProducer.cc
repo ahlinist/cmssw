5c3e08e86b6e5840f7ce08cff017c8a70f6d3d33
schiefer <>
2006-10-24 15:11:50 +0000

    tag V00-03-00: change DaqBaseReader::fillRawData(..) interface and adjust DaqSource accordingly. The FED collection is now passed as a pointer reference, so the derived reader class can either allocate a collection or pass a pointer to an already existing one.

--- IORawData/DaqSource/test/SecondaryProducer.cc
+++ IORawData/DaqSource/test/SecondaryProducer.cc
@@ -16,66 +16,87 @@
 #include <DataFormats/FEDRawData/interface/FEDRawData.h>
 #include <DataFormats/FEDRawData/interface/FEDRawDataCollection.h>
 
-
-using namespace std;
-
-namespace evf {
-
-  // Constructor 
-  SecondaryProducer::SecondaryProducer(const edm::ParameterSet& ps) {
-    // make secondary input source
-    secInput_ = makeSecInput(ps);
-
-    //    produces<FEDRawDataCollection>();
-  }
-
-  // Virtual destructor needed.
-  SecondaryProducer::~SecondaryProducer() { }  
-
-
-  
-  // Functions that get called by framework every event
-  bool SecondaryProducer::fillRawData(edm::EventID& eID,
-			     edm::Timestamp& tstamp, 
-			     FEDRawDataCollection& data)
-  { 
-
-    typedef  FEDRawDataCollection TC;
-    typedef edm::Wrapper<TC> WTC;
-
-    VectorInputSource::EventPrincipalVector result;
-    secInput_->readMany(1, result);
-
-    EventPrincipal *p = &**result.begin();
-    unsigned int irun = p->id().run();
-    unsigned int ievent = p->id().event();
-    //    std::cout << "run " << p->id().run() << " event " << p->id().event() << std::endl;
-    eID = EventID(irun, ievent);
-    EDProduct const* ep = p->getByType(TypeID(typeid(TC))).wrapper();
-    assert(ep);
-    WTC const* wtp = dynamic_cast<WTC const*>(ep);
-    assert(wtp);
-    TC const* tp = wtp->product();
-    //    auto_ptr<TC> thing(new TC(*tp));
-    data = *tp;
-    return true;
-  }
-
-
-  boost::shared_ptr<VectorInputSource> SecondaryProducer::makeSecInput(ParameterSet const& ps) {
-    ParameterSet sec_input = ps.getParameter<ParameterSet>("input");
-
-    boost::shared_ptr<VectorInputSource> input_(static_cast<VectorInputSource *>
-      (VectorInputSourceFactory::get()->makeVectorInputSource(sec_input,
-      InputSourceDescription()).release()));
-    return input_;
-  }
-
-} //edm
-
-
 #include "PluginManager/ModuleDef.h"
 #include <IORawData/DaqSource/interface/DaqReaderPluginFactory.h>
+
+
+using namespace std;
+using namespace evf;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// construction/destruction
+////////////////////////////////////////////////////////////////////////////////
+
+//______________________________________________________________________________
+SecondaryProducer::SecondaryProducer(const edm::ParameterSet& ps)
+{
+  // make secondary input source
+  secInput_=makeSecInput(ps);
+  //    produces<FEDRawDataCollection>();
+}
+
+//______________________________________________________________________________
+SecondaryProducer::~SecondaryProducer()
+{
+
+}  
+
+
+////////////////////////////////////////////////////////////////////////////////
+// implementation of member functions
+////////////////////////////////////////////////////////////////////////////////
+  
+//______________________________________________________________________________
+bool SecondaryProducer::fillRawData(edm::EventID& eID,
+				    edm::Timestamp& tstamp, 
+				    FEDRawDataCollection*& data)
+{ 
+  typedef  FEDRawDataCollection TC;
+  typedef edm::Wrapper<TC> WTC;
+  
+  VectorInputSource::EventPrincipalVector result;
+  secInput_->readMany(1,result);
+  
+  EventPrincipal *p  =&**result.begin();
+  unsigned int irun  =p->id().run();
+  unsigned int ievent=p->id().event();
+  //std::cout << "run "   << p->id().run()
+  //          << " event "<< p->id().event()<<std::endl;
+  eID = EventID(irun, ievent);
+  EDProduct const* ep = p->getByType(TypeID(typeid(TC))).wrapper();
+  assert(ep);
+  WTC const* wtp = dynamic_cast<WTC const*>(ep);
+  assert(wtp);
+  TC const* tp = wtp->product();
+  //    auto_ptr<TC> thing(new TC(*tp));
+  //data = *tp;
+  
+  data=new FEDRawDataCollection(*tp);
+  
+  return true;
+}
+
+
+//______________________________________________________________________________
+boost::shared_ptr<VectorInputSource>
+SecondaryProducer::makeSecInput(ParameterSet const& ps)
+{
+  ParameterSet sec_input = ps.getParameter<ParameterSet>("input");
+  
+  boost::shared_ptr<VectorInputSource> 
+    input_(static_cast<VectorInputSource*>
+	   (VectorInputSourceFactory::get()
+	    ->makeVectorInputSource(sec_input,
+				    InputSourceDescription()).release()));
+  return input_;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SEAL Framework Macros
+////////////////////////////////////////////////////////////////////////////////
+
 DEFINE_SEAL_MODULE();
 typedef evf::SecondaryProducer RawDataPlayback;
 DEFINE_SEAL_PLUGIN(DaqReaderPluginFactory, RawDataPlayback, "RawDataPlayback");
