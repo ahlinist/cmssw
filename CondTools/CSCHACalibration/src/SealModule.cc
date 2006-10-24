4bf146f3451bedac5d872c3309156fd9e31b50cd
xiezhen <>
2006-10-24 08:19:33 +0000

    add;

--- CondTools/CSCHACalibration/src/SealModule.cc
+++ CondTools/CSCHACalibration/src/SealModule.cc
@@ -4,7 +4,7 @@
 #include "CondTools/CSCHACalibration/src/PXsensorsDataConverter.h"
 #include "CondTools/CSCHACalibration/src/InclinometersDataConverter.h"
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(CSCZSensorsDataConverter)
-DEFINE_ANOTHER_FWK_MODULE(CSCRSensorsDataConverter)
-DEFINE_ANOTHER_FWK_MODULE(PXsensorsDataConverter)
-DEFINE_ANOTHER_FWK_MODULE(InclinometersDataConverter)
+DEFINE_ANOTHER_FWK_MODULE(CSCZSensorsDataConverter);
+DEFINE_ANOTHER_FWK_MODULE(CSCRSensorsDataConverter);
+DEFINE_ANOTHER_FWK_MODULE(PXsensorsDataConverter);
+DEFINE_ANOTHER_FWK_MODULE(InclinometersDataConverter);
