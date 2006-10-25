86c41e079abd1a09287785fd833805362296c96e
biallass <>
2006-10-25 10:09:37 +0000

    added ; to DEFINE_*

--- IOMC/CosmicMuonGenerator/src/SealModule.cc
+++ IOMC/CosmicMuonGenerator/src/SealModule.cc
@@ -3,6 +3,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOMC/CosmicMuonGenerator/interface/CosMuoGenSource.h"
 
-  DEFINE_SEAL_MODULE();
-  using edm::CosMuoGenSource;
-  DEFINE_ANOTHER_FWK_INPUT_SOURCE(CosMuoGenSource)
+DEFINE_SEAL_MODULE();
+using edm::CosMuoGenSource;
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(CosMuoGenSource);
