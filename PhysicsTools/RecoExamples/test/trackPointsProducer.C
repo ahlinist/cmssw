c7fa85036c81e4443db588dbe8f8fd42c61a5441
llista <>
2006-10-31 16:05:39 +0000

    - removed extrapolator example (too difficoult to maintain) - added simpler example module

---
+++ PhysicsTools/RecoExamples/test/trackPointsProducer.C
@@ -0,0 +1,35 @@
+{
+TFile f( "ntp.root" );
+TCanvas canv;
+
+TH1F pt( "pt", "transverse momentum", 50, 0, 20 );
+Events.Project( "pt", "ctfWithMaterialTracks.pt()" );
+pt.Draw();
+canv.SaveAs( "pt.eps" );
+
+TH2F rVsEta( "rVsEta","radius vs #eta", 50, -2.5, 2.5, 50, 0, 120 );
+Events.Project( "rVsEta", "outer.rho():outer.eta()" );
+rVsEta.Draw( "box" );
+canv.SaveAs( "rVsEta.eps" );
+
+TH1F outerR( "outerR", "outermost hit radius", 50, 0, 120 );
+Events.Project( "outerR", "outer.rho()", "fabs(outer.eta())<1" );
+outerR.Draw();
+canv.SaveAs( "outerR.eps" );
+
+TH1F deltaR( "deltaR", "#Delta radius (outer - inner)", 50, 0, 120 );
+Events.Project( "deltaR", "outer.rho() - inner.rho()", "fabs(outer.eta())<1" );
+deltaR.Draw();
+canv.SaveAs( "deltaR.eps" );
+
+TH1F deltaPhi( "deltaPhi", "#Delta #phi (outer - inner)", 80, -1.5, 1.5 );
+Events.Project( "deltaPhi", "outer.phi() - inner.phi()", "fabs(outer.eta())<1" );
+deltaPhi.Draw();
+canv.SaveAs( "deltaPhi.eps" );
+
+TH2F deltaPhiVsDeltaR( "deltaPhiVsDeltaR", "#Delta #phi vs #Delta r", 70, 0, 120, 40, -1.5, 1.5 );
+Events.Project( "deltaPhiVsDeltaR", "outer.phi()-inner.phi():outer.rho()-inner.rho()", "fabs(outer.eta())<1" );
+deltaPhiVsDeltaR.Draw( "box" );
+canv.SaveAs( "deltaPhiVsDeltaR.eps" );
+
+}
