9595ec8a55a7fcd6030aa0da82df65fcce87a3ff
wmtan <>
2006-11-04 18:56:23 +0000

    Add BranchType member to DataViewImpl

--- Framework/interface/Event.h
+++ Framework/interface/Event.h
@@ -45,22 +45,6 @@ namespace edm {
     EventID id() const {return aux_.id();}
     Timestamp time() const {return aux_.time();}
 
-    ///Put a new product where the product is gotten using a 'product instance name'
-    template <typename PROD>
-    OrphanHandle<PROD>
-    put(std::auto_ptr<PROD> product, std::string const& productInstanceName = std::string()) {
-      return put_<PROD>(InEvent, product, productInstanceName);
-    }
-
-    ///Returns a RefProd to a product before that product has been placed into the DataViewImpl
-    /// The RefProd (and any Ref's made from it) will no work properly until after the
-    /// DataViewImpl has been committed (which happens after leaving the EDProducer::produce method)
-    template <typename PROD>
-    RefProd<PROD>
-    getRefBeforePut(std::string const& productInstanceName = std::string()) {
-      return getRefBeforePut_<PROD>(InEvent, productInstanceName);
-    }
-
     using DataViewImpl::get;
     using DataViewImpl::getAllProvenance;
     using DataViewImpl::getByLabel;
@@ -68,6 +52,8 @@ namespace edm {
     using DataViewImpl::getMany;
     using DataViewImpl::getManyByType;
     using DataViewImpl::getProvenance;
+    using DataViewImpl::getRefBeforePut;
+    using DataViewImpl::put;
 
   private:
     // commit_() is called to complete the transaction represented by
