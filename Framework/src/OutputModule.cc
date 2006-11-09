35b0698ef5947578e2f6ff475442313d285468a4
paterno <>
2006-11-09 21:49:58 +0000

    Adding protection against misconfigured module descriptions.

--- Framework/src/OutputModule.cc
+++ Framework/src/OutputModule.cc
@@ -131,7 +131,7 @@ namespace edm
     detail::CPCSentry sentry(current_context_, c);
     //Save the current Mod Desc
     current_md_ = &md;
-
+    assert (current_md_ == c->moduleDescription());
     FDEBUG(2) << "writeEvent called\n";
     if(eventSelector_.wantAll() || wantEvent(ep)) {
          write(ep);
@@ -165,7 +165,7 @@ namespace edm
     detail::CPCSentry sentry(current_context_, c);
     //Save the current Mod Desc
     current_md_ = &md;
-
+    assert (current_md_ == c->moduleDescription());
     FDEBUG(2) << "beginRun called\n";
     beginRun(rp);
   }
@@ -177,7 +177,7 @@ namespace edm
     detail::CPCSentry sentry(current_context_, c);
     //Save the current Mod Desc
     current_md_ = &md;
-
+    assert (current_md_ == c->moduleDescription());
     FDEBUG(2) << "endRun called\n";
     endRun(rp);
   }
@@ -189,7 +189,7 @@ namespace edm
     detail::CPCSentry sentry(current_context_, c);
     //Save the current Mod Desc
     current_md_ = &md;
-
+    assert (current_md_ == c->moduleDescription());
     FDEBUG(2) << "beginLuminosityBlock called\n";
     beginLuminosityBlock(lbp);
   }
@@ -201,7 +201,7 @@ namespace edm
     detail::CPCSentry sentry(current_context_, c);
     //Save the current Mod Desc
     current_md_ = &md;
-
+    assert (current_md_ == c->moduleDescription());
     FDEBUG(2) << "endLuminosityBlock called\n";
     endLuminosityBlock(lbp);
   }
