ed6a91347b251acfabf14ba910bf492ffe041129
paterno <>
2006-11-15 23:11:49 +0000

    Fix error in test

--- Framework/test/eventprincipal_t.cppunit.cc
+++ Framework/test/eventprincipal_t.cppunit.cc
@@ -287,10 +287,18 @@ void testeventprincipal::getbySelectorTest()
   // something, because I don't now have the time to form a proper
   // ModuleDescription and to get its ID. This only makes sure that
   // the class and its functions are instantiable.
-  edm::ModuleDescription md;
-  edm::ModuleDescriptionID mdid = md.id();
-  edm::ModuleDescriptionSelector mdsel(mdid);
-  h = pEvent_->getBySelector(tid, mdsel);
+  try
+    {
+      edm::ModuleDescription md;
+      edm::ModuleDescriptionID mdid = md.id();
+      edm::ModuleDescriptionSelector mdsel(mdid);
+      h = pEvent_->getBySelector(tid, mdsel);
+      assert("Failed to throw required exception!" == 0);
+    }
+  catch( edm::Exception& x )
+    {
+      // This is expected
+    }
 }
 
 void testeventprincipal::processNameSelectorTest()
