9525f4a3ac1ba369dea1b6d14f0e34099b2d7d8e
tboccali <>
2006-11-17 14:59:55 +0000

    slc4 fixes

--- RecoBTag/MCTools/test/BtagMCTest.cc
+++ RecoBTag/MCTools/test/BtagMCTest.cc
@@ -85,4 +85,4 @@ void BtagMCTest::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
 
 //define this as a plug-in
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(BtagMCTest)
+DEFINE_ANOTHER_FWK_MODULE(BtagMCTest);
