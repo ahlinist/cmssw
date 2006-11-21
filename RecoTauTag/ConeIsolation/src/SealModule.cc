5e7702b61903801b76701eac12fceb7ec4a6f54e
gennai <>
2006-11-21 12:07:41 +0000

    *** empty log message ***

--- RecoTauTag/ConeIsolation/src/SealModule.cc
+++ RecoTauTag/ConeIsolation/src/SealModule.cc
@@ -1,11 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/ConeIsolation/interface/ConeIsolation.h"
-#include "RecoTauTag/ConeIsolation/interface/TauTagTest.h"
 
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ConeIsolation);
-DEFINE_ANOTHER_FWK_MODULE(TauTagTest);
 
