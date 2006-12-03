c40069e7ea638d9e65e6833f335d27c61835d618
delre <>
2006-12-03 19:08:47 +0000

    add stuff to perform pi0 analysis in H2

---
+++ RecoTBCalo/EcalTBH2SimpleAnalysis/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoTBCalo/EcalTBH2SimpleAnalysis/interface/EcalTBH2SimpleAnalyzer.h"
+
+DEFINE_SEAL_MODULE();
+
+DEFINE_ANOTHER_FWK_MODULE( EcalTBH2SimpleAnalyzer );
