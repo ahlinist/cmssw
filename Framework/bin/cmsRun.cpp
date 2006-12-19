6be0e6ea26aee97d77aaaccfb203eea9d671fe3d
wmtan <>
2006-12-19 00:39:17 +0000

    Make EnableFloatingPointExceptions a default service.

--- Framework/bin/cmsRun.cpp
+++ Framework/bin/cmsRun.cpp
@@ -95,10 +95,11 @@ int main(int argc, char* argv[])
   // 
   // The parameters for these can be overridden from the configuration files.
   std::vector<std::string> defaultServices;
-  defaultServices.reserve(4);
+  defaultServices.reserve(5);
   defaultServices.push_back("AdaptorConfig");
   defaultServices.push_back("InitRootHandlers");
   defaultServices.push_back("MessageLogger");
+  defaultServices.push_back("EnableFloatingPointExceptions");
   defaultServices.push_back("LoadAllDictionaries");
   
   // These cannot be overridden from the configuration files.
