9482dda48baac22c28c385e8f2a2a737465f4fb7
wmtan <>
2006-12-20 00:23:40 +0000

    Fix missing new-line at end of file

--- RootAutoLibraryLoader/src/stdNamespaceAdder.cc
+++ RootAutoLibraryLoader/src/stdNamespaceAdder.cc
@@ -22,15 +22,15 @@
 // constants, enums and typedefs
 //
 namespace edm {
-namespace root {
-   std::string stdNamespaceAdder(const std::string& iClassName)
-   {
+  namespace root {
+    std::string stdNamespaceAdder(const std::string& iClassName)
+    {
       //adds the std:: prefix to vector, string, map, list or deque if it is not
       // already there
       static const boost::regex e("(^|[^[:alnum:]_:])((?:vector)|(?:string)|(?:map)|(?:list)|(?:deque))");
       const std::string format("\\1std::\\2");
       
       return regex_replace(iClassName, e, format, boost::match_default | boost::format_sed);
-   }
-}
+    }
+  }
 }
