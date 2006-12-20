140231f350e3e2db7ed0d4bcd1ed0526871f9748
ric <>
2006-12-20 14:22:54 +0000

    First Version

---
+++ RecoTBCalo/EcalTBH2LocalReadout/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoTBCalo/EcalTBH2LocalReadout/interface/EcalTBH2LocalReadout.h"
+
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE( EcalTBH2LocalReadout );
