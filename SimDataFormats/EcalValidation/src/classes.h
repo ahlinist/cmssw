ffabba204e500754614c1af9dbcc983638cf5fba
wmtan <>
2007-01-04 04:32:54 +0000

    CLHEP data dictionaries should not be defined here

--- SimDataFormats/EcalValidation/src/classes.h
+++ SimDataFormats/EcalValidation/src/classes.h
@@ -1,11 +1,9 @@
 #include "SimDataFormats/EcalValidation/interface/PEcalValidInfo.h"
 #include "DataFormats/Common/interface/Wrapper.h"
-#include <CLHEP/Vector/LorentzVector.h>
 #include <vector>
 namespace {
   namespace {
     std::vector<float>  dumm1;
-    CLHEP::HepLorentzVector dummy1;
     edm::Wrapper<PEcalValidInfo> theValidData;
   }
 }
