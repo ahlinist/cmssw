563596931147c728dcaf99a55071efa4fd05842a
strang <>
2007-01-16 22:46:59 +0000

    *** empty log message ***

---
+++ SimDataFormats/GlobalRecHitValidation/src/classes.h
@@ -0,0 +1,10 @@
+#include "SimDataFormats/GlobalRecHitValidation/interface/PGlobalRecHit.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    std::vector<PGlobalRecHit::ECalRecHit> dummy1;
+    edm::Wrapper<PGlobalRecHit> theValidData1;
+  }
+}
