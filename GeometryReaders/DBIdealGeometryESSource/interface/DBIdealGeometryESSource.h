66ea7e2bce091775523cea91a9fef7fd235f2295
case <>
2007-01-18 17:27:27 +0000

    Fixes for split of compact view for magnetic field to IdealMagneticFieldRecord

--- GeometryReaders/DBIdealGeometryESSource/interface/DBIdealGeometryESSource.h
+++ GeometryReaders/DBIdealGeometryESSource/interface/DBIdealGeometryESSource.h
@@ -7,6 +7,7 @@
 
 #include "DetectorDescription/Core/interface/DDCompactView.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 #include <string>
 
@@ -16,7 +17,9 @@ class DBIdealGeometryESSource : public edm::ESProducer,
 public:
     DBIdealGeometryESSource(const edm::ParameterSet & pset);
     virtual ~DBIdealGeometryESSource(); 
-    std::auto_ptr<DDCompactView> produce(const IdealGeometryRecord &);
+    std::auto_ptr<DDCompactView> produceGeom(const IdealGeometryRecord &);
+    std::auto_ptr<DDCompactView> produceMagField(const IdealMagneticFieldRecord &);
+    std::auto_ptr<DDCompactView> produce();
 protected:
     virtual void setIntervalFor(const edm::eventsetup::EventSetupRecordKey &,
 				const edm::IOVSyncValue &, edm::ValidityInterval &);
