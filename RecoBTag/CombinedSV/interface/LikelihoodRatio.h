22a2a410406c47c6140c0a7bb8f9396b69425b35
walten <>
2007-01-22 08:51:18 +0000

    handle algorithmcalibration memory managerment prob

--- RecoBTag/CombinedSV/interface/LikelihoodRatio.h
+++ RecoBTag/CombinedSV/interface/LikelihoodRatio.h
@@ -11,9 +11,12 @@ class LikelihoodRatio : public AbstractDiscriminatorComputer {
 public:
   /**
    * \paramname tp  The likelihood ratio is specific to a certain vertex category
+   * \paramname calib FIXME pointer semantics because of ownership problem 
+   * with AlgorithmCalibration. We do not wish to copy an AlgorithmCalibration object.
    */
-  LikelihoodRatio ( const BTagAlgorithmCalibration & calib,
+  LikelihoodRatio ( const BTagAlgorithmCalibration * calib,
        reco::btag::Vertices::VertexType tp, double priorCharmInBG=0.25 );
+  ~LikelihoodRatio();
 
   double compute ( const reco::TaggingVariableList & ) const;
 
@@ -25,7 +28,7 @@ private:
   long double getPDFValue( double value, const combsv::CombinedSVCalibInput & ) const;
 
 private:
-  BTagAlgorithmCalibration bTagCalibration_;
+  const BTagAlgorithmCalibration * bTagCalibration_;
   double priorCharmInBG_;
   double priorUDSG_;
   reco::btag::Vertices::VertexType vtxtype_;
