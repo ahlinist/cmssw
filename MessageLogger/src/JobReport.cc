e5bc9fcd69dbd1aabe61fad64d6f8d1b56299d96
elmer <>
2007-01-29 02:15:38 +0000

    Restore HEAD version to combination of fix for generator info formatting and most recent changes.

--- MessageLogger/src/JobReport.cc
+++ MessageLogger/src/JobReport.cc
@@ -269,11 +269,11 @@ namespace edm
     }
   
     void
-    JobReport::eventReadFromFile(JobReport::Token fileToken, edm::EventID const& id)
+    JobReport::eventReadFromFile(JobReport::Token fileToken, unsigned int run, unsigned int)
     {
       JobReport::InputFile& f = impl_->getInputFileForToken(fileToken);
       f.numEventsRead++;
-      f.runsSeen.insert(id.run());
+      f.runsSeen.insert(run);
     }
 
     void
@@ -336,11 +336,11 @@ namespace edm
     }
 
     void
-    JobReport::eventWrittenToFile(JobReport::Token fileToken, edm::EventID const& id)
+    JobReport::eventWrittenToFile(JobReport::Token fileToken, unsigned int run, unsigned int)
     {
       JobReport::OutputFile& f = impl_->getOutputFileForToken(fileToken);
       f.numEventsWritten++;
-      f.runsSeen.insert(id.run());
+      f.runsSeen.insert(run);
     }
 
 
@@ -388,11 +388,11 @@ namespace edm
     }
 
     void 
-    JobReport::reportSkippedEvent(edm::EventID const& id)
+    JobReport::reportSkippedEvent(unsigned int run, unsigned int event)
     {
       std::ostringstream msg;
-      msg << "<SkippedEvent Run=\"" << id.run() << "\"";
-      msg << " Event=\"" << id.event() << "\" />\n";
+      msg << "<SkippedEvent Run=\"" << run << "\"";
+      msg << " Event=\"" << event << "\" />\n";
       LogInfo("FwkJob") << msg.str();
     }
 
