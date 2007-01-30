02d9804b9d326d60978137c6155688b3f26de7b7
arizzi <>
2007-01-30 16:12:40 +0000

    update

--- RecoBTau/TrackTools/src/TrackIPProducer.cc
+++ RecoBTau/TrackTools/src/TrackIPProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Andrea Rizzi
 //         Created:  Thu Apr  6 09:56:23 CEST 2006
-// $Id: TrackIPProducer.cc,v 1.20 2006/10/27 01:35:35 wmtan Exp $
+// $Id: TrackIPProducer.cc,v 1.1 2007/01/25 09:12:42 arizzi Exp $
 //
 //
 
@@ -47,6 +47,7 @@
 #include "RecoBTau/TrackTools/interface/SignedDecayLength3D.h"
 
 //#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include <iostream>
 
 using namespace std;
 using namespace reco;
@@ -112,16 +113,19 @@ TrackIPProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    }
    
    double pvZ=pv->z();
-   
+   SignedImpactParameter3D  sip3D;  
+   SignedTransverseImpactParameter sip2D;
+ 
    int i=0;
    JetTracksAssociationCollection::const_iterator it = jetTracksAssociation->begin();
-   for(; it != jetTracksAssociation->end(); it++)
+   for(; it != jetTracksAssociation->end(); it++, i++)
      {
 //      int i=it->key.key();
-        i++;    
+        int ii=it->key.key();  
         GlobalVector direction(it->key->px(),it->key->py(),it->key->pz());
 
         TracksInJetData data;
+        data.direction=direction; 
         TrackRefVector tracks = it->val;
         for (TrackRefVector::const_iterator itTrack = tracks.begin(); itTrack != tracks.end(); ++itTrack) {
              const Track & track = **itTrack;
@@ -130,19 +134,19 @@ TrackIPProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
              float dLen = SignedDecayLength3D::apply(*transientTrack,direction,*pv).second.value();
 
         TrackIPData ipData;
-        ipData.impactParameter3D=0.;
-        ipData.impactParameter3DError=1.;
-        ipData.impactParameter2D=0.;
-        ipData.impactParameter2DError=1.;
+        ipData.impactParameter3D=sip3D.apply(*transientTrack,direction,*pv).second.significance();
+        ipData.impactParameter2D=sip2D.apply(*transientTrack,direction,*pv).second.significance();
         data.ipData.push_back(ipData);
-
+         cout <<i << " " << ii << " IP: " <<   ipData.impactParameter3D.value() << endl; 
         }
-
-        outCollection->insert(edm::Ref<JetTracksAssociationCollection>(jetTracksAssociation, i), data); 
+       cout << "insert"  << endl;
+       outCollection->insert(edm::Ref<JetTracksAssociationCollection>(jetTracksAssociation, ii), data); 
+       cout << "done"  << endl;
      }
-  
+       cout << "put..."  << endl;
    std::auto_ptr<reco::JetTracksIPDataAssociationCollection> result(outCollection);
    iEvent.put(result);
+   cout << "done"  << endl;
  
    if(!pvFound) delete pv; //dummy pv deleted
 }
