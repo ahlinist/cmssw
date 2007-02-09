d597b6257952769af516611c4756c0a3b25e5a2b
dfortin <>
2007-02-09 22:03:48 +0000

    Added skeleton code

---
+++ AnalysisDataFormats/HiggsTo4Leptons/src/classes.h
@@ -0,0 +1,14 @@
+#include <AnalysisDataFormats/HiggsTo4Leptons/interface/HiggsTo4LeptonsMCObjects.h>
+#include <AnalysisDataFormats/HiggsTo4Leptons/interface/HiggsTo4LeptonsMCObjectsCollection.h>
+
+#include <DataFormats/Common/interface/Wrapper.h>
+#include <vector>
+
+namespace{
+  namespace {
+    HiggsTo4LeptonsMCObjectsCollection dummy1;
+    edm::Wrapper<HiggsTo4LeptonsMCObjectsCollection> dummy2;
+  }
+}
+
+
