974d610dc65e7e2245a2684c1cda2d3f47199e40
biery <>
2007-02-12 16:24:57 +0000

    Added DQMSenderService

--- EventFilter/FUSender/src/SealModule.cc
+++ EventFilter/FUSender/src/SealModule.cc
@@ -3,11 +3,16 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOPool/Streamer/interface/StreamerOutputModule.h"
 #include "EventFilter/FUSender/src/FUStreamerI2OWriter.h"
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "EventFilter/FUSender/interface/DQMSenderService.h"
 
 typedef edm::StreamerOutputModule<edm::FUStreamerI2OWriter> I2OStreamConsumer;
 
 using edm::FUStreamerI2OWriter;
+using namespace edm::serviceregistry;
+
+typedef AllArgsMaker<DQMSenderService> dssMaker;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(I2OStreamConsumer);
-
+DEFINE_ANOTHER_FWK_SERVICE_MAKER(DQMSenderService,dssMaker);
