0478a91bad853e125a4d3cb47f015047cdf64fa2
mansj <>
2007-03-07 19:54:51 +0000

    Move headers into src

---
+++ RecoLocalCalo/CaloTowersCreator/src/CaloTowersCreator.h
@@ -0,0 +1,34 @@
+#ifndef RECOLOCALCALO_CALOTOWERSCREATOR_CALOTOWERSCREATOR_H
+#define RECOLOCALCALO_CALOTOWERSCREATOR_CALOTOWERSCREATOR_H 1
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "Geometry/CaloTopology/interface/HcalTopology.h"
+
+#include "RecoLocalCalo/CaloTowersCreator/interface/CaloTowersCreationAlgo.h"
+
+/** \class CaloTowersCreator
+  *  
+  * $Date: 2007/03/07 19:43:13 $
+  * $Revision: 1.7 $
+  * \author J. Mans - Minnesota
+  */
+class CaloTowersCreator : public edm::EDProducer {
+public:
+  explicit CaloTowersCreator(const edm::ParameterSet& ps);
+  virtual ~CaloTowersCreator() { }
+  virtual void produce(edm::Event& e, const edm::EventSetup& c);
+private:
+  CaloTowersCreationAlgo algo_;
+  edm::InputTag hbheLabel_,hoLabel_,hfLabel_;
+  std::vector<edm::InputTag> ecalLabels_;
+  bool allowMissingInputs_;
+};
+
+#endif
