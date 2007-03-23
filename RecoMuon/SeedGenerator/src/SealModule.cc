e9f9963b3f8f1ec59bc240e5ec9a059bb5c6216d
dfortin <>
2007-03-23 23:40:00 +0000

    skeleton code

---
+++ RecoMuon/SeedGenerator/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoMuon/MuonSeedGenerator/src/MuonSeedProducer.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MuonSeedProducer);
+
