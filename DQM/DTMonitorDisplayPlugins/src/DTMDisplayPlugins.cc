7a4666c202151eec729266cce0eb80fe924a11a2
gmasetti <>
2007-04-03 09:27:53 +0000

    split of tasks

--- DQM/DTMonitorDisplayPlugins/src/DTMDisplayPlugins.cc
+++ DQM/DTMonitorDisplayPlugins/src/DTMDisplayPlugins.cc
@@ -1,11 +1,11 @@
-// $Id: DTMDisplayPlugins.cc,v 1.1 2007/03/29 15:24:00 $
+// $Id: DTMDisplayPlugins.cc,v 1.1 2007/03/29 09:10:24 gmasetti Exp $
 
 /*!
   \file DTMDisplayPlugins
   \brief Display Plugin for Quality Histograms (2D)
   \author G. Masetti 
   \version $Revision: 1.1 $
-  \date $Date: 2007/03/29 15:24:00 $
+  \date $Date: 2007/03/29 09:10:24 $
 */
 
 #include "DQM/DTMonitorDisplayPlugins/interface/DTMDisplayPlugins.h"
@@ -32,152 +32,75 @@ DTMDisplayPlugins::DTMDisplayPlugins( IgState *state ) : VisDQMDisplayPlugin( st
 
 bool DTMDisplayPlugins::applies( DisplayData *data ) {
  
-   name = (data->object)->GetName();
+  name = (data->object)->GetName();
+  
+  if (DigiME.isDigiME(name)) {
+    return true;
+  }
 
-   if( name.find( "Occupancy" ) == 0 ) {
-     return true;
-   }
-   if( name.find( "TimeBoxes" ) == 0 ) {
-     return true;
-   }
+  if (DataIntegrityME.isDataIntegrityME(name)) {
+    return true;
+  }
 
-   if( name.find( "FED770" ) == 0 ) {
-     return true;
-   }
+  if (tTrigTestME.istTrigTestME(name)) {
+    return true;
+  }
 
-   if( name.find( "tTrigTest" ) == 0 ) {
-     return true;
-   }
-
-
-   return false;
-
+  return false;
 
 }
 
 std::string DTMDisplayPlugins::preDraw( DisplayData *data ) {
 
-  if( dynamic_cast<TH2F*>( data->object ) ) {
-    return preDrawTH2F( data );
-  }
+  name = (data->object)->GetName();
   
-  if( dynamic_cast<TH1F*>( data->object ) ) {
-    return preDrawTH1F( data );
-  }
   
+  if (DigiME.isDigiME(name)) {
+    
+    return DigiME.preDraw(data);
+    
+  }
+
+  if (DataIntegrityME.isDataIntegrityME(name)) {
+    
+    return DataIntegrityME.preDraw(data);
+    
+  }
+ 
+  if (tTrigTestME.istTrigTestME(name)) {
+    
+    return tTrigTestME.preDraw(data);
+    
+  }
+
   return "";
   
 }
 
 
-std::string DTMDisplayPlugins::preDrawTH2F( DisplayData *data ) {
-
-  TH2F* obj = dynamic_cast<TH2F*>( data->object );
-
-  name = (data->object)->GetName();
-
-  if( obj ) {
-
-    // This applies to all
-    gStyle->SetCanvasBorderMode( 0 );
-    gStyle->SetPadBorderMode( 0 );
-    gStyle->SetPadBorderSize( 0 );
-    //    (data->pad)->SetLogy( 0 );
-    gStyle->SetOptStat( 0 );
-    obj->SetStats( kFALSE );
-
-    obj->SetOption( "box" );
-
-  }
-
-  return "";    
-
-}
-
-
-std::string DTMDisplayPlugins::preDrawTH1F( DisplayData *data ) {
-
-  TH1F* obj = dynamic_cast<TH1F*>( data->object );
-
-  name = (data->object)->GetName();
-
-  if( obj ) {
-
-      //     // This applies to all
-      gStyle->SetCanvasBorderMode( 0 );
-      gStyle->SetPadBorderMode( 0 );
-      gStyle->SetPadBorderSize( 0 );
-      //      (data->pad)->SetLogy( 1 );
-      gStyle->SetOptStat( 0 );
-      obj->SetStats( kFALSE );
-      
-    if ( name.find( "FED770_DDUChannel" ) == 0 ) {
-
-//       //     // This applies to all
-//       gStyle->SetCanvasBorderMode( 0 );
-//       gStyle->SetPadBorderMode( 0 );
-//       gStyle->SetPadBorderSize( 0 );
-      (data->pad)->SetLogy( 1 );
-//       gStyle->SetOptStat( 0 );
-//       obj->SetStats( kFALSE );
-      
-      //     obj->SetOption( "box" );
-      
-      obj->GetXaxis()->SetBinLabel(1,"ERR1");
-      obj->GetXaxis()->SetBinLabel(2,"ERR2");
-      obj->GetXaxis()->SetBinLabel(3,"ERR3");
-      obj->GetXaxis()->SetBinLabel(4,"ERR4");
-      obj->GetXaxis()->SetBinLabel(5,"ERR5");
-      obj->GetXaxis()->SetBinLabel(6,"ERR6");
-      obj->GetXaxis()->SetBinLabel(7,"ERR7");
-      obj->GetXaxis()->SetBinLabel(8,"ERR8");
-
-    }
-
-    if ( name.find( "tTrigTest" ) == 0 ) {
-      
-      TAttLine *line = dynamic_cast<TAttLine *> (data->object);
-
-      obj->GetXaxis()->SetBinLabel(1,"SL1");
-      obj->GetXaxis()->SetBinLabel(2,"SL2");
-      obj->GetXaxis()->SetBinLabel(3,"SL3");
-
-      if (line) {
-
-	MonitorElement* me = data->me;
-
-	if (me->hasError()) {
-	  line->SetLineColor(TColor::GetColor("#CC0000"));
-	  //	  std::cout << name << " has error" << std::endl;
-	}
-	else if (me->hasWarning()) {
-	  line->SetLineColor(TColor::GetColor("#993300"));
-	  //	  std::cout << name << " has worning" << std::endl;
-	}
-	else if (me->hasOtherReport()) { 
-	  line->SetLineColor(TColor::GetColor("#FFCC00"));
-	  //	  std::cout << name << " has other report" << std::endl;
-	}
-	else {
-	  line->SetLineColor(TColor::GetColor("#000000"));
-	  //	  std::cout << name << " has nothing" << std::endl;
-
-	}
-      }
-
-    }
-
-  }
-
-  return "";    
-
-}
-
-
-
 void DTMDisplayPlugins::postDraw( DisplayData *data ) {
 
-    return;    
+  name = (data->object)->GetName();
+    
+  if (DigiME.isDigiME(name)) {
+    
+    return DigiME.postDraw(data);
+    
+  }
+
+  if (DataIntegrityME.isDataIntegrityME(name)) {
+    
+    return DataIntegrityME.postDraw(data);
+    
+  }
+
+  if (tTrigTestME.istTrigTestME(name)) {
+    
+    return tTrigTestME.postDraw(data);
+    
+  }
+
+  return ;    
 
 }
 
