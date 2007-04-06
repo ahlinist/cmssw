909515bd899bb86cdb600c2780be990a7c4957e7
dkcira <>
2007-04-06 23:32:33 +0000

    avoid using directive as much as possible

--- GeneratorInterface/MadGraphInterface/src/HepMGProductAnalyzer.cc
+++ GeneratorInterface/MadGraphInterface/src/HepMGProductAnalyzer.cc
@@ -2,22 +2,12 @@
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include <iostream>
 
-HepMGProductAnalyzer::HepMGProductAnalyzer(const edm::ParameterSet& iConfig) :
-label_(iConfig.getUntrackedParameter("moduleLabel",std::string("source")))
-{
+HepMGProductAnalyzer::HepMGProductAnalyzer(const edm::ParameterSet& iConfig) : label_(iConfig.getUntrackedParameter("moduleLabel",std::string("source"))) {}
 
-}
+HepMGProductAnalyzer::~HepMGProductAnalyzer() {}
 
-
-HepMGProductAnalyzer::~HepMGProductAnalyzer()
-{
-}
-
-void HepMGProductAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
-   using namespace edm;
-
-   Handle<HepMCProduct> evt;
+void HepMGProductAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+   edm::Handle<edm::HepMCProduct> evt;
    iEvent.getByType(evt);
 
    //Print out the HepMCProduct
@@ -26,11 +16,9 @@ void HepMGProductAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
    //To print out status code, pdg, mass, etc table uncomment below
    //   HepMC::GenEvent * myGenEvent = new  HepMC::GenEvent(*(evt->GetEvent()));
    //   this->printTable(myGenEvent);
-
 }
 
-void HepMGProductAnalyzer::printTable(HepMC::GenEvent * myGenEvent)
-{
+void HepMGProductAnalyzer::printTable(HepMC::GenEvent * myGenEvent) {
 // this method is outdated, use print() method of GenEvent instead
   std::cout<<std::endl;
   std::cout<<"Barcode:  Pgd:     Status:  Mother:"<<std::endl;   
