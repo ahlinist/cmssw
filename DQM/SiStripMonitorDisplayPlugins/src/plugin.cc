3a2521ab682d28199a5dffe7551aa91b1d644c7b
dutta <>
2007-04-10 07:27:59 +0000

    changes due to plugin migration

--- DQM/SiStripMonitorDisplayPlugins/src/plugin.cc
+++ DQM/SiStripMonitorDisplayPlugins/src/plugin.cc
@@ -2,7 +2,7 @@
 
 #include "DQM/SiStripMonitorDisplayPlugins/interface/TrackerMapDisplayPlugins.h"
 #include "VisMonitoring/VisDQMBase/interface/VisDQMDisplayPluginsDB.h"
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 
 DEFINE_SEAL_MODULE ();
 DEFINE_SEAL_PLUGIN (VisDQMDisplayPluginsDB, TrackerMapDisplayPlugins, TrackerMapDisplayPlugins::catalogLabel ());
