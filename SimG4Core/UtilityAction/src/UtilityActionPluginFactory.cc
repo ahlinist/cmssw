50b6b81adad99ded6dfb1e1919b47cfc8d5c09a9
chrjones <>
2007-04-11 02:41:23 +0000

    updated to new plugin manager

--- SimG4Core/UtilityAction/src/UtilityActionPluginFactory.cc
+++ SimG4Core/UtilityAction/src/UtilityActionPluginFactory.cc
@@ -1,11 +1,3 @@
 #include "SimG4Core/UtilityAction/interface/UtilityActionPluginFactory.h"
  
-UtilityActionPluginFactory UtilityActionPluginFactory::s_instance;
- 
-UtilityActionPluginFactory::UtilityActionPluginFactory () :
-    seal::PluginFactory<UtilityActionMakerBase * ()>("CMS Simulation UtilityActionPluginFactory")
-{}
-
-UtilityActionPluginFactory::~UtilityActionPluginFactory () {}
-
-UtilityActionPluginFactory  *UtilityActionPluginFactory::get () { return &s_instance; }
+EDM_REGISTER_PLUGINFACTORY(UtilityActionPluginFactory,"CMS Simulation UtilityActionPluginFactory");
