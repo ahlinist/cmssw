9ab307829ee0b862101041e336206ac9abd95add
cerati <>
2007-04-16 16:53:56 +0000

    fixes for new plugin manager

--- TrackingTools/KalmanUpdators/src/module.cc
+++ TrackingTools/KalmanUpdators/src/module.cc
@@ -1,5 +1,3 @@
-#include "TrackingTools/KalmanUpdators/interface/KFUpdatorESProducer.h"
-#include "TrackingTools/KalmanUpdators/interface/Chi2MeasurementEstimatorESProducer.h"
 #include "TrackingTools/KalmanUpdators/interface/Chi2MeasurementEstimatorBase.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -10,6 +8,4 @@
 
 EVENTSETUP_DATA_REG(TrajectoryStateUpdator);
 EVENTSETUP_DATA_REG(Chi2MeasurementEstimatorBase);
-DEFINE_FWK_EVENTSETUP_MODULE(KFUpdatorESProducer);
-DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(Chi2MeasurementEstimatorESProducer);
 
