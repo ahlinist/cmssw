210d7c7b9b13e39a3281492b97cc5c3421cc1dc1
fronga <>
2007-04-18 16:12:20 +0000

    Moving reference trajectories to new package.

---
+++ Alignment/ReferenceTrajectories/src/ReferenceTrajectoryBase.cc
@@ -0,0 +1,12 @@
+#include "Alignment/ReferenceTrajectories/interface/ReferenceTrajectoryBase.h"
+
+ReferenceTrajectoryBase::ReferenceTrajectoryBase(unsigned int nPar, unsigned int nHits)
+  : theValidityFlag(false), theTsosVec(), theRecHits(),
+    theMeasurements(nMeasPerHit * nHits), theMeasurementsCov(nMeasPerHit * nHits, 0),
+    theTrajectoryPositions(nMeasPerHit * nHits), 
+    theTrajectoryPositionCov(nMeasPerHit * nHits,0),
+    theParameters(nPar), theDerivatives(nMeasPerHit * nHits, nPar, 0) 
+{
+  theTsosVec.reserve(nHits);
+  theRecHits.reserve(nHits);
+}
