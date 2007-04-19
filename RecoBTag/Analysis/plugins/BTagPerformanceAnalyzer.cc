7738295a6b8d8d9e425d16aea6376fbb76e8e572
arizzi <>
2007-04-19 07:28:53 +0000

    Fixes for new plugin system to allow Validation dependecy

---
+++ RecoBTag/Analysis/plugins/BTagPerformanceAnalyzer.cc
@@ -0,0 +1,45 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfoFwd.h"
+#include "DataFormats/BTauReco/interface/SoftLeptonTagInfoFwd.h"
+#include "RecoBTag/Analysis/interface/BTagPerformanceAnalyzer.h"
+#include "RecoBTag/Analysis/interface/BaseBTagPlotter.h"
+#include "RecoBTag/Analysis/interface/JetTagPlotter.h"
+#include "RecoBTag/Analysis/interface/TrackCountingTagPlotter.h"
+#include "RecoBTag/Analysis/interface/TrackProbabilityTagPlotter.h"
+#include "RecoBTag/Analysis/interface/SoftLeptonTagPlotter.h"
+#include "FWCore/Utilities/interface/CodedException.h"
+
+BTagPerformanceAnalyzer::BTagPerformanceAnalyzer(const edm::ParameterSet& pSet)
+{
+  std::string algorithm = pSet.getParameter<std::string>( "algorithm" );
+  if (algorithm == "TrackCounting") {
+    petBase = new BTagPABase<reco::TrackCountingTagInfoCollection, TrackCountingTagPlotter>(pSet);
+  } else if (algorithm == "TrackProbability") {
+    petBase = new BTagPABase<reco::TrackProbabilityTagInfoCollection, TrackProbabilityTagPlotter>(pSet);
+  } else if (algorithm == "SoftLepton") {
+    petBase = new BTagPABase<reco::SoftLeptonTagInfoCollection, SoftLeptonTagPlotter>(pSet);
+  } else {
+    throw cms::Exception("Configuration")
+      << "BTagPerformanceAnalyzer: Unknown algorithm " << algorithm << endl
+      << "Choose between JetTag, TrackCounting, TrackProbability, SoftLepton\n";
+  }
+}
+
+BTagPerformanceAnalyzer::~BTagPerformanceAnalyzer()
+{
+  delete petBase;
+}
+
+void BTagPerformanceAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  petBase->analyze(iEvent, iSetup);
+}
+
+void BTagPerformanceAnalyzer::endJob()
+{
+  petBase->endJob();
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(BTagPerformanceAnalyzer);
