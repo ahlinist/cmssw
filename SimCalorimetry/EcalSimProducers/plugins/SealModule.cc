53a1952be2ad3191ca43931e150c0dd68cc4c492
fabiocos <>
2007-04-20 16:58:04 +0000

    Adapted to the new plugin manager

---
+++ SimCalorimetry/EcalSimProducers/plugins/SealModule.cc
@@ -0,0 +1,12 @@
+/*  
+ *  \author  P. Govoni Univ Milano Bicocca - INFN Milano 
+ */
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "EcalDigiProducer.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_ANOTHER_FWK_MODULE (EcalDigiProducer);
+
