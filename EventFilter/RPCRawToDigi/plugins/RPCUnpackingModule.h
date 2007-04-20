faf1cc40a020bde9ea0b2e259c00aa178f354c65
konec <>
2007-04-20 15:41:48 +0000

    *** empty log message ***

---
+++ EventFilter/RPCRawToDigi/plugins/RPCUnpackingModule.h
@@ -0,0 +1,39 @@
+#ifndef RPCUnpackingModule_H
+#define RPCUnpackingModule_H
+
+
+/** \class RPCUnpackingModule
+ *  Driver class for unpacking RPC raw data (DCC format)
+ *
+ *  $Date: 2007/04/05 15:51:06 $
+ *  $Revision: 1.13 $
+ *  \author Ilaria Segoni - CERN
+ */
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+
+namespace edm { class Event; class EventSetup; }
+
+
+class RPCUnpackingModule: public edm::EDProducer {
+public:
+    
+    ///Constructor
+    RPCUnpackingModule(const edm::ParameterSet& pset);
+    
+    ///Destructor
+    virtual ~RPCUnpackingModule();
+ 
+   /** Retrieves a RPCDigiCollection from the Event, creates a
+      FEDRawDataCollection (EDProduct) using the DigiToRaw converter,
+      and attaches it to the Event. */
+    void produce(edm::Event & e, const edm::EventSetup& c); 
+  
+private:
+  edm::InputTag dataLabel_;
+  unsigned long eventCounter_;
+};
+
+
+#endif
