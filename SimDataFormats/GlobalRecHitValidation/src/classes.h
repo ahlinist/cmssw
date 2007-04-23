a240852d641fc35a18b73046fabeb433c12d1dd5
strang <>
2007-04-23 19:23:57 +0000

    first full version

--- SimDataFormats/GlobalRecHitValidation/src/classes.h
+++ SimDataFormats/GlobalRecHitValidation/src/classes.h
@@ -5,6 +5,12 @@
 namespace {
   namespace {
     std::vector<PGlobalRecHit::ECalRecHit> dummy1;
+    std::vector<PGlobalRecHit::HCalRecHit> dummy32;
+    std::vector<PGlobalRecHit::SiStripRecHit> dummy3;
+    std::vector<PGlobalRecHit::SiPixelRecHit> dummy4;
+    std::vector<PGlobalRecHit::DTRecHit> dummy5;
+    std::vector<PGlobalRecHit::CSCRecHit> dummy6;
+    std::vector<PGlobalRecHit::RPCRecHit> dummy7;
     edm::Wrapper<PGlobalRecHit> theValidData1;
   }
 }
