c231c879949bfdad03a5883c16558f33e108804b
bainbrid <>
2007-04-24 17:41:37 +0000

    plugin migration

---
+++ OnlineDB/SiStripESSources/plugins/module.cc
@@ -0,0 +1,12 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/SourceFactory.h"
+DEFINE_SEAL_MODULE();
+
+#include "OnlineDB/SiStripESSources/interface/SiStripFedCablingBuilderFromDb.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripFedCablingBuilderFromDb);
+
+#include "OnlineDB/SiStripESSources/interface/SiStripPedestalsBuilderFromDb.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripPedestalsBuilderFromDb);
+
+#include "OnlineDB/SiStripESSources/interface/SiStripNoiseBuilderFromDb.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripNoiseBuilderFromDb);
