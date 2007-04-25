c3d0c2373b74b3672b8826dd3863ff5b309f7c8e
bainbrid <>
2007-04-25 15:44:34 +0000

    plugin migration

---
+++ DQM/SiStripCommissioningSources/plugins/module.cc
@@ -0,0 +1,6 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+
+#include "DQM/SiStripCommissioningSources/interface/SiStripCommissioningSource.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripCommissioningSource);
+  
