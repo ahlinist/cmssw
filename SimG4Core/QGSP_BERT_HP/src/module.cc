198bd95a7f9f2390b4111ff0f13860a40324f93a
rpw <>
2007-04-25 03:07:24 +0000

    For neutron background studies

---
+++ SimG4Core/QGSP_BERT_HP/src/module.cc
@@ -0,0 +1,7 @@
+#include "SimG4Core/Physics/interface/PhysicsListFactory.h"
+#include "SimG4Core/QGSP_BERT_HP/src/QGSP_BERT_HP.hh"
+
+#include "PluginManager/ModuleDef.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_PHYSICSLIST(QGSP_BERT_HP);
