dab1bc04f203a193851979dbd9932d90854db639
wmtan <>
2007-04-26 19:47:49 +0000

    Switch from SEAL plug-in to iguana plug-in

--- DQM/SiStripMonitorDisplayPlugins/src/plugin.cc
+++ DQM/SiStripMonitorDisplayPlugins/src/plugin.cc
@@ -2,7 +2,6 @@
 
 #include "DQM/SiStripMonitorDisplayPlugins/interface/TrackerMapDisplayPlugins.h"
 #include "VisMonitoring/VisDQMBase/interface/VisDQMDisplayPluginsDB.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 
-DEFINE_SEAL_MODULE ();
-DEFINE_SEAL_PLUGIN (VisDQMDisplayPluginsDB, TrackerMapDisplayPlugins, TrackerMapDisplayPlugins::catalogLabel ());
+DEFINE_IGUANA_MODULE();
+DEFINE_IGUANA_PLUGIN (VisDQMDisplayPluginsDB, TrackerMapDisplayPlugins, TrackerMapDisplayPlugins::catalogLabel ());
