b5e6308bbc320ab8470d3561cd4f28bbe7ff78d4
igorvrb <>
2007-05-02 11:10:44 +0000

    changed run_num to runs

--- CondTools/CSC/sql/CSCGains_Online.sql
+++ CondTools/CSC/sql/CSCGains_Online.sql
@@ -3,7 +3,7 @@ REM Table GAINS
 REM
 create table GAINS (
 record_id         number NOT NULL,
-run_num           number UNIQUE NOT NULL,
+runs           number UNIQUE NOT NULL,
 data_taking_time  date DEFAULT sysdate NOT NULL,
 insertion_time    date DEFAULT sysdate NOT NULL,
 flag              number,
