8f4cf2da591a94d2a2d7149ba8f2356614acb1e8
arizzi <>
2007-05-10 22:03:45 +0000

    debug stuff removed, computer adapted

--- RecoBTag/TrackCounting/interface/TrackCountingComputer.h
+++ RecoBTag/TrackCounting/interface/TrackCountingComputer.h
@@ -1,5 +1,6 @@
 #include "RecoBTau/JetTagComputer/interface/JetTagComputer.h"
 #include "DataFormats/BTauReco/interface/TrackCountingTagInfo.h"
+#include "DataFormats/BTauReco/interface/TrackIPTagInfo.h"
 
 class TrackCountingComputer : public JetTagComputer
 {
@@ -10,12 +11,26 @@ class TrackCountingComputer : public JetTagComputer
    {
     const reco::TrackCountingTagInfo * tkti = dynamic_cast<const reco::TrackCountingTagInfo *>(&ti);
     if(tkti!=0)
-     return  tkti->discriminator(3,0);
+     return  tkti->discriminator(3,0); //FIXME: configurable
     else 
      {
-       //FIXME: exceptions
-       abort();
-      return 0;
+        const reco::TrackIPTagInfo * tkip = dynamic_cast<const reco::TrackIPTagInfo *>(&ti);
+      if(tkip!=0)  {
+          const std::vector<Measurement1D> & impactParameters((tkip->impactParameters(0))); //FIXME: configurable
+          std::multiset<float> significances;
+          for(std::vector<Measurement1D>::const_iterator it = impactParameters.begin(); it!=impactParameters.end(); ++it)
+           {
+             //FIXME:add extra cuts here
+             significances.insert(it->significance());
+           }
+          std::multiset<float>::iterator nth=significances.begin();
+          for(int i=0;i<2 && nth!=significances.end();i++) nth++;  //3rd track, FIXME:configurable
+          if(nth!=significances.end()) return *nth; else return -10.;
+        }
+        else {    //FIXME: exceptions
+                 abort();
+                return 0;
+             }
      }
    }
 };
