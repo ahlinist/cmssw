d04a417e5bede76a658fe606a0f335b06110b627
dmytro <>
2007-05-12 22:14:39 +0000

    Major reorganization of code with little new code. MuonIdProducer is creating the default muons (using global muon reconstruction) and trackerMuons(based on a silicon track and segments in Muon Chambers). "muons" for now only have energy filled, whereas "trackerMuons" have energy, matching, calo hypothesis compatibility. Isolation filling is not finished.

---
+++ RecoMuon/MuonIdentification/plugins/Module.cc
@@ -0,0 +1,10 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoMuon/MuonIdentification/plugins/MuonIdProducer.h"
+#include "RecoMuon/MuonIdentification/plugins/MuonProducer.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MuonIdProducer);
+DEFINE_ANOTHER_FWK_MODULE(MuonProducer);
