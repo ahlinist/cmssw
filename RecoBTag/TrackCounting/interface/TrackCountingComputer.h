bc2457091509bf2e9e79524777a073e36903d018
arizzi <>
2007-05-16 10:27:04 +0000

    using jet tag producer

--- RecoBTag/TrackCounting/interface/TrackCountingComputer.h
+++ RecoBTag/TrackCounting/interface/TrackCountingComputer.h
@@ -5,18 +5,22 @@
 class TrackCountingComputer : public JetTagComputer
 {
  public:
-  TrackCountingComputer(const edm::ParameterSet  & parameters ){}
+  TrackCountingComputer(const edm::ParameterSet  & parameters )
+  {
+     m_nthTrack         = parameters.getParameter<int>("nthTrack");
+     m_ipType           = parameters.getParameter<int>("impactParamterType");
+  }
  
   float discriminator(const reco::BaseTagInfo & ti) const 
    {
     const reco::TrackCountingTagInfo * tkti = dynamic_cast<const reco::TrackCountingTagInfo *>(&ti);
     if(tkti!=0)
-     return  tkti->discriminator(3,0); //FIXME: configurable
+     return  tkti->discriminator(m_nthTrack,m_ipType); 
     else 
      {
         const reco::TrackIPTagInfo * tkip = dynamic_cast<const reco::TrackIPTagInfo *>(&ti);
       if(tkip!=0)  {
-          const std::vector<Measurement1D> & impactParameters((tkip->impactParameters(0))); //FIXME: configurable
+          const std::vector<Measurement1D> & impactParameters((tkip->impactParameters(m_ipType))); 
           std::multiset<float> significances;
           for(std::vector<Measurement1D>::const_iterator it = impactParameters.begin(); it!=impactParameters.end(); ++it)
            {
@@ -24,13 +28,18 @@ class TrackCountingComputer : public JetTagComputer
              significances.insert(it->significance());
            }
           std::multiset<float>::reverse_iterator nth=significances.rbegin();
-          for(int i=0;i<2 && nth!=significances.rend();i++) nth++;  //3rd track, FIXME:configurable
-          if(nth!=significances.rend()) return *nth; else return -10.;
+          for(int i=0;i<m_nthTrack-1 && nth!=significances.rend();i++) nth++;  
+          if(nth!=significances.rend()) return *nth; else return -100.;
         }
-        else {    //FIXME: exceptions
-                 abort();
-                return 0;
-             }
+        else 
+          {
+            //FIXME: report some error? 
+            return -100. ;   
+          }
      }
    }
+ private:
+   int m_nthTrack;
+   int m_ipType;
+
 };
