92499b9aa5d7fbb233f43ddcad49ce81e22f465b
arizzi <>
2007-05-18 12:42:04 +0000

    change name

--- RecoBTag/TrackCounting/src/module.cc
+++ RecoBTag/TrackCounting/src/module.cc
@@ -2,6 +2,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoBTau/JetTagComputer/interface/JetTagComputerESProducer.h"
+#include "RecoBTag/TrackCounting/interface/NegativeTrackCountingComputer.h"
 #include "RecoBTag/TrackCounting/interface/TrackCountingComputer.h"
 #include "RecoBTag/TrackCounting/interface/TrackCounting.h"
 
@@ -10,4 +11,6 @@ DEFINE_FWK_MODULE(TrackCounting);
 
 typedef JetTagComputerESProducer<TrackCountingComputer>       TrackCountingESProducer;
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(TrackCountingESProducer);
+typedef JetTagComputerESProducer<NegativeTrackCountingComputer>       NegativeTrackCountingESProducer;
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(NegativeTrackCountingESProducer);
 
