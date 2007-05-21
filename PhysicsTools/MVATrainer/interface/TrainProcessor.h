e2adc0b7788f8f07d1b58cf9470c4851a2f7001a
saout <>
2007-05-21 02:06:45 +0000

    Tons of improvements and features: * cleanup XMLDocument formatted read/write template methods * fix a memory leak * ProcSplitter "trainer" added * add input variable to pass training weights * allow matrix decorrelation to do PCA on sig/bkg only * cleanup state load/save/cleanup methods and make them configurable * the MVATrainer can have a name (passed to TMVA as well) * cleanup Looper and port to new model * saver modules that read processor state from disk and write to CondDB * saver module templates that can be instantiated and run at the end   of the Looping chain automatically * saver modules can be used to selectively copy calibration records   from existing CondDB containers

---
+++ PhysicsTools/MVATrainer/interface/TrainProcessor.h
@@ -0,0 +1,67 @@
+#ifndef PhysicsTools_MVATrainer_TrainProcessor_h
+#define PhysicsTools_MVATrainer_TrainProcessor_h
+
+#include <vector>
+#include <string>
+
+#include <xercesc/dom/DOM.hpp>
+
+#include "PhysicsTools/MVAComputer/interface/AtomicId.h"
+#include "PhysicsTools/MVAComputer/interface/Variable.h"
+#include "PhysicsTools/MVAComputer/interface/Calibration.h"
+#include "PhysicsTools/MVAComputer/interface/ProcessRegistry.h"
+
+#include "PhysicsTools/MVATrainer/interface/Source.h"
+
+namespace PhysicsTools {
+
+class MVATrainer;
+
+class TrainProcessor : public Source,
+	public ProcessRegistry<TrainProcessor, AtomicId, MVATrainer>::Factory {
+    public:
+	template<typename Instance_t>
+	struct Registry {
+		typedef typename ProcessRegistry<
+			TrainProcessor,
+			AtomicId,
+			MVATrainer
+		>::Registry<Instance_t, AtomicId> Type;
+	};
+
+	inline TrainProcessor(const char *name,
+	                      const AtomicId *id,
+	                      MVATrainer *trainer) :
+		Source(*id), name(name), trainer(trainer) {}
+	virtual ~TrainProcessor() {}
+
+	virtual Variable::Flags getDefaultFlags() const
+	{ return Variable::FLAG_NONE; }
+
+	virtual void
+	configure(XERCES_CPP_NAMESPACE_QUALIFIER DOMElement *config) = 0;
+
+	virtual Calibration::VarProcessor *getCalibration() const = 0;
+
+	virtual void trainBegin() {}
+	virtual void trainData(const std::vector<double> *values,
+	                       bool target, double weight) {}
+	virtual void trainEnd() {}
+
+	virtual bool load() { return true; }
+	virtual void save() {}
+	virtual void cleanup() {}
+
+	inline const char *getId() const { return name.c_str(); }
+
+    protected:
+	virtual void *requestObject(const std::string &name) const
+	{ return 0; }
+
+	std::string	name;
+	MVATrainer	*trainer;
+};
+
+} // namespace PhysicsTools
+
+#endif // PhysicsTools_MVATrainer_TrainProcessor_h
