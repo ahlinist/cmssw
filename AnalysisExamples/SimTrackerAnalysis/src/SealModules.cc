3a4c2964abc1d3c47ee584b87ff455d4a4185a5a
fambrogl <>
2007-05-22 18:12:51 +0000

    SimTrack SimVertex dumper

--- AnalysisExamples/SimTrackerAnalysis/src/SealModules.cc
+++ AnalysisExamples/SimTrackerAnalysis/src/SealModules.cc
@@ -2,7 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "AnalysisExamples/SimTrackerAnalysis/interface/SimHitTrackerAnalyzer.h"
 #include "AnalysisExamples/SimTrackerAnalysis/interface/SimTrackSimVertexAnalyzer.h"
+#include "AnalysisExamples/SimTrackerAnalysis/interface/SimTrackSimVertexDumper.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SimHitTrackerAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimTrackSimVertexAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(SimTrackSimVertexDumper);
