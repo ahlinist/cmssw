3f10af32e74d17f17c89566973e20e7e93647a52
meschi <>
2007-05-23 09:21:10 +0000

    initial impl

---
+++ DQM/FEDMonitor/src/FEDMonitorClient.h
@@ -0,0 +1,59 @@
+
+#include <FWCore/Framework/interface/MakerMacros.h>
+#include <FWCore/Framework/interface/EDAnalyzer.h>
+#include <FWCore/Framework/interface/Event.h>
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "EventFilter/Utilities/interface/ModuleWeb.h"
+
+#include "DQMServices/UI/interface/MonitorUIRoot.h"
+//#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/ClientConfig/interface/QTestHandle.h"
+
+#include "xgi/include/xgi/Input.h"
+#include "xgi/include/xgi/Output.h"
+
+#include <string>
+
+namespace dqm{
+  namespace daq{
+
+    class FEDMonitorClient : public edm::EDAnalyzer, public evf::ModuleWeb
+    {
+    public:
+
+      explicit FEDMonitorClient( const edm::ParameterSet& );
+      ~FEDMonitorClient(){if(qtHandle_) delete qtHandle_;};
+      
+      void analyze(const edm::Event & e, const edm::EventSetup& c);
+      void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+      void defaultWebPage(xgi::Input *in, xgi::Output *out); 
+      void publish(xdata::InfoSpace *){};
+      
+    private:
+
+      MonitorElement * hfedsize;
+      MonitorElement * hfedprof;
+      MonitorElement ** hindfed;
+      QTestHandle * qtHandle_;
+
+      DaqMonitorBEInterface * dbe;
+      MonitorUserInterface * mui;
+
+      unsigned int runId_;
+      unsigned int eventId_;
+      unsigned int nUpdates_;
+
+      bool collationtodo_;
+      bool qtesttodo_;
+      bool autorefresh_;
+
+      void bookQTests( const edm::ParameterSet& );
+      void bookCollationMaybe();
+
+      std::string findSubsystemPath(std::string &);
+
+    };
+  }
+}
---
+++ DQM/FEDMonitor/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "FEDMonitorModule.h"
+#include "FEDMonitorClient.h"
+
+using namespace dqm;
+using namespace daq;
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(FEDMonitor);
+DEFINE_ANOTHER_FWK_MODULE(FEDMonitorClient);
