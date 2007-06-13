f5b5e6aec3e28a93402955debd9269ec4a2a8095
gbruno <>
2007-06-13 14:08:46 +0000

    added new module that stores random or strip lenght proportional noise values to DB

--- CalibTracker/SiStripPedestals/plugins/module.cc
+++ CalibTracker/SiStripPedestals/plugins/module.cc
@@ -10,3 +10,7 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDB);
 #include "CalibTracker/SiStripPedestals/interface/SiStripOfflinePedNoiseToDb.h"
 using cms::SiStripOfflinePedNoiseToDb;
 DEFINE_ANOTHER_FWK_MODULE(SiStripOfflinePedNoiseToDb);
+
+#include "CalibTracker/SiStripPedestals/interface/SiStripNoiseDummyCalculator.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDummyCalculator);
+
