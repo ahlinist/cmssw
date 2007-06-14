32ec9a736ad7e164a451fd56446349bc631c2ce5
ewidl <>
2007-06-14 12:32:24 +0000

    Renamed CosmicsReferenceTrajectory to BzeroReferenceTrajectory (incl. factory)

---
+++ Alignment/ReferenceTrajectories/plugins/BzeroReferenceTrajectoryFactory.cc
@@ -0,0 +1,49 @@
+
+#include "Alignment/ReferenceTrajectories/plugins/BzeroReferenceTrajectoryFactory.h"
+#include "Alignment/ReferenceTrajectories/interface/TrajectoryFactoryPlugin.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h" 
+
+
+BzeroReferenceTrajectoryFactory::BzeroReferenceTrajectoryFactory( const edm::ParameterSet & config ) :
+  TrajectoryFactoryBase( config )
+{
+  theMass = config.getParameter< double >( "ParticleMass" );
+  theMomentumEstimate = config.getParameter< double >( "MomentumEstimate" );
+}
+
+ 
+BzeroReferenceTrajectoryFactory::~BzeroReferenceTrajectoryFactory( void ) {}
+
+
+const BzeroReferenceTrajectoryFactory::ReferenceTrajectoryCollection
+BzeroReferenceTrajectoryFactory::trajectories( const edm::EventSetup & setup,
+					       const ConstTrajTrackPairCollection & tracks ) const
+{
+  ReferenceTrajectoryCollection trajectories;
+
+  edm::ESHandle< MagneticField > magneticField;
+  setup.get< IdealMagneticFieldRecord >().get( magneticField );
+
+  ConstTrajTrackPairCollection::const_iterator itTracks = tracks.begin();
+
+  while ( itTracks != tracks.end() )
+  { 
+    TrajectoryInput input = innermostStateAndRecHits( *itTracks );
+    // set the flag for reversing the RecHits to false, since they are already in the correct order.
+    trajectories.push_back( ReferenceTrajectoryPtr( new BzeroReferenceTrajectory( input.first, input.second, 
+										  false, magneticField.product(),
+										  theMaterialEffects, theMass,
+										  theMomentumEstimate ) ) );
+    ++itTracks;
+  }
+
+  return trajectories;
+}
+
+
+DEFINE_EDM_PLUGIN( TrajectoryFactoryPlugin, BzeroReferenceTrajectoryFactory, "BzeroReferenceTrajectoryFactory" );
