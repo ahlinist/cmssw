0e3049ec9530454bd9e8dd222d2ad8589ccea05d
jacksonj <>
2007-06-20 16:33:57 +0000

    Added TagProbeProducer to seal definition

--- EgammaAnalysis/EgammaEfficiencyProducers/src/SealModule.cc
+++ EgammaAnalysis/EgammaEfficiencyProducers/src/SealModule.cc
@@ -2,6 +2,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "EgammaAnalysis/EgammaEfficiencyProducers/interface/EmObjectProducer.h"
+#include "EgammaAnalysis/EgammaEfficiencyProducers/interface/TagProbeProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EmObjectProducer);
+DEFINE_ANOTHER_FWK_MODULE(TagProbeProducer);
