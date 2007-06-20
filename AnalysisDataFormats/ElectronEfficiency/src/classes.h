b865a1bf6b02d6e16794fdb4aa89db7f8562f5aa
jacksonj <>
2007-06-20 16:16:27 +0000

    Added tag + probe association map

--- AnalysisDataFormats/ElectronEfficiency/src/classes.h
+++ AnalysisDataFormats/ElectronEfficiency/src/classes.h
@@ -1,5 +1,6 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "AnalysisDataFormats/ElectronEfficiency/interface/EmObject.h"
+#include "AnalysisDataFormats/ElectronEfficiency/interface/TagProbeAssociation.h"
 #include "DataFormats/Common/interface/RefToBase.h"
 
 namespace
@@ -11,5 +12,12 @@ namespace
       edm::Ref<EgEff::EmObjectCollection> r1;
       edm::RefProd<EgEff::EmObjectCollection> rp1;
       edm::RefVector<EgEff::EmObjectCollection> rv1;
+
+      EgEff::TagProbeAssociationCollection c2;
+      edm::Wrapper<EgEff::TagProbeAssociationCollection> w2;
+      EgEff::TagProbeAssociation val;
+      EgEff::TagProbeAssociationRef vr2;
+      EgEff::TagProbeAssociationRefProd vrp1;
+      EgEff::TagProbeAssociationRefVector vrv1;
    }
 }
