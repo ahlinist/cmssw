75d2f2dd7032474471b66c8984539f4f124f3279
jacksonj <>
2007-06-20 09:57:10 +0000

    Add electronId cuts to Tag algorithm

--- EgammaAnalysis/EgammaEfficiencyAlgos/interface/TagEmObjectAlgo.h
+++ EgammaAnalysis/EgammaEfficiencyAlgos/interface/TagEmObjectAlgo.h
@@ -14,6 +14,7 @@ class TagEmObjectAlgo : public EmObjectAlgo
 
    protected:
       std::string _inputProducer;
+      std::string _electronIDAssocProducer;
 };
 
 #endif
--- EgammaAnalysis/EgammaEfficiencyAlgos/src/TagEmObjectAlgo.cc
+++ EgammaAnalysis/EgammaEfficiencyAlgos/src/TagEmObjectAlgo.cc
@@ -2,12 +2,15 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronID.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronIDAssociation.h"
 
 //#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
 
 void TagEmObjectAlgo::initialise(const edm::ParameterSet &params)
 {
    _inputProducer = params.getParameter<std::string>("InputProducer");
+   _electronIDAssocProducer = params.getParameter<std::string>("ElectronIDAssociationProducer");
 }
 
 void TagEmObjectAlgo::run(const edm::Event &event, EgEff::EmObjectCollection &outCol)
@@ -24,14 +27,34 @@ void TagEmObjectAlgo::run(const edm::Event &event, EgEff::EmObjectCollection &ou
       throw ex;
    }
 
-   // Create TagEmObjects
-   const reco::PixelMatchGsfElectronCollection *gsfEles = eleCandidatesHandle.product();
-   for(reco::PixelMatchGsfElectronCollection::const_iterator ele = gsfEles->begin(); ele != gsfEles->end(); ++ele)
+   // Get electronId association map
+   edm::Handle<reco::ElectronIDAssociationCollection> electronIDAssocHandle;
+   try
    {
-      EgEff::EmObject newEle(ele->charge(), ele->p4(), ele->vertex());
-      newEle.setGsfTrack(ele->gsfTrack());
-      newEle.setSuperCluster(ele->superCluster());
-      outCol.push_back(newEle);
+      event.getByLabel(_electronIDAssocProducer, electronIDAssocHandle);
    }
+   catch(cms::Exception &ex)
+   {
+      edm::LogError("TagEmObjectAlgo") << "Error! Can't get collection " << _electronIDAssocProducer;
+   }
+
+   // Loop over electrons
+   for(unsigned int i = 0; i < eleCandidatesHandle->size(); ++i)
+   {
+      // Get cut decision for each electron
+      edm::Ref<reco::PixelMatchGsfElectronCollection> electronRef(eleCandidatesHandle, i);
+      reco::ElectronIDAssociationCollection::const_iterator electronIDAssocItr;
+      electronIDAssocItr = electronIDAssocHandle->find(electronRef);
+      const reco::ElectronIDRef &id = electronIDAssocItr->val;
+      bool cutDecision = id->cutBasedDecision();
+      if(cutDecision)
+      {
+         // Create EM Object for gsf electron which passed all cuts
+         EgEff::EmObject newEle(electronRef->charge(), electronRef->p4(), electronRef->vertex());
+         newEle.setGsfTrack(electronRef->gsfTrack());
+         newEle.setSuperCluster(electronRef->superCluster());
+         outCol.push_back(newEle);
+      }
+   } 
 }
 
