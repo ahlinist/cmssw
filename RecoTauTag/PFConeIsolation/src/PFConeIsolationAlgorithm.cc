5a92af4c7d9cd7fb368c2ff75d25bf65bdde5df2
houchu <>
2007-06-22 12:04:32 +0000

    *** empty log message ***

--- RecoTauTag/PFConeIsolation/src/PFConeIsolationAlgorithm.cc
+++ RecoTauTag/PFConeIsolation/src/PFConeIsolationAlgorithm.cc
@@ -4,21 +4,37 @@ PFIsolatedTauTagInfo PFConeIsolationAlgorithm::tag(const GenericJetRef& theGener
   PFIsolatedTauTagInfo resultExtended(thePFCands);
   resultExtended.setgenericjetRef(theGenericJet);
 
+  math::XYZTLorentzVector alternatLorentzVect;
+  alternatLorentzVect.SetPx(0.);
+  alternatLorentzVect.SetPy(0.);
+  alternatLorentzVect.SetPz(0.);
+  alternatLorentzVect.SetE(0.);
+  for (PFCandidateRefVector::const_iterator iGammaCand=resultExtended.PFGammaCands().begin();iGammaCand!=resultExtended.PFGammaCands().end();iGammaCand++) alternatLorentzVect+=(**iGammaCand).p4();
+  for (PFCandidateRefVector::const_iterator iChargedHadrCand=resultExtended.PFChargedHadrCands().begin();iChargedHadrCand!=resultExtended.PFChargedHadrCands().end();iChargedHadrCand++) alternatLorentzVect+=(**iChargedHadrCand).p4();  
+  resultExtended.setalternatLorentzVect(alternatLorentzVect);
+
   resultExtended.filterPFChargedHadrCands(ChargedHadrCand_tkminPt_,ChargedHadrCand_tkminPixelHitsn_,ChargedHadrCand_tkminTrackerHitsn_,ChargedHadrCand_tkmaxipt_,ChargedHadrCand_tkmaxChi2_,ChargedHadrCand_tktorefpointDZ_,UsePVconstraint_,thePV.z(),UseOnlyChargedHadr_for_LeadCand_,MatchingConeSize_,LeadCand_minPt_);
   resultExtended.filterPFNeutrHadrCands(NeutrHadrCand_HcalclusminE_);
   resultExtended.filterPFGammaCands(GammaCand_EcalclusminE_);
 
-  double theSignalConeSize=SignalConeSize_;
+  double theTrackerSignalConeSize=TrackerSignalConeSize_;
   double theJet_E=(*theGenericJet).energy();
-  if(UseSignalConeVariableSize_){
-    theSignalConeSize=min(SignalConeVariableSize_max_,SignalConeVariableSize_Parameter_/theJet_E);
-    theSignalConeSize=max(theSignalConeSize,SignalConeVariableSize_min_);   
+  if(UseTrackerSignalConeVariableSize_){
+    theTrackerSignalConeSize=min(TrackerSignalConeVariableSize_max_,TrackerSignalConeVariableSize_Parameter_/theJet_E);
+    theTrackerSignalConeSize=max(theTrackerSignalConeSize,TrackerSignalConeVariableSize_min_);   
   }  
   math::XYZVector GenericJet_XYZVector=(*theGenericJet).momentum();   
   
+  double trackerdiscriminator=resultExtended.discriminatorByIsolPFChargedHadrCandsN(GenericJet_XYZVector,MatchingConeSize_,theTrackerSignalConeSize,TrackerIsolConeSize_,UseOnlyChargedHadr_for_LeadCand_,LeadCand_minPt_,ChargedHadrCand_minPt_,TrackerIsolRing_Candsn_);
+  if (trackerdiscriminator==1) resultExtended.setpassedtrackerisolation(true);
+  else resultExtended.setpassedtrackerisolation(false);
+  double ECALdiscriminator=resultExtended.discriminatorByIsolPFGammaCandsN(GenericJet_XYZVector,MatchingConeSize_,ECALSignalConeSize_,ECALIsolConeSize_,UseOnlyChargedHadr_for_LeadCand_,LeadCand_minPt_,GammaCand_minPt_,ECALIsolRing_Candsn_);
+  if (ECALdiscriminator==1) resultExtended.setpassedECALisolation(true);
+  else resultExtended.setpassedECALisolation(false);
+
   double discriminator;
-  if (UseOnlyChargedHadrCand_) discriminator=resultExtended.discriminatorByIsolPFChargedHadrCandsN(GenericJet_XYZVector,MatchingConeSize_,theSignalConeSize,IsolConeSize_,UseOnlyChargedHadr_for_LeadCand_,LeadCand_minPt_,Cand_minPt_,IsolRing_Candsn_);
-  else discriminator=resultExtended.discriminatorByIsolPFCandsN(GenericJet_XYZVector,MatchingConeSize_,theSignalConeSize,IsolConeSize_,UseOnlyChargedHadr_for_LeadCand_,LeadCand_minPt_,Cand_minPt_,IsolRing_Candsn_);
+  if ((!UseGammaCand_ && trackerdiscriminator==1) || (UseGammaCand_ && trackerdiscriminator==1 && ECALdiscriminator==1)) discriminator=1.;
+  else discriminator=0.;
   resultExtended.setdiscriminator(discriminator);
   return resultExtended; 
 }
