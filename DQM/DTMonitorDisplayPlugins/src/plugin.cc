7a8fc1026b76516e9db16a8fccb92082e8c05f33
mzanetti <>
2007-07-02 17:54:24 +0000

    head resynch with 15X and later

--- DQM/DTMonitorDisplayPlugins/src/plugin.cc
+++ DQM/DTMonitorDisplayPlugins/src/plugin.cc
@@ -1,8 +1,7 @@
 
 #include "DQM/DTMonitorDisplayPlugins/interface/DTMDisplayPlugins.h"
 #include "VisMonitoring/VisDQMBase/interface/VisDQMDisplayPluginsDB.h"
-#include "PluginManager/ModuleDef.h"
 
 
-DEFINE_SEAL_MODULE();
-DEFINE_SEAL_PLUGIN( VisDQMDisplayPluginsDB, DTMDisplayPlugins, DTMDisplayPlugins::catalogLabel() );
+DEFINE_IGUANA_MODULE();
+DEFINE_IGUANA_PLUGIN( VisDQMDisplayPluginsDB, DTMDisplayPlugins, DTMDisplayPlugins::catalogLabel() );
