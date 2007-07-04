e70e72ff93152d7f37512ff03b9f7b529b460c5d
walten <>
2007-07-04 09:36:02 +0000

    make tagging variable sets

--- RecoBTag/CombinedSV/interface/CombinedSVTaggingVariables.h
+++ RecoBTag/CombinedSV/interface/CombinedSVTaggingVariables.h
@@ -12,6 +12,20 @@ class CombinedSVTaggingVariables {
    */
 
    public:
+     /**
+      *  enum to refer to pre-defined sets of tagging variables.
+      *  FullSet: use the whole set of tagging variables.
+      *  VertexSet: use only a minimal vertexing-related set (VertexCategory
+      *  plus FlightDistance).
+      */
+     enum TaggingVariableSet { FullSet, VertexSet };
+
+     /**
+      *  Constructor that takes an enum to specify
+      *  pre-defined sets of tagging variables.
+      */
+     CombinedSVTaggingVariables ( CombinedSVTaggingVariables::TaggingVariableSet s );
+
      CombinedSVTaggingVariables (
          const std::vector < reco::TaggingVariableName > & recovertex,
          const std::vector < reco::TaggingVariableName > & pseudovertex,
@@ -24,6 +38,10 @@ class CombinedSVTaggingVariables {
          reco::btag::Vertices::VertexType ) const;
    
    private:
+      void pseudoFromNo(); //< copy no vars to pseudo vars
+      void recoFromPseudo(); //< copy pseudo vars to reco vars
+      void fullSet(); //< the "full" tagging var set
+      void vertexSet(); //< the "vertex" tagging var set
 
      const std::vector < reco::TaggingVariableName > & recoVertex() const;
      const std::vector < reco::TaggingVariableName > & pseudoVertex() const;
--- RecoBTag/CombinedSV/src/CombinedSVTaggingVariables.cc
+++ RecoBTag/CombinedSV/src/CombinedSVTaggingVariables.cc
@@ -1,10 +1,71 @@
 #include <algorithm>
-
 #include "RecoBTag/CombinedSV/interface/CombinedSVTaggingVariables.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 using namespace std;
 using namespace reco::btau;
+
+void CombinedSVTaggingVariables::pseudoFromNo()
+{
+  for ( vector< reco::TaggingVariableName >::const_iterator i=theNoVars.begin(); 
+        i!=theNoVars.end() ; ++i )
+  {
+    thePseudoVars.push_back ( *i );
+  }
+}
+
+void CombinedSVTaggingVariables::recoFromPseudo()
+{
+  for ( vector< reco::TaggingVariableName >::const_iterator i=thePseudoVars.begin(); 
+        i!=thePseudoVars.end() ; ++i )
+  {
+    theRecoVars.push_back ( *i );
+  }
+}
+
+void CombinedSVTaggingVariables::fullSet()
+{
+  theNoVars.push_back ( reco::btau::vertexCategory );
+  theNoVars.push_back ( reco::btau::trackSip2d );
+  pseudoFromNo();
+  thePseudoVars.push_back ( reco::btau::vertexMass );
+  thePseudoVars.push_back ( reco::btau::vertexMultiplicity );
+  thePseudoVars.push_back ( reco::btau::secondaryVtxEnergyRatio );
+  thePseudoVars.push_back ( reco::btau::trackSip2dAbCharm );
+  thePseudoVars.push_back ( reco::btau::trackEtaRel );
+  // thePseudoVars.push_back ( reco::btau::secondaryVtxWeightedEnergyRatio );
+  recoFromPseudo();
+  theRecoVars.push_back ( reco::btau::flightDistance2DSignificance );
+  // theRecoVars.push_back ( reco::btau::jetNVertices );
+}
+
+void CombinedSVTaggingVariables::vertexSet()
+{
+  theNoVars.push_back ( reco::btau::vertexCategory );
+  pseudoFromNo();
+  /*
+  thePseudoVars.push_back ( reco::btau::vertexMass );
+  thePseudoVars.push_back ( reco::btau::vertexMultiplicity );
+  thePseudoVars.push_back ( reco::btau::secondaryVtxEnergyRatio );
+   */
+  recoFromPseudo();
+  theRecoVars.push_back ( reco::btau::flightDistance2DSignificance );
+}
+
+CombinedSVTaggingVariables::CombinedSVTaggingVariables ( 
+    CombinedSVTaggingVariables::TaggingVariableSet s )
+{
+  switch ( s )
+  {
+    case FullSet:
+      { fullSet(); break; }
+    case VertexSet:
+      { vertexSet(); break; }
+    default:
+      edm::LogError("") << "TaggingVariableSet \"" << s << "\" unknown. Use \"full\" or \"vertex\"";
+      exit(0);
+  }
+}
 
 CombinedSVTaggingVariables::CombinedSVTaggingVariables (
     const vector < TaggingVariableName > & recovertex,
