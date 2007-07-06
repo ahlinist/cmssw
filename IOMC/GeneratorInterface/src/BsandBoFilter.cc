47979de9287e261ec4a0225ea19d19442c4ac0f9
fmoortga <>
2007-07-06 15:22:31 +0000

    fix minus sign

--- IOMC/GeneratorInterface/src/BsandBoFilter.cc
+++ IOMC/GeneratorInterface/src/BsandBoFilter.cc
@@ -19,27 +19,30 @@ BsandBoFilter::~BsandBoFilter()
   std::cout << "Total number of accepted events = " << noAccepted << std::endl;
 }
 
-
-
 bool BsandBoFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   edm::Handle<HepMCProduct> evt;
   iEvent.getByLabel(label_, evt);
   
   HepMC::GenEvent * generated_event = new HepMC::GenEvent(*(evt->GetEvent()));
+  cout << "Start\n";
 
   bool event_passed = true;
 							
   for(HepMC::GenEvent::particle_iterator particle_it = generated_event->particles_begin(); particle_it != generated_event->particles_end(); particle_it++){
-  	if((*particle_it)->momentum().pz() > (*particle_it)->momentum().e()){
+//   	cout << "pz: " << (*particle_it)->momentum().pz() << ", E: " << (*particle_it)->momentum().e() << endl;
+// 	cout << "eta: " << (*particle_it)->momentum().eta() << endl;
+  	if( ((*particle_it)->momentum().pz() > (*particle_it)->momentum().e()) ||  ((-(*particle_it)->momentum().pz()) > (*particle_it)->momentum().e())){
 		cout << "pz larger then E!" << endl;
 		event_passed = false;
 	}
   }
   
   if (event_passed) noAccepted++;
+  cout << "End filter\n";
   
   delete generated_event; 
   
   return event_passed;
 }
+
