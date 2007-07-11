68626ac0c2fc13ea918c532bb12ae9ac3277c0eb
gennai <>
2007-07-11 12:57:12 +0000

    *** empty log message ***

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -1,13 +1,10 @@
 
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoTauTag/HLTAnalyzers/interface/SingleDoubleTrkTau.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTAnalysis.h"
 #include "RecoTauTag/HLTAnalyzers/interface/TauJetMCFilter.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(SingleDoubleTrkTau);
-DEFINE_ANOTHER_FWK_MODULE(HLTAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
 
 
