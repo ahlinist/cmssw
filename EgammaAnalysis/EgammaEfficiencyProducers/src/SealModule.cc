c459bbff4d0754bdca10c5ece3669da2fae28a93
dlevans <>
2007-07-19 12:12:08 +0000

    addition of verification analyzer

--- EgammaAnalysis/EgammaEfficiencyProducers/src/SealModule.cc
+++ EgammaAnalysis/EgammaEfficiencyProducers/src/SealModule.cc
@@ -3,7 +3,10 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "EgammaAnalysis/EgammaEfficiencyProducers/interface/EmObjectProducer.h"
 #include "EgammaAnalysis/EgammaEfficiencyProducers/interface/TagProbeProducer.h"
+#include "EgammaAnalysis/EgammaEfficiencyProducers/interface/TagProbeVerification.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EmObjectProducer);
 DEFINE_ANOTHER_FWK_MODULE(TagProbeProducer);
+DEFINE_ANOTHER_FWK_MODULE(TagProbeVerification);
+
