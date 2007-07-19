2c26d28f33751eaafd5cd4e3f25d3b05bc90afdc
giordano <>
2007-07-19 20:15:58 +0000

    working example with CondObjects

--- CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryWrite.h
+++ CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryWrite.h
@@ -9,24 +9,29 @@
 
 // #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "CommonTools/ConditionDBWriter/interface/ConditionDBWriter.h"
+#include "CondFormats/SiStripObjects/interface/SiStripRunSummary.h"
 
 // Save Compile time by forwarding declarations
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 
 // class SiStripRunSummaryWrite: public edm::EDAnalyzer {
-class SiStripRunSummaryWrite: public ConditionDBWriter<std::string> {
+class SiStripRunSummaryWrite: public ConditionDBWriter<SiStripRunSummary> {
   public:
     SiStripRunSummaryWrite( const edm::ParameterSet &roCONFIG);
     virtual ~SiStripRunSummaryWrite() {}
 
   protected:
     // Leave possibility of inheritance
-    virtual void analyze ( const edm::Event      &roEVENT,
-                           const edm::EventSetup &roEVENT_SETUP) {}
-    virtual void endJob();
+    void algoAnalyze ( const edm::Event      &roEVENT,
+		   const edm::EventSetup &roEVENT_SETUP) {}
+
+    void algoEndRun ( const edm::Event      &roEVENT,
+		      const edm::EventSetup &roEVENT_SETUP) ;
+  
+    void algoEndJob(){};
 
   private:
-    virtual std::string *getNewObject();
+    virtual SiStripRunSummary *getNewObject();
 
     // Prevent object copying
     SiStripRunSummaryWrite( const SiStripRunSummaryWrite &);
