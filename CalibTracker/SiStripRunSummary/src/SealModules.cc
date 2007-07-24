2156394e58ff02bb683d179bdbad5febd12f4b0c
gbruno <>
2007-07-24 17:43:43 +0000

    several fixes

--- CalibTracker/SiStripRunSummary/src/SealModules.cc
+++ CalibTracker/SiStripRunSummary/src/SealModules.cc
@@ -2,6 +2,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryWrite.h"
+#include "CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryRead.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripRunSummaryWrite);
+DEFINE_ANOTHER_FWK_MODULE(SiStripRunSummaryRead);
