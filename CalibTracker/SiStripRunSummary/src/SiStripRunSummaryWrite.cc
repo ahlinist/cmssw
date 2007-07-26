b28aa50bb828a989d61c2e1a97b521d54aacf7a0
samvel <>
2007-07-26 21:00:56 +0000

    Now RunSummary flags are written into DB

--- CalibTracker/SiStripRunSummary/src/SiStripRunSummaryWrite.cc
+++ CalibTracker/SiStripRunSummary/src/SiStripRunSummaryWrite.cc
@@ -1,11 +1,16 @@
-#include <iostream>
+// Author : Samvel Khalatian (samvel at fnal dot gov) 
+// Created: 06/29/07 
+// Licence: GPL 
+
+#include <fstream>
+#include <sstream>
 
 #include "CalibTracker/SiStripRunSummary/interface/DQMFlagTxt.h"
+#include "CalibTracker/SiStripRunSummary/interface/DQMFlagXML.h"
 #include "CalibTracker/SiStripRunSummary/interface/SerializeTxt.h"
 #include "CalibTracker/SiStripRunSummary/interface/SerializeXML.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 
 #include "CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryWrite.h"
 
@@ -15,38 +20,47 @@ SiStripRunSummaryWrite::SiStripRunSummaryWrite(
     oFileXMLIn_( roCONFIG.getUntrackedParameter<std::string>( 
       "oFileXMLIn",
       "") ),
-    oFlagTreeTxt_( "") {
+    oFlagTreeTxt_( "") 
+{
 
   if( oFileXMLIn_.empty()) {
     throw cms::Exception( "Wrong Input parameters", 
                           "Input XML File is not specified: can not proceed.");
   }
+
+  // [ToDo: Add file check for existance]
 }
 
-void SiStripRunSummaryWrite::algoEndRun( const edm::Event      &roEVENT, const edm::EventSetup &roEVENT_SETUP){
-  /*
+void SiStripRunSummaryWrite::algoEndRun( const edm::Event      &roEVENT, 
+                                         const edm::EventSetup &roEVENT_SETUP) {
+
+  std::ifstream oFileIn( oFileXMLIn_.c_str());
+
   // Read Run Summary Flags XML file
   SerializeXML oSerializeXML;
-  
-  oSerializeXML.read( oFileXMLIn_.c_str());
+  if( DQMFlagXML *poFlagXML = oSerializeXML.read<DQMFlagXML>( oFileIn)) {
+    // Convert XML to Txt Tree
+    DQMFlagTxt oDQMFlagTxt( poFlagXML);
 
-  // Convert XML to Txt Tree
-  DQMFlagTxt oDQMFlagTxt( oSerializeXML.getFlag());
+    // Get Txt Tree state string
+    std::ostringstream oOut( oFlagTreeTxt_);
 
-  std::cout << oDQMFlagTxt << std::endl;
-
-  // Get Txt Tree state string
-  SerializeBin oSerializeBin;
-  oSerializeBin.writeStr( oFlagTreeTxt_, oDQMFlagTxt);
-  */
+    SerializeTxt oSerialize;
+    if( !oSerialize.write( oOut, oDQMFlagTxt)) {
+      // Failed to serialize tree
+      throw cms::Exception( "Run Summary Flags", 
+                            "Failed to serialize Run Summary Flags Tree in string");
+    }
+  } else {
+    // Failed to read Flags Tree from file
+    throw cms::Exception( "Run Summary Flags", 
+                          "Failed to read Run Summary Flags Tree from XML File");
+  }
 }
 
 SiStripRunSummary *SiStripRunSummaryWrite::getNewObject() {
-  //TMP... remove it
-  oFlagTreeTxt_="abcdefghilmno";
-  
-  SiStripRunSummary* obj=new SiStripRunSummary;
-  obj->put(oFlagTreeTxt_);
-  std::cout << "\n\n\n TEST REMOVE ME " << oFlagTreeTxt_ << " added value " << obj->getRunSummary() << "\n\n\n" << std::endl;
-  return obj;
+  SiStripRunSummary *poRunSummary = new SiStripRunSummary;
+  poRunSummary->put( oFlagTreeTxt_);
+
+  return poRunSummary;
 }
