e7eff6cf254b27f98a9d660f670bdf5673224087
samvel <>
2007-07-26 21:49:12 +0000

    Example of how to read Run Summary Flags from DB

--- CalibTracker/SiStripRunSummary/src/SiStripRunSummaryRead.cc
+++ CalibTracker/SiStripRunSummary/src/SiStripRunSummaryRead.cc
@@ -1,30 +1,41 @@
-#include <string>
-#include "FWCore/Framework/interface/ESHandle.h"
+// Author : Samvel Khalatian (samvel at fnal dot gov)
+// Created: 07/05/07
+// License: GPL
 
-#include "CondFormats/DataRecord/interface/SiStripRunSummaryRcd.h"
-#include "CondFormats/SiStripObjects/interface/SiStripRunSummary.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-// #include "CalibTracker/SiStripRunSummary/interface/SerializeTxt.h"
-
-#include "CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryRead.h"
 #include <iostream>
 
-SiStripRunSummaryRead::SiStripRunSummaryRead( 
-  const edm::ParameterSet &roCONFIG) {}
+#include "CalibTracker/SiStripRunSummary/interface/SerializeTxt.h"
+#include "CalibTracker/SiStripRunSummary/interface/DQMFlagTxt.h"
+#include "CondFormats/DataRecord/interface/SiStripRunSummaryRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripRunSummary.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "CalibTracker/SiStripRunSummary/interface/SiStripRunSummaryRead.h"
 
 void SiStripRunSummaryRead::beginJob( const edm::EventSetup &roEVENT_SETUP) {
 
+  edm::ESHandle<SiStripRunSummary> poESHRunSummary;
+  roEVENT_SETUP.get<SiStripRunSummaryRcd>().get( poESHRunSummary);
 
+  if( poESHRunSummary.isValid()) {
+    std::istringstream oIn( poESHRunSummary->getRunSummary());
 
-  //SiStripRunSummary oRecord( roEVENT_SETUP.get<SiStripRunSummary>()); 
-
-
-  std::cout <<"HERE I AM"<<std::endl;
-  edm::ESHandle<SiStripRunSummary> pDD;
-  roEVENT_SETUP.get<SiStripRunSummaryRcd>().get(pDD );
-
-  std::cout << pDD->getRunSummary();
-
-
+    SerializeTxt oSerializeTxt;
+    if( DQMFlagTxt *poFlagTxt = oSerializeTxt.read<DQMFlagTxt>( oIn)) {
+      std::cout << "Read Flag Tree" << std::endl;
+      std::cout << *poFlagTxt << std::endl;
+    } else {
+      // Failed to read Flags Tree
+      throw cms::Exception( "Run Summary Flags", 
+                            "Failed to read Run Summary Flags Tree from \
+string");
+    }
+  } else {
+    // Incorrect Handle: maybe string is not in DB?
+    throw cms::Exception( "Run Summary Flags", 
+                          "Incorrect ESHandle<SiStripRunSummary> - not valid \
+one. Are you sure Run Summary Flags Tree is in DB?");
+  }
 }
