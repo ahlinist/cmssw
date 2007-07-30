90343632aea24fd96f15231fc4641e16b97347f7
wmtan <>
2007-07-30 04:16:53 +0000

    Now contains two time stamps, one for the beginning and one for the end.

--- Framework/interface/RunPrincipal.h
+++ Framework/interface/RunPrincipal.h
@@ -25,12 +25,13 @@ namespace edm {
   typedef Principal Base;
   public:
     RunPrincipal(RunNumber_t const& id,
-        Timestamp const& time,
+        Timestamp const& beginTm,
+        Timestamp const& endTm,
 	boost::shared_ptr<ProductRegistry const> reg,
 	ProcessConfiguration const& pc,
 	ProcessHistoryID const& hist = ProcessHistoryID(),
 	boost::shared_ptr<DelayedReader> rtrv = boost::shared_ptr<DelayedReader>(new NoDelayedReader)) :
-	  Base(reg, pc, hist, rtrv), aux_(id, time) {}
+	  Base(reg, pc, hist, rtrv), aux_(id, beginTm, endTm) {}
     ~RunPrincipal() {}
 
     RunAuxiliary const& aux() const {
@@ -46,8 +47,16 @@ namespace edm {
       return aux().id();
     }
 
-    Timestamp const& time() const {
-      return aux().time();
+    Timestamp const& beginTime() const {
+      return aux().beginTime();
+    }
+
+    Timestamp const& endTime() const {
+      return aux().endTime();
+    }
+
+    Timestamp const& updateEndTime(Timestamp const& time) {
+      return aux_.updateEndTime(time);
     }
 
     using Base::addGroup;
