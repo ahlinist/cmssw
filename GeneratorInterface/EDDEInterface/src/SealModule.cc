6105b74700d63200c74177db0383358006faf946
kdatsko <>
2007-07-31 12:59:22 +0000

    Initial Version

---
+++ GeneratorInterface/EDDEInterface/src/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "GeneratorInterface/EDDEInterface/interface/EDDESource.h"
+
+  using edm::EDDESource;
+
+  DEFINE_SEAL_MODULE();
+  DEFINE_ANOTHER_FWK_INPUT_SOURCE(EDDESource);
+  DEFINE_ANOTHER_FWK_INPUT_SOURCE(EDDEAnalyzer);
