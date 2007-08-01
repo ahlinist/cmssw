518b79e9751476e4c870fa9d23bfd3d7342c34c2
beaudett <>
2007-08-01 14:34:01 +0000

    JPsi ee filter - first commmit

---
+++ RecoParticleFlow/PFSkims/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoParticleFlow/PFSkims/interface/JPsieeFilter.h"
+
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(JPsieeFilter);
+
