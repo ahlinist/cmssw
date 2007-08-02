07b93d65ec95ef4e9048c995dd192c9b89304314
kap01 <>
2007-08-02 18:45:55 +0000

    Added two more .cc's

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -2,11 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/HLTAnalyzers/interface/TauJetMCFilter.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
-
+#include "RecoTauTag/HLTAnalyzers/interface/HLTmcInfo.h"
+#include "RecoTauTag/HLTAnalyzers/interface/HLTPathComparison.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
-
+DEFINE_ANOTHER_FWK_MODULE(HLTmcInfo);
+DEFINE_ANOTHER_FWK_MODULE(HLTPathComparison);
 
 
 
