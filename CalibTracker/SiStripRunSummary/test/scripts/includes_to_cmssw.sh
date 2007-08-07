6b56ddea3cfe6779fecfb96fb74979eac6538fe6
samvel <>
2007-08-07 19:47:05 +0000

    Fixed script

--- CalibTracker/SiStripRunSummary/test/scripts/includes_to_cmssw.sh
+++ CalibTracker/SiStripRunSummary/test/scripts/includes_to_cmssw.sh
@@ -1,6 +1,19 @@
 #! /usr/bin/env bash
 
-cPath=${1-""}
+# Author : Samvel Khalatian (samvel at fnal dot gov)
+# Created: 07/30/07
+# License: GPL
+
+# Include function
+. bash_print_error.sh
+. bash_print_usage.sh
+
+# Declare constants
+declare -r cUSAGE="`basename $0` <PATTERN_OF_FILES_TO_FIX>"
+
+declare -r nERR_NO_PATTERN=65
+
+cFiles=${1-""}
 
 function correctInclude() {
   local _cFile=$1
@@ -10,20 +23,21 @@ function correctInclude() {
   echo "[done] ${_cFile}"
 }
 
-if [[ -nz "${cPath}" ]]; then
-  if [[ -d "${cPath}" ]]; then
-    # Path specified
-    for cFile in `ls -1 ${cPath}`
-    do
-      correctInclude "${cPath}/${cFile}"
-    done
-  else
-    # File specified
-    correctInclude "${cPath}"
-  fi
-else
-  # Path is not specified
-  echo "[error] Input is not specified"
+if [[ -z "${cFiles}" ]]; then
+  printError "Pattern of files to be fixed is not specified.\n"
+  printUsage
+  exit $nERR_NO_PATTERN
 fi
+
+# Might be pattern or folder
+cDir=`dirname "${cFiles}"`
+if [[ -d "${cFiles}" ]]; then
+  cDir="${cFiles}"
+fi
+
+for cFile in `ls -1 ${cFiles}`
+do
+  correctInclude "${cDir}/`basename ${cFile}`"
+done
 
 exit 0
