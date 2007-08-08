deff0112766f89ed51b9f70648110eee86e6974a
kap01 <>
2007-08-08 14:44:45 +0000

    For 16x

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -2,13 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/HLTAnalyzers/interface/TauJetMCFilter.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTmcInfo.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTPathComparison.h"
+#include "RecoTauTag/HLTAnalyzers/interface/HLTTauMcInfo.h"
+#include "RecoTauTag/HLTAnalyzers/interface/HLTTauPathComparison.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTmcInfo);
-DEFINE_ANOTHER_FWK_MODULE(HLTPathComparison);
+DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
+DEFINE_ANOTHER_FWK_MODULE(HLTTauPathComparison);
 
 
 
