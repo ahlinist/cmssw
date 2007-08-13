037a9eef9e37f5c741633026f91286eb324331e0
case <>
2007-08-13 18:48:13 +0000

    Add PGeometricDet for Tracker.

--- CondCore/IdealGeometryPlugins/plugins/plugin.cc
+++ CondCore/IdealGeometryPlugins/plugins/plugin.cc
@@ -1,7 +1,10 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondCore/IdealGeometryPlugins/interface/IdealGeometryProxy.h"
+#include "CondFormats/IdealGeometryObjects/interface/PGeometricDet.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "DetectorDescription/Core/interface/DDCompactView.h"
+#include "CondFormats/DataRecord/interface/PGeometricDetRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(IdealGeometryRecord,DDCompactView);
+REGISTER_PLUGIN(PGeometricDetRcd,PGeometricDet);
