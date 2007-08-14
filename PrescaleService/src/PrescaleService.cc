1ffe84290d4fcf9731aee3ec08d15b240244e0f6
gruen <>
2007-08-14 18:48:31 +0000

    Changes for use in offline setting (CSA07 etc.).

--- PrescaleService/src/PrescaleService.cc
+++ PrescaleService/src/PrescaleService.cc
@@ -35,7 +35,19 @@ namespace edm {
       lsold = 0;
       nops = 0; 
 
-      LogDebug("PrescaleService") << "PrescaleService::PrescaleService";
+      //
+      const std::vector<std::string> InitialConfig(iPS.getParameter< std::vector<std::string> >("InitialConfig"));
+      for (unsigned int I=0; I!=InitialConfig.size(); ++I) {
+	const int i(putPrescale(InitialConfig[I]));
+	if (i-1!=I) LogDebug("PrescaleService")
+	  << "Invalid config string " << I << ": '"
+	  << InitialConfig[I] << "' - Ignored!";
+      }
+      //
+
+      LogDebug("PrescaleService") << "PrescaleService::PrescaleService: "
+				  << prescalers.size() << " of "
+				  << InitialConfig.size() << " initialised!";
 
       iReg.watchPostBeginJob(this,&PrescaleService::postBeginJob);
       iReg.watchPostEndJob(this,&PrescaleService::postEndJob);
@@ -155,10 +167,10 @@ namespace edm {
       string a, b;
       istringstream iss(prescalers[i]);
       iss >> n;
-      while (!(iss.rdstate() & istringstream::goodbit)) {
+      while (iss.rdstate() == 0) {
 	iss >> a >> b >> m; 
 //	cout << "getPrescale " << n << "==" << ls << " a " << a << " b " << b << " m " << m << endl;
-	if (module == b) {
+	if ( (b=="*") || (b==module) ) { // allow wildcard after an explicit module list
             if (lsg == ls && lsc < n) {
 		bang++;
 		return -1;
