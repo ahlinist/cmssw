1773bf843e29ddce2517d1d6be9d13a2ae138b24
jaj99 <>
2007-08-16 22:35:47 +0000

    Update to Oracle distributor

--- RecoLuminosity/HLXReadOut/HLXCoreLibs/src/common/OracleDistributor.cc
+++ RecoLuminosity/HLXReadOut/HLXCoreLibs/src/common/OracleDistributor.cc
@@ -24,9 +24,10 @@ namespace HCAL_HLX
   using namespace oracle::occi;
 
   // Location, username and password for DB access (hard coded)
-  const string OracleDistributor::mOracleDBUserName = "CMS_LUMI_HF_WRITER";
-  const string OracleDistributor::mOracleDBPassword = "CMS_LUM_0RITER";
-  const string OracleDistributor::mOracleDBLocation = "cmscalir";
+  // cms_lumi_reader/everyoneneedsLUMI
+  const string OracleDistributor::mOracleDBUserName = "cms_lumi_writer"; //CMS_LUMI_HF_WRITER";
+  const string OracleDistributor::mOracleDBPassword = "July4th07_W"; //CMS_LUM_0RITER";
+  const string OracleDistributor::mOracleDBLocation = "omds"; //cmscalir";
   
   u32 OracleDistributor::GetErrorCount() {
     return mErrorCount;
@@ -34,33 +35,56 @@ namespace HCAL_HLX
 
   // Default constructor
   OracleDistributor::OracleDistributor() {
-    mErrorCount=0;
+    try{
+      mErrorCount=0;
     // TODO - add exception handling for this
-    mOracleEnvironment = Environment::createEnvironment(Environment::DEFAULT);
-    mOracleConnection = mOracleEnvironment->createConnection(mOracleDBUserName,
-							     mOracleDBPassword,
-							     mOracleDBLocation);
+      mOracleEnvironment = Environment::createEnvironment(Environment::DEFAULT);
+      mOracleConnection = mOracleEnvironment->createConnection(mOracleDBUserName,
+							       mOracleDBPassword,
+							       mOracleDBLocation);
+      cout << hex << mOracleConnection << endl;
+
+    } catch (SQLException & aExc) {
+      //cout << "SQL exception found" << endl;
+      //cout << aExc.getMessage() << endl;
+      HardwareAccessException lExc(aExc.getMessage());
+      RAISE(lExc);
+    }
+
   }
 
   // Destructor deletes the hardware interface
   OracleDistributor::~OracleDistributor() {
     // Terminate the connection
-    mOracleEnvironment->terminateConnection(mOracleConnection);
-    mOracleConnection = 0;
-    Environment::terminateEnvironment(mOracleEnvironment);
-    mOracleEnvironment = 0;
+    if ( mOracleConnection ) {
+      mOracleEnvironment->terminateConnection(mOracleConnection);
+      mOracleConnection = 0;
+    }
+    if ( mOracleEnvironment ) {
+      Environment::terminateEnvironment(mOracleEnvironment);
+      mOracleEnvironment = 0;
+    }
   }
 
   void OracleDistributor::ProcessSection(const LUMI_SECTION & lumiSection) {
     try {
-      std::string sqlStmt = "insert into cms_lumi_hf_owner.lumi_sections(IS_DATA_TAKING,BEGIN_ORBIT_NUMBER,RUN_NUMBER) VALUES (:bDataTaking, :mStartOrbit, :mRunNumber)";
-      //                FILL_NUMBER, SEC_START_TIME, SEC_STOP_TIME, COMMENTS)
-        //              VALUES (:sid, :bit ,:th,:setVnum, :dataTk, :bO, :eO,:run, :lsNum,
-          //            :fNum,:secStratT,:secStopT,:comm )";
+      std::string sqlStmt = "insert into schema_owner.lumi_sections(IS_DATA_TAKING,BEGIN_ORBIT_NUMBER,RUN_NUMBER) VALUES (:bDataTaking, :mStartOrbit, :mRunNumber)";
+      // FILL_NUMBER, SEC_START_TIME, SEC_STOP_TIME, COMMENTS)
+      // VALUES (:sid, :bit ,:th,:setVnum, :dataTk, :bO, :eO,:run, :lsNum,
+      // :fNum,:secStratT,:secStopT,:comm )";
       
+
+
       // Create the SQL statement
+      cout << "Create SQL statement" << endl; 
+      /*"insert into " + _dbOwner + ".lumi_sections(SECTION_ID,"
+                     "SET_VERSION_NUMBER, IS_DATA_TAKING, BEGIN_ORBIT_NUMBER,"
+                     "END_ORBIT_NUMBER, RUN_NUMBER, LUMI_SECTION_NUMBER,"
+                     "FILL_NUMBER, SEC_START_TIME, SEC_STOP_TIME, COMMENTS)"
+                     "VALUES (:sid, :setVnum, :dataTk, :bO, :eO,:run, :lsNum,"
+                     ":fNum,:secStratT,:secStopT,:comm )";*/
       Statement *mOracleStatement =  mOracleConnection->createStatement(sqlStmt);
-      cout << mOracleStatement << endl;
+      /*cout << mOracleStatement << endl;
 
       mOracleStatement->setUInt(1, static_cast<unsigned int>(lumiSection.hdr.bCMSLive));
       mOracleStatement->setUInt(2, static_cast<unsigned int>(lumiSection.hdr.startOrbit));
@@ -78,9 +102,10 @@ namespace HCAL_HLX
       //mOracleStatement->setString (13, comment);
 
       // Execute the SQL statement
-      mOracleStatement->executeUpdate();
+      mOracleStatement->executeUpdate();*/
 
       // Delete the SQL statement
+      cout << "Terminate statement" << endl;
       mOracleConnection->terminateStatement(mOracleStatement);
 
     } catch (SQLException & aExc) {
@@ -89,6 +114,9 @@ namespace HCAL_HLX
       mErrorCount++;
       //HardwareAccessException lExc(aExc.getMessage());
       //RAISE(lExc);
+    } catch (...) {
+      cout << "Unknown exception caught" << endl;
+      mErrorCount++;
     }
   }
 
