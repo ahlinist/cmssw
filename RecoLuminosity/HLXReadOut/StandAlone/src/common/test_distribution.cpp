b2ae6eedba66b903daf4b4f27335fcfbdfbbe2d9
jaj99 <>
2007-08-16 22:40:07 +0000

    Addition of new standalone test files Update to Make file to fit in TriDAS repository

---
+++ RecoLuminosity/HLXReadOut/StandAlone/src/common/test_distribution.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+#include <string>
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <signal.h>
+
+#include "ICException.hh"
+
+// NibbleCollector class
+#include "TestDistributor.hh"
+#include "NibbleCollector.hh"
+#include "SectionCollector.hh"
+
+int gContinue=1;
+
+void CtrlC(int aSigNum) {
+  std::cout << "Ctrl-c detected, stopping run" << std::endl;
+  gContinue=0;
+}
+
+using namespace std;
+using namespace HCAL_HLX;
+using namespace ICCoreUtils;
+
+#define NUM_HLXS 12
+
+int main(int argc, char ** argv) {
+  signal(SIGINT,CtrlC);
+  SectionCollector *lSectionCollector = 0;
+  NibbleCollector *lNibbleCollector = 0;
+  TestDistributor *lTestDistributor = 0;
+  try {
+    lSectionCollector = new SectionCollector(3564, //3564, // Num bunches
+					     3,    // Num nibbles per section
+					     4096,  //280,  // Num orbits in lumi nibble
+					     NUM_HLXS);   // Num HLXs
+    lNibbleCollector = new NibbleCollector(NUM_HLXS);
+    lTestDistributor = new TestDistributor;
+    lNibbleCollector->AttachSectionCollector(lSectionCollector);
+    lSectionCollector->AttachDistributor(lTestDistributor);
+
+    int startTime, tempTime, interTime = 0;
+    time((time_t*)&startTime);
+    tempTime=startTime;
+
+    while (gContinue) {
+      lNibbleCollector->RunServiceHandler();
+      time((time_t*)&tempTime);
+      if ( tempTime != interTime ) {
+	cout << endl << tempTime-startTime << endl;
+	cout << "Good packet count: " << lNibbleCollector->GetNumGoodPackets() << endl;
+	cout << "Bad packet count: " << lNibbleCollector->GetNumBadPackets() << endl;
+	//cout << "Good nibble count: " << lNibbleCollector.GetNumGoodNibbles() << endl;
+	//cout << "Bad nibble count: " << lNibbleCollector.GetNumBadNibbles() << endl;
+	cout << "Good section count:" << lSectionCollector->GetNumCompleteLumiSections() << endl;
+	cout << "Bad section count: " << lSectionCollector->GetNumIncompleteLumiSections() << endl;
+	cout << "Good section data count: " << lTestDistributor->GetNumGoodSections() << endl;
+	cout << "Bad section data count: " << lTestDistributor->GetNumBadSections() << endl;
+	cout << "Lost packet count: " << lNibbleCollector->GetNumLostPackets() << endl;
+	cout << "Total data volume: " << lNibbleCollector->GetTotalDataVolume() << endl;
+	cout << "Average data rate (Mb/s): " << (double)lNibbleCollector->GetTotalDataVolume()*8.0/(1024.0*1024.0*(double)(tempTime-startTime)) << endl;
+	interTime = tempTime;
+      }
+    }
+
+  }catch(ICException & aExc){
+    cerr<<aExc.what()<<endl;
+  }catch(std::exception & aExc){
+    std::cerr<<aExc.what()<<std::endl;
+  }catch(...){
+    std::cerr<<"Unknown exception caught."<<std::endl;
+  }
+
+  delete lNibbleCollector;
+  delete lSectionCollector;
+  delete lTestDistributor;
+  return 0;
+}
