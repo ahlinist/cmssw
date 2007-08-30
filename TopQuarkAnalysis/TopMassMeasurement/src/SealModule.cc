7d59a8b675e0080f4a2c3c8a278a92f457ab6d1b
heyninck <>
2007-08-30 22:16:50 +0000

    corrections in order to make the head compile again

--- TopQuarkAnalysis/TopMassMeasurement/src/SealModule.cc
+++ TopQuarkAnalysis/TopMassMeasurement/src/SealModule.cc
@@ -1,6 +1,4 @@
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "TopQuarkAnalysis/TopMassMeasurement/interface/TtSemiIdeogramProducer.h"
 
-DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TtSemiIdeogramProducer);
