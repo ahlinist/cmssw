4cb6ae786e4b0110533ac3532de9df901d8b66aa
arizzi <>
2007-09-12 15:41:02 +0000

    add back the plugins

---
+++ RecoBTag/TrackCounting/src/module.cc
@@ -0,0 +1,16 @@
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoBTau/JetTagComputer/interface/JetTagComputerESProducer.h"
+#include "RecoBTag/TrackCounting/interface/NegativeTrackCountingComputer.h"
+#include "RecoBTag/TrackCounting/interface/TrackCountingComputer.h"
+#include "RecoBTag/TrackCounting/interface/TrackCounting.h"
+
+
+DEFINE_FWK_MODULE(TrackCounting);
+
+typedef JetTagComputerESProducer<TrackCountingComputer>       TrackCountingESProducer;
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(TrackCountingESProducer);
+typedef JetTagComputerESProducer<NegativeTrackCountingComputer>       NegativeTrackCountingESProducer;
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(NegativeTrackCountingESProducer);
+
