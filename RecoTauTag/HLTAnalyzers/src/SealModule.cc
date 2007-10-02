686dc19e95001fdc4d8b4cc4455c282711864fc0
sgreder <>
2007-10-02 20:19:45 +0000

    added HLTTauAnalyzer

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -4,12 +4,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "RecoTauTag/HLTAnalyzers/interface/HLTTauMcInfo.h"
 #include "RecoTauTag/HLTAnalyzers/interface/HLTTauPathComparison.h"
+#include "RecoTauTag/HLTAnalyzers/interface/HLTTauAnalyzer.h"
 #include "RecoTauTag/HLTAnalyzers/interface/SingleDoubleTrkTau.h"
 #include "RecoTauTag/HLTAnalyzers/interface/L1TauAnalyzer.h"
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauPathComparison);
+DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SingleDoubleTrkTau);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
 
