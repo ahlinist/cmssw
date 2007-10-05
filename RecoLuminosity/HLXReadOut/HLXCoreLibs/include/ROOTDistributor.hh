830a7b0c622f707ad4425b56f5a6a9a418e7d2ad
ahunt <>
2007-10-05 20:58:41 +0000

    ROOTDistributor updated.

--- RecoLuminosity/HLXReadOut/HLXCoreLibs/include/ROOTDistributor.hh
+++ RecoLuminosity/HLXReadOut/HLXCoreLibs/include/ROOTDistributor.hh
@@ -1,6 +1,14 @@
 #ifndef ROOTDISTRIBUTOR_HH
 #define ROOTDISTRIBUTOR_HH
 
+/*
+
+Authors: Adam Hunt and John Jones - Princeton University
+date: 2007-10-05
+email: ahunt@princeton.edu
+
+*/
+
 #include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
@@ -23,18 +31,27 @@ namespace HCAL_HLX {
     LUMI_SUMMARY *Summary;
     LUMI_BUNCH_CROSSING *BX;
     LUMI_THRESHOLD *Threshold;
-    LUMI_SECTION* LumiSection;
+
+    ET_SUM_SECTION    *EtSum;
+    OCCUPANCY_SECTION *Occupancy;
+    LHC_SECTION       *LHC;
+
     LUMI_SECTION_HST *LumiSectionHist;
     LEVEL1_HLT_TRIGGER *L1HLTrigger;
     TRIGGER_DEADTIME *TriggerDeadtime;
-    
+
     ROOTDistributor(std::string filename = "LumiSchema",
 		    std::string treename = "LumiTree");
     ~ROOTDistributor();
 
     // Called by SectionCollector
     bool ProcessSection(const LUMI_SECTION & lumiSection);
-    //void FillTree(const HCAL_HLX::LUMI_SECTION&);
+
+    // MBCD = Make Branch, Copy Data
+    void MBCD(const ET_SUM_SECTION &in,    ET_SUM_SECTION **out,    int num, unsigned int compress);
+    void MBCD(const OCCUPANCY_SECTION &in, OCCUPANCY_SECTION **out, int num, unsigned int compress);
+    void MBCD(const LHC_SECTION &in,       LHC_SECTION **out,       int num, unsigned int compress);
+    // TO DO: make a template for these three functions
 
   private:
     ROOTMutex mROOTMutex;
--- RecoLuminosity/HLXReadOut/HLXCoreLibs/src/common/ROOTDistributor.cc
+++ RecoLuminosity/HLXReadOut/HLXCoreLibs/src/common/ROOTDistributor.cc
@@ -1,6 +1,13 @@
 #include "ROOTDistributor.hh"
-//#include "IntToString.hh"
 #include "ICUtilityToolbox.hh"
+
+/*
+
+Authors: Adam Hunt and John Jones - Princeton University
+Date: 2007-10-05
+email: ahunt@princeton.edu
+*/
+
 
 using namespace std;
 
@@ -14,19 +21,30 @@ namespace HCAL_HLX {
 				   std::string baseTreeName) {
     mBaseFileName = baseFileName;
     mBaseTreeName = baseTreeName;
+    
+    Header  = new LUMI_SECTION_HEADER;
+    Summary = new LUMI_SUMMARY;
+    BX      = new LUMI_BUNCH_CROSSING;
+    
+    EtSum     = new ET_SUM_SECTION[HCAL_HLX_MAX_HLXS];
+    Occupancy = new OCCUPANCY_SECTION[HCAL_HLX_MAX_HLXS];
+    LHC       = new LHC_SECTION[HCAL_HLX_MAX_HLXS];
 
-    Summary         = new LUMI_SUMMARY;
-    BX              = new LUMI_BUNCH_CROSSING;
     Threshold       = new LUMI_THRESHOLD;
-    LumiSection     = new LUMI_SECTION;
     LumiSectionHist = new LUMI_SECTION_HST;
     L1HLTrigger     = new LEVEL1_HLT_TRIGGER;
     TriggerDeadtime = new TRIGGER_DEADTIME;
   }
   
   ROOTDistributor::~ROOTDistributor(){   
+    delete Header;
     delete Summary;
     delete BX;
+
+    delete [] EtSum;
+    delete [] Occupancy;
+    delete [] LHC;
+
     delete Threshold;
     delete LumiSection;
     delete LumiSectionHist;
@@ -34,54 +52,14 @@ namespace HCAL_HLX {
     delete TriggerDeadtime;
   }
   
-  bool ROOTDistributor::ProcessSection(const HCAL_HLX::LUMI_SECTION& localSection){
+  bool ROOTDistributor::ProcessSection(const LUMI_SECTION& localSection){
     //cout << "Begin " << __PRETTY_FUNCTION__ << endl;
 
     unsigned int i, j;
 
-    //Summary
-    Summary->DeadtimeNormalization = 50;
-    Summary->DeadtimeNormalization = 50;
-    Summary->Normalization         = 50;
-    Summary->InstantLumi           = 50;
-    Summary->InstantLumiErr        = 50;
-    Summary->InstantLumiQlty       = 50;
-    Summary->InstantETLumi         = 50;
-    Summary->InstantETLumiErr      = 50;
-    Summary->InstantETLumiQlty     = 50;
-    for(i=0; i<2; i++){
-      Summary->InstantOccLumi[i]     = 50;
-      Summary->InstantOccLumiErr[i]  = 50;
-      Summary->InstantOccLumiQlty[i] = 50;
-    }
-
-    //Threshold
-    Threshold->Threshold1Set1      = 300;
-    Threshold->Threshold2Set1      =  10;
-    Threshold->Threshold1Set2      = 300;
-    Threshold->Threshold2Set2      =  10;
-    Threshold->ET                  =  60;
-    
-    // Bunch Crossing
-    for(i=0; i<3564 ; i++){
-      BX->ETLumi[i]     = 3;
-      BX->ETLumiErr[i]  = 3;
-      BX->ETLumiQlty[i] = 3;
-      for(j=0; j< 2; j++){
-	BX->OccLumi[j][i]    = 3;
-	BX->OccLumiErr[j][i] = 3;
-	BX->OccLumiQlty[j][i]= 3;
-      }
-    }
-    
-    // Section Header
-    // TODO: memcpy?!
-    //    for(i=0; i < 36; i ++){
-    // for(j =0; j <3564; j ++){
-    //	LumiSection->etSum[i].data[j] = localSection.etSum[i].data[j] ;
-    // }
-      
-    //}
+    ET_SUM_SECTION    *EtSumPtr[HCAL_HLX_MAX_HLXS];
+    OCCUPANCY_SECTION *OccupancyPtr[HCAL_HLX_MAX_HLXS];
+    LHC_SECTION       *LHCPtr[HCAL_HLX_MAX_HLXS];
 
     std::ostringstream outputString;
     outputString << "data/" << mBaseFileName << "_" 
@@ -95,15 +73,49 @@ namespace HCAL_HLX {
     m_file          = new TFile(fileName.c_str(), "RECREATE");  
     m_file->cd();    
     m_tree          = new TTree(mBaseTreeName.c_str(), "Lumi Section",1);
-    m_tree->Bronch("Summary.",          "HCAL_HLX::LUMI_SUMMARY",       &Summary, sizeof(HCAL_HLX::LUMI_SUMMARY));
-    m_tree->Bronch("BunchCrossing.",    "HCAL_HLX::LUMI_BUNCH_CROSSING",&BX);
-    m_tree->Bronch("Threshold.",        "HCAL_HLX::LUMI_THRESHOLD",     &Threshold);
-    m_tree->Bronch("LumiSection.",      "HCAL_HLX::LUMI_SECTION",       &LumiSection, sizeof(HCAL_HLX::LUMI_SECTION));
-    m_tree->Bronch("Lumi_Section_Hist.","HCAL_HLX::LUMI_SECTION_HST",   &LumiSectionHist);  
-    m_tree->Bronch("Level1_HLTrigger.", "HCAL_HLX::LEVEL1_HLT_TRIGGER", &L1HLTrigger);
-    m_tree->Bronch("Trigger_Deadtime.", "HCAL_HLX::TRIGGER_DEADTIME",   &TriggerDeadtime);
 
-    memcpy(LumiSection, &localSection, sizeof(localSection));    
+    m_tree->Bronch("Threshold.",        "HCAL_HLX::LUMI_THRESHOLD",      &Threshold, sizeof(Threshold)/compress);
+    m_tree->Bronch("Lumi_Section_Hist.","HCAL_HLX::LUMI_SECTION_HST",    &LumiSectionHist, sizeof(LumiSectionHist)/compress);  
+    m_tree->Bronch("Level1_HLTrigger.", "HCAL_HLX::LEVEL1_HLT_TRIGGER",  &L1HLTrigger, sizeof(L1HLTrigger)/compress);
+    m_tree->Bronch("Trigger_Deadtime.", "HCAL_HLX::TRIGGER_DEADTIME",    &TriggerDeadtime, sizeof(TriggerDeadtime)/compress);
+        
+    m_tree->Bronch("Header.",        "HCAL_HLX::LUMI_SECTION_HEADER", &Header, sizeof(Header)/compress);
+    m_tree->Bronch("Summary.",       "HCAL_HLX::LUMI_SUMMARY",        &Summary, sizeof(Summary)/compress);
+    m_tree->Bronch("BunchCrossing.", "HCAL_HLX::LUMI_BUNCH_CROSSING", &BX, sizeof(BX)/compress);
+    
+    for(int i = 0; i < HCAL_HLX_MAX_HLXS; i++){
+      EtSumPtr[i] = &EtSum[i];
+      MBCD(localSection.etSum[i], &EtSumPtr[i], i);
+      OccupancyPtr[i] = &Occupancy[i];
+      MBCD(localSection.occupancy[i], &OccupancyPtr[i], i);
+      LHCPtr[i] = &LHC[i];
+      MBCD(localSection.lhc[i], &LHCPtr[i], i);
+      // Yes, that is supposed to be the address of a pointer.  ROOT is strange.
+    }
+    
+    Threshold->Threshold1Set1 = 51;
+    Threshold->Threshold2Set1 = 52;
+    Threshold->Threshold1Set2 = 53;
+    Threshold->Threshold2Set2 = 54;
+    Threshold->ET             = 55;
+    
+    LumiSectionHist->IsDataTaking      = true;
+    LumiSectionHist->BeginOrbitNumber  = 61;
+    LumiSectionHist->EndOrbitNumber    = 62;
+    LumiSectionHist->RunNumber         = 63;
+    LumiSectionHist->LumiSectionNumber = 64;
+    LumiSectionHist->FillNumber        = 65;
+    LumiSectionHist->SecStopTime       = 66;
+    LumiSectionHist->SecStartTime      = 67;
+    
+    L1HLTrigger->TriggerValue          = 71;
+    L1HLTrigger->TriggerBitNumber      = 72;
+    
+    TriggerDeadtime->TriggerDeadtime   = 81;
+    
+    memcpy(Header,  &localSection.hdr,               sizeof (localSection.hdr));
+    memcpy(Summary, &localSection.lumiSummary,       sizeof(localSection.lumiSummary));
+    memcpy(BX,      &localSection.lumiBunchCrossing, sizeof(localSection.lumiBunchCrossing));
     
     m_tree->Fill();
     m_file->Write();
@@ -111,15 +123,67 @@ namespace HCAL_HLX {
 
     //delete m_tree; // NO!!! root does this when you delete m_file
     delete m_file;
-
-
-
- 
+    
     mROOTMutex.Unlock();
-
+    
     return true;
     //m_tree->Print();
     //cout << "End " << __PRETTY_FUNCTION__ << endl;
   }
 
-}
+  // MBCD = Make Branch, Copy Data
+  void ROOTDistributor::MBCD(const ET_SUM_SECTION &in, ET_SUM_SECTION **out, int num, unsigned int compress = 100){
+#ifdef DEBUG
+    cout << "In " << __PRETTY_FUNCTION__ << endl;
+#endif
+  
+    // TODO: Generalize the following line for ET_SUM, OCCUPANCY, and LHC. 
+    std::string branchName = "ET_SUM";
+    //  Size of class?  Sorry, sizeof(ET_SUM_SECTION) == sizeof(LHC_SECTION).
+
+    std::string className = "HCAL_HLX::" +branchName+ "_SECTION";
+
+    std::ostringstream numString;
+    numString << std::dec << num;
+    branchName = branchName + ((num / 10 == 0)? "0" : "") + numString.str() + "."; 
+    m_tree->Bronch(branchName.c_str() , className.c_str(), out, sizeof(in)/compress); 
+    memcpy(*out, &in, sizeof(in));    
+  }
+
+  void ROOTDistributor::MBCD(const OCCUPANCY_SECTION &in, OCCUPANCY_SECTION **out, int num, unsigned int compress = 100){
+#ifdef DEBUG
+    cout << "In " << __PRETTY_FUNCTION__ << endl;
+#endif
+  
+    // TODO: Generalize the following line.
+    std::string branchName = "OCCUPANCY";
+    //  Size of class?  Sorry, sizeof(ET_SUM_SECTION) == sizeof(LHC_SECTION).
+
+    std::string className = "HCAL_HLX::" + branchName + "_SECTION";
+    std::ostringstream numString;
+
+    numString << std::dec << num;
+    branchName = branchName + ((num / 10 == 0)? "0" : "") + numString.str() + "."; 
+    m_tree->Bronch(branchName.c_str() , className.c_str(), out, sizeof(in)/compress); 
+    memcpy(*out, &in, sizeof(in));  
+  }
+
+  void ROOTDistributor::MBCD(const LHC_SECTION &in, LHC_SECTION **out, int num,unsigned int compress = 100){
+#ifdef DEBUG
+    cout << "In " << __PRETTY_FUNCTION__ << endl;
+#endif
+  
+    // TODO: Generalize the following line.
+    std::string branchName = "LHC";
+    //  Size of class?  Sorry, sizeof(ET_SUM_SECTION) == sizeof(LHC_SECTION).
+
+    std::string className = "HCAL_HLX::" + branchName+ "_SECTION";
+    std::ostringstream numString;
+
+    numString << std::dec << num;
+    branchName = branchName + ((num / 10 == 0)? "0" : "") + numString.str() + "."; 
+    m_tree->Bronch(branchName.c_str() , className.c_str(), out, sizeof(in)/compress); 
+    memcpy(*out, &in, sizeof(in));  
+  }
+} // namespace HCAL_HLX
+
