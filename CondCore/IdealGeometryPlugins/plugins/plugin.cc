169af4a3166f26fbb00e19e333ad6beddbb4c6ab
dlange <>
2007-10-06 15:36:10 +0000

    correct header location

--- CondCore/IdealGeometryPlugins/plugins/plugin.cc
+++ CondCore/IdealGeometryPlugins/plugins/plugin.cc
@@ -3,7 +3,7 @@
 #include "CondFormats/IdealGeometryObjects/interface/PGeometricDet.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "DetectorDescription/Core/interface/DDCompactView.h"
-#include "CondFormats/DataRecord/interface/PGeometricDetRcd.h"
+#include "Geometry/Records/interface/PGeometricDetRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(IdealGeometryRecord,DDCompactView);
