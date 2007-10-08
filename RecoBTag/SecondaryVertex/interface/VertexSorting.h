7bd5fb12f218bfcbb1bfff67431db63bc2225098
saout <>
2007-10-08 20:17:12 +0000

    * More changes to cuts, also store all reconstructed secondary vertices   (first one is the one used for flight distance by CombinedSV).   Hopyfully as close to CombinedSV as possible now. * rename combinedSV -> combinedSecondaryVertex

---
+++ RecoBTag/SecondaryVertex/interface/VertexSorting.h
@@ -0,0 +1,34 @@
+#ifndef RecoBTag_SecondaryVertex_VertexSorting_h
+#define RecoBTag_SecondaryVertex_VertexSorting_h
+
+#include <vector>
+#include <string>
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoBTag/SecondaryVertex/interface/SecondaryVertex.h"
+
+class VertexSorting {
+    public:
+	VertexSorting(const edm::ParameterSet &params);
+	~VertexSorting() {}
+
+	std::vector<unsigned int>
+	operator () (const std::vector<SecondaryVertex> &svCandidates) const;
+
+    private:
+	enum SortCriterium {
+		sortDist3dVal = 0,
+		sortDist3dErr,
+		sortDist3dSig,
+		sortDist2dErr,
+		sortDist2dSig,
+		sortDist2dVal
+	};
+
+	static SortCriterium getSortCriterium(const std::string &criterium);
+
+	SortCriterium	sortCriterium;
+};
+
+#endif // RecoBTag_SecondaryVertex_VertexSorting_h
