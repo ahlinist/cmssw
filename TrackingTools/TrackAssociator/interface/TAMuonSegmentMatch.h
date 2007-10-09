2a6f6fb6828a55fe1493ad6db4d6dbf150dc3960
dmytro <>
2007-10-09 02:38:52 +0000

    renamed two classes two avoid name clashing with DataFormat

---
+++ TrackingTools/TrackAssociator/interface/TAMuonSegmentMatch.h
@@ -0,0 +1,20 @@
+#ifndef TrackAssociator_TAMuonSegmentMatch_h
+#define TrackAssociator_TAMuonSegmentMatch_h
+#include "DataFormats/Math/interface/Vector3D.h"
+#include "DataFormats/Math/interface/Point3D.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+class TAMuonSegmentMatch {
+ public:
+   math::XYZPoint  segmentGlobalPosition;
+   math::XYZPoint  segmentLocalPosition;
+   math::XYZVector segmentLocalDirection;
+   float  segmentLocalErrorXX;
+   float  segmentLocalErrorYY;
+   float  segmentLocalErrorXY;
+   float  segmentLocalErrorDxDz;
+   float  segmentLocalErrorDyDz;
+   float  segmentLocalErrorXDxDz;
+   float  segmentLocalErrorYDyDz;
+};
+#endif
