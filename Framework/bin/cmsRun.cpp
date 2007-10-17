047674b1944738507b415ef890c3a1dedaad21b9
wmtan <>
2007-10-17 22:37:26 +0000

    Remove LoadAllDictionaries again.

--- Framework/bin/cmsRun.cpp
+++ Framework/bin/cmsRun.cpp
@@ -119,9 +119,8 @@ int main(int argc, char* argv[])
   // 
   // The parameters for these can be overridden from the configuration files.
   std::vector<std::string> defaultServices;
-  defaultServices.reserve(6);
+  defaultServices.reserve(5);
   defaultServices.push_back("MessageLogger");
-  defaultServices.push_back("LoadAllDictionaries");
   defaultServices.push_back("InitRootHandlers");
   defaultServices.push_back("AdaptorConfig");
   defaultServices.push_back("EnableFloatingPointExceptions");
