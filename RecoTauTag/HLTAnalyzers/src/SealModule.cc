8b00acbbadb8ff90087bf82cbf86efdc7d19d893
gennai <>
2007-11-07 17:22:57 +0000

    *** empty log message ***

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -1,12 +1,14 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/HLTAnalyzers/interface/TauJetMCFilter.h"
+#include "RecoTauTag/HLTAnalyzers/interface/PFTauFilter.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "RecoTauTag/HLTAnalyzers/interface/HLTTauMcInfo.h"
 #include "RecoTauTag/HLTAnalyzers/interface/HLTTauPathComparison.h"
 #include "RecoTauTag/HLTAnalyzers/interface/HLTTauAnalyzer.h"
 #include "RecoTauTag/HLTAnalyzers/interface/SingleDoubleTrkTau.h"
 #include "RecoTauTag/HLTAnalyzers/interface/L1TauAnalyzer.h"
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
@@ -14,6 +16,6 @@ DEFINE_ANOTHER_FWK_MODULE(HLTTauPathComparison);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SingleDoubleTrkTau);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
-
+DEFINE_ANOTHER_FWK_MODULE(PFTauFilter);
 
 
