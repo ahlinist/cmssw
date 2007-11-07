8d2f6927a1f690c84f2989e14642d0ecaad2d73a
salerno <>
2007-11-07 16:04:44 +0000

    minor changes

--- EgammaAnalysis/ElectronIDAlgos/interface/PTDRElectronID.h
+++ EgammaAnalysis/ElectronIDAlgos/interface/PTDRElectronID.h
@@ -17,8 +17,22 @@ public:
  private:
 
   std::string quality_;
+
+  std::vector<int> useEoverPIn_;
+  std::vector<int> useDeltaEtaIn_;
+  std::vector<int> useDeltaPhiIn_;
+  std::vector<int> useHoverE_;
+  std::vector<int> useE9overE25_;
+  std::vector<int> useEoverPOut_;
+  std::vector<int> useDeltaPhiOut_;
+  std::vector<int> useInvEMinusInvP_;
+  std::vector<int> useBremFraction_;
+  std::vector<int> useSigmaEtaEta_;
+  std::vector<int> useSigmaPhiPhi_;
+ 
   edm::ParameterSet cuts_;
 
+  int variables_;
 };
 
 #endif // PTDRElectronID_H
