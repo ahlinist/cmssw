42eacb6c47061acc72df64921798f080cb4d45eb
gennai <>
2007-11-13 16:31:34 +0000

    Fixing some feature to work with HLTLevel1SeedGTFilter

--- RecoTauTag/HLTAnalyzers/src/SealModule.cc
+++ RecoTauTag/HLTAnalyzers/src/SealModule.cc
@@ -1,21 +1,11 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/HLTAnalyzers/interface/TauJetMCFilter.h"
-#include "RecoTauTag/HLTAnalyzers/interface/PFTauFilter.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTTauMcInfo.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTTauPathComparison.h"
-#include "RecoTauTag/HLTAnalyzers/interface/HLTTauAnalyzer.h"
 #include "RecoTauTag/HLTAnalyzers/interface/SingleDoubleTrkTau.h"
-#include "RecoTauTag/HLTAnalyzers/interface/L1TauAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
-DEFINE_ANOTHER_FWK_MODULE(HLTTauPathComparison);
-DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SingleDoubleTrkTau);
-DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(PFTauFilter);
 
 
