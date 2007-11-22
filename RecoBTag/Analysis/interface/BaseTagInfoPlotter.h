7173ded1cd81ae39ab89b6acac60c0bc4c5db5dd
saout <>
2007-11-22 16:19:05 +0000

    Interface changes to BaseTagInfoPlotter: setEventSetup, tagInfoRequirements, additional analyzeTag method (vector of BaseTagInfo pointers), with default forwarding to old method for non-special case. Adjustments in BTagPerformanceAnalyzer.cc to handle multiple TagInfos, if demanded by BaseTagInfoPlotter instance (GenericMVA). New TagInfoPlotter: TaggingVariablePlotter - uses taggingVariables() method in BaseTagInfo and plots those variables according to histogram range / binning information in PSet. New TagInfoPlotter: MVAJetTagPlotter - uses GenericMVAJetTagComputer instance to retrieve tagging variables via taggingVariables() and calls TaggingVariablePlotter for the contents, optionally for multple categories. Note: TaggingVariablePlotter lacks psPlot and epsPlot for now.

--- RecoBTag/Analysis/interface/BaseTagInfoPlotter.h
+++ RecoBTag/Analysis/interface/BaseTagInfoPlotter.h
@@ -1,8 +1,12 @@
 #ifndef BaseTagInfoPlotter_H
 #define BaseTagInfoPlotter_H
 
-#include "RecoBTag/Analysis/interface/BaseBTagPlotter.h"
+#include <vector>
+#include <string>
+
+#include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/BTauReco/interface/BaseTagInfo.h"
+#include "RecoBTag/Analysis/interface/BaseBTagPlotter.h"
 #include "RecoBTag/MCTools/interface/JetFlavour.h"
 
 class BaseTagInfoPlotter : public BaseBTagPlotter {
@@ -13,7 +17,11 @@ class BaseTagInfoPlotter : public BaseBTagPlotter {
 	    BaseBTagPlotter(tagName, etaPtBin) {};
 
   virtual ~BaseTagInfoPlotter () {};
-  virtual void analyzeTag(const reco::BaseTagInfo * tagInfo, const BTagMCTools::JetFlavour & jetFlavour) = 0;
+  virtual void analyzeTag(const reco::BaseTagInfo * tagInfo, const BTagMCTools::JetFlavour & jetFlavour);
+  virtual void analyzeTag(const std::vector<const reco::BaseTagInfo *> &tagInfos, const BTagMCTools::JetFlavour & jetFlavour);
+
+  virtual void setEventSetup(const edm::EventSetup & setup);
+  virtual vector<string> tagInfoRequirements() const;
   
 } ;
 
---
+++ RecoBTag/Analysis/src/TaggingVariablePlotter.cc
@@ -0,0 +1,135 @@
+#include "RecoBTag/Analysis/interface/TaggingVariablePlotter.h"
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+TaggingVariablePlotter::VariableConfig::VariableConfig(
+		const string &name, const ParameterSet &pSet, bool update,
+		const string &category) :
+	var(getTaggingVariableName(name)),
+	nBins(pSet.getParameter<unsigned int>("nBins")),
+	min(pSet.getParameter<double>("min")),
+	max(pSet.getParameter<double>("max"))
+{
+	if (var == btau::lastTaggingVariable)
+		throw cms::Exception("Configuration")
+			<< "Tagging variable \"" << name
+			<< "\" does not exist." << endl;
+
+	if (pSet.exists("logScale"))
+		logScale = pSet.getParameter<bool>("logScale");
+	else
+		logScale = false;
+
+	std::vector<unsigned int> indices;
+	if (pSet.exists("indices"))
+		indices = pSet.getParameter< vector<unsigned int> >("indices");
+	else
+		indices.push_back(0);
+
+	for(std::vector<unsigned int>::const_iterator iter = indices.begin();
+	    iter != indices.end(); iter++) {
+		Plot plot;
+		plot.histo.reset(new FlavourHistorgrams<double>(
+			name.c_str() + (*iter ? Form("%d", *iter) : TString())
+			             + (category.empty() ? TString()
+			              		: (TString("_") + category)),
+			TaggingVariableDescription[var], nBins, min, max,
+			false, logScale, true, "b", update));
+		plot.index = *iter;
+		plots.push_back(plot);
+	}
+}
+
+TaggingVariablePlotter::TaggingVariablePlotter(const TString &tagName,
+	const EtaPtBin &etaPtBin, const ParameterSet &pSet, bool update,
+	const string &category) : BaseTagInfoPlotter(tagName, etaPtBin)
+{
+	vector<string> pSets = pSet.getParameterNames();
+	for(vector<string>::const_iterator iter = pSets.begin();
+	    iter != pSets.end(); iter++) {
+		VariableConfig var(*iter,
+		                   pSet.getParameter<ParameterSet>(*iter),
+		                   update, category);
+		variables.push_back(var);
+	}
+
+	if (update) {
+		TString dir = "TaggingVariable" + theExtensionString;
+		gFile->cd();
+		gFile->cd(dir);
+	}
+}
+
+
+TaggingVariablePlotter::~TaggingVariablePlotter ()
+{
+}
+
+
+void TaggingVariablePlotter::analyzeTag (const BaseTagInfo *baseTagInfo,
+	const BTagMCTools::JetFlavour &jetFlavour)
+{
+	analyzeTag(baseTagInfo->taggingVariables(), jetFlavour);
+}
+
+void TaggingVariablePlotter::analyzeTag (const TaggingVariableList &vars,
+	const BTagMCTools::JetFlavour &jetFlavour)
+{
+	int jetFlav = jetFlavour.flavour();
+
+	for(vector<VariableConfig>::const_iterator iter = variables.begin();
+	    iter != variables.end(); iter++) {
+		std::vector<TaggingValue> values;
+		try {
+			values = vars.getList(iter->var);
+		} catch(edm::Exception e) {
+			// variable does not exist
+			// it is ok for multi-appearance variables
+			continue;
+		}
+
+		unsigned int size = values.size();
+		for(std::vector<VariableConfig::Plot>::const_iterator plot =
+						iter->plots.begin();
+		    plot != iter->plots.end(); plot++) {
+			if (plot->index == 0) {
+				for(unsigned int i = 0; i < size; i++)
+					plot->histo->fill(jetFlav, values[i]);
+			} else if (plot->index - 1 < size)
+				plot->histo->fill(jetFlav,
+				                  values[plot->index - 1]);
+		}
+	}
+}
+
+void TaggingVariablePlotter::finalize()
+{
+}
+
+void TaggingVariablePlotter::psPlot(const TString &name)
+{
+}
+
+void TaggingVariablePlotter::write(const bool allHisto)
+{
+	TString dir = "TaggingVariable" + theExtensionString;
+	gFile->cd();
+	gFile->mkdir(dir);
+	gFile->cd(dir);
+
+	for(vector<VariableConfig>::const_iterator iter = variables.begin();
+	    iter != variables.end(); iter++) {
+		for(std::vector<VariableConfig::Plot>::const_iterator plot =
+							iter->plots.begin();
+		    plot != iter->plots.end(); plot++)
+			plot->histo->write(allHisto);
+	}
+
+	gFile->cd();
+}
+
+void TaggingVariablePlotter::epsPlot(const TString &name)
+{
+}
