91baf515941fd77bc3e50ed8b6123af3b8cf5ff7
salerno <>
2007-12-06 14:49:41 +0000

    bug fix

--- EgammaAnalysis/ElectronIDAlgos/src/ElectronIDAlgo.cc
+++ EgammaAnalysis/ElectronIDAlgos/src/ElectronIDAlgo.cc
@@ -12,15 +12,21 @@ void ElectronIDAlgo::baseSetup(const edm::ParameterSet& conf) {
 const reco::ClusterShapeRef& ElectronIDAlgo::getClusterShape(const reco::PixelMatchGsfElectron* electron, const edm::Event& e) {
 
   // Get association maps linking BasicClusters to ClusterShape.
-  edm::Handle<reco::BasicClusterShapeAssociationCollection> clusterShapeHandle;
-  if (electron->classification()<100) {
-    e.getByLabel(barrelClusterShapeAssocProducer_, clusterShapeHandle);
-  } else {
-    e.getByLabel(endcapClusterShapeAssocProducer_, clusterShapeHandle);
-  }
+  edm::Handle<reco::BasicClusterShapeAssociationCollection> clusterShapeHandleBarrel;
+  edm::Handle<reco::BasicClusterShapeAssociationCollection> clusterShapeHandleEndcap;
+  e.getByLabel(barrelClusterShapeAssocProducer_, clusterShapeHandleBarrel);
+  e.getByLabel(endcapClusterShapeAssocProducer_, clusterShapeHandleEndcap);
 
   // Find entry in map corresponding to seed BasicCluster of SuperCluster
   reco::BasicClusterShapeAssociationCollection::const_iterator seedShpItr;
-  seedShpItr = clusterShapeHandle->find(electron->superCluster()->seed());
+
+  if (electron->classification()<100) {
+    seedShpItr=clusterShapeHandleBarrel->find(electron->superCluster()->seed());
+    if (electron->classification()==40 && seedShpItr == clusterShapeHandleBarrel->end()) 
+        seedShpItr=clusterShapeHandleEndcap->find(electron->superCluster()->seed());
+  } else {
+    seedShpItr=clusterShapeHandleEndcap->find(electron->superCluster()->seed());
+  }
+
   return seedShpItr->val;
 }
