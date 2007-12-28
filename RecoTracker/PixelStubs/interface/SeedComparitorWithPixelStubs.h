b336ec02f45691e32045feec8ff152705621aeeb
dfehling <>
2007-12-28 17:39:26 +0000

    First modified version of PixelStubs using Templates and position based on rechit

--- RecoTracker/PixelStubs/interface/SeedComparitorWithPixelStubs.h
+++ RecoTracker/PixelStubs/interface/SeedComparitorWithPixelStubs.h
@@ -2,23 +2,41 @@
 #define PixelStubs_SeedComparitorWithPixelStubs_h
 /** \class SeedComparitorWithPixelStubs SeedComparitorWithPixelStubs.h RecoTracker/PixelStubs/interface/SeedComparitorWithPixelStubs.h 
  * Inherits from abstract base class to compare pixel seeds using the PixelStub class.
- *  \author Aaron Dominguez (UNL)
+ *  \author Aaron Dominguez (UNL) edited by Dave Fehling
  */
 #include "RecoTracker/TkSeedingLayers/interface/SeedingHitSet.h"
 #include "RecoTracker/TkSeedingLayers/interface/SeedComparitor.h"
+//#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-namespace edm { class ParameterSet; }
+class MagneticField;
+class PixelCPEBase;
+
+
+//namespace edm { class ParameterSet; }
 
 class SeedComparitorWithPixelStubs : public SeedComparitor {
  public:
   SeedComparitorWithPixelStubs(const edm::ParameterSet & cfg);
-  SeedComparitorWithPixelStubs(double betaCutFactor);
+  SeedComparitorWithPixelStubs(double betaCutFactor, const edm::ParameterSet & cfg);
   virtual ~SeedComparitorWithPixelStubs();
   
-  virtual bool compatible(const SeedingHitSet &hits);
+  virtual bool compatible(const SeedingHitSet &hits, const edm::EventSetup &es);
+
+	//	virtual void eventInit(const edm::EventSetup &es);
 
  private:
   double betaCutFactor_;
+	double tempCutFactor_;
+	int method_flag;
+	std::string method_str;
+	const edm::ParameterSet theCfg ;
+	//	const edm::EventSetup theES;
+
+	const MagneticField *theMagField;
+	mutable const PixelCPEBase *theCPE;
 };
 
 #endif
--- RecoTracker/PixelStubs/src/SeedComparitorWithPixelStubs.cc
+++ RecoTracker/PixelStubs/src/SeedComparitorWithPixelStubs.cc
@@ -3,13 +3,41 @@
 #include "RecoTracker/PixelStubs/interface/PixelStub.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHit.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "RecoLocalTracker/SiPixelRecHits/interface/PixelCPEBase.h"
+#include "RecoLocalTracker/SiPixelRecHits/interface/PixelCPETemplateReco.h"
 
-SeedComparitorWithPixelStubs::SeedComparitorWithPixelStubs(const edm::ParameterSet & cfg)
-  : betaCutFactor_(cfg.getParameter<double>("betaCutFactor"))
-{}
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 
-SeedComparitorWithPixelStubs::SeedComparitorWithPixelStubs(double betaCutFactor)
-  : betaCutFactor_(betaCutFactor)
+SeedComparitorWithPixelStubs::SeedComparitorWithPixelStubs(const edm::ParameterSet &cfg)
+  : betaCutFactor_(cfg.getParameter<double>("betaCutFactor")),
+		tempCutFactor_(cfg.getParameter<double>("tempCutFactor")),
+		method_str(cfg.getParameter<std::string>("method")),
+		theCfg(cfg),
+		theCPE(0)
+		//theES(es)
+{
+	if(method_str=="stubs")
+		method_flag = 1;
+	else if (method_str=="crude")
+		method_flag = 2;
+	else if (method_str=="vectors")
+		method_flag = 3;
+	else if (method_str=="templates") {
+		method_flag = 4;
+		theCPE = new PixelCPETemplateReco(theCfg, 0);  //make it with NO magnetic field!  (to be done later)
+	}
+	else
+	method_flag = 0;
+  //dfehling TODO throw exception
+}
+
+SeedComparitorWithPixelStubs::SeedComparitorWithPixelStubs(double betaCutFactor, const edm::ParameterSet &cfg)
+  : betaCutFactor_(betaCutFactor),
+		theCfg(cfg)
+		//theES(es)
 {}
 
 SeedComparitorWithPixelStubs::~SeedComparitorWithPixelStubs()
@@ -18,20 +46,47 @@ SeedComparitorWithPixelStubs::~SeedComparitorWithPixelStubs()
 // Check to see if the vector of hits are compatible based on
 // their local angle beta as extracted using the PixelStubs. If one of the 
 // hits is not comptible with another, then the whole set is declared incompatible
-bool SeedComparitorWithPixelStubs::compatible(const SeedingHitSet &seed) {
-  bool good=true;
+bool SeedComparitorWithPixelStubs::compatible(const SeedingHitSet &seed, const edm::EventSetup &es) {
+	edm::ESHandle<MagneticField> magFieldHandle;
+	es.get<IdealMagneticFieldRecord>().get(magFieldHandle);
+	theMagField = &(*magFieldHandle);
+
+	bool good=true;
+	if (method_flag==4){
+	  assert(theCPE);	
+	  theCPE->setMagField( theMagField );
+	}
+	//std::cout << "\nCPE Created" << std::endl;
+	//std::cout << "Beginning compatible run\n\n";
   for (unsigned int i=0; i<seed.hits().size()-1; ++i) {
+    //std::cout << "Beginning loop 1: i = " << i << std::endl;
     const SiPixelRecHit &pix1 = dynamic_cast<const SiPixelRecHit&>(*(seed.hits()[i]));
-    PixelStub stub1(pix1, betaCutFactor_);
+    PixelStub stub1(pix1, betaCutFactor_, tempCutFactor_, method_flag, theCfg, es, *theCPE);
+    
     for (unsigned int j=i+1; j<seed.hits().size(); ++j) {
+      //std::cout << "Beginning loop 2: j = " << j << std::endl;
       const SiPixelRecHit &pix2 = dynamic_cast<const SiPixelRecHit&>(*(seed.hits()[j]));
-      if (pix1.geographicalId().subdetId() != pix2.geographicalId().subdetId()) continue;
-      PixelStub stub2(pix2, betaCutFactor_);
-      
-      // Now make the comparison
-      if (! stub1.compatible(stub2)) good=false;
+			//	std::cout << "\n\n\nIDs: " << pix1.geographicalId().subdetId() << " " << pix2.geographicalId().subdetId() << std::endl;
+			if (method_flag < 3) {
+				if (pix1.geographicalId().subdetId() != pix2.geographicalId().subdetId()){
+					//	std::cout << "Mixed Seeds" << std::endl;
+					continue;
+				}
+			}
+      PixelStub stub2(pix2, betaCutFactor_, tempCutFactor_, method_flag, theCfg, es, *theCPE);
+
+			// Now make the comparison
+      if (! stub1.compatible(stub2)) {
+				good=false;
+				//			std::cout << "Bad Seed Excluded!!!!!" << std::endl;
+			}
     }
   }
-  return good;
+	//std::cout << "Run Complete\n\n\n\n\n";
+	return good;
 }
 
+//void SeedComparitorWithPixelStubs::eventInit(const edm::EventSetup &es)
+//{
+//	theES = es;
+//}
