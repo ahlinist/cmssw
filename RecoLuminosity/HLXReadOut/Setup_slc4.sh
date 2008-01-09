8f3f2b13304c7ff17119de877f09ac4fe0d8be32
jaj99 <>
2008-01-09 14:58:06 +0000

    Removed old setup file, split configurations for slc3 and slc4

---
+++ RecoLuminosity/HLXReadOut/Setup_slc4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Move into config directory (quietly)
+pushd $BUILD_HOME/RecoLuminosity/HLXReadOut/Config &> /dev/null
+
+# define the project
+Project=RecoLuminosity/HLXReadOut
+
+# BaseSetup *MUST* come fist
+source BaseSetup_slc4
+source DIPSetup
+source OracleSetup_slc4
+
+popd &> /dev/null
