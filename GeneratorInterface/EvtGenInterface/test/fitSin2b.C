d7841d8f7065347d28fe8b2f185f50c34cc3ad02
covarell <>
2008-01-16 09:53:36 +0000

    Decay files for new interface version

--- GeneratorInterface/EvtGenInterface/test/fitSin2b.C
+++ GeneratorInterface/EvtGenInterface/test/fitSin2b.C
@@ -21,7 +21,7 @@
   TF1 *sin2b = new TF1("sin2b","[0]*sin([1]*x + [2])",0.,12.);
   sin2b->SetParameters(0.699,0.502,0.0);
 // sin2b->FixParameter(1,0.502);
-  hasymJpsiKs->Fit("sin2b","","e1",0.,10.5);
+  hasymJpsiKs->Fit("sin2b","","e1",0.,12.0);
 
   TLine *line1 = new TLine(0.,0.6997,12.,0.6997);
   TLine *line2 = new TLine(0.,-0.6997,12.,-0.6997);
