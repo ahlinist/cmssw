bf3b51cf4c5ee1ba33957a6e1c3f24beb89a38e9
muriel <>
2008-01-24 14:44:32 +0000

    First commit

---
+++ DQM/HLTObjects/interface/HLTMuonDQMClient.h
@@ -0,0 +1,88 @@
+#ifndef HLTMuonDQMClient_H
+#define HLTMuonDQMClient_H
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include <FWCore/Framework/interface/EDAnalyzer.h>
+#include <FWCore/Framework/interface/ESHandle.h>
+#include <DataFormats/Common/interface/Handle.h>
+#include <FWCore/Framework/interface/Event.h>
+#include <FWCore/Framework/interface/MakerMacros.h>
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/Core/interface/MonitorElementBaseT.h"
+#include "DQMServices/Daemon/interface/MonitorDaemon.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "TH1F.h"
+#include "TH2F.h"
+
+using namespace edm;
+using namespace std;
+
+class HLTMuonDQMClient: public EDAnalyzer{
+
+ public:
+  
+  HLTMuonDQMClient(const ParameterSet& ps);
+  virtual ~HLTMuonDQMClient();
+  
+ protected:
+  
+  void beginJob(const EventSetup& c);
+  void analyze(const Event& e, const EventSetup& c);
+  void endJob();
+  void setup();
+  void cleanup();
+  void doQT();
+
+  string getMEName(const int & zside, const int & plane, const int & type);
+
+  void htmlOutput(int run, string htmlDir, string htmlName);
+
+  void CRtd(void);          // initialize the track display
+  void PlotTrackDisplay(void);
+
+ private:
+  
+  bool writeHisto_;
+  bool writeHTML_;
+  int dumpRate_;
+  string outputFile_;
+  string outputFileName_;
+  string rootFolder_;
+  string htmlDir_;
+  string htmlName_;
+  int count_;
+  int run_;
+  bool sta_;
+  bool init_;
+
+  DaqMonitorBEInterface* dbe_;
+
+  TH1F *hNMu_[2];
+  TH1F *hPt_[2];
+  TH1F *hHPt_[2];
+  TH1F *hEta_[2];
+  TH1F *hPhi_[2];
+  TH2F *hEtaphi_[2];
+  TH1F *hDr_[2];
+  TH1F *hDz_[2];
+  TH1F *hErr0_[2];
+  TH1F *hNhit_[2];
+  TH1F *hIso_[2];
+  TH1F *hDimumass_[2];
+
+  bool x11_;                 // enable the ROOT application's x11 interface?
+  TApplication *theApp_;     // the ROOT application
+  TPad *CRtd_pad_[3];        // Cosmic Run track display pads
+};
+
+#endif
