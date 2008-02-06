b2dd483311d04b07f79be32deeebc1ba1ecce4a0
bainbrid <>
2008-02-06 15:00:28 +0000

    added commissioning calibrations as future new condition objects

---
+++ CondFormats/SiStripObjects/src/EventSetup_Registration.cc
@@ -0,0 +1,5 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+EVENTSETUP_DATA_REG(SiStripFedCabling);
+
