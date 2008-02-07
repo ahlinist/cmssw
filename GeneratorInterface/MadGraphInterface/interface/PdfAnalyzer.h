711c1bdc85fa053a64caa953756f84af22c040b7
dkcira <>
2008-02-07 13:29:11 +0000

    add pdfinfo stuff from C. Hof

--- GeneratorInterface/MadGraphInterface/interface/PdfAnalyzer.h
+++ GeneratorInterface/MadGraphInterface/interface/PdfAnalyzer.h
@@ -1,38 +1,21 @@
 // -*- C++ -*-
-//
 // Package:    PdfAnalyzer
 // Class:      PdfAnalyzer
-// 
 /**\class PdfAnalyzer PdfAnalyzer.cc GeneratorInterface/MadGraphInterface/src/PdfAnalyzer.cc
-
  Description: get pdf info for every event
-
  Implementation:
      <Notes on implementation>
 */
-//
 // Original Author: Carsten Hof
 //         Created:  Wed Jan 23 12:56:49 CET 2008
-// $Id$
-//
-
-// system include files
+// $Id: PdfAnalyzer.h,v 1.1 2008/02/06 17:19:13 dkcira Exp $
 #include <memory>
-
-// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
-
-//
-// class decleration
-//
 
 class PdfAnalyzer : public edm::EDAnalyzer {
    public:
