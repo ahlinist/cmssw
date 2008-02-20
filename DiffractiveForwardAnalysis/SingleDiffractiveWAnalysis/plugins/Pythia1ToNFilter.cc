f38d6285477a523e010f5bb6358876cbc4d5ae5e
antoniov <>
2008-02-20 18:30:27 +0000

    add filters to run on alpgen

--- DiffractiveForwardAnalysis/SingleDiffractiveWAnalysis/plugins/Pythia1ToNFilter.cc
+++ DiffractiveForwardAnalysis/SingleDiffractiveWAnalysis/plugins/Pythia1ToNFilter.cc
@@ -43,29 +43,54 @@ bool Pythia1ToNFilter::filter(edm::Event& evt, const edm::EventSetup& es){
 	evt.getByLabel(genParticlesTag_,genParticles);
 
 	//Find the mother particle...uses the first one it finds.
-	std::vector<const reco::Candidate*> daughters;
+	//std::vector<const reco::Candidate*> daughters;
+	bool accept = false;
 	for(reco::CandidateCollection::const_iterator part = genParticles->begin(); part != genParticles->end(); ++part){
-		if((part->pdgId() == pdgIDs_[0])&&((part->status() == statusCodes_[0])||(part->status() == -1))){
+		if((abs(part->pdgId()) == pdgIDs_[0])&&((part->status() == statusCodes_[0])||(statusCodes_[0] == -1))){
 			//if(part->numberOfDaughters() != pdgIDs_.size()) continue;
-			for(size_t idx = 0; idx < part->numberOfDaughters(); ++idx) daughters.push_back(part->daughter(idx));
-			break;
-		}	
-	}
-	//Loop over daughters
-	std::vector<int> auxvec = pdgIDs_;
-	for(size_t i = 1; i < pdgIDs_.size(); ++i){
-		for(std::vector<const reco::Candidate*>::iterator dau = daughters.begin(); dau != daughters.end(); ++dau){
-			if(((*dau)->pdgId() == pdgIDs_[i])&&(((*dau)->status() == statusCodes_[i])||((*dau)->status() == -1))){
-				auxvec.erase(auxvec.begin() + i);
+			 LogTrace("") << part->pdgId() << "  " << part->status() << "  "
+				      << part->px() << "  " << part->py() << "  "
+				      << part->pz() << "  " << part->energy() << "\n";
+			std::vector<int> auxpdgIds = pdgIDs_;
+			std::vector<int> auxstatusCodes = statusCodes_;
+			for(size_t idx = 0; idx < part->numberOfDaughters(); ++idx){
+				//daughters.push_back(part->daughter(idx));
+				const reco::Candidate* dau = part->daughter(idx);
+				LogTrace("") << dau->pdgId() << "  " << dau->status() << "  "
+                                             << dau->px() << "  " << dau->py() << "  "
+                                             << dau->pz() << "  " << dau->energy() << "\n";
+				//Try to find daughter in pdgId vector
+				for(size_t k = 1; k < auxpdgIds.size(); ++k){
+					if((abs(dau->pdgId()) == auxpdgIds[k])&&((dau->status() == auxstatusCodes[k])||(auxstatusCodes[k] == -1))){
+						LogTrace("") << "Found entry (" << auxpdgIds[k] << "," << auxstatusCodes[k] << ")\n";
+						auxpdgIds.erase(auxpdgIds.begin() + k);
+						auxstatusCodes.erase(auxstatusCodes.begin() + k);
+					}
+					
+				}
+			}
+			if((auxpdgIds.size() == 1)&&(auxpdgIds[0] == abs(part->pdgId()))){
+				accept = true;
+				//Print accepted chain
+				LogTrace("") << "Accepted 1->N chain:\n"
+					     << part->pdgId() << "  " << part->status() << "  "
+					     << part->px() << "  " << part->py() << "  "
+					     << part->pz() << "  " << part->energy() << "\n";
+				for(size_t idx = 0; idx < part->numberOfDaughters(); ++idx){
+					const reco::Candidate* dau = part->daughter(idx);
+					LogTrace("") << dau->pdgId() << "  " << dau->status() << "  "
+						     << dau->px() << "  " << dau->py() << "  "
+						     << dau->pz() << "  " << dau->energy() << "\n";
+				}		
 				break;
 			}
-		}
+		}	
 	}
-	
-	LogTrace("") << "Missing entries:\n";
-	for(size_t i = 1; i < pdgIDs_.size(); ++i) LogTrace("") << "(" << pdgIDs_[i] << "," << statusCodes_[i] << ")\n";
-	
-	return (auxvec.size() == 1);
+	if(!accept) LogTrace("") << "Event rejected!\n";
+	else LogTrace("") << "Event selected!\n";
+
+	return accept;
 }
+
 DEFINE_FWK_MODULE(Pythia1ToNFilter);
 
