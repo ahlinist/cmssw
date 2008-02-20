ef873543f394453ea6b8688a066cb3ea601cd845
ursl <>
2008-02-20 10:07:43 +0000

    put in second TH1 filling

--- HeavyFlavorAnalysis/Examples/src/HFDumpStuff.cc
+++ HeavyFlavorAnalysis/Examples/src/HFDumpStuff.cc
@@ -5,7 +5,7 @@
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
-
+#include "DataFormats/METReco/interface/GenMETCollection.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
 
@@ -27,12 +27,14 @@ using namespace edm;
 // ----------------------------------------------------------------------
 HFDumpStuff::HFDumpStuff(const edm::ParameterSet& iConfig):
   fGenEventScaleLabel(iConfig.getUntrackedParameter<string>("GenEventScaleLabel", string("genEventScale"))),
-  fMETLabel(iConfig.getUntrackedParameter<string>("METLabel", string("corMetType1Icone5")))
+  fMETLabel(iConfig.getUntrackedParameter<string>("METLabel", string("corMetType1Icone5"))),
+  fGenMETLabel(iConfig.getUntrackedParameter<string>("GenMETLabel", string("genMet")))
 {
   cout << "----------------------------------------------------------------------" << endl;
   cout << "--- HFDumpStuff constructor" << endl;
   cout << "--- " << fGenEventScaleLabel.c_str() << endl;
   cout << "--- " << fMETLabel.c_str() << endl;
+  cout << "--- " << fGenMETLabel.c_str() << endl;
   cout << "----------------------------------------------------------------------" << endl;
 }
 
@@ -48,8 +50,8 @@ void HFDumpStuff::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
   // -- pthat 
   gHFEvent->fPtHat = -1.; 
-  edm::Handle<double> genEventScaleHandle;
   try {
+    edm::Handle<double> genEventScaleHandle;
     iEvent.getByLabel(fGenEventScaleLabel.c_str(), genEventScaleHandle);
     gHFEvent->fPtHat = *genEventScaleHandle;
   } catch (cms::Exception &ex) {
@@ -59,16 +61,35 @@ void HFDumpStuff::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
 
   // -- MET
-  Handle<CaloMETCollection> metHandle;
   try {
+    Handle<CaloMETCollection> metHandle;
     iEvent.getByLabel(fMETLabel.c_str(), metHandle);
+    const reco::CaloMET *met = &metHandle.product()->at(0);
+    gHFEvent->fMET0 = TVector3(met->p4().x(), met->p4().y(), 0.);  
   } catch (cms::Exception &ex) {
     //    cout << ex.explainSelf() << endl;
     cout << "MET " << fMETLabel.c_str() << " not found " << endl;
   }
 
-  const reco::CaloMET *met = &metHandle.product()->at(0);
-  gHFEvent->fMET0 = TVector3(met->p4().x(), met->p4().y(), 0.);  
+
+  // -- MET from generator level
+  try{
+    Handle<GenMETCollection> gen;
+    iEvent.getByLabel(fGenMETLabel.c_str(), gen);
+    const GenMETCollection *genmetcol = gen.product();
+    const GenMET genmet = genmetcol->front();
+    gHFEvent->fGenMET = TVector3(genmet.px(), genmet.py(), 0.);  
+  } catch (cms::Exception &ex) {
+    //    cout << ex.explainSelf() << endl;
+    cout << "GenMET " << fGenMETLabel.c_str() << " not found " << endl;
+  }
+
+  cout << "==HFDumpStuff> GenMET =  (" 
+       << gHFEvent->fGenMET.X() << "," 
+       << gHFEvent->fGenMET.Y() << "), mag = " 
+       << gHFEvent->fGenMET.Mag() 
+       << endl;
+
   cout << "==HFDumpStuff> MET =  (" 
        << gHFEvent->fMET0.X() << "," 
        << gHFEvent->fMET0.Y() << "), mag = " 
