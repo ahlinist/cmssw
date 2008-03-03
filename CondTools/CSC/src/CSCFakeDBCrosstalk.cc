55b55aada0389e6190185698e4b944e1c46c9e54
boeriu <>
2008-03-03 17:44:47 +0000

    added

---
+++ CondTools/CSC/src/CSCFakeDBCrosstalk.cc
@@ -0,0 +1,45 @@
+#include "CondTools/CSC/interface/CSCFakeDBCrosstalk.h"
+#include "CondFormats/CSCObjects/interface/CSCDBCrosstalk.h"
+#include "CondFormats/DataRecord/interface/CSCDBCrosstalkRcd.h"
+#include "CondTools/CSC/interface/CSCCrosstalkDBConditions.h"
+
+CSCFakeDBCrosstalk::CSCFakeDBCrosstalk(const edm::ParameterSet& iConfig)
+{
+  //the following line is needed to tell the framework what
+  // data is being produced
+  cndbCrosstalk = prefillDBCrosstalk();
+  setWhatProduced(this,&CSCFakeDBCrosstalk::produceDBCrosstalk);
+  findingRecord<CSCDBCrosstalkRcd>();
+}
+
+
+CSCFakeDBCrosstalk::~CSCFakeDBCrosstalk()
+{
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+  delete cndbCrosstalk;
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+CSCFakeDBCrosstalk::ReturnType
+CSCFakeDBCrosstalk::produceDBCrosstalk(const CSCDBCrosstalkRcd& iRecord)
+{
+  //need a new object so to not be deleted at exit
+  CSCDBCrosstalk* mydata=new CSCDBCrosstalk( *cndbCrosstalk );
+  return mydata;
+
+}
+
+ void CSCFakeDBCrosstalk::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
+ edm::ValidityInterval & oValidity)
+ {
+ oValidity = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(),edm::IOVSyncValue::endOfTime());
+ 
+ }
+
+
---
+++ CondTools/CSC/src/CSCFakeDBPedestals.cc
@@ -0,0 +1,42 @@
+#include "CondTools/CSC/interface/CSCFakeDBPedestals.h"
+#include "CondFormats/CSCObjects/interface/CSCDBPedestals.h"
+#include "CondFormats/DataRecord/interface/CSCDBPedestalsRcd.h"
+#include "CondTools/CSC/interface/CSCPedestalsDBConditions.h"
+
+CSCFakeDBPedestals::CSCFakeDBPedestals(const edm::ParameterSet& iConfig)
+{
+  //the following line is needed to tell the framework what
+  // data is being produced
+  cndbPedestals = prefillDBPedestals();
+  setWhatProduced(this,&CSCFakeDBPedestals::produceDBPedestals);
+  findingRecord<CSCDBPedestalsRcd>();
+  //now do what ever other initialization is needed
+}
+
+
+CSCFakeDBPedestals::~CSCFakeDBPedestals()
+{
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+  delete cndbPedestals;
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+CSCFakeDBPedestals::ReturnType
+CSCFakeDBPedestals::produceDBPedestals(const CSCDBPedestalsRcd& iRecord)
+{
+  CSCDBPedestals* mydata = new CSCDBPedestals( *cndbPedestals);
+  return mydata;
+}
+
+ void CSCFakeDBPedestals::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
+ edm::ValidityInterval & oValidity)
+ {
+ oValidity = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(),edm::IOVSyncValue::endOfTime());
+ 
+ }
