d1b4dbc0e85f958f253a785e393f3bb5e30b2ea7
boeriu <>
2008-03-03 17:44:47 +0000

    added

---
+++ CondTools/CSC/src/CSCFakeDBNoiseMatrix.cc
@@ -0,0 +1,40 @@
+#include "CondTools/CSC/interface/CSCFakeDBNoiseMatrix.h"
+#include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
+#include "CondFormats/DataRecord/interface/CSCDBNoiseMatrixRcd.h"
+#include "CondTools/CSC/interface/CSCNoiseMatrixDBConditions.h"
+
+CSCFakeDBNoiseMatrix::CSCFakeDBNoiseMatrix(const edm::ParameterSet& iConfig)
+{
+  //tell the framework what data is being produced
+  cndbNoiseMatrix = prefillDBNoiseMatrix();  
+  setWhatProduced(this,&CSCFakeDBNoiseMatrix::produceDBNoiseMatrix);  
+  findingRecord<CSCDBNoiseMatrixRcd>();
+}
+
+
+CSCFakeDBNoiseMatrix::~CSCFakeDBNoiseMatrix()
+{
+  // do anything here that needs to be done at destruction time
+  // (e.g. close files, deallocate resources etc.)
+  delete cndbNoiseMatrix; 
+}
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+CSCFakeDBNoiseMatrix::ReturnType
+CSCFakeDBNoiseMatrix::produceDBNoiseMatrix(const CSCDBNoiseMatrixRcd& iRecord)
+{
+  //need a new object so to not be deleted at exit
+  CSCDBNoiseMatrix* mydata=new CSCDBNoiseMatrix( *cndbNoiseMatrix );
+  return mydata;
+}
+
+void CSCFakeDBNoiseMatrix::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
+						  edm::ValidityInterval & oValidity)
+{
+  oValidity = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(),edm::IOVSyncValue::endOfTime());
+  
+}
