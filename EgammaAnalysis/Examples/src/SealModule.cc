44c6e421171511e9a2e2411274dabd5348c17aeb
ppereira <>
2008-03-14 00:26:03 +0000

    First version

--- EgammaAnalysis/Examples/src/SealModule.cc
+++ EgammaAnalysis/Examples/src/SealModule.cc
@@ -1,12 +1,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "EgammaAnalysis/Examples/interface/EgammaIsolationAnalyser.h"
-#include "EgammaAnalysis/Examples/interface/ElectronIDValAnalyzer.h"
-#include "EgammaAnalysis/Examples/interface/ElectronIDFakeRateAnalyzer.h"
+#include "EgammaAnalysis/Examples/interface/ElectronIDAnalyzer.h"
+
 
 DEFINE_SEAL_MODULE();
 
-DEFINE_ANOTHER_FWK_MODULE(EgammaIsolationAnalyser);
-DEFINE_ANOTHER_FWK_MODULE(ElectronIDValAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(ElectronIDFakeRateAnalyzer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaIsolationAnalyser);
+DEFINE_ANOTHER_FWK_MODULE(ElectronIDAnalyzer);
+
 
