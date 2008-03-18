3acf9f9023ca5187e61b9f3ea16d0b3f9689e7c9
bazterra <>
2008-03-18 17:29:36 +0000

    New file names fo the files in PrelSolver directory.

---
+++ RecoBTag/PerformanceMeasurements/test/PtrelSolver/PtrelSolverLinkDef.h
@@ -0,0 +1,15 @@
+
+#ifdef NOSCRAMV
+#include "PtrelSolver.h"
+#else
+#include "RecoBTag/PerformanceMeasurements/test/PtrelSolver/PtrelSolver.h"
+#endif
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class PtrelSolver+; 
+
+#endif
