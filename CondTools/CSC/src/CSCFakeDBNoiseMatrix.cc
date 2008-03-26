c5fc8b392b6db46d31ae0937d47992344f67d699
boeriu <>
2008-03-26 13:48:19 +0000

    using boost/shared_ptr

--- CondTools/CSC/src/CSCFakeDBNoiseMatrix.cc
+++ CondTools/CSC/src/CSCFakeDBNoiseMatrix.cc
@@ -1,12 +1,12 @@
-#include "CondTools/CSC/interface/CSCFakeDBNoiseMatrix.h"
+#include "CalibMuon/CSCCalibration/interface/CSCFakeDBNoiseMatrix.h"
 #include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
 #include "CondFormats/DataRecord/interface/CSCDBNoiseMatrixRcd.h"
-#include "CondTools/CSC/interface/CSCNoiseMatrixDBConditions.h"
+#include "CalibMuon/CSCCalibration/interface/CSCNoiseMatrixDBConditions.h"
 
 CSCFakeDBNoiseMatrix::CSCFakeDBNoiseMatrix(const edm::ParameterSet& iConfig)
 {
+  cndbNoiseMatrix = boost::shared_ptr<CSCDBNoiseMatrix> ( prefillDBNoiseMatrix() );  
   //tell the framework what data is being produced
-  cndbNoiseMatrix = prefillDBNoiseMatrix();  
   setWhatProduced(this,&CSCFakeDBNoiseMatrix::produceDBNoiseMatrix);  
   findingRecord<CSCDBNoiseMatrixRcd>();
 }
@@ -14,22 +14,13 @@ CSCFakeDBNoiseMatrix::CSCFakeDBNoiseMatrix(const edm::ParameterSet& iConfig)
 
 CSCFakeDBNoiseMatrix::~CSCFakeDBNoiseMatrix()
 {
-  // do anything here that needs to be done at destruction time
-  // (e.g. close files, deallocate resources etc.)
-  delete cndbNoiseMatrix; 
 }
 
-//
-// member functions
-//
-
 // ------------ method called to produce the data  ------------
-CSCFakeDBNoiseMatrix::ReturnType
+CSCFakeDBNoiseMatrix::Pointer
 CSCFakeDBNoiseMatrix::produceDBNoiseMatrix(const CSCDBNoiseMatrixRcd& iRecord)
 {
-  //need a new object so to not be deleted at exit
-  CSCDBNoiseMatrix* mydata=new CSCDBNoiseMatrix( *cndbNoiseMatrix );
-  return mydata;
+  return cndbNoiseMatrix;
 }
 
 void CSCFakeDBNoiseMatrix::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
--- CondTools/CSC/src/CSCFakeDBPedestals.cc
+++ CondTools/CSC/src/CSCFakeDBPedestals.cc
@@ -1,37 +1,28 @@
-#include "CondTools/CSC/interface/CSCFakeDBPedestals.h"
+#include "CalibMuon/CSCCalibration/interface/CSCFakeDBPedestals.h"
 #include "CondFormats/CSCObjects/interface/CSCDBPedestals.h"
 #include "CondFormats/DataRecord/interface/CSCDBPedestalsRcd.h"
-#include "CondTools/CSC/interface/CSCPedestalsDBConditions.h"
+#include "CalibMuon/CSCCalibration/interface/CSCPedestalsDBConditions.h"
 
 CSCFakeDBPedestals::CSCFakeDBPedestals(const edm::ParameterSet& iConfig)
 {
+  cndbPedestals = boost::shared_ptr<CSCDBPedestals> ( prefillDBPedestals() );
+
   //the following line is needed to tell the framework what
   // data is being produced
-  cndbPedestals = prefillDBPedestals();
   setWhatProduced(this,&CSCFakeDBPedestals::produceDBPedestals);
   findingRecord<CSCDBPedestalsRcd>();
-  //now do what ever other initialization is needed
 }
 
 
 CSCFakeDBPedestals::~CSCFakeDBPedestals()
 {
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-  delete cndbPedestals;
 }
 
-
-//
-// member functions
-//
-
 // ------------ method called to produce the data  ------------
-CSCFakeDBPedestals::ReturnType
+CSCFakeDBPedestals::Pointer
 CSCFakeDBPedestals::produceDBPedestals(const CSCDBPedestalsRcd& iRecord)
 {
-  CSCDBPedestals* mydata = new CSCDBPedestals( *cndbPedestals);
-  return mydata;
+  return cndbPedestals;
 }
 
  void CSCFakeDBPedestals::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
