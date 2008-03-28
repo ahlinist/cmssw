9f26312125a30b77a40ca44da11315c51adf609d
rpw <>
2008-03-28 10:16:48 +0000

    adding new py configs

---
+++ RecoBTag/Analysis/python/bTagTrackIPAnalysis_cff.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+# TrackIP tag info configuration
+parameters = cms.PSet(
+    endEffPur = cms.double(1.005),
+    nBinEffPur = cms.int32(200),
+    startEffPur = cms.double(0.005)
+)
+
