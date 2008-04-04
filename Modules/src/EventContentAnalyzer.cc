050054e83f477e04d0d44a489517935fbf8b5973
wmtan <>
2008-04-04 22:35:26 +0000

    Begin event numbering at 1, not 0

--- Modules/src/EventContentAnalyzer.cc
+++ Modules/src/EventContentAnalyzer.cc
@@ -252,7 +252,7 @@ EventContentAnalyzer::EventContentAnalyzer(const edm::ParameterSet& iConfig) :
   verbose_(iConfig.getUntrackedParameter("verbose",false) || moduleLabels_.size()>0),
   getModuleLabels_(iConfig.getUntrackedParameter("getDataForModuleLabels",std::vector<std::string>())),
   getData_(iConfig.getUntrackedParameter("getData",false) || getModuleLabels_.size()>0),
-  evno_(0)
+  evno_(1)
 {
    //now do what ever initialization is needed
    edm::sort_all(moduleLabels_);
@@ -295,7 +295,6 @@ EventContentAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& i
    for(Provenances::iterator itProv = provenances.begin(), itProvEnd = provenances.end();
                              itProv != itProvEnd;
                            ++itProv) {
-     if((*itProv)->product().present()) {
        friendlyName = (*itProv)->friendlyClassName();
        //if(friendlyName.empty())  friendlyName = std::string("||");
        
@@ -350,7 +349,6 @@ EventContentAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& i
                              handle);
          }
        }      
-     }
    }
    //std::cout <<"Mine"<<std::endl;
    ++evno_;
