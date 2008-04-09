497d0a30c4d061ed3af6b79ba2363ce5bd37bac3
marafino <>
2008-04-09 14:25:40 +0000

    Insert new and changed files to accomodate running Comphep either as a source or as a producer.

--- GeneratorInterface/ComphepInterface/src/SealModule.cc
+++ GeneratorInterface/ComphepInterface/src/SealModule.cc
@@ -1,7 +1,11 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 #include "GeneratorInterface/ComphepInterface/interface/ComphepSource.h"
+#include "GeneratorInterface/ComphepInterface/interface/ComphepProducer.h"
 
   using edm::ComphepSource;
+  using edm::ComphepProducer;
 
   DEFINE_SEAL_MODULE();
-  DEFINE_ANOTHER_FWK_INPUT_SOURCE(ComphepSource);
+  DEFINE_FWK_INPUT_SOURCE(ComphepSource);
+  DEFINE_FWK_MODULE(ComphepProducer);
