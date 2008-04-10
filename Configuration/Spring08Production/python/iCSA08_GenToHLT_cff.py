546be171424e6c3834c7f503d93b05ef1e603a3c
hegner <>
2008-04-10 20:01:35 +0000

    iCSA08 config infrastructure

---
+++ Configuration/Spring08Production/python/iCSA08_GenToHLT_cff.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+# prepare the process
+process = cms.Process("HLT")
+
+# load defaults in old cfg syntax
+process.include("Configuration/Spring08Production/data/iCSA08_GenToHLT.cff")
+
+process.output_module.dataset.dataTier = "TOBEDEFINED"
+
