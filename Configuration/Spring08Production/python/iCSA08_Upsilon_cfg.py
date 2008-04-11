c0a388813f0fc7b36b2b6b091bb6eaf7a1ca1724
hegner <>
2008-04-11 18:16:25 +0000

    reducing verbosity for tests

--- Configuration/Spring08Production/python/iCSA08_Upsilon_cfg.py
+++ Configuration/Spring08Production/python/iCSA08_Upsilon_cfg.py
@@ -11,7 +11,7 @@ process.include("Configuration/Spring08Production/data/iCSA08_Upsilon.cff")
 # process metadata
 process.configurationMetadata = cms.untracked.PSet( 
                                 version =    cms.untracked.string( "$Revision: 1.1 $" ),
-                                name    =    cms.untracked.string( "$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Spring08Production/python/iCSA08_MuonPT11_cfg.py,v $" ),
+                                name    =    cms.untracked.string( "$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Spring08Production/python/iCSA08_Upsilon_cfg.py,v $" ),
                                 annotation = cms.untracked.string( "generation of prompt Upsilon COM+CSM, CTEQ6L1 used" ) )
 
 
@@ -40,9 +40,5 @@ for pathName in process.endpaths:
 # selection criteria
 process.output_module.SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring( "production") )
 
-
-# increasing verbosity
-process.MessageLogger.cerr.threshold = "INFO"
-process.add_(cms.Service("Tracer"))
 
 process.maxEvents.input = 1
