40c1c4625e363a8877dfe6e002769e8b5f2d979d
hegner <>
2008-04-21 16:42:40 +0000

    fixing Rick's last changes

--- Configuration/Spring08Production/python/iCSA08_TrackerLaser_cff.py
+++ Configuration/Spring08Production/python/iCSA08_TrackerLaser_cff.py
@@ -1,12 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
-configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Spring08Production/data/iCSA08_TrackerLaser.cff,v $'),
-    annotation = cms.untracked.string('Simulation of the Tracker Laser Alignment System for CSA08')
-)
-source = cms.Source("LaserAlignmentSource",
-    firstRun = cms.untracked.uint32(1)
-)
+def customise(process):
 
+  #Geometry
+  # add the description of the Alignment Tubes
+  process.XMLIdealGeometryESSource.geomXMLFiles.append("Alignment/LaserAlignmentSimulation/data/AlignmentTubes.xml")
 
+  process.siStripDigis.ZeroSuppression = False # produce "VirginRaw" SiStripRawDigis
+
+  process.g4SimHits.Physics.type = "SimG4Core/Physics/LaserOpticalPhysics"
+
+  process.g4SimHits.Watchers = cms.VPSet( 
+    cms.PSet( 
+       type = cms.string("LaserAlignmentSimulation"),
+       DebugLevel = cms.untracked.int32(0),
+       MaterialPropertiesDebugLevel = cms.untracked.int32(1),
+       SiAbsorptionLengthScalingFactor = cms.untracked.double(1.0),
+       EnergyLossScalingFactor = cms.untracked.double(1739.130435),
+       PhotonEnergy = cms.untracked.double(1.15),
+       NumberOfPhotonsInParticleGun = cms.untracked.int32(50),
+       NumberOfPhotonsInEachBeam = cms.untracked.int32(50),
+    )
+  )
+  
+
+  return process
