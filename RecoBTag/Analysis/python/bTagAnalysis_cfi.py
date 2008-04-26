b37c0312e7bb8bb210c3d546e0b3f650cfc8d8d5
saout <>
2008-04-26 12:31:29 +0000

    Python and runtime fixes.

--- RecoBTag/Analysis/python/bTagAnalysis_cfi.py
+++ RecoBTag/Analysis/python/bTagAnalysis_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 # BTagPerformanceAnalyzer configuration
-from RecoBTag.Analysis.bTagCombinedSVVariables_cff import *
-#includes added because of block refactoring replacing simple includes by using statements
 from RecoBTag.Analysis.bTagTrackIPAnalysis_cff import *
 from RecoBTag.Analysis.bTagCombinedSVAnalysis_cff import *
 from RecoBTag.Analysis.bTagTrackCountingAnalysis_cff import *
@@ -11,66 +9,72 @@ from RecoBTag.Analysis.bTagGenericAnalysis_cff import *
 from RecoBTag.Analysis.bTagSimpleSVAnalysis_cff import *
 from RecoBTag.Analysis.bTagSoftLeptonAnalysis_cff import *
 from RecoBTag.Analysis.bTagCommon_cff import *
+
 bTagAnalysis = cms.EDAnalyzer("BTagPerformanceAnalyzer",
-    # parameters common to all tagger algorithms
-    bTagCommonBlock,
-    # output root file
-    rootfile = cms.string('jetTagAnalysis.root'),
-    tagConfig = cms.VPSet(cms.PSet(
-        bTagTrackIPAnalysisBlock,
-        type = cms.string('TrackIP'),
-        label = cms.InputTag("impactParameterTagInfos")
-    ), 
-        cms.PSet(
-            bTagCombinedSVAnalysisBlock,
-            tagInfos = cms.VInputTag(cms.InputTag("impactParameterTagInfos"), cms.InputTag("secondaryVertexTagInfos")),
-            type = cms.string('GenericMVA'),
-            label = cms.InputTag("combinedSecondaryVertex")
-        ), 
-        cms.PSet(
-            bTagTrackCountingAnalysisBlock,
-            label = cms.InputTag("trackCountingHighEffBJetTags")
-        ), 
-        cms.PSet(
-            bTagTrackCountingAnalysisBlock,
-            label = cms.InputTag("trackCountingHighPurBJetTags")
-        ), 
-        cms.PSet(
-            bTagProbabilityAnalysisBlock,
-            label = cms.InputTag("jetProbabilityBJetTags")
-        ), 
-        cms.PSet(
-            bTagProbabilityAnalysisBlock,
-            label = cms.InputTag("jetBProbabilityBJetTags")
-        ), 
-        cms.PSet(
-            bTagGenericAnalysisBlock,
-            label = cms.InputTag("impactParameterMVABJetTags")
-        ), 
-        cms.PSet(
-            bTagSimpleSVAnalysisBlock,
-            label = cms.InputTag("simpleSecondaryVertexBJetTags")
-        ), 
-        cms.PSet(
-            bTagGenericAnalysisBlock,
-            label = cms.InputTag("combinedSecondaryVertexBJetTags")
-        ), 
-        cms.PSet(
-            bTagGenericAnalysisBlock,
-            label = cms.InputTag("combinedSecondaryVertexMVABJetTags")
-        ), 
-        cms.PSet(
-            bTagSoftLeptonAnalysisBlock,
-            label = cms.InputTag("softMuonBJetTags")
-        ), 
-        cms.PSet(
-            bTagSoftLeptonAnalysisBlock,
-            label = cms.InputTag("softMuonNoIPBJetTags")
-        ), 
-        cms.PSet(
-            bTagSoftLeptonAnalysisBlock,
-            label = cms.InputTag("softElectronBJetTags")
-        ))
+	# parameters common to all tagger algorithms
+	bTagCommonBlock,
+
+	# output root file
+	rootfile = cms.string('jetTagAnalysis.root'),
+
+	# all taggers and tagInfos to analyze
+	tagConfig = cms.VPSet(
+		cms.PSet(
+			bTagTrackIPAnalysisBlock,
+			type = cms.string('TrackIP'),
+			label = cms.InputTag("impactParameterTagInfos")
+		), 
+		cms.PSet(
+			bTagCombinedSVAnalysisBlock,
+			tagInfos = cms.VInputTag(cms.InputTag("impactParameterTagInfos"), cms.InputTag("secondaryVertexTagInfos")),
+			type = cms.string('GenericMVA'),
+			label = cms.InputTag("combinedSecondaryVertex")
+		), 
+		cms.PSet(
+			bTagTrackCountingAnalysisBlock,
+			label = cms.InputTag("trackCountingHighEffBJetTags")
+		), 
+		cms.PSet(
+			bTagTrackCountingAnalysisBlock,
+			label = cms.InputTag("trackCountingHighPurBJetTags")
+		), 
+		cms.PSet(
+			bTagProbabilityAnalysisBlock,
+			label = cms.InputTag("jetProbabilityBJetTags")
+		), 
+		cms.PSet(
+			bTagProbabilityAnalysisBlock,
+			label = cms.InputTag("jetBProbabilityBJetTags")
+		), 
+		cms.PSet(
+			bTagGenericAnalysisBlock,
+			label = cms.InputTag("impactParameterMVABJetTags")
+		), 
+		cms.PSet(
+			bTagSimpleSVAnalysisBlock,
+			label = cms.InputTag("simpleSecondaryVertexBJetTags")
+		), 
+		cms.PSet(
+			bTagGenericAnalysisBlock,
+			label = cms.InputTag("combinedSecondaryVertexBJetTags")
+		), 
+		cms.PSet(
+			bTagGenericAnalysisBlock,
+			label = cms.InputTag("combinedSecondaryVertexMVABJetTags")
+		), 
+		cms.PSet(
+			bTagSoftLeptonAnalysisBlock,
+			label = cms.InputTag("softMuonBJetTags")
+		), 
+		cms.PSet(
+			bTagSoftLeptonAnalysisBlock,
+			label = cms.InputTag("softMuonNoIPBJetTags")
+		), 
+		cms.PSet(
+			bTagSoftLeptonAnalysisBlock,
+			label = cms.InputTag("softElectronBJetTags")
+		)
+	)
 )
 
 
