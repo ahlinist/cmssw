dbb92776e301bd7726f482d0a894dbbecd4427c0
meschi <>
2008-04-28 13:51:11 +0000

    initial versions in cvs

---
+++ EventFilter/Deployment/scripts/fix_libs.pl
@@ -0,0 +1,5 @@
+#!/usr/local/bin/perl
+$libdir = @ARGV[0];
+$moddir = @ARGV[1];
+print "fixing library clone at $libdir\n";
+print "***no libraries currently need fixing***";
---
+++ EventFilter/Deployment/scripts/patch-cmssw-build.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+export PATH=${PATH}:EventFilter/Deployment/scripts
+export SCRAM_ARCH=slc4onl_ia32_gcc346
+cat > EventFilter/Deployments/scripts/scramv1 <<EOF
+/opt/cmssw/slc4onl_ia32_gcc346/lcg/SCRAMV1/V1_2_1/bin/scram \$@
+EOF
+chmod +x EventFilter/Deployment/scripts/scramv1
+numArgs=$#;
+if [ $numArgs -lt 2 ]; then
+    echo "Usage: patch-cmssw-build.sh CMSSW_X_Y_Z patchdir";
+    exit -1;
+fi
+CMS_SW_VERSION=$1;
+LOCAL_CODE_PATCHES_TOP=$2;
+#source /opt/cmssw/cmsset_default.sh; no longer needed 
+mkdir patch-cmssw-tmp;
+cd patch-cmssw-tmp;
+echo "now in $PWD";
+TOPD=$PWD;
+scramv1 project CMSSW $CMS_SW_VERSION;
+cd $CMS_SW_VERSION/src;
+echo "now in $PWD";
+# dirty kludge for now should get packages with tags from CVS 
+echo "I am supposed to check out some code here";
+cp -r $LOCAL_CODE_PATCHES_TOP/* .;
+# end kludge
+scramv1 b;
+cd -;
+echo "now in $PWD";
+echo "Moving patches to their destination";
+mkdir -p  opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION;
+cp -r $TOPD/$CMS_SW_VERSION/src $TOPD/opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/;
+cp -r $TOPD/$CMS_SW_VERSION/lib $TOPD/opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/;
+cp -r $TOPD/$CMS_SW_VERSION/module $TOPD/opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/;
+echo "Generating and populating summary directories";
+cd /opt/cmssw/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/src/; 
+echo "now in $PWD";
+install_env.pl $TOPD/opt/cmssw/lib/ $TOPD/opt/cmssw/module/ $TOPD/opt/cmssw/env.txt; 
+cd -;
+echo "now in $PWD";
+for x in opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/lib/slc4onl_ia32_gcc346/*.so 
+do
+  if [ -e $x ] 
+      then 
+      ln -sf $PWD/$x opt/cmssw/lib;
+  fi
+done
+for x in opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/module/slc4onl_ia32_gcc346/*.reg
+do
+  if [ -e $x ] 
+      then 
+      ln -sf /$x opt/cmssw/module;
+  fi
+done
+echo "Pasting seal cache entries from patch into mainstream";
+cat opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/module/slc4onl_ia32_gcc346/.cache >>  opt/cmssw/module/.cache;
+echo "Updating edm plugin registry with patches";
+cd $TOPD/$CMS_SW_VERSION;
+echo "now in $PWD";
+RESPONSE=`reset_edm_cache.pl $TOPD/opt/cmssw/lib $TOPD/opt/cmssw_module /dev/null`;
+echo "reset_edm_cache returned $RESPONSE";
+### Extract the root seal and oracle version from the scram runtime here before
+### making the symbolic links
+RT=`scramv1 runtime -sh | grep ROOTSYS | tr -d ";" | perl -p -e 's/.*=\"([^\"]*)\"$/$1/'`
+SL=`scramv1 runtime -sh | grep SEAL= | tr -d ";" | perl -p -e 's/.*=\"([^\"]*)\"$/$1/'`
+echo "extracting symbolic link locations for root and seal from scram runtime \n"; 
+echo "rootsys at $RT\n";
+echo "seal at $SL\n"; 
+#fix symbolic links for patched libs so it is visible after installation
+cd $TOPD;
+echo "now in $PWD";
+for x in opt/cmssw/patches/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION/lib/slc4onl_ia32_gcc346/*.so 
+do
+  if [ -e $x ] 
+      then 
+      ln -sf /$x opt/cmssw/lib;
+  fi
+done
+cd $TOPD;
+echo "now in $PWD";
+# remove temporary directory holding the local patches
+#rm -rf $TOPD/$CMS_SW_VERSION;
+ln -s /opt/cmssw/slc4onl_ia32_gcc346/cms/online/$CMS_SW_VERSION opt/cmssw/base
+ln -s $RT opt/cmssw/root
+ln -s $SL opt/cmssw/seal
+cat > patch-cmssw.spec <<EOF
+Name: patch-cmssw
+Version: $CMS_SW_VERSION
+Release: 6
+Summary: CMSSW Patches
+License: Unknown
+Group: Hacks
+Packager: scram
+Source: none
+%define _tmppath $TOPD
+BuildRoot: %{_tmppath}/patch-cmssw-2-0-0
+BuildArch: i386
+Provides:/opt/cmssw/lib
+Provides:/opt/cmssw/module
+Provides:/opt/cmssw/base
+Provides:/opt/cmssw/root
+Provides:/opt/cmssw/seal
+Provides:/opt/cmssw/env.txt
+Requires:cms+online+$CMS_SW_VERSION
+
+
+%description
+Creates directories with symbolic links to all cmssw-related libraries.
+Installs patches to cmssw code
+
+%prep
+%build
+%install
+rm -rf \$RPM_BUILD_ROOT
+mkdir -p \${RPM_BUILD_ROOT}
+tar -C $TOPD -c opt/cmssw |tar -xC \$RPM_BUILD_ROOT
+%files
+/opt/cmssw/lib
+/opt/cmssw/module
+/opt/cmssw/env.txt
+/opt/cmssw/lib/.edmplugincache
+/opt/cmssw/module/.cache
+/opt/cmssw/base
+/opt/cmssw/root
+/opt/cmssw/seal
+/opt/cmssw/patches
+EOF
+mkdir -p RPMBUILD/{RPMS/{i386,i586,i686,x86_64},SPECS,BUILD,SOURCES,SRPMS}
+rpmbuild --define "_topdir `pwd`/RPMBUILD" -bb patch-cmssw.spec
+#rm -rf patch-cmssw-tmp;
