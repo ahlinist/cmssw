ccd0df0db582acb8cf8f2de51d6ce138b0d243b7
dlange <>
2008-05-11 22:05:32 +0000

    remove unneeded customize stanza (replaced)

--- Configuration/Spring08Production/python/iCSA08_TkCosmicBOFF_RECO_cff.py
+++ Configuration/Spring08Production/python/iCSA08_TkCosmicBOFF_RECO_cff.py
@@ -2,10 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 def customise(process):
 
-    process.include("Configuration/GlobalRuns/data/ReconstructionGR.cff")
-
-    process.reconstruction_step._seq = process.reconstructionGR
-
     # switch off magnetic field
     process.UniformMagneticFieldESProducer = cms.ESProducer("UniformMagneticFieldESProducer", ZFieldInTesla = cms.double(0.0) )
     process.es_prefer_UniformMagneticFieldESProducer = cms.ESPrefer("UniformMagneticFieldESProducer")
