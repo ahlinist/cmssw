7176ef7c34a849ac7616b73c537b497309d154dd
hegner <>
2008-05-12 20:48:08 +0000

    sync of muon and tk cosmic settings

--- Configuration/Spring08Production/python/iCSA08_TkCosmicBON_RECO_cff.py
+++ Configuration/Spring08Production/python/iCSA08_TkCosmicBON_RECO_cff.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def customise(process):
 
-    process.include("Configuration/GlobalRuns/data/ReconstructionGR.cff")
 
-    process.reconstruction_step._seq = process.reconstructionGR
-
+    process.es_prefer_VolumeBasedMagneticFieldESProducer = cms.ESPrefer("VolumeBasedMagneticFieldESProducer")
+            
     return process
