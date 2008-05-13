5fa80b6b5cff2dd963f204c219484b180133eb3f
neadam <>
2008-05-13 07:13:53 +0000

    Updates to HLX render plugins.

--- DQM/RenderPlugins/src/HLXRenderPlugin.cc
+++ DQM/RenderPlugins/src/HLXRenderPlugin.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  
 //         Created:  Sat Apr 19 20:02:57 CEST 2008
-// $Id: HLXRenderPlugin.cc,v 1.2 2008/05/03 19:05:27 neadam Exp $
+// $Id$
 //
 
 // system include files
@@ -47,26 +47,31 @@ void HLXRenderPlugin::initialise( int argc, char **argv )
 
 bool HLXRenderPlugin::applies( const DQMNet::CoreObject &o, const VisDQMImgInfo &i ) 
 {
- 
-  if( o.name.find( "/HLX/HFPlus/Wedge" )  == 0 ){
+  if( o.name.find( "HLX/Luminosity" )  == 0 ){
     return true;
   }
-  if( o.name.find( "/HLX/HFMinus/Wedge" ) == 0 ){
+  if( o.name.find( "HLX/HFMinus" ) == 0 ){
     return true;
   }
-  if( o.name.find( "/HLX/HFCompare" )     == 0 ){
+  if( o.name.find( "HLX/HFPlus" ) == 0 ){
     return true;
   }
-  if( o.name.find( "/HLX/Average" )       == 0 ){ 
+  if( o.name.find( "HLX/HFCompare" ) == 0 ){
     return true;
   }
+  if( o.name.find( "HLX/Average" ) == 0 ){ 
+    return true;
+  }
+  if( o.name.find( "HLX/CheckSums" ) == 0 ){ 
+    return true;
+  }
+
   return false;
 }
 
 void HLXRenderPlugin::preDraw( TCanvas *c, const DQMNet::CoreObject &o, 
 			       const VisDQMImgInfo &i, VisDQMRenderInfo &r ) 
 {
-
 
    c->cd();
 
@@ -82,10 +87,11 @@ void HLXRenderPlugin::preDraw( TCanvas *c, const DQMNet::CoreObject &o,
    gStyle->SetStatBorderSize(1);
    gStyle->SetStatColor(kWhite);
    gStyle->SetTitleFillColor(kWhite);
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+   gStyle->SetPadLeftMargin(0.15);
 
    gROOT->ForceStyle();
-
-   c->GetPad(0)->SetTicks(1,1);
 
    if( dynamic_cast<TProfile*>( o.object ) ) {
       preDrawTProfile( c, o );
@@ -96,18 +102,22 @@ void HLXRenderPlugin::preDraw( TCanvas *c, const DQMNet::CoreObject &o,
 
    r.drawOptions = "";
 
-
 }
 
 void HLXRenderPlugin::preDrawTProfile( TCanvas *c, const DQMNet::CoreObject &o ) 
 {
-
    TProfile* obj = dynamic_cast<TProfile*>( o.object );
 
    assert( obj );
 
    // Average plots for Etsum/Tower Occupancy
    obj->SetStats(kTRUE);
+   obj->GetYaxis()->SetTitleOffset(1.6);
+
+   if( o.name.find("EtSum") != std::string::npos && o.name.find("Lumi") == std::string::npos )
+   {
+      if( obj->GetMaximum() > 0 ) gPad->SetLogy();
+   }
 
    return;
 }
@@ -115,27 +125,30 @@ void HLXRenderPlugin::preDrawTProfile( TCanvas *c, const DQMNet::CoreObject &o )
 void HLXRenderPlugin::preDrawTH1F( TCanvas *c, const DQMNet::CoreObject &o ) 
 {
 
-  TH1F* obj = dynamic_cast<TH1F*>( o.object );
+   TH1F* obj = dynamic_cast<TH1F*>( o.object );
 
-  assert( obj );
+   assert( obj );
 
-  // Average plots for Etsum/Tower Occupancy
-  obj->SetStats(kTRUE);
-  
-//   if( o.name.find("ETSum") == 0 && o.name.find("HFCompare") != 0 )
-//   {
-//     gPad->SetLogy();
-//   }
+   // Average plots for Etsum/Tower Occupancy
+   obj->SetStats(kTRUE);
+   
+   obj->SetFillColor(kBlue);
+   obj->SetLineColor(kBlue);
+   obj->SetFillStyle(1001);
+   obj->GetYaxis()->SetTitleOffset(1.6);
 
-
-  return;
+   if( o.name.find("EtSum") != std::string::npos || o.name.find("ETSum") != std::string::npos )
+   {
+      if( obj->GetMaximum() > 0 ) gPad->SetLogy();
+   }
+   
+   return;
 }
 
 // Anything that needs to be done after the histogram is drawn
 void HLXRenderPlugin::postDraw( TCanvas *c, const DQMNet::CoreObject &o, 
 			       const VisDQMImgInfo &i ) 
 {
-
 
    // Do nothing here for now ...
 
