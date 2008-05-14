240eaec92e95a6187956bf9fadae090710b20a2f
giordano <>
2008-05-14 10:07:24 +0000

    moved essources in CalibTracker/SiStripESProducers

--- CalibTracker/SiStripPedestals/plugins/module.cc
+++ CalibTracker/SiStripPedestals/plugins/module.cc
@@ -12,12 +12,4 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDB);
 using cms::SiStripOfflinePedNoiseToDb;
 DEFINE_ANOTHER_FWK_MODULE(SiStripOfflinePedNoiseToDb);
 
-#include "CalibTracker/SiStripPedestals/interface/SiStripNoiseDummyCalculator.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDummyCalculator);
-
-#include "CalibTracker/SiStripPedestals/interface/SiStripNoiseFakeESSource.h"
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripNoiseFakeESSource);
-
-#include "CalibTracker/SiStripPedestals/interface/SiStripPedestalsFakeESSource.h"
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripPedestalsFakeESSource);
 
