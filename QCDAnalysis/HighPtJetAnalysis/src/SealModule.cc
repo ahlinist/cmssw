c92becd25e34029bccbcb458193fd4b364c9c30f
oehler <>
2008-05-25 17:14:02 +0000

    Added Analyzer to produce jetcross section from whatver algorithm set together samples - works with 2_0_7

--- QCDAnalysis/HighPtJetAnalysis/src/SealModule.cc
+++ QCDAnalysis/HighPtJetAnalysis/src/SealModule.cc
@@ -3,7 +3,9 @@
 
 #include "QCDAnalysis/HighPtJetAnalysis/interface/InclusiveConeJetAnalyzer.h"
 #include "QCDAnalysis/HighPtJetAnalysis/interface/DijetTreeProducer.h"
+#include "QCDAnalysis/HighPtJetAnalysis/interface/InclusiveJetAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(InclusiveConeJetAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(InclusiveJetAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(DijetTreeProducer);
