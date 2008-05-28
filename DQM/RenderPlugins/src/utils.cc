79f621861e641f0380e32f9b9eddea133907babc
dellaric <>
2008-05-28 12:41:29 +0000

    - add & use new helper class (palette for reportSummaryMap)

---
+++ DQM/RenderPlugins/src/utils.cc
@@ -0,0 +1,65 @@
+// $Id: utils.cc,v 1.57 2008/05/20 15:28:16 dellaric Exp $
+
+/*!
+  \file utils.cc
+  \version $Revision: 1.57 $
+  \date $Date: 2008/05/20 15:28:16 $
+*/
+
+#include "TH2F.h"
+
+#include "TStyle.h"
+#include "TColor.h"
+#include "TROOT.h"
+
+#include "DQM/RenderPlugins/src/utils.h"
+
+using namespace dqm;
+
+bool utils::init = false;
+
+//-------------------------------------------------------------------------
+
+void utils::reportSummaryMapPalette(TH2F* obj) {
+
+  static int pcol[20];
+
+  if( ! utils::init ) {
+
+    utils::init = true;
+
+    float rgb[20][3];
+
+    for( int i=0; i<20; i++ ) {
+      if ( i < 18 ) {
+        rgb[i][0] = 0.80+0.01*i;
+        rgb[i][1] = 0.00+0.03*i;
+        rgb[i][2] = 0.00;
+      } else if ( i == 18 ) {
+        rgb[i][0] = 0.98;
+        rgb[i][1] = 0.60;
+        rgb[i][2] = 0.00;
+      } else if ( i == 19 ) {
+        rgb[i][0] = 0.00;
+        rgb[i][1] = 0.80;
+        rgb[i][2] = 0.00;
+      }
+      pcol[i] = 901+i;
+      TColor* color = gROOT->GetColor( 901+i );
+      if( ! color ) color = new TColor( 901+i, 0, 0, 0, "" );
+      color->SetRGB( rgb[i][0], rgb[i][1], rgb[i][2] );
+    }
+
+  }
+
+  gStyle->SetPalette(20, pcol);
+
+  if( obj ) {
+    obj->SetMinimum(-1.e-15);
+    obj->SetMaximum(+1.0);
+    obj->SetOption("colz");
+  }
+
+}
+
+//-------------------------------------------------------------------------
