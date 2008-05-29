ece77ab8e64e3a7c9a4d7cb32c51f772396935d7
fedor <>
2008-05-29 23:40:08 +0000

    Add L1 trigger filter able to dump all trigger bits

--- RecoHcal/HcalProm/src/SealModule.cc
+++ RecoHcal/HcalProm/src/SealModule.cc
@@ -4,8 +4,10 @@
 #include "HcalTemplate.h"
 #include "HcalProm.h"
 #include "FRPedestalsMaker.h"
+#include "L1GTFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalTemplate);
 DEFINE_ANOTHER_FWK_MODULE(HcalProm);
 DEFINE_ANOTHER_FWK_MODULE(FRPedestalsMaker);
+DEFINE_ANOTHER_FWK_MODULE(L1GTFilter);
