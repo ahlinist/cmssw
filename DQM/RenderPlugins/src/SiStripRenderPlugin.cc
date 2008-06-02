376d055a95d3796c8cbe4a6a26da7bfaf499b3ad
dutta <>
2008-06-02 10:54:10 +0000

    updating SiStrip Render plugins for reportSummaryMap

--- DQM/RenderPlugins/src/SiStripRenderPlugin.cc
+++ DQM/RenderPlugins/src/SiStripRenderPlugin.cc
@@ -2,11 +2,13 @@
   \file SiStripRenderPlugin
   \brief Display Plugin for SiStrip DQM Histograms
   \author S. Dutta 
-  \version $Revision: 1.3 $
-  \date $Date: 2008/02/14 20:24:54 $
+  \version $Revision: 1.4 $
+  \date $Date: 2008/03/10 15:22:18 $
 */
 
 #include "TProfile2D.h"
+#include "TH1F.h"
+#include "TH2F.h"
 #include "TStyle.h"
 #include "TCanvas.h"
 #include "TColor.h"
@@ -14,11 +16,23 @@
 #include <cassert>
 
 #include "DQM/RenderPlugins/src/SiStripRenderPlugin.h"
-
+#include "DQM/RenderPlugins/src/utils.h"
 
 bool SiStripRenderPlugin::applies( const DQMNet::CoreObject &o, const VisDQMImgInfo &i ) {
  
-  if( o.name.find( "SiStrip/" ) == 0 ) {
+  if( o.name.find( "SiStrip/EventInfo" ) != std::string::npos ) {
+    return true;
+  } 
+
+  if( o.name.find( "SiStrip/MechanicalView" ) != std::string::npos ) {
+    return true;
+  } 
+
+  if( o.name.find( "SiStrip/ReadoutView" ) != std::string::npos ) {
+    return true;
+  } 
+
+  if( o.name.find( "SiStrip/ReadoutView" ) != std::string::npos ) {
     return true;
   } 
 
@@ -30,19 +44,18 @@ void SiStripRenderPlugin::preDraw( TCanvas *c, const DQMNet::CoreObject &o, cons
 
   c->cd();
 
-  if( dynamic_cast<TH2*>( o.object ) ) {
-    preDrawTH2( c, o );
+  if( dynamic_cast<TH2F*>( o.object ) ) {
+    preDrawTH2F( c, o );
   }
-  else if( dynamic_cast<TH1*>( o.object ) ) {
-    preDrawTH1( c, o );
+  else if( dynamic_cast<TH1F*>( o.object ) ) {
+    preDrawTH1F( c, o );
   }
-
 }
 
 
-void SiStripRenderPlugin::preDrawTH2( TCanvas *c, const DQMNet::CoreObject &o ) {
+void SiStripRenderPlugin::preDrawTH2F( TCanvas *c, const DQMNet::CoreObject &o ) {
 
-  TH2* obj = dynamic_cast<TH2*>( o.object );
+  TH2F* obj = dynamic_cast<TH2F*>( o.object );
 
   assert( obj );
 
@@ -51,8 +64,8 @@ void SiStripRenderPlugin::preDrawTH2( TCanvas *c, const DQMNet::CoreObject &o ) 
   gStyle->SetPadBorderMode( 0 );
   gStyle->SetPadBorderSize( 0 );
   //    (data->pad)->SetLogy( 0 );;
-  gStyle->SetOptStat( 0 );
-  obj->SetStats( kFALSE );
+  //  gStyle->SetOptStat( 0 );
+
 
   TAxis* xa = obj->GetXaxis();
   TAxis* ya = obj->GetYaxis();
@@ -66,35 +79,42 @@ void SiStripRenderPlugin::preDrawTH2( TCanvas *c, const DQMNet::CoreObject &o ) 
   ya->SetLabelSize(0.04);
 
   
-  if( o.name.find( "PedsEvolution" ) < o.name.size() ) {
+  if( o.name.find( "PedsEvolution" ) != std::string::npos) {
     gStyle->SetOptStat( 1111 );
     obj->SetStats( kTRUE );
     obj->SetOption( "lego2" );
     return;
   } 
-  if( o.name.find( "CMDistribution " ) < o.name.size() ){
+  if( o.name.find( "CMDistribution " )  != std::string::npos){
     obj->GetXaxis()->LabelsOption("d");
     obj->SetOption( "lego2" );
     return;
   }
-  if( o.name.find( "CMSlopeDistribution " ) < o.name.size() ) {
+  if( o.name.find( "CMSlopeDistribution " )  != std::string::npos) {
     obj->GetXaxis()->LabelsOption("d");
     obj->SetOption( "lego2" );
     return;
   }
 
-  if( o.name.find( "PedestalDistribution " ) < o.name.size()) {
+  if( o.name.find( "PedestalDistribution " )  != std::string::npos) {
     obj->GetXaxis()->LabelsOption("d");
     obj->SetOption( "lego" );
+    return;
+  }
+
+  if( o.name.find( "reportSummaryMap" )  != std::string::npos) {
+    obj->SetStats( kFALSE );
+    dqm::utils::reportSummaryMapPalette(obj);
+    obj->SetOption("colztext");
     return;
   }
 
   return;
 }
 
-void SiStripRenderPlugin::preDrawTH1( TCanvas *c, const DQMNet::CoreObject &o ) {
+void SiStripRenderPlugin::preDrawTH1F( TCanvas *c, const DQMNet::CoreObject &o ) {
 
-  TH1* obj = dynamic_cast<TH1*>( o.object );
+  TH1F* obj = dynamic_cast<TH1F*>( o.object );
 
   assert( obj );
 
@@ -116,8 +136,12 @@ void SiStripRenderPlugin::postDraw( TCanvas *c, const DQMNet::CoreObject &o, con
 
   c->cd();
 
-  if( dynamic_cast<TH1*>( o.object ) ) {
-    postDrawTH1( c, o );
+  if( dynamic_cast<TH1F*>( o.object ) ) {
+    postDrawTH1F( c, o );
+  }
+
+  if( dynamic_cast<TH2F*>( o.object ) ) {
+    postDrawTH2F( c, o );
   }
 
 
@@ -127,7 +151,7 @@ void SiStripRenderPlugin::postDraw( TCanvas *c, const DQMNet::CoreObject &o, con
 
 }
 
-void SiStripRenderPlugin::postDrawTH1( TCanvas *c, const DQMNet::CoreObject &o ) {
+void SiStripRenderPlugin::postDrawTH1F( TCanvas *c, const DQMNet::CoreObject &o ) {
 
   TText tt;
   tt.SetTextSize(0.12);
@@ -142,12 +166,24 @@ void SiStripRenderPlugin::postDrawTH1( TCanvas *c, const DQMNet::CoreObject &o )
     } else if (o.flags & DQMNet::DQM_FLAG_REPORT_OTHER) { 
       tt.SetTextColor(1);
       tt.DrawTextNDC(0.5, 0.5, "Other ");      
-    } else {
-      tt.SetTextColor(3);
-      tt.DrawTextNDC(0.5, 0.5, "Ok ");
-    }      
+    } 
   }  
 
   return;
+
+}
+void SiStripRenderPlugin::postDrawTH2F( TCanvas *c, const DQMNet::CoreObject &o ) {
+
+  TH2F* obj = dynamic_cast<TH2F*>( o.object );
+
+  assert( obj );
+
+  std::string name = o.name.substr(o.name.rfind("/")+1);
+
+  if( name.find( "reportSummaryMap" ) != std::string::npos ) {
+    c->SetGridx();
+    c->SetGridy();
+    return;
+  }
 
 }
