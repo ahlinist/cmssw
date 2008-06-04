07e0da63a407e121aa806863394019e15bfb50aa
merkelp <>
2008-06-04 07:41:44 +0000

    fix: removed double declaration of obj

--- DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
+++ DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
@@ -2,8 +2,8 @@
   \file SiPixelRenderPlugin
   \brief Display Plugin for Pixel DQM Histograms
   \author P.Merkel
-  \version $Revision: 1.0 $
-  \date $Date: 2008/05/28 00:00:00 $
+  \version $Revision: 1.1 $
+  \date $Date: 2008/06/03 10:25:13 $
 */
 
 #include "TProfile2D.h"
@@ -65,10 +65,10 @@ void SiPixelRenderPlugin::preDrawTH2( TCanvas *c, const DQMNet::CoreObject &o ) 
   ya->SetTitleSize(0.05);
   ya->SetLabelSize(0.04);
 
-  TH2F* obj = dynamic_cast<TH2F*>( o.object );
+  TH2F* obj2 = dynamic_cast<TH2F*>( o.object );
 
    if( o.name.find( "reportSummaryMap" ) < o.name.size() ) {
-     dqm::utils::reportSummaryMapPalette(obj);
+     dqm::utils::reportSummaryMapPalette(obj2);
      return;
    }  
 
