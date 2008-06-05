989080ffb8a70e8749da1dc417f026e9ad699d91
adamwo <>
2008-06-05 12:51:04 +0000

    Change in SelectorSequence interface. The return type is now a SelectorDecision object instead of a simple vector - the object can provide the usual access methods (individual decision, cumulative, exclusive ..)

--- SusyAnalysis/EventSelector/interface/SelectorSequence.h
+++ SusyAnalysis/EventSelector/interface/SelectorSequence.h
@@ -6,7 +6,7 @@
 ///
 /// Original author: W. Adam, 10/4/08
 ///
-/// $Id: SelectorSequence.h,v 1.2 2008/05/23 15:48:22 fronga Exp $ 
+/// $Id: SelectorSequence.h,v 1.3 2008/06/02 15:27:44 adamwo Exp $ 
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -38,39 +38,47 @@ public:
   /// selector name from index
   std::string selectorName (size_t index) const;
 
-  /// selector results (in the same order as defined in selectors()
-  std::vector<bool> decisions (const edm::Event& event) const;
+  /// selector results (in the same order as defined in selectors())
+  SelectorDecisions decisions (const edm::Event& event) const;
 
-  /// selector result by index
-  bool decision (const edm::Event& event, size_t index) const;
+// obsolete; replaced by SelectorDecisions::decisions[index] 
+//   /// selector result by index 
+//   bool decision (const edm::Event& event, size_t index) const;
 
-  /// selector result by name
-  bool decision (const edm::Event& event, 
-		 const std::string& selectorName) const;
+// obsolete; replaced by
+//   size_t index = selectorIndex(selectorName);
+//   bool result = SelectorDecisions::decisions[index];
+//   /// selector result by name
+//   bool decision (const edm::Event& event, 
+// 		 const std::string& selectorName) const;
 
-  /// global decision (AND of all selectors)
-  bool globalDecision (const edm::Event& event) const;
+// obsolete; replaced by SelectorDecisions::globalDecision() 
+//   /// global decision (AND of all selectors)
+//   bool globalDecision (const edm::Event& event) const;
 
-  /// complementary selection result by index (i.e., excluding one selector)
-  bool complementaryDecision (const edm::Event& event, size_t index) const;
+// obsolete; replaced by SelectorDecisions::complementaryDecision(index)
+//   /// complementary selection result by index (i.e., excluding one selector)
+//   bool complementaryDecision (const edm::Event& event, size_t index) const;
 
-  /// complementary selection result by name (i.e., excluding one selector)
-  bool complementaryDecision (const edm::Event& event, 
-			      const std::string& selectorName) const;
+// obsolete; replaced by
+//   size_t index = selectorIndex(selectorName);
+//   bool result = SelectorDecisions::complementaryDecision[index];
+//   /// complementary selection result by name (i.e., excluding one selector)
+//   bool complementaryDecision (const edm::Event& event, 
+// 			      const std::string& selectorName) const;
 
-  /** cumulative selection result by index (AND of the selectors in the list 
-   *  from 0 to - and including - "index") */
-  bool cumulativeDecision (const edm::Event& event, size_t index) const;
+// obsolete; replaced by SelectorDecisions::cumulativeDecision(index)
+//  /** cumulative selection result by index (AND of the selectors in the list 
+//    *  from 0 to - and including - "index") */
+//   bool cumulativeDecision (const edm::Event& event, size_t index) const;
 
-  /** cumulative selection result by name (AND of the selectors in the list 
-   *  up to and including the one identified by "name") */
-  bool cumulativeDecision (const edm::Event& event, 
-			   const std::string& selectorName) const;
-
-  /// return decisions as object (with access functions)
-  SelectorDecisions decisionObject (const edm::Event& event) const {
-    return SelectorDecisions(decisions(event));
-  }
+// obsolete; replaced by
+//   size_t index = selectorIndex(selectorName);
+//   bool result = SelectorDecisions::cumulativeDecision[index];
+//   /** cumulative selection result by name (AND of the selectors in the list 
+//    *  up to and including the one identified by "name") */
+//   bool cumulativeDecision (const edm::Event& event, 
+// 			   const std::string& selectorName) const;
 
   /// number of variables
   size_t numberOfVariables () const;
@@ -85,14 +93,14 @@ public:
 private:
   void createSelectors (const std::vector<std::string>& sequence,
 			const edm::ParameterSet& selectors);
-  inline bool newEvent (const edm::Event& event) const {return event.id()!=currentEventId_;}
+//   inline bool newEvent (const edm::Event& event) const {return event.id()!=currentEventId_;}
   
 private:
   std::vector<const SusyEventSelector*> selectors_;
   mutable std::vector<std::string> selectorNames_;
 
-  mutable edm::EventID currentEventId_;
-  mutable std::vector<bool> currentDecisions_;
+//   mutable edm::EventID currentEventId_;
+//   mutable SelectorDecisions currentDecisions_;
 };
 
 #endif
