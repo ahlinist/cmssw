5a16516e59b86c3472ea0ca8e57cc078da08f9dc
rpw <>
2008-06-11 21:04:33 +0000

    update py configs

--- SUSYBSMAnalysis/CSA07Skims/python/AODSIMHEEPSkimEventContent_cff.py
+++ SUSYBSMAnalysis/CSA07Skims/python/AODSIMHEEPSkimEventContent_cff.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-# Save AOD, SIM and possibel extras
 from Configuration.EventContent.EventContent_cff import *
 from SUSYBSMAnalysis.CSA07Skims.HEEPSkimEventContent_cff import *
 AODSIMHEEPSkimEventContent = cms.PSet(
