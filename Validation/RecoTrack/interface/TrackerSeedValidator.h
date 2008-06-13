a924350621ee82381cfcf41fb0dc169bcd9df257
cerati <>
2008-06-13 18:41:24 +0000

    Rename SeedValidator as TrackerSeedValidator

---
+++ Validation/RecoTrack/interface/TrackerSeedValidator.h
@@ -0,0 +1,42 @@
+#ifndef TrackerSeedValidator_h
+#define TrackerSeedValidator_h
+
+/** \class TrackerSeedValidator
+ *  Class that prodecs histrograms to validate Track Reconstruction performances
+ *
+ *  $Date: 2008/06/13 18:16:28 $
+ *  $Revision: 1.1.2.1 $
+ *  \author cerati
+ */
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "Validation/RecoTrack/interface/MultiTrackValidatorBase.h"
+
+#include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
+#include "RecoTracker/TransientTrackingRecHit/interface/TkTransientTrackingRecHitBuilder.h"
+
+class TrackerSeedValidator : public edm::EDAnalyzer, protected MultiTrackValidatorBase {
+ public:
+  /// Constructor
+  TrackerSeedValidator(const edm::ParameterSet& pset):MultiTrackValidatorBase(pset){
+    builderName = pset.getParameter<std::string>("TTRHBuilder");
+  }
+  
+  /// Destructor
+  ~TrackerSeedValidator(){ }
+
+  /// Method called before the event loop
+  void beginJob( const edm::EventSetup &);
+  /// Method called once per event
+  void analyze(const edm::Event&, const edm::EventSetup& );
+  /// Method called at the end of the event loop
+  void endJob();
+  
+ private:
+  std::string builderName;
+  edm::ESHandle<TransientTrackingRecHitBuilder> theTTRHBuilder;
+};
+
+
+#endif
