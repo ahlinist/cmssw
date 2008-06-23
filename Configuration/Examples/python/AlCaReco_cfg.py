95daa50272b4347cc8b3d71e1dc39744f445a8f3
futyand <>
2008-06-23 12:07:06 +0000

    new cosmic

--- Configuration/Examples/python/AlCaReco_cfg.py
+++ Configuration/Examples/python/AlCaReco_cfg.py
@@ -1,6 +1,6 @@
 # The following comments couldn't be translated into the new config version:
 
-# last update: $Date: 2008/04/07 21:15:04 $ by $Author: futyand $
+# last update: $Date: 2008/04/21 00:53:45 $ by $Author: rpw $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -15,8 +15,8 @@ process.load("Configuration.StandardSequences.AlCaReco_cff")
 process.load("Configuration.EventContent.AlCaRecoOutput_cff")
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.26 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Examples/data/AlCaReco.cfg,v $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Examples/python/AlCaReco_cfg.py,v $'),
     annotation = cms.untracked.string('CFG for AlcaReco')
 )
 process.MessageLogger = cms.Service("MessageLogger",
@@ -28,9 +28,9 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/relval/2008/4/6/RelVal-RelValTTbar-1207410101-HLT/0000/0AE8E8BE-1404-DD11-8A26-00304885AA12.root', 
-        '/store/relval/2008/4/6/RelVal-RelValTTbar-1207410101-HLT/0000/0C53192B-1504-DD11-B99E-00304885AA28.root', 
-        '/store/relval/2008/4/6/RelVal-RelValTTbar-1207410101-HLT/0000/125A556B-1304-DD11-801E-000423D95220.root')
+    fileNames = cms.untracked.vstring('/store/relval/2008/6/22/RelVal-RelValTTbar-1213987236-IDEAL_V2-2nd/0003/C4807780-C140-DD11-A03E-000423D94700.root',
+	'/store/relval/2008/6/22/RelVal-RelValTTbar-1213987236-IDEAL_V2-2nd/0004/4C747DCB-C240-DD11-9FFD-001617C3B654.root',
+	'/store/relval/2008/6/22/RelVal-RelValTTbar-1213987236-IDEAL_V2-2nd/0004/AE397234-CA40-DD11-9413-001617E30F48.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
@@ -106,6 +106,33 @@ process.poolALCARECOTkAlCosmics = cms.OutputModule("PoolOutputModule",
         dataTier = cms.untracked.string('ALCARECO')
     ),
     fileName = cms.untracked.string('file:ALCARECOTkAlCosmics.root')
+)
+
+process.poolALCARECOTkAlCosmicsHLT = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOTkAlCosmicsHLT,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ALCARECOTkAlCosmicsHLT'),
+        dataTier = cms.untracked.string('ALCARECO')
+    ),
+    fileName = cms.untracked.string('file:ALCARECOTkAlCosmicsHLT.root')
+)
+
+process.poolALCARECOTkAlCosmics0T = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOTkAlCosmics0T,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ALCARECOTkAlCosmics0T'),
+        dataTier = cms.untracked.string('ALCARECO')
+    ),
+    fileName = cms.untracked.string('file:ALCARECOTkAlCosmics0T.root')
+)
+
+process.poolALCARECOTkAlCosmics0THLT = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOTkAlCosmics0THLT,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ALCARECOTkAlCosmics0THLT'),
+        dataTier = cms.untracked.string('ALCARECO')
+    ),
+    fileName = cms.untracked.string('file:ALCARECOTkAlCosmics0THLT.root')
 )
 
 process.poolALCARECOEcalCalPhiSym = cms.OutputModule("PoolOutputModule",
@@ -189,13 +216,31 @@ process.poolALCARECOHcalCalHO = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('file:ALCARECOHcalCalHO.root')
 )
 
-process.poolALCARECOMuAlZMuMu = cms.OutputModule("PoolOutputModule",
-    process.OutALCARECOMuAlZMuMu,
+process.poolALCARECOMuAlZeroFieldGlobalCosmics = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOMuAlZeroFieldGlobalCosmics,
     dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string('ALCARECOMuAlZMuMu'),
+        filterName = cms.untracked.string('ALCARECOMuAlZeroFieldGlobalCosmics'),
         dataTier = cms.untracked.string('ALCARECO')
     ),
-    fileName = cms.untracked.string('file:ALCARECOMuAlZMuMu.root')
+    fileName = cms.untracked.string('file:ALCARECOMuAlZeroFieldGlobalCosmics.root')
+)
+
+process.poolALCARECOMuCaliMinBias = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOMuCaliMinBias,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ALCARECOMuCaliMinBias'),
+        dataTier = cms.untracked.string('ALCARECO')
+    ),
+    fileName = cms.untracked.string('file:ALCARECOMuCaliMinBias.root')
+)
+
+process.poolALCARECOMuAlCalIsolatedMu = cms.OutputModule("PoolOutputModule",
+    process.OutALCARECOMuAlCalIsolatedMu,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ALCARECOMuAlCalIsolatedMu'),
+        dataTier = cms.untracked.string('ALCARECO')
+    ),
+    fileName = cms.untracked.string('file:ALCARECOMuAlCalIsolatedMu.root')
 )
 
 process.poolALCARECOMuAlOverlaps = cms.OutputModule("PoolOutputModule",
@@ -216,7 +261,7 @@ process.poolALCARECORpcCalHLT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('file:ALCARECORpcCalHLT.root')
 )
 
-process.e = cms.EndPath(process.poolALCARECOTkAlZMuMu*process.poolALCARECOTkAlMuonIsolated*process.poolALCARECOTkAlJpsiMuMu*process.poolALCARECOTkAlUpsilonMuMu*process.poolALCARECOTkAlMinBias*process.poolALCARECOSiPixelLorentzAngle*process.poolALCARECOSiStripCalMinBias*process.poolALCARECOEcalCalElectron*process.poolALCARECOEcalCalPhiSym*process.poolALCARECOEcalCalPi0Calib*process.poolALCARECOHcalCalMinBias*process.poolALCARECOHcalCalDijets*process.poolALCARECOHcalCalGammaJet*process.poolALCARECOHcalCalIsoTrkNoHLT*process.poolALCARECOHcalCalHO*process.poolALCARECOMuAlZMuMu*process.poolALCARECOMuAlOverlaps*process.poolALCARECORpcCalHLT)
-process.schedule = cms.Schedule(process.pathALCARECOTkAlZMuMu,process.pathALCARECOTkAlMuonIsolated,process.pathALCARECOTkAlJpsiMuMu,process.pathALCARECOTkAlUpsilonMuMu,process.pathALCARECOTkAlMinBias,process.pathALCARECOSiPixelLorentzAngle,process.pathALCARECOSiStripCalMinBias,process.pathALCARECOEcalCalElectron,process.pathALCARECOEcalCalPhiSym,process.pathALCARECOEcalCalPi0Calib,process.pathALCARECOHcalCalMinBias,process.pathALCARECOHcalCalDijets,process.pathALCARECOHcalCalGammaJet,process.pathALCARECOHcalCalIsoTrkNoHLT,process.pathALCARECOHcalCalHO,process.pathALCARECOMuAlZMuMu,process.pathALCARECOMuAlOverlaps,process.pathALCARECORpcCalHLT,process.e)
+process.e = cms.EndPath(process.poolALCARECOTkAlZMuMu*process.poolALCARECOTkAlMuonIsolated*process.poolALCARECOTkAlJpsiMuMu*process.poolALCARECOTkAlUpsilonMuMu*process.poolALCARECOTkAlMinBias*process.poolALCARECOSiPixelLorentzAngle*process.poolALCARECOSiStripCalMinBias*process.poolALCARECOEcalCalElectron*process.poolALCARECOEcalCalPhiSym*process.poolALCARECOEcalCalPi0Calib*process.poolALCARECOHcalCalMinBias*process.poolALCARECOHcalCalDijets*process.poolALCARECOHcalCalGammaJet*process.poolALCARECOHcalCalIsoTrkNoHLT*process.poolALCARECOHcalCalHO*process.poolALCARECOMuCaliMinBias*process.poolALCARECOMuAlCalIsolatedMu*process.poolALCARECOMuAlOverlaps*process.poolALCARECORpcCalHLT)
+process.schedule = cms.Schedule(process.pathALCARECOTkAlZMuMu,process.pathALCARECOTkAlMuonIsolated,process.pathALCARECOTkAlJpsiMuMu,process.pathALCARECOTkAlUpsilonMuMu,process.pathALCARECOTkAlMinBias,process.pathALCARECOSiPixelLorentzAngle,process.pathALCARECOSiStripCalMinBias,process.pathALCARECOEcalCalElectron,process.pathALCARECOEcalCalPhiSym,process.pathALCARECOEcalCalPi0Calib,process.pathALCARECOHcalCalMinBias,process.pathALCARECOHcalCalDijets,process.pathALCARECOHcalCalGammaJet,process.pathALCARECOHcalCalIsoTrkNoHLT,process.pathALCARECOHcalCalHO,process.poolALCARECOMuCaliMinBias,process.pathALCARECOMuAlCalIsolatedMu,process.pathALCARECOMuAlOverlaps,process.pathALCARECORpcCalHLT,process.e)
 
 
