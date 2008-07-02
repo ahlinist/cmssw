a6ed76aefd2458c290a7890cd0d3c4efb8549d6f
adamwo <>
2008-07-02 13:35:52 +0000

    first version PTDRv2 style HT selector

---
+++ SusyAnalysis/EventSelector/interface/HTPtdrEventSelector.h
@@ -0,0 +1,29 @@
+#ifndef SusyAnalysis_EventSelector_HTPtdrEventSelector_h_
+#define SusyAnalysis_EventSelector_HTPtdrEventSelector_h_
+/// Selector for HT
+///
+/// Computes HT from MET + jets 2 - 4 and cuts on it
+
+// system include files
+#include <memory>
+
+// user include files
+#include "SusyAnalysis/EventSelector/interface/SusyEventSelector.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+
+class HTPtdrEventSelector : public SusyEventSelector {
+public:
+  HTPtdrEventSelector (const edm::ParameterSet&);
+  virtual bool select (const edm::Event&) const;
+  virtual ~HTPtdrEventSelector () {}
+
+private:
+  edm::InputTag jetTag_; ///< tag for input collection
+  edm::InputTag metTag_; ///< tag for input collection
+  float minHT_;          ///< lower HT cut 
+  float minPt_;          ///< minimum Pt of jets taken into account
+};
+#endif
