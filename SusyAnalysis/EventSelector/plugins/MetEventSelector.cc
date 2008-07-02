4ed50a0ae228011033a2ce35844b34d97dfb170a
trommers <>
2008-07-02 14:55:16 +0000

    included steering possibility of MET correction by a string

--- SusyAnalysis/EventSelector/plugins/MetEventSelector.cc
+++ SusyAnalysis/EventSelector/plugins/MetEventSelector.cc
@@ -6,19 +6,46 @@
 #include <vector>
 
 MetEventSelector::MetEventSelector (const edm::ParameterSet& pset) :
-  SusyEventSelector(pset) {
+  SusyEventSelector(pset)  {
   // input collection
   metTag_ = pset.getParameter<edm::InputTag>("metTag");
   // lower cut on MET
   minMet_ = pset.getParameter<double>("minMET");
 
-  // definition of the variable to be cached
+  uncorrString_ = pset.getParameter<std::string>("uncorrType");
+
+  uncorrect_  = uncorrectionType(uncorrString_);
+ 
+
+ // definition of the variable to be cached
   defineVariable("MET");
 
   edm::LogInfo("MetEventSelector") << "constructed with \n"
 				   << "  metTag = " << metTag_ << "\n"
 				   << "  minMET = " << minMet_;
 }
+
+/// sorry ugly hack needs to be changed once pat::MET changes but still safer then to use indices
+bool MetEventSelector::uncorrectionType (const std::string& correctionName)
+{
+  if(correctionName == "uncorrALL"){
+    uncorrType =  pat::MET::UncorectionType(pat::MET::uncorrALL);
+    return true;
+  }
+  if(correctionName == "uncorrJES"){
+    uncorrType = pat::MET::UncorectionType(pat::MET::uncorrJES);
+    return true;
+  }
+  if(correctionName == "uncorrMUON"){
+    uncorrType = pat::MET::UncorectionType(pat::MET::uncorrMUON);
+    return true;
+  }
+  else { 
+    edm::LogInfo("MetEventSelector")<< "given uncorrection not valid return corrected MET";
+    return false; 
+  }
+}
+
 
 bool
 MetEventSelector::select (const edm::Event& event) const
@@ -34,6 +61,8 @@ MetEventSelector::select (const edm::Event& event) const
     edm::LogWarning("MetEventSelector") << "No Met results for InputTag " << metTag_;
     return false;
   }
+  
+ 
   //
   // sanity check on collection
   //
@@ -42,12 +71,21 @@ MetEventSelector::select (const edm::Event& event) const
 					<< metHandle->size() << " instead of 1";
     return false;
   }
+
+  //get the uncorrected/corrected MET
+  float myMET = -10;
+  
+  if(uncorrect_ == true) myMET = metHandle->front().uncorrectedPt(uncorrType);
+  else myMET = metHandle->front().et(); //if given string not known correct everything
+  
+
   //
   // apply cut
   //
-  setVariable(0,metHandle->front().et());
-  LogTrace("MetEventSelector") << "result = " << (metHandle->front().et()>minMet_);
-  return metHandle->front().et()>minMet_;
+ 
+  setVariable(0,myMET);
+  LogTrace("MetEventSelector") << "result = " << (myMET > minMet_);
+  return myMET > minMet_;
 }
 
 //________________________________________________________________________________________
