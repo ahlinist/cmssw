cb6d08d6723c75c5e5c55d91908652ac0a89ba5a
fronga <>
2008-07-16 12:54:54 +0000

    Added a little description.

--- SusyAnalysis/EventSelector/interface/uncorrectionTypeMET.h
+++ SusyAnalysis/EventSelector/interface/uncorrectionTypeMET.h
@@ -3,6 +3,13 @@
 
 #include "DataFormats/PatCandidates/interface/MET.h"
 
+/// Function to convert MET un-correction string to types
+/// The string simply corresponds to the enum
+/// (e.g., "uncorrALL" -> pat::MET::uncorrALL).
+/// String "none" corresponds to corrected MET.
+///
+/// See also https://twiki.cern.ch/twiki/bin/view/CMS/SWGuidePATLayer1#MET
+
 namespace pat {
   /// pat::MET uncorrection type from string (uncorrMAXN = corrected MET)
   pat::MET::UncorectionType
