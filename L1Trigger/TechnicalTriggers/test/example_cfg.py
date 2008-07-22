abbea541dd0f7877b3501906f1264dad9d60ae66
jbrooke <>
2008-07-22 11:30:23 +0000

    jobs run and triggers are produced

--- L1Trigger/TechnicalTriggers/test/example_cfg.py
+++ L1Trigger/TechnicalTriggers/test/example_cfg.py
@@ -13,8 +13,12 @@ process.load("L1Trigger.TechnicalTriggers.l1TechTrigTestDigis_cfi")
 
 process.MessageLogger = cms.Service("MessageLogger")
 
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:TTbar_cfi_GEN_SIM_DIGI.root')
+    fileNames = cms.untracked.vstring('/store/relval/2008/7/20/RelVal-RelValQCD_Pt_80_120-1216579481/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-unmerged/0000/0280B738-0757-DD11-9ED6-000423D99B3E.root')
 )
 
 process.Products = cms.OutputModule("PoolOutputModule",
