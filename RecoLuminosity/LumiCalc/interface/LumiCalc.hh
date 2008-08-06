4d395304020ede1d5422abc7d562a6d9fb458924
ahunt <>
2008-08-06 16:11:51 +0000

    Correct error calculations.

--- RecoLuminosity/LumiCalc/interface/LumiCalc.hh
+++ RecoLuminosity/LumiCalc/interface/LumiCalc.hh
@@ -19,8 +19,6 @@ Summary:  Library to calculate relative luminosity from HLX data
 
 ******************************************************************************/
 
-#include "RecoLuminosity/TCPReceiver/interface/ICTypeDefs.hh"
-#include "RecoLuminosity/TCPReceiver/interface/LumiStructures.hh"
 #include <cmath>
 #include <vector>
 
@@ -31,54 +29,89 @@ Summary:  Library to calculate relative luminosity from HLX data
 #define set2BetweenIndex 4
 #define set2AboveIndex   5
 
+#define HCAL_HLX_NUM_BUNCHES 3564
+#define HCAL_HLX_NUM_HLXS 36
+#define NUM_CAP_BANKS 4
+
+using namespace ICCoreUtils;
+
 namespace HCAL_HLX {
 
-  class LumiCalc {
-  public:
-    //LumiCalc();
-    LumiCalc();
-    ~LumiCalc() {}
+   class LumiCalc {
+      public:
 
-    // Do the calculations
-    void DoCalc(HCAL_HLX::LUMI_SECTION & localSection);
+	 typedef std::vector< std::vector< float > > LumiVec;
 
-    // configuration
-    unsigned int SetBXMask( const std::vector< unsigned int > &BXMask );
-    unsigned int SetHLXMask( const std::vector< unsigned int > &HLXMask );
+	 //LumiCalc();
+	 LumiCalc( );
+	 ~LumiCalc(){
+	    etSumAvgPerLB_.clear();
+	    etSumNoiseAvgPerLB_.clear();
+	 }
+
+
+	 // Do the calculations
+	 void DoCalc(HCAL_HLX::LUMI_SECTION & localSection);
+
+	 // configuration
+	 unsigned int SetBXMask( const std::vector< unsigned int > &BXMask );
+	 unsigned int SetHLXMask( const std::vector< unsigned int > &HLXMask );
     
-  private:
+      private:
 
-    void CountActiveTowers(HCAL_HLX::LUMI_SECTION& localSection);
-    void CalcETSumLumi(HCAL_HLX::LUMI_SECTION& localSection);
-    float CalcETSumError( unsigned int numNibbles, 
-			  unsigned int numTowers, 
-			  unsigned int numBunches = 1, 
-			  float intPerBX = 0.01 );
-    void CalcLHCLumi(HCAL_HLX::LUMI_SECTION& localSection);
-    void CalcOccLumi(HCAL_HLX::LUMI_SECTION& localSection);
-    float CalcOccError( unsigned int numNibbles, 
-			unsigned int numTowers, 
-			unsigned int numBunches = 1, 
-			float intPerBX = 0.01 );
+	 void CountActiveTowers(HCAL_HLX::LUMI_SECTION& localSection);
+	 void CountActiveTowersEt(HCAL_HLX::LUMI_SECTION& localSection);
+	 void CalcETSumLumi(HCAL_HLX::LUMI_SECTION& localSection);
+	 float CalcETSumError( HCAL_HLX::LUMI_SECTION &localSection );
+	 float CalcETSumNoiseError( HCAL_HLX::LUMI_SECTION &localSection );
+	 void CalcLHCLumi(HCAL_HLX::LUMI_SECTION& localSection);
+	 void CalcOccLumi(HCAL_HLX::LUMI_SECTION& localSection);
+	 float CalcOccErrorTotal( HCAL_HLX::LUMI_SECTION &localSection, unsigned int set ); 
+	 float CalcOccErrorBX( HCAL_HLX::LUMI_SECTION &localSection, unsigned int set ); 
 
-    void CalcLumi( HCAL_HLX::LUMI_SECTION& localSection );
-    float CalcLumiError( unsigned int numNibbles, 
-			unsigned int numTowers, 
-			unsigned int numBunches = 1, 
-			float intPerBX = 0.01 );
+	 void CalcLumi( HCAL_HLX::LUMI_SECTION& localSection );
+	 float CalcLumiError( unsigned int numNibbles, 
+	 unsigned int numTowers, 
+	 unsigned int numBunches = 1, 
+	 float intPerBX = 0.01 );
 
-    // Configuration
-    unsigned int sizeNoise_[4];
-    unsigned int numBX_, numHLX_; 
+	 // Configuration
+	 //unsigned int startAG_, endAG_, sizeAG_; // AG = Abort Gap.
+	 unsigned int sizeNoise_[NUM_CAP_BANKS];
+	 float totalEtSumNoise_[NUM_CAP_BANKS];
+	 unsigned int etSumNoiseNorm_[NUM_CAP_BANKS];
+	 float totalOccNoise_[HCAL_HLX_NUM_HLXS][2][NUM_CAP_BANKS];
+	 unsigned int occNoiseNorm_[HCAL_HLX_NUM_HLXS][2][NUM_CAP_BANKS];
+	 unsigned int numBX_, numHLX_; 
 
-    unsigned int numActiveTowers_[HCAL_HLX_NUM_HLXS][2];
-    unsigned int TotalActiveTowers_;
+	 // Should only be called once per run
+	 unsigned int numActiveTowers_[HCAL_HLX_NUM_HLXS][2];
+	 unsigned int numActiveTowersBX_[HCAL_HLX_NUM_BUNCHES][2];
+	 unsigned int TotalActiveTowers_;
 
-    bool Block( unsigned short int iBX, unsigned short int numBlock);
-    bool isBunch( unsigned short int iBX, unsigned short int scheme = 1 );  
+	 bool         noMasks_;
+	 unsigned int numOrbitsPerNB_;
+	 unsigned int numActiveTowersEt_[HCAL_HLX_NUM_HLXS];
+	 unsigned int numActiveTowersBXEt_[HCAL_HLX_NUM_BUNCHES];
+	 unsigned int TotalActiveTowersEt_;
 
-    std::vector< unsigned int > BXMask_;
-    std::vector< unsigned int > HLXMask_;
-  };
+
+	 bool Block( unsigned short int iBX, unsigned short int numBlock);
+	 bool isBunch( unsigned short int iBX, unsigned short int scheme = 1 );  
+
+	 std::vector< unsigned int > BXMask_;
+	 std::vector< unsigned int > HLXMask_;
+
+	 // Make an array to store the previous averages, this way we can calculate 
+	 // an estimate of the error as we go. We store an average for each four lumi
+	 // nibbles (a lumi "bite")
+	 LumiVec etSumAvgPerLB_;
+	 LumiVec etSumNoiseAvgPerLB_;
+	 unsigned int etStoreMaxSize_;
+	 float sigmaEt_;
+	 float sigmaEtWeight_;
+	 float sigmaEtNoise_;
+	 float sigmaEtNoiseWeight_;
+   };
 }
   
