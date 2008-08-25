7b684dae561c1d33f3729a2300577448aecd7b0f
stoyan <>
2008-08-25 17:09:29 +0000

    comment

---
+++ RecoMuon/SeedGenerator/src/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoMuon/SeedGenerator/src/MuonSeedTester.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MuonSeedTester);
+
