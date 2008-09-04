06a3d352183f14c3425ccd4ab6efb9d1113e086d
kkousour <>
2008-09-04 16:51:07 +0000

    Modified to register the template class DijetBalanceTreeProducer.icc

--- JetMETCorrections/DijetBalance/src/SealModule.cc
+++ JetMETCorrections/DijetBalance/src/SealModule.cc
@@ -1,11 +1,16 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "JetMETCorrections/DijetBalance/interface/DijetBalance.h"
-#include "JetMETCorrections/DijetBalance/interface/DijetBalanceMCTruth.h"
 #include "JetMETCorrections/DijetBalance/interface/DijetBalanceTreeProducer.h"
-
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+using namespace cms;
+using namespace reco;
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(DijetBalance);
-DEFINE_ANOTHER_FWK_MODULE(DijetBalanceMCTruth);
-DEFINE_ANOTHER_FWK_MODULE(DijetBalanceTreeProducer);
+typedef DijetBalanceTreeProducer<CaloJet> CaloDBTreeProducer;
+DEFINE_ANOTHER_FWK_MODULE(CaloDBTreeProducer);
+typedef DijetBalanceTreeProducer<GenJet> GenDBTreeProducer;
+DEFINE_ANOTHER_FWK_MODULE(GenDBTreeProducer);
+typedef DijetBalanceTreeProducer<PFJet> PFDBTreeProducer;
+DEFINE_ANOTHER_FWK_MODULE(PFDBTreeProducer);
