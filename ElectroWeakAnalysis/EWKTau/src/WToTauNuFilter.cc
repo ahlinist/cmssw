12308071a6cee854661dabf9435bfe77e704d157
sdas <>
2008-09-14 21:26:36 +0000

    Added a configurable MET cut, a Jet Cut and a Jet Veto

--- ElectroWeakAnalysis/EWKTau/src/WToTauNuFilter.cc
+++ ElectroWeakAnalysis/EWKTau/src/WToTauNuFilter.cc
@@ -11,16 +11,21 @@ using namespace std;
 //
 WToTauNuFilter::WToTauNuFilter(const edm::ParameterSet& iConfig)
 {
-  display_  = iConfig.getParameter<bool>("Display");
-  metSource_ = iConfig.getParameter<InputTag>("METSource");
-  jetSource_ = iConfig.getParameter<InputTag>("JetSource");
-  tauSource_ = iConfig.getParameter<InputTag>("TauSource");
+  display_       = iConfig.getParameter<bool>("Display");
+  metSource_     = iConfig.getParameter<InputTag>("METSource");
+  caloJetSource_ = iConfig.getParameter<InputTag>("CaloJetSource");
+  tauSource_     = iConfig.getParameter<InputTag>("TauSource");
+  
+  metCut_        = iConfig.getParameter<double>("METCut");
+  jetCut1_       = iConfig.getParameter<double>("JetCut1");
+  jetCut2_       = iConfig.getParameter<double>("JetCut2");
   
   
-  
-  
-  
-  
+  // Histograms
+  edm::Service<TFileService> fs;
+  TFileDirectory dir = fs->mkdir("histograms");
+  h_MET_Et = dir.make<TH1F>("MET_Et", "MET Et", 100, 0., 100.);
+  h_CaloJet_Et = dir.make<TH1F>("CaloJet_Et", "Jet Et", 100, 0., 100.);
   
 }
 
@@ -42,20 +47,45 @@ bool WToTauNuFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   Handle<CaloMETCollection> theCaloMETHandle;
   iEvent.getByLabel(metSource_, theCaloMETHandle);
   CaloMETRef theCaloMET(theCaloMETHandle, 0);
-  if (theCaloMET->et() > 40)
+  float caloMETEt=theCaloMET->et();
+  h_MET_Et->Fill(caloMETEt);
+  if (caloMETEt > metCut_)
   {
     passedMETCut=true;
     ++nPassedMETCut_;
   }
   
+  
   // Jet Cut 1
   bool passedJetCut1=false;
-  passedJetCut1=true;
+  bool passedJetCut2=false;
+  Handle<CaloJetCollection> theCaloJetHandle;
+  iEvent.getByLabel(caloJetSource_, theCaloJetHandle);
+  unsigned int nOuterJets=0;
+  unsigned int nInnerJets=0;
+  for (unsigned int i=0; i<theCaloJetHandle->size(); ++i)
+  {
+    CaloJetRef theCaloJet(theCaloJetHandle, i);
+    double caloJetEt=theCaloJet->et();
+    h_CaloJet_Et->Fill(caloJetEt);
+    if (caloJetEt > jetCut1_) ++nOuterJets;
+    if (caloJetEt > jetCut2_ && caloJetEt < jetCut2_) ++nInnerJets;
+  }
+  if (nOuterJets==1)
+  {
+    passedJetCut1=true;
+    ++nPassedJetCut1_;
+  }
+  if (nInnerJets==0)
+  {
+    passedJetCut2=true;
+    ++nPassedJetCut2_;
+  }
   
   
   
   
-  return (passedMETCut && passedJetCut1);
+  return (passedMETCut && passedJetCut1 && passedJetCut2);
 
 }
 
@@ -66,7 +96,6 @@ void WToTauNuFilter::beginJob(const edm::EventSetup&)
   nPassedMETCut_=0;
   nPassedJetCut1_=0;
   nPassedJetCut2_=0;
-  nPassedAllCuts=0;
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
