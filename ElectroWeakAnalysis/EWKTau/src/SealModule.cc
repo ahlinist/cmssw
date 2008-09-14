9e89481a9555086e369ea63f20a5c6eee19c1158
sdas <>
2008-09-14 12:33:06 +0000

    Added the WToTauNuFilter as a SealModule

--- ElectroWeakAnalysis/EWKTau/src/SealModule.cc
+++ ElectroWeakAnalysis/EWKTau/src/SealModule.cc
@@ -2,10 +2,12 @@
 #include "ElectroWeakAnalysis/EWKTau/interface/PFTauFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/CaloTauFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/MuTauFilter.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/WToTauNuFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MuonFilter);
 DEFINE_ANOTHER_FWK_MODULE(PFTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(CaloTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(MuTauFilter);
+DEFINE_ANOTHER_FWK_MODULE(WToTauNuFilter);
 
