47e4158cd8d957a5c6d977d0e8311ec3bb75c859
slehti <>
2008-09-17 08:00:29 +0000

    Initial commit

---
+++ ElectroWeakAnalysis/TauTriggerEfficiency/interface/OfflineTauIDFilter.h
@@ -0,0 +1,59 @@
+#ifndef OfflineTauIDFilter_H
+#define OfflineTauIDFilter_H
+
+/** \class OfflineTauIDFilter
+ *
+ *
+ *  This class is an EDProducer for selecting events
+ *  passing tau identification
+ *
+ *  \author Sami Lehti  -  HIP Helsinki
+ *
+ */
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+#include "DataFormats/TauReco/interface/CaloTau.h"
+#include "RecoTauTag/TauTagTools/interface/CaloTauElementsOperators.h"
+
+#include "DataFormats/TauReco/interface/PFTau.h"
+#include "RecoTauTag/TauTagTools/interface/PFTauElementsOperators.h"
+
+//#include "DataFormats/Math/interface/LorentzVector.h"
+#include "TLorentzVector.h"
+
+#include <string>
+
+using namespace edm;
+using namespace reco;
+using namespace math;
+using namespace std;
+
+class OfflineTauIDFilter : public edm::EDProducer {
+
+    public:
+        explicit OfflineTauIDFilter(const edm::ParameterSet&);
+        ~OfflineTauIDFilter();
+
+        virtual void produce(edm::Event&, const edm::EventSetup&);
+	virtual bool tauTag(reco::CaloTau&);
+        virtual bool tauTag(reco::PFTau&);
+
+   private:
+        double matchingConeSize,
+               signalConeSize,
+               isolationConeSize,
+               ptLeadingTrackMin,
+               ptOtherTracksMin;
+        string metric;
+        unsigned int isolationAnnulus_Tracksmaxn;
+
+        int nEvents,
+            nSelectedEvents;
+};
+#endif
---
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/L1TauEfficiencyAnalyzer.cc
@@ -0,0 +1,87 @@
+#include "ElectroWeakAnalysis/TauTriggerEfficiency/interface/L1TauEfficiencyAnalyzer.h"
+
+L1TauEfficiencyAnalyzer::L1TauEfficiencyAnalyzer(const edm::ParameterSet& iConfig) {
+
+	L1extraTauJetSource = iConfig.getParameter<edm::InputTag>("L1extraTauJetSource");
+
+	jetMatchingCone = iConfig.getParameter<double>("JetMatchingCone");
+
+	nEvents		= 0;
+	nSelectedEvents	= 0;
+}
+
+L1TauEfficiencyAnalyzer::~L1TauEfficiencyAnalyzer(){
+}
+
+void L1TauEfficiencyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup ){
+	nEvents++;
+
+	try{
+          iEvent.getByLabel(L1extraTauJetSource,l1TauHandle);
+	}catch(...) {;}
+
+
+	Handle<CaloTauCollection> caloTauHandle;
+	try{
+          iEvent.getByLabel("IdentifiedTaus",caloTauHandle);
+        }catch(...) {;}
+
+	if(caloTauHandle.isValid()){
+	  const CaloTauCollection & caloTaus = *(caloTauHandle.product());
+
+	  cout << "calotau collection size " << caloTaus.size() << endl;
+
+	  CaloTauCollection::const_iterator iTau;
+          for(iTau = caloTaus.begin(); iTau != caloTaus.end(); ++iTau){
+		if(L1TauFound(iTau->p4())){
+
+		}
+	  }
+	}
+
+        Handle<PFTauCollection> pfTauHandle;
+        try{
+          iEvent.getByLabel("IdentifiedTaus",pfTauHandle);
+        }catch(...) {;}
+
+        if(pfTauHandle.isValid()){
+          const PFTauCollection & pfTaus = *(pfTauHandle.product());
+
+          cout << "pftau collection size " << pfTaus.size() << endl;
+
+          PFTauCollection::const_iterator iTau;
+          for(iTau = pfTaus.begin(); iTau != pfTaus.end(); ++iTau){
+                if(L1TauFound(iTau->p4())){
+
+                }
+          }
+        }
+
+}
+
+void L1TauEfficiencyAnalyzer::beginJob(const edm::EventSetup& iSetup){}
+
+void L1TauEfficiencyAnalyzer::endJob(){
+	cout << "Events analyzed: " << nEvents << endl;
+}
+
+#include "PhysicsTools/Utilities/interface/deltaR.h"
+bool L1TauEfficiencyAnalyzer::L1TauFound(const math::XYZTLorentzVector& p4){
+
+	bool L1found = false;
+
+        if(l1TauHandle.isValid()){
+	  const L1JetParticleCollection & l1Taus = *(l1TauHandle.product());
+	  L1JetParticleCollection::const_iterator iTau;
+	  for(iTau = l1Taus.begin(); iTau != l1Taus.end(); ++iTau){
+		double DR = deltaR(iTau->eta(),iTau->phi(),p4.eta(),p4.phi());
+		if(DR < jetMatchingCone) L1found = true;
+	  }
+	}
+	return L1found;
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(L1TauEfficiencyAnalyzer);
+
+
