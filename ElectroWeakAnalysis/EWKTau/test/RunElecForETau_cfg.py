cd3176f14932ebfcca8c5b0496649c902020857c
kap01 <>
2008-09-29 13:19:12 +0000

    Run analyzer for ETau

--- ElectroWeakAnalysis/EWKTau/test/RunElecForETau_cfg.py
+++ ElectroWeakAnalysis/EWKTau/test/RunElecForETau_cfg.py
@@ -2,13 +2,32 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Comm")
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
 process.load("DQMOffline.Trigger.Tau.HLTTauDQMOffline_cff")
+process.TauRefProducer.Electrons.doElectrons = False
+process.TauRefProducer.Electrons.doElecFromZ = True
+process.TauRefProducer.Electrons.doTrackIso = False
+process.TauRefProducer.Electrons.doID = True
+process.TauRefProducer.Electrons.ElecEtFromZcut = 15.
+process.TauRefProducer.Electrons.ptMin = 15.
+
 process.load("ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauPatProducer_cff")
+process.load("ElectroWeakAnalysis.EWKTau.tauForETauMod.tauForETauPFPatProducer_cff")
+process.load("ElectroWeakAnalysis.EWKTau.analyzerForETau_cfi")
+process.load("ElectroWeakAnalysis.EWKTau.metForETauPatConfig_cff")
+
+# register TFileService
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('analyzeETau.root')
+)
 
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
+
+        
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/0C3B40D7-F87D-DD11-A9FB-000423D998BA.root',
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/3A5455F3-F87D-DD11-AEF4-000423D94534.root',
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/420F04EB-F87D-DD11-95CF-001617E30D0A.root',
@@ -26,18 +45,20 @@ process.source = cms.Source("PoolSource",
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/E20313D4-F87D-DD11-A3B8-001617DC1F70.root',
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/FE2C34C8-F87D-DD11-B0BF-000423D94534.root',
         '/store/relval/CMSSW_2_1_7/RelValZEE/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0002/2AA81C2A-437E-DD11-8C3C-000423DD2F34.root'
+
      )
 )
 
-process.FEVT = cms.OutputModule("PoolOutputModule",
-    outputCommands = cms.untracked.vstring(                
-        'keep *_*_*_*'
-    ),
-    fileName = cms.untracked.string('testFEVT.root')
-)
 
-
-process.p = cms.Path(process.layer0ElecForETau*process.layer1ElecForETau)
-process.outpath = cms.EndPath(process.FEVT)
+process.p = cms.Path(
+                      process.layer0ElecForETau
+                     *process.layer1ElecForETau
+                     *process.PFTauForETauPreamble
+                     *process.PFTauForETauPat
+                     *process.patMET
+                     *process.electronIdCutBasedRobust
+                     *process.TauRefProducer
+                     *process.analyzeETau
+                     )
 
 
