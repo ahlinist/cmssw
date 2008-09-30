7c2aff9cee475184f221d550ddc4ce28f386220e
smaruyam <>
2008-09-30 09:21:17 +0000

    for L25 eff

--- ElectroWeakAnalysis/TauTriggerEfficiency/src/SealModule.cc
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "ElectroWeakAnalysis/TauTriggerEfficiency/interface/L2TauEfficiencyAnalyzer.h"
+#include "ElectroWeakAnalysis/TauTriggerEfficiency/interface/L25TauEfficiencyAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L2TauEfficiencyAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(L25TauEfficiencyAnalyzer);
