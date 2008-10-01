88ab653f101bebd891e7cf608204010923d79f8d
chinhan <>
2008-10-01 11:33:29 +0000

    Removed process.

--- ElectroWeakAnalysis/TauTriggerEfficiency/python/L2TauTriggerEfficiency_cfi.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/python/L2TauTriggerEfficiency_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process.L2TauTriggerEfficiency = cms.EDAnalyzer("L2TauEfficiencyAnalyzer",
+L2TauTriggerEfficiency = cms.EDAnalyzer("L2TauEfficiencyAnalyzer",
                                                  PFTauCollection = cms.InputTag("pfRecoTauProducer"),
                                                  L2AssociationCollection = cms.InputTag("hltL2DoubleTauIsolationProducer","L2TauIsolationInfoAssociator"),
                                                  outputFileName = cms.string("test.root"),
