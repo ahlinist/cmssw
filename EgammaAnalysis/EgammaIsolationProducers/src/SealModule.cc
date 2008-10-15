e7457bfac072f6c9de939582a4d2aa489c619cb6
mmozer <>
2008-10-15 07:05:10 +0000

    moved module which will run during RECO to RecoEgamma/EgammaIsolationAlgos/plugins

--- EgammaAnalysis/EgammaIsolationProducers/src/SealModule.cc
+++ EgammaAnalysis/EgammaIsolationProducers/src/SealModule.cc
@@ -4,21 +4,21 @@
 
 #include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaHcalIsolationProducer.h"
 #include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaEcalIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaElectronTkIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaElectronTkNumIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaPhotonTkIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaPhotonTkNumIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaTowerIsolationProducer.h"
-#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaEcalRecHitIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaElectronTkIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaElectronTkNumIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaPhotonTkIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaPhotonTkNumIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaTowerIsolationProducer.h"
+//#include "EgammaAnalysis/EgammaIsolationProducers/interface/EgammaEcalRecHitIsolationProducer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EgammaHcalIsolationProducer);
 DEFINE_ANOTHER_FWK_MODULE(EgammaEcalIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaElectronTkIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaElectronTkNumIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaPhotonTkIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaPhotonTkNumIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaTowerIsolationProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaEcalRecHitIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaElectronTkIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaElectronTkNumIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaPhotonTkIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaPhotonTkNumIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaTowerIsolationProducer);
+//DEFINE_ANOTHER_FWK_MODULE(EgammaEcalRecHitIsolationProducer);
 
