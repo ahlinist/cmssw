7bad3dacc17631e3be4954046c60bc3f341c9835
kap01 <>
2008-10-16 11:30:14 +0000

    Added correct version of Tau Veto Filter

--- ElectroWeakAnalysis/EWKTau/python/tauVetoFilter_cff.py
+++ ElectroWeakAnalysis/EWKTau/python/tauVetoFilter_cff.py
@@ -5,7 +5,7 @@ PfTauTag = cms.InputTag(""),
 CaloTauTag = cms.InputTag(""),
 PatTauTag = cms.InputTag(""),
 MinEtCut = cms.double(20.),
-MaxNCut = cms.double(1.),
+MaxNCut = cms.int32(1),
 Type = cms.string("Pf")
 )     
 
