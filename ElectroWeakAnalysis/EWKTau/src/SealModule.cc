3563ffad7ab92440b1f92a43e22e43b04cb4d06a
kap01 <>
2008-10-16 10:52:19 +0000

    Added Tau Veto Filter

--- ElectroWeakAnalysis/EWKTau/src/SealModule.cc
+++ ElectroWeakAnalysis/EWKTau/src/SealModule.cc
@@ -1,3 +1,6 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
 #include "ElectroWeakAnalysis/EWKTau/interface/MuonFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/PFTauFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/CaloTauFilter.h"
@@ -5,6 +8,8 @@
 #include "ElectroWeakAnalysis/EWKTau/interface/ETauAnalyzer.h"
 //#include "ElectroWeakAnalysis/EWKTau/interface/WToTauNuFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByLdgTrackProd.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByMassWithElec.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/TauVetoFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/PATLeptonPATMetDelPhiSelector.h"
 
 DEFINE_SEAL_MODULE();
@@ -14,6 +19,8 @@ DEFINE_ANOTHER_FWK_MODULE(CaloTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(MuTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(ETauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(DiscriminationByLdgTrackProd);
+DEFINE_ANOTHER_FWK_MODULE(DiscriminationByMassWithElec);
+DEFINE_ANOTHER_FWK_MODULE(TauVetoFilter);
 DEFINE_ANOTHER_FWK_MODULE(PATLeptonPATMetDelPhiSelector);
 //DEFINE_ANOTHER_FWK_MODULE(WToTauNuFilter);
 
