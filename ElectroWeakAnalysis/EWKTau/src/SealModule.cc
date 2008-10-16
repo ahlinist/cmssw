8ec0b82f6b9773ee3922491bc7ef8b90058febe2
gurrola <>
2008-10-16 17:55:02 +0000

    Changed the Muon selector to a selector that works for both muons & electrons

--- ElectroWeakAnalysis/EWKTau/src/SealModule.cc
+++ ElectroWeakAnalysis/EWKTau/src/SealModule.cc
@@ -8,11 +8,12 @@
 #include "ElectroWeakAnalysis/EWKTau/interface/ETauAnalyzer.h"
 //#include "ElectroWeakAnalysis/EWKTau/interface/WToTauNuFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByLdgTrackProd.h"
-//#include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByMassWithElec.h"
-//#include "ElectroWeakAnalysis/EWKTau/interface/TauVetoFilter.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByMassWithElec.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/TauVetoFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/PATLeptonPATMetDelPhiSelector.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/PATLeptonPFParticleIsolationSelector.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/PATLeptonSVAssociatorSelector.h"
+#include "ElectroWeakAnalysis/EWKTau/interface/PATLeptonPATTauSeperationSelector.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MuonFilter);
@@ -21,9 +22,10 @@ DEFINE_ANOTHER_FWK_MODULE(CaloTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(MuTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(ETauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(DiscriminationByLdgTrackProd);
-//DEFINE_ANOTHER_FWK_MODULE(DiscriminationByMassWithElec);
-//DEFINE_ANOTHER_FWK_MODULE(TauVetoFilter);
+DEFINE_ANOTHER_FWK_MODULE(DiscriminationByMassWithElec);
+DEFINE_ANOTHER_FWK_MODULE(TauVetoFilter);
 DEFINE_ANOTHER_FWK_MODULE(PATLeptonPATMetDelPhiSelector);
 DEFINE_ANOTHER_FWK_MODULE(PATLeptonPFParticleIsolationSelector);
 DEFINE_ANOTHER_FWK_MODULE(PATLeptonSVAssociatorSelector);
+DEFINE_ANOTHER_FWK_MODULE(PATLeptonPATTauSeperationSelector);
 //DEFINE_ANOTHER_FWK_MODULE(WToTauNuFilter);
