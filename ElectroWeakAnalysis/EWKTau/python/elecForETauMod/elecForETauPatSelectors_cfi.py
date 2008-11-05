0c2d207125f7a63f79da0eb3b73d79e469a9c80a
kap01 <>
2008-11-05 18:02:43 +0000

    Tuned Isolations

--- ElectroWeakAnalysis/EWKTau/python/elecForETauMod/elecForETauPatSelectors_cfi.py
+++ ElectroWeakAnalysis/EWKTau/python/elecForETauMod/elecForETauPatSelectors_cfi.py
@@ -7,10 +7,17 @@ import FWCore.ParameterSet.Config as cms
 
 ###################### KINE ##########################
 
-selectKinElecForETau = cms.EDFilter("PATElectronSelector",
+
+selectEtaElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("allLayer1ElecForETau"),
-     cut = cms.string('pt > 10. & abs(eta) < 2.5')
+     cut = cms.string('abs(superCluster.eta)<1.4442 || abs(superCluster.eta)>1.56')
 )
+
+selectKinElecForETau = cms.EDFilter("PATElectronSelector",
+     src = cms.InputTag("selectEtaElecForETau"),
+     cut = cms.string(' pt > 16.')
+)
+
 
 ##################### MATCH HLT ######################
 selectMchHLTElecForETau = cms.EDFilter("PATElectronSelector",
@@ -34,49 +41,47 @@ selectTrkIsoAfterMchHLTElecForETau = cms.EDFilter("PATElectronSelector",
 
 selectEcalIsoAfterTrkIsoElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectTrkIsoAfterMchHLTElecForETau"),
-     cut = cms.string('ecalIso<1.')             
+     cut = cms.string('ecalIso<3.8')             
 )
 
 selectEcalIsoElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectKinElecForETau"),
-     cut = cms.string('ecalIso<1.')             
+     cut = cms.string('ecalIso<3.8')             
 )
-
 
 ################### HCAL #################################
 
 selectHcalIsoAfterEcalIsoElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectEcalIsoAfterTrkIsoElecForETau"),
-     cut = cms.string('hcalIso<2.5')             
+     cut = cms.string('hcalIso<1.5')             
 )
 
 selectHcalIsoElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectKinElecForETau"),
-     cut = cms.string('hcalIso<2.5')             
+     cut = cms.string('hcalIso<1.5')             
 )
-
 
 #################### ID ##################################
 
 selectIdAfterIsoElecForETau = cms.EDFilter("PATElectronSelector",       
      src = cms.InputTag("selectHcalIsoAfterEcalIsoElecForETau"),   
-     cut = cms.string('leptonID("robust")>0.')  
+     cut = cms.string('electronID("tight")>0.')  
 )
  
 selectIdElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectKinElecForETau"),
-     cut = cms.string('leptonID("robust")>0.')
+     cut = cms.string('electronID("tight")>0.')
 )
 
 #################### IP ############################
 
 selectIpAfterIsoElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectIdAfterIsoElecForETau"),
-     cut = cms.string('abs(gsfTrack.d0)<1000000.02')  
+     cut = cms.string('abs(gsfTrack.d0)<0.05')  
 )
 
 selectIpElecForETau = cms.EDFilter("PATElectronSelector",
      src = cms.InputTag("selectKinElecForETau"),      
-     cut = cms.string('abs(gsfTrack.d0)<0.02')               
+     cut = cms.string('abs(gsfTrack.d0)<0.05')               
 )
 
