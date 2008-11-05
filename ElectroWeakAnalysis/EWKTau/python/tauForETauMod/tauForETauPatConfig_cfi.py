d6a6065dbef7f075b86733c3cfe774673ec88f6f
kap01 <>
2008-11-05 17:54:14 +0000

    Cleaning

--- ElectroWeakAnalysis/EWKTau/python/tauForETauMod/tauForETauPatConfig_cfi.py
+++ ElectroWeakAnalysis/EWKTau/python/tauForETauMod/tauForETauPatConfig_cfi.py
@@ -1,33 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 import copy
+
+from PhysicsTools.PatAlgos.cleaningLayer0.pfTauCleaner_cfi import *
 from PhysicsTools.PatAlgos.producersLayer1.tauProducer_cfi import * 
 
 #####################  PAT LAYER 0  #########################
 
-allLayer0PfTausForETau = cms.EDFilter("PATPFTauCleaner",
-     tauSource              = cms.InputTag("pfRecoTauProducerForETau"),
-     tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETau"),
- 
-     removeOverlaps = cms.PSet(),
-
-     markItems    = cms.bool(True),
-     bitsToIgnore = cms.vstring(''),
-     saveRejected = cms.string(''),
-     saveAll      = cms.string(''),
- )
-
+allLayer0PfTausForETau = copy.deepcopy(allLayer0Taus)
+allLayer0PfTausForETau.tauSource = cms.InputTag("pfRecoTauProducerForETau")
+allLayer0PfTausForETau.tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETau")
+allLayer0PfTausForETau.removeOverlaps = cms.PSet()
+allLayer0PfTausForETau.markItems = cms.bool(True)
+allLayer0PfTausForETau.bitsToIgnore = cms.vstring('Core/Preselection')
+allLayer0PfTausForETau.saveRejected = cms.string('')
+allLayer0PfTausForETau.saveAll      = cms.string('')
 
 #####################  PAT LAYER 0 FOR EFF TAU ID MEASUREMENT ###########
 
-allLayer0PfTausForETauEff = cms.EDFilter("PATPFTauCleaner",
-     tauSource              = cms.InputTag("pfRecoTauProducerForETauEff"),
-     tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETauEff"),
-     removeOverlaps = cms.PSet(),
-     markItems    = cms.bool(True),
-     bitsToIgnore = cms.vstring('Core/Preselectio'),
-     saveRejected = cms.string(''),
-     saveAll      = cms.string(''),
- )
+allLayer0PfTausForETauEff= copy.deepcopy(allLayer0Taus)
+allLayer0PfTausForETauEff.tauSource = cms.InputTag("pfRecoTauProducerForETauEff")
+allLayer0PfTausForETauEff.tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETauEff")
+allLayer0PfTausForETauEff.removeOverlaps = cms.PSet()
+allLayer0PfTausForETauEff.markItems = cms.bool(True)
+allLayer0PfTausForETauEff.bitsToIgnore = cms.vstring('Core/Preselection')
+allLayer0PfTausForETauEff.saveRejected = cms.string('')
+allLayer0PfTausForETauEff.saveAll      = cms.string('')
 
 #################### PAT LAYER 1 ##########################
 
@@ -46,6 +43,7 @@ layer1PfTausForETau.tauIDSources = cms.PSet(
 layer1PfTausForETau.addTrigMatch = False
 layer1PfTausForETau.addGenMatch = False
 layer1PfTausForETau.addGenJetMatch = False
+
 #################### PAT LAYER 1 FOR TAU ID EFF MEASUREMENT ###########
 
 layer1PfTausForETauEff = copy.deepcopy(allLayer1Taus)
@@ -60,7 +58,6 @@ layer1PfTausForETauEff.tauIDSources = cms.PSet(
     againstMuon = cms.InputTag("pfRecoTauMuonRejDiscrForETauEff")
     )
 layer1PfTausForETauEff.addTrigMatch = False
-layer1PfTausForETauEff.addGenMatch = False
 layer1PfTausForETauEff.addGenMatch = False
 layer1PfTausForETauEff.addGenJetMatch = False
 
@@ -137,5 +134,28 @@ selectorPfTausForETauProngEff = cms.EDFilter("PATTauSelector",
 )
 
 
+
+
+#allLayer0PfTausForETauEff = cms.EDFilter("PATPFTauCleaner",
+#     tauSource              = cms.InputTag("pfRecoTauProducerForETauEff"),
+#     tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETauEff"),
+#     removeOverlaps = cms.PSet(),
+#     markItems    = cms.bool(True),
+#     bitsToIgnore = cms.vstring('Core/Preselectio'),
+#     saveRejected = cms.string(''),
+#     saveAll      = cms.string(''),
+# )
+
+#allLayer0PfTausForETau = cms.EDFilter("PATPFTauCleaner",
+#     tauSource              = cms.InputTag("pfRecoTauProducerForETau"),
+#     tauDiscriminatorSource = cms.InputTag("pfRecoTauIsoDiscrForETau"),
+# 
+#     removeOverlaps = cms.PSet(),
+#
+#     markItems    = cms.bool(True),
+#     bitsToIgnore = cms.vstring('Core/Preselection'), 
+#     saveRejected = cms.string(''),
+#     saveAll      = cms.string(''),
+# )
 
 
