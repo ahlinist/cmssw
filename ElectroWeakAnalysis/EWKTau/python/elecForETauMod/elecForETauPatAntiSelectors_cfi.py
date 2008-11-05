a7b44e607b79fdeee7044ee969f5c61e1dadad6e
kap01 <>
2008-11-05 18:03:35 +0000

    Anti Iso Selectors

---
+++ ElectroWeakAnalysis/EWKTau/python/elecForETauMod/elecForETauPatAntiSelectors_cfi.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+  
+# module to select Electrons
+# See https://twiki.cern.ch/twiki/bin/view/CMS/SWGuidePhysicsCutParser
+# on how to use the cut-string
+
+
+#################### TRK ##############################
+
+selectAntiTrkIsoAfterMchHLTElecForETau = cms.EDFilter("PATElectronSelector",
+     src = cms.InputTag("selectMchHLTElecForETau"),
+     cut = cms.string('trackIso>0')
+)
+
+####################### ECAL #############################
+
+selectAntiEcalIsoAfterTrkIsoElecForETau = cms.EDFilter("PATElectronSelector",
+     src = cms.InputTag("selectAntiTrkIsoAfterMchHLTElecForETau"),
+     cut = cms.string('ecalIso>3.5')             
+)
+
+################### HCAL #################################
+
+selectAntiHcalIsoAfterEcalIsoElecForETau = cms.EDFilter("PATElectronSelector",
+     src = cms.InputTag("selectAntiEcalIsoAfterTrkIsoElecForETau"),
+     cut = cms.string('hcalIso>3.5')             
+)
+
+#################### ID ##################################
+
+selectAntiIdAfterIsoElecForETau = cms.EDFilter("PATElectronSelector",       
+     src = cms.InputTag("selectAntiHcalIsoAfterEcalIsoElecForETau"),   
+     cut = cms.string('electronID("robust")>-1')#No Id by default  
+)
+ 
+#################### IP ############################
+
+selectAntiIpAfterIsoElecForETau = cms.EDFilter("PATElectronSelector",
+     src = cms.InputTag("selectAntiIdAfterIsoElecForETau"),
+     cut = cms.string('abs(gsfTrack.d0)<1000000.02')  
+)
+
+
