3ed94d5afe13c0f05cbd6f0f422197fa96b5d8a8
kap01 <>
2008-11-05 15:35:40 +0000

    Adding ETauTauAnalyzer.cc

---
+++ ElectroWeakAnalysis/EWKTau/python/tauForETauAnalyzer_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+analyzeTauForETau = cms.EDAnalyzer("ETauTauAnalyzer",
+
+  TauInputTags = cms.VInputTag(
+                                "selectorPfTausForETauKinEff",
+                                "selectorPfTausForETauLdgTrkEff",
+                                "selectorPfTausForETauLdgTrkPtEff",
+                                "selectorPfTausForETauEclIsodEff",
+                                "selectorPfTausForETauTrkIsodEff",
+                                "selectorPfTausForETauElecRejEff",
+                                "selectorPfTausForETauProngEff"
+                              ),
+  FinalElecTag = cms.InputTag("selectIpAfterIsoElecForETau"),
+  RefInputTags = cms.VInputTag("TauRefProducer:ElectronsFromZ"),
+
+  DoTauIdAfterElecId = cms.bool(True),
+  DoMatching = cms.bool(False),
+  DoTuning = cms.bool(True),
+  EventWeight = cms.double(1.)
+)
