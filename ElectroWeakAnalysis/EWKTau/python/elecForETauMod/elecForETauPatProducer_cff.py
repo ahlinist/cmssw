5b52d612bc39dde253f700366a4b5a96b02ca5bb
kap01 <>
2008-11-05 18:03:01 +0000

    Cleaning

--- ElectroWeakAnalysis/EWKTau/python/elecForETauMod/elecForETauPatProducer_cff.py
+++ ElectroWeakAnalysis/EWKTau/python/elecForETauMod/elecForETauPatProducer_cff.py
@@ -1,9 +1,10 @@
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauPatConfig_cfi import *
 from ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauPatSelectors_cfi import *
+from ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauPatAntiSelectors_cfi import *
 from ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauId_cff import *
 from ElectroWeakAnalysis.EWKTau.elecForETauMod.elecForETauIso_cff import *
-from PhysicsTools.PatAlgos.mcMatchLayer0.mcMatchSequences_cff import *
+#from PhysicsTools.PatAlgos.mcMatchLayer0.mcMatchSequences_cff import *
 from PhysicsTools.PatAlgos.triggerLayer0.trigMatchSequences_cff import *
 electronTrigMatchCandHLT1ElectronStartup.src = "allLayer0ElecForETau"
 electronTrigMatchHLT1Electron.src = "allLayer0ElecForETau"
@@ -16,9 +17,10 @@ patTrigMatchElecForETau = cms.Sequence(
                                       )
 
 patHighLevelRecoElecForETau = cms.Sequence(patElecIdForETau * patAODElectronIsolation * patLayer0ElectronIsolation)
-layer0ElecForETau = cms.Sequence(allLayer0ElecForETau * patTrigMatchElecForETau * patTrigMatchElecForETau * patHighLevelRecoElecForETau)
+layer0ElecForETau = cms.Sequence(allLayer0ElecForETau * patTrigMatchElecForETau * patHighLevelRecoElecForETau)
 layer1ElecForETau = cms.Sequence(
                                   allLayer1ElecForETau
+                                 *selectEtaElecForETau
                                  *selectKinElecForETau
                                  *selectMchHLTElecForETau
                                  *selectTrkIsoAfterMchHLTElecForETau
