5a9723dea504d8aa5627bccfa5ff4bb68cf6e8aa
mkortela <>
2008-11-06 14:42:55 +0000

    Added matching with L1 central jets

--- ElectroWeakAnalysis/TauTriggerEfficiency/src/L1TauEfficiencyAnalyzer.cc
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/L1TauEfficiencyAnalyzer.cc
@@ -20,6 +20,7 @@ void L1TauEfficiencyAnalyzer::Setup(const edm::ParameterSet& iConfig,TTree *trig
 {
   PFTauCollectionSource = iConfig.getParameter<edm::InputTag>("PFTauCollection");
   L1extraTauJetSource = iConfig.getParameter<edm::InputTag>("L1extraTauJetSource");
+  L1extraCentralJetSource = iConfig.getParameter<edm::InputTag>("L1extraCentralJetSource");
   L1bitInfoSource = iConfig.getParameter<edm::InputTag>("L1bitInfoSource");
   jetMatchingCone = iConfig.getParameter<double>("JetMatchingCone");
   //rootFile_ = iConfig.getParameter<std::string>("outputFileName");
@@ -41,6 +42,8 @@ void L1TauEfficiencyAnalyzer::Setup(const edm::ParameterSet& iConfig,TTree *trig
   l1tree->Branch("L1Soft", &hasSoft, "L1Soft/B");
   l1tree->Branch("L1Hard", &hasHard, "L1Hard/B");
   l1tree->Branch("hasMatchedL1Jet", &hasL1Jet, "hasMatchedL1Jet/B");
+  l1tree->Branch("hasMatchedL1TauJet", &hasL1TauJet, "hasMatchedL1TauJet/B");
+  l1tree->Branch("hasMatchedL1CenJet", &hasL1CenJet, "hasMatchedL1CenJet/B");
 }
 
 void L1TauEfficiencyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup ){
@@ -105,9 +108,12 @@ void L1TauEfficiencyAnalyzer::fill(const edm::Event& iEvent, const LorentzVector
 
   // Reset variables
   jetPt = 0.0;
+  jetEt = 0.0;
   jetEta = 0.0;
   jetPhi = 0.0;
   hasL1Jet = 0;
+  hasL1TauJet = 0;
+  hasL1CenJet = 0;
   hasTauVeto = 0;
   hasEmTauVeto = 0;
   hasHadTauVeto = 0;
@@ -120,44 +126,74 @@ void L1TauEfficiencyAnalyzer::fill(const edm::Event& iEvent, const LorentzVector
   // Get data from event 
   Handle<FastL1BitInfoCollection> bitInfos;
   iEvent.getByLabel(L1bitInfoSource, bitInfos);
+  if(!bitInfos.isValid())
+    return;
 
   Handle<L1JetParticleCollection> l1TauHandle;
   iEvent.getByLabel(L1extraTauJetSource, l1TauHandle);
+  if(!l1TauHandle.isValid())
+    return;
+
+  Handle<L1JetParticleCollection> l1CentralJetHandle;
+  iEvent.getByLabel(L1extraCentralJetSource, l1CentralJetHandle);
+  if(!l1CentralJetHandle.isValid())
+    return;
 
   // Process L1 triggered taus
-  if(l1TauHandle.isValid()) {  
-    const L1JetParticleCollection & l1Taus = *(l1TauHandle.product());
-    L1JetParticleCollection::const_iterator iTau;
+  const L1JetParticleCollection & l1Taus = *(l1TauHandle.product());
+  L1JetParticleCollection::const_iterator iJet;
 
-    // Match for PF tauand L1extra tau
-    float minDR = 99999999.;
-    for(iTau = l1Taus.begin(); iTau != l1Taus.end(); ++iTau) {
-      double DR = deltaR(iTau->eta(), iTau->phi(), tau.Eta(), tau.Phi());
+  // Match for PF tau and L1extra tau
+  float minDR = 99999999.;
+  for(iJet = l1Taus.begin(); iJet != l1Taus.end(); ++iJet) {
+    if(iJet->et() <= 5.)
+      continue;
+
+    double DR = deltaR(iJet->eta(), iJet->phi(), tau.Eta(), tau.Phi());
+    if(DR < jetMatchingCone && DR < minDR) {
+      minDR = DR;
+      jetPt = iJet->pt();
+      jetEt = iJet->et();
+      jetEta = iJet->eta();
+      jetPhi = iJet->phi();
+      hasL1Jet = 1;
+      hasL1TauJet = 1;
+    }
+  }
+
+  // If not found, match PF tau and L1extra central jet
+  if(!hasL1Jet) {
+    for(iJet = l1CentralJetHandle->begin(); iJet != l1CentralJetHandle->end(); ++iJet) {
+      if(iJet->et() <= 5.)
+        continue;
+
+      double DR = deltaR(iJet->eta(), iJet->phi(), tau.Eta(), tau.Phi());
       if(DR < jetMatchingCone && DR < minDR) {
         minDR = DR;
-        jetPt = iTau->pt();
-        jetEta = iTau->eta();
-        jetPhi = iTau->phi();
+        jetPt = iJet->pt();
+        jetEt = iJet->et();
+        jetEta = iJet->eta();
+        jetPhi = iJet->phi();
         hasL1Jet = 1;
-
+        hasL1CenJet = 1;
       }
     }
+  }
   
-    // If match found, find the corresponding bit field
-    if(hasL1Jet && bitInfos.isValid()) {
-      for(FastL1BitInfoCollection::const_iterator bitInfo = bitInfos->begin(); bitInfo != bitInfos->end(); ++bitInfo) {
-        if(fabs(bitInfo->getEta() - jetEta) < 0.1 &&
-           fabs(bitInfo->getPhi() - jetPhi) < 0.1) {
+  // If match found, find the corresponding bit field
+  if(hasL1Jet) {
+    for(FastL1BitInfoCollection::const_iterator bitInfo = bitInfos->begin(); bitInfo != bitInfos->end(); ++bitInfo) {
+      if(fabs(bitInfo->getEta() - jetEta) < 0.1 &&
+         fabs(bitInfo->getPhi() - jetPhi) < 0.1) {
 
-          hasTauVeto = bitInfo->getTauVeto() ? 1 : 0;
-          hasEmTauVeto = bitInfo->getEmTauVeto() ? 1 : 0;
-          hasHadTauVeto = bitInfo->getHadTauVeto() ? 1 : 0;
-          hasIsolationVeto = bitInfo->getIsolationVeto() ? 1 : 0;
-          hasSumEtBelowThres = bitInfo->getSumEtBelowThres() ? 1 : 0;
-          hasMaxEt = bitInfo->getMaxEt() ? 1 : 0;
-          hasSoft = bitInfo->getSoft() ? 1 : 0;
-          hasHard = bitInfo->getHard() ? 1 : 0;
-        }
+        hasTauVeto = bitInfo->getTauVeto() ? 1 : 0;
+        hasEmTauVeto = bitInfo->getEmTauVeto() ? 1 : 0;
+        hasHadTauVeto = bitInfo->getHadTauVeto() ? 1 : 0;
+        hasIsolationVeto = bitInfo->getIsolationVeto() ? 1 : 0;
+        hasSumEtBelowThres = bitInfo->getSumEtBelowThres() ? 1 : 0;
+        hasMaxEt = bitInfo->getMaxEt() ? 1 : 0;
+        hasSoft = bitInfo->getSoft() ? 1 : 0;
+        hasHard = bitInfo->getHard() ? 1 : 0;
       }
     }
   }
