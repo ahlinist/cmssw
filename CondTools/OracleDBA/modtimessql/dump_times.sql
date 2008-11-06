f07b48d8fabd1e37ca472f8883da26e26a05d1ed
dwd <>
2008-11-06 16:18:58 +0000

    Change the time format printed to be unambiguous for Americans & Europeans, year first

--- CondTools/OracleDBA/modtimessql/dump_times.sql
+++ CondTools/OracleDBA/modtimessql/dump_times.sql
@@ -1,1 +1,1 @@
-select table_name, to_char(change_time, 'mm/dd/yyyy hh24:mi:ss') from last_modified_times order by change_time;
+select table_name, to_char(change_time, 'yyyy/mm/dd hh24:mi:ss') from last_modified_times order by change_time;
