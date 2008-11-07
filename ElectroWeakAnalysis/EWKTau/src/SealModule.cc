8615d7524d22ee1e6f637c4aef5baf084c9eafea
gurrola <>
2008-11-07 15:44:16 +0000

    Removed unused files

--- ElectroWeakAnalysis/EWKTau/src/SealModule.cc
+++ ElectroWeakAnalysis/EWKTau/src/SealModule.cc
@@ -1,10 +1,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
-#include "ElectroWeakAnalysis/EWKTau/interface/MuonFilter.h"
-#include "ElectroWeakAnalysis/EWKTau/interface/PFTauFilter.h"
-#include "ElectroWeakAnalysis/EWKTau/interface/CaloTauFilter.h"
-#include "ElectroWeakAnalysis/EWKTau/interface/MuTauFilter.h"
+//#include "ElectroWeakAnalysis/EWKTau/interface/MuonFilter.h"
+//#include "ElectroWeakAnalysis/EWKTau/interface/PFTauFilter.h"
+//#include "ElectroWeakAnalysis/EWKTau/interface/CaloTauFilter.h"
+//#include "ElectroWeakAnalysis/EWKTau/interface/MuTauFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/ETauAnalyzer.h"
 //#include "ElectroWeakAnalysis/EWKTau/interface/WToTauNuFilter.h"
 #include "ElectroWeakAnalysis/EWKTau/interface/DiscriminationByLdgTrackProd.h"
@@ -17,10 +17,10 @@
 #include "ElectroWeakAnalysis/EWKTau/interface/EWKTauIDAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(MuonFilter);
-DEFINE_ANOTHER_FWK_MODULE(PFTauFilter);
-DEFINE_ANOTHER_FWK_MODULE(CaloTauFilter);
-DEFINE_ANOTHER_FWK_MODULE(MuTauFilter);
+//DEFINE_ANOTHER_FWK_MODULE(MuonFilter);
+//DEFINE_ANOTHER_FWK_MODULE(PFTauFilter);
+//DEFINE_ANOTHER_FWK_MODULE(CaloTauFilter);
+//DEFINE_ANOTHER_FWK_MODULE(MuTauFilter);
 DEFINE_ANOTHER_FWK_MODULE(ETauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(DiscriminationByLdgTrackProd);
 DEFINE_ANOTHER_FWK_MODULE(DiscriminationByMassWithElec);
