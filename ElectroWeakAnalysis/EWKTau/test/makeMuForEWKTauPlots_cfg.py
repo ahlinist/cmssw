1565a1d5e8793bbce0b81d71c344c2e09655944c
veelken <>
2008-11-19 16:31:18 +0000

    switched to EmptySource (no delay by opening edm file anymore!)

--- ElectroWeakAnalysis/EWKTau/test/makeMuForEWKTauPlots_cfg.py
+++ ElectroWeakAnalysis/EWKTau/test/makeMuForEWKTauPlots_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('EWKTauMuon')
 
-process.load("ElectroWeakAnalysis.EWKTau.plotterForMuTau_cfi")
+process.load("ElectroWeakAnalysis.EWKTau.plotterForMuTau_cff")
 
 process.DQMStore = cms.Service("DQMStore")
 
@@ -10,8 +10,9 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(0)         
 )
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_7/RelValZMM/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0001/12F2BEA9-F97D-DD11-8262-000423D9890C.root')
-)
+process.source = cms.Source("EmptySource")
 
-process.p = cms.Path( process.plotMuTau )
+process.p = cms.Path( process.loadMuTau
+                     +process.addMuTau
+                     +process.saveMuTau 
+                     +process.plotMuTau )
