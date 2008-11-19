e53cc6864dfa808188b0de5f5ac6e044602ddb1e
p2k <>
2008-11-19 17:37:46 +0000

    *** empty log message ***

--- CondTools/OracleDBA/monitorsql/condmonitor/histmon.sql
+++ CondTools/OracleDBA/monitorsql/condmonitor/histmon.sql
@@ -6,13 +6,11 @@ create table hs_cond_occupancy_hourly (
   tablespace varchar2(30),
   occupancy number,
   quota number,
-  constraint cs_cond_occupancy_hourly check (id_snapshot is not null and username is not null and tablespace is not null and occupancy is not null and quota is not null),
-  constraint pk_cond_occupancy_hourly primary key (id_snapshot, username, tablespace)
+  constraint pk_cond_occupancy_hourly primary key (id_snapshot, username, tablespace),
+  constraint cs_cond_occupancy_hourly check (id_snapshot is not null and username is not null and tablespace is not null and occupancy is not null and quota is not null)
 );
 
-grant select on hs_cond_occupancy_hourly to cms_cond_history_r with grant option;
-
-grant select on hs_cond_occupancy_hourly to cms_cond_general with grant option;
+grant select on hs_cond_occupancy_hourly to cms_cond_history_r, cms_cond_general with grant option;
 
 create table hs_cond_occupancy_daily (
   id_snapshot varchar2(6),
@@ -20,13 +18,11 @@ create table hs_cond_occupancy_daily (
   tablespace varchar2(30),
   occupancy number,
   quota number,
-  constraint cs_cond_occupancy_daily check (id_snapshot is not null and username is not null and tablespace is not null and occupancy is not null and quota is not null),
-  constraint pk_cond_occupancy_daily primary key (id_snapshot, username, tablespace)
+  constraint pk_cond_occupancy_daily primary key (id_snapshot, username, tablespace),
+  constraint cs_cond_occupancy_daily check (id_snapshot is not null and username is not null and tablespace is not null and occupancy is not null and quota is not null)
 );
 
-grant select on hs_cond_occupancy_daily to cms_cond_history_r with grant option;
-
-grant select on hs_cond_occupancy_daily to cms_cond_general with grant option;
+grant select on hs_cond_occupancy_daily to cms_cond_history_r, cms_cond_general with grant option;
 
 create table hs_det_details_hourly (
   id_snapshot varchar2(2),
@@ -37,13 +33,11 @@ create table hs_det_details_hourly (
   status varchar2(31),
   tablespace varchar2(30),
   last_modified varchar2(57),
-  constraint cs_det_details_hourly check (id_snapshot is not null and owner is not null and object is not null and type is not null and occupancy is not null and status is not null and tablespace is not null and last_modified is not null),
-  constraint pk_det_details_hourly primary key (id_snapshot, owner, object, type)
+  constraint pk_det_details_hourly primary key (id_snapshot, owner, object, type),
+  constraint cs_det_details_hourly check (id_snapshot is not null and owner is not null and object is not null and type is not null and occupancy is not null and status is not null and tablespace is not null and last_modified is not null)
 );
 
-grant select on hs_det_details_hourly to cms_cond_history_r with grant option;
-
-grant select on hs_det_details_hourly to cms_cond_general with grant option;
+grant select on hs_det_details_hourly to cms_cond_history_r, cms_cond_general with grant option;
 
 create table hs_det_details_daily (
   id_snapshot varchar2(6),
@@ -54,37 +48,35 @@ create table hs_det_details_daily (
   status varchar2(31),
   tablespace varchar2(30),
   last_modified varchar2(57),
-  constraint cs_det_details_daily check (id_snapshot is not null and owner is not null and object is not null and type is not null and occupancy is not null and status is not null and tablespace is not null and last_modified is not null),
-  constraint pk_det_details_daily primary key (id_snapshot, owner, object, type)
+  constraint pk_det_details_daily primary key (id_snapshot, owner, object, type),
+  constraint cs_det_details_daily check (id_snapshot is not null and owner is not null and object is not null and type is not null and occupancy is not null and status is not null and tablespace is not null and last_modified is not null)
 );
 
-grant select on hs_det_details_daily to cms_cond_history_r with grant option;
-
-grant select on hs_det_details_daily to cms_cond_general with grant option;
+grant select on hs_det_details_daily to cms_cond_history_r, cms_cond_general with grant option;
 
 create table hs_server_workload_hourly (
   id_snapshot varchar2(2),
+  id_statement number,
   who varchar2(30),
   what varchar2(1000),
   when date,
+  constraint pk_server_workload_hourly primary key (id_snapshot, id_statement),
   constraint cs_server_workload_hourly check (id_snapshot is not null and who is not null and what is not null and when is not null)
 );
 
-grant select on hs_server_workload_hourly to cms_cond_history_r with grant option;
-
-grant select on hs_server_workload_hourly to cms_cond_general with grant option;
+grant select on hs_server_workload_hourly to cms_cond_history_r, cms_cond_general with grant option;
 
 create table hs_server_workload_daily (
   id_snapshot varchar2(6),
+  id_statement number,
   who varchar2(30),
   what varchar2(1000),
   when date,
+  constraint pk_server_workload_daily primary key (id_snapshot, id_statement),
   constraint cs_server_workload_daily check (id_snapshot is not null and who is not null and what is not null and when is not null)
 );
 
-grant select on hs_server_workload_daily to cms_cond_history_r with grant option;
-
-grant select on hs_server_workload_daily to cms_cond_general with grant option;
+grant select on hs_server_workload_daily to cms_cond_history_r, cms_cond_general with grant option;
 
 
 
@@ -150,8 +142,10 @@ create bitmap index bi_server_workload_daily on hs_server_workload_daily (id_sna
 
 create or replace procedure hist_recorder is
   myn sys.all_users.username%TYPE;
-  type my_arr is varray(3) of sys.user_views.view_name%TYPE;
+  seq_name varchar2(8) := 'hist_seq';
+  type my_arr is varray(3) of varchar2(20);
   inv_obj my_arr := my_arr('cond_occupancy', 'det_details', 'server_workload');
+  pks my_arr := my_arr('username, tablespace', 'owner, object, type', 'id_statement');
   type my_freq is varray(2) of varchar2(6);
   inv_freq my_freq := my_freq('hourly', 'daily');
   i number;
@@ -162,9 +156,15 @@ create or replace procedure hist_recorder is
   fsep varchar2(20) := ' is not null and ';
   cursor curr_vw(tbl in varchar2) is select lower(column_name) as cname, lower(column_name || ' ' || data_type || case when data_type like '%VARCHAR%' then '(' || data_length || ')' else '' end) as field from sys.user_tab_columns where table_name = upper(tbl) order by column_id;
   strtt curr_vw%ROWTYPE;
+  mys varchar2(40) := '';
   tbl varchar2(100);
 begin
   select lower(user) into myn from dual;
+  select count(*) into k from sys.user_sequences where sequence_name = upper(seq_name);
+  if k = 0 then
+    execute immediate 'create sequence ' || seq_name || ' nocycle order';
+    execute immediate 'grant select on ' || seq_name || ' to cms_cond_general, ' || myn || '_r with grant option';
+  end if;
   for i in inv_obj.first..inv_obj.last loop
     for j in inv_freq.first..inv_freq.last loop
       select count(*) into k from sys.user_tables where table_name = upper('hs_' || inv_obj(i) || '_' || inv_freq(j));
@@ -176,25 +176,38 @@ begin
           strhh := strhh || '6), ';
         end if;
         fseq := 'id_snapshot' || fsep;
+        if i = inv_obj.last then
+          strhh := strhh || 'id_statement number, ';
+          fseq := fseq || 'id_statement' || fsep;
+        end if;
         for strtt in curr_vw(inv_obj(i))
         loop
           strhh := strhh || strtt.field || ', ';
           fseq := fseq || strtt.cname || fsep;
         end loop;
-        strhh := substr(strhh, 1, length(strhh) - 2) || ', constraint cs_' || inv_obj(i) || '_' || inv_freq(j) || ' check (' || substr(fseq, 1, length(fseq) - 5) || '))';
-        execute immediate(strhh);
-        execute immediate('grant select on ' || myn || '.hs_' || inv_obj(i) || '_' || inv_freq(j) || ' to cms_cond_general, ' || myn || '_r with grant option');
+        strhh := strhh || 'constraint pk_' || inv_obj(i) || '_' || inv_freq(j) || ' primary key (id_snapshot, ' || pks(i) || '), ';
+        strhh := strhh || 'constraint cs_' || inv_obj(i) || '_' || inv_freq(j) || ' check (' || substr(fseq, 1, length(fseq) - 5) || '))';
+        execute immediate strhh;
+        execute immediate 'grant select on ' || myn || '.hs_' || inv_obj(i) || '_' || inv_freq(j) || ' to cms_cond_general, ' || myn || '_r with grant option';
         select count(*) into k from sys.user_indexes where index_name = upper('bi_' || inv_obj(i) || '_' || inv_freq(j));
         if k = 0 then
-          execute immediate('create bitmap index ' || myn || '.bi_' || inv_obj(i) || '_' || inv_freq(j) || ' on hs_' || inv_obj(i) || '_' || inv_freq(j) || ' (id_snapshot)');
+          execute immediate 'create bitmap index ' || myn || '.bi_' || inv_obj(i) || '_' || inv_freq(j) || ' on hs_' || inv_obj(i) || '_' || inv_freq(j) || ' (id_snapshot)';
         end if;
       end if;
       tbl := myn || '.hs_' || inv_obj(i) || '_' || inv_freq(j);
+      if i = inv_obj.last then
+        execute immediate 'alter sequence ' || seq_name || ' minvalue 0';
+        execute immediate 'select ' || seq_name || '.nextval from dual' into k;
+        execute immediate 'alter sequence ' || seq_name || ' increment by -' || k;
+        execute immediate 'select ' || seq_name || '.nextval from dual' into k;
+        execute immediate 'alter sequence ' || seq_name || ' increment by 1';
+        mys := seq_name || '.nextval as id_statement, ';
+      end if;
       if inv_freq(j) like 'hour%' then
-        execute immediate('delete from ' || tbl || ' where id_snapshot = to_char(sysdate, ''HH24'')');
-        execute immediate('insert into ' || tbl || ' select to_char(sysdate, ''HH24'') as id_snapshot, ' || myn || '.' || inv_obj(i) || '.* from ' || myn || '.' || inv_obj(i));
+        execute immediate 'delete from ' || tbl || ' where id_snapshot = to_char(sysdate, ''HH24'')';
+        execute immediate 'insert into ' || tbl || ' select to_char(sysdate, ''HH24'') as id_snapshot, ' || mys || myn || '.' || inv_obj(i) || '.* from ' || myn || '.' || inv_obj(i);
       else
-        execute immediate('insert into ' || tbl || ' select to_char(sysdate, ''YYMMDD'') as id_snapshot, ' || myn || '.' || inv_obj(i) || '.* from ' || myn || '.' || inv_obj(i) || ' where not exists (select 1 from ' || tbl || ' where id_snapshot = to_char(sysdate, ''YYMMDD''))');
+        execute immediate 'insert into ' || tbl || ' select to_char(sysdate, ''YYMMDD'') as id_snapshot, ' || mys || myn || '.' || inv_obj(i) || '.* from ' || myn || '.' || inv_obj(i) || ' where not exists (select 1 from ' || tbl || ' where id_snapshot = to_char(sysdate, ''YYMMDD''))';
       end if;
     end loop;
     commit;
@@ -202,14 +215,18 @@ begin
 end;
 /
 
-create or replace procedure pk_add is
-begin
-  execute immediate('alter table hs_cond_occupancy_hourly add constraint pk_cond_occupancy_hourly primary key (id_snapshot, username, tablespace)');
-  execute immediate('alter table hs_cond_occupancy_daily add constraint pk_cond_occupancy_daily primary key (id_snapshot, username, tablespace)');
-  execute immediate('alter table hs_det_details_hourly add constraint pk_det_details_hourly primary key (id_snapshot, owner, object, type)');
-  execute immediate('alter table hs_det_details_daily add constraint pk_det_details_daily primary key (id_snapshot, owner, object, type)');
-end;
-/
+
+
+/* SEQUENCES */
+
+create sequence hist_seq
+minvalue 1
+nomaxvalue
+increment by 1
+nocycle
+order;
+
+grant select on hist_seq to cms_cond_history_r, cms_cond_general with grant option;
 
 
 
--- CondTools/OracleDBA/monitorsql/condmonitor/readme.txt
+++ CondTools/OracleDBA/monitorsql/condmonitor/readme.txt
@@ -9,7 +9,7 @@ detactivity.php     Web script for showing condition accounts' activity on offli
 detdetails.php      Web script for showing detailed information about condition accounts' database object on offline servers
 histmon.php         Web script for selecting the which snapshot is to be considered by main script
 histmon.sql         Set of SQL statements and PL/SQL scripts for recording historical data for condition accounts,
-                    including tables, grants, views, indexes, procedures and jobs used within
+                    including tables, grants, views, indexes, procedures, sequences and jobs used within
 int2rmon.php        Web script for showing summary information about condition accounts on INT2R, a.k.a. CMS_ORCOFF_PREP
 int9rmon.php        Web script for showing summary information about condition accounts on INT9R, a.k.a. CMS_ORCOFF_INT
 mon.php             Web script containing the engine for int2rmon.php, int9rmon and cmsrmon.php
