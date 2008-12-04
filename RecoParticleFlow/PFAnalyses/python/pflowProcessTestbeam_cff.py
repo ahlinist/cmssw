1198800072507b7198ba0e46a8bfa8495bef2052
ballin <>
2008-12-04 17:15:36 +0000

    Tidying up python configs

--- RecoParticleFlow/PFAnalyses/python/pflowProcessTestbeam_cff.py
+++ RecoParticleFlow/PFAnalyses/python/pflowProcessTestbeam_cff.py
@@ -2,9 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoParticleFlow.PFAnalyses.pflowExtraction_cfi import *
 from RecoParticleFlow.PFAnalyses.pflowFaketracks_cfi import *
-
-from Configuration.StandardSequences.MagneticField_0T_cff import *
-VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
+from RecoParticleFlow.PFAnalyses.pflowProcessTestbeam_cfi import *
 
 TFileService = cms.Service("TFileService",
     fileName=cms.string("outputtree.root")
@@ -16,54 +14,13 @@ finishup = cms.OutputModule("PoolOutputModule",
     
 )
 
-from RecoParticleFlow.PFBlockProducer.particleFlowBlock_cff import *
-particleFlowBlock.RecTracks = cms.InputTag("faketracks", "pfRecTracks")
-particleFlowBlock.GsfRecTracks = cms.InputTag("faketracks", "gsfPfRecTracks")
-particleFlowBlock.PFClustersPS = cms.InputTag("faketracks", "pfPS")
-particleFlowBlock.RecMuons = cms.InputTag("faketracks", "muons")
-#particleFlowBlock.verbose = cms.untracked.bool(True)
-#particleFlowBlock.debug = cms.untracked.bool(True)
+#Clustering only
+pflowClusteringTestbeam = cms.Sequence(pfClusteringECAL * pfClusteringHCAL)
 
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import *
-particleFlowRecHitECAL.ecalRecHitsEE = cms.InputTag("ecalRecHitMaker", "EcalRecHitsEB")
-particleFlowRecHitECAL.ecalRecHitsEB = cms.InputTag("ecalRecHitMaker", "EcalRecHitsEB")
-particleFlowRecHitHCAL.hcalRecHitsHBHE = cms.InputTag("hbhereco")
-particleFlowRecHitHCAL.caloTowers = cms.InputTag("")
-particleFlowRecHitHCAL.isTestbeam = cms.bool(True)
+#Tracking, clustering, pflow - no cleaning
+pflowNoCleaning = cms.Sequence(faketracks * pfClusteringECAL * pfClusteringHCAL * particleFlowBlock * particleFlow)
 
-from Configuration.StandardSequences.Services_cff import *
-from Configuration.StandardSequences.Geometry_cff import *
-from RecoParticleFlow.PFProducer.particleFlow_cff import *
-
-from Geometry.CaloEventSetup.CaloTowerConstituents_cfi import *
-from Configuration.StandardSequences.Reconstruction_cff import *
-
-MessageLogger = cms.Service("MessageLogger",
-    debugModules=cms.untracked.vstring('*'),
-    cout=cms.untracked.PSet(
-        INFO=cms.untracked.PSet(
-            limit=cms.untracked.int32(0)
-        ),
-        PFBlockProducer=cms.untracked.PSet(
-            limit=cms.untracked.int32(10000000)
-        ),
-        PFClusterProducer=cms.untracked.PSet(
-            limit=cms.untracked.int32(10000000)
-        ),
-        DEBUG=cms.untracked.PSet(
-            limit=cms.untracked.int32(0)
-        ),
-        PFProducer=cms.untracked.PSet(
-            limit=cms.untracked.int32(10000000)
-        ),
-        threshold=cms.untracked.string('INFO')
-    ),
-    categories=cms.untracked.vstring('PFClusterProducer',
-        'PFBlockProducer',
-        'PFProducer'),
-    destinations=cms.untracked.vstring('cout')
-)
-
+#The works.
 pflowProcessTestbeam = cms.Sequence(faketracks * 
                                     pfClusteringECAL * 
                                     pfClusteringHCAL * 
