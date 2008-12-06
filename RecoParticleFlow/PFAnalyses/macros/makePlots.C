b9c283165663cbfe589db32521681f802316e231
ballin <>
2008-12-06 12:14:22 +0000

    updates to macros

--- RecoParticleFlow/PFAnalyses/macros/makePlots.C
+++ RecoParticleFlow/PFAnalyses/macros/makePlots.C
@@ -4,28 +4,14 @@
 	gSystem->Load("libFWCoreFWLite.so");
 	gSystem->Load("libRecoParticleFlowPFAnalyses.so");
 	Cintex::Enable();
+
+	std::string graphicsFile("plots/plots_7GeV.ps");
+	std::string macroFile("plot_macros/macro_7GeV.C");
 	
-	std::vector<std::string> runs;
-	runs.push_back("30109");
-	runs.push_back("29737");
-	
-	std::string graphicsFile("plots_7GeV_B.ps");
-	std::string macroFile("macro_7GeV_B.C");
-	
-	
-	//TFile file(run.c_str());
-	//TTree* tree_ = (TTree*) gDirectory->FindObjectAny("Extraction");
 	TChain* chain = new TChain("extraction/Extraction");
-	chain->Add("rfio:///castor/cern.ch/user/b/ballin/outputtree_7GeV_B.root");
-//	for(unsigned i = 0; i < runs.size(); ++i) {
-//		std::string run("../test/outputtree_");
-//		run.append(runs[i]);
-//		run.append(".root");
-//
-//		chain->Add(run.c_str());
-//	}
+	chain->Add("outputtree_4GeV.root");
 	
-	EnergyProcessing ep(chain, graphicsFile.c_str(), macroFile.c_str());
+	EnergyProcessing ep(chain, graphicsFile.c_str(), macroFile.c_str(), 2000);
 	ep.evaluatePlots(true);
 	ep.closeFiles();
 	std::string process(".!ps2pdf ");
