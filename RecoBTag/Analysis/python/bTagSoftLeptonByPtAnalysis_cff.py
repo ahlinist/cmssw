e2829042e1385240c77987247d916cc437c5163e
fwyzard <>
2008-12-12 15:39:01 +0000

    python/

--- RecoBTag/Analysis/python/bTagSoftLeptonByPtAnalysis_cff.py
+++ RecoBTag/Analysis/python/bTagSoftLeptonByPtAnalysis_cff.py
@@ -4,11 +4,13 @@ import FWCore.ParameterSet.Config as cms
 bTagSoftLeptonByPtAnalysisBlock = cms.PSet(
     parameters = cms.PSet(
         discriminatorStart = cms.double(-0.01),
-        nBinEffPur = cms.int32(200),
+        discriminatorEnd   = cms.double(8.01),
+
+        nBinEffPur  = cms.int32(200),
+        startEffPur = cms.double(0.005),
+        endEffPur   = cms.double(0.205),
+
         # the constant b-efficiency for the differential plots versus pt and eta
-        effBConst = cms.double(0.05),
-        endEffPur = cms.double(0.205),
-        discriminatorEnd = cms.double(8.01),
-        startEffPur = cms.double(0.005)
+        effBConst   = cms.double(0.05)
     )
 )
