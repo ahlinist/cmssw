fdcf256785fc4d81c550fb79ae5593accd48df4f
veelken <>
2008-12-15 16:31:48 +0000

    corrected spelling mistake

--- ElectroWeakAnalysis/EWKTau/python/analyzerForMuTau_cfi.py
+++ ElectroWeakAnalysis/EWKTau/python/analyzerForMuTau_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 from ElectroWeakAnalysis.EWKTau.muForEWKTau.muForEWKTauAnalyzer_cfi import *
 
 # import config for muon efficiency histogram producer
-from ElectroWeakAnalysis.EWKTau.muForEWKTau.muForEWKTauHistogramEff_cfi import *
+from ElectroWeakAnalysis.EWKTau.muForEWKTau.muForEWKTauHistEff_cfi import *
 
 # import config for tau analyzer
 #from ElectroWeakAnalysis.EWKTau.tauForEWKTau.tauForEWKTauAnalyzer_cfi import *
