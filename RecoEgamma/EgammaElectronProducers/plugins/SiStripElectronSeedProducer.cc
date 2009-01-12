ad61e208c17672d74accd5a46ee1d2beb6ea21ed
chamont <>
2009-01-12 16:45:53 +0000

    adapt to new electron seed

---
+++ RecoEgamma/EgammaElectronProducers/plugins/SiStripElectronSeedProducer.cc
@@ -0,0 +1,73 @@
+// Producer for making electron seeds from Si Strip hits.
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/EgammaReco/interface/ElectronSeed.h"
+#include "RecoEgamma/EgammaElectronAlgos/interface/SiStripElectronSeedGenerator.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+
+#include "SiStripElectronSeedProducer.h"
+
+#include <iostream>
+
+using namespace reco;
+
+SiStripElectronSeedProducer::SiStripElectronSeedProducer(const edm::ParameterSet& iConfig) : conf_(iConfig)
+{
+  matcher_ = new SiStripElectronSeedGenerator();
+
+  //  get labels from config
+  superClusters_[0]=iConfig.getParameter<edm::InputTag>("barrelSuperClusters");
+  superClusters_[1]=iConfig.getParameter<edm::InputTag>("endcapSuperClusters");
+
+  //register your products
+  produces<ElectronSeedCollection>();
+}
+
+SiStripElectronSeedProducer::~SiStripElectronSeedProducer()
+{
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
+  delete matcher_;
+}
+
+void SiStripElectronSeedProducer::beginJob(edm::EventSetup const&iSetup)
+{
+  matcher_->setupES(iSetup,conf_);
+}
+
+void SiStripElectronSeedProducer::produce(edm::Event& e, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+  using namespace std;
+
+  LogDebug("entering");
+  LogDebug("")  <<"[SiStripElectronSeedProducer::produce] entering " ;
+
+  ElectronSeedCollection *seeds = new ElectronSeedCollection;
+  std::auto_ptr<ElectronSeedCollection> pSeeds;
+
+  // do both barrel and endcap instances
+  for (unsigned int i=0; i<2; i++) {
+
+    // get the superclusters
+    edm::Handle<SuperClusterCollection> clusters;
+    if(e.getByLabel(superClusters_[i],clusters)) {
+      // run the seed generator and put the ElectronSeeds into a collection
+      matcher_->run(e,clusters,*seeds);
+    }
+
+  }
+
+  pSeeds = std::auto_ptr<ElectronSeedCollection>(seeds);
+
+  e.put(pSeeds);
+
+}
+
---
+++ RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeedsParameters_cff.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+ecalDrivenElectronSeedsParameters = cms.PSet(
+
+    # steering
+    fromTrackerSeeds = cms.bool(True),
+    initialSeeds = cms.InputTag("newCombinedSeeds"),
+    preFilteredSeeds = cms.bool(False),
+    dynamicPhiRoad = cms.bool(True),
+    searchInTIDTEC = cms.bool(True), ##  possibility to inhibit extended forward coverage
+
+    # SC filtering
+    SCEtCut = cms.double(4.0),
+    hcalRecHits = cms.InputTag("hbhereco"),
+    maxHOverE = cms.double(0.1),
+    
+    # r/z windows
+    z2MinB = cms.double(-0.09),
+    z2MaxB = cms.double(0.09),
+    r2MinF = cms.double(-0.15),
+    r2MaxF = cms.double(0.15),
+    rMinI = cms.double(-0.2), ## intermediate region SC in EB and 2nd hits in PXF
+    rMaxI = cms.double(0.2), ## intermediate region SC in EB and 2nd hits in PXF
+
+    # phi windows (dynamic)
+    LowPtThreshold = cms.double(5.0),
+    HighPtThreshold = cms.double(35.0),
+    SizeWindowENeg = cms.double(0.675),
+    DeltaPhi1Low = cms.double(0.23),
+    DeltaPhi1High = cms.double(0.08),
+    DeltaPhi2 = cms.double(0.004),
+
+    # phi windows (non dynamic, overwritten in case dynamic is selected)
+    ePhiMin1 = cms.double(-0.125),
+    ePhiMax1 = cms.double(0.075),
+    pPhiMin1 = cms.double(-0.075),
+    pPhiMax1 = cms.double(0.125),
+    PhiMin2 = cms.double(-0.002),
+    PhiMax2 = cms.double(0.002),
+    
+)
+
