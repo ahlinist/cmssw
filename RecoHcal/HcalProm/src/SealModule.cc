59d4ee46ea8e10e5373ea80966a8a3514ab600ef
efe <>
2009-01-13 08:40:33 +0000

    update to add HcalPromBremss

--- RecoHcal/HcalProm/src/SealModule.cc
+++ RecoHcal/HcalProm/src/SealModule.cc
@@ -5,9 +5,11 @@
 #include "HcalProm.h"
 #include "FRPedestalsMaker.h"
 #include "L1GTFilter.h"
+#include "HcalPromBremss.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalTemplate);
 DEFINE_ANOTHER_FWK_MODULE(HcalProm);
 DEFINE_ANOTHER_FWK_MODULE(FRPedestalsMaker);
 DEFINE_ANOTHER_FWK_MODULE(L1GTFilter);
+DEFINE_ANOTHER_FWK_MODULE(HcalPromBremss);
