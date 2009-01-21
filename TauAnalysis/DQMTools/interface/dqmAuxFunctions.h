2e450c46b5d4a611ac7c5d62ebee2d017f60c587
veelken <>
2009-01-21 16:05:00 +0000

    added DQM-based histogram manipulation tools

---
+++ TauAnalysis/DQMTools/interface/dqmAuxFunctions.h
@@ -0,0 +1,23 @@
+#ifndef TauAnalysis_DQMTools_dqmAuxFunctions_h
+#define TauAnalysis_DQMTools_dqmAuxFunctions_h
+
+#include "DQMServices/Core/interface/DQMStore.h"
+
+#include <string>
+
+const std::string parKeyword = "#PAR#";
+const std::string plotKeyword = "#PLOT#";
+const std::string rangeKeyword = "#RANGE";
+const std::string processDirKeyword = "#PROCESSDIR#";
+
+std::string dqmDirectoryName(const std::string&);
+std::string dqmSubDirectoryName_merged(const std::string&, const std::string&);
+void dqmCopyRecursively(DQMStore&, const std::string&, const std::string&, double, int, bool);
+
+const std::string dqmSeparator = "/";
+//const std::string dqmRootDirectory = std::string(dqmSeparator).append("DQMData").append(dqmSeparator);
+const std::string dqmRootDirectory = "";
+
+void separateHistogramFromDirectoryName(const std::string&, std::string&, std::string&);
+
+#endif
