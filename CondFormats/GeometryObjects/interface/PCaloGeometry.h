f828ef50d09db92a491297fa514fc768361db37c
heltsley <>
2009-01-21 18:53:37 +0000

    Changed name of calo geometry class

---
+++ CondFormats/GeometryObjects/interface/PCaloGeometry.h
@@ -0,0 +1,25 @@
+#ifndef CondFormats_PCaloGeometry_h
+#define CondFormats_PCaloGeometry_h
+
+#include <vector>
+
+class PCaloGeometry{
+
+ public:
+  PCaloGeometry();
+  PCaloGeometry(std::vector<double>,std::vector<double>,std::vector<uint32_t>);
+  ~PCaloGeometry(){};
+
+  std::vector<double> getTranslation() const {return m_translation;};
+  std::vector<double> getDimension() const {return m_dimension;};
+  std::vector<uint32_t> getIndexes() const {return m_indexes;};
+
+ private:
+  std::vector<double> m_translation;
+  std::vector<double> m_dimension;
+  std::vector<uint32_t> m_indexes;
+
+};
+
+#endif
+
