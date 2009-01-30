75732c8e5747a60fe176d057351a7e01dbff0dfa
hyteng <>
2009-01-30 13:49:25 +0000

    adding RPCSeedGenerator module

--- RecoMuon/SeedGenerator/src/SealModule.cc
+++ RecoMuon/SeedGenerator/src/SealModule.cc
@@ -4,6 +4,9 @@
 
 #include "RecoMuon/SeedGenerator/src/SETMuonProducer.h"
 
+#include "RecoMuon/SeedGenerator/src/RPCSeedGenerator.h"
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SETMuonProducer);
+DEFINE_ANOTHER_FWK_MODULE(RPCSeedGenerator);
 
