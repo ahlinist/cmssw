5decb5b5d9a6c5d3ec7a66aefe850348bf711574
chinhan <>
2009-02-01 17:39:50 +0000

    Added muCaloTau skim path.

--- TauAnalysis/Skimming/test/getSkimEfficiencyFromTrigReport.py
+++ TauAnalysis/Skimming/test/getSkimEfficiencyFromTrigReport.py
@@ -8,7 +8,7 @@ def main(argv) :
     from Trigger Report outputs
     
     Usage:
-    getSkimEfficiencyFromLogs <FILELIST>
+    ./getSkimEfficiencyFromLogs <FILELIST>
     e.g.: ./getSkimEfficiencyFromLogs.py LOGDIR/*.log'
     
     """
@@ -20,14 +20,17 @@ def main(argv) :
     FLIST   = argv
                 
     ### Define list of filter modules here!!! ###
-    Filters =  ['selectedTaus','selectedMuons','muTauPairs','selectedMuTauPairs']
+    PathName = 'muPFTauSkimPath'
+    Filters =  ['selectedMuons','selectedPFTaus','muPFTauPairs','selectedMuPFTauPairs']
+    PathName2 = 'muCaloTauSkimPath'
+    Filters2 =  ['selectedMuons','selectedCaloTaus','muCaloTauPairs','selectedMuCaloTauPairs']
 
     ### Init counters ###
     NEvents = 0
     NPassed = [0] * len(Filters)
 
     # Reg. Expr. patterns to find right lines
-    fullEntryPattern = re.compile('TrigReport ---------- Modules in Path: muTauSkimPath ----[\t\n\r\f\v\W\w]+TrigReport ------ Modules in End-Path:')
+    fullEntryPattern = re.compile('TrigReport ---------- Modules in Path: '+PathName+' ----[\t\n\r\f\v\W\w]+TrigReport ---------- Modules in Path: muCaloTauSkimPath')
     ppat = 'TrigReport [\W\w]+ '
     p = []
     for i in range(0,len(Filters)):
@@ -51,7 +54,7 @@ def main(argv) :
         input.close()
 
     print '\n******************************'
-    print '*** skimMuTau Efficiencies ***'
+    print PathName+' Efficiencies ***'
     print '******************************'
     print 'Events processed:',NEvents
     print 'Filter                      Passed   Efficiency   Cumul. Efficiency'
@@ -70,8 +73,61 @@ def main(argv) :
     print '---------------------------------------------------------------'
     last = len(Filters)-1
     print '%24s: %8d                 %5.3f'%('Total',NPassed[last],float(NPassed[last])/float(NEvents))
+
+
+
+
+### Second loop on muCaloTauFulter
+
+    ### Init counters ###
+    NEvents = 0
+    NPassed = [0] * len(Filters2)
+
+    # Reg. Expr. patterns to find right lines
+    fullEntryPattern = re.compile('TrigReport ---------- Modules in Path: '+PathName2+' ----[\t\n\r\f\v\W\w]+TrigReport ------ Modules in End-Path:')
+    ppat = 'TrigReport [\W\w]+ '
+    p = []
+    for i in range(0,len(Filters2)):
+        p.append(re.compile(ppat+Filters2[i]))
+
+    # Loop over all files
+    for filen in FLIST:
+        input = file(filen)
+        fullTxt = input.read() # read full txt
+        m0 = fullEntryPattern.search(fullTxt) # extract region
+        if (m0):
+            lines = re.split('\n',m0.group())
+            for line in lines:
+                for i in range(0,len(p)):
+                    m = p[i].search(line)
+                    if (m):
+                        tabs = re.split('[\s]+',m.group())
+                        if (i==0):
+                            NEvents = NEvents + int(tabs[3])
+                        NPassed[i] = NPassed[i] + int(tabs[4])            
+        input.close()
+
+    print '\n******************************'
+    print PathName2+' Efficiencies ***'
+    print '******************************'
+    print 'Events processed:',NEvents
+    print 'Filter                      Passed   Efficiency   Cumul. Efficiency'
+    print '---------------------------------------------------------------'
+    eff = -1.
+    # Treat first entry
+    if (float(NEvents!=0)):
+        eff = float(NPassed[0])/float(NEvents)
+    print '%24s: %8d     %5.3f       %5.3f'%(Filters2[0],NPassed[0],float(NPassed[0])/float(NEvents),float(NPassed[0])/float(NEvents))
+    for i in range(1,len(Filters2)):
+        if (float(NPassed[i-1]==0)):
+            eff = -1.
+        else:
+            eff = float(NPassed[i])/float(NPassed[i-1])                
+        print '%24s: %8d     %5.3f       %5.3f'%(Filters2[i],NPassed[i],eff,float(NPassed[i])/float(NEvents))
+    print '---------------------------------------------------------------'
+    last = len(Filters2)-1
+    print '%24s: %8d                 %5.3f'%('Total',NPassed[last],float(NPassed[last])/float(NEvents))
     
     
 if __name__ == '__main__' :
     main(sys.argv[1:])
-
