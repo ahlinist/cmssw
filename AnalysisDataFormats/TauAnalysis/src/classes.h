7c7ea349be49b03d79b3b4169a48631c68c485b3
veelken <>
2009-02-04 15:37:00 +0000

    make DiTauCandidate depend on two template parameters for the visible tau decay products

--- AnalysisDataFormats/TauAnalysis/src/classes.h
+++ AnalysisDataFormats/TauAnalysis/src/classes.h
@@ -1,16 +1,40 @@
+
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositeRefCandidateT1T2MEt.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositeRefCandidateT1T2MEtFwd.h"
+
 #include "DataFormats/Common/interface/Wrapper.h"
 
-#include "AnalysisDataFormats/TauAnalysis/interface/DiTauCandidate.h"
-
-#include <vector>
-
 namespace {
-   struct AnalysisDataFormats_TauAnalysis {
+  struct AnalysisDataFormats_TauAnalysis_dictionary {
+    /// create dictionaries for DiCandidatePair objects
+    DiCandidatePair dummyDiCandidatePair;
+    edm::Wrapper<DiCandidatePair> dummyDiCandidatePairWrapper;
+    DiCandidatePairCollection dummyDiCandidatePairCollection;
+    edm::Wrapper<DiCandidatePairCollection> dummyDiCandidatePairCollectionWrapper;
 
-     DiTauCandidate d10;
-     edm::Wrapper<DiTauCandidate> dummy10;
-     std::vector<DiTauCandidate> dummy20;
-     edm::Wrapper<std::vector<DiTauCandidate> > dummy30;
-
-   };
+    /// create dictionaries for PATElecTauPair objects
+    PATElecTauPair dummyPATElecTauPair;
+    edm::Wrapper<PATElecTauPair> dummyPATElecTauPairWrapper;
+    PATElecTauPairCollection dummyPATElecTauPairCollection;
+    edm::Wrapper<PATElecTauPairCollection> dummyPATElecTauPairCollectionWrapper;
+    
+    /// create dictionaries for PATMuTauPair objects
+    PATMuTauPair dummyPATMuTauPair;
+    edm::Wrapper<PATMuTauPair> dummyPATMuTauPairWrapper;
+    PATMuTauPairCollection dummyPATMuTauPairCollection;
+    edm::Wrapper<PATMuTauPairCollection> dummyPATMuTauPairCollectionWrapper;
+    
+    /// create dictionaries for PATDiTauPair objects
+    PATDiTauPair dummyPATDiTauPair;
+    edm::Wrapper<PATDiTauPair> dummyPATDiTauPairWrapper;
+    PATDiTauPairCollection dummyPATDiTauPairCollection;
+    edm::Wrapper<PATDiTauPairCollection> dummyPATDiTauPairCollectionWrapper;
+    
+    /// create dictionaries for PATElecMuPair objects
+    //PATElecMuPair dummyPATElecMuPair;
+    CompositeRefCandidateT1T2MEt<pat::Electron, pat::Muon> dummyPATElecMuPair;
+    edm::Wrapper<PATElecMuPair> dummyPATElecMuPairWrapper;
+    PATElecMuPairCollection dummyPATElecMuPairCollection;
+    edm::Wrapper<PATElecMuPairCollection> dummyPATElecMuPairCollectionWrapper;
+  };
 }
