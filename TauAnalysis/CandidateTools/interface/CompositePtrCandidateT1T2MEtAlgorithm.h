91fff8c4eb633948fa43922d510bd429cbcf7d8b
veelken <>
2009-02-04 17:32:49 +0000

    changes accoring to replacing edm::Ref by edm::Ptr for template arguments of DiTauCandidate

---
+++ TauAnalysis/CandidateTools/interface/CompositePtrCandidateT1T2MEtAlgorithm.h
@@ -0,0 +1,157 @@
+#ifndef TauAnalysis_CandidateTools_CompositePtrCandidateT1T2MEtAlgorithm_h
+#define TauAnalysis_CandidateTools_CompositePtrCandidateT1T2MEtAlgorithm_h
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "PhysicsTools/Utilities/interface/deltaR.h"
+#include "DataFormats/Math/interface/normalizedPhi.h"
+
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
+
+#include "DataFormats/Candidate/interface/CandidateFwd.h" 
+#include "DataFormats/Candidate/interface/Candidate.h" 
+
+#include <TMath.h>
+
+template<typename T1, typename T2>
+class CompositePtrCandidateT1T2MEtAlgorithm 
+{
+  typedef edm::Ptr<T1> T1Ptr;
+  typedef edm::Ptr<T2> T2Ptr;
+
+ public:
+
+  CompositePtrCandidateT1T2MEtAlgorithm(const edm::ParameterSet& cfg)
+  {
+    recoMode_ = cfg.getParameter<std::string>("recoMode");
+    verbosity_ = cfg.getUntrackedParameter<int>("verbosity", 0);
+  }
+  ~CompositePtrCandidateT1T2MEtAlgorithm() {}
+
+  CompositePtrCandidateT1T2MEt<T1,T2> buildCompositePtrCandidate(const T1Ptr leg1, 
+								 const T2Ptr leg2, 
+								 const reco::CandidatePtr met)
+  {
+    CompositePtrCandidateT1T2MEt<T1,T2> compositePtrCandidate(leg1, leg2, met);
+  
+    if ( leg1.isNull() || 
+         leg2.isNull() ) {
+      edm::LogError ("CompositePtrCandidateT1T2MEtAlgorithm") << " Pointers to visible Decay products invalid !!";
+      return compositePtrCandidate;
+    }
+
+//--- compute quantities that are independent of MET	
+    compositePtrCandidate.setCharge(leg1->charge() + leg2->charge());
+    compositePtrCandidate.setP4Vis(leg1->p4() + leg2->p4());
+    compositePtrCandidate.setDR12(reco::deltaR(leg1->p4(), leg2->p4()));
+    compositePtrCandidate.setDPhi12(normalizedPhi(leg1->phi() - leg2->phi()));
+    compositePtrCandidate.setVisEtaMin(TMath::Min(leg1->eta(), leg2->eta()));
+    compositePtrCandidate.setVisEtaMax(TMath::Max(leg1->eta(), leg2->eta()));
+
+//--- compute quantities that do dependent on MET
+    if ( met.isNonnull() ) {
+      compCollinearApprox(compositePtrCandidate, leg1->p4(), leg2->p4(), met->px(), met->py());
+    
+      compositePtrCandidate.setP4CDFmethod(compP4CDFmethod(leg1->p4(), leg2->p4(), met->px(), met->py()));
+      compositePtrCandidate.setMt12MET(compMt(leg1->p4(), leg2->p4(), met->px(), met->py()));    
+      compositePtrCandidate.setMt1MET(compMt(leg1->p4(), met->px(), met->py()));
+      compositePtrCandidate.setMt2MET(compMt(leg2->p4(), met->px(), met->py()));
+      compositePtrCandidate.setDPhi1MET(normalizedPhi(leg1->phi() - met->phi()));
+      compositePtrCandidate.setDPhi2MET(normalizedPhi(leg2->phi() - met->phi()));
+    } else {
+      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), -1, -1, false);
+    }
+ 
+//--- set compositePtr four-momentum
+//    (depending on recoMode configuration parameter)
+    if ( recoMode_ == "collinearApprox" ) {
+      if ( met.isNonnull() ) {
+        compositePtrCandidate.setP4(compositePtrCandidate.p4CollinearApprox());
+      } else {
+        edm::LogError ("buildCompositePtrCandidate") << " Failed to set four-momentum:"
+						     << " recoMode = " << recoMode_ << " requires MET pointer to be valid !!";
+      }
+    } else if ( recoMode_ == "cdfMethod" ) {
+      if ( met.isNonnull() ) {
+	compositePtrCandidate.setP4(compositePtrCandidate.p4CDFmethod());
+      } else {
+	edm::LogError ("buildCompositePtrCandidate") << " Failed to set four-momentum:"
+						     << " recoMode = " << recoMode_ << " requires MET pointer to be valid !!";
+      }
+    } else if ( recoMode_ == "" ) {
+      compositePtrCandidate.setP4(compositePtrCandidate.p4Vis());
+    } else {
+      edm::LogError ("buildCompositePtrCandidate") << " Failed to set four-momentum:"
+						   << " recoMode = " << recoMode_ << " undefined !!";
+    }  
+    
+    return compositePtrCandidate;
+  }
+
+ private: 
+
+  void compCollinearApprox(CompositePtrCandidateT1T2MEt<T1,T2>& compositePtrCandidate,
+			   const reco::Candidate::LorentzVector& leg1,
+			   const reco::Candidate::LorentzVector& leg2,
+			   double metPx, double metPy)
+  {
+    double x1_numerator = leg1.px()*leg2.py() - leg2.px()*leg1.py();
+    double x1_denominator = leg2.py()*(leg1.px() + metPx) - leg2.px()*(leg1.py() + metPy);
+    double x1 = ( x1_denominator != 0. ) ? x1_numerator/x1_denominator : -1.;
+
+    double x2_numerator = x1_numerator;
+    double x2_denominator = leg1.px()*(leg2.py() + metPy) - leg1.py()*(leg2.px() + metPx);
+    double x2 = ( x2_denominator != 0. ) ? x2_numerator/x2_denominator : -1.;
+
+    if ( (x1 > 0. && x1 < 1.) &&
+         (x2 > 0. && x2 < 1.) ) {
+      reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+      compositePtrCandidate.setCollinearApproxQuantities(p4, x1, x2, true);
+    } else {
+      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), -1, -1, false);
+    }
+  }
+  reco::Candidate::LorentzVector compP4CDFmethod(const reco::Candidate::LorentzVector& leg1, 
+						 const reco::Candidate::LorentzVector& leg2, 
+						 double metPx, double metPy)
+  {
+    double px = leg1.px() + leg2.px() + metPx;
+    double py = leg1.py() + leg2.py() + metPy;
+    double pz = leg1.pz() + leg2.pz();
+    double e = leg1.energy() + leg2.energy() + TMath::Sqrt(metPx*metPx + metPy*metPy);
+    reco::Candidate::LorentzVector p4(px, py, pz, e);
+    return p4;
+  }
+  double compMt(const reco::Candidate::LorentzVector& leg1, 
+		const reco::Candidate::LorentzVector& leg2, 
+		double metPx, double metPy)
+  {
+    double px = leg1.px() + leg2.px() + metPx;
+    double py = leg1.py() + leg2.py() + metPy;
+    double et = leg1.Et() + leg2.Et() + TMath::Sqrt(metPx*metPx + metPy*metPy);
+    double mt2 = et*et - (px*px + py*py);
+    if ( mt2 < 0 ) {
+      edm::LogError ("compMt") << " mt2 = " << mt2 << " must not be negative !!";
+      return -1.;
+    }
+    return TMath::Sqrt(mt2);
+  }
+  double compMt(const reco::Candidate::LorentzVector& visParticle, 
+		double metPx, double metPy)
+  {
+    double px = visParticle.px() + metPx;
+    double py = visParticle.py() + metPy;
+    double et = visParticle.Et() + TMath::Sqrt(metPx*metPx + metPy*metPy);
+    double mt2 = et*et - (px*px + py*py);
+    if ( mt2 < 0 ) {
+      edm::LogError ("compMt") << " mt2 = " << mt2 << " must not be negative !!";
+      return -1.;
+    }
+    return TMath::Sqrt(mt2);
+  }
+
+  std::string recoMode_;
+  int verbosity_;
+};
+#endif 
+
---
+++ TauAnalysis/Core/plugins/CompositePtrCandidateT1T2MEtHistManager.h
@@ -0,0 +1,74 @@
+#ifndef TauAnalysis_Core_DiTauCandidateHistManager_h  
+#define TauAnalysis_Core_DiTauCandidateHistManager_h
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "TauAnalysis/Core/interface/HistManagerBase.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
+
+#include <vector>
+#include <string>
+
+template<typename T1, typename T2>
+class CompositePtrCandidateT1T2MEtHistManager : public HistManagerBase 
+{
+ public:
+  
+  explicit CompositePtrCandidateT1T2MEtHistManager(const edm::ParameterSet&);
+  ~CompositePtrCandidateT1T2MEtHistManager();
+  
+  void bookHistograms(const edm::EventSetup&);
+  void fillHistograms(const edm::Event&, const edm::EventSetup&);
+
+ private:
+
+//--- configuration parameters
+  edm::InputTag diTauCandidateSrc_;
+
+  std::string dqmDirectory_store_;
+
+  bool requireGenMatch_;
+
+//--- histograms
+  MonitorElement* hDiTauCandidatePt_;
+  MonitorElement* hDiTauCandidateEta_;
+  MonitorElement* hDiTauCandidatePhi_;
+  MonitorElement* hDiTauCandidateMass_;
+
+  MonitorElement* hVisPt_;
+  MonitorElement* hVisPhi_;
+  MonitorElement* hVisMass_;
+
+  MonitorElement* hCollinearApproxEta_;
+  MonitorElement* hCollinearApproxMass_;
+  MonitorElement* hCollinearApproxMassVsPt_;
+  MonitorElement* hCollinearApproxMassVsDPhi12_;
+  MonitorElement* hCollinearApproxX1_;
+  MonitorElement* hCollinearApproxX2_;
+  MonitorElement* hCollinearApproxX1vsX2_;
+
+  MonitorElement* hCDFmethodMass_;
+
+  MonitorElement* hMt12MET_;
+
+  MonitorElement* hMt1MET_;
+  MonitorElement* hMt2MET_;
+  
+  MonitorElement* hDPhi12_;
+  MonitorElement* hDR12_;
+
+  MonitorElement* hVisEtaMin_;
+  MonitorElement* hVisEtaMax_;
+
+  MonitorElement* hDPhi1MET_;
+  MonitorElement* hDPhi2MET_;
+  MonitorElement* hDPhi1METvsDPhi2MET_;
+};
+
+#endif  
+
+
