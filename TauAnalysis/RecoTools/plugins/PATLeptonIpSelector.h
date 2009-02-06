16e43d29363e2966fb17527810146b13b5fe8c87
veelken <>
2009-02-06 14:18:59 +0000

    - added transverse impact parameter selection for leading track of pat::Taus - bugfix with temporary object ("patLeptonOutput"), the address of which was stored in the "selected_" vector,   causing a segmentation violation of cmsRun

--- TauAnalysis/RecoTools/plugins/PATLeptonIpSelector.h
+++ TauAnalysis/RecoTools/plugins/PATLeptonIpSelector.h
@@ -1,51 +1,54 @@
-//
-// 
-//
-
 #ifndef TauAnalysis_RecoTools_PATLeptonIpSelector_h
 #define TauAnalysis_RecoTools_PATLeptonIpSelector_h
+
+/** \class PATLeptonIpSelector
+ *
+ * Select electrons, muons and tau-jets based
+ * on track transverse impact parameter
+ * with respect to primary event vertex
+ * 
+ * \author Konstantinos A. Petridis, Imperial College;
+ *  modified by Christian Veelken
+ *
+ * \version $Revision: 1.1 $
+ *
+ * $Id: PATLeptonIpSelector.h,v 1.1 2009/01/29 13:22:18 veelken Exp $
+ *
+ */
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 
-#include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
-#include "DataFormats/PatCandidates/interface/Electron.h"
-#include "DataFormats/PatCandidates/interface/Muon.h"
-#include "DataFormats/PatCandidates/interface/Tau.h"
-#include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
-
 #include <vector>
-#include <string>
 
-template <class T>
+template <typename T, typename TExtr>
 class PATLeptonIpSelector
 {
-
   public:
     
     typedef std::vector<T> collection;
   
-    explicit PATLeptonIpSelector(const edm::ParameterSet & iConfig);
+    explicit PATLeptonIpSelector(const edm::ParameterSet&);
     ~PATLeptonIpSelector();
 
     typename std::vector<const T*>::const_iterator begin() const { return selected_.begin(); }
     typename std::vector<const T*>::const_iterator end() const { return selected_.end(); }
 
-
-    void select(const edm::Handle<collection>&,edm::Event & iEvent, const edm::EventSetup & iSetup);
+    void select(const edm::Handle<collection>&, edm::Event&, const edm::EventSetup&);
     
     size_t size() const { return selected_.size(); }
 
   private:
     std::vector<const T*> selected_;
-    edm::InputTag leptonSrc_;
-    std::string leptonType_;
     edm::InputTag vertexSrc_;
-    double ipCut_;
+    double ipMin_;
+    bool applyIpMin_;
+    double ipMax_;
+    bool applyIpMax_;
 
+    TExtr ipExtractor_;
 };
 
 #endif
