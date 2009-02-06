6ac0a9001de26566ebef70000342264cf717154d
veelken <>
2009-02-06 14:18:59 +0000

    - added transverse impact parameter selection for leading track of pat::Taus - bugfix with temporary object ("patLeptonOutput"), the address of which was stored in the "selected_" vector,   causing a segmentation violation of cmsRun

--- TauAnalysis/RecoTools/plugins/PATLeptonIpSelector.cc
+++ TauAnalysis/RecoTools/plugins/PATLeptonIpSelector.cc
@@ -1,75 +1,120 @@
-//
-// 
-//
-
 #include "TauAnalysis/RecoTools/plugins/PATLeptonIpSelector.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
-template <class T>
-PATLeptonIpSelector<T>::PATLeptonIpSelector(const edm::ParameterSet & iConfig) 
+template <typename T, typename TExtr>
+PATLeptonIpSelector<T,TExtr>::PATLeptonIpSelector(const edm::ParameterSet& cfg) 
 {
- 
-  leptonSrc_ = iConfig.getParameter<edm::InputTag>("leptonSource");
-  leptonType_ = iConfig.getParameter<std::string>("leptonType");
-  vertexSrc_ = iConfig.getParameter<edm::InputTag>("vertexSource");
-  ipCut_ = iConfig.getParameter<double>("IpCut");
+  vertexSrc_ = cfg.getParameter<edm::InputTag>("vertexSource");
+
+  if ( cfg.exists("IpMin") ) {
+    ipMin_ = cfg.getParameter<double>("IpMin");
+    applyIpMin_ = true;
+  } else {
+    applyIpMin_ = false;
+  }
+
+  if ( cfg.exists("IpMax") ) {
+    ipMax_ = cfg.getParameter<double>("IpMax");
+    applyIpMax_ = true;
+  } else {
+    applyIpMax_ = false;
+  }
 }
 
-template <class T>
-PATLeptonIpSelector<T>::~PATLeptonIpSelector() 
+template <typename T, typename TExtr>
+PATLeptonIpSelector<T,TExtr>::~PATLeptonIpSelector() 
 {
 //--- nothing to be done yet...
 }
 
-template <class T>
-void PATLeptonIpSelector<T>::select(const edm::Handle<collection>& patCollection,
-				    edm::Event & iEvent, const edm::EventSetup & iSetup) 
+template <typename T, typename TExtr>
+void PATLeptonIpSelector<T,TExtr>::select(const edm::Handle<collection>& patLeptonCollection,
+					  edm::Event& evt, const edm::EventSetup& es) 
 {
   selected_.clear();
   
-  typedef std::vector<T> patLeptonCollection;
-  
-  typedef edm::Handle<patLeptonCollection> patLeptonCollectionHandle;
-  std::vector<patLeptonCollectionHandle> patSelLeptonCollections;
-  
-  patLeptonCollectionHandle patLeptonInputCollection;
-  iEvent.getByLabel(leptonSrc_, patLeptonInputCollection);
-  
-  edm::Handle<reco::VertexCollection> thePVs;
-  iEvent.getByLabel(vertexSrc_, thePVs);
-  const reco::VertexCollection vertCollection=*(thePVs.product());
-  reco::Vertex thePV;
-  thePV=*(vertCollection.begin());
+  edm::Handle<reco::VertexCollection> primaryEventVertexCollection;
+  evt.getByLabel(vertexSrc_, primaryEventVertexCollection);
+  if ( primaryEventVertexCollection->size() < 1 ) {
+    return;
+  } else if ( primaryEventVertexCollection->size() > 1 ) {
+    edm::LogError ("PATLeptonIpSelector::select") << " Cannot have more than one primary event vertex --> skipping !!";
+    return;
+  } 
 
-  patLeptonCollection collection=*patCollection;
-  for ( typename patLeptonCollection::const_iterator patLeptonInput = patLeptonInputCollection->begin(); 
-	patLeptonInput != patLeptonInputCollection->end(); ++patLeptonInput ) {
+  const reco::Vertex& thePrimaryEventVertex = (*primaryEventVertexCollection->begin());
 
-    //--- copy pat::Lepton object
-    T patLeptonOutput(*patLeptonInput);
-    double ip = -100000.;
-    if(leptonType_=="electron")
-      ip=patLeptonOutput.gsfTrack()->dxy(thePV.position());
-    else if(leptonType_=="muon")
-      ip=patLeptonOutput.track()->dxy(thePV.position());
-    else
-      std::cout<<"==== USE electron or muon TYPE ===="<<std::endl;
-    
-    if(fabs(ip)<ipCut_)
-      selected_.push_back(&patLeptonOutput);
+  for ( typename collection::const_iterator patLepton = patLeptonCollection->begin(); 
+	patLepton != patLeptonCollection->end(); ++patLepton ) {
+
+    bool ipIsValid = false;
+    double ip = ipExtractor_(*patLepton, thePrimaryEventVertex, ipIsValid);
+    if ( ipIsValid && (!applyIpMin_ || ip > ipMin_) &&
+	              (!applyIpMax_ || ip < ipMax_) ) {
+      selected_.push_back(&(*patLepton));
+    }
   }
-  
 }
 
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
 
+struct PATElectronIpExtractor
+{
+  double operator()(const pat::Electron& electron, const reco::Vertex& primaryEventVertex, bool& isValid) 
+  {
+    if ( electron.gsfTrack().isNonnull() ) {
+      isValid = true;
+      return electron.gsfTrack()->dxy(primaryEventVertex.position());
+    } else {
+      isValid = false;
+      return 0.;
+    }
+  }
+};
 
-typedef ObjectSelector<PATLeptonIpSelector<pat::Electron> > PATElectronIpSelector;
-typedef ObjectSelector<PATLeptonIpSelector<pat::Muon> > PATMuonIpSelector;
+struct PATMuonIpExtractor
+{
+  double operator()(const pat::Muon& muon, const reco::Vertex& primaryEventVertex, bool& isValid) 
+  {
+    if ( muon.track().isNonnull() ) {
+      isValid = true;
+      return muon.track()->dxy(primaryEventVertex.position());
+    } else {
+      isValid = false;
+      return 0.;
+    }
+  }
+};
 
+struct PATTauIpExtractor
+{
+  double operator()(const pat::Tau& tau, const reco::Vertex& primaryEventVertex, bool& isValid) 
+  {
+    if ( tau.leadTrack().isNonnull() ) {
+      isValid = true;
+      return tau.leadTrack()->dxy(primaryEventVertex.position());
+    } else {
+      isValid = false;
+      return 0.;
+    }
+  }
+};
 
+#include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
+
+typedef ObjectSelector<PATLeptonIpSelector<pat::Electron, PATElectronIpExtractor> > PATElectronIpSelector;
+typedef ObjectSelector<PATLeptonIpSelector<pat::Muon, PATMuonIpExtractor> > PATMuonIpSelector;
+typedef ObjectSelector<PATLeptonIpSelector<pat::Tau, PATTauIpExtractor> > PATTauIpSelector;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 DEFINE_ANOTHER_FWK_MODULE(PATElectronIpSelector);
 DEFINE_ANOTHER_FWK_MODULE(PATMuonIpSelector);
-                          
+DEFINE_ANOTHER_FWK_MODULE(PATTauIpSelector);
