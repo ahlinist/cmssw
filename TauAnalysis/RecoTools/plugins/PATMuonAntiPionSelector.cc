998a8e71949ed59b7c56d93319d86c62941127dd
veelken <>
2009-02-06 14:19:27 +0000

    minor code clean-up

--- TauAnalysis/RecoTools/plugins/PATMuonAntiPionSelector.cc
+++ TauAnalysis/RecoTools/plugins/PATMuonAntiPionSelector.cc
@@ -1,18 +1,35 @@
-// ********************************************************************************************
-// *** The selector chooses only the PAT muons that satisfy a invariant mass requirement    ***
-// *** when combined with a PAT tau.                                                        ***
-// ***                                                                                      ***
-// ***                                                                  October 8, 2008     ***
-// ***                                                                  Alfredo Gurrola     ***
-// ********************************************************************************************
-
 #include "TauAnalysis/RecoTools/plugins/PATMuonAntiPionSelector.h"
+
+#include "DataFormats/MuonReco/interface/MuonSelectors.h"
+
+PATMuonAntiPionSelectorImp::PATMuonAntiPionSelectorImp(const edm::ParameterSet& cfg )
+{ 
+  coeffCaloComp_ = cfg.getParameter<double>("CaloCompCoefficient");
+  coeffSegmComp_ = cfg.getParameter<double>("SegmCompCoefficient");
+  cut_ = cfg.getParameter<double>("AntiPionCut");
+}
+
+void PATMuonAntiPionSelectorImp::select(const edm::Handle<collection>& patMuonCollection,
+					edm::Event& evt, const edm::EventSetup& es) 
+{
+  selected_.clear();
+
+  for ( collection::const_iterator patMuon = patMuonCollection->begin();
+	patMuon != patMuonCollection->end(); ++patMuon ) {
+    double discriminant = coeffCaloComp_*muon::caloCompatibility(*patMuon) 
+                         + coeffSegmComp_*muon::segmentCompatibility(*patMuon);
+    if ( discriminant > cut_ ) {
+      selected_.push_back(&(*patMuon));
+    }
+  }
+}
 
 #include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-typedef ObjectSelector<PATMuonAntiPionSelector> PATMuonAntiPionSelection;
-DEFINE_ANOTHER_FWK_MODULE(PATMuonAntiPionSelection);
+typedef ObjectSelector<PATMuonAntiPionSelectorImp> PATMuonAntiPionSelector;
+
+DEFINE_ANOTHER_FWK_MODULE(PATMuonAntiPionSelector);
 
 
