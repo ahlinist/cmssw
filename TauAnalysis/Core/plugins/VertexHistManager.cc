8bd6729088db457769e40384d0dd5d19d150d1f9
veelken <>
2009-02-10 14:04:35 +0000

    added filling of primary vertex related histograms

---
+++ TauAnalysis/Core/plugins/VertexHistManager.cc
@@ -0,0 +1,82 @@
+#include "TauAnalysis/Core/plugins/VertexHistManager.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+
+#include "TauAnalysis/Core/interface/histManagerAuxFunctions.h"
+
+#include <TMath.h>
+
+VertexHistManager::VertexHistManager(const edm::ParameterSet& cfg)
+{
+  //std::cout << "<VertexHistManager::VertexHistManager>:" << std::endl;
+
+  vertexSrc_ = cfg.getParameter<edm::InputTag>("vertexSource");
+  //std::cout << " vertexSrc = " << vertexSrc_ << std::endl;
+
+  dqmDirectory_store_ = cfg.getParameter<std::string>("dqmDirectory_store");
+  //std::cout << " dqmDirectory_store = " << dqmDirectory_store_ << std::endl;
+}
+
+VertexHistManager::~VertexHistManager()
+{
+//--- nothing to be done yet...
+}
+
+void VertexHistManager::bookHistograms(const edm::EventSetup& setup)
+{
+  //std::cout << "<VertexHistManager::bookHistograms>:" << std::endl;
+
+  if ( edm::Service<DQMStore>().isAvailable() ) {
+    DQMStore& dqmStore = (*edm::Service<DQMStore>());
+
+    dqmStore.setCurrentFolder(dqmDirectory_store_);
+
+    hVertexX_ = dqmStore.book1D("VertexX", "VertexX", 200, -1., +1.);
+    hVertexSigmaX_ = dqmStore.book1D("VertexSigmaX", "VertexSigmaX", 200, -0.01, +0.01);
+    hVertexY_ = dqmStore.book1D("VertexY", "VertexY", 200, -1., +1.);
+    hVertexSigmaY_ = dqmStore.book1D("VertexSigmaY", "VertexSigmaY", 200, -0.01, +0.01);
+    hVertexXvsY_ = dqmStore.book2D("VertexXvsY", "VertexXvsY", 200, -1., +1., 200, -1, +1.);
+    hVertexZ_ = dqmStore.book1D("VertexZ", "VertexZ", 100, -50., 50.);
+    hVertexSigmaZ_ = dqmStore.book1D("VertexSigmaZ", "VertexSigmaZ", 200, -0.10, 0.10);
+
+    hVertexNumTracks_ = dqmStore.book1D("VertexNumTracks", "VertexNumTracks", 101, -0.5, 100.5);
+
+    hVertexChi2Prob_ = dqmStore.book1D("VertexChi2Prob", "VertexChi2Prob", 102, -0.01, 1.01);
+  }
+}
+
+void VertexHistManager::fillHistograms(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+
+{  
+  //std::cout << "<VertexHistManager::fillHistograms>:" << std::endl; 
+
+  edm::Handle<std::vector<reco::Vertex> > recoVertices;
+  iEvent.getByLabel(vertexSrc_, recoVertices);
+  if ( recoVertices->size() >= 1 ) {
+    const reco::Vertex& thePrimaryEventVertex = (*recoVertices->begin());
+
+    hVertexX_->Fill(thePrimaryEventVertex.x());
+    hVertexSigmaX_->Fill(thePrimaryEventVertex.xError());
+    hVertexY_->Fill(thePrimaryEventVertex.y());
+    hVertexSigmaY_->Fill(thePrimaryEventVertex.yError());
+    hVertexXvsY_->Fill(thePrimaryEventVertex.x(), thePrimaryEventVertex.y());
+    hVertexZ_->Fill(thePrimaryEventVertex.z());
+    hVertexSigmaZ_->Fill(thePrimaryEventVertex.zError());
+
+    hVertexNumTracks_->Fill(thePrimaryEventVertex.tracksSize());
+
+    hVertexChi2Prob_->Fill(TMath::Prob(thePrimaryEventVertex.chi2(), TMath::Nint(thePrimaryEventVertex.ndof())));
+  }
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_EDM_PLUGIN(HistManagerPluginFactory, VertexHistManager, "VertexHistManager");
+
+#include "TauAnalysis/Core/interface/HistManagerAdapter.h"
+
+typedef HistManagerAdapter<VertexHistManager> VertexAnalyzer;
+
+DEFINE_ANOTHER_FWK_MODULE(VertexAnalyzer);
---
+++ TauAnalysis/Core/plugins/VertexHistManager.h
@@ -0,0 +1,49 @@
+#ifndef TauAnalysis_Core_VertexHistManager_h  
+#define TauAnalysis_Core_VertexHistManager_h
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "TauAnalysis/Core/interface/HistManagerBase.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+
+#include <vector>
+#include <string>
+
+class VertexHistManager : public HistManagerBase 
+{
+ public:
+  
+  explicit VertexHistManager(const edm::ParameterSet&);
+  ~VertexHistManager();
+  
+  void bookHistograms(const edm::EventSetup&);
+  void fillHistograms(const edm::Event&, const edm::EventSetup&);
+
+ private:
+
+//--- configuration parameters
+  edm::InputTag vertexSrc_;
+
+  std::string dqmDirectory_store_;
+
+//--- histograms
+  MonitorElement* hVertexX_;
+  MonitorElement* hVertexSigmaX_;
+  MonitorElement* hVertexY_;
+  MonitorElement* hVertexSigmaY_;
+  MonitorElement* hVertexXvsY_;
+  MonitorElement* hVertexZ_;
+  MonitorElement* hVertexSigmaZ_;
+
+  MonitorElement* hVertexNumTracks_;
+
+  MonitorElement* hVertexChi2Prob_;
+};
+
+#endif  
+
+
