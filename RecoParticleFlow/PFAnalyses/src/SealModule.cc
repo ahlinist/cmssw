a4068c777df4affa2798aee2a68e12fd257c1c2f
ballin <>
2009-02-11 14:36:29 +0000

    Removing obsolete files

--- RecoParticleFlow/PFAnalyses/src/SealModule.cc
+++ RecoParticleFlow/PFAnalyses/src/SealModule.cc
@@ -1,6 +1,3 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoParticleFlow/PFAnalyses/interface/AnalElId.h"
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(AnalElId);
 
