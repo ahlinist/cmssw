90488c5d7f18f141a3cb35ddb96225a2eff5c0e6
veelken <>
2009-02-13 09:54:45 +0000

    list of L1 trigger bits and HLT decisions empty per default; filled with electron, muons and tau-jet triggers in analysis configs of different channels

--- TauAnalysis/Core/python/triggerHistManager_cfi.py
+++ TauAnalysis/Core/python/triggerHistManager_cfi.py
@@ -6,13 +6,10 @@ triggerHistManager = cms.PSet(
 
   l1GtReadoutRecordSource = cms.InputTag('hltGtDigis::HLT'),
   l1GtObjectMapRecordSource = cms.InputTag('hltL1GtObjectMap::HLT'),
-  l1Bits = cms.vstring('L1_SingleEG5', 'L1_SingleEG8', 'L1_SingleEG10', 'L1_SingleEG12', 'L1_SingleEG15',
-                       'L1_SingleIsoEG5', 'L1_SingleIsoEG8', 'L1_SingleIsoEG10', 'L1_SingleIsoEG12', 'L1_SingleIsoEG15',
-                       'L1_SingleMu3', 'L1_SingleMu5', 'L1_SingleMu7', 'L1_SingleMu10', 'L1_SingleMu14'),
+  l1Bits = cms.vstring(''),
 
   hltResultsSource = cms.InputTag('TriggerResults::HLT'),
-  hltPaths = cms.vstring('HLT_IsoEle15_L1I',
-                         'HLT_Mu15', 'HLT_IsoMu11'),
+  hltPaths = cms.vstring(''),
 
   dqmDirectory_store = cms.string('TriggerQuantities')
 )
