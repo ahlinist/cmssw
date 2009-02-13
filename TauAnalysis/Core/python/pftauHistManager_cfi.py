3fb706927ded231496bdb15d9c35d73798b65dbb
veelken <>
2009-02-13 09:57:58 +0000

    added configuration parameter that allows to fill histograms for 1./2. tau only (needed for pure hadronic tau-jet + tau-jet channel) default = histograms are filled for all taus

--- TauAnalysis/Core/python/pftauHistManager_cfi.py
+++ TauAnalysis/Core/python/pftauHistManager_cfi.py
@@ -7,6 +7,8 @@ tauHistManager = cms.PSet(
   tauSource = cms.InputTag('allLayer1PFTausSelForTauAnalyses'),
   vertexSource = cms.InputTag('selectedPrimaryVertexPosition'),
 
+  tauIndicesToPlot = cms.string('all'),
+
   dqmDirectory_store = cms.string('TauQuantities'),
 
   #requireGenTauMatch = cms.bool(True)
