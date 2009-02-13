774b2f5ae7abd580428e31e696e4de8525de3568
trommers <>
2009-02-13 11:09:07 +0000

    commented out all selector stuff

--- SusyAnalysis/AnalysisSkeleton/test/sample_dijet_cross_cleaning_cfg.py
+++ SusyAnalysis/AnalysisSkeleton/test/sample_dijet_cross_cleaning_cfg.py
@@ -107,81 +107,7 @@ process.dijet = cms.EDFilter("SusyDiJetAnalysis",
 
           
 
-     selections = cms.PSet(
-        selectionSequence = cms.vstring('Preselection', 
-            'FinalJet', 
-            'FinalDirectLeptonVeto', 
-            'FinalMaxNumJetsSelector', 
-            'DPhi', 
-            'Alpha', 
-            'Hemisphere'),
-        selectors = cms.PSet(
-            FinalMaxNumJetsSelector = cms.PSet(
-                maxEt = cms.double(30.0),
-              #   jetTag = cms.InputTag("selectedLayer1Jets"),
-                jetTag = cms.InputTag("patcrosscleaner:ccJets"),
-                maxNumJets = cms.uint32(100),
-                selector = cms.string('MaxNumJetsEventSelector')
-            ),
-            DPhi = cms.PSet(
-                maxDPhi = cms.double(3.15),
-               #  jetTag = cms.InputTag("selectedLayer1Jets"),
-               jetTag = cms.InputTag("patcrosscleaner:ccJets"),
-                selector = cms.string('DPhiEventSelector')
-            ),
-            Hemisphere = cms.PSet(
-                dPhiHemispheresMin = cms.double(0.0),
-                dPhiHemispheresMax = cms.double(3.2),
-                hemisphereTag = cms.InputTag("selectedLayer2Hemispheres"),
-                selector = cms.string('HemisphereSelector')
-            ),
-            FinalDirectLeptonVeto = cms.PSet(
-                #electronTag = cms.InputTag("selectedLayer1Electrons"),
-                electronTag = cms.InputTag("patcrosscleaner:ccElectrons"),
-                tauTag = cms.InputTag("selectedLayer1Taus"),
-                minMuonEt = cms.double(30000.0),
-                tauIsolation = cms.double(0.5),
-                selector = cms.string('DirectLeptonVetoSelector'),
-               muonTag = cms.InputTag("patcrosscleaner:ccMuons"),
-               # muonTag = cms.InputTag("selectedLayer1Muons"),
-                minTauEt = cms.double(30000.0),
-                minElectronEt = cms.double(30000.0),
-                muonIsolation = cms.double(0.5),
-                electronIsolation = cms.double(0.5)
-            ),
-            FinalJet = cms.PSet(
-                maxEMFraction = cms.vdouble(999.0, 999.0),
-                maxEta = cms.vdouble(10.0, 10.0),
-                correction = cms.string('HAD'),
-                flavour = cms.string('GLU'),
-                selector = cms.string('JetEventSelector'),
-                # jetTag = cms.InputTag("selectedLayer1Jets"),
-                jetTag = cms.InputTag("patcrosscleaner:ccJets"),
-                minEt = cms.vdouble(50.0, 50.0)
-            ),
-            PreJet = cms.PSet(
-                maxEMFraction = cms.vdouble(999.0, 999.0),
-                maxEta = cms.vdouble(10.0, 10.0),
-                correction = cms.string('HAD'),
-                 flavour = cms.string('GLU'),
-                selector = cms.string('JetEventSelector'),
-                # jetTag = cms.InputTag("selectedLayer1Jets"),
-                jetTag = cms.InputTag("patcrosscleaner:ccJets"),
-                minEt = cms.vdouble(20.0, 20.0)
-            ),
-            Preselection = cms.PSet(
-                components = cms.vstring('PreJet'),
-                #components = cms.vstring(),
-                selector = cms.string('EventSelectorAND')
-            ),
-            Alpha = cms.PSet(
-                minAlpha = cms.double(0.0),
-                jetTag = cms.InputTag("patcrosscleaner:ccJets"),
-               # jetTag = cms.InputTag("selectedLayer1Jets"),
-                selector = cms.string('AlphaSelector')
-            )
-        )
-    ),
+     
                              
                   
 
@@ -190,7 +116,7 @@ process.dijet = cms.EDFilter("SusyDiJetAnalysis",
     eventWeight = cms.double(19.67249),
   
     triggerResults = cms.InputTag("TriggerResults","","HLT"),
-    plotSelection = cms.vstring('Preselection'),
+    plotSelection = cms.vstring(''),
                              
          
     
