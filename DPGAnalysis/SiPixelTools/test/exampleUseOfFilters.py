714265fa85a35cf7074235bfc2832d29707848e5
fblekman <>
2009-03-03 19:34:56 +0000

    renamed file and showing how to use other filters.

---
+++ DPGAnalysis/SiPixelTools/test/exampleUseOfFilters.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.threshold = 'Info'
+process.MessageLogger.categories.extend(["MuonTOFFilter_trackQuality"])
+process.MessageLogger.categories.extend(["FEDInRunFilter"])
+
+
+
+# DQM services
+process.load("DQMServices.Core.DQM_cfg")
+
+# Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+#fake conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+
+process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRAFT_ALL_V11::All"
+
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+# reconstruction sequence for Cosmics
+process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
+
+process.load("DPGAnalysis.SiPixelTools.muonTOF_cfi")
+process.load("DPGAnalysis.SiPixelTools.FEDInRunFilter_cfi")
+process.MuonTOFFilter_trackQuality.max_goodmuons = 2
+
+process.source = cms.Source("PoolSource",
+
+    fileNames = cms.untracked.vstring(
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/B8FB3273-5DFF-DD11-BEAB-00304875A7B5.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/C0E4F880-5CFF-DD11-B561-0030487624FD.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/EAA9AE47-5FFF-DD11-A966-001A92810ADE.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0006/20993712-7C00-DE11-8BAA-003048678B5E.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0006/26B79166-7C00-DE11-AA81-0030486790A0.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0006/32C72A0C-7C00-DE11-B984-001731A288A1.root',
+       '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0006/36BDFBA4-7C00-DE11-A74E-001A92971BB8.root'
+
+        )  )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1) )
+
+process.p = cms.Path(process.fedInRunFilter*process.MuonTOFFilter_trackQuality)
+
+
+        
