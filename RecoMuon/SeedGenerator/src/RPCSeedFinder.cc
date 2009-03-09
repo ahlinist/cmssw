7d68abf74f1b3c2f45d4942d6c91bf5908c4cce9
hyteng <>
2009-03-09 15:31:37 +0000

    scheme change, wrapped into other classes

--- RecoMuon/SeedGenerator/src/RPCSeedFinder.cc
+++ RecoMuon/SeedGenerator/src/RPCSeedFinder.cc
@@ -5,156 +5,83 @@
 
 
 #include "RecoMuon/SeedGenerator/src/RPCSeedFinder.h"
-#include "RecoMuon/SeedGenerator/src/RPCSeedHits.h"
-#include "RecoMuon/TrackingTools/interface/MuonPatternRecoDumper.h"
-
-#include "Geometry/CommonDetUnit/interface/GeomDetType.h"
-#include "DataFormats/GeometryVector/interface/Pi.h"
-#include "Geometry/CommonDetUnit/interface/GeomDet.h"
-#include "DataFormats/GeometryVector/interface/CoordinateSets.h"
-#include "DataFormats/GeometrySurface/interface/BoundPlane.h"
-#include "DataFormats/GeometrySurface/interface/RectangularPlaneBounds.h"
-
-#include "DataFormats/Common/interface/OwnVector.h"
-#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
-#include "DataFormats/TrajectoryState/interface/PTrajectoryStateOnDet.h"
-
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateTransform.h"
-
-#include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
-
-#include "MagneticField/Engine/interface/MagneticField.h"
-#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-
 #include <iomanip>
 
 using namespace std;
+using namespace edm;
 
-typedef MuonTransientTrackingRecHit::MuonRecHitPointer MuonRecHitPointer;
-typedef MuonTransientTrackingRecHit::ConstMuonRecHitPointer ConstMuonRecHitPointer;
-typedef MuonTransientTrackingRecHit::MuonRecHitContainer MuonRecHitContainer;
 
-// Comparator function must be global function?
-// Could not in include in .h file, or there will be 2 lessPhi() functions in both RPCSeedGenerator and RPCSeedFinder module
-bool lessPhi(const MuonTransientTrackingRecHit::MuonRecHitPointer& it1, const MuonTransientTrackingRecHit::MuonRecHitPointer& it2) {
-    
-    return (it1->globalPosition().phi() < it2->globalPosition().phi());
+RPCSeedFinder::RPCSeedFinder() {
+
+    // Initiate the member
+    isrecHitsset = false;
+    isConfigured = false;
+    isOutputset = false;
+    isEventSetupset = false;
+    oneSeed.clear();
 }
 
+RPCSeedFinder::~RPCSeedFinder() {
 
-RPCSeedFinder::RPCSeedFinder(){}
+}
 
-//vector<TrajectorySeed> RPCSeedFinder::seeds(const edm::EventSetup& eSetup, double MaxRSD) const {
-void RPCSeedFinder::seeds(const edm::EventSetup& eSetup, double MaxRSD, double deltaRThreshold, std::vector<TrajectorySeed>& goodSeeds, std::vector<TrajectorySeed>& candidateSeeds, unsigned int AlgorithmType) const {
-    
-    cout << "[RPCSeedFinder] --> seeds class called" << endl;
-    
-    //vector<TrajectorySeed> goodSeeds(0);
-    //vector<TrajectorySeed> candidateSeeds(0);
-    TrajectorySeed theSeed;
-    RPCSeedHits oneSeed;
-    
-    int NumberofHitsinSeed = 0;
-    for (MuonRecHitContainer::const_iterator iter = theRecHits.begin(); iter!= theRecHits.end(); iter++)
-    {
+void RPCSeedFinder::configure(const edm::ParameterSet& iConfig) {
+
+    oneSeed.configure(iConfig);
+    isConfigured = true;
+}
+
+void RPCSeedFinder::setOutput(std::vector<TrajectorySeed> *goodRef, std::vector<TrajectorySeed> *candidateRef) {
+
+    goodSeedsRef = goodRef;
+    candidateSeedsRef = candidateRef;
+    isOutputset = true;
+}
+
+void RPCSeedFinder::setrecHits(ConstMuonRecHitContainer &recHits) {
+
+    oneSeed.clear();
+    for (ConstMuonRecHitContainer::const_iterator iter = recHits.begin(); iter!= recHits.end(); iter++)
         oneSeed.add(*iter);
-        NumberofHitsinSeed++;
-    }
-    int isGoodSeed = 0;
-    if(NumberofHitsinSeed >= 3)
-    {
-        theSeed = oneSeed.seed(eSetup, MaxRSD, deltaRThreshold, isGoodSeed, AlgorithmType);
-        // Push back the good seed
-        if(isGoodSeed == 1)
-        {
-            cout << "[RPCSeedFinder] --> Seeds from " << NumberofHitsinSeed << " recHits." << endl;
-            goodSeeds.push_back(theSeed);
-        }
-        // Push back cadidate seed but not the fake seed
-        if(isGoodSeed >= 0)
-        {
-            candidateSeeds.push_back(theSeed);
-        }
-    }
-    //return theSeeds;
+    isrecHitsset = true;
 }
 
-double RPCSeedFinder::GetdeltaPhifromHits() {
-    
-    MuonTransientTrackingRecHit::MuonRecHitContainer sortRecHits = theRecHits;
-    sort(sortRecHits.begin(), sortRecHits.end(), lessPhi);
-    cout << "Sorted recHit's Phi: ";
-    for(MuonRecHitContainer::const_iterator iter = sortRecHits.begin(); iter != sortRecHits.end(); iter++)
-        cout << (*iter)->globalPosition().phi() << ", ";
-    cout << endl;
-    // Calculate the deltaPhi, take care Geom::Phi always in range [-pi,pi)
-    // In case of some deltaPhi larger then pi, use value() in Geom::Phi to get the true value in radians of Phi, then do the calculation
-    double deltaPhi = 0;
-    if(sortRecHits.size() <= 1)
-        return deltaPhi;
-    if(sortRecHits.size() == 2)
-    {
-        MuonRecHitContainer::const_iterator iter1 = sortRecHits.begin();
-        MuonRecHitContainer::const_iterator iter2 = sortRecHits.begin();
-        iter2++;
-        deltaPhi = (((*iter2)->globalPosition().phi().value() - (*iter1)->globalPosition().phi().value()) > M_PI) ? (2 * M_PI - ((*iter2)->globalPosition().phi().value() - (*iter1)->globalPosition().phi().value())) : ((*iter2)->globalPosition().phi().value() - (*iter1)->globalPosition().phi().value());
-        return deltaPhi;
-    }
-    else
-    {
-        deltaPhi = 2 * M_PI;
-        int n = 0;
-        for(MuonRecHitContainer::const_iterator iter = sortRecHits.begin(); iter != sortRecHits.end(); iter++)
-        {   
-            cout << "Before this loop deltaPhi is " << deltaPhi << endl;
-            n++;
-            double deltaPhi_more = 0;
-            double deltaPhi_less = 0;
-            if(iter == sortRecHits.begin())
-            {
-                cout << "Calculateing frist loop..." << endl;
-                MuonRecHitContainer::const_iterator iter_more = ++iter;
-                --iter;
-                MuonRecHitContainer::const_iterator iter_less = sortRecHits.end();
-                --iter_less;
-                cout << "more_Phi: " << (*iter_more)->globalPosition().phi() << ", less_Phi: " << (*iter_less)->globalPosition().phi() << ", iter_Phi: " << (*iter)->globalPosition().phi() << endl;
-                deltaPhi_more = (2 * M_PI) - ((*iter_more)->globalPosition().phi().value() - (*iter)->globalPosition().phi().value());
-                deltaPhi_less = (*iter_less)->globalPosition().phi().value() - (*iter)->globalPosition().phi().value();
-            }
-            else if(iter == (--sortRecHits.end()))
-            {
-                cout << "Calculateing last loop..." << endl;
-                MuonRecHitContainer::const_iterator iter_less = --iter;
-                ++iter;
-                MuonRecHitContainer::const_iterator iter_more = sortRecHits.begin();
-                cout << "more_Phi: " << (*iter_more)->globalPosition().phi() << ", less_Phi: " << (*iter_less)->globalPosition().phi() << ", iter_Phi: " << (*iter)->globalPosition().phi() << endl;
-                deltaPhi_less = (2 * M_PI) - ((*iter)->globalPosition().phi().value() - (*iter_less)->globalPosition().phi().value());
-                deltaPhi_more = (*iter)->globalPosition().phi().value() - (*iter_more)->globalPosition().phi().value();
-            }
-            else
-            {
-                cout << "Calculateing " << n <<"st loop..." << endl;
-                MuonRecHitContainer::const_iterator iter_less = --iter;
-                ++iter;
-                MuonRecHitContainer::const_iterator iter_more = ++iter;
-                --iter;
-                cout << "more_Phi: " << (*iter_more)->globalPosition().phi() << ", less_Phi: " << (*iter_less)->globalPosition().phi() << ", iter_Phi: " << (*iter)->globalPosition().phi() << endl;
-                deltaPhi_less = (2 * M_PI) - ((*iter)->globalPosition().phi().value() - (*iter_less)->globalPosition().phi().value());
-                deltaPhi_more = (2 * M_PI) - ((*iter_more)->globalPosition().phi().value() - (*iter)->globalPosition().phi().value());
-            }
-            if(deltaPhi > deltaPhi_more)
-                deltaPhi = deltaPhi_more;
-            if(deltaPhi > deltaPhi_less)
-                deltaPhi = deltaPhi_less;
-            
-            cout << "For this loop deltaPhi_more is " << deltaPhi_more << endl;
-            cout << "For this loop deltaPhi_less is " << deltaPhi_less << endl;
-            cout << "For this loop deltaPhi is " << deltaPhi << endl;
-        }
-        return deltaPhi;
-    }
+void RPCSeedFinder::setEventSetup(const edm::EventSetup& iSetup) {
+
+    eSetup = &iSetup;
+    isEventSetupset = true;
 }
+
+void RPCSeedFinder::seed() {
+
+    cout << "[RPCSeedFinder] --> seeds called" << endl;
+
+    if(isrecHitsset == false || isOutputset == false || isConfigured == false || isEventSetupset == false)
+    {
+        cout << "Configuration or IO is not set yet" << endl;
+        return;
+    }
+    
+    TrajectorySeed theSeed;
+    int isGoodSeed = 0;
+    const edm::EventSetup &iSetup = *eSetup;
+    theSeed = oneSeed.seed(iSetup, isGoodSeed);
+    // Push back the good seed
+    if(isGoodSeed == 1)
+    {
+        cout << "[RPCSeedFinder] --> Seeds from " << oneSeed.nrhit() << " recHits." << endl;
+        goodSeedsRef->push_back(theSeed);
+    }
+    // Push back cadidate seed but not the fake seed
+    if(isGoodSeed >= 0)
+    {
+        candidateSeedsRef->push_back(theSeed);
+    }
+
+    // Unset the signal
+    oneSeed.clear();
+    isrecHitsset = false;
+}
+
+
 
--- RecoMuon/SeedGenerator/src/RPCSeedFinder.h
+++ RecoMuon/SeedGenerator/src/RPCSeedFinder.h
@@ -1,44 +1,51 @@
-#ifndef RecoMuon_MuonSeedGenerator_RPCSeedFinder_H
-#define RecoMuon_MuonSeedGenerator_RPCSeedFinder_H
+#ifndef RecoMuon_SeedGenerator_RPCSeedFinder_H
+#define RecoMuon_SeedGenerator_RPCSeedFinder_H
+
 
 /** \class RPCSeedFinder
  *  
- *
+ *   \author Haiyun.Teng - Peking University
  *
  *  
  */
 
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 
-#include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
-#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
-
-#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
-
+#include "RecoMuon/SeedGenerator/src/RPCSeedPattern.h"
+#include <DataFormats/TrajectorySeed/interface/TrajectorySeed.h>
+#include <RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h>
+#include <FWCore/ParameterSet/interface/ParameterSet.h>
+#include <FWCore/Framework/interface/EventSetup.h>
 #include <vector>
 #include <algorithm>
 
 namespace edm {class EventSetup;}
 
-class RPCSeedFinder {
-public:
-  
-  RPCSeedFinder();
+class RPCSeedFinder 
+{
+    typedef MuonTransientTrackingRecHit::MuonRecHitPointer MuonRecHitPointer;
+    typedef MuonTransientTrackingRecHit::ConstMuonRecHitPointer ConstMuonRecHitPointer;
+    typedef MuonTransientTrackingRecHit::MuonRecHitContainer MuonRecHitContainer;
+    typedef MuonTransientTrackingRecHit::ConstMuonRecHitContainer ConstMuonRecHitContainer;
 
-  virtual ~RPCSeedFinder(){};
+    public:
+        RPCSeedFinder();
+        ~RPCSeedFinder();
+        void configure(const edm::ParameterSet& iConfig);
+        void setOutput(std::vector<TrajectorySeed> *goodRef, std::vector<TrajectorySeed> *candidateRef);
+        void setrecHits(ConstMuonRecHitContainer &recHits);
+        void setEventSetup(const edm::EventSetup& iSetup);
+        void seed();
 
-  void add(MuonTransientTrackingRecHit::MuonRecHitPointer hit) { theRecHits.push_back(hit); }
-  void pop() { theRecHits.pop_back(); }
-  
-  //std::vector<TrajectorySeed> seeds(const edm::EventSetup& eSetup, double MaxRSD) const;
-  void seeds(const edm::EventSetup& eSetup, double MaxRSD, double deltaRThreshold, std::vector<TrajectorySeed>& goodSeeds, std::vector<TrajectorySeed>& candidateSeeds, unsigned int AlgorithmType) const;
-  
-  MuonTransientTrackingRecHit::ConstMuonRecHitPointer firstRecHit() const { return theRecHits.front(); }
-  unsigned int nrhit() const { return theRecHits.size(); }
-  double GetdeltaPhifromHits();
-private:
-    
-  MuonTransientTrackingRecHit::MuonRecHitContainer theRecHits;
- 
+    private:
+        // Signal for call fillLayers()
+        bool isrecHitsset;
+        bool isConfigured;
+        bool isOutputset;
+        bool isEventSetupset;
+        const edm::EventSetup *eSetup;
+        RPCSeedPattern oneSeed;
+        //ConstMuonRecHitContainer theRecHits;
+        std::vector<TrajectorySeed> *goodSeedsRef;
+        std::vector<TrajectorySeed> *candidateSeedsRef;
 };
 #endif
