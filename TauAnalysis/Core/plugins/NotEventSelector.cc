0b201522f36245b7de34d5b45fd9949d82aaf824
veelken <>
2009-03-20 17:05:18 +0000

    added and, or and not selectors for combining cuts

---
+++ TauAnalysis/Core/plugins/NotEventSelector.cc
@@ -0,0 +1,17 @@
+#include "TauAnalysis/Core/plugins/NotEventSelector.h"
+
+NotEventSelector::NotEventSelector(const edm::ParameterSet& cfg)
+{
+  edm::ParameterSet cfgSelector = cfg.getParameter<edm::ParameterSet>("selector");
+  std::string selectorType = cfg.getParameter<std::string>("type");
+  selector_ = EventSelectorPluginFactory::get()->create(selectorType, cfg);
+}
+
+NotEventSelector::~NotEventSelector()
+{    
+  delete selector_;
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, NotEventSelector, "NotEventSelector");
---
+++ TauAnalysis/Core/plugins/OrEventSelector.h
@@ -0,0 +1,43 @@
+#ifndef TauAnalysis_Core_OrEventSelector_h
+#define TauAnalysis_Core_OrEventSelector_h
+
+ /** \class OrEventSelector
+  *
+  * Selects an event if it passes at least one of the associated event selectors
+  * 
+  * \author Chriastian Veelken, UC Davis
+  *
+  * \version $Revision: 1.1 $
+  *
+  * $Id: OrEventSelector.h,v 1.1 2009/03/03 13:07:27 llista Exp $
+  *
+  */
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "PhysicsTools/UtilAlgos/interface/EventSelectorBase.h"
+
+class OrEventSelector : public EventSelectorBase
+{
+ public:
+  /// constructor 
+  explicit OrEventSelector(const edm::ParameterSet&);
+
+  /// destructor
+  virtual ~OrEventSelector();
+  
+  bool operator()(edm::Event& evt, const edm::EventSetup& es) {
+    for ( std::vector<EventSelectorBase*>::const_iterator selector = selectors_.begin();
+	  selector != selectors_.end(); ++selector ) {
+      if ( (**selector)(evt, es) ) return true;
+    }
+    return false;
+  }
+
+ protected:
+  /// associated selectors
+  std::vector<EventSelectorBase*> selectors_;
+};
+ 
+#endif
