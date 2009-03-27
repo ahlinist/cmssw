ea485cf2656222cb409889521ba7f02225bc7740
dlomidze <>
2009-03-27 23:16:14 +0000

    update RPC render Plugin

--- DQM/RenderPlugins/src/RPCRenderPlugin.cc
+++ DQM/RenderPlugins/src/RPCRenderPlugin.cc
@@ -7,6 +7,9 @@
 #include "TLine.h"
 #include "TText.h"
 //#include "TAxis.h"
+#include "TPaletteAxis.h"
+#include "TH2F.h"
+#include "TList.h"
 
 #include "DQM/RenderPlugins/src/RPCRenderPlugin.h"
 #include "DQM/RenderPlugins/src/utils.h"
@@ -91,6 +94,10 @@ void RPCRenderPlugin::preDrawTH2( TCanvas *c, const DQMNet::CoreObject &o ) {
   }
 
 
+ 
+
+
+
   return;
 }
 
@@ -120,9 +127,10 @@ void  RPCRenderPlugin::postDrawTH2(TCanvas *c, const DQMNet::CoreObject &o){
    return;
  }
 
- if(o.name.find("Occupancy") != std::string::npos && 
-    o.name.find("Sector") != std::string::npos &&
-   o.name.find("Wheel") != std::string::npos  ){ //sector occupancy plots
+ if(o.name.find("Occupancy") != std::string::npos)
+  //  && o.name.find("Sector") != std::string::npos &&
+//    o.name.find("Wheel") != std::string::npos  )
+{ //sector occupancy plots
    TLine line;
    line.SetLineWidth(2);// Draw lines to delimitate the end of the roll
    //rb1in
@@ -139,16 +147,38 @@ void  RPCRenderPlugin::postDrawTH2(TCanvas *c, const DQMNet::CoreObject &o){
    line.DrawLine(85, 9.5, 43, 9.5);
    //rb3
    line.DrawLine(43, 9.5, 43, 13.5);
-   return;
+ 
+   gPad->Update();
+   TPaletteAxis *palette;
+   palette = (TPaletteAxis*)obj->GetListOfFunctions()->FindObject("palette");
+   palette->GetAxis()->SetLabelSize(0.024); 
+   gPad->Update();
+   
+
+
+  
  }
 
- if(o.name.find("Occupancy") != std::string::npos){//summary occupancy plots
+
+ 
+
+
+ if(o.name.find("Occupancy") != std::string::npos){          //summary occupancy plots
    obj->GetXaxis()->SetNdivisions(-510);
    obj->GetYaxis()->SetNdivisions(-510);
    obj->GetXaxis()->CenterLabels();
    obj->GetYaxis()->CenterLabels();
    c->SetGridx();
    c->SetGridy(); 
+//    return;
+ }
+
+
+ if(o.name.find("OccupancyNormByGeoAndRPCEvents_Wheel")!= std::string::npos) {
+
+   obj->SetMaximum(0.2);
+   //gStyle->SetPalette(100);
+   return;
  }
  
  if( o.name.find("AfterPulse")!= std::string::npos) {//afterpulse 2D plots
@@ -161,6 +191,184 @@ void  RPCRenderPlugin::postDrawTH2(TCanvas *c, const DQMNet::CoreObject &o){
 	obj->GetYaxis()->SetNdivisions(-510);
 	return;
   }
+ 
+ 
+ if(o.name.find("ClusterSizeIn1Bin_Roll_vs_Sector_Wheel") != std::string::npos) {
+   
+   obj->SetMinimum(0.0);
+   obj->SetMaximum(1.0);
+   
+   int colorPalette[20];
+   for(int i=0; i<15; i++) {
+     colorPalette[i]=416;
+   }
+   
+   colorPalette[15]= 400; // Yallow
+   colorPalette[16]= 807; // Orange
+   colorPalette[17]= 632; // Red
+   colorPalette[18]= 632; 
+   colorPalette[19]= 632; 
+   
+   gStyle->SetPalette(20, colorPalette);
+   
+   
+   return;
+ }
+ 
+
+  if(o.name.find("AsymmetryLeftRight_Roll_vs_Sector_Wheel") != std::string::npos) {
+   
+   obj->SetMinimum(0.0);
+   obj->SetMaximum(1.0);
+   
+   int colorPalette1[10];
+   
+   colorPalette1[0]=416;
+   colorPalette1[1]=416;
+   colorPalette1[2]=416;
+    
+   colorPalette1[3]= 400; // Yallow
+   colorPalette1[4]= 400; // Yallow
+
+   colorPalette1[5]= 807; // Orange
+   colorPalette1[6]= 807; // Orange
+
+   colorPalette1[7]= 632; // Red
+   colorPalette1[8]= 632; 
+   colorPalette1[9]= 632; 
+   
+   gStyle->SetPalette(10, colorPalette1);
+   
+   
+   return;
+ }
+
+
+ if(o.name.find("DeadChannelFraction_Roll_vs_Sector") != std::string::npos) {
+   
+   obj->SetMinimum(0.0);
+   obj->SetMaximum(0.1);
+   
+   int colorPalette2[10];
+   
+   colorPalette2[0]=416;
+   colorPalette2[1]=416;
+   colorPalette2[2]=416;
+    
+   colorPalette2[3]= 400; // Yallow
+   colorPalette2[4]= 400; // Yallow
+   colorPalette2[5]= 400; // Yallow
+
+   colorPalette2[6]= 807; // Orange
+   colorPalette2[7]= 807; // Orange
+
+   colorPalette2[8]= 632; // Red
+   colorPalette2[9]= 632; 
+   
+   gStyle->SetPalette(10, colorPalette2);
+   
+   
+   return;
+ }
+
+if(o.name.find("RPCChamberQuality") != std::string::npos) {
+   
+   obj->SetMinimum(0.5);
+   obj->SetMaximum(7.5);
+   
+   int colorPalette3[7];
+   
+   // colorPalette3[0]=416;
+   colorPalette3[0]=416; // Gren OK
+   colorPalette3[1]=860; // Blue OFF
+   colorPalette3[2]= 400; // Yallow Noisily Strip
+   colorPalette3[3]= 807; // Orange Noisily Chamber
+   colorPalette3[4]= 616;  // Pink Partly Dead
+   colorPalette3[5]= 632; // red Dead
+   colorPalette3[6]= 432;
+   
+   gStyle->SetPalette(7, colorPalette3);
+   
+   //  obj->GetXaxis()->SetNdivisions(-510);
+   //    obj->GetYaxis()->SetNdivisions(-510);
+   //    obj->GetXaxis()->CenterLabels();
+   //    obj->GetYaxis()->CenterLabels();
+   //    c->SetGridx();
+   //    c->SetGridy(); 
+   
+   c->cd();
+   gPad->Update();
+
+   TPaletteAxis *palette;
+   palette = (TPaletteAxis*)obj->GetListOfFunctions()->FindObject("palette");
+   palette->GetAxis()->SetLabelSize(0); 
+   //   palette->GetAxis()->SetTitle("OK        OFF     NoisSt    NoisCh     Dead    ");
+   palette->GetAxis()->SetTitle("OK        OFF        Nois.St       Nois.Ch   Part.Dead       Dead    Bad.Shape");
+   palette->SetTitleOffset(0.3);
+   palette->SetTitleSize(0.025);
+   
+   //    gPad->Update();
+
+
+   obj->GetXaxis()->SetNdivisions(-510);
+   obj->GetYaxis()->SetNdivisions(-510);
+   obj->GetXaxis()->CenterLabels();
+   obj->GetYaxis()->CenterLabels();
+   c->SetGridx();
+   c->SetGridy(); 
+   
+
+   return;
+ }
+
+
+if(o.name.find("VStatus_Wheel") != std::string::npos) {
+   
+   obj->SetMinimum(0.5);
+   obj->SetMaximum(3.5);
+   
+   int colorPalette4[3];
+   
+   colorPalette4[0]=416; // Blue OFF
+   
+   colorPalette4[1]=860; // Green ON
+    
+   colorPalette4[2]= 400; // Yallow Error
+   
+  
+   
+   gStyle->SetPalette(3, colorPalette4);
+   
+   
+   return;
+ }
+
+
+// 
+if(o.name.find("NumberOfDigi_Mean_Roll_vs_Sector") != std::string::npos) {
+   
+  obj->SetMinimum(0.5);
+  obj->SetMaximum(10.5);
+   
+   int colorPalette5[5];
+   
+   colorPalette5[0] = 416; // G
+   colorPalette5[1] = 416; // G
+   
+   colorPalette5[2] = 400; // Y
+   
+   colorPalette5[3] = 807; // O
+    
+   colorPalette5[4] = 632; // R
+   
+  
+   
+   gStyle->SetPalette(5, colorPalette5);
+   
+   
+   return;
+ }
+
 
  return;
 }
