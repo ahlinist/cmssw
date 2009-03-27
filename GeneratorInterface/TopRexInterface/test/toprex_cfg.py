365766df208e7f098190ae038f317c4455b962f2
fabstoec <>
2009-03-27 11:54:08 +0000

    fixed test file

--- GeneratorInterface/TopRexInterface/test/toprex_cfg.py
+++ GeneratorInterface/TopRexInterface/test/toprex_cfg.py
@@ -2,28 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('GEN')
 
-# import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
-process.load('FWCore/MessageService/MessageLogger_cfi')
-process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
-process.load('Configuration/StandardSequences/GeometryIdeal_cff')
-process.load('Configuration/StandardSequences/MagneticField_38T_cff')
-process.load('Configuration/StandardSequences/Generator_cff')
-process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
-process.load('Configuration/StandardSequences/EndOfProcess_cff')
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.load('Configuration/EventContent/EventContent_cff')
 
-process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.110 $'),
-    annotation = cms.untracked.string('TTbar_cfi nevts:100'),
-    name = cms.untracked.string('PyReleaseValidation')
-)
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
-)
-process.options = cms.untracked.PSet(
-    Rethrow = cms.untracked.vstring('ProductNotFound')
 )
 
 # Input source
@@ -31,6 +13,7 @@ process.source = cms.Source("EmptySource")
 
 #generator definition
 process.load("GeneratorInterface.Pythia6Interface.pythiaDefault_cff")
+
 process.generator = cms.EDProducer("ToprexProducer",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -68,26 +51,16 @@ process.generator = cms.EDProducer("ToprexProducer",
     )
 )
 
+
+process.generation_step = cms.Path(process.generator)
+
+
 # Output definition
 process.output = cms.OutputModule("PoolOutputModule",
-    outputCommands = process.RECOSIMEventContent.outputCommands,
     fileName = cms.untracked.string('TTbar_cfi_GEN.root'),
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string(''),
-        filterName = cms.untracked.string('')
-    ),
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('generation_step')
-    )
-)
-
-
-# Other statements
-process.GlobalTag.globaltag = 'IDEAL_30X::All'
+                                  )
 
 # Path and EndPath definitions
-process.generation_step = cms.Path(process.generator * process.pgen)
+
 process.out_step = cms.EndPath(process.output)
 
-# Schedule definition
-process.schedule = cms.Schedule(process.generation_step)
