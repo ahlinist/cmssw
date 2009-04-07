e7cd0c90395e73e687a8399e2bf1e5959f538586
cerati <>
2009-04-07 12:59:36 +0000

    first commit of AHemu config files

---
+++ TauAnalysis/Configuration/python/analyzeAHtoElecMu_cff.py
@@ -0,0 +1,87 @@
+import FWCore.ParameterSet.Config as cms
+
+# import config for event selection, event print-out and analysis sequence
+from TauAnalysis.Configuration.analyzeAHtoElecMu_cfi import *
+
+analyzeAHtoElecMu = cms.EDAnalyzer("GenericAnalyzer",
+
+  name = cms.string('ahElecMuAnalyzer'),
+
+  eventSelection = cms.VPSet(
+    # generator level phase-space selection
+    # (NOTE: (1) to be used in case of Monte Carlo samples
+    #            overlapping in simulated phase-space only !!
+    #        (2) genPhaseSpaceCut needs to be **always** the first entry in the list of cuts
+    #           - otherwise the script submitToBatch.csh for submission of cmsRun jobs
+    #            to the CERN batch system will not work !!)
+    genPhaseSpaceCut,
+
+    # generator level selection of Z --> e + mu events
+    # passing basic acceptance and kinematic cuts
+    # (NOTE: to be used for efficiency studies only !!)
+    #genElectronCut,
+    #genMuonCut,
+
+    # trigger selection
+    Trigger,
+    
+    # primary event vertex selection
+    primaryEventVertex,
+    primaryEventVertexQuality,
+    primaryEventVertexPosition,
+
+    # electron candidate selection
+    tightElectronIdMinCut,
+    tightElectronIdMaxCut,
+    electronAntiCrackCut,
+    electronEtaCut, #default 2.1, switch to 2.4
+    electronPtCut, #now 15, too low?
+    electronTrkIsoCut,
+    electronEcalIsoCut,
+    #electron hcal iso?
+    electronTrkCut,
+    ##electronTrkIPcut, #mmm... we used to cut on combined d0 significance!
+
+    # muon candidate selection
+    globalMuonMinCut,
+    globalMuonMaxCut,
+    muonEtaCut, #default 2.1, switch to 2.4
+    muonPtCut, #now 15, too low?
+    muonTrkIsoCut,
+    muonEcalIsoCut,
+    #muonHcalIsoCut, #why not?
+    muonAntiPionCut,
+    ##muonTrkIPcut, #mmm... we used to cut on combined d0 significance!
+
+    # di-tau candidate selection
+    ##diTauCandidateForElecMuAcoplanarityCut, #use it in case of no collinear approximation?
+    diTauCandidateForElecMuZeroChargeCut,
+    diTauCandidateForElecMuDPhiCut,
+    diTauCandidateForElecMuImpParamSigCut,
+
+    # jet candidate selection...
+    jetMinCut, #default eta<2.1 too tight, need 2.4 for b-tagging
+    jetMaxCut,    
+    # b-tagging candidate selection...
+    jetBtagMinCut,
+    jetBtagMaxCut
+  ),
+
+  histManagers = cms.VPSet(
+    electronHistManager,
+    muonHistManager,
+    diTauCandidateHistManagerForElecMu,
+    metHistManager,
+    jetHistManager,
+    vertexHistManager,
+    triggerHistManager,
+    ahemuHistManager
+  ),
+
+  eventDumps = cms.VPSet(
+    elecMuEventDump
+  ),
+
+  analysisSequence = elecMuAnalysisSequence
+)
+
---
+++ TauAnalysis/Configuration/test/runAHtoElecMu_cfg.py
@@ -0,0 +1,97 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('runAHtoElecMu')
+
+# import of standard configurations for RECOnstruction
+# of electrons, muons and tau-jets with non-standard isolation cones
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'IDEAL_V11::All'
+#process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.load("TauAnalysis.Configuration.customSelectionForAHtoElecMu_cff")
+
+process.load("TauAnalysis.Configuration.analyzeAHtoElecMu_cff")
+
+# import configuration parameters for submission of jobs to CERN batch system
+# (running over skimmed samples stored on CASTOR)
+from TauAnalysis.Configuration.sampleDefinitionsAHtoElecMu_cfi import *
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.saveAHtoElecMu = cms.EDAnalyzer("DQMSimpleFileSaver",
+  outputFileName = cms.string('plotsAHtoElecMu.root')
+)
+
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#  ignoreTotal = cms.untracked.int32(1) # default is one
+#)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(2000)    
+#    input = cms.untracked.int32(10)    
+)
+
+#process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.source = cms.Source("PoolSource",
+    #firstEvent = cms.untracked.uint32(4097),
+    #firstRun = cms.untracked.uint32(1),
+    fileNames = cms.untracked.vstring(
+#        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160bb_tautau_1.root',
+#       'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160bb_tautau_2.root',
+#        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160bb_tautau_3.root',
+#        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160bb_tautau_4.root',
+#        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160bb_tautau_5.root'
+        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160_tautau_2l_1.root',
+        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160_tautau_2l_2.root',
+        'rfio:/castor/cern.ch/user/c/cerati/SkimmingElecMu/elecMuSkim_AH160_tautau_2l_3.root'
+    )
+)
+
+#--------------------------------------------------------------------------------
+# define "hooks" for replacing configuration parameters
+# in case running jobs on the CERN batch system:
+#
+#---This_is_a_Hook_for_Replacement_of_fileNames_Parameter
+#
+# to be replaced by e.g.
+#
+#  "process.source.fileNames = fileNamesQCD_BCtoE_Pt20to30"
+#
+#---This_is_a_Hook_for_Replacement_of_maxEvents_Parameter
+#
+# to be replaced by e.g.
+#
+#  "process.maxEvents.input = cms.untracked.int32(100)"
+#
+#---This_is_a_Hook_for_Replacement_of_genPhaseSpaceCut_Parameter
+#
+# to be replaced by e.g.
+#
+#  "extEventSelection = cms.VPSet()
+#   extEventSelection.insert(genPhaseSpaceCutQCD_BCtoE_Pt20to30)
+#   extEventSelection.insert(process.analyzeZtoElecMu.eventSelection)
+#   process.analyzeZtoElecMu.eventSelection = extEventSelection"
+#
+#---This_is_a_Hook_for_Replacement_of_outputFileName_Parameter
+#
+# to be replaced by e.g.
+#  "process.saveZtoElecMu.outputFileName = outputFileNameQCD_BCtoE_Pt20to30"
+#
+#--------------------------------------------------------------------------------
+
+
+process.p = cms.Path( process.producePatLayer1ForTauAnalyses
+                     +process.produceElecMuPairs
+                     +process.analyzeAHtoElecMu
+                     +process.saveAHtoElecMu )
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
+
+
