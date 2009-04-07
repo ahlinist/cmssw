03ddf8ef1605975608ecd26d3bd61d8046812696
chinhan <>
2009-04-07 13:17:15 +0000

    Remove recursive call in a fill() method.

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
@@ -3,7 +3,7 @@ import copy
 process = cms.Process("TTEffAnalysis")
 
 process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(1000)
+        input = cms.untracked.int32(-1)
 )
 
 process.load("FWCore/MessageService/MessageLogger_cfi")
@@ -33,7 +33,8 @@ process.source = cms.Source("PoolSource",
 
 process.load("L1Trigger/Configuration/L1Config_cff")
 process.load("Configuration/StandardSequences/L1TriggerDefaultMenu_cff")
-#process.load("L1TriggerConfig/L1GtConfigProducers/Luminosity/lumi1030/L1Menu_2008MC_2E30_Unprescaled_cff")
+#process.load("L1TriggerConfig/L1GtConfigProducers/Luminosity/lumi1031/L1Menu_2008MC_1E31_Unprescaled_cff")
+#process.load("L1TriggerConfig/L1GtConfigProducers/Luminosity/lumi1031/L1Menu_MC2009_v1_L1T_Scales_20080922_Imp0_Unprescaled_cff")
 process.load("HLTrigger/HLTfilters/hltLevel1GTSeed_cfi")
 process.tteffL1GTSeed = copy.deepcopy(process.hltLevel1GTSeed)
 process.tteffL1GTSeed.L1TechTriggerSeeding = cms.bool(False)
@@ -47,19 +48,19 @@ process.tteffL1GTSeed.L1MuonCollectionTag = cms.InputTag("hltL1extraParticles")
 
 
 process.TTEffAnalysis = cms.EDAnalyzer("TTEffAnalyzer",
-        #PFTauCollection         = cms.InputTag("IdentifiedTaus"),
+        #PFTauCollection        = cms.InputTag("IdentifiedTaus"),
         PFTauCollection         = cms.InputTag("PFTausSelected"),
-        PFTauIsoCollection         = cms.InputTag("thisPFTauDiscriminationByIsolationHighEfficiency"),
-# Check that Isolation collection below actually matched up with Tau Collection above
+        PFTauIsoCollection      = cms.InputTag("thisPFTauDiscriminationByIsolationHighEfficiency"),
+        # Check that Isolation collection below actually matched up with Tau Collection above
         #PFTauCollection         = cms.InputTag("pfRecoTauProducerHighEfficiency"),
-        #PFTauIsoCollection         = cms.InputTag("pfRecoTauDiscriminationByIsolationHighEfficiency"),
+        #PFTauIsoCollection      = cms.InputTag("pfRecoTauDiscriminationByIsolationHighEfficiency"),
 
 	L1extraTauJetSource	= cms.InputTag("hltL1extraParticles", "Tau", "HLT2"),
 	L1extraCentralJetSource	= cms.InputTag("hltL1extraParticles", "Central", "HLT2"),
         L1bitInfoSource         = cms.InputTag("l1CaloSim", "L1BitInfos"),
         L1GtReadoutRecord       = cms.InputTag("hltGtDigis"),
         L1GtObjectMapRecord     = cms.InputTag("hltL1GtObjectMap"),
-        HltResults              = cms.InputTag("TriggerResults::HLT"),
+        HltResults              = cms.InputTag("TriggerResults::HLT2"),
         L1TauTriggerSource      = cms.InputTag("tteffL1GTSeed"),
 	L1JetMatchingCone	= cms.double(0.5),
         L2AssociationCollection = cms.InputTag("hltL2TauNarrowConeIsolationProducer"),
@@ -71,14 +72,14 @@ process.TTEffAnalysis = cms.EDAnalyzer("TTEffAnalyzer",
         HLTPFTau                = cms.bool(False),
         MCTauCollection         = cms.InputTag("TauMCProducer:HadronicTauOneAndThreeProng"),
 
-        outputFileName          = cms.string("/tmp/chinhan/qcd_tteffAnalysis.root")
-#        outputFileName          = cms.string("/tmp/chinhan/ztt_tteffAnalysis.root")
+        outputFileName          = cms.string("qcd_tteffAnalysis.root")
+#        outputFileName          = cms.string("ztt_tteffAnalysis.root")
 )
 
 process.runEDAna = cms.Path(
 #    process.PFTausSelected*
-    process.tteffL1GTSeed
-    *process.TTEffAnalysis
+    process.tteffL1GTSeed*
+    process.TTEffAnalysis
 ) 
 
 #process.o1 = cms.OutputModule("PoolOutputModule",
