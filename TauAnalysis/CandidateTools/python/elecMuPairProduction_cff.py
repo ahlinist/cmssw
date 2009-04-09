b47675922621b439510ef3a92595cc3e0568b06c
veelken <>
2009-04-09 13:19:13 +0000

    - changed inputTag for PAT layer 1 MET object from 'allLayer1METs' to 'layer1METs'  (change according to new PAT v2 workflow) - renamed python/elecMuPairProducer_cff.py to python/elecMuPairProduction_cff.py  (consistency with other diTau combinations)

--- TauAnalysis/CandidateTools/python/elecMuPairProduction_cff.py
+++ TauAnalysis/CandidateTools/python/elecMuPairProduction_cff.py
@@ -1,19 +1,20 @@
 import FWCore.ParameterSet.Config as cms
+import copy
 
 #--------------------------------------------------------------------------------
-# produce combinations of electron + tau-jet pairs
+# produce combinations of electron + muons pairs
 #--------------------------------------------------------------------------------
 
-allElecTauPairs = cms.EDProducer("PATElecTauPairProducer",
+allElecMuPairs = cms.EDProducer("PATElecMuPairProducer",
   useLeadingTausOnly = cms.bool(False),
   srcLeg1 = cms.InputTag('selectedLayer1ElectronsTrkIPcumulative'),
-  srcLeg2 = cms.InputTag('selectedLayer1TausForElecTauElectronVetoCumulative'),
-  dRmin12 = cms.double(0.3),
+  srcLeg2 = cms.InputTag('selectedLayer1MuonsTrkIPcumulative'),
+  dRmin12 = cms.double(-1.),
   srcMET = cms.InputTag('layer1METs'),
   recoMode = cms.string(""),
   verbosity = cms.untracked.int32(0)
 )
 
-produceElecTauPairs = cms.Sequence( allElecTauPairs )
+produceElecMuPairs = cms.Sequence( allElecMuPairs )
 
 
