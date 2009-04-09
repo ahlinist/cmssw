e012227d554748d849db91eb711dab4bd2682946
veelken <>
2009-04-09 14:50:46 +0000

    custom configuration for PAT MEt objects (tau corrections applied)

---
+++ TauAnalysis/RecoTools/python/patMetConfig_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.PatAlgos.recoLayer0.jetMETCorrections_cff import *
+from PhysicsTools.PatAlgos.producersLayer1.metProducer_cfi import *
+
+from JetMETCorrections.Type1MET.TauMetCorrections_cff import * 
+
+#--------------------------------------------------------------------------------  
+# PAT layer 0 MET configuration parameters
+#--------------------------------------------------------------------------------
+
+# apply tau-jet specific corrections to reco::MET per default
+# (comment-out the next line in case you prefer to **not** apply the tau-jet specific MET corrections)
+patMETCorrections._seq = patMETCorrections._seq * MetTauCorrections
+
+#--------------------------------------------------------------------------------  
+# PAT layer 1 MET configuration parameters
+#--------------------------------------------------------------------------------
+
+# change input for pat::MET production to reco::MET collection
+# containing the tau-jet specific corrections
+# (comment-out the next line in case you prefer to **not** apply the tau-jet specific MET corrections)
+layer1METs.metSource  = cms.InputTag("tauMetCorr")
