0156eb56c849832faee82c2f1831e16b53800fb1
veelken <>
2009-04-09 13:17:20 +0000

    added sequence for production of e + mu pair combination

--- TauAnalysis/CandidateTools/python/diTauPairProductionAllKinds_cff.py
+++ TauAnalysis/CandidateTools/python/diTauPairProductionAllKinds_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from TauAnalysis.CandidateTools.elecMuPairProduction_cff import *
 from TauAnalysis.CandidateTools.elecTauPairProduction_cff import *
 from TauAnalysis.CandidateTools.muTauPairProduction_cff import *
 from TauAnalysis.CandidateTools.diTauPairProduction_cff import *
