7712be929fcc08915afdfbac32d357bb23cf42f5
veelken <>
2009-04-10 14:47:49 +0000

    define event-content of patTuple produced by TauAnalysis (note, in the new PAT v2 workflow, a custom event-content **needs** to be defined,  as cmsRun throws an exception caused by "electronsNoDuplicates" in case that product is not dropped)

---
+++ TauAnalysis/Configuration/python/patTupleEventContent_cff.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.PatAlgos.patEventContent_cff import *
+
+#--------------------------------------------------------------------------------
+# per default, drop everything that is not specifically kept
+#--------------------------------------------------------------------------------
+
+patTupleEventContent = cms.PSet(
+    outputCommands = cms.untracked.vstring('drop *')
+)
+
+#--------------------------------------------------------------------------------
+# keep PAT layer 1 objects
+#--------------------------------------------------------------------------------
+
+patTupleEventContent.outputCommands.extend(patEventContent)
+patTupleEventContent.outputCommands.extend(patExtraAodEventContent)
+patTupleEventContent.outputCommands.extend(patTriggerEventContent)
+
+#--------------------------------------------------------------------------------
+# keep collections of selected PAT layer 1 particles
+#--------------------------------------------------------------------------------
+
+patTupleEventContent.outputCommands.extend(
+    [ 'keep *_selectedLayer1Electrons*_*_*',
+      'keep *_selectedLayer1Muons*_*_*',
+      'keep *_selectedLayer1Taus*_*_*',
+      'keep *_selectedLayer1Jets*_*_*' ]
+)
+
+#--------------------------------------------------------------------------------
+# keep di-tau objects
+#--------------------------------------------------------------------------------
+
+patTupleEventContent.outputCommands.extend(
+    [ 'keep *_allElecMuPairs*_*_*',
+      'keep *_selectedElecMuPairs*_*_*',
+      'keep *_allElecTauPairs*_*_*',
+      'keep *_selectedElecTauPairs*_*_*',
+      'keep *_allMuTauPairs*_*_*',
+      'keep *_selectedMuTauPairs*_*_*',
+      'keep *_allDiTauPairs*_*_*',
+      'keep *_selectedDiTauPairs*_*_*' ]
+)
+
+#--------------------------------------------------------------------------------
+# keep generator level tau-jets
+#--------------------------------------------------------------------------------
+
+patTupleEventContent.outputCommands.extend(
+    [ 'keep *_tauGenJets*_*_*', ]
+)
