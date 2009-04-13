7555e1a2864cf2626078688b579ed58fdd9b306b
veelken <>
2009-04-13 11:46:49 +0000

    changed all pat::Electron, pat::Muon, pat::Tau and pat::Jet selections to use new objSelConfigurator python class

--- TauAnalysis/RecoTools/python/patPFTauSelection_cfi.py
+++ TauAnalysis/RecoTools/python/patPFTauSelection_cfi.py
@@ -1,81 +1,79 @@
 import FWCore.ParameterSet.Config as cms
-import copy
 
 # require tau candidate to be within geometric acceptance of Pixel + SiTracker detectors
-selectedLayer1TausEta21Individual = cms.EDFilter("PATTauSelector",
-    src = cms.InputTag("cleanLayer1Taus"),
+selectedLayer1TausEta21 = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausEta21"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string("abs(eta) < 2.1"),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to have transverse energy above threshold
-selectedLayer1TausPt20Individual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausPt20 = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausPt20"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string("pt > 20."),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to have a leading track
 # (track of Pt > 1. GeV within matching cone of size dR = 0.2 around jet-axis)
-selectedLayer1TausLeadTrkIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,                                                   
+selectedLayer1TausLeadTrk = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausLeadTrk"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("leadingTrackFinding") > 0.5'),
     filter = cms.bool(False)                                 
 )
 
 # require leading track of tau candidate to have Pt > 5. GeV
-selectedLayer1TausLeadTrkPtIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausLeadTrkPt = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausLeadTrkPt"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("leadingTrackPtCut") > 0.5'),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to have no tracks of Pt > 1. GeV
 # in isolation cone of size dR = 0.8, surrounding signal cone of size dR = 5./Et
-selectedLayer1TausTrkIsoIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausTrkIso = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausTrkIso"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("trackIsolation") > 0.5'),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to be isolated
 # with respect to energy deposits in ECAL
-selectedLayer1TausEcalIsoIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausEcalIso = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausEcalIso"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("ecalIsolation") > 0.5'),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to have either one or three tracks within signal cone
-selectedLayer1TausProngIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausProng = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausProng"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string("signalTracks.size() = 1 | signalTracks.size() = 3"),                                   
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to pass electron veto
-selectedLayer1TausElectronVetoIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausElectronVeto = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausElectronVeto"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("againstElectron") > 0.5'),
     filter = cms.bool(False)                                 
 )
 
 # require tau candidate to pass muon veto
-selectedLayer1TausMuonVetoIndividual = cms.EDFilter("PATTauSelector",
-    src = selectedLayer1TausEta21Individual.src,
+selectedLayer1TausMuonVeto = cms.PSet(
+    pluginName = cms.string("selectedLayer1TausMuonVeto"),
+    pluginType = cms.string("PATTauSelector"),
     cut = cms.string('tauID("againstMuon") > 0.5'),
     filter = cms.bool(False)                                 
 )
-
-selectLayer1Taus = cms.Sequence( selectedLayer1TausEta21Individual
-                                *selectedLayer1TausPt20Individual 
-                                *selectedLayer1TausLeadTrkIndividual 
-                                *selectedLayer1TausLeadTrkPtIndividual 
-                                *selectedLayer1TausTrkIsoIndividual
-                                *selectedLayer1TausEcalIsoIndividual
-                                *selectedLayer1TausProngIndividual
-                                *selectedLayer1TausElectronVetoIndividual
-                                *selectedLayer1TausMuonVetoIndividual )
 
 cleanLayer1TausSel = cms.EDProducer("PATTauSelProducer",
 
@@ -112,4 +110,4 @@ cleanLayer1TausSel = cms.EDProducer("PATTauSelProducer",
   )
 )
 
-produceLayer1SelTaus = cms.Sequence( selectLayer1Taus * cleanLayer1TausSel )
+produceLayer1SelTaus = cms.Sequence( cleanLayer1TausSel )
