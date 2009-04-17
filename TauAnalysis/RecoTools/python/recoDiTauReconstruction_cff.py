8edf80e0a4e459015b4c7e78695146872c3dcbb9
mbluj <>
2009-04-17 12:48:21 +0000

    updated for compatibility with PAT_v2

--- TauAnalysis/RecoTools/python/recoDiTauReconstruction_cff.py
+++ TauAnalysis/RecoTools/python/recoDiTauReconstruction_cff.py
@@ -2,18 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 from TauAnalysis.CandidateTools.diCandidatePairProducer_cfi import diTauProducer
 recoDiTau = diTauProducer.clone()
-recoDiTau.srcLeg1 = 'selectedLayer1Muons'
-recoDiTau.srcLeg2 = 'selectedLayer1Taus'
-recoDiTau.srcMET = 'selectedLayer1METs'
+#Be careful in standard PAT_v2 'selectedLayer1' collections are not cleaned
+recoDiTau.srcLeg1 = 'cleanLayer1Muons'
+recoDiTau.srcLeg2 = 'cleanLayer1Taus'
+#Be careful in standard PAT_v2 MET collection is called 'layer1METs'
+recoDiTau.srcMET = 'layer1METs'
 
 # PAT filters
-from PhysicsTools.PatAlgos.selectionLayer1.tauMinFilter_cfi import minLayer1Taus
-recoAtLeastOneTau = minLayer1Taus.clone()
+from PhysicsTools.PatAlgos.selectionLayer1.tauCountFilter_cfi import countLayer1Taus
+recoAtLeastOneTau = countLayer1Taus.clone()
 recoAtLeastOneTau.minNumber = 1
+recoAtLeastOneTau.maxNumber = 999999 #not changed, only for information
+recoAtLeastOneTau.src = recoDiTau.srcLeg2 #same collection as for 'recoDiTau'
 
-from PhysicsTools.PatAlgos.selectionLayer1.muonMinFilter_cfi import minLayer1Muons
-recoAtLeastOneMuon = minLayer1Muons.clone()
+from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import countLayer1Muons
+recoAtLeastOneMuon = countLayer1Muons.clone()
 recoAtLeastOneMuon.minNumber = 1
+recoAtLeastOneMuon.maxNumber = 999999 #not changed, only for information
+recoAtLeastOneMuon.src = recoDiTau.srcLeg1 #same collection as for 'recoDiTau'
+
 
 recoDiTauReconstruction = cms.Sequence(
     recoAtLeastOneTau + 
--- TauAnalysis/RecoTools/python/recoZllReconstruction_cff.py
+++ TauAnalysis/RecoTools/python/recoZllReconstruction_cff.py
@@ -3,16 +3,19 @@ import FWCore.ParameterSet.Config as cms
 # generic
 from TauAnalysis.CandidateTools.diCandidatePairProducer_cfi import diTauProducer
 recoZll = diTauProducer.clone()
-recoZll.srcLeg1 = 'selectedLayer1Muons'
-recoZll.srcLeg2 = 'selectedLayer1Muons'
+#Be careful in standard PAT_v2 'selectedLayer1Muons' are not cleaned. 
+recoZll.srcLeg1 = 'cleanLayer1Muons'
+recoZll.srcLeg2 = 'cleanLayer1Muons'
 recoZll.srcMET = ''
 recoZll.recoMode = ''
 recoZll.useLeadingTausOnly = False
 
 # PAT filters
-from PhysicsTools.PatAlgos.selectionLayer1.muonMinFilter_cfi import minLayer1Muons
-recoAtLeastTwoMuons = minLayer1Muons.clone()
+from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import countLayer1Muons
+recoAtLeastTwoMuons = countLayer1Muons.clone()
 recoAtLeastTwoMuons.minNumber = 2
+recoAtLeastTwoMuons.maxNumber = 999999 #not changed, only for information
+recoAtLeastTwoMuons.src = recoZll.srcLeg1 #same collection as for 'recoZll'
 
 
 recoZllReconstruction = cms.Sequence(
