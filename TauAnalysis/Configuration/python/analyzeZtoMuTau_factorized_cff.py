a9e42c3a6c42eda21430d35d5678c0517ffff5f5
veelken <>
2009-04-20 12:12:41 +0000

    bug-fix: analyzeZtoMuTauEvents_factorizedWithMuonIsolation needs to be defined before analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation

--- TauAnalysis/Configuration/python/analyzeZtoMuTau_factorized_cff.py
+++ TauAnalysis/Configuration/python/analyzeZtoMuTau_factorized_cff.py
@@ -12,48 +12,11 @@ from TauAnalysis.Configuration.analyzeZtoMuTau_cff import *
 
 #--------------------------------------------------------------------------------
 # define Z --> mu + tau-jet analysis module
-# for the path with "loose" muon isolation criteria applied
-#--------------------------------------------------------------------------------
-
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation = copy.copy(analyzeZtoMuTauEvents)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.name = cms.string('zMuTauAnalyzer_factorizedWithoutMuonIsolation')
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonTrkIso)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonTrkIsoLooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonEcalIso)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonEcalIsoLooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonAntiPion)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonAntiPionLooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonTrkIP)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonTrkIPlooseMuonIsolation)      
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauAntiOverlapVeto)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauAntiOverlapVetoLooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauZeroCharge)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauZeroChargeLooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauMt1MET)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauMt1METlooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelMuonTrkIso, evtSelMuonTrkIsoLooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelMuonEcalIso, evtSelMuonEcalIsoLooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelMuonAntiPion, evtSelMuonAntiPionLooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelMuonTrkIP, evtSelMuonTrkIPlooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelDiTauCandidateForMuTauAntiOverlapVeto, evtSelDiTauCandidateForMuTauAntiOverlapVetoLooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelDiTauCandidateForMuTauZeroCharge, evtSelDiTauCandidateForMuTauZeroChargeLooseMuonIsolation)
-#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
-#                      evtSelDiTauCandidateForMuTauMt1MET, evtSelDiTauCandidateForMuTauMt1METlooseMuonIsolation)
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventDumps[0] = muTauEventDump_factorizedWithoutMuonIsolation
-analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.analysisSequence = muTauAnalysisSequence_factorizedWithoutMuonIsolation
-
-#--------------------------------------------------------------------------------
-# define Z --> mu + tau-jet analysis module
 # for the path with "regular" muon isolation criteria applied
 #--------------------------------------------------------------------------------
 
 analyzeZtoMuTauEvents_factorizedWithMuonIsolation = copy.deepcopy(analyzeZtoMuTauEvents)
+analyzeZtoMuTauEvents_factorizedWithMuonIsolation.name = cms.string('zMuTauAnalyzer_factorizedWithMuonIsolation')
 analyzeZtoMuTauEvents_factorizedWithMuonIsolation.histManagers = cms.VPSet(
     genPhaseSpaceEventInfoHistManager,
     muonHistManager,
@@ -62,4 +25,49 @@ analyzeZtoMuTauEvents_factorizedWithMuonIsolation.histManagers = cms.VPSet(
 )
 analyzeZtoMuTauEvents_factorizedWithMuonIsolation.eventDumps[0] = muTauEventDump_factorizedWithMuonIsolation
 analyzeZtoMuTauEvents_factorizedWithMuonIsolation.analysisSequence = muTauAnalysisSequence_factorizedWithMuonIsolation
+
+#--------------------------------------------------------------------------------
+# define Z --> mu + tau-jet analysis module
+# for the path with "loose" muon isolation criteria applied
+#
+# NOTE: modifications to analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation
+#       modify the original analyzeZtoMuTauEvents sequence
+#
+#      --> analyzeZtoMuTauEvents_factorizedWithMuonIsolation needs to be defined
+#          before analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation !!
+#
+#--------------------------------------------------------------------------------
+
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation = copy.copy(analyzeZtoMuTauEvents)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.name = cms.string('zMuTauAnalyzer_factorizedWithoutMuonIsolation')
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonTrkIso)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonTrkIsoLooseIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonEcalIso)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonEcalIsoLooseIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonAntiPion)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonAntiPionLooseIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelMuonTrkIP)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelMuonTrkIPlooseIsolation)      
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauAntiOverlapVeto)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauAntiOverlapVetoLooseMuonIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauZeroCharge)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauZeroChargeLooseMuonIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.remove(evtSelDiTauCandidateForMuTauMt1MET)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection.append(evtSelDiTauCandidateForMuTauMt1METlooseMuonIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelMuonTrkIso, evtSelMuonTrkIsoLooseIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelMuonEcalIso, evtSelMuonEcalIsoLooseIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelMuonAntiPion, evtSelMuonAntiPionLooseIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelMuonTrkIP, evtSelMuonTrkIPlooseIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelDiTauCandidateForMuTauAntiOverlapVeto, evtSelDiTauCandidateForMuTauAntiOverlapVetoLooseMuonIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelDiTauCandidateForMuTauZeroCharge, evtSelDiTauCandidateForMuTauZeroChargeLooseMuonIsolation)
+#replaceEventSelection(analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventSelection,
+#                      evtSelDiTauCandidateForMuTauMt1MET, evtSelDiTauCandidateForMuTauMt1METlooseMuonIsolation)
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.eventDumps[0] = muTauEventDump_factorizedWithoutMuonIsolation
+analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation.analysisSequence = muTauAnalysisSequence_factorizedWithoutMuonIsolation
 
