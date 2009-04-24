41f98204e54bda5ee2e9bb53c41a3da20e2dbf55
chinhan <>
2009-04-24 13:59:53 +0000

    Changes for new workflow in 22X.

--- ElectroWeakAnalysis/TauTriggerEfficiency/src/L25and3TauEfficiencyAnalyzer.cc
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/L25and3TauEfficiencyAnalyzer.cc
@@ -1,7 +1,7 @@
 // Class:      L25and3TauEfficiencyAnalyzer
 // Original Author:  Eduardo Luiggi, modified by Sho Maruyama
 //         Created:  Fri Apr  4 16:37:44 CDT 2008
-// $Id: L25and3TauEfficiencyAnalyzer.cc,v 1.19 2009/03/25 15:08:19 gfball Exp $
+// $Id: L25and3TauEfficiencyAnalyzer.cc,v 1.1 2009/04/04 06:43:50 smaruyam Exp $
 #include "ElectroWeakAnalysis/TauTriggerEfficiency/interface/L25and3TauEfficiencyAnalyzer.h"
 using namespace edm;
 using namespace reco;
@@ -78,121 +78,152 @@ void L25and3TauEfficiencyAnalyzer::fill(const edm::Event& iEvent, const LorentzV
   l25LooseDisc21 = 0;
   l25LooseDisc13 = 0;
   l25LooseDisc23 = 0;
-if(HLTPFTau == true){
-  Handle<PFTauTagInfoCollection> tags;
-  Handle<PFTauCollection> ptJets;
-  Handle<PFTauCollection> isoJets;
-  if(iEvent.getByLabel(l25JetSource, tags)){
-    for(unsigned int j = 0; j < tags->size(); j++){ // bare L2 Taus
-            if(deltaR(tau, *(tags->at(j).pfjetRef())) < l25MatchingCone){ // dr < l25MatchingCone
-                if(l25Depth < 1) l25Depth = 1; // L2 match
-                l25Eta  = (tags->at(j).pfjetRef()->eta());
-                l25Phi  = (tags->at(j).pfjetRef()->phi());
-                l25Et   = (tags->at(j).pfjetRef()->et());
-                const PFCandidateRefVector chargedHads = tags->at(j).PFChargedHadrCands();
-                for(unsigned int k = 0; k < chargedHads.size(); k++){
-                        if(chargedHads.at(k)->pt() > 1.0 && deltaR(*(tags->at(j).pfjetRef()),*(chargedHads.at(k)) ) < 0.2){ // lead charged had in cone 0.2
-                            l25Pt  =  chargedHads.at(k)->pt();
-                            if(l25Depth < 2){
-                                l25Depth = 2; // lead track
-                                break;
-                            }
-                        }// good lead cand
-                } // charged hadron loop
-            }// offline and l25 pf tau match dr < l25MatchingCone
-        } // tag loop
-  }// non empty collection
-  if(iEvent.getByLabel(l25PtCutSource, ptJets)){ // Leading Pt Cut > X GeV/c applied, check HLT Config file
-    for(unsigned int j = 0; j < ptJets->size(); j++){
-      if(deltaR(tau, ptJets->at(j) ) < l25MatchingCone){ // dr < l25MatchingCone
-    if(l25Depth < 3){
-        l25Depth = 3; // lead pt cut match
-        break;
-        }
-      }// pf and l25 tau match dr < l25MatchingCone
-    }// for jet loop
-  }// non empty collection
-  if(iEvent.getByLabel(l3IsoSource, isoJets)){
-    for(unsigned int j = 0; j < isoJets->size(); j++){
-      if(deltaR(tau, isoJets->at(j)) < l25MatchingCone){ // dr < l25MatchingCone
-    if(l25Depth < 4){
-        l25Depth = 4; // iso match
-        break;
-        }
-      }
-    }
-  }
-}
-else {
-  Handle<IsolatedTauTagInfoCollection> tags;
-  Handle<CaloJetCollection> ptJets;
-  Handle<CaloJetCollection> isoJets;
-  if(iEvent.getByLabel(l25JetSource, tags)){
-    for(unsigned int j = 0; j < tags->size(); j++){ // bare L2 Taus
-      if(deltaR(tau, *(tags->at(j).jet())) < l25MatchingCone){ // dr < l25MatchingCone
-        if(l25Depth < 1) l25Depth = 1; // L2 match
-        l25Eta  = (tags->at(j).jet()->eta());
-        l25Phi  = (tags->at(j).jet()->phi());
-        l25Et   = (tags->at(j).jet()->et());
-        const TrackRef leadTrk = tags->at(j).leadingSignalTrack(0.2,1.0);// track finding cone = 0.2
-        if(leadTrk.isNonnull()) {
-          if(l25Depth < 2){
-            l25Depth = 2;
-            l25Pt = (leadTrk->pt() );
-            l25InvPt = 1./(leadTrk->pt() );
-          }// good lead cand
-        }
-        const TrackRef leadTrkLoose = tags->at(j).leadingSignalTrack(100.,1.0);// Loose Track finding
-        if(leadTrkLoose.isNonnull()){
-          l25PtLdgLoose = leadTrkLoose->pt();
-          l25EtaLdgLoose = leadTrkLoose->eta(); 
-          l25PhiLdgLoose = leadTrkLoose->phi();
-          double dphi = fabs(l25PhiLdgLoose-l25Phi);
-          if(dphi>2*acos(-1.))dphi=2*acos(-1.)-dphi;
-          double deta = fabs(l25EtaLdgLoose-l25Eta);
-          l25PtLdgJetDRLoose = sqrt(dphi*dphi+deta*deta);
-        }
-        
-          //evaluate a series of different discriminator parameters
-        if(tags->at(j).discriminator(0.1,0.15,0.5,3.,1.,0,0.2))
-          l25DefDisc13=1;
-        if(tags->at(j).discriminator(0.2,0.15,0.5,3.,1.,0,0.2))
-          l25DefDisc23=1;
-        if(tags->at(j).discriminator(0.2,0.15,0.5,1.,1.,0,0.2))
-          l25DefDisc21=1;
-        if(tags->at(j).discriminator(0.2,0.15,0.2,1.,1.,0,0.2))
-          l25LooseDisc21=1;
-        if(tags->at(j).discriminator(0.1,0.15,0.2,3.,1.,0,0.2))
-          l25LooseDisc13=1;
-        if(tags->at(j).discriminator(0.2,0.15,0.2,3.,1.,0,0.2))
-          l25LooseDisc23=1;
+
+  if(HLTPFTau == true){
+    Handle<PFTauTagInfoCollection> tags;
+    Handle<PFTauCollection> ptJets;
+    Handle<PFTauCollection> isoJets;
     
-      }// calo and l25 tau match dr < l25MatchingCone
-    }// for jet loop
-  }// non empty collection
-  
-  if(iEvent.getByLabel(l25PtCutSource, ptJets)){ // Leading Pt Cut > X GeV/c applied, check HLT Config file
-    for(unsigned int j = 0; j < ptJets->size(); j++){
-      if(deltaR(tau, ptJets->at(j) ) < l25MatchingCone){ // dr < l25MatchingCone
-        if(l25Depth < 3){
-          l25Depth = 3; // lead pt cut match
-          break;
-        }
-      }// pf and l25 tau match dr < l25MatchingCone
-    }// for jet loop
-  }// non empty collection
-  
-  if(iEvent.getByLabel(l3IsoSource, isoJets)){
-    for(unsigned int j = 0; j < isoJets->size(); j++){
-      if(deltaR(tau, isoJets->at(j)) < l25MatchingCone){ // dr < l25MatchingCone
-        if(l25Depth < 4){
-          l25Depth = 4; // iso match
-          break;
-        }
+    iEvent.getByLabel(l25JetSource, tags);
+    iEvent.getByLabel(l25PtCutSource, ptJets); // Leading Pt Cut > X GeV/c applied, check HLT Config file
+    iEvent.getByLabel(l3IsoSource, isoJets);
+    
+    if(tags.isValid()){
+      for(unsigned int j = 0; j < tags->size(); j++){ // bare L2 Taus
+	if(deltaR(tau, *(tags->at(j).pfjetRef())) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 1) l25Depth = 1; // L2 match
+	  l25Eta  = (tags->at(j).pfjetRef()->eta());
+	  l25Phi  = (tags->at(j).pfjetRef()->phi());
+	  l25Et   = (tags->at(j).pfjetRef()->et());
+	  const PFCandidateRefVector chargedHads = tags->at(j).PFChargedHadrCands();
+	  for(unsigned int k = 0; k < chargedHads.size(); k++){
+	    if(chargedHads.at(k)->pt() > 1.0 && deltaR(*(tags->at(j).pfjetRef()),*(chargedHads.at(k)) ) < 0.2){ // lead charged had in cone 0.2
+	      l25Pt  =  chargedHads.at(k)->pt();
+	      if(l25Depth < 2){
+		l25Depth = 2; // lead track
+		break;
+	      }
+	    }// good lead cand
+	  } // charged hadron loop
+	}// offline and l25 pf tau match dr < l25MatchingCone
+      } // tag loop
+    }// non empty collection
+    else {
+      std::cout<<"No L25Jetsource in event!"<<std::endl;
+    }
+    if(ptJets.isValid()){ // Leading Pt Cut > X GeV/c applied, check HLT Config file
+      for(unsigned int j = 0; j < ptJets->size(); j++){
+	if(deltaR(tau, ptJets->at(j) ) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 3){
+	    l25Depth = 3; // lead pt cut match
+	    break;
+	  }
+	}// pf and l25 tau match dr < l25MatchingCone
+      }// for jet loop
+    }// non empty collection
+    else {
+      std::cout<<"No L25PtJetsource in event!"<<std::endl;
+    }
+    if(isoJets.isValid()){
+      for(unsigned int j = 0; j < isoJets->size(); j++){
+	if(deltaR(tau, isoJets->at(j)) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 4){
+	    l25Depth = 4; // iso match
+	    break;
+	  }
+	}
       }
     }
+    else {
+      std::cout<<"No L3IsoJetsource in event!"<<std::endl;
+    }
+    
   }
-}
+  else {
+    Handle<IsolatedTauTagInfoCollection> tags;
+    Handle<CaloJetCollection> ptJets;
+    Handle<CaloJetCollection> isoJets;
+
+    iEvent.getByLabel(l25JetSource, tags);
+    iEvent.getByLabel(l25PtCutSource, ptJets); // Leading Pt Cut > X GeV/c applied, check HLT Config file
+    iEvent.getByLabel(l3IsoSource, isoJets);
+
+    if(tags.isValid()){
+      for(unsigned int j = 0; j < tags->size(); j++){ // bare L2 Taus
+	if(deltaR(tau, *(tags->at(j).jet())) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 1) l25Depth = 1; // L2 match
+	  l25Eta  = (tags->at(j).jet()->eta());
+	  l25Phi  = (tags->at(j).jet()->phi());
+	  l25Et   = (tags->at(j).jet()->et());
+	  const TrackRef leadTrk = tags->at(j).leadingSignalTrack(0.2,1.0);// track finding cone = 0.2
+	  if(leadTrk.isNonnull()) {
+	    if(l25Depth < 2){
+	      l25Depth = 2;
+	      l25Pt = (leadTrk->pt() );
+	      l25InvPt = 1./(leadTrk->pt() );
+	    }// good lead cand
+	  }
+	  const TrackRef leadTrkLoose = tags->at(j).leadingSignalTrack(100.,1.0);// Loose Track finding
+	  if(leadTrkLoose.isNonnull()){
+	    l25PtLdgLoose = leadTrkLoose->pt();
+	    l25EtaLdgLoose = leadTrkLoose->eta(); 
+	    l25PhiLdgLoose = leadTrkLoose->phi();
+	    double dphi = fabs(l25PhiLdgLoose-l25Phi);
+	    if(dphi>2*acos(-1.))dphi=2*acos(-1.)-dphi;
+	    double deta = fabs(l25EtaLdgLoose-l25Eta);
+	    l25PtLdgJetDRLoose = sqrt(dphi*dphi+deta*deta);
+	  }
+	  
+          //evaluate a series of different discriminator parameters
+	  if(tags->at(j).discriminator(0.1,0.15,0.5,3.,1.,0,0.2))
+	    l25DefDisc13=1;
+	  if(tags->at(j).discriminator(0.2,0.15,0.5,3.,1.,0,0.2))
+	    l25DefDisc23=1;
+	  if(tags->at(j).discriminator(0.2,0.15,0.5,1.,1.,0,0.2))
+	    l25DefDisc21=1;
+	  if(tags->at(j).discriminator(0.2,0.15,0.2,1.,1.,0,0.2))
+	    l25LooseDisc21=1;
+	  if(tags->at(j).discriminator(0.1,0.15,0.2,3.,1.,0,0.2))
+	    l25LooseDisc13=1;
+	  if(tags->at(j).discriminator(0.2,0.15,0.2,3.,1.,0,0.2))
+	    l25LooseDisc23=1;
+	  
+	}// calo and l25 tau match dr < l25MatchingCone
+      }// for jet loop
+    }// non empty collection
+    else {
+      std::cout<<"No L25Jetsource in event!"<<std::endl;
+    }
+    
+    if(ptJets.isValid()){ // Leading Pt Cut > X GeV/c applied, check HLT Config file
+      for(unsigned int j = 0; j < ptJets->size(); j++){
+	if(deltaR(tau, ptJets->at(j) ) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 3){
+	    l25Depth = 3; // lead pt cut match
+	    break;
+	  }
+	}// pf and l25 tau match dr < l25MatchingCone
+      }// for jet loop
+    }// non empty collection
+    else {
+      std::cout<<"No L25PtJetsource in event!"<<std::endl;
+    }
+    
+    if(isoJets.isValid()){
+      for(unsigned int j = 0; j < isoJets->size(); j++){
+	if(deltaR(tau, isoJets->at(j)) < l25MatchingCone){ // dr < l25MatchingCone
+	  if(l25Depth < 4){
+	    l25Depth = 4; // iso match
+	    break;
+	  }
+	}
+      }
+    }
+    else {
+      std::cout<<"No L3IsoJetsource in event!"<<std::endl;
+    }
+    
+  }
 }// tau ends
 
 void L25and3TauEfficiencyAnalyzer::beginJob(const edm::EventSetup&){} 
--- ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
@@ -3,7 +3,7 @@ import copy
 process = cms.Process("TTEffAnalysis")
 
 process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(-1)
+        input = cms.untracked.int32(1000)
 )
 
 process.load("FWCore/MessageService/MessageLogger_cfi")
@@ -20,8 +20,9 @@ process.MessageLogger.debugModules = cms.untracked.vstring("TTEffAnalyzer")
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 # 	"rfio:/castor/cern.ch/user/s/slehti/test.root"
-#	"file:test.root"
- 	"rfio:/castor/cern.ch/user/s/slehti/TauTriggerEfficiencyMeasurementData/QCDpt30_Summer08_IDEAL_V11_redigi_v1_HLT_RECO_PFTauFiltered/HLTFromDigiRaw_960_RECO_sampleProducer.root"
+      "file:/tmp/chinhan/hltExtra.root"
+#	"file:/tmp/chinhan/test.root"
+# 	"rfio:/castor/cern.ch/user/s/slehti/TauTriggerEfficiencyMeasurementData/QCDpt30_Summer08_IDEAL_V11_redigi_v1_HLT_RECO_PFTauFiltered/HLTFromDigiRaw_960_RECO_sampleProducer.root"
 # 	"rfio:/castor/cern.ch/user/s/slehti/TauTriggerEfficiencyMeasurementData/Ztautau_Summer08_IDEAL_V11_redigi_v2_HLT_RECO_PFTauFiltered_run2/HLTFromDigiRaw_91_RECO_sampleProducer.root"
     )
 )
@@ -34,6 +35,7 @@ process.source = cms.Source("PoolSource",
 process.load("L1Trigger/Configuration/L1Config_cff")
 process.load("Configuration/StandardSequences/L1TriggerDefaultMenu_cff")
 #process.load("L1TriggerConfig/L1GtConfigProducers/Luminosity/lumi1031/L1Menu_MC2009_v0_L1T_Scales_20080922_Imp0_Unprescaled_cff")
+
 process.load("HLTrigger/HLTfilters/hltLevel1GTSeed_cfi")
 process.tteffL1GTSeed = copy.deepcopy(process.hltLevel1GTSeed)
 process.tteffL1GTSeed.L1TechTriggerSeeding = cms.bool(False)
@@ -53,27 +55,26 @@ process.TTEffAnalysis = cms.EDAnalyzer("TTEffAnalyzer",
         # Check that Isolation collection below actually matched up with Tau Collection above
         #PFTauCollection         = cms.InputTag("pfRecoTauProducerHighEfficiency"),
         #PFTauIsoCollection      = cms.InputTag("pfRecoTauDiscriminationByIsolationHighEfficiency"),
-
-	L1extraTauJetSource	= cms.InputTag("hltL1extraParticles", "Tau", "HLT2"),
-	L1extraCentralJetSource	= cms.InputTag("hltL1extraParticles", "Central", "HLT2"),
+	L1extraTauJetSource	= cms.InputTag("hltL1extraParticles", "Tau", "HLT"),
+	L1extraCentralJetSource	= cms.InputTag("hltL1extraParticles", "Central", "HLT"),
         L1bitInfoSource         = cms.InputTag("l1CaloSim", "L1BitInfos"),
         L1GtReadoutRecord       = cms.InputTag("hltGtDigis"),
         L1GtObjectMapRecord     = cms.InputTag("hltL1GtObjectMap"),
-        HltResults              = cms.InputTag("TriggerResults::HLT2"),
+        HltResults              = cms.InputTag("TriggerResults::HLT"),
         L1TauTriggerSource      = cms.InputTag("tteffL1GTSeed"),
 	L1JetMatchingCone	= cms.double(0.5),
-        L2AssociationCollection = cms.InputTag("hltL2TauNarrowConeIsolationProducer"),
+        L2AssociationCollection = cms.InputTag("hltL2TauIsolationProducer"),
         L2matchingDeltaR        = cms.double(0.3),
-        l25JetSource            = cms.InputTag("hltL25TauConeIsolation"),
-        l25PtCutSource          = cms.InputTag("hltL25TauLeadingTrackPtCutSelector"),
-        l3IsoSource             = cms.InputTag("hltL3TauIsolationSelector"),
+        l25JetSource            = cms.InputTag("hltL25TauPixelTracksConeIsolation"),
+        l25PtCutSource          = cms.InputTag("hltL25TauPixelTracksLeadingTrackPtCutSelector"),
+        l3IsoSource             = cms.InputTag("hltL25TauPixelTracksIsolationSelector"),
         l25MatchingCone         = cms.double(0.3),
         MCMatchingCone         = cms.double(0.2),
         HLTPFTau                = cms.bool(False),
         MCTauCollection         = cms.InputTag("TauMCProducer:HadronicTauOneAndThreeProng"),
 
-        outputFileName          = cms.string("qcd_tteffAnalysis.root")
-#        outputFileName          = cms.string("ztt_tteffAnalysis.root")
+#        outputFileName          = cms.string("qcd_tteffAnalysis.root")
+        outputFileName          = cms.string("/tmp/chinhan/qcd_tteffAnalysis.root")
 )
 
 process.runEDAna = cms.Path(
