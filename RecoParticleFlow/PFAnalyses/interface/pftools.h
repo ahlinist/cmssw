23a5640c707f9909452beb929db834497dbfa28e
ballin <>
2009-04-30 08:59:39 +0000

    First commit

---
+++ RecoParticleFlow/PFAnalyses/interface/pftools.h
@@ -0,0 +1,27 @@
+
+/**
+ * @namespace pftools
+ * @brief Contains all classes related to Partice Flow tooling and utlities
+ *
+ * Separates out the madness.
+ *
+ * @author Jamie Ballin
+ * @date 2008 onwards
+ * @since CMSSW 2_1_X
+ *
+ * @mainpage Welcome to RecoParticleFlow/PFAnalyses
+ * @author Jamie Ballin
+ * @date 2008 onwards
+ * @since CMSSW 2_1_X
+ *
+ * @brief Contains non critcal-path particle flow analysis tools, mostly aimed at testbeam and collision data applications.
+ *
+ * @section intro_sec Introduction
+ * Take a look at lolcats to brighten up your day.
+ *
+ * @section Usage
+ * To be included at some point
+ */
+namespace pftools {
+
+}
