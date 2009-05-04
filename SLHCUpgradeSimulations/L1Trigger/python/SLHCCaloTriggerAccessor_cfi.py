f7d5a7fe62db3976392fa9dbeda7249d9e845224
kflood <>
2009-05-04 21:09:48 +0000

    default configuration for ntuple writing module

---
+++ SLHCUpgradeSimulations/L1Trigger/python/SLHCCaloTriggerAccessor_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+CaloTriggerAccessor = cms.EDFilter("SLHCCaloTriggerAccessor",
+    L1EGamma       = cms.InputTag("L1ExtraMaker","EGamma"),
+    L1IsoEGamma    = cms.InputTag("L1ExtraMaker","IsoEGamma"),
+    L1Tau          = cms.InputTag("L1ExtraMaker","Taus"),
+    Jets           = cms.InputTag("L1ExtraMaker","Jets"),
+    OutputFileName = cms.string('analysis.root')
+)
