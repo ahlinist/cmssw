8cbaacadc34bf786ee72f6235836691d8ceb3655
kflood <>
2009-05-06 00:15:11 +0000

    add SLHCCaloTriggerAccessor

--- SLHCUpgradeSimulations/L1Trigger/plugins/SealModule.cc
+++ SLHCUpgradeSimulations/L1Trigger/plugins/SealModule.cc
@@ -3,6 +3,7 @@
 #include "SLHCUpgradeSimulations/L1Trigger/interface/L1CaloClusterFilter.h"
 #include "SLHCUpgradeSimulations/L1Trigger/interface/L1JetMaker.h"
 #include "SLHCUpgradeSimulations/L1Trigger/interface/L1ExtraMaker.h"
+#include "SLHCUpgradeSimulations/L1Trigger/interface/SLHCCaloTriggerAccessor.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_FWK_MODULE(L1CaloTowerProducer);
@@ -10,4 +11,5 @@ DEFINE_FWK_MODULE(L1CaloClusterProducer);
 DEFINE_FWK_MODULE(L1CaloClusterFilter);
 DEFINE_FWK_MODULE(L1JetMaker);
 DEFINE_FWK_MODULE(L1ExtraMaker);
+DEFINE_FWK_MODULE(SLHCCaloTriggerAccessor);
 
