4452a3c06474b388cf1a4f8065ff864c13cb2024
jkolb <>
2009-05-13 09:53:02 +0000

    added sample definitions file and message logger

--- TauAnalysis/Configuration/test/runZtoElecTau_cfg.py
+++ TauAnalysis/Configuration/test/runZtoElecTau_cfg.py
@@ -10,53 +10,44 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
 process.load('Configuration/StandardSequences/MagneticField_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
-#process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_noesprefer_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = 'IDEAL_V9::All'
 
-#--------------------------------------------------------------------------------
-# import sequence for PAT-tuple production
-process.load("TauAnalysis.Configuration.producePatTuple_cff")
+process.load("TauAnalysis.Configuration.producePatLayer1_cff")
+process.load("TauAnalysis.RecoTools.pftauPatSelectorForElecTau_cfi")
+process.load("TauAnalysis.CandidateTools.elecTauPairProducer_cff")
 
-# import sequence for event selection
-process.load("TauAnalysis.Configuration.selectZtoElecTau_cff")
-
-# import sequence for filling of histograms, cut-flow table
-# and of run + event number pairs for events passing event selection
 process.load("TauAnalysis.Configuration.analyzeZtoElecTau_cff")
 
-# import event-content definition of products to be stored in patTuple
-from TauAnalysis.Configuration.patTupleEventContent_cff import *
-#--------------------------------------------------------------------------------
+# import configuration parameters for submission of jobs to CERN batch system
+# (running over skimmed samples stored on CASTOR)
+from TauAnalysis.Configuration.sampleDefinitionsZtoElecTau_cfi import *
 
 process.DQMStore = cms.Service("DQMStore")
 
-process.saveZtoElecTauPlots = cms.EDAnalyzer("DQMSimpleFileSaver",
-    outputFileName = cms.string('plotsZtoElecTau.root')
+process.saveZtoElecTau = cms.EDAnalyzer("DQMSimpleFileSaver",
+  outputFileName = cms.string('plotsZtoElecTau.root')
 )
 
-process.saveZtoElecTauPatTuple = cms.OutputModule("PoolOutputModule",
-    patTupleEventContent,
-    fileName = cms.untracked.string('elecTauSkim_patTuple.root')
-)
+# control output frequency
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+#process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#  ignoreTotal = cms.untracked.int32(1) # default is one
+#)
 
 process.maxEvents = cms.untracked.PSet(            
-    input = cms.untracked.int32(10000)    
+    input = cms.untracked.int32(1)    
 )
 
 process.source = cms.Source("PoolSource",
+    #firstEvent = cms.untracked.uint32(4097),
+    #firstRun = cms.untracked.uint32(1),
     fileNames = cms.untracked.vstring(
-#
-# Z --> tau tau (all decay modes; simulated with TAUOLA)
-# 10k events RelVal sample
-#
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/A4A3988A-BCCB-DD11-A103-001617E30E28.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/D412FFFC-BCCB-DD11-8B20-000423D952C0.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/F01E4F34-BDCB-DD11-B87D-001617C3B77C.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0004/1CAA08F8-D3CB-DD11-ADF9-000423D6B358.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0004/2800478C-08CC-DD11-94BB-0019B9F72BAA.root'
-        'rfio:/castor/cern.ch/user/v/veelken/CMSSW_2_2_3/elecTauSkim.root'
-#        'file:/afs/cern.ch/user/v/veelken/scratch0/CMSSW_2_2_7/src/TauAnalysis/Configuration/test/muTauSkim.root'    
+        'rfio:/castor/cern.ch/user/j/jkolb/eTauSkims/Ztautau/skimElecTau_1.root'
+#        'rfio:/castor/cern.ch/user/j/jkolb/eTauSkims/QCD_EMenriched_Pt30to80/skimElecTau_QCD_EMenriched_Pt30to80_1.root'
     )
     #skipBadFiles = cms.untracked.bool(True)    
 )
@@ -93,28 +84,11 @@ process.source = cms.Source("PoolSource",
 #
 #--------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------
-# import utility function for switching pat::Tau input
-# to different reco::Tau collection stored on AOD
-from PhysicsTools.PatAlgos.tools.tauTools import * 
-
-# comment-out to take reco::CaloTaus instead of reco::PFTaus
-# as input for pat::Tau production
-#switchToCaloTau(process)
-
-# comment-out to take shrinking dR = 5.0/Et(PFTau) signal cone
-# instead of fixed dR = 0.07 signal cone reco::PFTaus
-# as input for pat::Tau production
-#switchToPFTauShrinkingCone(process)
-switchToPFTauFixedCone(process)
-#--------------------------------------------------------------------------------
-
-process.p = cms.Path( process.producePatTuple
-#                    +process.content                # uncomment to enable dump of event content after PAT-tuple production
-                     +process.selectZtoElecTauEvents
-#                    +process.saveZtoElecTauPatTuple # uncomment to write-out produced PAT-tuple                      
-                     +process.analyzeZtoElecTauEvents
-                     +process.saveZtoElecTauPlots )
+process.p = cms.Path( process.producePatLayer1ForTauAnalyses
+                     +process.selectPFTausForElecTau
+                     +process.produceElecTauPairs
+                     +process.analyzeZtoElecTau
+                     +process.saveZtoElecTau ) 
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
