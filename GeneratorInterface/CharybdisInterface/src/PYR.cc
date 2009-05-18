0aa707ea73b838cb5891f13871dff862fa188df6
halil <>
2009-05-18 14:56:04 +0000

    CMSSW_3_1_X migration

---
+++ GeneratorInterface/CharybdisInterface/src/PYR.cc
@@ -0,0 +1,11 @@
+#include "GeneratorInterface/HydjetInterface/interface/PYR.h"
+#include "CLHEP/Random/RandomEngine.h"
+
+CLHEP::HepRandomEngine* randomEngine;
+
+extern "C" {
+  double pyr_(int* idummy)
+  {
+    return randomEngine->flat();
+  }
+}
