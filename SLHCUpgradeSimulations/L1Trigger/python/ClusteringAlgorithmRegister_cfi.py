ac5732a9ed39102b6e0b6fdeceb67f7fecb6568e
arose <>
2009-05-18 17:09:55 +0000

    first commit after move from L1TriggerOffline/TriggerSimulation folder

---
+++ SLHCUpgradeSimulations/L1Trigger/python/ClusteringAlgorithmRegister_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+ClusteringAlgorithm_a_PSimHit_ = cms.ESProducer("ClusteringAlgorithm_a_PSimHit_")
+
+ClusteringAlgorithm_a_PixelDigi_ = cms.ESProducer("ClusteringAlgorithm_a_PixelDigi_")
+
+ClusteringAlgorithm_a_TTHit_ = cms.ESProducer("ClusteringAlgorithm_a_TTHit_")
+
+
