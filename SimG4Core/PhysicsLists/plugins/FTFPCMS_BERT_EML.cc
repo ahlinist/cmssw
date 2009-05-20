d004c4e359e04b8e6624459cd1d6b2b8ebc25e85
sunanda <>
2009-05-20 22:54:39 +0000

    Update EML physics lists to separate region dependent part from ApplyCut option

--- SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EML.cc
+++ SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EML.cc
@@ -12,6 +12,8 @@
 #include "G4DataQuestionaire.hh"
 #include "HadronPhysicsFTFP_BERT.hh"
 
+#include <string>
+
 FTFPCMS_BERT_EML::FTFPCMS_BERT_EML(G4LogicalVolumeToDDLogicalPartMap& map,
 				   const edm::ParameterSet & p) : PhysicsList(map, p) {
 
@@ -20,14 +22,16 @@ FTFPCMS_BERT_EML::FTFPCMS_BERT_EML(G4LogicalVolumeToDDLogicalPartMap& map,
   int  ver     = p.getUntrackedParameter<int>("Verbosity",0);
   bool emPhys  = p.getUntrackedParameter<bool>("EMPhysics",true);
   bool hadPhys = p.getUntrackedParameter<bool>("HadPhysics",true);
+  std::string region = p.getUntrackedParameter<std::string>("Region", " ");
   edm::LogInfo("PhysicsList") << "You are using the simulation engine: "
 			      << "FTFP_BERT_EML 4.2 with Flags for EM Physics "
 			      << emPhys << " and for Hadronic Physics "
-			      << hadPhys << "\n";
+			      << hadPhys << " and special region " << region
+			      << "\n";
 
   if (emPhys) {
     // EM Physics
-    RegisterPhysics( new CMSEmStandardPhysics92("standard EM EML",ver));
+    RegisterPhysics( new CMSEmStandardPhysics92("standard EM EML",ver,region));
 
     // Synchroton Radiation & GN Physics
     RegisterPhysics( new G4EmExtraPhysics("extra EM"));
