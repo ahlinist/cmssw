e0079986ce786053913167cec1b024ef571f33f2
nancy <>
2009-05-20 19:19:13 +0000

    First commit

---
+++ TauAnalysis/Configuration/test/makeZtoElecTauPlots_cfg.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('makeZtoElecTauPlots')
+
+process.load("TauAnalysis.Configuration.dumpZtoElecTau_cff")
+process.load("TauAnalysis.Configuration.plotZtoElecTau_cff")
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.maxEvents = cms.untracked.PSet(            
+    input = cms.untracked.int32(0)         
+)
+
+process.source = cms.Source("EmptySource")
+
+# import utility function to enable factorization
+#from TauAnalysis.Configuration.factorizationTools import enableFactorization_makeZtoElecTauPlots
+#enableFactorization_makeZtoElecTauPlots(process)
+
+process.makeZtoElecTauPlots = cms.Sequence( process.loadZtoElecTau
+                                         +process.addZtoElecTau
+                                         +process.saveZtoElecTau
+#                                         +process.dumpZtoElecTau
+                                         +process.plotZtoElecTau )
+
+process.p = cms.Path(process.makeZtoElecTauPlots)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
