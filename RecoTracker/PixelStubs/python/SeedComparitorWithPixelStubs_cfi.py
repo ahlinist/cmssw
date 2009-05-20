ea88050b9551a7def88325e36f91e07df548607e
dfehling <>
2009-05-20 16:36:31 +0000

    Updated to have all needed variables for templates.

--- RecoTracker/PixelStubs/python/SeedComparitorWithPixelStubs_cfi.py
+++ RecoTracker/PixelStubs/python/SeedComparitorWithPixelStubs_cfi.py
@@ -1,8 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-SeedComparitorPSet = cms.PSet(
+SeedComparitorWithPixelStubs = cms.PSet(
     ComponentName = cms.string('SeedComparitorWithPixelStubs'),
-    tempCutFactor = cms.double(1E-9)
-)
+    tempCutFactor = cms.double(1E-9),
+    
+    TanLorentzAnglePerTesla = cms.double(0.106),
+    Alpha2Order = cms.bool(True),
+    speed = cms.int32(3),
+    ClusterProbComputationFlag = cms.int32(0),
+    DoCosmics = cms.bool(False),
+    LoadTemplatesFromDB = cms.bool(True),
+    UseClusterSplitter = cms.bool(False)
+    )
 
 
