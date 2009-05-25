9f33d2922d1394cf39354b2f3d62804275371024
veelken <>
2009-05-25 09:29:19 +0000

    added combinations of muon + loosely isolated electron (for factorization studies in Z --> e + mu channel)

--- TauAnalysis/CandidateTools/python/elecMuPairProduction_cff.py
+++ TauAnalysis/CandidateTools/python/elecMuPairProduction_cff.py
@@ -17,4 +17,24 @@ allElecMuPairs = cms.EDProducer("PATElecMuPairProducer",
 
 produceElecMuPairs = cms.Sequence( allElecMuPairs )
 
+# define additional collections of electron + muon candidates
+# with loose track and ECAL isolation applied on electron leg
+# (NOTE: to be used for the purpose of factorizing efficiencies
+#        of electron isolation from other event selection criteria,
+#        in order to avoid problems with limited Monte Carlo statistics)
+
+allElecMuPairsLooseElectronIsolation = cms.EDProducer("PATElecMuPairProducer",
+  useLeadingTausOnly = cms.bool(False),
+  srcLeg1 = cms.InputTag('selectedLayer1ElectronsTrkIPlooseIsolationCumulative'),
+  srcLeg2 = cms.InputTag('selectedLayer1MuonsTrkIPcumulative'),
+  dRmin12 = cms.double(-1.),
+  srcMET = cms.InputTag('layer1METs'),
+  recoMode = cms.string(""),
+  verbosity = cms.untracked.int32(0)
+)
+
+produceElecMuPairsLooseElectronIsolation = cms.Sequence( allElecMuPairsLooseElectronIsolation )
+
+produceElecMuPairsAll = cms.Sequence( produceElecMuPairs * produceElecMuPairsLooseElectronIsolation )
+
 
--- TauAnalysis/CandidateTools/python/elecMuPairSelection_cfi.py
+++ TauAnalysis/CandidateTools/python/elecMuPairSelection_cfi.py
@@ -1,37 +1,44 @@
 import FWCore.ParameterSet.Config as cms
+import copy
 
+#--------------------------------------------------------------------------------  
+# produce collections of electron + muon pairs passing selection criteria
+#--------------------------------------------------------------------------------
 
 # require electron and muon to be separated in space
 selectedElecMuPairsAntiOverlapVeto = cms.EDFilter("PATElecMuPairSelector",
-#     src = cms.InputTag("allElecMuPairs"),
      cut = cms.string('dR12 > 0.7'),
      filter = cms.bool(False)
 )
 
 # require muon and electron to form a zero-charge pair
 selectedElecMuPairsZeroCharge = cms.EDFilter("PATElecMuPairSelector",
-#     src = selectedElecMuPairsAntiOverlapVeto.src,
      cut = cms.string('charge = 0'),
      filter = cms.bool(False)
 )
-#require cut transverse mass of electron and MET
+
+# require low transverse mass of electron and MET
 selectedElecMuPairsMt1MET = cms.EDFilter("PATElecMuPairSelector",
-#    src = selectedElecMuPairsAntiOverlapVeto.src,
     cut = cms.string('mt1MET < 50.'),
     filter = cms.bool(False)
 )
 
-#require cut transverse mass of muon and MET
+# require low transverse mass of muon and MET
 selectedElecMuPairsMt2MET = cms.EDFilter("PATElecMuPairSelector",    
-#    src = selectedElecMuPairsAntiOverlapVeto.src,
     cut = cms.string('mt2MET < 50.'),
     filter = cms.bool(False)
 )
 
+# define additional collections of electron + muon candidates
+# with loose track and ECAL isolation applied on electron leg
+# (NOTE: to be used for the purpose of factorizing efficiencies
+#        of electron isolation from other event selection criteria,
+#        in order to avoid problems with limited Monte Carlo statistics)
 
-selectElecMuPairs = cms.Sequence(
-                                  selectedElecMuPairsAntiOverlapVeto         
-	                         *selectedElecMuPairsZeroCharge
-                                 *selectedElecMuPairsMt1MET
-                                 *selectedElecMuPairsMt2MET
-)
+selectedElecMuPairsAntiOverlapVetoLooseElectronIsolation = copy.deepcopy(selectedElecMuPairsAntiOverlapVeto)
+
+selectedElecMuPairsZeroChargeLooseElectronIsolation = copy.deepcopy(selectedElecMuPairsZeroCharge)
+
+selectedElecMuPairsMt1METlooseElectronIsolation = copy.deepcopy(selectedElecMuPairsMt1MET)
+
+selectedElecMuPairsMt2METlooseElectronIsolation = copy.deepcopy(selectedElecMuPairsMt2MET)
