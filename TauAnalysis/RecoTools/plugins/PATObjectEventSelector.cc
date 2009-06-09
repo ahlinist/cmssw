ad5033a5806a613bc45d02b0c90ac3cac0f848a8
veelken <>
2009-06-09 12:37:54 +0000

    - added PATCandViewCountEventSelector which allows for simultaneous min and max cuts - cleanup

--- TauAnalysis/RecoTools/plugins/PATObjectEventSelector.cc
+++ TauAnalysis/RecoTools/plugins/PATObjectEventSelector.cc
@@ -5,26 +5,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 using namespace pat;
-/*
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATElectronMinEventSelector, "PATElectronMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATMuonMinEventSelector, "PATMuonMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATTauMinEventSelector, "PATTauMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATPhotonMinEventSelector, "PATPhotonMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATJetMinEventSelector, "PATJetMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATMETMinEventSelector, "PATMETMinEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATParticleMinEventSelector, "PATParticleMinEventSelector");
- */
 
 DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATCandViewMinEventSelector, "PATCandViewMinEventSelector");
-
-/*
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATElectronMaxEventSelector, "PATElectronMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATMuonMaxEventSelector, "PATMuonMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATTauMaxEventSelector, "PATTauMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATPhotonMaxEventSelector, "PATPhotonMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATJetMaxEventSelector, "PATJetMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATMETMaxEventSelector, "PATMETMaxEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATParticleMaxEventSelector, "PATParticleMaxEventSelector");
- */
-
 DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATCandViewMaxEventSelector, "PATCandViewMaxEventSelector");
+DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, PATCandViewCountEventSelector, "PATCandViewCountEventSelector");
+
+
