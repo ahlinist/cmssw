3c9c64dd0b3dfac028bbfb7a9ede121501b931eb
veelken <>
2009-06-11 15:49:06 +0000

    added HistManagerBase.cc file back, to allow for HistManagerBasePluginFactory (in addition to AnalyzerPluginFactory)

---
+++ TauAnalysis/Core/src/HistManagerBase.cc
@@ -0,0 +1,5 @@
+#include "TauAnalysis/Core/interface/HistManagerBase.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+EDM_REGISTER_PLUGINFACTORY(HistManagerPluginFactory, "HistManagerPluginFactory");
