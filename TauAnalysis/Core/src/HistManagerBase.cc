2bb1bd58dd0cb8a149f3541af3962a94109f7dce
veelken <>
2009-06-11 13:39:32 +0000

    HistManagerPluginFactory replaced by AnalyzerPluginFactory

--- TauAnalysis/Core/src/HistManagerBase.cc
+++
@@ -1,6 +0,0 @@
-#include "TauAnalysis/Core/interface/HistManagerBase.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-EDM_REGISTER_PLUGINFACTORY(HistManagerPluginFactory, "HistManagerPluginFactory");
-
