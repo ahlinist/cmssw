fbc0078c4474e59a09209975c968c98843c9e6a3
veelken <>
2009-06-12 15:03:33 +0000

    added scripts for DQM harvesting (summing of all MonitorElements contained in .root files of same process)

---
+++ TauAnalysis/Configuration/test/harvestZtoMuTauPlots_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('harvestZtoMuTauPlots')
+
+from TauAnalysis.Configuration.plotZtoMuTau_processes_cfi import *
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.maxEvents = cms.untracked.PSet(            
+    input = cms.untracked.int32(0)         
+)
+
+process.source = cms.Source("EmptySource")
+
+#--------------------------------------------------------------------------------
+# define "hooks" for replacing configuration parameters
+# in case running jobs on the CERN batch system
+#
+process.loadZtoMuTau = cms.EDAnalyzer("DQMFileLoader",
+    #process# = copy.deepcopy(#inputFileNames#)
+)
+
+process.saveZtoMuTau = cms.EDAnalyzer("DQMSimpleFileSaver",
+    outputFileName = cms.string(#outputFileName#)
+)
+#--------------------------------------------------------------------------------
+
+process.harvestZtoMuTauPlots = cms.Sequence( process.loadZtoMuTau
+                                            +process.saveZtoMuTau )
+
+process.p = cms.Path(process.harvestZtoMuTauPlots)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
---
+++ TauAnalysis/Configuration/test/submitHarvestingZtoElecTau.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from TauAnalysis.Configuration.submitToBatch import submitToBatch
+from TauAnalysis.Configuration.makeReplacementsHarvesting import makeReplacementsHarvesting
+
+# name of the directory (either on afs area or castor)
+# to which all .root files produced by the cmsRun job will be copied
+outputDirectory = "/castor/cern.ch/user/v/veelken/plots/ZtoElecTau/"
+
+#--------------------------------------------------------------------------------
+#
+# Add histograms, numbers in FilterStatisticsTables and run + event numbers
+# stored as DQM MonitorElements in different ROOT files
+#
+# NOTE: The jobs get submitted to the '1nh' queue,
+#       which allows for an execution time of the cmsRun jobs of up to 1 hour
+#       (the queues are {'1nh' (1 hour), '1nd' (24 hours) and '1nw' (1 week execution time limit);
+#        see https://twiki.cern.ch/twiki/bin/view/CMS/CMSUKCMSSWBatch for details about the CERN batch system)           
+#
+#--------------------------------------------------------------------------------
+
+# harvest Z --> tau tau 
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "Ztautau",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+# harvest Z --> e e
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "Zee",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+# harvest QCD_BCtoE 
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_BCtoE_Pt20to30",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_BCtoE_Pt30to80",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_BCtoE_Pt80to170",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+# harvest QCD_EMenriched
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_EMenriched_Pt20to30",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_EMenriched_Pt30to80",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "QCD_EMenriched_Pt80to170",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+# harvest W/Z + jets
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "WplusJets",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "ZtautauPlusJets",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "ZeePlusJets",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
+
+# harvest TT + jets
+submitToBatch(configFile = "harvestZtoElecTauPlots_cfg.py", channel = "ZtoElecTau", sample = "TTplusJets",
+              replFunction = makeReplacementsHarvesting, replacements = "",
+              job = "harvesting", queue = "1nh", outputDirectory = outputDirectory)
