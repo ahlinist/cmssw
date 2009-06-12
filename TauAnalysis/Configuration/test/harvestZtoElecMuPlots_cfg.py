52ed7cab03c297402b111f8eced3b8750e5da767
veelken <>
2009-06-12 15:03:33 +0000

    added scripts for DQM harvesting (summing of all MonitorElements contained in .root files of same process)

---
+++ TauAnalysis/Configuration/test/harvestZtoElecMuPlots_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('harvestZtoElecMuPlots')
+
+from TauAnalysis.Configuration.plotZtoElecMu_processes_cfi import *
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.maxEvents = cms.untracked.PSet(            
+    input = cms.untracked.int32(0)         
+)
+
+process.source = cms.Source("EmptySource")
+
+#--------------------------------------------------------------------------------
+# define "hooks" for replacing configuration parameters
+# in case running jobs on the CERN batch system
+#
+process.loadZtoElecMu = cms.EDAnalyzer("DQMFileLoader",
+    #process# = copy.deepcopy(#inputFileNames#)
+)
+
+process.saveZtoElecMu = cms.EDAnalyzer("DQMSimpleFileSaver",
+    outputFileName = cms.string(#outputFileName#)
+)
+#--------------------------------------------------------------------------------
+
+process.harvestZtoElecMuPlots = cms.Sequence( process.loadZtoElecMu
+                                             +process.saveZtoElecMu )
+
+process.p = cms.Path(process.harvestZtoElecMuPlots)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
