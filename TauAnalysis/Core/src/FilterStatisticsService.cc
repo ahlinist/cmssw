6c7983bb41ebd8dff038345d72a76e392f82de16
veelken <>
2009-06-17 12:34:10 +0000

    moved #include statement for DQMStore from .h into .cc file

--- TauAnalysis/Core/src/FilterStatisticsService.cc
+++ TauAnalysis/Core/src/FilterStatisticsService.cc
@@ -5,7 +5,7 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
 
 #include <iostream>
 #include <iomanip>
