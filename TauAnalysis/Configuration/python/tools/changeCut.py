82e3084eead3a1c8bd9e501ca0c0edc90120efe5
veelken <>
2009-06-18 13:58:35 +0000

    added print-out of which cut is replaced by what value

--- TauAnalysis/Configuration/python/tools/changeCut.py
+++ TauAnalysis/Configuration/python/tools/changeCut.py
@@ -8,6 +8,9 @@ def changeCut(process, name, value, attribute = "cut"):
     cutNames.append(objSelConfigurator._composeModuleName(name, "Cumulative"))
     cutNames.append(objSelConfigurator._composeModuleName(name, "Individual"))
 
+    oldValue = 0.
+    oldValue_initialized = False
+
     for cutName in cutNames:
         if not hasattr(process, cutName):
             raise ValueError("No cut = " + cutName + " attached to process object !!")
@@ -17,5 +20,14 @@ def changeCut(process, name, value, attribute = "cut"):
         if not hasattr(cut, attribute):
             raise ValueError("Attribute = " + attribute + " not defined for cut = " + cutName + " !!")
 
+        # Note: "cumulative" and "individual" cuts both point to the same cut value object;
+        #       as a consequence, the cut value actually changes for both
+        #       the "cumulative" and "individual" cuts upon the first setattr call
+        #      --> need to initialize oldValue variable before first setattr call
+        if not oldValue_initialized:
+            oldValue = getattr(cut, attribute).value()
+        oldValue_initialized = True
+            
         setattr(cut, attribute, value)
-
+        
+        print("Replaced " + cutName + "." + attribute + ": '" + oldValue + "' => '" + value + "'")
