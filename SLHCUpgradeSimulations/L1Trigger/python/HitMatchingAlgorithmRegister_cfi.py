26b0d2f61fa319f4ca141051eba223a0b1d16918
arose <>
2009-06-19 15:01:47 +0000

    Removed the code commenting out various methods. You should now use an ESprefer statement in your config. If in doubt see the wiki: https://twiki.cern.ch/twiki/bin/view/CMS/TrackTriggerHitsAndStubs

--- SLHCUpgradeSimulations/L1Trigger/python/HitMatchingAlgorithmRegister_cfi.py
+++ SLHCUpgradeSimulations/L1Trigger/python/HitMatchingAlgorithmRegister_cfi.py
@@ -1,25 +1,31 @@
+##############################################################################
+# if you are unsure as to why this used to work but does not any more you 
+# probably need to include an ESPrefer module to your config file.
+# For details see the wikipage:
+#
+# https://twiki.cern.ch/twiki/bin/view/CMS/TrackTriggerHitsAndStubs
+#
+# A.W. Rose
+##############################################################################
+
+
 import FWCore.ParameterSet.Config as cms
-"""
-HitMatchingAlgorithm_a_PSimHit_ = cms.ESProducer(
-    "HitMatchingAlgorithm_a_PSimHit_"
-    )
 
-HitMatchingAlgorithm_a_PixelDigi_ = cms.ESProducer(
-    "HitMatchingAlgorithm_a_PixelDigi_"
-    )
+##############################################################################
 
-HitMatchingAlgorithm_a_TTHit_ = cms.ESProducer(
-	"HitMatchingAlgorithm_a_TTHit_"
-	)
-"""
+HitMatchingAlgorithm_a_PSimHit_ = cms.ESProducer( "HitMatchingAlgorithm_a_PSimHit_" )
 
+HitMatchingAlgorithm_a_PixelDigi_ = cms.ESProducer( "HitMatchingAlgorithm_a_PixelDigi_" )
+ 
+HitMatchingAlgorithm_a_TTHit_ = cms.ESProducer( "HitMatchingAlgorithm_a_TTHit_" )
+
+##############################################################################
 
 HitMatchingAlgorithm_globalgeometry_PSimHit_ = cms.ESProducer(
     "HitMatchingAlgorithm_globalgeometry_PSimHit_",
-    ipWidth = cms.double(200.0),
+    ipWidth = cms.double(15.0),
     minPtThreshold = cms.double(2.0)
     )
-
 
 HitMatchingAlgorithm_globalgeometry_PixelDigi_ = cms.ESProducer(
     "HitMatchingAlgorithm_globalgeometry_PixelDigi_",
@@ -27,15 +33,16 @@ HitMatchingAlgorithm_globalgeometry_PixelDigi_ = cms.ESProducer(
     minPtThreshold = cms.double(2.0)
     )
 
-HitMatchingAlgorithm_globalgeometry_a_TTHit_ = cms.ESProducer(
+HitMatchingAlgorithm_globalgeometry_TTHit_ = cms.ESProducer(
     "HitMatchingAlgorithm_globalgeometry_TTHit_",
     ipWidth = cms.double(200.0),
     minPtThreshold = cms.double(2.0)
     )
 
+##############################################################################
 
+HitMatchingAlgorithm_window_PSimHit_ = HitMatchingAlgorithm_globalgeometry_PSimHit_
 
-"""
 HitMatchingAlgorithm_window_PixelDigi_ = cms.ESProducer(
     "HitMatchingAlgorithm_window_PixelDigi_",
     ipWidth = cms.double(15.0),
@@ -44,19 +51,18 @@ HitMatchingAlgorithm_window_PixelDigi_ = cms.ESProducer(
     ColResolution = cms.double(1)
     )
 
-HitMatchingAlgorithm_window_a_TTHit_ = cms.ESProducer(
+HitMatchingAlgorithm_window_TTHit_ = cms.ESProducer(
     "HitMatchingAlgorithm_window_TTHit_",
     ipWidth = cms.double(15.0),
     minPtThreshold = cms.double(5.0),
     RowResolution = cms.double(1),
     ColResolution = cms.double(1)
     )
-"""
 
+##############################################################################
 
+HitMatchingAlgorithm_thresholds_PSimHit_ = HitMatchingAlgorithm_globalgeometry_PSimHit_
 
-
-"""
 HitMatchingAlgorithm_thresholds_PixelDigi_ = cms.ESProducer(
     
     "HitMatchingAlgorithm_thresholds_PixelDigi_",
@@ -112,4 +118,6 @@ HitMatchingAlgorithm_thresholds_TTHit_ = cms.ESProducer(
     )
     )
     )
-"""
+
+##############################################################################
+
