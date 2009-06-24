f33f36836b798e63edf0ed56db1dfd8a644ac17c
yjlei <>
2009-06-24 08:02:43 +0000

    Add ReportSummaryMap

--- DQM/RenderPlugins/src/ESRenderPlugin.cc
+++ DQM/RenderPlugins/src/ESRenderPlugin.cc
@@ -15,120 +15,134 @@
 
 class ESRenderPlugin : public DQMRenderPlugin {
 
- public:
+   public:
 
 
-  virtual bool applies( const DQMNet::CoreObject &o, const VisDQMImgInfo &i );
+      virtual bool applies( const DQMNet::CoreObject &o, const VisDQMImgInfo &i );
 
-  virtual void preDraw( TCanvas *c, const DQMNet::CoreObject &o, const VisDQMImgInfo &i, VisDQMRenderInfo&  r);
+      virtual void preDraw( TCanvas *c, const DQMNet::CoreObject &o, const VisDQMImgInfo &i, VisDQMRenderInfo&  r);
 
- private:
+   private:
 
-  void preDrawTH2F( TCanvas *c, const DQMNet::CoreObject &o );
+      void preDrawTH2F( TCanvas *c, const DQMNet::CoreObject &o );
 
-  double NEntries;
+      double NEntries;
 
 };
 
 
 bool ESRenderPlugin::applies( const DQMNet::CoreObject &o, const VisDQMImgInfo & ) {
 
-	if( o.name.find( "EcalPreshower" ) != std::string::npos ) {
-		if( o.name.find( "ESOccupancyTask" ) != std::string::npos ){
-			return true;
-		}
-		if( o.name.find( "ESIntegrityTask" ) != std::string::npos ){
-			return true;
-		}
-		if( o.name.find( "ESIntegrityClient" ) != std::string::npos ){
-			return true;
-		}
-	}
+   if( o.name.find( "EcalPreshower" ) != std::string::npos ) {
+      if( o.name.find( "ESOccupancyTask" ) != std::string::npos ){
+	 return true;
+      }
+      if( o.name.find( "ESIntegrityTask" ) != std::string::npos ){
+	 return true;
+      }
+      if( o.name.find( "ESIntegrityClient" ) != std::string::npos ){
+	 return true;
+      }
+      if( o.name.find( "EventInfo" ) != std::string::npos ){
+	 return true;
+      }
+   }
 
-	return false;
+   return false;
 
 }
 
 
 void ESRenderPlugin::preDraw( TCanvas *c, const DQMNet::CoreObject &o, const VisDQMImgInfo &, VisDQMRenderInfo & ) {
 
-	c->cd();
+   c->cd();
 
-	gStyle->Reset("Default");
+   gStyle->Reset("Default");
 
-	gStyle->SetCanvasColor(10);
-	gStyle->SetPadColor(10);
-	gStyle->SetFillColor(10);
-	gStyle->SetFrameFillColor(10);
-	gStyle->SetStatColor(10);
-	gStyle->SetTitleFillColor(10);
+   gStyle->SetCanvasColor(10);
+   gStyle->SetPadColor(10);
+   gStyle->SetFillColor(10);
+   gStyle->SetFrameFillColor(10);
+   gStyle->SetStatColor(10);
+   gStyle->SetTitleFillColor(10);
 
-	gStyle->SetOptTitle(kTRUE);
-	gStyle->SetTitleBorderSize(0);
+   gStyle->SetOptTitle(kTRUE);
+   gStyle->SetTitleBorderSize(0);
 
-	gStyle->SetOptStat(kFALSE);
-	gStyle->SetStatBorderSize(1);
-	gStyle->SetOptFit(kFALSE);
+   gStyle->SetOptStat(kFALSE);
+   gStyle->SetStatBorderSize(1);
+   gStyle->SetOptFit(kFALSE);
 
 
-	if( dynamic_cast<TH2F*>( o.object ) ) {
-		preDrawTH2F( c, o );
-	}
+   if( dynamic_cast<TH2F*>( o.object ) ) {
+      preDrawTH2F( c, o );
+   }
 
 }
 
 
 void ESRenderPlugin::preDrawTH2F( TCanvas *, const DQMNet::CoreObject &o ) {
 
-	TH2F* obj = dynamic_cast<TH2F*>( o.object );
+   TH2F* obj = dynamic_cast<TH2F*>( o.object );
 
-	assert( obj );
+   assert( obj );
 
-	std::string name = o.name.substr(o.name.rfind("/")+1);
+   std::string name = o.name.substr(o.name.rfind("/")+1);
 
-	int colorbar[6] = {1,2,3,4,5,6};
+   int colorbar[6] = {1,2,3,4,5,6};
+   int colorbar2[5] = {0,3,800,4,2};
 
-	gStyle->SetPaintTextFormat();
+   gStyle->SetPaintTextFormat();
 
-	gStyle->SetOptStat(kFALSE);
-	obj->SetStats(kFALSE);
-	gPad->SetLogy(kFALSE);
+   gStyle->SetOptStat(kFALSE);
+   obj->SetStats(kFALSE);
+   gPad->SetLogy(kFALSE);
 
-	if( name.find( "Integrity Summary" ) != std::string::npos ) {
-		gStyle->SetPalette(6,colorbar);
-		obj->SetMinimum(0.5);
-		obj->SetMaximum(6.5);
-		obj->SetOption("colz");
-		gPad->SetRightMargin(0.15);
-		gStyle->SetPaintTextFormat("+g");
-		return;
-	}
+   if( name.find( "Integrity Summary" ) != std::string::npos ) {
+      gStyle->SetPalette(6,colorbar);
+      obj->SetMinimum(0.5);
+      obj->SetMaximum(6.5);
+      obj->SetOption("colz");
+      gPad->SetRightMargin(0.15);
+      gStyle->SetPaintTextFormat("+g");
+      return;
+   }
 
 
-	if( name.find( "RecHit 2D Occupancy" ) != std::string::npos ) {
-		gStyle->SetPalette(1);
-      		obj->GetXaxis()->SetRange(0,39);
-      		obj->GetYaxis()->SetRange(0,39);
-		NEntries = obj->GetBinContent(40,40);
-		obj->Scale(1/NEntries);
-		obj->SetMaximum(33);
-		obj->SetOption("colz");
-		gPad->SetRightMargin(0.15);
-		gStyle->SetPaintTextFormat("+g");
-		return;
-	}
+   if( name.find( "RecHit 2D Occupancy" ) != std::string::npos ) {
+      gStyle->SetPalette(1);
+      obj->GetXaxis()->SetRange(0,39);
+      obj->GetYaxis()->SetRange(0,39);
+      NEntries = obj->GetBinContent(40,40);
+      obj->Scale(1/NEntries);
+      obj->SetMaximum(33);
+      obj->SetOption("colz");
+      gPad->SetRightMargin(0.15);
+      gStyle->SetPaintTextFormat("+g");
+      return;
+   }
 
-	if( name.find( "DigiHit 2D Occupancy" ) != std::string::npos ) {
-		gStyle->SetPalette(1);
-      		obj->GetXaxis()->SetRange(0,39);
-      		obj->GetYaxis()->SetRange(0,39);
-		NEntries = obj->GetBinContent(40,40);
-		obj->Scale(1/NEntries);
-		obj->SetOption("colz");
-		gPad->SetRightMargin(0.15);
-		gStyle->SetPaintTextFormat("+g");
-		return;
-	}
+   if( name.find( "DigiHit 2D Occupancy" ) != std::string::npos ) {
+      gStyle->SetPalette(1);
+      obj->GetXaxis()->SetRange(0,39);
+      obj->GetYaxis()->SetRange(0,39);
+      NEntries = obj->GetBinContent(40,40);
+      obj->Scale(1/NEntries);
+      obj->SetOption("colz");
+      gPad->SetRightMargin(0.15);
+      gStyle->SetPaintTextFormat("+g");
+      return;
+   }
+
+   if( name.find( "reportSummaryMap" ) != std::string::npos ) {
+      gStyle->SetPalette(6,colorbar);
+      obj->SetMinimum(-0.5);
+      obj->SetMaximum(4.5);
+      obj->SetOption("col");
+      gPad->SetRightMargin(0.15);
+      gStyle->SetPaintTextFormat("+g");
+      return;
+   }
 
 
 
