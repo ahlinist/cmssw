80465e3f9c81dfc22dad18aac0c55be2201571fb
carrillo <>
2009-07-03 14:55:39 +0000

    example with 31X MC

--- DQM/RPCMonitorModule/python/seg2D31X.py
+++ DQM/RPCMonitorModule/python/seg2D31X.py
@@ -15,27 +15,14 @@ process.load("DQMServices.Components.MEtoEDMConverter_cfi")
 process.load("DQMServices.Core.DQM_cfg")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-<<<<<<< seg2D31X.py
 process.GlobalTag.globaltag = 'IDEAL_31X::All'
 #process.GlobalTag.globaltag = 'MC_31X_V1::All'
-=======
-#process.GlobalTag.globaltag = 'IDEAL_31X::All'
-process.GlobalTag.globaltag = 'MC_31X_V1::All'
->>>>>>> 1.2
 
 process.maxEvents = cms.untracked.PSet(
-<<<<<<< seg2D31X.py
     input = cms.untracked.int32(100)
-=======
-    input = cms.untracked.int32(-1)
->>>>>>> 1.2
 )
 process.source = cms.Source("PoolSource",
-<<<<<<< seg2D31X.py
     fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0/RelValSingleMuPt10/ALCARECO/MC_31X_V1_StreamALCARECORpcCalHLT-v1/0002/32FEE565-E266-DE11-8F9F-001D09F251CC.root')
-=======
-    fileNames = cms.untracked.vstring('-input-')
->>>>>>> 1.2
 )
 
 process.museg = cms.EDFilter("MuonSegmentEff",
@@ -63,11 +50,7 @@ process.museg = cms.EDFilter("MuonSegmentEff",
     dt4DSegments = cms.untracked.string('dt4DSegments'),
 
     EffSaveRootFile = cms.untracked.bool(True),
-<<<<<<< seg2D31X.py
     EffRootFileName = cms.untracked.string('/tmp/carrillo/rpcStream.root'),
-=======
-    EffRootFileName = cms.untracked.string('-output-'),
->>>>>>> 1.2
     EffSaveRootFileEventsInterval = cms.untracked.int32(100)
 )
 process.p = cms.Path(process.museg)
