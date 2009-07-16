9ec8e0d90daf226be931c5625c1aced711bb6db4
veelken <>
2009-07-16 16:52:05 +0000

    improved tabbing

--- TauAnalysis/Skimming/python/EventContent_cff.py
+++ TauAnalysis/Skimming/python/EventContent_cff.py
@@ -13,15 +13,19 @@ tauAnalysisEventContent = copy.deepcopy(AODSIMEventContent)
 #--------------------------------------------------------------------------------
 
 allEcalRecHits = cms.PSet(
-  outputCommands = cms.untracked.vstring('keep *_ecalRecHit_EcalRecHitsEB_*',
-                                         'keep *_ecalRecHit_EcalRecHitsEE_*')
-#                                         'keep *_ecalRecHit_EcalRecHitsES_*')
+    outputCommands = cms.untracked.vstring(
+        'keep *_ecalRecHit_EcalRecHitsEB_*',
+        'keep *_ecalRecHit_EcalRecHitsEE_*'
+        #'keep *_ecalRecHit_EcalRecHitsES_*'
+    )
 )
 tauAnalysisEventContent.outputCommands.extend(allEcalRecHits.outputCommands)
 
 allHcalRecHits = cms.PSet(
-  outputCommands = cms.untracked.vstring('keep *_hbhereco_*_*')
-#                                         'keep *_hfreco_*_*',
-#                                         'keep *_horeco_*_*')
+    outputCommands = cms.untracked.vstring(
+        'keep *_hbhereco_*_*'
+        #'keep *_hfreco_*_*',
+        #'keep *_horeco_*_*'
+    )
 )
 tauAnalysisEventContent.outputCommands.extend(allHcalRecHits.outputCommands)
