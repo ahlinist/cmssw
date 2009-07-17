42d931dc885cf3b2b129df4baea8e520a49435a7
veelken <>
2009-07-17 14:19:07 +0000

    added data-format for Z --> e+ e- mass hypothesis of electron + tau-jet pair

--- AnalysisDataFormats/TauAnalysis/src/classes.h
+++ AnalysisDataFormats/TauAnalysis/src/classes.h
@@ -2,6 +2,9 @@
 #include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
 #include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEtFwd.h"
 
+#include "AnalysisDataFormats/TauAnalysis/interface/PATElecTauPairZeeHypothesis.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/PATElecTauPairZeeHypothesisFwd.h"
+
 #include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateTMEt.h"
 #include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateTMEtFwd.h"
 
@@ -27,11 +30,35 @@ namespace {
     DiCandidatePairCollection dummyDiCandidatePairCollection;
     edm::Wrapper<DiCandidatePairCollection> dummyDiCandidatePairCollectionWrapper;
 
-    /// create dictionaries for edm::Ptr<pat::Electron>, edm::Ptr<pat::Muon> and edm::Ptr<pat::Tau>,
+    /// create dictionaries for 
+    ///  o edm::Ptr<pat::Electron> 
+    ///  o edm::Ptr<pat::Muon> 
+    ///  o edm::Ptr<pat::Tau>
+    ///  o edm::Ptr<pat::Jet>
     /// as these dictionaries are not yet created in DataFormats/PatCandidates/src/classes.h
     edm::Ptr<pat::Electron> dummyPATElectronPtr;
     edm::Ptr<pat::Muon> dummyPATMuonPtr;
     edm::Ptr<pat::Tau> dummyPATTauPtr;
+    edm::Ptr<pat::Jet> dummyPATJetPtr;
+
+    /// create dictionaries for 
+    ///  o edm::Ptr<reco::CaloJet>
+    ///  o edm::Ptr<reco::PFJet>
+    /// as these dictionaries are not yet created in DataFormats/JetReco/src/classes.h
+    edm::Ptr<reco::CaloJet> dummyCaloJetPtr;
+    edm::Ptr<reco::PFJet> dummyPFJetPtr;
+
+    /// create dictionaries for
+    ///  o edm::Ptr<reco::GenParticle>
+    /// as these dictionaries are not yet created in DataFormats/HepMCCandidate/src/classes.h
+    edm::Ptr<reco::GenParticle> dummyGenParticlePtr;
+
+    /// create dictionaries for
+    ///  o edm::Ptr<reco::Track>
+    ///  o edm::Ptr<reco::GsfTrack>
+    /// as these dictionaries are not yet created in DataFormats/TrackReco/src/classes.h (DataFormats/GsfTrackReco/src/classes.h)
+    edm::Ptr<reco::Track> dummyTrackPtr;
+    edm::Ptr<reco::GsfTrack> dummyGsfTrackPtr;
 
     /// create dictionaries for PATElecPair objects
     PATElecPair dummyPATElecPair;
@@ -50,30 +77,41 @@ namespace {
     edm::Wrapper<PATElecTauPair> dummyPATElecTauPairWrapper;
     PATElecTauPairCollection dummyPATElecTauPairCollection;
     edm::Wrapper<PATElecTauPairCollection> dummyPATElecTauPairCollectionWrapper;
+    edm::Ptr<PATElecTauPair> dummyPATElecTauPairPtr;
+
+    /// create dictionaries for PATElecTauPairZeeHypothesis objects
+    PATElecTauPairZeeHypothesis dummyPATElecTauPairZeeHypothesis;
+    edm::Wrapper<PATElecTauPairZeeHypothesis> dummyPATElecTauPairZeeHypothesisWrapper;
+    PATElecTauPairZeeHypothesisCollection dummyPATElecTauPairZeeHypothesisCollection;
+    edm::Wrapper<PATElecTauPairZeeHypothesisCollection> dummyPATElecTauPairCollectionZeeHypothesisWrapper;
     
     /// create dictionaries for PATMuTauPair objects
     PATMuTauPair dummyPATMuTauPair;
     edm::Wrapper<PATMuTauPair> dummyPATMuTauPairWrapper;
     PATMuTauPairCollection dummyPATMuTauPairCollection;
     edm::Wrapper<PATMuTauPairCollection> dummyPATMuTauPairCollectionWrapper;
+    edm::Ptr<PATMuTauPair> dummyPATMuTauPairPtr;
     
     /// create dictionaries for PATDiTauPair objects
     PATDiTauPair dummyPATDiTauPair;
     edm::Wrapper<PATDiTauPair> dummyPATDiTauPairWrapper;
     PATDiTauPairCollection dummyPATDiTauPairCollection;
     edm::Wrapper<PATDiTauPairCollection> dummyPATDiTauPairCollectionWrapper;
+    edm::Ptr<PATDiTauPair> dummyPATDiTauPairPtr;
     
     /// create dictionaries for PATElecMuPair objects
     PATElecMuPair dummyPATElecMuPair;
     edm::Wrapper<PATElecMuPair> dummyPATElecMuPairWrapper;
     PATElecMuPairCollection dummyPATElecMuPairCollection;
     edm::Wrapper<PATElecMuPairCollection> dummyPATElecMuPairCollectionWrapper;
+    edm::Ptr<PATElecMuPair> dummyPATElecMuPairPtr;
 
     /// create dictionaries for PATTauNuPair objects
     PATTauNuPair dummyPATTauNuPair;
     edm::Wrapper<PATTauNuPair> dummyPATTauNuPairWrapper;
     PATTauNuPairCollection dummyPATTauNuPairCollection;
     edm::Wrapper<PATTauNuPairCollection> dummyPATTauNuPairCollectionWrapper;
+    edm::Ptr<PATTauNuPair> dummyPATTauNuPairPtr;
 
     /// create dictionaries for PATLeptonRecoilEnergyFromJets objects
     PATTauRecoilEnergyFromJets dummyPATTauRecoilEnergyFromJets;
