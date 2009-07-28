da4e4f3b8e3f16254754ff4386d6bf52b0f6f3c3
dhidas <>
2009-07-28 15:56:26 +0000

    Update for plotting

---
+++ DQM/SiPixelHistoricInfoClient/scripts/MakePlots.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+TagName=$1
+First=$2
+
+PlotDir="CurrentPixelPlots"
+
+if [ $3 ]; then
+SiPixelHDQMInspector $TagName $First $3
+else
+SiPixelHDQMInspector $TagName $First
+fi
+mkdir -pv $PlotDir
+mv *.gif $PlotDir
+mv *.eps $PlotDir
+cp diow.pl $PlotDir
+cd $PlotDir
+./diow.pl
+cd ..
+
