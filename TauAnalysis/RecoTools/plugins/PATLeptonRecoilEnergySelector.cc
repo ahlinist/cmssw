ad6d0f79206bced744029720415846b95800f8c1
veelken <>
2009-07-29 13:03:39 +0000

    migrated from TauAnalysis/CandidateTools

---
+++ TauAnalysis/RecoTools/plugins/PATLeptonRecoilEnergySelector.cc
@@ -0,0 +1,9 @@
+#include "TauAnalysis/RecoTools/plugins/PATLeptonRecoilEnergySelector.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(PATTauRecoilEnergyFromJetsSelector);
+DEFINE_FWK_MODULE(PATTauRecoilEnergyFromCaloTowersSelector);
+
+DEFINE_FWK_MODULE(PATTauRecoilEnergyFromJetsRefSelector);
+DEFINE_FWK_MODULE(PATTauRecoilEnergyFromCaloTowersRefSelector);
---
+++ TauAnalysis/RecoTools/python/elecTauPairZeeHypothesis_cff.py
@@ -0,0 +1,68 @@
+import FWCore.ParameterSet.Config as cms
+
+#from SimGeneral.HepPDTESSource.pdt_cfi import *
+
+#--------------------------------------------------------------------------------
+# produce data-formats providing information 
+# about compatibility of an elecron + tau-jet pair
+# with the hypothesis of being a pair of electron,
+# resulting from a Z --> e+ e- decay
+#--------------------------------------------------------------------------------
+
+elecTauPairZeeHypotheses = cms.EDProducer("PATElecTauPairZeeHypothesisProducer",
+    elecTauPairSource = cms.InputTag('selectedElecTauPairsPzetaDiffCumulative'),
+
+    genElectronsFromZsSource = cms.InputTag('genElectronsFromZs'),
+                                          
+    caloJetSource = cms.InputTag('iterativeCone5CaloJets'),
+    pfJetSource = cms.InputTag('iterativeCone5PFJets'),                                       
+    trackSource = cms.InputTag('generalTracks'),
+    gsfElectronSource = cms.InputTag('pixelMatchGsfElectrons'),
+    gsfTrackSource = cms.InputTag('pixelMatchGsfFit'),
+                                          
+    tkminPixelHits = cms.int32(1),
+    tkminTrackerHits = cms.int32(8),	
+    tkmaxChi2 = cms.double(100.),
+
+    dRmatch = cms.double(0.5),
+
+    verbosity = cms.untracked.int32(1)                                      
+)
+
+selectedElecTauPairZeeHypotheses = cms.EDFilter("PATElecTauPairZeeHypothesisSelector",
+     src = cms.InputTag('elecTauPairZeeHypotheses'),
+     cut = cms.string('p4Z0bestMatch.mass > 80. & p4Z0bestMatch.mass < 100.'),
+     filter = cms.bool(False)
+)
+
+elecTauPairZeeHypothesesLooseElectronIsolation = cms.EDProducer("PATElecTauPairZeeHypothesisProducer",
+    elecTauPairSource = cms.InputTag('selectedElecTauPairsPzetaDiffLooseElectronIsolationCumulative'),
+
+    genElectronsFromZsSource = cms.InputTag('genElectronsFromZs'),
+                                          
+    caloJetSource = cms.InputTag('iterativeCone5CaloJets'),
+    pfJetSource = cms.InputTag('iterativeCone5PFJets'),                                       
+    trackSource = cms.InputTag('generalTracks'),
+    gsfElectronSource = cms.InputTag('pixelMatchGsfElectrons'),
+    gsfTrackSource = cms.InputTag('pixelMatchGsfFit'),
+                                          
+    tkminPixelHits = cms.int32(1),
+    tkminTrackerHits = cms.int32(8),	
+    tkmaxChi2 = cms.double(100.),
+
+    dRmatch = cms.double(0.5),
+
+    verbosity = cms.untracked.int32(1)                                      
+)
+
+selectedElecTauPairZeeHypothesesLooseElectronIsolation = cms.EDFilter("PATElecTauPairZeeHypothesisSelector",
+     src = cms.InputTag('elecTauPairZeeHypothesesLooseElectronIsolation'),
+     cut = cms.string('p4Z0bestMatch.mass > 80. & p4Z0bestMatch.mass < 100.'),
+     filter = cms.bool(False)
+)
+
+produceElecTauPairZeeHypotheses = cms.Sequence( elecTauPairZeeHypotheses
+                                               * selectedElecTauPairZeeHypotheses
+                                               * elecTauPairZeeHypothesesLooseElectronIsolation
+                                               * selectedElecTauPairZeeHypothesesLooseElectronIsolation )
+                                                  
