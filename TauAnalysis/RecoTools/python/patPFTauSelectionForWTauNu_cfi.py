66faee45378e1ef6d59f315bd0efe5525b76a2e2
liis <>
2009-08-04 14:19:19 +0000

    First check-in of the cuts for tau selection for WTauNu studies

---
+++ TauAnalysis/RecoTools/python/patPFTauSelectionForWTauNu_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+import copy
+
+from TauAnalysis.RecoTools.patPFTauSelection_cfi import *
+
+selectedLayer1TausForWTauNuEta21 = copy.deepcopy(selectedLayer1TausEta21)
+selectedLayer1TausForWTauNuPt20 = copy.deepcopy(selectedLayer1TausPt20)
+selectedLayer1TausForWTauNuLeadTrk = copy.deepcopy(selectedLayer1TausLeadTrk)
+selectedLayer1TausForWTauNuLeadTrkPt = copy.deepcopy(selectedLayer1TausLeadTrkPt)
+selectedLayer1TausForWTauNuTrkIso = copy.deepcopy(selectedLayer1TausTrkIso)
+selectedLayer1TausForWTauNuMuonVeto = copy.deepcopy(selectedLayer1TausMuonVeto)
+selectedLayer1TausForWTauNuElectronVeto = copy.deepcopy(selectedLayer1TausElectronVeto)
+selectedLayer1TausForWTauNuProng = copy.deepcopy(selectedLayer1TausProng)
