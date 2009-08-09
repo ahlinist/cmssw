df8ed08f73e551571beb5c7c4a826b424fe5dfcf
veelken <>
2009-08-09 16:59:25 +0000

    - added plots of "softElectron" and "softMuon" b-tagging discriminators - restructured handling of configuration parameters for plotting of b-tagging discriminators

--- TauAnalysis/Core/python/jetHistManager_cfi.py
+++ TauAnalysis/Core/python/jetHistManager_cfi.py
@@ -1,22 +1,42 @@
 import FWCore.ParameterSet.Config as cms
 
 jetHistManager = cms.PSet(
-  pluginName = cms.string('jetHistManager'),
-  pluginType = cms.string('JetHistManager'),
+    pluginName = cms.string('jetHistManager'),
+    pluginType = cms.string('JetHistManager'),
       
-  jetSource = cms.InputTag('selectedLayer1JetsAntiOverlapWithLeptonsVetoCumulative'),
+    jetSource = cms.InputTag('selectedLayer1JetsAntiOverlapWithLeptonsVetoCumulative'),
 
-  dqmDirectory_store = cms.string('JetQuantities'),
+    dqmDirectory_store = cms.string('JetQuantities'),
 
-  #requireGenJetMatch = cms.bool(True),
-  requireGenJetMatch = cms.bool(False),
+    #requireGenJetMatch = cms.bool(True),
+    requireGenJetMatch = cms.bool(False),
 
-  centralJetsToBeVetoed = cms.PSet(
-    etMin = cms.vdouble(10., 15., 20., 30.),
-    etaMax = cms.vdouble(2.1, 2.5, 3.0),
-    alphaMin = cms.vdouble(0., 0.1, 0.2, 0.3, 0.4, 0.5)
-  ),
+    centralJetsToBeVetoed = cms.PSet(
+        etMin = cms.vdouble(10., 15., 20., 30.),
+        etaMax = cms.vdouble(2.1, 2.5, 3.0),
+        alphaMin = cms.vdouble(0., 0.1, 0.2, 0.3, 0.4, 0.5)
+    ),
   
-  btaggingAlgos = cms.vstring('simpleSecondaryVertex','combinedSecondaryVertex','trackCountingHighEffBJetTags'),
-  discriminators = cms.vdouble(2.,0.4,2.5)
+    bTaggingDiscriminators = cms.VPSet(
+        cms.PSet(
+            name = cms.string('simpleSecondaryVertex'),
+            threshold = cms.double(2.)
+        ),
+        cms.PSet(
+            name = cms.string('combinedSecondaryVertex'),
+            threshold = cms.double(0.4)
+        ),
+        cms.PSet(
+            name = cms.string('trackCountingHighEffBJetTags'),
+            threshold = cms.double(2.5)
+        ),
+        cms.PSet(
+            name = cms.string('softElectronBJetTags'),
+            threshold = cms.double(0.5)
+        ),
+        cms.PSet(
+            name = cms.string('softMuonBJetTags'),
+            threshold = cms.double(0.5)
+        )
+    )
 )
