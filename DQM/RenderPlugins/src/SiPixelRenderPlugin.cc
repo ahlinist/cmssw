83d8c575233a1ab74b45caebdbcbf54429844add
merkelp <>
2009-08-10 14:09:46 +0000

    *** empty log message ***

--- DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
+++ DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
@@ -2,8 +2,8 @@
   \file SiPixelRenderPlugin
   \brief Display Plugin for Pixel DQM Histograms
   \author P.Merkel
-  \version $Revision: 1.14 $
-  \date $Date: 2009/07/28 10:59:25 $
+  \version $Revision: 1.15 $
+  \date $Date: 2009/07/28 11:42:32 $
 */
 
 #include "VisMonitoring/DQMServer/interface/DQMRenderPlugin.h"
@@ -90,7 +90,8 @@ private:
 
       if( o.name.find( "reportSummaryMap" ) != std::string::npos )
       {
-        gPad->SetLeftMargin(0.3);
+        if(obj->GetNbinsX()==7) gPad->SetLeftMargin(0.3);
+	if(obj->GetNbinsX()==40){TLine tl1; tl1.DrawLine(32.,1.,32.,37.); TLine tl2; tl2.DrawLine(32.,27.,40.,27.); }  
         dqm::utils::reportSummaryMapPalette(obj2);
         return;
       }
