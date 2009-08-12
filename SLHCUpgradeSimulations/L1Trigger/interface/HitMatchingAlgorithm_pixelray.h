6050ec9c6fc3359faebf84772bf9b9c93c83d088
khenriks <>
2009-08-12 20:28:49 +0000

    Fixel pixelray hit matching algorithm to support clustering

--- SLHCUpgradeSimulations/L1Trigger/interface/HitMatchingAlgorithm_pixelray.h
+++ SLHCUpgradeSimulations/L1Trigger/interface/HitMatchingAlgorithm_pixelray.h
@@ -39,64 +39,59 @@ namespace cmsUpgrades{
 template <typename T>
 class HitMatchingAlgorithm_pixelray : public HitMatchingAlgorithm<T>
 {
-    public:
-        HitMatchingAlgorithm_pixelray(const cmsUpgrades::StackedTrackerGeometry *i,
-                                   double aCompatibilityScalingFactor, double aIPWidth) :
-            cmsUpgrades::HitMatchingAlgorithm<T>(i),
-			mClassInfo( new cmsUpgrades::classInfo(__PRETTY_FUNCTION__) ),
-			mCompatibilityScalingFactor(aCompatibilityScalingFactor),
-			mIPWidth(aIPWidth)
-		{
-		    
-		}
-		
-        ~HitMatchingAlgorithm_pixelray()
-        {
+public:
+    HitMatchingAlgorithm_pixelray(const cmsUpgrades::StackedTrackerGeometry *i,
+                                  double aCompatibilityScalingFactor, double aIPWidth) :
+        cmsUpgrades::HitMatchingAlgorithm<T>(i),
+        mClassInfo( new cmsUpgrades::classInfo(__PRETTY_FUNCTION__) ),
+        mCompatibilityScalingFactor(aCompatibilityScalingFactor),
+        mIPWidth(aIPWidth)
+    {
+    }
+        
+    ~HitMatchingAlgorithm_pixelray()
+    {
+    }
+
+    bool CheckTwoMemberHitsForCompatibility(const cmsUpgrades::LocalStub<T> & aLocalStub) const
+    {
+        std::pair<double,double> * rayEndpoints;
+        
+        // Just call the helper function to do all the work
+        
+        rayEndpoints = getPixelRayEndpoints(aLocalStub,
+            HitMatchingAlgorithm<T>::theStackedTracker, mCompatibilityScalingFactor);
             
+        if (rayEndpoints) {
+            // Establish the valid window
+            double positiveZBoundary = mIPWidth/2;
+            double negativeZBoundary = -mIPWidth/2;
+            
+            if ((rayEndpoints->second > negativeZBoundary) &&
+                (rayEndpoints->first < positiveZBoundary))
+            {
+                delete rayEndpoints;
+                
+                return true;
+            }
         }
-
-        /* WARNING: This only works without clusters, i.e. with one hit in the cluster.
-         * It needs to be fixed for clustering
-         */
-		bool CheckTwoMemberHitsForCompatibility(const cmsUpgrades::LocalStub<T> & aLocalStub) const
-		{
-			std::vector<double> * rayEndpoints;
-			
-			// Just call the helper function to do all the work
-			
-			rayEndpoints = getPixelRayEndpoints(aLocalStub,
-				HitMatchingAlgorithm<T>::theStackedTracker, mCompatibilityScalingFactor);
-			
-			if (rayEndpoints) {
-				// Establish the valid window
-                double positiveZBoundary = mIPWidth/2;
-                double negativeZBoundary = -mIPWidth/2;
-				
-				if ((rayEndpoints->at(1) > negativeZBoundary) &&
-			        (rayEndpoints->at(0) < positiveZBoundary))
-			    {
-					delete rayEndpoints;
-					
-					return true;
-				}
-			}
-
-			delete rayEndpoints;
-			
-			return false;
-		}
-
-		std::string AlgorithmName() const
-		{ 
-			return mClassInfo->FunctionName() + "<" +
-			    mClassInfo->TemplateTypes().begin()->second + ">";
-		}
-
-	private:
-		const cmsUpgrades::classInfo *mClassInfo;
-
-		double mCompatibilityScalingFactor;
-		double mIPWidth;
+        
+        delete rayEndpoints;
+        
+        return false;
+    }
+    
+    std::string AlgorithmName() const
+    { 
+        return mClassInfo->FunctionName() + "<" +
+               mClassInfo->TemplateTypes().begin()->second + ">";
+    }
+    
+private:
+    const cmsUpgrades::classInfo *mClassInfo;
+    
+    double mCompatibilityScalingFactor;
+    double mIPWidth;
 
 };
 
@@ -105,48 +100,48 @@ class HitMatchingAlgorithm_pixelray : public HitMatchingAlgorithm<T>
 // Declare the algorithm to the framework
 
 template <typename T>
-class  ES_HitMatchingAlgorithm_pixelray : public edm::ESProducer{
-	public:
-		ES_HitMatchingAlgorithm_pixelray(const edm::ParameterSet & p) :
-		    mPtThreshold(p.getParameter<double>("minPtThreshold")),
-			mIPWidth(p.getParameter<double>("ipWidth"))
-		{
-		    setWhatProduced(this);
-		}
-
-		virtual ~ES_HitMatchingAlgorithm_pixelray()
-		{
-		    
-		}
-
-		boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> >
-		produce(const cmsUpgrades::HitMatchingAlgorithmRecord & record)
-		{ 
-			edm::ESHandle<MagneticField> magnet;
-			record.getRecord<IdealMagneticFieldRecord>().get(magnet);
-			double mMagneticFieldStrength = magnet->inTesla(GlobalPoint(0,0,0)).z();
-			double mCompatibilityScalingFactor = (100.0 * 2.0e+9 * mPtThreshold) /
-			    (cmsUpgrades::KGMS_C * mMagneticFieldStrength);
-			// Invert so we use multiplication instead of division in the comparison
-			mCompatibilityScalingFactor = 1.0 / mCompatibilityScalingFactor;
-
-			edm::ESHandle<cmsUpgrades::StackedTrackerGeometry> StackedTrackerGeomHandle;
-			record.getRecord<cmsUpgrades::StackedTrackerGeometryRecord>().get(StackedTrackerGeomHandle);
+class  ES_HitMatchingAlgorithm_pixelray : public edm::ESProducer
+{
+public:
+    ES_HitMatchingAlgorithm_pixelray(const edm::ParameterSet & p) :
+        mPtThreshold(p.getParameter<double>("minPtThreshold")),
+        mIPWidth(p.getParameter<double>("ipWidth"))
+    {
+        setWhatProduced(this);
+    }
+    
+    virtual ~ES_HitMatchingAlgorithm_pixelray()
+    {
+    }
+    
+    boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> >
+    produce(const cmsUpgrades::HitMatchingAlgorithmRecord & record)
+    { 
+        edm::ESHandle<MagneticField> magnet;
+        record.getRecord<IdealMagneticFieldRecord>().get(magnet);
+        double mMagneticFieldStrength = magnet->inTesla(GlobalPoint(0,0,0)).z();
+        double mCompatibilityScalingFactor = (100.0 * 2.0e+9 * mPtThreshold) /
+            (cmsUpgrades::KGMS_C * mMagneticFieldStrength);
+        // Invert so we use multiplication instead of division in the comparison
+        mCompatibilityScalingFactor = 1.0 / mCompatibilityScalingFactor;
+        
+        edm::ESHandle<cmsUpgrades::StackedTrackerGeometry> StackedTrackerGeomHandle;
+        record.getRecord<cmsUpgrades::StackedTrackerGeometryRecord>().get(StackedTrackerGeomHandle);
   
-			cmsUpgrades::HitMatchingAlgorithm<T>* HitMatchingAlgo =
-			    new cmsUpgrades::HitMatchingAlgorithm_pixelray<T>(&(*StackedTrackerGeomHandle),
-			                                                            mCompatibilityScalingFactor,
-			                                                            mIPWidth);
+        cmsUpgrades::HitMatchingAlgorithm<T>* HitMatchingAlgo =
+            new cmsUpgrades::HitMatchingAlgorithm_pixelray<T>(&(*StackedTrackerGeomHandle),
+                                                              mCompatibilityScalingFactor,
+                                                              mIPWidth);
 
-			_theAlgo = boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> >(HitMatchingAlgo);
+        _theAlgo = boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> >(HitMatchingAlgo);
 
-			return _theAlgo;
-		} 
-
-	private:
-		boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> > _theAlgo;
-		double mPtThreshold;
-		double mIPWidth;
+        return _theAlgo;
+    } 
+    
+private:
+    boost::shared_ptr< cmsUpgrades::HitMatchingAlgorithm<T> > _theAlgo;
+    double mPtThreshold;
+    double mIPWidth;
 };
 
 #endif
