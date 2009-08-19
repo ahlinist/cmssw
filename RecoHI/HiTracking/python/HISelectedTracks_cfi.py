2fc4b99c4d905a341740885b30c0182ee1c1f0e6
edwenger <>
2009-08-19 08:47:42 +0000

    reorganization of tracking configurations

---
+++ RecoHI/HiTracking/python/HISelectedTracks_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+# reco track quality cuts (dca, nhits, prob) and minpt selection
+hiSelectedTracks = cms.EDFilter("TrackSelector",
+  src = cms.InputTag("hiGlobalPrimTracks"),
+  cut = cms.string('pt > 0.9 && d0/d0Error<3 && recHitsSize>12 && chi2prob(chi2,ndof)>0.01')
+)
