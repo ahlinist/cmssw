8d3abc4adf8c195dc43f6b1953190d75337e5432
cerati <>
2009-08-24 13:04:40 +0000

    added function addTCMet

--- TauAnalysis/Configuration/python/tools/metTools.py
+++ TauAnalysis/Configuration/python/tools/metTools.py
@@ -24,9 +24,18 @@ def addPFMet(process,redoGenMet=True,correct=False):
     process.allLayer1Objects.replace(process.layer1METs,
                                      process.layer1METs + process.layer1PFMETs)
 
+def addTCMet(process,):
+    process.layer1TCMETs = process.layer1METs.clone()
+    process.layer1TCMETs.addMuonCorrections = False
+    process.layer1TCMETs.addTrigMatch = False
+    process.layer1TCMETs.metSource = cms.InputTag('tcMet')
+    process.layer1TCMETs.genMETSource = cms.InputTag('genMETWithMu')
+    process.allLayer1Objects.replace(process.layer1METs,
+                                     process.layer1METs + process.layer1TCMETs)
+
 def replaceMETforDiTaus(process,
-                        oldMet = cms.InputTag('layer1METs'),
-                        newMet = cms.InputTag('layer1PFMETs') ):
+                        oldMet,# = cms.InputTag('layer1METs'),
+                        newMet):# = cms.InputTag('layer1PFMETs') ):
     massSearchReplaceParam(process.produceDiTauPairsAllKinds,
                            'srcMET', oldMet, newMet)
 
