691e64240bc155998628b5b9a6ecd1d1222907f1
veelken <>
2009-08-28 13:12:43 +0000

    added histogram manager to monitor event weights

---
+++ TauAnalysis/Core/python/eventWeightHistManager_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+eventWeightHistManager = cms.PSet(
+  pluginName = cms.string('eventWeightHistManager'),
+  pluginType = cms.string('EventWeightHistManager'),
+
+  dqmDirectory_store = cms.string('EventWeights')
+)
