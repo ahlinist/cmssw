4f92f0eeda8b2c348ba27e7e4922b639dc7b2395
veelken <>
2009-08-28 13:25:33 +0000

    - loosened tau track and ECAL isolation criteria (to less than 8 GeV) - do not apply tau 1||3-prong cut in preselection

--- TauAnalysis/BgEstimationTools/python/bgEstPreselZtoMuTau_cff.py
+++ TauAnalysis/BgEstimationTools/python/bgEstPreselZtoMuTau_cff.py
@@ -26,17 +26,36 @@ muonTrkCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
     minNumber = cms.uint32(1)
 )
 
-tauProngCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
-    pluginName = cms.string("tauProngCutBgEstPreselection"),
+tausBgEstPreselLooseTrkIso = cms.EDFilter("PATTauSelector",
+    src = cms.InputTag('selectedLayer1TausLeadTrkPtCumulative'),    
+    cut = cms.string('tauID("trackIsolation") > 0.5 | chargedParticleIso < 8.'),
+    filter = cms.bool(False)                                   
+)
+
+tausBgEstPreselLooseEcalIso = cms.EDFilter("PATTauSelector",
+    src = cms.InputTag('tausBgEstPreselLooseTrkIso'),    
+    cut = cms.string('tauID("ecalIsolation") > 0.5 | gammaParticleIso < 8.'),
+    filter = cms.bool(False)                                   
+)
+
+tausBgEstPreselection = cms.EDFilter("PATTauSelector",
+    src = cms.InputTag('tausBgEstPreselLooseEcalIso'),    
+    srcNotToBeFiltered = cms.InputTag('muonsBgEstPreselection'),                                        
+    dRmin = cms.double(0.7),                       
+    filter = cms.bool(False)
+)
+
+tauAntiOverlapWithMuonsVetoBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
+    pluginName = cms.string("tauAntiOverlapWithMuonsVetoBgEstPreselection"),
     pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('selectedLayer1TausProngCumulative'),
+    src = cms.InputTag('tausBgEstPreselection'),
     minNumber = cms.uint32(1)                                                
 )
 
 muTauPairsBgEstPreselection = cms.EDProducer("PATMuTauPairProducer",
     useLeadingTausOnly = cms.bool(False),
     srcLeg1 = cms.InputTag('muonsBgEstPreselection'),
-    srcLeg2 = cms.InputTag('selectedLayer1TausProngCumulative'),
+    srcLeg2 = cms.InputTag('tausBgEstPreselection'),
     dRmin12 = cms.double(0.7),
     srcMET = cms.InputTag('layer1METs'),
     recoMode = cms.string(""),
@@ -52,8 +71,9 @@ muTauPairCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
 
 produceBoolEventSelFlags = cms.Sequence(
     muonsBgEstPreselection + muonTrkCutBgEstPreselection
-   +tauProngCutBgEstPreselection
-   +muTauPairsBgEstPreselection + muTauPairCutBgEstPreselection
+   + tausBgEstPreselLooseTrkIso + tausBgEstPreselLooseEcalIso
+   + tausBgEstPreselection + tauAntiOverlapWithMuonsVetoBgEstPreselection
+   + muTauPairsBgEstPreselection + muTauPairCutBgEstPreselection
 )
 
 selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
@@ -63,7 +83,7 @@ selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
         cms.InputTag('primaryEventVertexQuality'),
         cms.InputTag('primaryEventVertexPosition'),
         cms.InputTag('muonTrkCutBgEstPreselection'),
-        cms.InputTag('tauProngCutBgEstPreselection'),
+        cms.InputTag('tauAntiOverlapWithMuonsVetoBgEstPreselection'),
         cms.InputTag('muTauPairCutBgEstPreselection')
     )
 )
@@ -75,5 +95,5 @@ from TauAnalysis.RecoTools.patJetSelection_cff import *
 selectedLayer1JetsAntiOverlapWithLeptonsVeto.srcNotToBeFiltered = cms.VInputTag(
     "selectedLayer1ElectronsTrkIPcumulative",
     "muonsBgEstPreselection",
-    "selectedLayer1TausProngCumulative"
+    "tausBgEstPreselection"
 )
