bdcdb25e73da4761284baf20f9cb720522dfa9c6
veelken <>
2009-09-01 07:03:12 +0000

    - moved production and selection of electron, muon, tau and jet collections   specific to data-driven background estimation methods into separate cfi and cff files - introduced separate preselections for Zmumu, WplusJets, TTplusJets and QCD   enriched samples (in order to increase efficiency for individual background processes)

--- TauAnalysis/BgEstimationTools/python/bgEstPreselZtoMuTau_cff.py
+++ TauAnalysis/BgEstimationTools/python/bgEstPreselZtoMuTau_cff.py
@@ -13,68 +13,38 @@ genPhaseSpaceFilter = cms.EDFilter("EventSelPluginFilter",
     )
 )
 
-muonsBgEstPreselection = cms.EDFilter("PATMuonSelector",
-    src = cms.InputTag('selectedLayer1MuonsEcalIsoLooseIsolationCumulative'),                                        
-    cut = cms.string('innerTrack.isNonnull'),
-    filter = cms.bool(False)
-)
-
-muonTrkCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
-    pluginName = cms.string("muonTrkCutBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('muonsBgEstPreselection'),
-    minNumber = cms.uint32(1)
-)
-
-tausBgEstPreselLooseTrkIso = cms.EDFilter("PATTauSelector",
-    src = cms.InputTag('selectedLayer1TausLeadTrkPtCumulative'),    
-    cut = cms.string('tauID("trackIsolation") > 0.5 | chargedParticleIso < 8.'),
-    filter = cms.bool(False)                                   
-)
-
-tausBgEstPreselLooseEcalIso = cms.EDFilter("PATTauSelector",
-    src = cms.InputTag('tausBgEstPreselLooseTrkIso'),    
-    cut = cms.string('tauID("ecalIsolation") > 0.5 | gammaParticleIso < 8.'),
-    filter = cms.bool(False)                                   
-)
-
-tausBgEstPreselection = cms.EDFilter("PATTauAntiOverlapSelector",
-    src = cms.InputTag('tausBgEstPreselLooseEcalIso'),    
-    srcNotToBeFiltered = cms.VInputTag('muonsBgEstPreselection'),                                        
-    dRmin = cms.double(0.7),                       
-    filter = cms.bool(False)
-)
-
-tauAntiOverlapWithMuonsVetoBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
-    pluginName = cms.string("tauAntiOverlapWithMuonsVetoBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('tausBgEstPreselection'),
-    minNumber = cms.uint32(1)                                                
-)
-
-muTauPairsBgEstPreselection = cms.EDProducer("PATMuTauPairProducer",
-    useLeadingTausOnly = cms.bool(False),
-    srcLeg1 = cms.InputTag('muonsBgEstPreselection'),
-    srcLeg2 = cms.InputTag('tausBgEstPreselection'),
-    dRmin12 = cms.double(0.7),
-    srcMET = cms.InputTag('layer1METs'),
-    recoMode = cms.string(""),
-    verbosity = cms.untracked.int32(0)
-)
-
-muTauPairCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
+muTauPairCutBgEstPreselection = cms.EDProducer("BoolEventSelFlagProducer",
     pluginName = cms.string("muTauPairCutBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('muTauPairsBgEstPreselection'),
-    minNumber = cms.uint32(1)
-)                                                                             
+    pluginType = cms.string("OrEventSelector"),
+    selectors = cms.VPSet(
+        cms.PSet(
+            pluginName = cms.string("muTauPairCutBgEstPreselZmumu"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('muTauPairsForBgEstZmumuEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("muTauPairCutBgEstPreselWplusJets"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('muTauPairsForBgEstWplusJetsEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("muTauPairCutBgEstPreselTTplusJets"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('muTauPairsForBgEstTTplusJetsEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("muTauPairCutBgEstPreselQCD"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('muTauPairsForBgEstQCDenriched'),
+            minNumber = cms.uint32(1)
+        )
+    )
+) 
 
-produceBoolEventSelFlags = cms.Sequence(
-    muonsBgEstPreselection + muonTrkCutBgEstPreselection
-   + tausBgEstPreselLooseTrkIso + tausBgEstPreselLooseEcalIso
-   + tausBgEstPreselection + tauAntiOverlapWithMuonsVetoBgEstPreselection
-   + muTauPairsBgEstPreselection + muTauPairCutBgEstPreselection
-)
+produceBoolEventSelFlags = cms.Sequence( muTauPairCutBgEstPreselection )
 
 selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
     flags = cms.VInputTag(
@@ -82,18 +52,6 @@ selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
         cms.InputTag('primaryEventVertex'),
         cms.InputTag('primaryEventVertexQuality'),
         cms.InputTag('primaryEventVertexPosition'),
-        cms.InputTag('muonTrkCutBgEstPreselection'),
-        cms.InputTag('tauAntiOverlapWithMuonsVetoBgEstPreselection'),
         cms.InputTag('muTauPairCutBgEstPreselection')
     )
-)
-
-# exclude loosely selected muons and tau-jets from jets collections
-# (otherwise, non-isolated muons may get double-counted,
-#  once in the muon collection plus once in the jet collection, for example)
-from TauAnalysis.RecoTools.patJetSelection_cff import *
-selectedLayer1JetsAntiOverlapWithLeptonsVeto.srcNotToBeFiltered = cms.VInputTag(
-    "selectedLayer1ElectronsTrkIPcumulative",
-    "muonsBgEstPreselection",
-    "tausBgEstPreselection"
 )
