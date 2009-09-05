cf948438f4357867373a70460c8bd95e45d3fc1a
liis <>
2009-09-05 16:38:17 +0000

    first version

---
+++ TauAnalysis/Configuration/test/makeWtoTauNuPlots_cfg.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('makeWtoTauNuPlots')
+
+process.load("TauAnalysis.Configuration.dumpWtoTauNu_cff")
+process.load("TauAnalysis.Configuration.plotWtoTauNu_cff")
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.maxEvents = cms.untracked.PSet(            
+    input = cms.untracked.int32(0)         
+)
+
+process.source = cms.Source("EmptySource")
+
+# import utility function to enable factorization
+#from TauAnalysis.Configuration.factorizationTools import enableFactorization_makeWtoTauNuPlots
+#enableFactorization_makeWtoTauNuPlots(process)
+
+process.makeWtoTauNuPlots = cms.Sequence( process.loadWtoTauNu
+                                         +process.addWtoTauNu
+                                         +process.saveWtoTauNu
+                                         +process.dumpWtoTauNu
+                                         +process.plotWtoTauNu )
+
+process.p = cms.Path(process.makeWtoTauNuPlots)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
