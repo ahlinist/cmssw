9fa15a3fd6d5718ee34f7eccf6e4f6782dc90212
merkelp <>
2009-09-18 15:14:52 +0000

    *** empty log message ***

--- DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
+++ DQM/RenderPlugins/src/SiPixelRenderPlugin.cc
@@ -2,8 +2,8 @@
   \file SiPixelRenderPlugin
   \brief Display Plugin for Pixel DQM Histograms
   \author P.Merkel
-  \version $Revision: 1.16 $
-  \date $Date: 2009/08/10 14:07:24 $
+  \version $Revision: 1.15 $
+  \date $Date: 2009/07/28 11:42:32 $
 */
 
 #include "VisMonitoring/DQMServer/interface/DQMRenderPlugin.h"
@@ -12,10 +12,15 @@
 #include "TProfile2D.h"
 #include "TStyle.h"
 #include "TCanvas.h"
+#include "TGraphPolar.h"
 #include "TColor.h"
 #include "TText.h"
 #include "TLine.h"
 #include <cassert>
+
+#define PI_12 0.261799
+#define PI    3.141592
+#define PI_2  1.570796
 
 class SiPixelRenderPlugin : public DQMRenderPlugin
 {
@@ -45,13 +50,144 @@ public:
     {
       c->cd();
 
-      if( dynamic_cast<TH1*>( o.object ) )
+      if( dynamic_cast<TH2*>( o.object ) )
+      {
+        postDrawTH2( c, o );
+      }
+      else if( dynamic_cast<TH1*>( o.object ) )
       {
         postDrawTH1( c, o );
       }
     }
 
 private:
+/*
+void paletteGraph(Double_t min, Double_t max){
+	gStyle->SetPalette(1);
+	gStyle->SetOptStat(0);
+	gStyle->SetNumberContours(255);
+	TCanvas* pal = new TCanvas("Palette", "Palette", 150, 600);
+	TH2F* palet = new TH2F("Palette", "Palette", 1, 0, 1, 255, min, max);
+	Double_t dc = (max - min) / 255;
+	for(Int_t i = 0; i < 255; i++)
+		palet->SetBinContent(1, i + 1, min + i * dc);
+	palet->Draw("COL");
+	gPad->SetLeftMargin(0.5);
+	palet->SetLabelSize(0.2,"Y");
+	palet->SetLabelOffset(0.05, "Y");
+	palet->SetLabelOffset(99, "X");
+	palet->SetTitle("");
+	palet->SetTickLength(0,"X");
+	palet->SetLabelSize(0,"X");
+	gPad->SetGrid(false,false);
+}
+*/
+Int_t getColor(TH2F* fH, Int_t i, Int_t j, Double_t zmin, Double_t zmax){
+	Int_t ncolors  = gStyle->GetNumberOfColors();
+	
+	Double_t zc;
+    	Double_t dz = zmax - zmin;
+	if(dz <= 0)
+		return -1;
+  
+	Int_t ndiv = fH->GetContour();
+	if (ndiv == 0 ) {
+		ndiv = gStyle->GetNumberContours();
+		fH->SetContour(ndiv);
+	}
+
+	Int_t ndivz  = TMath::Abs(ndiv);
+
+	if (fH->TestBit(TH1::kUserContour) == 0)
+		fH->SetContour(ndiv);
+
+	Double_t scale = ndivz/dz;
+	Int_t color;
+	const Double_t z = fH->GetBinContent(i,j);
+	
+	if (z < zmin.)
+		return -1;	
+	if (fH->TestBit(TH1::kUserContour)) {
+		zc = fH->GetContourLevelPad(0);
+		if (z < zc)
+			return -1;
+		color = -1;
+		for (Int_t k=0; k<ndiv; k++) {
+			zc = fH->GetContourLevelPad(k);
+			if (z < zc)
+				continue;
+			else
+				color++;
+		}
+	} else
+		color = Int_t(0.01+(z-zmin)*scale);
+
+	Int_t theColor = Int_t((color+0.99)*Double_t(ncolors)/Double_t(ndivz));
+	if (z >= zmax)
+		theColor = ncolors-1;
+	return theColor;
+}
+
+void polarGraph(TCanvas *c, TH2F* map){
+	TGraphPolar *graphs[24][7];
+	TColor *colors = new TColor();
+
+	colors->SetPalette(1, 0);
+	gStyle->SetPalette(1);
+	gStyle->SetNumberContours(255);
+
+		for(Int_t i = 0 ; i < 24; i++)
+			for(Int_t j = 0 ; j < 7; j++)
+			{
+				graphs[i][j] = makeSlice(j + 1., j + 1.9999, PI_2-(i+1)*PI_12, PI_2-(i)*PI_12,
+						colors->GetColorPalette(getColor(map,
+i + 1, j + 1, map->GetMinimum(), map->GetMaximum())), 5,
+						map->GetName());
+				graphs[i][j]->Draw("F same");
+			}
+		c->Update();
+		graphs[0][0]->GetPolargram()->SetToRadian();
+		graphs[0][0]->SetMaximum(8);
+		graphs[0][0]->GetPolargram()->SetNdivPolar(24);
+		graphs[0][0]->GetPolargram()->SetNdivRadial(8);
+	
+		Int_t cnt(7);
+
+		for(Int_t i = 0 ; i < 6 ; i++)
+			graphs[0][0]->GetPolargram()->SetPolarLabel(i,Form("%d", cnt--));
+		for(Int_t i = 6 ; i < 18 ; i++)
+			graphs[0][0]->GetPolargram()->SetPolarLabel(i,Form("%d", i-5));
+		cnt = 12;
+		for(Int_t i = 19 ; i < 24 ; i++)
+			graphs[0][0]->GetPolargram()->SetPolarLabel(i,Form("%d", cnt--));
+
+}
+
+
+TGraphPolar* makeSlice(Double_t rA, Double_t rB, Double_t phiA, Double_t phiB, Int_t
+color, Int_t npts, std::string title){
+	Double_t *r = new Double_t[2*npts+3];
+	Double_t *th = new Double_t[2*npts+3];
+	
+	r[0] = rA;
+	th[0] = phiA;
+	for(Int_t i = 0 ; i <= npts ; i++){
+		r[i + 1] = rB;
+		th[i + 1] = i * (phiB - phiA) / npts + phiA;
+		r[npts+2+i] = rA;
+		th[npts+2+i] = phiB - i * (phiB - phiA) / npts;
+	}	
+	
+	
+	TGraphPolar *grP = new TGraphPolar(2*npts+3,th,r);
+	
+	grP->SetTitle(title.c_str());
+	grP->SetFillColor(color);
+	grP->SetLineColor(color);
+	grP->SetLineWidth(2);
+	return grP;
+}
+
   void preDrawTH2( TCanvas *, const DQMNet::CoreObject &o )
     {
       TH2* obj = dynamic_cast<TH2*>( o.object );
@@ -79,12 +215,14 @@ private:
       if( o.name.find( "endcapOccupancyMap" ) != std::string::npos ) obj->SetTitle("Endcap Digi Occupancy Map");
       if( o.name.find( "hitmap" ) != std::string::npos  ||
           o.name.find( "Occupancy" ) != std::string::npos ||
-	  o.name.find( "OnTrack/position_siPixelClusters_" ) != std::string::npos )
+	  o.name.find( "position_siPixelClusters" ) != std::string::npos ||
+	  (o.name.find( "TRKMAP" ) != std::string::npos && o.name.find( "Layer" ) != std::string::npos))
       {
         gStyle->SetPalette(1);
         obj->SetOption("colz");
         return;
       }
+      
       
       
       TH2F* obj2 = dynamic_cast<TH2F*>( o.object );
@@ -217,6 +355,22 @@ private:
       }
       
     }
+
+  void postDrawTH2( TCanvas *c, const DQMNet::CoreObject &o )
+{
+
+      TH2* obj = dynamic_cast<TH2*>( o.object );
+      assert( obj );			  	
+      if( o.name.find( "TRKMAP" ) != std::string::npos && o.name.find( "Disc" ) != std::string::npos ){
+        c->Clear();
+	polarGraph(c, dynamic_cast<TH2F*>(obj));
+      }
+
+
+
+
+}
+
 };
 
 static SiPixelRenderPlugin instance;
