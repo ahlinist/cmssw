e1d8bd9632f66bb04dbded861e9397802d3d8bf5
sunil <>
2009-09-22 12:29:31 +0000

    *** empty log message ***

--- TauAnalysis/BgEstimationTools/test/submitBgEstSkimZtoElecMu.py
+++ TauAnalysis/BgEstimationTools/test/submitBgEstSkimZtoElecMu.py
@@ -5,7 +5,7 @@ from TauAnalysis.BgEstimationTools.makeReplacementsBgEstSkim import makeReplacem
 
 # name of the directory (either on afs area or castor)
 # to which all .root files produced by the cmsRun job will be copied
-outputDirectory = "/castor/cern.ch/user/v/veelken/bgEstSkim/ZtoElecMu/"
+outputDirectory = "/castor/cern.ch/user/s/sunil/bgEstSkim/ZtoElecMu/"
 
 # small cmsRun job for testing purposes...
 #submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Ztautau_part01",
@@ -25,7 +25,8 @@ outputDirectory = "/castor/cern.ch/user/v/veelken/bgEstSkim/ZtoElecMu/"
 #--------------------------------------------------------------------------------
 
 # Z --> tau tau jobs
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Ztautau",
+for i in range(3):
+    submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Ztautau_part%(i)02d" % {"i" : (i + 1)},
               replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
               job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
 
@@ -35,16 +36,17 @@ submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample
               job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
 
 # Z --> mu mu jobs
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Zmumu",
+for i in range(2):
+    submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Zmumu_part%(i)02d" % {"i" : (i + 1)},
               replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
               job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
 
 # pp --> mu X QCD jobs
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "InclusivePPmuX",
-              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
-              job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
+#submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "InclusivePPmuX",
+#              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
+#              job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
 
-for i in range(21):
+for i in range(52):
     submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "PPmuXptGt20_part%(i)02d" % {"i" : (i + 1)},
                   replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
                   job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
@@ -53,31 +55,33 @@ for i in range(21):
 # (not yet skimmed...)
 
 # QCD_EMenriched jobs
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "QCD_BCtoE_Pt20to30",
-              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
-              job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "QCD_BCtoE_Pt30to80",
-              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
-              job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
+#submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "QCD_BCtoE_Pt20to30",
+#              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
+#              job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
+#submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "QCD_BCtoE_Pt30to80",
+#              replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
+#              job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 
 # W/Z + jets jobs
-for i in range(2):
+for i in range(5):
     submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "WplusJets_part%(i)02d" % {"i" : (i + 1)},
                   replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
                   job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
-
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZeePlusJets",
+for i in range(2):
+    submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZeePlusJets_part%(i)02d" % {"i" : (i + 1)},
               replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
               job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZmumuPlusJets",
+for i in range(2):
+   submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZmumuPlusJets_part%(i)02d" % {"i" : (i + 1)},
               replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
               job = "bgEstSkim", queue = "1nw", outputDirectory = outputDirectory)
-submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZtautauPlusJets",
+for i in range(2):
+   submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "ZtautauPlusJets_part%(i)02d" % {"i" : (i + 1)},
               replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
               job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
 
 # TT + jets  jobs
-for i in range(9):
+for i in range(13):
     submitToBatch(configFile = "skimZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "TTplusJets_part%(i)02d" % {"i" : (i + 1)},
                   replFunction = makeReplacementsBgEstSkim, replacements = "maxEvents = -1",
                   job = "bgEstSkim", queue = "1nd", outputDirectory = outputDirectory)
