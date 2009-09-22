92d40f9e90f3eeda14808b0b7dab75cd088dac9a
sunil <>
2009-09-22 12:27:13 +0000

    modified according to latest skimmed samples

--- TauAnalysis/BgEstimationTools/python/bgEstPreselZtoElecMu_cff.py
+++ TauAnalysis/BgEstimationTools/python/bgEstPreselZtoElecMu_cff.py
@@ -13,54 +13,38 @@ genPhaseSpaceFilter = cms.EDFilter("EventSelPluginFilter",
     )
 )
 
-electronsBgEstPreselection = cms.EDFilter("PATElectronSelector",
-    src = cms.InputTag('selectedLayer1ElectronsEcalIsoLooseIsolationCumulative'),                                        
-    cut = cms.string('gsfTrack.isNonnull'),
-    filter = cms.bool(False)
-)
-
-electronTrkCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
-    pluginName = cms.string("electronTrkCutBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('electronsBgEstPreselection'),
-    minNumber = cms.uint32(1)
-)
-
-muonsBgEstPreselection = cms.EDFilter("PATMuonSelector",
-    src = cms.InputTag('selectedLayer1MuonsEcalIsoLooseIsolationCumulative'),                                        
-    cut = cms.string('innerTrack.isNonnull'),
-    filter = cms.bool(False)
-)
-
-muonTrkCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
-    pluginName = cms.string("muonTrkCutBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('muonsBgEstPreselection'),
-    minNumber = cms.uint32(1)
-)
-
-elecMuPairsBgEstPreselection = cms.EDProducer("PATElecMuPairProducer",
-    useLeadingTausOnly = cms.bool(False),
-    srcLeg1 = cms.InputTag('electronsBgEstPreselection'),
-    srcLeg2 = cms.InputTag('muonsBgEstPreselection'),
-    dRmin12 = cms.double(0.7),
-    srcMET = cms.InputTag('layer1METs'),
-    recoMode = cms.string(""),
-    verbosity = cms.untracked.int32(0)
-)
-
-elecMuPairCutBgEstPreselection = cms.EDFilter("BoolEventSelFlagProducer",
+elecMuPairCutBgEstPreselection = cms.EDProducer("BoolEventSelFlagProducer",
     pluginName = cms.string("elecMuPairCutBgEstPreselection"),
-    pluginType = cms.string("PATCandViewMinEventSelector"),
-    src = cms.InputTag('elecMuPairsBgEstPreselection'),
-    minNumber = cms.uint32(1)
-)                                                                             
+    pluginType = cms.string("OrEventSelector"),
+    selectors = cms.VPSet(
+        cms.PSet(
+            pluginName = cms.string("elecMuPairCutBgEstPreselZmumu"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('elecMuPairsForBgEstZmumuEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("elecMuPairCutBgEstPreselWplusJets"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('elecMuPairsForBgEstWplusJetsEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("elecMuPairCutBgEstPreselTTplusJets"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('elecMuPairsForBgEstTTplusJetsEnriched'),
+            minNumber = cms.uint32(1)
+        ),
+        cms.PSet(
+            pluginName = cms.string("elecMuPairCutBgEstPreselQCD"),
+            pluginType = cms.string("PATCandViewMinEventSelector"),
+            src = cms.InputTag('elecMuPairsForBgEstQCDenriched'),
+            minNumber = cms.uint32(1)
+        )
+    )
+) 
 
-produceBoolEventSelFlags = cms.Sequence(
-    electronsBgEstPreselection + electronTrkCutBgEstPreselection
-   +muonsBgEstPreselection + muonTrkCutBgEstPreselection
-   +elecMuPairsBgEstPreselection + elecMuPairCutBgEstPreselection
-)
+produceBoolEventSelFlags = cms.Sequence( elecMuPairCutBgEstPreselection )
 
 selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
     flags = cms.VInputTag(
@@ -68,19 +52,6 @@ selectEventsByBoolEventSelFlags = cms.EDFilter("MultiBoolEventSelFlagFilter",
         cms.InputTag('primaryEventVertex'),
         cms.InputTag('primaryEventVertexQuality'),
         cms.InputTag('primaryEventVertexPosition'),
-        cms.InputTag('electronTrkCutBgEstPreselection'),
-        cms.InputTag('muonTrkCutBgEstPreselection'),
         cms.InputTag('elecMuPairCutBgEstPreselection')
     )
 )
-
-# exclude loosely selected electrons and muons from jets collections
-# (otherwise, non-isolated electrons/muons may get double-counted,
-#  once in the electron/muon collection plus once in the jet collection)
-from TauAnalysis.RecoTools.patJetSelection_cff import *
-selectedLayer1JetsAntiOverlapWithLeptonsVeto.srcNotToBeFiltered = cms.VInputTag(
-    "electronsBgEstPreselection",
-    "muonsBgEstPreselection",
-    "selectedLayer1TausProngCumulative"
-)
-
--- TauAnalysis/BgEstimationTools/python/bgEstSampleDefinitionsZtoElecMu_cfi.py
+++ TauAnalysis/BgEstimationTools/python/bgEstSampleDefinitionsZtoElecMu_cfi.py
@@ -4,14 +4,16 @@ import copy
 # define configuration parameters for submission of Z --> mu + tau-jet jobs to CERN batch system
 # (running over skimmed samples stored on CASTOR)
 
-bgEstSampleDirectoryName = cms.string("rfio:/castor/cern.ch/user/v/veelken/bgEstSkim/ZtoElecMu/")
+bgEstSampleDirectoryName = cms.string("rfio:/castor/cern.ch/user/s/sunil/bgEstSkim/ZtoElecMu/")
 
 # definitions for producing skimmed samples
-bgEstSampleOutputFileNameZtautau = cms.untracked.string('bgEstSampleZtoElecMu_Ztautau.root')
+bgEstSampleOutputFileNameZtautau = cms.untracked.string('bgEstSampleZtoElecMu_Ztautau_partXX.root')
 
 # definitions for reading skimmed samples
 bgEstSampleFileNamesZtautau = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Ztautau.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Ztautau_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Ztautau_part02.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Ztautau_part03.root'
 )
 
 # definitions for producing ntuples
@@ -20,11 +22,12 @@ bgEstNtupleOutputFileNameZtautau = cms.string('bgEstNtupleZtoElecMu_Ztautau.root
 #--------------------------------------------------------------------------------
 
 # definitions for producing skimmed samples
-bgEstSampleOutputFileNameZmumu = cms.untracked.string('bgEstSampleZtoElecMu_Zmumu.root')
+bgEstSampleOutputFileNameZmumu = cms.untracked.string('bgEstSampleZtoElecMu_Zmumu_partXX.root')
 
 # definitions for reading skimmed samples
 bgEstSampleFileNamesZmumu = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Zmumu.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Zmumu_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_Zmumu_part02.root'
 )
 
 # definitions for producing ntuples
@@ -46,23 +49,26 @@ bgEstNtupleOutputFileNameZee = cms.string('bgEstNtupleZtoElecMu_Zee.root')
 #--------------------------------------------------------------------------------
 
 # definitions for producing skimmed samples
-bgEstSampleOutputFileNameZtautauPlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZtautauPlusJets.root')
+bgEstSampleOutputFileNameZtautauPlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZtautauPlusJets_partXX.root')
 
-bgEstSampleOutputFileNameZmumuPlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZmumuPlusJets.root')
+bgEstSampleOutputFileNameZmumuPlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZmumuPlusJets_partXX.root')
 
-bgEstSampleOutputFileNameZeePlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZeePlusJets.root')
+bgEstSampleOutputFileNameZeePlusJets = cms.untracked.string('bgEstSampleZtoElecMu_ZeePlusJets_partXX.root')
 
 # definitions for reading skimmed samples
 bgEstSampleFileNamesZtautauPlusJets = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZtautauPlusJets.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZtautauPlusJets_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZtautauPlusJets_part02.root'
 )
 
-bgEstSampleFileNamesZmumuPlusJets_part01 = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZmumuPlusJets.root'
+bgEstSampleFileNamesZmumuPlusJets = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZmumuPlusJets_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZmumuPlusJets_part02.root'
 )
 
 bgEstSampleFileNamesZeePlusJets = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZeePlusJets.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZeePlusJets_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_ZeePlusJets_part02.root'
 )
 
 # definitions for producing ntuples
@@ -80,7 +86,11 @@ bgEstSampleOutputFileNameWplusJets = cms.untracked.string('bgEstSampleZtoElecMu_
 # definitions for reading skimmed samples
 bgEstSampleFileNamesWplusJets = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part01.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part02.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part02.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part03.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part04.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_WplusJets_part05.root'
+
 )
 
 # definitions for producing ntuples
@@ -89,7 +99,7 @@ bgEstNtupleOutputFileNameWplusJets = cms.string('bgEstNtupleZtoElecMu_WplusJets.
 #--------------------------------------------------------------------------------
 
 # definitions for producing skimmed samples
-bgEstSampleOutputFileNameInclusivePPmuX = cms.untracked.string('bgEstSampleZtoElecMu_InclusivePPmuX.root')
+#bgEstSampleOutputFileNameInclusivePPmuX = cms.untracked.string('bgEstSampleZtoElecMu_InclusivePPmuX.root')
 
 bgEstSampleOutputFileNamePPmuXptGt20 = cms.untracked.string('bgEstSampleZtoElecMu_PPmuXptGt20_partXX.root')
 
@@ -104,10 +114,7 @@ bgEstSampleFileNamesPPmuXptGt20_part01  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part07.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part08.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part09.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part10.root'
-)
-
-bgEstSampleFileNamesPPmuXptGt20_part02  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part10.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part11.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part12.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part13.root',
@@ -115,14 +122,50 @@ bgEstSampleFileNamesPPmuXptGt20_part02  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part15.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part16.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part17.root',
-    #bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part18.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part18.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part19.root',
-    #bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part20.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part21.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part20.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part21.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part22.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part23.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part24.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part25.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part26.root'
+
+)
+
+bgEstSampleFileNamesPPmuXptGt20_part02  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part27.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part28.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part29.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part30.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part31.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part32.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part33.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part34.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part35.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part36.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part37.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part38.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part39.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part40.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part41.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part42.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part43.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part44.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part45.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part46.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part47.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part48.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part49.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part50.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part51.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_PPmuXptGt20_part52.root'
+
 )
 
 # definitions for producing ntuples
-bgEstNtupleOutputFileNameInclusivePPmuX = cms.string('bgEstNtupleZtoElecMu_InclusivePPmuX.root')
+#bgEstNtupleOutputFileNameInclusivePPmuX = cms.string('bgEstNtupleZtoElecMu_InclusivePPmuX.root')
 
 bgEstNtupleOutputFileNamePPmuXptGt20 = cms.string('bgEstNtupleZtoElecMu_PPmuXptGt20_partXX.root')
 
@@ -133,18 +176,22 @@ bgEstSampleOutputFileNameTTplusJets = cms.untracked.string('bgEstSampleZtoElecMu
 
 # definitions for reading skimmed samples
 bgEstSampleFileNamesTTplusJets_part01  = cms.untracked.vstring(
-    #bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part01.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part01.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part02.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part03.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part04.root'
-    #bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part05.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part04.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part05.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part06.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part07.root' 
 )
 
 bgEstSampleFileNamesTTplusJets_part02  = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part06.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part07.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part08.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part09.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part09.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part10.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part11.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part12.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoElecMu_TTplusJets_part13.root'
 )
 
 # definitions for producing ntuples
