13a3c0ebe591954c5171e2b58914610f07fc644e
kkousour <>
2009-09-23 13:49:03 +0000

    Added separate analysis classes for PF and Calo

--- JetMETCorrections/DijetBalance/src/SealModule.cc
+++ JetMETCorrections/DijetBalance/src/SealModule.cc
@@ -2,6 +2,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "JetMETCorrections/DijetBalance/interface/DijetBalanceTreeProducer.h"
+#include "JetMETCorrections/DijetBalance/interface/CaloTreeProducer.h"
+#include "JetMETCorrections/DijetBalance/interface/PFTreeProducer.h"
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
 #include "DataFormats/JetReco/interface/PFJet.h"
@@ -14,3 +16,5 @@ typedef DijetBalanceTreeProducer<GenJet> GenDBTreeProducer;
 DEFINE_ANOTHER_FWK_MODULE(GenDBTreeProducer);
 typedef DijetBalanceTreeProducer<PFJet> PFDBTreeProducer;
 DEFINE_ANOTHER_FWK_MODULE(PFDBTreeProducer);
+DEFINE_ANOTHER_FWK_MODULE(CaloTreeProducer);
+DEFINE_ANOTHER_FWK_MODULE(PFTreeProducer);
