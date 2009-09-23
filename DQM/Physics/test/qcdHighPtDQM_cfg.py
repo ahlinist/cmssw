76a8a3f76b6abb5487429585974ce53be02bf571
krose <>
2009-09-23 11:41:40 +0000

    fixing QCD High Pt DQM cfi

--- DQM/Physics/test/qcdHighPtDQM_cfg.py
+++ DQM/Physics/test/qcdHighPtDQM_cfg.py
@@ -15,15 +15,14 @@ process.source = cms.Source("PoolSource",
 '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_Pt_50_80/GEN-SIM-RECO/STARTUP_30X_v1/0002/E827B5EF-FE16-DE11-8C9F-00304867C136.root',
         '/store/relval/CMSSW_3_1_0_pre4/RelValQCD_Pt_50_80/GEN-SIM-RECO/STARTUP_30X_v1/0002/9C49DDC4-5C18-DE11-96DB-001A92811736.root'
                            )
-                            )
-
+                    )
 
 process.QcdHighPtDQM = cms.EDAnalyzer("QcdHighPtDQM",
-                                     jetTag = cms.untracked.InputTag("sisCone5CaloJets"),
-                                      metTag1 = cms.untracked.InputTag("met"),
-                                      metTag2 = cms.untracked.InputTag("metHO"),
-                                      metTag3 = cms.untracked.InputTag("metNoHF"),
-                                      metTag4 = cms.untracked.InputTag("metNoHFHO")
+                                    jetTag = cms.untracked.InputTag("sisCone5CaloJets"),
+                                    metTag1 = cms.untracked.InputTag("met"),
+                                    metTag2 = cms.untracked.InputTag("metHO"),
+                                    metTag3 = cms.untracked.InputTag("metNoHF"),
+                                    metTag4 = cms.untracked.InputTag("metNoHFHO")
                                      
 )
 process.p = cms.Path(process.QcdHighPtDQM+process.dqmSaver)
