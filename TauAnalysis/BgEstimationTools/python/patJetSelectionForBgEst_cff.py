05cd511d381d4a03aa7fab111c0573210509e1c1
veelken <>
2009-09-27 17:24:41 +0000

    added jet collections for Z --> e + tau-jet channel

--- TauAnalysis/BgEstimationTools/python/patJetSelectionForBgEst_cff.py
+++ TauAnalysis/BgEstimationTools/python/patJetSelectionForBgEst_cff.py
@@ -18,11 +18,17 @@ jetsEt20ForBgEst = cms.EDFilter("PATJetSelector",
     filter = cms.bool(False)
 )
 
+#--------------------------------------------------------------------------------
+# collections of pat::Jets speficic to Z --> muon + tau-jet channel
+#--------------------------------------------------------------------------------
+
 jetsAntiOverlapWithLeptonsVetoForMuTauBgEstZmumuEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
     src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
-    srcNotToBeFiltered = cms.VInputTag("selectedLayer1ElectronsTrkIPcumulative",
-                                       "muonsTrkTightIsolationForBgEst",
-                                       "selectedLayer1TausChargeCumulative"),
+    srcNotToBeFiltered = cms.VInputTag(
+        "selectedLayer1ElectronsTrkIPcumulative",
+        "muonsTrkTightIsolationForBgEst",
+        "selectedLayer1TausChargeCumulative"
+    ),
     dRmin = cms.double(0.7),
     filter = cms.bool(False)                                           
 )
@@ -41,9 +47,11 @@ selectJetsForMuTauBgEstZmumuEnriched = cms.Sequence( jetsAntiOverlapWithLeptonsV
 
 jetsAntiOverlapWithLeptonsVetoForMuTauBgEstWplusJetsEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
     src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
-   srcNotToBeFiltered = cms.VInputTag("selectedLayer1ElectronsTrkIPcumulative",
-                                       "muonsTrkTightIsolationForBgEst",
-                                       "tausAntiOverlapWithMuonsVetoLooseIsolationForBgEst"),
+    srcNotToBeFiltered = cms.VInputTag(
+        "selectedLayer1ElectronsTrkIPcumulative",
+        "muonsTrkTightIsolationForBgEst",
+        "tausAntiOverlapWithMuonsVetoLooseIsolationForBgEst"
+    ),
     dRmin = cms.double(0.7),
     filter = cms.bool(False)                                           
 )
@@ -59,9 +67,11 @@ selectJetsForMuTauBgEstWplusJetsEnriched = cms.Sequence( jetsAntiOverlapWithLept
 
 jetsAntiOverlapWithLeptonsVetoForMuTauBgEstTTplusJetsEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
     src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
-    srcNotToBeFiltered = cms.VInputTag("selectedLayer1ElectronsTrkIPcumulative",
-                                       "muonsTrkTightIsolationForBgEst",
-                                       "selectedLayer1TausForMuTauMuonVetoCumulative"),
+    srcNotToBeFiltered = cms.VInputTag(
+        "selectedLayer1ElectronsTrkIPcumulative",
+        "muonsTrkTightIsolationForBgEst",
+        "selectedLayer1TausForMuTauMuonVetoCumulative"
+    ),
     dRmin = cms.double(0.7),
     filter = cms.bool(False)                                           
 )
@@ -82,10 +92,87 @@ selectJetsForMuTauBgEstTTplusJetsEnriched = cms.Sequence( jetsAntiOverlapWithLep
                                                          * jetsEt40ForMuTauBgEstTTplusJetsEnriched
                                                          * jetsEt60ForMuTauBgEstTTplusJetsEnriched )
 
+#--------------------------------------------------------------------------------
+# collections of pat::Jets speficic to Z --> electron + tau-jet channel
+#--------------------------------------------------------------------------------
+
+jetsAntiOverlapWithLeptonsVetoForElecTauBgEstZeeEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
+    src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
+    srcNotToBeFiltered = cms.VInputTag(
+        "electronsTrkTightIsolationForBgEst",
+        "selectedLayer1MuonsTrkIPcumulative",
+        "selectedLayer1TausChargeCumulative"
+    ),
+    dRmin = cms.double(0.7),
+    filter = cms.bool(False)                                           
+)
+
+# select jets with quantity alpha > 0.1
+# (defined as ratio of sum of charged particle transverse momenta 
+#  to sum of charged plus neutral particle transverse momenta)
+jetsAlpha0point1ForElecTauBgEstZeeEnriched = cms.EDFilter("PATJetAlphaSelector",
+    src = cms.InputTag("jetsAntiOverlapWithLeptonsVetoForElecTauBgEstZeeEnriched"),
+    alphaMin = cms.double(0.1),
+    filter = cms.bool(False)
+)
+
+selectJetsForElecTauBgEstZeeEnriched = cms.Sequence( jetsAntiOverlapWithLeptonsVetoForElecTauBgEstZeeEnriched
+                                                    * jetsAlpha0point1ForElecTauBgEstZeeEnriched )
+
+jetsAntiOverlapWithLeptonsVetoForElecTauBgEstWplusJetsEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
+    src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
+    srcNotToBeFiltered = cms.VInputTag(
+        "electronsTrkTightIsolationForBgEst",
+        "selectedLayer1MuonsTrkIPcumulative",
+        "tausAntiOverlapWithElectronsVetoLooseIsolationForBgEst"
+    ),
+    dRmin = cms.double(0.7),
+    filter = cms.bool(False)                                           
+)
+
+jetsAlpha0point1ForElecTauBgEstWplusJetsEnriched = cms.EDFilter("PATJetAlphaSelector",
+    src = cms.InputTag("jetsAntiOverlapWithLeptonsVetoForElecTauBgEstWplusJetsEnriched"),
+    alphaMin = cms.double(0.1),
+    filter = cms.bool(False)
+)
+
+selectJetsForElecTauBgEstWplusJetsEnriched = cms.Sequence( jetsAntiOverlapWithLeptonsVetoForElecTauBgEstWplusJetsEnriched
+                                                        * jetsAlpha0point1ForElecTauBgEstWplusJetsEnriched )
+
+jetsAntiOverlapWithLeptonsVetoForElecTauBgEstTTplusJetsEnriched = cms.EDFilter("PATJetAntiOverlapSelector",
+    src = cms.InputTag("jetsEt20ForBgEst"),                                                                  
+    srcNotToBeFiltered = cms.VInputTag(
+        "electronsTrkTightIsolationForBgEst",
+        "selectedLayer1MuonsTrkIPcumulative",
+        "selectedLayer1TausForElecTauElectronVetoCumulative"
+    ),
+    dRmin = cms.double(0.7),
+    filter = cms.bool(False)                                           
+)
+
+jetsEt40ForElecTauBgEstTTplusJetsEnriched = cms.EDFilter("PATJetSelector",
+    src = cms.InputTag("jetsAntiOverlapWithLeptonsVetoForElecTauBgEstTTplusJetsEnriched"),                     
+    cut = cms.string('et > 40.'),
+    filter = cms.bool(False)
+)
+
+jetsEt60ForElecTauBgEstTTplusJetsEnriched = cms.EDFilter("PATJetSelector",
+    src = cms.InputTag("jetsAntiOverlapWithLeptonsVetoForElecTauBgEstTTplusJetsEnriched"),                     
+    cut = cms.string('et > 60.'),
+    filter = cms.bool(False)
+)
+
+selectJetsForElecTauBgEstTTplusJetsEnriched = cms.Sequence( jetsAntiOverlapWithLeptonsVetoForElecTauBgEstTTplusJetsEnriched
+                                                           * jetsEt40ForElecTauBgEstTTplusJetsEnriched
+                                                           * jetsEt60ForElecTauBgEstTTplusJetsEnriched )
+
 selectJetsForBgEst = cms.Sequence( jetsEta21ForBgEst * jetsEt20ForBgEst
                                   * selectJetsForMuTauBgEstZmumuEnriched
                                   * selectJetsForMuTauBgEstWplusJetsEnriched
-                                  * selectJetsForMuTauBgEstTTplusJetsEnriched )
+                                  * selectJetsForMuTauBgEstTTplusJetsEnriched
+                                  * selectJetsForElecTauBgEstZeeEnriched
+                                  * selectJetsForElecTauBgEstWplusJetsEnriched
+                                  * selectJetsForElecTauBgEstTTplusJetsEnriched )
 
 
 
