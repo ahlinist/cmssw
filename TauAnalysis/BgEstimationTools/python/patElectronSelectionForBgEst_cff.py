8c00870cf0acc2d8348bda49695c687c22da1c7a
veelken <>
2009-09-27 17:26:53 +0000

    updated configuration files for Z --> e + tau-jet channel to recent changes in Z --> mu + tau-jet channel

--- TauAnalysis/BgEstimationTools/python/patElectronSelectionForBgEst_cff.py
+++ TauAnalysis/BgEstimationTools/python/patElectronSelectionForBgEst_cff.py
@@ -10,5 +10,26 @@ electronsTrkLooseIsolationForBgEst = cms.EDFilter("PATElectronSelector",
     filter = cms.bool(False)
 )
 
-selectElectronsForBgEst = cms.Sequence( electronsTrkLooseIsolationForBgEst )
+electronsTrkIsoForBgEst = cms.EDFilter("PATElectronSelector",
+    src = cms.InputTag('selectedLayer1ElectronsPt15Cumulative'),
+    cut = cms.string('trackIso < 2.'),
+    filter = cms.bool(False)
+)
+
+electronsEcalIsoForBgEst = cms.EDFilter("PATElectronSelector",
+    src = cms.InputTag('electronsTrkIsoForBgEst'),                          
+    cut = cms.string('ecalIso < 2.'),
+    filter = cms.bool(False)
+)
+
+electronsTrkTightIsolationForBgEst = cms.EDFilter("PATElectronSelector",
+    src = cms.InputTag('electronsEcalIsoForBgEst'),                                        
+    cut = cms.string('gsfTrack.isNonnull'),
+    filter = cms.bool(False)
+)
+
+selectElectronsForBgEst = cms.Sequence(
+    electronsTrkLooseIsolationForBgEst
+   * electronsTrkIsoForBgEst * electronsEcalIsoForBgEst * electronsTrkTightIsolationForBgEst
+)
 
