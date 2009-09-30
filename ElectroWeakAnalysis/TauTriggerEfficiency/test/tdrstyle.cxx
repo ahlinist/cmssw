7c45828a6217b5e0246ed5783ac4062cfa5565ee
mkortela <>
2009-09-30 12:38:38 +0000

    Commented tdrGrid so that the script can be compiled

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/tdrstyle.cxx
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/tdrstyle.cxx
@@ -1,11 +1,12 @@
 #include "TStyle.h"
 
 // tdrGrid: Turns the grid lines on (true) or off (false)
-
+/*
 void tdrGrid(bool gridOn) {
   tdrStyle->SetPadGridX(gridOn);
   tdrStyle->SetPadGridY(gridOn);
 }
+*/
 
 // fixOverlay: Redraws the axis
 
