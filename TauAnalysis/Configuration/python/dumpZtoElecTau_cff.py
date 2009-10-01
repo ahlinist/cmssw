f3abcfa1b88764d6a69fe3a7bfe793cddc024353
jkolb <>
2009-10-01 14:13:32 +0000

    update for new column names

--- TauAnalysis/Configuration/python/dumpZtoElecTau_cff.py
+++ TauAnalysis/Configuration/python/dumpZtoElecTau_cff.py
@@ -7,14 +7,14 @@ import FWCore.ParameterSet.Config as cms
 dumpZtoElecTau = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
     dqmDirectories = cms.PSet(
         Ztautau = cms.string('harvested/Ztautau/zElecTauAnalyzer/FilterStatistics/'),
-        #Zee = cms.string('harvested/Zee/zElecTauAnalyzer/FilterStatistics/'),
-        #ZtautauPlusJets = cms.string('harvested/ZtautauPlusJets/zElecTauAnalyzer/FilterStatistics/'),
+        Zee = cms.string('harvested/Zee/zElecTauAnalyzer/FilterStatistics/'),
+        ZtautauPlusJets = cms.string('harvested/ZtautauPlusJets/zElecTauAnalyzer/FilterStatistics/'),
         ZeePlusJets = cms.string('harvested/ZeePlusJets/zElecTauAnalyzer/FilterStatistics/'),
         WplusJets = cms.string('harvested/WplusJets/zElecTauAnalyzer/FilterStatistics/'),
         TTplusJets = cms.string('harvested/TTplusJets/zElecTauAnalyzer/FilterStatistics/'),
         QCD = cms.string('harvested/qcdSum/zElecTauAnalyzer/FilterStatistics/'),
         gammaPlusJetsSum = cms.string('harvested/gammaPlusJetsSum/zElecTauAnalyzer/FilterStatistics/')
     ),
-    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
+    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency", "excl. Rejection")
 )
  
