3e6c8572a12b5d4292add77193cfaf1cd880497f
ursl <>
2009-10-02 11:54:07 +0000

    changed printout and Makefile cleanup

--- AnalysisDataFormats/HeavyFlavorObjects/rootio/TGenCand.cc
+++ AnalysisDataFormats/HeavyFlavorObjects/rootio/TGenCand.cc
@@ -12,7 +12,7 @@ TGenCand::TGenCand(Int_t Option) { }
 
 void TGenCand::dump() {
   char line[200];
-  sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%4d,%4d) p=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
+  sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%5d,%5d) p=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
 	  fNumber, fID, fStatus, fMom1, fMom2, fDau1, fDau2,
 	  fP.Rho(), 
 	  fP.X(), fP.Y(), fP.Z(),
@@ -24,7 +24,7 @@ void TGenCand::dump() {
 
 void TGenCand::dump(ofstream &OUT) {
   char line[200];
-  sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%4d,%4d) p=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
+  sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%5d,%5d) p=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
 	  fNumber, fID, fStatus, fMom1, fMom2, fDau1, fDau2,
 	  fP.Rho(), 
 	  fP.X(), fP.Y(), fP.Z(),
