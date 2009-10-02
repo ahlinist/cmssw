5567244c481e5e7a837d6b6918ae7179b6fef534
ceballos <>
2009-10-02 11:52:05 +0000

    new config files

--- Configuration/DataOps/python/skimQCD_EMEnriched_Pt20to30.py
+++ Configuration/DataOps/python/skimQCD_EMEnriched_Pt20to30.py
@@ -22,34 +22,41 @@ process.hltHighLevelDev.HLTOverallPrescale = cms.uint32(1)
 
 # Once the hltHighLevelDev filter has been defined I can clone it and configure as needed
 process.hltHighLevelDev2 = process.hltHighLevelDev.clone(andOr = True)
-process.hltHighLevelDev2.HLTPaths = ("HLT_Ele15_LW_L1R","HLT_Ele15_SC10_LW_L1R","HLT_Ele20_LW_L1R")
+process.hltHighLevelDev2.HLTPaths = ("HLT_Ele10_LW_EleId_L1R",)
 process.hltHighLevelDev2.andOr = True # True = OR, False = AND
-process.hltHighLevelDev2.HLTPathsPrescales  = cms.vuint32(1,1,1)
+process.hltHighLevelDev2.HLTPathsPrescales  = cms.vuint32(1,)
 process.hltHighLevelDev2.HLTOverallPrescale = cms.uint32(1)
 
+# Once the hltHighLevelDev filter has been defined I can clone it and configure as needed
 process.hltHighLevelDev3 = process.hltHighLevelDev.clone(andOr = True)
-process.hltHighLevelDev3.HLTPaths = ("HLT_Photon15_L1R",)
+process.hltHighLevelDev3.HLTPaths = ("HLT_Ele15_LW_L1R","HLT_Ele15_SC10_LW_L1R","HLT_Ele20_LW_L1R")
 process.hltHighLevelDev3.andOr = True # True = OR, False = AND
-process.hltHighLevelDev3.HLTPathsPrescales  = cms.vuint32(5,)
+process.hltHighLevelDev3.HLTPathsPrescales  = cms.vuint32(1,1,1)
 process.hltHighLevelDev3.HLTOverallPrescale = cms.uint32(1)
 
 process.hltHighLevelDev4 = process.hltHighLevelDev.clone(andOr = True)
-process.hltHighLevelDev4.HLTPaths = ("HLT_Photon20_L1R","HLT_Photon30_L1R_8E29")
+process.hltHighLevelDev4.HLTPaths = ("HLT_Photon15_L1R",)
 process.hltHighLevelDev4.andOr = True # True = OR, False = AND
-process.hltHighLevelDev4.HLTPathsPrescales  = cms.vuint32(1,1)
+process.hltHighLevelDev4.HLTPathsPrescales  = cms.vuint32(5,)
 process.hltHighLevelDev4.HLTOverallPrescale = cms.uint32(1)
 
 process.hltHighLevelDev5 = process.hltHighLevelDev.clone(andOr = True)
-process.hltHighLevelDev5.HLTPaths = ("HLT_SingleLooseIsoTau20",)
+process.hltHighLevelDev5.HLTPaths = ("HLT_Photon20_L1R","HLT_Photon30_L1R_8E29")
 process.hltHighLevelDev5.andOr = True # True = OR, False = AND
-process.hltHighLevelDev5.HLTPathsPrescales  = cms.vuint32(5,)
+process.hltHighLevelDev5.HLTPathsPrescales  = cms.vuint32(1,1)
 process.hltHighLevelDev5.HLTOverallPrescale = cms.uint32(1)
 
 process.hltHighLevelDev6 = process.hltHighLevelDev.clone(andOr = True)
-process.hltHighLevelDev6.HLTPaths = ("HLT_DoubleLooseIsoTau15",)
+process.hltHighLevelDev6.HLTPaths = ("HLT_SingleLooseIsoTau20",)
 process.hltHighLevelDev6.andOr = True # True = OR, False = AND
-process.hltHighLevelDev6.HLTPathsPrescales  = cms.vuint32(4,)
+process.hltHighLevelDev6.HLTPathsPrescales  = cms.vuint32(5,)
 process.hltHighLevelDev6.HLTOverallPrescale = cms.uint32(1)
+
+process.hltHighLevelDev7 = process.hltHighLevelDev.clone(andOr = True)
+process.hltHighLevelDev7.HLTPaths = ("HLT_DoubleLooseIsoTau15",)
+process.hltHighLevelDev7.andOr = True # True = OR, False = AND
+process.hltHighLevelDev7.HLTPathsPrescales  = cms.vuint32(4,)
+process.hltHighLevelDev7.HLTOverallPrescale = cms.uint32(1)
 
 
 #############   pt_hat Filter  #####
@@ -61,7 +68,7 @@ process.ptHat_filter = cms.EDFilter("MCProcessFilter",
 
 
 process.MessageLogger = cms.Service("MessageLogger",
-    destinations = cms.untracked.vstring('log/skimQCD_EMEnriched_Pt20to30_EleGamma_Tau.log')
+    destinations = cms.untracked.vstring('log/skimQCD_EMEnriched_Pt20to30.log')
     )
 
 process.configurationMetadata = cms.untracked.PSet(
@@ -118,10 +125,10 @@ process.output1 = cms.OutputModule("PoolOutputModule",
 process.output2 = cms.OutputModule("PoolOutputModule",
                                    splitLevel = cms.untracked.int32(0),
                                    outputCommands = cms.untracked.vstring('keep *'),
-                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Ele15_QCD_EMEnriched_Pt20to30.root'),
+                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Ele10_EleId_QCD_EMEnriched_Pt20to30.root'),
                                    dataset = cms.untracked.PSet(
                                       dataTier = cms.untracked.string('GEN-SIM-RECO'),
-                                      filterName = cms.untracked.string('SD_Ele15')
+                                      filterName = cms.untracked.string('SD_Ele10_EleId')
                                       ),
                                    SelectEvents = cms.untracked.PSet(
                                       SelectEvents = cms.vstring('skim2')
@@ -132,10 +139,10 @@ process.output2 = cms.OutputModule("PoolOutputModule",
 process.output3 = cms.OutputModule("PoolOutputModule",
                                    splitLevel = cms.untracked.int32(0),
                                    outputCommands = cms.untracked.vstring('keep *'),
-                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Photon15_QCD_EMEnriched_Pt20to30.root'),
+                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Ele15_QCD_EMEnriched_Pt20to30.root'),
                                    dataset = cms.untracked.PSet(
                                       dataTier = cms.untracked.string('GEN-SIM-RECO'),
-                                      filterName = cms.untracked.string('SD_Photon15')
+                                      filterName = cms.untracked.string('SD_Ele15')
                                       ),
                                    SelectEvents = cms.untracked.PSet(
                                       SelectEvents = cms.vstring('skim3')
@@ -146,10 +153,10 @@ process.output3 = cms.OutputModule("PoolOutputModule",
 process.output4 = cms.OutputModule("PoolOutputModule",
                                    splitLevel = cms.untracked.int32(0),
                                    outputCommands = cms.untracked.vstring('keep *'),
-                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Photon20_QCD_EMEnriched_Pt20to30.root'),
+                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Photon15_QCD_EMEnriched_Pt20to30.root'),
                                    dataset = cms.untracked.PSet(
                                       dataTier = cms.untracked.string('GEN-SIM-RECO'),
-                                      filterName = cms.untracked.string('SD_Photon20')
+                                      filterName = cms.untracked.string('SD_Photon15')
                                       ),
                                    SelectEvents = cms.untracked.PSet(
                                       SelectEvents = cms.vstring('skim4')
@@ -160,10 +167,10 @@ process.output4 = cms.OutputModule("PoolOutputModule",
 process.output5 = cms.OutputModule("PoolOutputModule",
                                    splitLevel = cms.untracked.int32(0),
                                    outputCommands = cms.untracked.vstring('keep *'),
-                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_SingleLooseTau20_QCD_EMEnriched_Pt20to30.root'),
+                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_Photon20_QCD_EMEnriched_Pt20to30.root'),
                                    dataset = cms.untracked.PSet(
                                       dataTier = cms.untracked.string('GEN-SIM-RECO'),
-                                      filterName = cms.untracked.string('SD_SingleLooseTau20')
+                                      filterName = cms.untracked.string('SD_Photon20')
                                       ),
                                    SelectEvents = cms.untracked.PSet(
                                       SelectEvents = cms.vstring('skim5')
@@ -174,13 +181,27 @@ process.output5 = cms.OutputModule("PoolOutputModule",
 process.output6 = cms.OutputModule("PoolOutputModule",
                                    splitLevel = cms.untracked.int32(0),
                                    outputCommands = cms.untracked.vstring('keep *'),
+                                   fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_SingleLooseTau20_QCD_EMEnriched_Pt20to30.root'),
+                                   dataset = cms.untracked.PSet(
+                                      dataTier = cms.untracked.string('GEN-SIM-RECO'),
+                                      filterName = cms.untracked.string('SD_SingleLooseTau20')
+                                      ),
+                                   SelectEvents = cms.untracked.PSet(
+                                      SelectEvents = cms.vstring('skim6')
+                                      )
+)
+
+# Yet another output definition
+process.output7 = cms.OutputModule("PoolOutputModule",
+                                   splitLevel = cms.untracked.int32(0),
+                                   outputCommands = cms.untracked.vstring('keep *'),
                                    fileName = cms.untracked.string('/data/rossin/CMS/Triggers/SDexercise/SD_DoubleLooseTau15_QCD_EMEnriched_Pt20to30.root'),
                                    dataset = cms.untracked.PSet(
                                       dataTier = cms.untracked.string('GEN-SIM-RECO'),
                                       filterName = cms.untracked.string('SD_DoubleLooseTau15')
                                       ),
                                    SelectEvents = cms.untracked.PSet(
-                                      SelectEvents = cms.vstring('skim6')
+                                      SelectEvents = cms.vstring('skim7')
                                       )
 )
 
@@ -195,10 +216,11 @@ process.skim3 = cms.Path(process.ptHat_filter+process.hltHighLevelDev3)
 process.skim4 = cms.Path(process.ptHat_filter+process.hltHighLevelDev4)
 process.skim5 = cms.Path(process.ptHat_filter+process.hltHighLevelDev5)
 process.skim6 = cms.Path(process.ptHat_filter+process.hltHighLevelDev6)
+process.skim7 = cms.Path(process.ptHat_filter+process.hltHighLevelDev7)
 
 
-process.out_step = cms.EndPath(process.output1+process.output2+process.output3+process.output4+process.output5+process.output6)
+process.out_step = cms.EndPath(process.output1+process.output2+process.output3+process.output4+process.output5+process.output6+process.output7)
 
-process.schedule = cms.Schedule(process.hlttrigreport,process.skim1,process.skim2,process.skim3,process.skim4,process.skim5,process.skim6)
+process.schedule = cms.Schedule(process.hlttrigreport,process.skim1,process.skim2,process.skim3,process.skim4,process.skim5,process.skim6,process.skim7)
 process.schedule.append(process.out_step)
 
