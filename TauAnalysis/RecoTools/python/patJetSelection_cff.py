3496d4e4deca3385b0bd62614a28265efe6c1349
veelken <>
2009-10-14 13:09:34 +0000

    removed selectLayer1Jets sequence and renamed selectLayer1JetsDefault --> selectLayer1Jets sequence

--- TauAnalysis/RecoTools/python/patJetSelection_cff.py
+++ TauAnalysis/RecoTools/python/patJetSelection_cff.py
@@ -38,7 +38,7 @@ patJetSelConfigurator = objSelConfigurator(
     pyModuleName = __name__,
     doSelIndividual = False
 )
-selectLayer1JetsDefault = patJetSelConfigurator.configure(namespace = locals())
+selectLayer1Jets = patJetSelConfigurator.configure(namespace = locals())
 
 # Select jets for W->tau nu analysis
 selectedLayer1JetsAntiOverlapWithTausVetoForWTauNu = cms.EDFilter("PATJetAntiOverlapSelector",
@@ -72,10 +72,6 @@ patJetSelConfiguratorForWTauNu = objSelConfigurator(
     doSelIndividual = False
 )
 
-selectLayer1JetsForWTauNu =patJetSelConfiguratorForWTauNu.configure(namespace = locals())
-
-selectLayer1Jets = cms.Sequence( selectLayer1JetsDefault
-                                 *selectLayer1JetsForWTauNu
-                                 )
+selectLayer1JetsForWTauNu = patJetSelConfiguratorForWTauNu.configure(namespace = locals())
 
 
