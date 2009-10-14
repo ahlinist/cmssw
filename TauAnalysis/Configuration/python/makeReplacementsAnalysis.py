3e88a243bb7a2cadf256a7a418adff3011ee1f31
veelken <>
2009-10-14 13:24:43 +0000

    added possibility to run analysis either on RECO/AOD files or on PAT-tuples as input

--- TauAnalysis/Configuration/python/makeReplacementsAnalysis.py
+++ TauAnalysis/Configuration/python/makeReplacementsAnalysis.py
@@ -37,81 +37,98 @@
 
 def makeReplacementsAnalysis(channel = None, sample = None, replacements = None):
 
-	# check that channel, sample and replacements parameters are defined and non-empty
-	if channel is None:
-		raise ValueError("Undefined channel Parameter !!")
-	if sample is None:
-		raise ValueError("Undefined sample Parameter !!")
-	if replacements is None:
-		raise ValueError("Undefined replacements Parameter !!")
+    # check that channel, sample and replacements parameters are defined and non-empty
+    if channel is None:
+        raise ValueError("Undefined channel Parameter !!")
+    if sample is None:
+        raise ValueError("Undefined sample Parameter !!")
+    if replacements is None:
+        raise ValueError("Undefined replacements Parameter !!")
 
-	# remove all white-space characters from replacements parameter string
-	replacements = replacements.replace(" ", "")
+    # remove all white-space characters from replacements parameter string
+    replacements = replacements.replace(" ", "")
 
-	# split replacements string into list of individual replace statements
-	# (separated by ";" character)
-	replaceStatements = replacements.split(";")
+    # split replacements string into list of individual replace statements
+    # (separated by ";" character)
+    replaceStatements = replacements.split(";")
 
-	replaceStatements_retVal = []
+    replaceStatements_retVal = []
 
-	factorization = None
+    factorization = None
 
-	for replaceStatement in replaceStatements:
+    for replaceStatement in replaceStatements:
 
-		# split replacement string into name, value pairs
-		paramNameValuePair = replaceStatement.split("=")
+        # split replacement string into name, value pairs
+	paramNameValuePair = replaceStatement.split("=")
 
-		# check that replacement string matches 'paramName=paramValue' format
-		if len(paramNameValuePair) != 2:
-			raise ValueError("Invalid format of replace Statement: " + replaceStatement + " !!")
+	# check that replacement string matches 'paramName=paramValue' format
+	if len(paramNameValuePair) != 2:
+	    raise ValueError("Invalid format of replace Statement: " + replaceStatement + " !!")
+    
+        # extract name and value to be used for replacement
+	paramName = paramNameValuePair[0]
+	paramValue = paramNameValuePair[1]
 
-		# extract name and value to be used for replacement
-		paramName = paramNameValuePair[0]
-		paramValue = paramNameValuePair[1]
+	if paramName == "inputFilePath":
+	    inputFilePath = paramValue
+	if paramName == "inputFileType":
+	    inputFileType = paramValue
+	if paramName == "maxEvents":
+	    replaceStatements_retVal.append(replaceStatement)
+	if paramName == "applyFactorization":
+	    factorization = None
+	    if paramValue.lower() == "false":
+	        factorization = ""
+            elif paramValue.lower() == "true":
+	        factorization = "enableFactorization_run" + channel + "(process)"
+            else:
+	        raise ValueError("Invalid factorization option = " + paramValue + " !!")
+    # check that factorization option has been defined
+    if factorization is None:
+        raise ValueError("Undefined factorization option !!")
+    replaceStatements_retVal.append("factorization = " + factorization)
+	
+    # check that the input filename path option has been defined
+    if inputFilePath is None:
+        raise ValueError("Undefined inputFilePath option !!")
+    # check that the input filename path ends with "/"
+    if not inputFilePath.endswith("/"):
+        inputFilePath += "/"
 
-		if paramName == "inputFilePath":
-			inputFilePath = paramValue
-		if paramName == "maxEvents":
-			replaceStatements_retVal.append(replaceStatement)
-		if paramName == "applyFactorization":
-			factorization = None
-			if paramValue.lower() == "false":
-				factorization = ""
-			elif paramValue.lower() == "true":
-				factorization = "enableFactorization_run" + channel + "(process)"
-			else:
-				raise ValueError("Invalid factorization option = " + paramValue + " !!")
+    # check that the input file type option has been defined
+    inputFileNames = None
+    patTupleProduction = None
+    if inputFileType is None:
+        raise ValueError("Undefined inputFileType option !!")
+    if inputFileType == "RECO/AOD":
+         inputFileNames = "fileNames" + channel + "_" + sample
+	 patTupleProduction = "process.p.replace(process.producePatTuple" + channel + "Specific, process.producePatTupleAll)"
+    elif inputFileType == "PATTuple":
+         inputFileNames = "cms.untracked.vstring('rfio:" + inputFilePath + "' + patTupleOutputFileName" + channel + "_" + sample
+	 if sample.find("_part") != -1:
+	     inputFileNames = "cms.untracked.vstring(" + inputFileNames[:inputFileNames.rfind("_part")]
+	     inputFileNames += ".value().replace(\'_partXX', '" + sample[sample.rfind("_part"):] + "'))"
+	 patTupleProduction = ""
+    else:
+	 raise ValueError("Invalid inputFileType parameter = " + inputFileType + " !!")
+    inputFileNames += ".value())"
+    replaceStatements_retVal.append("inputFileNames = " + inputFileNames)
+    replaceStatements_retVal.append("patTupleProduction = " patTupleProduction)
 
-	# check that the input filename path option has been defined
-	if inputFilePath is None:
-		raise ValueError("Undefined inputFilePath option !!")
-	# check that the input filename path ends with "/"
-	if not inputFilePath.endswith("/"):
-		inputFilePath += "/"	
-	# check that factorization option has been defined
-	if factorization is None:
-		raise ValueError("Undefined factorization option !!")
-	replaceStatements_retVal.append("factorization = " + factorization)
+    # replace genPhaseSpaceCut and plotsOutputFileName parameters
+    # (ommit "_part.." suffix of sample name in case of processes split
+    #  into multiple cmsRun job parts, in order to avoid having to specify
+    #   genPhaseSpaceCut, plotsOutputFileName and patTupleOutputFileName
+    #  again and again for each part)
+    genPhaseSpaceCut = "genPhaseSpaceCut" + sample
+    plotsOutputFileName = "plotsOutputFileName" + channel + "_" + sample
+    if sample.find("_part") != -1:
+        genPhaseSpaceCut = genPhaseSpaceCut[:genPhaseSpaceCut.rfind("_part")]
+        plotsOutputFileName = "cms.string(" + plotsOutputFileName[:plotsOutputFileName.rfind("_part")]
+        plotsOutputFileName += ".value().replace(\'_partXX', '" + sample[sample.rfind("_part"):] + "'))"
+    replaceStatements_retVal.append("genPhaseSpaceCut = " + genPhaseSpaceCut)
+    replaceStatements_retVal.append("plotsOutputFileName = " + plotsOutputFileName)
 
-	# replace genPhaseSpaceCut, plotsOutputFileName and inputFileName parameters
-	# (ommit "_part.." suffix of sample name in case of processes split
-	#  into multiple cmsRun job parts, in order to avoid having to specify
-	#   genPhaseSpaceCut, plotsOutputFileName and patTupleOutputFileName
-	#  again and again for each part)
-	genPhaseSpaceCut = "genPhaseSpaceCut" + sample
-	plotsOutputFileName = "plotsOutputFileName" + sample
-	inputFileNames = "cms.untracked.vstring('rfio:" + inputFilePath + "' + patTupleOutputFileName" + sample
-	if sample.find("_part") != -1:
-		genPhaseSpaceCut = genPhaseSpaceCut[:genPhaseSpaceCut.rfind("_part")]
-		plotsOutputFileName = "cms.string(" + plotsOutputFileName[:plotsOutputFileName.rfind("_part")]
-		plotsOutputFileName += ".value().replace(\'_partXX', '" + sample[sample.rfind("_part"):] + "'))"
-		inputFileNames = "cms.untracked.vstring(" + inputFileNames[:inputFileNames.rfind("_part")]
-		inputFileNames += ".value().replace(\'_partXX', '" + sample[sample.rfind("_part"):] + "'))"
-	inputFileNames += ".value())"
-	replaceStatements_retVal.append("genPhaseSpaceCut = " + genPhaseSpaceCut)
-	replaceStatements_retVal.append("plotsOutputFileName = " + plotsOutputFileName)
-	replaceStatements_retVal.append("inputFileNames = " + inputFileNames)
+    replacements_retVal = "; ".join(replaceStatements_retVal)
 
-	replacements_retVal = "; ".join(replaceStatements_retVal)
-
-	return replacements_retVal
+    return replacements_retVal
