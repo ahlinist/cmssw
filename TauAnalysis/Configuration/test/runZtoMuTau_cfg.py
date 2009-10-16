9e31a2f03417eaea964339c5dab7106069a17be1
veelken <>
2009-10-16 15:52:14 +0000

    removed configs for PAT-tuple output

--- TauAnalysis/Configuration/test/runZtoMuTau_cfg.py
+++ TauAnalysis/Configuration/test/runZtoMuTau_cfg.py
@@ -34,9 +34,6 @@ process.load("TauAnalysis.Configuration.analyzeZtoMuTau_cff")
 # import configuration parameters for submission of jobs to CERN batch system
 # (running over skimmed samples stored on CASTOR)
 from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_cfi import *
-
-# import event-content definition of products to be stored in patTuple
-from TauAnalysis.Configuration.patTupleEventContent_cff import *
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
@@ -63,11 +60,6 @@ process.DQMStore = cms.Service("DQMStore")
 
 process.saveZtoMuTauPlots = cms.EDAnalyzer("DQMSimpleFileSaver",
     outputFileName = cms.string('plotsZtoMuTau.root')
-)
-
-process.saveZtoMuTauPatTuple = cms.OutputModule("PoolOutputModule",                                                
-    patTupleEventContent,                                               
-    fileName = cms.untracked.string('muTauSkim_patTuple.root')
 )
 
 process.maxEvents = cms.untracked.PSet(            
