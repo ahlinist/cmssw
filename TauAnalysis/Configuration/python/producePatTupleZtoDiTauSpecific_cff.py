efec48b8685f5a5d4ed0bdebd17f0cffed0eb99d
veelken <>
2009-10-19 15:52:53 +0000

    updated Z --> tau-jet tau-jet channel to new config file structure

---
+++ TauAnalysis/Configuration/python/producePatTupleZtoDiTauSpecific_cff.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# produce all objects specific to Z --> tau-jet + tau-jet channel
+# which do **not** get stored in PATTuple
+#--------------------------------------------------------------------------------
+
+#
+# produce collections of pat::Electrons, pat::Muons and
+# pat::(PF)Taus passing different selection criteria
+#
+from TauAnalysis.RecoTools.patLeptonSelection_cff import *
+#
+# produce collection of pat::Jets passing Et threshold and
+# Eta acceptance cuts and not overlapping with any object
+# passing selection criteria for pat::Electron, pat::Muon or pat::(PF)Tau
+# (pat::Jet collection to be considered for central-jet veto)
+#
+from TauAnalysis.RecoTools.patJetSelection_cff import *
+#
+# produce collections of pat::(Calo)MET objects
+# passing different selection criteria
+#
+from TauAnalysis.RecoTools.patMetSelection_cff import *
+#
+# produce collections of muon + tau-jet pairs
+# passing different selection criteria
+#
+from TauAnalysis.CandidateTools.diTauPairProduction_cff import *
+from TauAnalysis.CandidateTools.diTauPairSelectionAllKinds_cff import *
+
+producePatTupleZtoDiTauSpecific = cms.Sequence(
+    selectLayer1Electrons
+   + selectLayer1Muons
+   + selectLayer1Taus + selectLayer1TausForDiTau
+   + produceDiTauPairs
+   + selectDiTauPairs
+   + selectLayer1Jets
+)
--- TauAnalysis/Configuration/test/runZtoDiTau_cfg.py
+++ TauAnalysis/Configuration/test/runZtoDiTau_cfg.py
@@ -7,6 +7,8 @@ process = cms.Process('runZtoDiTau')
 # of electrons, muons and tau-jets with non-standard isolation cones
 process.load('Configuration/StandardSequences/Services_cff')
 process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+#process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
 process.load('Configuration/StandardSequences/MagneticField_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
@@ -16,6 +18,7 @@ process.GlobalTag.globaltag = 'IDEAL_V12::All'
 #--------------------------------------------------------------------------------
 # import sequence for PAT-tuple production
 process.load("TauAnalysis.Configuration.producePatTuple_cff")
+process.load("TauAnalysis.Configuration.producePatTupleZtoDiTauSpecific_cff")
 
 # import sequence for event selection
 process.load("TauAnalysis.Configuration.selectZtoDiTau_cff")
@@ -24,8 +27,29 @@ process.load("TauAnalysis.Configuration.selectZtoDiTau_cff")
 # and of run + event number pairs for events passing event selection
 process.load("TauAnalysis.Configuration.analyzeZtoDiTau_cff")
 
-# import event-content definition of products to be stored in patTuple
-from TauAnalysis.Configuration.patTupleEventContent_cff import *
+# import configuration parameters for submission of jobs to CERN batch system
+# (running over skimmed samples stored on CASTOR; **not implemented for ZtoDiTau channel yet**)
+#from TauAnalysis.Configuration.recoSampleDefinitionsZtoDiTau_cfi import *
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# print memory consumed by cmsRun
+# (for debugging memory leaks)
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#    ignoreTotal = cms.untracked.int32(1) # default is one
+#)
+
+process.printGenParticleList = cms.EDAnalyzer("ParticleListDrawer",
+    src = cms.InputTag("genParticles"),
+    maxEventsToPrint = cms.untracked.int32(10)
+)
+
+# print event content 
+process.printEventContent = cms.EDAnalyzer("EventContentAnalyzer")
+
+# print debug information whenever plugins get loaded dynamically from libraries
+# (for debugging problems with plugin related dynamic library loading)
+#process.add_( cms.Service("PrintLoadingPlugins") )
 #--------------------------------------------------------------------------------
 
 process.DQMStore = cms.Service("DQMStore")
@@ -34,61 +58,26 @@ process.saveZtoDiTauPlots = cms.EDAnalyzer("DQMSimpleFileSaver",
     outputFileName = cms.string('plotsZtoDiTau.root')
 )
 
-process.saveZtoDiTauPatTuple = cms.OutputModule("PoolOutputModule",
-    patTupleEventContent,
-    fileName = cms.untracked.string('diTauSkim_patTuple.root')
-)
-
 process.maxEvents = cms.untracked.PSet(            
     input = cms.untracked.int32(-1)    
 )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-#
-# Z --> tau tau (all decay modes; simulated with TAUOLA)
-# 10k events RelVal sample
-#
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/A4A3988A-BCCB-DD11-A103-001617E30E28.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/D412FFFC-BCCB-DD11-8B20-000423D952C0.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0003/F01E4F34-BDCB-DD11-B87D-001617C3B77C.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0004/1CAA08F8-D3CB-DD11-ADF9-000423D6B358.root',
-#        '/store/relval/CMSSW_2_2_3/RelValZTT/GEN-SIM-RECO/STARTUP_V7_v4/0004/2800478C-08CC-DD11-94BB-0019B9F72BAA.root'
         'rfio:/castor/cern.ch/user/v/veelken/CMSSW_2_2_3/diTauSkim.root'
-#        'file:/afs/cern.ch/user/v/veelken/scratch0/CMSSW_2_2_7/src/TauAnalysis/Configuration/test/muTauSkim.root'
     )
     #skipBadFiles = cms.untracked.bool(True)
 )
 
 #--------------------------------------------------------------------------------
 # define "hooks" for replacing configuration parameters
-# in case running jobs on the CERN batch system:
+# in case running jobs on the CERN batch system
+# (**not implemented for ZtoDiTau channel yet**)
 #
-#---This_is_a_Hook_for_Replacement_of_fileNames_Parameter
-#
-# to be replaced by e.g.
-#
-#  "process.source.fileNames = fileNamesQCD_BCtoE_Pt20to30"
-#
-#---This_is_a_Hook_for_Replacement_of_maxEvents_Parameter
-#
-# to be replaced by e.g.
-#
-#  "process.maxEvents.input = cms.untracked.int32(100)"
-#
-#---This_is_a_Hook_for_Replacement_of_genPhaseSpaceCut_Parameter
-#
-# to be replaced by e.g.
-#
-#  "extEventSelection = cms.VPSet()
-#   extEventSelection.insert(genPhaseSpaceCutQCD_BCtoE_Pt20to30)
-#   extEventSelection.insert(process.analyzeZtoElecMu.eventSelection)
-#   process.analyzeZtoElecMu.eventSelection = extEventSelection"
-#
-#---This_is_a_Hook_for_Replacement_of_outputFileName_Parameter
-#
-# to be replaced by e.g.
-#  "process.saveZtoElecMu.outputFileName = outputFileNameQCD_BCtoE_Pt20to30"
+#__process.source.fileNames = #inputFileNames#
+#__process.maxEvents.input = cms.untracked.int32(#maxEvents#)
+#__process.analyzeZtoDiTauEvents.filters[0] = copy.deepcopy(#genPhaseSpaceCut#)
+#__process.saveZtoDiTauPlots.outputFileName = #plotsOutputFileName#
 #
 #--------------------------------------------------------------------------------
 
@@ -104,16 +93,30 @@ from PhysicsTools.PatAlgos.tools.tauTools import *
 # comment-out to take shrinking dR = 5.0/Et(PFTau) signal cone
 # instead of fixed dR = 0.07 signal cone reco::PFTaus
 # as input for pat::Tau production
-#switchToPFTauShrinkingCone(process)
-switchToPFTauFixedCone(process)
+switchToPFTauShrinkingCone(process)
+#switchToPFTauFixedCone(process)
 #--------------------------------------------------------------------------------
 
-process.p = cms.Path( process.producePatTuple
-#                    +process.content              # uncomment to enable dump of event content after PAT-tuple production
-                     +process.selectZtoDiTauEvents
-#                    +process.saveZtoDiTauPatTuple # uncomment to write-out produced PAT-tuple                      
-                     +process.analyzeZtoDiTauEvents
-                     +process.saveZtoDiTauPlots )
+process.p = cms.Path(
+    process.producePatTupleZtoDiTauSpecific
+# + process.printGenParticleList # uncomment to enable print-out of generator level particles
+# + process.printEventContent    # uncomment to enable dump of event content after PAT-tuple production
+  + process.selectZtoDiTauEvents 
+  + process.analyzeZtoDiTauEvents
+  + process.saveZtoDiTauPlots 
+)
+
+#--------------------------------------------------------------------------------
+#
+process.producePatTupleAll = cms.Sequence( process.producePatTuple + process.producePatTupleZtoDiTauSpecific )
+#
+# define "hook" for enabling/disabling production of PAT-tuple event content,
+# depending on whether RECO/AOD or PAT-tuples are used as input for analysis
+# (**not implemented for ZtoDiTau channel yet**)
+#
+#__#patTupleProduction#
+process.p.replace(process.producePatTupleZtoDiTauSpecific, process.producePatTuple + process.producePatTupleZtoDiTauSpecific)
+#--------------------------------------------------------------------------------
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
