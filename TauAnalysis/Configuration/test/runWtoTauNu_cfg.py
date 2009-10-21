1dfe72d4a59e9251922453e6df9082a9128e9859
liis <>
2009-10-21 08:35:57 +0000

    updated to new config file structure, consider both calo-MEt and PF-MEt

--- TauAnalysis/Configuration/test/runWtoTauNu_cfg.py
+++ TauAnalysis/Configuration/test/runWtoTauNu_cfg.py
@@ -14,7 +14,21 @@ process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_noesprefer_cff')
 process.GlobalTag.globaltag = 'IDEAL_V9::All'
 
-from TauAnalysis.Configuration.recoSampleDefinitionsWtoTauNu_cfi import * #import data samples
+# import configuration parameters for submission of jobs to CERN batch system
+from TauAnalysis.Configuration.recoSampleDefinitionsWtoTauNu_cfi import *
+
+#--------------------------------------------------------------------------------
+# print memory consumed by cmsRun
+# (for debugging memory leaks)
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#  ignoreTotal = cms.untracked.int32(1) # default is one
+#)
+#---------------------------------------------------------------------------------
+process.printGenParticleList = cms.EDAnalyzer("ParticleListDrawer",
+   src = cms.InputTag("genParticles"),
+   maxEventsToPrint = cms.untracked.int32(100)
+)
+
 
 # import particle data table, needed for print-out of generator level information
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -22,7 +36,6 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 #import sequences for PAT-tuple production
 process.load("TauAnalysis.Configuration.producePatTuple_cff")
 process.load("TauAnalysis.Configuration.producePatTupleWtoTauNuSpecific_cff")
-
 
 process.saveWtoTauNuPlots = cms.EDAnalyzer("DQMSimpleFileSaver",
     outputFileName = cms.string('plotsWtoTauNu.root') 
@@ -32,17 +45,26 @@ process.load("TauAnalysis.Configuration.selectWtoTauNu_cff")
 process.load("TauAnalysis.Configuration.analyzeWtoTauNu_cff")
 
 process.DQMStore = cms.Service("DQMStore")
+
+#print event content
 process.printEventContent = cms.EDAnalyzer("EventContentAnalyzer")
 
+#-------------------------------------------------------------------------------
+# print debug information whenever plugins get loaded dynamically from libraries
+# (for debugging problems with plugin related dynamic library loading)
+#process.add_( cms.Service("PrintLoadingPlugins") )
+#--------------------------------------------------------------------------------
+
 process.maxEvents = cms.untracked.PSet(            
-    input = cms.untracked.int32(2000)    
+    input = cms.untracked.int32(-1)    
 )
+
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 #    'rfio:/castor/cern.ch/user/l/liis/SelEvents/qcdEvents_passedEcalCrack.root'
-    'rfio:/castor/cern.ch/user/l/liis/SelEvents/selEvents_WtoTauNu_Wenu_part01.root',
-    'rfio:/castor/cern.ch/user/l/liis/SelEvents/selEvents_WtoTauNu_Wenu_part02.root'
-    
+   'rfio:/castor/cern.ch/user/l/liis/SelEvents/selEvents_WtoTauNu_Wtaunu_part01.root'
+#    'rfio:/castor/cern.ch/user/l/liis/SelEvents/selEvents_WtoTauNu_Wenu_part02.root'    
+#    'rfio:/castor/cern.ch/user/l/liis/SelEvents/qcdEvents_passedEcalCrack.root'
 #    'file:QCDSkim_1.root'   
 #    'file:QCD_PtTrack15_FASTSIM_1000.root'
 #    'file:/afs/cern.ch/user/l/liis/scratch0/CMSSW_2_2_13/src/TauAnalysis/Skimming/test/QCDSkim.root'
@@ -62,17 +84,18 @@ from PhysicsTools.PatAlgos.tools.tauTools import *
 switchToPFTauShrinkingCone(process)
 #switchToPFTauFixedCone(process)
 
-#------------------------Switching to PF-MET ----------------------------------
+#------------------------Switching to PF-MET and raw Calo-MET----------------------------------
+process.layer1METs.metSource = cms.InputTag('met')
+
 from TauAnalysis.Configuration.tools.metTools import *
+
 # comment-out the addPFMet() function to add pfMET: first Boolean switch on production of genMET with mu's, second Boolean switch on type-1 corrections
 addPFMet(process,True,False)
-# comment-out to replace caloMET by pfMET in all tau-Nu objects
+# replace caloMET by pfMET in all tau-Nu objects
+process.load("TauAnalysis.CandidateTools.tauNuPairProduction_cff")
 replaceMETforTauNu(process,
                                         cms.InputTag('layer1METs'),
                                         cms.InputTag('layer1PFMETs'))
-replaceMETforMet(process,
-                 cms.InputTag('layer1METs'),
-                 cms.InputTag('layer1PFMETs'))
 
 # comment-out to add genMET with mu's to layer1MET (caloMET)
 #process.layer1METs.genMETSource = cms.InputTag('genMETWithMu')
@@ -89,17 +112,13 @@ changeCut(process, "selectedLayer1ElectronsTightId","electronID('robust') > 0")
 
 process.p = cms.Path( 
     process.producePatTupleWtoTauNuSpecific
+#    +process.layer1CaloMETs
 #    +process.printGenParticleList # print-out of generator level particles
 #    +process.printEventContent    # dump of event content after PAT-tuple production
     +process.selectWtoTauNuEvents
-#    +process.saveWtoTauNuPatTuple # uncomment to write-out produced PAT-tuple
     +process.analyzeWtoTauNuEvents
     +process.saveWtoTauNuPlots 
 )
-
-#replace reco->aod. Necessary for fast-sim produced sample
-from TauAnalysis.Configuration.tools.aodTools import *
-switchToAOD(process)
 
 #--------------------------------------------------------------------------------
 # import utility function for factorization
@@ -112,6 +131,16 @@ switchToAOD(process)
 # (needs to be done after process.p has been defined)
 #__#factorization#
 #--------------------------------------------------------------------------------
+# define "hook" for enabling/disabling production of PAT-tuple event content,
+# depending on whether RECO/AOD or PAT-tuples are used as input for analysis
+#
+#__#patTupleProduction#
+process.p.replace(process.producePatTupleWtoTauNuSpecific, process.producePatTuple+process.producePatTupleWtoTauNuSpecific)
+#--------------------------------------------------------------------------------
+
+#replace reco->aod. Necessary for fast-sim produced qcd sample
+from TauAnalysis.Configuration.tools.aodTools import *
+switchToAOD(process)
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
