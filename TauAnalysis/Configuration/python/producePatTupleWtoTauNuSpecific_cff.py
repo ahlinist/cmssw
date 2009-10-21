74307de19df89c6e0b9a09b47f0719fad206a3ea
liis <>
2009-10-21 07:55:56 +0000

    add selectedPFMet to the pat-tuple

--- TauAnalysis/Configuration/python/producePatTupleWtoTauNuSpecific_cff.py
+++ TauAnalysis/Configuration/python/producePatTupleWtoTauNuSpecific_cff.py
@@ -7,20 +7,29 @@ import FWCore.ParameterSet.Config as cms
 #produce pat::tau collections
 from TauAnalysis.RecoTools.patLeptonSelection_cff import *
 
-#produce pat::jet collections
+#produce pat::jet collections (jets not overlapping with selected W-->tau nu analysis tuas)
+from TauAnalysis.RecoTools.patJetSelection_cff import *
 from TauAnalysis.RecoTools.patJetSelectionForWTauNu_cff import *
-#produce collections for pat::(calo/PF)MET objects
+#produce collections for pat::caloMET objects
 from TauAnalysis.RecoTools.patMetSelection_cff import *
+#produce collections for pat::PFMET objects
+from TauAnalysis.RecoTools.patPFMetSelection_cff import *
 #produce collections for tau+met pairs
 from TauAnalysis.CandidateTools.tauNuPairProduction_cff import *
 #produce collections for tau recoil energy
 from TauAnalysis.RecoTools.tauRecoilEnergy_cff import *
 
+
 producePatTupleWtoTauNuSpecific = cms.Sequence(
     selectLayer1TausForWTauNu+
     selectLayer1TausForWTauNuLooseIsolation+
-    selectLayer1JetsForWTauNu+
+    selectLayer1SelJetsForWTauNu+
     selectLayer1METs+
+    selectLayer1PFMETs+
     produceTauNuPairs+
-    produceTauRecoilEnergy
+    produceTauRecoilEnergy+    
+    selectLayer1Electrons+    
+    selectLayer1Muons+
+    selectLayer1Taus+
+    selectLayer1Jets
 )
