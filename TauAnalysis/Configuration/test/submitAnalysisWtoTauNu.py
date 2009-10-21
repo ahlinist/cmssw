031739032a5abb3d5f41708304c1ee8fe80a4c26
liis <>
2009-10-21 10:30:13 +0000

    updated according to new generic PAT-tuple + channel specific objects structure

--- TauAnalysis/Configuration/test/submitAnalysisWtoTauNu.py
+++ TauAnalysis/Configuration/test/submitAnalysisWtoTauNu.py
@@ -7,6 +7,11 @@ from TauAnalysis.Configuration.makeReplacementsAnalysis import makeReplacementsA
 #outputDirectory = "/afs/cern.ch/user/l/liis/scratch0/CMSSW_2_2_13/src/TauAnalysis/Configuration/test/TESTING"
 outputDirectory = "/castor/cern.ch/user/l/liis/WTauNuPlots"
 
+inputFileType = "RECO/AOD"
+#inputFileType = "PATTuple"
+
+inputFilePath = "/castor/cern.ch/user/l/liis/SkimJuly09/WtaunuSkim_PFCaloTauMet"
+
 #--------------------------------------------------------------------------------
 # NOTE: The jobs get submitted to the '1nd' queue,
 #       which allows for an execution time of the cmsRun jobs of up to 24 hours
@@ -17,34 +22,34 @@ outputDirectory = "/castor/cern.ch/user/l/liis/WTauNuPlots"
 # W --> tau nu jobs
 for i in range(4): #4
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "Wtaunu_part%(i)02d" % {"i" : (i + 1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
                   job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 
 #QCD jobs
 for i in range(33): #33
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "qcd_W_part%(i)02d" % {"i" : (i+1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
                   job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 
 # W --> mu nu jobs
 for i in range(13):#13
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "Wmunu_part%(i)02d" % {"i" : (i + 1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
                   job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 
 # W --> e nu jobs
 for i in range(10):#10
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "Wenu_part%(i)02d" % {"i" : (i + 1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
                   job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 
 # Z + jets jobs
 for i in range(10): #10
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "ZplusJets_part%(i)02d" % {"i" : (i + 1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
                   job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
 # Ttbar jobs
 for i in range(18): #18
     submitToBatch(configFile = "runWtoTauNu_cfg.py", channel = "WtoTauNu", sample = "TTplusJets_part%(i)02d" % {"i" : (i + 1)},
-                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; applyFactorization = false",
+                  replFunction = makeReplacementsAnalysis, replacements = "maxEvents = -1; inputFileType = "+inputFileType+";inputFilePath = "+inputFilePath+"; applyFactorization = false",
 		  job = "analysis", queue = "1nd", outputDirectory = outputDirectory)
