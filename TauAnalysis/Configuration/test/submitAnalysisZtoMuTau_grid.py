2fa75878f6ce526b015f840dc77c09f85b23f7a5
veelken <>
2009-10-23 14:01:13 +0000

    improved tabbing

--- TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
@@ -3,7 +3,7 @@
 from TauAnalysis.Configuration.submitToGrid import submitToGrid
 from TauAnalysis.Configuration.makeReplacementsCrab import makeReplacementsAnalysis
 
-from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_cfi import *
+from TauAnalysis.Configuration.recoSampleDefinitionsZtoElecMu_cfi import *
 
 # name of the directory (either on afs area or castor)
 # to which all .root files produced by the cmsRun job will be copied
@@ -11,8 +11,10 @@ outputDirectory = "/castor/cern.ch/user/v/veelken/plots/ZtoMuTau_grid/"
 #outputDirectory = "/castor/cern.ch/user/l/lusito/ZMuTauAnalysis/"
 
 # small cmsRun job for testing purposes...
-submitToGrid(configFile = "runZtoElecMu_cfg.py", channel = "ZtoElecMu", sample = "Ztautau",
+submitToGrid(configFile = "runZtoElecMu_cfg.py", channel = "ZtoElecMu",
+             sample = "Ztautau",
              dbs_name = "/Ztautau/sunil-Summer08_Skim-411de0fd9a45f2e9090c88c36bde639d/USER",
              dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_local_09/servlet/DBSServlet",             
-             replFunction = makeReplacementsAnalysis, replacements = "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false",
-             outputFiles = [ plotsOutputFileNameZtautau ], outputDirectory = outputDirectory)
+             replFunction = makeReplacementsAnalysis, replacements =
+             "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false",
+             outputFiles = [ plotsOutputFileNameZtoElecMu_Ztautau ], outputDirectory = outputDirectory)
