e80b15e59f011e4a13ffb49a32ddceeca6d5bbda
veelken <>
2009-10-23 13:56:36 +0000

    minor clean-up

--- TauAnalysis/Configuration/python/submitToGrid.py
+++ TauAnalysis/Configuration/python/submitToGrid.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 
-#from TauAnalysis.Configuration.makeReplacementsCrab import makeReplacementsCrab
 from TauAnalysis.Configuration.prepareConfigFile import prepareConfigFile
 
 #--------------------------------------------------------------------------------
