daa6d60c2a1ba30b44624df3262902e9176fbb32
veelken <>
2009-11-02 11:06:05 +0000

    fixed bug in computation of event weights for fake-rate technique

--- TauAnalysis/BgEstimationTools/plugins/FakeRateEventWeightProducer.cc
+++ TauAnalysis/BgEstimationTools/plugins/FakeRateEventWeightProducer.cc
@@ -32,35 +32,36 @@ void FakeRateEventWeightProducer::produce(edm::Event& evt, const edm::EventSetup
   edm::Handle<edm::View<reco::Candidate> > preselTauJets;
   evt.getByLabel(preselTauJetSource_, preselTauJets);
 
-  double tauJetIdEffSum = 0.;
-  double qcdJetFakeRateSum = 0.;
-  unsigned numTauJetDiscrPassed = 0;
+  double fakeRateJetWeightSum = 0.;
 
   unsigned numTauJets = allTauJets->size();
   for ( unsigned iTauJet = 0; iTauJet < numTauJets; ++iTauJet ) {
     edm::RefToBase<reco::BaseTau> tauJetRef = allTauJets->refAt(iTauJet);
-
+    
     double tauJetIdEff = 1.;
     double qcdJetFakeRate = 1.;
-
+    
     bool tauJetDiscr_passed = true;
-
+    
     getTauJetProperties(evt, tauJetRef, iTauJet, preselTauJets, tauJetIdEff, qcdJetFakeRate, tauJetDiscr_passed);
+    
+    double fakeRateJetWeight = 0.;
+    
+    if ( method_ == "simple" ) {
+      fakeRateJetWeight = qcdJetFakeRate;
+    } else if ( method_ == "CDF" ) {
+      if ( tauJetIdEff > qcdJetFakeRate ) {
+	fakeRateJetWeight = ( tauJetDiscr_passed ) ? 
+	  -qcdJetFakeRate*(1 - tauJetIdEff)/(tauJetIdEff - qcdJetFakeRate) : qcdJetFakeRate*tauJetIdEff/(tauJetIdEff - qcdJetFakeRate);
+      }
+    }
 
-    tauJetIdEffSum += tauJetIdEff;
-    qcdJetFakeRateSum += qcdJetFakeRate;
+    //std::cout << " --> jet weight = " << fakeRateJetWeight << std::endl;
 
-    if ( tauJetDiscr_passed ) ++numTauJetDiscrPassed;
+    fakeRateJetWeightSum += fakeRateJetWeight;
   }
 
-  double effTerm = 0.5*(tauJetIdEffSum + qcdJetFakeRateSum);
-  double frTerm = qcdJetFakeRateSum;
-
-  double fakeRateEventWeight = 0.;
-  if ( effTerm > frTerm ) {
-    fakeRateEventWeight = ( numTauJetDiscrPassed > 0 ) ?
-      -frTerm*(1 - effTerm)/(effTerm - frTerm) : frTerm*effTerm/(effTerm - frTerm);
-  }
+  double fakeRateEventWeight = fakeRateJetWeightSum;
 
   //std::cout << " --> event weight = " << fakeRateEventWeight << std::endl;
 
