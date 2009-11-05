e1a91612a12b05d0a2666b95124774c067fcc033
liis <>
2009-11-05 14:23:34 +0000

    updated for skimming the CMSSW_3_1_x samples

--- TauAnalysis/Skimming/test/skimWTauNu_cfg.py
+++ TauAnalysis/Skimming/test/skimWTauNu_cfg.py
@@ -1,96 +1,93 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("wTauNuSkim1")
+process = cms.Process("muTauSkim")
 
 from TauAnalysis.Skimming.EventContent_cff import *
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.FakeConditions_cff")
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = cms.string('MC_31X_V2::All')
 process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(-1)
 )
 
 process.source = cms.Source("PoolSource",
-  fileNames = cms.untracked.vstring(
-'file:ttbarSkim.root'
-#    'file:../../Configuration/test/QCD_PtTrack15_FASTSIM_1000.root'
-#  'rfio:/castor/cern.ch/user/l/liis/SkimJuly09/WtaunuSkim_PFCaloTauMet/WtaunuSkim_1.root'  
-  )
+    fileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_3_1_2/RelValZTT/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/A4DD1FAE-B178-DE11-B608-001D09F24EAC.root',
+        '/store/relval/CMSSW_3_1_2/RelValZTT/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/9408B54D-CB78-DE11-9AEB-001D09F2503C.root'
+    )
 )
 
 #--------------------------------------------------------------------------------
-# select taus and met
+# select met and tau-jets
 #--------------------------------------------------------------------------------
-
 
 process.selectedPFMet = cms.EDFilter("PFMEtSelector",
-  src = cms.InputTag('pfMet'),
-  cut = cms.string("pt > 10"),
-  filter = cms.bool(True)
-)
+                                     src = cms.InputTag('pfMet'),
+                                     cut = cms.string("pt > 15"),
+                                     filter = cms.bool(True)
+                                     )
 
 process.selectedCaloMet = cms.EDFilter("CaloMEtSelector",
-   src = cms.InputTag('met'),
-   cut = cms.string("pt > 10"),
-   filter = cms.bool(True)
+                                       src = cms.InputTag('met'),
+                                       cut = cms.string("pt > 15"),
+                                       filter = cms.bool(True)
+                                       )
+
+process.selectedPFTaus = cms.EDFilter("PFTauSelector",
+    src = cms.InputTag('shrinkingConePFTauProducer'),
+    discriminators = cms.VPSet(
+        cms.PSet(
+            discriminator = cms.InputTag("shrinkingConePFTauDiscriminationByLeadingTrackPtCut"),
+            selectionCut = cms.double(0.5)
+        )
+    ),
+    cut = cms.string("abs(eta) < 2.5 & leadTrack().isNonnull() & leadTrack().pt() > 15."),
+    filter = cms.bool(True)
 )
 
-                           
-process.leadTrPFTaus = cms.EDFilter("PFTauSelector",
-  src = cms.InputTag('pfRecoTauProducer'),
-  discriminators = cms.VPSet(
-    cms.PSet(
-      discriminator = cms.InputTag("pfRecoTauDiscriminationByLeadingTrackPtCut"),
-      selectionCut = cms.double(0.5)
-    )
-  ),
-  cut = cms.string("abs(eta) < 2.5 & leadPFChargedHadrCand().isNonnull() & leadPFChargedHadrCand().pt() > 15 "),                                    
-  filter = cms.bool(True)
+process.selectedCaloTaus = cms.EDFilter("CaloTauSelector",
+    src = cms.InputTag('caloRecoTauProducer'),
+    discriminators = cms.VPSet(
+        cms.PSet(
+            discriminator = cms.InputTag("caloRecoTauDiscriminationByLeadingTrackPtCut"),
+            selectionCut = cms.double(0.5)
+        )
+    ),
+    cut = cms.string("abs(eta) < 2.5 & leadTrack().isNonnull() & leadTrack().pt() > 15."),
+    filter = cms.bool(True)
 )
-
-process.leadTrCaloTaus = cms.EDFilter("CaloTauSelector",
-  src = cms.InputTag('caloRecoTauProducer'),
-  discriminators = cms.VPSet(
-    cms.PSet(
-      discriminator = cms.InputTag("caloRecoTauDiscriminationByLeadingTrackPtCut"),
-      selectionCut = cms.double(0.5)
-    )
-  ),
-  cut = cms.string("abs(eta) < 2.5 & leadTrack().isNonnull() & leadTrack().pt() > 15"),
-  filter = cms.bool(True)
-)
-
 
 #--------------------------------------------------------------------------------
-# keep event in case it passed either the pfTau or caloTau selection
+# keep event in case it passed either the MET + pfTau or MET + caloTau selection
 #--------------------------------------------------------------------------------
 
-process.PFTauSkimPath = cms.Path(
-    (process.leadTrPFTaus + process.selectedPFMet)
+process.PFTauNuSkimPath = cms.Path(
+    (process.selectedPFTaus + process.selectedPFMet)
 )
 
-process.CaloTauSkimPath = cms.Path(
-    (process.leadTrCaloTaus  + process.selectedCaloMet)
+process.CaloTauNuSkimPath = cms.Path(
+    (process.selectedCaloTaus + process.selectedCaloMet)
 )
 
 wTauNuEventSelection = cms.untracked.PSet(
-  SelectEvents = cms.untracked.PSet(
-    SelectEvents = cms.vstring('PFTauSkimPath','CaloTauSkimPath')
-  )
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('PFTauNuSkimPath', 'CaloTauNuSkimPath')
+    )
 )
 
 process.wTauNuSkimOutputModule = cms.OutputModule("PoolOutputModule",                                 
-  tauAnalysisEventContent,                                               
-  wTauNuEventSelection,
-  fileName = cms.untracked.string('ttbarSkim.root')
+    tauAnalysisEventContent,                                               
+    wTauNuEventSelection,
+    fileName = cms.untracked.string('wTauNuSkim.root')
 )
 
 process.options = cms.untracked.PSet(
-  wantSummary = cms.untracked.bool(True)
+    wantSummary = cms.untracked.bool(True)
 )
 
 process.o = cms.EndPath( process.wTauNuSkimOutputModule )
+
