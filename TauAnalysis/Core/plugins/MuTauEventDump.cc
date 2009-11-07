cb934eef0e1de7205a71d936f1e0095625781dc4
veelken <>
2009-11-07 14:23:53 +0000

    added print-out of Zmumu hypotheses

--- TauAnalysis/Core/plugins/MuTauEventDump.cc
+++ TauAnalysis/Core/plugins/MuTauEventDump.cc
@@ -4,15 +4,27 @@
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
+#include "DataFormats/Common/interface/Handle.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/ZllHypothesisT1T2.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
+
 #include "TauAnalysis/Core/interface/eventDumpAuxFunctions.h"
 #include "TauAnalysis/DQMTools/interface/generalAuxFunctions.h"
 
 #include <TMath.h>
 
+edm::InputTag getInputTag(const edm::ParameterSet& cfg, const char* parameter)
+{
+  return ( cfg.exists(parameter) ) ? cfg.getParameter<edm::InputTag>(parameter) : edm::InputTag();
+}
+
 MuTauEventDump::MuTauEventDump(const edm::ParameterSet& cfg)
   : GenericEventDump(cfg)
 {
   //std::cout << "<MuTauEventDump::MuTauEventDump>:" << std::endl;
+
+  muTauZmumuHypothesisSource_ = getInputTag(cfg, "muTauZmumuHypothesisSource");
+  diMuZmumuHypothesisSource_ = getInputTag(cfg, "diMuZmumuHypothesisSource");
 }
 
 MuTauEventDump::~MuTauEventDump()
@@ -48,10 +60,57 @@ void MuTauEventDump::print(const edm::Event& iEvent, const edm::EventSetup& iSet
   printTauInfo(iEvent);
 
   printDiTauCandidateInfo(iEvent);
+  printMuTauZmumuHypothesisInfo(iEvent);
+  printDiMuZmumuHypothesisInfo(iEvent);
 
   printMissingEtInfo(iEvent);
 
   printJetInfo(iEvent);
+}
+
+void MuTauEventDump::printMuTauZmumuHypothesisInfo(const edm::Event& iEvent) const
+{
+  if ( muTauZmumuHypothesisSource_.label() == "" ) return;
+
+  typedef std::vector<ZllHypothesisT1T2<pat::Muon, pat::Tau> > ZmumuHypothesisCollection;
+  edm::Handle<ZmumuHypothesisCollection> ZmumuHypotheses;
+  iEvent.getByLabel(muTauZmumuHypothesisSource_, ZmumuHypotheses);
+
+  for ( ZmumuHypothesisCollection::const_iterator ZmumuHypothesis = ZmumuHypotheses->begin();
+	ZmumuHypothesis != ZmumuHypotheses->end(); ++ZmumuHypothesis ) {
+    *outputStream_ << "best matching muon:"
+		   << " Pt = " << ZmumuHypothesis->p4Lepton1bestMatch().pt() << "," 
+		   << " eta = " << ZmumuHypothesis->p4Lepton1bestMatch().eta() << ","
+		   << " phi = " << ZmumuHypothesis->p4Lepton1bestMatch().phi()*180./TMath::Pi() << std::endl;
+    *outputStream_ << "best matching tau-jet:"
+		   << " Pt = " << ZmumuHypothesis->p4Lepton2bestMatch().pt() << "," 
+		   << " eta = " << ZmumuHypothesis->p4Lepton2bestMatch().eta() << ","
+		   << " phi = " << ZmumuHypothesis->p4Lepton2bestMatch().phi()*180./TMath::Pi() << std::endl;
+    reco::Particle::LorentzVector hypotheticZmumuMomentum = ZmumuHypothesis->p4Lepton1bestMatch() + ZmumuHypothesis->p4Lepton2bestMatch();
+    *outputStream_ << "hypothetic Z mass of muon + tau-jet combination = " << hypotheticZmumuMomentum.mass() << std::endl;
+  }
+}
+
+void MuTauEventDump::printDiMuZmumuHypothesisInfo(const edm::Event& iEvent) const
+{
+  if ( diMuZmumuHypothesisSource_.label() == "" ) return;
+
+  typedef std::vector<CompositePtrCandidateT1T2MEt<reco::Candidate, reco::Candidate> > ZmumuHypothesisCollection;
+  edm::Handle<ZmumuHypothesisCollection> ZmumuHypotheses;
+  iEvent.getByLabel(diMuZmumuHypothesisSource_, ZmumuHypotheses);
+
+  for ( ZmumuHypothesisCollection::const_iterator ZmumuHypothesis = ZmumuHypotheses->begin();
+	ZmumuHypothesis != ZmumuHypotheses->end(); ++ZmumuHypothesis ) {
+    *outputStream_ << "muon(1):"
+		   << " Pt = " << ZmumuHypothesis->leg1()->pt() << "," 
+		   << " eta = " << ZmumuHypothesis->leg1()->eta() << ","
+		   << " phi = " << ZmumuHypothesis->leg1()->phi()*180./TMath::Pi() << std::endl;
+    *outputStream_ << "muon(2):"
+		   << " Pt = " << ZmumuHypothesis->leg2()->pt() << "," 
+		   << " eta = " << ZmumuHypothesis->leg2()->eta() << ","
+		   << " phi = " << ZmumuHypothesis->leg2()->phi()*180./TMath::Pi() << std::endl;
+    *outputStream_ << "hypothetic Z mass of muon + muon combination = " << ZmumuHypothesis->p4Vis().mass() << std::endl;
+  }
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
