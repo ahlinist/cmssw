54638b2766c60d6d83b01cfa5f35796adc5535a2
veelken <>
2009-11-07 15:13:16 +0000

    added sequence for production of Zmumu hypotheses from muon + muon combinations (forward-port from B2_2_X into CMSSW_3_1_x branch)

---
+++ TauAnalysis/RecoTools/python/diMuPairZmumuHypothesis_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# produce combinations of muon + muon pairs,
+# the hypothesis being that the pair of muons results from a Z --> mu+ mu- decay
+#--------------------------------------------------------------------------------
+
+# require muon candidates considered for Z --> mu+ mu- hypothesis
+# to be reconstructed in muon system
+# (with or without a track reconstructed in Pixel/SiStrip tracking detectors linked to it)
+selectedLayer1MuonsForZmumuHypotheses = cms.EDFilter("PATMuonSelector",
+    src = cms.InputTag("allLayer1Muons"),
+    cut = cms.string('isGlobalMuon() | isStandAloneMuon()'),
+    filter = cms.bool(False)
+)
+
+allDiMuPairZmumuHypotheses = cms.EDProducer("DiCandidatePairProducer",
+    useLeadingTausOnly = cms.bool(False),
+    srcLeg1 = cms.InputTag('selectedLayer1MuonsForZmumuHypotheses'),
+    srcLeg2 = cms.InputTag('selectedLayer1MuonsForZmumuHypotheses'),
+    dRmin12 = cms.double(0.5),
+    srcMET = cms.InputTag(''),
+    recoMode = cms.string(""),
+    verbosity = cms.untracked.int32(0)
+)
+
+selectedDiMuPairZmumuHypotheses = cms.EDFilter("DiCandidatePairSelector",
+    src = cms.InputTag("allDiMuPairZmumuHypotheses"),                                   
+    cut = cms.string('p4Vis.mass > 80. & p4Vis.mass < 100.'),
+    filter = cms.bool(False)
+)
+
+produceDiMuPairs = cms.Sequence( selectedLayer1MuonsForZmumuHypotheses * allDiMuPairZmumuHypotheses * selectedDiMuPairZmumuHypotheses )
