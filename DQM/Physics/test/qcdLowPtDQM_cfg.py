bf7e8d61454de34e67fa6e5d6fda48eb392c67a9
loizides <>
2009-11-11 16:01:00 +0000

    First attempt to factorize test and config

--- DQM/Physics/test/qcdLowPtDQM_cfg.py
+++ DQM/Physics/test/qcdLowPtDQM_cfg.py
@@ -1,4 +1,4 @@
-# $Id:$
+# $Id: qcdLowPtDQM_cfg.py,v 1.1 2009/11/06 16:56:57 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -10,13 +10,15 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('DQM/Physics/qcdLowPtDQM_cfi')
 
 process.GlobalTag.globaltag = 'MC_31X_V9::All'
 
 process.dqmSaver.workflow = cms.untracked.string('/Physics/QCDPhysics/LowPt')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(2500)
+    input = cms.untracked.int32(-1)
+#    input = cms.untracked.int32(500)
 )
 
 process.source = cms.Source("PoolSource",
@@ -25,50 +27,4 @@ process.source = cms.Source("PoolSource",
     )
 )
 
-process.load('EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi')
-process.load('RecoLocalTracker.Configuration.RecoLocalTracker_cff')
-process.load('RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff')
-
-#import RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cfi
-#process.load('RecoPixelVertexing.PixelVertexFinding.PixelVertexes')
-#pixel3Vertices = RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cfi.pixelVertices.clone();
-#pixel3Vertices.TrackCollection = 'pixel3ProtoTracks'
-#pixel3Vertices.UseError = True
-#pixel3Vertices.WtAverage = True
-#pixel3Vertices.ZOffset = 5.
-#pixel3Vertices.ZSeparation = 0.3
-#pixel3Vertices.NTrkMin = 3
-#pixel3Vertices.PtMin = 0.15
-
-#import RecoPixelVertexing.PixelLowPtUtilities.AllPixelTracks_cfi
-#pixel3ProtoTracks = RecoPixelVertexing.PixelLowPtUtilities.AllPixelTracks_cfi.allPixelTracks.clone()
-
-#pixelVertexFromClusters = cms.EDProducer('PixelVertexProducerClusters')
-
-#dump = cms.EDAnalyzer('EventContentAnalyzer')
-
-#from RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff import *
-
-process.mySiPixelRecHits = cms.EDFilter("SiPixelRecHitConverter",
-    src = cms.InputTag("siPixelClusters"),
-    CPE = cms.string('PixelCPETemplateReco'),
-    VerboseLevel = cms.untracked.int32(0),
-)
-
-myRecoSequence = cms.Sequence(process.siPixelDigis*
-                              process.siPixelClusters*
-                              process.siPixelRecHits)
-#                            pixel3ProtoTracks *
-#                            pixel3Vertices *
-#                            pixelVertexFromClusters)
-
-process.siPixelDigis.InputLabel = cms.InputTag("rawDataCollector")
-
-process.QcdLowPtDQM = cms.EDAnalyzer("QcdLowPtDQM",
-    hltTrgNames  = cms.untracked.vstring('HLT_MinBiasHcal',
-                                         'HLT_MinBiasEcal',
-                                         'HLT_MinBiasPixel'),
-    verbose      = cms.untracked.int32(0),                                     
-)
-
-process.p = cms.Path(myRecoSequence*process.QcdLowPtDQM+process.dqmSaver)
+process.p = cms.Path(process.myRecoSequence*process.QcdLowPtDQM+process.dqmSaver)
