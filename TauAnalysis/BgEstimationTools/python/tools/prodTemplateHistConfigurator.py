f13b31a01dbb16cc8c2df5fba368197af787c01e
veelken <>
2009-11-17 15:17:03 +0000

    added utility function to configure whole template histogram production sequence

--- TauAnalysis/BgEstimationTools/python/tools/prodTemplateHistConfigurator.py
+++ TauAnalysis/BgEstimationTools/python/tools/prodTemplateHistConfigurator.py
@@ -23,6 +23,7 @@ class prodTemplateHistConfigurator(cms._ParameterTypeBase):
         self.numBinsX = dict()
         self.xMin = dict()
         self.xMax = dict()
+        self.xBins = dict()
         self.sequence = None
 
     def addProcess(self, processName, fileNames):
@@ -32,11 +33,20 @@ class prodTemplateHistConfigurator(cms._ParameterTypeBase):
         self.selections[processName] = treeSelection
         self.kineEventReweights[processName] = kineEventReweight
 
-    def addTemplate(self, meName, branchName, numBinsX, xMin, xMax):
+    def addTemplate(self, meName, branchName, numBinsX, xMin = None, xMax = None, xBins = None):
+        # check that either xMin and xMax or xBins parameters (but not all) are specified
+        if (xMin is None or xMax is None) and xBins is None:
+            raise ValueError("Definition of either 'xMin' and 'xMax' Parameters or 'xBins' Parameter missing !!")
+        if (xMin is not None or xMax is not None) and xBins is not None:
+            raise ValueError("Duplicate definition of 'xMin', 'xMax' and 'xBins' Parameters !!")
+        
         self.branchNames[meName] = branchName
-        self.numBinsX[meName] = numBinsX
-        self.xMin[meName] = xMin
-        self.xMax[meName] = xMax
+        self.numBinsX[meName] = numBinsX       
+        if xMin is not None and xMax is not None:
+            self.xMin[meName] = xMin
+            self.xMax[meName] = xMax
+        if xBins is not None:
+            self.xBins[meName] = xBins
 
     def configure(self, process):
         for processName_sel, treeSelection in self.selections.items():
@@ -72,8 +82,11 @@ class prodTemplateHistConfigurator(cms._ParameterTypeBase):
                     setattr(configEntry, "meName", cms.string(dqmDirectory_process + "/" + meName))
                     setattr(configEntry, "branchName", cms.string(branchName))
                     setattr(configEntry, "numBinsX", cms.uint32(self.numBinsX[meName]))
-                    setattr(configEntry, "xMin", cms.double(self.xMin[meName]))
-                    setattr(configEntry, "xMax", cms.double(self.xMax[meName]))
+                    if self.xMin.get(meName) is not None and self.xMax.get(meName) is not None:
+                        setattr(configEntry, "xMin", cms.double(self.xMin[meName]))
+                        setattr(configEntry, "xMax", cms.double(self.xMax[meName]))
+                    if self.xBins.get(meName) is not None:
+                        setattr(configEntry, "xBins", cms.vdouble(self.xBins[meName]))
 
                     config.append(configEntry)
 
@@ -86,3 +99,38 @@ class prodTemplateHistConfigurator(cms._ParameterTypeBase):
                     self.sequence *= module
 
         return cms.Sequence(self.sequence)                 
+
+def makeTemplateHistProdSequence(process, moduleTemplate, dataSets, bgEstEventSelections, branchNames, kineEventReweights,
+                                 dqmDirectory, meName, numBinsX, xMin = None, xMax = None, xBins = None):
+    
+    sequence = None
+    isFirstModule = True
+
+    for processName, branchName in branchNames.items():
+ 
+        moduleName = "prodTemplateHistBgEst" + processName + "Enriched" + "_" + meName
+
+        configurator = prodTemplateHistConfigurator(
+            moduleName, moduleTemplate, dqmDirectory
+        )
+
+        for dataSetName, fileNames in dataSets.items():
+            configurator.addProcess(dataSetName, fileNames)
+
+        bgEstEventSelection = bgEstEventSelections[processName]
+        kineEventReweight = kineEventReweights[processName]
+        configurator.addSelection(processName, bgEstEventSelection, kineEventReweight)
+
+        branchName = branchNames[processName]
+        configurator.addTemplate(meName, branchName, numBinsX, xMin = xMin, xMax = xMax, xBins = xBins)
+
+        module = configurator.configure(process)
+        setattr(process, moduleName, module)
+
+        if isFirstModule:
+            sequence = cms.Sequence( module )
+            isFirstModule = False
+        else:    
+            sequence._seq = sequence._seq * module
+
+    return sequence
