6f80a9a088434e439735b3f3a8ef9f3e9538f295
veelken <>
2009-11-18 14:32:16 +0000

    added possibility to extract charge of either leg1 or leg2 of diTau object

--- TauAnalysis/BgEstimationTools/plugins/PATDiTauLeptonIpExtractor.cc
+++ TauAnalysis/BgEstimationTools/plugins/PATDiTauLeptonIpExtractor.cc
@@ -2,6 +2,8 @@
 
 #include "DataFormats/Common/interface/View.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 
@@ -11,9 +13,16 @@ const double substituteValue = 1.e+3;
 
 template <typename T1, typename T2>
 PATDiTauLeptonIpExtractor<T1,T2>::PATDiTauLeptonIpExtractor(const edm::ParameterSet& cfg)
+  : error_(0)
 {
   src_ = cfg.getParameter<edm::InputTag>("src");
   vertexSrc_ = cfg.getParameter<edm::InputTag>("vertexSource");
+
+  leg_ = cfg.getParameter<unsigned>("leg");
+  if ( leg_ != 1 && leg_ != 2 ) {
+    edm::LogError("PATDiTauLeptonIpExtractor") << " Configuration parameter 'leg' = " << leg_ << " invalid !!";
+    error_ = 1;
+  }
 
   index_ = ( cfg.exists("index") ) ? cfg.getParameter<unsigned>("index") : 0;
 }
@@ -27,6 +36,8 @@ PATDiTauLeptonIpExtractor<T1,T2>::~PATDiTauLeptonIpExtractor()
 template <typename T1, typename T2>
 double PATDiTauLeptonIpExtractor<T1,T2>::operator()(const edm::Event& evt) const
 {
+  if ( error_ ) return substituteValue;
+
   typedef CompositePtrCandidateT1T2MEt<T1,T2> diTauType;
   typedef edm::View<diTauType> diTauCollectionType;
   edm::Handle<diTauCollectionType> diTaus;
@@ -46,7 +57,9 @@ double PATDiTauLeptonIpExtractor<T1,T2>::operator()(const edm::Event& evt) const
 
     const reco::Vertex& thePrimaryEventVertex = (*primaryEventVertexCollection->begin());
 
-    const reco::Track* track = trackExtractor_(*diTauPtr->leg1());
+    const reco::Track* track = 0;
+    if ( leg_ == 1 ) track = trackExtractorLeg1_(*diTauPtr->leg1());
+    else if ( leg_ == 2 ) track = trackExtractorLeg2_(*diTauPtr->leg2());
     if ( track ) {
       double ip = track->dxy(thePrimaryEventVertex.position());
 
@@ -61,14 +74,14 @@ double PATDiTauLeptonIpExtractor<T1,T2>::operator()(const edm::Event& evt) const
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/Tau.h"
 
-typedef PATDiTauLeptonIpExtractor<pat::Electron, pat::Tau> PATElecTauPairElectronIpExtractor;
-typedef PATDiTauLeptonIpExtractor<pat::Electron, pat::Muon> PATElecMuPairElectronIpExtractor;
-typedef PATDiTauLeptonIpExtractor<pat::Electron, pat::Muon> PATElecMuPairMuonIpExtractor;
-typedef PATDiTauLeptonIpExtractor<pat::Muon, pat::Tau> PATMuTauPairMuonIpExtractor;
+typedef PATDiTauLeptonIpExtractor<pat::Electron, pat::Tau> PATElecTauPairLeptonIpExtractor;
+typedef PATDiTauLeptonIpExtractor<pat::Electron, pat::Muon> PATElecMuPairLeptonIpExtractor;
+typedef PATDiTauLeptonIpExtractor<pat::Muon, pat::Tau> PATMuTauPairLeptonIpExtractor;
+typedef PATDiTauLeptonIpExtractor<pat::Tau, pat::Tau> PATDiTauPairLeptonIpExtractor;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATElecTauPairElectronIpExtractor, "PATElecTauPairElectronIpExtractor");
-DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATElecMuPairElectronIpExtractor, "PATElecMuPairElectronIpExtractor");
-DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATElecMuPairMuonIpExtractor, "PATElecMuPairMuonIpExtractor");
-DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATMuTauPairMuonIpExtractor, "PATMuTauPairMuonIpExtractor");
+DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATElecTauPairLeptonIpExtractor, "PATElecTauPairLeptonIpExtractor");
+DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATElecMuPairLeptonIpExtractor, "PATElecMuPairLeptonIpExtractor");
+DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATMuTauPairLeptonIpExtractor, "PATMuTauPairLeptonIpExtractor");
+DEFINE_EDM_PLUGIN(ObjValExtractorPluginFactory, PATDiTauPairLeptonIpExtractor, "PATDiTauPairLeptonIpExtractor");
