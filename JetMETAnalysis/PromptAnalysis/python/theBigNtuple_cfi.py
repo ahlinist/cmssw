d290c1fc506ca170799b528b5648db298fd04446
apresyan <>
2009-11-23 10:15:59 +0000

    Include BeamHalo Variables

--- JetMETAnalysis/PromptAnalysis/python/theBigNtuple_cfi.py
+++ JetMETAnalysis/PromptAnalysis/python/theBigNtuple_cfi.py
@@ -3,25 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from JetMETAnalysis.PromptAnalysis.PromptAna_Event_cfi import *
 from JetMETAnalysis.PromptAnalysis.PromptAna_MET_cfi import *
 from JetMETAnalysis.PromptAnalysis.PromptAna_Jet_cfi import *
-
-promptanaTree = cms.EDAnalyzer("PromptAnaTree",
-    outputCommands = cms.untracked.vstring(
-    'drop *',
-    'keep *_promptanaevent_*_*',
-    'keep *_promptanamet_*_*',
-    'keep *_promptananohf_*_*',
-    'keep *_promptanaic5calojet_*_*',
-    'keep *_promptanakt4calojet_*_*',
-    'keep *_promptanasc5calojet_*_*',
-#    'keep *_promptanaakt5calojet_*_*'
-    ))
-
-theBigNtuple = cms.Sequence( (
-    promptanaevent +
-    promptanamet   +
-    promptananohf  +
-    promptanaic5calojet +
-    promptanakt4calojet +
-    promptanasc5calojet #+
-#    promptanaakt5calojet
-    ) * promptanaTree )
+from JetMETAnalysis.PromptAnalysis.PromptAna_BeamHalo_cfi import *
