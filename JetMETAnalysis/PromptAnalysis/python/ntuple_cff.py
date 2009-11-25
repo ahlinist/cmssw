1e754c58528fd4ad464e1e9692ada4dd8364fc1e
santanas <>
2009-11-25 10:51:12 +0000

    files changed to adpat with new trigger module

--- JetMETAnalysis/PromptAnalysis/python/ntuple_cff.py
+++ JetMETAnalysis/PromptAnalysis/python/ntuple_cff.py
@@ -5,3 +5,4 @@ from JetMETAnalysis.PromptAnalysis.PromptAna_MET_cfi import *
 from JetMETAnalysis.PromptAnalysis.PromptAna_Jet_cfi import *
 from JetMETAnalysis.PromptAnalysis.PromptAna_BeamHalo_cfi import *
 from JetMETAnalysis.PromptAnalysis.PromptAna_CaloTowers_cfi import *
+from JetMETAnalysis.PromptAnalysis.PromptAna_Trigger_cfi import *
