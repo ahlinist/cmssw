35f20550f8b6e792bb3e74dcfed1134c03a96024
veelken <>
2009-11-27 18:32:08 +0000

    moved histogramAuxFunctions from TauAnalysis/BgEstimationTools to TauAnalysis/DQMTools

---
+++ TauAnalysis/DQMTools/interface/histogramAuxFunctions.h
@@ -0,0 +1,8 @@
+#ifndef TauAnalysis_DQMTools_histogramAuxFunctions_h
+#define TauAnalysis_DQMTools_histogramAuxFunctions_h
+
+#include <TH1.h>
+
+bool isCompatibleBinning(const TH1*, const TH1*);
+
+#endif
