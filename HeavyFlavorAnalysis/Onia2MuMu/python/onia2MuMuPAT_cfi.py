0a8831fa5e9cd53f2e6abe52e1bb3635e1f80f02
covarell <>
2009-11-27 17:34:30 +0000

    Updating python files, try/catch problem to be fixed

--- HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cfi.py
+++ HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cfi.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 onia2MuMuPAT = cms.EDProducer('Onia2MuMuPAT',
   muons = cms.InputTag("patMuons"),
+  beamSpotTag = cms.InputTag("offlineBeamSpot"),
+  primaryVertexTag = cms.InputTag("offlinePrimaryVertices"),
   higherPuritySelection = cms.int32(1),
   lowerPuritySelection  = cms.int32(1),
   addCommonVertex = cms.bool(True), ## Embed the full reco::Vertex out of the common vertex fit
-  addMCTruth = cms.bool(True), ## Add the common MC mother of the two muons, if any
+  addMCTruth = cms.bool(True),      ## Add the common MC mother of the two muons, if any
 )
