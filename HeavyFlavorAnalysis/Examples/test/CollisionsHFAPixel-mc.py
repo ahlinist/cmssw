03f5c5d7b2271a98b39c98c61a2406db6a5156fb
ursl <>
2009-12-04 22:19:28 +0000

    updated steering

--- HeavyFlavorAnalysis/Examples/test/CollisionsHFAPixel-mc.py
+++ HeavyFlavorAnalysis/Examples/test/CollisionsHFAPixel-mc.py
@@ -27,7 +27,16 @@ process.GlobalTag.globaltag = "MC_31X_V5::All"
 process.source = cms.Source(
     "PoolSource", 
     fileNames = cms.untracked.vstring(
-    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/00EEC933-8888-DE11-9954-00304865C456.root"
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/00EEC933-8888-DE11-9954-00304865C456.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/0607B847-5388-DE11-A5C2-00238BBD7584.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/08A30884-4688-DE11-A66A-00163691DC02.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/226283DD-6688-DE11-9F66-00144FCAB064.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/30537D20-ED89-DE11-8AAE-00E081329E7A.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/3232AC9A-4B88-DE11-8D7E-00163691DBB2.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/365B610E-7888-DE11-802D-003048344C22.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/4A77E45D-5288-DE11-B1FE-001EC9AAA2B5.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/5C0C1706-7188-DE11-86C2-00304865C466.root", 
+    "/store/mc/Summer09/MinBias900GeV/GEN-SIM-RECO/MC_31X_V3-v1/0017/94D13C1B-4888-DE11-BE75-00163691D236.root"
     )
     )
 
@@ -48,13 +57,26 @@ process.PixelTree = cms.EDAnalyzer(
     "PixelTree",
     verbose                = cms.untracked.int32(0),
     rootFileName           = cms.untracked.string(rootFileName),
-    muonCollectionLabel    = cms.untracked.string('muons'),
-    trackCollectionLabel   = cms.untracked.string('generalTracks'),
-    trajectoryInputLabel   = cms.untracked.string('TrackRefitter'),
-    pixelClusterLabel      = cms.untracked.string('siPixelClusters'),
-    L1GTReadoutRecordLabel = cms.untracked.string("gtDigis"), 
+    muonCollectionLabel    = cms.untracked.InputTag('muons'),
+    trackCollectionLabel   = cms.untracked.InputTag('generalTracks'),
+    trajectoryInputLabel   = cms.untracked.InputTag('TrackRefitter'),
+    pixelClusterLabel      = cms.untracked.InputTag('siPixelClusters'),
+    L1GTReadoutRecordLabel = cms.untracked.InputTag("gtDigis"), 
     hltL1GtObjectMap       = cms.untracked.InputTag("hltL1GtObjectMap"), 
     HLTResultsLabel        = cms.untracked.InputTag("TriggerResults::HLT")
+    )
+
+# ----------------------------------------------------------------------
+process.PixelFilter = cms.EDFilter(
+    "SkimEvents",
+    verbose                      = cms.untracked.int32(10),
+    filterOnPrimaryVertex        = cms.untracked.int32(1),
+    primaryVertexCollectionLabel = cms.untracked.InputTag('offlinePrimaryVertices'),
+    filterOnTracks               = cms.untracked.int32(1),
+    trackCollectionLabel         = cms.untracked.InputTag('generalTracks'),
+    filterOnPixelCluster         = cms.untracked.int32(1),
+    PixelClusterCollectionLabel  = cms.untracked.InputTag('siPixelClusters'),
+    
     )
 
 
@@ -188,22 +210,35 @@ process.bspsiphiDump = cms.EDFilter(
     type = cms.untracked.int32(100531) 
     )
 
+# ----------------------------------------------------------------------
+process.b2muD0Dump = cms.EDFilter(
+    "HFB2muD0",
+    verbose = cms.untracked.int32(0), 
+    muonsLabel = cms.untracked.InputTag("muons"),
+    tracksLabel = cms.untracked.string('generalTracks'),
+    muonPt = cms.untracked.double(3.0),
+    trackPt = cms.untracked.double(0.1),
+    deltaR =  cms.untracked.double(1.5),
+    deltaMD0 = cms.untracked.double(0.3),
+    deltaMDs = cms.untracked.double(0.3)
+    )
 
 # ----------------------------------------------------------------------
 process.p = cms.Path(
-    process.TrackRefitter*
-    process.PixelTree
-#    process.genParticles* 
-#    process.genDump*
-#    process.trkDump*
-#    process.muonDump*
-#    process.triggerDump*
-#    process.bmmDump*
-#    process.bmtDump*
-#    process.bupsikpDump*
-#    process.bdpsikstarDump*
-#    process.bspsiphiDump*
-#    process.tree
+#    process.TrackRefitter*
+#    process.PixelTree*
+    process.PixelFilter* 
+    process.genParticles* 
+    process.genDump*
+    process.trkDump*
+    process.muonDump*
+    process.triggerDump*
+    process.bmmDump*
+    process.bmtDump*
+    process.bupsikpDump*
+    process.bdpsikstarDump*
+    process.bspsiphiDump*
+    process.tree
 )
 
 
