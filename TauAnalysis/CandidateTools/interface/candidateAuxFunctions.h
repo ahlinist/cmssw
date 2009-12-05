c2bf9a6e202778b6f885b58a80ef0d9a7f345746
veelken <>
2009-12-05 15:15:37 +0000

    added functions to access total visible/invisible momentum of tau decay products

--- TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h
+++ TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h
@@ -4,11 +4,20 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 
+#include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
+
 const reco::GenParticle* findGenParticle(const reco::Candidate::LorentzVector&, 
 					 const reco::GenParticleCollection&, double = 0.5, int = -1);
 
 void findDaughters(const reco::GenParticle*, std::vector<const reco::GenParticle*>&, int = -1);
 
+bool isNeutrino(const reco::GenParticle*);
+
 reco::Candidate::LorentzVector getVisMomentum(const std::vector<const reco::GenParticle*>&, int = -1);
+reco::Candidate::LorentzVector getVisMomentum(const reco::GenParticle*, const reco::GenParticleCollection*);
+reco::Candidate::LorentzVector getInvisMomentum(const std::vector<const reco::GenParticle*>&, int = -1);
+reco::Candidate::LorentzVector getInvisMomentum(const reco::GenParticle*, const reco::GenParticleCollection*);
+
+double getPhysX(double x, bool& isWithinPhysRange);
 
 #endif
