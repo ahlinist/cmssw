5af9f39eefb74c9c6f32e9c753b79817176917dc
veelken <>
2009-12-05 15:06:01 +0000

    removed configuration parameters for collinear approximation control plots

--- TauAnalysis/Core/python/diTauCandidateHistManager_cfi.py
+++ TauAnalysis/Core/python/diTauCandidateHistManager_cfi.py
@@ -1,25 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 diTauCandidateHistManager = cms.PSet(    
-  pluginName = cms.string('diTauCandidateHistManager'),
-  pluginType = cms.string('DiCandidatePairHistManager'),
+    pluginName = cms.string('diTauCandidateHistManager'),
+    pluginType = cms.string('DiCandidatePairHistManager'),
       
-  diTauCandidateSource = cms.InputTag(''),
+    diTauCandidateSource = cms.InputTag(''),
 
-  vertexSource = cms.InputTag('selectedPrimaryVertexPosition'),
+    vertexSource = cms.InputTag('selectedPrimaryVertexPosition'),
 
-  dqmDirectory_store = cms.string('DiTauCandidateQuantities'),
+    dqmDirectory_store = cms.string('DiTauCandidateQuantities'),
 
-  #requireGenMatch = cms.bool(True),
-  requireGenMatch = cms.bool(False),
+    #requireGenMatch = cms.bool(True),
+    requireGenMatch = cms.bool(False),
 
-  #normalization = cms.string("diTauCandidates"),
-  normalization = cms.string("events"),
-
-  # upper limit on acoplanarity angle between muon and tau-jet
-  # for which muon + tau-jet mass is computed via collinear approximation
-  # (value in units of degrees)
-  maxDPhi12collinearApprox = cms.double(160.),
-
-  makeCollinearApproxMassDepHistograms = cms.bool(True)
+    #normalization = cms.string("diTauCandidates"),
+    normalization = cms.string("events"),
 )
