2005343308752f67cd39ece3f47efaa103a3679d
ameyer <>
2009-12-07 22:13:33 +0000

    first version of info render plugin

---
+++ DQM/RenderPlugins/src/DQMInfoRenderPlugin.cc
@@ -0,0 +1,67 @@
+/*!
+  \file DQMInfoRenderPlugin.cc
+  \\
+  \\ Code shamelessly borrowed from J. Temple's HcalRenderPlugin.cc code,
+  \\ which was shamelessly borrowed from S. Dutta's SiStripRenderPlugin.cc
+  \\ code, G. Della Ricca and B. Gobbo's EBRenderPlugin.cc, and other existing
+  \\ subdetector plugins
+  \\ preDraw and postDraw methods now check whether histogram was a TH1
+  \\ or TH2, and call a private method appropriate for the histogram type
+*/
+
+#include "VisMonitoring/DQMServer/interface/DQMRenderPlugin.h"
+#include "utils.h"
+
+#include "TProfile2D.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TText.h"
+#include <cassert>
+
+class DQMInfoRenderPlugin : public DQMRenderPlugin
+{
+public:
+  // These functions may be different that parent version
+  virtual bool applies(const VisDQMObject &o, const VisDQMImgInfo &)
+    {
+      // determine whether core object is an Info object
+      if(o.name.find( "Info/EventInfo/" ) != std::string::npos)  
+        return true;
+
+      return false;
+    }
+
+  virtual void preDraw (TCanvas * c, const VisDQMObject &o, const VisDQMImgInfo &, VisDQMRenderInfo &)
+    {
+      c->cd();
+      TH2F* obj = dynamic_cast<TH2F*>( o.object );
+      assert( obj );
+
+      //put in preDrawTH2F
+      if( o.name.find( "reportSummaryMap" )  != std::string::npos)
+      {
+        obj->SetStats( kFALSE );
+        dqm::utils::reportSummaryMapPalette(obj);
+        obj->SetOption("colz");
+        obj->SetTitle("Info Report Summary Map");
+
+        obj->GetXaxis()->SetNdivisions(1,true);
+        obj->GetYaxis()->SetNdivisions(1,true);
+//        obj->GetXaxis()->CenterLabels();
+//        obj->GetYaxis()->CenterLabels();
+
+//        gPad->SetGrid(1,1);
+      }
+
+//      gStyle->SetCanvasBorderMode( 0 );
+//      gStyle->SetPadBorderMode( 0 );
+//      gStyle->SetPadBorderSize( 0 );
+    }
+
+private:
+};
+
+static DQMInfoRenderPlugin instance;
