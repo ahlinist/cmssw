77fcefa614bd53c8b492fa5ec9ba7773654d56b4
friis <>
2009-12-07 22:51:38 +0000

    Improving ntuple plotting tools, adding a working example and tracking the twiki for it in CVS

--- RecoTauTag/TauAnalysisTools/python/tools/ntauples.py
+++ RecoTauTag/TauAnalysisTools/python/tools/ntauples.py
@@ -1,10 +1,8 @@
 import re
 import string
 
-from ROOT import gROOT, TFile, TTree, TH1F, TH3F, gDirectory, TChain
-#from PhysicsTools.PythonAnalysis import *
-#gSystem.Load("libFWCoreFWLite.so")
-#AutoLibraryLoader.enable()
+from ROOT import gROOT, TFile, TTree, gDirectory, TChain
+from ROOT import TH1F, TH3F, TGraphAsymmErrors, TCanvas
 
 class TauNtuple(object):
    def __init__(self, name):
@@ -46,12 +44,12 @@ class TauNtuple(object):
       output += expression
       return output
    def __repr__(self):
-      output =  ' * %s\n' % self.name
+      output =  ' * Ntuple: %s\n' % self.name
       for col, exprs in self.expressions.iteritems():
          if len(exprs):
-            output += ' * -- %s \n' % col
+            output += ' * Collection "%s" \n' % col
             for expr in exprs:
-               output += ' * ----- %s \n' % expr
+               output += ' * ------ %s \n' % expr
       return output
    
    def dictionary(self):
@@ -61,24 +59,9 @@ class TauNtuple(object):
       # Add truth info if applicable
       if self.current_collection == 'matched':
          for expr in self.expressions['ref']:
-            output['truth_%s' % expr] = self.parse_expression(expr, 'ref')
+            output['ref_%s' % expr] = self.parse_expression(expr, 'ref')
       return output
 
-def items_matching(regex, collection, getter = lambda x: x, check_match = lambda match: match is not None):
-   ''' Generator to yield items in the collection that match given regex 
-
-       Optional getter method is applied to each item for in the string, and 
-       check_match is applied on the output
-
-   '''
-   regex = re.compile(regex)
-   for item in collection:
-      if check_match(regex.match(getter(item))):
-         yield item
-
-def split_items(delimiter, collection):
-   for item in collection:
-      yield item.split(delimiter)
 
 def copy_aliases(tchain):
    ''' Ensure the TChain has aliases set correctly '''
@@ -92,52 +75,63 @@ def copy_aliases(tchain):
       tchain.SetAlias(alias, temp_tree.GetAlias(alias))
 
 class TauNtupleManager(object):
-   def __init__(self, events, grep="NtupleProducer"):
-      if isinstance(events, TChain):
-	 copy_aliases(events)
-      # Populate list of available aliases
-      aliases = [ alias.GetName() for alias in events.GetListOfAliases() ]
-      # Ensure these are all from Ntuple producers
-      aliases[:] = [alias for alias in aliases if grep in events.GetAlias(alias)]
-      # Split by the delimiter
-      #aliases[:] = [alias.split('#') for alias in aliases]
-      # Get all the basenames
-      self.ntuples = set( alias.split('#')[0] for alias in aliases )
-      # Build our tau ntuples
-      self.ntuples_dict = dict( (basename, TauNtuple(basename))
-            for basename in self.ntuples )
+    def __init__(self, events, grep="NtupleProducer"):
+        # If this is a TChain, we need to make sure
+        # the aliases are copied over correctly
+        if isinstance(events, TChain):
+            copy_aliases(events)
+        # Populate list of available aliases
+        aliases = [ alias.GetName() for alias in events.GetListOfAliases() ]
+        # Ensure these are all from Ntuple producers
+        aliases[:] = [alias for alias in aliases if grep in events.GetAlias(alias)]
+        # Split by the delimiter
+        #aliases[:] = [alias.split('#') for alias in aliases]
+        # Get all the basenames
+        self.ntuples = set( alias.split('#')[0] for alias in aliases )
+        # Build our tau ntuples
+        self.ntuples_dict = dict( (basename, TauNtuple(basename))
+                                 for basename in self.ntuples )
+        # Make the different ntuples attributes of the manager
+        for key, value in self.ntuples_dict.iteritems():
+            setattr(self, key, value)
 
-      # Make the different ntuples attributes of the manager
-      for key, value in self.ntuples_dict.iteritems():
-         setattr(self, key, value)
+        # Helper functions to parse the nutple
+        def items_matching(regex, collection, getter = lambda x: x, check_match = lambda match: match is not None):
+            ''' Generator to yield items in the collection that match given regex '''
+            regex = re.compile(regex)
+            for item in collection:
+                if check_match(regex.match(getter(item))):
+                    yield item
+        # Fill our ntuple columns
+        for ntuple in self.ntuples_dict.values():
+            # Get all the aliases relevant to this ntuple
+            ntuple_aliases = list(items_matching(r'^%s' % ntuple.name, aliases))
+            matched_aliases = list(items_matching(r'^[^#]*#matched', ntuple_aliases))
 
-      # Fill our ntuple columns
-      for ntuple in self.ntuples_dict.values():
-         # Get all the aliases relevant to this ntuple
-         ntuple_aliases = [item for item in items_matching(r'^%s' % ntuple.name, aliases)]
-         matched_aliases = [item for item in items_matching(r'^[^#]*#matched', ntuple_aliases)]
-         # These don't need to be expanded so we can keep them as generators
-         unmatched_aliases = items_matching(r'^[^#]*#unmatched', ntuple_aliases)
-         # Invert criteria
-         all_aliases = items_matching(r'^[^#]*#(un)*matched', ntuple_aliases, 
-               check_match=lambda match: match is None)
+            # These don't need to be expanded so we can keep them as generators
+            unmatched_aliases = items_matching(r'^[^#]*#unmatched', ntuple_aliases)
+            # Invert criteria
+            all_aliases = items_matching(r'^[^#]*#(un)*matched', ntuple_aliases, 
+                                         check_match=lambda match: match is None)
 
-         # Parse matched ones into true/reco collections
-         ref_regex = r'^[^#]*#matched#ref#'
-         matched_ref_aliases = items_matching(ref_regex, matched_aliases)
+            # Parse matched ones into true/reco collections
+            ref_regex = r'^[^#]*#matched#ref#'
+            matched_ref_aliases = items_matching(ref_regex, matched_aliases)
+            matched_reco_aliases = items_matching(ref_regex, matched_aliases, 
+                                                  check_match=lambda match: match is None)
 
-         matched_reco_aliases = items_matching(ref_regex, matched_aliases, 
-               check_match=lambda match: match is None)
-
-         to_fill = [ ('unmatched', unmatched_aliases),
-                     ('all', all_aliases),
-                     ('matched', matched_reco_aliases),
-                     ('ref', matched_ref_aliases) ]
-
-         # Register expressions for the different collections
-         for match_option, collection in to_fill:
-            for alias in split_items('#', collection):
-               ntuple.registerExpression(alias[-1], match_option)
+            to_fill = [ ('unmatched', unmatched_aliases),
+                       ('all', all_aliases),
+                       ('matched', matched_reco_aliases),
+                       ('ref', matched_ref_aliases) ]
+            # Register expressions for the different collections
+            for match_option, collection in to_fill:
+                for alias in [ item.split('#') for item in collection ]:
+                    ntuple.registerExpression(alias[-1], match_option)
+    def printNtuples(self):
+        print "Available ntuples:"
+        for ntuple in self.ntuples_dict.keys():
+            print "\t%s" % ntuple
 
 def draw(events, ntuple=None, expr=None, selection="", output_hist="", binning=(), options="goff"): 
    # Return value when we are returning the temp histogram
@@ -170,6 +164,31 @@ def draw(events, ntuple=None, expr=None, selection="", output_hist="", binning=(
 
    return return_func()
 
+def efficiency(events, ntuple=None, expr=None,
+               numerator="",
+               denominator="",
+               output_eff="",
+               binning=(),
+               options="goff"):
+
+    ''' Return a TGraphAsymmErrors w.r.t. expr where the numerator and
+    denominator are given by the selection expressions '''
+
+    if output_eff == "":
+        output_eff = "eff_temp"
+    numerator_h = draw(events, ntuple, expr, numerator, "numerator_temp", binning)
+    denominator_h = draw(events, ntuple, expr, denominator, "denom_temp", binning)
+    # Build a blank histogram w/ correct x & y axes to draw the efficiency on
+    histogram_background = numerator_h.Clone("%s_bkg" % output_eff)
+    histogram_background.GetXaxis().SetTitle(expr)
+    histogram_background.GetYaxis().SetTitle("Efficiency")
+    histogram_background.GetYaxis().SetRangeUser(1e-4, 1)
+    histogram_background.Reset()
+    histogram_background.SetTitle("%s/%s" % (numerator, denominator))
+    output = TGraphAsymmErrors(numerator_h, denominator_h)
+    output.SetName(output_eff)
+    return (histogram_background, output)
+
 if __name__ == "__main__":
    gROOT.SetBatch(True)
    file = TFile("taste_ntupled.root", "READ")
@@ -189,12 +208,21 @@ if __name__ == "__main__":
 
    # Make a histogram of pt resolution for taus that pass isolation
    my_histo = draw(events, pippo.shrinkingConePFTau, 
-         expr="$pt - $truth_pt",
+         expr="$pt - $ref_pt",
          selection="$ByIsolation", 
          output_hist="test",
          binning=(20, -10, 10))
 
-   my_histo.Draw()
+   bkg, my_eff = efficiency(events, pippo.shrinkingConePFTau,
+                            expr="$pt",
+                            numerator="$ByIsolation && $pt > 15",
+                            denominator="$pt > 15",
+                            output_eff="iso_eff",
+                            binning=(20, 0, 60))
+
+   c1 = TCanvas()
+   bkg.Draw()
+   my_eff.Draw()
    c1.SaveAs("test.png")
 
 
