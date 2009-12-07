a66ac337f6d6115ec242df9b0cec6a2750f16735
innocent <>
2009-12-07 16:29:11 +0000

    more cleanup

---
+++ CondCore/ESSources/interface/ProxyFactory.h
@@ -0,0 +1,32 @@
+#ifndef CONDCORE_PLUGINSYSTEM_PROXYFACTORY_H
+#define CONDCORE_PLUGINSYSTEM_PROXYFACTORY_H
+// -*- C++ -*-
+//
+// Package:     PluginSystem
+// Class  :     ProxyFactory
+// 
+/**\class ProxyFactory ProxyFactory.h CondCore/PluginSystem/interface/ProxyFactory.h
+   
+Description: <one line class summary>
+
+Usage:
+<usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Sat Jul 23 19:14:06 EDT 2005
+//
+#include "FWCore/PluginManager/interface/PluginFactory.h"
+#include<string>
+
+namespace cond {
+  class DataProxyWrapperBase;
+  class DbSession;
+
+  typedef edmplugin::PluginFactory< cond::DataProxyWrapperBase* ( cond::DbSession&, std::pair< std::string, std::string> const & ) > ProxyFactory;
+
+   const char* pluginCategory();
+}
+
+#endif /* CONDCORE_PLUGINSYSTEM_PROXYFACTORY_H */
