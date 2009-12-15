e503c7c010ccea90704bc2279ca27a40cef950de
friis <>
2009-12-15 07:55:32 +0000

    Adding effs!

--- RecoTauTag/TauAnalysisTools/test/firstData/plot.py
+++ RecoTauTag/TauAnalysisTools/test/firstData/plot.py
@@ -22,7 +22,6 @@ data_events = TChain("Events")
 for i, file in enumerate(mc_files):
     print "Adding MC file:", file
     mc_events.AddFile(file)
-
 for i, file in enumerate(data_files):
     print "Adding data file:", file
     data_events.AddFile(file)
@@ -93,11 +92,76 @@ def compare_distributions(
     Label.SetBorderSize(0)
     Label.Draw()
     keep.append(Label)
+
+def compare_effs(
+    ntuple,
+    expr=None,
+    numerator=None,
+    denominator=None,
+    binning=None,
+    output_hist="my_eff",
+    label="",
+    title="MyEff",
+    x_axis = "blah",
+    y_axis = "Fake rate"):
+
+    mc_hist_name = "mc_eff_"+output_hist
+    expr = expr
+    mc_plot, mc_eff = efficiency(
+        mc_events,
+        ntuple,
+        expr=expr,
+        numerator=numerator,
+        denominator=denominator,
+        output_eff=mc_hist_name,
+        binning=binning)
+
+    mc_plot.SetTitle(title)
+    mc_eff.SetMarkerStyle(20)
+    mc_plot.GetXaxis().SetTitle(x_axis)
+    mc_plot.GetYaxis().SetTitle(y_axis)
+    mc_plot.GetYaxis().SetRangeUser(1e-4, 1)
+    mc_plot.Draw()
+    mc_eff.Draw("pe")
+    
+    data_hist_name = "data_"+output_hist
+    data_plot, data_eff = efficiency(
+        data_events,
+        ntuple,
+        expr=expr,
+        numerator=numerator,
+        denominator=denominator,
+        output_eff=data_hist_name,
+        binning=binning)
+
+    data_eff.SetMarkerStyle(22)
+    data_eff.SetMarkerColor(2)
+    data_eff.Draw("pe")
+
+    Legend = TLegend(0.7, 0.7, 0.92, 0.9)
+    Legend.AddEntry(data_eff, "Data")
+    Legend.AddEntry(mc_eff, "MC (MinBias)")
+    Legend.SetFillColor(0)
+    Legend.Draw()
+    keep.append(Legend)
+
+    Label = TPaveText(0.2, 0.7, 0.5, 0.9, "brNDC")
+    Label.AddText(label)
+    Label.SetFillStyle(0)
+    Label.SetBorderSize(0)
+    Label.Draw()
+    keep.append(Label)
+    keep.append(mc_plot)
+    keep.append(mc_eff)
+    keep.append(data_plot)
+    keep.append(data_eff)
+
+
     
 canvas = TCanvas("blah", "blah", 700, 500)
 
 #std_cut = "$pt > 5 && $emFraction > 0.1"
-std_cut = ""
+std_cut = "1"
 std_cut_label="p_{T} > 5, emFraction > 0.1"
 
 for current_ntuple in ntuples_to_plot:
@@ -210,4 +274,93 @@ for current_ntuple in ntuples_to_plot:
         binning=(10, -0.5, 9.5),
         output_hist="nIsotHad")
     canvas.SaveAs("%s_nIsolationHadrons.png" % current_ntuple.name)
+
+    #Efficiencies
+    numerator = std_cut + "&& $ByLeadTrack"
+    denominator = std_cut  
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="Lead Track finding eff. w.r.t jet matching",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effLeadTrack")
+    gPad.SetLogy(True)
+    canvas.SaveAs("%s_LeadTrackFindingEff.png" % current_ntuple.name)
+
+    numerator = std_cut + "&& $ByLeadTrackPt"
+    denominator = std_cut  
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="Lead Track p_{T} > 5 GeV eff. w.r.t jet matching",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effLeadTrackPt")
+    canvas.SaveAs("%s_LeadTrackPtEff.png" % current_ntuple.name)
+
+
+    numerator = std_cut + "&& $ByLeadPionPt"
+    denominator = std_cut  
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="Lead Pion p_{T} > 5 GeV eff. w.r.t jet matching",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effLeadPionPt")
+    canvas.SaveAs("%s_LeadPionPtEff.png" % current_ntuple.name)
+
+    numerator = std_cut + "&& $ByIsolation"
+    denominator = std_cut + "&& $ByLeadPionPt"
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="Isolation w.r.t Lead Pion p_{T} > 5 GeV eff",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effIsolationRel")
+    canvas.SaveAs("%s_IsolationRel.png" % current_ntuple.name)
+
+
+    numerator = std_cut + "&& $ByTrackIsolation"
+    denominator = std_cut + "&& $ByLeadPionPt"
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="Charged Isolation w.r.t Lead Pion p_{T} > 5 GeV eff",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effTrackIsolationRel")
+    canvas.SaveAs("%s_TrackIsolationRel.png" % current_ntuple.name)
+
+
+    numerator = std_cut + "&& $ByTaNCfrOne"
+    denominator = std_cut + "&& $ByLeadPionPt"
+    compare_effs(
+        current_ntuple,
+        expr="$pt",
+        title="TaNC (1%) w.r.t Lead Pion p_{T} > 5 GeV eff",
+        numerator=numerator,
+        denominator=denominator,
+        label=std_cut_label,
+        x_axis = "p_{T} (GeV/c)",
+        binning = (8, 0, 40),
+        output_hist="effTaNCIsolationRel")
+    canvas.SaveAs("%s_TaNCOneIsolationRel.png" % current_ntuple.name)
 
