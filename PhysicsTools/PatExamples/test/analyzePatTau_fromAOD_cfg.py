c2041d341a2e5adbccb00b41f44f011d69a613a0
veelken <>
2009-12-18 13:39:00 +0000

    removed tau id. sequences (run per default in CMSSW_3_3_x)

--- PhysicsTools/PatExamples/test/analyzePatTau_fromAOD_cfg.py
+++ PhysicsTools/PatExamples/test/analyzePatTau_fromAOD_cfg.py
@@ -10,7 +10,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.load('Configuration.StandardSequences.Geometry_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'IDEAL_V12::All'
+process.GlobalTag.globaltag = cms.string('STARTUP31X_V1::All')
 
 process.load('PhysicsTools.PatAlgos.patSequences_cff')
 
@@ -20,7 +20,7 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/mc/Summer08/ZJets-madgraph/GEN-SIM-RECO/IDEAL_V11_redigi_v1/0016/84672FA2-0DEC-DD11-A868-00E08178C10F.root'
+        '/store/relval/CMSSW_3_3_6/RelValZTT/GEN-SIM-RECO/STARTUP3X_V8H-v1/0009/24052BB1-9EE4-DE11-87C2-002618943957.root'
     )
 )
 
@@ -36,27 +36,6 @@ process.analyzePatTau = cms.EDAnalyzer("PatTauAnalyzer",
     discrByTaNC = cms.string("byTaNCfrHalfPercent")
 )
 
-#--------------------------------------------------------------------------------
-# import configuration files for reco::PFTau reconstruction
-# using most recent RecoTauTag tags back-ported from CMSSW_3_1_x
-process.load('PhysicsTools.PFCandProducer.pfAllChargedHadrons_cfi')
-process.load('PhysicsTools.PFCandProducer.pfAllNeutralHadrons_cfi')
-process.load('PhysicsTools.PFCandProducer.pfAllPhotons_cfi')
-process.load('RecoTauTag.Configuration.RecoPFTauTag_cff')
-process.load('RecoTauTag.TauTagTools.TauMVADiscriminator_cfi')
-process.load('RecoTauTag.TauTagTools.TauNeuralClassifiers_cfi')
-process.load('RecoTauTag.TauTagTools.TancConditions_cff')
-
-# import utility function for switching pat::Tau input
-# to different reco::Tau collection stored on AOD
-from PhysicsTools.PatAlgos.tools.tauTools import * 
-
-# use either shrinking dR = 5.0/Et signal cone or fixed dR = 0.07 signal cone
-# reco::PFTau collection as input for pat::Tau production
-switchToPFTauShrinkingCone(process)
-#switchToPFTauFixedCone(process)
-#--------------------------------------------------------------------------------
-
 # disable preselection on pat::Tau objects
 # (neccessary in order to make efficiency plots)
 process.cleanLayer1Taus.preselection = cms.string('')
@@ -65,11 +44,7 @@ process.TFileService = cms.Service("TFileService",
     fileName = cms.string('patTau_Histograms.root')
 )
 
-process.p = cms.Path( process.pfAllChargedHadrons + process.pfAllNeutralHadrons + process.pfAllPhotons
-                     +process.PFTau
-                     +process.shrinkingConePFTauDiscriminationByTaNC + process.RunTanc
-                     +process.patDefaultSequence 
-                     +process.analyzePatTau )
+process.p = cms.Path( process.patDefaultSequence + process.analyzePatTau )
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
