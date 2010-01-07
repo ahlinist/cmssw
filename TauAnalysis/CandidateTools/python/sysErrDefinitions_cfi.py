5144d29a7e55f83c10969c6299aeaae06111a14d
veelken <>
2010-01-07 13:41:09 +0000

    added definitions of muon, tau-jet and theory (PDF, ISR, FSR) uncertainties

---
+++ TauAnalysis/CandidateTools/python/sysErrDefinitions_cfi.py
@@ -0,0 +1,72 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# define sources of systematic uncertainties
+# considered in Z --> tau+ tau- and W --> tau nu cross-section measurements
+# (Note that the same values for systematic uncertainties are used for all channels)
+#--------------------------------------------------------------------------------
+
+electronSystematics = {
+    "sysElectronEnUp"       : "smearedLayer1Electrons::sysElectronEnUp",
+    "sysElectronEnDown"     : "smearedLayer1Electrons::sysElectronEnDown"
+}
+
+muonSystematics = {
+    "sysMuonPtUp"           : "smearedLayer1Muons::sysMuonPtUp",
+    "sysMuonPtDown"         : "smearedLayer1Muons::sysMuonPtDown" 
+}
+
+tauSystematics = {   
+    "sysTauJetEnUp"         : "smearedLayer1Taus::sysTauJetEnUp",
+    "sysTauJetEnDown"       : "smearedLayer1Taus::sysTauJetEnDown",    
+    "sysTauJetThetaUp"      : "smearedLayer1Taus::sysTauJetThetaUp",
+    "sysTauJetThetaDown"    : "smearedLayer1Taus::sysTauJetThetaDown",
+    "sysTauJetPhiUp"        : "smearedLayer1Taus::sysTauJetPhiUp",
+    "sysTauJetPhiDown"      : "smearedLayer1Taus::sysTauJetPhiDown",
+    "sysTauLeadTrackPtUp"   : "smearedLayer1Taus::sysTauLeadTrackPtUp",
+    "sysTauLeadTrackPtDown" : "smearedLayer1Taus::sysTauLeadTrackPtDown"
+}
+
+muTauPairSystematics = {
+    "sysMuonPtUp"           : "smearedMuTauPairsSysMuonPtUp",
+    "sysMuonPtDown"         : "smearedMuTauPairsSysMuonPtDown",
+    "sysTauJetEnUp"         : "smearedMuTauPairsSysTauJetEnUp",
+    "sysTauJetEnDown"       : "smearedMuTauPairsSysTauJetEnDown",
+    "sysTauJetThetaUp"      : "smearedMuTauPairsSysTauJetThetaUp",
+    "sysTauJetThetaDown"    : "smearedMuTauPairsSysTauJetThetaDown",
+    "sysTauJetPhiUp"        : "smearedMuTauPairsSysTauJetPhiUp",
+    "sysTauJetPhiDown"      : "smearedMuTauPairsSysTauJetPhiDown",
+    "sysTauLeadTrackPtUp"   : "smearedMuTauPairsSysTauLeadTrackPtUp",
+    "sysTauLeadTrackPtDown" : "smearedMuTauPairsSysTauLeadTrackPtDown"
+}
+
+theorySystematics = {
+    "sysPdfWeights[40]"     : "pdfWeights:cteq65",
+    "sysIsrWeight"          : "isrWeight",
+    "sysFsrWeight"          : "fsrWeight"
+}
+
+def getSysUncertaintyNames(sysUncertaintyMaps):
+    
+    sysUncertaintyNames = []
+    
+    for sysUncertaintyMap in sysUncertaintyMaps:
+        for sysUncertaintyName in sysUncertaintyMap.keys():
+            sysUncertaintyNames.append(sysUncertaintyName)
+
+    return sysUncertaintyNames
+
+def getSysUncertaintyParameterSets(sysUncertaintyMaps):
+    
+    sysUncertaintyParameterSets = []
+    
+    for sysUncertaintyMap in sysUncertaintyMaps:
+        for sysUncertaintyName, sysUncertaintyInputTag in sysUncertaintyMap.items():
+            sysUncertaintyParameterSet = cms.PSet()
+
+            setattr(sysUncertaintyParameterSet, "name", cms.string(sysUncertaintyName))
+            setattr(sysUncertaintyParameterSet, "src", cms.string(sysUncertaintyInputTag))
+
+            sysUncertaintyParameterSets.append(sysUncertaintyParameterSet)
+
+    return cms.VPSet(sysUncertaintyParameterSets)
