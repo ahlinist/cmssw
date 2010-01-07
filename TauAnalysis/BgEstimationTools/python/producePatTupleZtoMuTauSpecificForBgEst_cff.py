e7b78b8eefa3e90dd654f50136c882118a1f5b2c
veelken <>
2010-01-07 14:26:29 +0000

    minor clean-up

--- TauAnalysis/BgEstimationTools/python/producePatTupleZtoMuTauSpecificForBgEst_cff.py
+++ TauAnalysis/BgEstimationTools/python/producePatTupleZtoMuTauSpecificForBgEst_cff.py
@@ -4,10 +4,8 @@ import FWCore.ParameterSet.Config as cms
 # produce all objects which get stored in PATTuple specific to data-driven background estimation methods
 #--------------------------------------------------------------------------------
 
-#from TauAnalysis.BgEstimationTools.patElectronSelectionForBgEst_cff import *
 from TauAnalysis.BgEstimationTools.patMuonSelectionForBgEst_cff import *
 from TauAnalysis.BgEstimationTools.patPFTauSelectionForBgEst_cff import *
-#from TauAnalysis.RecoTools.patLeptonSelection_cff import *
 
 from TauAnalysis.BgEstimationTools.muTauPairProductionForBgEst_cff import *
 
@@ -15,7 +13,6 @@ from TauAnalysis.BgEstimationTools.patJetSelectionForBgEst_cff import *
 from TauAnalysis.BgEstimationTools.patJetSelectionForZtoMuTauBgEst_cff import *
 
 producePatTupleZtoMuTauSpecificForBgEst = cms.Sequence(
-   #selectLayer1ElectronsLooseIsolation * selectElectronsForBgEst 
     selectMuonsForBgEst
    * selectTausForBgEst
    * produceMuTauPairsForBgEst
