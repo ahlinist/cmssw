f235bf479017daf145d7c5bd30a634dd17e17102
scooper <>
2010-01-08 21:34:04 +0000

    Module to separate HW/SW calibrations.

--- CalibCalorimetry/EcalTiming/plugins/plugins.cc
+++ CalibCalorimetry/EcalTiming/plugins/plugins.cc
@@ -1,6 +1,8 @@
 #include "CalibCalorimetry/EcalTiming/interface/EcalTimingCorrection.h"
 #include "CalibCalorimetry/EcalTiming/interface/EcalXMLDelaysPlotter.h"
+#include "CalibCalorimetry/EcalTiming/interface/EcalCreateTTAvgTimes.h"
 
 
 DEFINE_FWK_MODULE(EcalTimingCorrection);
 DEFINE_FWK_MODULE(EcalXMLDelaysPlotter);
+DEFINE_FWK_MODULE(EcalCreateTTAvgTimes);
