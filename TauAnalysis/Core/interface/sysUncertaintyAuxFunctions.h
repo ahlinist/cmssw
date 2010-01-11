cbde7eba3266cf77a24f3d9789c5b8af4fdc371d
veelken <>
2010-01-11 10:17:47 +0000

    added auxiliary functions to "expand" names of systematic uncertainties (e.g. "pdfWeights(41)" --> { "pdfWeight(0)", "pdfWeight(1)",...,"pdfWeight(40)" }

---
+++ TauAnalysis/Core/interface/sysUncertaintyAuxFunctions.h
@@ -0,0 +1,9 @@
+#ifndef TauAnalysis_Core_sysUncertaintyAuxFunctions_h
+#define TauAnalysis_Core_sysUncertaintyAuxFunctions_h
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> expandSysName(const std::string&);
+
+#endif
