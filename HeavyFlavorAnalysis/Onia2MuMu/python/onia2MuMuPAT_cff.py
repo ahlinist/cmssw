7f7e2dfcfd5476f53bba9ecf7884c5dcb33d782a
gpetrucc <>
2010-01-13 11:43:46 +0000

    Clean up configuration file Calo muon merging is in by default

--- HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cff.py
+++ HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cff.py
@@ -1,5 +1,19 @@
 import FWCore.ParameterSet.Config as cms
-import  HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi 
+
+##    __  __                        ____      _       __  __                       
+##   |  \/  | ___ _ __ __ _  ___   / ___|__ _| | ___ |  \/  |_   _  ___  _ __  ___ 
+##   | |\/| |/ _ \ '__/ _` |/ _ \ | |   / _` | |/ _ \| |\/| | | | |/ _ \| '_ \/ __|
+##   | |  | |  __/ | | (_| |  __/ | |__| (_| | | (_) | |  | | |_| | (_) | | | \__ \
+##   |_|  |_|\___|_|  \__, |\___|  \____\__,_|_|\___/|_|  |_|\__,_|\___/|_| |_|___/
+##                    |___/                                                        
+##   
+## ==== Merge CaloMuons into the collection of reco::Muons  ====
+from RecoMuon.MuonIdentification.calomuons_cfi import calomuons;
+mergedMuons = cms.EDProducer("CaloMuonMerger",
+    muons     = cms.InputTag("muons"), 
+    caloMuons = cms.InputTag("calomuons"),
+    minCaloCompatibility = calomuons.minCaloCompatibility
+)
 
 ##    __  __  ____   _____           _   _     
 ##   |  \/  |/ ___| |_   _| __ _   _| |_| |__  
@@ -9,7 +23,7 @@ import  HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi
 ##                                             
 ##   
 from PhysicsTools.PatAlgos.mcMatchLayer0.muonMatch_cfi import muonMatch
-
+muonMatch.src = 'mergedMuons'
 
 ##    __  __       _          ____   _  _____   __  __                       
 ##   |  \/  | __ _| | _____  |  _ \ / \|_   _| |  \/  |_   _  ___  _ __  ___ 
@@ -21,6 +35,7 @@ from PhysicsTools.PatAlgos.mcMatchLayer0.muonMatch_cfi import muonMatch
 ### ==== Make PAT Muons ====
 from PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi import allLayer1Muons
 patMuonsWithoutTrigger = allLayer1Muons.clone(
+    muonSource = 'mergedMuons',
     # embed the tracks, so we don't have to carry them around
     embedTrack          = True,
     embedCombinedMuon   = True,
@@ -54,6 +69,7 @@ muonMatchHLTMu3       = muonTriggerMatchHLT1MuonIso.clone(src = 'patMuonsWithout
 muonMatchHLTMu5       = muonTriggerMatchHLT1MuonIso.clone(src = 'patMuonsWithoutTrigger',pathNames = [ "HLT_Mu5" ])
 muonMatchHLTDoubleMu0 = muonTriggerMatchHLT1MuonIso.clone(src = 'patMuonsWithoutTrigger',pathNames = [ "HLT_DoubleMu0" ])
 muonMatchHLTDoubleMu3 = muonTriggerMatchHLT1MuonIso.clone(src = 'patMuonsWithoutTrigger',pathNames = [ "HLT_DoubleMu3" ])
+
 ## ==== Embed ====
 patMuonsWithTrigger = cms.EDProducer( "PATTriggerMatchMuonEmbedder",
     src     = cms.InputTag(  "patMuonsWithoutTrigger" ),
@@ -64,7 +80,10 @@ patMuonsWithTrigger = cms.EDProducer( "PATTriggerMatchMuonEmbedder",
         cms.InputTag('muonMatchHLTDoubleMu0'),
         cms.InputTag('muonMatchHLTDoubleMu3'),
     )
+
 )
+
+## ==== Trigger Sequence ====
 patTriggerMatching = cms.Sequence(
     patTrigger * 
     ( muonMatchHLTL1MuOpen  +
@@ -75,33 +94,58 @@ patTriggerMatching = cms.Sequence(
     patMuonsWithTrigger
 )
 
-### ==== Apply some final selection ====
+### ==== Apply some final selection (none by default) ====
 patMuons = cms.EDFilter("PATMuonSelector",
     src = cms.InputTag("patMuonsWithTrigger"),
     cut = cms.string(""), 
 )
+
 ### ==== Sequence ====
 patMuonSequence = cms.Sequence( 
+    mergedMuons *
     muonMatch *
     patMuonsWithoutTrigger *
     patTriggerMatching *
     patMuons  
 )
 
+##    ____  _       __  __                       
+##   |  _ \(_)     |  \/  |_   _  ___  _ __  ___ 
+##   | | | | |_____| |\/| | | | |/ _ \| '_ \/ __|
+##   | |_| | |_____| |  | | |_| | (_) | | | \__ \
+##   |____/|_|     |_|  |_|\__,_|\___/|_| |_|___/
+##                                               
+##   
+import HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi 
 
 onia2MuMuPatGlbGlb = HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi.onia2MuMuPAT.clone()
 
 onia2MuMuPatGlbTrk = HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi.onia2MuMuPAT.clone()
 onia2MuMuPatGlbTrk.lowerPuritySelection  = cms.string("isGlobalMuon || isTrackerMuon")
 
+onia2MuMuPatGlbCal = HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi.onia2MuMuPAT.clone()
+onia2MuMuPatGlbCal.lowerPuritySelection  = cms.string("isGlobalMuon || isTrackerMuon || (track.isNonnull && isCaloMuon)")
+
 onia2MuMuPatTrkTrk = HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi.onia2MuMuPAT.clone()
 onia2MuMuPatTrkTrk.higherPuritySelection  = cms.string("isGlobalMuon || isTrackerMuon")
 onia2MuMuPatTrkTrk.lowerPuritySelection   = cms.string("isGlobalMuon || isTrackerMuon")
 
+onia2MuMuPatTrkCal = HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi.onia2MuMuPAT.clone()
+onia2MuMuPatTrkCal.higherPuritySelection  = cms.string("isGlobalMuon || isTrackerMuon")
+onia2MuMuPatTrkCal.lowerPuritySelection   = cms.string("isGlobalMuon || isTrackerMuon || (track.isNonnull && isCaloMuon)")
 
+##    _____           _     
+##   |_   _|__   ___ | |___ 
+##     | |/ _ \ / _ \| / __|
+##     | | (_) | (_) | \__ \
+##     |_|\___/ \___/|_|___/
+##                          
+##   
 def onia2MuMu_isNotMC(process):
     process.patMuonSequence.remove(muonMatch)
     process.patMuonsWithoutTrigger.addGenMatch = False
     process.onia2MuMuPatGlbGlb.addMCTruth = False
+    process.onia2MuMuPatGlbCal.addMCTruth = False
     process.onia2MuMuPatGlbTrk.addMCTruth = False
     process.onia2MuMuPatTrkTrk.addMCTruth = False
+    process.onia2MuMuPatTrkCal.addMCTruth = False
