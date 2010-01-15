d8b9b5c13ee089215464a345e7e103c37604ea8d
veelken <>
2010-01-15 17:40:05 +0000

    added definition of binEntryTypes and functions to compute acceptance, purity and stability

--- TauAnalysis/Core/interface/binningAuxFunctions.h
+++ TauAnalysis/Core/interface/binningAuxFunctions.h
@@ -4,6 +4,47 @@
 #include <vector>
 #include <string>
 
+struct binEntryType
+{
+  binEntryType()
+    : binContent_(0.), binSumw2_(0.) {}
+  binEntryType& operator+=(double value)
+  {
+    binContent_ += value;
+    binSumw2_ += (value*value);
+    return (*this);
+  }
+  binEntryType& operator+=(const binEntryType& operand)
+  {
+    binContent_ += operand.binContent_;
+    binSumw2_ += operand.binSumw2_;
+    return (*this);
+  }
+  double binContent_;
+  double binSumw2_;
+};
+
+struct binEntryType_model
+{
+  binEntryType_model() {}
+  binEntryType_model& operator+=(const binEntryType_model& operand)
+  {
+    gen_ += operand.gen_;
+    rec_ += operand.rec_;
+    stay_ += operand.stay_;
+    lost_ += operand.lost_;
+    smearIn_ += operand.smearIn_;
+    smearOut_ += operand.smearOut_;
+    return (*this);
+  }
+  binEntryType gen_;
+  binEntryType rec_;
+  binEntryType stay_;
+  binEntryType lost_;
+  binEntryType smearIn_;
+  binEntryType smearOut_;
+};
+
 std::string encodeBinningStringRep(const std::string&, const std::string&, const std::string&);
 void decodeBinningStringRep(const std::string&, std::string&, std::string&, std::string&, int&);
 
@@ -12,5 +53,9 @@ std::vector<double> decodeVDoubleStringRep(const std::string&, int&);
 
 std::string encodeVStringStringRep(const std::vector<std::string>&);
 std::vector<std::string> decodeVStringStringRep(const std::string&, int&);
+
+void computeAcceptance(const binEntryType_model&, double&, double&);
+void computePurity(const binEntryType_model&, double&, double&);
+void computeStability(const binEntryType_model&, double&, double&);
 
 #endif
