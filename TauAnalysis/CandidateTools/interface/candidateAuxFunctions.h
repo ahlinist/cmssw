62c581a131d5560c076abc095c351e501389307c
veelken <>
2010-01-19 14:04:02 +0000

    added optional std::vector<int>* parameter that allows to specific pdgIds in findGenParticle function

--- TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h
+++ TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h
@@ -6,16 +6,19 @@
 
 #include "AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateT1T2MEt.h"
 
+#include <vector>
+
 const reco::GenParticle* findGenParticle(const reco::Candidate::LorentzVector&, 
-					 const reco::GenParticleCollection&, double = 0.5, int = -1);
+					 const reco::GenParticleCollection&, double = 0.5, int = -1,
+					 std::vector<int>* = 0);
 
 void findDaughters(const reco::GenParticle*, std::vector<const reco::GenParticle*>&, int = -1);
 
 bool isNeutrino(const reco::GenParticle*);
 
-reco::Candidate::LorentzVector getVisMomentum(const std::vector<const reco::GenParticle*>&, int = -1);
+reco::Candidate::LorentzVector getVisMomentum(const std::vector<const reco::GenParticle*>&, int = +1);
 reco::Candidate::LorentzVector getVisMomentum(const reco::GenParticle*, const reco::GenParticleCollection*);
-reco::Candidate::LorentzVector getInvisMomentum(const std::vector<const reco::GenParticle*>&, int = -1);
+reco::Candidate::LorentzVector getInvisMomentum(const std::vector<const reco::GenParticle*>&, int = +1);
 reco::Candidate::LorentzVector getInvisMomentum(const reco::GenParticle*, const reco::GenParticleCollection*);
 
 double getPhysX(double x, bool& isWithinPhysRange);
--- TauAnalysis/CandidateTools/src/candidateAuxFunctions.cc
+++ TauAnalysis/CandidateTools/src/candidateAuxFunctions.cc
@@ -5,18 +5,30 @@
 #include "TMath.h"
 
 const reco::GenParticle* findGenParticle(const reco::Candidate::LorentzVector& direction, 
-					 const reco::GenParticleCollection& genParticles, double dRmax, int status)
+					 const reco::GenParticleCollection& genParticles, double dRmax, int status,
+					 std::vector<int>* pdgIds)
 {
   const reco::GenParticle* bestMatch = 0;
 
   for ( reco::GenParticleCollection::const_iterator genParticle = genParticles.begin();
 	genParticle != genParticles.end(); ++genParticle ) {
-    if ( (status == -1 || genParticle->status() == status) && (bestMatch == 0 || genParticle->energy() > bestMatch->energy()) ) {
+    bool matchesPdgId = false;
+    if ( pdgIds ) {
+      for ( std::vector<int>::const_iterator pdgId = pdgIds->begin();
+	    pdgId != pdgIds->end(); ++pdgId ) {
+	if ( genParticle->pdgId() == (*pdgId) ) matchesPdgId = true;
+      }
+    } else {
+      matchesPdgId = true;
+    }
+
+    if ( matchesPdgId && (status == -1 || genParticle->status() == status) && 
+	 (bestMatch == 0 || genParticle->energy() > bestMatch->energy()) ) {
       double dR = reco::deltaR(direction, genParticle->p4());
       if ( dR < dRmax ) bestMatch = &(*genParticle);
     }
   }
-
+  
   return bestMatch;
 }
 
