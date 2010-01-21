f37759724aca46a57f56b6b634d5610249747c42
covarell <>
2010-01-21 13:40:15 +0000

    Updates were missing!

--- HeavyFlavorAnalysis/Onia2MuMu/test/jpsianalyzerpat_cfg.py
+++ HeavyFlavorAnalysis/Onia2MuMu/test/jpsianalyzerpat_cfg.py
@@ -7,9 +7,9 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
+    duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
     fileNames = cms.untracked.vstring(
-        'file:JPsi_10TeV_PAT.root'
+      'file:myfile.root'
     )
 )
 
@@ -17,13 +17,21 @@ process.source = cms.Source("PoolSource",
 #    IgnoreCompletely = cms.untracked.vstring( "ProductNotFound" )
 # )
 
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+
 process.demo = cms.EDAnalyzer('JPsiAnalyzerPAT',
+
+    src = cms.InputTag("onia2MuMuPatTrkTrk"),
+    srcWithCaloMuons = cms.InputTag("onia2MuMuPatGlbCal"),
 
     histFileName = cms.string("Histos.root"),		
     dataSetName = cms.string("DataSet.root"),
                               
-    pTBinRanges = cms.vdouble(3.0, 5.0, 6.0, 7.0, 8.0, 9.0, 11.0, 14.0, 18.0, 25.0, 35.0, 60.0),
-    etaBinRanges = cms.vdouble(0.0, 1.1, 2.5), 
+    # pTBinRanges = cms.vdouble(3.0, 5.0, 6.0, 7.0, 8.0, 9.0, 11.0, 14.0, 18.0, 25.0, 35.0, 60.0),
+    pTBinRanges = cms.vdouble(0.5, 3.0, 6.0, 8.0, 10.0, 15.0, 35.0),
+    # etaBinRanges = cms.vdouble(0.0, 1.1, 2.5),
+    etaBinRanges = cms.vdouble(0.0, 2.5),
     onlyTheBest = cms.bool(True),		
     applyCuts = cms.bool(True),			
     storeEfficiency = cms.bool(False),	
