b64ca2d39644c105935718a80e5caa478daa9533
veelken <>
2010-01-22 15:36:48 +0000

    reenabled full plotting sequence

--- TauAnalysis/Configuration/test/makeZtoMuTauPlots_cfg.py
+++ TauAnalysis/Configuration/test/makeZtoMuTauPlots_cfg.py
@@ -14,19 +14,23 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("EmptySource")
 
 # define directory from which .root files containing the histograms get loaded
-process.loadZtoMuTau.inputFilePath = cms.string("rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_1_2/plots/ZtoMuTau/")
+process.loadZtoMuTau.inputFilePath = cms.string("rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_1_2/plots/ZtoMuTau/10TeV/")
+#process.loadZtoMuTau.inputFilePath = cms.string("rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_1_2/plots/ZtoMuTau/7TeV/")
 #process.loadZtoMuTau.inputFilePath = cms.string("rfio:/castor/cern.ch/user/l/lusito/ZtoMuTauAnalysis/")
 
 # import utility function to enable factorization
 from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeZtoMuTauPlots
 enableFactorization_makeZtoMuTauPlots(process)
 
+process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
+
 process.makeZtoMuTauPlots = cms.Sequence(
     process.loadZtoMuTau
-   ##+ process.addZtoMuTau
-   ##+ process.saveZtoMuTau
+   + process.dumpDQMStore
+   + process.addZtoMuTau
+   + process.saveZtoMuTau
    + process.dumpZtoMuTau
-   ##+ process.plotZtoMuTau
+   + process.plotZtoMuTau
 )
 
 process.p = cms.Path(process.makeZtoMuTauPlots)
