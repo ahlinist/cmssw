ffe191f2405a784f663cb086642780226652b9de
santanas <>
2010-01-25 19:45:02 +0000

    ET tower vs Ratiop plots

--- JetMETAnalysis/PromptAnalysis/test/rootNtupleMacros/plotting/HFplots.C
+++ JetMETAnalysis/PromptAnalysis/test/rootNtupleMacros/plotting/HFplots.C
@@ -1,7 +1,52 @@
 {
 
-TFile Data("output_DATA900.root");
-TFile MC("output_MC900.root");
+gROOT->Reset();
+gStyle->SetOptStat("0");
+
+TFile Data("output_DATA900GeV.root");
+TFile MC("output_MC900GeV.root");
+
+TCanvas c_Data_tcMET;
+c_Data_tcMET.SetGridy();
+c_Data_tcMET.SetLogz();
+Data.cd();
+h2_ET_vs_HFratio->SetTitle("");
+h2_ET_vs_HFratio->GetXaxis()->SetTitle("HF ratio (L-S/L+S)");
+h2_ET_vs_HFratio->GetYaxis()->SetTitle("E_{T} of HF tower [GeV]");
+h2_ET_vs_HFratio->GetXaxis()->SetRangeUser(-1.1,1.1);
+h2_ET_vs_HFratio->GetYaxis()->SetRangeUser(0,50);
+// h2_ET_vs_HFratio->SetMarkerStyle(20);
+// h2_ET_vs_HFratio->SetMarkerSize(0.6);
+// h2_ET_vs_HFratio->SetMarkerColor(kBlack);
+h2_ET_vs_HFratio->Draw("colz");
+c_Data_tcMET.SaveAs("hf_towerET_vs_ratio_DATA.eps");
+
+TLatex l_900;
+l_900.SetTextAlign(12);
+l_900.SetTextSize(0.04);
+l_900.SetTextFont(62);
+l_900.SetNDC();
+l_900.DrawLatex(0.314655,0.84322,"CMS 2009 Preliminary - 900 GeV DATA");
+
+
+TCanvas c_MC_tcMET;
+c_MC_tcMET.SetGridy();
+c_MC_tcMET.SetLogz();
+MC.cd();
+h2_ET_vs_HFratio->SetTitle("");
+h2_ET_vs_HFratio->GetXaxis()->SetTitle("HF ratio (L-S/L+S)");
+h2_ET_vs_HFratio->GetYaxis()->SetTitle("E_{T} of HF tower [GeV]");
+h2_ET_vs_HFratio->GetXaxis()->SetRangeUser(-1.1,1.1);
+h2_ET_vs_HFratio->GetYaxis()->SetRangeUser(0,50);
+// h2_ET_vs_HFratio->SetMarkerStyle(20);
+// h2_ET_vs_HFratio->SetMarkerSize(0.6);
+// h2_ET_vs_HFratio->SetMarkerColor(kRed);
+h2_ET_vs_HFratio->Draw("colz");
+c_MC_tcMET.SaveAs("hf_towerET_vs_ratio_MC.eps");
+
+l_900.DrawLatex(0.314655,0.84322,"CMS 2009 Preliminary - 900 GeV MinBias MC");
+
+/*
 
 // TCanvas all;
 // all.Divide(2,2);
@@ -54,25 +99,6 @@ h2_ETS_vs_HFratio->GetYaxis()->SetRangeUser(0,20);
 h2_ETS_vs_HFratio->Draw("colz");
 c_MC_Short.SaveAs("hf_shortfiberET_vs_ratio_MC.eps");
 
-
-TCanvas c_Data_MC_tcMET;
-c_Data_MC_tcMET.SetGridy();
-Data.cd();
-
-h2_ET_vs_HFratio->GetXaxis()->SetTitle("HF ratio (L-S/L+S)");
-h2_ET_vs_HFratio->GetYaxis()->SetTitle("ET tower (GeV)");
-h2_ET_vs_HFratio->GetXaxis()->SetRangeUser(-1.1,1.1);
-h2_ET_vs_HFratio->GetYaxis()->SetRangeUser(0,20);
-h2_ET_vs_HFratio->SetMarkerStyle(20);
-h2_ET_vs_HFratio->SetMarkerSize(0.6);
-h2_ET_vs_HFratio->SetMarkerColor(kBlack);
-h2_ET_vs_HFratio->Draw();
-
-MC.cd();
-h2_ET_vs_HFratio->SetMarkerStyle(20);
-h2_ET_vs_HFratio->SetMarkerSize(0.6);
-h2_ET_vs_HFratio->SetMarkerColor(kRed);
-h2_ET_vs_HFratio->Draw("sames");
-c_Data_MC_tcMET.SaveAs("hf_towerET_vs_ratio_DATA_vs_MC.eps");
+*/
 
 }
