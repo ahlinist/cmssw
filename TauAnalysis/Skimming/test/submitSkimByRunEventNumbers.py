3fea00bc268075a677a6b6bed4aa08ca90438af2
veelken <>
2010-01-28 16:42:55 +0000

    - moved makeReplacementsSkimByRunEventNumbers function into tools subdirectory - added support for 10 TeV/ 7TeV recoSampleDefinitions files

--- TauAnalysis/Skimming/test/submitSkimByRunEventNumbers.py
+++ TauAnalysis/Skimming/test/submitSkimByRunEventNumbers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from TauAnalysis.Configuration.submitToBatch import submitToBatch
-from TauAnalysis.Skimming.makeReplacementsSkimByRunEventNumbers import makeReplacementsSkimByRunEventNumbers
+from TauAnalysis.Skimming.tools.makeReplacementsSkimByRunEventNumbers import makeReplacementsSkimByRunEventNumbers
 
 # name of the directory (either on afs area or castor)
 # to which all .root files produced by the cmsRun job will be copied
@@ -21,5 +21,5 @@ for i in range(21):
     submitToBatch(configFile = "skimByRunEventNumbers_cfg.py", channel = "ZtoMuTau",
                   sample = "InclusivePPmuX_10TeV_part%(i)02d" % {"i" : (i + 1)},
                   replFunction = makeReplacementsSkimByRunEventNumbers, replacements =
-                  "maxEvents = -1; runEventNumberFileName = /afs/cern.ch/user/v/veelken/scratch0/CMSSW_3_1_4/src/TauAnalysis/DQMTools/test/selEvents_ZtoTauNu_InclusivePPmuX_passed.txt",
+                  "maxEvents = -1; recoSampleDefinitionsFileName = recoSampleDefinitionsZtoMuTau_10TeV_cfi; runEventNumberFileName = /afs/cern.ch/user/v/veelken/scratch0/CMSSW_3_1_4/src/TauAnalysis/DQMTools/test/selEvents_ZtoTauNu_InclusivePPmuX_passed.txt",
                   job = "skimByRunEventNumbers", queue = "1nd", outputFilePath = outputFilePath)
