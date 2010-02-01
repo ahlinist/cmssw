8d8bbf43d02f0fb72d371101769e9d4a99c04e17
liis <>
2010-02-01 15:02:43 +0000

    added selection for metsignificance objects

--- TauAnalysis/RecoTools/python/patMetSelection_cff.py
+++ TauAnalysis/RecoTools/python/patMetSelection_cff.py
@@ -9,10 +9,19 @@ import copy
 # on how to use the cut-string parser
 
 # require at least 30 GeV missing transverse energy in the event
-selectedLayer1METsPt15 = cms.EDFilter("PATMETSelector",
+selectedLayer1METsPt25 = cms.EDFilter("PATMETSelector",
     src = cms.InputTag("layer1METs"),                                 
-    cut = cms.string('pt > 15.'),
+    cut = cms.string('pt > 25.'),
     filter = cms.bool(False)
 )
 
-selectLayer1METs = cms.Sequence( selectedLayer1METsPt15 )
+selectedMetSignificance15 = cms.EDFilter("METSignificanceSelector",
+                                           src = cms.InputTag("metsignificance"),
+                                           cut = cms.string('metSignificance() > 15.'),
+                                           filter = cms.bool(False)
+                                       )
+
+
+selectLayer1METs = cms.Sequence( selectedLayer1METsPt25
+                                 *selectedMetSignificance15)
+
