08ac36c1db583f5385ef49313bb0359b5c5431ec
liis <>
2010-02-02 10:13:44 +0000

    added tau Recoil energy calculated from only forward of central calorimeter regions

--- TauAnalysis/RecoTools/python/tauRecoilEnergy_cff.py
+++ TauAnalysis/RecoTools/python/tauRecoilEnergy_cff.py
@@ -24,7 +24,27 @@ tauRecoilEnergyFromJetsPt1 = cms.EDFilter("PATTauRecoilEnergyFromJetsSelector",
 tauRecoilEnergyFromCaloTowers = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
     srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoCumulative'),
     srcEnergyObjects = cms.InputTag('towerMaker'),
-    etaMin = cms.double(-5.),
+    etaMin = cms.double(0.),
+    etaMax = cms.double(+5.),
+    etMin = cms.double(0.5),
+    dPhiMin = cms.double(2.64),
+    verbosity = cms.untracked.int32(0)
+)
+
+tauRecoilEnergyFromCentralCaloTowers = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
+    srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoCumulative'),
+    srcEnergyObjects = cms.InputTag('towerMaker'),
+    etaMin = cms.double(0.),
+    etaMax = cms.double(+3.),
+    etMin = cms.double(0.5),
+    dPhiMin = cms.double(2.64),
+    verbosity = cms.untracked.int32(0)
+)
+
+tauRecoilEnergyFromForwardCaloTowers = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
+    srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoCumulative'),
+    srcEnergyObjects = cms.InputTag('towerMaker'),
+    etaMin = cms.double(+3.),
     etaMax = cms.double(+5.),
     etMin = cms.double(0.5),
     dPhiMin = cms.double(2.64),
@@ -34,6 +54,25 @@ tauRecoilEnergyFromCaloTowers = cms.EDProducer("PATTauRecoilEnergyFromCaloTowers
 tauRecoilEnergyFromCaloTowersPt5 = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
     src = cms.InputTag("tauRecoilEnergyFromCaloTowers"),
     cut = cms.string('etSum < 5.'),
+    filter = cms.bool(False)
+)  
+
+tauRecoilEnergyFromCaloTowersPt10 = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+                                                    src = cms.InputTag("tauRecoilEnergyFromCaloTowers"),
+                                                    cut = cms.string('etSum < 10.'),
+                                                    filter = cms.bool(False)
+                                                )
+
+
+tauRecoilEnergyFromCentralCaloTowersPt5 = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+    src = cms.InputTag("tauRecoilEnergyFromCentralCaloTowers"),
+    cut = cms.string('etSum < 5.'),
+    filter = cms.bool(False)
+)  
+
+tauRecoilEnergyFromForwardCaloTowersPt5 = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+    src = cms.InputTag("tauRecoilEnergyFromForwardCaloTowers"),
+    cut = cms.string('etSum < 3.'),
     filter = cms.bool(False)
 )  
 
@@ -57,7 +96,7 @@ tauRecoilEnergyFromJetsPt1LooseIsolation = cms.EDFilter("PATTauRecoilEnergyFromJ
 tauRecoilEnergyFromCaloTowersLooseIsolation = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
     srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoLooseIsolationCumulative'),
     srcEnergyObjects = cms.InputTag('towerMaker'),
-    etaMin = cms.double(-5.),
+    etaMin = cms.double(0.),
     etaMax = cms.double(+5.),
     etMin = cms.double(0.5),
     dPhiMin = cms.double(2.64),
@@ -70,7 +109,50 @@ tauRecoilEnergyFromCaloTowersPt5LooseIsolation = cms.EDFilter("PATTauRecoilEnerg
     filter = cms.bool(False)
 )  
 
+tauRecoilEnergyFromCaloTowersPt10LooseIsolation = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+                                                                  src = cms.InputTag("tauRecoilEnergyFromCaloTowersLooseIsolation"),
+                                                                  cut = cms.string('etSum < 10.'),
+                                                                  filter = cms.bool(False)
+                                                              )
+
+
+tauRecoilEnergyFromCentralCaloTowersLooseIsolation = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
+    srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoLooseIsolationCumulative'),
+    srcEnergyObjects = cms.InputTag('towerMaker'),
+    etaMin = cms.double(0.),
+    etaMax = cms.double(+3.),
+    etMin = cms.double(0.5),
+    dPhiMin = cms.double(2.64),
+    verbosity = cms.untracked.int32(0)
+)
+
+tauRecoilEnergyFromCentralCaloTowersPt5LooseIsolation = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+    src = cms.InputTag("tauRecoilEnergyFromCentralCaloTowersLooseIsolation"),
+    cut = cms.string('etSum < 5.'),
+    filter = cms.bool(False)
+)  
+
+tauRecoilEnergyFromForwardCaloTowersLooseIsolation = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
+    srcLeptons = cms.InputTag('selectedLayer1TausForWTauNuEcalCrackVetoLooseIsolationCumulative'),
+    srcEnergyObjects = cms.InputTag('towerMaker'),
+    etaMin = cms.double(+3.),
+    etaMax = cms.double(+5.),
+    etMin = cms.double(0.5),
+    dPhiMin = cms.double(2.64),
+    verbosity = cms.untracked.int32(0)
+)
+
+tauRecoilEnergyFromForwardCaloTowersPt5LooseIsolation = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
+    src = cms.InputTag("tauRecoilEnergyFromForwardCaloTowersLooseIsolation"),
+    cut = cms.string('etSum < 3.'),
+    filter = cms.bool(False)
+)  
+
 produceTauRecoilEnergy = cms.Sequence( tauRecoilEnergyFromJets * tauRecoilEnergyFromJetsPt1
-                                      * tauRecoilEnergyFromCaloTowers * tauRecoilEnergyFromCaloTowersPt5
-                                      * tauRecoilEnergyFromJetsLooseIsolation * tauRecoilEnergyFromJetsPt1LooseIsolation
-                                      * tauRecoilEnergyFromCaloTowersLooseIsolation * tauRecoilEnergyFromCaloTowersPt5LooseIsolation )
+                                       * tauRecoilEnergyFromCaloTowers * tauRecoilEnergyFromCaloTowersPt5 * tauRecoilEnergyFromCaloTowersPt10
+                                       * tauRecoilEnergyFromCentralCaloTowers * tauRecoilEnergyFromCentralCaloTowersPt5
+                                       * tauRecoilEnergyFromForwardCaloTowers * tauRecoilEnergyFromForwardCaloTowersPt5
+                                       * tauRecoilEnergyFromJetsLooseIsolation * tauRecoilEnergyFromJetsPt1LooseIsolation
+                                       * tauRecoilEnergyFromCaloTowersLooseIsolation * tauRecoilEnergyFromCaloTowersPt5LooseIsolation * tauRecoilEnergyFromCaloTowersPt10LooseIsolation
+                                       * tauRecoilEnergyFromCentralCaloTowersLooseIsolation * tauRecoilEnergyFromCentralCaloTowersPt5LooseIsolation
+                                       * tauRecoilEnergyFromForwardCaloTowersLooseIsolation * tauRecoilEnergyFromForwardCaloTowersPt5LooseIsolation )
