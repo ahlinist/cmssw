8c5b6a6aa3832cbe3476865a6efa56b36b2c5d6e
liis <>
2010-02-02 11:49:12 +0000

    added separate histograms for Recoil energy in central and forward calo-towers

--- TauAnalysis/Core/plugins/PATLeptonRecoilEnergyHistManager.cc
+++ TauAnalysis/Core/plugins/PATLeptonRecoilEnergyHistManager.cc
@@ -16,12 +16,12 @@ PATLeptonRecoilEnergyHistManager<T1,T2>::PATLeptonRecoilEnergyHistManager(const 
   : HistManagerBase(cfg)
 {
   //std::cout << "<PATLeptonRecoilEnergyHistManager::PATLeptonRecoilEnergyHistManager>:" << std::endl;
-
+  
   leptonRecoilEnergySrc_ = cfg.getParameter<edm::InputTag>("leptonRecoilEnergySource");
   //std::cout << " leptonRecoilEnergySrc = " << leptonRecoilEnergySrc_ << std::endl;
-
+  
   leptonWeightExtractors_ = getTauJetWeightExtractors<T1>(cfg, "leptonWeightSource");
-
+  
   std::string normalization_string = cfg.getParameter<std::string>("normalization");
   normMethod_ = getNormMethod(normalization_string, "leptons");
 }
@@ -40,10 +40,10 @@ void PATLeptonRecoilEnergyHistManager<T1,T2>::bookHistogramsImp()
 {
   //std::cout << "<PATLeptonRecoilEnergyHistManager::bookHistogramsImp>:" << std::endl;
   
-  bookWeightHistograms(*dqmStore_, "LeptonWeight", "Lepton Weight", 
-		       hLeptonWeightPosUnweighted_, hLeptonWeightPosWeighted_, 
+  bookWeightHistograms(*dqmStore_, "LeptonWeight", "Lepton Weight",
+		       hLeptonWeightPosUnweighted_, hLeptonWeightPosWeighted_,
 		       hLeptonWeightNegUnweighted_, hLeptonWeightNegWeighted_);
-
+  
   hEtSum_ = book1D("EtSum", "#Sigma E_{T}^{recoil}", 100, 0., 50.);
   hNumObjects_  = book1D("NumObjects", "Recoil Energy Objects", 50, -0.5, 49.5);
 }
@@ -56,28 +56,28 @@ double PATLeptonRecoilEnergyHistManager<T1,T2>::getLeptonWeight(const PATLeptonR
 
 template<typename T1, typename T2>
 void PATLeptonRecoilEnergyHistManager<T1,T2>::fillHistogramsImp(const edm::Event& evt, const edm::EventSetup& es, double evtWeight)
-{  
-  //std::cout << "<PATLeptonRecoilEnergyHistManager::fillHistogramsImp>:" << std::endl; 
-
+{
+  //std::cout << "<PATLeptonRecoilEnergyHistManager::fillHistogramsImp>:" << std::endl;
+  
   typedef std::vector<PATLeptonRecoilEnergy<T1,T2> > PATLeptonRecoilEnergyCollection;
   edm::Handle<PATLeptonRecoilEnergyCollection> leptonRecoilEnergyCollection;
   evt.getByLabel(leptonRecoilEnergySrc_, leptonRecoilEnergyCollection);
-  
+
   double leptonWeightSum = 0.;
   for ( typename PATLeptonRecoilEnergyCollection::const_iterator leptonRecoilEnergy = leptonRecoilEnergyCollection->begin();
 	leptonRecoilEnergy != leptonRecoilEnergyCollection->end(); ++leptonRecoilEnergy ) {
     leptonWeightSum += getLeptonWeight(*leptonRecoilEnergy);
   }
-
+  
   for ( typename PATLeptonRecoilEnergyCollection::const_iterator leptonRecoilEnergy = leptonRecoilEnergyCollection->begin();
 	leptonRecoilEnergy != leptonRecoilEnergyCollection->end(); ++leptonRecoilEnergy ) {
-
+    
     double leptonWeight = getLeptonWeight(*leptonRecoilEnergy);
     double weight = getWeight(evtWeight, leptonWeight, leptonWeightSum);
-
-    fillWeightHistograms(hLeptonWeightPosUnweighted_, hLeptonWeightPosWeighted_, 
+    
+    fillWeightHistograms(hLeptonWeightPosUnweighted_, hLeptonWeightPosWeighted_,
 			 hLeptonWeightNegUnweighted_, hLeptonWeightNegWeighted_, leptonWeight);
-
+    
     hEtSum_->Fill(leptonRecoilEnergy->etSum(), weight);
     hNumObjects_->Fill(leptonRecoilEnergy->recoilEnergyObjects().size(), weight);
   }
@@ -89,6 +89,10 @@ void PATLeptonRecoilEnergyHistManager<T1,T2>::fillHistogramsImp(const edm::Event
 
 typedef PATLeptonRecoilEnergyHistManager<pat::Tau, reco::Jet> TauRecoilEnergyFromJetsHistManager;
 typedef PATLeptonRecoilEnergyHistManager<pat::Tau, CaloTower> TauRecoilEnergyFromCaloTowersHistManager;
+typedef PATLeptonRecoilEnergyHistManager<pat::Tau, CaloTower> TauRecoilEnergyFromForwardCaloTowersHistManager;
+typedef PATLeptonRecoilEnergyHistManager<pat::Tau, CaloTower> TauRecoilEnergyFromCentralCaloTowersHistManager;
+
+
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
@@ -97,10 +101,19 @@ DEFINE_EDM_PLUGIN(HistManagerPluginFactory, TauRecoilEnergyFromJetsHistManager, 
 DEFINE_EDM_PLUGIN(AnalyzerPluginFactory, TauRecoilEnergyFromCaloTowersHistManager, "TauRecoilEnergyFromCaloTowersHistManager");
 DEFINE_EDM_PLUGIN(HistManagerPluginFactory, TauRecoilEnergyFromCaloTowersHistManager, "TauRecoilEnergyFromCaloTowersHistManager");
 
+DEFINE_EDM_PLUGIN(AnalyzerPluginFactory, TauRecoilEnergyFromForwardCaloTowersHistManager, "TauRecoilEnergyFromForwardCaloTowersHistManager");
+DEFINE_EDM_PLUGIN(HistManagerPluginFactory, TauRecoilEnergyFromForwardCaloTowersHistManager, "TauRecoilEnergyFromForwardCaloTowersHistManager");
+
+DEFINE_EDM_PLUGIN(AnalyzerPluginFactory, TauRecoilEnergyFromCentralCaloTowersHistManager, "TauRecoilEnergyFromCentralCaloTowersHistManager");
+DEFINE_EDM_PLUGIN(HistManagerPluginFactory, TauRecoilEnergyFromCentralCaloTowersHistManager, "TauRecoilEnergyFromCentralCaloTowersHistManager");
+
 #include "TauAnalysis/Core/interface/HistManagerAdapter.h"
 
 typedef HistManagerAdapter<TauRecoilEnergyFromJetsHistManager> TauRecoilEnergyFromJetsAnalyzer;
 typedef HistManagerAdapter<TauRecoilEnergyFromCaloTowersHistManager> TauRecoilEnergyFromCaloTowersAnalyzer;
+typedef HistManagerAdapter<TauRecoilEnergyFromCaloTowersHistManager> TauRecoilEnergyFromForwardCaloTowersAnalyzer;
+typedef HistManagerAdapter<TauRecoilEnergyFromCaloTowersHistManager> TauRecoilEnergyFromCentralCaloTowersAnalyzer;
+
 
 DEFINE_ANOTHER_FWK_MODULE(TauRecoilEnergyFromJetsAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(TauRecoilEnergyFromCaloTowersAnalyzer);
