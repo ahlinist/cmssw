0a80283df1533525d2ff18a1a3fcf1d088a82128
pandolf <>
2010-02-08 11:48:47 +0000

    added vertex information and jet energy corrections for pfakt5 and pfakt7

--- JetMETCorrections/GammaJet/test/config_cfg.py
+++ JetMETCorrections/GammaJet/test/config_cfg.py
@@ -55,7 +55,7 @@ process.source = cms.Source("PoolSource",
 # fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_1_0_pre11/RelValZmumuJets_Pt_20_300_GEN/GEN-SIM-RECO/MC_31X_V1_LowLumiPileUp-v1/0001/FE549F7D-2C65-DE11-9B82-001D09F2AF1E.root')
 #    fileNames = cms.untracked.vstring('file:/tmp/voutila/Cern/data/summer09/raw/PhotonJet_Pt80to120_Summer09-MC_31X_V3-v1_x100.root')
     fileNames = cms.untracked.vstring(
-'file:/cmsrm/pc18/pandolf/data/DiJetFilter5/DiJetSkim_123592_596_615_732_734_818.root'
+'file:/cmsrm/pc18/pandolf/CMSSW_3_3_3/src/JetMETCorrections/GammaJet/test/events_QCDDiJets_Pt0_15_Summer09.root'
 #'file:/cmsrm/pc18/pandolf/data/DiJetFilter5/DiJetSkim_124009.root',
 #'file:/cmsrm/pc18/pandolf/data/DiJetFilter5/DiJetSkim_124020.root',
 #'file:/cmsrm/pc18/pandolf/data/DiJetFilter5/DiJetSkim_124022.root',
@@ -76,33 +76,10 @@ process.printTree = cms.EDFilter("ParticleTreeDrawer",
     printIndex = cms.untracked.bool(True)
 )
 
-#physics declared technical bit filter
-process.hltPhysicsDeclared = cms.EDFilter("HLTHighLevel",
-                                TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-                                HLTPaths = cms.vstring("HLT_PhysicsDeclared"
-                                                       ),
-                                eventSetupPathsKey = cms.string(''),
-                                andOr = cms.bool(True),
-                                throw = cms.bool(True)
-)
 
-#bsc technical bit filter
-process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
-process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
-process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
-process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT ( 36 OR 37 OR 38 OR 39 )')
-
-#monster track event cleaning
-process.monster = cms.EDFilter(
-   "FilterOutScraping",
-   applyfilter = cms.untracked.bool(True),
-   debugOn = cms.untracked.bool(False),
-   numtrack = cms.untracked.uint32(10),
-   thresh = cms.untracked.double(0.2)
-)
 
 #############   Include the jet corrections ##########
-process.load("JetMETCorrections.Configuration.L2L3Corrections_900GeV_cff")
+process.load("JetMETCorrections.Configuration.L2L3Corrections_Summer09_7TeV_ReReco332_cff")
 # set the record's IOV. Must be defined once. Choose ANY correction service. #
 process.prefer("L2L3JetCorrectorAK5PF") 
 
@@ -117,7 +94,7 @@ process.myanalysis = cms.EDAnalyzer("GammaJetAnalyzer",
     Photonsrc = cms.untracked.InputTag("photons"),
     recoCollection = cms.string('EcalRecHitsEB'),
     JetCorrectionService_pfakt5 = cms.string('L2L3JetCorrectorAK5PF'),
-    JetCorrectionService_pfakt7 = cms.string('L2L3JetCorrectorAK7PF'),
+    JetCorrectionService_pfakt7 = cms.string('L2L3JetCorrectorAK5PF'),
     jetsite = cms.untracked.InputTag("iterativeCone5CaloJets"),
     jetskt4 = cms.untracked.InputTag("kt4CaloJets"),
     jetskt6 = cms.untracked.InputTag("kt6CaloJets"),
@@ -129,20 +106,42 @@ process.myanalysis = cms.EDAnalyzer("GammaJetAnalyzer",
     jetspfkt4 = cms.untracked.InputTag("kt4PFJets"),
     jetspfkt6 = cms.untracked.InputTag("kt6PFJets"),
     jetspfakt5 = cms.untracked.InputTag("ak5PFJets"),
-    jetspfakt7 = cms.untracked.InputTag("ak7PFJets"),
+    jetspfakt7 = cms.untracked.InputTag("ak5PFJets"),
     jetspfsis5 = cms.untracked.InputTag("sisCone5PFJets"),
     jetspfsis7 = cms.untracked.InputTag("sisCone7PFJets"),
     hbhits = cms.untracked.InputTag("hbhereco"),
     jetsgenite = cms.untracked.InputTag("iterativeCone5GenJets"),
     jetsgenkt4 = cms.untracked.InputTag("kt4GenJets"),
     jetsgenkt6 = cms.untracked.InputTag("kt6GenJets"),
-    jetsgenakt5 = cms.untracked.InputTag("ak5GenJets"),
-    jetsgenakt7 = cms.untracked.InputTag("ak7GenJets"),
+    jetsgenakt5 = cms.untracked.InputTag("ak5GenJetsptmin1"),
+    jetsgenakt7 = cms.untracked.InputTag("sisCone5GenJets"),
     jetsgensis5 = cms.untracked.InputTag("sisCone5GenJets"),
     jetsgensis7 = cms.untracked.InputTag("sisCone7GenJets"),
     TriggerTag = cms.untracked.InputTag("TriggerResults::HLT"),
     vertices = cms.untracked.InputTag("offlinePrimaryVertices")
 )
+
+from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
+process.ak5GenJetsptmin1 = ak5GenJets.clone()
+process.ak5GenJetsptmin1.jetPtMin = cms.double(1.0)
+
+process.genParticlesForJets = cms.EDFilter("InputGenJetsParticleSelector",
+    src = cms.InputTag("genParticles"),
+    ignoreParticleIDs = cms.vuint32(
+         1000022,
+         1000012, 1000014, 1000016,
+         2000012, 2000014, 2000016,
+         1000039, 5100039,
+         4000012, 4000014, 4000016,
+         9900012, 9900014, 9900016,
+         39),
+    partonicFinalState = cms.bool(False),
+    excludeResonances = cms.bool(True),
+    excludeFromResonancePids = cms.vuint32(12, 13, 14, 16),
+    tausAsJets = cms.bool(False)
+)
+
+newGenJets = cms.Sequence(process.genParticlesForJets* process.ak5GenJetsptmin1)
 
 # histogram service
 process.TFileService = cms.Service("TFileService",
@@ -151,4 +150,4 @@ process.TFileService = cms.Service("TFileService",
 #process.p = cms.Path(process.myanalysis)
 # produce JPT jets before running analysis
 #process.p = cms.Path(process.ZSPJetCorrections*process.JetPlusTrackCorrections*process.myanalysis)
-process.p = cms.Path(process.hltPhysicsDeclared*process.hltLevel1GTSeed*process.monster*process.myanalysis)
+process.p = cms.Path(newGenJets*process.myanalysis)
