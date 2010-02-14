f8f6857137c046cae8dfa4b060e093471cff99a8
veelken <>
2010-02-14 13:45:43 +0000

    comment-out corrupt files

--- TauAnalysis/Configuration/python/plotZtoMuTau_processes_10TeV_cfi.py
+++ TauAnalysis/Configuration/python/plotZtoMuTau_processes_10TeV_cfi.py
@@ -24,7 +24,7 @@ processZtoMuTau_Ztautau.config_dqmFileLoader.inputFileNames = cms.vstring(
     'plotsZtoMuTau_Ztautau_10TeV_part11.root',
     'plotsZtoMuTau_Ztautau_10TeV_part12.root',
     'plotsZtoMuTau_Ztautau_10TeV_part13.root',
-    #'plotsZtoMuTau_Ztautau_10TeV_part14.root',
+    'plotsZtoMuTau_Ztautau_10TeV_part14.root',
     'plotsZtoMuTau_Ztautau_10TeV_part15.root'    
 )
 processZtoMuTau_Ztautau.config_dqmFileLoader.scaleFactor = cms.double(corrFactorZtoMuTau_Ztautau_10TeV*intLumiZtoMuTau_Data_10TeV/intLumiZtoMuTau_Ztautau_10TeV)
@@ -95,8 +95,8 @@ processZtoMuTau_WplusJets.config_dqmFileLoader.inputFileNames = cms.vstring(
     'plotsZtoMuTau_WplusJets_10TeV_part22.root',
     'plotsZtoMuTau_WplusJets_10TeV_part23.root',
     'plotsZtoMuTau_WplusJets_10TeV_part24.root',
-    'plotsZtoMuTau_WplusJets_10TeV_part25.root',
-    'plotsZtoMuTau_WplusJets_10TeV_part26.root'
+    'plotsZtoMuTau_WplusJets_10TeV_part25.root'
+    #'plotsZtoMuTau_WplusJets_10TeV_part26.root'
 )
 
 processZtoMuTau_WplusJets.config_dqmFileLoader.scaleFactor = cms.double(corrFactorZtoMuTau_WplusJets_10TeV*intLumiZtoMuTau_Data_10TeV/intLumiZtoMuTau_WplusJets_10TeV)
@@ -113,7 +113,7 @@ processZtoMuTau_WplusJetsSum.config_dqmFileLoader.scaleFactor = cms.double(1.)
 processZtoMuTau_InclusivePPmuX = copy.deepcopy(process_InclusivePPmuX)
 processZtoMuTau_InclusivePPmuX.config_dqmFileLoader.inputFileNames = cms.vstring(
     'plotsZtoMuTau_InclusivePPmuX_10TeV_part01.root',
-    #'plotsZtoMuTau_InclusivePPmuX_10TeV_part02.root',
+    'plotsZtoMuTau_InclusivePPmuX_10TeV_part02.root',
     'plotsZtoMuTau_InclusivePPmuX_10TeV_part03.root',
     'plotsZtoMuTau_InclusivePPmuX_10TeV_part04.root',
     'plotsZtoMuTau_InclusivePPmuX_10TeV_part05.root',
@@ -154,21 +154,21 @@ processZtoMuTau_PPmuXptGt20_part01.config_dqmFileLoader.inputFileNames = cms.vst
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part04.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part05.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part06.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part07.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part07.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part08.root',
-    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part09.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part10.root',
+    'plotsZtoMuTau_PPmuXptGt20_10TeV_part09.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part10.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part11.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part12.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part13.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part14.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part15.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part14.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part15.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part16.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part17.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part18.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part19.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part20.root',
-    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part21.root',
+    'plotsZtoMuTau_PPmuXptGt20_10TeV_part21.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part22.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part23.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part24.root',
@@ -178,7 +178,7 @@ processZtoMuTau_PPmuXptGt20_part01.config_dqmFileLoader.scaleFactor = cms.double
 
 processZtoMuTau_PPmuXptGt20_part02 = copy.deepcopy(process_PPmuXptGt20)
 processZtoMuTau_PPmuXptGt20_part02.config_dqmFileLoader.inputFileNames = cms.vstring(    
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part26.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part26.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part27.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part28.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part29.root',
@@ -187,7 +187,7 @@ processZtoMuTau_PPmuXptGt20_part02.config_dqmFileLoader.inputFileNames = cms.vst
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part32.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part33.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part34.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part35.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part35.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part36.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part37.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part38.root',
@@ -195,14 +195,14 @@ processZtoMuTau_PPmuXptGt20_part02.config_dqmFileLoader.inputFileNames = cms.vst
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part40.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part41.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part42.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part43.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part43.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part44.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part45.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part46.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part47.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part48.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part49.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part50.root'
+    'plotsZtoMuTau_PPmuXptGt20_10TeV_part49.root'
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part50.root'
 )
 processZtoMuTau_PPmuXptGt20_part02.config_dqmFileLoader.scaleFactor = cms.double(corrFactorZtoMuTau_PPmuXptGt20_10TeV*intLumiZtoMuTau_Data_10TeV/intLumiZtoMuTau_PPmuXptGt20_10TeV)
 
@@ -214,7 +214,7 @@ processZtoMuTau_PPmuXptGt20_part03.config_dqmFileLoader.inputFileNames = cms.vst
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part54.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part55.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part56.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part57.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part57.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part58.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part59.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part60.root',
@@ -222,10 +222,10 @@ processZtoMuTau_PPmuXptGt20_part03.config_dqmFileLoader.inputFileNames = cms.vst
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part62.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part63.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part64.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part65.root',
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part65.root',
     'plotsZtoMuTau_PPmuXptGt20_10TeV_part66.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part67.root',
-    'plotsZtoMuTau_PPmuXptGt20_10TeV_part68.root'
+    'plotsZtoMuTau_PPmuXptGt20_10TeV_part67.root'
+    #'plotsZtoMuTau_PPmuXptGt20_10TeV_part68.root'
 )
 processZtoMuTau_PPmuXptGt20_part03.config_dqmFileLoader.scaleFactor = cms.double(corrFactorZtoMuTau_PPmuXptGt20_10TeV*intLumiZtoMuTau_Data_10TeV/intLumiZtoMuTau_PPmuXptGt20_10TeV)
 
