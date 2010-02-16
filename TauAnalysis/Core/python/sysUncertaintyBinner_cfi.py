c791a70d03fc8d466688dce3b049de81815d2500
veelken <>
2010-02-16 15:25:52 +0000

    restructuring according to change from SysUnCeryaintyBinning to SysUncertaintyBinner (which uses DataBinning/ModelBinning objects internally)

--- TauAnalysis/Core/python/sysUncertaintyBinner_cfi.py
+++ TauAnalysis/Core/python/sysUncertaintyBinner_cfi.py
@@ -4,24 +4,7 @@ import copy
 sysUncertaintyBinner = cms.PSet(
     pluginName = cms.string('sysUncertaintyBinner'),
     pluginType = cms.string('SysUncertaintyBinner'),
-    binning = cms.PSet(
-        name = cms.string("sysUncertaintyBinning"),
-        config = cms.VPSet(
-            cms.PSet(
-                extractor = cms.PSet(
-                    pluginType = cms.string("ConstObjValExtractor"),
-                    value = cms.double(0)
-                ),
-                binning = cms.PSet(
-                    min = cms.double(-0.5),
-                    max = cms.double(+0.5)
-                )
-            )
-        ),
-        systematics = cms.vstring()
-    ),
-    binningService = cms.PSet(
-        pluginType = cms.string("SysUncertaintyBinningService")
-    ),
+    binnerPlugins = cms.VPSet(),
+    systematics = cms.vstring(),
     dqmDirectory_store = cms.string('sysUncertaintyBinningResults')
 )
