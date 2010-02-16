9f50b10b8a90d70ca61554aa638b168bd1117b2f
veelken <>
2010-02-16 15:22:08 +0000

    removed definition of SysUncertaintyBinningService

--- TauAnalysis/Core/plugins/BinningService.cc
+++ TauAnalysis/Core/plugins/BinningService.cc
@@ -19,14 +19,12 @@ BinningBase* BinningService<T>::createBinning() const
 
 #include "TauAnalysis/Core/interface/DataBinning.h"
 #include "TauAnalysis/Core/interface/ModelBinning.h"
-#include "TauAnalysis/Core/interface/SysUncertaintyBinning.h"
 
 typedef BinningService<DataBinning> DataBinningService;
 typedef BinningService<ModelBinning> ModelBinningService;
-typedef BinningService<SysUncertaintyBinning> SysUncertaintyBinningService;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_EDM_PLUGIN(BinningServicePluginFactory, DataBinningService, "DataBinningService");
 DEFINE_EDM_PLUGIN(BinningServicePluginFactory, ModelBinningService, "ModelBinningService");
-DEFINE_EDM_PLUGIN(BinningServicePluginFactory, SysUncertaintyBinningService, "SysUncertaintyBinningService");
+
