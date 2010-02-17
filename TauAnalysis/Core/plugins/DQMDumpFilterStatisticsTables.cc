a864d2baa5c66f8103c000fe6d9e06bc10ee85c5
veelken <>
2010-02-17 17:09:12 +0000

    - fixed bug with unsigned int becoming negative (and becoming interpreted as very large positive values)   in case the length of column labels exceeds the column width - added flag to print summary table only (default = print all tables)

--- TauAnalysis/Core/plugins/DQMDumpFilterStatisticsTables.cc
+++ TauAnalysis/Core/plugins/DQMDumpFilterStatisticsTables.cc
@@ -81,14 +81,16 @@ void printSummaryTable(std::ostream& stream, unsigned widthNameColumn, unsigned 
     if ( columnLabel == columnLabels.begin() ) {
       stream << std::setw(widthNameColumn) << std::left << (*columnLabel);
     } else {
-      for ( int iCharacter = 0; iCharacter < (widthNumberColumns - columnLabel->length()); ++iCharacter ) {
-	stream << " ";
+      if ( columnLabel->length() < widthNumberColumns ) {
+	for ( unsigned iCharacter = 0; iCharacter < (widthNumberColumns - columnLabel->length()); ++iCharacter ) {
+	  stream << " ";
+	}
       }
       stream << " " << std::setw(columnLabel->length()) << std::left << (*columnLabel);
     }
   }
   stream << std::endl;
-  for ( int iCharacter = 0; iCharacter < (widthNameColumn + columnLabels.size()*widthNumberColumns + 4); ++iCharacter ) {
+  for ( unsigned iCharacter = 0; iCharacter < (widthNameColumn + columnLabels.size()*widthNumberColumns + 4); ++iCharacter ) {
     stream << "-";
   }
   stream << std::endl;
@@ -100,7 +102,7 @@ void printSummaryTable(std::ostream& stream, unsigned widthNameColumn, unsigned 
     }
     stream << std::endl;
   } 
-  for ( int iCharacter = 0; iCharacter < (widthNameColumn + columnLabels.size()*widthNumberColumns + 4); ++iCharacter ) {
+  for ( unsigned iCharacter = 0; iCharacter < (widthNameColumn + columnLabels.size()*widthNumberColumns + 4); ++iCharacter ) {
     stream << "-";
   }
   stream << std::endl << std::endl;  
