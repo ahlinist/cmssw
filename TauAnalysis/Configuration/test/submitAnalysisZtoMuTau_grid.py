4a34b5562036bffcde8f5ea35596e7722eca33aa
veelken <>
2010-02-22 14:15:15 +0000

    updated example to skimmed CMSSW_3_1_2 Ztautau sample from Summer'09 production

--- TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
@@ -3,7 +3,7 @@
 from TauAnalysis.Configuration.submitToGrid import submitToGrid
 from TauAnalysis.Configuration.makeReplacementsCrab import makeReplacementsAnalysis
 
-from TauAnalysis.Configuration.recoSampleDefinitionsZtoElecMu_cfi import *
+from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_10TeV_cfi import *
 
 # name of the directory (either on afs area or castor)
 # to which all .root files produced by the cmsRun job will be copied
@@ -11,10 +11,10 @@ outputDirectory = "/castor/cern.ch/user/v/veelken/plots/ZtoMuTau_grid/"
 #outputDirectory = "/castor/cern.ch/user/l/lusito/ZMuTauAnalysis/"
 
 # small cmsRun job for testing purposes...
-submitToGrid(configFile = "runZtoElecMu_cfg.py", channel = "ZtoElecMu",
-             sample = "Ztautau",
-             dbs_name = "/Ztautau/sunil-Summer08_Skim-411de0fd9a45f2e9090c88c36bde639d/USER",
+submitToGrid(configFile = "runZtoMuTau_cfg.py", channel = "ZtoMuTau",
+             sample = "Ztautau_10TeV",
+             dbs_name = "/Ztautau/sarkar-diTauSkim_CMSSW314_Summer09-MC_31X_V3-v1_RECO-7645e7582a8ca89f38eb471c03cbf450/USER",
              dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_local_09/servlet/DBSServlet",             
              replFunction = makeReplacementsAnalysis, replacements =
              "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false; estimateSysUncertainties = false",
-             outputFiles = [ plotsOutputFileNameZtoElecMu_Ztautau ], outputDirectory = outputDirectory)
+             outputFiles = [ plotsOutputFileNameZtoMuTau_Ztautau_10TeV ], outputDirectory = outputDirectory)
