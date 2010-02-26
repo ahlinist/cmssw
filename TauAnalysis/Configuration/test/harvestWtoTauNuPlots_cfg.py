2422d673d7430942acb6992e895e829df339796b
liis <>
2010-02-26 11:57:33 +0000

    updated for CMSSW_3_1_X analysis

--- TauAnalysis/Configuration/test/harvestWtoTauNuPlots_cfg.py
+++ TauAnalysis/Configuration/test/harvestWtoTauNuPlots_cfg.py
@@ -2,8 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('harvestWtoTauNuPlots')
 
-from TauAnalysis.Configuration.plotWtoTauNu_processes_cfi import *
-
 process.DQMStore = cms.Service("DQMStore")
 
 process.maxEvents = cms.untracked.PSet(            
@@ -16,13 +14,16 @@ process.source = cms.Source("EmptySource")
 # define "hooks" for replacing configuration parameters
 # in case running jobs on the CERN batch system
 #
-process.loadWtoTauNu = cms.EDAnalyzer("DQMFileLoader",
-    #process# = copy.deepcopy(#inputFileNames#)
-)
+from #recoSampleDefinitionsFile# import *
 
+process.loadWtoTauNu = cms.EDAnalyzer("DQMFileLoader",
+   #process# = copy.deepcopy(#inputFileNames#),
+   inputFilePath = cms.string(#inputFilePath#)
+    )
+                                      
 process.saveWtoTauNu = cms.EDAnalyzer("DQMSimpleFileSaver",
     outputFileName = cms.string(#outputFileName#)
-)
+    )
 #--------------------------------------------------------------------------------
 
 process.harvestWtoTauNuPlots = cms.Sequence( process.loadWtoTauNu
