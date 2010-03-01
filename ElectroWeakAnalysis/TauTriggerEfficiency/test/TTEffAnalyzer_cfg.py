91faf4fd7b547577b0576f4e567e10d5e33a735a
slehti <>
2010-03-01 09:10:35 +0000

    Commented L1CaloSim from the cfg

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalyzer_cfg.py
@@ -60,13 +60,13 @@ process.load('L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1031.L1Menu_MC2
 #    process.CaloTowerConstituentsMapBuilder
 #)
 
-process.load("RecoTauTag.L1CaloSim.l1calosim_cfi")
-process.l1CaloSim.AlgorithmSource = "RecHits"
-process.l1CaloSim.EmInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE"))
-process.l1CaloSim.DoBitInfo = cms.bool(True)
-process.l1CaloSim.EMActiveLevelIso = cms.double(4.0)
-process.l1CaloSim.HadActiveLevelIso = cms.double(4.0)
-process.l1CaloSim.IsolationEt = cms.double(2.0)
+#process.load("RecoTauTag.L1CaloSim.l1calosim_cfi")
+#process.l1CaloSim.AlgorithmSource = "RecHits"
+#process.l1CaloSim.EmInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE"))
+#process.l1CaloSim.DoBitInfo = cms.bool(True)
+#process.l1CaloSim.EMActiveLevelIso = cms.double(4.0)
+#process.l1CaloSim.HadActiveLevelIso = cms.double(4.0)
+#process.l1CaloSim.IsolationEt = cms.double(2.0)
 ###
 
 
