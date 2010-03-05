0b1b198cecf2ea975245831ed604c66399e84415
gcheveni <>
2010-03-05 10:15:26 +0000

    add methods

--- OnlineDB/ESCondDB/interface/ODESFEPedestalOffsetInfo.h
+++ OnlineDB/ESCondDB/interface/ODESFEPedestalOffsetInfo.h
@@ -38,13 +38,18 @@ class ODESFEPedestalOffsetInfo : public IODConfig {
   
   inline void setUser_comment(string comment) { m_user_comment = comment; }
   inline string getUser_comment() const { return m_user_comment; }
-  
+
+  inline void setRunIov_P(int run) {m_iov_P = run;}
+  inline void setRunIov_M(int run) {m_iov_M = run;}
+  inline int getRunIov_P() const { return m_iov_P; }
+  inline int getRunIov_M() const { return m_iov_M; }
+
   int fetchNextId() throw(std::runtime_error);
   void setParameters(std::map<string,string> my_keys_map);
   int fetchID()  throw(std::runtime_error);
 
-  vector<int>  fetchRunPlus(const int recid)  throw(std::runtime_error);
   std::map< int,int > fetchGain(const string gain)  throw(std::runtime_error);
+  std::map< int,int > fetchIovId(const int runp, const int runm)  throw(std::runtime_error);
  
 private:
   void prepareWrite()  throw(std::runtime_error);
@@ -63,6 +68,8 @@ private:
   int m_iov_id_pl;
   int m_iov_id_mi;
   string m_user_comment;
+  int m_iov_P;
+  int m_iov_M;
   
 };
 
