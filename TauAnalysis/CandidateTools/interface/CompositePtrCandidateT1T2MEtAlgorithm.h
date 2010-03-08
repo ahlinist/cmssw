4cfaa974c1095e738afd76969178e9d8d8804097
akalinow <>
2010-03-08 08:41:44 +0000

    * added methods for using the improved collinear approximation developed by L. Bianchini.

--- TauAnalysis/CandidateTools/interface/CompositePtrCandidateT1T2MEtAlgorithm.h
+++ TauAnalysis/CandidateTools/interface/CompositePtrCandidateT1T2MEtAlgorithm.h
@@ -15,7 +15,10 @@
 
 #include "TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h"
 
-#include <TMath.h>
+#include "TMath.h"
+#include "TF1.h"
+
+#include <string>
 
 template<typename T1, typename T2>
 class CompositePtrCandidateT1T2MEtAlgorithm 
@@ -29,8 +32,15 @@ class CompositePtrCandidateT1T2MEtAlgorithm
   {
     recoMode_ = cfg.getParameter<std::string>("recoMode");
     verbosity_ = cfg.getUntrackedParameter<int>("verbosity", 0);
+    scaleFuncImprovedCollinearApprox_ = cfg.getParameter<std::string>("scaleFuncImprovedCollinearApprox");
+    /// compute the scale factor to weight the diTau mass
+    /// computed in the improved collinear approximation;
+    /// NO re-scaling of the p4 is made at this stage.
+    scaleFunc_ = new TF1("scaleFunc_",scaleFuncImprovedCollinearApprox_.c_str(),10,300);
   }
-  ~CompositePtrCandidateT1T2MEtAlgorithm() {}
+  ~CompositePtrCandidateT1T2MEtAlgorithm() {
+    delete scaleFunc_;  
+  }
 
   CompositePtrCandidateT1T2MEt<T1,T2> buildCompositePtrCandidate(const T1Ptr leg1, 
 								 const T2Ptr leg2, 
@@ -56,7 +66,9 @@ class CompositePtrCandidateT1T2MEtAlgorithm
 //--- compute quantities that do dependent on MET
     if ( met.isNonnull() ) {
       compCollinearApprox(compositePtrCandidate, leg1->p4(), leg2->p4(), met->px(), met->py());
-    
+      // add a Improved collinear approximation
+      compImprovedCollinearApprox(compositePtrCandidate, leg1->p4(), leg2->p4(), met->px(), met->py());
+
       compositePtrCandidate.setP4CDFmethod(compP4CDFmethod(leg1->p4(), leg2->p4(), met->px(), met->py()));
       compositePtrCandidate.setMt12MET(compMt(leg1->p4(), leg2->p4(), met->px(), met->py()));    
       compositePtrCandidate.setMt1MET(compMt(leg1->p4(), met->px(), met->py()));
@@ -66,7 +78,7 @@ class CompositePtrCandidateT1T2MEtAlgorithm
 
       compZeta(compositePtrCandidate, leg1->p4(), leg2->p4(), met->px(), met->py());
     } else {
-      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), -1, -1, false);
+      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), -1, -1, false, 0);
     }
  
 //--- compute gen. level quantities
@@ -83,7 +95,14 @@ class CompositePtrCandidateT1T2MEtAlgorithm
         edm::LogError ("buildCompositePtrCandidate") << " Failed to set four-momentum:"
 						     << " recoMode = " << recoMode_ << " requires MET pointer to be valid !!";
       }
-    } else if ( recoMode_ == "cdfMethod" ) {
+    } else if ( recoMode_ == "ImprovedCollinearApprox" ) {
+      if ( met.isNonnull() ) {
+	compositePtrCandidate.setP4(compositePtrCandidate.p4ImprovedCollinearApprox());
+      } else {
+	edm::LogError ("buildCompositePtrCandidate") << " Failed to set four-momentum:"
+						     << " recoMode = " << recoMode_ << " requires MET pointer to be valid !!";
+      } 
+    }  else if ( recoMode_ == "cdfMethod" ) {
       if ( met.isNonnull() ) {
 	compositePtrCandidate.setP4(compositePtrCandidate.p4CDFmethod());
       } else {
@@ -141,11 +160,103 @@ class CompositePtrCandidateT1T2MEtAlgorithm
 
     if ( x1phys != 0. && x2phys != 0. ) {
       reco::Candidate::LorentzVector p4 = leg1/x1phys + leg2/x2phys;
-      compositePtrCandidate.setCollinearApproxQuantities(p4, x1, x2, isX1withinPhysRange && isX2withinPhysRange);
+      compositePtrCandidate.setCollinearApproxQuantities(p4, x1, x2, isX1withinPhysRange && isX2withinPhysRange,1);
     } else {
-      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), x1, x2, false);
+      compositePtrCandidate.setCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), x1, x2, false, 0);
     }
   }
+
+  /// compute the diTau mass in improved collinear approximation;
+  /// this new algorithm allows to recover events for which
+  /// the collinear approximation fails
+
+  void compImprovedCollinearApprox(CompositePtrCandidateT1T2MEt<T1,T2>& compositePtrCandidate,
+				   const reco::Candidate::LorentzVector& leg1,
+				   const reco::Candidate::LorentzVector& leg2,
+				   double metPx, double metPy)
+    {
+      double x1_numerator = leg1.px()*leg2.py() - leg2.px()*leg1.py();
+      double x1_denominator = leg2.py()*(leg1.px() + metPx) - leg2.px()*(leg1.py() + metPy);
+      double x1 = ( x1_denominator != 0. ) ? x1_numerator/x1_denominator : -1.;
+      
+      double x2_numerator = x1_numerator;
+      double x2_denominator = leg1.px()*(leg2.py() + metPy) - leg1.py()*(leg2.px() + metPx);
+      double x2 = ( x2_denominator != 0. ) ? x2_numerator/x2_denominator : -1.;
+      
+      /// define scalar products useful for later computations
+      double sp1 = (leg1.px()*metPx + leg1.py()*metPy);
+      double sp2 = (leg2.px()*metPx + leg2.py()*metPy);
+      double module_leg1 = leg1.pt();
+      double module_leg2 = leg2.pt();
+      double module_Met = sqrt(metPx*metPx + metPy*metPy);
+      double cos1 = sp1/(module_leg1*module_Met);
+      double cos2 = sp2/(module_leg2*module_Met);
+      double Enu1 = 0.;
+      double Enu2 = 0.;
+      /// define the angular windows to accept the events
+      bool collinear_leg1 = (cos1 >= 0.940);
+      bool collinear_leg2 = (cos2 >= 0.940);
+      bool backToback = (cos( leg1.phi()-leg2.phi() ) < -0.90 ); 
+      /// collinear approximation
+      if ( (x1 > 0. && x1 < 1.) &&  (x2 > 0. && x2 < 1.) ) {
+	reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+	compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, x1, x2, 1.0, true, 1);
+      }
+      /// rescue not-back-to-back events close to leg1
+      else if ( (x1 > 0. && x1 < 1.) && !(x2 > 0. && x2 < 1.) && collinear_leg1 && !backToback){
+	x1 = x1;
+	x2 = 1.;
+	reco::Candidate::LorentzVector p4Vis = leg1+leg2;
+	reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+	if(p4Vis.pt()>10.0) compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, x1, -99, scaleFunc_->Eval(p4Vis.pt()), true, 2);
+	else compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, x1, -99, 1.0, true, 2);
+      }
+      /// rescue not-back-to-back events close to leg2
+      else if ( !(x1 > 0. && x1 < 1.) && (x2 > 0. && x2 < 1.) && collinear_leg2 && !backToback ){
+	x1 = 1.;
+	x2 = x2;
+	reco::Candidate::LorentzVector p4Vis = leg1+leg2;
+	reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+	if(p4Vis.pt()>10.0) compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, -99, x2, scaleFunc_->Eval(p4Vis.pt()) , true, 2);
+	else compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, -99, x2, 1.0 , true, 2);
+      }
+      /// rescue back-to-back events using the known mean tau-energy fraction carried away
+      /// by the neutrino in the tau -> l + nu_l + nu_tau decay;
+      /// the first condition is a XOR between A=(x1 > 0. && x1 < 1.) and B=(x2 > 0. && x2 < 1.)
+      else if ( ( (!(x1 > 0. && x1 < 1.) && (x2 > 0. && x2 < 1.)) || ((x1 > 0. && x1 < 1.) && !(x2 > 0. && x2 < 1.))  ) && 
+		(collinear_leg1 || collinear_leg2) && 
+		backToback ){
+	
+	if(collinear_leg1){
+	  Enu1= 13./7.*leg1.pt();
+	  Enu2 = Enu1-module_Met;
+	  x1 = (Enu2 > 0) ? 7./20. : x1;
+	  x2 = (Enu2 > 0) ? leg2.pt()/(leg2.pt()+Enu2) : 1;
+	  reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+	  compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, 99, -99, 1.0, true, 3);
+	}
+	else {
+	  Enu1= 13./7.*leg1.pt();
+	  Enu2 = Enu1+module_Met;
+	  x1 = 7./20.;
+	  x2 = leg2.pt()/(leg2.pt()+Enu2);
+	  reco::Candidate::LorentzVector p4 = leg1/x1 + leg2/x2;
+	  compositePtrCandidate.setImprovedCollinearApproxQuantities(p4, -99, 99, 1.0, true, 3);
+	}
+	
+      }
+      /// if none of the previous conditions has been fulfilled,
+      /// don't reconstruct the diTau
+      else {
+	compositePtrCandidate.setImprovedCollinearApproxQuantities(reco::Candidate::LorentzVector(0,0,0,0), -1, -1, 1.0, false, 0);
+      }
+      
+    }
+  //////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////
+  
+
+
   void compZeta(CompositePtrCandidateT1T2MEt<T1,T2>& compositePtrCandidate,
 		const reco::Candidate::LorentzVector& leg1,
 		const reco::Candidate::LorentzVector& leg2,
@@ -238,6 +349,8 @@ class CompositePtrCandidateT1T2MEtAlgorithm
 
   std::string recoMode_;
   int verbosity_;
+  std::string scaleFuncImprovedCollinearApprox_;
+  TF1* scaleFunc_;
 };
 
 #endif 
