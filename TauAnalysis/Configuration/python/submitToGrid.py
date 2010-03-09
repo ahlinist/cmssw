31c1e2843f5a75e98ca8a4fb5a81ccec0ec526b5
veelken <>
2010-03-09 16:50:24 +0000

    added support for "job" label parameter

--- TauAnalysis/Configuration/python/submitToGrid.py
+++ TauAnalysis/Configuration/python/submitToGrid.py
@@ -8,7 +8,7 @@ from TauAnalysis.Configuration.prepareConfigFile import prepareConfigFile
 # crab configuration file for submission of cmsRun job to the Grid,
 # then submit the job to the Grid via crab
 #
-# NOTE: Function needs to be passed the first nine of the following ten arguments
+# NOTE: Function needs to be passed the first nine of the following eleven arguments
 #       (the tenth argument is an optional parameter)
 #
 #      (1) configFile
@@ -21,32 +21,35 @@ from TauAnalysis.Configuration.prepareConfigFile import prepareConfigFile
 #          name of the signal/background Monte Carlo sample to be analyzed,
 #          in "user-friendly format"
 #          (e.g. Ztautau)
-#      (4) dbs_name
+#      (4) job
+#          name of the job submitted to the batch system
+#          (allows to distinguish different jobs of same type running simultaneously)
+#      (5) dbs_name
 #          name of the signal/background Monte Carlo sample to be analyzed,
 #          as registered in DBS
 #          (e.g. /Ztautau/lusito-CMSSW_2_2_3_SkimMuTauZTT02_GEN_SIM_DIGI_L1_DIGI2RAW_HLT-5c432a197f799755d712fe824b01cf85/USER)
-#      (5) dbs_url
+#      (6) dbs_url
 #          url of the DBS server in which the sample has been registered
 #          (e.g. https://cmsdbsprod.cern.ch:8443/cms_dbs_prod_local_09_writer/servlet/DBSServlet)
-#      (6) replFunction
+#      (7) replFunction
 #          (python) function to be called for processing of replacements parameter string,
 #          depending of type of cmsRun job
 #          (e.g. TauAnalysis/Configuration/python/makeReplacementsAnalysis)
-#      (7) replacements
+#      (8) replacements
 #          list of replace statements to be applied to the original config file,
 #          in the format 'paramName1=paramValue1; paramName2=paramValue2;...';
 #          in the original config file, each occurence of any paramName
 #          will be replaced by the associated paramValue
-#      (8) outputFileNames
+#      (9) outputFileNames
 #          names of all .root files produced by cmsRun job
 #
 #          NOTE: .root files missing in this list will not be copied to castor !!
 #
-#      (9) outputFilePath
+#     (10) outputFilePath
 #          name of the directory (either on afs area or castor)
 #          to which all .root files produced by the cmsRun job will be copied
 #          (e.g. "/castor/cern.ch/user/v/veelken/")
-#     (10) submit
+#     (11) submit
 #          if "yes", jobs will be submitted to the batch system, otherwise config files
 #          and scripts are generated but no submission
 #
@@ -55,7 +58,7 @@ from TauAnalysis.Configuration.prepareConfigFile import prepareConfigFile
 #--------------------------------------------------------------------------------
 
 def submitToGrid(configFile = None, channel = None,
-                 sample = None, dbs_name = None, dbs_url = None, 
+                 sample = None, job = None, dbs_name = None, dbs_url = None, 
                  replFunction = None, replacements = "",
                  outputFileNames = None, outputFilePath = None,
                  submit = "yes"):
@@ -67,6 +70,8 @@ def submitToGrid(configFile = None, channel = None,
         raise ValueError("Undefined channel Parameter !!")
     if sample is None:
         raise ValueError("Undefined sample Parameter !!")
+    if job is None:
+        raise ValueError("Undefined job Parameter !!")
     if dbs_name is None:
         raise ValueError("Undefined dbs_name Parameter !!")
     if dbs_url is None:
@@ -106,7 +111,7 @@ def submitToGrid(configFile = None, channel = None,
     # if it exists, delete previous version of crab config file
     # for submission of cmsRun job to the Grid
     crabConfigFile_orig = workingDirectory + "crab_template.cfg"
-    crabConfigFile_mod = submissionDirectory + "crab" + "_" + configFile.replace("_cfg.py", "_" + sample + ".cfg")
+    crabConfigFile_mod = submissionDirectory + "crab" + "_" + configFile.replace("_cfg.py", "_" + sample + "_" + job + ".cfg")
 
     # delete previous version of crab config file if it exists
     if os.path.exists(crabConfigFile_mod):
@@ -125,7 +130,7 @@ def submitToGrid(configFile = None, channel = None,
             maxEvents = paramNameValuePair[1]
         if paramNameValuePair[0] == "eventsPerJob":
             eventsPerJob = paramNameValuePair[1]
-    crabDirectory = submissionDirectory + configFile.replace("_cfg.py", "_" + sample + "/")
+    crabDirectory = submissionDirectory + configFile.replace("_cfg.py", "_" + sample + "_" + job + "/")
     crabOutputFiles = ""
     for outputFile in outputFileNames:
         if crabOutputFiles != "":
