854e21baa6ec7bcb6310e6216f044e3fe0154d7b
kvtsang <>
2010-03-15 01:14:27 +0000

    Update to DQM plots.

--- Validation/L1Trigger/src/HcalTPGValidation.cc
+++ Validation/L1Trigger/src/HcalTPGValidation.cc
@@ -5,14 +5,14 @@
 // 
 /**\class HcalTPGValidation HcalTPGValidation.cc Validation/HcalTPGValidation/src/HcalTPGValidation.cc
 
- Description: <one line class summary>
+ Description: [one line class summary]
 
  Implementation:
-     <Notes on implementation>
+     [Notes on implementation]
 */
 //
-// Original Author:  Ka Vang TSANG
-//         Created:  Fri Aug 28 15:27:42 CEST 2009
+// Original Author:  Ka Vang Tsang,8 R-004,+41227676479,
+//         Created:  Fri Mar 12 18:25:36 CET 2010
 // $Id$
 //
 //
@@ -20,40 +20,35 @@
 
 // system include files
 #include <memory>
-#include <map>
-#include <iostream>
-#include <functional>
-#include <cmath>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalTrigTowerDetId.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
 #include "Geometry/HcalTowerAlgo/interface/HcalTrigTowerGeometry.h"
 
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-#include "DataFormats/GeometryVector/interface/Pi.h"
+#include "FWCore/Framework/interface/ESHandle.h"                
+#include "CalibFormats/CaloTPG/interface/HcalTPGCompressor.h"   
+#include "CalibFormats/CaloTPG/interface/CaloTPGRecord.h"       
+#include "CalibFormats/CaloTPG/interface/CaloTPGTranscoder.h"   
 
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
-#include "CalibFormats/CaloTPG/interface/HcalTPGCompressor.h"
-#include "CalibFormats/CaloTPG/interface/CaloTPGRecord.h"
-#include "CalibFormats/CaloTPG/interface/CaloTPGTranscoder.h"
-#include "CondFormats/DataRecord/interface/L1CaloEcalScaleRcd.h"
-#include "CondFormats/L1TObjects/interface/L1CaloEcalScale.h"
-#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
-
-//TFileService
+#include "DQMServices/Core/interface/DQMStore.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
-#include "TProfile.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+//#include "FWCore/ServiceRegistry/interface/Service.h"
+//#include "CommonTools/UtilAlgos/interface/TFileService.h"
+//#include "TH1F.h"
+//#include "TH2F.h"
 
 //
-// class decleration
+// class declaration
 //
 
 class HcalTPGValidation : public edm::EDAnalyzer {
@@ -67,30 +62,24 @@ class HcalTPGValidation : public edm::EDAnalyzer {
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
-      // functinal for mapping eta to ieta
-      struct in_the_range: public std::binary_function< std::pair<double, double>,
-                                                        std::pair<double, double>, bool>{
-         bool operator()(std::pair<double, double> const & lhs, std::pair<double, double> const & rhs){
-            return (lhs.first < rhs.first && lhs.second <= rhs.second);
-         }
-      };
-      typedef std::map< std::pair<double, double>, int, in_the_range> range_map_type;
-
       // ----------member data ---------------------------
-      edm::InputTag hcalTag_;
-      edm::InputTag ecalTag_;
-      edm::InputTag genTag_;
-      double jetPtThreshold_;
-
-      // Map ieta to eta
-      range_map_type ietaFromEtaRange;
-
-      // Map eta to ieta
-      std::map<int, double> realEta;
-      std::map<int, double> realPhi;
-
-      // Histogram for TFileService
-      TProfile *mcEfficiency;
+      std::string dqm_folder_;
+      edm::InputTag simhits_label_;
+      edm::InputTag tp_label_;
+      double factor_HB_;
+      double factor_HE_;
+      double factor_HF1_;
+      double factor_HF2_;
+      double threshold_;
+      HcalTrigTowerGeometry geometry_;
+      std::map<int, double> cosh_ieta_;
+      DQMStore * dbe_;
+      MonitorElement* histo_Et_corr_HB;
+      MonitorElement* histo_Et_corr_HE;
+      MonitorElement* histo_Et_corr_HF;
+      MonitorElement* histo_Et_ratio_HB;
+      MonitorElement* histo_Et_ratio_HE;
+      MonitorElement* histo_Et_ratio_HF;
 };
 
 //
@@ -105,33 +94,29 @@ class HcalTPGValidation : public edm::EDAnalyzer {
 // constructors and destructor
 //
 HcalTPGValidation::HcalTPGValidation(const edm::ParameterSet& iConfig) :
-   hcalTag_(iConfig.getParameter<edm::InputTag>("hcalTag")),
-   ecalTag_(iConfig.getParameter<edm::InputTag>("ecalTag")),
-   genTag_(iConfig.getParameter<edm::InputTag>("genTag")),
-   jetPtThreshold_(iConfig.getUntrackedParameter<double>("jetPtThreshold",0))
+   dqm_folder_(iConfig.getParameter<std::string>("DQMFolder")),
+   simhits_label_(iConfig.getParameter<edm::InputTag>("SimHitsLabel")),
+   tp_label_(iConfig.getParameter<edm::InputTag>("HcalTPLabel")),
+   factor_HB_(iConfig.getParameter<double>("SamplingFactorHB")),
+   factor_HE_(iConfig.getParameter<double>("SamplingFactorHE")),
+   factor_HF1_(iConfig.getParameter<double>("SamplingFactorHF1")),
+   factor_HF2_(iConfig.getParameter<double>("SamplingFactorHF2")),
+   threshold_(iConfig.getParameter<double>("EtThreshold"))
 {
    //now do what ever initialization is needed
-   HcalTrigTowerGeometry theTrigTowerGeometry;
-   for (int ieta=-32; ieta<=32; ++ieta){
-     double eta1 = 0.0;
-     double eta2 = 0.0;
-     theTrigTowerGeometry.towerEtaBounds(ieta,eta1,eta2);
-     realEta[ieta] = (eta1 + eta2) / 2.;
+   dbe_ = edm::Service<DQMStore>().operator->();
+   dbe_->setCurrentFolder(dqm_folder_);
 
-     ietaFromEtaRange[ std::pair<double, double>(eta1, eta2) ] = ieta;
-   }
-   for (int iphi=1; iphi <= 72; ++iphi){
-      double phi = (iphi - 1) / 72. * 2 * M_PI;
-      if (phi > M_PI) phi -= 2 * M_PI;
-      realPhi[iphi] = phi;
-   }
-
-   // TFileService
-   edm::Service<TFileService> fs;
-   mcEfficiency = fs->make<TProfile>("mcEfficiency","MC Efficiency",65,-32.5,32.5,-1,1);
-   mcEfficiency->SetXTitle("ieta");
-   mcEfficiency->SetYTitle("(HCAL TP Et + ECAL TP Et p_{T}^{MC}) / p_{T}^{MC}");
+   for (int ieta = -geometry_.nTowers(); ieta <= geometry_.nTowers(); ++ieta) {
+      if (ieta == 0)
+         continue;
+      double eta1 = 0.0;
+      double eta2 = 0.0;
+      geometry_.towerEtaBounds(ieta, eta1, eta2);
+      cosh_ieta_[ieta] = cosh((eta1 + eta2) / 2.0);
+   }//for ieta
 }
+
 
 HcalTPGValidation::~HcalTPGValidation()
 {
@@ -152,83 +137,92 @@ HcalTPGValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 {
    using namespace edm;
 
-   Handle<reco::GenParticleCollection> genParticles;
-   iEvent.getByLabel(genTag_, genParticles);
+   Handle<PCaloHitContainer> hit_col;
+   iEvent.getByLabel(simhits_label_, hit_col);
 
-   // Setup HCAL
    edm::Handle<HcalTrigPrimDigiCollection> hcalTPCol;
-   iEvent.getByLabel(hcalTag_, hcalTPCol);
+   iEvent.getByLabel(tp_label_, hcalTPCol);
 
-   edm::ESHandle<CaloTPGTranscoder> outTranscoder;
-   iSetup.get<CaloTPGRecord>().get(outTranscoder);
+   edm::ESHandle<CaloTPGTranscoder> outTranscoder; 
+   iSetup.get<CaloTPGRecord>().get(outTranscoder); 
+   outTranscoder->setup(iSetup);                   
 
-   // Setup ECAL
-   edm::Handle<EcalTrigPrimDigiCollection> ecalTPCol;
-   iEvent.getByLabel(ecalTag_, ecalTPCol);
-   ////Setup Calo Scales
-   edm::ESHandle<L1CaloEcalScale> ecalScale;
-   iSetup.get<L1CaloEcalScaleRcd>().get(ecalScale);
-   const L1CaloEcalScale *eScale = ecalScale.product();
+   std::map<HcalTrigTowerDetId, double> energy_from_simhits;
 
-   for (reco::GenParticleCollection::const_iterator p = genParticles->begin(); p != genParticles->end(); ++p){
+   if (hit_col.isValid()) {
+      for (PCaloHitContainer::const_iterator hit = hit_col->begin(); hit != hit_col->end(); ++hit) {
+         double sim_energy = hit->energy();
 
-      //Choose final state quarks with pT > 20 GeV
-      if (p->pdgId()<1 || p->pdgId()>6 || p->status()!=2) continue;
-      if (p->pt() < jetPtThreshold_) continue;
+         if (sim_energy < 0)
+            continue;
 
-      double mc_eta = p->eta();
-      double mc_phi = p->phi();
-      int mc_ieta = ietaFromEtaRange[std::pair<double, double>(mc_eta, mc_eta)];
+         HcalDetId detid(hit->id());
+         // Calibration for simHits
+         switch (detid.subdet()) {
+            case HcalBarrel:
+               sim_energy *= factor_HB_;
+               break;
+            case HcalEndcap:
+               sim_energy *= factor_HE_;
+               break;
+            case HcalForward:
+               sim_energy *= detid.depth() == 1 ? factor_HF1_ : factor_HF2_;
+               break;
+            default:
+               sim_energy *= 0.0;
+         }
 
-      double mc_et = p->et();
-      double hcalet = 0.0;
-      double ecalet = 0.0;
+         std::vector<HcalTrigTowerDetId> trigId = geometry_.towerIds(detid);
+         double tp_energy = sim_energy / trigId.size();
+         for (size_t i=0; i<trigId.size(); ++i) {
+            std::map<HcalTrigTowerDetId, double>::iterator itr = energy_from_simhits.find(trigId[i]);
+            if (itr == energy_from_simhits.end())
+               energy_from_simhits.insert(std::make_pair(trigId[i], tp_energy));
+            else
+               itr->second += tp_energy;
+         }//for trigId
+      }//for hit_col
+   }//hit_col.isValid
 
-      // Match MC to HCAL TP towers
-      for (HcalTrigPrimDigiCollection::const_iterator tp = hcalTPCol->begin(); tp != hcalTPCol->end(); ++tp){
+   if (hcalTPCol.isValid()) {
+      for (HcalTrigPrimDigiCollection::const_iterator tp = hcalTPCol->begin(); tp != hcalTPCol->end(); ++tp) {
          int ieta = tp->id().ieta();
          int iphi = tp->id().iphi();
-         double eta = realEta[ieta];
-         double phi = realPhi[iphi];
 
-         double dphi = phi - mc_phi;
-         if ( dphi > M_PI ) dphi -= 2.0*M_PI;
-         else if ( dphi <= -M_PI) dphi += 2.0*M_PI;
-         double deta = eta - mc_eta;
-         double dR = std::sqrt( deta*deta + dphi*dphi );
+         double tp_et = outTranscoder->hcaletValue(ieta, iphi, tp->SOI_compressedEt());
 
-         double et = outTranscoder->hcaletValue(ieta, iphi, tp->SOI_compressedEt());
+         std::map<HcalTrigTowerDetId, double>::const_iterator simhit = energy_from_simhits.find(tp->id());
+         double simhit_energy = simhit != energy_from_simhits.end() ? simhit->second : 0.0;
+         double simhit_et = simhit_energy / cosh_ieta_[ieta];
 
-         if (dR < 0.5) hcalet += et;
-      }
-
-      // Match MC to ECAL TP towers
-      for (EcalTrigPrimDigiCollection::const_iterator tp = ecalTPCol->begin(); tp != ecalTPCol->end(); ++tp){
-         int ieta =tp->id().ieta();
-         int iphi =tp->id().iphi();
-         double eta = realEta[ieta];
-         double phi = realPhi[iphi];
-
-         double dphi = phi - mc_phi;
-         if ( dphi > M_PI ) dphi -= 2.0*M_PI;
-         else if ( dphi <= -M_PI) dphi += 2.0*M_PI;
-         double deta = eta - mc_eta;
-         double dR = std::sqrt( deta*deta + dphi*dphi );
-
-         double et = eScale->et(tp->compressedEt(), abs(ieta), ieta/abs(ieta));
-
-         if (dR < 0.5) ecalet += et;
-      }
-
-      mcEfficiency->Fill(mc_ieta, (hcalet + ecalet - mc_et)/mc_et);
-   }
+         if (simhit_et > threshold_) {
+            if (abs(ieta) < 16) {
+               histo_Et_corr_HB->Fill(simhit_et, tp_et);
+               histo_Et_ratio_HB->Fill(tp_et / simhit_et);
+            }//HB
+            else if (abs(ieta) < 29) {
+               histo_Et_corr_HE->Fill(simhit_et, tp_et);
+               histo_Et_ratio_HE->Fill(tp_et / simhit_et);
+            }//HE
+            else {
+               histo_Et_corr_HF->Fill(simhit_et, tp_et);
+               histo_Et_ratio_HF->Fill(tp_et / simhit_et);
+            }//HF
+         }//simhit_et > 2
+      }//for hcalTPCol
+   }//hcalTPCol.isValid()
 }
-
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
 HcalTPGValidation::beginJob()
 {
+   histo_Et_corr_HB = dbe_->book2D("Et_corr_HB", "Et_corr_HB", 100, 0, 100, 100, 0, 100);
+   histo_Et_corr_HE = dbe_->book2D("Et_corr_HE", "Et_corr_HE", 100, 0, 100, 100, 0, 100);
+   histo_Et_corr_HF = dbe_->book2D("Et_corr_HF", "Et_corr_HF", 100, 0, 100, 100, 0, 100);
+   histo_Et_ratio_HB = dbe_->book1D("Et_ratio_HB", "Et_ratio_HB", 50, 0.01, 3);
+   histo_Et_ratio_HE = dbe_->book1D("Et_ratio_HE", "Et_ratio_HE", 50, 0.01, 3);
+   histo_Et_ratio_HF = dbe_->book1D("Et_ratio_HF", "Et_ratio_HF", 50, 0.01, 3);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
--- Validation/L1Trigger/test/hcalTPGValidation_cfg.py
+++ Validation/L1Trigger/test/hcalTPGValidation_cfg.py
@@ -9,25 +9,34 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-       '/store/relval/CMSSW_3_2_5/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V5-v2/0011/FA82B15B-D290-DE11-A70B-000423D9853C.root'
+       '/store/relval/CMSSW_3_5_4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V24-v1/0004/D24293FA-2C2C-DF11-988E-00261894380A.root',
+       '/store/relval/CMSSW_3_5_4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V24-v1/0003/EC9BD81D-9D2B-DF11-89FA-003048678FF8.root',
+       '/store/relval/CMSSW_3_5_4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V24-v1/0003/E4DCAD2B-9F2B-DF11-8C1E-003048678FFE.root',
+       '/store/relval/CMSSW_3_5_4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V24-v1/0003/D8618CC1-9D2B-DF11-900D-001731AF6867.root',
+       '/store/relval/CMSSW_3_5_4/RelValQCD_FlatPt_15_3000/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V24-v1/0003/C8BCE998-9B2B-DF11-913C-0018F3D09710.root'
    )
 )
 
+#DQM
+process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+#Save root file to /tmp/user
+import os
+user=os.getenv("USER")
+process.dqmSaver.dirName='/tmp/%s'%user
+process.dqmSaver.workflow = cms.untracked.string('/A/N/C')
+
 process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "MC_31X_V5::All"
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.GlobalTag.globaltag = "MC_3XY_V24::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
-
-process.load('EventFilter.HcalRawToDigi.HcalRawToDigi_cfi')
-
 process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
-process.valHcalTriggerPrimitiveDigis = process.simHcalTriggerPrimitiveDigis.clone()
-process.valHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag('simHcalDigis', 'simHcalDigis')
 
-process.load('Validation/L1Trigger/hcaltpgvalidation_cfi')
-process.TFileService = cms.Service("TFileService",
-      fileName = cms.string('histoHcalTPGValidation.root')
+process.load('Validation.L1Trigger.hcalTPGValidation_cfi')
+
+process.p = cms.Path(
+   process.hcalTPGValidation +
+   process.dqmSaver
 )
-
-process.p = cms.Path(process.hcalTPGValidation)
