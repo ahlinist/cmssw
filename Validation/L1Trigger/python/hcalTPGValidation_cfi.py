9feff2464d1b1678cc1b1a5e6d806f7ea9ba47a6
kvtsang <>
2010-03-15 01:14:27 +0000

    Update to DQM plots.

--- Validation/L1Trigger/python/hcalTPGValidation_cfi.py
+++ Validation/L1Trigger/python/hcalTPGValidation_cfi.py
@@ -1,8 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 hcalTPGValidation = cms.EDAnalyzer('HcalTPGValidation',
-   hcalTag = cms.InputTag("simHcalTriggerPrimitiveDigis"),
-   ecalTag = cms.InputTag("simEcalTriggerPrimitiveDigis"),
-   genTag = cms.InputTag("genParticles"),
-   jetPtThreshold = cms.untracked.double(20.)
+      DQMFolder = cms.string('L1Trigger/HcalTPGValidation'),
+      HcalTPLabel = cms.InputTag('simHcalTriggerPrimitiveDigis'),
+      SimHitsLabel = cms.InputTag('g4SimHits:HcalHits'),
+      SamplingFactorHB = cms.double(110),
+      SamplingFactorHE = cms.double(160),
+      SamplingFactorHF1 = cms.double(1.0 / 0.383),
+      SamplingFactorHF2 = cms.double(1.0 / 0.368),
+      EtThreshold = cms.double(2.0)
 )
