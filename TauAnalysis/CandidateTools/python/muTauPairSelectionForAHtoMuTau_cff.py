998677a665dab10fabea025a268c5914cc6699fd
veelken <>
2010-03-18 14:27:52 +0000

    always apply PzetaDiff cut in AHtoMuTau channel

--- TauAnalysis/CandidateTools/python/muTauPairSelectionForAHtoMuTau_cff.py
+++ TauAnalysis/CandidateTools/python/muTauPairSelectionForAHtoMuTau_cff.py
@@ -32,31 +32,19 @@ selectedMuTauPairsForAHtoMuTauValidCollinearApprox = cms.EDFilter("PATMuTauPairS
     filter = cms.bool(False)
 )
 
-patMuTauPairSelConfiguratorForAHtoMuTau_path01 = objSelConfigurator(
+patMuTauPairSelConfiguratorForAHtoMuTau = objSelConfigurator(
     [ selectedMuTauPairsForAHtoMuTauAntiOverlapVeto,
       selectedMuTauPairsForAHtoMuTauZeroCharge,
       selectedMuTauPairsForAHtoMuTauMt1MET,
-      selectedMuTauPairsForAHtoMuTauPzetaDiff ],
+      selectedMuTauPairsForAHtoMuTauPzetaDiff,
+      selectedMuTauPairsForAHtoMuTauNonBackToBack,
+      selectedMuTauPairsForAHtoMuTauValidCollinearApprox ],
     src = "allMuTauPairs",
     pyModuleName = __name__,
     doSelIndividual = True
 )
 
-selectMuTauPairsForAHtoMuTau_path01 = patMuTauPairSelConfiguratorForAHtoMuTau_path01.configure(pyNameSpace = locals())
-
-patMuTauPairSelConfiguratorForAHtoMuTau_path02 = objSelConfigurator(
-    [ selectedMuTauPairsForAHtoMuTauNonBackToBack,
-      selectedMuTauPairsForAHtoMuTauValidCollinearApprox ],
-    src = "selectedMuTauPairsForAHtoMuTauMt1METcumulative",
-    pyModuleName = __name__,
-    doSelIndividual = True
-)
-
-selectMuTauPairsForAHtoMuTau_path02 = patMuTauPairSelConfiguratorForAHtoMuTau_path02.configure(pyNameSpace = locals())
-
-selectMuTauPairsForAHtoMuTau = cms.Sequence(
-    selectMuTauPairsForAHtoMuTau_path01 * selectMuTauPairsForAHtoMuTau_path02
-)
+selectMuTauPairsForAHtoMuTau = patMuTauPairSelConfiguratorForAHtoMuTau.configure(pyNameSpace = locals())
 
 # define additional collections of muon + tau-jet candidates
 # with loose track and ECAL isolation applied on muon leg
@@ -76,30 +64,17 @@ selectedMuTauPairsForAHtoMuTauNonBackToBackLooseMuonIsolation = copy.deepcopy(se
 
 selectedMuTauPairsForAHtoMuTauValidCollinearApproxLooseMuonIsolation = copy.deepcopy(selectedMuTauPairsForAHtoMuTauValidCollinearApprox)
 
-patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation_path01 = objSelConfigurator(
+patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation = objSelConfigurator(
     [ selectedMuTauPairsForAHtoMuTauAntiOverlapVetoLooseMuonIsolation,
       selectedMuTauPairsForAHtoMuTauZeroChargeLooseMuonIsolation,
       selectedMuTauPairsForAHtoMuTauMt1METlooseMuonIsolation,
-      selectedMuTauPairsForAHtoMuTauPzetaDiffLooseMuonIsolation ],
+      selectedMuTauPairsForAHtoMuTauPzetaDiffLooseMuonIsolation,
+      selectedMuTauPairsForAHtoMuTauNonBackToBackLooseMuonIsolation,
+      selectedMuTauPairsForAHtoMuTauValidCollinearApproxLooseMuonIsolation ],
     src = "allMuTauPairsLooseMuonIsolation",
     pyModuleName = __name__,
     doSelIndividual = True
 )
 
-selectMuTauPairsForAHtoMuTauLooseMuonIsolation_path01 = \
-  patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation_path01.configure(pyNameSpace = locals())
-
-patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation_path02 = objSelConfigurator(
-    [ selectedMuTauPairsForAHtoMuTauNonBackToBackLooseMuonIsolation,
-      selectedMuTauPairsForAHtoMuTauValidCollinearApproxLooseMuonIsolation ],
-    src = "selectedMuTauPairsForAHtoMuTauMt1METlooseMuonIsolationCumulative",
-    pyModuleName = __name__,
-    doSelIndividual = True
-)
-
-selectMuTauPairsForAHtoMuTauLooseMuonIsolation_path02 = \
-  patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation_path02.configure(pyNameSpace = locals())
-
-selectMuTauPairsForAHtoMuTauLooseMuonIsolation = cms.Sequence(
-    selectMuTauPairsForAHtoMuTauLooseMuonIsolation_path01 * selectMuTauPairsForAHtoMuTauLooseMuonIsolation_path02
-)    
+selectMuTauPairsForAHtoMuTauLooseMuonIsolation = \
+  patMuTauPairSelConfiguratorForAHtoMuTauLooseMuonIsolation.configure(pyNameSpace = locals())
