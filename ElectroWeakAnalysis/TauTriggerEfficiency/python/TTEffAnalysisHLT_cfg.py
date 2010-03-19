aafea485ef7657c69dba50447a07b3f8824de7ba
mkortela <>
2010-03-19 13:28:17 +0000

    Commented MessageLogger

--- ElectroWeakAnalysis/TauTriggerEfficiency/python/TTEffAnalysisHLT_cfg.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/python/TTEffAnalysisHLT_cfg.py
@@ -16828,91 +16828,91 @@ HLT_ZeroBias = cms.Path(HLTBeginSequence+hltL1sZeroBias+hltPreZeroBias+HLTEndSeq
 HLTAnalyzerEndpath = cms.EndPath(hltL1GtTrigReport+hltTrigReport)
 
 
-MessageLogger = cms.Service("MessageLogger",
-    suppressInfo = cms.untracked.vstring(),
-    debugs = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    suppressDebug = cms.untracked.vstring(),
-    cout = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    cerr_stats = cms.untracked.PSet(
-        threshold = cms.untracked.string('WARNING'),
-        output = cms.untracked.string('cerr'),
-        optionalPSet = cms.untracked.bool(True)
-    ),
-    warnings = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    default = cms.untracked.PSet(
+# MessageLogger = cms.Service("MessageLogger",
+#     suppressInfo = cms.untracked.vstring(),
+#     debugs = cms.untracked.PSet(
+#         placeholder = cms.untracked.bool(True)
+#     ),
+#     suppressDebug = cms.untracked.vstring(),
+#     cout = cms.untracked.PSet(
+#         placeholder = cms.untracked.bool(True)
+#     ),
+#     cerr_stats = cms.untracked.PSet(
+#         threshold = cms.untracked.string('WARNING'),
+#         output = cms.untracked.string('cerr'),
+#         optionalPSet = cms.untracked.bool(True)
+#     ),
+#     warnings = cms.untracked.PSet(
+#         placeholder = cms.untracked.bool(True)
+#     ),
+#     default = cms.untracked.PSet(
 
-    ),
-    statistics = cms.untracked.vstring('cerr_stats'),
-    cerr = cms.untracked.PSet(
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        noTimeStamps = cms.untracked.bool(False),
-        FwkReport = cms.untracked.PSet(
-            reportEvery = cms.untracked.int32(100),
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(10000000)
-        ),
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(10000000)
-        ),
-        Root_NoDictionary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        threshold = cms.untracked.string('INFO'),
-        FwkJob = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        FwkSummary = cms.untracked.PSet(
-            reportEvery = cms.untracked.int32(1),
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(10000000)
-        ),
-        optionalPSet = cms.untracked.bool(True)
-    ),
-    FrameworkJobReport = cms.untracked.PSet(
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        optionalPSet = cms.untracked.bool(True),
-        FwkJob = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(10000000)
-        )
-    ),
-    suppressWarning = cms.untracked.vstring(),
-    errors = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    destinations = cms.untracked.vstring('warnings', 
-        'errors', 
-        'infos', 
-        'debugs', 
-        'cout', 
-        'cerr'),
-    debugModules = cms.untracked.vstring(),
-    infos = cms.untracked.PSet(
-        optionalPSet = cms.untracked.bool(True),
-        Root_NoDictionary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        placeholder = cms.untracked.bool(True)
-    ),
-    categories = cms.untracked.vstring('FwkJob', 
-        'FwkReport', 
-        'FwkSummary', 
-        'Root_NoDictionary'),
-    fwkJobReports = cms.untracked.vstring('FrameworkJobReport')
-)
+#     ),
+#     statistics = cms.untracked.vstring('cerr_stats'),
+#     cerr = cms.untracked.PSet(
+#         INFO = cms.untracked.PSet(
+#             limit = cms.untracked.int32(0)
+#         ),
+#         noTimeStamps = cms.untracked.bool(False),
+#         FwkReport = cms.untracked.PSet(
+#             reportEvery = cms.untracked.int32(100),
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(10000000)
+#         ),
+#         default = cms.untracked.PSet(
+#             limit = cms.untracked.int32(10000000)
+#         ),
+#         Root_NoDictionary = cms.untracked.PSet(
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(0)
+#         ),
+#         threshold = cms.untracked.string('INFO'),
+#         FwkJob = cms.untracked.PSet(
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(0)
+#         ),
+#         FwkSummary = cms.untracked.PSet(
+#             reportEvery = cms.untracked.int32(1),
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(10000000)
+#         ),
+#         optionalPSet = cms.untracked.bool(True)
+#     ),
+#     FrameworkJobReport = cms.untracked.PSet(
+#         default = cms.untracked.PSet(
+#             limit = cms.untracked.int32(0)
+#         ),
+#         optionalPSet = cms.untracked.bool(True),
+#         FwkJob = cms.untracked.PSet(
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(10000000)
+#         )
+#     ),
+#     suppressWarning = cms.untracked.vstring(),
+#     errors = cms.untracked.PSet(
+#         placeholder = cms.untracked.bool(True)
+#     ),
+#     destinations = cms.untracked.vstring('warnings', 
+#         'errors', 
+#         'infos', 
+#         'debugs', 
+#         'cout', 
+#         'cerr'),
+#     debugModules = cms.untracked.vstring(),
+#     infos = cms.untracked.PSet(
+#         optionalPSet = cms.untracked.bool(True),
+#         Root_NoDictionary = cms.untracked.PSet(
+#             optionalPSet = cms.untracked.bool(True),
+#             limit = cms.untracked.int32(0)
+#         ),
+#         placeholder = cms.untracked.bool(True)
+#     ),
+#     categories = cms.untracked.vstring('FwkJob', 
+#         'FwkReport', 
+#         'FwkSummary', 
+#         'Root_NoDictionary'),
+#     fwkJobReports = cms.untracked.vstring('FrameworkJobReport')
+# )
 
 
 DTDataIntegrityTask = cms.Service("DTDataIntegrityTask",
