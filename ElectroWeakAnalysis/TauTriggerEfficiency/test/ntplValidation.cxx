7def90ed3d6812c6153cd7bf3715181633a4293f
slehti <>
2010-03-19 14:33:14 +0000

    Bugfix

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/ntplValidation.cxx
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/ntplValidation.cxx
@@ -1,6 +1,9 @@
+// macro for checking empty branches in TTEffTree
+// 19.3.2010/S.Lehti
+
 void ntplValidation(){
 
-	TFile* fIN = TFile::Open("tteffAnalysis.root");
+	TFile* fIN = TFile::Open("tteffAnalysis-pftau.root");
 	fIN->ls();
 	cout << " TTEffTree entries " << TTEffTree->GetEntries() << endl;
 
@@ -14,7 +17,7 @@ void ntplValidation(){
 		TH1F* htemp = (TH1F*)gPad->GetPrimitive("htemp");
 
 		string branchName = string(branches->At(i)->GetName());
-                while(branchName.size() < 50) branchName+=" ";
+                while(branchName.size() < 35) branchName+=" ";
                 cout << branchName << " ";
 /*
 		cout << htemp->GetEntries() << " "
@@ -23,25 +26,22 @@ void ntplValidation(){
 */
 
 		if(htemp->GetEntries() == 0) {
-			cout << "NO ENTRIES! " << endl;
+			cout << "Warning, No entries! " << endl;
 			htemp->DrawClone();
 			continue;
 		}
 		if(htemp->GetMaximum() <= htemp->GetMinimum()){
-			cout << "EMPTY, max <= min" << endl;
+			cout << "Warning, max <= min" << endl;
 			htemp->DrawClone();
 			continue;
 		}
 		int nFilledBins  = 0;
 		int iNonEmptyBin = -1;
-		for(int iBin = 1; iBin < htemp->GetNbinsX(); ++iBin){
+		for(int iBin = 1; iBin <= htemp->GetNbinsX(); ++iBin){
 			if(htemp->GetBinContent(iBin) != 0) {
 				nFilledBins++;
 				iNonEmptyBin = iBin;
 			}
-//			cout << htemp->GetBinContent(iBin) << "  ";
-//			cout << htemp->GetBinLowEdge(iBin) << endl;
-//" " << GetBinLowEdge(iBin) << endl;
 		}
 		if(nFilledBins < 2){
 			cout << "WARNING, only one bin filled, value = " << htemp->GetBinLowEdge(iNonEmptyBin) << endl;
