0406deafe90f6c40edf06310c3c3e9c6fda8eefb
weng <>
2010-03-30 09:15:15 +0000

    correct file

--- JetMETAnalysis/PromptAnalysis/test/rootNtupleMacros/plotting/makeWebpage.py
+++ JetMETAnalysis/PromptAnalysis/test/rootNtupleMacros/plotting/makeWebpage.py
@@ -1,54 +1,81 @@
- <html> 
- <!-- DO NOT ERASE 
-UF Analsis Viewer (v1.5) 
-Author : Ronny Remington, University of Florida 
-Inspired by technology developed by M.Schmitt, B. Scurlock, and R. Remington (UF) 
-Date : 02/12/09  -->
-<head> 
- <title> 
- 
- </title> 
- <h3 align="center"> 
- blablabla
-   </h3> 
-  <script type="text/javascript">    
- function ThumbnailPlot(plot) {              
- <!-- clear_l()-->                    
- document.getElementById("thumblink_l").href = plot     
- document.getElementById("thumb_l").src = plot          
- document.getElementById("thumb_l").width = " 500 "   
- document.getElementById("thumb_l").height = " 500 "    
- document.getElementById("thumb_l").border = "1"      
- }    
- function clear_l() {      
- document.getElementById("thumblink_l").href = "#"   
- document.getElementById("thumb_l").src = ""         
- document.getElementById("thumb_l").width = "0"      
- document.getElementById("thumb_l").height = "0"     
- document.getElementById("thumb_l").border = "0"     
- }     
-function ThumbnailNote(flag,id) { 
-if (flag=="1"){ 
-if( document.layers) document.all[''+id+''].visibility = "show" 
-else if ( document.all) document.all[''+id+''].visibility = "visible" 
-else if ( document.getElementById) document.getElementById(''+id+'').style.visibility = "visible" 
-}
-  else 
- if( flag=="0"){ 
- if( document.layers) document.all[''+id+''].visibility = "hide" 
-else if ( document.all) document.all[''+id+''].visibility = "hidden" 
-else if ( document.getElementById) document.getElementById(''+id+'').style.visibility = "hidden" 
-} 
-} 
- function ClearNotes() { 
-  } 
- </script>   
- <style type="text/css">   
- #thumb_gen {position: absolute; position: fixed; left: 720; top:120; text-align: center;}   
- #main_d {position: absolute; position: fixed; left: 10px;}    
-</style> 
+#!/usr/bin/python
 
-</head >  
-  <body bgcolor="#ffffff" > 
- <br>
-<br>
+## This script writes an index.html page
+## displaying all plots contained in the 
+## directory.
+##
+## The file: makeWebpage.html is needed
+## to be kept in the same place as the
+## script and is used as a template.
+## -------------------------------------
+## Usage: ./makeWebpage.py DIR
+## -------------------------------------
+## with DIR = the path to the directoy
+## containig the plots.
+## DIR will also be the title of the
+## webpage.
+##
+## For the moment it works for .gif's.
+## For other formats, change line 42
+## accordingly.
+##
+## After running the script, copy the
+## folder conatining the plots and the
+## index.html to an apropriate place.
+## (This can be autmated too, if needed.)
+
+
+
+
+
+import sys
+import os
+import re
+
+directory=sys.argv[1]
+
+# get the plots in the directory
+list = os.listdir(directory)
+
+# open a tmeplate file and the new .html file
+template=open("makeWebpage.html","r")
+page=open(directory+"/index.html","w")
+
+# write the common part of the index.html
+for line in template:
+    if(re.search(r'blablabla',line)!=None):
+        page.write(sys.argv[1])
+    else:
+        page.write(line)
+
+page.write('<strong>'+sys.argv[1]+' </strong><br>')
+
+# add all the plots in the directory
+for i in range(len(list)):
+    if(re.search(r'gif',list[i])!=None):
+        print list[i]
+        page.write('<a href=" '+list[i])
+        page.write('" onMouseOver="ThumbnailPlot(')
+        page.write("'"+list[i]+"'), ClearNotes(),  ThumbnailNote() ")
+        page.write('"> <img src="'+list[i]+' " style="height:22px; width:25px;"><small>'+list[i]+' </small></a> <br> \n')
+
+# write the last lines
+page.write('<br>\n')
+page.write('<div id="thumb_gen"><a href="#" id="thumblink_l"><img src="" id="thumb_l" width=0 height=0 border=0></a></div> \n')
+page.write('</body> \n')
+page.write(' </html>\n')
+
+
+# now copy everything on the wwweth server...
+#os.system("scp -r "+directory+" wwweth.cern.ch:JetMETPromptAnalysis/")
+
+
+
+
+
+
+
+
+
+
+
