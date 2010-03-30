1c6d963b3a488899ea201bcab2a3830f0ae79729
kkousour <>
2010-03-30 11:30:42 +0000

    Some simplifications

--- QCDAnalysis/HighPtJetAnalysis/test/InclusiveJetTreeProducer_data_cfg.py
+++ QCDAnalysis/HighPtJetAnalysis/test/InclusiveJetTreeProducer_data_cfg.py
@@ -13,7 +13,8 @@ process.maxEvents = cms.untracked.PSet(
 )
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/592/E609699F-2BE2-DE11-A59D-003048D2C108.root')
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_5_5/RelValQCD_FlatPt_15_3000/GEN-SIM-RECO/MC_3XY_V25-v1/0007/E0707AC5-0F38-DF11-B1FD-0026189437E8.root'
+)
 )
 #############   inclusive jet tree producer ##################
 process.TFileService = cms.Service("TFileService",fileName = cms.string('InclusiveJetTree.root'))
@@ -31,23 +32,17 @@ process.ak5calo = cms.EDAnalyzer('InclusiveJetTreeProducer',
     metNoHF                = cms.string('metNoHF'),
     hcalNoiseTag           = cms.InputTag('hcalnoise'),
     minJetPt               = cms.double(3.0), 
-    jetTriggerNames        = cms.vstring(),
-#    A pile of BeamCommissioning09 HLT triggers
-#    jetTriggerNames        = cms.vstring('HLT_MinBiasBSC','HLT_L1Jet6U','HLT_Jet15U','HLT_Jet30U','HLT_Jet50U','HLT_DiJetAve15U_8E29','HLT_DiJetAve30U_8E29','HLT_QuadJet15U','HLT_CSCBeamHalo','HLT_SplashBSC','HLT_ForwardBSC','HLT_BackwardBSC','HLT_HighMultiplicityBSC','HLT_GlobalRunHPDNoise','HLT_PhysicsDeclared'),
+    jetTriggerNames        = cms.vstring('HLT_MinBiasBSC','HLT_Jet15U','HLT_Jet30','HLT_Jet50','HLT_Jet50','HLT_PhysicsDeclared'),
     triggerProcessName     = cms.string('HLT'), 
     triggerResultsTag      = cms.InputTag('TriggerResults','','HLT'),
-    l1TriggerNames         = cms.vstring(),
-#    A pile of BeamCommisioning09 L1 triggers
-#    l1TriggerNames         = cms.vstring('L1Tech_BSC_minBias_threshold1.v0','L1Tech_BSC_minBias_thresholdr2.v0','L1Tech_BSC_splash_beam1.v0','L1Tech_BSC_splash_beam2.v0','L1_SingleJet6','L1_SingleJet10','L1_SingleJet20','L1_SingleJet30','L1_SingleJet40','L1_SingleJet50','L1_SingleJet60'),     
+    l1TriggerNames         = cms.vstring('L1_SingleJet6','L1_SingleJet30','L1_SingleJet40','L1_SingleJet50','L1_SingleJet60'),     
     L1GTReadoutRcdSource   = cms.InputTag('gtDigis'),
     L1GTObjectMapRcdSource = cms.InputTag('hltL1GtObjectMap','','HLT')
 )
 
 process.ak7calo = process.ak5calo.clone(jets = 'ak7CaloJets', jetsID = 'ak7JetID', jetExtender = 'ak7JetExtender')
-#process.iterativeCone = process.ak5calo.clone(jets = 'iterativeCone5CaloJets', jetsID = 'ic5JetID', jetExtender = 'iterativeCone5JetExtender')
 
-#process.p = cms.Path(process.ak5JTA * process.ak5calo * process.ak7JTA * process.ak7calo * process.iterativeCone5JTA * process.iterativeCone)
 process.p = cms.Path(process.ak5JTA * process.ak5calo * process.ak7JTA * process.ak7calo )
 #############   Format MessageLogger #################
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
