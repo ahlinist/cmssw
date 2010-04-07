142ef67a286e036e76d836a52a8d1dc084ec97df
veelken <>
2010-04-07 14:27:43 +0000

    added control plots

--- TauAnalysis/BgEstimationTools/macros/bgEstTemplateShapeBias_ZtoMuTau.C
+++ TauAnalysis/BgEstimationTools/macros/bgEstTemplateShapeBias_ZtoMuTau.C
@@ -1,38 +1,45 @@
 
 //Int_t lineColors[] = { 15, 13, 1 };
-Int_t lineColors[] = { 8, 4, 1 };
-Int_t lineStyles[] = { 4, 2, 1 };
+//Int_t lineColors[] = { 14, 628, 797 };
+//Int_t lineStyles[] = { 4, 7, 1 };
+Int_t lineColors[] = { 12, 797 };
+Int_t lineStyles[] = { 4, 1 };
 
 void bgEstTemplateShapeBias_ZtoMuTau()
 {
   //TString dqmDirectoryName = "DQMData/harvested/WplusJets/zMuTauAnalyzer/";
   //TString dqmDirectoryName = "DQMData/harvested/qcdSum/zMuTauAnalyzer/";
-  TString dqmDirectoryName = "DQMData/harvested/PPmuXptGt20_factorized/zMuTauAnalyzer/";
+  //TString dqmDirectoryName = "DQMData/harvested/PPmuXptGt20_factorized/zMuTauAnalyzer/";
+  TString dqmDirectoryName = "DQMData/harvested/Zmumu/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/";
 
   TObjArray dqmSubDirectoryNames;
   //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauAcoplanarity12_beforeEvtSelDiTauCandidateForMuTauMt1MET/"));
   //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauMt1MET_beforeEvtSelDiTauCandidateForMuTauPzetaDiff/"));
-  //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauPzetaDiff/"));
-  dqmSubDirectoryNames.Add(new TObjString("afterEvtSelTauPt_beforeEvtSelMuonTrkIso/"));
-  dqmSubDirectoryNames.Add(new TObjString("afterEvtSelMuonTrkIso_beforeEvtSelMuonEcalIso/"));
-  dqmSubDirectoryNames.Add(new TObjString("afterEvtSelMuonEcalIso_beforeEvtSelMuonAntiPion/"));
+  //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauPzetaDiff_beforeEvtSelDiMuPairZmumuHypothesisVeto/"));
+  //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelTauPt_beforeEvtSelMuonTrkIso/"));
+  //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelMuonTrkIso_beforeEvtSelMuonEcalIso/"));
+  //dqmSubDirectoryNames.Add(new TObjString("afterEvtSelMuonEcalIso_beforeEvtSelMuonAntiPion/"));
+  dqmSubDirectoryNames.Add(new TObjString("afterDiMuonPairZeroChargeBgEstZmumuJetMisIdEnriched_beforeDiMuonPairInvMassBgEstZmumuJetMisIdEnriched/"));
+  dqmSubDirectoryNames.Add(new TObjString("afterDiMuonPairInvMassBgEstZmumuJetMisIdEnriched/"));
 
   TObjArray legendLabels;
   //legendLabels.Add(new TObjString("before M_{T}^{#mu + MET} Cut"));
-  //legendLabels.Add(new TObjString("after M_{T}^{#mu + MET} Cut, before Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
+  //legendLabels.Add(new TObjString("after M_{T}^{#mu + MET}, before P_{#zeta} - 1.5*P_{#zeta}^{vis} Cut"));
   //legendLabels.Add(new TObjString("after Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
-  legendLabels.Add(new TObjString("before Muon Track iso. Cut"));
-  legendLabels.Add(new TObjString("after Muon Track iso. Cut, before Cut on Muon ECAL iso."));
-  legendLabels.Add(new TObjString("after Cut on Muon ECAL iso."));
+  //legendLabels.Add(new TObjString("before Muon Track iso. Cut"));
+  //legendLabels.Add(new TObjString("after Muon Track iso. Cut, before Cut on Muon ECAL iso."));
+  //legendLabels.Add(new TObjString("after Cut on Muon ECAL iso."));
+  legendLabels.Add(new TObjString("before 40 < M^{#mu^{+}#mu^{-}} < 100 GeV Cut"));
+  legendLabels.Add(new TObjString("after 40 < M^{#mu^{+}#mu^{-}} < 100 GeV Cut"));
 
   TString outputFileName_unnormalized = "bgEstTemplateShapeBias_unnormalized.ps";
   TString outputFileName_normalized = "bgEstTemplateShapeBias_normalized.ps";
 
-  //TString inputFileName = "rfio:/castor/cern.ch/user/v/veelken/bgEstPlots/ZtoMuTau/plotsZtoMuTau_all_shrinkingCone.root";
-  TString inputFileName = "../../Configuration/test/plotsZtoMuTau_all.root";
+  //TString inputFileName = "rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_3_x/plots/ZtoMuTau/7TeV/plotsZtoMuTau_all.root";
+  TString inputFileName = "../test/bgEstTemplatesZtoMuTau.root";
 
   //showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
-  //			    "DiTauCandidateQuantities/DPhi12", "#Delta#phi_{#mu#tau}", false, 
+  //		  	    "DiTauCandidateQuantities/DPhi12", "#Delta#phi_{#mu#tau}", false, 
   //			    "bgEstTemplateShapeBias_ZtoMuTau_WplusJets_DPhi12_unnormalized.eps");
   //showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
   //			    "DiTauCandidateQuantities/DPhi12", "#Delta#phi_{#mu#tau}", true, 
@@ -43,12 +50,18 @@ void bgEstTemplateShapeBias_ZtoMuTau()
   //showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
   //			    "DiTauCandidateQuantities/VisMass", "M_{vis}^{#mu#tau}", true,
   //			    "bgEstTemplateShapeBias_ZtoMuTau_WplusJets_Mvis_normalized.eps");
+  //showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+  //			    "MuonQuantities/MuonEta", "#eta_{#mu}", false,
+  //			    "bgEstTemplateShapeBias_ZtoMuTau_QCD_muonEta_unnormalized.png");
+  //showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+  //			    "MuonQuantities/MuonEta", "#eta_{#mu}", true,
+  //			    "bgEstTemplateShapeBias_ZtoMuTau_QCD_muonEta_normalized.png");
   showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
-			  "MuonQuantities/MuonEta", "#eta_{#mu}", false,
-			  "bgEstTemplateShapeBias_ZtoMuTau_QCD_muonEta_unnormalized.png");
+			  "DiTauCandidateQuantities/VisMass", "M_{vis}^{#mu#tau}", false,
+			  "bgEstTemplateShapeBias_ZtoMuTau_ZmumuJetMisId_Mvis_unnormalized.eps");
   showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
-			  "MuonQuantities/MuonEta", "#eta_{#mu}", true,
-			  "bgEstTemplateShapeBias_ZtoMuTau_QCD_muonEta_normalized.png");
+			  "DiTauCandidateQuantities/VisMass", "M_{vis}^{#mu#tau}", true,
+			  "bgEstTemplateShapeBias_ZtoMuTau_ZmumuJetMisId_Mvis_normalized.eps");
 }
 
 void showTemplateShapeBias_i(const TString& inputFileName, const TString& dqmDirectoryName, 
@@ -66,8 +79,8 @@ void showTemplateShapeBias_i(const TString& inputFileName, const TString& dqmDir
   canvas->SetFillColor(10);
   canvas->SetBorderSize(2);
 
-  //canvas->SetLogy(false);
-  canvas->SetLogy(true);
+  canvas->SetLogy(false);
+  //canvas->SetLogy(true);
 
   TFile* inputFile = TFile::Open(inputFileName);
 
@@ -106,13 +119,15 @@ void showTemplateShapeBias_i(const TString& inputFileName, const TString& dqmDir
   for ( int iDistribution = 0; iDistribution < numDistributions; ++iDistribution ) {
     TH1* dqmMonitorElement = (TH1*)dqmMonitorElements.At(iDistribution);
     
-    //dqmMonitorElement->SetMaximum(1.4*yMax);
-    //dqmMonitorElement->SetMinimum(0.);
-    dqmMonitorElement->SetMaximum(1.e+2*yMax);
+    dqmMonitorElement->SetMaximum(1.4*yMax);
+    dqmMonitorElement->SetMinimum(0.);
+    //dqmMonitorElement->SetMaximum(1.e+2*yMax);
+/*
     if ( normalize )
       dqmMonitorElement->SetMinimum(1.e-3);
     else
       dqmMonitorElement->SetMinimum(1.e+1);
+ */
     
     dqmMonitorElement->SetStats(false);
 
@@ -123,7 +138,7 @@ void showTemplateShapeBias_i(const TString& inputFileName, const TString& dqmDir
     dqmMonitorElement->SetXTitle(xAxisLabel);
     dqmMonitorElement->GetYaxis()->SetTitleOffset(1.2);
 
-    const char* drawOption = ( iDistribution == 0 ) ? "" : "same";
+    const char* drawOption = ( iDistribution == 0 ) ? "hist" : "histsame";
     dqmMonitorElement->Draw(drawOption);
     
     TString legendLabel = ((TObjString*)legendLabels.At(iDistribution))->GetString();
