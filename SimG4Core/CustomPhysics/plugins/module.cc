a894d79058442f326c7839f6bd1df36dfc653734
sunanda <>
2010-04-08 12:44:33 +0000

    Add Fedor as developer

---
+++ SimG4Core/CustomPhysics/plugins/module.cc
@@ -0,0 +1,8 @@
+#include "SimG4Core/Physics/interface/PhysicsListFactory.h"
+#include "SimG4Core/CustomPhysics/interface/CustomPhysics.h"
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+
+DEFINE_PHYSICSLIST(CustomPhysics);
+
