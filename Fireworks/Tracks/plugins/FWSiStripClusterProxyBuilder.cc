3670d6292c7038809829d16990bea31b5470ac53
yana <>
2010-04-08 11:15:47 +0000

    Migrate to a new proxy builder

---
+++ Fireworks/Tracks/plugins/FWSiStripClusterProxyBuilder.cc
@@ -0,0 +1,127 @@
+// -*- C++ -*-
+// $Id: FWSiStripClusterProxyBuilder.cc,v 1.2 2010/03/11 14:21:47 mccauley Exp $
+//
+
+// system include files
+#include "TEveManager.h"
+#include "TEveCompound.h"
+#include "TEveGeoNode.h"
+#include "TEveStraightLineSet.h"
+
+// user include files
+#include "Fireworks/Core/interface/FWProxyBuilderBase.h"
+#include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Core/interface/DetIdToMatrix.h"
+// FIXME: If it's in src, it is private and should not be used...
+#include "Fireworks/Core/src/changeElementAndChildren.h"
+#include "Fireworks/Tracks/interface/TrackUtils.h"
+
+#include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+class FWSiStripClusterProxyBuilder : public FWProxyBuilderBase
+{
+public:
+   FWSiStripClusterProxyBuilder() {
+   }
+   virtual ~FWSiStripClusterProxyBuilder() {
+   }
+
+   REGISTER_PROXYBUILDER_METHODS();
+private:
+   virtual void build(const FWEventItem* iItem, TEveElementList** product);
+   FWSiStripClusterProxyBuilder(const FWSiStripClusterProxyBuilder&);    // stop default
+   const FWSiStripClusterProxyBuilder& operator=(const FWSiStripClusterProxyBuilder&);    // stop default
+   void modelChanges(const FWModelIds& iIds, TEveElement* iElements);
+   void applyChangesToAllModels(TEveElement* iElements);
+};
+
+
+void FWSiStripClusterProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
+{
+   TEveElementList* tList = *product;
+
+   if(0 == tList) {
+      tList =  new TEveElementList(iItem->name().c_str(),"SiStripCluster",true);
+      *product = tList;
+      tList->SetMainColor(iItem->defaultDisplayProperties().color());
+      gEve->AddElement(tList);
+   } else {
+      tList->DestroyElements();
+   }
+
+   const edmNew::DetSetVector<SiStripCluster>* clusters=0;
+   iItem->get(clusters);
+
+   if(0 == clusters ) return;
+   std::set<DetId> modules;
+   int index=0;
+   for(edmNew::DetSetVector<SiStripCluster>::const_iterator set = clusters->begin();
+       set != clusters->end(); ++set,++index) {
+      const unsigned int bufSize = 1024;
+      char title[bufSize];
+      char name[bufSize];
+      unsigned int id = set->detId();
+      snprintf(name,  bufSize,"module%d",index);
+      snprintf(title, bufSize,"Module %d",id);
+      TEveCompound* list = new TEveCompound(name, title);
+      list->OpenCompound();
+      //guarantees that CloseCompound will be called no matter what happens
+      boost::shared_ptr<TEveCompound> sentry(list,boost::mem_fn(&TEveCompound::CloseCompound));
+      list->SetRnrSelf(     iItem->defaultDisplayProperties().isVisible() );
+      list->SetRnrChildren( iItem->defaultDisplayProperties().isVisible() );
+
+      if (iItem->getGeom()) {
+         // const TGeoHMatrix* matrix = iItem->getGeom()->getMatrix( id );
+         TEveGeoShape* shape = iItem->getGeom()->getShape( id );
+         if(0!=shape) {
+            shape->SetMainTransparency(75);
+            shape->SetMainColor( iItem->defaultDisplayProperties().color() );
+            shape->SetPickable(true);
+            list->AddElement(shape);
+         }
+      }
+      gEve->AddElement(list,tList);
+/////////////////////////////////////////////////////	   
+//LatB
+	   static int C2D=1;
+	   static int PRINT=0;
+	   if (C2D) {
+			if (PRINT) std::cout<<"SiStripCluster  "<<index<<", "<<title<<std::endl;
+			TEveStraightLineSet *scposition = new TEveStraightLineSet(title);
+			for(edmNew::DetSet<SiStripCluster>::const_iterator ic = set->begin (); ic != set->end (); ++ic) { 
+				short fs = (*ic).firstStrip ();
+				double bc = (*ic).barycenter();
+				TVector3 point, pointA, pointB; fireworks::localSiStrip(point, pointA, pointB, bc, id, iItem);
+				if (PRINT) std::cout<<"SiStripCluster first strip "<<fs<<", bary center "<<bc<<", phi "<<point.Phi()<<std::endl;
+				scposition->AddLine(pointA.X(), pointA.Y(), pointA.Z(), pointB.X(), pointB.Y(), pointB.Z());
+				scposition->SetLineColor(kRed);
+			}
+			gEve->AddElement(scposition,tList);
+	   }
+/////////////////////////////////////////////////////	   
+	   
+   }
+}
+
+void
+FWSiStripClusterProxyBuilder::modelChanges(const FWModelIds& iIds, TEveElement* iElements)
+{
+   applyChangesToAllModels(iElements);
+}
+
+void
+FWSiStripClusterProxyBuilder::applyChangesToAllModels(TEveElement* iElements)
+{
+   if(0!=iElements && item() && item()->size()) {
+      //make the bad assumption that everything is being changed indentically
+      const FWEventItem::ModelInfo info(item()->defaultDisplayProperties(),false);
+      changeElementAndChildren(iElements, info);
+      iElements->SetRnrSelf(info.displayProperties().isVisible());
+      iElements->SetRnrChildren(info.displayProperties().isVisible());
+      iElements->ElementChanged();
+   }
+}
+
+REGISTER_FWPROXYBUILDER(FWSiStripClusterProxyBuilder,edmNew::DetSetVector<SiStripCluster>,"SiStrip", FWViewType::k3DBit | FWViewType::kRhoPhiBit  | FWViewType::kRhoZBit);
---
+++ Fireworks/Tracks/plugins/FWSiStripDigiProxyBuilder.cc
@@ -0,0 +1,107 @@
+#include "TEveManager.h"
+#include "TEveCompound.h"
+#include "TEveGeoNode.h"
+#include "TEvePointSet.h"
+
+#include "Fireworks/Tracks/interface/TrackUtils.h"
+#include "Fireworks/Core/interface/FWProxyBuilderBase.h"
+#include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Core/interface/DetIdToMatrix.h"
+// FIXME: If it's in src, it is private and should not be used...
+#include "Fireworks/Core/src/changeElementAndChildren.h"
+
+#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+class FWSiStripDigiProxyBuilder : public FWProxyBuilderBase
+{
+public:
+  FWSiStripDigiProxyBuilder() {}
+  virtual ~FWSiStripDigiProxyBuilder() {}
+
+  REGISTER_PROXYBUILDER_METHODS();
+
+private:
+  virtual void build(const FWEventItem* iItem, TEveElementList** product);
+  FWSiStripDigiProxyBuilder(const FWSiStripDigiProxyBuilder&);    
+  const FWSiStripDigiProxyBuilder& operator=(const FWSiStripDigiProxyBuilder&);
+  void modelChanges(const FWModelIds& iIds, TEveElement* iElements);
+  void applyChangesToAllModels(TEveElement* iElements);
+};
+
+void FWSiStripDigiProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
+{
+  TEveElementList* tList = *product;
+
+  if( 0 == tList ) 
+  {
+    tList =  new TEveElementList(iItem->name().c_str(),"SiPixelDigi",true);
+    *product = tList;
+    tList->SetMainColor(iItem->defaultDisplayProperties().color());
+    gEve->AddElement(tList);
+  } 
+  
+  else 
+  {
+    tList->DestroyElements();
+  }
+
+  const edm::DetSetVector<SiStripDigi>* digis = 0;
+  iItem->get(digis);
+
+  if( 0 == digis ) 
+    return;
+
+  for ( edm::DetSetVector<SiStripDigi>::const_iterator it = digis->begin(), end = digis->end();
+        it != end; ++it)     
+  { 
+    TEveCompound* compound = new TEveCompound("si strip digi compound", "siStripDigis");
+    compound->OpenCompound();
+    tList->AddElement(compound);
+    
+    edm::DetSet<SiStripDigi> ds = *it;
+    const uint32_t& detID = ds.id;
+    DetId detid(detID);
+        
+    for ( edm::DetSet<SiStripDigi>::const_iterator idigi = ds.data.begin(), idigiEnd = ds.data.end();
+          idigi != idigiEnd; ++idigi )        
+    {
+      TEvePointSet* pointSet = new TEvePointSet();
+      pointSet->SetMarkerSize(2);
+      pointSet->SetMarkerStyle(2);
+      pointSet->SetMarkerColor(2);
+      tList->AddElement(pointSet);
+
+      // For now, take the center of the strip as the local position 
+      const DetIdToMatrix* detIdToGeo = iItem->getGeom();
+      const TGeoHMatrix* matrix = detIdToGeo->getMatrix(detid);
+      double local[3]  = {0.0, 0.0, 0.0};
+      double global[3] = {0.0, 0.0, 0.0};
+      matrix->LocalToMaster(local, global);
+      pointSet->SetNextPoint(global[0], global[1], global[2]);
+
+    } // end of iteration over digis  
+  } // end of iteratin over the DetSetVector
+}
+
+void
+FWSiStripDigiProxyBuilder::modelChanges(const FWModelIds& iIds, TEveElement* iElements)
+{
+   applyChangesToAllModels(iElements);
+}
+
+void
+FWSiStripDigiProxyBuilder::applyChangesToAllModels(TEveElement* iElements)
+{
+   if( 0 != iElements && item() && item()->size() ) 
+   {
+      const FWEventItem::ModelInfo info(item()->defaultDisplayProperties(),false);
+      changeElementAndChildren(iElements, info);
+      iElements->SetRnrSelf(info.displayProperties().isVisible());
+      iElements->SetRnrChildren(info.displayProperties().isVisible());
+      iElements->ElementChanged();
+   }
+}
+
+REGISTER_FWPROXYBUILDER(FWSiStripDigiProxyBuilder,edm::DetSetVector<SiStripDigi>,"SiStripDigi", FWViewType::k3DBit | FWViewType::kRhoPhiBit  | FWViewType::kRhoZBit);
