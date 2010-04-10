b95135691cb74261296453c09b91f64bd0b86ac4
veelken <>
2010-04-10 13:51:37 +0000

    corrected pfMEt x, y resolutions

--- TauAnalysis/CandidateTools/python/resolutions_cfi.py
+++ TauAnalysis/CandidateTools/python/resolutions_cfi.py
@@ -4,6 +4,6 @@ import FWCore.ParameterSet.Config as cms
 # define resolutions shared by different analysis channels
 #--------------------------------------------------------------------------------
 
-pfMEtResolutionPx = cms.string("0.70*sqrt(x)")
+pfMEtResolutionPx = cms.string("0.50*sqrt(x)")
 pfMEtResolutionPy = pfMEtResolutionPx
 
