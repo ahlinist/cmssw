842692be63d669cf27867f13219ea02129f6d2e0
jkolb <>
2010-04-12 09:51:21 +0000

    bug fix: muonSource -> electronSource

--- TauAnalysis/Core/python/patElectronDump_cfi.py
+++ TauAnalysis/Core/python/patElectronDump_cfi.py
@@ -4,7 +4,7 @@ patElectronDump = cms.PSet(
     pluginName = cms.string('patElectronDump'),
     pluginType = cms.string('PATElectronDump'),
 
-    muonSource = cms.InputTag('cleanLayer1Electrons'),
+    electronSource = cms.InputTag('cleanLayer1Electrons'),
     genParticleSource = cms.InputTag('genParticles'),
 
     skipPdgIdsGenParticleMatch = cms.vint32(12, 14, 16)
