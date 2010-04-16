4f6a3dbaf6c0d1d8fb33428999f4e66a08df35c5
lgray <>
2010-04-16 11:44:29 +0000

    Factored out ZMuMu and ZEE into their own configs

---
+++ ElectroWeakAnalysis/MultiBosons/python/ZEEGamma_cff.py
@@ -0,0 +1,27 @@
+#Electron Selection
+from ElectroWeakAnalysis.MultiBosons.ElectronSelection_cfi import *
+
+goodElectrons = electronSelection.clone()
+goodElectrons.cut = cms.string('et > 0')
+
+#define and configure Z selection
+from ElectroWeakAnalysis.MultiBosons.ZSelectionChargedLeptons_cfi import *
+
+#Z -> E E
+ZEECandidates = ZSelection.clone()
+ZEECandidates.decay = cms.string('goodElectrons@+ goodElectrons@-')
+ZEECandidates.cut = cms.string('mass > 0')
+
+ZEECandidatesSeq = cms.Sequence(ZEECandidates)
+
+#define and configure Zgamma Selection
+from ElectroWeakAnalysis.MultiBosons.ZGammaSelectionChargedLeptons_cfi import *
+
+ZEEGammaCandidates = ZGammaSelectionChargedLeptons.clone()
+ZEEGammaCandidates.decay = cms.string('ZEECandidates photons')
+ZEEGammaCandidates.cut = cms.string('mass > 0')
+
+ZEEGammaCandidatesSeq = cms.Sequence(ZEEGammaCandidates)
+
+#setup sequences
+ZEEGammaWorkflow = cms.Sequence(ZEECandidatesSeq*ZEEGammaCandidatesSeq)
