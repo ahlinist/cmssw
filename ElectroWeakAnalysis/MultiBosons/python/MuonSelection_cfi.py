55d7f7a325557ad907319847bb67cbba9437eb46
lgray <>
2010-04-16 18:19:18 +0000

    More selectors and producers.. Trying to stick to using edm::Ptr where possible.

--- ElectroWeakAnalysis/MultiBosons/python/MuonSelection_cfi.py
+++ ElectroWeakAnalysis/MultiBosons/python/MuonSelection_cfi.py
@@ -1,5 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-muonSelection = cms.EDProducer('MuonViewRefSelector',
-                               cut = cms.string('pt > 0'),
+muonPtrSelection = cms.EDProducer('MuonViewPtrSelector',
+                               src = cms.InputTag('muons'),
+                               cut = cms.string('pt > 0')
                                )
+
+muonSelection = cms.EDProducer('MuonViewSelector',
+                               src = cms.InputTag('muons'),
+                               cut = cms.string('pt > 0')
+                               )
+
