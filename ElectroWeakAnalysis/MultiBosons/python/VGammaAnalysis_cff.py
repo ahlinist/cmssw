ae3f5f35335eb549a1a7f84086844eca53b6fcbc
lgray <>
2010-04-16 11:44:29 +0000

    Factored out ZMuMu and ZEE into their own configs

--- ElectroWeakAnalysis/MultiBosons/python/VGammaAnalysis_cff.py
+++ ElectroWeakAnalysis/MultiBosons/python/VGammaAnalysis_cff.py
@@ -1,48 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-#Muon Selection
-from ElectroWeakAnalysis.MultiBosons.MuonSelection_cfi import *
+import ElectroWeakAnalysis.MultiBosons.ZMuMuGamma_cff as ZMuMuGamma
+import ElectroWeakAnalysis.MultiBosons.ZEEGamma_cff as ZEEGamma
 
-goodGlobalMuons = muonSelection.clone()
-goodGlobalMuons.cut = cms.string('isGlobalMuon & pt > 0 & innerTrack().found() >= 15')
-
-goodGlobalAndTrackerMuons = muonSelection.clone()
-goodGlobalAndTrackerMuons.cut = cms.string('isGlobalMuon | isTrackerMuon & pt > 0 & innerTrack().found() >= 15')
-
-#Electron Selection
-from ElectroWeakAnalysis.MultiBosons.ElectronSelection_cfi import *
-
-goodElectrons = electronSelection.clone()
-goodElectrons.cut = cms.string('et > 0')
-
-#define and configure Z selection
-from ElectroWeakAnalysis.MultiBosons.ZSelectionChargedLeptons_cfi import *
-
-#Z -> Mu MU
-ZMuMuCandidates = ZSelection.clone()
-ZMuMuCandidates.decay = cms.string('goodGlobalMuons@+ goodGlobalMuons@-')
-ZMuMuCandidates.cut = cms.string('mass > 0')
-
-#Z -> E E
-ZEECandidates = ZSelection.clone()
-ZEECandidates.decay = cms.string('goodElectrons@+ goodElectrons@-')
-ZEECandidates.cut = cms.string('mass > 0')
-
-#define and configure Zgamma Selection
-from ElectroWeakAnalysis.MultiBosons.ZGammaSelectionChargedLeptons_cfi import *
-
-ZMuMuGammaCandidates = ZGammaSelectionChargedLeptons.clone()
-ZMuMuGammaCandidates.decay = cms.string('ZMuMuCandidates photons')
-ZMuMuGammaCandidates.cut = cms.string('mass > 0')
-
-ZEEGammaCandidates = ZGammaSelectionChargedLeptons.clone()
-ZEEGammaCandidates.decay = cms.string('ZEECandidates photons')
-ZEEGammaCandidates.cut = cms.string('mass > 0')
-
-#setup sequences
-ZGammaWorkflowChargedLeptons = cms.Sequence(ZMuMuCandidates*ZMuMuGammaCandidates+
-                                            ZEECandidates*ZEEGammaCandidates)
-
-VGammaAnalysis = cms.Sequence(ZGammaWorkflowChargedLeptons)
+VGammaAnalysis = cms.Sequence(ZMuMuGamma.ZMuMuGammaWorkflow+
+                              ZEEGamma.ZEEGammaWorkflow)
                              
 
---
+++ ElectroWeakAnalysis/MultiBosons/python/ZMuMuGamma_cff.py
@@ -0,0 +1,47 @@
+#Muon Selection
+from ElectroWeakAnalysis.MultiBosons.MuonSelection_cfi import *
+
+goodGlobalMuons = muonSelection.clone()
+goodGlobalMuons.cut = cms.string('isGlobalMuon & pt > 0')
+
+goodGlobalMuonsTkTrackRequirement = muonSelection.clone()
+goodGlobalMuonsTkTrackRequirement.cut = cms.string('innerTrack().found() >= 15')
+
+goodGlobalAndTrackerMuons = muonSelection.clone()
+goodGlobalAndTrackerMuons.cut = cms.string('isGlobalMuon | isTrackerMuon & pt > 0 & innerTrack().found() >= 15')
+
+goodGlobalAndTrackerMuonsTkTrackRequirement = muonSelection.clone()
+goodGlobalAndTrackerMuonsTkTrackRequirement.cut = cms.string('innerTrack().found() >= 15')
+
+muonSelectionSeq = cms.Sequence(goodGlobalMuons*goodGlobalMuonsTkTrackRequirement+
+                                goodGlobalAndTrackerMuons*goodGlobalAndTrackerMuonsTkTrackRequirement)
+
+#define and configure Z selection
+from ElectroWeakAnalysis.MultiBosons.ZSelectionChargedLeptons_cfi import *
+
+#Z -> Mu MU
+ZMuMuCandidates_gg = ZSelection.clone()
+ZMuMuCandidates_gg.decay = cms.string('goodGlobalMuons@+ goodGlobalMuons@-')
+ZMuMuCandidates_gg.cut = cms.string('mass > 0')
+
+ZMuMuCandidates_gt = ZSelection.clone()
+ZMuMuCandidates_gt.decay = cms.string('goodGlobalAndTrackerMuons@+ goodGlobalandTrackerMuons@-')
+ZMuMuCandidates_gt.cut = cms.string('mass > 0 & (muDaughter1().isGlobalMuon | muDaughter2().isGlobalMuon)')
+
+ZMuMuCandidatesSeq = cms.Sequence(ZMuMuCandidates_gg+ZMuMuCandidates_gt)
+
+#define and configure Zgamma Selection
+from ElectroWeakAnalysis.MultiBosons.ZGammaSelectionChargedLeptons_cfi import *
+
+ZMuMuGammaCandidates_gg = ZGammaSelectionChargedLeptons.clone()
+ZMuMuGammaCandidates_gg.decay = cms.string('ZMuMuCandidates_gg photons')
+ZMuMuGammaCandidates_gg.cut = cms.string('mass > 0')
+
+ZMuMuGammaCandidates_gt = ZGammaSelectionChargedLeptons.clone()
+ZMuMuGammaCandidates_gt.decay = cms.string('ZMuMuCandidates_gt photons')
+ZMuMuGammaCandidates_gt.cut = cms.string('mass > 0')
+
+ZMuMuGammaCandidatesSeq = cms.Sequence(ZMuMuGammaCandidates_gg+ZMuMuGammaCandidates_gt)
+
+#define workflow
+ZMuMuGammaWorkflow = cms.Sequence(muonSelectionSeq*ZMuMuCandidatesSeq*ZMuMuGammaCandidatesSeq)
