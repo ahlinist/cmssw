4d7bc25c25493b8aabc82c22233708abd8bdbf5b
ferencek <>
2010-04-21 17:05:02 +0000

    cleaned up config files

--- JetMETAnalysis/HcalReflagging/python/HFrechitreflaggerJETMET_cff.py
+++ JetMETAnalysis/HcalReflagging/python/HFrechitreflaggerJETMET_cff.py
@@ -7,7 +7,6 @@ from math import log
 # v1 = "Loose conditions" -- same as default
 HFrechitreflaggerJETMET_v1 = deepcopy(hcalrechitReflaggerJETMET)
 
-
 # v2 = "Medium conditions"  -- short R cut set to 0.8, rather than 0.98
 HFrechitreflaggerJETMET_v2 = deepcopy(hcalrechitReflaggerJETMET)
 HFrechitreflaggerJETMET_v2.hf_Short_Rcut=cms.PSet(
@@ -25,7 +24,7 @@ HFrechitreflaggerJETMET_v2.hf_Short_Rcut=cms.PSet(
     hf_Short_Rcut_values39=cms.vdouble([0.8]),
     hf_Short_Rcut_values40=cms.vdouble([0.8]),
     hf_Short_Rcut_values41=cms.vdouble([0.8]),
-    )
+)
 
 # v3 = "Tight conditions"  -- short R cut set to 0.8
 # long S9S1 cut set to:  slope = 0.9, int = -0.09*log(55)
@@ -45,20 +44,21 @@ HFrechitreflaggerJETMET_v3.hf_Short_Rcut=cms.PSet(
     hf_Short_Rcut_values39=cms.vdouble([0.8]),
     hf_Short_Rcut_values40=cms.vdouble([0.8]),
     hf_Short_Rcut_values41=cms.vdouble([0.8]),
-    )
+)
 HFrechitreflaggerJETMET_v3.hf_Long_Rcut.hf_Long_Rcut_values29=cms.vdouble([0.8])
 # Slope is 0.09, intercept = -0.09*log(55) for all ieta
-HFrechitreflaggerJETMET_v3.hf_Long_S9S1_Slope_Intercept = cms.PSet(hf_Long_S9S1_SlopeIntercept29=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept30=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept31=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept32=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept33=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept34=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept35=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept36=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept37=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept38=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept39=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept40=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   hf_Long_S9S1_SlopeIntercept41=cms.vdouble([0.09,eval('-0.09*log(55)')]),
-                                                                   )
+HFrechitreflaggerJETMET_v3.hf_Long_S9S1_Slope_Intercept = cms.PSet(
+    hf_Long_S9S1_SlopeIntercept29=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept30=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept31=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept32=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept33=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept34=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept35=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept36=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept37=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept38=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept39=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept40=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+    hf_Long_S9S1_SlopeIntercept41=cms.vdouble([0.09,eval('-0.09*log(55)')]),
+)
---
+++ JetMETAnalysis/HcalReflagging/test/hf_reflagging_example_cfg.py
@@ -0,0 +1,82 @@
+import FWCore.ParameterSet.Config as cms
+
+maxevents = 100
+isMC = False
+version = 2   # version 1 = default (loose), version 2 = (medium), version 3 = (tight)
+
+process = cms.Process('USER')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/GeometryExtended_cff')
+process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+# GLOBAL TAGS: REPLACE WITH WHATEVER IS APPROPRIATE FOR YOUR WORK!
+
+# MC
+if (isMC):
+    process.GlobalTag.globaltag ='START3X_V26A::All'
+
+# DATA
+else:
+    process.GlobalTag.globaltag ='GR10_P_V4::All'
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(maxevents)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v8/000/132/601/F65A94F7-4141-DF11-9F4E-003048D47A80.root'
+    )
+)
+
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.RECOEventContent.outputCommands,
+    fileName = cms.untracked.string('output_file.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    )
+)
+
+# Other statements
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+# New SeverityLevelComputer that forces RecHits with UserDefinedBit0 set to be excluded from new rechit collection
+import JetMETAnalysis.HcalReflagging.RemoveAddSevLevel as RemoveAddSevLevel
+process.hcalRecAlgos=RemoveAddSevLevel.RemoveFlag(process.hcalRecAlgos,"HFLongShort")
+process.hcalRecAlgos=RemoveAddSevLevel.AddFlag(process.hcalRecAlgos,"UserDefinedBit0")
+
+# HF RecHit reflagger
+process.load("JetMETAnalysis/HcalReflagging/HFrechitreflaggerJETMET_cff")
+if version==1:
+    process.hfrecoReflagged = process.HFrechitreflaggerJETMET_v1.clone()
+elif version==2:
+    process.hfrecoReflagged = process.HFrechitreflaggerJETMET_v2.clone()
+elif version==3:
+    process.hfrecoReflagged = process.HFrechitreflaggerJETMET_v3.clone()
+
+# Use the reflagged HF RecHits to make the CaloTowers
+process.towerMaker.hfInput = cms.InputTag("hfrecoReflagged")
+process.towerMakerWithHO.hfInput = cms.InputTag("hfrecoReflagged")
+
+# Path and EndPath definitions
+process.reflagging_step = cms.Path(process.hfrecoReflagged)
+process.rereco_step = cms.Path(process.caloTowersRec*(process.recoJets*process.recoJetIds+process.recoTrackJets)*process.recoJetAssociations*process.metreco) # re-reco jets and met
+#process.rereco_step = cms.Path(process.towerMaker*process.ak5CaloJets*process.met) # a simpler use case
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.reflagging_step,process.rereco_step,process.out_step)
