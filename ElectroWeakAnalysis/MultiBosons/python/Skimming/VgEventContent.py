cded7ea0f4a79d91807b06e28faa9d20dd4dbcd8
veverka <>
2010-04-27 23:01:27 +0000

    Added more VgKitAnalyzer event content

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/VgEventContent.py
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/VgEventContent.py
@@ -11,6 +11,21 @@ vgExtraPhotonEventContent = [
   'keep *_correctedHybridSuperClusters_*_*',
 ]
 
+vgExtraAnalyzerKitEventContent = [
+  'keep *_allTrackCandidates_*_*',
+  'keep *_genParticles_*_*',
+  'keep *_hltGtDigis_*_*',
+  'keep *_TriggerResults_*_*',
+  'keep *_gtDigis_*_*',
+  'keep *_offlinePrimaryVertices_*_*',
+  'keep *_towerMaker_*_*',
+  'keep *_generalTracks_*_*',
+  'keep *_tcMet_*_*',
+  'keep *_pfMet_*_*',
+  'keep *_reducedEcalRecHitsEB_*_*',
+  'keep *_reducedEcalRecHitsEE_*_*',
+]
+
 vgMuMuGammaEventContent = [
   'keep *_MuMuGammas_*_*',
   'keep *_MuMuGammasOneTrack_*_*',
