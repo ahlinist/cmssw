35871640bb5de77cf59947b4f93098ca0476d2d9
veverka <>
2010-04-27 22:57:03 +0000

    Initial commit.

---
+++ ElectroWeakAnalysis/MultiBosons/python/ZMuMuAdapter_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+vgMuons = cms.EDProducer("ZMuMuAdapter",
+  src = cms.InputTag("dimuons")
+)
+
+vgMuonsGlobal = cms.EDProducer("ZMuMuAdapter",
+  src = cms.InputTag("dimuonsGlobal")
+)
