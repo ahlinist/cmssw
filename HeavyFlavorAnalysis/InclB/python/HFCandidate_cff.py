b7642b148ee2f3653e346c5e4995bab11a56d4c3
leac <>
2010-04-28 12:16:35 +0000

    CMSSW_3_5_6

---
+++ HeavyFlavorAnalysis/InclB/python/HFCandidate_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+
+candidateDump = cms.EDFilter("HFDumpCandidate",
+    muonLabel = cms.untracked.string('muons'),
+    trackLabel = cms.untracked.string('generalTracks'),
+    trackptmin = cms.untracked.double(1.0),
+    deltaR  = cms.untracked.double(1.5),
+    verbose = cms.untracked.int32(0)
+)
+
+CandidateDump = cms.Sequence(candidateDump)
+
+ 
