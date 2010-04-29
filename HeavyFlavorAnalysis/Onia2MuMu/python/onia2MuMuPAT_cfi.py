6f1fd42993aa6fb28c70f722b8d986ca1da70500
zgecse <>
2010-04-29 06:16:35 +0000

    cut on the mass and deltaZ of the dimuon to avoid vertexing near parallel and far appart muons

--- HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cfi.py
+++ HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cfi.py
@@ -6,6 +6,7 @@ onia2MuMuPAT = cms.EDProducer('Onia2MuMuPAT',
   primaryVertexTag = cms.InputTag("offlinePrimaryVertices"),
   higherPuritySelection = cms.string("isGlobalMuon"), ## At least one muon must pass this selection
   lowerPuritySelection  = cms.string("isGlobalMuon"), ## BOTH muons must pass this selection
+  dimuonSelection  = cms.string("2 < mass && abs(daughter('muon1').innerTrack.dz - daughter('muon2').innerTrack.dz) < 25"), ## The dimuon must pass this selection before vertexing
   addCommonVertex = cms.bool(True), ## Embed the full reco::Vertex out of the common vertex fit
   addMuonlessPrimaryVertex = cms.bool(True), ## Embed the primary vertex re-made from all the tracks except the two muons
   addMCTruth = cms.bool(True),      ## Add the common MC mother of the two muons, if any
