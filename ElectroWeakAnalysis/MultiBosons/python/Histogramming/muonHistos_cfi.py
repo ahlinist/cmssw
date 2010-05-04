0fd23dd5ff6f8275fb0f68728f09d80460f394ba
veverka <>
2010-05-04 21:16:23 +0000

    Fixing typo Photon -> Muon

--- ElectroWeakAnalysis/MultiBosons/python/Histogramming/muonHistos_cfi.py
+++ ElectroWeakAnalysis/MultiBosons/python/Histogramming/muonHistos_cfi.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from ElectroWeakAnalysis.MultiBosons.Histogramming.leafKineHistos_cff import *
 from ElectroWeakAnalysis.MultiBosons.Histogramming.isoHistos_cff import *
-muonHistos = cms.EDAnalyzer("PatPhotonHistoAnalyzer",
+muonHistos = cms.EDAnalyzer("PatMuonHistoAnalyzer",
 #photonHistos = cms.EDAnalyzer("LazyCandViewHistoAnalyzer",
   cms.PSet(histograms = (leafKineHistos + isoHistos).copy() ),
-  src = cms.InputTag("cleanPatPhotons")
+  src = cms.InputTag("cleanPatMuons")
 )
 
 from ElectroWeakAnalysis.MultiBosons.Histogramming.histoTools_cff import *
