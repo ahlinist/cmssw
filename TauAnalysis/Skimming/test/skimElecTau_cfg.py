d09d8de3fdbba6d4fd0bdfa101d7027871349d88
jkolb <>
2010-05-04 14:41:02 +0000

    update for data

--- TauAnalysis/Skimming/test/skimElecTau_cfg.py
+++ TauAnalysis/Skimming/test/skimElecTau_cfg.py
@@ -8,9 +8,9 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = cms.string('MC_31X_V2::All')
+#process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+#process.GlobalTag.globaltag = cms.string('MC_31X_V2::All')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -18,8 +18,7 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_1_2/RelValZTT/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/A4DD1FAE-B178-DE11-B608-001D09F24EAC.root',
-        '/store/relval/CMSSW_3_1_2/RelValZTT/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/9408B54D-CB78-DE11-9AEB-001D09F2503C.root'
+		'rfio:///castor/cern.ch/cms/store/caf/user/meridian/EGMPromptFilter_Beam10/133483/EGMPromptFilter_Electrons_133483_19627412_1_1.root'
     )
 )
 
