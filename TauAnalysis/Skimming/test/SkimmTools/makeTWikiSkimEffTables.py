56cf098d711e235add713556066f94340d5d1aeb
akalinow <>
2010-05-07 10:21:47 +0000

    *update script for the new event counting in the TriggerReport

--- TauAnalysis/Skimming/test/SkimmTools/makeTWikiSkimEffTables.py
+++ TauAnalysis/Skimming/test/SkimmTools/makeTWikiSkimEffTables.py
@@ -3,15 +3,19 @@
 import sys, re, os
     
 ##############
-def getEffFromTrigReport(selectionPath,fileList,opt):
+def getEffFromTrigReport(selectionPath,skimType,fileList,opt):
     NEvents = 0
     NPassedAll = 0
     ### Init counters ###
     NPassedPath = [0] * 10
     filters = []
     #########################
-    fullEntryPattern = re.compile('TrigReport ---------- Event  Summary ------------[\t\n\r\f\v\W\w]+TrigReport ---------- Path   Summary')   
-    ppat = 'TrigReport Events total[\W\w]+'
+    counterPrefix = skimType
+    counterPathName = counterPrefix+"Counter"
+    counterName = counterPrefix+"Filter"
+    #########################
+    fullEntryPattern = re.compile('------ Modules in End-Path: '+counterPathName+' ------------[\t\n\r\f\v\W\w]+TrigReport ---------- Module')   
+    ppat = 'TrigReport [\W\w]+'
     p = []
     p.append(re.compile(ppat))
     ########################
@@ -20,19 +24,26 @@ def getEffFromTrigReport(selectionPath,fileList,opt):
     pPath = []
     ########################
     # Loop over all files
+    if(len(fileList)<1):
+        print "No crab output files present!"
+        return
     for filen in fileList:
         input = file(filen)
         fullTxt = input.read() # read full txt
-        ######Total efficiency
-        m0 = fullEntryPattern.search(fullTxt) # extract region        
+        ######Total efficiency        
+        m0 = fullEntryPattern.search(fullTxt) # extract region
         if (m0):
             lines = re.split('\n',m0.group())
             for line in lines:
-                m = p[0].search(line)
-                if (m):
-                    tabs = re.split('[\s]+',m.group())
-                    NEvents = NEvents + int(tabs[4])
-                    NPassedAll = NPassedAll + int(tabs[7])
+                if len(line.split())==8 and line.split()[1]=='0':
+                    expr = re.compile(ppat+line.split()[7])
+                    for i in range(0,len(p)):
+                        m = p[i].search(line)
+                        if (m):
+                            tabs = re.split('[\s]+',m.group())
+                            if tabs[7]==counterName:
+                                NEvents = NEvents + int(tabs[3])
+                                NPassedAll = NPassedAll + int(tabs[4])        
         #############################################
         ####Efficiency for each filter       
         m1 = fullEntryPatternPath.search(fullTxt) # extract region
@@ -50,7 +61,7 @@ def getEffFromTrigReport(selectionPath,fileList,opt):
                         tabs = re.split('[\s]+',m.group())
                         NPassedPath[i] = NPassedPath[i] + int(tabs[4])
         #############################################                        
-    input.close()
+        input.close()
 
 
 #####
@@ -68,7 +79,7 @@ def getEffFromTrigReport(selectionPath,fileList,opt):
     # Treat first entry
     if (float(NEvents!=0)):
         eff = float(NPassedPath[0])/float(NEvents)
-    print '%24s: %8d     %5.3f       %5.3f'%(filters[0],NPassedPath[i],eff,float(NPassedPath[0])/float(NEvents))
+    print '%24s: %8d     %5.3f       %5.3f'%(filters[0],NPassedPath[0],eff,float(NPassedPath[0])/float(NEvents))
     outputString += "<tr> "
     outputString+="<td>"+filters[0]+"</td> <td>"+str(NPassedPath[0])+"</td> <td>"+'%5.3f'%(eff)+"</td> <td>"+'%5.3f'%(float(NPassedPath[0])/float(NEvents))+"</td> "
     outputString += "</tr> "    
@@ -76,7 +87,7 @@ def getEffFromTrigReport(selectionPath,fileList,opt):
         if (float(NPassedPath[i-1]==0)):
             eff = -1.
         else:
-            eff = float(NPassedPath[i])/float(NPassedPath[i-1])                
+            eff = float(NPassedPath[i])/float(NPassedPath[i-1])
         print '%24s: %8d     %5.3f       %5.3f'%(filters[i],NPassedPath[i],eff,float(NPassedPath[i])/float(NEvents))
         outputString += "<tr> "
         outputString+="<td>"+filters[i]+"</td> <td>"+str(NPassedPath[i])+"</td> <td>"+'%5.3f'%(eff)+"</td> <td>"+'%5.3f'%(float(NPassedPath[i])/float(NEvents))+"</td> "
@@ -92,7 +103,7 @@ def getEffFromTrigReport(selectionPath,fileList,opt):
     exit
 ##################
 ##################
-def getReportForSample(path):
+def getReportForSample(path,skimType):
     for crabDir in os.listdir(path):
         if re.search("crab_0_",crabDir)!=None:
             print "----------------------------------------------"
@@ -126,29 +137,27 @@ def getReportForSample(path):
                     tmp = line.split("=")[1].rstrip("\n")
                     dbsInst = tmp.split("/")[3]
                     tmp1 = dbsInst[:dbsInst.rfind("_writer")]
-                    print "tmp1: ",tmp1
                     dbsInst = tmp1
             print "datasetpath: ",datasetpath," pset: ",pset," SE: ",storage_element
             print "publish_data_name: ",publish_data_name
             dbsDatasetName="/"+datasetpath.split("/")[1]+"/akalinow-"+publish_data_name
             print "DBS Instance: ",dbsInst
-            print "DBS Link: ","https://cmsweb.cern.ch/dbs_discovery/aSearch?caseSensitive=on&userMode=user&sortOrder=desc&sortName=&grid=1&method=dbsapi&dbsInst="+dbsInst+"&userInput=find+dataset+where+dataset+like+*"+dbsDatasetName+"*+and+dataset.status+like+VALID*"
+            print "DBS Link: ","https://cmsweb.cern.ch/dbs_discovery/aSearch?caseSensitive=on&userMode=user&sortOrder=desc&sortName=&grid=1&method=dbsapi&dbsInst="+dbsInst+"&userInput=find+dataset+where+dataset+like+*"+dbsDatasetName+"*"+skimType+"*+and+dataset.status+like+VALID*"
         ##############
             selectionPaths = ['']
-            if  re.search("skimMuTau_cfg.py",pset)!=None:
+            if  re.search("muTau",skimType)!=None:
                 selectionPaths = ['muPFTauSkimPath','muCaloTauSkimPath']    
-            if  re.search("skimDiTau",pset)!=None:
+            if  re.search("diTau",skimType)!=None:
                 selectionPaths = ['caloTauSkimPath','pfTauSkimPath','mixedTauSkimPathI','mixedTauSkimPathII']
-                #selectionPaths = ['caloTauSkimPath']                    
-            if  re.search("skimElecTau_cfg.py",pset)!=None:
+            if  re.search("elecTau",skimType)!=None:
                 selectionPaths = ['elecPFTauSkimPath','elecCaloTauSkimPath']
-            if  re.search("skimElecMu_cfg.py",pset)!=None:
+            if  re.search("elecMu",skimType)!=None:
                 selectionPaths = ['elecMuSkimPath']
-                            
+                                            
             fileListTmp = os.listdir(path+"/"+crabDir+"/res/")
             fileList = []
             for tmp in fileListTmp:
-                if re.search("stderr",tmp)!=None:
+                if re.search("stdout",tmp)!=None:
                     jobNumber = tmp.split("_")[1].split(".")[0]
                     stdoutFileName = "CMSSW_"+str(jobNumber)+".stdout"
                     stdoutFile = open(path+"/"+crabDir+"/res/"+stdoutFileName,"r")
@@ -161,27 +170,14 @@ def getReportForSample(path):
                     if int(exitCode)!=0:
                         print "Will skip job number:",jobNumber," with exitCode= ",exitCode
                     pass
-            '''
-            #Resubmited outputs are moved into /res dir, previous submits are
-            #moved into Submission_X
-            for tmp in fileListTmp:
-                if re.search("Submission_",tmp)!=None:
-                    fileListTmpResub = os.listdir(path+"/"+crabDir+"/res/"+tmp)
-                    print "Resubmits: ",tmp
-                    for tmp1 in fileListTmpResub:
-                        if re.search("stderr",tmp1)!=None:
-                            if fileList.count(path+"/"+crabDir+"/res/"+tmp1):
-                                fileList.remove(path+"/"+crabDir+"/res/"+tmp1)
-                                #fileList.append(path+"/"+crabDir+"/res/"+tmp+"/"+tmp1)
-                                pass     
-            '''                    
+                              
             outputString=""
-            outputString += getEffFromTrigReport(selectionPaths[0],fileList,0)
+            outputString += getEffFromTrigReport(selectionPaths[0],skimType,fileList,0)
             outputString += "<td> \n %TREEBROWSER{\"thread\" noroot=\"on\" noindent=\"on\" autotoggle=\"on\" style=\"dmenu\"}%\n"
             outputString += "   * Click for details \n"
 
             for selectionPath in selectionPaths:
-                outputString += getEffFromTrigReport(selectionPath,fileList,1)
+                outputString += getEffFromTrigReport(selectionPath,skimType,fileList,1)
 
 
             print "datasetpath: ",datasetpath," pset: ",pset," SE: ",storage_element
@@ -191,10 +187,7 @@ def getReportForSample(path):
             print "dbsDatasetName: ",dbsDatasetName
             print "publishName: ",publishName
             print "dashboardLink: ",dashboardLink
-            fileName = "TWiki_"+pset.split("/Skimms/")[1].split(".")[0].split("_")[0]+".txt"
-            #fileName = "TWiki_"+pset.split("/")[1].split(".")[0].split("_")[0]+".txt"
-            print "Split: ",pset.split("Skimms")
-            
+            fileName = "TWiki_"+skimType+".txt"
             f = open(fileName,"a")
             
             f.write("<tr> <td>[[https://cmsweb.cern.ch/dbs_discovery/aSearch?caseSensitive=on&userMode=user&sortOrder=desc&sortName=&grid=1&method=dbsapi&dbsInst="+dbsInst+"&userInput=find+dataset+where+dataset+like+*"+dbsDatasetName+"*+and+dataset.status+like+VALID*]["+publishName+"]]"
@@ -207,9 +200,14 @@ def getReportForSample(path):
 ##################
 
 
-rootPath = "/afs/cern.ch/user/a/akalinow/scratch0/CMS/ZtauTauSkimms/Crab/"
+rootPath = "/afs/cern.ch/user/a/akalinow/scratch0/CMS/ZtauTauSkimms/Spring10/Crab/"
 #rootPath = "/home/akalinow/scratch/CMS/ZtauTauSkimms/Crab/"
 ###
-path = rootPath+"Zee/Summer09-MC_31X_V3_7TeV_TrackingParticles-v1/GEN-SIM-RECO/"
-getReportForSample(path)
-###
+path = rootPath+"Ztautau/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO/"
+getReportForSample(path,"muTau")
+getReportForSample(path,"diTau")
+getReportForSample(path,"elecTau")
+getReportForSample(path,"elecMu")
+
+
+
