5d24fdc0601a7a4de0cf07910c585b0498bae9c7
kkousour <>
2010-05-17 07:14:19 +0000

    Code adjustments

--- JetMETCorrections/DijetBalance/bin/ReadDijetTree.cc
+++ JetMETCorrections/DijetBalance/bin/ReadDijetTree.cc
@@ -21,7 +21,7 @@ int main(int argc, char**argv)
   //----------------------------------------------------------------------------
   string         TreeFilename   = c1.getValue<string> ("TreeFilename");
   string         HistoFilename  = c1.getValue<string> ("HistoFilename");
-  string         JetType        = c1.getValue<string> ("JetType");
+  string         JetAlgo        = c1.getValue<string> ("JetAlgo");
   bool           IsMCarlo       = c1.getValue<bool>   ("IsMCarlo",false);
   bool           ApplyWeights   = c1.getValue<bool>   ("ApplyWeights",false);  
   bool           ETA_SYMMETRY   = c1.getValue<bool>   ("ETA_SYMMETRY",false);
@@ -43,18 +43,6 @@ int main(int argc, char**argv)
   const int MAX_NPT  = 21;
   int NETA = EtaBnd.size()-1;
   int NPT  = DijetPtBnd.size()-1;
-  int jet;  
-  if      (((int)JetType.find("Calo")>-1) || ((int)JetType.find("CALO")>-1) || ((int)JetType.find("calo")>-1))
-    jet = 0;
-  else if (((int)JetType.find("Pf")>-1)   || ((int)JetType.find("PF")>-1)   || ((int)JetType.find("pf")>-1))
-    jet = 1;
-  else if (((int)JetType.find("Gen")>-1)  || ((int)JetType.find("GEN")>-1)  || ((int)JetType.find("gen")>-1))
-    jet = 2;
-  else
-    {
-      cout<<"Unknown jet type...."<<endl;
-      jet = -1;
-    } 
   if (NETA>MAX_NETA)
     {
       cout<<"WARNING: too many eta bins!!!! Setting default value 82"<<endl;
@@ -74,9 +62,13 @@ int main(int argc, char**argv)
   TFile *inf;      
   inf = TFile::Open(TreeFilename.c_str(),"R");
   TFile *outf = new TFile(HistoFilename.c_str(),"RECREATE");   
-  double NEVENTS = ((TH1F*)inf->Get("ptHat"))->GetEntries();
-  cout<<"Number of events processed: "<<NEVENTS<<endl;
-     
+  double NEVENTS(1.0);
+  TString alg(JetAlgo);
+  if (IsMCarlo)
+    {
+      NEVENTS = ((TH1F*)inf->Get(alg+"/ptHat"))->GetEntries();
+      cout<<"Number of events processed: "<<NEVENTS<<endl;
+    } 
   TH1F *hPtProbe[MAX_NPT][MAX_NETA][MAX_NHLT];
   TH1F *hBalance[MAX_NPT][MAX_NETA][MAX_NHLT];          
   TH1F *hDPhi[MAX_NPT][MAX_NHLT];
@@ -93,25 +85,18 @@ int main(int argc, char**argv)
   TH2F *h2DPtCuts[MAX_NHLT];
   TH2F *h2DDPhiRatio[MAX_NHLT];
   TH2F *h2DDPhiRatioCuts[MAX_NHLT];
-  TH2F *h2DIdJet3Pt[MAX_NHLT];
-  TH2F *h2DIdJet3Ratio[MAX_NHLT];
-  TH1F *hIdJet3[MAX_NPT][3][MAX_NHLT];
-  TH1F *hIdJet3Cuts[MAX_NPT][3][MAX_NHLT];
-  TH1F *hIdProbe[MAX_NPT][3][MAX_NHLT];
-  TH1F *hIdProbeCuts[MAX_NPT][3][MAX_NHLT];
-  TH1F *hIdBarrel[MAX_NPT][MAX_NHLT];
-  TH1F *hIdBarrelCuts[MAX_NPT][MAX_NHLT];
   char name[1024];
   int i,j,k,ipt,ieta;
-  bool cut_dphi,cut_ratio,cut_jet3;
-  float dphi,ratio,ptBarrel,ptProbe,ptJet3,etaProbe,eta,etaJet3,dijetPt,balance,pthat,wt,idProbe,idBarrel,idJet3;
+  int passLooseIdBarrel,passLooseIdProbe,passLooseIdJet3;
+  bool cut_dphi,cut_ratio,cut_jet3,cut_id;
+  float dphi,ratio,ptBarrel,ptProbe,ptJet3,etaProbe,eta,etaJet3,dijetPt,balance,pthat,wt;
   int HLTBits[6];
   double veta[MAX_NETA]; 
   for(i=0;i<=NETA;i++)
     veta[i] = EtaBnd[i];
         
-  /////////////////////////////////////////////////////////////// 
-  TTree *tr = (TTree*)inf->Get("DijetTree");
+  ///////////////////////////////////////////////////////////////  
+  TTree *tr = (TTree*)inf->Get(alg+"/DiJetTree");
   TBranch *brEtaProbe = (TBranch*)tr->GetBranch("etaProbe");
   brEtaProbe->SetAddress(&etaProbe);
   TBranch *brEtaJet3 = (TBranch*)tr->GetBranch("etaJet3");
@@ -126,24 +111,12 @@ int main(int argc, char**argv)
   brPtJet3->SetAddress(&ptJet3);
   TBranch *brHLTBits = (TBranch*)tr->GetBranch("HLTBits");
   brHLTBits->SetAddress(&HLTBits);
-  if (jet==0)
-    {
-      TBranch *brIdBarrel = (TBranch*)tr->GetBranch("emfBarrel");
-      TBranch *brIdProbe = (TBranch*)tr->GetBranch("emfProbe");
-      TBranch *brIdJet3 = (TBranch*)tr->GetBranch("emfJet3");
-      brIdBarrel->SetAddress(&idBarrel);
-      brIdProbe->SetAddress(&idProbe);
-      brIdJet3->SetAddress(&idJet3); 
-    }
-  if (jet==1)
-    {
-      TBranch *brIdBarrel = (TBranch*)tr->GetBranch("nhfBarrel");
-      TBranch *brIdProbe = (TBranch*)tr->GetBranch("nhfProbe");
-      TBranch *brIdJet3 = (TBranch*)tr->GetBranch("nhfJet3");
-      brIdBarrel->SetAddress(&idBarrel);
-      brIdProbe->SetAddress(&idProbe);
-      brIdJet3->SetAddress(&idJet3); 
-    }
+  TBranch *brIdBarrel = (TBranch*)tr->GetBranch("passLooseIdBarrel");
+  TBranch *brIdProbe = (TBranch*)tr->GetBranch("passLooseIdProbe");
+  TBranch *brIdJet3 = (TBranch*)tr->GetBranch("passLooseIdJet3");
+  brIdBarrel->SetAddress(&passLooseIdBarrel);
+  brIdProbe->SetAddress(&passLooseIdProbe);
+  brIdJet3->SetAddress(&passLooseIdJet3); 
   if (IsMCarlo)
     {
       TBranch *brPtHat = (TBranch*)tr->GetBranch("pthat");
@@ -174,13 +147,6 @@ int main(int argc, char**argv)
       sprintf(name,"DPhi_vs_Ratio_Cuts_HLT%d",k);
       h2DDPhiRatioCuts[k] = new TH2F(name,name,100,0,1,630,0,6.3);
 
-      if (jet==0 || jet==1)
-        {
-          sprintf(name,"IdJet3_vs_Pt_HLT%d",k);
-          h2DIdJet3Pt[k] = new TH2F(name,name,nbins,0,max,210,-1,1.1);
-          sprintf(name,"IdJet3_vs_Ratio_HLT%d",k);
-          h2DIdJet3Ratio[k] = new TH2F(name,name,100,0,1,210,-1,1.1);
-        }
       for(i=0;i<NPT;i++)
         {
           sprintf(name,"DPhi_DijetPt%d_HLT%d",i,k);
@@ -207,57 +173,13 @@ int main(int argc, char**argv)
           sprintf(name,"EventsCuts_DijetPt%d_HLT%d",i,k);
           hEventsCuts[i][k] = new TH1F(name,name,NETA,veta);
 
-          if (jet==0 || jet==1)
-            {
-              sprintf(name,"IdJet3_DijetPt%d_HLT%d_Barrel",i,k);
-              hIdJet3[i][0][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdJet3Cuts_DijetPt%d_HLT%d_Barrel",i,k);
-              hIdJet3Cuts[i][0][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdJet3_DijetPt%d_HLT%d_Endcap",i,k);
-              hIdJet3[i][1][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdJet3Cuts_DijetPt%d_HLT%d_Endcap",i,k);
-              hIdJet3Cuts[i][1][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdJet3_DijetPt%d_HLT%d_Fwd",i,k);
-              hIdJet3[i][2][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdJet3Cuts_DijetPt%d_HLT%d_Fwd",i,k);
-              hIdJet3Cuts[i][2][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdBarrel_DijetPt%d_HLT%d",i,k);
-              hIdBarrel[i][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdBarrelCuts_DijetPt%d_HLT%d",i,k);
-              hIdBarrelCuts[i][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdProbe_DijetPt%d_HLT%d_Barrel",i,k);
-              hIdProbe[i][0][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdProbeCuts_DijetPt%d_HLT%d_Barrel",i,k);
-              hIdProbeCuts[i][0][k] = new TH1F(name,name,210,-1,1.1); 
-
-              sprintf(name,"IdProbe_DijetPt%d_HLT%d_Endcap",i,k);
-              hIdProbe[i][1][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdProbeCuts_DijetPt%d_HLT%d_Endcap",i,k);
-              hIdProbeCuts[i][1][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdProbe_DijetPt%d_HLT%d_Fwd",i,k);
-              hIdProbe[i][2][k] = new TH1F(name,name,210,-1,1.1);
-
-              sprintf(name,"IdProbeCuts_DijetPt%d_HLT%d_Fwd",i,k);
-              hIdProbeCuts[i][2][k] = new TH1F(name,name,210,-1,1.1);
-            } 
           for(j=0;j<NETA;j++)
             {  
               sprintf(name,"PtProbe_DijetPt%d_Eta%d_HLT%d",i,j,k);
               hPtProbe[i][j][k] = new TH1F(name,name,nbins,0,max);
 
               sprintf(name,"B_DijetPt%d_Eta%d_HLT%d",i,j,k);
-              hBalance[i][j][k] = new TH1F(name,name,100,-2,2);
+              hBalance[i][j][k] = new TH1F(name,name,200,-4,4);
             } 
         }
     } 
@@ -285,26 +207,14 @@ int main(int argc, char**argv)
       ratio     = ptJet3/dijetPt;
       cut_dphi  = (fabs(dphi)>DPHI_CUT);
       cut_ratio = (ratio<RATIO_CUT);
-      if (jet==0)
-        {
-          if (fabs(idJet3)<0.001)//jet3 coming probabaly from noise. Accept the event.
-            cut_jet3 = true;
-          else 
-            cut_jet3 = (cut_ratio || (ptJet3<PTJET3_CUT));
-        }
-      else if (jet==1)
-        {
-          if (idJet3>0.99)//jet3 coming probabaly from noise. Accept the event.
-            cut_jet3 = true;
-          else 
-            cut_jet3 = (cut_ratio || (ptJet3<PTJET3_CUT));
-        }
-      else
+      cut_id    = (passLooseIdBarrel == 1 && passLooseIdProbe == 1);
+      if (passLooseIdJet3 == 0)//jet3 coming probabaly from noise. Accept the event.
+        cut_jet3 = true;
+      else 
         cut_jet3 = (cut_ratio || (ptJet3<PTJET3_CUT));
-
       ipt       = getBin(dijetPt,DijetPtBnd);
       ieta      = getBin(eta,EtaBnd); 
-      if (ipt<0 || ieta<0) continue;
+      if (ipt<0 || ieta<0 || !cut_id) continue;
       for(k=0;k<NHLT;k++)
         {
           if (HLTBits[k]==0) continue;  
@@ -315,25 +225,6 @@ int main(int argc, char**argv)
           hDijetPt[k]        ->Fill(dijetPt,wt);
           h2DPt[k]           ->Fill(ptBarrel,ptProbe,wt);  
           h2DDPhiRatio[k]    ->Fill(ratio,Phi_0_2pi(dphi),wt); 
-          if (jet==0 || jet==1)
-            {
-              hIdBarrel[ipt][k]     ->Fill(idBarrel,wt);
-              if (fabs(etaProbe)<1.3) 
-                hIdProbe[ipt][0][k]      ->Fill(idProbe,wt);
-              if ((fabs(etaProbe)<3) && (fabs(etaProbe)>=1.3))
-                hIdProbe[ipt][1][k]      ->Fill(idProbe,wt);
-              if ((fabs(etaProbe)<5) && (fabs(etaProbe)>=3.0))
-                hIdProbe[ipt][2][k]      ->Fill(idProbe,wt);
-              if (fabs(etaJet3)<1.3) 
-                hIdJet3[ipt][0][k]      ->Fill(idJet3,wt);
-              if ((fabs(etaJet3)<3) && (fabs(etaJet3)>=1.3))
-                hIdJet3[ipt][1][k]      ->Fill(idJet3,wt);
-              if ((fabs(etaJet3)<5) && (fabs(etaJet3)>=3.0))
-                hIdJet3[ipt][2][k]      ->Fill(idJet3,wt); 
-              
-              h2DIdJet3Pt[k]        ->Fill(dijetPt,idJet3,wt);
-              h2DIdJet3Ratio[k]     ->Fill(ratio,idJet3,wt); 
-            }
           if (cut_dphi && cut_jet3)
             {
               hPtProbe[ipt][ieta][k]->Fill(ptProbe,wt);
@@ -345,22 +236,6 @@ int main(int argc, char**argv)
               hDijetPtCuts[k]       ->Fill(dijetPt,wt);
               h2DPtCuts[k]          ->Fill(ptBarrel,ptProbe,wt);
               h2DDPhiRatioCuts[k]   ->Fill(ratio,Phi_0_2pi(dphi),wt);
-              if (jet==0 || jet==1)
-                {
-                  hIdBarrelCuts[ipt][k]     ->Fill(idBarrel,wt);
-                  if (fabs(etaProbe)<1.3) 
-                    hIdProbeCuts[ipt][0][k]      ->Fill(idProbe,wt);
-                  if ((fabs(etaProbe)<3) && (fabs(etaProbe)>=1.3))
-                    hIdProbeCuts[ipt][1][k]      ->Fill(idProbe,wt);
-                  if ((fabs(etaProbe)<5) && (fabs(etaProbe)>=3.0))
-                    hIdProbeCuts[ipt][2][k]      ->Fill(idProbe,wt);
-                  if (fabs(etaJet3)<1.3) 
-                    hIdJet3Cuts[ipt][0][k]      ->Fill(idJet3,wt);
-                  if ((fabs(etaJet3)<3) && (fabs(etaJet3)>=1.3))
-                    hIdJet3Cuts[ipt][1][k]      ->Fill(idJet3,wt);
-                  if ((fabs(etaJet3)<5) && (fabs(etaJet3)>=3.0))
-                    hIdJet3Cuts[ipt][2][k]      ->Fill(idJet3,wt); 
-                }
             }
         }
     }
