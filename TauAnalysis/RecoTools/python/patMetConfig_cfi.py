d4bc88a344d8e27877fc5e23e83c15ab797a6308
veelken <>
2010-05-26 07:42:37 +0000

    added CaloMET type-II corrections for "unclustered" energy

--- TauAnalysis/RecoTools/python/patMetConfig_cfi.py
+++ TauAnalysis/RecoTools/python/patMetConfig_cfi.py
@@ -10,14 +10,19 @@ from JetMETCorrections.Type1MET.TauMetCorrections_cff import *
 # PAT layer 0 MET configuration parameters
 #--------------------------------------------------------------------------------
 
+# enable CaloMET type-II corrections for "unclustered" energy
+# ( see https://twiki.cern.ch/twiki/bin/view/Main/TypeIMETWithCollisionData2010#Extra_tag_for_Type_II_correc_AN1 ;
+#       http://indico.cern.ch/getFile.py/access?contribId=6&sessionId=1&resId=0&materialId=slides&confId=93979 )
+metJESCorAK5CaloJetMuons.useTypeII = cms.bool(True)
+metJESCorAK5CaloJetMuons.hasMuonsCorr = cms.bool(True)
+
 # apply tau-jet specific corrections to reco::MET per default
 # (comment-out the next line in case you prefer to **not** apply the tau-jet specific MET corrections)
-
 tauMetCorr.InputCaloJetsLabel = metJESCorAK5CaloJet.inputUncorJetsLabel
 tauMetCorr.jetPTthreshold = metJESCorAK5CaloJet.jetPTthreshold
 tauMetCorr.jetEMfracLimit = metJESCorAK5CaloJet.jetEMfracLimit
 tauMetCorr.correctorLabel = metJESCorAK5CaloJet.corrector
-tauMetCorr.InputMETLabel = cms.string('metJESCorAK5CaloJet')
+tauMetCorr.InputMETLabel = cms.string('metJESCorAK5CaloJetMuons')
 
 patMETCorrections._seq = patMETCorrections._seq * MetTauCorrections
 
