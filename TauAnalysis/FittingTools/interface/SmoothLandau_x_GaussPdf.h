cacad23d12756079044558733ee9edf34946060f
veelken <>
2010-05-26 15:00:27 +0000

    first checkin of auxiliary "SmoothPdfWrapper" classes

---
+++ TauAnalysis/FittingTools/interface/SmoothLandau_x_GaussPdf.h
@@ -0,0 +1,47 @@
+#ifndef TauAnalysis_FittingTools_SmoothLandau_x_GaussPdf_h
+#define TauAnalysis_FittingTools_SmoothLandau_x_GaussPdf_h
+
+/** \class SmoothLandau_x_GaussPdf
+ *
+ * Numerical implementation of Landau density convoluted with Gaussian
+ * 
+ * \author Christian Veelken, UC Davis
+ *
+ * \version $Revision: 1.1 $
+ *
+ * $Id: SmoothLandau_x_GaussPdf.h,v 1.1 2009/11/27 15:46:33 veelken Exp $
+ *
+ */
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RooAbsPdf.h"
+#include "RooRealProxy.h"
+
+class SmoothLandau_x_GaussPdf : public RooAbsPdf 
+{
+ public:
+  // constructor(s)
+  SmoothLandau_x_GaussPdf();   
+  SmoothLandau_x_GaussPdf(const char*, const char*, RooAbsReal&, RooAbsReal&, RooAbsReal&, RooAbsReal&, RooAbsReal&);
+  SmoothLandau_x_GaussPdf(const SmoothLandau_x_GaussPdf&, const char* = "");
+  
+  // destructor
+  virtual ~SmoothLandau_x_GaussPdf();
+
+  virtual TObject* clone(const char* newName) const { return new SmoothLandau_x_GaussPdf(*this, newName); }
+
+ protected:
+  Double_t evaluate() const;
+
+  RooRealProxy x_;
+
+  RooRealProxy width_;
+  RooRealProxy mp_;
+  RooRealProxy area_;
+  RooRealProxy gsigma_;
+};
+
+
+#endif  
+
---
+++ TauAnalysis/FittingTools/src/SmoothPdfWrapperBase.cc
@@ -0,0 +1,89 @@
+#include "TauAnalysis/FittingTools/interface/SmoothPdfWrapperBase.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <TPRegexp.h>
+#include <TString.h>
+#include <TObjArray.h>
+#include <TObjString.h>
+
+#include <vector>
+#include <string>
+
+SmoothPdfWrapperBase::SmoothPdfWrapperBase(const edm::ParameterSet& cfg)
+  : name_(""),
+    title_(""),
+    x_(0),
+    pdf_(0),
+    templateHist_(0)
+{
+  fitOptions.Add(new RooCmdArg(RooFit::PrintLevel(-1)));
+  fitOptions.Add(new RooCmdArg(RooFit::PrintEvalErrors(false)));
+  fitOptions.Add(new RooCmdArg(RooFit::Warnings(false)));
+}
+
+SmoothPdfWrapperBase::~SmoothPdfWrapperBase()
+{
+ 
+
+  delete pdf_;
+}
+
+void RooRealVar* SmoothPdfWrapperBase::makeRooRealVar(const char* name, const char* title, const edm::ParameterSet& cfg)
+{
+//--- check that parameter does not already exist
+  if ( parameters_.find("name") != parameters_.end() ) {
+    edm::LogError ("makeRooRealVar") << " Parameter with name = " << name << " already exists !!";
+    return parameters_[name];
+  }
+
+  double initial = cfg.getParameter<double>("initial");
+  double min = cfg.getParameter<double>("min");
+  double max = cfg.getParameter<double>("max");
+  double uncertainty = ( cfg.exists("uncertainty") ) ? cfg.getParameter<double>("uncertainty") : -1.;
+
+//--- create new parameter  
+  parameterType* parameter = new parameterType(name, title, initial, min, max, uncertainty);
+
+//--- add parameter to list of existing parameters
+  parameters_[name] = parameter;
+
+//--- return pointer to PDF coefficient
+  return parameter->pdfCoeff_;
+}
+
+void SmoothPdfWrapperBase::fit()
+{
+  if ( !templateHist_ ) {
+    edm::LogError ("fit") << " Uninitialized template Histogram --> skipping !!";
+    return;
+  }
+
+  if ( !pdf_ ) {
+    edm::LogError ("fit") << " Uninitialized PDF --> skipping !!";
+    return;
+  }
+
+  pdf_->fitTo(*templateHist_, fitOptions);
+
+  if ( !fitSimultaneously_ ) {
+    for ( std::map<std::string, parameterType*>::iterator parameter = parameters_.begin();
+	  parameter != parameters_.end(); ++paremeter ) {
+      if ( parameter->second->uncertainty_ <= 0. ) parameter->second->pdfCoeff_->setConstant();
+    }
+  }
+}
+
+void SmoothPdfWrapperBase::reinitializeParameter()
+{
+  for ( std::map<std::string, parameterType*>::iterator parameter = parameters_.begin();
+	parameter != parameters_.end(); ++paremeter ) {
+    parameter->second->pdfCoeff_->setVal(parameter->second->initial_);
+  }
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+EDM_REGISTER_PLUGINFACTORY(SmoothPdfWrapperPluginFactory, "SmoothPdfWrapperPluginFactory");
+
+
