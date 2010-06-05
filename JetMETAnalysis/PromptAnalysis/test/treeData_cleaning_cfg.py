7cd4173dc64ece68a521dda1d34316aef095c22f
apresyan <>
2010-06-05 10:42:24 +0000

    add instuctions on how to get GT

--- JetMETAnalysis/PromptAnalysis/test/treeData_cleaning_cfg.py
+++ JetMETAnalysis/PromptAnalysis/test/treeData_cleaning_cfg.py
@@ -1,27 +1,30 @@
 import FWCore.ParameterSet.Config as cms
+import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
 
 process = cms.Process('USER')
 
 process.load('JetMETAnalysis.PromptAnalysis.ntuple_cff')
 
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration/StandardSequences/GeometryExtended_cff')
-process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
+process.load("Configuration/StandardSequences/Geometry_cff")
+process.load("Configuration/StandardSequences/MagneticField_cff")
 process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load("Configuration/StandardSequences/FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag ='GR_R_35X_V7A::All'
+process.GlobalTag.globaltag ='GR_R_35X_V8B::All'
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.add_( cms.Service( "TFileService",
-fileName = cms.string("MinimumBias__Commissioning10-GOODCOLL-v9__RAW-RECO.root"),
+fileName = cms.string("MinimumBias__Commissioning10-SD_JetMETTau-v9.root"),
                            closeFileFast = cms.untracked.bool(True)  ) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source (
     "PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v8/000/132/601/F65A94F7-4141-DF11-9F4E-003048D47A80.root'
-        #'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/BSCNOBEAMHALO-Dec14thSkim_v1/0102/BABAF8C3-71EA-DE11-9D8C-0024E8768446.root'
+#        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v8/000/132/601/F65A94F7-4141-DF11-9F4E-003048D47A80.root' #this run works
+#        '/store/data/Commissioning10/MinimumBias/RAW-RECO/May6thPDSkim_GOODCOLL-v1/0003/26E87BA3-D05C-DF11-8FE0-001BFCDBD100.root'
+        '/store/data/Commissioning10/MinimumBias/RAW-RECO/May6thPDSkim_GOODCOLL-v1/0128/BC0068A1-375D-DF11-B249-001A92810AF4.root'
+#'file:/tmp/apresyan/picked_events_CaloMET_and_tcMET_gt_45GeV_Artur.root'
         ),
     
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
@@ -35,6 +38,10 @@ process.MessageLogger.cerr.default.limit = 100
 # summary
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
+# jet corrections
+#process.load("JetMETCorrections.Configuration.L2L3Corrections_Summer09_7TeV_ReReco332_cff")
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+
 ##################################################
 ####### OFFICIAL RECOMMENDATION STARTS HERE#######
 ##################################################
@@ -47,16 +54,16 @@ process.hltPhysicsDeclared.L1GtReadoutRecordTag = 'gtDigis'
 process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
 process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
 process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
-process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))')
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
 
 # this is for filtering on HLT path
 process.hltHighLevel = cms.EDFilter("HLTHighLevel",
-                                    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-                                    HLTPaths = cms.vstring('HLT_MinBiasBSC'),        # provide list of HLT paths (or patterns) you want
-                                    eventSetupPathsKey = cms.string(''),             # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
-                                    andOr = cms.bool(True),                          # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
-                                    throw = cms.bool(True)                           # throw exception on unknown path names
-                                    )
+                                   TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+                                   HLTPaths = cms.vstring('HLT_JET15'),        # provide list of HLT paths (or patterns) you want
+                                   eventSetupPathsKey = cms.string(''),             # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
+                                   andOr = cms.bool(True),                          # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
+                                   throw = cms.bool(True)                           # throw exception on unknown path names
+                                   )
 
 # Primary vertex filter
 process.primaryVertexFilter = cms.EDFilter("VertexSelector",
@@ -75,11 +82,11 @@ process.scrapingVeto = cms.EDFilter("FilterOutScraping",
 
 #is it MC or DATA
 isMC = False
-useHBHEcleaning = True
+useHBHEcleaning = False
 useHBHEfilter = False
 
 HFPMTcleaningversion = 4   # version 1 = default (loose), version 2 = (medium), version 3 = (tight)
-# VERSION 2 is the currently recommended version, as of 22 April 2010.
+# VERSION 4 is the currently recommended version, as of 22 April 2010.
 
 if useHBHEfilter == True:
     process.load('CommonTools/RecoAlgos/HBHENoiseFilter_cfi')
@@ -117,13 +124,6 @@ process.load("JetMETAnalysis/HcalReflagging/hbherechitreflaggerJETMET_cfi")
 process.hbherecoReflagged = process.hbherechitreflaggerJETMET.clone()
 process.hbherecoReflagged.debug=0
 
-# Best current cuts (as of 12 May 2010):  require at least 14 hits with E > 0.5 GeV
-
-# Set energy threshold for identifying noise
-process.hbherecoReflagged.Ethresh=0.5
-# Set number of channels/hpd that must be above threshold in order for the HPD to be marked noisy
-process.hbherecoReflagged.Nhits=14
-
 # Use the reflagged HF RecHits to make the CaloTowers
 process.towerMaker.hfInput = "hfrecoReflagged"
 process.towerMakerWithHO.hfInput = "hfrecoReflagged"
@@ -138,6 +138,10 @@ else:
     process.towerMaker.hbheInput = "hbherecoReflagged"
     process.towerMakerWithHO.hbheInput = "hbherecoReflagged"
 
+# Instead of rejecting the event, add a flag indicating the HBHE noise 
+process.load('CommonTools/RecoAlgos/HBHENoiseFilterResultProducer_cfi')
+process.hbheflag = cms.Path(process.HBHENoiseFilterResultProducer)
+
 ##################################################
 ####### OFFICIAL RECOMMENDATION ENDS HERE#######
 ##################################################
@@ -147,24 +151,26 @@ process.promptanaTree = cms.EDAnalyzer("PromptAnaTree",
     'drop *',
     'keep *_promptanaevent_*_*',
     'keep *_promptanamet_*_*',
+#    'keep *_promptanametdefault_*_*',
+#    'keep *_promptanatcmetdefault_*_*',
     'keep *_promptanatcmet_*_*',
     'keep *_promptanapfmet_*_*',
-    'keep *_promptananohf_*_*',
+#    'keep *_promptananohf_*_*',
     'keep *_promptanaic5calojet_*_*',
     'keep *_promptanaak5calojet_*_*',
     'keep *_promptanaJPTak5_*_*',
     'keep *_promptanaak5pfjet_*_*',
-    'keep *_promptanacalotowers_*_*',
+#    'keep *_promptanacalotowers_*_*',
     'keep *_promptanatrigger_*_*',
     'keep *_promptanavtx_*_*',
-    'keep *_promptanatrack_*_*',
-    'keep *_promptanacleanup_*_*',
-    'keep *_promptanaecalspikes_*_*'
+#    'keep *_promptanatrack_*_*',
+    'keep *_promptanaecalspikes_*_*',
+    'keep *_HBHENoiseFilterResultProducer_*_*'
     ))
 
 # Filter sequence
 process.filterSequence = cms.Sequence(
-    process.hltPhysicsDeclared*
+#    process.hltPhysicsDeclared*
     process.hltHighLevel*
     process.hltLevel1GTSeed*
     process.primaryVertexFilter*
@@ -172,7 +178,6 @@ process.filterSequence = cms.Sequence(
 )
 
 # Path and EndPath definitions
-#process.rereco_step = cms.Path(process.towerMaker*(process.met+process.muonMETValueMapProducer+process.muonTCMETValueMapProducer+process.tcMet))
 process.rereco_step = cms.Path(process.filterSequence*process.caloTowersRec*(process.recoJets*process.recoJetIds+process.recoTrackJets)*process.recoJetAssociations*process.btagging*process.metreco) # re-reco jets and MET
 
 process.ntuple_step=cms.Path(
@@ -180,21 +185,22 @@ process.ntuple_step=cms.Path(
     (
     process.promptanaevent +
     process.promptanamet   +
+#    process.promptanametdefault +
+#    process.promptanatcmetdefault +
     process.promptanatcmet   +
     process.promptanapfmet   +
-    process.promptananohf  +
-#    process.promptanaic5calojet +
-#    process.promptanaak5calojet +
-#    process.promptanaJPTak5 +
-#    process.promptanaak5pfjet +
-    process.promptanacalotowers +
+#     process.promptananohf  +
+    process.promptanaic5calojet +
+    process.promptanaak5calojet +
+    process.promptanaJPTak5 +
+    process.promptanaak5pfjet +
+#    process.promptanacalotowers +
     process.promptanatrigger +
     process.promptanavtx +
-    process.promptanatrack +
-    process.promptanacleanup +
-    process.promptanaecalspikes
+#    process.promptanatrack +
+    process.promptanaecalspikes 
     )
     *process.promptanaTree
 )
 
-process.schedule = cms.Schedule(process.reflagging_step, process.rereco_step, process.ntuple_step)
+process.schedule = cms.Schedule(process.reflagging_step, process.rereco_step, process.hbheflag, process.ntuple_step)
