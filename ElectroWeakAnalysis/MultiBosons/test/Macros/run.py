42b0d90de4a721526d16d3585d1034326e1173fb
iraklis <>
2010-06-08 08:58:07 +0000

    updated for new nTuples

--- ElectroWeakAnalysis/MultiBosons/test/Macros/run.py
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/run.py
@@ -12,10 +12,14 @@ os.system("source clean.sh")
 # list of nTuples
 # ===============
 filesToRun=[]
-filesToRun.append("rfio:/castor/cern.ch//user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_Wgamma_Pythia/35x_Wgamma_MC.root")
-filesToRun.append("Zgamma.root")
-filesToRun.append("Zee.root")
-filesToRun.append("Wenu.root")
+filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_Wgamma_Pythia.root")
+filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_Zgamma_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_WW_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_WZ_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_ZZ_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_Wenu_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_Zee_Pythia.root")
+#filesToRun.append("rfio:/castor/cern.ch/user/p/poter/VGamma_Ntuples/CMSSW357_Jun04_Skim/35x_TTbar_Pythia.root")
 
 for fileToRun in filesToRun:
     Main(fileToRun)
