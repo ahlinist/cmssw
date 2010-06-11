e8156f966f4d221adf9eb05955a91de5f0518c9e
veelken <>
2010-06-11 08:56:07 +0000

    initialize name, title from configuration parameterset instead of calling setter function

--- TauAnalysis/FittingTools/interface/SmoothPdfWrapperBase.h
+++ TauAnalysis/FittingTools/interface/SmoothPdfWrapperBase.h
@@ -7,9 +7,9 @@
  * 
  * \author Christian Veelken, UC Davis
  *
- * \version $Revision: 1.2 $
+ * \version $Revision: 1.3 $
  *
- * $Id: SmoothPdfWrapperBase.h,v 1.2 2010/05/26 15:26:07 veelken Exp $
+ * $Id: SmoothPdfWrapperBase.h,v 1.3 2010/05/27 12:41:20 veelken Exp $
  *
  */
 
@@ -38,10 +38,8 @@ class SmoothPdfWrapperBase
   // "setter" functions
   // (cannot be passed in constructor of SmoothPdfWrapperBase,
   //  in order to be compatible with plugin mechanism)
-  void setName(const char* name) { name_ = name; }
-  void setTitle(const char* title) { title_ = title; }
-  void setTemplateHist(RooDataHist* hist) { templateHist_ = hist; }
-  void setX(RooRealVar& x) { x_ = &x; }
+  virtual void setTemplateHist(RooDataHist* hist) { templateHist_ = hist; }
+  virtual void setX(RooRealVar& x) { x_ = &x; }
   
   // function to "build" RooAbsPdf object
   // (cannot be done in constructor of SmoothPdfWrapperBase,
@@ -87,7 +85,7 @@ class SmoothPdfWrapperBase
     RooAbsPdf* pdfExternalConstraint_;
   };
   
-  RooRealVar* makeRooRealVar(const char*, const char*, const edm::ParameterSet&);
+  RooRealVar* makeRooRealVar(const std::string&, const std::string&, const edm::ParameterSet&);
 
   // fit smooth probability density function to template histogram
   virtual void fit();
@@ -98,8 +96,8 @@ class SmoothPdfWrapperBase
   std::map<std::string, parameterType*> parameters_;
 
   // name, title
-  const char* name_;
-  const char* title_;
+  std::string name_;
+  std::string title_;
 
   // pointer to RooRealVar for "dependent" variable x
   RooRealVar* x_;
--- TauAnalysis/FittingTools/src/SmoothPdfWrapperBase.cc
+++ TauAnalysis/FittingTools/src/SmoothPdfWrapperBase.cc
@@ -23,6 +23,10 @@ SmoothPdfWrapperBase::parameterType::parameterType(
     pdfExternalConstraintName_(std::string(name).append("_pdfExternalConstraint")),
     pdfExternalConstraint_(0)
 {
+  std::cout << "<parameterType::parameterType>:" << std::endl;
+  std::cout << " name = " << name_ << std::endl;
+  std::cout << " title = " << title_ << std::endl;
+
   pdfCoeff_ = new RooRealVar(name, title, initial, min, max);
   
   if ( uncertainty > 0. ) {
@@ -47,16 +51,25 @@ SmoothPdfWrapperBase::parameterType::~parameterType()
 //
 
 SmoothPdfWrapperBase::SmoothPdfWrapperBase(const edm::ParameterSet& cfg)
-  : name_(""),
-    title_(""),
-    x_(0),
+  : x_(0),
     pdf_(0),
     templateHist_(0),
     externalConstraints_(0)
 {
+  std::cout << "<SmoothPdfWrapperBase::SmoothPdfWrapperBase>:" << std::endl;
+
+  name_ = cfg.getParameter<std::string>("name");
+  std::cout << " name = " << name_ << std::endl;
+  title_ = cfg.getParameter<std::string>("title");
+  std::cout << " title = " << title_ << std::endl;
+
   fitOptions_.Add(new RooCmdArg(RooFit::PrintLevel(-1)));
   fitOptions_.Add(new RooCmdArg(RooFit::PrintEvalErrors(false)));
   fitOptions_.Add(new RooCmdArg(RooFit::Warnings(false)));
+  fitOptions_.Add(new RooCmdArg(RooFit::SumW2Error(false)));
+  
+  fitSimultaneously_ = cfg.getParameter<bool>("fitSimultaneously");
+  std::cout << " fitSimultaneously = " << fitSimultaneously_ << std::endl;
 }
 
 SmoothPdfWrapperBase::~SmoothPdfWrapperBase()
@@ -66,22 +79,25 @@ SmoothPdfWrapperBase::~SmoothPdfWrapperBase()
   delete externalConstraints_;
 }
 
-RooRealVar* SmoothPdfWrapperBase::makeRooRealVar(const char* name, const char* title, const edm::ParameterSet& cfg)
+RooRealVar* SmoothPdfWrapperBase::makeRooRealVar(const std::string& name, const std::string& title, const edm::ParameterSet& cfg)
 {
+  std::cout << "<SmoothPdfWrapperBase::makeRooRealVar>:" << std::endl;
+  std::cout << " name = " << name << std::endl;
+  std::cout << " title = " << title << std::endl;
+
 //--- check that parameter does not already exist
-  if ( parameters_.find("name") != parameters_.end() ) {
+  if ( parameters_.find(name) != parameters_.end() ) {
     edm::LogError ("makeRooRealVar") << " Parameter with name = " << name << " already exists !!";
     return parameters_[name]->pdfCoeff_;
   }
 
   double initial = cfg.getParameter<double>("initial");
-  double min = cfg.getParameter<double>("min");
-  double max = cfg.getParameter<double>("max");
+  double min = ( cfg.exists("min") ) ? cfg.getParameter<double>("min") : -RooNumber::infinity();
+  double max = ( cfg.exists("max") ) ? cfg.getParameter<double>("max") : +RooNumber::infinity();
   double uncertainty = ( cfg.exists("uncertainty") ) ? cfg.getParameter<double>("uncertainty") : -1.;
 
 //--- create new parameter  
-  std::string parameterName = std::string(name_).append("name");
-  parameterType* parameter = new parameterType(parameterName.data(), title, initial, min, max, uncertainty);
+  parameterType* parameter = new parameterType(name.data(), title.data(), initial, min, max, uncertainty);
 
 //--- add parameter to list of existing parameters
   parameters_[name] = parameter;
@@ -92,6 +108,8 @@ RooRealVar* SmoothPdfWrapperBase::makeRooRealVar(const char* name, const char* t
 
 void SmoothPdfWrapperBase::fit()
 {
+  std::cout << "<SmoothPdfWrapperBase::fit>:" << std::endl;
+
   if ( !templateHist_ ) {
     edm::LogError ("fit") << " Uninitialized template Histogram --> skipping !!";
     return;
@@ -107,7 +125,11 @@ void SmoothPdfWrapperBase::fit()
   if ( !fitSimultaneously_ ) {
     for ( std::map<std::string, parameterType*>::iterator parameter = parameters_.begin();
 	  parameter != parameters_.end(); ++parameter ) {
-      if ( parameter->second->uncertainty_ <= 0. ) parameter->second->pdfCoeff_->setConstant();
+      if ( parameter->second->uncertainty_ <= 0. ) {
+	std::cout << "---> setting pdfCoeff = " << parameter->second->pdfCoeff_->GetName() 
+		  << " to constant value = " << parameter->second->pdfCoeff_->getVal() << std::endl;
+	parameter->second->pdfCoeff_->setConstant();
+      }
     }
   }
 }
