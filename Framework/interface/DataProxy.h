b035ecd3327ca4836c6cc04007661e249b430114
chrjones <>
2010-06-17 15:58:02 +0000

    Moved interface for getting data to the base class.  This removes some internal type-safety but will allow for greater flexibility.

--- Framework/interface/DataProxy.h
+++ Framework/interface/DataProxy.h
@@ -10,7 +10,8 @@
  Description: Base class for data Proxies held by a EventSetupRecord
 
  Usage:
-    <usage>
+    This class defines the interface used to handle retrieving data from an
+ EventSetup Record. 
 
 */
 //
@@ -30,60 +31,69 @@ namespace edm {
       class DataKey;
       class ComponentDescription;
       
-class DataProxy
-{
-
-   public:
-      DataProxy();
-      virtual ~DataProxy();
-
-      // ---------- const member functions ---------------------
-      bool cacheIsValid() const { return cacheIsValid_; }
-      
-      virtual void doGet(const EventSetupRecord& iRecord, const DataKey& iKey, bool iTransiently) const = 0;
-
-      ///returns the description of the DataProxyProvider which owns this Proxy
-      const ComponentDescription* providerDescription() const {
-         return description_;
-      }
-      // ---------- static member functions --------------------
-
-      // ---------- member functions ---------------------------
-      void invalidate() {
-         clearCacheIsValid();
-         invalidateCache();
-      }
-      
-      void resetIfTransient();
-
-      void setProviderDescription(const ComponentDescription* iDesc) {
-         description_ = iDesc;
-      }
-   protected:
-      /** indicates that the Proxy should invalidate any cached information
-           as that information has 'expired' (i.e. we have moved to a new IOV)
-         */
-      virtual void invalidateCache() = 0;
-   
-      /** indicates that the Proxy should invalidate any cached information
+      class DataProxy {
+         
+      public:
+         DataProxy();
+         virtual ~DataProxy();
+         
+         // ---------- const member functions ---------------------
+         bool cacheIsValid() const { return cacheIsValid_; }
+         
+         void doGet(const EventSetupRecord& iRecord, const DataKey& iKey, bool iTransiently) const;
+         const void* get(const EventSetupRecord&, const DataKey& iKey, bool iTransiently) const;
+         
+         ///returns the description of the DataProxyProvider which owns this Proxy
+         const ComponentDescription* providerDescription() const {
+            return description_;
+         }
+         // ---------- static member functions --------------------
+         
+         // ---------- member functions ---------------------------
+         void invalidate() {
+            clearCacheIsValid();
+            invalidateCache();
+         }
+         
+         void resetIfTransient();
+         
+         void setProviderDescription(const ComponentDescription* iDesc) {
+            description_ = iDesc;
+         }
+      protected:
+         /**This is the function which does the real work of getting the data if it is not
+          already cached.  The returning 'const void*' must point to an instance of the class
+          type corresponding to the type designated in iKey. So if iKey refers to a base class interface
+          the pointer must be a pointer to that base class interface and not a pointer to an inheriting class
+          instance.
+          */
+         virtual const void* getImpl(const EventSetupRecord&, const DataKey& iKey) =0;
+         
+         /** indicates that the Proxy should invalidate any cached information
+          as that information has 'expired' (i.e. we have moved to a new IOV)
+          */
+         virtual void invalidateCache() = 0;
+         
+         /** indicates that the Proxy should invalidate any cached information
           as that information was accessed transiently and therefore is not
           intended to be kept over the entire IOV.  Default is to call
           invalidateCache().
-       */
-      virtual void invalidateTransientCache();
-
-      void setCacheIsValidAndAccessType(bool iTransientAccessOnly);
-      void clearCacheIsValid();      
-   private:
-      DataProxy(const DataProxy&); // stop default
-
-      const DataProxy& operator=(const DataProxy&); // stop default
-
-      // ---------- member data --------------------------------
-      bool cacheIsValid_;
-      bool nonTransientAccessRequested_;
-      const ComponentDescription* description_;
-};
+          */
+         virtual void invalidateTransientCache();
+         
+         void clearCacheIsValid();      
+      private:
+         DataProxy(const DataProxy&); // stop default
+         
+         const DataProxy& operator=(const DataProxy&); // stop default
+         void setCacheIsValidAndAccessType(bool iTransientAccessOnly) const;
+         
+         // ---------- member data --------------------------------
+         mutable const void* cache_;
+         mutable bool cacheIsValid_;
+         mutable bool nonTransientAccessRequested_;
+         const ComponentDescription* description_;
+      };
    }
 }
 #endif
