5c13465f8a5fe9deb43903ce9f0dece52c65425b
veelken <>
2010-06-21 13:42:06 +0000

    added definitions of bin-grids (tau id. passed/failed, Mt(muon + MEt), rel. muon iso [,muon + tau-jet charge]) for 3d/4d generalized matrix method for tau id. efficiency estimation

--- TauAnalysis/BgEstimationTools/python/bgEstBinGridZtoMuTau_cfi.py
+++ TauAnalysis/BgEstimationTools/python/bgEstBinGridZtoMuTau_cfi.py
@@ -1,45 +1,97 @@
 import FWCore.ParameterSet.Config as cms
 
-genMatrixBinningZtoMuTau = cms.PSet(
-    name = cms.string("genMatrixBinningZtoMuTau"),
+tauIdEffBinningZtoMuTau_genMatrix3d = cms.PSet(
+    name = cms.string("tauIdEffBinningZtoMuTau_genMatrix3d"),
     config = cms.VPSet(
         cms.PSet(
             extractor = cms.PSet(
-                pluginType = cms.string("PATMuonValExtractor"),
-                src = cms.InputTag('selectedPatMuonsEcalIsoLooseIsolationCumulative'),
-                value = cms.string("trackIso + ecalIso")
+                pluginType = cms.string("PATTauValExtractor"),
+                src = cms.InputTag('tausForTauIdEffZtoMuTauMuonVetoCumulative'),
+                value = cms.string("tauID('ewkTauId')")
             ),
-            branchName = cms.string('muonIso'),
             binning = cms.PSet(
-                boundaries = cms.vdouble(2.01),
+                boundaries = cms.vdouble(0.5),
                 min = cms.double(-0.01),
-                max = cms.double(16.)
+                max = cms.double(1.01)
+            )
+        ),
+        cms.PSet(
+            extractor = cms.PSet(
+                pluginType = cms.string("PATMuonValExtractor"),
+                src = cms.InputTag('muonsForTauIdEffZtoMuTauTrkIPrelIsolationCumulative'),
+                value = cms.string("? pt > 0 ? (userIsolation('pat::TrackIso') + userIsolation('pat::EcalIso'))/pt : -1")
+            ),
+            binning = cms.PSet(
+                boundaries = cms.vdouble(0.09),
+                min = cms.double(-0.01),
+                max = cms.double(1000.)
             )
         ),
         cms.PSet(
             extractor = cms.PSet(
                 pluginType = cms.string("PATMuTauPairValExtractor"),
-                src = cms.InputTag('muTauPairsLooseSelection'),
-                value = cms.string("abs(charge)")
+                src = cms.InputTag('muTauPairsTauIdEffZtoMuTauRelMuonIsolation'),
+                value = cms.string("mt1MET")
+            ),
+            binning = cms.PSet(
+                boundaries = cms.vdouble(40.),
+                min = cms.double(-0.01),
+                max = cms.double(1000.)
+            )
+        )
+    )
+)
+
+tauIdEffBinningZtoMuTau_genMatrix4d = cms.PSet(
+    name = cms.string("tauIdEffBinningZtoMuTau_genMatrix4d"),
+    config = cms.VPSet(
+        cms.PSet(
+            extractor = cms.PSet(
+                pluginType = cms.string("PATTauValExtractor"),
+                src = cms.InputTag('tausForTauIdEffZtoMuTauMuonVetoCumulative'),
+                value = cms.string("tauID('ewkTauId')")
+            ),
+            binning = cms.PSet(
+                boundaries = cms.vdouble(0.5),
+                min = cms.double(-0.01),
+                max = cms.double(1.01)
+            )
+        ),
+        cms.PSet(
+            extractor = cms.PSet(
+                pluginType = cms.string("PATMuonValExtractor"),
+                src = cms.InputTag('muonsForTauIdEffZtoMuTauTrkIPrelIsolationCumulative'),
+                value = cms.string("? pt > 0 ? (userIsolation('pat::TrackIso') + userIsolation('pat::EcalIso'))/pt : -1")
+            ),
+            binning = cms.PSet(
+                boundaries = cms.vdouble(0.09),
+                min = cms.double(-0.01),
+                max = cms.double(1000.)
+            )
+        ),
+        cms.PSet(
+            extractor = cms.PSet(
+                pluginType = cms.string("PATMuTauPairValExtractor"),
+                src = cms.InputTag('muTauPairsTauIdEffZtoMuTauRelMuonIsolation'),
+                value = cms.string("mt1MET")
+            ),
+            binning = cms.PSet(
+                boundaries = cms.vdouble(40.),
+                min = cms.double(-0.01),
+                max = cms.double(1000.)
+            )
+        ),
+        cms.PSet(
+            extractor = cms.PSet(
+                pluginType = cms.string("PATMuTauPairValExtractor"),
+                src = cms.InputTag('muTauPairsTauIdEffZtoMuTauRelMuonIsolation'),
+                value = cms.string("? leg2.leadPFChargedHadrCand.isNonnull ? leg1.charge + leg2.leadPFChargedHadrCand.charge : 0")
             ),
             branchName = cms.string('diTauAbsCharge'),
             binning = cms.PSet(
                 boundaries = cms.vdouble(0.5),
                 min = cms.double(-0.01),
                 max = cms.double(+2.01)
-            )
-        ),
-        cms.PSet(
-            extractor = cms.PSet(
-                pluginType = cms.string("PATMuTauPairValExtractor"),
-                src = cms.InputTag('muTauPairsLooseSelection'),
-                value = cms.string("mt1MET")
-            ),
-            branchName = cms.string('diTauMt1MET'),
-            binning = cms.PSet(
-                boundaries = cms.vdouble(40.),
-                min = cms.double(-0.01),
-                max = cms.double(100.)
             )
         )
     )
