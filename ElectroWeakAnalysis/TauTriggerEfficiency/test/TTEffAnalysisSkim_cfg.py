1cba66d64796d973c95c854cf10b7f233ba68aef
eluiggi <>
2010-06-22 17:09:53 +0000

    Use of pftau skim incorporated

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalysisSkim_cfg.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/TTEffAnalysisSkim_cfg.py
@@ -10,47 +10,40 @@ process.maxEvents = cms.untracked.PSet(
 process.load("FWCore/MessageService/MessageLogger_cfi")
 process.MessageLogger.destinations = cms.untracked.vstring("cout")
 process.MessageLogger.cout = cms.untracked.PSet(
-#    threshold = cms.untracked.string("DEBUG")    # pring LogDebugs and above
     threshold = cms.untracked.string("INFO")     # print LogInfos and above
-#    threshold = cms.untracked.string("WARNING")  # print LogWarnings and above
     )
 # This is also neede for printing debugs
 process.MessageLogger.debugModules = cms.untracked.vstring("IdentifiedTaus","IdentifiedTauFilter")
 
-from ElectroWeakAnalysis.TauTriggerEfficiency.MinimumBias_BeamCommissioning09_SD_AllMinBias_Dec19thSkim_336p3_v1_RAW_RECO import *
-process.source = source
-#process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring(
-##	"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_AllMinBias-Dec19thSkim_336p3_v1/0008/FC895826-E8F0-DE11-8E36-001D0967DA76.root"
-#    )
-#)
+process.load('CondCore.DBCommon.CondDBSetup_cfi')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+#process.GlobalTag.globaltag = 'GR10_E_V4A::All'
+process.GlobalTag.globaltag = 'MC_3XY_V25::All'
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
 
-# The proper luminosity sections
-process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange(
-    '123596:2-123596:9999',   # OK
-    '123615:70-123615:9999',  # OK
-    '123732:62-123732:9999',  # 62 -9999 (Pixel off in 56-61)
-    '123815:8-123815:9999',   # 8 - 9999 ( why not 7 ?)
-    '123818:2-123818:42',     # OK
-    '123908:2-123908:12',     # 2 - 12 (why not 13 ?)
-    '124008:1-124008:1',      # OK
-    '124009:1-124009:68',     # OK
-    '124020:12-124020:94',    # OK
-    '124022:66-124022:179',   # OK
-    '124023:38-124023:9999',  # OK
-    '124024:2-124024:83',     # OK
-    '124025:5-124025:13',     # 5 - 13 (why not 3 & 4 ?)
-    '124027:24-124027:9999',  # OK
-    '124030:2-124030:9999',   # 2 - 9999 ( why not 1 ?)
-    '124120:1-124120:9999',   # OK
-    '124275:3-124275:30'
+#from ElectroWeakAnalysis.TauTriggerEfficiency.MinimumBias_BeamCommissioning09_SD_AllMinBias_Dec19thSkim_336p3_v1_RAW_RECO import *
+#process.source = source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FE8C8E24-7C50-DF11-8490-0030486790A6.root',
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FE8323A7-7C50-DF11-A7C6-00248C0BE016.root',
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FE3BD1F6-7B50-DF11-A603-0026189438E0.root',
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FCEC93A7-7C50-DF11-B786-0026189438E0.root',
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FCADDDAB-7C50-DF11-9B0D-003048679296.root',
+        '/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V26A_357ReReco-v3/0188/FC6793CC-7D50-DF11-8323-003048679228.root'
+    )
 )
+
 
 # This is for filtering on L1 technical trigger bit: MB and no beam halo
 process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
 process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
 process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
-process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('(0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39))')
+#process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('(0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39))')
+# For MC
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('(40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
 
 process.scrapping = cms.EDFilter("FilterOutScraping",
     	applyfilter = cms.untracked.bool(True),
@@ -59,20 +52,30 @@ process.scrapping = cms.EDFilter("FilterOutScraping",
         thresh = cms.untracked.double(0.25)
 )
 
-process.PFTausSelected = cms.EDFilter("PFTauSelector",
-    src = cms.InputTag("shrinkingConePFTauProducer"),
-    discriminators = cms.VPSet(
-#       cms.PSet( discriminator=cms.InputTag("pfRecoTauDiscriminationByTrackIsolationHighEfficiency"),selectionCut$
-#       cms.PSet( discriminator=cms.InputTag("pfRecoTauDiscriminationByLeadingPionPtCutHighEfficiency"),selectionC$
-#       cms.PSet( discriminator=cms.InputTag("thisPFRecoTauDiscriminationByLeadingPionPtCut"),selectionCut=cms.dou$
-#       cms.PSet( discriminator=cms.InputTag("thisPFRecoTauDiscriminationByLeadingPionPtCut"),selectionCut=cms.dou$
-    )
-)
+###
+#process.load("RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingPionPtCut_cfi")
+#process.thisPFRecoTauDiscriminationByLeadingPionPtCut = cms.EDFilter("PFRecoTauDiscriminationByLeadingPionPtCut",
+#    PFTauProducer = cms.InputTag('shrinkingConePFTauProducer'),
+#    MinPtLeadingPion = cms.double(3.0)
+#)
+#
+#process.PFTausSelected = cms.EDFilter("PFTauSelector",
+#    src = cms.InputTag("shrinkingConePFTauProducer"),
+#    discriminators = cms.VPSet(
+#       cms.PSet( discriminator=cms.InputTag("pfRecoTauDiscriminationByTrackIsolationHighEfficiency"),selectionCut=cms.double(0.5)),
+#       cms.PSet( discriminator=cms.InputTag("thisPFRecoTauDiscriminationByLeadingPionPtCut"),selectionCut=cms.double(0.5))
+#    )
+#)
 
+process.load("ElectroWeakAnalysis.TauTriggerEfficiency.pftauSkim_cff")
 process.tauFilter = cms.Path(
 	process.hltLevel1GTSeed *
 	process.scrapping *
-	process.PFTausSelected
+	process.looseShrikingConePFTaus *
+	process.thePFTauDiscByLeadTrkFinding *
+	process.thePFTauDiscByIsolation *
+	process.PFTausSelected *
+	process.PFTauSkimmed
 )
 
 from ElectroWeakAnalysis.TauTriggerEfficiency.eventContent_cfi import *
