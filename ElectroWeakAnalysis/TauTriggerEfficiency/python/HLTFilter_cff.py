4d501631eac97edf76137df54c5b9eefcb4315a5
slehti <>
2010-06-28 12:04:05 +0000

    Updated HLT filtering to use HLTrigger.HLTfilters.triggerResultsFilter_cfi

--- ElectroWeakAnalysis/TauTriggerEfficiency/python/HLTFilter_cff.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/python/HLTFilter_cff.py
@@ -1,5 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
+import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
+
+MuonHLTs = hlt.triggerResultsFilter.clone(
+    hltResults    = cms.InputTag('TriggerResults::HLT'),
+    triggerConditions =  (
+        'HLT_L1MuOpen',
+        'HLT_L1Mu',
+        'HLT_L2Mu9',
+        'HLT_L2Mu11',
+        'HLT_Mu3',
+        'HLT_Mu5',
+        'HLT_Mu7',
+        'HLT_Mu9',
+        'HLT_Mu11',
+        'HLT_DoubleMu0',
+        'HLT_DoubleMu3',
+        'HLT_DoubleMu5',
+        'HLT_L1DoubleMuOpen'
+    ),
+    l1tResults = '',
+    throw = False
+)
+
+//HLTFilter code located in TTEff
 IncludedHLTs = cms.EDFilter("HLTFilter",
 	HLTResults   = cms.InputTag("TriggerResults::HLT"),
 	HLTSelection = cms.VInputTag(cms.InputTag("HLT_L1MuOpen"),
