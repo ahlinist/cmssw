951cb40281cc0e12e33880e7becdf68a87f937f0
slehti <>
2010-06-28 12:16:26 +0000

    Commented the old filter to silence the warnings

--- ElectroWeakAnalysis/TauTriggerEfficiency/python/HLTFilter_cff.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/python/HLTFilter_cff.py
@@ -24,21 +24,21 @@ MuonHLTs = hlt.triggerResultsFilter.clone(
 )
 
 #HLTFilter code located in TTEff
-IncludedHLTs = cms.EDFilter("HLTFilter",
-	HLTResults   = cms.InputTag("TriggerResults::HLT"),
-	HLTSelection = cms.VInputTag(cms.InputTag("HLT_L1MuOpen"),
-                                     cms.InputTag("HLT_L1Mu"),
-                                     cms.InputTag("HLT_L2Mu9"),
-                                     cms.InputTag("HLT_L2Mu11"),
-                                     cms.InputTag("HLT_Mu3"),
-                                     cms.InputTag("HLT_Mu5"),
-                                     cms.InputTag("HLT_Mu7"),
-                                     cms.InputTag("HLT_Mu9"),
-                                     cms.InputTag("HLT_Mu11"),
-                                     cms.InputTag("HLT_Mu5"),
-                                     cms.InputTag("HLT_DoubleMu0"),
-                                     cms.InputTag("HLT_DoubleMu3"),
-                                     cms.InputTag("HLT_DoubleMu5"),
-                                     cms.InputTag("HLT_L1DoubleMuOpen")
-	)
-)
+#IncludedHLTs = cms.EDFilter("HLTFilter",
+#	HLTResults   = cms.InputTag("TriggerResults::HLT"),
+#	HLTSelection = cms.VInputTag(cms.InputTag("HLT_L1MuOpen"),
+#                                     cms.InputTag("HLT_L1Mu"),
+#                                     cms.InputTag("HLT_L2Mu9"),
+#                                     cms.InputTag("HLT_L2Mu11"),
+#                                     cms.InputTag("HLT_Mu3"),
+#                                     cms.InputTag("HLT_Mu5"),
+#                                     cms.InputTag("HLT_Mu7"),
+#                                     cms.InputTag("HLT_Mu9"),
+#                                     cms.InputTag("HLT_Mu11"),
+#                                     cms.InputTag("HLT_Mu5"),
+#                                     cms.InputTag("HLT_DoubleMu0"),
+#                                     cms.InputTag("HLT_DoubleMu3"),
+#                                     cms.InputTag("HLT_DoubleMu5"),
+#                                     cms.InputTag("HLT_L1DoubleMuOpen")
+#	)
+#)
