b4b3b2d7406b3fd501626998f8214808cdab964a
lgray <>
2010-07-02 15:01:47 +0000

    Fully fleshed out selector config! Now to test it...

--- ElectroWeakAnalysis/MultiBosons/bin/vGammaAnalysis_cfg.py
+++ ElectroWeakAnalysis/MultiBosons/bin/vGammaAnalysis_cfg.py
@@ -2,33 +2,38 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
-
 process.inputs = cms.PSet (
-    fileNames = cms.vstring(
-        # Your data goes here:
-#         '/Volumes/MyBook/Data/TTbar/shyft_35x_v3/ljmet_1.root'
-#         'file:zmm_pat.root'
-        'file:/tmp/veverka/tmp.YPPUW29322/CMSSW_3_6_2/src/PhysicsTools/SelectorUtils/bin/wm_pat.root'
-        )
+    fileNames = cms.vstring('file:/tmp/veverka/tmp.YPPUW29322/CMSSW_3_6_2/src/PhysicsTools/SelectorUtils/bin/wm_pat.root'
+                            )
 )
 
 process.outputs = cms.PSet (
-    outputName = cms.string('muonPlots.root')
+    outputName = cms.string('analysisPlots.root')
 )
 
-# process.load("ElectroWeakAnalysis.MultiBosons.Histogramming.leafKineHistos_cff")
+## Define the selector configuration you want to use in FWLite
+from ElectroWeakAnalysis.MultiBosons.Selectors.vGammaSelector_cfi import sw_commissioning_selection
 
-## Convert cms.EDAnalyzer to cms.PSet
+process.SelectorConfig = sw_commissioning_selection.copy()
+
+## Create the histogram definitions for the FWLite program
 from ElectroWeakAnalysis.MultiBosons.Histogramming.muonHistos_cfi import muonHistos
-process.muonHistos = cms.PSet(
-  src = cms.InputTag(muonHistos.src.value() ),
-  histograms = muonHistos.histograms.copy(),
-  outputDirectory = cms.string('test')
-)
+from ElectroWeakAnalysis.MultiBosons.Histogramming.photonHistos_cfi import photonHistos
+from ElectroWeakAnalysis.MultiBosons.Histogramming.mmgHistos_cfi import mmgHistos
 
-# process.wplusjetsAnalysis.muonSrc = "cleanPatMuons"
-# process.wplusjetsAnalysis.electronSrc = "cleanPatElectrons"
-# process.wplusjetsAnalysis.jetSrc = "cleanPatJets"
-#
-# process.wplusjetsAnalysis.minJets = 2
+process.ZMuMuGamma = cms.PSet(
+    muonHistos = cms.PSet(src = cms.InputTag(muonHistos.src.value() ),
+                          histograms = muonHistos.histograms.copy(),
+                          outputDirectory = cms.string('ZMuMuGamma/Muons')
+                          ),
+    photonHistos = cms.PSet(src = cms.InputTag(photonHistos.src.value()),
+                            histograms = photonHistos.histograms.copy(),
+                            outputDirectory = cms.string('ZMuMuGamma/Photons')
+                            ),
+    ZMuMuGammaHistos = cms.PSet(src = cms.InputTag(photonHistos.src.value()),
+                                histograms = photonHistos.histograms.copy(),
+                                outputDirectory = cms.string('ZMuMuGamma')
+                                )
+    
+    )
 
