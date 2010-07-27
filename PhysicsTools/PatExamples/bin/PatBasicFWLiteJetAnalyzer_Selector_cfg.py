afca73710cfffe58c2f8e00b913122182e1aacc7
srappocc <>
2010-07-27 15:35:59 +0000

    Adding an example to run on a good run/lumi list from FWLite

--- PhysicsTools/PatExamples/bin/PatBasicFWLiteJetAnalyzer_Selector_cfg.py
+++ PhysicsTools/PatExamples/bin/PatBasicFWLiteJetAnalyzer_Selector_cfg.py
@@ -2,6 +2,19 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
+#input stuff for Run/Lumi selection with the "JSON"-formatted files from the PVT group
+import FWCore.PythonUtilities.LumiList as LumiList
+
+
+# setup process
+process = cms.Process("FWLitePlots")
+
+# get JSON file correctly parced
+JSONfile = 'DCSTRONLY_132440-140388'
+myList = LumiList.LumiList (filename = JSONfile).getCMSSWString().split(',')
+
+
+# Set up the parameters for the calo jet analyzer
 process.jetStudies = cms.PSet(
     # input parameter sets
     jetSrc = cms.InputTag('selectedPatJets'),
@@ -10,6 +23,8 @@ process.jetStudies = cms.PSet(
     pfMetSrc = cms.InputTag('patMETsPF'),
     useCalo = cms.bool(True)
 )
+
+# Set up the parameters for the PF jet analyzer
 process.pfJetStudies = process.jetStudies.clone( useCalo = cms.bool(False) )
 
 
@@ -18,15 +33,16 @@ process.load('PhysicsTools.SelectorUtils.jetIDSelector_cfi')
 
 process.plotParameters = cms.PSet (
     doTracks = cms.bool(False),
-    useMC = cms.bool(False),
-    runs = cms.vint32([])
+    useMC = cms.bool(False)
 )
 
 
 process.inputs = cms.PSet (
     fileNames = cms.vstring(
-        'reco_7TeV_firstdata_356_pat.root'
-        )
+        'reco_7TeV_380_pat.root'
+        ),
+    lumisToProcess = cms.untracked.VLuminosityBlockRange( myList )
+
 )
 
 process.outputs = cms.PSet (
