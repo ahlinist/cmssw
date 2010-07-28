8f5e36634c829ae392750b695204ba624d356c6f
rwolf <>
2010-07-28 15:18:50 +0000

    port and adaptations for PatCleaning Exercise 07

--- PhysicsTools/PatExamples/bin/PatCleaningExercise.cc
+++ PhysicsTools/PatExamples/bin/PatCleaningExercise.cc
@@ -15,28 +15,44 @@
 #include "FWCore/PythonParameterSet/interface/PythonProcessDesc.h"
 
 
-using namespace std;
-using namespace reco;
-using namespace pat;
+//using namespace std;
+//using namespace reco;
+//using namespace pat;
 
 
 int main(int argc, char *argv[]){
+  // ----------------------------------------------------------------------
+  // First Part: 
+  //
+  //  * enable the AutoLibraryLoader 
+  //  * book the histograms of interest 
+  //  * open the input file
+  // ----------------------------------------------------------------------
+
   // load framework libraries
   gSystem->Load( "libFWCoreFWLite" );
   AutoLibraryLoader::enable();
+
+  // only allow one argument for this simple example which should be the
+  // the python cfg file
+  if ( argc < 2 ) {
+    std::cout << "Usage : " << argv[0] << " [parameters.py]" << std::endl;
+    return 0;
+  }
 
   // get the python configuration
   PythonProcessDesc builder(argv[1]);
   const edm::ParameterSet& fwliteParameters = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("FWLiteParams");
 
   // now get each parameter
-  std::string   input_   ( fwliteParameters.getParameter<std::string  >("input"   ) );
+  std::string   input_   ( fwliteParameters.getParameter<std::string  >("inputFile"  ) );
+  std::string   output_  ( fwliteParameters.getParameter<std::string  >("outputFile" ) );
   std::string   overlaps_( fwliteParameters.getParameter<std::string  >("overlaps") );
-  edm::InputTag jets_    ( fwliteParameters.getParameter<edm::InputTag>("jetSrc"  ) );
+  edm::InputTag jets_    ( fwliteParameters.getParameter<edm::InputTag>("jets"  ) );
 
   // book a set of histograms
-  fwlite::TFileService fs = fwlite::TFileService("analyzePatBasics.root");
-  TFileDirectory theDir = fs.mkdir("analyzePatCleaning.root");
+  fwlite::TFileService fs = fwlite::TFileService(output_.c_str());
+  TFileDirectory theDir = fs.mkdir("analyzePatCleaning");
   TH1F* emfAllJets_    = theDir.make<TH1F>("emfAllJets"    , "f_{emf}(All Jets)"    ,  20,  0.,  1.);
   TH1F* emfCleanJets_  = theDir.make<TH1F>("emfCleanJets"  , "f_{emf}(Clean Jets)"  ,  20,  0.,  1.);
   TH1F* emfOverlapJets_= theDir.make<TH1F>("emfOverlapJets", "f_{emf}(Overlap Jets)",  20,  0.,  1.);
@@ -47,11 +63,24 @@ int main(int argc, char *argv[]){
   // open input file (can be located on castor)
   TFile* inFile = TFile::Open(input_.c_str());
 
+  // ----------------------------------------------------------------------
+  // Second Part: 
+  //
+  //  * loop the events in the input file 
+  //  * receive the collections of interest via fwlite::Handle
+  //  * fill the histograms
+  //  * after the loop close the input file
+  // ----------------------------------------------------------------------
+
   // loop the events
   unsigned int iEvent=0;
   fwlite::Event ev(inFile); 
-  for(ev.toBegin(); !ev.atEnd(); ++ev){
+  for(ev.toBegin(); !ev.atEnd(); ++ev, ++iEvent){
     edm::EventBase const & event = ev;
+
+    // break loop after end of file is reached 
+    // or after 1000 events have been processed
+    if( iEvent==1000 ) break;
 
     // simple event counter
     if(iEvent>0 && iEvent%1==0){
@@ -63,8 +92,8 @@ int main(int argc, char *argv[]){
     event.getByLabel(jets_, jets);
 
     // loop over the jets in the event
-    for( vector<pat::Jet>::const_iterator jet = jets->begin(); jet != jets->end(); jet++ ){
-      if(jet->pt() > 20){
+    for( std::vector<pat::Jet>::const_iterator jet = jets->begin(); jet != jets->end(); jet++ ){
+      if(jet->pt()>20 && jet==jets->begin()){
 	emfAllJets_->Fill( jet->emEnergyFraction() );
 	if(! jet->hasOverlaps(overlaps_)){
 	  emfCleanJets_->Fill( jet->emEnergyFraction() );
