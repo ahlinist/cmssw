eb6a82f8b05a04f67d456e8e13c66f1adc599b6c
rwolf <>
2010-07-28 14:52:11 +0000

    put the Tutorial exercise back in place ;-)...

--- PhysicsTools/PatExamples/bin/PatBasicFWLiteAnalyzer.cc
+++ PhysicsTools/PatExamples/bin/PatBasicFWLiteAnalyzer.cc
@@ -12,42 +12,58 @@
 
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/FWLite/interface/Event.h"
-#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "FWCore/ParameterSet/interface/ProcessDesc.h"
 #include "FWCore/FWLite/interface/AutoLibraryLoader.h"
 #include "PhysicsTools/FWLite/interface/TFileService.h"
+#include "FWCore/PythonParameterSet/interface/PythonProcessDesc.h"
 
-int main(int argc, char* argv[])
+
+int main(int argc, char* argv[]) 
 {
   // ----------------------------------------------------------------------
-  // First Part:
+  // First Part: 
   //
-  //  * enable the AutoLibraryLoader
-  //  * book the histograms of interest
+  //  * enable the AutoLibraryLoader 
+  //  * book the histograms of interest 
   //  * open the input file
   // ----------------------------------------------------------------------
-
-  if ( argc < 4 ) return 0;
 
   // load framework libraries
   gSystem->Load( "libFWCoreFWLite" );
   AutoLibraryLoader::enable();
- 
+
+  // only allow one argument for this simple example which should be the
+  // the python cfg file
+  if ( argc < 2 ) {
+    std::cout << "Usage : " << argv[0] << " [parameters.py]" << std::endl;
+    return 0;
+  }
+
+  // get the python configuration
+  PythonProcessDesc builder(argv[1]);
+  const edm::ParameterSet& fwliteParameters = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("FWLiteParams");
+
+  // now get each parameter
+  std::string   input_ ( fwliteParameters.getParameter<std::string  >("inputFile"  ) );
+  std::string   output_( fwliteParameters.getParameter<std::string  >("outputFile" ) );
+  edm::InputTag muons_ ( fwliteParameters.getParameter<edm::InputTag>("muons"  ) );
+
+  
   // book a set of histograms
-  fwlite::TFileService fs = fwlite::TFileService(argv[2]);
+  fwlite::TFileService fs = fwlite::TFileService(output_.c_str());
   TFileDirectory theDir = fs.mkdir("analyzeBasicPat");
-  TH1F* jetPt_  = theDir.make<TH1F>("jetPt", "pt",    100,  0.,300.);
-  TH1F* jetEta_ = theDir.make<TH1F>("jetEta","eta",   100, -3.,  3.);
-  TH1F* jetPhi_ = theDir.make<TH1F>("jetPhi","phi",   100, -5.,  5.);
-  TH1F* disc_   = theDir.make<TH1F>("disc", "Discriminant", 100, 0.0, 10.0);
-  TH1F* constituentPt_ = theDir.make<TH1F>("constituentPt", "Constituent pT", 100, 0, 300.0);
- 
+  TH1F* muonPt_  = theDir.make<TH1F>("muonPt", "pt",    100,  0.,300.);
+  TH1F* muonEta_ = theDir.make<TH1F>("muonEta","eta",   100, -3.,  3.);
+  TH1F* muonPhi_ = theDir.make<TH1F>("muonPhi","phi",   100, -5.,  5.);  
+  
   // open input file (can be located on castor)
-  TFile* inFile = TFile::Open( argv[1] );
+  TFile* inFile = TFile::Open(input_.c_str());
 
   // ----------------------------------------------------------------------
-  // Second Part:
+  // Second Part: 
   //
-  //  * loop the events in the input file
+  //  * loop the events in the input file 
   //  * receive the collections of interest via fwlite::Handle
   //  * fill the histograms
   //  * after the loop close the input file
@@ -59,51 +75,37 @@ int main(int argc, char* argv[])
   for(ev.toBegin(); !ev.atEnd(); ++ev, ++iEvent){
     edm::EventBase const & event = ev;
 
-    // break loop after end of file is reached
+    // break loop after end of file is reached 
     // or after 1000 events have been processed
     if( iEvent==1000 ) break;
-   
+    
     // simple event counter
     if(iEvent>0 && iEvent%1==0){
       std::cout << "  processing event: " << iEvent << std::endl;
     }
 
-    // Handle to the jet collection
-    edm::Handle<std::vector<pat::Jet> > jets;
-    edm::InputTag jetLabel( argv[3] );
-    event.getByLabel(jetLabel, jets);
-   
-    // loop jet collection and fill histograms
-    for(unsigned i=0; i<jets->size(); ++i){
-      jetPt_ ->Fill( (*jets)[i].pt()  );
-      jetEta_->Fill( (*jets)[i].eta() );
-      jetPhi_->Fill( (*jets)[i].phi() );
-      reco::SecondaryVertexTagInfo const * svTagInfos = (*jets)[i].tagInfoSecondaryVertex("secondaryVertex");
-      if ( svTagInfos != 0 ) {
-    if ( svTagInfos->nVertices() > 0 )
-      disc_->Fill( svTagInfos->flightDistance(0).value() );
-      }
-      std::vector<CaloTowerPtr> const & caloConstituents =  (*jets)[i].getCaloConstituents();
-      for ( std::vector<CaloTowerPtr>::const_iterator ibegin = caloConstituents.begin(),
-          iend = caloConstituents.end(),
-          iconstituent = ibegin;
-        iconstituent != iend; ++iconstituent ) {
-    constituentPt_->Fill( (*iconstituent)->pt() );
-      }
+    // Handle to the muon collection
+    edm::Handle<std::vector<pat::Muon> > muons;
+    event.getByLabel(muons_, muons);
+    
+    // loop muon collection and fill histograms
+    for(unsigned i=0; i<muons->size(); ++i){
+      muonPt_ ->Fill( (*muons)[i].pt()  );
+      muonEta_->Fill( (*muons)[i].eta() );
+      muonPhi_->Fill( (*muons)[i].phi() );
     }
-  } 
+  }  
   // close input file
   inFile->Close();
 
   // ----------------------------------------------------------------------
-  // Third Part:
+  // Third Part: 
   //
   //  * never forget to free the memory of objects you created
   // ----------------------------------------------------------------------
 
-  // in this example there is nothing to do
- 
+  // in this example there is nothing to do 
+  
   // that's it!
   return 0;
 }
-
