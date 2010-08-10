9e1a4ad5eff483b5bf1207d8d32f4d3a0aa4f982
friis <>
2010-08-10 13:47:27 +0000

    Slowing updating this...

--- TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
@@ -11,22 +11,22 @@ from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_10TeV_cfi import *
 # NOTE: outputFilePath must **not** contain "/castor/cern.ch" !!
 #       (internally added by crab)
 #
-outputFilePath = "/user/v/veelken/plots/ZtoMuTau_grid/"
+outputFilePath = "/user/f/friis/ZtoMuTau_grid/"
 #outputFilePath = "/user/l/lusito/ZMuTauAnalysis/"
 
 # small cmsRun job for testing purposes...
 submitToGrid(configFile = "runZtoMuTau_cfg.py", channel = "ZtoMuTau",
-             sample = "Ztautau_10TeV", job = "03092010",
+             sample = "Ztautau_10TeV", job = "03092010", type='mc',
              dbs_name = "/Ztautau/sarkar-diTauSkim_CMSSW314_Summer09-MC_31X_V3-v1_RECO-7645e7582a8ca89f38eb471c03cbf450/USER",
              dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_local_09/servlet/DBSServlet",             
              replFunction = makeReplacementsCrab, replacements =
              "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false; estimateSysUncertainties = false",
              outputFileNames = [ plotsOutputFileNameZtoMuTau_Ztautau_10TeV ], outputFilePath = outputFilePath)
 
-submitToGrid(configFile = "producePatTuple_cfg.py", channel = "ZtoMuTau",
-             sample = "Ztautau_10TeV", job = "03092010",
-             dbs_name = "/Ztautau/sarkar-diTauSkim_CMSSW314_Summer09-MC_31X_V3-v1_RECO-7645e7582a8ca89f38eb471c03cbf450/USER",
-             dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_local_09/servlet/DBSServlet",             
-             replFunction = makeReplacementsCrab, replacements =
-             "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false; estimateSysUncertainties = false",
-             outputFileNames = [ patTupleOutputFileNameZtoMuTau_Ztautau_10TeV ], outputFilePath = outputFilePath)
+#submitToGrid(configFile = "producePatTuple_cfg.py", channel = "ZtoMuTau",
+#             sample = "Ztautau_10TeV", job = "03092010", type='mc',
+#             dbs_name = "/Ztautau/sarkar-diTauSkim_CMSSW314_Summer09-MC_31X_V3-v1_RECO-7645e7582a8ca89f38eb471c03cbf450/USER",
+#             dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_local_09/servlet/DBSServlet",             
+#             replFunction = makeReplacementsCrab, replacements =
+#             "maxEvents = 100; eventsPerJob = 20000; applyFactorization = false; estimateSysUncertainties = false",
+#             outputFileNames = [ patTupleOutputFileNameZtoMuTau_Ztautau_10TeV ], outputFilePath = outputFilePath)
