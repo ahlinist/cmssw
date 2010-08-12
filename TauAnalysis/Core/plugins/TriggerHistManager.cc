294e84e0f2fe96d3aa0ef5051e04109199b36051
jkolb <>
2010-08-12 08:42:36 +0000

    added -1 bin, for triggers not defined in the event

--- TauAnalysis/Core/plugins/TriggerHistManager.cc
+++ TauAnalysis/Core/plugins/TriggerHistManager.cc
@@ -47,7 +47,7 @@ void TriggerHistManager::bookHistogramsImp()
   for ( vstring::const_iterator l1Bit = l1Bits_.begin();
 	l1Bit != l1Bits_.end(); ++l1Bit ) {
     std::string meName = std::string("Trigger").append(*l1Bit);
-    MonitorElement* me = book1D(meName, meName, 2, -0.5, 1.5);
+    MonitorElement* me = book1D(meName, meName, 2, -1.5, 1.5);
     hL1triggerBits_.insert(std::pair<std::string, MonitorElement*>(*l1Bit, me));
   }
 
@@ -55,7 +55,7 @@ void TriggerHistManager::bookHistogramsImp()
   for ( vstring::const_iterator hltPath = hltPaths_.begin();
 	hltPath != hltPaths_.end(); ++hltPath ) {
     std::string meName = std::string("Trigger").append(*hltPath);
-    MonitorElement* me = book1D(meName, meName, 2, -0.5, 1.5);
+    MonitorElement* me = book1D(meName, meName, 2, -1.5, 1.5);
     hHLTresults_.insert(std::pair<std::string, MonitorElement*>(*hltPath, me));
   }
 }
@@ -63,58 +63,62 @@ void TriggerHistManager::bookHistogramsImp()
 void TriggerHistManager::fillHistogramsImp(const edm::Event& evt, const edm::EventSetup& es, double evtWeight)
 
 {  
-  //std::cout << "<TriggerHistManager::fillHistogramsImp>:" << std::endl; 
+	//std::cout << "<TriggerHistManager::fillHistogramsImp>:" << std::endl; 
 
-//--- fill histograms for L1 trigger bits
-  if ( l1GtReadoutRecordSrc_.label() != "" && l1GtObjectMapRecordSrc_.label() != "" ) {
-    edm::Handle<L1GlobalTriggerReadoutRecord> l1GtReadoutRecord;
-    evt.getByLabel(l1GtReadoutRecordSrc_, l1GtReadoutRecord);
-    edm::Handle<L1GlobalTriggerObjectMapRecord> l1GtObjectMapRecord;
-    evt.getByLabel(l1GtObjectMapRecordSrc_, l1GtObjectMapRecord);
-    
-    DecisionWord l1GtDecision = l1GtReadoutRecord->decisionWord();
-    const std::vector<L1GlobalTriggerObjectMap>& l1GtObjectMaps = l1GtObjectMapRecord->gtObjectMap();
-    
-    for ( vstring::const_iterator l1Bit = l1Bits_.begin();
-	  l1Bit != l1Bits_.end(); ++l1Bit ) {
-      bool isMatch = false;
-      for ( std::vector<L1GlobalTriggerObjectMap>::const_iterator l1GtObjectMap = l1GtObjectMaps.begin();
-	    l1GtObjectMap != l1GtObjectMaps.end(); ++l1GtObjectMap ) {
-	std::string l1Bit_i = (*l1GtObjectMap).algoName();
-	if ( l1Bit_i == (*l1Bit) ) {
-	  int index = (*l1GtObjectMap).algoBitNumber();
-	  bool isTriggered = l1GtDecision[index];	
-	  hL1triggerBits_[*l1Bit]->Fill(isTriggered, evtWeight);
-	  isMatch = true;
+	//--- fill histograms for L1 trigger bits
+	if ( l1GtReadoutRecordSrc_.label() != "" && l1GtObjectMapRecordSrc_.label() != "" ) {
+		edm::Handle<L1GlobalTriggerReadoutRecord> l1GtReadoutRecord;
+		evt.getByLabel(l1GtReadoutRecordSrc_, l1GtReadoutRecord);
+		edm::Handle<L1GlobalTriggerObjectMapRecord> l1GtObjectMapRecord;
+		evt.getByLabel(l1GtObjectMapRecordSrc_, l1GtObjectMapRecord);
+
+		DecisionWord l1GtDecision = l1GtReadoutRecord->decisionWord();
+		const std::vector<L1GlobalTriggerObjectMap>& l1GtObjectMaps = l1GtObjectMapRecord->gtObjectMap();
+
+		for ( vstring::const_iterator l1Bit = l1Bits_.begin();
+				l1Bit != l1Bits_.end(); ++l1Bit ) {
+			bool isMatch = false;
+			for ( std::vector<L1GlobalTriggerObjectMap>::const_iterator l1GtObjectMap = l1GtObjectMaps.begin();
+					l1GtObjectMap != l1GtObjectMaps.end(); ++l1GtObjectMap ) {
+				std::string l1Bit_i = (*l1GtObjectMap).algoName();
+				if ( l1Bit_i == (*l1Bit) ) {
+					int index = (*l1GtObjectMap).algoBitNumber();
+					bool isTriggered = l1GtDecision[index];	
+					hL1triggerBits_[*l1Bit]->Fill(isTriggered, evtWeight);
+					isMatch = true;
+				}
+			}
+
+			if ( !isMatch ) {
+				// fill with -1 if L1 bit isn't in trigger record
+				hL1triggerBits_[*l1Bit]->Fill(-1, evtWeight);	
+				//edm::LogError ("TriggerHistManager::fillHistograms") << " Undefined L1 bit = " << (*l1Bit) << " --> skipping !!";
+				//continue;
+			}
+		}
 	}
-      }
-    
-      if ( !isMatch ) {
-	edm::LogError ("TriggerHistManager::fillHistograms") << " Undefined L1 bit = " << (*l1Bit) << " --> skipping !!";
-	continue;
-      }
-    }
-  }
 
 //--- fill histograms for HLT results 
-  if ( hltResultsSrc_.label() != "" ) {
-    edm::Handle<edm::TriggerResults> hltResults;
-    evt.getByLabel(hltResultsSrc_, hltResults);
+	if ( hltResultsSrc_.label() != "" ) {
+		edm::Handle<edm::TriggerResults> hltResults;
+		evt.getByLabel(hltResultsSrc_, hltResults);
 
-    const edm::TriggerNames& triggerNames = evt.triggerNames(*hltResults);
-    
-    for ( vstring::const_iterator hltPath = hltPaths_.begin();
-	  hltPath != hltPaths_.end(); ++hltPath ) {
-      unsigned int index = triggerNames.triggerIndex(*hltPath);
-      if ( index < triggerNames.size() ) {
-	bool isTriggered = ( hltResults->accept(index) ) ? true : false;
-	hHLTresults_[*hltPath]->Fill(isTriggered, evtWeight);
-      } else {
-	edm::LogError ("TriggerResultEventSelector::operator") << " Undefined HLT path = " << (*hltPath) << " --> skipping !!";
-	continue;
-      }
-    }
-  }
+		const edm::TriggerNames& triggerNames = evt.triggerNames(*hltResults);
+
+		for ( vstring::const_iterator hltPath = hltPaths_.begin();
+				hltPath != hltPaths_.end(); ++hltPath ) {
+			unsigned int index = triggerNames.triggerIndex(*hltPath);
+			if ( index < triggerNames.size() ) {
+				bool isTriggered = ( hltResults->accept(index) ) ? true : false;
+				hHLTresults_[*hltPath]->Fill(isTriggered, evtWeight);
+			} else {
+				// HLT path not present in event
+				hHLTresults_[*hltPath]->Fill(-1, evtWeight);
+				//edm::LogError ("TriggerResultEventSelector::operator") << " Undefined HLT path = " << (*hltPath) << " --> skipping !!";
+				//continue;
+			}
+		}
+	}
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
