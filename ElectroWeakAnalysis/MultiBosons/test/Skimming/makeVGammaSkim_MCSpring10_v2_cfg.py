f16a0f2101d3658449fe6deba47fb569a7e621fd
veverka <>
2010-08-12 23:41:02 +0000

    Added pythia parton shower filters to the MC skimming

--- ElectroWeakAnalysis/MultiBosons/test/Skimming/makeVGammaSkim_MCSpring10_v2_cfg.py
+++ ElectroWeakAnalysis/MultiBosons/test/Skimming/makeVGammaSkim_MCSpring10_v2_cfg.py
@@ -36,6 +36,21 @@ process.ZEEGammaPath   = cms.Path(process.patDefaultSequence * process.ZEEGammaS
 process.ZMuMuGammaPath = cms.Path(process.patDefaultSequence * process.ZMuMuGammaSequence)
 process.ZInvisibleGammaPath = cms.Path(process.patDefaultSequence * process.ZInvisibleGammaSequence)
 
+process.isLeadingPhotonPythiaPartonShowerIsr = cms.Path(
+  process.patDefaultSequence * 
+  process.pythiaPartonShowerIsrSequence
+)
+
+process.isLeadingPhotonPythiaPartonShowerFsr = cms.Path(
+  process.patDefaultSequence *
+  process.pythiaPartonShowerFsrSequence
+)
+
+process.hasPhotonCandidateNotPythiaPartonShower = cms.Path(
+  process.patDefaultSequence *
+  process.pythiaPartonShowerPhotonVeto
+)
+
 ## PAT Trigger
 process.load("PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff")
 from PhysicsTools.PatAlgos.tools.trigTools import *
@@ -46,23 +61,23 @@ switchOnTriggerMatchEmbedding(process)
 process.GlobalTag.globaltag = cms.string('START3X_V26::All')
 
 ## Input
-#fileNamePrefix = "rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_5_7/RelValZMM/GEN-SIM-RECO/START3X_V26-v1/0012/"
-fileNamePrefix = "dcap://cmsdca.fnal.gov:24137/pnfs/fnal.gov/usr/cms/WAX/11/store/mc/Spring10/ZJets-madgraph/GEN-SIM-RECO/START3X_V26_S09-v1/0013/"
-#fileList = """
-#10B71379-4549-DF11-9D80-003048D15D22.root
-#34FD3B1D-6949-DF11-9529-0018F3D09612.root
-#4C8D7358-4449-DF11-86BB-003048678A6C.root
-#B0EF52E8-4549-DF11-91D4-003048678AE4.root
-#E0784FDE-4449-DF11-AA23-003048678C9A.root
-#F6369161-4749-DF11-8D77-003048678B8E.root
-#""".split()
+fileNamePrefix = "rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_5_7/RelValZMM/GEN-SIM-RECO/START3X_V26-v1/0012/"
+#fileNamePrefix = "dcap://cmsdca.fnal.gov:24137/pnfs/fnal.gov/usr/cms/WAX/11/store/mc/Spring10/ZJets-madgraph/GEN-SIM-RECO/START3X_V26_S09-v1/0013/"
 fileList = """
-4C6A688E-3947-DF11-942E-0030487D710F.root
+10B71379-4549-DF11-9D80-003048D15D22.root
+34FD3B1D-6949-DF11-9529-0018F3D09612.root
+4C8D7358-4449-DF11-86BB-003048678A6C.root
+B0EF52E8-4549-DF11-91D4-003048678AE4.root
+E0784FDE-4449-DF11-AA23-003048678C9A.root
+F6369161-4749-DF11-8D77-003048678B8E.root
 """.split()
+#fileList = """
+#4C6A688E-3947-DF11-942E-0030487D710F.root
+#""".split()
 process.source.fileNames = [fileNamePrefix + file for file in fileList]
 
-process.maxEvents.input = -1
-# process.maxEvents = cms.untracked.PSet(output = cms.untracked.int32(100))
+# process.maxEvents.input = -1
+process.maxEvents = cms.untracked.PSet(output = cms.untracked.int32(100))
 # process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 
