0195633ae124cb3f261560b0e055667176316089
friis <>
2010-08-12 11:47:29 +0000

    HPS is in default PFTau sequence, disabling non-default sequences

--- TauAnalysis/RecoTools/python/prePatProduction_cff.py
+++ TauAnalysis/RecoTools/python/prePatProduction_cff.py
@@ -22,9 +22,6 @@ from TauAnalysis.RecoTools.recoPFTauIdentification_cfi import *
 from RecoTauTag.Configuration.RecoPFTauTag_cff import *
 #from RecoTauTag.TauTagTools.TancCVTransform_cfi import *
 
-# produce HPS taus and discriminants
-from RecoTauTag.Configuration.HPSPFTaus_cfi import *
-
 # produce tau id. efficiencies & fake-rates
 from RecoTauTag.TauAnalysisTools.PFTauEfficiencyAssociator_cfi import *
 
@@ -60,11 +57,10 @@ producePrePat = cms.Sequence(
    + recoMuonIsolation
    + redoAk5Jets
    + PFTau
-   + shrinkingConePFTauTancCVTransform
-   + associateTauFakeRates
+   #+ shrinkingConePFTauTancCVTransform
+   #+ associateTauFakeRates
    + ewkTauId
    + genParticlesForJets + ak5GenJets
    + metsignificance
    + produceEventShapeVars
-   + produceAndDiscriminateHPSPFTaus
 )
