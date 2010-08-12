636b3fe7df3ee68bcdbb1561b700ff0ca25062e5
zgecse <>
2010-08-12 18:20:56 +0000

    move HLTConfigProvider init to beginRun(), needed for 3.8.X, needs testing!

--- HeavyFlavorAnalysis/Onia2MuMu/interface/Onia2MuMu.h
+++ HeavyFlavorAnalysis/Onia2MuMu/interface/Onia2MuMu.h
@@ -9,7 +9,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Utilities/interface/EDMException.h"
@@ -99,6 +99,7 @@ class Onia2MuMu : public edm::EDAnalyzer {
       ~Onia2MuMu();
  private:
       virtual void beginJob() ;
+      virtual void beginRun(const Run & iRun, const EventSetup & iSetup);
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
       virtual void l1Report(const edm::Event &iEvent); 
