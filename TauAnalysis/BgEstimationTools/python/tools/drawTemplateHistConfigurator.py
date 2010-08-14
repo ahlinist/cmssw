9e3414d9875af5664d6c6cc3ca9bf23cbb507762
veelken <>
2010-08-14 14:32:42 +0000

    extended functionality to handle case of != 3 plots

--- TauAnalysis/BgEstimationTools/python/tools/drawTemplateHistConfigurator.py
+++ TauAnalysis/BgEstimationTools/python/tools/drawTemplateHistConfigurator.py
@@ -14,16 +14,15 @@ import copy
 
 class drawTemplateHistConfigurator(cms._ParameterTypeBase):
 
-    def __init__(self, template):
+    def __init__(self, template):        
         self.template = template
+        
         self.meNames = dict()
         self.title = dict()
+        
         self.pset = cms.PSet()
 
     def add(self, meNames, name, title):
-
-        if not len(meNames) == 3:
-            raise ValueError("'meNames' Parameter invalid, must contain exactly tree Entries !!")
         
         self.meNames[name] = meNames
         self.title[name] = title
@@ -32,9 +31,8 @@ class drawTemplateHistConfigurator(cms._ParameterTypeBase):
         for name, meNames in self.meNames.items():
             drawJob = copy.deepcopy(self.template)
 
-            drawJob.plots[0].dqmMonitorElements = cms.vstring(meNames[0])
-            drawJob.plots[1].dqmMonitorElements = cms.vstring(meNames[1])
-            drawJob.plots[2].dqmMonitorElements = cms.vstring(meNames[2])
+            for iMonitorElement in range(len(meNames)):
+                drawJob.plots[iMonitorElement].dqmMonitorElements = cms.vstring(meNames[iMonitorElement])
             drawJob.title = self.title[name]
 
             # add drawJob to configuration parameter set
