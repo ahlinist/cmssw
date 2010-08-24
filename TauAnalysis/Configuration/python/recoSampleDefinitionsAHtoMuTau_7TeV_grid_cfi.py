2f924eadd26b24a46e2020a2699b4e72abda72c2
friis <>
2010-08-24 13:37:03 +0000

    Adding data samples

--- TauAnalysis/Configuration/python/recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi.py
+++ TauAnalysis/Configuration/python/recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi.py
@@ -4,29 +4,110 @@ import TauAnalysis.DQMTools.plotterStyleDefinitions_cfi as styles
 import copy
 
 # List of samples to run in the analysis
-SAMPLES_TO_ANALYZE = ['A120', 'A130', 'A200', 'Ztautau', 'Ztautau09', 'Zmumu',
-                      'InclusivePPmuX', 'PPmuXptGt20', 'WplusJets',
-                      'TTplusJets' ] 
+SAMPLES_TO_ANALYZE = [
+    #'dataJul16',
+    'data_Mu_132440_137436_Jun14ReReco',
+    'data_Mu_137437_139558_Prompt',
+    'data_Mu_139559_140159_Jul16ReReco',
+    'data_Mu_140160_999999_Prompt',
+    'A120', 'bbA120', 
+    'A130', 'bbA130', 
+    'A200', 'bbA200', 
+    'Ztautau', 
+    'Zmumu',
+    'InclusivePPmuX', 
+    'PPmuXptGt20', 
+    'WplusJets',
+    'TTplusJets',
+] 
 
 # List of samples to include in the final level plots.  May include selections
-# from the MERGED_SAMPLES defined at the bottom.
-SAMPLES_TO_PLOT = ['A120', 'A130', 'A200', 
-                   'Ztautau', 'qcdSum', 'WplusJets', 'Zmumu', 'TTplusJets' ]
+# from the MERGE_SAMPLES defined at the bottom.
+#SAMPLES_TO_PLOT = ['A120', 'A130', 'A200', 'qcdSum', 'WplusJets', 'Zmumu', 'TTplusJets', 'Ztautau', ]
+SAMPLES_TO_PLOT = [
+    'data', 
+    'A120Sum', 
+    'A130Sum', 
+    'A200Sum', 
+    'qcdSum', 
+    'WplusJets', 
+    'Zmumu', 
+    'TTplusJets',
+    'Ztautau', 
+]
 
 
 SAMPLE_DEFAULTS = {
     'dbs_url' : "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_02/servlet/DBSServlet",
     'conditions' : 'startup',
-    'isData' : False,
     'genPhaseSpaceCut' : '',
+    'factorize' : False,
+    'lumi_mask' : '',
+    'runselection' : '',
 }
 
 # Conversions to pico barns
 _femtobarns = 1.0e-3
 _millibarns = 1.0e9
 _picobarns = 1.0
+_nanobarns = 1000.0
+_microbarns = 1.0e6
+
+# Integrated luminosity to normalize 
+TARGET_LUMI = 115785.778/_microbarns
 
 RECO_SAMPLES = {
+    'dataJul16' : {
+        'datasetpath' : "/JetMETTau/Run2010A-Jul16thReReco-v1/RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_139779-140159_7TeV_July16thReReco_Collisions10_JSON.txt",
+        'conditions' : 'GR_R_36X_V12B::All',
+        'events_processed' : 3413445,
+        'skim_eff' : 1.0,
+        'legendEntry' : 'DATA',
+        'type' : 'Data',
+        'drawOption' : styles.drawOption_Data,
+    },
+    'data_Mu_132440_137436_Jun14ReReco' : {
+        'datasetpath' : "/Mu/Run2010A-Jun14thReReco_v1/RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON.txt",
+        'runselection' : "132440 - 137436",
+        'conditions' : 'GR_R_36X_V12B::All',
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+        'drawOption' : styles.drawOption_Data,
+    },
+    'data_Mu_137437_139558_Prompt' : {
+        'datasetpath' : '/Mu/Run2010A-PromptReco-v4/RECO',
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'runselection' : '137437 - 139558',
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/StreamExpress/Cert_132440-143179_7TeV_StreamExpress_Collisions10_JSON.txt",
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+    },
+    'data_Mu_139559_140159_Jul16ReReco' : {
+        'datasetpath' : "/Mu/Run2010A-Jul16thReReco-v1/RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_139779-140159_7TeV_July16thReReco_Collisions10_JSON.txt",
+        'runselection' : "139559 - 140159",
+        'conditions' : 'GR_R_36X_V12B::All',
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+        'drawOption' : styles.drawOption_Data,
+    },
+    'data_Mu_140160_999999_Prompt' : {
+        'datasetpath' : '/Mu/Run2010A-PromptReco-v4/RECO',
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'runselection' : '140160 - 999999',
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/StreamExpress/Cert_132440-143179_7TeV_StreamExpress_Collisions10_JSON.txt",
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+    },
     'A120' : {
         # Not skimmed
         'datasetpath' : "/Atautau_M120/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO",
@@ -34,23 +115,27 @@ RECO_SAMPLES = {
         'events_processed' : 110670,
         'skim_eff' : 1.0,
         # Configuration/GenProduction/python/PYTHIA6_Tauola_gg_H120_tautau_7TeV_cff.py
-        'x_sec' : 51947.8*_femtobarns*0.122789,
-        # Feynhiggs input to calculate xsec*br
-        #MT 173.1
-        #TB 30
-        #MA0 120
-        #Abs(MUE) 200
-        #Arg(MUE) 0
-        #MSusy 1000
-        #Abs(Xt) 2000
-        #Arg(Xt) 0
-        #Abs(M_2) 200
-        #Arg(M_2) 0
-        #Abs(M_3) 800.0
-        #Arg(M_3) 0
-        #prodSqrts 7
-        # 
-        'legendEntry' : 'A(120) #rightarrow #tau^{+} #tau^{-}',
+        'x_sec' : (
+            51947.8*_femtobarns*0.122789 + # (gg -> A0)*(A0->tautau)
+            39445.6*_femtobarns*0.123462 + # (gg -> h0)*(h0->tautau)
+            20753.1*_femtobarns*0.125908   # (gg -> HH)*(HH->tautau)
+        ),
+        # Feynhiggs v2.7.1 input to calculate xsec*br -  mhmax, 7TeV
+        'legendEntry' : 'ggA(120) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : styles.drawOption_darkBlue_separate,
+    },
+    'bbA120' : {
+        'datasetpath' : "/bbAtautau_M130/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'events_processed' : 99470,
+        'skim_eff' : 1.0,
+        'x_sec' : (
+            64468.1*_femtobarns*0.122789 + # (bb -> A0)*(A0->tautau)
+            60252.8*_femtobarns*0.123462 + # (bb -> h0)*(h0->tautau)
+            5133.30*_femtobarns*0.125908   # (bb -> HH)*(HH->tautau)
+        ),
+        'legendEntry' : 'bbA(120) #rightarrow #tau^{+} #tau^{-}',
         'type' : 'bsmMC',
         'drawOption' : styles.drawOption_darkBlue_separate,
     },
@@ -61,22 +146,29 @@ RECO_SAMPLES = {
         'events_processed' : 115290,
         'skim_eff' : 1.0,
         # Configuration/GenProduction/python/PYTHIA6_Tauola_gg_H130_tautau_7TeV_cff.py
-        'x_sec' : 35094.3*_femtobarns*0.124340,
-        # Feynhiggs input to calculate xsec*br
-        #MT 173.1
-        #TB 30
-        #MA0 130
-        #Abs(MUE) 200
-        #Arg(MUE) 0
-        #MSusy 1000
-        #Abs(Xt) 2000
-        #Arg(Xt) 0
-        #Abs(M_2) 200
-        #Arg(M_2) 0
-        #Abs(M_3) 800.0
-        #Arg(M_3) 0
-        #prodSqrts 7
-        'legendEntry' : 'A(130) #rightarrow #tau^{+} #tau^{-}',
+        'x_sec' : (
+            35094.3*_femtobarns*0.124340 + # (gg -> A0)*(A0->tautau)
+            12089.4*_femtobarns*0.124263 + # (gg -> h0)*(h0->tautau)
+            30851.8*_femtobarns*0.125984   # (gg -> HH)*(HH->tautau)
+        ),
+        # Feynhiggs v2.7.1 input to calculate xsec*br -  mhmax, 7TeV
+        'legendEntry' : 'ggA(130) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : styles.drawOption_green_separate,
+    },
+    'bbA130' : {
+        # Not skimmed
+        'datasetpath' : "/Atautau_M130/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'events_processed' : 101900,
+        'skim_eff' : 1.0,
+        'x_sec' : (
+            104316*_femtobarns*0.124340 + # (bb -> A0)*(A0->tautau)
+            65638.3*_femtobarns*0.124263 + # (bb -> h0)*(h0->tautau)
+            39203.8*_femtobarns*0.125984   # (bb -> HH)*(HH->tautau)
+        ),
+        # Feynhiggs v2.7.1 input to calculate xsec*br -  mhmax, 7TeV
+        'legendEntry' : 'bbA(130) #rightarrow #tau^{+} #tau^{-}',
         'type' : 'bsmMC',
         'drawOption' : styles.drawOption_green_separate,
     },
@@ -87,24 +179,30 @@ RECO_SAMPLES = {
         'events_processed' : 111100,
         'skim_eff' : 1.0,
         # Configuration/GenProduction/python/PYTHIA6_Tauola_gg_H200_tautau_7TeV_cff.py
-        'x_sec' : 3482.30*_femtobarns*0.131356, 
-        # Feynhiggs input to calculate xsec*br
-        #MT 173.1
-        #TB 30
-        #MA0 200
-        #Abs(MUE) 200
-        #Arg(MUE) 0
-        #MSusy 1000
-        #Abs(Xt) 2000
-        #Arg(Xt) 0
-        #Abs(M_2) 200
-        #Arg(M_2) 0
-        #Abs(M_3) 800.0
-        #Arg(M_3) 0
-        #prodSqrts 7
-        'legendEntry' : 'A(200) #rightarrow #tau^{+} #tau^{-}',
+        'x_sec' : (
+            3482.30*_femtobarns*0.131356 + # (gg -> A0)*(A0->tautau)
+            #12232.9*_femtobarns*9.501279E-02 + # (gg -> h0)*(h0->tautau)
+            3902.27*_femtobarns*0.132576   # (gg -> HH)*(HH->tautau)
+        ),
+        'legendEntry' : 'ggA(200) #rightarrow #tau^{+} #tau^{-}',
         'type' : 'bsmMC',
-        'drawOption' : styles.drawOption_darkGray_stacked,
+        'drawOption' : styles.drawOption_darkGray_separate,
+    },
+    'bbA200' : {
+        # Not skimmed
+        'datasetpath' : "/bbAtautau_M200/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO",
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'events_processed' : 101360,
+        'skim_eff' : 1.0,
+        # Configuration/GenProduction/python/PYTHIA6_Tauola_gg_H200_tautau_7TeV_cff.py
+        'x_sec' : (
+            21620.8*_femtobarns*0.131356 + # (bb -> A0)*(A0->tautau)
+            #556.482*_femtobarns*9.501279E-02 + # (bb -> h0)*(h0->tautau)
+            21467.6*_femtobarns*0.132576   # (bb -> HH)*(HH->tautau)
+        ), 
+        'legendEntry' : 'bbA(200) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : styles.drawOption_darkGray_separate,
     },
     'Ztautau' : {
         'datasetpath' : "/Ztautau/akalinow-SkimTauTau_356_pass1-0a3d3891f015a95324f94837322fb8aa-muTauSkim/USER",
@@ -127,10 +225,11 @@ RECO_SAMPLES = {
         'type' : plotter.process_Ztautau.config_dqmHistPlotter.type.value(),
         'drawOption' : styles.drawOption_Ztautau,
     },
+    #    Using the unskimmed dataset
     'Zmumu' : {
-        'datasetpath' : "/Zmumu/akalinow-SkimTauTau_356_pass1-0a3d3891f015a95324f94837322fb8aa-muTauSkim/USER",
-        'events_processed' : 2015268,
-        'skim_eff' : 0.739,
+        'datasetpath' : "/Zmumu/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO",
+        'events_processed' : 2111268,
+        'skim_eff' : 1.0,
         'x_sec' : 1300*_picobarns,
         'legendEntry' : plotter.process_Zmumu.config_dqmHistPlotter.legendEntry.value(),
         'type' : plotter.process_Zmumu.config_dqmHistPlotter.type.value(),
@@ -145,15 +244,18 @@ RECO_SAMPLES = {
         'legendEntry' : plotter.process_InclusivePPmuX.config_dqmHistPlotter.legendEntry.value(),
         'type' : plotter.process_InclusivePPmuX.config_dqmHistPlotter.type.value(),
         'drawOption' : styles.drawOption_QCD,
+        'factorize' : True,
     },
+    #    Using the unskimmed dataset
     'PPmuXptGt20' : {
-        'datasetpath' : "/QCD_Pt-20_MuEnrichedPt10_7TeV-pythia6/akalinow-SkimTauTau_356_pass1-af7269240adb80f379a817aa7b511727-muTauSkim/USER",
+        'datasetpath' : "/QCD_Pt-20_MuEnrichedPt10_7TeV-pythia6/Spring10-START3X_V26-v1/GEN-SIM-RECO",
         'events_processed' : 6342864, 
-        'skim_eff' : 0.836,
+        'skim_eff' : 1.0,
         'x_sec' : 296500000*_picobarns*0.00116, # xsec (pb) * gen filter efficiency
         'legendEntry' : plotter.process_PPmuXptGt20.config_dqmHistPlotter.legendEntry.value(),
         'type' : plotter.process_PPmuXptGt20.config_dqmHistPlotter.type.value(),
         'drawOption' : styles.drawOption_QCD,
+        'factorize' : True,
     },
     'WplusJets' : {
         'datasetpath' : "/WJets-madgraph/akalinow-SkimTauTau_356_pass1-0a3d3891f015a95324f94837322fb8aa-muTauSkim/USER",
@@ -177,11 +279,40 @@ RECO_SAMPLES = {
 
 # Define samples that get merged together
 MERGE_SAMPLES = {
+    'data' : {
+        'samples' : [
+            'data_Mu_132440_137436_Jun14ReReco',
+            'data_Mu_137437_139558_Prompt',
+            'data_Mu_139559_140159_Jul16ReReco',
+            'data_Mu_140160_999999_Prompt',
+        ],
+        'legendEntry' : 'DATA',
+        'type' : 'smMC',
+        'drawOption' : styles.drawOption_Data
+    },
     'qcdSum' : {
         'samples' : ['InclusivePPmuX', 'PPmuXptGt20'],
         'legendEntry' : 'QCD',
         'type' : 'smMC', 
         'drawOption' : styles.drawOption_QCD,
+    },
+    'A120Sum' : {
+        'samples' : ['A120', 'bbA120'],
+        'legendEntry' : 'A(120) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : RECO_SAMPLES['A120']['drawOption'],
+    },
+    'A130Sum' : {
+        'samples' : ['A130', 'bbA130'],
+        'legendEntry' : 'A(130) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : RECO_SAMPLES['A130']['drawOption'],
+    },
+    'A200Sum' : {
+        'samples' : ['A200', 'bbA200'],
+        'legendEntry' : 'A(200) #rightarrow #tau^{+} #tau^{-}',
+        'type' : 'bsmMC',
+        'drawOption' : RECO_SAMPLES['A200']['drawOption'],
     },
     'smBgSum' : {
         'samples' : ['Ztautau', 'Zmumu', 'InclusivePPmuX', 
@@ -192,8 +323,19 @@ MERGE_SAMPLES = {
     }
 }
 
-ALL_SAMPLES = {}
 
+# List of all subsamples used in any plot job.  i.e. if qcdSum is included in
+# samples to plot it will be expanded to the inclusive/exclusive ppMux samples
+FLATTENED_SAMPLES_TO_PLOT = []
+
+for sample in SAMPLES_TO_PLOT:
+    if sample in MERGE_SAMPLES:
+        for subsample in MERGE_SAMPLES[sample]['samples']:
+            FLATTENED_SAMPLES_TO_PLOT.append(subsample)
+    else:
+        FLATTENED_SAMPLES_TO_PLOT.append(sample)
+
+ALL_SAMPLES = {}
 # Update to use the defaults if necessary
 for sample in RECO_SAMPLES.keys():
     defaults = copy.copy(SAMPLE_DEFAULTS)
@@ -203,4 +345,8 @@ for sample in RECO_SAMPLES.keys():
     # for simple access 
     ALL_SAMPLES.update(MERGE_SAMPLES)
     ALL_SAMPLES.update(RECO_SAMPLES)
+
+
+
+
 
