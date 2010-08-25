49ce63202d80561a87644124216b0f04333d4a28
hcheung <>
2010-08-25 19:29:09 +0000

    Analysis of secondary vertices reconstructed with btag package

--- SLHCUpgradeSimulations/Validation/src/SealModule.cc
+++ SLHCUpgradeSimulations/Validation/src/SealModule.cc
@@ -1,12 +1,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPrimaryVertexAnalyzer.h"
-#include "DQM/SiStripCommon/interface/SiStripFolderOrganizer.h"
-#include "DataFormats/MuonReco/interface/Muon.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPixelHitAnalyzer.h"
+#include "SLHCUpgradeSimulations/Validation/interface/SLHCAllPixelHitAnalyzer.h"
+#include "SLHCUpgradeSimulations/Validation/interface/SLHCrecoBSVTagInfoValidationAnalyzer.h"
 
 using namespace edm;
 using namespace reco;
 
 DEFINE_FWK_MODULE(SLHCPrimaryVertexAnalyzer);
 DEFINE_FWK_MODULE(SLHCPixelHitAnalyzer);
+DEFINE_FWK_MODULE(SLHCAllPixelHitAnalyzer);
+DEFINE_FWK_MODULE(SLHCrecoBSVTagInfoValidationAnalyzer);
