3984e6a5e317bd20dff7b789cc4a402cd2ef8a8c
veelken <>
2010-08-27 07:39:13 +0000

    first check-in of auxiliary functions for new SVfit algorithm

---
+++ TauAnalysis/CandidateTools/interface/svFitAuxFunctions.h
@@ -0,0 +1,8 @@
+#ifndef TauAnalysis_CandidateTools_svFitAuxFunctions_h
+#define TauAnalysis_CandidateTools_svFitAuxFunctions_h
+
+double pVisRestFrame(double, double);
+
+reco::Candidate::Point operator+(const reco::Candidate::Point&, const reco::Candidate::Point&);
+
+#endif
