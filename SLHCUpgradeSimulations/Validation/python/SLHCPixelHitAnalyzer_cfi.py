b64c11484b1e5446a6c9926432709b249bcc74d3
hcheung <>
2010-08-28 03:41:43 +0000

    Add branch for matched simTrack/TrackingParticle for track only tree so we can look at track-related resolutions like IP resolutions

--- SLHCUpgradeSimulations/Validation/python/SLHCPixelHitAnalyzer_cfi.py
+++ SLHCUpgradeSimulations/Validation/python/SLHCPixelHitAnalyzer_cfi.py
@@ -1,12 +1,19 @@
 import FWCore.ParameterSet.Config as cms
+from SimTracker.TrackAssociation.LhcParametersDefinerForTP_cfi import *
 
 SLHCPixelHitAnalyzer = cms.EDFilter("SLHCPixelHitAnalyzer",
     trajectoryInput = cms.string('TrackRefitter'),
     useAllPixel = cms.bool(False),
-    isCosmic = cms.bool(True),
-    isSim = cms.bool(False),
+    isCosmic = cms.bool(False),
+    isSim = cms.bool(True),
+    trackProducer = cms.InputTag("TrackRefitter"),
+    associator = cms.string('TrackAssociatorByHits'),
+    label_tp = cms.InputTag("mergedtruth","MergedTrackTruth"),
+    # collision like tracks
+    parametersDefiner = cms.string('LhcParametersDefinerForTP'),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
 
-# should be moved to TFileService.                                       
+# moved to TFileService.                                       
     OutputFile = cms.string('TTreeFile.root') 
 )
 
