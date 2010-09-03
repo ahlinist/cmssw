c234f517f27f252df08c6b3c9fdb6911e43690f4
cmkuo <>
2010-09-03 13:33:05 +0000

    trigger matcher for Vg group

---
+++ ElectroWeakAnalysis/MultiBosons/python/VgTriggerMatcher_cfi.py
@@ -0,0 +1,113 @@
+import FWCore.ParameterSet.Config as cms
+
+# matches to HLT_Mu9
+muonTriggerMatchHLTMu9 = cms.EDFilter("PATTriggerMatcherDRDPtLessByR",
+                                      src     = cms.InputTag( "cleanPatMuons" ),
+                                      matched = cms.InputTag( "patTrigger" ),
+                                      andOr          = cms.bool( False ),
+                                      filterIdsEnum  = cms.vstring( '*' ),
+                                      filterIds      = cms.vint32( 0 ),
+                                      filterLabels   = cms.vstring( '*' ),
+                                      pathNames      = cms.vstring( 'HLT_Mu9' ),
+                                      collectionTags = cms.vstring( '*' ),
+                                      maxDPtRel = cms.double( 0.5 ),
+                                      maxDeltaR = cms.double( 0.5 ),
+                                      resolveAmbiguities    = cms.bool( True ),
+                                      resolveByMatchQuality = cms.bool( False )
+                                      )
+
+# matches to HLT_Ele15_LW_L1R
+electronTriggerMatchHLTEle15LWL1R = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+                                                  src     = cms.InputTag( "cleanPatElectrons" ),
+                                                  matched = cms.InputTag( "patTrigger" ),
+                                                  andOr          = cms.bool( False ),
+                                                  filterIdsEnum  = cms.vstring( '*' ),
+                                                  filterIds      = cms.vint32( 0 ),
+                                                  filterLabels   = cms.vstring( '*' ),
+                                                  pathNames      = cms.vstring( 'HLT_Ele15_LW_L1R' ),
+                                                  collectionTags = cms.vstring( '*' ),
+                                                  maxDPtRel = cms.double( 0.5 ),
+                                                  maxDeltaR = cms.double( 0.5 ),
+                                                  resolveAmbiguities    = cms.bool( True ),
+                                                  resolveByMatchQuality = cms.bool( False )
+                                                  )
+
+# matches to HLT_Ele15_SW_L1R
+electronTriggerMatchHLTEle15SWL1R = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+                                                  src     = cms.InputTag( "cleanPatElectrons" ),
+                                                  matched = cms.InputTag( "patTrigger" ),
+                                                  andOr          = cms.bool( False ),
+                                                  filterIdsEnum  = cms.vstring( '*' ),
+                                                  filterIds      = cms.vint32( 0 ),
+                                                  filterLabels   = cms.vstring( '*' ),
+                                                  pathNames      = cms.vstring( 'HLT_Ele15_SW_L1R' ),
+                                                  collectionTags = cms.vstring( '*' ),
+                                                  maxDPtRel = cms.double( 0.5 ),
+                                                  maxDeltaR = cms.double( 0.5 ),
+                                                  resolveAmbiguities    = cms.bool( True ),
+                                                  resolveByMatchQuality = cms.bool( False )
+                                                  )
+
+# matches to HLT_Ele20_SW_L1R
+electronTriggerMatchHLTEle20SWL1R = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+                                                  src     = cms.InputTag( "cleanPatElectrons" ),
+                                                  matched = cms.InputTag( "patTrigger" ),
+                                                  andOr          = cms.bool( False ),
+                                                  filterIdsEnum  = cms.vstring( '*' ),
+                                                  filterIds      = cms.vint32( 0 ),
+                                                  filterLabels   = cms.vstring( '*' ),
+                                                  pathNames      = cms.vstring( 'HLT_Ele20_SW_L1R' ),
+                                                  collectionTags = cms.vstring( '*' ),
+                                                  maxDPtRel = cms.double( 0.5 ),
+                                                  maxDeltaR = cms.double( 0.5 ),
+                                                  resolveAmbiguities    = cms.bool( True ),
+                                                  resolveByMatchQuality = cms.bool( False )
+                                                  )
+
+# matches to HLT_Ele15_SW_EleId_L1R
+electronTriggerMatchHLTEle15SWEleIdL1R = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+                                                  src     = cms.InputTag( "cleanPatElectrons" ),
+                                                  matched = cms.InputTag( "patTrigger" ),
+                                                  andOr          = cms.bool( False ),
+                                                  filterIdsEnum  = cms.vstring( '*' ),
+                                                  filterIds      = cms.vint32( 0 ),
+                                                  filterLabels   = cms.vstring( '*' ),
+                                                  pathNames      = cms.vstring( 'HLT_Ele15_SW_EleId_L1R' ),
+                                                  collectionTags = cms.vstring( '*' ),
+                                                  maxDPtRel = cms.double( 0.5 ),
+                                                  maxDeltaR = cms.double( 0.5 ),
+                                                  resolveAmbiguities    = cms.bool( True ),
+                                                  resolveByMatchQuality = cms.bool( False )
+                                                  )
+
+# matches to HLT_Ele15_SW_CaloEleId_L1R
+electronTriggerMatchHLTEle15SWCaloEleIdL1R = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+                                                           src     = cms.InputTag( "cleanPatElectrons" ),
+                                                           matched = cms.InputTag( "patTrigger" ),
+                                                           andOr          = cms.bool( False ),
+                                                           filterIdsEnum  = cms.vstring( '*' ),
+                                                           filterIds      = cms.vint32( 0 ),
+                                                           filterLabels   = cms.vstring( '*' ),
+                                                           pathNames      = cms.vstring( 'HLT_Ele15_SW_CaloEleId_L1R' ),
+                                                           collectionTags = cms.vstring( '*' ),
+                                                           maxDPtRel = cms.double( 0.5 ),
+                                                           maxDeltaR = cms.double( 0.5 ),
+                                                           resolveAmbiguities    = cms.bool( True ),
+                                                           resolveByMatchQuality = cms.bool( False )
+                                                           )
+
+vgTriggerMatcherElectron = cms.Sequence(
+        electronTriggerMatchHLTEle15LWL1R +
+        electronTriggerMatchHLTEle15SWL1R +
+        electronTriggerMatchHLTEle20SWL1R +
+        electronTriggerMatchHLTEle15SWEleIdL1R +
+        electronTriggerMatchHLTEle15SWCaloEleIdL1R
+        )
+
+vgTriggerMatcherMuon = cms.Sequence(
+    muonTriggerMatchHLTMu9     
+    )
+
+vgTriggerMatcher = cms.Sequence(
+    vgTriggerMatcherElectron + vgTriggerMatcherMuon    
+    )
