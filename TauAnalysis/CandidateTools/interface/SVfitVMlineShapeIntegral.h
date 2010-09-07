7c890ff6b46548e71a1ff8e23d915f56780065d5
veelken <>
2010-09-07 15:50:14 +0000

    first check-in of top-level class for computing vector meson line-shape integral

---
+++ TauAnalysis/CandidateTools/interface/SVfitVMlineShapeIntegral.h
@@ -0,0 +1,51 @@
+#ifndef TauAnalysis_CandidateTools_SVfitVMlineShapeIntegral_h
+#define TauAnalysis_CandidateTools_SVfitVMlineShapeIntegral_h
+
+/** \class SVfitVMlineShapeIntegral
+ *
+ * Auxiliary class for computing decay distributions for polarized tau leptons
+ * averaged over vector-meson (either rho or a1) line-shape factors
+ *
+ * The class implements formulas taken from the papers
+ *  [1] "Tau polarization and its correlations as a probe of new physics",
+ *      B.K. Bullock, K. Hagiwara and A.D. Martin,
+ *      Nucl. Phys. B395 (1993) 499.
+ *     (formulas 2.16 to 2.25)
+ *  [2] "Charged Higgs boson search at the TeVatron upgrade using tau polarization",
+ *      S. Raychaudhuri and D.P. Roy,
+ *      Phys. Rev.  D52 (1995) 1556.           
+ *     (formulas 32 to 38)
+ *
+ * \author Christian Veelken, UC Davis
+ *
+ * \version $Revision: 1.1 $
+ *
+ * $Id: SVfitVMlineShapeIntegral.h,v 1.1 2010/09/07 13:42:04 veelken Exp $
+ *
+ */
+
+#include "TauAnalysis/CandidateTools/interface/SVfitVMlineShapeIntegrand.h"
+
+#include <Math/Integrator.h>
+
+class SVfitVMlineShapeIntegral
+{
+ public:
+  SVfitVMlineShapeIntegral(SVfitVMlineShapeIntegrand::VMtype, SVfitVMlineShapeIntegrand::VMpol, bool);
+  ~SVfitVMlineShapeIntegral();
+
+  double operator()(double theta, double tauLeptonPol, double z) const;
+
+ private:
+  ROOT::Math::Integrator* integrator_;
+  mutable SVfitVMlineShapeIntegrand integrand_;
+
+//--- temporary variables to speed-up computations
+//    (computed once in constructor)
+  double minMass2_;
+  double norm_;
+
+  static const unsigned int debugLevel_ = 0;
+};
+
+#endif
