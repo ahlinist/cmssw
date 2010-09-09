6fd8d2e5f37d2207cf34e63ac904e10ed5b1c48b
kkousour <>
2010-09-09 10:16:34 +0000

    Minor changes

--- QCDAnalysis/HighPtJetAnalysis/test/InclusiveJetTreeProducer_data_cfg.py
+++ QCDAnalysis/HighPtJetAnalysis/test/InclusiveJetTreeProducer_data_cfg.py
@@ -11,7 +11,7 @@ process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_
 process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
 #############   Set the number of events #############
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(1000)
 )
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
@@ -37,30 +37,27 @@ process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
 #############   inclusive jet tree producer ##################
 process.TFileService = cms.Service("TFileService",fileName = cms.string('InclusiveJetTree.root'))
 
-process.ak5calo = cms.EDAnalyzer('InclusiveJetTreeProducer',
-    pfjets                 = cms.string('ak5PFJets'),
-    jets                   = cms.string('ak5CaloJets'), 
-    jetsID                 = cms.string('ak5JetID'),
-    jetExtender            = cms.string('ak5JetExtender'),
+process.ak7calo = cms.EDAnalyzer('InclusiveJetTreeProducer',
+    pfjets                 = cms.string('ak7PFJets'),
+    jets                   = cms.string('ak7CaloJets'), 
+    jetsID                 = cms.string('ak7JetID'),
+    jetExtender            = cms.string('ak7JetExtender'),
     met                    = cms.string('met'),
     metNoHF                = cms.string('metNoHF'),
     hcalNoiseTag           = cms.InputTag('hcalnoise'),
-    minJetPt               = cms.double(4.0), 
-    minPFJetPt             = cms.double(4.0),
-    jetTriggerNames        = cms.vstring('HLT_L1Jet6U','HLT_Jet15U','HLT_Jet30U','HLT_Jet50U'),
+    minJetPt               = cms.double(6.0), 
+    minPFJetPt             = cms.double(15.0),
+    jetTriggerNames        = cms.vstring('HLT_Jet15U','HLT_Jet30U','HLT_Jet50U'),
     triggerResultsTag      = cms.InputTag('TriggerResults','','HLT'),
     l1TriggerNames         = cms.vstring(),     
     L1GTReadoutRcdSource   = cms.InputTag('gtDigis'),
     L1GTObjectMapRcdSource = cms.InputTag('hltL1GtObjectMap','','HLT')
 )
 
-process.ak7calo = process.ak5calo.clone(pfjets = 'ak7PFJets',jets = 'ak7CaloJets', jetsID = 'ak7JetID', jetExtender = 'ak7JetExtender')
-
 process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
 process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND NOT (36 OR 37 OR 38 OR 39)')
 
-process.p = cms.Path(process.hltLevel1GTSeed * process.primaryVertexFilter * process.monster * process.trackExtrapolator *  process.ak7JTA 
-* process.ak7calo)
+process.p = cms.Path(process.hltLevel1GTSeed * process.primaryVertexFilter * process.trackExtrapolator *  process.ak7JTA * process.ak7calo)
 #############   Format MessageLogger #################
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
