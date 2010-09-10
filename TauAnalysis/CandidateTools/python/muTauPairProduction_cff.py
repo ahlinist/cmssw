84ccbe82f38f64c6b88a4ed6965575f21281c309
veelken <>
2010-09-10 12:24:58 +0000

    added likelihoods for polarization + MET, polarization + MET + diTauPtBalance

--- TauAnalysis/CandidateTools/python/muTauPairProduction_cff.py
+++ TauAnalysis/CandidateTools/python/muTauPairProduction_cff.py
@@ -49,31 +49,51 @@ allMuTauPairs = cms.EDProducer("PATMuTauPairProducer",
                 numSamplings = cms.int32(-1)
             )
         ),
-        ##psKine_MEt = cms.PSet(
-        ##    likelihoodFunctions = cms.VPSet(
-        ##        svFitLikelihoodDiTauKinematicsPhaseSpace,
-        ##        svFitLikelihoodMEt
-        ##    ),
-        ##    estUncertainties = cms.PSet(
-        ##        numSamplings = cms.int32(-1)
-        ##    )
-        ##),
-        ##psKine_MEt_ptBalance = cms.PSet(
-        ##    likelihoodFunctions = cms.VPSet(
-        ##        svFitLikelihoodDiTauKinematicsPhaseSpace,
-        ##        svFitLikelihoodMEt,
-        ##        svFitLikelihoodDiTauPtBalance
-        ##    ),
-        ##    estUncertainties = cms.PSet(
-        ##        #numSamplings = cms.int32(1000)
-        ##        numSamplings = cms.int32(-1)
-        ##    )
-        ##),
+        psKine_MEt = cms.PSet(
+            likelihoodFunctions = cms.VPSet(
+                svFitLikelihoodDiTauKinematicsPhaseSpace,
+                svFitLikelihoodMEt
+            ),
+            estUncertainties = cms.PSet(
+                numSamplings = cms.int32(-1)
+            )
+        ),
+        psKine_MEt_ptBalance = cms.PSet(
+            likelihoodFunctions = cms.VPSet(
+                svFitLikelihoodDiTauKinematicsPhaseSpace,
+                svFitLikelihoodMEt,
+                svFitLikelihoodDiTauPtBalance
+            ),
+            estUncertainties = cms.PSet(
+                #numSamplings = cms.int32(1000)
+                numSamplings = cms.int32(-1)
+            )
+        ),
         polKine = cms.PSet(
             likelihoodFunctions = cms.VPSet(
                 svFitLikelihoodDiTauKinematicsPolarized        
             ),
             estUncertainties = cms.PSet(
+                numSamplings = cms.int32(-1)
+            )
+        ),
+        polKine_MEt = cms.PSet(
+            likelihoodFunctions = cms.VPSet(
+                svFitLikelihoodDiTauKinematicsPolarized,
+                svFitLikelihoodMEt
+            ),
+            estUncertainties = cms.PSet(
+                numSamplings = cms.int32(-1)
+            )
+        ),
+        polKine_MEt_ptBalance = cms.PSet(
+            likelihoodFunctions = cms.VPSet(
+                svFitLikelihoodDiTauKinematicsPolarized,
+                svFitLikelihoodMEt,
+                svFitLikelihoodDiTauPtBalance
+            ),
+            estUncertainties = cms.PSet(
+                #numSamplings = cms.int32(1000)
                 numSamplings = cms.int32(-1)
             )
         )
