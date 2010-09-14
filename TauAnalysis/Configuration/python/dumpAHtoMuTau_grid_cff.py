80c157a94b78f88bba44fc1c98e7c6d3d794c5d4
friis <>
2010-09-14 17:44:51 +0000

    Make specification more flexible

--- TauAnalysis/Configuration/python/dumpAHtoMuTau_grid_cff.py
+++ TauAnalysis/Configuration/python/dumpAHtoMuTau_grid_cff.py
@@ -21,7 +21,7 @@ dumpAHtoMuTau_woBtag = cms.EDAnalyzer(
             (sample, cms.string(
                 '/harvested/%s/ahMuTauAnalyzer_woBtag/FilterStatistics' 
                 % sample_dqm_name(sample))
-            ) for sample in samples.SAMPLES_TO_PLOT)
+            ) for sample in samples.SAMPLES_TO_PRINT)
     ), 
     columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
 )
@@ -33,7 +33,7 @@ dumpAHtoMuTau_wBtag = cms.EDAnalyzer(
             (sample, cms.string(
                 '/harvested/%s/ahMuTauAnalyzer_wBtag/FilterStatistics' 
                 % sample_dqm_name(sample))
-            ) for sample in samples.SAMPLES_TO_PLOT)
+            ) for sample in samples.SAMPLES_TO_PRINT)
     ),
     columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
 )
