38dfeae84d44f2d884115ba879d5c233d67fd7b4
veverka <>
2010-09-20 16:21:04 +0000

    Renamed pi0DiscValMapMaker_cfi.py to pi0Discriminator_cfi.py

---
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/pi0Discriminator_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+pi0Discriminator = cms.EDProducer("PhotonAsscToValMap",
+    collectionSrc = cms.InputTag('photons'),
+    associationSrc = cms.InputTag('piZeroDiscriminators',
+                                  'PhotonPi0DiscriminatorAssociationMap'),
+    label = cms.string("pi0Discriminator")
+    )
