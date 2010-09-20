6058a37ba8293a8ca0be85d821ad636f8b46d6b7
hcheung <>
2010-09-20 18:58:19 +0000

    Remove SLHCrecoBSVTagInfoValidationAnalyzer that is not needed anymore

--- SLHCUpgradeSimulations/Validation/src/SealModule.cc
+++ SLHCUpgradeSimulations/Validation/src/SealModule.cc
@@ -3,7 +3,6 @@
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPrimaryVertexAnalyzer.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPixelHitAnalyzer.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCAllPixelHitAnalyzer.h"
-#include "SLHCUpgradeSimulations/Validation/interface/SLHCrecoBSVTagInfoValidationAnalyzer.h"
 
 using namespace edm;
 using namespace reco;
@@ -11,4 +10,3 @@ using namespace reco;
 DEFINE_FWK_MODULE(SLHCPrimaryVertexAnalyzer);
 DEFINE_FWK_MODULE(SLHCPixelHitAnalyzer);
 DEFINE_FWK_MODULE(SLHCAllPixelHitAnalyzer);
-DEFINE_FWK_MODULE(SLHCrecoBSVTagInfoValidationAnalyzer);
