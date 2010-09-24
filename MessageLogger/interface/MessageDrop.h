830b11e5107b623848acf9c0ab8725d051b69fdd
fischler <>
2010-09-24 22:00:16 +0000

    Implemented "automatic suppression" of messages below the lowest severity that any destination will react to.  Suppression means that nothing about the message will pass to the business end of the logger to be checked for whether each destination should react to it.  In the case of automatic suppression, nobody will check about anytihing depending on the module that this thread is in, either.
    For example, if the only destination is configured to have a threshold of
    WARNING, then edm::LogInfo, which would in any case not have any effect on the
    destination's output, will be suppressed and handled with much less overhead.
    
    This addresses Savanah task #11808.

--- MessageLogger/interface/MessageDrop.h
+++ MessageLogger/interface/MessageDrop.h
@@ -52,6 +52,10 @@
 //  8  cdj 2/08/10      Make debugEnabled, infoEnabled, warningEnabled statics
 //                      to avoid overhead of thread specific singleton access
 //                      when deciding to keep or throw away messages
+//
+//  9  mf 9/23/10	Support for situations where no thresholds are low
+//                      enough to react to LogDebug (or info, or warning)
+//
 
 // user include files
 
@@ -76,6 +80,9 @@ public:
   static bool warningEnabled;                           // change log 8
   static unsigned char messageLoggerScribeIsRunning;	// change log 7
   static edm::Exception * ex_p;				// change log 4
+  static bool debugAlwaysSuppressed;			// change log 9
+  static bool infoAlwaysSuppressed;			// change log 9
+  static bool warningAlwaysSuppressed;			// change log 9
 };
 
 static const unsigned char  MLSCRIBE_RUNNING_INDICATOR = 29; // change log 7
--- MessageLogger/src/MessageDrop.cc
+++ MessageLogger/src/MessageDrop.cc
@@ -23,6 +23,9 @@
 // 1 12/13/07 mf     	the static drops had been file-global level; moved it
 //		     	into the instance() method to cure a 24-byte memory
 //			leak reported by valgrind. Suggested by MP.
+//
+// 2 9/23/10 mf		Variables supporting situations where no thresholds are
+//                      low enough to react to LogDebug (or info, or warning)
 
 using namespace edm;
 
@@ -31,6 +34,13 @@ edm::Exception * MessageDrop::ex_p = 0;
 bool MessageDrop::debugEnabled=true;
 bool MessageDrop::infoEnabled=true;
 bool MessageDrop::warningEnabled=true;
+// The following are false at initialization (in case configure is not done)
+// and are set true at the start of configure_ordinary_destinations, 
+// but are set false once a destination is thresholded to react to the 
+// corresponding severity: 
+bool MessageDrop::debugAlwaysSuppressed=false;		// change log 2
+bool MessageDrop::infoAlwaysSuppressed=false;	 	// change log 2
+bool MessageDrop::warningAlwaysSuppressed=false; 	// change log 2
 
 MessageDrop *
 MessageDrop::instance()
