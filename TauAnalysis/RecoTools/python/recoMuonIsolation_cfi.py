2fd777e64b6cb5a9dd5795fda7feb8aa05930981
veelken <>
2010-09-26 16:53:53 +0000

    improved tabbing, added comments

--- TauAnalysis/RecoTools/python/recoMuonIsolation_cfi.py
+++ TauAnalysis/RecoTools/python/recoMuonIsolation_cfi.py
@@ -2,29 +2,30 @@ import FWCore.ParameterSet.Config as cms
 
 #--------------------------------------------------------------------------------
 # compute particle flow based IsoDeposits
-# names and settings taken from 22X, to be tuned
+# names and settings taken from CMSSW_2_2_X, to be tuned
 #--------------------------------------------------------------------------------
 
 from PhysicsTools.PFCandProducer.Isolation.pfMuonIsolation_cff import *
 muonCollection = "muons"
-pfmuIsoDepositPFCandidates = isoDepositReplace(muonCollection,"particleFlow")
-pfmuIsoChDepositPFCandidates = isoDepositReplace(muonCollection,"pfAllChargedHadrons")
-pfmuIsoNeDepositPFCandidates = isoDepositReplace(muonCollection,"pfAllNeutralHadrons")
-pfmuIsoGaDepositPFCandidates = isoDepositReplace(muonCollection,"pfAllPhotons")
+pfmuIsoDepositPFCandidates   = isoDepositReplace(muonCollection, "particleFlow")
+pfmuIsoChDepositPFCandidates = isoDepositReplace(muonCollection, "pfAllChargedHadrons")
+pfmuIsoNeDepositPFCandidates = isoDepositReplace(muonCollection, "pfAllNeutralHadrons")
+pfmuIsoGaDepositPFCandidates = isoDepositReplace(muonCollection, "pfAllPhotons")
 
-
-pfMuonIsolCandidates = cms.Sequence( pfmuIsoDepositPFCandidates
-                                     * pfmuIsoChDepositPFCandidates
-                                     * pfmuIsoNeDepositPFCandidates
-                                     * pfmuIsoGaDepositPFCandidates )
+pfMuonIsolCandidates = cms.Sequence(
+    pfmuIsoDepositPFCandidates
+   * pfmuIsoChDepositPFCandidates
+   * pfmuIsoNeDepositPFCandidates
+   * pfmuIsoGaDepositPFCandidates
+)
 
 pfMuIsoDeposit = cms.EDProducer("CandIsolatorFromDeposits",
     deposits = cms.VPSet(
         cms.PSet(
             src = cms.InputTag("pfmuIsoDepositPFCandidates"),
-            deltaR = cms.double(0.3),
+            deltaR = cms.double(0.6),
             weight = cms.string('1'),
-            vetos = cms.vstring('0.01','Threshold(0.5)'),
+            vetos = cms.vstring('0.01', 'Threshold(0.5)'),
             skipDefaultVeto = cms.bool(True),
             mode = cms.string('sum')
         )
@@ -37,10 +38,14 @@ pfMuIsoNeDeposit.deposits.src = 'pfmuIsoNeDepositPFCandidates'
 pfMuIsoGaDeposit = pfMuIsoDeposit.clone()
 pfMuIsoGaDeposit.deposits.src = 'pfmuIsoGaDepositPFCandidates'
 
-muonIsoDeposits =  cms.Sequence( pfMuIsoDeposit
-                                 * pfMuIsoChDeposit
-                                 * pfMuIsoNeDeposit
-                                 * pfMuIsoGaDeposit )
+muonIsoDeposits = cms.Sequence(
+    pfMuIsoDeposit
+   * pfMuIsoChDeposit
+   * pfMuIsoNeDeposit
+   * pfMuIsoGaDeposit
+)
 
-recoMuonIsolation = cms.Sequence( pfMuonIsolCandidates
-                                  * muonIsoDeposits )
+recoMuonIsolation = cms.Sequence(
+    pfMuonIsolCandidates
+   * muonIsoDeposits
+)
