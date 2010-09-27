53885ff7ccccc78f205fc758111aa69372bf4e62
jkolb <>
2010-09-27 11:36:27 +0000

    remove unnecessary import statements

--- TauAnalysis/Configuration/python/tools/patElectronTools.py
+++ TauAnalysis/Configuration/python/tools/patElectronTools.py
@@ -1,10 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.PatAlgos.recoLayer0.electronIsolation_cff import *
-#from PhysicsTools.PatAlgos.mcMatchLayer0.electronMatch_cfi import *
-#from PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi import *
-
-#from TauAnalysis.RecoTools.patElectronConfig_cfi import *
 
 def producePatElectronIsolation(process):
 	process.makePatElectrons = cms.Sequence(
