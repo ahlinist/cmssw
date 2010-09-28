67c871ae1e8d6d553da689aa38a43c36ab47deac
veelken <>
2010-09-28 08:40:55 +0000

    fixed typos in spelling of analysis channel (was "ZtoDiTau" instead of "ZtoMuTau")

--- TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-INPUT_FILES_DIR = '/data1/veelken/CMSSW_3_6_x/plots/ZtoDiTau'
-sample_mapper = lambda sample : "harvested_ZtoDiTau_%s_7TeV.root" % sample
+INPUT_FILES_DIR = '/data1/veelken/CMSSW_3_6_x/plots/ZtoMuTau'
+sample_mapper = lambda sample : "harvested_ZtoMuTau_%s_7TeV.root" % sample
 
-process = cms.Process('makeZtoDiTauPlots')
+process = cms.Process('makeZtoMuTauPlots')
 
-import TauAnalysis.Configuration.recoSampleDefinitionsZtoDiTau_7TeV_grid_cfi as samples
+import TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi as samples
 
-process.load("TauAnalysis.Configuration.dumpZtoDiTau_grid_cff")
-process.load("TauAnalysis.Configuration.plotZtoDiTau_grid_cff")
+process.load("TauAnalysis.Configuration.dumpZtoMuTau_grid_cff")
+process.load("TauAnalysis.Configuration.plotZtoMuTau_grid_cff")
 
 process.DQMStore = cms.Service("DQMStore")
 
@@ -20,16 +20,16 @@ process.source = cms.Source("EmptySource")
 
 # define directory from which .root files containing histograms 
 # for individual processes get loaded
-process.loadZtoDiTauSamples.inputFilePath = cms.string(INPUT_FILES_DIR)
+process.loadZtoMuTauSamples.inputFilePath = cms.string(INPUT_FILES_DIR)
 
 # Update our input files
 for sample in samples.FLATTENED_SAMPLES_TO_PLOT:
-    getattr(process.loadZtoDiTauSamples, sample).inputFileNames = cms.vstring(
+    getattr(process.loadZtoMuTauSamples, sample).inputFileNames = cms.vstring(
         sample_mapper(sample)
     )
 
 # import utility function to enable factorization
-from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeZtoDiTauPlots_grid
+from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeZtoMuTauPlots_grid
 
 samplesToFactorize = [sample for sample in samples.FLATTENED_SAMPLES_TO_PLOT 
                       if samples.ALL_SAMPLES[sample].get('factorize', False)]
@@ -41,7 +41,7 @@ relevantMergedSamples = [sample for sample, sample_info in samples.MERGE_SAMPLES
 print "Factorizing", samplesToFactorize
 print "Updating", relevantMergedSamples
 
-enableFactorization_makeZtoDiTauPlots_grid(
+enableFactorization_makeZtoMuTauPlots_grid(
     process,
     samplesToFactorize = samplesToFactorize,
     relevantMergedSamples = relevantMergedSamples,
@@ -51,19 +51,19 @@ enableFactorization_makeZtoDiTauPlots_grid(
 process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
 
 # define name and directory in which .root file containing all histograms gets saved
-process.saveZtoDiTau.outputFileName = cms.string(
-    "/data1/veelken/CMSSW_3_6_x/plots/ZtoDiTau/plotsZtoDiTau_all.root"
+process.saveZtoMuTau.outputFileName = cms.string(
+    "/data1/veelken/CMSSW_3_6_x/plots/ZtoMuTau/plotsZtoMuTau_all.root"
 )
 
-process.makeZtoDiTauPlots = cms.Sequence(
-    process.loadZtoDiTau
+process.makeZtoMuTauPlots = cms.Sequence(
+    process.loadZtoMuTau
    #+ process.dumpDQMStore
-   + process.saveZtoDiTau
-   + process.dumpZtoDiTau
-   + process.plotZtoDiTau
+   + process.saveZtoMuTau
+   + process.dumpZtoMuTau
+   + process.plotZtoMuTau
 )
 
-process.p = cms.Path(process.makeZtoDiTauPlots)
+process.p = cms.Path(process.makeZtoMuTauPlots)
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
