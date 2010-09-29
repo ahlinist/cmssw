6f383736dd3ce17be0a2cf6040b14110d6b9e9a6
friis <>
2010-09-29 10:11:42 +0000

    Add api interface

--- TauAnalysis/Configuration/python/tools/flattenCfgFile.py
+++ TauAnalysis/Configuration/python/tools/flattenCfgFile.py
@@ -11,15 +11,21 @@ Author: Evan K. Friis, UC Davis
 import sys
 import os
 
-if __name__ == "__main__":
-    file_to_dump = sys.argv[1]
-    output_file = sys.argv[2]
+def dumpPython(file_to_dump, output_file_obj):
     path_to_file = os.path.dirname(os.path.abspath(file_to_dump))
     # Add the directory containing the file to the PYTHON_PATH
     sys.path.append(path_to_file)
     cfg_module_name = os.path.basename(file_to_dump).replace('.py', '')
     __import__(cfg_module_name)
     cfg = sys.modules[cfg_module_name]
+    output_file_obj.write(cfg.process.dumpPython())
+    # Restore state
+    del sys.path[-1:] # remove added path
+    del sys.modules[cfg_module_name] # remove added module
+
+if __name__ == "__main__":
+    file_to_dump = sys.argv[1]
+    output_file = sys.argv[2]
     output = open(output_file, 'w')
-    output.write(cfg.process.dumpPython())
+    dumpPython(file_to_dump, output)
     output.close()
