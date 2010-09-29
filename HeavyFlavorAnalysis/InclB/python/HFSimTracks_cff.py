f0ccc43e740c18d4f6ccbc3608a7cce4d2c70375
werdmann <>
2010-09-29 06:41:03 +0000

    update for new version

--- HeavyFlavorAnalysis/InclB/python/HFSimTracks_cff.py
+++ HeavyFlavorAnalysis/InclB/python/HFSimTracks_cff.py
@@ -1,11 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-<<<<<<< HFSimTracks_cff.py
 simtrackDump = cms.EDAnalyzer("HFDumpSimTracks",    
-=======
-simtrkDump = cms.EDAnalyzer("HFDumpSimTracks",
-    
->>>>>>> 1.2
     tracksLabel = cms.untracked.string('g4SimHits'),
     verbose = cms.untracked.int32(0)
 )
