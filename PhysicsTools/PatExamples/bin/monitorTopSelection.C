432409eecabf0ca825eeba3513ad3be51e92c241
rwolf <>
2010-09-29 16:41:23 +0000

    added data entry to macro

--- PhysicsTools/PatExamples/bin/monitorTopSelection.C
+++ PhysicsTools/PatExamples/bin/monitorTopSelection.C
@@ -48,7 +48,7 @@ void monitorTopSelection(const std::string& histName="yield", std::string select
   std::vector<std::string> validSteps_;
   validSteps_.push_back("Step1"   );
   validSteps_.push_back("Step2"   );
-  validSteps_.push_back("Step3b"  );
+  validSteps_.push_back("Step3a"  );
   validSteps_.push_back("Step4"   );
   validSteps_.push_back("Step5"   );
   validSteps_.push_back("Step6a"  );
@@ -61,12 +61,13 @@ void monitorTopSelection(const std::string& histName="yield", std::string select
     return;
   }
   
-  // list of input smaples
+  // list of input samples (ATTENTION obey order)
   std::vector<string> samples_;
   samples_.push_back("analyzePatTopSelection_ttbar.root");
   samples_.push_back("analyzePatTopSelection_wjets.root");
   samples_.push_back("analyzePatTopSelection_zjets.root");
   samples_.push_back("analyzePatTopSelection_qcd.root"  );
+  samples_.push_back("analyzePatTopSelection.root");
 
   //open files
   std::vector<TFile*> files;
@@ -86,14 +87,14 @@ void monitorTopSelection(const std::string& histName="yield", std::string select
       hists.push_back((TH1F*)files[idx]->Get((std::string("mon").append(histPath)).c_str()));
     }
   }
-  // scale and stack histograms
+  float lumi = 2.;
+  // scale and stack histograms for simulated events
   // scales for 1pb-1: ttbar,  wjets,  zjets,  qcd 
-  float lumi = 1.;
-  float scales[] =    {0.165,  0.280,  0.312, 0.287};
-  for(unsigned int idx=0; idx<samples_.size(); ++idx){
+  float scales[] =    {0.165,  0.312,  0.280, 0.287};
+  for(unsigned int idx=0; idx<samples_.size()-1; ++idx){
     hists[idx]->Scale(lumi*scales[idx]);
   }
-  for(unsigned int idx=1; idx<samples_.size(); ++idx){
+  for(unsigned int idx=1; idx<samples_.size()-1; ++idx){
     hists[idx]->Add(hists[idx-1]);
   }
   // setup the canvas and draw the histograms
@@ -104,7 +105,7 @@ void monitorTopSelection(const std::string& histName="yield", std::string select
     hists[3]->SetTitle("Selection Steps");
   }
   hists[3]->SetMinimum(1.);
-  hists[3]->SetMaximum(2.*hists[3]->GetMaximum());
+  hists[3]->SetMaximum(20.*hists[4]->GetMaximum());
   hists[3]->SetFillColor(kYellow);
   hists[3]->Draw();
   hists[2]->SetFillColor(kAzure-2);
@@ -113,12 +114,19 @@ void monitorTopSelection(const std::string& histName="yield", std::string select
   hists[1]->Draw("same");
   hists[0]->SetFillColor(kRed+1);
   hists[0]->Draw("same");
+  // plot data points
+  hists[4]->SetLineWidth(3.);
+  hists[4]->SetLineColor(kBlack);
+  hists[4]->SetMarkerColor(kBlack);
+  hists[4]->SetMarkerStyle(20.);
+  hists[4]->Draw("esame");
   canv0->RedrawAxis();
   
-  TLegend* leg = new TLegend(0.4,0.6,0.90,0.90);
+  TLegend* leg = new TLegend(0.35,0.6,0.85,0.90);
   leg->SetFillStyle ( 0);
   leg->SetFillColor ( 0);
   leg->SetBorderSize( 0);
+  leg->AddEntry( hists[4], "CMS Data 2010 (2 pb^{-1})"     , "PL");
   leg->AddEntry( hists[3], "QCD"                           , "F");
   leg->AddEntry( hists[2], "Z/#gamma#rightarrowl^{+}l^{-}" , "F");
   leg->AddEntry( hists[1], "W#rightarrowl#nu"              , "F");
