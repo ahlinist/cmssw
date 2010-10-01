f7b9a3d5fb4c1a44bebad29908c5ac17cc86b6e5
santaola <>
2010-10-01 16:45:38 +0000

    for STA muons

--- MuonAnalysis/MomentumResolution/test/ZMM/EdmToTreeConverter.py
+++ MuonAnalysis/MomentumResolution/test/ZMM/EdmToTreeConverter.py
@@ -12,23 +12,24 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-    "file:EdmZmmTreeMCShort.root",
-    "file:EdmZmmTreeDataShort.root"
+    "file:EdmZmmTreeMC_STA.root",
+    "file:EdmZmmTreeData_STA.root"
     )
                            
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.TFileService = cms.Service("TFileService", 
-                                   fileName = cms.string("treeMC_histoData.root"),
+                                   fileName = cms.string("treeMC_histoData_STA.root"),
                                    closeFileFast = cms.untracked.bool(True)
                                    )
 
 process.edmtotreeconverter = cms.EDAnalyzer("EdmToTreeConverterZ",
+                                            UseSTA = cms.untracked.bool(True),
                                             Doups = cms.untracked.bool(False),
                                             Nbins_histomassZ = cms.untracked.int32(40),
-                                            Inibin_histomassZ = cms.untracked.int32(70),
-                                            Endbin_histomassZ = cms.untracked.int32(110),
+                                            Inibin_histomassZ = cms.untracked.int32(50),
+                                            Endbin_histomassZ = cms.untracked.int32(130),
                                             Nbins_histomassU = cms.untracked.int32(10),
                                             Inibin_histomassU = cms.untracked.int32(6),
                                             Endbin_histomassU = cms.untracked.int32(8)
