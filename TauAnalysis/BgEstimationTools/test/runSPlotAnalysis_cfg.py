6be596e6873a92afab028e91b351d7f50b6f1d76
veelken <>
2010-10-11 10:18:35 +0000

    - added support for fitting 2d/3d histogram templates - enabled plots of control variables (using sPlot weights) - improved style of control plots - added debug output (somehow, the RooStats::SPlot algorithm does not take event weights into account;   cf. posting on RootTalk mailing list http://root.cern.ch/phpBB3/viewtopic.php?f=15&t=11385 ?!)

--- TauAnalysis/BgEstimationTools/test/runSPlotAnalysis_cfg.py
+++ TauAnalysis/BgEstimationTools/test/runSPlotAnalysis_cfg.py
@@ -20,11 +20,15 @@ dqmDirectory_WplusJets_template = 'template/WplusJets'
 
 dqmDirectory_data = 'data'
 
-meName_muonPt = 'muonPt'
-meName_muonAbsEta = 'muonAbsEta'
+meName_muonPt_vs_muonAbsEta = 'muonPt_vs_muonAbsEta'
 
 ntuplePath = "/data1/veelken/CMSSW_3_6_x/ntuples"
 treeName = "ntupleProducer/ZtoMuTauNtuple"
+
+branchName_muonPt = 'muonPt_0'
+branchName_muonAbsEta = 'muonAbsEta_0'
+
+branchName_eventWeight = 'eventWeight'
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
@@ -41,28 +45,22 @@ process.prodTemplateHist_Ztautau = cms.EDAnalyzer("TemplateHistProducer",
         cms.PSet(
             variables = cms.VPSet(
                 cms.PSet(
-                    branchName = cms.string('muonPt_0'),
-                    numBins = cms.uint32(20),
-                    min = cms.double(0.),
-                    max = cms.double(100.)
-                )
-            ),
-            meName = cms.string(dqmDirectory_Ztautau_template + "/" + meName_muonPt)
-	),
-        cms.PSet(
-            variables = cms.VPSet(
+                    branchName = cms.string(branchName_muonPt),
+                    numBins = cms.uint32(8),
+                    binEdges = cms.vdouble(15., 20., 25., 30., 40., 50., 60., 80., 100.)
+                ),
                 cms.PSet(
-                    branchName = cms.string('muonAbsEta_0'),
-                    numBins = cms.uint32(14),
+                    branchName = cms.string(branchName_muonAbsEta),
+                    numBins = cms.uint32(7),
                     min = cms.double(0.),
                     max = cms.double(2.1)
                 )
             ),
-            meName = cms.string(dqmDirectory_Ztautau_template + "/" + meName_muonAbsEta)
+            meName = cms.string(dqmDirectory_Ztautau_template + "/" + meName_muonPt_vs_muonAbsEta)
         )
     ),
                                                   
-    branchNamesEventWeight = cms.vstring('eventWeight'),
+    branchNamesEventWeight = cms.vstring(branchName_eventWeight),
 
     norm = cms.double(1.)
 )
@@ -71,15 +69,13 @@ process.prodTemplateHist_QCD = process.prodTemplateHist_Ztautau.clone()
 process.prodTemplateHist_QCD.fileNames = cms.vstring(
     ntuplePath + '/' + 'ntupleZtoMuTau_PPmuXptGt20_7TeV_part*'
 )
-process.prodTemplateHist_QCD.config[0].meName = cms.string(dqmDirectory_QCD_template + "/" + meName_muonPt)
-process.prodTemplateHist_QCD.config[1].meName = cms.string(dqmDirectory_QCD_template + "/" + meName_muonAbsEta)
+process.prodTemplateHist_QCD.config[0].meName = cms.string(dqmDirectory_QCD_template + "/" + meName_muonPt_vs_muonAbsEta)
 
 process.prodTemplateHist_WplusJets = process.prodTemplateHist_Ztautau.clone()
 process.prodTemplateHist_WplusJets.fileNames = cms.vstring(
     ntuplePath + '/' + 'ntupleZtoMuTau_WplusJets_7TeV_part*'
 )
-process.prodTemplateHist_WplusJets.config[0].meName = cms.string(dqmDirectory_WplusJets_template + "/" + meName_muonPt)
-process.prodTemplateHist_WplusJets.config[1].meName = cms.string(dqmDirectory_WplusJets_template + "/" + meName_muonAbsEta)
+process.prodTemplateHist_WplusJets.config[0].meName = cms.string(dqmDirectory_WplusJets_template + "/" + meName_muonPt_vs_muonAbsEta)
 
 process.prodTemplateHist = cms.Sequence(
     process.prodTemplateHist_Ztautau
@@ -95,8 +91,7 @@ process.prodDistribution_data.fileNames = cms.vstring(
     '/data1/veelken/CMSSW_3_6_x/ntuples/ntupleZtoMuTau_PPmuXptGt20_7TeV_part*',
     '/data1/veelken/CMSSW_3_6_x/ntuples/ntupleZtoMuTau_WplusJets_7TeV_part*'
 )
-process.prodDistribution_data.config[0].meName = cms.string(dqmDirectory_data + "/" + meName_muonPt)
-process.prodDistribution_data.config[1].meName = cms.string(dqmDirectory_data + "/" + meName_muonAbsEta)
+process.prodDistribution_data.config[0].meName = cms.string(dqmDirectory_data + "/" + meName_muonPt_vs_muonAbsEta)
 process.prodDistribution_data.norm = cms.double(-1.) # do not normalize data
 #--------------------------------------------------------------------------------
 
@@ -105,61 +100,55 @@ process.runSPlotAnalysis = cms.EDAnalyzer("SPlotAnalyzer",
     processes = cms.PSet(
         Ztautau = cms.PSet(
             templates = cms.PSet(
-                muonPt = cms.PSet(
-                    meName = cms.string(dqmDirectory_Ztautau_template + '/' + meName_muonPt)
-                ),
-                muonAbsEta = cms.PSet(
-                    meName = cms.string(dqmDirectory_Ztautau_template + '/' + meName_muonAbsEta)
+                muonPt_vs_muonAbsEta = cms.PSet(
+                    meName = cms.string(dqmDirectory_Ztautau_template + '/' + meName_muonPt_vs_muonAbsEta)
                 )
             ),
-            
+        
             fileNames = cms.vstring(
                 ntuplePath + '/' + 'ntupleZtoMuTau_Ztautau_7TeV_part*'
             ),
-            
-            treeName = cms.string(treeName)
+        
+            treeName = cms.string(treeName),
+
+            branchNameEventWeight = cms.string(branchName_eventWeight)
         ),
         QCD = cms.PSet(
             templates = cms.PSet(
-                muonPt = cms.PSet(
-                    meName = cms.string(dqmDirectory_QCD_template + '/' + meName_muonPt)
-                ),
-                muonAbsEta = cms.PSet(
-                    meName = cms.string(dqmDirectory_QCD_template + '/' + meName_muonAbsEta)
+                muonPt_vs_muonAbsEta = cms.PSet(
+                    meName = cms.string(dqmDirectory_QCD_template + '/' + meName_muonPt_vs_muonAbsEta)
                 )
             ),
-            
+        
             fileNames = cms.vstring(
                 ntuplePath + '/' + 'ntupleZtoMuTau_PPmuXptGt20_7TeV_part*'
             ),
-            
-            treeName = cms.string(treeName)
+        
+            treeName = cms.string(treeName),
+
+            branchNameEventWeight = cms.string(branchName_eventWeight)
         ),
         WplusJets = cms.PSet(
             templates = cms.PSet(
-                muonPt = cms.PSet(
-                    meName = cms.string(dqmDirectory_WplusJets_template + '/' + meName_muonPt)
-                ),
-                muonAbsEta = cms.PSet(
-                    meName = cms.string(dqmDirectory_WplusJets_template + '/' + meName_muonAbsEta)
+                muonPt_vs_muonAbsEta = cms.PSet(
+                    meName = cms.string(dqmDirectory_WplusJets_template + '/' + meName_muonPt_vs_muonAbsEta)
                 )
             ),
-            
+        
             fileNames = cms.vstring(
                 ntuplePath + '/' + 'ntupleZtoMuTau_WplusJets_7TeV_part*'
             ),
-            
-            treeName = cms.string(treeName)
+        
+            treeName = cms.string(treeName),
+
+            branchNameEventWeight = cms.string(branchName_eventWeight)
         )
     ),
 
     data = cms.PSet(
         distributions = cms.PSet(
-            muonPt = cms.PSet(
-                meName = cms.string(dqmDirectory_data + '/' + meName_muonPt)
-            ),
-            muonAbsEta = cms.PSet(
-                meName = cms.string(dqmDirectory_data + '/' + meName_muonAbsEta)
+            muonPt_vs_muonAbsEta = cms.PSet(
+                meName = cms.string(dqmDirectory_data + '/' + meName_muonPt_vs_muonAbsEta)
             )
         ),
         
@@ -169,33 +158,40 @@ process.runSPlotAnalysis = cms.EDAnalyzer("SPlotAnalyzer",
             ntuplePath + '/' + 'ntupleZtoMuTau_WplusJets_7TeV_part*'
         ),
         
-        treeName = cms.string(treeName)
+        treeName = cms.string(treeName),
+
+        branchNameEventWeight = cms.string(branchName_eventWeight)
     ),
+
+    fitVariables = cms.vstring(
+        branchName_muonPt,
+        branchName_muonAbsEta
+    ),                                    
 
     sPlots = cms.PSet(
         diTauVisMass = cms.PSet(
-            branchName = cms.string('diTauVisMass'),
+            branchName = cms.string('diTauVisMass_0'),
             title = cms.string("muon + tau-jet visible Mass"),
             xMin = cms.double(0.),
             xMax = cms.double(200.),
             selProcesses = cms.vstring('Ztautau')
         ),
         tauCharge = cms.PSet(
-            branchName = cms.string('tauCharge'),
+            branchName = cms.string('tauCharge_0'),
             title = cms.string("tau-jet charge"),
             xMin = cms.double(-5.5),
             xMax = cms.double(+5.5),
             selProcesses = cms.vstring('Ztautau')
         ),
         tauLeadChargedParticlePt = cms.PSet(
-            branchName = cms.string('tauLeadChargedParticlePt'),
+            branchName = cms.string('tauLeadChargedParticlePt_0'),
             title = cms.string("tau-jet lead. charged hadron P_{T}"),
             xMin = cms.double(0.),
             xMax = cms.double(150.),
             selProcesses = cms.vstring('Ztautau')
         ),
         tauNumChargedParticlesSignalCone = cms.PSet(
-            branchName = cms.string('tauNumChargedParticlesSignalCone'),
+            branchName = cms.string('tauNumChargedParticlesSignalCone_0'),
             title = cms.string("tau-jet num. charged hadrons in signal cone"),
             xMin = cms.double(-0.5),
             xMax = cms.double(+9.5),
