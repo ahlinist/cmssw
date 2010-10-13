0193465d4afa37127490ca5bffc63831dd756295
veelken <>
2010-10-13 16:24:05 +0000

    correct label of muon collection passing Pt cut (thresholds lowered from 15 GeV to 10 GeV)

--- TauAnalysis/Configuration/python/selectZtoElecMu_cff.py
+++ TauAnalysis/Configuration/python/selectZtoElecMu_cff.py
@@ -117,8 +117,8 @@ cfgMuonEtaCut = cms.PSet(
 cfgMuonPtCut = cms.PSet(
     pluginName = cms.string('muonPtCut'),
     pluginType = cms.string('PATCandViewMinEventSelector'),
-    src_cumulative = cms.InputTag('selectedPatMuonsPt15Cumulative'),
-    src_individual = cms.InputTag('selectedPatMuonsPt15Individual'),
+    src_cumulative = cms.InputTag('selectedPatMuonsPt10Cumulative'),
+    src_individual = cms.InputTag('selectedPatMuonsPt10Individual'),
     minNumber = cms.uint32(1)
 )
 cfgMuonTrkIsoCut = cms.PSet(
