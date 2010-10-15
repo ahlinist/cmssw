d81ea2e776542d36a05990e1b3bcb2364ff7f017
bmillanm <>
2010-10-15 12:08:41 +0000

    working in CMSSW_3_8_4_patch3

--- HeavyFlavorAnalysis/BsToJpsiPhi/src/SealModules.cc
+++ HeavyFlavorAnalysis/BsToJpsiPhi/src/SealModules.cc
@@ -2,5 +2,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "HeavyFlavorAnalysis/BsToJpsiPhi/interface/BsToJpsiPhiAnalysis.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(BsToJpsiPhiAnalysis);
+//DEFINE_SEAL_MODULE();
+DEFINE_FWK_MODULE(BsToJpsiPhiAnalysis);
+
