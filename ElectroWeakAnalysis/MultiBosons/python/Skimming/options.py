e2b8bd9f2b946a803a191c830fa684dfba352abd
veverka <>
2010-10-17 12:12:00 +0000

    * Renamed the outputEvents option ot outEvents. * Added the skimType and outEvents tags. * Changed the default filename from VGammaSkim.root to VGammaPAT.root. (Skim type is now propagated to the name through the skimType tag.)

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/options.py
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/options.py
@@ -4,72 +4,62 @@ from ElectroWeakAnalysis.MultiBosons.tools.VarParsingWithBool \
 
 options = VarParsing("analysis")
 
-options.setDefault("outputFile", "VGammaSkim.root")
+options.setDefault("outputFile", "VGammaPAT.root")
 
 options.register("globalTag",
     "GR10_P_V7::All", # default value is latest prompt reco (August 2010)
     VarParsing.multiplicity.singleton, # singleton or list
     VarParsing.varType.string,         # bool, string, int, or float
-    "Global tag to be used."
-    )
-
+    "Global tag to be used.")
 options.register("reportEvery",
     100, # default value
     VarParsing.multiplicity.singleton, # singleton or list
     VarParsing.varType.int,            # bool, string, int, or float
-    "Frequency of ouput."
-    )
-
+    "Frequency of ouput.")
 options.register("isRealData",
     False, # default value
     VarParsing.multiplicity.singleton, # singleton or list
     VarParsing.varType.bool,           # bool, string, int, or float
-    "Is this real data?"
-    )
-
+    "Is this real data?")
 options.register("use35XInput",
     False,
     VarParsing.multiplicity.singleton, # singleton or list
     VarParsing.varType.bool,           # bool, string, int, or float
-    "Is this 35X data?"
-    )
-
-options.register("outputEvents",
+    "Is this 35X data?")
+options.register("outEvents",
     -1,
     VarParsing.multiplicity.singleton, # singleton or list
     VarParsing.varType.int,            # bool, string, int, or float
-    "What is the maximum number of output events?"
-    )
-
+    "What is the maximum number of output events?")
 options.register("wantSummary",
-  True,
-  VarParsing.multiplicity.singleton, # singleton or list
-  VarParsing.varType.bool,           # bool, string, int, or float
-  "Do you want long trigger and timing reports at the end?"
-  )
-
+    True,
+    VarParsing.multiplicity.singleton, # singleton or list
+    VarParsing.varType.bool,           # bool, string, int, or float
+    "Do you want long trigger and timing reports at the end?")
 options.register("hltProcessName",
-  "HLT",                             # default value
-  VarParsing.multiplicity.singleton, # singleton or list
-  VarParsing.varType.string,         # bool, string, int, or float
-  "Name of the Process that produced the HLT information."
-  )
-
+    "HLT",                             # default value
+    VarParsing.multiplicity.singleton, # singleton or list
+    VarParsing.varType.string,         # bool, string, int, or float
+    "Name of the Process that produced the HLT information.")
 options.register("skimType",
-  "Muon", # default value
-  VarParsing.multiplicity.singleton, # singleton or list
-  VarParsing.varType.string,         # bool, string, int, or float
-  "What is the type of this skim, e.g. MuonPhoton?"
-  )
-
+    "Muon", # default value
+    VarParsing.multiplicity.singleton, # singleton or list
+    VarParsing.varType.string,         # bool, string, int, or float
+    "What is the type of this skim, e.g. MuonPhoton?")
 options.register("crabOptions",
-  "", # default value
-  VarParsing.multiplicity.singleton, # singleton or list
-  VarParsing.varType.string,         # bool, string, int, or float
-  "Pass several options in one string delimited by a `,'?"
-  )
-
-options.setupTags(tag = "%s", ifCond = "jobType != ''", tagArg = "jobType")
+    "", # default value
+    VarParsing.multiplicity.singleton, # singleton or list
+    VarParsing.varType.string,         # bool, string, int, or float
+    "Pass several options in one string delimited by a `,'?")
+options.setupTags(tag = "outEvent%d",
+                  ifCond = "outEvents > 0",
+                  tagArg = "outEvents")
+options.setupTags(tag = "%s",
+                  ifCond = "jobType != ''",
+                  tagArg = "jobType")
+options.setupTags(tag = "%sSkim",
+                  ifCond = "skimType != ''",
+                  tagArg = "skimType")
 
 ## Use this to work around bug with passing multiple option parameters with
 ## crab.  Pass just this one parameter and associate it with several other
