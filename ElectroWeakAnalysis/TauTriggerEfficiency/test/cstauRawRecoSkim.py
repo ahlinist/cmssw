13c30c24c3a03dc97f659fdd46ae119da3907b2a
eluiggi <>
2010-10-18 12:26:15 +0000

    add filter to raw + skim cfg

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/cstauRawRecoSkim.py
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/cstauRawRecoSkim.py
@@ -1,8 +1,3 @@
-# Auto generated configuration file
-# using: 
-# Revision: 1.172 
-# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: reco -s RAW2DIGI,L1Reco,RECO,DQM --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent RECO --customise Configuration/GlobalRuns/customise_Collision_36X.py --no_exec --python_filename=rereco_Collision_36X.py --conditions GR_R_36X_V12B::All
 import FWCore.ParameterSet.Config as cms
 import copy
 
@@ -16,31 +11,44 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.StandardSequences.GeometryExtended_cff')
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 #process.load('DQMOffline.Configuration.DQMOffline_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContent_cff')
+if(isData):
+  process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+else:
+  process.load('Configuration.StandardSequences.RawToDigi_cff')
+
+process.MessageLogger.cerr.threshold = 'INFO'
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.172 $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('reco nevts:1'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(1000)
 )
 process.options = cms.untracked.PSet(
 
 )
 # Input source
-process.source = cms.Source("PoolSource",
+if(isData):
+  process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/data/Run2010A/MinimumBias/RAW/v1/000/144/114/9AB9A0B1-F1B3-DF11-BCBF-001D09F24FEC.root'
+      '/store/data/Run2010A/MinimumBias/RAW/v1/000/144/114/9AB9A0B1-F1B3-DF11-BCBF-001D09F24FEC.root'
     )
-)
+  )
+else:
+  process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+      '/store/mc/Fall10/MinBias_TuneZ2_7TeV-pythia6/GEN-SIM-RAW/START38_V12-v1/0003/FEE7EA26-DCC7-DF11-BCD1-0030486790FE.root'
+    )
+  )
 
 #skiming 
 process.load("L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff")
@@ -87,18 +95,20 @@ process.output = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('CSTauSkim.root'),
     dataset = cms.untracked.PSet(
         dataTier = cms.untracked.string('RAW-RECO'),
-        filterName = cms.untracked.string('tauFilter')
-    )
+        #filterName = cms.untracked.string('tauFilter')
+    ),
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('tauFilter'))
 )
 
 # Additional output definition
 
 # Other statements
 if(isData):
-  process.GlobalTag.globaltag = "GR_R_37X_V6D::All"  
+  #process.GlobalTag.globaltag = "GR10_P_V10::All" # propt reco 
+  process.GlobalTag.globaltag = "GR_R_38X_V13A::All" # reprocessing  
 else:
-  #process.GlobalTag.globaltag = 'START36_V10::All'
-  process.GlobalTag.globaltag = 'MC_36Y_V9::All'
+  process.GlobalTag.globaltag = 'START38_V12::All'
+  #process.GlobalTag.globaltag = 'MC_36Y_V9::All'
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
@@ -109,12 +119,12 @@ process.endjob_step = cms.Path(process.endOfProcess)
 process.out_step = cms.EndPath(process.output)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.out_step)
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.tauFilter,process.endjob_step,process.out_step)
 
 
 # Automatic addition of the customisation function
 
-def customise(process):
+def customiseCommon(process):
     
     #####################################################################################################
     ####
@@ -127,13 +137,9 @@ def customise(process):
     process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=20000
     process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
     process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 20000
-    process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters= 20000
+    process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=20000
     process.thTripletsA.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
     process.thTripletsB.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
-
-    ## local tracker strip reconstruction
-    process.OutOfTime.TOBlateBP=0.071
-    process.OutOfTime.TIBlateBP=0.036
 
     ###### FIXES TRIPLETS FOR LARGE BS DISPLACEMENT ######
 
@@ -142,79 +148,91 @@ def customise(process):
     
     ### pixelTracks
     #---- new parameters ----
-    process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06)
+    process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = 4.06
     process.pixelTracks.RegionFactoryPSet.RegionPSet.originHalfLength = cms.double(40.6)
-    
+
     ### 0th step of iterative tracking
     #---- new parameters ----
-    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)
+    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)  
     process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 40.6
 
     ### 2nd step of iterative tracking
     #---- new parameters ----
-    process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)
+    process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)  
     process.secTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 44.7
-
-    ## Primary Vertex
-    process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
-    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
-    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
-    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelLayersWithHits = 2
-    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconLayersWithHits = 5
-    process.offlinePrimaryVerticesWithBS.TkClusParameters.TkGapClusParameters.zSeparation = 1
-    process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
-    process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
-    process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
-    process.offlinePrimaryVertices.TkFilterParameters.minPixelLayersWithHits = 2
-    process.offlinePrimaryVertices.TkFilterParameters.minSiliconLayersWithHits = 5
-    process.offlinePrimaryVertices.TkClusParameters.TkGapClusParameters.zSeparation = 1
 
     ## ECAL 
     process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 78, 142 ]
 
-    ##Preshower
-    process.ecalPreshowerRecHit.ESBaseline = cms.int32(0)
-
-    ## HCAL temporary fixes
-    process.hfreco.firstSample  = 3
-    process.hfreco.samplesToAdd = 4
-    
-    ## EGAMMA
-    process.photons.minSCEtBarrel = 5.
-    process.photons.minSCEtEndcap =5.
-    process.photonCore.minSCEt = 5.
-    process.conversionTrackCandidates.minSCEt =5.
-    process.conversions.minSCEt =5.
-    process.trackerOnlyConversions.rCut = 2.
-    process.trackerOnlyConversions.vtxChi2 = 0.0005
-    
     ###
     ###  end of top level replacements
     ###
     ###############################################################################################
 
-
-    #add the DQM stream for this time only
-    # DQMStream output definition
-    #process.outputDQMStream = cms.OutputModule("PoolOutputModule",
-    #                                           outputCommands = cms.untracked.vstring('drop *',
-    #                                                                                  'keep *_MEtoEDMConverter_*_*'),
-    #                                           fileName = cms.untracked.string('DQMStream.root'),
-    #                                           dataset = cms.untracked.PSet(
-    #    filterName = cms.untracked.string(''),
-    #    dataTier = cms.untracked.string('DQM')
-    #    )
-    #)
-    #process.outputDQMStreamOutPath = cms.EndPath(process.outputDQMStream)
-    #process.schedule.append( process.outputDQMStreamOutPath )
-
-    
-
     return (process)
+
+
+##############################################################################
+def customisePPData(process):
+    process= customiseCommon(process)
+
+    ## particle flow HF cleaning
+    process.particleFlowRecHitHCAL.LongShortFibre_Cut = 30.
+    process.particleFlowRecHitHCAL.ApplyPulseDPG = True
+
+    ## HF cleaning for data only
+    process.hcalRecAlgos.SeverityLevels[3].RecHitFlags.remove("HFDigiTime")
+    process.hcalRecAlgos.SeverityLevels[4].RecHitFlags.append("HFDigiTime")
+
+    ##beam-halo-id for data only
+    process.CSCHaloData.ExpectedBX = cms.int32(3)
+
+    ## hcal hit flagging
+    process.hfreco.PETstat.flagsToSkip  = 2
+    process.hfreco.S8S1stat.flagsToSkip = 18
+    process.hfreco.S9S1stat.flagsToSkip = 26
+    
+    return process
+
+
+##############################################################################
+def customisePPMC(process):
+    process=customiseCommon(process)
+    
+    return process
+
+##############################################################################
+def customiseCosmicData(process):
+
+    return process
+
+##############################################################################
+def customiseCosmicMC(process):
+    
+    return process
+        
+
+##############################################################################
+def customiseExpress(process):
+    process= customisePPData(process)
+
+    import RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi
+    process.offlineBeamSpot = RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi.onlineBeamSpotProducer.clone()
+    
+    return process
+
+##############################################################################
+def customisePrompt(process):
+    process= customisePPData(process)
+
+    import RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi
+    process.offlineBeamSpot = RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi.onlineBeamSpotProducer.clone()
+    
+    return process
 
 
 # End of customisation function definition
 
-process = customise(process)
+process = customisePPMC(process)
 
 
