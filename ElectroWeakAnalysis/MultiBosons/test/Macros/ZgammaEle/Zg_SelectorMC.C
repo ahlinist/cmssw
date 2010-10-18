58aca0b06c02c6d2a67aa5937040ed9a344bd0d9
zkliu <>
2010-10-18 20:42:03 +0000

    include anti-selection for ration method (bkg estimation) change photonID table, select for Z: choose 2 electrons with highest pt

--- ElectroWeakAnalysis/MultiBosons/test/Macros/ZgammaEle/Zg_SelectorMC.C
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/ZgammaEle/Zg_SelectorMC.C
@@ -14,24 +14,31 @@
 void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_index, Double_t EvtWeight, TString ProcessTag,TString TSaveFileName,Float_t DelRCut)
 {
    //photonID table
-   Double_t PhoIDCutEB[8][5]={{50 ,50 ,3.9,0.5 ,0.012},{2.6,50 ,2.5,0.04,0.012},
+   Double_t PhoIDCutEB[9][5]={{50 ,50 ,3.9,0.5 ,0.012},{2.6,50 ,2.5,0.04,0.012},
                               {2.6,50 ,1.6,0.04,0.012},{2.6,1.2,1.3,0.04,0.011},
                               {2.6,1.2,0.9,0.04,0.011},{1.6,1.2,0.7,0.01,0.011},
-                              {4.2,2.2,2.0,0.05,0.5  },{4.2,2.2,2.0,0.05,0.01}};
+                              {4.2,2.2,3.5,0.05,0.5  },{4.2,2.2,2.0,0.05,0.013},  {4.2,2.2,2.0,0.05,0.5}};
 
-   Double_t PhoIDCutEE[8][5]={{50 ,50 ,2.6,0.045,0.1  },{50 ,50 ,1.5,0.045,0.038},
+   Double_t PhoIDCutEE[9][5]={{50 ,50 ,2.6,0.045,0.1  },{50 ,50 ,1.5,0.045,0.038},
                               {50 ,1.4,1.2,0.045,0.038},{1.4,0.8,1.0,0.045,0.038},
                               {1.4,0.8,0.8,0.045,0.035},{1.4,0.6,0.8,0.025,0.03 },
-                              {4.2,2.2,2.0,0.05 ,0.5  },{4.2,2.2,2.0,0.05 ,0.03  }};
+                              {4.2,2.2,3.5,0.05 ,0.5  },{4.2,2.2,2.0,0.05 ,0.03 },{4.2,2.2,2.0,0.05,0.5}};
+
+   Double_t PhoIDScale[9][3]={{0.004,0.001 ,0.001},{0.004,0.001 ,0.001},{0.004,0.001,0.001},
+                              {0.004,0.001 ,0.001},{0.004,0.001 ,0.001},{0.004,0.001,0.001},
+                              {0.006,0.0025,0.001},{0.006,0.0025,0.001},{0.003,0.001,0.001}};
 
    //Set Cut
    float ElePtCut(20), PhoPtCut(10),PhoHoverPreCut(0.5);
-   float ZMassCutL(10), ZMassCutU(999);
-
+   float ZMassCutL(50), ZMassCutU(200);
+   float AntiUEB(999);
+   float AntiUEE(999);
+   float AntiLEB(3);
+   float AntiLEE(3);
    //-------------------------
-   int EleN_num(0), EleP_num(0), Pho_num(0);
-   int EleN_num0(0), EleP_num0(0);
-   int PassEIDN[65], PassEIDP[65];
+   int Ele_num(0),Pho_num(0);
+   int Ele_num0(0);
+   int PassEID[65];
    int ele1_index(0), ele2_index(0), pho_index(0);
 
    int PhoInEE(0),Ele1InEE(0),Ele2InEE(0);
@@ -39,11 +46,12 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
    float ThisValue(0);
    float MZee(0),dMZee(0);
    float MC_dPhi(0), MC_dEta(0), DelR1(0), DelR2(0);
+   float Leading1(0), Leading2(0);
 
    TLorentzVector ele1,ele2,gamma,Zee,Mllg;
 
    //Histogram
-   TH1D *hNEvt  = new TH1D("hNEvt","Event Yield counting",15,0,15);
+   TH1D *hNEvt  = new TH1D("hNEvt","Event Yield counting",20,0,20);
    TH1D *h1Et[3], *h2Pt[3];
    TH1D *heEta[3],*hpEta[3];
    TH1D *hpVar[3][6][2], *heVar[3][8][2];
@@ -93,6 +101,14 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
      }
    }
 
+   TH1D *hIso[4];
+
+   hIso[0]= new TH1D("NonIsoPho_Loose_EB", "", 500, 0, 500);
+   hIso[1]= new TH1D("NonIsoPho_Loose_EE", "", 500, 0, 500);
+   hIso[2]= new TH1D("IsoPho_Loose_EB", "", 500, 0, 500);
+   hIso[3]= new TH1D("IsoPho_Loose_EE", "", 500, 0, 500);
+
+   //-----
    //Set Process Tag Name
    if (ProcessTag=="Zg") hNEvt->SetBinContent(14,0);
    else if (ProcessTag=="ZJet") hNEvt->SetBinContent(14,1);
@@ -102,6 +118,7 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
    else if (ProcessTag=="PJet") hNEvt->SetBinContent(14,5);
    else if (ProcessTag=="QCD")  hNEvt->SetBinContent(14,6);
 
+   hNEvt->SetBinContent(15,EvtWeight);
    //--------------------------   
    
    if (fChain == 0) return;
@@ -117,22 +134,18 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
       // if (Cut(ientry) < 0) continue;
 
       //HLT
-      //if (HLT[HLTIndex[5]]==0) continue;
+      if (HLT[HLTIndex[5]]==0) continue;
 
-      if (nVtx ==0 ) continue;
       if (IsVtxGood ==0 ) continue;
       if (IsTracksGood ==0 ) continue;
 
-      EleN_num0=0,EleP_num0=0;
-      EleN_num=0,EleP_num=0;
+      Ele_num0=0,Ele_num0=0;
       Ele1InEE=-1, Ele2InEE=-1;
 
-      for (Int_t ii=0;ii<65;ii++) {
-        PassEIDN[ii]=-1;
-        PassEIDP[ii]=-1;
-      }
+      for (Int_t ii=0;ii<65;ii++) PassEID[ii]=-1;
+      
+      Ele_num=0;
 
-      EleN_num=0,EleP_num=0;
       for (Int_t iEle=0;iEle<nEle;iEle++){
          if (elePt[iEle]<ElePtCut) continue;
          if (fabs(eleSCEta[iEle])>2.5) continue;
@@ -145,14 +158,7 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
            if (eleSeverity[iEle]==4) continue;
          } 
 
-         //MC macthing
-         if (abs(eleGenMomPID[iEle])!=11 && abs(eleGenGMomPID[iEle])!=23 && abs(eleGenGMomPID[iEle])!=24) continue;
-
-         if (eleCharge[iEle]==1){
-            EleP_num0++;
-         } else if (eleCharge[iEle]==-1){
-            EleN_num0++;
-         }
+         Ele_num0++;
 
          if ( fabs(eleSCEta[iEle]) < 1.4442 ) Ele1InEE = 0;
          else if (fabs(eleSCEta[iEle])<2.5 && fabs(eleSCEta[iEle])>1.566) Ele1InEE = 1;
@@ -170,43 +176,41 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
          heVar[0][7][Ele1InEE] -> Fill(fabs(eledPhiAtVtx[iEle]),EvtWeight);
 
          if (eleID[iEle][EleID_index]!=7) continue;
+         //in 38x data samples, dEtain(EE) is included, but for 36x MC, it's not, so apply by ourself
+         if (Ele1InEE==1 && fabs(eledEtaAtVtx[iEle])>0.01) continue;
  
-         if (eleCharge[iEle]==1){
-            PassEIDP[EleP_num]=iEle;
-            EleP_num++;
-         } else if (eleCharge[iEle]==-1){
-            PassEIDN[EleN_num]=iEle;
-            EleN_num++;
-         }
+         PassEID[Ele_num]=iEle;
+         Ele_num++;
       }
 
-      if (EleN_num0>=1 && EleP_num0>=1) {
+      if (Ele_num0>=2) {
          hNEvt->Fill(0.5,EvtWeight);
       }
 
       MZee=-1;
 
-      if (EleN_num>=1 && EleP_num>=1) {
+      if (Ele_num>=2) {
          hNEvt->Fill(1.5,EvtWeight);
         
-         //(3) pass the Z mass cut, no cut for now... 
-         dMZee=999.0;
-         ThisValue=0,MZee=0;
-
-         for (Int_t i1=0;i1<EleP_num;i1++){
-            for (Int_t i2=0;i2<EleN_num;i2++){
-               ele1.SetPtEtaPhiE(elePt[PassEIDP[i1]],eleEta[PassEIDP[i1]],elePhi[PassEIDP[i1]],eleEn[PassEIDP[i1]]);  
-               ele2.SetPtEtaPhiE(elePt[PassEIDN[i2]],eleEta[PassEIDN[i2]],elePhi[PassEIDN[i2]],eleEn[PassEIDN[i2]]);
-               Zee=ele1+ele2;
-               ThisValue=Zee.M();
-
-               if (fabs(ThisValue-91.18)<dMZee){
-                  dMZee=fabs(ThisValue-91.18);
-                  ele1_index=PassEIDP[i1], ele2_index=PassEIDN[i2];
-                  MZee=ThisValue;
-               }
-            } 
-         }
+         //pass the Z mass cut, no cut for now... 
+         Leading1=-1,Leading2=-1;
+         ele2_index=0,ele1_index=0;
+         for (Int_t i1=0; i1<Ele_num; i1++){
+           if (elePt[PassEID[i1]]>Leading1 && elePt[PassEID[i1]]>Leading2) {
+             Leading2=Leading1;
+             ele2_index=ele1_index;
+            
+             Leading1=elePt[PassEID[i1]];
+             ele1_index=PassEID[i1];     
+           } else if (elePt[PassEID[i1]]<Leading1 && elePt[PassEID[i1]]>Leading2) {
+             Leading2=elePt[PassEID[i1]];
+             ele2_index=PassEID[i1];
+           } 
+         } 
+         ele1.SetPtEtaPhiE(elePt[ele1_index],eleEta[ele1_index],elePhi[ele1_index],eleEn[ele1_index]);
+         ele2.SetPtEtaPhiE(elePt[ele2_index],eleEta[ele2_index],elePhi[ele2_index],eleEn[ele2_index]);
+         Zee=ele1+ele2;
+         MZee=Zee.M();
       }
 
       Ele1InEE=-1, Ele2InEE=-1, PhoInEE=-1;
@@ -215,10 +219,10 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
          hNEvt->Fill(2.5,EvtWeight);
 
          if ( fabs(eleSCEta[ele1_index]) < 1.4442 ) Ele1InEE = 0;
-         else if (fabs(eleSCEta[ele1_index])<2.56 && fabs(eleSCEta[ele1_index])>1.566) Ele1InEE = 1;
+         else if (fabs(eleSCEta[ele1_index])<2.5 && fabs(eleSCEta[ele1_index])>1.566) Ele1InEE = 1;
 
          if ( fabs(eleSCEta[ele2_index]) < 1.4442 ) Ele2InEE = 0;
-         else if (fabs(eleSCEta[ele2_index])<2.56 && fabs(eleSCEta[ele2_index])>1.566) Ele2InEE = 1;
+         else if (fabs(eleSCEta[ele2_index])<2.5 && fabs(eleSCEta[ele2_index])>1.566) Ele2InEE = 1;
          
          //Fill Ele2 & Mee1
          h2Pt[1]->Fill(elePt[ele1_index],EvtWeight);
@@ -256,19 +260,14 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
             if (fabs(phoSCEta[iPho]) > 1.4442 && fabs(phoSCEta[iPho]) < 1.566 ) continue;
             if (phoOverlap[iPho] == 1) continue;
             if (phohasPixelSeed[iPho] == 1) continue;
-            //if (phoHoverE[iPho] > PhoHoverPreCut) continue;
+            if (phoHoverE[iPho] > PhoHoverPreCut) continue;
 
             //spike cleaning in EB
             if (fabs(phoSCEta[iPho]) < 1.4442) {
-             if (phoRecoFlag[iPho]==2) continue;
-             if (phoSeverity[iPho]==3) continue;
-             if (phoSeverity[iPho]==4) continue;
-           }
-
-            //MC matching
-            if ( fabs(phoGenMomPID[iPho])!=21  &&
-                 fabs(phoGenMomPID[iPho])>=7    && fabs(phoGenGMomPID[iPho])>=7  && fabs(phoGenGMomPID[iPho])!=21 &&
-                 fabs(phoGenGMomPID[iPho])!=24 && fabs(phoGenGMomPID[iPho])!=23 ) continue;
+              if (phoRecoFlag[iPho]==2) continue;
+              if (phoSeverity[iPho]==3) continue;
+              if (phoSeverity[iPho]==4) continue;
+            }
 
             //Remove ISR/FSR in Madgrapg W/Z+Jet
             if (ProcessTag == "WJet" || ProcessTag == "ZJet") {
@@ -284,7 +283,7 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
             hpEta[0]-> Fill(phoEta[iPho],EvtWeight);
 
             if ( fabs(phoSCEta[iPho]) < 1.4442 ) PhoInEE = 0;
-            else if (fabs(phoSCEta[iPho])<2.56 && fabs(phoSCEta[iPho])>1.566) PhoInEE = 1;
+            else if (fabs(phoSCEta[iPho])<2.5 && fabs(phoSCEta[iPho])>1.566) PhoInEE = 1;
 
             hpVar[0][0][PhoInEE] -> Fill(phoEcalIsoDR04[iPho],EvtWeight);
             hpVar[0][1][PhoInEE] -> Fill(phoHcalIsoDR04[iPho],EvtWeight);
@@ -294,18 +293,18 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
             hpVar[0][5][PhoInEE] -> Fill(phoHoverE[iPho],EvtWeight);
 
             if ( fabs(phoSCEta[iPho]) < 1.4442 ) {
-               if ((phoEcalIsoDR04[iPho]-0.004*phoEt[iPho]) > PhoIDCutEB[phoID_index][0]) continue;
-               if ((phoHcalIsoDR04[iPho]-0.001*phoEt[iPho]) > PhoIDCutEB[phoID_index][1]) continue;
-               if ((phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho]) > PhoIDCutEB[phoID_index][2]) continue;
+               if ((phoEcalIsoDR04[iPho]-PhoIDScale[phoID_index][0]*phoEt[iPho]) > PhoIDCutEB[phoID_index][0]) continue;
+               if ((phoHcalIsoDR04[iPho]-PhoIDScale[phoID_index][1]*phoEt[iPho]) > PhoIDCutEB[phoID_index][1]) continue;
+               if ((phoTrkIsoHollowDR04[iPho]-PhoIDScale[phoID_index][2]*phoEt[iPho]) > PhoIDCutEB[phoID_index][2]) continue;
                if (phoHoverE[iPho] > PhoIDCutEB[phoID_index][3]) continue;
                if (phoSigmaIEtaIEta[iPho]> PhoIDCutEB[phoID_index][4]) continue;
-            } else if (fabs(phoSCEta[iPho])<2.56 && fabs(phoSCEta[iPho])>1.566) {
-               if ((phoEcalIsoDR04[iPho]-0.004*phoEt[iPho]) > PhoIDCutEE[phoID_index][0]) continue;
-               if ((phoHcalIsoDR04[iPho]-0.001*phoEt[iPho]) > PhoIDCutEE[phoID_index][1]) continue;
-               if ((phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho]) > PhoIDCutEE[phoID_index][2]) continue;
+            } else if (fabs(phoSCEta[iPho])<2.5 && fabs(phoSCEta[iPho])>1.566) {
+               if ((phoEcalIsoDR04[iPho]-PhoIDScale[phoID_index][0]*phoEt[iPho]) > PhoIDCutEE[phoID_index][0]) continue;
+               if ((phoHcalIsoDR04[iPho]-PhoIDScale[phoID_index][1]*phoEt[iPho]) > PhoIDCutEE[phoID_index][1]) continue;
+               if ((phoTrkIsoHollowDR04[iPho]-PhoIDScale[phoID_index][2]*phoEt[iPho]) > PhoIDCutEE[phoID_index][2]) continue;
                if (phoHoverE[iPho] > PhoIDCutEE[phoID_index][3]) continue;
                if (phoSigmaIEtaIEta[iPho]> PhoIDCutEE[phoID_index][4]) continue;
-            } else {
+            } else{
                continue;
             }
 
@@ -319,7 +318,7 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
 
          if (pho_index!=-1) {
            if ( fabs(phoSCEta[pho_index]) < 1.4442 ) PhoInEE = 0;
-           else if (fabs(phoSCEta[pho_index]) < 2.56 && fabs(phoSCEta[pho_index]) > 1.566) PhoInEE = 1;
+           else if (fabs(phoSCEta[pho_index]) < 2.5 && fabs(phoSCEta[pho_index]) > 1.566) PhoInEE = 1;
 
            hNEvt->Fill(4.5,EvtWeight);
 
@@ -377,6 +376,11 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
                h1Et[2] -> Fill(phoEt[pho_index],EvtWeight);
                hpEta[2]-> Fill(phoEta[pho_index],EvtWeight);
 
+               if (fabs(phoSCEta[pho_index]) < 1.4442 )
+                 hIso[2]->Fill(phoEt[pho_index],EvtWeight);
+               else
+                 hIso[3]->Fill(phoEt[pho_index],EvtWeight);
+
                hpVar[2][0][PhoInEE] -> Fill(phoEcalIsoDR04[pho_index],EvtWeight);
                hpVar[2][1][PhoInEE] -> Fill(phoHcalIsoDR04[pho_index],EvtWeight);
                hpVar[2][2][PhoInEE] -> Fill(phoTrkIsoHollowDR04[pho_index],EvtWeight);
@@ -421,6 +425,78 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
                
            }//DRCut
          }
+
+         //Non-Iso selection
+         ThisValue=0;
+         pho_index=-1;
+         for (Int_t iPho=0;iPho<nPho;iPho++) {
+            if (phoEt[iPho] < PhoPtCut) continue;
+            if (fabs(phoSCEta[iPho]) > 2.5) continue;
+            if (fabs(phoSCEta[iPho]) > 1.4442 && fabs(phoSCEta[iPho]) < 1.566 ) continue;
+            if (phoOverlap[iPho] == 1) continue;
+            if (phohasPixelSeed[iPho] == 1) continue;
+            //if (phoHoverE[iPho] > PhoHoverPreCut) continue;
+
+            //spike cleaning in EB
+            if (fabs(phoSCEta[iPho]) < 1.4442) {
+              if (phoRecoFlag[iPho]==2) continue;
+              if (phoSeverity[iPho]==3) continue;
+              if (phoSeverity[iPho]==4) continue;
+            }
+
+            //Remove ISR/FSR in Madgrapg W/Z+Jet
+            if (ProcessTag == "WJet" || ProcessTag == "ZJet") {
+              //ISR
+              if (phoGenMomPID[iPho]==21 || abs(phoGenMomPID[iPho])<7) continue;
+              //FSR
+              if ( (abs(phoGenMomPID[iPho])==11 || abs(phoGenMomPID[iPho])==13 || abs(phoGenMomPID[iPho])==15) &&
+                 (abs(phoGenGMomPID[iPho])==23 || abs(phoGenGMomPID[iPho])==24)) continue;
+            }
+
+            //Anti-Selection
+            if (fabs(phoSCEta[iPho]) < 1.4442) {
+               if (phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho] < AntiLEB) continue;
+               if (phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho] > AntiUEB) continue;
+            } else if (fabs(phoSCEta[iPho]) > 1.566 ) {
+               if (phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho] < AntiLEE) continue;
+               if (phoTrkIsoHollowDR04[iPho]-0.001*phoEt[iPho] > AntiUEE) continue;
+            } else {
+               continue;
+            }
+
+            if (ThisValue < phoEt[iPho]){
+              ThisValue = phoEt[iPho];
+              pho_index = iPho;
+            }
+         }
+
+         if (pho_index!=-1) {
+           if ( fabs(phoSCEta[pho_index]) < 1.4442 ) PhoInEE = 0;
+           else if (fabs(phoSCEta[pho_index]) < 2.5 && fabs(phoSCEta[pho_index]) > 1.566) PhoInEE = 1;
+           MC_dPhi=0.0, MC_dEta=0.0;
+           DelR1=0.0, DelR2=0.0;
+
+           MC_dPhi = phoPhi[pho_index] - elePhi[ele1_index];
+           if (MC_dPhi >  3.1415927) MC_dPhi -= 2*3.1415927;
+           if (MC_dPhi < -3.1415927) MC_dPhi += 2*3.1415927;
+           MC_dEta = phoEta[pho_index] - eleEta[ele1_index];
+           DelR1=sqrt(MC_dEta*MC_dEta + MC_dPhi*MC_dPhi);
+
+           MC_dPhi=0.0, MC_dEta=0.0;
+
+           MC_dPhi = phoPhi[pho_index] - elePhi[ele2_index];
+           if (MC_dPhi >  3.1415927) MC_dPhi -= 2*3.1415927;
+           if (MC_dPhi < -3.1415927) MC_dPhi += 2*3.1415927;
+           MC_dEta = phoEta[pho_index] - eleEta[ele2_index];
+           DelR2=sqrt(MC_dEta*MC_dEta + MC_dPhi*MC_dPhi);
+           if (DelR1 > DelRCut && DelR2 > DelRCut){
+               if (fabs(phoSCEta[pho_index]) < 1.4442 )
+                  hIso[0]->Fill(phoEt[pho_index],EvtWeight);
+               else
+                  hIso[1]->Fill(phoEt[pho_index],EvtWeight);              
+           }
+         }
+         //
       }
    }
    //Save Histogtam
@@ -446,5 +522,6 @@ void Zg_SelectorMC::Loop(Int_t sample_index, Int_t phoID_index, Int_t EleID_inde
          for (Int_t kk=0;kk<6;kk++) hpVar[ii][kk][jj] -> Write();
        }
      }
+     for (Int_t ii=0;ii<4;ii++) hIso[ii]->Write();
    f1->Close();
 }
