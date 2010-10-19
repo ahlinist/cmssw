2ba7dd5dd4942c31e4ac8ffe920a8f6e6248aa56
temple <>
2010-10-19 23:51:02 +0000

    *** empty log message ***

--- JetMETAnalysis/UFAV/RunUFAVScript.py
+++ JetMETAnalysis/UFAV/RunUFAVScript.py
@@ -7,11 +7,45 @@ import string
 from optparse import OptionParser
 
 
+def SearchDir(dir=None,dict={},subdircount=0,maxrecursion=2,debug=False):
+    ''' This will search a directory and all its subdirectories (up to a maximum recursion level of "maxrecursion") for files of the type "*_R*.root". Any time in which multiple files are found for one run, an error message will be thrown, and the program will exit.'''
+
+    # 'dict' stores the list of files for the runs, with each run used as a key
+    subdircount=subdircount+1
+    if (subdircount>maxrecursion):  # set maximum recursion level
+        return dict
+    if not os.path.isdir(dir):
+        print("Sorry, directory '%s' does not exist -- skipping!"%dir)
+        return dict
+    for i in os.listdir(dir):  # Search all files in directory
+        if os.path.isfile(os.path.join(dir,i)):
+            # not sure about the re.search value here -- why not use string .endswith?
+            if(i.find("_R")==-1 or not re.search(".root$",i)):  # look only at root files
+                continue
+
+            # Get the run number from the root file name
+            try:
+                run=string.atoi(i[(i.find("_R")+2):(i.find("_R")+11)])
+            except:
+                print("Unable to parse run number from file '%s'"%os.path.join(dir,i))
+                continue
+            # Add run to dictionary
+            if run in dict.keys():
+                if (dict[run]==None):
+                    dict[run]=os.path.join(dir,i)
+                else:
+                    print ("ERROR!  Run #%i has multiple files!"%run)
+                    print ("File #1:  ",dict[run])
+                    print ("File #2:  ",os.path.join(dir,i))
 
 
-
-
-
+        # If subdirectory found, process files in that subdirectory
+        elif os.path.isdir(os.path.join(dir,i)):
+            dict=SearchDir(dir=os.path.join(dir,i),
+                           dict=dict,
+                           subdircount=subdircount,
+                           maxrecursion=maxrecursion)
+    return dict
 
 
 
@@ -20,57 +54,90 @@ from optparse import OptionParser
 
 if __name__=="__main__":
 
-        parser=OptionParser()
-        parser.add_option("-r","--run",
-                          dest="run",
-                          default=[],
-                          action="append",
-                          type="int",
-                          help="Specify run to be processed")
-        parser.add_option("-f","--ref",
-                          dest="ref",
-                          default=None,
-                          type=int,
-                          help="Specify reference run")
-        parser.add_option("-R","--runlistfile",
-                          dest="runlistfile",
-                          default=None,
-                          help="Specify list of runs to be processed")
-	parser.add_option("-m","--runRangeMode",
-			  default=False,
-			  dest="runRangeMode",
-			  action="store_true",
-			  help="If set true, all runs in the range [min(run), max(run)] will be evaluated.")
-        parser.add_option("-v","--verbose",
-                          dest="verbose",
-                          default=False,
-                          action="store_true",
-                          help="print debug information")
-        parser.add_option("-c","--copy",
-                          dest="copy",
-                          action="store_true",
-                          default=False,
-                          help="If active, will scp output directory to Baylor at hep06-cmsjmet:/www/htdocs/cmsjmet/JetMETDQM/Commissioning10")
-        parser.add_option("-H","--Help",
-                          dest="Help",
-                          action="store_true",
-                          default=False,
-                          help="alternate help action")
+    parser=OptionParser()
+    parser.add_option("-r","--run",
+		      dest="run",
+		      default=[],
+		      action="append",
+		      type="int",
+		      help="Specify run to be processed")
+    parser.add_option("-f","--ref",
+		      dest="ref",
+		      default=None,
+		      type=int,
+		      help="Specify reference run")
+    parser.add_option("-R","--runlistfile",
+		      dest="runlistfile",
+		      default=None,
+		      help="Specify list of runs to be processed")
+    parser.add_option("-m","--runRangeMode",
+		      default=False,
+		      dest="runRangeMode",
+		      action="store_true",
+		      help="If set true, all runs in the range [min(run), max(run)] will be evaluated.")
+    parser.add_option("-v","--verbose",
+		      dest="verbose",
+		      default=False,
+		      action="store_true",
+		      help="print debug information")
+    parser.add_option("-c","--copy",
+		      dest="copy",
+		      action="store_true",
+		      default=False,
+		      help="If active, will scp output directory to Baylor at hep06-cmsjmet:/www/htdocs/cmsjmet/JetMETDQM/Commissioning10")
+    parser.add_option("-H","--Help",
+		      dest="Help",
+		      action="store_true",
+		      default=False,
+		      help="alternate help action")
+    
+    parser.add_option("-b","--basedir",
+		      dest=basedir,
+		      default="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/data/OfflineData/Run2010/StreamExpress",
+		      help="Set base directory where files can be found.  Default is /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/data/OfflineData/Run2010/StreamExpress")
+    
+    (options,args)=parser.parse_args()
+    if options.Help==True:
+        parser.print_help()
+        sys.exit()
 
-	parser.add_option("-b","--basedir",
-			  dest=basedir,
-			  default="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/data/OfflineData/Run2010/StreamExpress",
-			  help="Set base directory where files can be found.  Default is /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/data/OfflineData/Run2010/StreamExpress")
+    ref=options.ref
+    runs=options.run
+    rundict={}
+    for r in runs:
+        if r not in rundict.keys():
+            rundict[r]=None
+        else:
+            print "WARNING!  Run %i has already been included!"%r
+            
+    if options.runlistfile<>None:
+        if not os.path.isfile(options.runlistfile):
+            print "ERROR!  Run list file '%s' does not exist!"%options.runlistfile
+    else:
+        myfile=open(options.runlistfile)
+        for i in myfile.readlines:
+            try:
+                run=string.strip(i)
+                run=string.atoi(i)
+                if (run>0):
+                    if run not in rundict.keys():
+                        rundict[run]=None
+                    else:
+                        print "Warning!  Run %i has already been included!"%run
+                elif run<0:
+                    if ref<>None:
+                        print "ERROR!  Trying to specify reference run '%i' when another reference run '%i' has already been chosen!"%(run,ref)
+                        print "Exiting..."
+                        sys.exit()
+                    else:
+                        ref=run
+
+    
+
+    # Get directory names
+    rundict=SearchDir(dir=options.basedir,
+                      dict=rundict,
+                      debug=options.verbose)
+    for i in rundict.keys():
+        print i, rundict[i]
         
-        (options,args)=parser.parse_args()
-        if options.Help==True:
-		parser.print_help()
-		sys.exit()
-
-	ref=options.ref
-	runs=options.run
-	if options.runlistfile<>None:
-		if not os.path.isfile(options.runlistfile):
-			print "ERROR!  Run list file '%s' does not exist!"%options.runlistfile
-		else:
-			
