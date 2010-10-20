fafe2c03072aa38aff0a6c8a4188833bf16d204e
veelken <>
2010-10-20 15:33:53 +0000

    - corrected dqmDirectpry parameter of DQMFileLoader module - added example for usage

--- TauAnalysis/Configuration/python/tools/genericTrimmer.py
+++ TauAnalysis/Configuration/python/tools/genericTrimmer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env cmsRun
 
+# usage: ../python/tools/genericTrimmer.py test.root /data1/friis/Run10_v2/harvested_AHtoMuTau_Zmumu_Run10.root "drop ahMuTauAnalyzer_wBtag/*" 
+
 import FWCore.ParameterSet.Config as cms
 import sys
 
@@ -26,14 +28,14 @@ process.source = cms.Source("EmptySource")
 process.load = cms.EDAnalyzer("DQMFileLoader",
     toMerge = cms.PSet(
         inputFileNames = cms.vstring(inputFileName),
-        dqmDirectory_store = cms.string('/')
+        dqmDirectory_store = cms.string('')
     )
 )
 
 process.save = cms.EDAnalyzer(
     "DQMSimpleFileSaver",
     outputFileName = cms.string(outputFileName),
-    outputCommands = cms.vstring(keeps)                                                                                                                                                              
+    outputCommands = cms.vstring(keeps)
 )
 
 print process.save
