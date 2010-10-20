45612938601f4e551aa62c97820862dc4818f663
temple <>
2010-10-20 00:05:48 +0000

    *** empty log message ***

--- JetMETAnalysis/UFAV/RunUFAVScript.py
+++ JetMETAnalysis/UFAV/RunUFAVScript.py
@@ -175,3 +175,4 @@ if __name__=="__main__":
                                                                 refrun,
                                                                 stream)
         print cmd
+        os.system(cmd)
