8a60ccef583e03849da565fbdd65bc09c259be4f
veelken <>
2010-10-22 11:52:34 +0000

    improved tabbing

--- TauAnalysis/RecoTools/python/tools/sysProdConfigurator.py
+++ TauAnalysis/RecoTools/python/tools/sysProdConfigurator.py
@@ -5,7 +5,7 @@ import random
 
 class sysProdConfigurator(cms._ParameterTypeBase):
 
-    def __init__(self,pyModuleName,pyNameSpace):
+    def __init__(self, pyModuleName, pyNameSpace):
         self.pyModuleName = pyModuleName,
         self.pyNameSpace = pyNameSpace 
         self.sequence = None
@@ -30,12 +30,12 @@ class sysProdConfigurator(cms._ParameterTypeBase):
             module.gaussianSmearingSigmaPt = cms.double(ptSmearing)
             module.gaussianSmearingSigmaEta = cms.double(etaSmearing)
             module.gaussianSmearingSigmaPhi = cms.double(phiSmearing)
-            module.gaussianSmearingSigmaEScale   = cms.double(energyScaleSmearing)
+            module.gaussianSmearingSigmaEScale = cms.double(energyScaleSmearing)
             module.setLabel(modulePrefix)
             if moduleType =='SmearedTauProducer': #Additional Tau default Config
                 module.smearConstituents = cms.bool(False)
-                module.hadronEnergyScale        = cms.double(1.0)
-                module.gammaEnergyScale         = cms.double(1.0)  
+                module.hadronEnergyScale = cms.double(1.0)
+                module.gammaEnergyScale = cms.double(1.0)  
 
             randomPSet = cms.PSet(
                 initialSeed = cms.untracked.uint32(987346),
@@ -43,11 +43,11 @@ class sysProdConfigurator(cms._ParameterTypeBase):
             )
 
             #ENERGY SCALE
-            energyScaleValues = [energyScaleMean-energyScaleShift, energyScaleMean ,energyScaleMean+energyScaleShift]
-            energyScaleLabels = ['EnScaleDown' ,'', 'EnScaleUp']
+            energyScaleValues = [ energyScaleMean - energyScaleShift, energyScaleMean ,energyScaleMean + energyScaleShift ]
+            energyScaleLabels = [ 'EnScaleDown', '', 'EnScaleUp' ]
 
             for label,eScale in zip(energyScaleLabels,energyScaleValues):
-                m =module.clone()
+                m = module.clone()
                 m.energyScale = cms.double(eScale)
                 #Register the filter in the namespace
                 pyModule = sys.modules[self.pyModuleName[0]]
@@ -55,80 +55,77 @@ class sysProdConfigurator(cms._ParameterTypeBase):
                    raise ValueError("'pyModuleName' Parameter invalid")
                 setattr(pyModule,modulePrefix+label,m)
                 if self.sequence == None:
-                   self.sequence=m
+                   self.sequence = m
                 else:
-                  self.sequence*=m
+                  self.sequence *= m
 
                 #Create an entry in the random number service
                 randomSet = randomPSet.clone()
                 randomSet.initialSeed = random.randint(100000,300000)
                 setattr(randomNumberService,modulePrefix+label,randomSet)
-              
 
+            #Pt
+            ptValues = [ -ptShift, ptShift ]
+            ptLabels = [ 'PtShiftDown', 'PtShiftUp' ]
 
-            #PT
-            ptValues = [-ptShift,ptShift]
-            ptLabels = ['PtShiftDown' ,'PtShiftUp']
-
-            for label,value in zip(ptLabels,ptValues):
-                m =module.clone()
+            for label, value in zip(ptLabels, ptValues):
+                m = module.clone()
                 m.deltaPt = cms.double(value)
                 m.setLabel(modulePrefix+label)
                 #Register the filter in the namespace
                 pyModule = sys.modules[self.pyModuleName[0]]
                 if pyModule is None:
-                   raise ValueError("'pyModuleName' Parameter invalid")
+                    raise ValueError("'pyModuleName' Parameter invalid")
                 setattr(pyModule,modulePrefix+label,m)
                 if self.sequence == None:
-                   self.sequence=m
+                    self.sequence = m
                 else:
-                  self.sequence*=m
+                    self.sequence *= m
                 #Create an entry in the random number service
                 randomSet = randomPSet.clone()
                 randomSet.initialSeed = random.randint(100000,300000)
                 setattr(randomNumberService,modulePrefix+label,randomSet)
 
-
             #eta
-            etaValues = [-etaShift,etaShift]
-            etaLabels = ['EtaShiftDown' ,'EtaShiftUp']
+            etaValues = [ -etaShift, etaShift]
+            etaLabels = [ 'EtaShiftDown', 'EtaShiftUp' ]
 
-            for label,value in zip(etaLabels,etaValues):
-                m =module.clone()
+            for label, value in zip(etaLabels, etaValues):
+                m = module.clone()
                 m.deltaEta = cms.double(value)
                 m.setLabel(modulePrefix+label)
                 #Register the filter in the namespace
                 pyModule = sys.modules[self.pyModuleName[0]]
                 if pyModule is None:
-                   raise ValueError("'pyModuleName' Parameter invalid")
-                setattr(pyModule,modulePrefix+label,m)
+                    raise ValueError("'pyModuleName' Parameter invalid")
+                setattr(pyModule, modulePrefix + label, m)
                 if self.sequence == None:
-                   self.sequence=m
+                    self.sequence = m
                 else:
-                  self.sequence*=m
+                    self.sequence *= m
                 randomSet = randomPSet.clone()
-                randomSet.initialSeed = random.randint(100000,300000)
-                setattr(randomNumberService,modulePrefix+label,randomSet)
+                randomSet.initialSeed = random.randint(100000, 300000)
+                setattr(randomNumberService, modulePrefix+label, randomSet)
 
             #phi
-            phiValues = [-phiShift,phiShift]
-            phiLabels = ['PhiShiftDown' ,'PhiShiftUp']
+            phiValues = [ -phiShift, phiShift ]
+            phiLabels = [ 'PhiShiftDown', 'PhiShiftUp' ]
 
-            for label,value in zip(phiLabels,phiValues):
-                m =module.clone()
+            for label, value in zip(phiLabels, phiValues):
+                m = module.clone()
                 m.deltaPhi = cms.double(value)
                 m.setLabel(modulePrefix+label)
                 #Register the filter in the namespace
                 pyModule = sys.modules[self.pyModuleName[0]]
                 if pyModule is None:
-                   raise ValueError("'pyModuleName' Parameter invalid")
-                setattr(pyModule,modulePrefix+label,m)
+                    raise ValueError("'pyModuleName' Parameter invalid")
+                setattr(pyModule, modulePrefix + label, m)
                 if self.sequence == None:
-                   self.sequence=m
+                    self.sequence = m
                 else:
-                  self.sequence*=m
+                    self.sequence *= m
                 randomSet = randomPSet.clone()
-                randomSet.initialSeed = random.randint(100000,300000)
-                setattr(randomNumberService,modulePrefix+label,randomSet)
+                randomSet.initialSeed = random.randint(100000, 300000)
+                setattr(randomNumberService, modulePrefix + label, randomSet)
 
             return cms.Sequence(self.sequence)
