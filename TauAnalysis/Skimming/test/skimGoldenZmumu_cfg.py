621224b81dc8adc67fc3bc21f415fc51b72a6a03
veelken <>
2010-10-24 12:21:40 +0000

    - do not require muon isolation on one mu+/mu- leg - added ZmumuEventContent

--- TauAnalysis/Skimming/test/skimGoldenZmumu_cfg.py
+++ TauAnalysis/Skimming/test/skimGoldenZmumu_cfg.py
@@ -16,11 +16,14 @@ process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = cms.string('GR_R_38X_V13A::All')
 
-# import definition of FEVT (RAW + RECO) event content
-from Configuration.EventContent.EventContent_cff import *
+# import event content definition:
+# keep full FEVT (RAW + RECO) event content
+# plus collections of goodMuons, goldenZmumuCandidates and "tag" & "probe" flags
+from TauAnalysis.Skimming.goldenZmmEventContent_cff import *
 
 # load definition of VBTF Z --> mu+ mu- event selection
-process.load("ElectroWeakAnalysis.Utilities.goldenZmmSelectionVBTF_cfi")
+# (with no isolation cuts applied on one of the two muons)
+process.load("TauAnalysis.Skimming.goldenZmmSelectionVBTFnoMuonIsolation_cfi")
 
 # load definitions of data-quality filters
 process.load("TauAnalysis.TauIdEfficiency.filterDataQuality_cfi")
@@ -31,17 +34,16 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        ##'file:/data1/veelken/CMSSW_3_6_x/skims/pseudoData_Ztautau.root'
         'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_1_1_adj.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_2_1_3uZ.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_3_1_PXh.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_4_1_yHg.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_5_1_I5Q.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_6_1_9m7.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_7_1_y8p.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_8_1_6fE.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_9_1_ejQ.root',
-	'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_10_1_Kst.root'
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_2_1_3uZ.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_3_1_PXh.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_4_1_yHg.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_5_1_I5Q.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_6_1_9m7.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_7_1_y8p.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_8_1_6fE.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_9_1_ejQ.root',
+	##'rfio:/castor/cern.ch/user/s/smaruyam/ZmmSkim/smaruyam/Mu/CMSSW_3_8_5_ZmmSkimV4/88174dd9b4b05e756497e260fa3e4f76/goldenZmumuEvents_RAW_RECO_10_1_Kst.root'
     )
 )
 
@@ -51,7 +53,7 @@ process.source = cms.Source("PoolSource",
 
 process.goldenZmumuSkimPath = cms.Path(
     process.dataQualityFilters 
-   + process.goldenZmmSelectionSequence
+   + process.goldenZmumuSelectionSequence
 )
 
 goldenZmumuEventSelection = cms.untracked.PSet(
@@ -65,9 +67,9 @@ goldenZmumuEventSelection = cms.untracked.PSet(
 #--------------------------------------------------------------------------------
 
 process.goldenZmumuSkimOutputModule = cms.OutputModule("PoolOutputModule",                                 
-    FEVTEventContent,                                               
+    goldenZmumuEventContent,
     goldenZmumuEventSelection,
-    fileName = cms.untracked.string('goldenZmumuEvents_RAW_RECO.root')                                                  
+    fileName = cms.untracked.string('goldenZmumuEvents_runs132440to144114_RAW_RECO.root')
 )
 
 process.options = cms.untracked.PSet(
@@ -76,3 +78,5 @@ process.options = cms.untracked.PSet(
 
 process.o = cms.EndPath(process.goldenZmumuSkimOutputModule)
 
+# print-out all python configuration parameter information
+#print process.dumpPython()
