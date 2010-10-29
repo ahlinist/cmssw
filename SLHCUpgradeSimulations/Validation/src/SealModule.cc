27776b07714b0bf82bb7c0bf43270704fdb93b35
hcheung <>
2010-10-29 13:14:55 +0000

    Add track selector that filters on tracks used in b-tagging

--- SLHCUpgradeSimulations/Validation/src/SealModule.cc
+++ SLHCUpgradeSimulations/Validation/src/SealModule.cc
@@ -3,6 +3,8 @@
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPrimaryVertexAnalyzer.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCPixelHitAnalyzer.h"
 #include "SLHCUpgradeSimulations/Validation/interface/SLHCAllPixelHitAnalyzer.h"
+#include "CommonTools/RecoAlgos/interface/TrackSelector.h"
+#include "SLHCUpgradeSimulations/Validation/interface/SLHCRecoTrackSelector.h"
 
 using namespace edm;
 using namespace reco;
@@ -10,3 +12,9 @@ using namespace reco;
 DEFINE_FWK_MODULE(SLHCPrimaryVertexAnalyzer);
 DEFINE_FWK_MODULE(SLHCPixelHitAnalyzer);
 DEFINE_FWK_MODULE(SLHCAllPixelHitAnalyzer);
+namespace reco {
+  typedef ObjectSelector<SLHCRecoTrackSelector> SLHCRecoTrackSelector;
+  DEFINE_FWK_MODULE(SLHCRecoTrackSelector);
+}
+
+
