674aad6f16743cb2f06d7e47f7920462f7caefa7
veelken <>
2010-10-30 14:22:39 +0000

    first check-in of config file specying user preferences for file paths

---
+++ TauAnalysis/Configuration/python/userRegistry.py
@@ -0,0 +1,54 @@
+import FWCore.ParameterSet.Config as cms
+
+import os
+
+userSettings = {
+    'friis' : {
+        'AHtoMuTau' : {
+            'analysisFilePath' : '/user/f/friis/AHtoMuTau/',
+            'harvestingFilePath' : "/data1/friis/",            
+            'tmpFilePath' : "/tmp/friis/",
+            'jobId' : 'Run10'
+        }
+    },
+    'veelken': {
+        'AHtoMuTau' : {
+            'analysisFilePath' : "/user/v/veelken/CMSSW_3_8_x/plots/AHtoMuTau/",
+            'harvestingFilePath' : "/data1/veelken/CMSSW_3_8_x/plots/AHtoMuTau/",
+            'tmpFilePath' : "/tmp/veelken/",
+            'jobId' : "7TeV"
+         },
+         'ZtoMuTau' : {
+            'analysisFilePath' : "/user/v/veelken/CMSSW_3_8_x/plots/ZtoMuTau/",
+            'harvestingFilePath' : "/data1/veelken/CMSSW_3_8_x/plots/ZtoMuTau/",
+            'tmpFilePath' : "/tmp/veelken/",
+            'jobId' : "2010Nov01"
+         },
+         'ZtoMuTau_bgEstTemplate' : {
+            'analysisFilePath' : "/user/v/veelken/CMSSW_3_8_x/plots/ZtoMuTau_bgEstTemplate/",
+            'harvestingFilePath' : "/data1/veelken/CMSSW_3_8_x/plots/ZtoMuTau_bgEstTemplate/",
+            'tmpFilePath' : "/tmp/veelken/",
+            'jobId' : "7TeV"
+         },
+         'ZtoDiTau' : {
+            'analysisFilePath' : "/user/v/veelken/CMSSW_3_8_x/plots/ZtoDiTau/",
+            'harvestingFilePath' : "/data1/veelken/CMSSW_3_8_x/plots/ZtoDiTau/",
+            'tmpFilePath' : "/tmp/veelken/",
+            'jobId' : "2010Nov01"
+         }
+    }
+}
+
+def getAnalysisFilePath(channel):
+    return userSettings[os.environ['LOGNAME']][channel]['analysisFilePath']
+
+def getHarvestingFilePath(channel):
+    return userSettings[os.environ['LOGNAME']][channel]['harvestingFilePath']
+
+def getTmpFilePath(channel):
+    return userSettings[os.environ['LOGNAME']][channel]['tmpFilePath']
+
+def getJobId(channel):
+    return userSettings[os.environ['LOGNAME']][channel]['jobId']
+
+
