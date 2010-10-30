ba37b1b489e905b5b8d12872651ab5c0bd2a80eb
veelken <>
2010-10-30 14:22:05 +0000

    replaced prepareHarvestingCHANNEL_grid.py by new (simplified) harvestCHANNEL_grid.py scripts

---
+++ TauAnalysis/Configuration/test/harvestZtoMuTauPlots_grid.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsZtoMuTau_7TeV
+from TauAnalysis.Configuration.harvestAnalysisResults_grid import harvestAnalysisResults
+from TauAnalysis.Configuration.userRegistry import import getAnalysisFilePath, getHarvestingFilePath, getJobId, getTmpFilePath
+
+channel = 'ZtoMuTau'
+analysisFilePath = getAnalysisFilePath(channel)
+harvestingFilePath = getHarvestingFilePath(channel)
+jobId = getJobId(channel)
+tmpFilePath = getTmpFilePath(channel)
+
+harvestAnalysisResults(channel = channel, samples = recoSampleDefinitionsZtoMuTau_7TeV,
+                       inputFilePath = analysisFilePath, outputFilePath = harvestingFilePath, jobId = jobId,
+                       tmpFilePath = tmpFilePath)
