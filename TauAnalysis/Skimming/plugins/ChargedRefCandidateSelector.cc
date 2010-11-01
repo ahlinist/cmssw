398e0b455d787a636b81e632fedd99513e094f73
veelken <>
2010-11-01 16:08:31 +0000

    first check-in of config files + code to perform loose skimming of Zmumu candidates (for muon reco., id. , iso. and trigger efficiency measurement)

---
+++ TauAnalysis/Skimming/plugins/ChargedRefCandidateSelector.cc
@@ -0,0 +1,9 @@
+#include "TauAnalysis/Skimming/plugins/ChargedRefCandidateSelector.h"
+
+using namespace reco;
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(ChargedRefCandidateSelector);
+
+DEFINE_FWK_MODULE(ChargedRefCandidateRefSelector);
