eb66f401bdd3cda0f3bee7d7152349ca219baabf
gpetrucc <>
2010-11-03 12:14:02 +0000

    Add a few extra plots

--- MuonAnalysis/Examples/python/inclusiveMuonPlots_cfi.py
+++ MuonAnalysis/Examples/python/inclusiveMuonPlots_cfi.py
@@ -16,17 +16,19 @@ def _evenBins(min,max,delta):
         ret.append(x)
     return ret 
 
-def makeInclusiveMuonPlots(rebinFactor=1):
+def makeInclusiveMuonPlots(rebinFactor=1,ptScale=1):
     return cms.PSet(        # ---- Muons per event ----
         nMuonsBins = _iBins(7),
         # ---- Kinematics ----
-        ptBins = _evenBins( 0, 30, 0.25 * rebinFactor),
-        pBins  = _evenBins( 0, 50, 0.5  * rebinFactor),
+        ptBins = _evenBins( 0, 30*ptScale, 0.25 * rebinFactor * ptScale),
+        pBins  = _evenBins( 0, 50*ptScale, 0.5  * rebinFactor * ptScale),
         etaBins = _evenBins( -2.6, 2.6, 0.2 * rebinFactor),
         phiBins = _evenBins(-pi, pi, pi/16 * rebinFactor),
         chargeBins = cms.vdouble(-2,0,2),
-        qptBins = _evenBins( -30, 30, 0.25 * rebinFactor),
-        qpBins  = _evenBins( -50, 50, 0.5  * rebinFactor),
+        qptBins = _evenBins( -30*ptScale, 30*ptScale, 0.25 * rebinFactor * ptScale),
+        qpBins  = _evenBins( -50*ptScale, 50*ptScale, 0.5  * rebinFactor * ptScale),
+        ptErrorOverPtBins = _nBins(20*rebinFactor, 0, 0.2),
+        ptStaOverPtBins   = _nBins(40*rebinFactor, 0, 2.0),
         # ---- Vertex ----
         dxyFineBins = _evenBins(-0.2, 0.2, 0.005), #  50um
         dzFineBins  = _evenBins(-0.5, 0.5, 0.010), # 100um
@@ -36,6 +38,13 @@ def makeInclusiveMuonPlots(rebinFactor=1):
         ip3dBins = _evenBins(0, 0.2, 0.0050), #  50um
         tipSigBins  = _evenBins(0, 10, 0.25),
         ip3dSigBins = _evenBins(0, 15, 0.25),
+        # ---- Signed variants of the above when jets are available ----
+        stipBins  = _evenBins(-0.1, 0.1, 0.0025), #  25um
+        sip3dBins = _evenBins(-0.2, 0.2, 0.0050), #  50um
+        sdl3dBins = _evenBins(-0.2, 0.2, 0.0050), #  50um
+        stipSigBins  = _evenBins(-10, 10, 0.25),
+        sip3dSigBins = _evenBins(-15, 15, 0.25),
+        sdl3dSigBins = _evenBins(-15, 15, 0.25),
         # ---- Tracks ----
         pixelHitsBins       = _iBins(8),
         pixelLayersBins     = _iBins(5),
@@ -59,7 +68,13 @@ def makeInclusiveMuonPlots(rebinFactor=1):
         timeAtIpInOutBins    = _evenBins(-100., 100., 5.0),
         timeAtIpInOutSigBins = _nBins(50/rebinFactor, -25, 25),
         # ---- Phi at MB/ME 1 Surface ----
-        trkPhiAtSurfaceBins = _nBins(37,-3.23,3.23)
+        trkPhiAtSurfaceBins = _nBins(37,-3.23,3.23),
+        # ---- Extra kinematics ---
+        ptRelBins = _evenBins( 0, 10*ptScale, 0.1 * rebinFactor * ptScale),
+        mtBins  = _evenBins( 0, 140, 5 * rebinFactor),
+        metBins = _evenBins( 0, 140, 5 * rebinFactor),
+        jetMuonPtRatioBins     = _nBins(20/rebinFactor, 0, 2.0),
+        oppoJetMuonPtRatioBins = _nBins(20/rebinFactor, 0, 5.0),
     )
 
 inclusiveMuonPlots = cms.EDAnalyzer("InclusiveMuonPlots",
