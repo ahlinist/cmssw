10cf43aaea75271cafa1eb68ae1256c8e9623cbf
lgray <>
2010-11-03 11:46:33 +0000

    Incremental

--- ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/aTGCRooStats.py
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/aTGCRooStats.py
@@ -18,37 +18,44 @@ def main(options):
     ws = ROOT.RooWorkspace(str(options.workspaceName))
 
     ws.Print("v")
-
-
+    
     setupWorkspace(dataTree,mcTree,ws,options)
 
-    theNLL = ROOT.RooNLLVar('aTGCNLL','The -log(likelihood) for the dataset',
+    #create -log(likelihood)
+    theNLL = ROOT.RooNLLVar(options.couplingType+'_aTGCNLL',
+                            'The -log(likelihood) for the dataset',
                             ws.function('corePoisson'),ws.data('aTGCData'))
 
+    #create profile likelihood, set POI's
+    theProfileLL = ROOT.RooProfileLL(options.couplingType+'_ProfileLLaTGC',
+                                     'The Profile Log-Likelihood',
+                                     theNLL,ROOT.RooArgSet(ws.var(options.couplingType+'_h3'),
+                                                           ws.var(options.couplingType+'_h4')))
 
-    theProfileLL = ROOT.RooProfileLL('ProfileLLaTGC','The Profile Loglikelihood',
-                                     theNLL,ROOT.RooArgSet(*ws.var(options.couplingType+'_h3'),
-                                                           *ws.var(options.couplingType+'_h4')))
-    
-    theLHInterval = ROOT.RooStats.LikelihoodInterval('aTGCLikelihoodInterval',theProfileLL,
-                                                     ROOT.RooArgSet(*ws.var(options.couplingType+'_h3'),
-                                                                    *ws.var(options.couplingType+'_h4')))
-    
+    #calculate the .95 confidence interval
+    theLHInterval = ROOT.RooStats.LikelihoodInterval(options.couplingType+'_aTGCLikelihoodInterval',
+                                                     theProfileLL,
+                                                     ROOT.RooArgSet(ws.var(options.couplingType+'_h3'),
+                                                                    ws.var(options.couplingType+'_h4')))
+    theLHInterval.SetConfidenceLevel(.95) # .95 confidence interval
+
+    #create the interval plotter, set POI's, ranges
     theLHplot = ROOT.RooStats.LikelihoodIntervalPlot(theLHInterval)
-
+    theLHplot.SetPlotParameters(ROOT.RooArgSet(ws.var(options.couplingType+'_h3'),
+                                               ws.var(options.couplingType+'_h4')))
+    theLHplot.SetRange(-options.h3Max,-options.h4Max,
+                       options.h3Max,options.h4Max)
+    
+    #save all the likelihood info
     getattr(ws,'import')(theNLL)
     getattr(ws,'import')(theProfileLL)
     getattr(ws,'import')(theLHInterval)
-    
 
-    print "HelloWorld"
-    
+    #really, that's all I had to do??
+
 
 def setupWorkspace(dataTree,mcTree,ws,options):
-    # what we do here is make two RooDataSets/Hists with variables pT, acceptance, acceptance error, number of background,
-    # background frac. error (for data and background), respectively.
-    # Luminosity and lumi error are defined from commandline.
-    # What we need is a space binned in h3,h4,pT... we have to do this in a funny way, though...
+    # explanation forthcoming...
     if not isinstance(ws,ROOT.RooWorkspace):
         print "You didn't pass a RooWorkspace!"
         exit(1)
@@ -64,18 +71,32 @@ def setupWorkspace(dataTree,mcTree,ws,options):
     bkgErr = ROOT.RooRealVar('backgroundError','Fractional Error on the expected number of background events')
     lumiErr = ROOT.RooRealVar('luminosityError','Fractional Error on the luminosity')
     
+    polyC=[]
+    polyP_0=[]
+    polyP_1=[]
+    polyP_2=[]
+    polyP_3=[]
+    polyP_4=[]
+    nExpectedSignal=[]
 
     #this needs a little care
-    polyC = ROOT.RooRealVar('polyC','Constant Term for aTGC polynomial description')
-    polyP_0 = ROOT.RooRealVar('polyP_0','Linear h_3 Term for aTGC polynomial description')
-    polyP_1 = ROOT.RooRealVar('polyP_1','Linear h_4 Term for aTGC polynomial description')
-    polyP_2 = ROOT.RooRealVar('polyP_2','h_3*h_4 Term for aTGC polynomial description')
-    polyP_3 = ROOT.RooRealVar('polyP_3','Quadratic h_3 Term for aTGC polynomial description')
-    polyP_4 = ROOT.RooRealVar('polyP_4','Quadratic h_4 Term for aTGC polynomial description')
-    
-    nExpectedSignal = ROOT.RooFormulaVar('nExpectedSignal','The expected number of signal events in (h3,h4)'
-                                         '@2 + @3*@0 + @4*@1 + @5*@0*@1 + @6*@0*@0 + @7*@1*@1',
-                                         RooArgList(h3,h4,polyC,polyP_0,polyP_1,polyP_2,polyP_3,polyP_4))
+    for i in range(options.nEtBins):
+        polyC.append(ROOT.RooRealVar('polyC'+i,'Constant Term for aTGC polynomial description in '+i+'th pt bin'))
+        polyP_0.append(ROOT.RooRealVar('polyP_0'+i,'Linear h_3 Term for aTGC polynomial description'+i+'th pt bin'))
+        polyP_1.append(ROOT.RooRealVar('polyP_1'+i,'Linear h_4 Term for aTGC polynomial description'+i+'th pt bin'))
+        polyP_2.append(ROOT.RooRealVar('polyP_2'+i,'h_3*h_4 Term for aTGC polynomial description'+i+'th pt bin'))
+        polyP_3.append(ROOT.RooRealVar('polyP_3'+i,'Quadratic h_3 Term for aTGC polynomial description'+i+'th pt bin'))
+        polyP_4.append(ROOT.RooRealVar('polyP_4'+i,'Quadratic h_4 Term for aTGC polynomial description'+i+'th pt bin'))
+        nExpectedSignal.append(ROOT.RooFormulaVar('nExpectedSignal','The expected number of signal events in (h3,h4) in'+i+'th pt bin',
+                                                  '@2 + @3*@0 + @4*@1 + @5*@0*@1 + @6*@0*@0 + @7*@1*@1',
+                                                  RooArgList(h3,h4,polyC[i],polyP_0[i],polyP_1[i],polyP_2[i],polyP_3[i],polyP_4[i])))
+        getattr(ws,'import')(polyC[i])
+        getattr(ws,'import')(polyP_0[i])
+        getattr(ws,'import')(polyP_1[i])
+        getattr(ws,'import')(polyP_2[i])
+        getattr(ws,'import')(polyP_3[i])
+        getattr(ws,'import')(polyP_4[i])
+        getattr(ws,'import')(nExpectedSignal[i])
     #we're not done with this yet, but we're going to put it in the workspace
 
     
@@ -90,13 +111,7 @@ def setupWorkspace(dataTree,mcTree,ws,options):
     getattr(ws,'import')(h4)
     getattr(ws,'import')(acc)
     getattr(ws,'import')(acc_err)
-    getattr(ws,'import')(polyC)
-    getattr(ws,'import')(polyP_0)
-    getattr(ws,'import')(polyP_1)
-    getattr(ws,'import')(polyP_2)
-    getattr(ws,'import')(polyP_3)
-    getattr(ws,'import')(polyP_4)
-    getattr(ws,'import')(nExpectedSignal)
+    
     getattr(ws,'import')(nExpectedBackground)
     getattr(ws,'import')(evSelErr)
     getattr(ws,'import')(bkgErr)
@@ -105,9 +120,7 @@ def setupWorkspace(dataTree,mcTree,ws,options):
 
     aTGCGrid = createATGCGrid(mcTree,options)
 
-    
-    
-    
+        
 def fitATGCExpectedYield(mcTree,options):
     #create the variables for the 3x3 grid
     h3_3x3 = ROOT.RooRealVar('h3_3x3','temp h3 to extrapolate grid',-options.h3Max,options.h3Max)
@@ -141,8 +154,10 @@ def makeATGCExpectationPdf(ws):
     ws.factory("RooPoisson::corePoisson(pho_et,nExpectedSignal(pT)*err_x_gs*err_x_gl+nExpectedBackground*err_x_gb)")
       
 
-def makePlots():
+def makePlots(LLInterval,options):
     print "not done yet"
+    theCanvas = ROOT.TCanvas("Likelihood Plot")
+    
 
 if __name__ == "__main__":
     parser = OptionParser(description="aTGCRooStats: A RooStats Implementation of Anomalous Triple Gauge Coupling Analysis.",
