18410d4da2f89382ac166608c13697ad5f5ddd25
lgray <>
2010-11-08 11:20:26 +0000

    Hmmm, but do I believe it!?!!?

--- ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python aTGCRooStats.py --workspaceName=test --couplingType=ZgZ --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=70 --nEtBins=6 --intLumi=15.0 --lumiErr=0.1 --h3Max=0.20 --h4Max=.012 --inputData=/scratch/lgray/zgamma_workdir/Data_mm.root --inputMC=ZgZ_MC_merged.root --MCbackground --backgroundFile=ZJets_mm_15pb.root 
+python aTGCRooStats.py --workspaceName=test --couplingType=ZgZ --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=70 --nEtBins=6 --intLumi=15.0 --lumiErr=0.1 --h3Max=.3 --h4Max=.01 --inputData=/scratch/lgray/zgamma_workdir/Data_mm.root --inputMC=ZgZ_MC_merged.root --MCbackground --backgroundFile=ZJets_mm_15pb.root 
