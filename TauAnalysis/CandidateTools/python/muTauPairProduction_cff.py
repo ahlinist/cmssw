02bdee809d50103b74deaddafab2229e1e6959a9
friis <>
2010-11-08 15:49:42 +0000

    Add commented out track likelihoods

--- TauAnalysis/CandidateTools/python/muTauPairProduction_cff.py
+++ TauAnalysis/CandidateTools/python/muTauPairProduction_cff.py
@@ -74,6 +74,18 @@ allMuTauPairs = cms.EDProducer("PATMuTauPairProducer",
                 numSamplings = cms.int32(-1)
             )
         ),
+        #psKine_MEt_Track_ptBalance = cms.PSet(
+            #likelihoodFunctions = cms.VPSet(
+                #svFitLikelihoodMuTauPairKinematicsPhaseSpace,
+                #svFitLikelihoodMuTauPairMEt,
+                #svFitLikelihoodMuTauPairTrackInfo,
+                #svFitLikelihoodMuTauPairPtBalance
+            #),
+            #estUncertainties = cms.PSet(
+                ##numSamplings = cms.int32(1000)
+                #numSamplings = cms.int32(-1)
+            #)
+        #),
         #polKine = cms.PSet(
             #likelihoodFunctions = cms.VPSet(
                 #svFitLikelihoodMuTauPairKinematicsPolarized
