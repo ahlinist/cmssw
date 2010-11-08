eaf815c15f1d23263193fb59fa1db76bdb692106
lgray <>
2010-11-08 10:18:32 +0000

    Hmm, looks like I *can* integrate the likelihood directly.. Neat.

--- ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python aTGCRooStats.py --workspaceName=test --couplingType=ZgZ --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=70 --nEtBins=6 --intLumi=15.0 --lumiErr=0.1 --h3Max=0.12 --h4Max=.004 --inputData=/scratch/lgray/zgamma_workdir/Data_mm.root --inputMC=ZgZ_MC_merged.root --MCbackground --backgroundFile=ZJets_mm_15pb.root 
+python aTGCRooStats.py --workspaceName=test --couplingType=ZgZ --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=70 --nEtBins=6 --intLumi=15.0 --lumiErr=0.1 --h3Max=0.20 --h4Max=.012 --inputData=/scratch/lgray/zgamma_workdir/Data_mm.root --inputMC=ZgZ_MC_merged.root --MCbackground --backgroundFile=ZJets_mm_15pb.root 
