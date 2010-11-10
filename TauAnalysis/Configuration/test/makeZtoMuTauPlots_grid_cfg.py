3ec41b5f3d2d18cbc57849a3371afdf4d6d53f77
veelken <>
2010-11-10 08:18:02 +0000

    first check-in of new (even more) simplified version of plotting code

--- TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
@@ -1,74 +1,34 @@
 import FWCore.ParameterSet.Config as cms
 
-INPUT_FILES_DIR = '/data1/veelken/CMSSW_3_6_x/plots/ZtoMuTau'
-sample_mapper = lambda sample : "harvested_ZtoMuTau_%s_7TeV.root" % sample
-
-process = cms.Process('makeZtoMuTauPlots')
-
-import TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi as samples
-
-process.load("TauAnalysis.Configuration.dumpZtoMuTau_grid_cff")
-process.load("TauAnalysis.Configuration.plotZtoMuTau_grid_cff")
-
-process.DQMStore = cms.Service("DQMStore")
-
-process.maxEvents = cms.untracked.PSet(            
-    input = cms.untracked.int32(0)         
-)
-
-process.source = cms.Source("EmptySource")
-
-# define directory from which .root files containing histograms 
-# for individual processes get loaded
-process.loadZtoMuTauSamples.inputFilePath = cms.string(INPUT_FILES_DIR)
-
-# Update our input files
-for sample in samples.FLATTENED_SAMPLES_TO_PLOT:
-    getattr(process.loadZtoMuTauSamples, sample).inputFileNames = cms.vstring(
-        sample_mapper(sample)
-    )
+from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsZtoMuTau_7TeV
+from TauAnalysis.Configuration.plotZtoMuTau_drawJobs_cfi import *
+from TauAnalysis.Configuration.makePlots2_grid import makePlots
+from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath, getJobId
 
 # import utility function to enable factorization
 from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeZtoMuTauPlots_grid
 
-samplesToFactorize = [sample for sample in samples.FLATTENED_SAMPLES_TO_PLOT 
-                      if samples.ALL_SAMPLES[sample].get('factorize', False)]
+process = cms.Process('makeZtoMuTauPlots')
 
-relevantMergedSamples = [sample for sample, sample_info in samples.MERGE_SAMPLES.iteritems() if 
-                         [subsample for subsample in sample_info['samples'] 
-                          if subsample in samplesToFactorize]]
+process.load("TauAnalysis.Configuration.dumpZtoMuTau_grid_cff")
+##process.load("TauAnalysis.Configuration.plotZtoMuTau_grid_cff")
 
-print "Factorizing", samplesToFactorize
-print "Updating", relevantMergedSamples
+channel = 'ZtoMuTau'
+inputFilePath = getHarvestingFilePath(channel)
+jobId = getJobId(channel)
 
-enableFactorization_makeZtoMuTauPlots_grid(
-    process,
-    samplesToFactorize = samplesToFactorize,
-    relevantMergedSamples = relevantMergedSamples,
-    mergedToRecoSampleDict = samples.MERGE_SAMPLES,
-)
-
-process.plotZtoMuTau.labels.mcNormScale.text = cms.vstring(
-    '%0.1fpb^{-1}' % samples.TARGET_LUMI,
-    '#sqrt{s}=7TeV'
-)
-
-process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
-
-# define name and directory in which .root file containing all histograms gets saved
-process.saveZtoMuTau.outputFileName = cms.string(
-    "/data1/veelken/CMSSW_3_6_x/plots/ZtoMuTau/plotsZtoMuTau_all.root"
-)
-
-process.makeZtoMuTauPlots = cms.Sequence(
-    process.loadZtoMuTau
-   #+ process.dumpDQMStore
-   + process.saveZtoMuTau
-   + process.dumpZtoMuTau
-   + process.plotZtoMuTau
-)
-
-process.p = cms.Path(process.makeZtoMuTauPlots)
+makePlots(process, channel = channel, samples = recoSampleDefinitionsZtoMuTau_7TeV,
+          inputFilePath = inputFilePath, jobId = jobId,
+          analyzer_drawJobConfigurator_indOutputFileName_sets = [
+              [ "zMuTauAnalyzer", drawJobConfigurator_ZtoMuTau, "plotZtoMuTau_#PLOT#.png" ]
+          ],
+          drawJobTemplate = plots_ZtoMuTau,
+          enableFactorizationFunction = enableFactorization_makeZtoMuTauPlots_grid,
+          dqmDirectoryFilterStatistics = {
+              'factorizationDisabled' : 'zDiTauAnalyzer/FilterStatistics',
+              'factorizationEnabled' : 'zDiTauAnalyzer_factorizedTight2ndTau/FilterStatistics'
+          },
+          dumpDQMStore = False)
 
 # print-out all python configuration parameter information
 #print process.dumpPython()
