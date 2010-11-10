339adffc4399235986fa101ceb629dbd085a5dda
veelken <>
2010-11-10 08:33:53 +0000

    first check-in of new (even more) simplified version of plotting code

--- TauAnalysis/BgEstimationTools/test/prodBgEstTemplatesZtoMuTau_cfg.py
+++ TauAnalysis/BgEstimationTools/test/prodBgEstTemplatesZtoMuTau_cfg.py
@@ -1,133 +1,85 @@
 import FWCore.ParameterSet.Config as cms
 
+from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsZtoMuTau_7TeV
+from TauAnalysis.Configuration.makePlots2_grid import makePlots
+from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath, getJobId
+
+# import utility function to enable factorization
+from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeZtoMuTauPlots_grid
+
 process = cms.Process('prodBgEstTemplatesZtoMuTau')
 
-process.load("TauAnalysis.Configuration.dumpZtoMuTau_cff")
-process.load("TauAnalysis.Configuration.plotZtoMuTau_cff")
+channel = 'ZtoMuTau_bgEstTemplate'
+inputFilePath = getHarvestingFilePath(channel)
+jobId = getJobId(channel)
 
-process.DQMStore = cms.Service("DQMStore")
-
-process.maxEvents = cms.untracked.PSet(            
-    input = cms.untracked.int32(0)         
-)
-
-process.source = cms.Source("EmptySource")
-
-# define directory from which .root files containing the histograms get loaded
-process.loadZtoMuTau.inputFilePath = cms.string("rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_6_x/bgEstPlots/ZtoMuTau_bgEstTemplates/7TeV/")
-
-process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
-
-process.addZtoMuTauBgEstTemplates_qcdSum = cms.EDAnalyzer("DQMHistAdder",
-    qcdSum = cms.PSet(
-        dqmDirectories_input = cms.vstring(
-            ##'harvested/InclusivePPmuX/',
-            'harvested/PPmuXptGt20/'
-        ),
-        dqmDirectory_output = cms.string('harvested/qcdSum/')
-    )                          
-)
-
-process.addZtoMuTauBgEstTemplates_smSum = cms.EDAnalyzer("DQMHistAdder",
-    smSum = cms.PSet(
-        dqmDirectories_input = cms.vstring(
-            'harvested/Ztautau/',
-            'harvested/Zmumu/',
-            'harvested/WplusJets/',
-            'harvested/TTplusJets/',
-            'harvested/qcdSum/'
-        ),
-        dqmDirectory_output = cms.string('harvested/smSum/')
-    )
-)
-
-process.addZtoMuTauBgEstTemplates = cms.Sequence(process.addZtoMuTauBgEstTemplates_qcdSum + process.addZtoMuTauBgEstTemplates_smSum)
-
-process.saveZtoMuTau.outputFileName = cms.string('bgEstTemplatesZtoMuTau.root')
+makePlots(process, channel = channel, samples = recoSampleDefinitionsZtoMuTau_7TeV,
+          inputFilePath = inputFilePath, jobId = jobId,
+          analyzer_drawJobConfigurator_indOutputFileName_sets = [
+              [ 'BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched' ],
+              [ 'BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched' ],
+              [ 'BgEstTemplateAnalyzer_WplusJetsEnriched' ],
+              [ 'BgEstTemplateAnalyzer_TTplusJetsEnriched' ],
+              [ 'BgEstTemplateAnalyzer_QCDenriched' ]
+          ],
+          dqmDirectoryFilterStatistics = {
+              'factorizationDisabled' : 'BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/',
+              'factorizationEnabled' : 'BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'
+          },
+          dumpDQMStore = False)
 
 #--------------------------------------------------------------------------------
 # Print-out cut-flow information for background enriched samples
 # from which template histograms are obtained
-process.dumpZtoMuTau_bgEstZmumuJetMisIdEnriched = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        Ztautau = cms.string('harvested/Ztautau/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/'),
-        Zmumu = cms.string('harvested/Zmumu/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/'),
-        QCD = cms.string('harvested/qcdSum/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/'),
-        smSum = cms.string('harvested/smSum/BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics/')
-    ),
-    columnsSummaryTable = cms.vstring("Passed"),
-    printSummaryTableOnly = cms.bool(True)                                                    
-)
+#
+def configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess, dqmDirectoryFilterStatisticsTable):
+    module = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
+        dqmDirectories = cms.PSet(),
+        columnsSummaryTable = cms.vstring("Passed"),
+        printSummaryTableOnly = cms.bool(True)                                                    
+    )
 
-process.dumpZtoMuTau_bgEstZmumuMuonMisIdEnriched = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        Ztautau = cms.string('harvested/Ztautau/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-        Zmumu = cms.string('harvested/Zmumu/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-        QCD = cms.string('harvested/qcdSum/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-        smSum = cms.string('harvested/smSum/BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/'),
-    ),
-    columnsSummaryTable = cms.vstring("Passed"),
-    printSummaryTableOnly = cms.bool(True)                                                    
-)
+    for processName, dqmDirectoryProcess in dqmDirectoriesProcess.items():
+        dqmDirectory_full = 'harvested' + '/' + dqmDirectoryProcess + '/' + dqmDirectoryFilterStatisticsTable + '/'
+        setattr(module.dqmDirectories, processName, cms.string(dqmDirectory_full))
 
-process.dumpZtoMuTau_bgEstWplusJetsEnriched = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        Ztautau = cms.string('harvested/Ztautau/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/'),
-        Zmumu = cms.string('harvested/Zmumu/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/'),
-        QCD = cms.string('harvested/qcdSum/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/'),
-        smSum = cms.string('harvested/smSum/BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics/')
-    ),
-    columnsSummaryTable = cms.vstring("Passed"),
-    printSummaryTableOnly = cms.bool(True)       
-)
+    return module
 
-process.dumpZtoMuTau_bgEstTTplusJetsEnriched = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        Ztautau = cms.string('harvested/Ztautau/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/'),
-        Zmumu = cms.string('harvested/Zmumu/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/'),
-        QCD = cms.string('harvested/qcdSum/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/'),
-        smSum = cms.string('harvested/smSum/BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics/')
-    ),
-    columnsSummaryTable = cms.vstring("Passed"),
-    printSummaryTableOnly = cms.bool(True)       
-)
+dqmDirectoriesProcess = {
+    'Ztautau' : 'Ztautau',
+    'Zmumu' : 'Zmumu',
+    'WplusJets' : 'WplusJets',
+    'QCD' : 'qcdSum',
+    'TTplusJets' : 'TTplusJets',
+    'smSum' : 'smSum',
+    'Data' : 'data'
+}    
 
-process.dumpZtoMuTau_bgEstQCDenriched = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        Ztautau = cms.string('harvested/Ztautau/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'),
-        Zmumu = cms.string('harvested/Zmumu/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'),
-        QCD = cms.string('harvested/qcdSum/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/'),
-        smSum = cms.string('harvested/smSum/BgEstTemplateAnalyzer_QCDenriched/FilterStatistics/')
-    ),
-    columnsSummaryTable = cms.vstring("Passed"),
-    printSummaryTableOnly = cms.bool(True)       
-)
+process.dumpZtoMuTau_bgEstZmumuJetMisIdEnriched = configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess,
+    'BgEstTemplateAnalyzer_ZmumuJetMisIdEnriched/FilterStatistics')
+process.dumpZtoMuTau_bgEstZmumuMuonMisIdEnriched = configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess,
+    'BgEstTemplateAnalyzer_ZmumuMuonMisIdEnriched/FilterStatistics/')
+process.dumpZtoMuTau_bgEstWplusJetsEnriched = configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess,
+    'BgEstTemplateAnalyzer_WplusJetsEnriched/FilterStatistics')
+process.dumpZtoMuTau_bgEstTTplusJetsEnriched = configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess,
+    'BgEstTemplateAnalyzer_TTplusJetsEnriched/FilterStatistics')
+process.dumpZtoMuTau_bgEstQCDenriched = configureDQMDumpFilterStatisticsTable(dqmDirectoriesProcess,
+    'BgEstTemplateAnalyzer_QCDenriched/FilterStatistics')
+
+process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
 
 process.dumpZtoMuTauBgEstTemplates = cms.Sequence(
-    process.dumpZtoMuTau_bgEstZmumuJetMisIdEnriched + process.dumpZtoMuTau_bgEstZmumuMuonMisIdEnriched
+    process.dumpDQMStore
+   + process.dumpZtoMuTau_bgEstZmumuJetMisIdEnriched + process.dumpZtoMuTau_bgEstZmumuMuonMisIdEnriched
    + process.dumpZtoMuTau_bgEstWplusJetsEnriched
    + process.dumpZtoMuTau_bgEstTTplusJetsEnriched
    + process.dumpZtoMuTau_bgEstQCDenriched
 )    
 #--------------------------------------------------------------------------------
 
-process.p = cms.Path(
-    process.loadZtoMuTau
-  #+ process.dumpDQMStore
-   + process.addZtoMuTauBgEstTemplates
-   + process.dumpZtoMuTauBgEstTemplates 
-   + process.saveZtoMuTau
-)
+process.q = cms.Path(process.dumpZtoMuTauBgEstTemplates)
+
+process.schedule = cms.Schedule(process.p, process.q)
 
 # print-out all python configuration parameter information
-#print process.dumpPython()
+print process.dumpPython()
