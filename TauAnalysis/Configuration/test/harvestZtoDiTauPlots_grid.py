006cc68fe8d4590aee5f20d7dcc54468dd4f7e44
veelken <>
2010-11-11 11:13:19 +0000

    removed duplixate import statement

--- TauAnalysis/Configuration/test/harvestZtoDiTauPlots_grid.py
+++ TauAnalysis/Configuration/test/harvestZtoDiTauPlots_grid.py
@@ -2,7 +2,7 @@
 
 from TauAnalysis.Configuration.recoSampleDefinitionsZtoDiTau_7TeV_grid_cfi import recoSampleDefinitionsZtoDiTau_7TeV
 from TauAnalysis.Configuration.harvestAnalysisResults_grid import harvestAnalysisResults
-from TauAnalysis.Configuration.userRegistry import import getAnalysisFilePath, getHarvestingFilePath, getJobId, getTmpFilePath
+from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getHarvestingFilePath, getJobId, getTmpFilePath
 
 channel = 'ZtoDiTau'
 analysisFilePath = getAnalysisFilePath(channel)
