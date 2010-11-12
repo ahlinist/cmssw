9ec0f142753e6a936ba4859dcc2d7b38efe0e4ad
friis <>
2010-11-12 13:10:41 +0000

    Improve indentation, remove import *

--- TauAnalysis/Configuration/test/makeAHtoMuTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeAHtoMuTauPlots_grid_cfg.py
@@ -1,12 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
-from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsAHtoMuTau_7TeV
-from TauAnalysis.Configuration.plotAHtoMuTau_drawJobs_cfi import *
+from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi \
+        import recoSampleDefinitionsAHtoMuTau_7TeV
+
+from TauAnalysis.Configuration.plotAHtoMuTau_drawJobs_cfi import \
+        drawJobConfigurator_AHtoMuTau_woBtag, plots_AHtoMuTau, \
+        drawJobConfigurator_AHtoMuTau_wBtag
+
 from TauAnalysis.Configuration.makePlots2_grid import makePlots
-from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath, getJobId
+from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath, \
+        getJobId
 
 # import utility function to enable factorization
-from TauAnalysis.Configuration.tools.factorizationTools import enableFactorization_makeAHtoMuTauPlots_grid
+from TauAnalysis.Configuration.tools.factorizationTools import \
+        enableFactorization_makeAHtoMuTauPlots_grid
 
 process = cms.Process('makeAHtoMuTauPlots')
 
@@ -17,11 +24,16 @@ channel = 'AHtoMuTau'
 inputFilePath = getHarvestingFilePath(channel)
 jobId = getJobId(channel)
 
-makePlots(process, channel = channel, samples = recoSampleDefinitionsAHtoMuTau_7TeV,
+makePlots(process, channel = channel,
+          samples = recoSampleDefinitionsAHtoMuTau_7TeV,
           inputFilePath = inputFilePath, jobId = jobId,
           analyzer_drawJobConfigurator_indOutputFileName_sets = [
-              [ "ahMuTauAnalyzer_woBtag", drawJobConfigurator_AHtoMuTau_woBtag, "plotAHtoMuTau_woBtag_#PLOT#.png" ],
-            ##[ "ahMuTauAnalyzer_wBtag",  drawJobConfigurator_AHtoMuTau_wBtag,  "plotAHtoMuTau_wBtag_#PLOT#.png"  ]
+              [ "ahMuTauAnalyzer_woBtag",
+               drawJobConfigurator_AHtoMuTau_woBtag,
+               "plotAHtoMuTau_woBtag_#PLOT#.png" ],
+            #[ "ahMuTauAnalyzer_wBtag",
+              #drawJobConfigurator_AHtoMuTau_wBtag,
+             #"plotAHtoMuTau_wBtag_#PLOT#.png"  ]
           ],
           drawJobTemplate = plots_AHtoMuTau,
           enableFactorizationFunction = enableFactorization_makeAHtoMuTauPlots_grid,
