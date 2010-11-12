8d246f2e5eebf4bc7e04e4c0e97634f3f68ddb97
friis <>
2010-11-12 13:30:18 +0000

    Disable wBtag analysis in dump

--- TauAnalysis/Configuration/python/dumpAHtoMuTau_grid_cff.py
+++ TauAnalysis/Configuration/python/dumpAHtoMuTau_grid_cff.py
@@ -21,10 +21,10 @@ dumpAHtoMuTau_woBtag = cms.EDAnalyzer(
     dqmDirectories = cms.PSet(
         **dict(
             (sample, cms.string(
-                '/harvested/%s/ahMuTauAnalyzer_woBtag/FilterStatistics' 
+                '/harvested/%s/ahMuTauAnalyzer_woBtag/FilterStatistics'
                 % sample_dqm_name(sample))
             ) for sample in samples.SAMPLES_TO_PRINT)
-    ), 
+    ),
     columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
 )
 
@@ -33,12 +33,13 @@ dumpAHtoMuTau_wBtag = cms.EDAnalyzer(
     dqmDirectories = cms.PSet(
         **dict(
             (sample, cms.string(
-                '/harvested/%s/ahMuTauAnalyzer_wBtag/FilterStatistics' 
+                '/harvested/%s/ahMuTauAnalyzer_wBtag/FilterStatistics'
                 % sample_dqm_name(sample))
             ) for sample in samples.SAMPLES_TO_PRINT)
     ),
     columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
 )
 
-dumpAHtoMuTau = cms.Sequence(dumpAHtoMuTau_woBtag * dumpAHtoMuTau_wBtag)
+#dumpAHtoMuTau = cms.Sequence(dumpAHtoMuTau_woBtag * dumpAHtoMuTau_wBtag)
+dumpAHtoMuTau = cms.Sequence(dumpAHtoMuTau_woBtag)
 
