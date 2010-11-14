d5cd0cc6a64ddcb4cf0a47790882b338c80b3668
veelken <>
2010-11-14 15:40:21 +0000

    extended flexibility of changeCut tool to work with name, name + "Cumulative", name + "Individual" collections, so that it can be used by BgEstimationTools package

--- TauAnalysis/Configuration/python/tools/changeCut.py
+++ TauAnalysis/Configuration/python/tools/changeCut.py
@@ -5,29 +5,32 @@ from TauAnalysis.CandidateTools.tools.composeModuleName import *
 def changeCut(process, name, value, attribute = "cut"):
 
     cutNames = []
+    cutNames.append(name)
     cutNames.append(composeModuleName(name, "Cumulative"))
     cutNames.append(composeModuleName(name, "Individual"))
 
-    oldValue = 0.
+    oldValue = None
     oldValue_initialized = False
 
+    numCutsChanged = 0
+
     for cutName in cutNames:
-        if not hasattr(process, cutName):
-            raise ValueError("No cut = " + cutName + " attached to process object !!")
+        if hasattr(process, cutName):
+             cut = getattr(process, cutName)
 
-        cut = getattr(process, cutName)
-
-        if not hasattr(cut, attribute):
-            raise ValueError("Attribute = " + attribute + " not defined for cut = " + cutName + " !!")
-
-        # Note: "cumulative" and "individual" cuts both point to the same cut value object;
-        #       as a consequence, the cut value actually changes for both
-        #       the "cumulative" and "individual" cuts upon the first setattr call
-        #      --> need to initialize oldValue variable before first setattr call
-        if not oldValue_initialized:
-            oldValue = getattr(cut, attribute).value()
-        oldValue_initialized = True
+             # Note: "cumulative" and "individual" cuts both point to the same cut value object;
+             #       as a consequence, the cut value actually changes for both
+             #       the "cumulative" and "individual" cuts upon the first setattr call
+             #      --> need to initialize oldValue variable before first setattr call
+             if not oldValue_initialized:
+                 oldValue = getattr(cut, attribute).value()
+             oldValue_initialized = True
             
-        setattr(cut, attribute, value)
-        
+             setattr(cut, attribute, value)
+
+             numCutsChanged += 1
+
+    if numCutsChanged > 0:
         print("Replaced " + cutName + "." + attribute + ": '" + str(oldValue) + "' => '" + str(value) + "'")
+    else:
+        raise ValueError("No cut = " + cutName + " attached to process object !!")
