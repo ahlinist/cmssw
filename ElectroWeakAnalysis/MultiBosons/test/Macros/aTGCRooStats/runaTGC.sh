634b2ba26befac3ae298270b934ca1cf20c32905
lgray <>
2010-11-15 17:50:47 +0000

    more fixes

--- ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
+++ ElectroWeakAnalysis/MultiBosons/test/Macros/aTGCRooStats/runaTGC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python aTGCRooStats.py --workspaceName=ZZg_ee_15pb_limits --couplingType=ZZg --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=90 --nEtBins=8 --intLumi=11.0 --lumiErr=0.1 --h3Max=.4 --h4Max=.01 --inputData=Data_mm_35pb.root --inputMC=ZZg_mm_MC_merged_35pb.root --MCbackground --backgroundFile=ZJets_mm_35pb.root
+python aTGCRooStats.py --workspaceName=ZZg_mm_35pb_limits --couplingType=ZZg --treeName=photonTree --phoEtVar=photonEt --phoEtMin=10 --phoEtMax=90 --nEtBins=8 --intLumi=11.0 --lumiErr=0.1 --h3Max=.4 --h4Max=.01 --inputData=Data_mm_35pb.root --inputMC=ZZg_mm_MC_merged_35pb.root --MCbackground --backgroundFile=ZJets_mm_35pb.root
