9fa581cef007d7fb2cd877aee5919821d5e315ab
delre <>
2010-11-16 10:43:45 +0000

    slim ntuples for all photon+jet proje cts

--- JetMETCorrections/GammaJet/test/config_NOTRIG_DATA_cfg.py
+++ JetMETCorrections/GammaJet/test/config_NOTRIG_DATA_cfg.py
@@ -1,4 +1,9 @@
 import FWCore.ParameterSet.Config as cms
+import RecoJets.JetProducers.JetIDParams_cfi
+theJetIDParams = RecoJets.JetProducers.JetIDParams_cfi.JetIDParams.clone()
+from RecoJets.Configuration.RecoGenJets_cff import *
+from RecoMET.Configuration.RecoGenMET_cff import *
+from RecoMET.Configuration.GenMETParticles_cff import *
 
 process = cms.Process("myprocess")
 
@@ -33,14 +38,12 @@ process.maxEvents = cms.untracked.PSet(
 
 #process.options = cms.untracked.PSet(
 #    SkipEvent = cms.untracked.vstring('ProductNotFound')
-#    #wantSummary = cms.untracked.bool(True)
+    #wantSummary = cms.untracked.bool(True)
 #)
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
 
-#############   Include the jet corrections ##########
-process.load("JetMETCorrections.Configuration.DefaultJEC_cff")
 
 
 #monster track event cleaning
@@ -78,8 +81,22 @@ process.ecalCleanClustering = cms.Sequence(process.cleanedEcalClusters*process.c
 ###########  EB SPIKE CLEANING END   #####################
 
 ## produce JPT jets
-process.load('RecoJets.Configuration.RecoJPTJets_cff')
+#process.load('RecoJets.Configuration.RecoJPTJets_cff')
 
+#############   Include the corrections ##########
+process.load("RecoMET.Configuration.RecoGenMET_cff")
+process.load("RecoMET.Configuration.GenMETParticles_cff")
+process.load("JetMETCorrections.Configuration.DefaultJEC_cff")
+process.load("JetMETCorrections.Type1MET.MetType1Corrections_cff")
+
+process.metMuonJESCorAK5 = process.metJESCorAK5CaloJet.clone()
+process.metMuonJESCorAK5.inputUncorJetsLabel = "ak5CaloJets"
+process.metMuonJESCorAK5.corrector = "ak5CaloL2L3"
+process.metMuonJESCorAK5.inputUncorMetLabel = "corMetGlobalMuons"
+#process.metMuonJESCorAK5.hasMuonsCorr = True
+#process.metMuonJESCorAK5.useTypeII = True
+ 
+process.metCorSequence = cms.Sequence(process.metMuonJESCorAK5)
 
 
 process.myanalysis = cms.EDAnalyzer("GammaJetAnalyzer",
@@ -91,31 +108,24 @@ process.myanalysis = cms.EDAnalyzer("GammaJetAnalyzer",
     tracks = cms.untracked.InputTag("generalTracks"),
     Photonsrc = cms.untracked.InputTag("photons"),
     recoCollection = cms.string('EcalRecHitsEB'),
-    JetCorrectionService_akt5 = cms.string('ak5CaloL2L3Residual'),
-    JetCorrectionService_pfakt5 = cms.string('ak5PFL2L3Residual'),
-    JetCorrectionService_pfakt7 = cms.string('ak7PFL2L3Residual'),
-    jetsite = cms.untracked.InputTag("iterativeCone5CaloJets"),
+    JetCorrectionService_akt5 = cms.string('ak5CaloL2L3'),
+    JetCorrectionService_akt7 = cms.string('ak7CaloL2L3'),
+    JetCorrectionService_pfakt5 = cms.string('ak5PFL2L3'),
+    JetCorrectionService_pfakt7 = cms.string('ak7PFL2L3'),
     jetskt4 = cms.untracked.InputTag("kt4CaloJets"),
     jetskt6 = cms.untracked.InputTag("kt6CaloJets"),
     jetsakt5 = cms.untracked.InputTag("ak5CaloJets"),
-    jetssis5 = cms.untracked.InputTag("sisCone5CaloJets"),
-    jetssis7 = cms.untracked.InputTag("sisCone7CaloJets"),
+    jetsakt7 = cms.untracked.InputTag("ak7CaloJets"),
     jetsjptak5 = cms.untracked.InputTag("JetPlusTrackZSPCorJetAntiKt5"),
-    jetspfite = cms.untracked.InputTag("iterativeCone5PFJets"),
     jetspfkt4 = cms.untracked.InputTag("kt4PFJets"),
     jetspfkt6 = cms.untracked.InputTag("kt6PFJets"),
     jetspfakt5 = cms.untracked.InputTag("ak5PFJets"),
     jetspfakt7 = cms.untracked.InputTag("ak7PFJets"),
-    jetspfsis5 = cms.untracked.InputTag("sisCone5PFJets"),
-    jetspfsis7 = cms.untracked.InputTag("sisCone7PFJets"),
     hbhits = cms.untracked.InputTag("hbhereco"),
-    jetsgenite = cms.untracked.InputTag("iterativeCone5GenJets"),
     jetsgenkt4 = cms.untracked.InputTag("kt4GenJets"),
     jetsgenkt6 = cms.untracked.InputTag("kt6GenJets"),
     jetsgenakt5 = cms.untracked.InputTag("ak5GenJets"),
     jetsgenakt7 = cms.untracked.InputTag("ak7GenJets"),
-    jetsgensis5 = cms.untracked.InputTag("sisCone5GenJets"),
-    jetsgensis7 = cms.untracked.InputTag("sisCone7GenJets"),
     TriggerTag = cms.untracked.InputTag("TriggerResults::HLT"),
     vertices = cms.untracked.InputTag("offlinePrimaryVertices"),
     genjetptthr = cms.double(5.),
@@ -124,10 +134,13 @@ process.myanalysis = cms.EDAnalyzer("GammaJetAnalyzer",
     jptjetptthr = cms.double(4.),
     genjetnmin = cms.int32(10),
     pfjetnmin = cms.int32(10),
-    jptjetnmin = cms.int32(10)
+    jptjetnmin = cms.int32(10),
+    JetIDParams = theJetIDParams,
+    Xsec = cms.double(1.)
 )
 
 
-process.p = cms.Path(process.monster*process.myanalysis)
+process.p = cms.Path(process.monster*process.metCorSequence*process.myanalysis)
+#process.p = cms.Path(process.monster*process.myanalysis)
 #process.p = cms.Path(process.ecalCleanClustering*process.recoJPTJets*process.myanalysis)
 #process.p = cms.Path(process.myanalysis)
