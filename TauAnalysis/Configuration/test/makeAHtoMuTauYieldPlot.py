d923c5ee28ac6c95fb1d91904fd5d11661cefd32
veelken <>
2010-11-16 09:22:33 +0000

    fixed typo in import statement

--- TauAnalysis/Configuration/test/makeAHtoMuTauYieldPlot.py
+++ TauAnalysis/Configuration/test/makeAHtoMuTauYieldPlot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 from TauAnalysis.Configuration.makeYieldPlot import makeYieldPlot
-from TauAnalysis.Configuration.userRegistry import userSettings
+from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath
 
 import os
 
 #inputFileName = getHarvestingFilePath('AHtoMuTau') + '/' + "plotsAHtoMuTau_all.root"
-inputFileName = "/data1/friis/Run16/plotsAHtoMuTau_all.root"
+inputFileName = "/data1/friis/Run17/plotsAHtoMuTau_all.root"
 inputFileName.replace('//', '/')
 
 dqmDirectory = 'harvested/data/ahMuTauAnalyzer_woBtag/FilterStatistics/evtSelNonCentralJetEt20bTag/events_passed_cumulative/'
