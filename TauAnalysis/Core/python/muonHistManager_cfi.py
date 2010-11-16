7404a302c0c7e0cab2b247540a441ac257a6dd8c
veelken <>
2010-11-16 09:20:31 +0000

    added PFlow combined isolation plot (rel. and abs.)

--- TauAnalysis/Core/python/muonHistManager_cfi.py
+++ TauAnalysis/Core/python/muonHistManager_cfi.py
@@ -1,12 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+from TauAnalysis.RecoTools.patLeptonSelection_cff import selectedPatMuonsPFRelIso
+
 muonHistManager = cms.PSet(
     pluginName = cms.string('muonHistManager'),
     pluginType = cms.string('MuonHistManager'),
       
     muonSource = cms.InputTag('cleanPatMuons'),
     vertexSource = cms.InputTag('selectedPrimaryVertexPosition'),
-    beamSpotSource = cms.InputTag('offlineBeamSpot'),
+    beamSpotSource = cms.InputTag('offlineBeamSpot'),    
     jetSource = cms.InputTag('selectedPatJetsEt20Cumulative'),
     genParticleSource = cms.InputTag('genParticles'),
 
@@ -17,6 +19,14 @@ muonHistManager = cms.PSet(
 
     skipPdgIdsGenParticleMatch = cms.vint32( 12, 14, 16 ),
 
+    pfCombIsoExtractor = cms.PSet(
+        chargedHadronIso = selectedPatMuonsPFRelIso.chargedHadronIso,
+        neutralHadronIso = selectedPatMuonsPFRelIso.neutralHadronIso,
+        photonIso = selectedPatMuonsPFRelIso.photonIso
+    ),
+
+    pfCandidateSource = selectedPatMuonsPFRelIso.pfCandidateSource,
+    
     #normalization = cms.string("muons"),
     normalization = cms.string("events"),
 
