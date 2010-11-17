50f60c1bdd964d31853670b6d2b49c4559f286d2
veelken <>
2010-11-17 10:53:03 +0000

    first check-in of plugin for computing effective cross-section for sum of (background) MC samples

---
+++ TauAnalysis/FittingTools/plugins/DQMEffXsecCalculator.cc
@@ -0,0 +1,63 @@
+#include "TauAnalysis/FittingTools/plugins/DQMEffXsecCalculator.h"
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <iostream>
+#include <iomanip>
+
+DQMEffXsecCalculator::DQMEffXsecCalculator(const edm::ParameterSet& cfg)
+{
+  std::cout << "<DQMEffXsecCalculator::DQMEffXsecCalculator>:" << std::endl;
+
+  dataIntLumi_ = cfg.getParameter<double>("dataIntLumi");
+
+  if ( cfg.exists("channels") ) {
+    edm::ParameterSet cfgChannels = cfg.getParameter<edm::ParameterSet>("channels");
+    typedef std::vector<std::string> vstring;
+    vstring channelNames = cfgChannels.getParameterNamesForType<edm::ParameterSet>();
+    for ( vstring::const_iterator channelName = channelNames.begin();
+	  channelName != channelNames.end(); ++channelName ) {
+      edm::ParameterSet cfgChannel = cfgChannels.getParameter<edm::ParameterSet>(*channelName);
+      channelEntryType* channelEntry = new channelEntryType(*channelName, cfgChannel, dataIntLumi_);
+      channels_.push_back(channelEntry);
+    }
+  } else {
+    channelEntryType* channelEntry = new channelEntryType("undefined", cfg, dataIntLumi_);
+    channels_.push_back(channelEntry);
+  }
+}
+
+DQMEffXsecCalculator::~DQMEffXsecCalculator()
+{
+  for ( std::vector<channelEntryType*>::iterator it = channels_.begin();
+	it != channels_.end(); ++it ) {
+    delete (*it);
+  }
+}
+
+void DQMEffXsecCalculator::endJob()
+{
+  std::cout << "<DQMEffXsecCalculator::endJob>:" << std::endl;
+
+//--- check that DQMStore service is available
+  if ( !edm::Service<DQMStore>().isAvailable() ) {
+    edm::LogError ("endJob") << " Failed to access dqmStore --> histograms will NOT be plotted !!";
+    return;
+  }
+
+  DQMStore& dqmStore = (*edm::Service<DQMStore>());  
+
+  for ( std::vector<channelEntryType*>::iterator channel = channels_.begin();
+	channel != channels_.end(); ++channel ) {
+    std::cout << (*channel)->name_ << ": " << (*channel)->getEffXsec(dqmStore) << std::endl;
+  }
+
+  std::cout << "done." << std::endl;
+}
+
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(DQMEffXsecCalculator);
---
+++ TauAnalysis/FittingTools/plugins/DQMEffXsecCalculator.h
@@ -0,0 +1,100 @@
+#ifndef TauAnalysis_FittingTools_DQMEffXsecCalculator_h  
+#define TauAnalysis_FittingTools_DQMEffXsecCalculator_h
+
+/** \class DQMEffXsecCalculator
+ *
+ * Auxiliary class to compute "effective" cross-section
+ * for a combination of Monte Carlo samples
+ *
+ * \author Christian Veelken, UC Davis
+ *
+ * \version $Revision: 1.3 $
+ *
+ * $Id: DQMEffXsecCalculator.h,v 1.3 2010/10/27 15:49:52 veelken Exp $
+ *
+ */
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "TauAnalysis/DQMTools/interface/dqmAuxFunctions.h"
+
+#include <vector>
+#include <string>
+
+class DQMEffXsecCalculator : public edm::EDAnalyzer
+{
+ public:
+  
+  explicit DQMEffXsecCalculator(const edm::ParameterSet&);
+  ~DQMEffXsecCalculator();
+  
+ private:
+
+  void beginJob() {}
+  void analyze(const edm::Event&, const edm::EventSetup&) {}	
+  void endJob();
+
+//--- configuration parameters
+  double dataIntLumi_;
+
+  struct channelEntryType
+  { 
+    channelEntryType(const std::string& name, const edm::ParameterSet& cfg, double dataIntLumi)
+      : name_(name),
+	dataIntLumi_(dataIntLumi)
+    {
+      std::string dqmDirectory = cfg.getParameter<std::string>("dqmDirectory");
+
+      edm::ParameterSet cfgEfficiency = cfg.getParameter<edm::ParameterSet>("efficiency");
+      std::string meNameNumerator = cfgEfficiency.getParameter<std::string>("numerator");
+      meNameNumerator_ = dqmDirectoryName(dqmDirectory).append(meNameNumerator);
+      std::string meNameDenominator = cfgEfficiency.getParameter<std::string>("denominator");
+      meNameDenominator_ = dqmDirectoryName(dqmDirectory).append(meNameDenominator);
+
+      std::string meNameNumEventsPassed = cfg.getParameter<std::string>("numEventsPassed");
+      meNameNumEventsPassed_ = dqmDirectoryName(dqmDirectory).append(meNameNumEventsPassed);
+    }
+    ~channelEntryType() {}
+    double getEffXsec(DQMStore& dqmStore) const
+    {
+      bool error = false;
+      double numerator = getValue(dqmStore, meNameNumerator_, error);
+      double denominator = getValue(dqmStore, meNameDenominator_, error);
+
+      double numEventsPassed = getValue(dqmStore, meNameNumEventsPassed_, error);
+
+      if ( error ) {
+	edm::LogError ("getEffXsec") 
+	  << " Failed to access MonitorElements --> returning dummy value !!";
+	return -1.;
+      }
+
+      if ( numerator > denominator ) {
+	edm::LogWarning ("getEffXsec") 
+	  << " Value of numerator exceeds denominator !!";
+	return 1.;
+      }
+      
+      double efficiency = ( denominator != 0. ) ? (numerator/denominator) : 0.5;
+
+      return numEventsPassed/(dataIntLumi_*efficiency);
+    }
+    std::string name_;	
+    double dataIntLumi_;
+    std::string meNameNumerator_;	
+    std::string meNameDenominator_;
+    std::string meNameNumEventsPassed_;
+  };
+
+  std::vector<channelEntryType*> channels_;
+};
+
+#endif  
+
+
