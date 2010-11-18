19fae3bc326ae5e16dcdb20b4dde09f992f11146
friis <>
2010-11-18 15:35:44 +0000

    Use standard lib, add local files option, and ability to update jobId from a cfg

--- TauAnalysis/Configuration/python/userRegistry.py
+++ TauAnalysis/Configuration/python/userRegistry.py
@@ -5,10 +5,12 @@ import os
 userSettings = {
     'friis' : {
         'AHtoMuTau' : {
-            'analysisFilePath' : '/user/f/friis/AHtoMuTauOutput/',
+            'analysisFilePath' : '/user/f/friis/AHtoMuTauNov17/',
             'harvestingFilePath' : "/data1/friis/",
             'tmpFilePath' : "/tmp/friis/",
-            'jobId' : 'Run16'
+            'jobId' : 'Run18',
+            # Directory containing selected events
+            'pickevents' : '/data1/friis/Run17/get_events',
         },
         'global' : {
             'drawOptions' : {
@@ -57,13 +59,24 @@ userSettings = {
     }
 }
 
+def overrideJobId(channel, jobId):
+    # Set the job ID to something else temporarily
+    userSettings[os.environ['LOGNAME']][channel]['jobId'] = jobId
+
+def getPickEventsPath(channel):
+    return  userSettings[os.environ['LOGNAME']][channel]['pickevents']
+
 def getAnalysisFilePath(channel):
     return userSettings[os.environ['LOGNAME']][channel]['analysisFilePath']
 
 def getHarvestingFilePath(channel):
-    harvestingFilePath = userSettings[os.environ['LOGNAME']][channel]['harvestingFilePath'] \
-                        + '/' + userSettings[os.environ['LOGNAME']][channel]['jobId']
-    harvestingFilePath.replace('//', '/')
+    user_settings = userSettings[os.environ['LOGNAME']][channel]
+    harvestingFilePath = os.path.join(
+        user_settings['harvestingFilePath'], user_settings['jobId'])
+    if not os.path.exists(harvestingFilePath):
+        os.makedirs(harvestingFilePath)
+    if not os.path.isdir(harvestingFilePath):
+        print "WARNING: Harvesting file path %s is not a directory!"
     return harvestingFilePath
 
 def getTmpFilePath(channel):
