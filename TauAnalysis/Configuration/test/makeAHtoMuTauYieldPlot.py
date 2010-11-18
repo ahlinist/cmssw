756087479b94f1b447dfab30499e9abcbb689a46
friis <>
2010-11-18 15:34:39 +0000

    Use stdlib

--- TauAnalysis/Configuration/test/makeAHtoMuTauYieldPlot.py
+++ TauAnalysis/Configuration/test/makeAHtoMuTauYieldPlot.py
@@ -2,13 +2,12 @@
 
 from TauAnalysis.Configuration.makeYieldPlot import makeYieldPlot
 from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath
-
 import os
 
-#inputFileName = getHarvestingFilePath('AHtoMuTau') + '/' + "plotsAHtoMuTau_all.root"
-inputFileName = "/data1/friis/Run17/plotsAHtoMuTau_all.root"
-inputFileName.replace('//', '/')
+inputFileName = os.path.join(
+    getHarvestingFilePath('AHtoMuTau'), "plotsAHtoMuTau_all.root")
 
 dqmDirectory = 'harvested/data/ahMuTauAnalyzer_woBtag/FilterStatistics/evtSelNonCentralJetEt20bTag/events_passed_cumulative/'
 
-makeYieldPlot(inputFileName = inputFileName, dqmDirectory = dqmDirectory, outputFileName = "plotAHtoMuTau_woBtag_yield.png")
+makeYieldPlot(inputFileName = inputFileName, dqmDirectory = dqmDirectory,
+              outputFileName = "plotAHtoMuTau_woBtag_yield.png")
