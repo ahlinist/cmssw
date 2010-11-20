3b5869645c5b0574266fb6520d6664c1a4fefec4
friis <>
2010-11-20 14:17:36 +0000

    Disable systematics

--- TauAnalysis/Configuration/test/submitAnalysisAHtoMuTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisAHtoMuTau_grid.py
@@ -2,9 +2,12 @@
 
 from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsAHtoMuTau_7TeV
 from TauAnalysis.Configuration.submitAnalysisToGrid import submitAnalysisToGrid
-from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getJobId
+from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, \
+        getJobId, overrideJobId
+
 
 channel = 'AHtoMuTau'
+
 configFile = 'runAHtoMuTau_cfg.py'
 analysisFilePath = getAnalysisFilePath(channel)
 jobId = getJobId(channel)
@@ -13,13 +16,12 @@ samplesToAnalyze = [
     # modify in case you want to submit crab jobs for some of the samples only...
 ]
 
-enableSystematics = True
-#enableSystematics = False
+#enableSystematics = True
+enableSystematics = False
 
 submitAnalysisToGrid(configFile = configFile, channel = 'AHtoMuTau',
                      samples = recoSampleDefinitionsAHtoMuTau_7TeV,
                      outputFilePath = analysisFilePath, jobId = jobId,
                      samplesToAnalyze = samplesToAnalyze,
                      disableSysUncertainties = not enableSystematics,
-                     submit = False, saveFinalEvents=True)
-
+                     submit = True, saveFinalEvents=True)
