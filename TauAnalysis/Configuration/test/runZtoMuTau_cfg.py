59ee2b0d1c564383b6040cd92c4fd98954a82e25
veelken <>
2010-11-22 16:18:53 +0000

    use new switchJetCollection syntax

--- TauAnalysis/Configuration/test/runZtoMuTau_cfg.py
+++ TauAnalysis/Configuration/test/runZtoMuTau_cfg.py
@@ -70,9 +70,9 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         #'/store/relval/CMSSW_3_6_1/RelValZTT/GEN-SIM-RECO/START36_V7-v1/0021/F405BC9A-525D-DF11-AB96-002618943811.root',
         #'/store/relval/CMSSW_3_6_1/RelValZTT/GEN-SIM-RECO/START36_V7-v1/0020/EE3E8F74-365D-DF11-AE3D-002618FDA211.root'
-        'file:/data1/veelken/CMSSW_3_6_x/skims/Ztautau_1_1_sXK.root'        
+        #'file:/data1/veelken/CMSSW_3_6_x/skims/Ztautau_1_1_sXK.root'        
         #'file:/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/selEvents_AHtoMuTau_woBtag_runs145762to148058_RECO.root'
-        #'file:/data1/veelken/CMSSW_3_8_x/skims/test/mcDYttPU156bx_GEN_SIM_RECO_1_1_1VV.root'
+        'file:/data1/veelken/CMSSW_3_8_x/skims/test/mcDYttPU156bx_GEN_SIM_RECO_1_1_1VV.root'
     )
     #skipBadFiles = cms.untracked.bool(True) 
 )
@@ -128,7 +128,6 @@ from PhysicsTools.PatAlgos.tools.jetTools import *
 
 # uncomment to replace caloJets by pfJets
 switchJetCollection(process, jetCollection = cms.InputTag("ak5PFJets"), outputModule = '')
-runBTagging(process, cms.InputTag("ak5PFJets", 'AOD')
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
