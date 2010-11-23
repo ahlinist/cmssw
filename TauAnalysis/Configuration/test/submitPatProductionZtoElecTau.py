0cde4186c3a783b227c14d97ba4d70ba66a7ffb4
jkolb <>
2010-11-23 10:34:37 +0000

    updated for fall 10 samples

--- TauAnalysis/Configuration/test/submitPatProductionZtoElecTau.py
+++ TauAnalysis/Configuration/test/submitPatProductionZtoElecTau.py
@@ -5,7 +5,7 @@ from TauAnalysis.Configuration.makeReplacementsPatProduction import makeReplacem
 
 # name of the directory (either on afs area or castor)
 # to which all .root files produced by the cmsRun job will be copied
-outputFilePath = "/castor/cern.ch/user/j/jkolb/elecTauPatTuples/spring10/current/"
+outputFilePath = "/castor/cern.ch/user/j/jkolb/elecTauPatTuples/fall10/current/"
 
 # small cmsRun job for testing purposes...
 #submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "Ztautau_part01",
@@ -14,8 +14,8 @@ outputFilePath = "/castor/cern.ch/user/j/jkolb/elecTauPatTuples/spring10/current
 
 #--------------------------------------------------------------------------------
 #
-# Monte Carlo samples from Summer'09 production
-# reprocessed with CMSSW_3_1_4, skimmed by Jeff Kolb
+# Monte Carlo samples from fall 2010 production
+# processed with CMSSW_38_x, skimmed by Jeff Kolb
 #
 # NOTE: The jobs get submitted to the '1nd' queue,
 #       which allows for an execution time of the cmsRun jobs of up to 24 hours
@@ -28,13 +28,13 @@ outputFilePath = "/castor/cern.ch/user/j/jkolb/elecTauPatTuples/spring10/current
 # 7TeV data samples
 #
 
-for i in range(417):
+for i in range(402):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
 		sample = "Data2010A_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = GR_R_36X_V12B::All",
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = GR_R_38X_V14::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "data")
 
-for i in range(417,959):
+for i in range(417,1097):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
 		sample = "Data2010B_part%(i)02d" % {"i" : (i + 1)},
 		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = GR10_P_V10::All",
@@ -45,96 +45,93 @@ for i in range(417,959):
 #
 
 # Z --> tau tau jobs
-for i in range(18):
+for i in range(21):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "Ztautau_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "Ztautau_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V13::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
 
 # Z --> e e jobs
-for i in range(44):
+for i in range(81):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "Zee_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
-		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-
-# Photon + jets jobs
-submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "PhotonPlusJets_Pt15to20",
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
-	job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
-submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "PhotonPlusJets_Pt20to30",
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
-	job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
-submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "PhotonPlusJets_Pt30to50",
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
-	job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
-submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "PhotonPlusJets_Pt50to80",
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
-	job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
-submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", sample = "PhotonPlusJets_Pt80to120",
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
-	job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
-for i in range(10):
-	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "PhotonPlusJets_Pt30_part%(i)02d" % {"i" : (i + 1)},
+		sample = "Zee_part%(i)02d" % {"i" : (i + 1)},
 		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
 
+# Photon + jets jobs
+for i in range(3):
+	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
+		sample = "PhotonPlusJets_Pt15to30_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
+		job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
+for i in range(4):
+	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
+		sample = "PhotonPlusJets_Pt30to50_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
+		job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
+for i in range(4):
+	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
+		sample = "PhotonPlusJets_Pt50to80_part%(i)02d" % {"i" : (i + 1)}, 
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
+		job = "PatProduction", queue = "8nh", outputFilePath = outputFilePath, type = "mc")
+
+#for i in range(10):
+#	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
+#		sample = "PhotonPlusJets_Pt30_part%(i)02d" % {"i" : (i + 1)},
+#		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
+#		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
+
 # QCD_BCtoE jobs
-for i in range(70):
+for i in range(101):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "QCD_BCtoE_Pt20to30_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "QCD_BCtoE_Pt20to30_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-for i in range(46):
+for i in range(201):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "QCD_BCtoE_Pt30to80_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "QCD_BCtoE_Pt30to80_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-for i in range(35):
+for i in range(206):
 		submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "QCD_BCtoE_Pt80to170_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "QCD_BCtoE_Pt80to170_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")    
 
 # QCD_EMenriched jobs
-for i in range(174):
+for i in range(226):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "QCD_EMenriched_Pt20to30_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "QCD_EM_Pt20to30_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-for i in range(266):    
+for i in range(479):    
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-	sample = "QCD_EMenriched_Pt30to80_7TeV_part%(i)02d" % {"i" : (i + 1)},
-	replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
-	job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-for i in range(105):    
+		sample = "QCD_EM_Pt30to80_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
+		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
+for i in range(201):    
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau",
-		sample = "QCD_EMenriched_Pt80to170_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "QCD_EM_Pt80to170_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")    
 
-# W/Z + jets jobs
-for i in range(82):
+# W jobs
+for i in range(80):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "WplusJets_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "WtoENu_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
 
-for i in range(56):
+for i in range(32):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "ZeePlusJets_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
-		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
-	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "ZtautauPlusJets_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "WtoTauNu_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
 
 # TT + jets jobs
-for i in range(56):
+for i in range(55):
 	submitToBatch(configFile = "producePatTuple_cfg.py", channel = "ZtoElecTau", 
-		sample = "TTplusJets_7TeV_part%(i)02d" % {"i" : (i + 1)},
-		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START3X_V27::All",
+		sample = "TTbar_part%(i)02d" % {"i" : (i + 1)},
+		replFunction = makeReplacementsPatProduction, replacements = "maxEvents = -1; globalTag = START38_V12::All",
 		job = "PatProduction", queue = "1nd", outputFilePath = outputFilePath, type = "mc")
 
