290ad803be20ce2241a290c6922d5662ec8ee009
friis <>
2010-11-23 19:32:26 +0000

    use hps tanc

--- TauAnalysis/RecoTools/python/recoPFTauIdentification_cfi.py
+++ TauAnalysis/RecoTools/python/recoPFTauIdentification_cfi.py
@@ -3,25 +3,17 @@ import FWCore.ParameterSet.Config as cms
 # produce discriminator summarizing tau id. criteria applied in EWK-tau analyses
 # (used as binning variable in tau id. efficiency measurement)
 ewkTauId = cms.EDProducer("PFRecoTauDiscriminationByStringCut",
-    PFTauProducer = cms.InputTag('shrinkingConePFTauProducer'),
+    PFTauProducer = cms.InputTag('hpsTancTaus'),
     Prediscriminants = cms.PSet(
         BooleanOperator = cms.string("and"),
         leadTrack = cms.PSet(
-            Producer = cms.InputTag('shrinkingConePFTauDiscriminationByLeadingTrackFinding'),
+            Producer = cms.InputTag('hpsTancTausDiscriminationByLeadingTrackPtCut'),
             cut = cms.double(0.5)
         ),
         leadTrackPt = cms.PSet(
-            Producer = cms.InputTag('shrinkingConePFTauDiscriminationByLeadingTrackPtCut'),
+            Producer = cms.InputTag('hpsTancTausDiscriminationByTancMedium'),
             cut = cms.double(0.5)
         ),
-        trackIsolation = cms.PSet(
-            Producer = cms.InputTag('shrinkingConePFTauDiscriminationByTrackIsolation'),
-            cut = cms.double(0.5)
-        ),
-        ecalIsolation = cms.PSet(
-            Producer = cms.InputTag('shrinkingConePFTauDiscriminationByECALIsolation'),
-            cut = cms.double(0.5)
-        )
     ),
     cut = cms.string('(signalTracks.size() = 1 | signalTracks.size() = 3) && (abs(charge) > 0.5 & abs(charge) < 1.5)')
 )
