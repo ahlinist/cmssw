29515d9657f22a9fc323f843b1a3a7491ba3ee43
covarell <>
2010-11-24 18:48:31 +0000

    First test of merged analyzer

--- HeavyFlavorAnalysis/Onia2MuMu/test/jpsianalyzerpat_cfg.py
+++ HeavyFlavorAnalysis/Onia2MuMu/test/jpsianalyzerpat_cfg.py
@@ -9,22 +9,26 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
     fileNames = cms.untracked.vstring(
-    "file:testNewCSCTF_data.root"
- 
+                 "file:onia2MuMuPAT.root"
     )
 )
 
-# process.options = cms.untracked.PSet(
-#    IgnoreCompletely = cms.untracked.vstring( "ProductNotFound" )
-# )
-
-process.hltMu3 = cms.EDFilter("HLTHighLevel",
+process.hltMuF = cms.EDFilter("HLTHighLevel",
                  TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
                  HLTPaths = cms.vstring("HLT_L1DoubleMuOpen"),
                  eventSetupPathsKey = cms.string(''),
                  andOr = cms.bool(True),
                  throw = cms.bool(False)
 )
+
+
+# filter on good vertex
+process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
+                                           vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+                                           minimumNDOF = cms.uint32(4),
+                                           maxAbsZ = cms.double(15),	
+                                           maxd0 = cms.double(2)	
+                                           )
 
 process.MessageLogger.destinations = ['cout', 'cerr']
 process.MessageLogger.cerr.FwkReport.reportEvery = 500
@@ -34,27 +38,38 @@ process.demo = cms.EDAnalyzer('JPsiAnalyzerPAT',
     src = cms.InputTag("onia2MuMuPatTrkTrk"),
     srcWithCaloMuons = cms.InputTag("onia2MuMuPatGlbCal"),
 
-    histFileName = cms.string("Histos_362_data.root"),   
-    dataSetName = cms.string("DataSet_362_data.root"),
-                              
+    writeTree = cms.bool(True),
+    treeFileName = cms.string("testTree.root"),
+
+    writeDataSet = cms.bool(True),                 
+    dataSetName = cms.string("testDataSet.root"),
+    triggerForDataset = cms.string("HLT_L1DoubleMuOpen"),
+
+    massMin = cms.double(2.6),
+    massMax = cms.double(3.5),
     pTBinRanges = cms.vdouble(0.0, 6.0, 8.0, 9.0, 10.0, 12.0, 15.0, 40.0),
-    # pTBinRanges = cms.vdouble(0.0, 40.0),
     etaBinRanges = cms.vdouble(0.0, 1.3, 2.5),
-    # etaBinRanges = cms.vdouble(0.0, 3.0),
     onlyTheBest = cms.bool(True),		
-    applyCuts = cms.bool(True),			
-    storeEfficiency = cms.bool(False),	
+    applyCuts = cms.bool(True),				
     useBeamSpot = cms.bool(True),
-    useRapidity = cms.bool(True),
     useCaloMuons = cms.untracked.bool(False),
     removeSignalEvents = cms.untracked.bool(False),
     removeTrueMuons = cms.untracked.bool(False),
     storeWrongSign = cms.untracked.bool(True),
     writeOutCandidates = cms.untracked.bool(False),
-    includePsiPrime = cms.untracked.bool(False),
-    massCorrectionMode=cms.int32(3),    # mode 0 no correction, mode 1 constant corr, mode 2 pt dependent corr, mode 3 pt and eta dependent corr
-    makeJSON = cms.untracked.bool(True) # make a JSON (PseudoJSON.txt) file out of the runs and lumis analyzed 
+    massCorrectionMode = cms.int32(3),    # mode 0 no correction, mode 1 constant corr, mode 2 pt dependent corr, mode 3 pt and eta dependent corr
+    oniaPDG = cms.int32(443),
+    genParticles = cms.InputTag("genMuons"),
+    isMC = cms.untracked.bool(True),
+    isPromptMC = cms.untracked.bool(True),
+    TriggerResultsLabel = cms.InputTag("TriggerResults","","HLT")
 )
 
+## no filter
+# process.p = cms.Path(process.demo)
 
-process.p = cms.Path(process.demo)
+## filter on vertex
+process.p = cms.Path(process.primaryVertexFilter*process.demo)
+
+## filter on vertex and HLT
+# process.p = cms.Path(process.primaryVertexFilter*process.hltMuF*process.demo)
