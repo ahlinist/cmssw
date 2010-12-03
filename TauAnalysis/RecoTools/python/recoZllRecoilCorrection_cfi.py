3b04bf014ff1465978902e306e37e7ca11e52960
veelken <>
2010-12-03 16:00:06 +0000

    updated Z-recoil correction parameter to Nov4ReReco data vs. PU156bx MC

--- TauAnalysis/RecoTools/python/recoZllRecoilCorrection_cfi.py
+++ TauAnalysis/RecoTools/python/recoZllRecoilCorrection_cfi.py
@@ -4,23 +4,23 @@ recoZllRecoilCorrectionParameter = cms.PSet(
     parameter = cms.PSet(
         data = cms.PSet(
             d = cms.double(0.),
-            k = cms.double(-0.92461),
-            sigma1 = cms.double(9.16832),
-            b1 = cms.double(-1.37362e-2),
-            c1 = cms.double(+7.51767e-5),
-            sigma2 = cms.double(7.4436),
-            b2 = cms.double(-7.60317e-3),
-            c2 = cms.double(-2.30955e-6)
+            k = cms.double(-0.943492),
+            sigma1 = cms.double(10.0368),
+            b1 = cms.double(-1.6628e-2),
+            c1 = cms.double(+1.12634e-4),
+            sigma2 = cms.double(8.17098),
+            b2 = cms.double(-1.08149e-2),
+            c2 = cms.double(+3.32754e-05)
         ),
         mc = cms.PSet(
             d = cms.double(0.),
-            k = cms.double(-0.928701),
-            sigma1 = cms.double(7.86255),
-            b1 = cms.double(-1.08423e-2),
-            c1 = cms.double(+4.26271e-5),
-            sigma2 = cms.double(6.14236),
-            b2 = cms.double(-1.05838e-2),
-            c2 = cms.double(+2.51923e-5)
+            k = cms.double(-0.947483),
+            sigma1 = cms.double(9.32262),
+            b1 = cms.double(-1.33763e-2),
+            c1 = cms.double(+6.50963e-5),
+            sigma2 = cms.double(7.44516),
+            b2 = cms.double(-1.01039e-2),
+            c2 = cms.double(+2.11601e-05)
         )
     )
-)    
+)
