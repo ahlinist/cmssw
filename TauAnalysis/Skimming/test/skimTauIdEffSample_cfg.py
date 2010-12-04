3388fda23a2d043c5050dc0cae35bd735a647815
veelken <>
2010-12-04 16:30:21 +0000

    first check-in of config files for submitting skimming jobs for tau id. efficiency measurement

---
+++ TauAnalysis/Skimming/test/skimTauIdEffSample_cfg.py
@@ -0,0 +1,165 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# skim event sample for tau id. efficiency measurement
+#--------------------------------------------------------------------------------
+
+process = cms.Process("skimTauIdEffSample")
+
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+#process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = cms.string('START38_V13::All')
+
+# load definition of event selection 
+process.load("TauAnalysis.TauIdEfficiency.filterTauIdEffSample_cfi")
+
+# import definition of FEVT (RAW + RECO) event content
+from Configuration.EventContent.EventContent_cff import *
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #'file:/data1/veelken/CMSSW_3_6_x/skims/Ztautau_1_1_sXK.root'
+        #'file:/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/selEvents_AHtoMuTau_woBtag_runs145762to148058_RECO.root'
+        #'file:/data1/friis/PickMikesEvents/mikes_events_2010b_incomplete.root',
+        #'file:/data1/friis/PickMikesEvents/mikes_events_2010b_incomplete001.root'
+        'file:/data1/veelken/CMSSW_3_8_x/skims/test/mcDYttPU156bx_GEN_SIM_RECO_1_1_1VV.root'
+    )
+)
+
+isMC = True # use for MC
+##isMC = False # use for Data
+##HLTprocessName = "HLT" # use for non-reprocessed MC samples and Data
+##HLTprocessName = "REDIGI36X" # use for Spring'10 reprocessed MC
+HLTprocessName = "REDIGI38XPU" # use for Fall'10 reprocessed MC with pile-up
+##HLTprocessName = "REDIGI38X" # use for Fall'10 reprocessed MC without pile-up
+pfCandidateCollection = "particleFlow" # pile-up removal disabled
+##pfCandidateCollection = "pfNoPileUp" # pile-up removal enabled
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# define GlobalTag to be used for event reconstruction
+# (only relevant for HPS tau reconstruction algorithm)
+if isMC:
+    process.GlobalTag.globaltag = cms.string('START38_V12::All')
+else:   
+    process.GlobalTag.globaltag = cms.string('GR_R_38X_V13::All')
+#--------------------------------------------------------------------------------    
+
+#--------------------------------------------------------------------------------
+# define skimming criteria
+# (in order to be able to produce Tau Ntuple directly from unskimmed Monte Carlo/datasets;
+#  HLT single jet trigger passed && either two CaloJets or two PFJets of Pt > 10 GeV within |eta| < 2.5)
+process.load('TauAnalysis.TauIdEfficiency.filterTauIdEffSample_cfi')
+
+process.hltMu.selector.src = cms.InputTag('TriggerResults::%s' % HLTprocessName)
+
+if isMC:
+    process.dataQualityFilters.remove(process.hltPhysicsDeclared)
+    process.dataQualityFilters.remove(process.dcsstatus)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# produce collections of objects needed as input for PAT-tuple production
+# (e.g. rerun reco::Tau identification algorithms with latest tags)
+#
+from TauAnalysis.TauIdEfficiency.tools.configurePrePatProduction import configurePrePatProduction
+
+configurePrePatProduction(process, pfCandidateCollection = pfCandidateCollection, addGenInfo = isMC)
+
+process.prePatProductionSequence.remove(process.tautagging)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# produce PAT objects
+#
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+# configure PAT trigger matching
+from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
+switchOnTrigger(process, hltProcess = HLTprocessName, outputModule = '')
+process.patTrigger.addL1Algos = cms.bool(True)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# import utility function for switching pat::Tau input
+# to different reco::Tau collection stored on AOD
+from PhysicsTools.PatAlgos.tools.tauTools import *
+
+# comment-out to take reco::CaloTaus instead of reco::PFTaus
+# as input for pat::Tau production
+#switchToCaloTau(process)
+
+# comment-out to take shrinking dR = 5.0/Et(PFTau) signal cone
+# instead of fixed dR = 0.07 signal cone reco::PFTaus
+# as input for pat::Tau production
+#switchToPFTauShrinkingCone(process)
+#switchToPFTauFixedCone(process)
+
+# comment-out to take new HPS + TaNC combined tau id. algorithm
+switchToPFTauHPSpTaNC(process)
+
+# disable preselection on of pat::Taus
+# (disabled also in TauAnalysis/RecoTools/python/patPFTauConfig_cfi.py ,
+#  but re-enabled after switching tau collection)
+process.cleanPatTaus.preselection = cms.string('')
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# import utility function for managing pat::Jets
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+# uncomment to replace caloJets by pfJets
+switchJetCollection(process, jetCollection = cms.InputTag("ak5PFJets"), outputModule = '')
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# import utility function for managing pat::METs
+from TauAnalysis.Configuration.tools.metTools import *
+
+# uncomment to add pfMET
+# set Boolean swich to true in order to apply type-1 corrections
+addPFMet(process, correct = False)
+
+# uncomment to replace caloMET by pfMET in all di-tau objects
+process.load("TauAnalysis.CandidateTools.diTauPairProductionAllKinds_cff")
+replaceMETforDiTaus(process, cms.InputTag('patMETs'), cms.InputTag('patPFMETs'))
+#--------------------------------------------------------------------------------
+
+process.p = cms.Path(
+    process.prePatProductionSequence
+   + process.patDefaultSequence
+)    
+
+process.skimOutputModule = cms.OutputModule("PoolOutputModule",                                 
+    FEVTSIMEventContent,
+    process.tauIdEffSampleEventSelection,
+    verbose = cms.untracked.bool(False),
+    fileName = cms.untracked.string("tauIdEffSample_RECO.root")
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.o = cms.EndPath(process.skimOutputModule)
+
+# define order in which different paths are run
+process.schedule = cms.Schedule(
+    process.p,
+    process.muonPFTauSkimPath,
+    process.o
+)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
