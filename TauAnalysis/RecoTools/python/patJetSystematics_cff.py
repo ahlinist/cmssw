aaf3737aaae8cff549e4b3e340da1517c2c2c712
veelken <>
2010-12-08 10:19:21 +0000

    - use "official" JEC uncertainties for tau-jet and quark/gluon jets - use MuScleFit uncertainties for muon momentum uncertainties

--- TauAnalysis/RecoTools/python/patJetSystematics_cff.py
+++ TauAnalysis/RecoTools/python/patJetSystematics_cff.py
@@ -1,21 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-from TauAnalysis.RecoTools.tools.sysProdConfigurator import *
-from TauAnalysis.RecoTools.randomNumberGenerator_cfi import *
+#--------------------------------------------------------------------------------
+# shift/smear jet collection
+#--------------------------------------------------------------------------------
 
-sysConfiguratorJets = sysProdConfigurator(pyModuleName = __name__, pyNameSpace = locals())
-prodSmearedJets = sysConfiguratorJets.createSequence(
-    randomNumberService = RandomNumberGeneratorService,
-    modulePrefix        = 'smearedJets',
-    moduleType          = 'SmearedJetProducer',
-    src                 = 'cleanPatJets',
-    energyScaleMean     = 1.0,
-    energyScaleShift    = 0.05,
-    energyScaleSmearing = 0.05,
-    ptShift             = 1.0,
-    ptSmearing          = 0.5,
-    etaShift            = 0.05,
-    etaSmearing         = 0.025,
-    phiShift            = 0.05,
-    phiSmearing         = 0.025
+patJetsJECshiftUp = cms.EDProducer("SmearedJetProducer",
+    src                   = cms.InputTag('cleanPatJets'),
+    jetCorrPayloadName    = cms.string('AK5PF'),
+    jetCorrUncertaintyTag = cms.string('L2L3Residual'),
+    shiftByJECuncertainty = cms.double(+1.)
 )
+
+patJetsJECshiftDown = patJetsJECshiftUp.clone(
+    shiftByJECuncertainty = cms.double(-1.)
+)
+
+prodSmearedJets = cms.Sequence(patJetsJECshiftUp * patJetsJECshiftDown)
+
+
+
--- TauAnalysis/RecoTools/python/patLeptonSystematics_cff.py
+++ TauAnalysis/RecoTools/python/patLeptonSystematics_cff.py
@@ -3,53 +3,83 @@ import FWCore.ParameterSet.Config as cms
 from TauAnalysis.RecoTools.tools.sysProdConfigurator import *
 from TauAnalysis.RecoTools.randomNumberGenerator_cfi import *
 
+from TauAnalysis.RecoTools.patMuonMomentumCorrection_cfi import *
+
+#--------------------------------------------------------------------------------
+# shift/smear electron collection
+#--------------------------------------------------------------------------------
+
 sysConfiguratorElectrons = sysProdConfigurator(pyModuleName = __name__, pyNameSpace = locals())
 prodSmearedElectrons = sysConfiguratorElectrons.createSequence(
-    randomNumberService = RandomNumberGeneratorService,
-    modulePrefix        = 'smearedElectrons',
-    moduleType          = 'SmearedElectronProducer',
-    src                 = 'patElectrons',
-    energyScaleMean     = 1.0,
-    energyScaleShift    = 0.025,
-    energyScaleSmearing = 0.025,
-    ptShift             = 1.0,
-    ptSmearing          = 0.5,
-    etaShift            = 0.025,
-    etaSmearing         = 0.01,
-    phiShift            = 0.025,
-    phiSmearing         = 0.025
+    randomNumberService   = RandomNumberGeneratorService,
+    modulePrefix          = 'smearedElectrons',
+    moduleType            = 'SmearedElectronProducer',
+    src                   = 'patElectrons',
+    energyScaleMean       = 1.0,
+    energyScaleShift      = 0.025,
+    energyScaleSmearing   = 0.025,
+    ptShift               = 1.0,
+    ptSmearing            = 0.5,
+    etaShift              = 0.025,
+    etaSmearing           = 0.01,
+    phiShift              = 0.025,
+    phiSmearing           = 0.025
 )
 
-sysConfiguratorMuons = sysProdConfigurator(pyModuleName = __name__, pyNameSpace = locals())
-prodSmearedMuons = sysConfiguratorMuons.createSequence(
-    randomNumberService = RandomNumberGeneratorService,
-    modulePrefix        = 'smearedMuons',
-    moduleType          = 'SmearedMuonProducer',
-    src                 = 'patMuons',
-    energyScaleMean     = 1.0,
-    energyScaleShift    = 0.01,
-    energyScaleSmearing = 0.01,
-    ptShift             = 0.0,
-    ptSmearing          = 0.1,
-    etaShift            = 0.025,
-    etaSmearing         = 0.01,
-    phiShift            = 0.025,
-    phiSmearing         = 0.01
+#--------------------------------------------------------------------------------
+# shift/smear muon collection
+#--------------------------------------------------------------------------------
+
+poolDBESSourceMuScleFitShiftUp = poolDBESSourceMuScleFitCentralValue.clone(
+    connect = cms.string('sqlite_fip:TauAnalysis/RecoTools/data/Z_20_invNb_innerTrack_plusError.db'),
+    appendToDataLabel = cms.string("shiftUp"),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('MuScleFitDBobjectRcd'),
+            tag = cms.string('Z_20_invNb_innerTrack')
+        )
+    )
 )
 
-sysConfiguratorTaus = sysProdConfigurator(pyModuleName = __name__, pyNameSpace = locals())
-prodSmearedTaus = sysConfiguratorTaus.createSequence(
-    randomNumberService = RandomNumberGeneratorService,
-    modulePrefix        = 'smearedTaus',
-    moduleType          = 'SmearedTauProducer',
-    src                 = 'patTaus',
-    energyScaleMean     = 1.0,
-    energyScaleShift    = 0.05,
-    energyScaleSmearing = 0.025,
-    ptShift             = 1.0,
-    ptSmearing          = 0.5,
-    etaShift            = 0.05,
-    etaSmearing         = 0.025,
-    phiShift            = 0.05,
-    phiSmearing         = 0.025
+patMuonsMuScleFitCorrectedMomentumShiftUp = patMuonsMuScleFitCorrectedMomentum.clone(
+    dbObjectLabel = cms.string("shiftUp")
 )
+
+poolDBESSourceMuScleFitShiftDown = poolDBESSourceMuScleFitCentralValue.clone(
+    connect = cms.string('sqlite_fip:TauAnalysis/RecoTools/data/Z_20_invNb_innerTrack_minusError.db'),
+    appendToDataLabel = cms.string("shiftDown"),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('MuScleFitDBobjectRcd'),
+            tag = cms.string('Z_20_invNb_innerTrack')
+        )
+    )
+)
+
+patMuonsMuScleFitCorrectedMomentumShiftDown = patMuonsMuScleFitCorrectedMomentum.clone(
+    dbObjectLabel = cms.string("shiftDown")
+)
+
+prodSmearedMuons = cms.Sequence(patMuonsMuScleFitCorrectedMomentumShiftUp * patMuonsMuScleFitCorrectedMomentumShiftDown)
+
+#--------------------------------------------------------------------------------
+# shift/smear tau-jet collection
+#--------------------------------------------------------------------------------
+
+patTausJECshiftUp = cms.EDProducer("SmearedTauProducer",
+    src                   = cms.InputTag('cleanPatTaus'),
+    jetCorrPayloadName    = cms.string('AK5PF'),
+    ##jetCorrUncertaintyTag = cms.string('START38_V13'),
+    jetCorrUncertaintyTag = cms.string('L2L3Residual'),                               
+    jecFlavorUncertainty  = cms.double(2.),
+    shiftByJECuncertainty = cms.double(+1.)
+)
+
+patTausJECshiftDown = patTausJECshiftUp.clone(
+    shiftByJECuncertainty = cms.double(-1.)
+)
+
+prodSmearedTaus = cms.Sequence(patTausJECshiftUp * patTausJECshiftDown)
+
+
+
