872ae941993749758d2eaf30d4bd290863193c6c
jkolb <>
2010-12-09 15:53:33 +0000

    update sample names

--- TauAnalysis/Configuration/python/dumpZtoElecTau_cff.py
+++ TauAnalysis/Configuration/python/dumpZtoElecTau_cff.py
@@ -6,18 +6,15 @@ import FWCore.ParameterSet.Config as cms
 
 dumpZtoElecTau = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
     dqmDirectories = cms.PSet(
-        Data = cms.string('harvested/Data/zElecTauAnalyzer/FilterStatistics/'),
-        Ztautau = cms.string('harvested/Ztautau/zElecTauAnalyzer/FilterStatistics/'),
-        Zee = cms.string('harvested/Zee/zElecTauAnalyzer/FilterStatistics/'),
-        ZtautauPlusJets = cms.string('harvested/ZtautauPlusJets/zElecTauAnalyzer/FilterStatistics/'),
-        ZeePlusJets = cms.string('harvested/ZeePlusJets/zElecTauAnalyzer/FilterStatistics/'),
-        WplusJets = cms.string('harvested/WplusJets/zElecTauAnalyzer/FilterStatistics/'),
-        TTplusJets = cms.string('harvested/TTplusJets/zElecTauAnalyzer/FilterStatistics/'),
-        QCD_EMenriched = cms.string('harvested/qcdEMenrichedSum/zElecTauAnalyzer/FilterStatistics/'),
-        QCD_BCtoE = cms.string('harvested/qcdBCtoESum/zElecTauAnalyzer/FilterStatistics/'),
-		#PhotonPlusJets = cms.string('harvested/gammaPlusJetsSum/zElecTauAnalyzer/FilterStatistics/')
-        PhotonPlusJets = cms.string('harvested/gammaPlusJets_Pt30_factorized/zElecTauAnalyzer/FilterStatistics/')
+        Data = cms.string('summed/harvested/Data/zElecTauAnalyzer/FilterStatistics/'),
+        Ztautau = cms.string('summed/harvested/Ztautau/zElecTauAnalyzer/FilterStatistics/'),
+        Zee = cms.string('summed/harvested/Zee/zElecTauAnalyzer/FilterStatistics/'),
+        WplusJets = cms.string('summed/harvested/wPlusJetsSum/zElecTauAnalyzer/FilterStatistics/'),
+		#TTplusJets = cms.string('summed/harvested/TTplusJets/zElecTauAnalyzer/FilterStatistics/'),
+        QCD_EMenriched = cms.string('summed/harvested/qcdEMenrichedSum/zElecTauAnalyzer/FilterStatistics/'),
+        QCD_BCtoE = cms.string('summed/harvested/qcdBCtoESum/zElecTauAnalyzer/FilterStatistics/'),
+		PhotonPlusJets = cms.string('summed/harvested/gammaPlusJetsSum/zElecTauAnalyzer/FilterStatistics/')
     ),
-    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
+    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
 )
  
