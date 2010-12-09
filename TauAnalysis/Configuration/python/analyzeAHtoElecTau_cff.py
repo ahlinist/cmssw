12f714e532f6021e96e5cfa94540ca19b1502b9a
jkolb <>
2010-12-09 15:57:09 +0000

    add AHtoElecTau files

---
+++ TauAnalysis/Configuration/python/analyzeAHtoElecTau_cff.py
@@ -0,0 +1,111 @@
+import FWCore.ParameterSet.Config as cms
+import copy
+
+# import config for event selection, event print-out and analysis sequence
+from TauAnalysis.Configuration.analyzeAHtoElecTau_cfi import *
+
+# define auxiliary service
+# for handling of systematic uncertainties
+from TauAnalysis.CandidateTools.sysErrDefinitions_cfi import *
+SysUncertaintyService = cms.Service("SysUncertaintyService",
+    weights = getSysUncertaintyParameterSets(
+        [ electronSystematics,
+          tauSystematics,
+          theorySystematics ]
+    )
+)
+
+analyzeAHtoElecTauEvents = cms.EDAnalyzer("GenericAnalyzer",
+  
+    name = cms.string('zElecTauAnalyzer'), 
+                            
+    filters = cms.VPSet(
+        # generator level phase-space selection
+        evtSelGenPhaseSpace,
+    
+        # generator level selection of Z --> e + tau-jet events
+        # passing basic acceptance and kinematic cuts
+        # (NOTE: to be used for efficiency studies only !!)
+				#genElectronCut,
+        #genTauCut,
+    
+        # trigger selection
+        evtSelTrigger,
+        
+        # primary event vertex selection
+        evtSelPrimaryEventVertex,
+        evtSelPrimaryEventVertexQuality,
+        evtSelPrimaryEventVertexPosition,
+        
+        # electron candidate selection
+        evtSelElectronId,
+        evtSelElectronAntiCrack,
+        evtSelElectronEta,
+        evtSelElectronPt,
+        evtSelElectronTrkIso,
+        evtSelElectronEcalIso,
+        evtSelElectronConversionVeto,
+        evtSelElectronTrkIP,
+
+        # tau candidate selection
+        evtSelTauAntiOverlapWithElectronsVeto,
+        evtSelTauEta,
+        evtSelTauPt,
+        evtSelTauLeadTrk,
+        evtSelTauLeadTrkPt,
+				evtSelTauTaNCdiscr,
+        evtSelTauTrkIso,
+        evtSelTauEcalIso,
+        evtSelTauProng,
+        evtSelTauCharge,
+        evtSelTauElectronVeto,
+        evtSelTauEcalCrackVeto,
+        evtSelTauMuonVeto,
+        
+        # di-tau candidate selection
+        evtSelDiTauCandidateForElecTauAntiOverlapVeto,
+        evtSelDiTauCandidateForElecTauZeroCharge,
+        evtSelDiTauCandidateForElecTauAcoplanarity12,
+        evtSelDiTauCandidateForElecTauMt1MET,
+        evtSelDiTauCandidateForElecTauPzetaDiff,
+
+        # veto events compatible with Z --> e+ e- hypothesis
+        # (based on reconstructed (visible) invariant mass of e + tau-jet pair)
+        evtSelElecTauPairZeeHypothesisVeto
+    ),
+  
+    analyzers = cms.VPSet(
+        genPhaseSpaceEventInfoHistManager,
+        eventWeightHistManager,
+        electronHistManager,
+        tauHistManager,
+        diTauCandidateHistManagerForElecTau,
+        diTauCandidateSVfitHistManagerForElecTau,                                     
+        diTauCandidateZeeHypothesisHistManagerForElecTau,
+        jetHistManager,
+        caloMEtHistManager,
+        pfMEtHistManager,
+        particleMultiplicityHistManager,
+        vertexHistManager,
+        triggerHistManagerForElecTau
+    ),
+
+    analyzers_systematic = cms.VPSet(),                                     
+
+    eventDumps = cms.VPSet(
+        elecTauEventDump
+    ),
+   
+    analysisSequence = elecTauAnalysisSequence,
+
+    #estimateSysUncertainties = cms.bool(True),                                       
+    estimateSysUncertainties = cms.bool(False), 
+    systematics = cms.vstring(
+        getSysUncertaintyNames(
+            [ electronSystematics,
+              tauSystematics,
+              theorySystematics ]
+        )
+    )                                         
+)
+
---
+++ TauAnalysis/Configuration/python/producePatTupleAHtoElecTauSpecific_cff.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+from TauAnalysis.Configuration.producePatTupleZtoElecTauSpecific_cff import *
+
+#--------------------------------------------------------------------------------
+# produce all objects specific to MSSM Higgs A/H --> electron + tau-jet channel
+# which do **not** get stored in PATTuple
+#--------------------------------------------------------------------------------
+#
+# produce collections of electron + tau-jet pairs
+# passing different selection criteria
+#
+#from TauAnalysis.CandidateTools.elecTauPairSelectionForAHtoElecTau_cff import *
+#
+# produce data-formats providing information about compatibility
+# of an electron + tau-jet pair with the hypothesis
+# of being an electron pair resulting from a Z --> e+ e- decay
+#
+#from TauAnalysis.RecoTools.elecTauPairZeeHypothesisForAHtoElecTau_cff import *
+#
+# produce collections of central jets
+# passing b-tagging discriminator
+#
+#from TauAnalysis.RecoTools.patJetSelectionForAHtoElecTau_cff import *
+
+producePatTupleAHtoElecTauSpecific = cms.Sequence(
+    producePatTupleZtoElecTauSpecific
+	#   + selectMuTauPairsForAHtoMuTau + selectMuTauPairsForAHtoMuTauLooseMuonIsolation
+	#   + produceMuTauPairZmumuHypothesesForAHtoMuTau + produceMuTauPairZmumuHypothesesForAHtoMuTauLooseMuonIsolation
+)
+
