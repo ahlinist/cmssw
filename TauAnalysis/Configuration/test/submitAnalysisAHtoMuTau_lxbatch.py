de87b08f70083c50555f93c4d63434a8a73d5103
friis <>
2010-12-13 15:54:10 +0000

    Add tools for fake rates

--- TauAnalysis/Configuration/test/submitAnalysisAHtoMuTau_lxbatch.py
+++ TauAnalysis/Configuration/test/submitAnalysisAHtoMuTau_lxbatch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi \
         import recoSampleDefinitionsAHtoMuTau_7TeV as samples
 
@@ -8,9 +9,16 @@ import os
 
 channel = 'AHtoMuTau'
 configFile = 'runAHtoMuTau_cfg.py'
+
+reg.overrideJobId(channel, 'Run33SYS')
+
+# If this is a list, only the items in the list will be analyzed.
+samplesToAnalyze = None
+
 # Where we will send the output on castor
 outputPath = reg.getAnalysisFilePath(channel)
 jobId = reg.getJobId(channel)
+# Figure out where our root files were stored for the desired skim
 skimPath = reg.getSkimEvents(channel)
 
 # Get all the skim files from the castor directory
@@ -26,15 +34,23 @@ def inputFileMapper(channel, sample, jobId):
 def outputFileMapper(channel, sample, jobId):
     return "plots_%s_%s_%s.root" % (channel, sample, jobId)
 
-enableSystematics = False
+enableSystematics = True
+enableFakeRates = False
+
+# If we are running with fake rates, only analyze samples when it matters.
+if enableFakeRates:
+    samplesToAnalyze = [sample for sample in samples['SAMPLES_TO_ANALYZE']
+                        if samples['RECO_SAMPLES'][sample]['enableFakeRates']]
 
 submit.submitAnalysisToLXBatch(
     configFile=configFile,
     channel=channel,
     samples=samples,
+    samplesToAnalyze = samplesToAnalyze,
     outputDirectory = outputPath,
     jobId = jobId,
     disableSysUncertainties = not enableSystematics,
+    enableFakeRates = enableFakeRates,
     inputFileMap = inputFileMapper,
     outputFileMap = outputFileMapper,
     processName = 'lxbatch',
