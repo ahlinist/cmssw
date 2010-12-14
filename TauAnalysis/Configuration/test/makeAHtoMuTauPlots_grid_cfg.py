b5e6cf4e515c180c6f08b059130d40977567dffe
friis <>
2010-12-14 11:30:15 +0000

    Use correct analyzer names for OS/SS

--- TauAnalysis/Configuration/test/makeAHtoMuTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeAHtoMuTauPlots_grid_cfg.py
@@ -6,11 +6,12 @@ from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi \
 
 from TauAnalysis.Configuration.plotAHtoMuTau_drawJobs_cfi import \
         drawJobConfigurator_AHtoMuTau_woBtag, plots_AHtoMuTau, \
-        drawJobConfigurator_AHtoMuTau_wBtag
+        drawJobConfigurator_AHtoMuTau_wBtag,\
+        drawJobConfigurator_AHtoMuTau_woBtagSS,\
+        drawJobConfigurator_AHtoMuTau_wBtagSS\
 
 from TauAnalysis.Configuration.makePlots2_grid import makePlots
-from TauAnalysis.Configuration.userRegistry import getHarvestingFilePath, \
-        getJobId, overrideJobId
+import TauAnalysis.Configuration.userRegistry as reg
 
 # import utility function to enable factorization
 from TauAnalysis.Configuration.tools.factorizationTools import \
@@ -21,29 +22,47 @@ process = cms.Process('makeAHtoMuTauPlots')
 process.load("TauAnalysis.Configuration.dumpAHtoMuTau_grid_cff")
 
 channel = 'AHtoMuTau'
-#overrideJobId(channel, 'Run28')
-inputFilePath = getHarvestingFilePath(channel)
-jobId = getJobId(channel)
+#reg.overrideJobId(channel, 'Run33')
+inputFilePath = reg.getHarvestingFilePath(channel)
+jobId = reg.getJobId(channel)
 
 # Uncomment to enable local file running
-#inputFilePath = os.path.join(inputFilePath, 'local')
+def skimStatFileMapper(sample):
+    return os.path.join(
+        '/data1/friis/Run32',  'harvested_%s_%s_Run32.root' % (
+            channel, sample))
+        #getHarvestingFilePath(channel), 'harvested_%s_%s_%s.root' % (
+        #    channel, sample, jobId))
+
 
 makePlots(process, channel = channel,
           samples = recoSampleDefinitionsAHtoMuTau_7TeV,
           inputFilePath = inputFilePath, jobId = jobId,
           analyzer_drawJobConfigurator_indOutputFileName_sets = [
-              [ "ahMuTauAnalyzer_woBtag",
+              [ "ahMuTauAnalyzerOS_woBtag",
                drawJobConfigurator_AHtoMuTau_woBtag,
-               "plotAHtoMuTau_woBtag_#PLOT#.pdf" ],
-            [ "ahMuTauAnalyzer_wBtag",
-              drawJobConfigurator_AHtoMuTau_wBtag,
-             "plotAHtoMuTau_wBtag_#PLOT#.pdf"  ]
+               "plotAHtoMuTauOS_woBtag_#PLOT#.pdf" ],
+
+              [ "ahMuTauAnalyzerOS_wBtag",
+               drawJobConfigurator_AHtoMuTau_wBtag,
+               "plotAHtoMuTauOS_wBtag_#PLOT#.pdf"  ],
+
+              [ "ahMuTauAnalyzerSS_woBtag",
+               drawJobConfigurator_AHtoMuTau_woBtagSS,
+               "plotAHtoMuTauSS_woBtag_#PLOT#.pdf" ],
+
+              [ "ahMuTauAnalyzerSS_wBtag",
+               drawJobConfigurator_AHtoMuTau_wBtagSS,
+               "plotAHtoMuTauSS_wBtag_#PLOT#.pdf"  ],
           ],
+          # Uncomment to enable local file running
+          #skimStatFileMapper = skimStatFileMapper,
+          skimFilterStatistic = 'evtSelDiMuPairZmumuHypothesisVetoByLooseIsolation',
           drawJobTemplate = plots_AHtoMuTau,
           enableFactorizationFunction = enableFactorization_makeAHtoMuTauPlots_grid2,
           dqmDirectoryFilterStatistics = {
-              'factorizationDisabled' : 'ahMuTauAnalyzer_woBtag/FilterStatistics',
-              'factorizationEnabled' : 'ahMuTauAnalyzer_woBtag_factorizedWithMuonIsolation/FilterStatistics'
+              'factorizationDisabled' : 'ahMuTauAnalyzerOS_woBtag/FilterStatistics',
+              'factorizationEnabled' : 'ahMuTauAnalyzerOS_woBtag_factorizedWithMuonIsolation/FilterStatistics'
           },
           dumpDQMStore = False)
 
