92a4111951b7795508e233d35e58049414b30fb8
veelken <>
2010-12-22 10:03:35 +0000

    set muon trigger efficiency/isolation efficiency correction factors to 0.98 (values determined by Evan via tag & probe)

--- TauAnalysis/RecoTools/macros/makeMuonTriggerEfficiencyLUT.C
+++ TauAnalysis/RecoTools/macros/makeMuonTriggerEfficiencyLUT.C
@@ -3,8 +3,8 @@ makeMuonTriggerEfficiencyLUT()
   TH1* histoLUT = new TH2F("muonTriggerEfficiencyCorrection", "muonTriggerEfficiencyCorrection", 
 			   1, -2.5, +2.5, 1, 0., 10000.);
 
-  histoLUT->SetBinContent(1, 1, 0.95);
-  histoLUT->SetBinError(1, 1, 0.05);
+  histoLUT->SetBinContent(1, 1, 0.98);
+  histoLUT->SetBinError(1, 1, 0.02);
 
   TFile* outputFile = new TFile("../data/muonTriggerEfficiencyCorrection.root", "RECREATE");
   histoLUT->Write();
