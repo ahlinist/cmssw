3bed60e815afab18fae89e55ed8c4d3a6e13e65f
friis <>
2010-12-27 16:38:24 +0000

    Add logging for input and output hashes, and enable reading from crab output directories

--- TauAnalysis/Configuration/test/harvestAHtoMuTauPlots_lxbatch.py
+++ TauAnalysis/Configuration/test/harvestAHtoMuTauPlots_lxbatch.py
@@ -7,14 +7,15 @@ Harvest AHtoMu tau plots on LXBATCH
 '''
 
 from TauAnalysis.Configuration.tools.harvestingLXBatch import \
-        make_harvest_scripts
+        make_harvest_scripts, castor_source, crabdir_sources
 import TauAnalysis.Configuration.userRegistry as reg
 import os
+import glob
 
 channel = 'AHtoMuTau'
 
 #reg.overrideJobId(channel, 'Run35SYS')
-reg.overrideJobId(channel, 'Run35test')
+reg.overrideJobId(channel, 'Run33FR')
 analysisFilePath = reg.getAnalysisFilePath(channel)
 harvestingFilePath = reg.getHarvestingFilePath(channel)
 jobId = reg.getJobId(channel)
@@ -32,10 +33,16 @@ def local_copy_mapper(sample):
         harvestingFilePath,
         "_".join(['harvested', channel, sample, jobId]) + ".root")
 
+crab_dirs = glob.glob('crab/crabdir_*_%s' % jobId)
+
 make_harvest_scripts(
     plot_regex,
     skim_regex,
-    analysisFilePath,
+    channel,
+    #analysisFilePath,
+    #castor_source(analysisFilePath),
+    castor_source(analysisFilePath),
+    #crabdir_sources(*crab_dirs),
     tmpFilePath,
     local_copy_mapper = local_copy_mapper
 )
