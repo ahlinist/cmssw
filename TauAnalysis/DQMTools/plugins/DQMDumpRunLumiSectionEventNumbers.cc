c89eb329bb788e10169e728386c32ded4eaf4ff1
friis <>
2011-01-10 12:56:35 +0000

    Add helpful output

--- TauAnalysis/DQMTools/plugins/DQMDumpRunLumiSectionEventNumbers.cc
+++ TauAnalysis/DQMTools/plugins/DQMDumpRunLumiSectionEventNumbers.cc
@@ -29,25 +29,25 @@ DQMDumpRunLumiSectionEventNumbers::DQMDumpRunLumiSectionEventNumbers(const edm::
 
   dqmDirectories_ = cfg.getParameter<vstring>("dqmDirectories");
   if ( dqmDirectories_.size() == 0 ) {
-    edm::LogError("DQMDumpRunLumiSectionEventNumbers") 
+    edm::LogError("DQMDumpRunLumiSectionEventNumbers")
       << " Configuration Parameter dqmDirectories = " << format_vstring(dqmDirectories_)
       << " contains no Entries --> skipping !!";
     cfgError_ = 1;
   }
-  
+
   runLumiSectionEventNumberFileName_ = cfg.getParameter<std::string>("runLumiSectionEventNumberFileName");
   if ( runLumiSectionEventNumberFileName_ == "" ) {
-    edm::LogError("DQMDumpRunLumiSectionEventNumbers") 
-      << " Configuration Parameter runLumiSectionEventNumberFileName_ = " << runLumiSectionEventNumberFileName_ 
+    edm::LogError("DQMDumpRunLumiSectionEventNumbers")
+      << " Configuration Parameter runLumiSectionEventNumberFileName_ = " << runLumiSectionEventNumberFileName_
       << " invalid --> skipping !!";
     cfgError_ = 1;
   }
 
-  separator_ = cfg.exists("separator") ? 
+  separator_ = cfg.exists("separator") ?
     cfg.getParameter<std::string>("separator") : " ";
 }
 
-DQMDumpRunLumiSectionEventNumbers::~DQMDumpRunLumiSectionEventNumbers() 
+DQMDumpRunLumiSectionEventNumbers::~DQMDumpRunLumiSectionEventNumbers()
 {
 //--- nothing to be done yet
 }
@@ -59,18 +59,18 @@ void DQMDumpRunLumiSectionEventNumbers::analyze(const edm::Event&, const edm::Ev
 
 void DQMDumpRunLumiSectionEventNumbers::endJob()
 {
-  //std::cout << "<DQMDumpRunLumiSectionEventNumbers::endJob>:" << std::endl;
+  std::cout << "<DQMDumpRunLumiSectionEventNumbers::endJob>:" << std::endl;
 
 //--- check that configuration parameters contain no errors
   if ( cfgError_ ) {
-    edm::LogError ("endjob") 
+    edm::LogError ("endjob")
       << " Error in Configuration ParameterSet --> run + event Numbers will NOT be written !!";
     return;
   }
 
 //--- check that DQMStore service is available
   if ( !edm::Service<DQMStore>().isAvailable() ) {
-    edm::LogError ("endJob") 
+    edm::LogError ("endJob")
       << " Failed to access dqmStore --> run + event Numbers will NOT be written !!";
     return;
   }
@@ -81,16 +81,17 @@ void DQMDumpRunLumiSectionEventNumbers::endJob()
   typedef std::set<edm::EventNumber_t> eventNumberSet;
   typedef std::map<edm::LuminosityBlockNumber_t, eventNumberSet> lumiSectionEventNumberMap;
   std::map<edm::RunNumber_t, lumiSectionEventNumberMap> runLumiSectionEventNumbers;
-  
-//--- decode run + event numbers from names of MonitorElements 
+
+//--- decode run + event numbers from names of MonitorElements
   DQMStore& dqmStore = (*edm::Service<DQMStore>());
 
   for ( vstring::const_iterator dqmDirectory = dqmDirectories_.begin();
 	dqmDirectory != dqmDirectories_.end(); ++dqmDirectory ) {
-    //std::cout << "dqmDirectory = " << (*dqmDirectory) << std::endl;
+    std::cout << "dqmDirectory = " << (*dqmDirectory);
 
     dqmStore.setCurrentFolder(*dqmDirectory);
     std::vector<std::string> meNames = dqmStore.getMEs();
+    std::cout << " contains " << meNames.size() << " monitor elements. " << std::endl;
     for ( std::vector<std::string>::const_iterator meName = meNames.begin();
 	  meName != meNames.end(); ++meName ) {
       //std::cout << "meName = " << (*meName) << std::endl;
@@ -114,19 +115,19 @@ void DQMDumpRunLumiSectionEventNumbers::endJob()
 	  edm::LuminosityBlockNumber_t lumiSectionNumber = ((TObjString*)subStrings->At(2))->GetString().Atoll();
 	  edm::EventNumber_t eventNumber = ((TObjString*)subStrings->At(3))->GetString().Atoll();
 
-	  std::cout << "--> adding Run# = " << runNumber << "," 
-		    << " Luminosity Section# " << lumiSectionNumber << "," 
+	  std::cout << "--> adding Run# = " << runNumber << ","
+		    << " Luminosity Section# " << lumiSectionNumber << ","
 		    << " Event# " << eventNumber << std::endl;
 	  runLumiSectionEventNumbers[runNumber][lumiSectionNumber].insert(eventNumber);
 	} else {
 	  parseError = true;
-	}      
-      } else { 
+	}
+      } else {
 	parseError = true;
       }
 
       if ( parseError ) {
-	edm::LogError ("endJob") 
+	edm::LogError ("endJob")
 	  << " Failed to decode run + luminosity section + event Number from meName = " << (*meName) << " --> skipping !!";
       }
     }
