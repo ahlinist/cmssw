25ea2ccbb56a0b5e9a5625b2269aaa13e9c183de
veelken <>
2011-01-12 13:38:09 +0000

    python syntax fix

--- TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeZtoMuTauPlots_grid_cfg.py
@@ -19,8 +19,8 @@ jobId = getJobId(channel)
 makePlots(process, channel = channel, samples = recoSampleDefinitionsZtoMuTau_7TeV,
           inputFilePath = inputFilePath, jobId = jobId,
           analyzer_drawJobConfigurator_indOutputFileName_sets = [
-              [ "zMuTauAnalyzerOS", drawJobConfigurator_ZtoMuTauOS, "plotZtoMuTauOS_#PLOT#.png",
-	        "zMuTauAnalyzerSS", drawJobConfigurator_ZtoMuTauSS, "plotZtoMuTauSS_#PLOT#.png" ]
+              [ "zMuTauAnalyzerOS", drawJobConfigurator_ZtoMuTauOS, "plotZtoMuTauOS_#PLOT#.pdf" ],
+	      [ "zMuTauAnalyzerSS", drawJobConfigurator_ZtoMuTauSS, "plotZtoMuTauSS_#PLOT#.pdf" ]
           ],
           drawJobTemplate = plots_ZtoMuTau,
           enableFactorizationFunction = enableFactorization_makeZtoMuTauPlots_grid2,
