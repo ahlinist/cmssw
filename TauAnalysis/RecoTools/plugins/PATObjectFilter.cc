1d97902e731c0b826f8b1c08e0f66285e500920f
veelken <>
2011-01-19 10:11:20 +0000

    added PATCandViewMinFilter, PATCandViewMaxFilter

---
+++ TauAnalysis/RecoTools/plugins/PATObjectFilter.cc
@@ -0,0 +1,10 @@
+#include "TauAnalysis/RecoTools/plugins/PATObjectFilter.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+using namespace pat;
+
+DEFINE_FWK_MODULE(PATCandViewMinFilter);
+DEFINE_FWK_MODULE(PATCandViewMaxFilter);
+//DEFINE_FWK_MODULE(PATCandViewCountFilter);
+
