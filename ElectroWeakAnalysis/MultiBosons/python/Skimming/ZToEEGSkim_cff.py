72583f3b7eb64d2700a20a7b3c06ced7c8000195
veverka <>
2011-01-26 10:18:05 +0000

    fixed bug: hlt filter was missing in the path

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToEEGSkim_cff.py
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToEEGSkim_cff.py
@@ -15,33 +15,6 @@ ZToEEGHltFilter.HLTPaths = """
   HLT_Ele17_SW_TightCaloEleId_SC8HE_L1R
   """.split()
 
-
-# 196        if (eleRef->dr03TkSumPt()/eleRef->pt()>eb_trIsoCut_) return false;
-# 197        if (eleRef->dr03EcalRecHitSumEt()/eleRef->pt()>eb_ecalIsoCut_) return false;
-# 198        if (eleRef->dr03HcalTowerSumEt()/eleRef->pt()>eb_hcalIsoCut_) return false;
-# 199        if (eleRef->sigmaIetaIeta()>eb_seeCut_) return false;
-# 200        if (eleRef->hcalOverEcal()>eb_hoeCut_) return false;
-# if (eleRef->gsfTrack()->trackerExpectedHitsInner().numberOfHits()>missHitCut_) return false;
-
-# 008     ptCut = cms.double(20.),
-# 009     missHitsCut = cms.int32(1),
-
-# 010     eb_trIsoCut = cms.double(0.1),
-# 011     eb_ecalIsoCut = cms.double(0.1),
-# 012     eb_hcalIsoCut = cms.double(0.1),
-# 013     eb_hoeCut = cms.double(0.1),
-
-# 014     eb_seeCut = cms.double(0.014),
-
-# 015     ee_trIsoCut = cms.double(0.1),
-# 016     ee_ecalIsoCut = cms.double(0.1),
-# 017     ee_hcalIsoCut = cms.double(0.1),
-# 018     ee_hoeCut = cms.double(0.1),
-
-# 019     ee_seeCut = cms.double(0.035),
-
-# 021     invMassCut = cms.double(40.)
-
 ZToEEGLooseElectrons = cms.EDFilter("GsfElectronViewRefSelector",
     src = cms.InputTag("gsfElectrons"),
     cut = cms.string("""
@@ -49,38 +22,24 @@ ZToEEGLooseElectrons = cms.EDFilter("GsfElectronViewRefSelector",
         dr03TkSumPt                                        < 0.1 * pt &
         dr03HcalTowerSumEt                                 < 0.1 * pt &
         hcalOverEcal                                       < 0.1      &
-        gsfTrack().trackerExpectedHitsInner().numberOfHits > 1
+        gsfTrack().trackerExpectedHitsInner().numberOfHits > 1        &
+        (
+          (isEB & sigmaIetaIeta < 0.014) |
+          (isEE & sigmaIetaIeta < 0.035)
+        )
         """),
     filter = cms.bool(True)
     )
 
-ZToEEGLooseElectronsEB = cms.EDFilter("GsfElectronViewRefSelector",
+ZToEEGTightElectrons = cms.EDFilter("CandViewRefSelector",
     src = cms.InputTag("ZToEEGLooseElectrons"),
-    cut = cms.string("isEB & sigmaIetaIeta < 0.014"),
-    filter = cms.bool(False)
-    )
-
-ZToEEGLooseElectronsEE = cms.EDFilter("GsfElectronViewRefSelector",
-    src = cms.InputTag("ZToEEGLooseElectrons"),
-    cut = cms.string("isEE & sigmaIetaIeta < 0.035"),
-    filter = cms.bool(False)
-    )
-
-ZToEEGLooseElectronsEBEE = cms.EDProducer("CandViewRefMerger",
-    src = cms.VInputTag("ZToEEGLooseElectronsEB",
-                        "ZToEEGLooseElectronsEE"
-                        )
-    )
-
-ZToEEGTightElectrons = ZToEEGLooseElectrons.clone(
-    src = "ZToEEGLooseElectronsEBEE",
-    cut = "pt > 15",
+    cut = cms.string("pt > 15"),
     filter = cms.bool(True)
     )
 
 ZToEEGDielectrons = cms.EDProducer("CandViewShallowClonePtrCombiner",
-    decay = cms.string("ZToEEGTightElectrons@+ ZToEEGLooseElectrons@-"),
-    checkCharge = cms.bool(True),
+    decay = cms.string("ZToEEGTightElectrons ZToEEGLooseElectrons"),
+    checkCharge = cms.bool(False),
     cut = cms.string("35 < mass")
     )
 
@@ -90,10 +49,8 @@ ZToEEGDielectronsFilter = cms.EDFilter("CandViewCountFilter",
     )
 
 ZToEEGSkimFilterSequence = cms.Sequence(
+    ZToEEGHltFilter          *
     ZToEEGLooseElectrons     *
-    ZToEEGLooseElectronsEB   *
-    ZToEEGLooseElectronsEE   *
-    ZToEEGLooseElectronsEBEE *
     ZToEEGTightElectrons     *
     ZToEEGDielectrons        *
     ZToEEGDielectronsFilter
--- ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToMMGSkim_cff.py
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToMMGSkim_cff.py
@@ -5,39 +5,7 @@ import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 ZToMMGHltFilter = copy.deepcopy(hltHighLevel)
 ZToMMGHltFilter.throw = cms.bool(False)
-ZToMMGHltFilter.HLTPaths = """
-  HLT_Mu9
-  HLT_Mu11
-  HLT_Mu15
-  HLT_Mu15_v*
-
-  HLT_DoubleMu3_v2
-  HLT_DoubleMu5_v1
-  HLT_IsoMu9_PFTau15_v1
-  HLT_L2DoubleMu20_NoVertex_v1
-  HLT_Mu11_PFTau15_v1
-  HLT_Mu15_v1
-  HLT_Mu3_Ele8_HT70U_v1
-  HLT_Mu5_HT70U_v1
-  HLT_Mu5_Jet50U_v1
-  HLT_Mu5_MET45_v1
-  OpenHLT_DoubleMu3_HT50U
-  OpenHLT_IsoMu15
-  OpenHLT_IsoMu17
-  OpenHLT_Mu11_Ele8
-  OpenHLT_Mu17
-  OpenHLT_Mu19
-  OpenHLT_Mu21
-  OpenHLT_Mu30_NoVertex
-  OpenHLT_Mu5_Ele13
-  OpenHLT_Mu5_Ele17
-  OpenHLT_Mu5_HT100U
-  OpenHLT_Mu5_Jet70U
-  OpenHLT_Mu7_Photon13_L1R
-  OpenHLT_Mu8_Ele8
-  OpenHLT_IsoMu13
-  """.split()
-
+ZToMMGHltFilter.HLTPaths = ["*Mu*"]
 
 ZToMMGLooseMuons = cms.EDFilter("MuonViewRefSelector",
     src = cms.InputTag("muons"),
@@ -66,6 +34,7 @@ ZToMMGDimuonsFilter = cms.EDFilter("CandViewCountFilter",
     )
 
 ZToMMGSkimFilterSequence = cms.Sequence(
+    ZToMMGHltFilter *
     ZToMMGLooseMuons *
     ZToMMGTightMuons *
     ZToMMGDimuons *
