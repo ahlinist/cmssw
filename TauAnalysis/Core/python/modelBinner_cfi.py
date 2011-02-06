c525867f6791a4618a74d80d548490a2034a0bb5
veelken <>
2011-02-06 10:58:10 +0000

    added mZ binning in 3 ranges: 20-60, 60-120, 120-1000

--- TauAnalysis/Core/python/modelBinner_cfi.py
+++ TauAnalysis/Core/python/modelBinner_cfi.py
@@ -36,3 +36,42 @@ modelBinner = cms.PSet(
     ),
     dqmDirectory_store = cms.string('modelBinningResults')
 )
+
+modelBinner3mZbins = cms.PSet(
+    pluginName = cms.string('modelBinner3mZbins'),
+    pluginType = cms.string('ModelBinner'),
+    binning = cms.PSet(
+        name = cms.string("modelBinning3mZbins"),
+        config = cms.VPSet(
+            cms.PSet(
+                extractor = cms.PSet(
+                    pluginType = cms.string("PATMuTauPairValExtractor"),
+                    src = cms.InputTag(''),
+                    value = cms.string("svFitSolution('psKine_MEt_ptBalance').mass()")
+                ),
+                binning = cms.PSet(
+                    boundaries = cms.vdouble(20., 60., 120.),
+                    min = cms.double(-1.01),
+                    max = cms.double(1000.)
+                )
+            )
+        )
+    ),
+    genBinning = cms.PSet(
+        config = cms.VPSet(
+            cms.PSet(
+                extractor = cms.PSet(
+                    pluginType = cms.string("PATMuTauPairValExtractor"),
+                    src = cms.InputTag(''),
+                    value = cms.string("p4gen().mass()")
+                )
+            )
+        )
+    ),
+    srcGenFlag = cms.InputTag(""),
+    srcRecFlag = cms.InputTag(""),
+    binningService = cms.PSet(
+        pluginType = cms.string("ModelBinningService")
+    ),
+    dqmDirectory_store = cms.string('modelBinningResults3mZbins')
+)
