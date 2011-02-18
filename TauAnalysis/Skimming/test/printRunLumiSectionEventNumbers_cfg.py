8f9195bd35fefc19e8f76ea841c6e1e10fc3d373
veelken <>
2011-02-18 11:37:23 +0000

    first check-in of tool for checking crab output, print run:lumi-section:event numbers and producing file for iSpy event display

---
+++ TauAnalysis/Skimming/test/printRunLumiSectionEventNumbers_cfg.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("printRunLumiSectionEventNumbers")
+
+# import of standard configurations for RECOnstruction
+# of electrons, muons and tau-jets with non-standard isolation cones
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
+#process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = cms.string('START38_V14::All')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'rfio:/castor/cern.ch/user/f/friis/weirdMuonSpike/2010B_data_muonSpike.root'
+    )
+)
+
+process.printRunLumiSectionEventNumbers = cms.EDAnalyzer("PrintRunLumiSectionEventNumber")
+
+process.o = cms.EndPath(process.printRunLumiSectionEventNumbers)
+
