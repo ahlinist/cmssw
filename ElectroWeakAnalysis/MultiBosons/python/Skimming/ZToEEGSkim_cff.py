ae4383a8a2e466eb4de02a4bacc6c8e8da226195
veverka <>
2011-02-23 08:09:09 +0000

    fixed the missing hits cut

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToEEGSkim_cff.py
+++ ElectroWeakAnalysis/MultiBosons/python/Skimming/ZToEEGSkim_cff.py
@@ -6,37 +6,41 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 ZToEEGHltFilter = copy.deepcopy(hltHighLevel)
 ZToEEGHltFilter.throw = cms.bool(False)
 ZToEEGHltFilter.HLTPaths = """
-  HLT_Ele15_LW_L1R
-  HLT_Ele15_SW_L1R
-  HLT_Ele15_SW_CaloEleId_L1R
-  HLT_Ele17_SW_CaloEleId_L1R
-  HLT_Ele17_SW_L1R
-  HLT_Ele17_SW_TightEleId_L1R
-  HLT_Ele17_SW_TighterEleIdIsol_L1R_v2
-  HLT_Ele17_SW_TighterEleIdIsol_L1R_v3
-  HLT_Ele17_SW_TightCaloEleId_SC8HE_L1R
-  HLT_DoubleEle10_SW_L1R
-  HLT_DoubleEle15_SW_L1R_v1
-  HLT_DoubleEle17_SW_L1R_v1
-  HLT_Ele17_SW_TightCaloEleId_SC8HE_L1R_v1
-  HLT_Ele17_SW_TightCaloEleId_Ele8HE_L1R_v1
-  HLT_Ele17_SW_TightCaloEleId_Ele8HE_L1R_v2
-  """.split()
+    HLT_Photon15_L1R
+    HLT_Photon15_Cleaned_L1R
+    HLT_Photon20_Cleaned_L1R
+    HLT_Ele15_LW_L1R
+    HLT_Ele15_SW_L1R
+    HLT_Ele15_SW_CaloEleId_L1R
+    HLT_Ele17_SW_CaloEleId_L1R
+    HLT_Ele17_SW_L1R
+    HLT_Ele17_SW_TightEleId_L1R
+    HLT_Ele17_SW_TighterEleIdIsol_L1R_v2
+    HLT_Ele17_SW_TighterEleIdIsol_L1R_v3
+    HLT_Ele17_SW_TightCaloEleId_SC8HE_L1R
+    HLT_DoubleEle10_SW_L1R
+    HLT_DoubleEle15_SW_L1R_v1
+    HLT_DoubleEle17_SW_L1R_v1
+    HLT_Ele17_SW_TightCaloEleId_SC8HE_L1R_v1
+    HLT_Ele17_SW_TightCaloEleId_Ele8HE_L1R_v1
+    HLT_Ele17_SW_TightCaloEleId_Ele8HE_L1R_v2
+    """.split()
 
 ZToEEGLooseElectrons = cms.EDFilter("GsfElectronViewRefSelector",
     src = cms.InputTag("gsfElectrons"),
     cut = cms.string("""
-        pt                                                 > 10       &
-        dr03TkSumPt                                        < 0.1 * pt &
-        dr03HcalTowerSumEt                                 < 0.1 * pt &
-        hcalOverEcal                                       < 0.1      &
-        gsfTrack().trackerExpectedHitsInner().numberOfHits > 1        &
-        (
-          (isEB & sigmaIetaIeta < 0.014) |
-          (isEE & sigmaIetaIeta < 0.035)
-        )
+        pt                      > 10  &
+        hcalOverEcal            < 0.1 &
+        dr03TkSumPt / pt        < 0.1 &
+        dr03HcalTowerSumEt / pt < 0.1 &
+        gsfTrack().trackerExpectedHitsInner().numberOfHits <= 1
         """),
     filter = cms.bool(True)
+    )
+
+ZToEEGLooseElectronsCountFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("ZToEEGLooseElectrons"),
+    minNumber = cms.uint32(2)
     )
 
 ZToEEGTightElectrons = cms.EDFilter("CandViewRefSelector",
@@ -46,9 +50,9 @@ ZToEEGTightElectrons = cms.EDFilter("CandViewRefSelector",
     )
 
 ZToEEGDielectrons = cms.EDProducer("CandViewShallowClonePtrCombiner",
-    decay = cms.string("ZToEEGTightElectrons ZToEEGLooseElectrons"),
-    checkCharge = cms.bool(False),
-    cut = cms.string("35 < mass")
+    decay = cms.string("ZToEEGTightElectrons@+ ZToEEGLooseElectrons@-"),
+    checkCharge = cms.bool(True),
+    cut = cms.string("20 < mass")
     )
 
 ZToEEGDielectronsFilter = cms.EDFilter("CandViewCountFilter",
@@ -57,8 +61,9 @@ ZToEEGDielectronsFilter = cms.EDFilter("CandViewCountFilter",
     )
 
 ZToEEGSkimFilterSequence = cms.Sequence(
-    ZToEEGHltFilter          *
+    ZToEEGHltFilter          +
     ZToEEGLooseElectrons     *
+    ZToEEGLooseElectronsCountFilter *
     ZToEEGTightElectrons     *
     ZToEEGDielectrons        *
     ZToEEGDielectronsFilter
