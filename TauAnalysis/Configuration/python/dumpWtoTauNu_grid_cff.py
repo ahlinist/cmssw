85da4a82a13c5c21c8e4d43dd42c57863792f6db
liis <>
2011-02-27 07:01:32 +0000

    correct tabbing

--- TauAnalysis/Configuration/python/dumpWtoTauNu_grid_cff.py
+++ TauAnalysis/Configuration/python/dumpWtoTauNu_grid_cff.py
@@ -13,15 +13,15 @@ def sample_dqm_name(sample):
         return "%s_factorized" % sample
     else:
         return sample
-    
-    dumpWtoTauNu = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-                                    dqmDirectories = cms.PSet(
-        **dict(
-        (sample, cms.string(
-        '/harvested/%s/wTauNuAnalyzer/FilterStatistics'
-        % sample_dqm_name(sample))
-         ) for sample in samples.SAMPLES_TO_PRINT)
-        ),
-                                  columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
-                                  )
+
+dumpWtoTauNu = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
+                              dqmDirectories = cms.PSet(
+    **dict(
+    (sample, cms.string(
+    '/harvested/%s/wTauNuAnalyzer/FilterStatistics'
+    % sample_dqm_name(sample))
+     ) for sample in samples.SAMPLES_TO_PRINT)
+    ),
+                              columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
+                              )
     
