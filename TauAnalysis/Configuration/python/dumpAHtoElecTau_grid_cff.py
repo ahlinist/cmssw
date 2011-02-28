26370afd761e4cd2e3a0f54a8acd52b1dbc3d902
jkolb <>
2011-02-28 15:19:42 +0000

    update for OS/SS workflows

--- TauAnalysis/Configuration/python/dumpAHtoElecTau_grid_cff.py
+++ TauAnalysis/Configuration/python/dumpAHtoElecTau_grid_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-import TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi as samples
+from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import ALL_SAMPLES as all_samples
+from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import SAMPLES_TO_PRINT as samples_to_print
 
 #--------------------------------------------------------------------------------
 # Print-out cut-flow information for Z --> muon + tau-jet channel
@@ -8,7 +9,7 @@ import TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi as sa
 
 def sample_dqm_name(sample):
     " Retrieve the containing DQM folder for a given sample "
-    sample_info = samples.ALL_SAMPLES[sample]
+    sample_info = all_samples[sample]
     if 'factorize' in sample_info and sample_info['factorize']:
         return "%s_factorized" % sample
     else:
@@ -18,9 +19,9 @@ dumpAHtoElecTau = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
     dqmDirectories = cms.PSet(
         **dict(
             (sample, cms.string(
-                '/harvested/%s/ahElecTauAnalyzer/FilterStatistics' 
+                '/harvested/%s/ahElecTauAnalyzerOS_woBtag/FilterStatistics' 
                 % sample_dqm_name(sample))
-            ) for sample in samples.SAMPLES_TO_PRINT)
+            ) for sample in samples_to_print)
     ), 
     columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
 )
