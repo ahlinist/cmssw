bb07863afd9bf2576e3c700034b6466006663b6e
veverka <>
2011-03-01 20:04:51 +0000

    Added options for hlt, collision cleaning and skimming filter on the fly

--- ElectroWeakAnalysis/MultiBosons/test/MmgFsrRecoAnalyzer_cfg.py
+++ ElectroWeakAnalysis/MultiBosons/test/MmgFsrRecoAnalyzer_cfg.py
@@ -1,15 +1,48 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
 
+## setup 'analysis'  options
+options = VarParsing.VarParsing ('analysis')
+
+## register customized options
+def newBoolOption(name, default, description):
+    options.register(name,
+                     default,
+                     options.multiplicity.singleton,
+                     options.varType.bool,
+                     description)
+
+newBoolOption("applyHltFilter", False, "Apply the HLT filter?")
+newBoolOption("applyNoScrapingFilter", True, "Apply the no scraping filter?")
+newBoolOption("applyPrimaryVertexFilter", True,
+              "Apply the primary vertex filter?")
+newBoolOption("applySkimFilter", True, "Apply the skim filter?")
+
+options.register("hltProcessName",
+    "REDIGI39X",                    # default value
+    options.multiplicity.singleton, # singleton or list
+    options.varType.string,         # bool, string, int, or float
+    "Name of the Process that produced the HLT information."
+    )
+
+## setup any defaults you want
+options.outputFile = "MmgFsrRecoAnalyzer.root"
+options.maxEvents = -1 # -1 means all events
+options.inputFiles = ["/store/mc/Winter10/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/GEN-SIM-RECO/E7TeV_ProbDist_2010Data_BX156_START39_V8-v1/0033/FE75602D-4810-E011-B662-1CC1DE051038.root"]
+
+## get and parse the command line arguments
+options.parseArguments()
+
+
+## define the process
 process = cms.Process("Test")
 
 process.source = cms.Source("PoolSource",
-  fileNames = cms.untracked.vstring(
-    '/store/mc/Winter10/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/GEN-SIM-RECO/E7TeV_ProbDist_2010Data_BX156_START39_V8-v1/0033/FE75602D-4810-E011-B662-1CC1DE051038.root'
-  )
+  fileNames = cms.untracked.vstring() + options.inputFiles
 )
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(-1)
+  input = cms.untracked.int32(options.maxEvents)
 )
 
 ## Detector Conditions (needed for a EcalChannelStatusRcd)
@@ -23,22 +56,52 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
-## Add cleaning of collision data (no scraping events etc.)
-##+ https://twiki.cern.ch/twiki/bin/viewauth/CMS/Collisions2010Recipes
-basePath = "ElectroWeakAnalysis.MultiBosons.Skimming." # shorthand
-process.load(basePath + "goodCollisionDataSequence_cff")
-## Remove the hltPhysicsDeclared - it kills some good events, reference?
+## Load all sequences
+process.load("ElectroWeakAnalysis.MultiBosons.Skimming.goodCollisionDataSequence_cff")
+process.load("ElectroWeakAnalysis.MultiBosons.Skimming.ZToMMGSkim_cff")
+process.load("ElectroWeakAnalysis.MultiBosons.MmgFsrRecoAnalyzer_cfi")
+
+process.ZToMMGSkimFilterSequence.replace(
+    process.ZToMMGHltFilter,
+    process.ZToMMGHltFilter + process.goodCollisionDataSequence
+    )
+
+process.p = cms.Path(process.ZToMMGSkimFilterSequence + process.analyzeMmgFsr)
+
+## Remove the hltPhysicsDeclared - it kills some good events
 process.goodCollisionDataSequence.remove(process.hltPhysicsDeclared)
 
-process.load("ElectroWeakAnalysis.MultiBosons.MmgFsrRecoAnalyzer_cfi")
+if not options.applyHltFilter:
+    process.p.remove(process.ZToMMGHltFilter)
+
+if not options.applyNoScrapingFilter:
+    process.p.remove(process.noScraping)
+
+if not options.applyPrimaryVertexFilter:
+    process.p.remove(process.primaryVertexFilter)
+
+if not options.applySkimFilter:
+    for module in """ZToMMGMuonsCountFilter
+                     ZToMMGLooseMuons
+                     ZToMMGLooseMuonsCountFilter
+                     ZToMMGTightMuons
+                     ZToMMGDimuons
+                     ZToMMGDimuonsFilter""".split():
+        process.p.remove( getattr(process, module) )
+
+process.ZToMMGHltFilter.TriggerResultsTag = cms.InputTag(
+    "TriggerResults", "", options.hltProcessName
+    )
 
 process.TFileService = cms.Service("TFileService",
   fileName = cms.string('MmgFsrRecoAnalyzer.root')
 )
 
-process.p = cms.Path(process.goodCollisionDataSequence + process.analyzeMmgFsr)
+process.options.wantSummary = True
 
 ## Enable LogDebug for analyzeMmgFsr module
 # process.MessageLogger.debugModules = ["analyzeMmgFsr"]
 # process.MessageLogger.cerr.threshold = "DEBUG"
+
+if __name__ == "__main__": import user
 
