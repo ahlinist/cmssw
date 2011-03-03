b5487918e26856192ab67d264a3a8fe7d7217227
jkolb <>
2011-03-03 13:44:12 +0000

    add config for conversion plots

--- TauAnalysis/Core/python/electronHistManager_cfi.py
+++ TauAnalysis/Core/python/electronHistManager_cfi.py
@@ -3,41 +3,44 @@ import FWCore.ParameterSet.Config as cms
 from TauAnalysis.RecoTools.patLeptonSelection_cff import selectedPatElectronsForElecTauIso
 
 electronHistManager = cms.PSet(
-    pluginName = cms.string('electronHistManager'),
-    pluginType = cms.string('ElectronHistManager'),
-      
-    electronSource = cms.InputTag('cleanPatElectrons'),
-    vertexSource = cms.InputTag('selectedPrimaryVertexHighestPtTrackSum'),
-    jetSource = cms.InputTag('selectedPatJetsEt20Cumulative'),
-    genParticleSource = cms.InputTag('genParticles'),
+	pluginName = cms.string('electronHistManager'),
+	pluginType = cms.string('ElectronHistManager'),
 
-    dqmDirectory_store = cms.string('ElectronQuantities'),
+	electronSource = cms.InputTag('cleanPatElectrons'),
+	vertexSource = cms.InputTag('selectedPrimaryVertexHighestPtTrackSum'),
+	jetSource = cms.InputTag('selectedPatJetsEt20Cumulative'),
+	genParticleSource = cms.InputTag('genParticles'),
 
-    #requireGenElectronMatch = cms.bool(True),
-    requireGenElectronMatch = cms.bool(False),
+	dqmDirectory_store = cms.string('ElectronQuantities'),
 
-    skipPdgIdsGenParticleMatch = cms.vint32( 12, 14, 16 ),
+	#requireGenElectronMatch = cms.bool(True),
+	requireGenElectronMatch = cms.bool(False),
+
+	skipPdgIdsGenParticleMatch = cms.vint32( 12, 14, 16 ),
 
 	pfCombIsoExtractor = cms.PSet(
 		chargedHadronIso = selectedPatElectronsForElecTauIso.chargedHadronIso,
 		neutralHadronIso = selectedPatElectronsForElecTauIso.neutralHadronIso,
 		photonIso = selectedPatElectronsForElecTauIso.photonIso
-	),
+		),
 	pfChargedHadronIsoExtractor = cms.PSet(
 		chargedHadronIso = selectedPatElectronsForElecTauIso.chargedHadronIso
-	),
+		),
 	pfNeutralHadronIsoExtractor = cms.PSet(
 		neutralHadronIso = selectedPatElectronsForElecTauIso.neutralHadronIso
-	),
+		),
 	pfPhotonIsoExtractor = cms.PSet(
 		photonIso = selectedPatElectronsForElecTauIso.photonIso
-	),
+		),
 
 	pfCandidateSource = selectedPatElectronsForElecTauIso.pfCandidateSource,
 
-    #normalization = cms.string("electrons"),
-    normalization = cms.string("events"),
+	#normalization = cms.string("electrons"),
+	normalization = cms.string("events"),
 
-    makeIsoPtCtrlHistograms = cms.bool(False),
-    makeIsoPtConeSizeDepHistograms = cms.bool(False)
+	makeIsoPtCtrlHistograms = cms.bool(False),
+	makeIsoPtConeSizeDepHistograms = cms.bool(False),
+
+	makeConversionHistograms = cms.bool(False),
+	conversionPartnerTrackSrc = cms.InputTag("generalTracks")
 )
