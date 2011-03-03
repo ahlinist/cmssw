c65020fc77703fa3d18a16335e7cda2c4a6f2f30
jkolb <>
2011-03-03 13:41:35 +0000

    new conversion veto module

--- TauAnalysis/RecoTools/plugins/PATElectronConversionFinder.cc
+++ TauAnalysis/RecoTools/plugins/PATElectronConversionFinder.cc
@@ -1,72 +1,52 @@
 //
 // Original Author:  Konstantinos A. Petridis
 //         Created:  Wed Oct 15 21:33:34 CEST 2008
-// $Id: PATElectronConversionFinder.cc,v 1.4 2010/02/24 10:25:12 jkolb Exp $
+//		Major re-write: Jeff Kolb, March 2011
+// $Id: PATElectronConversionFinder.cc,v 1.5 2010/05/21 07:49:25 veelken Exp $
 //
 //
+#include "RecoEgamma/EgammaTools/interface/ConversionFinder.h"
 
 #include "TauAnalysis/RecoTools/plugins/PATElectronConversionFinder.h"
-#include "DataFormats/Math/interface/Vector3D.h" 
-#include "DataFormats/GsfTrackReco/interface/GsfTrackExtra.h"
-#include "DataFormats/Math/interface/deltaR.h"
-#include "DataFormats/PatCandidates/interface/Photon.h"
 
 PATElectronConversionFinderImp::PATElectronConversionFinderImp(const edm::ParameterSet& iConfig)
 {
-  tracks_ = iConfig.getParameter<edm::InputTag>("trackSource");
-  conversions_ = iConfig.getParameter<edm::InputTag>("conversionSource");
-  photons_ = iConfig.getParameter<edm::InputTag>("photonSource");
-  cotTheta_ = iConfig.getParameter<double>("cotThetaCut");
-  dca_ = iConfig.getParameter<double>("docaElecTrack");
-  dRin_ = iConfig.getParameter<double>("dRElecTrack");
-  doPXBcut_= iConfig.getParameter<bool>("doPixCut");
-  nMax_ = iConfig.getParameter<double>("nTrkMax");
-  doHists_ = iConfig.getParameter<bool>("doHists");
-  useInnerParsForElec_ = iConfig.getParameter<bool>("useInnerParsForElec");
-  useInnerParsForTrks_ = iConfig.getParameter<bool>("useInnerParsForTrks");
-  useConversionColl_ = iConfig.getParameter<bool>("useConversionColl");
-  
-  
-  if(doHists_){
-    edm::Service<TFileService> fs;             
-    
-	hDeltaCotTheta=fs->make<TH1F>("DcotTheta","#Deltacot(#Theta)",500,-0.5,0.5);
-    hDeltaCotThetaFilt=fs->make<TH1F>("DcotThetaFiltered","DcotTheta for events with 2 trks passing selection",200,-0.5,0.5);
-    hDeltaCotThetaOS=fs->make<TH1F>("DcotThetaOS","DcotTheta for gsfElectron + OS trks",200,-0.5,0.5);
-  	
-	hDeltaR = fs->make<TH1F>("dR","#DeltaR",300,0,2);
-    hDeltaRFilt=fs->make<TH1F>("dRinFiltered","#Delta R_{in} for events with 2 trks passing selection",100,0,0.4);
-    hDeltaROS=fs->make<TH1F>("dRinOS","#Delta R_{in} for gsfElectron + OS tracks",100,0,0.4);
-    
-	hMassTrkAndElec=fs->make<TH1F>("mTrkAndElec","inv. mass of track and electron",300,0,30);
-    hMassTrkAndElecFilt=fs->make<TH1F>("mTrkAndElecFiltered","inv. mass of track and electron for for evth 2 trks passing selection",100,0,1);
-    hMassTrkAndElecOS=fs->make<TH1F>("mTrkAndElecOS","inv. mass of track and electron for gsfElectron + OS trks",100,0,1);
-    
-	hAbsPtDiff = fs->make<TH1F>("hAbsPtDiff","abs(Pt_{e}-Pt_{trk})",500,0,50);
-    hAbsPtDiffFilt = fs->make<TH1F>("hAbsPtDiffFiltered","abs(Pt_{1}-Pt_{2}) for events with 2 tracks passing selection",100,0,50);
-    hAbsPtDiffOS = fs->make<TH1F>("hAbsPtDiffOS","abs(Pt_{1}-Pt_{2}) for gsfElectron + OS trks",100,0,50);
-    
-	hPt = fs->make<TH1F>("hPt","Pt of all tracks",50,0,20);
-	hPtOSFilt = fs->make<TH1F>("hPtFiltered","Pt of OS trk for events with 2 trks passing selection ",50,0,20);
-	hPtOS = fs->make<TH1F>("hPtOS","Pt of OS tracks ",50,0,20);
+	//  get partner track source
+	tracks_ = iConfig.getParameter<edm::InputTag>("trackSource");
 	
-	hOS = fs->make<TH1F>("hOS","Opposite sign tracks",2,0,2);
-	hOSFilt = fs->make<TH1F>("hOSFiltered","Opposite sign for 2 tracks passing selection",2,0,2);
-    
-	hnTracksPass=fs->make<TH1F>("nTracksPass","num tracks passing #Delta cot(#Theta) and #DeltaR_{in} cuts",10,0,10);
-    
-	hDzin = fs->make<TH1F>("dZin","#Deltaz_{in}",500,-20,20);
-	hDzinFilt = fs->make<TH1F>("dZinFiltered","#Deltaz_{in} for events with 2 trks passing selection",500,-20,20);
-	hDzinOS = fs->make<TH1F>("dZinOS","#Deltaz_{in} for gElectron + OS trks",500,-20,20);
+	//  choose algorithm
+	usePogMethod_ = iConfig.getParameter<bool>("usePogMethod");
+	if( usePogMethod_ ) {
+		dcsTag_ = iConfig.getParameter<edm::InputTag>("dcsTag");
+		isData_ = iConfig.getParameter<bool>("isData");
+		docaMax_ = iConfig.getParameter<double>("docaElecTrackMax");
+	} else {
+		conversions_ = iConfig.getParameter<edm::InputTag>("conversionSource");
+		photons_ = iConfig.getParameter<edm::InputTag>("photonSource");
+		useConversionColl_ = iConfig.getParameter<bool>("useConversionColl");
+		useOSPairs_ = iConfig.getParameter<bool>("useOnlyOSPairs");
+		dRinMax_ = iConfig.getParameter<double>("dRElecTrackMax");
+	}
+
+	//  choose pixel cut
+	if( iConfig.exists("doPixCut") ) 
+		doPXBcut_= iConfig.getParameter<bool>("doPixCut");
+	if( iConfig.exists("doMissingHitsCut") ) 
+		doMissingHitsCut_= iConfig.getParameter<bool>("doMissingHitsCut");
 	
-	hPXFDisk=fs->make<TH1F>("PXFDisk","FPix disk of innermost electron hit",5,0,5);
-    hPXBLayer=fs->make<TH1F>("PXBLayer","BPix layer of innermost electron hit",5,0,5);
-  
-  	nElecs = fs->make<TH1F>("nElectrons","Number of Electrons",10,0,10);
-  	nPhotons = fs->make<TH1F>("nPhotons","Number of Photons",10,0,10);
-  	nConv = fs->make<TH1F>("nConv","Number of Conversions",10,0,10);
-  	nConvTrks = fs->make<TH1F>("nConvTrks","Number of Conversion Tracks",20,0,20);
-  }
+	if( doPXBcut_ && doMissingHitsCut_ ) {
+		edm::LogWarning("PATElectronConversionFinder") << "Must not set "
+			<< " both doPixCut and doMissingHitsCut to TRUE!" << std::endl
+			<< " Setting doMissingHitsCut=False";
+		doMissingHitsCut_ = 0;
+	}
+	
+	//  choose deltaCot(theta) limits
+	cotThetaMax_ = iConfig.getParameter<double>("cotThetaMax");
+	
+	//  choose limits on number of conversion track pairs
+	nPairMax_ = iConfig.getParameter<double>("nConvPairMax");
+	nPairMin_ = iConfig.getParameter<double>("nConvPairMin");
 }
 
 
@@ -78,185 +58,113 @@ void PATElectronConversionFinderImp::select(const edm::Handle<collection>& elecs
 
 	selected_.clear();
 
-	using namespace edm;
-	using namespace reco;
-
-	eventWeight_ = 1;
-
 	edm::Handle<reco::TrackCollection> tracks;
 	iEvent.getByLabel(tracks_,tracks);
-	const reco::TrackCollection* trackCollection = tracks.product();
 
-	edm::Handle<reco::ConversionCollection> conversions;
-	iEvent.getByLabel(conversions_, conversions);
+	//using namespace edm;
+	//using namespace reco;
 
-	edm::Handle<reco::PhotonCollection> photons;
-	//edm::Handle<pat::PhotonCollection> photons;	
-	iEvent.getByLabel(photons_, photons);
-
-	/*	
-		edm::ESHandle<MagneticField> magFieldHandle;
-		iSetup.get<IdealMagneticFieldRecord>().get( magFieldHandle );
-		const MagneticField* magField = magFieldHandle.product();
-	 */
-	
-	
-	if(doHists_) nElecs->Fill(elecs->size(), eventWeight_);
-	
-	for( std::vector<pat::Electron>::const_iterator elec=elecs->begin();elec!=elecs->end(); ++elec)
+	// iterate over GSF electrons in event
+	for( std::vector<pat::Electron>::const_iterator elec = elecs->begin(); elec != elecs->end(); ++elec)
 	{
 		int nTracksPass = 0;
 
-		// save electron and tracks for later comparison
-		reco::TrackCollection trackRefVec;
-		reco::GsfTrackRef elecRef = (*elec).gsfTrack();
+		if( !usePogMethod_) {  // use OLD method
 
-		// use conversion tracks instead of general tracks
-		if(useConversionColl_) {
-			reco::TrackCollection *trackCollectionTmp = new reco::TrackCollection();
+			edm::Handle<reco::ConversionCollection> conversions;
+			iEvent.getByLabel(conversions_, conversions);
+
+			edm::Handle<reco::PhotonCollection> photons;
+			iEvent.getByLabel(photons_, photons);
 			
-			if(doHists_) nPhotons->Fill(photons->size(),eventWeight_);
+			const reco::TrackCollection* trackCollection = tracks.product();
 			
-			for( unsigned int iPhoton = 0; iPhoton < photons->size(); iPhoton++) {
-				reco::Photon photon = photons->at(iPhoton);
-				//pat::Photon photon = photons->at(iPhoton);
-				reco::ConversionRefVector conversions = photon.conversions();
-				if(doHists_) nConv->Fill(conversions.size(),eventWeight_);
-				for( unsigned int iConversion = 0; iConversion < conversions.size(); iConversion++) {
-					std::vector<reco::TrackRef> tracks = conversions.at(iConversion)->tracks();
-					if(doHists_) nConvTrks->Fill(tracks.size(),eventWeight_);
-					for( unsigned int iTrk = 0; iTrk < tracks.size(); iTrk++) {
-						trackCollectionTmp->push_back( *(tracks.at(iTrk)) );
+			if(useConversionColl_) {
+				// use conversion tracks instead of general tracks
+				// get track from best conversion by taking conversion from photon
+				reco::TrackCollection *trackCollectionTmp = new reco::TrackCollection();
+
+				for( unsigned int iPhoton = 0; iPhoton < photons->size(); iPhoton++) {
+					reco::Photon photon = photons->at(iPhoton);
+					reco::ConversionRefVector conversions = photon.conversions();
+					for( unsigned int iConversion = 0; iConversion < conversions.size(); iConversion++) {
+						std::vector<reco::TrackRef> tracks = conversions.at(iConversion)->tracks();
+						for( unsigned int iTrk = 0; iTrk < tracks.size(); iTrk++) {
+							trackCollectionTmp->push_back( *(tracks.at(iTrk)) );
+						}
 					}
 				}
+				trackCollection = trackCollectionTmp;
 			}
-			trackCollection = trackCollectionTmp;
-		
-		}
-		
-		// loop over general tracks or conversion tracks 
-		for(size_t trk = 0; trk < trackCollection->size(); trk++)
-		{
-			// get dR between gsf electron and track
-			double dR = 1000;
-			// get z-component of dR between gsf electron and track
-			double dZ = 1000;
-			// get cot(theta) of opening angle between gsf electron and track
-			double cotTheta = 1000;
-			if( useInnerParsForElec_ && useInnerParsForTrks_ ) { 
+
+			// loop over partner tracks
+			for(size_t trk = 0; trk < trackCollection->size(); trk++)
+			{
+				if(useOSPairs_ && ( trackCollection->at(trk).charge()*(*elec).gsfTrack()->charge() ) == 0) continue;
+				// get dR between gsf electron and track
+				double dR = 1000;
+				// get z-component of dR between gsf electron and track
+				double dZ = 1000;
+				// get cot(theta) of opening angle between gsf electron and track
+				double cotTheta = 1000;
+
 				dR = ROOT::Math::VectorUtil::DeltaR(trackCollection->at(trk).innerPosition(),
 						(*elec).gsfTrack()->innerPosition());
 				dZ = trackCollection->at(trk).innerPosition().z()-(*elec).gsfTrack()->innerPosition().z();
 				cotTheta = 1./tan((*elec).gsfTrack()->innerMomentum().Theta())-1./tan(trackCollection->at(trk).innerMomentum().Theta());
-			} else if( useInnerParsForElec_ && !useInnerParsForTrks_ ) { 
-				dR = ROOT::Math::VectorUtil::DeltaR(trackCollection->at(trk).referencePoint(),
-						(*elec).gsfTrack()->innerPosition());
-				dZ = trackCollection->at(trk).referencePoint().z()-(*elec).gsfTrack()->innerPosition().z();
-				cotTheta = 1./tan((*elec).gsfTrack()->innerMomentum().Theta())-1./tan(trackCollection->at(trk).momentum().Theta());
-			} else if( !useInnerParsForElec_ && useInnerParsForTrks_ ) { 
-				dR = ROOT::Math::VectorUtil::DeltaR(trackCollection->at(trk).innerPosition(),
-						(*elec).gsfTrack()->referencePoint());
-				dZ = trackCollection->at(trk).innerPosition().z()-(*elec).gsfTrack()->referencePoint().z();
-				cotTheta = 1./tan((*elec).gsfTrack()->momentum().Theta())-1./tan(trackCollection->at(trk).innerMomentum().Theta());
-			} else {
-				dR = ROOT::Math::VectorUtil::DeltaR(trackCollection->at(trk).referencePoint(),
-						(*elec).gsfTrack()->referencePoint());
-				dZ = trackCollection->at(trk).referencePoint().z()-(*elec).gsfTrack()->referencePoint().z();
-				cotTheta = 1./tan((*elec).gsfTrack()->momentum().Theta())-1./tan(trackCollection->at(trk).momentum().Theta());
-			}
-			// check if electron and track have opposite charge
-			bool os = ( (trackCollection->at(trk).charge() * ( (*elec).gsfTrack()->charge() )) == -1);
-			// get distance of closest approach (DOCA) for gsf electron and track 
-			//double doca = GetDocaBetweenTracks(iEvent, iSetup,magField, trackCollection->at(trk), *((*elec).gsfTrack()));//NEED TO CHECK THIS
 
-			// also calculate invariant mass of gsf electron and track
-			TLorentzVector trackP,elecP;
-			trackP.SetPtEtaPhiM(trackCollection->at(trk).pt(),
-					trackCollection->at(trk).eta(),
-					trackCollection->at(trk).phi(),
-					0.);
-			elecP.SetPtEtaPhiM((*elec).pt(),(*elec).eta(),
-					(*elec).phi(),0.);
-			double mTrackElec = (trackP + elecP).M();// Could also cut on Invariant Mass but its equivalent to DCotTheta
-
-			if(doHists_) {
-				hDzin->Fill(dZ,eventWeight_);
-				hDeltaCotTheta->Fill(cotTheta,eventWeight_);
-				hMassTrkAndElec->Fill(mTrackElec, eventWeight_);
-				hDeltaR->Fill(dR, eventWeight_);
-				hAbsPtDiff->Fill( fabs(trackCollection->at(trk).pt()-(*elec).pt()), eventWeight_);
-				hOS->Fill( os, eventWeight_);
-				hPt->Fill( trackCollection->at(trk).pt(), eventWeight_);
-				if( os ) { 
-					hDzinOS->Fill(dZ,eventWeight_);
-					hDeltaCotThetaOS->Fill(cotTheta,eventWeight_);
-					hMassTrkAndElecOS->Fill(mTrackElec, eventWeight_);
-					hDeltaROS->Fill(dR, eventWeight_);
-					hAbsPtDiffOS->Fill( fabs(trackCollection->at(trk).pt()-(*elec).pt()), eventWeight_);
-					hPtOS->Fill( trackCollection->at(trk).pt(), eventWeight_);
+				// check if track satisfies conversion criteria
+				if( fabs(cotTheta) < cotThetaMax_ && dR < dRinMax_) {
+					nTracksPass++;
 				}
-				//hDoca->Fill(doca,dRin,eventWeight_);
+			} // end tracks loop
+
+		} else {
+			//
+			// NEW EGAMMA method
+			//
+			edm::Handle<DcsStatusCollection> dcsHandle;
+			iEvent.getByLabel(dcsTag_, dcsHandle);
+			double evt_bField;
+			// need the magnetic field
+			//
+			// if isData then derive bfield using the
+			// magnet current from DcsStatus
+			// otherwise take it from the IdealMagneticFieldRecord
+			if (isData_) {
+				// scale factor = 3.801/18166.0 which are
+				// average values taken over a stable two
+				// week period
+				float currentToBFieldScaleFactor = 2.09237036221512717e-04;
+				float current = (*dcsHandle)[0].magnetCurrent();
+				evt_bField = current*currentToBFieldScaleFactor;
+			} else {
+				edm::ESHandle<MagneticField> magneticField;
+				iSetup.get<IdealMagneticFieldRecord>().get(magneticField);
+
+				evt_bField = magneticField->inTesla(GlobalPoint(0.,0.,0.)).z();
 			}
+
+			ConversionFinder convFinder;
+			ConversionInfo convInfo = convFinder.getConversionInfo(*elec, tracks, evt_bField);
+
+			double dist = convInfo.dist();
+			double dcot = convInfo.dcot();
+			//double convradius = convInfo.radiusOfConversion();
+			//math::XYZPoint convPoint = convInfo.pointOfConversion();
 
 			// check if track satisfies conversion criteria
-			if( fabs(cotTheta) < cotTheta_ && dR < dRin_ ) {
+			if( fabs(dcot) < cotThetaMax_ && dist < docaMax_ ) {
 				nTracksPass++;
-				// save track for alter analysis
-				trackRefVec.push_back( trackCollection->at(trk) );
 			}
-			
-		} // end tracks loop
-		if(doHists_) hnTracksPass->Fill(nTracksPass);
 
-		if(nTracksPass == 2 && doHists_) {
-			
-			bool os = ( (trackRefVec.at(0).charge() * trackRefVec.at(1).charge() ) == -1);
-			hOSFilt->Fill( os, eventWeight_);
-			
-			if( ( (*elec).gsfTrack()->charge() * trackRefVec.at(1).charge() ) == -1) {
-				double cotTheta = 1./tan((*elec).gsfTrack()->innerMomentum().Theta()) - 1./tan( trackRefVec.at(1).innerMomentum().Theta() );
-				hDeltaCotThetaFilt->Fill(cotTheta,eventWeight_);
-			
-				hPtOSFilt->Fill(trackRefVec.at(1).pt(), eventWeight_);
-				
-				double dR = ROOT::Math::VectorUtil::DeltaR((*elec).gsfTrack()->innerPosition(), trackRefVec.at(1).innerPosition());
-				hDeltaRFilt->Fill(dR, eventWeight_);
-			
-				hAbsPtDiffFilt->Fill( fabs((*elec).gsfTrack()->pt() - trackRefVec.at(1).pt()), eventWeight_);
-				//hMassTrkAndElecFilt->Fill(mTrackElec, eventWeight_);
-			} else {
-				double cotTheta = 1./tan((*elec).gsfTrack()->innerMomentum().Theta()) - 1./tan( trackRefVec.at(0).innerMomentum().Theta() );
-				hDeltaCotThetaFilt->Fill(cotTheta,eventWeight_);
-			
-				hPtOSFilt->Fill(trackRefVec.at(0).pt(), eventWeight_);
-				
-				double dR = ROOT::Math::VectorUtil::DeltaR((*elec).gsfTrack()->innerPosition(), trackRefVec.at(0).innerPosition());
-				hDeltaRFilt->Fill(dR, eventWeight_);
-			
-				hAbsPtDiffFilt->Fill( fabs((*elec).gsfTrack()->pt() - trackRefVec.at(0).pt()), eventWeight_);
-				//hMassTrkAndElecFilt->Fill(mTrackElec, eventWeight_);
-			}
 		}
-
-		// Check for pixel inner layer hit
-		int innerDetIdRaw = (*elec).gsfTrack()->innerDetId();
-		DetId innerDetId(innerDetIdRaw);
-		if(innerDetId.det() == DetId::Tracker) {
-			if(innerDetId.subdetId()==1) { 
-				PXBDetId pxbDetId(innerDetId); 
-				if(doHists_)
-					hPXBLayer->Fill(pxbDetId.layer(),eventWeight_);
-			}
-			if(innerDetId.subdetId()==2) { 
-				PXFDetId pxfDetId(innerDetId); 
-				if(doHists_) 
-					hPXFDisk->Fill(pxfDetId.disk(),eventWeight_);
-			}
-		}
-
+		
 		// add electrons to output collection if no conversion track pairs are found
-		if(nTracksPass <= nMax_) { //nMax_ should be <=1 since gsf electron track might appear in the track list
+		if(nTracksPass <= nPairMax_ && nTracksPass >= nPairMin_) { 
 			if(doPXBcut_) {
+				int innerDetIdRaw = (*elec).gsfTrack()->innerDetId();
+				DetId innerDetId(innerDetIdRaw);
 				if(innerDetId.det() == DetId::Tracker) {
 					if(innerDetId.subdetId()==1) {
 						PXBDetId pxbDetId(innerDetId);
@@ -268,34 +176,15 @@ void PATElectronConversionFinderImp::select(const edm::Handle<collection>& elecs
 						selected_.push_back(&(*elec));
 					}
 				}
+			} else if( doMissingHitsCut_) {
+				const reco::Track *elec_track = (const reco::Track*)(elec->gsfTrack().get());  
+				const reco::HitPattern& p_inner = elec_track->trackerExpectedHitsInner(); 
+			    if( p_inner.numberOfHits() < 1) 
+					selected_.push_back(&(*elec));
 			} else
 				selected_.push_back(&(*elec));
 		}	
 	} // end PAT electron loop
-}
-
-double PATElectronConversionFinderImp::GetDocaBetweenTracks(const edm::Event& evt, 
-							const edm::EventSetup& setup,
-							const MagneticField* magField,
-							const reco::Track& trk,
-							const reco::GsfTrack& gsf)
-{
- /*
-  double gx=gsf.trackPosition().x(),gy=gsf.innerPosition().y(),gz=gsf.innerPosition().z();
-  double rad_gsf = gsf.trackMomentumAtVtx().Rho()/(0.3*magField->inTesla(GlobalPoint(gx,gy,gz)).z())*100.;
-  
-  double tx=gsf.innerPosition().x(),ty=trk.innerPosition().y(),tz=trk.innerPosition().z();
-  double rad_trk = trk.trackMomentumAtVtx().Rho()/(0.3*magField->inTesla(GlobalPoint(tx,ty,tz)).z())*100.;
-  
-  double x0_g = gx+rad_gsf*sin(gsf.trackMomentumAtVtx().phi())*gsf.charge();
-  double y0_g = gy-rad_gsf*cos(gsf.trackMomentumAtVtx().phi())*gsf.charge();
-
-  double x0_t = tx+rad_trk*sin(trk.trackMomentumAtVtx().phi())*trk.charge();
-  double y0_t = ty-rad_trk*cos(trk.trackMomentumAtVtx().phi())*trk.charge();
-  
-  double doca = sqrt((x0_t-x0_g)*(x0_t-x0_g)+(y0_t-y0_g)*(y0_t-y0_g))-rad_gsf-rad_trk;
-*/
-  return 0;
 }
 
 typedef ObjectSelector<PATElectronConversionFinderImp> PATElectronConversionFinder;
--- TauAnalysis/RecoTools/plugins/PATElectronConversionFinder.h
+++ TauAnalysis/RecoTools/plugins/PATElectronConversionFinder.h
@@ -12,23 +12,24 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
+
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
 
-//Tracker tracks
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
-#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
-#include "Math/GenVector/VectorUtil.h"
 
-
+#include "DataFormats/Scalers/interface/DcsStatus.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrackExtra.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
 #include "DataFormats/EgammaCandidates/interface/Photon.h"
 #include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
 #include "DataFormats/EgammaCandidates/interface/Conversion.h"
@@ -37,10 +38,12 @@
 #include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
 #include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
 #include "DataFormats/Math/interface/deltaR.h"
+#include "DataFormats/Math/interface/Vector3D.h" 
 
-#include "TH1F.h"
-#include "TH2F.h"
+#include "Math/GenVector/VectorUtil.h"
 #include "TLorentzVector.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+
 
 class PATElectronConversionFinderImp
 {
@@ -58,53 +61,14 @@ class PATElectronConversionFinderImp
       void select(const edm::Handle<PATElectronCollection>&,edm::Event & iEvent, const edm::EventSetup & iSetup);
 
    private:
-      double GetDocaBetweenTracks(const edm::Event&, 
-				 const edm::EventSetup&,
-				 const MagneticField*,
-				 const reco::Track&,
-				 const reco::GsfTrack&);  
-      
       
       std::vector<const pat::Electron*> selected_;
 
-      edm::InputTag tracks_, conversions_, photons_;
+      edm::InputTag tracks_, conversions_, photons_, dcsTag_;
       
-	  double cotTheta_,dca_,dRin_,eventWeight_;
-      bool doPXBcut_;
-	  bool doHists_;
-	  bool useInnerParsForElec_;
-	  bool useInnerParsForTrks_;
-	  bool useConversionColl_;
-      double nMax_;
+      bool doPXBcut_, doMissingHitsCut_, useConversionColl_, useOSPairs_, usePogMethod_, isData_;
+	  double cotThetaMax_, docaMax_, dRinMax_, nPairMax_, nPairMin_;
       
-	  TH1F *hnTracksPass, 
-		*hPXBLayer, 
-		*hPXFDisk, 
-	  	*hDzin,
-	  	*hDzinFilt,
-	  	*hDzinOS,
-		*hDeltaCotTheta, 
-		*hDeltaCotThetaFilt, 
-		*hDeltaCotThetaOS, 
-		*hMassTrkAndElec,
-		*hMassTrkAndElecFilt,
-		*hMassTrkAndElecOS,
-		*hPt,
-		*hPtOSFilt,
-		*hPtOS,
-		*hOS,
-		*hOSFilt,
-		*hAbsPtDiff,
-		*hAbsPtDiffFilt,
-		*hAbsPtDiffOS,
-		*hDeltaR,
-		*hDeltaRFilt,
-		*hDeltaROS,
-		*nElecs,
-		*nPhotons,
-		*nConv,
-		*nConvTrks;
-//      TH2F *hDoca; 
 };
 
 #endif
