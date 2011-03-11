2e27d3b0be19734834065e3f227f8364cc1c5327
veverka <>
2011-03-11 18:45:40 +0000

    Added missing version in some switches and replaced tabs with spaces

--- ElectroWeakAnalysis/MultiBosons/src/VGammaMuonSelector.cc
+++ ElectroWeakAnalysis/MultiBosons/src/VGammaMuonSelector.cc
@@ -1,9 +1,9 @@
 #include "ElectroWeakAnalysis/MultiBosons/interface/VGammaMuonSelector.h"
 
 VGammaMuonSelector::VGammaMuonSelector( const edm::ParameterSet& conf ) {
-  
+
   std::string verStr(conf.getParameter<std::string>("version"));
-  
+
   version v;
   if( verStr == "Jun252010")
     v = Jun252010;
@@ -13,47 +13,47 @@ VGammaMuonSelector::VGammaMuonSelector( const edm::ParameterSet& conf ) {
     v = Sep062010;
   else
     throw cms::Exception("InvalidInput") << "\'version\' must be one of: Jun252010 Jun262010_jpsi" << std::endl;
-  
+
   init( v,
-	conf.getParameter<std::vector<std::string> >("muonTypeRequirement"),
-	conf.getParameter<std::string>("TrackerMuonIdType"),
-	conf.getParameter<unsigned>("minPixHits"),
-	conf.getParameter<unsigned>("minTkHits"),
-	conf.getParameter<double>("maxD0"),
-	conf.getParameter<double>("maxDZ"),
-	conf.getParameter<double>("maxChi2"),
-	conf.getParameter<unsigned>("minChambers"),
-	conf.getParameter<unsigned>("minStations"),
-	conf.getParameter<double>("minP"),
-	conf.getParameter<double>("minPt"),
-	conf.getParameter<double>("maxEta"),
-	conf.getParameter<double>("maxIso")
+        conf.getParameter<std::vector<std::string> >("muonTypeRequirement"),
+        conf.getParameter<std::string>("TrackerMuonIdType"),
+        conf.getParameter<unsigned>("minPixHits"),
+        conf.getParameter<unsigned>("minTkHits"),
+        conf.getParameter<double>("maxD0"),
+        conf.getParameter<double>("maxDZ"),
+        conf.getParameter<double>("maxChi2"),
+        conf.getParameter<unsigned>("minChambers"),
+        conf.getParameter<unsigned>("minStations"),
+        conf.getParameter<double>("minP"),
+        conf.getParameter<double>("minPt"),
+        conf.getParameter<double>("maxEta"),
+        conf.getParameter<double>("maxIso")
       );
   if( conf.exists("cutToIgnore") )
     setIgnoredCuts( conf.getParameter<std::vector<std::string> >("cutsToIgnore") );
-  
+
   retInternal_ = getBitTemplate();
 }
 
 void VGammaMuonSelector::init( const version& v,
-			       const std::vector<std::string>& vmt, // valid muon types
-			       const std::string & tmid,
-			       const int & pixHits, // minimum number of pix hits
-			       const int & tkHits, // min. tk hits
-			       const double & d0, // d0 w.r.t. beam spot
-			       const double & dz, // dz w.r.t. beam spot
-			       const double & chi2, // global chi2/ndof
-			       const int & chambers, // number of chambers in SA muon
-			       const int & stations, // number of stations
-			       const double & p, // min p
-			       const double & pt, // min pt 
-			       const double & eta, // max eta
-			       const double & maxIso // max iso
-			       ) {
+                               const std::vector<std::string>& vmt, // valid muon types
+                               const std::string & tmid,
+                               const int & pixHits, // minimum number of pix hits
+                               const int & tkHits, // min. tk hits
+                               const double & d0, // d0 w.r.t. beam spot
+                               const double & dz, // dz w.r.t. beam spot
+                               const double & chi2, // global chi2/ndof
+                               const int & chambers, // number of chambers in SA muon
+                               const int & stations, // number of stations
+                               const double & p, // min p
+                               const double & pt, // min pt
+                               const double & eta, // max eta
+                               const double & maxIso // max iso
+                               ) {
   version_ = v;
   muonTypeRequirement_ = vmt;
   TMidType_ = tmid;
-  int vmt_size = vmt.size();  
+  int vmt_size = vmt.size();
   push_back("MuonTypeRequirements", vmt_size);
   push_back("TrackerMuonIdType", 1);
   push_back("minPixHits", pixHits);
@@ -67,7 +67,7 @@ void VGammaMuonSelector::init( const version& v,
   push_back("minPt", pt);
   push_back("maxEta", eta);
   push_back("maxIso", maxIso);
-  
+
   set("MuonTypeRequirements");
   set("TrackerMuonIdType");
   set("minPixHits");
@@ -81,9 +81,9 @@ void VGammaMuonSelector::init( const version& v,
   set("minPt");
   set("maxEta");
   set("maxIso");
-  
+
   switch (version_) {
-  case Jun252010 : 
+  case Jun252010 :
     set("dz",false);
     set("minP",false);
     break;
@@ -102,27 +102,27 @@ void VGammaMuonSelector::init( const version& v,
   }
 }
 
-bool VGammaMuonSelector::operator()( const pat::Muon & mu, 
-				     edm::EventBase const & evt, 
-				     pat::strbitset & ret ) {
+bool VGammaMuonSelector::operator()( const pat::Muon & mu,
+                                     edm::EventBase const & evt,
+                                     pat::strbitset & ret ) {
   switch (version_) {
   case Jun252010:
     return Jun252010Cuts(mu, ret);
+    break;
+  case Jun262010_jpsi:
+    return Jun262010_jpsiCuts(mu, ret);
     break;
   case Sep062010:
     return Sep062010Cuts(mu, ret);
     break;
-  case Jun262010_jpsi:
-    return Jun262010_jpsiCuts(mu, ret);
-    break;
   default:
     break;
   }
   return false;
 }
 
-bool VGammaMuonSelector::operator()( const pat::Muon & mu, 
-				     pat::strbitset &  ret) {
+bool VGammaMuonSelector::operator()( const pat::Muon & mu,
+                                     pat::strbitset &  ret) {
   switch (version_) {
   case Jun252010:
     return Jun252010Cuts(mu, ret);
@@ -130,16 +130,19 @@ bool VGammaMuonSelector::operator()( const pat::Muon & mu,
   case Jun262010_jpsi:
     return Jun262010_jpsiCuts(mu, ret);
     break;
+  case Sep062010:
+    return Sep062010Cuts(mu, ret);
+    break;
   default:
     break;
   }
   return false;
 }
 
-bool VGammaMuonSelector::Jun252010Cuts( const pat::Muon& mu, 
-					pat::strbitset & ret ) {
+bool VGammaMuonSelector::Jun252010Cuts( const pat::Muon& mu,
+                                        pat::strbitset & ret ) {
   ret.set(false);
-  
+
   if(!mu.innerTrack().isNonnull()) return false;
   if(!mu.globalTrack().isNonnull()) return false; // the basically requires a global muon.
 
@@ -147,21 +150,21 @@ bool VGammaMuonSelector::Jun252010Cuts( const pat::Muon& mu,
     relIso((mu.trackIso() + mu.caloIso())/pt);
   int pixHits(mu.innerTrack()->hitPattern().numberOfValidPixelHits()),
     tkHits(mu.innerTrack()->hitPattern().numberOfValidStripHits()),
-    //    muHits(mu.outerTrack()->hitPattern().numberOfValidMuonDTHits()+
-    //	   mu.outerTrack()->hitPattern().numberOfValidMuonCSCHits()),
+    //   muHits(mu.outerTrack()->hitPattern().numberOfValidMuonDTHits()+
+    //   mu.outerTrack()->hitPattern().numberOfValidMuonCSCHits()),
     chambers(mu.numberOfMatches()),
     stations(0);
   unsigned stationMask(mu.stationMask());
   int typeMatches(0), tkMuId(0);
-  
+
   for(unsigned i=0; i < 8; ++i)
     if(stationMask & 1 << i) ++stations;
-  
-  for(unsigned i=0;i<muonTypeRequirement_.size();++i) 
+
+  for(unsigned i=0;i<muonTypeRequirement_.size();++i)
     if(mu.muonID(muonTypeRequirement_[i])) ++typeMatches;
-  
+
   if(mu.muonID(TMidType_)) tkMuId = 1;
-  
+
   if(typeMatches == cut("MuonTypeRequirements", int()) || ignoreCut("MuonTypeRequirements")) passCut(ret,"MuonTypeRequirements");
   if(tkMuId == cut("TrackerMuonIdType", int()) || ignoreCut("TrackerMuonIdType")) passCut(ret,"TrackerMuonIdType");
   if(pixHits > cut("minPixHits",int()) || ignoreCut("minPixHits")) passCut(ret,"minPixHits");
@@ -173,16 +176,16 @@ bool VGammaMuonSelector::Jun252010Cuts( const pat::Muon& mu,
   if(pt > cut("minPt", double()) || ignoreCut("minPt")) passCut(ret,"minPt");
   if(eta < cut("maxEta", double()) || ignoreCut("maxEta")) passCut(ret,"maxEta");
   if(relIso < cut("maxRelIso", double()) || ignoreCut("maxRelIso")) passCut(ret,"maxRelIso");
-  
+
   setIgnored(ret);
-  
+
   return (bool)ret;
 }
 
-bool VGammaMuonSelector::Sep062010Cuts( const pat::Muon& mu, 
-					pat::strbitset & ret ) {
+bool VGammaMuonSelector::Sep062010Cuts( const pat::Muon& mu,
+                                        pat::strbitset & ret ) {
   ret.set(false);
-  
+
   if(!mu.innerTrack().isNonnull()) return false;
   if(!mu.globalTrack().isNonnull()) return false; // the basically requires a global muon.
 
@@ -191,20 +194,20 @@ bool VGammaMuonSelector::Sep062010Cuts( const pat::Muon& mu,
   int pixHits(mu.innerTrack()->hitPattern().numberOfValidPixelHits()),
     tkHits(mu.innerTrack()->hitPattern().numberOfValidStripHits()),
     //    muHits(mu.outerTrack()->hitPattern().numberOfValidMuonDTHits()+
-    //	   mu.outerTrack()->hitPattern().numberOfValidMuonCSCHits()),
+    //    mu.outerTrack()->hitPattern().numberOfValidMuonCSCHits()),
     chambers(mu.numberOfMatches()),
     stations(0);
   unsigned stationMask(mu.stationMask());
   int typeMatches(0), tkMuId(0);
-  
+
   for(unsigned i=0; i < 8; ++i)
     if(stationMask & 1 << i) ++stations;
-  
-  for(unsigned i=0;i<muonTypeRequirement_.size();++i) 
+
+  for(unsigned i=0;i<muonTypeRequirement_.size();++i)
     if(mu.muonID(muonTypeRequirement_[i])) ++typeMatches;
-  
+
   if(mu.muonID(TMidType_)) tkMuId = 1;
-  
+
   if(typeMatches == cut("MuonTypeRequirements", int()) || ignoreCut("MuonTypeRequirements")) passCut(ret,"MuonTypeRequirements");
   if(tkMuId == cut("TrackerMuonIdType", int()) || ignoreCut("TrackerMuonIdType")) passCut(ret,"TrackerMuonIdType");
   if(pixHits > cut("minPixHits",int()) || ignoreCut("minPixHits")) passCut(ret,"minPixHits");
@@ -216,29 +219,29 @@ bool VGammaMuonSelector::Sep062010Cuts( const pat::Muon& mu,
   if(pt > cut("minPt", double()) || ignoreCut("minPt")) passCut(ret,"minPt");
   if(eta < cut("maxEta", double()) || ignoreCut("maxEta")) passCut(ret,"maxEta");
   if(Iso < cut("maxIso", double()) || ignoreCut("maxIso")) passCut(ret,"maxIso");
-  
+
   setIgnored(ret);
-  
+
   return (bool)ret;
 }
 
 
 bool VGammaMuonSelector::Jun262010_jpsiCuts( const pat::Muon& mu,
-					     pat::strbitset & ret ) {
+                                             pat::strbitset & ret ) {
   ret.set(false);
-  
+
   if(!mu.innerTrack().isNonnull()) return false;
 
-  double pt(mu.pt()), eta(fabs(mu.eta())), norm_chi2(mu.innerTrack()->normalizedChi2()), dz(mu.innerTrack()->dz()), 
+  double pt(mu.pt()), eta(fabs(mu.eta())), norm_chi2(mu.innerTrack()->normalizedChi2()), dz(mu.innerTrack()->dz()),
     corr_d0(mu.dB()), p(mu.p());
   int tkHits(mu.innerTrack()->found());
   int typeMatches(0), tkMuId(0);
-  
-  for(unsigned i=0;i<muonTypeRequirement_.size();++i) 
+
+  for(unsigned i=0;i<muonTypeRequirement_.size();++i)
     if(mu.muonID(muonTypeRequirement_[i])) ++typeMatches;
-  
+
   if(mu.muonID(TMidType_)) tkMuId = 1;
-  
+
   if(typeMatches == cut("MuonTypeRequirements", int()) || ignoreCut("MuonTypeRequirements")) passCut(ret,"MuonTypeRequirements");
   if(tkMuId == cut("TrackerMuonIdType", int()) || ignoreCut("TrackerMuonIdType")) passCut(ret,"TrackerMuonIdType");
   if(tkHits > cut("minTkHits",int()) || ignoreCut("minTkHits")) passCut(ret,"minTkHits");
@@ -248,8 +251,8 @@ bool VGammaMuonSelector::Jun262010_jpsiCuts( const pat::Muon& mu,
   if(p > cut("minP", double()) || ignoreCut("minP")) passCut(ret,"minP");
   if(eta < cut("maxEta",double()) || ignoreCut("maxEta")) passCut(ret,"maxEta");
   if(pt > cut("minPt",double()) || ignoreCut("minPt")) passCut(ret,"minPt");
-  
+
   setIgnored(ret);
-  
+
   return (bool)ret;
 }
