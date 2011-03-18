b36ef51af4705e52ca83bd635d97b33d7ce2bb81
srappocc <>
2011-03-18 18:24:37 +0000

    Updating to 2011A dataset

--- PhysicsTools/PatExamples/test/patTuple_data_cfg.py
+++ PhysicsTools/PatExamples/test/patTuple_data_cfg.py
@@ -6,27 +6,62 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from PhysicsTools.PatAlgos.tools.coreTools import *
 
 ## global tag for data
-process.GlobalTag.globaltag = cms.string('GR_R_38X_V14::All')
+process.GlobalTag.globaltag = 'GR_R_311_V2::All'
+
+
+# Triggers for the /Jet PD are from:
+# http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/HLTrigger/Configuration/python/HLT_FULL_data_cff.py?revision=1.63&view=markup
+# The list is here, let's take all of them :
+## Jet = cms.vstring( 'HLT_DiJetAve100U_v4',
+##   'HLT_DiJetAve140U_v4',
+##   'HLT_DiJetAve15U_v4',
+##   'HLT_DiJetAve180U_v4',
+##   'HLT_DiJetAve300U_v4',
+##   'HLT_DiJetAve30U_v4',
+##   'HLT_DiJetAve50U_v4',
+##   'HLT_DiJetAve70U_v4',
+##   'HLT_Jet110_v1',
+##   'HLT_Jet150_v1',
+##   'HLT_Jet190_v1',
+##   'HLT_Jet240_v1',
+##   'HLT_Jet30_v1',
+##   'HLT_Jet370_NoJetID_v1',
+##   'HLT_Jet370_v1',
+##   'HLT_Jet60_v1',
+##   'HLT_Jet80_v1' ),
+mytrigs = ['*']
+
+# Jet energy corrections to use: 
+inputJetCorrLabel = ('AK5PF', ['L1Offset', 'L2Relative', 'L3Absolute', 'L2L3Residual'])
 
 # add pf met
 from PhysicsTools.PatAlgos.tools.metTools import *
 removeMCMatching(process, ['All'])
 addPfMET(process, 'PF')
 
-
-# add PF jets
+# Add PF jets
 from PhysicsTools.PatAlgos.tools.jetTools import *
-addJetCollection(process,cms.InputTag('ak5PFJets'),
-                 'AK5', 'PF',
+switchJetCollection(process,cms.InputTag('ak5PFJets'),
                  doJTA        = True,
                  doBTagging   = True,
-                 jetCorrLabel = ('AK5PF', cms.vstring(['L2Relative', 'L3Absolute', 'L2L3Residual'])),
-                 doType1MET   = False,
-                 doL1Cleaning = False,                 
-                 doL1Counters = False,
+                 jetCorrLabel = inputJetCorrLabel,
+                 doType1MET   = True,
                  genJetCollection=cms.InputTag("ak5GenJets"),
-                 doJetID      = False
+                 doJetID      = True
                  )
+
+# Apply loose PF jet ID
+from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
+process.goodPatJets = cms.EDFilter("PFJetIDSelectionFunctorFilter",
+                                   filterParams = pfJetIDSelector.clone(),
+                                   src = cms.InputTag("selectedPatJets"),
+                                   filter = cms.bool(True)
+                                   )
+
+
+# Taus are currently broken in 4.1.x
+removeSpecificPATObjects( process, ['Taus'] )
+process.patDefaultSequence.remove( process.patTaus )
 
 # require physics declared
 process.load('HLTrigger.special.hltPhysicsDeclared_cfi')
@@ -39,109 +74,101 @@ process.scrapingVeto = cms.EDFilter("FilterOutScraping",
                                     numtrack = cms.untracked.uint32(10),
                                     thresh = cms.untracked.double(0.2)
                                     )
+# HB + HE noise filtering
+process.load('CommonTools/RecoAlgos/HBHENoiseFilter_cfi')
 
 
-# switch on PAT trigger
-from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
-switchOnTrigger( process )
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+if mytrigs is not None :
+    process.hltSelection = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs)
+    process.hltSelection.throw = False
+
 
 process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
                                            vertexCollection = cms.InputTag('offlinePrimaryVertices'),
                                            minimumNDOF = cms.uint32(4) ,
-                                           maxAbsZ = cms.double(15), 
+                                           maxAbsZ = cms.double(24), 
                                            maxd0 = cms.double(2) 
                                            )
 
+
 # Select jets
-process.selectedPatJets.cut = cms.string('pt > 10')
-process.selectedPatJetsAK5PF.cut = cms.string('pt > 10')
+process.selectedPatJets.cut = cms.string('pt > 25')
 
-
-# Add the files 
+# Add the files
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 
+
 readFiles.extend( [
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/940B070B-DA97-DF11-8617-002618943926.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/9281D65A-DD97-DF11-A291-002618FDA265.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/88DA882B-DE97-DF11-BC66-00248C55CC97.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/888AA340-AE97-DF11-A4F6-003048678A78.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/86341F45-B897-DF11-B123-003048678B14.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/846FB748-B697-DF11-9424-002618FDA216.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/74E90F3D-AA97-DF11-B477-0026189438D4.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/74CBB955-DD97-DF11-8A29-00261894393A.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/74422470-B997-DF11-B5B6-002618943964.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/7435EB3D-AA97-DF11-9542-002618943949.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/72DDC354-DD97-DF11-901F-002618FDA211.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/72686F6C-E497-DF11-9426-002618943836.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/6A237350-E097-DF11-A811-00261894396E.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/688CC9A8-D497-DF11-A82A-002618943926.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/66BB531E-D597-DF11-9B8A-0026189438BA.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/62AC2C6B-A997-DF11-98A4-002618FDA210.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/6299FC2A-E197-DF11-8773-002618FDA265.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/6091AA48-B197-DF11-9BCF-00261894396A.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/5CC39069-B397-DF11-893F-003048678A78.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/58D9079B-A897-DF11-8B6B-003048678A78.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/563D9DA8-AD97-DF11-8CC7-002618943946.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/54127B46-B497-DF11-8758-00261894393C.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/52445FC5-AD97-DF11-B021-002618FDA216.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/520B2B2F-DE97-DF11-80B3-002618FDA287.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4CB31A7F-DE97-DF11-B046-002618FDA262.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4C589749-B697-DF11-8059-00261894396A.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4AB60F1F-D597-DF11-9AC0-00248C55CC97.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4A3AC46E-A797-DF11-BB72-003048678B5E.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4809E146-B397-DF11-94B3-003048678B14.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4217C067-AB97-DF11-B4B0-002618FDA21D.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/4035A012-AF97-DF11-91C1-003048678B5E.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/3A8F3B49-AC97-DF11-9E4B-002618943946.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/3A75E46D-DA97-DF11-B845-002618943836.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/367EDB45-B197-DF11-84A9-002618FDA216.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/32571795-A697-DF11-8320-00248C55CC40.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/2C935C68-AD97-DF11-9519-0026189438D4.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/264BE66D-B297-DF11-82B3-002618943860.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/261BF88D-E297-DF11-9F26-002618FDA287.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/247AEBA5-9D97-DF11-B5F4-00261894385D.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/22744443-AF97-DF11-8450-002618943964.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1E62C747-B697-DF11-A581-002618943949.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1CE7213F-D797-DF11-A0C9-00248C55CC97.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1A368544-B897-DF11-A6DB-00261894393C.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1A11F06D-B797-DF11-857F-002618943964.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1898FD6F-AF97-DF11-AF4D-002354EF3BE1.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/162AD99E-E297-DF11-87E2-002618943985.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/161AF945-B397-DF11-9FAE-002618FDA216.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/14DB0EA6-A697-DF11-B754-0026189438D4.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/14507168-AD97-DF11-99B4-0030486792DE.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/1273FA44-B397-DF11-BFB6-002618943946.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/10B71087-AF97-DF11-83CC-003048678A78.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/10A5BF6E-B797-DF11-84A5-002618943915.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/0C3A4055-E297-DF11-8321-00248C55CC97.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/06C5A183-F097-DF11-885C-002618FDA211.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/00E19D5E-DB97-DF11-A4D3-002618943926.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/009B303F-E597-DF11-9FC6-002618943811.root',
-'/store/data/Run2010A/JetMETTau/RECO/Jul23ReReco_PreProd_v1/0157/0003F63E-E597-DF11-94EB-002618FDA208.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/547/DAFCA3B7-B850-E011-9ADF-0030487C778E.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/502/7297DBD1-6B50-E011-9FA5-0030487CD162.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/500/9E1BCB0E-6C50-E011-BC4D-0030487CD7EE.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/499/3850D990-9450-E011-BF72-0030487CD716.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/498/AC8FE79D-8450-E011-A58B-0030487CAEAC.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/495/88BEEA68-3850-E011-9F13-0030487CD17C.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/494/1C35E662-3350-E011-BAA0-003048F1183E.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/488/66D0A0F1-1850-E011-97C0-003048F1BF68.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/486/E2FB2282-1850-E011-BA61-001D09F2527B.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/484/6E1A5CBE-1650-E011-B823-003048D2C108.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/472/CA224AEB-1450-E011-84A1-001D09F2527B.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/471/5C765497-1450-E011-AD91-001D09F248F8.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/469/204A3C03-1A50-E011-B8D7-0030487C8CB6.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/467/6ABD98CE-1B50-E011-9D5F-003048F11114.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/466/F296557B-1E50-E011-8C7F-0030487CD6B4.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/463/72D3943E-2350-E011-B098-001617C3B654.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/462/E650148E-2650-E011-96BD-001617C3B654.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/456/863BF1F0-4C50-E011-9ABC-001617C3B5E4.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/455/46A4141B-4A50-E011-8ED4-001D09F24934.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/454/C2041925-DC50-E011-B0C2-0030487C7828.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/450/2EE61936-5050-E011-9731-0030487C7392.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/449/5C5D099B-4C50-E011-9779-000423D98B6C.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/447/B8CFC010-5650-E011-A6CA-0030487CD162.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/446/6810F3AF-5C50-E011-9EA3-003048F11114.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/445/5E83EDE8-5650-E011-B45D-001D09F23A3E.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/444/0E40C747-AC50-E011-8A91-0030487CD718.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/443/EAA93DE5-5750-E011-A127-001617C3B76E.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/442/70B27507-5B50-E011-99A9-0030487CD162.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/439/6658D888-B34F-E011-BE9D-001D09F24D67.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/433/12B7CF7F-B54F-E011-94B6-001D09F23C73.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/432/96E7E8AA-1150-E011-878B-001D09F28F1B.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/431/44A96589-2D50-E011-861C-003048F118D4.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/428/00E2E174-B34F-E011-9858-003048F110BE.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/427/52C35785-814F-E011-A17C-003048F117EC.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/425/F4681D39-844F-E011-B3E5-003048F024F6.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/423/2A0114B3-7C4F-E011-B719-001D09F24303.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/421/0A0F17E0-774F-E011-93C1-000423D94E70.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/413/28A09525-EB4F-E011-9AE5-003048F0258C.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/410/8094C4AE-5F4F-E011-B390-003048F118D2.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/406/4E1472A9-5F4F-E011-B062-0030487C7828.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/405/3AB185A4-D64F-E011-8394-0030487C2B86.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/404/38F2FFDA-374F-E011-AD2A-003048F024F6.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/403/64B3598B-364F-E011-8683-001D09F29849.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/386/5059F123-0B4F-E011-8DDB-0030487C2B86.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/384/463D8003-094F-E011-A4B1-0030487CD17C.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/383/7EDAB706-0D4F-E011-BBBF-0030487C6A66.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/379/14D52E14-0C4F-E011-B5AF-003048F11C58.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/377/5CBF6B69-0B4F-E011-855D-0030487CD906.root',
+'/store/data/Run2011A/Jet/AOD/PromptReco-v1/000/160/329/6631A77A-2F4E-E011-AE5A-003048F118C2.root',
+ ] )
 
-
-        ] );
 process.source.fileNames = readFiles
 
 process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
 
 # let it run
 
-#print
-#print "============== Warning =============="
-#print "technical trigger filter:    DISABLED"
-#print "physics declare bit filter:  DISABLED"
-#print "primary vertex filter:       DISABLED"
-
 process.p = cms.Path(
+    process.hltSelection*
     process.scrapingVeto*
     process.primaryVertexFilter*
-    process.patDefaultSequence
+    process.HBHENoiseFilter*
+    process.patDefaultSequence*
+    process.goodPatJets
     )
 
 # rename output file
-process.out.fileName = cms.untracked.string('reco_7TeV_380_pat.root')
+process.out.fileName = cms.untracked.string('jet2011A_aod.root')
 
 # reduce verbosity
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
@@ -151,8 +178,11 @@ process.maxEvents.input = 1000
 process.options.wantSummary = True
 
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
-from PhysicsTools.PatAlgos.patEventContent_cff import patTriggerEventContent
+from PhysicsTools.PatAlgos.patEventContent_cff import patExtraAodEventContent
 process.out.outputCommands = patEventContentNoCleaning
-process.out.outputCommands += patTriggerEventContent
-process.out.outputCommands += [ 'keep recoTracks_generalTracks_*_*',
-                                'keep LumiSummary_lumiProducer_*_*']
+process.out.outputCommands += patExtraAodEventContent
+process.out.outputCommands += [
+    'drop patJets_selectedPatJets_*_*',
+    'keep patJets_goodPatJets_*_*',    
+    'keep recoPFCandidates_particleFlow_*_*'
+    ]
