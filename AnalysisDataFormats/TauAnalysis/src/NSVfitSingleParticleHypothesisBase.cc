e8675925731475a25b9f41df6ab8074524848795
veelken <>
2011-03-24 11:59:08 +0000

    added vector meson masses

---
+++ AnalysisDataFormats/TauAnalysis/src/NSVfitSingleParticleHypothesisBase.cc
@@ -0,0 +1,32 @@
+#include "AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesisBase.h"
+
+NSVfitSingleParticleHypothesisBase::NSVfitSingleParticleHypothesisBase(const NSVfitSingleParticleHypothesisBase& bluePrint)
+  : name_(bluePrint.name_),
+    barcode_(bluePrint.barcode_),
+    particle_(bluePrint.particle_),
+    p4_(bluePrint.p4_),
+    p4_fitted_(bluePrint.p4_fitted_),
+    dp4_(bluePrint.dp4_),
+    tracks_(bluePrint.tracks_)
+{}
+
+NSVfitSingleParticleHypothesisBase& NSVfitSingleParticleHypothesisBase::operator =(const NSVfitSingleParticleHypothesisBase& bluePrint)
+{
+  name_ = bluePrint.name_;
+  barcode_ = bluePrint.barcode_;
+  particle_ = bluePrint.particle_;
+  p4_ = bluePrint.p4_;
+  p4_fitted_ = bluePrint.p4_fitted_;
+  dp4_ = bluePrint.dp4_;
+  tracks_ = bluePrint.tracks_;
+  return (*this);
+}
+
+//
+//-------------------------------------------------------------------------------
+//
+
+bool operator<(const NSVfitSingleParticleHypothesisBase& hypothesis1, const NSVfitSingleParticleHypothesisBase& hypothesis2)
+{
+  return (hypothesis1.p4().pt() < hypothesis2.p4().pt());
+}
