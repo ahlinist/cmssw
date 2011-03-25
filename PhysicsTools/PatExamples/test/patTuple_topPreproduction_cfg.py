d67000e24a6696f51224feccb74050cc3884155b
rwolf <>
2011-03-25 11:41:54 +0000

    small update for TopExample

--- PhysicsTools/PatExamples/test/patTuple_topPreproduction_cfg.py
+++ PhysicsTools/PatExamples/test/patTuple_topPreproduction_cfg.py
@@ -2,30 +2,18 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 ## ---
-## adjust inputs if necessary
-## ---
-## ---
 ## Adjust inputs if necessary
 ## ---
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
-run36xOn35xInput(process)
-
-## This might be needed when running on 383 rereco'ed data
-process.load("RecoJets.Configuration.GenJetParticles_cff")
-process.load("RecoJets.Configuration.RecoGenJets_cff")
-
-process.p0 = cms.Path(
-    process.genJetParticles *
-    process.ak5GenJets
-)
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
+#run36xOn35xInput(process)
 
 ## --- 
 ## adjust workflow to need in TopPAG
 ## ---
 from PhysicsTools.PatAlgos.tools.coreTools import *
-removeSpecificPATObjects(process, ['Photons','Taus'])
-removeMCMatching(process, ['All'])
 removeCleaning(process)
+removeMCMatching(process, ['All'])
+removeSpecificPATObjects(process, ['Photons','Taus'])
 
 ## ---
 ## adjust content
