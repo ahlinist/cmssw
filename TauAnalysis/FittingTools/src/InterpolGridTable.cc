045e0cd3c8136dfb56179a4d798480b0b443d011
liis <>
2011-03-26 15:24:01 +0000

    add #include <set>

--- TauAnalysis/FittingTools/src/InterpolGridTable.cc
+++ TauAnalysis/FittingTools/src/InterpolGridTable.cc
@@ -11,6 +11,7 @@
 #include "xercesc/sax/HandlerBase.hpp"
 #include "xercesc/util/XMLString.hpp"
 #include "xercesc/util/PlatformUtils.hpp"
+#include <set>
 
 InterpolGridTable::InterpolGridTable()
   : numRows_(0),
