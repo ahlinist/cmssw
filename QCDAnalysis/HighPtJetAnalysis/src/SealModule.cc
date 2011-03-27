b4b008886cce457bb63177e8ceb24062000725d3
kkousour <>
2011-03-27 14:24:06 +0000

    Removed obsolete code and added the ProcessedTreeProducer

--- QCDAnalysis/HighPtJetAnalysis/src/SealModule.cc
+++ QCDAnalysis/HighPtJetAnalysis/src/SealModule.cc
@@ -1,23 +1,13 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "QCDAnalysis/HighPtJetAnalysis/interface/GenDijetTreeProducer.h"
 #include "QCDAnalysis/HighPtJetAnalysis/interface/InclusiveJetTreeProducer.h"
+#include "QCDAnalysis/HighPtJetAnalysis/interface/ProcessedTreeProducer.h"
 #include "QCDAnalysis/HighPtJetAnalysis/interface/InclusiveJetAnalyzer.h"
-#include "QCDAnalysis/HighPtJetAnalysis/interface/SimpleInclusiveJetAnalyzer.h"
-#include "QCDAnalysis/HighPtJetAnalysis/interface/DijetAnalyzer.h"
-#include "DataFormats/JetReco/interface/CaloJet.h"
 using namespace reco;
 
 //DEFINE_SEAL_MODULE();
 DEFINE_FWK_MODULE(InclusiveJetTreeProducer);
+DEFINE_FWK_MODULE(ProcessedTreeProducer);
 DEFINE_FWK_MODULE(InclusiveJetAnalyzer);
-DEFINE_FWK_MODULE(DijetAnalyzer);
-DEFINE_FWK_MODULE(GenDijetTreeProducer);
-
-typedef SimpleInclusiveJetAnalyzer<CaloJet> SimpleInclusiveCaloJetAnalyzer;
-DEFINE_FWK_MODULE(SimpleInclusiveCaloJetAnalyzer);
-
-typedef SimpleInclusiveJetAnalyzer<GenJet> SimpleInclusiveGenJetAnalyzer;
-DEFINE_FWK_MODULE(SimpleInclusiveGenJetAnalyzer);
 
