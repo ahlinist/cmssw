1bd5f9cc7fcce86881a04955cb159a615792869a
friis <>
2011-03-27 14:16:58 +0000

    Adding intermediate tau decay inheritance

--- AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesisBase.h
+++ AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesisBase.h
@@ -1,8 +1,9 @@
 #ifndef AnalysisDataFormats_TauAnalysis_NSVfitSingleParticleHypothesisBase_h
 #define AnalysisDataFormats_TauAnalysis_NSVfitSingleParticleHypothesisBase_h
 
-#include "DataFormats/Candidate/interface/Candidate.h" 
+#include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/Common/interface/Ptr.h"
 
@@ -12,11 +13,11 @@ class NSVfitSingleParticleHypothesisBase
 {
  public:
   NSVfitSingleParticleHypothesisBase() {}
-  NSVfitSingleParticleHypothesisBase(const std::string& name, int barcode) 
+  NSVfitSingleParticleHypothesisBase(const std::string& name, int barcode)
     : name_(name),
       barcode_(barcode)
   {}
-  NSVfitSingleParticleHypothesisBase(const edm::Ptr<reco::Candidate>& particle, const std::string& name, int barcode) 
+  NSVfitSingleParticleHypothesisBase(const edm::Ptr<reco::Candidate>& particle, const std::string& name, int barcode)
     : name_(name),
       barcode_(barcode),
       particle_(particle),
@@ -37,28 +38,35 @@ class NSVfitSingleParticleHypothesisBase
 
   /// momentum of particle before fit, after fit
   /// and difference in momentum (after - before) fit
-  reco::Candidate::LorentzVector p4() const { return p4_; }
-  reco::Candidate::LorentzVector p4_fitted() const { return p4_fitted_; }
-  reco::Candidate::LorentzVector dp4_fitted() const { return dp4_; }
+  const reco::Candidate::LorentzVector& p4() const { return p4_; }
+  const reco::Candidate::LorentzVector& p4_fitted() const { return p4_fitted_; }
+  const reco::Candidate::LorentzVector& dp4_fitted() const { return dp4_; }
 
   /// collection of tracks associated to reco::Candidate
   virtual const std::vector<reco::TrackBaseRef>& tracks() const { return tracks_; }
 
   /// access to position of secondary vertex (tau lepton decay vertex)
   virtual bool hasDecayVertex() const { return false; }
-  virtual const AlgebraicVector3& decayVertexPos() const 
-  { 
+  virtual const AlgebraicVector3& decayVertexPos() const
+  {
     throw cms::Exception("NSVfitSingleParticleHypothesisBase::decayVertexPos")
       << " Function not implemented for base-class !!\n";
   }
-  virtual const reco::Candidate::Vector& flightPath() const 
-  { 
+  virtual const reco::Candidate::Vector& flightPath() const
+  {
     throw cms::Exception("NSVfitSingleParticleHypothesisBase::flightPath")
       << " Function not implemented for base-class !!\n";
   }
-  virtual double decayDistance() const 
-  { 
+  virtual double decayDistance() const
+  {
     throw cms::Exception("NSVfitSingleParticleHypothesisBase::decayDistance")
+      << " Function not implemented for base-class !!\n";
+  }
+
+  /// Flag determine whether a reconstructed vertex is associated
+  virtual bool hasFittedVertex() const { return false; }
+  virtual const reco::Vertex& fittedVertex() const {
+    throw cms::Exception("NSVfitSingleParticleHypothesisBase::fittedVertex")
       << " Function not implemented for base-class !!\n";
   }
 
@@ -67,9 +75,9 @@ class NSVfitSingleParticleHypothesisBase
     stream << "<NSVfitSingleParticleHypothesisBase::print>:" << std::endl;
     stream << " name = " << name_ << std::endl;
     stream << " barcode = " << barcode_ << std::endl;
-    stream << " p4: Pt = " << p4_.pt() << "," 
+    stream << " p4: Pt = " << p4_.pt() << ","
 	   << " eta = " << p4_.eta() << ", phi = " << p4_.phi() << std::endl;
-    stream << " p4_fitted: Pt = " << p4_fitted().pt() << "," 
+    stream << " p4_fitted: Pt = " << p4_fitted().pt() << ","
 	   << " eta = " << p4_fitted().eta() << ", phi = " << p4_fitted().phi() << std::endl;
     stream << " #tracks = " << tracks_.size() << std::endl;
   }
