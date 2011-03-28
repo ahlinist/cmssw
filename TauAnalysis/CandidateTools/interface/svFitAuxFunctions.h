b9e4384cea4de22a1986a8615a3c24d110a0fe8c
friis <>
2011-03-28 10:10:38 +0000

    Fix bug in log likelihood calculation of ND gaussian

--- TauAnalysis/CandidateTools/interface/svFitAuxFunctions.h
+++ TauAnalysis/CandidateTools/interface/svFitAuxFunctions.h
@@ -173,7 +173,7 @@ namespace SVfit_namespace
 
     //std::cout << "--> residual^T V^-1 redidual = " << ROOT::Math::Dot(residual, covInverse*residual) << std::endl;
 
-    return -0.5*numDimensions*TMath::Log(2*TMath::Pi()) - 0.5*det - 0.5*(ROOT::Math::Dot(residual, covInverse*residual));
+    return -0.5*numDimensions*TMath::Log(2*TMath::Pi()) - 0.5*TMath::Log(det) - 0.5*(ROOT::Math::Dot(residual, covInverse*residual));
   }
 
   /// Determine sign of tau lepton polarization
