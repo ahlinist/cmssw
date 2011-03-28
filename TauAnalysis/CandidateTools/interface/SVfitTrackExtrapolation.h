1393c057583e3b8b06ef85ae54fbfb08f482672e
friis <>
2011-03-28 11:00:41 +0000

    Improve implemenatation of track likelihoods and add tests

--- TauAnalysis/CandidateTools/interface/SVfitTrackExtrapolation.h
+++ TauAnalysis/CandidateTools/interface/SVfitTrackExtrapolation.h
@@ -12,28 +12,46 @@
  */
 
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "DataFormats/GeometryVector/interface/GlobalVector.h"
 
 namespace SVfit { namespace track {
-struct TrackExtrapolation {
-  TrackExtrapolation(const reco::TransientTrack&, const AlgebraicVector3&);
+class TrackExtrapolation {
+  public:
+    // Default construct
+    TrackExtrapolation() {}
+    // Constructor from a transient track and reference spatial point
+    TrackExtrapolation(const reco::TransientTrack&, const AlgebraicVector3&);
+    // Constructor from values for testing
+    TrackExtrapolation(const GlobalPoint& dcaPosition,
+        const GlobalVector& tangent, const AlgebraicMatrix33& covMatrix);
 
-  const AlgebraicVector3& tangent() const { return tangent_; }
-  const AlgebraicVector3& dcaPosition() const { return dcaPosition_; }
-  const AlgebraicVector3& refPoint() const { return dcaPosition_; }
+    const AlgebraicVector3& tangent() const { return tangent_; }
+    const AlgebraicVector3& dcaPosition() const { return dcaPosition_; }
+    const AlgebraicVector3& refPoint() const { return dcaPosition_; }
 
-  // Log-likelihood given a secondary vertex
-  double logLikelihood(const AlgebraicVector3&) const;
+    // Log-likelihood given a secondary vertex
+    double logLikelihood(const AlgebraicVector3&) const;
 
-  // Log-likelihood given the displacement
-  double logLikelihoodFromDisplacement(const AlgebraicVector3&) const;
+    // Log-likelihood given the displacement
+    double logLikelihoodFromDisplacement(const AlgebraicVector3&) const;
 
-  AlgebraicVector3 tangent_;
-  AlgebraicVector3 dcaPosition_;
-  AlgebraicMatrix33 invRotationMatrix_;
-  AlgebraicMatrix33 rotCovMatrix_;
-  AlgebraicMatrix22 rotCovMatrix2_;
+  private:
+    // Implementation of the constructors
+    void construct(const GlobalPoint& dcaPosition,
+        const GlobalVector& tangent, const AlgebraicMatrix33& covMatrix);
 
-  int errorFlag_;
+    AlgebraicVector3 tangent_;
+    AlgebraicVector3 dcaPosition_;
+    AlgebraicMatrix33 invRotationMatrix_;
+    AlgebraicMatrix33 rotCovMatrix_;
+    AlgebraicMatrix22 rotCovMatrix2_;
+    // Inverted rotated covariance matrix
+    AlgebraicMatrix22 invRotCovMatrx2_;
+    // Determinant of rotated covariance matrix
+    double detRotCovMatrix2_;
+
+    int errorFlag_;
 };
 }} // end namespace SVfit::track
 #endif
