49667dcfd735bf114b7d2506ac1eca2ac375d9b0
jkolb <>
2011-03-30 15:26:59 +0000

    bugfix in conversion veto parameters

--- TauAnalysis/Configuration/python/tools/switchToData.py
+++ TauAnalysis/Configuration/python/tools/switchToData.py
@@ -36,89 +36,90 @@ def switchToData(process):
 	# iterate over all sequences attached to process object and
 	# recursively disable gen. matching for all modules in sequence
 	for processAttrName in dir(process):
-            processAttr = getattr(process, processAttrName)
-	    if isinstance(processAttr, cms.Sequence):
-	        print "--> Disabling gen. Matching for sequence:", processAttrName
-	        # replace all InputTags referring to
-		#  o genParticleSource = cms.InputTag('genParticles') --> ""
-   	        #  o genJetSource = cms.InputTag('iterativeCone5GenJets') --> ""
-		#  o genJetSource = cms.InputTag('ak5GenJets') --> ""
-		#  o genTauJetSource = cms.InputTag('tauGenJets') --> ""
-		#  o genMEtSource = cms.InputTag('genMetTrue') --> ""
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genParticles'), cms.InputTag(''))
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('iterativeCone5GenJets'), cms.InputTag(''))
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('ak5GenJets'), cms.InputTag(''))
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('tauGenJets'), cms.InputTag(''))
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genMetTrue'), cms.InputTag(''))
-
-		# replace all InputTags referring to gen. Z decay products
-		# in electron + tau-jet Zee (muon + tau-jet Zmumu) hypotheses
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genElectronsFromZs'), cms.InputTag(''))
-		patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genMuonsFromZs'), cms.InputTag(''))
-
-		# switch all 'isData' configuration parameters
-		_setattr_ifexists(processAttr, "isData", cms.bool(True))
+		processAttr = getattr(process, processAttrName)
+		if isinstance(processAttr, cms.Sequence):
+			print "--> Disabling gen. Matching for sequence:", processAttrName
+			# replace all InputTags referring to
+			#  o genParticleSource = cms.InputTag('genParticles') --> ""
+			#  o genJetSource = cms.InputTag('iterativeCone5GenJets') --> ""
+			#  o genJetSource = cms.InputTag('ak5GenJets') --> ""
+			#  o genTauJetSource = cms.InputTag('tauGenJets') --> ""
+			#  o genMEtSource = cms.InputTag('genMetTrue') --> ""
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genParticles'), cms.InputTag(''))
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('iterativeCone5GenJets'), cms.InputTag(''))
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('ak5GenJets'), cms.InputTag(''))
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('tauGenJets'), cms.InputTag(''))
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genMetTrue'), cms.InputTag(''))
+			
+			# replace all InputTags referring to gen. Z decay products
+			# in electron + tau-jet Zee (muon + tau-jet Zmumu) hypotheses
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genElectronsFromZs'), cms.InputTag(''))
+			patutils.massSearchReplaceAnyInputTag(processAttr, cms.InputTag('genMuonsFromZs'), cms.InputTag(''))
 
 
 	# disable access to generator level information in all
 	# histogram managers, binner and event-dump plugins of GenericAnalyzer module
 	for processAttrName in dir(process):
-	    processAttr = getattr(process, processAttrName)
-            if isinstance(processAttr, cms.EDAnalyzer):
-	        if processAttr.type_() == "GenericAnalyzer":
-		    # disable matching between gen. and rec. particles
-		    # in all histogram managers and binner plugins by setting
-		    #  o genParticleSource = cms.InputTag('genParticles') --> ""
-		    #  o genJetSource = cms.InputTag('iterativeCone5GenJets') --> ""
-		    #  o genJetSource = cms.InputTag('ak5GenJets') --> ""
-		    #  o genTauJetSource = cms.InputTag('tauGenJets') --> ""
-		    #  o genMEtSource = cms.InputTag('genMetTrue') --> ""
-		    if hasattr(processAttr, "analyzers"):
-		        analyzerPlugins = getattr(processAttr, "analyzers")
-			for analyzerPlugin in analyzerPlugins:
-			    _setattr_ifexists(analyzerPlugin, "genParticleSource", cms.InputTag(''))
-			    _setattr_ifexists(analyzerPlugin, "genJetSource", cms.InputTag(''))
-			    _setattr_ifexists(analyzerPlugin, "genTauJetSource", cms.InputTag(''))
-			    _setattr_ifexists(analyzerPlugin, "genMEtSource", cms.InputTag(''))
+		processAttr = getattr(process, processAttrName)
+		if isinstance(processAttr, cms.EDAnalyzer):
+			if processAttr.type_() == "GenericAnalyzer":
+				# disable matching between gen. and rec. particles
+				# in all histogram managers and binner plugins by setting
+				#  o genParticleSource = cms.InputTag('genParticles') --> ""
+				#  o genJetSource = cms.InputTag('iterativeCone5GenJets') --> ""
+				#  o genJetSource = cms.InputTag('ak5GenJets') --> ""
+				#  o genTauJetSource = cms.InputTag('tauGenJets') --> ""
+				#  o genMEtSource = cms.InputTag('genMetTrue') --> ""
+				if hasattr(processAttr, "analyzers"):
+					analyzerPlugins = getattr(processAttr, "analyzers")
+					for analyzerPlugin in analyzerPlugins:
+						_setattr_ifexists(analyzerPlugin, "genParticleSource", cms.InputTag(''))
+						_setattr_ifexists(analyzerPlugin, "genJetSource", cms.InputTag(''))
+						_setattr_ifexists(analyzerPlugin, "genTauJetSource", cms.InputTag(''))
+						_setattr_ifexists(analyzerPlugin, "genMEtSource", cms.InputTag(''))
 
-                    # remove from all analysis sequences the following objects:
-		    #  o genPhaseSpaceInfoHistManager
-		    #  o modelBinnerForMuTauGenTauLeptonPairAcc
-		    #  o modelBinnerForMuTauWrtGenTauLeptonPairAcc
-                    #  o modelBinnerForMuTauGenTauLeptonPairAcc3mZbins
-                    #  o modelBinnerForMuTauWrtGenTauLeptonPairAcc3mZbins 
-		    #  o modelBinnerForMuTauCentralJetVetoWrtGenTauLeptonPairAcc
-		    #  o modelBinnerForMuTauCentralJetBtagWrtGenTauLeptonPairAcc
-		    #  o sysUncertaintyBinnerForMuTau
-		    #  o diTauCandidateCollinearApproxHistManagerBinnedForMuTau
-		    analyzerPluginsToRemove = [
-		       "genPhaseSpaceEventInfoHistManager",
-		       "genTauHistManager",	    
-		       "modelBinnerForMuTauGenTauLeptonPairAcc",
-		       "modelBinnerForMuTauWrtGenTauLeptonPairAcc",
-                       "modelBinnerForMuTauGenTauLeptonPairAcc3mZbins",
-                       "modelBinnerForMuTauWrtGenTauLeptonPairAcc3mZbins",
-		       "modelBinnerForMuTauCentralJetVetoWrtGenTauLeptonPairAcc",
-		       "modelBinnerForMuTauCentralJetBtagWrtGenTauLeptonPairAcc",
-		       "sysUncertaintyBinnerForMuTau",
-		       "diTauCandidateCollinearApproxHistManagerBinnedForMuTau"
-		    ]
-		    for analyzerPluginName in analyzerPluginsToRemove:
-  		        if hasattr(process, analyzerPluginName):
-			    analyzerPlugin = getattr(process, analyzerPluginName)
-			    if processAttr.analyzers.count(analyzerPlugin) > 0:
-			        processAttr.analyzers.remove(analyzerPlugin)
-			    removeAnalyzer(processAttr.analysisSequence, analyzerPluginName)
+				# remove from all analysis sequences the following objects:
+				#  o genPhaseSpaceInfoHistManager
+				#  o modelBinnerForMuTauGenTauLeptonPairAcc
+				#  o modelBinnerForMuTauWrtGenTauLeptonPairAcc
+                #  o modelBinnerForMuTauGenTauLeptonPairAcc3mZbins
+				#  o modelBinnerForMuTauWrtGenTauLeptonPairAcc3mZbins 
+				#  o modelBinnerForMuTauCentralJetVetoWrtGenTauLeptonPairAcc
+				#  o modelBinnerForMuTauCentralJetBtagWrtGenTauLeptonPairAcc
+				#  o sysUncertaintyBinnerForMuTau
+				#  o diTauCandidateCollinearApproxHistManagerBinnedForMuTau
+				analyzerPluginsToRemove = [
+					"genPhaseSpaceEventInfoHistManager",
+					"genTauHistManager",	    
+					"modelBinnerForMuTauGenTauLeptonPairAcc",
+					"modelBinnerForMuTauWrtGenTauLeptonPairAcc",
+					"modelBinnerForMuTauGenTauLeptonPairAcc3mZbins",
+                    "modelBinnerForMuTauWrtGenTauLeptonPairAcc3mZbins",
+					"modelBinnerForMuTauCentralJetVetoWrtGenTauLeptonPairAcc",
+					"modelBinnerForMuTauCentralJetBtagWrtGenTauLeptonPairAcc",
+					"sysUncertaintyBinnerForMuTau",
+					"diTauCandidateCollinearApproxHistManagerBinnedForMuTau"
+				]
+				for analyzerPluginName in analyzerPluginsToRemove:
+					if hasattr(process, analyzerPluginName):
+						analyzerPlugin = getattr(process, analyzerPluginName)
+						if processAttr.analyzers.count(analyzerPlugin) > 0:
+							processAttr.analyzers.remove(analyzerPlugin)
+						removeAnalyzer(processAttr.analysisSequence, analyzerPluginName)
 
 	            # disable matching between gen. and rec. particles in all event-dump plugins by setting
-		    #  o doGenInfo = cms.bool(True) --> cms.bool(False)
-		    #  o genParticleSource = cms.InputTag('genParticles') --> ""
-		    if hasattr(processAttr, "eventDumps"):
-		        eventDumpPlugins = getattr(processAttr, "eventDumps")
-			for eventDumpPlugin in eventDumpPlugins:
-			    eventDumpPlugin.doGenInfo = cms.bool(False)
-			    eventDumpPlugin.genParticleSource = cms.InputTag('')
+				#  o doGenInfo = cms.bool(True) --> cms.bool(False)
+				#  o genParticleSource = cms.InputTag('genParticles') --> ""
+				if hasattr(processAttr, "eventDumps"):
+					eventDumpPlugins = getattr(processAttr, "eventDumps")
+					for eventDumpPlugin in eventDumpPlugins:
+						eventDumpPlugin.doGenInfo = cms.bool(False)
+						eventDumpPlugin.genParticleSource = cms.InputTag('')
 
-		    # disable estimation of systematic uncertainties by setting
-		    #  o analyzers_systematic = cms.VPSet(..) --> cms.VPSet()
-		    processAttr.analyzers.analyzers_systematic = cms.VPSet()
+				# disable estimation of systematic uncertainties by setting
+				#  o analyzers_systematic = cms.VPSet(..) --> cms.VPSet()
+				processAttr.analyzers.analyzers_systematic = cms.VPSet()
+
+	for processAttrName in dir(process):
+		processAttr = getattr(process, processAttrName)
+		_setattr_ifexists(processAttr, "isData", cms.bool(True) )
