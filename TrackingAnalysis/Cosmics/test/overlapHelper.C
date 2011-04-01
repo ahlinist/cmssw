11903f376e1e8b5f425a0ff472e49b76d4c5705a
speer <>
2011-04-01 08:44:49 +0000

    Bug fix and addition

--- TrackingAnalysis/Cosmics/test/overlapHelper.C
+++ TrackingAnalysis/Cosmics/test/overlapHelper.C
@@ -210,8 +210,16 @@ AdjacentType checkAdjacentType(unsigned int id1, unsigned int id2)
     return parallelToStrips;
   }
   else if ( subdet1==1 ) {	// PXE
-    // We are on same ladder, so overlap parallel to strips.
-    if (info1[0]==info2[0]) return parallelToStrips;
+    // We are on same ladder, so overlap perpendicularToStrips to strips.
+//     cout << "checking ";
+//   cout << id1<< " ( ";
+//   printId(id1);
+//   cout <<" ) " << id2 << " ( ";
+//   printId(id2);
+//   cout <<" ) - "<<
+//     info1[0]<<" "<<info2[0]<<endl;
+
+    if (info1[0]==info2[0]) return perpendicularToStrips;
     return parallelToStrips;
   }
   return notAdjacent;
@@ -392,4 +400,235 @@ bool slopeOK(AdjacentType type, float slope)
     }
   }
   return true;
+}
+
+  /**
+   * Print the full module info for the pair
+   */
+void idPrint(DetIdPair idPair)
+{
+  cout << idPair.first<< " ( ";
+  printId(idPair.first);
+  cout <<" ) " << idPair.second << " ( ";
+  printId(idPair.second);
+  cout <<" ) - ";
+  AdjacentType adj = checkAdjacentType(idPair.first, idPair.second);
+  switch ( adj ) {
+    case notAdjacent : 
+      cout << "NotAdjacant\n";
+    break;
+    case parallelToStrips : 
+      cout << "Ovrlp parallel in Z\n";
+    break;
+    case perpendicularToStrips : 
+      cout << "Ovrlp perpendicular in Z\n";
+  }
+
+}
+
+bool halfBarrelOverlap(DetIdPair idPair)
+{
+  unsigned int subdet1, layer1, stereo1, subdet2, layer2, stereo2;
+  int part1, part2;
+  vector<unsigned int> info1, info2;
+  fullDecode (idPair.first, subdet1, layer1, part1,info1, stereo1);
+  fullDecode (idPair.second, subdet2, layer2, part2,info2, stereo2);
+  if (subdet1!=1) return false;
+  if (layer1==1) {
+    if (info1[0]==5 && info2[0]==6) return true;
+    if (info1[0]==6 && info2[0]==5) return true;
+    if (info1[0]==15 && info2[0]==16) return true;
+    if (info1[0]==16 && info2[0]==15) return true;
+  } else if (layer1==2) {
+    if (info1[0]==8 && info2[0]==9) return true;
+    if (info1[0]==9 && info2[0]==8) return true;
+    if (info1[0]==25 && info2[0]==24) return true;
+    if (info1[0]==24 && info2[0]==25) return true;
+  } else if (layer1==3) {
+    if (info1[0]==11 && info2[0]==12) return true;
+    if (info1[0]==12 && info2[0]==11) return true;
+    if (info1[0]==33 && info2[0]==34) return true;
+    if (info1[0]==34 && info2[0]==33) return true;
+  }
+//   if (layer1==1) {
+//     if (info1[0]==0 && info2[0]==19) return true;
+//     if (info1[0]==19 && info2[0]==0) return true;
+//     if (info1[0]==9 && info2[0]==10) return true;
+//     if (info1[0]==10 && info2[0]==9) return true;
+//   } else if (layer1==2) {
+//     if (info1[0]==0 && info2[0]==31) return true;
+//     if (info1[0]==31 && info2[0]==0) return true;
+//     if (info1[0]==15 && info2[0]==16) return true;
+//     if (info1[0]==16 && info2[0]==15) return true;
+//   } else if (layer1==3) {
+//     if (info1[0]==0 && info2[0]==41) return true;
+//     if (info1[0]==41 && info2[0]==0) return true;
+//     if (info1[0]==21 && info2[0]==22) return true;
+//     if (info1[0]==22 && info2[0]==21) return true;
+//   }
+  return false;
+}
+
+bool halfBarrelOverlap(unsigned int id)
+{
+  unsigned int subdet1, layer1, stereo1;
+  int part1;
+  vector<unsigned int> info1;
+  fullDecode (id, subdet1, layer1, part1,info1, stereo1);
+  if (subdet1!=1) return false;
+  if (layer1==1) {
+    if (info1[0]==5 || info1[0]==6 || info1[0]==15 ||info1[0]==16) return true;
+  } else if (layer1==2) {
+    if (info1[0]==8 || info1[0]==9 || info1[0]==24 ||info1[0]==25) return true;
+  } else if (layer1==3) {
+    if (info1[0]==11|| info1[0]==12|| info1[0]==33 ||info1[0]==34) return true;
+  }
+  return false;
+}
+
+TStyle* setTDRStyle() {
+  TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
+
+// For the canvas:
+  tdrStyle->SetCanvasBorderMode(0);
+  tdrStyle->SetCanvasColor(kWhite);
+  tdrStyle->SetCanvasDefH(600); //Height of canvas
+  tdrStyle->SetCanvasDefW(600); //Width of canvas
+  tdrStyle->SetCanvasDefX(0);   //POsition on screen
+  tdrStyle->SetCanvasDefY(0);
+
+// For the Pad:
+  tdrStyle->SetPadBorderMode(0);
+  // tdrStyle->SetPadBorderSize(Width_t size = 1);
+  tdrStyle->SetPadColor(kWhite);
+  tdrStyle->SetPadGridX(false);
+  tdrStyle->SetPadGridY(false);
+  tdrStyle->SetGridColor(0);
+  tdrStyle->SetGridStyle(3);
+  tdrStyle->SetGridWidth(1);
+
+// For the frame:
+  tdrStyle->SetFrameBorderMode(0);
+  tdrStyle->SetFrameBorderSize(1);
+  tdrStyle->SetFrameFillColor(0);
+  tdrStyle->SetFrameFillStyle(0);
+  tdrStyle->SetFrameLineColor(1);
+  tdrStyle->SetFrameLineStyle(1);
+  tdrStyle->SetFrameLineWidth(1);
+
+// For the histo:
+  // tdrStyle->SetHistFillColor(1);
+  // tdrStyle->SetHistFillStyle(0);
+  tdrStyle->SetHistLineColor(1);
+  tdrStyle->SetHistLineStyle(0);
+  tdrStyle->SetHistLineWidth(1);
+  // tdrStyle->SetLegoInnerR(Float_t rad = 0.5);
+  // tdrStyle->SetNumberContours(Int_t number = 20);
+
+  tdrStyle->SetEndErrorSize(15);
+//   tdrStyle->SetErrorMarker(20);
+  tdrStyle->SetErrorX(1);
+  
+  tdrStyle->SetMarkerStyle(21);
+  tdrStyle->SetMarkerSize(1.);
+
+//For the fit/function:
+  tdrStyle->SetOptFit(0);
+  tdrStyle->SetFitFormat("5.4g");
+  tdrStyle->SetFuncColor(2);
+  tdrStyle->SetFuncStyle(1);
+  tdrStyle->SetFuncWidth(1);
+
+//For the date:
+  tdrStyle->SetOptDate(0);
+  // tdrStyle->SetDateX(Float_t x = 0.01);
+  // tdrStyle->SetDateY(Float_t y = 0.01);
+
+// For the statistics box:
+//  tdrStyle->SetOptFile(1111);
+//  tdrStyle->SetOptStat(1111111); // To display the mean and RMS:   SetOptStat("mr");
+  tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
+  tdrStyle->SetStatColor(kWhite);
+  tdrStyle->SetStatFont(42);
+  tdrStyle->SetStatFontSize(0.025);
+  tdrStyle->SetStatTextColor(1);
+  tdrStyle->SetStatFormat("6.4g");
+  tdrStyle->SetStatBorderSize(1);
+  tdrStyle->SetStatH(0.2);
+  tdrStyle->SetStatW(0.15);
+  // tdrStyle->SetStatStyle(Style_t style = 1001);
+  // tdrStyle->SetStatX(Float_t x = 0);
+  // tdrStyle->SetStatY(Float_t y = 0);
+
+// Margins:
+  tdrStyle->SetPadTopMargin(0.05);
+  tdrStyle->SetPadBottomMargin(0.13);
+  tdrStyle->SetPadLeftMargin(0.16);
+  tdrStyle->SetPadRightMargin(0.02);
+
+// For the Global title:
+
+  tdrStyle->SetOptTitle(0);
+  tdrStyle->SetTitleW(0.8); // Set the width of the title box
+
+  tdrStyle->SetTitleFont(42);
+  tdrStyle->SetTitleColor(1);
+  tdrStyle->SetTitleTextColor(1);
+  tdrStyle->SetTitleFillColor(10);
+  tdrStyle->SetTitleFontSize(0.05);
+  // tdrStyle->SetTitleH(0); // Set the height of the title box
+  // tdrStyle->SetTitleX(0); // Set the position of the title box
+  // tdrStyle->SetTitleY(0.985); // Set the position of the title box
+  // tdrStyle->SetTitleStyle(Style_t style = 1001);
+  // tdrStyle->SetTitleBorderSize(2);
+
+// For the axis titles:
+
+  tdrStyle->SetTitleColor(1, "XYZ");
+  tdrStyle->SetTitleFont(42, "XYZ");
+  tdrStyle->SetTitleSize(0.06, "XYZ");
+  // tdrStyle->SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
+  // tdrStyle->SetTitleYSize(Float_t size = 0.02);
+  tdrStyle->SetTitleXOffset(0.9);
+  tdrStyle->SetTitleYOffset(1.);
+  // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+
+// For the axis labels:
+
+  tdrStyle->SetLabelColor(1, "XYZ");
+  tdrStyle->SetLabelFont(42, "XYZ");
+  tdrStyle->SetLabelOffset(0.007, "XYZ");
+  tdrStyle->SetLabelSize(0.05, "XYZ");
+
+// For the axis:
+
+  tdrStyle->SetAxisColor(1, "XYZ");
+  tdrStyle->SetStripDecimals(kTRUE);
+  tdrStyle->SetTickLength(0.03, "XYZ");
+  tdrStyle->SetNdivisions(510, "XYZ");
+  tdrStyle->SetPadTickX(0);  // To get tick marks on the opposite side of the frame
+  tdrStyle->SetPadTickY(0);
+
+// Change for log plots:
+  tdrStyle->SetOptLogx(0);
+  tdrStyle->SetOptLogy(0);
+  tdrStyle->SetOptLogz(0);
+
+// Postscript options:
+//   tdrStyle->SetPaperSize(21.,28.);
+  tdrStyle->SetPaperSize(20.,20.);
+  // tdrStyle->SetLineScalePS(Float_t scale = 3);
+  // tdrStyle->SetLineStyleString(Int_t i, const char* text);
+  // tdrStyle->SetHeaderPS(const char* header);
+  // tdrStyle->SetTitlePS(const char* pstitle);
+
+  // tdrStyle->SetBarOffset(Float_t baroff = 0.5);
+  // tdrStyle->SetBarWidth(Float_t barwidth = 0.5);
+  // tdrStyle->SetPaintTextFormat(const char* format = "g");
+  // tdrStyle->SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
+  // tdrStyle->SetTimeOffset(Double_t toffset);
+  // tdrStyle->SetHistMinimumZero(kTRUE);
+
+  tdrStyle->cd();
+  return tdrStyle;
 }
