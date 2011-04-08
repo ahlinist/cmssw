714dd32bd7b1d79348f6d1acc901796c1b9966d4
lilopera <>
2011-04-08 14:16:34 +0000

    Initial State

---
+++ DQMServices/Processor/src/Web.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+namespace dqmevf {
+
+class Web {
+	public:
+  void importStaticJS(std::ostringstream *, std::string);
+  void importStaticHTML(std::ostringstream *, std::string);
+
+};
+
+}
