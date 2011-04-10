a43492dd641409796b17ea4fc8be306b71bfa746
veverka <>
2011-04-10 12:24:13 +0000

    Fixed titles

--- ElectroWeakAnalysis/MultiBosons/python/Histogramming/isoHistos_cff.py
+++ ElectroWeakAnalysis/MultiBosons/python/Histogramming/isoHistos_cff.py
@@ -6,7 +6,7 @@ trackIsoHisto = cms.PSet(
   max = cms.untracked.double(100),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("trackIso"),
-  description = cms.untracked.string("Tracker Isolation [GeV]:#Sigma p_{T} [GeV]:N per 1 GeV"),
+  description = cms.untracked.string("Tracker Isolation [GeV];#Sigma p_{T} [GeV];N per 1 GeV"),
   plotquantity = cms.untracked.string("trackIso")
 )
 
@@ -16,7 +16,7 @@ relativeTrackIsoHisto = cms.PSet(
   max = cms.untracked.double(2),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("relativeTrackIso"),
-  description = cms.untracked.string("Relative Tracker Isolation:(#Sigma p_{T})/p_{T}:N per .02"),
+  description = cms.untracked.string("Relative Tracker Isolation;(#Sigma p_{T})/p_{T};N per .02"),
   plotquantity = cms.untracked.string("trackIso/pt")
 )
 
@@ -26,7 +26,7 @@ ecalIsoHisto = cms.PSet(
   max = cms.untracked.double(100),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("ecalIso"),
-  description = cms.untracked.string("ECAL Isolation [GeV]:#Sigma E_{T} [GeV]:N per 1 GeV"),
+  description = cms.untracked.string("ECAL Isolation [GeV];#Sigma E_{T} [GeV];N per 1 GeV"),
   plotquantity = cms.untracked.string("ecalIso")
 )
 
@@ -36,7 +36,7 @@ relativeEcalIsoHisto = cms.PSet(
   max = cms.untracked.double(2),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("relativeEcalIso"),
-  description = cms.untracked.string("Relative ECAL Isolation:(#Sigma E_{T})/E_{T}:N per .02"),
+  description = cms.untracked.string("Relative ECAL Isolation;(#Sigma E_{T})/E_{T};N per .02"),
   plotquantity = cms.untracked.string("ecalIso/pt")
 )
 
@@ -46,7 +46,7 @@ hcalIsoHisto = cms.PSet(
   max = cms.untracked.double(100),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("hcalIso"),
-  description = cms.untracked.string("HCAL Isolation [GeV]:#Sigma E_{T} [GeV]:N per 1 GeV"),
+  description = cms.untracked.string("HCAL Isolation [GeV];#Sigma E_{T} [GeV];N per 1 GeV"),
   plotquantity = cms.untracked.string("hcalIso")
 )
 
@@ -56,7 +56,7 @@ relativeHcalIsoHisto = cms.PSet(
   max = cms.untracked.double(2),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("relativeHcalIso"),
-  description = cms.untracked.string("Relative HCAL Isolation:(#Sigma E_{T})/E_{T}:N per .02"),
+  description = cms.untracked.string("Relative HCAL Isolation;(#Sigma E_{T})/E_{T};N per .02"),
   plotquantity = cms.untracked.string("hcalIso/pt")
 )
 
@@ -66,7 +66,7 @@ combinedIsoHisto = cms.PSet(
   max = cms.untracked.double(100),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("combinedIso"),
-  description = cms.untracked.string("Combined Isolation [GeV]:#Sigma E_{T} [GeV]:N per 1 GeV"),
+  description = cms.untracked.string("Combined Isolation [GeV];#Sigma E_{T} [GeV];N per 1 GeV"),
   plotquantity = cms.untracked.string("trackIso + ecalIso + hcalIso")
 )
 
@@ -76,7 +76,7 @@ relativeCombinedIsoHisto = cms.PSet(
   max = cms.untracked.double(2),
   nbins = cms.untracked.int32(100),
   name = cms.untracked.string("combinedRelativeIso"),
-  description = cms.untracked.string("Combined Relative Isolation:(#Sigma E_{T})/E_{T}:N per .02"),
+  description = cms.untracked.string("Combined Relative Isolation;(#Sigma E_{T})/E_{T};N per .02"),
   plotquantity = cms.untracked.string("(trackIso + ecalIso + hcalIso) / pt")
 )
 
