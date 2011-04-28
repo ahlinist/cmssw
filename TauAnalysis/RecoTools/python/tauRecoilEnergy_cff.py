e49db2825f17e8b2f6f7a6ec6895a824810ebd93
liis <>
2011-04-28 10:53:47 +0000

    remove loose isolation selection

--- TauAnalysis/RecoTools/python/tauRecoilEnergy_cff.py
+++ TauAnalysis/RecoTools/python/tauRecoilEnergy_cff.py
@@ -35,39 +35,8 @@ tauRecoilEnergyFromCaloTowersPt5 = cms.EDFilter("PATTauRecoilEnergyFromCaloTower
 
 
 
-# -------- Loose isolation selection -----------------# 
-tauRecoilEnergyFromJetsLooseIsolation = cms.EDProducer("PATTauRecoilEnergyFromJetsProducer",
-    srcLeptons = cms.InputTag('selectedPatTausForWTauNuEcalCrackVetoLooseIsolationCumulative'),
-    srcEnergyObjects = cms.InputTag('patJets'),
-    etaMin = cms.double(-2.5),
-    etaMax = cms.double(+2.5),
-    etMin = cms.double(0.),
-    dPhiMin = cms.double(2.64),
-    verbosity = cms.untracked.int32(0)
-)
-
-tauRecoilEnergyFromCaloTowersLooseIsolation = cms.EDProducer("PATTauRecoilEnergyFromCaloTowersProducer",
-    srcLeptons = cms.InputTag('selectedPatTausForWTauNuEcalCrackVetoLooseIsolationCumulative'),
-    srcEnergyObjects = cms.InputTag('towerMaker'),
-    etaMin = cms.double(0.),
-    etaMax = cms.double(+5.),
-    etMin = cms.double(0.5),
-    dPhiMin = cms.double(2.64),
-    verbosity = cms.untracked.int32(0)
-)
-
-tauRecoilEnergyFromCaloTowersPt5LooseIsolation = cms.EDFilter("PATTauRecoilEnergyFromCaloTowersSelector",
-    src = cms.InputTag("tauRecoilEnergyFromCaloTowersLooseIsolation"),
-    cut = cms.string('etSum < 1e5'),
-    filter = cms.bool(False)
-)  
-
-
-
 
 produceTauRecoilEnergy = cms.Sequence( tauRecoilEnergyFromJets 
                                        * tauRecoilEnergyFromCaloTowers
                                        * tauRecoilEnergyFromCaloTowersPt5                                        
-                                       * tauRecoilEnergyFromCaloTowersLooseIsolation
-                                       * tauRecoilEnergyFromCaloTowersPt5LooseIsolation 
-                                       * tauRecoilEnergyFromJetsLooseIsolation)
+                                       )
