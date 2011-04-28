dcba4e18683659a2b30375b8ec13921694d1b9c0
liis <>
2011-04-28 10:37:06 +0000

    change selection cuts

--- TauAnalysis/RecoTools/python/patJetSelectionForWTauNu_cff.py
+++ TauAnalysis/RecoTools/python/patJetSelectionForWTauNu_cff.py
@@ -10,14 +10,14 @@ from TauAnalysis.CandidateTools.tools.objSelConfigurator import *
 # on how to use the cut-string parser
 
 # Select jets for W->tau nu analysis
-selectedPatJetsAntiOverlapWithTausVetoForWTauNu = cms.EDFilter("PATJetAntiOverlapSelector",
-                                                                  srcNotToBeFiltered = cms.VInputTag("selectedPatTausForWTauNuEcalCrackVetoCumulative"),
-                                                                  dRmin = cms.double(0.7),
-                                                                  filter = cms.bool(False)
-                                                                  )
+#selectedPatJetsAntiOverlapWithTausVetoForWTauNu = cms.EDFilter("PATJetAntiOverlapSelector",
+#                                                                  srcNotToBeFiltered = cms.VInputTag("selectedPatTausForWTauNuEcalCrackVetoCumulative"),
+#                                                                  dRmin = cms.double(0.7),
+#                                                                  filter = cms.bool(False)
+#                                                                  )
 
-selectedPatJetsEta21ForWTauNu = cms.EDFilter("PATJetSelector",
-                                                cut = cms.string('abs(eta) < 2.1'),
+selectedPatJetsEtaForWTauNu = cms.EDFilter("PATJetSelector",
+                                                cut = cms.string('abs(eta) < 3.0'),
                                                 filter = cms.bool(False)
                                                 )
 
@@ -37,8 +37,8 @@ selectedPatJetsEtL20ForWTauNu = cms.EDFilter("PATJetSelector",
                                              )
 
 patJetSelConfiguratorForWTauNu = objSelConfigurator(
-    [ selectedPatJetsAntiOverlapWithTausVetoForWTauNu,
-      selectedPatJetsEta21ForWTauNu,
+    [ #selectedPatJetsAntiOverlapWithTausVetoForWTauNu,
+      selectedPatJetsEtaForWTauNu,
       selectedPatJetsEt15ForWTauNu,
       selectedPatJetsEt20ForWTauNu ],
     src = "cleanPatJets",
@@ -47,8 +47,8 @@ patJetSelConfiguratorForWTauNu = objSelConfigurator(
     )
 
 patJetSelConfiguratorForWTauNu2 = objSelConfigurator(
-        [ selectedPatJetsAntiOverlapWithTausVetoForWTauNu,
-                selectedPatJetsEta21ForWTauNu,
+        [ #selectedPatJetsAntiOverlapWithTausVetoForWTauNu,
+                selectedPatJetsEtaForWTauNu,
                 selectedPatJetsEt15ForWTauNu,
                 selectedPatJetsEtL20ForWTauNu ],
             src = "cleanPatJets",
@@ -59,38 +59,6 @@ patJetSelConfiguratorForWTauNu2 = objSelConfigurator(
 selectPatJetsForWTauNu = patJetSelConfiguratorForWTauNu.configure(pyNameSpace = locals())
 selectPatJetsForWTauNu2 = patJetSelConfiguratorForWTauNu2.configure(pyNameSpace = locals())
 
-#select jets for W->taunu analysis, loose isolation
-selectedPatJetsAntiOverlapWithTausVetoForWTauNuLooseIsolation = cms.EDFilter("PATJetAntiOverlapSelector",
-                                                                                srcNotToBeFiltered = cms.VInputTag("selectedPatTausForWTauNuEcalCrackVetoLooseIsolationCumulative"),
-                                                                                dRmin = cms.double(0.7),
-                                                                                filter = cms.bool(False)
-                                                                                )
-selectedPatJetsEta21ForWTauNuLooseIsolation = cms.EDFilter("PATJetSelector",
-                                                              cut = cms.string('abs(eta) < 2.1'),
-                                                              filter = cms.bool(False)
-                                                              )
-
-selectedPatJetsEt15ForWTauNuLooseIsolation = cms.EDFilter("PATJetSelector",
-                                                             cut = cms.string('et > 15.'),
-                                                             filter = cms.bool(False)
-                                                             )
-
-selectedPatJetsEt20ForWTauNuLooseIsolation = cms.EDFilter("PATJetSelector",
-                                                             cut = cms.string('et > 20.'),
-                                                             filter = cms.bool(False)
-                                                             )
-
-patJetSelConfiguratorForWTauNuLooseIsolation = objSelConfigurator(
-    [ selectedPatJetsAntiOverlapWithTausVetoForWTauNuLooseIsolation,
-      selectedPatJetsEta21ForWTauNuLooseIsolation,
-      selectedPatJetsEt15ForWTauNuLooseIsolation,
-      selectedPatJetsEt20ForWTauNuLooseIsolation ],
-    src = "cleanPatJets",
-    pyModuleName = __name__,
-    doSelIndividual = False
-    )
-selectPatJetsForWTauNuLooseIsolation =patJetSelConfiguratorForWTauNuLooseIsolation.configure(pyNameSpace = locals())
-
 selectPatSelJetsForWTauNu = cms.Sequence (selectPatJetsForWTauNu
                                           *selectPatJetsForWTauNu2
-                                             *selectPatJetsForWTauNuLooseIsolation) 
+                                          ) 
