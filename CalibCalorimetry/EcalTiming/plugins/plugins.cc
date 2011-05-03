02316022cb7e40dc42cdb9c94079fd3c74e6af91
franzoni <>
2011-05-03 14:16:09 +0000

    fix to register producer from electrons

--- CalibCalorimetry/EcalTiming/plugins/plugins.cc
+++ CalibCalorimetry/EcalTiming/plugins/plugins.cc
@@ -2,6 +2,7 @@
 #include "CalibCalorimetry/EcalTiming/interface/EcalXMLDelaysPlotter.h"
 #include "CalibCalorimetry/EcalTiming/interface/EcalCreateTTAvgTimes.h"
 #include "CalibCalorimetry/EcalTiming/plugins/EcalTimeTreeMaker.h"
+#include "CalibCalorimetry/EcalTiming/plugins/EcalTimeEleTreeMaker.h"
 #include "CalibCalorimetry/EcalTiming/plugins/EcalTimeCalibrationValidator.h"
 #include "CalibCalorimetry/EcalTiming/plugins/EcalCreateTimeCalibrations.h"
 
@@ -10,5 +11,6 @@ DEFINE_FWK_MODULE(EcalTimingCorrection);
 DEFINE_FWK_MODULE(EcalXMLDelaysPlotter);
 DEFINE_FWK_MODULE(EcalCreateTTAvgTimes);
 DEFINE_FWK_MODULE(EcalTimeTreeMaker);
+DEFINE_FWK_MODULE(EcalTimeEleTreeMaker);
 DEFINE_FWK_MODULE(EcalTimeCalibrationValidator);
 DEFINE_FWK_MODULE(EcalCreateTimeCalibrations);
