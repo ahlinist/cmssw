da37f1765b46d834cbcf013a00d2085af71272dd
lgray <>
2011-05-18 14:30:56 +0000

    Runs on grid properly now.

--- ElectroWeakAnalysis/MultiBosons/test/VgNtuplizer_cfg.py
+++ ElectroWeakAnalysis/MultiBosons/test/VgNtuplizer_cfg.py
@@ -12,13 +12,14 @@ options.setupTags (tag = '%d', ifCond = 'totalSections > 0', tagArg = 'section')
 
 ## setup any defaults you want
 #options.outputFile = 'vgtree.root'
-options.outputFile = '/raid1/veverka/VGammaSkims_v3/ElectronPhotonSkim_NoPhotonTriggers/vgtree.root'
-inputDir = "/mnt/tier2/store/user/veverka/EG/ElectronPhotonVGammaSkim-Sept17ReReco-Unmerged/7c145e2bfc574b9c8ba5f2e4bc572835/"
-options.inputFiles=",".join(["file:" + os.path.join(inputDir, f) for f in os.listdir(inputDir)])
+options.outputFile = 'vgtree.root'
+#inputDir = "/hdfs/store/user/lgray/SingleMu/Run2011A-v2/e1d9c80e015666f76e1af789462cb36a/"
+#options.inputFiles=",".join(["file:" + os.path.join(inputDir, f) for f in os.listdir(inputDir)])
+options.inputFiles=[]
 options.maxEvents = -1 # -1 means all events
 
 # get and parse the command line arguments
-options.parseArguments()
+#options.parseArguments()
 
 process = cms.Process("VgKit")
 
@@ -27,14 +28,14 @@ if options.maxEvents < 0:
     process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START38_V10::All')
+process.GlobalTag.globaltag = cms.string('GR_R_311_V4::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(options.maxEvents)
     )
 
-thelist = LumiList(filename="merged_JsonReRecoSep17_JsonStreamExpressV2.txt")
+#thelist = LumiList(filename="lumi.json")
 
 
 process.source = cms.Source("PoolSource",
@@ -43,7 +44,7 @@ process.source = cms.Source("PoolSource",
                             lumisToProcess = cms.untracked.VLuminosityBlockRange()
                             )
 
-process.source.lumisToProcess = [ str(a) for a in thelist.getCMSSWString().split(',') ] 
+#process.source.lumisToProcess = [ str(a) for a in thelist.getCMSSWString().split(',') ] 
 
 
 process.load("ElectroWeakAnalysis.MultiBosons.VgNtuplizer_cfi")
