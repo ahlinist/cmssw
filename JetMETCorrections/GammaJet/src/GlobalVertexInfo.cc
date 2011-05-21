3c0168ee4954ceb6699a21f300d8bc04fccc361c
rahatlou <>
2011-05-21 15:50:15 +0000

    version prepared by Paolo for 414p4 production with addition of new variables

---
+++ JetMETCorrections/GammaJet/src/GlobalVertexInfo.cc
@@ -0,0 +1,70 @@
+#include "JetMETCorrections/GammaJet/interface/GlobalVertexInfo.h"
+
+void GlobalVertexInfo::fillInfo(const edm::Event& iEvent, const edm::EventSetup& iSetup) 
+{
+  //Dump tracks
+  tk_p4->Clear();
+  tk_n = 0;
+  edm::Handle<reco::TrackCollection> tkH;
+  iEvent.getByLabel("generalTracks", tkH);
+  for(unsigned int i=0; i<tkH->size(); i++) {
+    if (tk_n >= MAX_TRACKS) {
+      std::cout << "GLobeTracks: WARNING TOO MANY TRACK: " << tkH->size() << " (allowed " << MAX_TRACKS << ")" << std::endl;
+      break;
+    }
+    reco::TrackRef tk(tkH, i);
+    new ((*tk_p4)[tk_n]) TLorentzVector();
+    ((TLorentzVector *)tk_p4->At(tk_n))->SetXYZT(tk->px(), tk->py(), tk->pz(), tk->p());
+    tk_pterr[tk_n] = tk->ptError();
+    tk_quality[tk_n] = tk->qualityMask();
+    tk_n++;
+  } // end loop overtracks
+    //Dump vertex
+  edm::Handle<reco::VertexCollection> vtxH;
+  iEvent.getByLabel("offlinePrimaryVertices", vtxH);
+  vtx_tkind.clear();
+  vtx_tkweight.clear();
+  vtx_n = 0;
+  vtx_xyz->Clear();
+  for(unsigned int i=0; i<vtxH->size(); i++) {
+    if (vtx_n >= MAX_VERTICES) {
+      std::cout << "GlobeVertex: WARNING TOO MANY VERTEX: " << vtxH->size() << " (allowed " << MAX_VERTICES << ")" << std::endl;
+      break;
+    }
+    reco::VertexRef vtx(vtxH, i);
+    // Any cut on vertex?? gCUT ??  
+    new((*vtx_xyz)[vtx_n]) TVector3();
+    ((TVector3 *) vtx_xyz->At(vtx_n))->SetXYZ(vtx->x(), vtx->y(), vtx->z());
+    vtx_ntks[vtx_n] = vtx->tracksSize();
+      
+    std::vector<reco::TrackBaseRef>::const_iterator tk;
+      
+    std::vector<unsigned short> temp;
+    std::vector<float> temp_float;
+      
+    for(tk=vtx->tracks_begin();tk!=vtx->tracks_end();++tk) {
+      int index = 0;
+      bool ismatched = false; 
+      for(reco::TrackCollection::size_type j = 0; j<tkH->size(); ++j) {
+	reco::TrackRef track(tkH, j);
+	if (&(**tk) == &(*track)) {
+	  //std::cout << "MWL: " << k << " " << index << " " << vtx->trackWeight(track) << std::endl;
+	  temp.push_back(index);
+	  temp_float.push_back(vtx->trackWeight(track));
+	  ismatched = true;
+	  break;
+	}
+	index++;
+      }
+       if( ! ismatched ) {
+	 std::cout << "This should not happen" << std::endl;
+// 	temp.push_back(-9999);
+// 	temp_float.push_back(-9999);
+       }
+    }
+      
+    vtx_tkind.push_back(temp);
+    vtx_tkweight.push_back(temp_float);
+    vtx_n++;
+  }  //end loop over vertices
+}
