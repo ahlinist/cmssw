8eafa7599b2fe9f35d27f5bb3df09a5d794973ff
jkolb <>
2011-06-14 09:25:52 +0000

    tidy things up a bit

--- TauAnalysis/Configuration/test/submitAnalysisAHtoElecTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisAHtoElecTau_grid.py
@@ -5,38 +5,37 @@ from TauAnalysis.Configuration.submitAnalysisToGrid import submitAnalysisToGrid
 import TauAnalysis.Configuration.userRegistry as reg
 
 channel = 'AHtoElecTau'
-reg.overrideJobId(channel,"Run24")
+##reg.overrideJobId(channel,"Run25")
 configFile = 'runAHtoElecTau_cfg.py'
 analysisFilePath = reg.getAnalysisFilePath(channel)
 jobId = reg.getJobId(channel)
 
 samplesToAnalyze = [
-    #'data_TauPlusX_Run2011A_PR_v1',
-    'data_TauPlusX_Run2011A_PR_v2',
-    #'data_TauPlusX_Run2011A_PR_nonGolden',
-    #'Ztautau_powheg_T3','DYtautauM10to20_powheg_T3',
-    #'qqZll',
-    #'DYeeM10to20_pythia_T3','Zee_powheg_T3',
-    #'WplusJets_madgraph_T3',
-    #'TTplusJets_madgraph_T3',
-    #'WW_T3','WZ_T3','ZZ_T3',
-    #'PhotonPlusJets_Pt15to30_T3','PhotonPlusJets_Pt30to50_T3','PhotonPlusJets_Pt50to80_T3',
-    #'QCD_BCtoE_Pt20to30_T3','QCD_BCtoE_Pt30to80_T3','QCD_BCtoE_Pt80to170_T3',
-    #'QCD_EM_Pt20to30_T3','QCD_EM_Pt30to80_T3','QCD_EM_Pt80to170_T3',
-    #'A90',  'bbA90',
-    #'A100', 'bbA100',
-    #'A120', 'bbA120',
-    #'A130_T3', 'bbA130_T3',
-    #'A140', 'bbA140',
-    #'A160', 'bbA160',
-    #'A180', 'bbA180',
-    #'A200', 'bbA200',
-    #'A250', 'bbA250',
-    #'A300', 'bbA300',
-    #'A350', 'bbA350',
-    #'A400', 'bbA400',
-    #'A450', 'bbA450',
-    #'A500', 'bbA500',
+    'Ztautau_powheg_T3',
+    'Zee_powheg_T3',
+    'WplusJets_madgraph_T3',
+    'TTplusJets_madgraph_T3',
+    'PhotonPlusJets_Pt15to30_T3','PhotonPlusJets_Pt30to50_T3','PhotonPlusJets_Pt50to80_T3',
+    'QCD_BCtoE_Pt20to30_T3','QCD_BCtoE_Pt30to80_T3','QCD_BCtoE_Pt80to170_T3',
+    'QCD_EM_Pt20to30_T3','QCD_EM_Pt30to80_T3','QCD_EM_Pt80to170_T3',
+    'WW_T3','WZ_T3','ZZ_T3',
+    'DYtautauM10to20_powheg_T3',
+    'DYeeM10to20_pythia_T3',
+    'qqZll_T3',
+    'A90_T3',  'bbA90_T3',
+    'A100_T3', 'bbA100_T3',
+    'A120_T3', 'bbA120_T3',
+    'A130_T3', 'bbA130_T3',
+    'A140_T3', 'bbA140_T3',
+    'A160_T3', 'bbA160_T3',
+    'A180_T3', 'bbA180_T3',
+    'A200_T3', 'bbA200_T3',
+    'A250_T3', 'bbA250_T3',
+    'A300_T3', 'bbA300_T3',
+    'A350_T3', 'bbA350_T3',
+    'A400_T3', 'bbA400_T3',
+    'A450_T3', 'bbA450_T3',
+    'A500_T3', 'bbA500_T3',
 ]
 
 #enableSystematics = True
