2039f70bc02bf524820e5b0cfb0febb05d6844be
liis <>
2011-06-22 22:03:38 +0000

    add objProdConfigurator

--- TauAnalysis/CandidateTools/python/tauNuPairProduction_cff.py
+++ TauAnalysis/CandidateTools/python/tauNuPairProduction_cff.py
@@ -1,15 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
+from TauAnalysis.CandidateTools.tools.objProdConfigurator import *
+from TauAnalysis.CandidateTools.svFitAlgorithmWtauNu_cfi import *
+
 #--------------------------------------------------------------------------------
 # produce combinations of tau-jet + missing transverse momentum 
 #--------------------------------------------------------------------------------
 
 allTauNuPairs = cms.EDProducer("PATTauNuPairProducer",
     srcVisDecayProducts = cms.InputTag('selectedPatTausForWTauNuEcalCrackVetoCumulative'),
-
     srcMET = cms.InputTag('patMETs'),
-
     verbosity = cms.untracked.int32(0)
 )
 
+tauNuPairProdConfigurator = objProdConfigurator(
+    allTauNuPairs,
+    pyModuleName = __name__
+    )
+
+produceTauNuPairs = tauNuPairProdConfigurator.configure(pyNameSpace = locals())
 produceTauNuPairs = cms.Sequence(allTauNuPairs)
