6f8616e8282e3b42ea4ee0c1a1156112322544d4
scooper <>
2011-06-23 15:48:12 +0000

    Make defaults more sensible.

--- CalibCalorimetry/EcalTiming/python/ecalTimeCalibrationValidator_cfg.py
+++ CalibCalorimetry/EcalTiming/python/ecalTimeCalibrationValidator_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("EcalTimeCalibrationValidator")
 
 # Global Tag -- for original timing calibrations
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'FT_R_39X_V4A::All'
+process.GlobalTag.globaltag = 'GR_R_43_V3::All'
 
 # shaping our Message logger to suit our needs
 process.MessageLogger = cms.Service("MessageLogger",
@@ -14,18 +14,19 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.expressValidator = cms.EDAnalyzer("EcalTimeCalibrationValidator",
-  InputFileNames = cms.vstring("file:/data2/kubota/TimingCalibrationOct384/src/CalibCalorimetry/EcalTiming/test/input_files/ecaltime-run-143953-144114/2ndhalf1.root",
+  InputFileNames = cms.vstring(#"file:/data2/kubota/TimingCalibrationOct384/src/CalibCalorimetry/EcalTiming/test/input_files/ecaltime-run-143953-144114/2ndhalf1.root",
                               #"file:/data2/kubota/TimingCalibrationOct384/src/CalibCalorimetry/EcalTiming/test/input_files/ecaltime-run-143953-144114/EcalTimeTree_999999_170_1_f5s.root"
+                       'file:/data/franzoni/data/Ecaltime/skimming-jun17/rhQuinq-SingleEletron-Run2011A-WElectron-PromptSkim-jun16_HADDED.root'
     ),
   OutputFileName = cms.string("file:converted1.root"),
-  CalibConstantXMLFileName = cms.string("myCalibBoth.xml"),
-  MaxTreeEntriesToProcess = cms.untracked.int32(100000000)
+  CalibConstantXMLFileName = cms.string("EcalTimeCalibs.xml"),
+  MaxTreeEntriesToProcess = cms.untracked.int32(-1)
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 process.source = cms.Source("EmptySource",
        numberEventsInRun = cms.untracked.uint32(1),
-       firstRun = cms.untracked.uint32(888888)
+       firstRun = cms.untracked.uint32(888888) # Use last IOV
 )
 
 
