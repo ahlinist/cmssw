04265d4cd4a6ded4582a3c0cd1f940893f56ce74
liis <>
2011-06-28 06:43:01 +0000

    add gen selection for W->tau nu analysis, remove produceMEtTopology from produceRecObjects sequence

--- TauAnalysis/RecoTools/python/postPatProduction_cff.py
+++ TauAnalysis/RecoTools/python/postPatProduction_cff.py
@@ -6,8 +6,10 @@ import FWCore.ParameterSet.Config as cms
 
 # import sequence for selection of generated particles
 # produced in Z, A/H decays to electrons, muons and tau leptons
+# and W decays to tau leptons
 from TauAnalysis.GenSimTools.gen_decaysFromZs_cfi import *
 from TauAnalysis.GenSimTools.gen_decaysFromAHs_cfi import *
+from TauAnalysis.GenSimTools.gen_decaysFromWs_cfi import *
 
 # import sequence for production of generator level information about phase-space simulated in Monte Carlo
 # (needed to avoid overlap in phase-space simulated in different QCD background Monte Carlo samples)
@@ -25,24 +27,24 @@ from TauAnalysis.RecoTools.recoVertexSelection_cff import *
 from TauAnalysis.CandidateTools.metTopology_cfi import *
 
 # import sequence for selection of generated Z --> tau+ tau- --> ...,
-# generated A/H --> tau+ tau- --> ... decay modes
+# generated A/H --> tau+ tau- --> ... and W -> tau nudecay modes
 from TauAnalysis.RecoTools.selectGenZtautauDecayModes_cfi import *
 from TauAnalysis.RecoTools.selectGenAHtautauDecayModes_cfi import *
+from TauAnalysis.RecoTools.selectGenWtaunuDecayModes_cfi import *
 
 # import sequence for production of PDF/ISR/FSR (re)weights
 # for estimating systematic (theory) uncertainties
 from TauAnalysis.GenSimTools.sysErrGenEventReweights_cfi import *
 
 produceGenObjects = cms.Sequence(
-    produceGenDecayProductsFromZs + produceGenDecayProductsFromAHs 
-   + produceGenPhaseSpaceEventInfo
-   + produceTauGenJets
-   + selectGenZtautauDecayModes + selectGenAHtautauDecayModes 
+    produceGenDecayProductsFromZs + produceGenDecayProductsFromAHs + produceGenDecayProductsFromWs
+    + produceGenPhaseSpaceEventInfo
+    + produceTauGenJets
+    + selectGenZtautauDecayModes + selectGenAHtautauDecayModes + selectGenWtaunuDecayModes
 )
 
 produceRecObjects = cms.Sequence(
     selectPrimaryVertex
-   + produceMEtTopology
 )
 
 producePostPat = cms.Sequence(produceGenObjects + produceRecObjects)
