0b8583cadceaaa43d6f9cdd80e3e56e44dcdfef9
mverzett <>
2011-06-29 14:32:35 +0000

    Updating the samples for TauId Efficiency measurement

--- TauAnalysis/Configuration/python/recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi.py
+++ TauAnalysis/Configuration/python/recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi.py
@@ -6,7 +6,9 @@ import TauAnalysis.DQMTools.plotterStyleDefinitions_cfi as styles
 
 # List of samples to run in the analysis
 SAMPLES_TO_ANALYZE = [
-    'data_SingleMu_Run2011A_May10ReReco_v1ex',
+#    'data_SingleMu_Run2011A_May10ReReco_v1ex',
+    'data_SingleMu_Run2011A_PromptReco_v4', #TO BE UPDATED
+    'data_SingleMu_Run2011A_May10ReReco_v1_IsoMu17Only',
     'data_TauPlusX_Run2011A_May10ReReco_v1',
     'data_TauPlusX_Run2011A_PromptReco_v4',
     #'DYtautauM10to20_powheg',
@@ -140,6 +142,46 @@ RECO_SAMPLES = {
         'hlt_paths' : {
             'HLT_IsoMu12_v1' : '160431:MIN-163261:MAX',
             'HLT_IsoMu17_v6' : '163270:MIN-163869:MAX'
+        },
+        'enableSysUncertainties' : False,
+        'enableFakeRates' : True,
+        'hlt' : cms.InputTag("TriggerResults", "", "HLT")
+    },
+    'data_SingleMu_Run2011A_May10ReReco_v1_IsoMu17Only' : {
+        'datasetpath' : '/SingleMu/Run2011A-May10ReReco-v1/AOD',
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON.txt",
+        'runselection' : "160329-163869",
+        'number_of_jobs' : 500,
+        'conditions' : 'GR_R_42_V14::All',
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+        'drawOption' : styles.drawOption_Data,
+        'hlt_paths' : {
+            'HLT_IsoMu17_v5' : '160431:MIN-163261:MAX',
+            'HLT_IsoMu17_v6' : '163270:MIN-163869:MAX'
+        },
+        'enableSysUncertainties' : False,
+        'enableFakeRates' : True,
+        'hlt' : cms.InputTag("TriggerResults", "", "HLT")
+    },
+    'data_SingleMu_Run2011A_PromptReco_v4' : {
+        'datasetpath' : '/SingleMu/Run2011A-PromptReco-v4/AOD',
+        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-167151_7TeV_PromptReco_Collisions11_CMSSWConfig.txt", #TO BE UPDATED!
+        'runselection' : "165088-167151", #TO BE UPDATED TOO!
+        'number_of_jobs' : 500,
+        'conditions' : 'GR_R_42_V14::All',
+        'events_processed' : -1,
+        'skim_eff' : 1.0,
+        'type' : 'Data',
+        'drawOption' : styles.drawOption_Data,
+        'hlt_paths' : {
+            'HLT_IsoMu17_v8'  : '165088:MIN-165633:MAX',
+            'HLT_IsoMu17_v9'  : '165970:MIN-167043:MAX',
+            'HLT_IsoMu17_v10' : '166346:MIN-166346:MAX',
+            'HLT_IsoMu17_v11' : '167078:MIN-167151:MAX'
         },
         'enableSysUncertainties' : False,
         'enableFakeRates' : True,
