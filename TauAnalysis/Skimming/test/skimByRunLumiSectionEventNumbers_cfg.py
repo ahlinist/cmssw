9911d8ee25cc905f51365e95792dec9ecfdea345
veelken <>
2011-07-05 17:08:06 +0000

    update to CMSSW_4_2_x and clean-up

--- TauAnalysis/Skimming/test/skimByRunLumiSectionEventNumbers_cfg.py
+++ TauAnalysis/Skimming/test/skimByRunLumiSectionEventNumbers_cfg.py
@@ -20,17 +20,7 @@ castorFilePath = 'rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_8_x/skims/tauCommi
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #'file:/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/selEvents_AHtoMuTau_HPSloose_2011Jan29_friis_RECO.root'
-        #'file:/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/selEvents_AHtoMuTau_HPSloose_2011Feb03_bachtis_RECO.root'
-        #'file:/data1/veelken/CMSSW_3_8_x/skims/ZtoMuTau/selEvents_ZtoMuTau_HPSloose_2011Feb03_veelken_RECO.root'
-        #'/store/data/Run2010A/Mu/RECO/Nov4ReReco_v1/0001/E82DC3B2-1AEB-DF11-B51A-0017A477102C.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0000/641FB9B3-61EA-DF11-A147-E0CB4E29C4FB.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0000/CC42DC6D-08EA-DF11-A05B-E0CB4E29C4CA.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0001/FAA6E118-F8E9-DF11-9B42-E0CB4E553637.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0001/EC0DA164-48EA-DF11-9B81-E0CB4E29C51A.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0000/7A1972FA-10EA-DF11-876D-485B39800BF0.root',
-        #'/store/data/Run2010B/Mu/RECO/Nov4ReReco_v1/0000/644B8C0A-57EA-DF11-895E-E0CB4E19F975.root'
-        'file:/data1/friis/CMSSW_3_8_x/skims/ZtoMuTau/skim_Ztautau_powheg_chunk_8_fcc0.root'                        
+        'file:/data1/veelken/CMSSW_4_2_x/skims/skimGenZtoMuTauWithinAcc_Ztautau_2011Jun30v2_AOD.root'
     )
 )
 
@@ -57,13 +47,7 @@ process.options = cms.untracked.PSet(
 
 process.selectEventsByRunLumiSectionEventNumber = cms.EDFilter("RunLumiSectionEventNumberFilter",
     runLumiSectionEventNumberFileName = cms.string(
-        #'/afs/cern.ch/user/v/veelken/public/selEvents_ZtoMuTau_2011Feb01_HPSloose_twoOSglobalMuons.txt'
-        #'selEvents_ZtoMuTau_bachtis_2011Feb03_converted.txt'
-        #'uw_only_events.txt'
-        #'selEvents_ZtoMuTau_approval_2011Mar03_converted.txt'
-        #'selEvents_ZtoElecTau_approval_2011Mar03_converted.txt'
-        #'selEvents_ZtoMuMu_approval_2011Mar03.txt'
-        'selEvents_ZtautauPU156bx.txt'                                                           
+        '/afs/cern.ch/user/v/veelken/scratch0/CMSSW_4_2_4_patch1/src/TauAnalysis/Test/test/selEvent_genZtoMuTau.txt'
     ),
     separator = cms.string(':')
 )
@@ -92,13 +76,7 @@ process.skimOutputModule = cms.OutputModule("PoolOutputModule",
     eventSelection,
     process.origFEVTSIMEventContent,
     fileName = cms.untracked.string(
-        #'/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/selEvents_AHtoMuTau_HPSloose_2011Feb03_bachtis_RECO.root'
-        #'/data1/veelken/CMSSW_3_8_x/skims/AHtoMuTau/uwOnlyEvents_AHtoMuTau_HPSloose_2011Feb03_bachtis_RECOslimmed.root'
-        #'/data1/veelken/CMSSW_3_8_x/skims/ZtoMuTau/selEvents_ZtoMuTau_HPSloose_2011Feb03_veelken_twoOSglobalMuons_RECO.root'
-        #'/data1/veelken/CMSSW_3_8_x/skims/ZtoMuTau/selEvents_ZtoMuTau_approval_2011Mar03_RECO.root'
-        #'/data1/veelken/CMSSW_3_8_x/skims/ZtoMuTau/selEvents_ZtoElecTau_approval_2011Mar03_RECO.root'
-        #'/data1/veelken/CMSSW_3_8_x/skims/ZtoMuTau/selEvents_ZtoMuMu_approval_2011Mar03_RECO.root'
-        '/data1/veelken/CMSSW_3_8_x/skims/test/selEvents_ZtautauPU156bx_RECO.root'                                        
+        '/data1/veelken/CMSSW_4_2_x/skims/skimGenZtoMuTauWithinAcc_Ztautau_2011Jun30v2_C1sel_AOD.root'
     )
 )
 
