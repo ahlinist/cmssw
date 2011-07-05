e802c3b29bc5e05a67bbbf73abd48b5f7b292c49
liis <>
2011-07-05 10:33:27 +0000

    add objSelConfigurator

--- TauAnalysis/CandidateTools/python/metTopology_cfi.py
+++ TauAnalysis/CandidateTools/python/metTopology_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from TauAnalysis.CandidateTools.tools.objProdConfigurator import *
+from TauAnalysis.CandidateTools.tools.objSelConfigurator import *
 #--------------------------------------------------------------------------------
 # produce data-formats providing information 
 # about distribution of energy deposits in the event 
@@ -7,7 +9,9 @@ import FWCore.ParameterSet.Config as cms
 #--------------------------------------------------------------------------------
 
 metTopologies = cms.EDProducer("MEtTopologyProducer",
-    srcEnergyDeposits = cms.VInputTag('pfNoPileUp'),                           
+    srcEnergyDeposits = cms.VInputTag('selectedPatTausForWTauNuEcalCrackVetoCumulative',
+                                      'selectedPatJetsAntiOverlapWithTausVetoForWTauNuCumulative'
+                                      ),                           
     globalThreshold = cms.double(0.5),
     verbosity = cms.untracked.int32(0)
 )
@@ -25,7 +29,18 @@ selectedMEtTopology025 = cms.EDFilter("MEtTopologySelector",
 )
 
 
-produceMEtTopology = cms.Sequence( metTopologies)
+metTopologyProdConfigurator = objProdConfigurator(
+    metTopologies,
+    pyModuleName = __name__
+    )
+produceMEtTopology = metTopologyProdConfigurator.configure(pyNameSpace = locals())
 
-selectMEtTopology = cms.Sequence( selectedMEtTopology05
-                                  *selectedMEtTopology025)
+metTopologySelConfigurator = objSelConfigurator(
+    [selectedMEtTopology05,
+     selectedMEtTopology025],
+    src = "metTopologies",
+    pyModuleName = __name__,
+    doSelIndividual = False
+    )
+
+selectMEtTopology = metTopologySelConfigurator.configure(pyNameSpace = locals())
