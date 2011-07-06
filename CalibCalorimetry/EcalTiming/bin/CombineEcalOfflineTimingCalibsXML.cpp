1f0d4b52e7c5755764bccee0fd05f152e491f6d2
scooper <>
2011-07-06 14:02:20 +0000

    Update: New calibrations simply replace the old one; errors are no longer used.

--- CalibCalorimetry/EcalTiming/bin/CombineEcalOfflineTimingCalibsXML.cpp
+++ CalibCalorimetry/EcalTiming/bin/CombineEcalOfflineTimingCalibsXML.cpp
@@ -11,10 +11,9 @@
 void usage()
 {
   std::cout
-    << "Usage: CombineEcalOfflineTimingCalibsXML [originalCalibsFile] [originalCalibErrorsFile] [fileWithChanges] [fileWithChangesErrors]"
+    << "Usage: CombineEcalOfflineTimingCalibsXML [originalCalibsFile] [fileWithNewCalibs]"
     << std::endl
-    << "\tThe values in fileWithChanges will be added to those in originalCalibsFile."
-    << " If only one file contains a good calibration, that file's calibration will be used."
+    << "\tThe values in fileWithNewCalibs will replace the corresponding ones in originalCalibsFile; all other calibs in originalCalibsFile will be untouched."
     << std::endl;
 }
 
@@ -22,11 +21,12 @@ void usage()
 int main(int argc, char* argv[])
 {
   //
-  // Binary to combine 2 sets of timing calibrations, using original as the baseline
+  // Binary to combine 2 sets of timing calibrations
   // Seth Cooper
-  // November 8th 2010
-  // Usage: ShiftEcalOfflineTimingCalibsXML <originalCalibsFile> <originalCalibErrorsFile> <fileWithChanges> <fileWithChangesErrors>
-  //   The calibs in the file to shift will be (intelligently) added to those in the original file.
+  // Updated July 6 2011: No longer uses the error files (we ignore them for now anyway)
+  //                      Just replaces the calibrations in the original file with those passed in
+  // Usage: ShiftEcalOfflineTimingCalibsXML <originalCalibsFile> <fileWithChanges>
+  //   The calibs in the fileWithChanges will replace those in the original file.
   //
 
   using namespace std;
@@ -38,23 +38,9 @@ int main(int argc, char* argv[])
     usage();
     return -1;
   }
-  char* calibErrorFile = argv[2];
-  if (!calibErrorFile)
-  {
-    cout << "Error: Missing input file." << endl;
-    usage();
-    return -1;
-  }
 
-  char* shiftFile = argv[3];
+  char* shiftFile = argv[2];
   if (!shiftFile)
-  {
-    cout << "Error: Missing input file." << endl;
-    usage();
-    return -1;
-  }
-  char* shiftErrorFile = argv[4];
-  if (!shiftErrorFile)
   {
     cout << "Error: Missing input file." << endl;
     usage();
@@ -62,19 +48,13 @@ int main(int argc, char* argv[])
   }
 
   EcalCondHeader calibFileHeader;
-  EcalCondHeader calibErrorFileHeader;
   EcalCondHeader shiftFileHeader;
-  EcalCondHeader shiftErrorFileHeader;
 
   string calibFileStr(calibFile);
-  string calibErrorFileStr(calibErrorFile);
   string shiftFileStr(shiftFile);
-  string shiftErrorFileStr(shiftErrorFile);
 
   EcalTimeCalibConstants calibConstants;
-  EcalTimeCalibErrors calibErrors;
   EcalTimeCalibConstants shiftConstants;
-  EcalTimeCalibErrors shiftErrors;
 
   // Populate the EcalTimeCalibConstants objects by reading in the files
   int ret = EcalTimeCalibConstantsXMLTranslator::readXML(calibFileStr,calibFileHeader,calibConstants);
@@ -83,169 +63,51 @@ int main(int argc, char* argv[])
     cout << "Error reading calibration XML file" << endl;
     return -2;
   }
-  ret = EcalTimeCalibConstantsXMLTranslator::readXML(calibErrorFileStr,calibErrorFileHeader,calibErrors);
-  if(ret)
-  {
-    cout << "Error reading calib error XML file" << endl;
-    return -2;
-  }
   ret = EcalTimeCalibConstantsXMLTranslator::readXML(shiftFileStr,shiftFileHeader,shiftConstants);
   if(ret)
   {
     cout << "Error reading shift XML file" << endl;
     return -2;
   }
-  ret = EcalTimeCalibConstantsXMLTranslator::readXML(shiftErrorFileStr,shiftErrorFileHeader,shiftErrors);
-  if(ret)
-  {
-    cout << "Error reading shift error XML file" << endl;
-    return -2;
-  }
 
-  float EBavgShift = 0;
-  int numEBcrys = 0;
+  // Loop over the calib constants and apply the replacements
   for(int hash = 0; hash < EBDetId::MAX_HASH; ++hash)
   {
+    float newCalib = 0;
     EBDetId thisDet = EBDetId::unhashIndex(hash);
     if(thisDet==EBDetId())
       continue;
 
-    if(shiftErrors[thisDet.rawId()]!=999)
-    {
-      EBavgShift+=shiftConstants[thisDet.rawId()];
-      ++numEBcrys;
-    }
-  }
-  EBavgShift/=numEBcrys;
+    // Look for constant to replace
+    if(shiftConstants.find(thisDet.rawId())==shiftConstants.end())
+      continue;
 
-  float EEavgShift = 0;
-  int numEEcrys = 0;
+    newCalib = shiftConstants[thisDet.rawId()];
+    //cout << "Crystal " << thisDet << " calibration now " << newCalib
+    //  << "; was " << calibConstants[thisDet.rawId()]
+    //  << endl;
+    calibConstants[thisDet.rawId()] = newCalib;
+  }
   for(int hash = 0; hash < EEDetId::kSizeForDenseIndexing; ++hash)
   {
+    float newCalib = 0;
     EEDetId thisDet = EEDetId::unhashIndex(hash);
     if(thisDet==EEDetId())
       continue;
 
-    if(shiftErrors[thisDet.rawId()]!=999)
-    {
-      EEavgShift+=shiftConstants[thisDet.rawId()];
-      ++numEEcrys;
-    }
-  }
-  EEavgShift/=numEEcrys;
-
-  // Loop over the calib constants and apply any necessary shifts
-  for(int hash = 0; hash < EBDetId::MAX_HASH; ++hash)
-  {
-    EBDetId thisDet = EBDetId::unhashIndex(hash);
-    if(thisDet==EBDetId())
+    if(shiftConstants.find(thisDet.rawId())==shiftConstants.end())
       continue;
 
-    bool origCalibFound = true;
-    bool shiftCalibFound = true;
-
-    if(calibConstants.find(thisDet.rawId())==calibConstants.end())
-      origCalibFound = false;
-    if(calibConstants[thisDet.rawId()]==0)
-      origCalibFound = false;
-    if(shiftConstants.find(thisDet.rawId())==shiftConstants.end())
-      shiftCalibFound = false;
-    if(shiftConstants[thisDet.rawId()]==0)
-      shiftCalibFound = false;
-    if(calibErrors[thisDet.rawId()]==999)
-      origCalibFound = false;
-    if(shiftErrors[thisDet.rawId()]==999)
-      shiftCalibFound = false;
-
-    float newCalib = 0;
-    float newCalibError = 0;
-    if(origCalibFound && shiftCalibFound) // Calib found and there is one to shift as well. Shift it.
-    {
-      newCalib = calibConstants[thisDet.rawId()]+shiftConstants[thisDet.rawId()];
-      newCalibError = sqrt(pow(calibErrors[thisDet.rawId()],2)+pow(shiftErrors[thisDet.rawId()],2));
-    }
-    if(origCalibFound && !shiftCalibFound) // good original, but no shifted calib -- use original + new avg shift
-    {
-      newCalib = calibConstants[thisDet.rawId()] + EBavgShift;
-      newCalibError = calibErrors[thisDet.rawId()];
-    }
-    if(!origCalibFound && shiftCalibFound) // good shifted calib, but no original calib -- use new shifted calib
-    {
-      newCalib = shiftConstants[thisDet.rawId()];
-      newCalibError = shiftErrors[thisDet.rawId()];
-    }
-    if(!origCalibFound && !shiftCalibFound) // if neither has a good one, use the (new) average shift - global shift
-    {
-      newCalib = EBavgShift - 2.0833;
-      newCalibError = 999; // set error to 999 to indicate avg calib used
-    }
-
+    newCalib = calibConstants[thisDet.rawId()];
     //cout << "Crystal " << thisDet << " calibration now " << newCalib
-    //  << " = " << calibConstants[thisDet.rawId()]
-    //  << " + " << shiftConstants[thisDet.rawId()]
+    //  << "; was  " << calibConstants[thisDet.rawId()]
     //  << endl;
     calibConstants[thisDet.rawId()] = newCalib;
-    calibErrors[thisDet.rawId()] = newCalibError;
-  }
-  for(int hash = 0; hash < EEDetId::kSizeForDenseIndexing; ++hash)
-  {
-    EEDetId thisDet = EEDetId::unhashIndex(hash);
-    if(thisDet==EEDetId())
-      continue;
-
-    bool origCalibFound = true;
-    bool shiftCalibFound = true;
-
-    if(calibConstants.find(thisDet.rawId())==calibConstants.end())
-      origCalibFound = false;
-    if(calibConstants[thisDet.rawId()]==0)
-      origCalibFound = false;
-    if(shiftConstants.find(thisDet.rawId())==shiftConstants.end())
-      shiftCalibFound = false;
-    if(shiftConstants[thisDet.rawId()]==0)
-      shiftCalibFound = false;
-    if(calibErrors[thisDet.rawId()]==999)
-      origCalibFound = false;
-    if(shiftErrors[thisDet.rawId()]==999)
-      shiftCalibFound = false;
-
-    float newCalib = 0;
-    float newCalibError = 0;
-    if(origCalibFound && shiftCalibFound) // Calib found and there is one to shift as well. Shift it.
-    {
-      newCalib = calibConstants[thisDet.rawId()]+shiftConstants[thisDet.rawId()];
-      newCalibError = sqrt(pow(calibErrors[thisDet.rawId()],2)+pow(shiftErrors[thisDet.rawId()],2));
-    }
-    if(origCalibFound && !shiftCalibFound) // good original, but no shifted calib -- use original + new avg shift
-    {
-      newCalib = calibConstants[thisDet.rawId()] + EEavgShift;
-      newCalibError = calibErrors[thisDet.rawId()];
-    }
-    if(!origCalibFound && shiftCalibFound) // good shifted calib, but no original calib -- use new shifted calib
-    {
-      newCalib = shiftConstants[thisDet.rawId()];
-      newCalibError = shiftErrors[thisDet.rawId()];
-    }
-    if(!origCalibFound && !shiftCalibFound) // if neither has a good one, use the (new) average shift - global shift
-    {
-      newCalib = EEavgShift - 2.0833;
-      newCalibError = 999; // set error to 999 to indicate avg calib used
-    }
-
-    //cout << "Crystal " << thisDet << " calibration now " << newCalib
-    //  << " = " << calibConstants[thisDet.rawId()]
-    //  << " + " << shiftConstants[thisDet.rawId()]
-    //  << endl;
-    calibConstants[thisDet.rawId()] = newCalib;
-    calibErrors[thisDet.rawId()] = newCalibError;
   }
 
   // Write new XML file
   string outputFile = "ecalOfflineTimingCalibsCombined.xml";
-  string outputErrorsFile = "ecalOfflineTimingErrorsCombined.xml";
   EcalTimeCalibConstantsXMLTranslator::writeXML(outputFile,calibFileHeader,calibConstants);
-  EcalTimeCalibErrorsXMLTranslator::writeXML(outputErrorsFile,calibErrorFileHeader,calibErrors);
-
 
 }
 
