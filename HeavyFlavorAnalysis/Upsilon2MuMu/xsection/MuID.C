c923c38d2bae4164eedd4bb3cf21f57c121c1deb
bora <>
2011-07-16 08:39:25 +0000

    *** empty log message ***

--- HeavyFlavorAnalysis/Upsilon2MuMu/xsection/MuID.C
+++ HeavyFlavorAnalysis/Upsilon2MuMu/xsection/MuID.C
@@ -5,7 +5,7 @@
 
 void MuIDCheckVsfMuIDmmb(){
 
-  TFile *f = new TFile("101201.fl10.mm.ups1s.tnpMCtruth.xsReader.default.root");
+  TFile *f = new TFile("101201.fl10.mm.ups1s.xsReader_1SBin_MomCorr.default.root");
   TH2D *MuIDCheck_Deno_1S;
   MuIDCheck_Deno_1S = (TH2D*)gFile->Get("MuIDCheck_Deno_1S");
   TH2D *MuIDCheck_Numa_1S;
@@ -36,7 +36,7 @@ void MuIDCheckVsfMuIDmmb(){
     }
   }
   
-  TFile *f1 = new TFile("MuID_MC.root");
+  TFile *f1 = new TFile("MuID.root");
   TH2D *fMuIDEff;
   fMuIDEff = (TH2D*)gFile->Get("fMuIDEff");
   
@@ -241,4 +241,46 @@ void MuIDCheckVsfMuID_DATA(){
   fMuIDEff->Draw("colz");
   c1->cd(3);
   Diff->Draw("colz");
+}
+
+
+void UG_Acceptance(){
+
+  TFile *f = new TFile("Acc.xsReader_1S.default.root");
+  TH2D *UG_AllGenRes_1S;
+  UG_AllGenRes_1S = (TH2D*)gFile->Get("UG_AllGenRes_1S");
+  TH2D *UG_RecoGenRes_1S;
+  UG_RecoGenRes_1S = (TH2D*)gFile->Get("UG_RecoGenRes_1S");
+  TH2D *Ratio = (TH2D*)UG_RecoGenRes_1S->Clone(); 
+  Ratio->SetName("Acceptance");
+  Ratio->Reset();
+    
+  double bin_contentBefore(0); double bin_contentAfter(0); double bin_ratio(0.);
+  for (int i = 1; i <= UG_RecoGenRes_1S->GetNbinsX(); ++i) {
+    for (int j = 1; j <= UG_RecoGenRes_1S->GetNbinsY(); ++j){	
+      
+      bin_contentBefore = UG_AllGenRes_1S->GetCellContent(i,j);
+      bin_contentAfter = UG_RecoGenRes_1S->GetCellContent(i,j);
+      
+      cout << "bin_contentAllGenRes = " << bin_ contentBefore << endl;
+      cout << "bin_contentRecoGenRes = " << bin_ contentAfter << endl;
+      
+      bin_ratio = bin_contentAfter / bin_contentBefore;
+      cout << "bin_ratio = " << bin_ratio << endl;
+      Ratio->SetCellContent(i,j,bin_ratio);
+      bin_contentBefore=0;
+      bin_contentAfter=0;
+      bin_ratio=0;
+    }
+  }
+  
+  TCanvas *c1 = new TCanvas("c1","c1",1200,600); 
+  //c1->Divide(3,1); 
+  c1->cd(1);
+  //UG_AllGenRes_1S->Draw("colz");
+  //c1->cd(2);
+  //UG_RecoGenRes_1S->Draw("colz");
+  //c1->cd(3);
+  Ratio->Draw("colz");
+
 }
