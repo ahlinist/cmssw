c5682743d946eef0fd526ae07844ce7e5fbe7d1c
hwoehri <>
2011-07-18 21:09:36 +0000

    - added arbitration to the muon selection cuts - M > 2 GeV cut removed - allow the storage of T&P pairs in the output module

--- HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cff.py
+++ HeavyFlavorAnalysis/Onia2MuMu/python/onia2MuMuPAT_cff.py
@@ -95,12 +95,13 @@ def onia2MuMuPAT(process, GlobalTag, MC=False, HLT='HLT', Filter=True):
         beamSpotTag = cms.InputTag("offlineBeamSpot"),
         primaryVertexTag = cms.InputTag("offlinePrimaryVertices"),
         # At least one muon must pass this selection
-        higherPuritySelection = cms.string("(isGlobalMuon || isTrackerMuon || (innerTrack.isNonnull && genParticleRef(0).isNonnull)) && abs(innerTrack.dxy)<4 && abs(innerTrack.dz)<35"),
+        higherPuritySelection = cms.string("(isGlobalMuon || isTrackerMuon || (innerTrack.isNonnull && genParticleRef(0).isNonnull)) && abs(innerTrack.dxy)<4 && abs(innerTrack.dz)<35 && muonID('TrackerMuonArbitrated')"),
         # BOTH muons must pass this selection
-        lowerPuritySelection  = cms.string("(isGlobalMuon || isTrackerMuon || (innerTrack.isNonnull && genParticleRef(0).isNonnull)) && abs(innerTrack.dxy)<4 && abs(innerTrack.dz)<35"),
-        dimuonSelection  = cms.string("2 < mass"), ## The dimuon must pass this selection before vertexing
+        lowerPuritySelection  = cms.string("(isGlobalMuon || isTrackerMuon || (innerTrack.isNonnull && genParticleRef(0).isNonnull)) && abs(innerTrack.dxy)<4 && abs(innerTrack.dz)<35 && muonID('TrackerMuonArbitrated')"),
+        #dimuonSelection  = cms.string("2 < mass"), ## The dimuon must pass this selection before vertexing
+        dimuonSelection  = cms.string(""), ## The dimuon must pass this selection before vertexing
         addCommonVertex = cms.bool(True), ## Embed the full reco::Vertex out of the common vertex fit
-        addMuonlessPrimaryVertex = cms.bool(False), ## Embed the primary vertex re-made from all the tracks except the two muons
+        addMuonlessPrimaryVertex = cms.bool(True), ## Embed the primary vertex re-made from all the tracks except the two muons
         addMCTruth = cms.bool(MC),      ## Add the common MC mother of the two muons, if any
         resolvePileUpAmbiguity = cms.bool(True)   ## Order PVs by their vicinity to the J/psi vertex, not by sumPt                            
     )
@@ -183,7 +184,7 @@ def onia2MuMuPAT(process, GlobalTag, MC=False, HLT='HLT', Filter=True):
             'keep *_l1GtRecord_*_*',                               # Prescale info    
                                                
         ),
-        SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('Onia2MuMuPAT') ) if Filter else cms.untracked.PSet()
+        SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('Onia2MuMuPAT', 'TagAndProbe') ) if Filter else cms.untracked.PSet()
     )
     process.e = cms.EndPath(process.out)
 
