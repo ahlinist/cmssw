629599a1713f069740251b4981091b82000adbbc
franzoni <>
2011-07-20 23:24:10 +0000

    EleMakeTree selects on eleID and acceps sc's |eta|>2.5

--- CalibCalorimetry/EcalTiming/python/ecalTimeEleMakeTree_cfg.py
+++ CalibCalorimetry/EcalTiming/python/ecalTimeEleMakeTree_cfg.py
@@ -1,6 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TIMECALIBANALYSIS")
+process = cms.Process("TIMECALIBANALYSISELE")
+
+
+# Output - dummy
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    outputCommands = cms.untracked.vstring(),
+    fileName = cms.untracked.string('file:pippo.root'),
+    )
+
 
 # gfworks: to get clustering 
 process.load('Configuration/StandardSequences/GeometryExtended_cff')
@@ -14,10 +23,24 @@ process.load("Geometry.EcalMapping.EcalMappingRecord_cfi")
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi") # gfwork: need this?
 
 
+# pat needed to work out electron id/iso
+from PhysicsTools.PatAlgos.tools.metTools import *
+from PhysicsTools.PatAlgos.tools.tauTools import *
+from PhysicsTools.PatAlgos.tools.jetTools import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+from PhysicsTools.PatAlgos.tools.pfTools import *
+
+from PhysicsTools.PatAlgos.selectionLayer1.leptonCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.photonCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+
+
 # Global Tag
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_noesprefer_cff")
-process.GlobalTag.globaltag = 'GR_R_35X_V8A::All'
+process.GlobalTag.globaltag = 'GR_R_42_V14::All'
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 
 
 # Trigger
@@ -34,77 +57,45 @@ process.gtDigis = EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi.l1GtUnpack
 
 
 
-# general basic- and super- clustering sequences
-import RecoEcal.EgammaClusterProducers.multi5x5ClusteringSequence_cff
+#------------------
+#Load PAT sequences
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+process.load("PhysicsTools.PatAlgos.tools.pfTools")
+#
+## THis is NOT MC => remove matching
+removeMCMatching(process, ['All'])
+#
+#
+## bugfix for DATA Run2011 (begin)
+removeSpecificPATObjects( process, ['Taus'] )
+process.patDefaultSequence.remove( process.patTaus )
 
-# 3x3 clustering for barrel
-process.multi5x5BasicClustersTimePi0Barrel =  RecoEcal.EgammaClusterProducers.multi5x5BasicClusters_cfi.multi5x5BasicClusters.clone(
-    # which regions should be clusterized
-    doEndcap = cms.bool(False),
-    doBarrel = cms.bool(True),
-
-    # gfwork: this is standard, can go away 
-    barrelHitProducer = cms.string('ecalRecHit'),
-    barrelHitCollection = cms.string('EcalRecHitsEB'),
-    endcapHitProducer = cms.string('ecalRecHit'),
-    endcapHitCollection = cms.string('EcalRecHitsEE'),
-    
-    IslandBarrelSeedThr = cms.double(0.5),   # barrelSeedThreshold
-    IslandEndcapSeedThr = cms.double(1.0),   # endcapSeedThreshold
-
-    barrelClusterCollection = cms.string('multi5x5BarrelBasicClusters'),
-    endcapClusterCollection = cms.string('multi5x5EndcapBasicClusters'),
-    clustershapecollectionEE = cms.string('multi5x5EndcapShape'),
-    clustershapecollectionEB = cms.string('multi5x5BarrelShape'),
-    barrelShapeAssociation = cms.string('multi5x5BarrelShapeAssoc'),
-    endcapShapeAssociation = cms.string('multi5x5EndcapShapeAssoc'),
-    )
-
-
-# 3x3 clustering for endcap
-process.multi5x5BasicClustersTimePi0Endcap =  RecoEcal.EgammaClusterProducers.multi5x5BasicClusters_cfi.multi5x5BasicClusters.clone(
-    # which regions should be clusterized
-    doEndcap = cms.bool(True),
-    doBarrel = cms.bool(False),
-
-    barrelHitProducer = cms.string('ecalRecHit'),
-    barrelHitCollection = cms.string('EcalRecHitsEB'),
-    endcapHitProducer = cms.string('ecalRecHit'),
-    endcapHitCollection = cms.string('EcalRecHitsEE'),
-    
-    IslandBarrelSeedThr = cms.double(0.5),              # endcapSeedThreshold
-    IslandEndcapSeedThr = cms.double(1.0),             # barrelSeedThreshold
-
-    barrelClusterCollection = cms.string('multi5x5BarrelBasicClusters'),
-    endcapClusterCollection = cms.string('multi5x5EndcapBasicClusters'),
-    clustershapecollectionEE = cms.string('multi5x5EndcapShape'),
-    clustershapecollectionEB = cms.string('multi5x5BarrelShape'),
-    barrelShapeAssociation = cms.string('multi5x5BarrelShapeAssoc'),
-    endcapShapeAssociation = cms.string('multi5x5EndcapShapeAssoc'),
-    )
-
-
-# super clustering for the ECAL BARREL, staring from multi5x5 3x3 clusters
-process.multi5x5SuperClustersTimePi0Barrel =  RecoEcal.EgammaClusterProducers.multi5x5SuperClusters_cfi.multi5x5SuperClusters.clone(
-    doBarrel = cms.bool(True),
-    doEndcaps = cms.bool(False),
-    barrelClusterProducer = cms.string('multi5x5BasicClustersTimePi0Barrel'),
-    barrelClusterCollection = cms.string('multi5x5BarrelBasicClusters'),
-    endcapClusterProducer = cms.string('multi5x5BasicClustersTimePi0Endcap'),
-    endcapClusterCollection = cms.string('multi5x5EndcapBasicClusters')
- )
-
-
-# super clustering for the ECAL ENDCAP, staring from multi5x5 3x3 clusters
-process.multi5x5SuperClustersTimePi0Endcap =  RecoEcal.EgammaClusterProducers.multi5x5SuperClusters_cfi.multi5x5SuperClusters.clone(
-    doBarrel = cms.bool(False),
-    doEndcaps = cms.bool(True),
-    barrelClusterProducer = cms.string('multi5x5BasicClustersTimePi0Barrel'),
-    barrelClusterCollection = cms.string('multi5x5BarrelBasicClusters'),
-    endcapClusterProducer = cms.string('multi5x5BasicClustersTimePi0Endcap'),
-    endcapClusterCollection = cms.string('multi5x5EndcapBasicClusters')
- )
-
+#
+###
+process.patElectrons.isoDeposits = cms.PSet()
+#
+process.patElectrons.addElectronID = cms.bool(True)
+process.patElectrons.electronIDSources = cms.PSet(
+        simpleEleId95relIso= cms.InputTag("simpleEleId95relIso"),
+            simpleEleId90relIso= cms.InputTag("simpleEleId90relIso"),
+            simpleEleId85relIso= cms.InputTag("simpleEleId85relIso"),
+            simpleEleId80relIso= cms.InputTag("simpleEleId80relIso"),
+            simpleEleId70relIso= cms.InputTag("simpleEleId70relIso"),
+            simpleEleId60relIso= cms.InputTag("simpleEleId60relIso"),
+            simpleEleId95cIso= cms.InputTag("simpleEleId95cIso"),
+            simpleEleId90cIso= cms.InputTag("simpleEleId90cIso"),
+            simpleEleId85cIso= cms.InputTag("simpleEleId85cIso"),
+            simpleEleId80cIso= cms.InputTag("simpleEleId80cIso"),
+            simpleEleId70cIso= cms.InputTag("simpleEleId70cIso"),
+            simpleEleId60cIso= cms.InputTag("simpleEleId60cIso"),
+            )
+###
+process.load("ElectroWeakAnalysis.WENu.simpleEleIdSequence_cff")
+process.patElectronIDs = cms.Sequence(process.simpleEleIdSequence)
+process.makePatElectrons = cms.Sequence(process.patElectronIDs *
+                                        process.patElectrons)
+process.makePatCandidates = cms.Sequence( process.makePatElectrons   )
+process.patMyDefaultSequence = cms.Sequence(process.makePatCandidates)
 
 
 
@@ -113,56 +104,36 @@ process.load("CalibCalorimetry.EcalTiming.ecalTimeEleTree_cfi")
 process.ecalTimeEleTree.fileName = 'EcalTimeTree'
 process.ecalTimeEleTree.muonCollection = cms.InputTag("muons")
 process.ecalTimeEleTree.runNum = 999999
-# gfworks: replathese names
-process.ecalTimeEleTree.barrelSuperClusterCollection = cms.InputTag("multi5x5SuperClustersTimePi0Barrel","multi5x5BarrelSuperClusters")
-process.ecalTimeEleTree.endcapSuperClusterCollection = cms.InputTag("multi5x5SuperClustersTimePi0Endcap","multi5x5EndcapSuperClusters")
-process.ecalTimeEleTree.barrelBasicClusterCollection = cms.InputTag("multi5x5BasicClustersTimePi0Barrel","multi5x5BarrelBasicClusters")
-process.ecalTimeEleTree.endcapBasicClusterCollection = cms.InputTag("multi5x5BasicClustersTimePi0Endcap","multi5x5EndcapBasicClusters")
-process.ecalTimeEleTree.barrelClusterShapeAssociationCollection = cms.InputTag("multi5x5BasicClustersTimePi0Barrel","multi5x5BarrelShapeAssoc")
-process.ecalTimeEleTree.endcapClusterShapeAssociationCollection = cms.InputTag("multi5x5BasicClustersTimePi0Endcap","multi5x5EndcapShapeAssoc") 
-
-
-
+#process.ecalTimeTree.endcapSuperClusterCollection = cms.InputTag("correctedMulti5x5SuperClustersWithPreshower","")
 
 
 
 process.dumpEvContent = cms.EDAnalyzer("EventContentAnalyzer")
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10000))
 
 process.p = cms.Path(
+    process.patMyDefaultSequence *
     # process.dumpEvContent  *
     process.ecalTimeEleTree
     )
 
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 250
 
-
-process.MessageLogger = cms.Service("MessageLogger",
-    cout = cms.untracked.PSet(
-        threshold = cms.untracked.string('WARNING')
-    ),
-    categories = cms.untracked.vstring('ecalTimeEleTree'),
-    destinations = cms.untracked.vstring('cout')
-)
-
-
-
-
-# GF: some legacy reco files to test; replace w/ collision data
 # dbs search --query "find file where dataset=/ExpressPhysics/BeamCommissioning09-Express-v2/FEVT and run=124020" | grep store | awk '{printf "\"%s\",\n", $1}'
 process.source = cms.Source(
     "PoolSource",
     skipEvents = cms.untracked.uint32(0),
 
-    # a few files from:    /MinimumBias/Commissioning10-GR_R_35X_V7A_SD_EG-v2/RECO
     fileNames = (cms.untracked.vstring(
     #'/store/data/Commissioning10/MinimumBias/RAW-RECO/v9/000/135/494/A4C5C9FA-C462-DF11-BC35-003048D45F7A.root',
     #'/store/relval/CMSSW_4_2_0_pre8/EG/RECO/GR_R_42_V7_RelVal_wzEG2010A-v1/0043/069662C9-9A56-E011-9741-0018F3D096D2.root'
     #'/store/data/Run2010A/EG/RECO/v4/000/144/114/EEC21BFA-25B4-DF11-840A-001617DBD5AC.root'
 
-    'file:/data/franzoni/data/CMSSW_4_2_0_pre8__EG__RECO__GR_R_42_V7_RelVal_wzEG2010A-v1__0043__069662C9-9A56-E011-9741-0018F3D096D2.root'
-     )
-                  )
+    'file:/data/franzoni/data/Run2011A_DoubleElectron_AOD_PromptReco-v4_000_166_946_CE9FBCFF-4B98-E011-A6C3-003048F11C58.root'
+    )
+                 )
     )
 
 
