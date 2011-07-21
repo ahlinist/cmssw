2234231916b86b94300cdd4fa072924d5946fee2
veverka <>
2011-07-21 01:21:59 +0000

    05Jul2011ReReco and Summer11 S4 MC

--- ElectroWeakAnalysis/MultiBosons/scripts/makeMulticrabConfig.py
+++ ElectroWeakAnalysis/MultiBosons/scripts/makeMulticrabConfig.py
@@ -1,66 +1,92 @@
 #/usr/bin/env python
 
-scheduler = 'glite'
-#scheduler = 'condor'
+#scheduler = 'glite'
+scheduler = 'condor'
 
 samples = {
-    "Dimuon_AOD_data": """
-        /Mu/Run2010A-Apr21ReReco-v1/AOD
-        /Mu/Run2010B-Apr21ReReco-v1/AOD
-        /SingleMu/Run2011A-May10ReReco-v1/AOD
-        /DoubleMu/Run2011A-May10ReReco-v1/AOD
+    "Dimuon": """
+        /DoubleMu/Run2011A-ZMu-05Jul2011ReReco-ECAL-v1/RAW-RECO
+        /DoubleMu/Run2011A-05Jul2011ReReco-ECAL-v1/RECO
+        /DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM         
+        /DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Summer11-PU_S4_START42_V11-v1/AODSIM        
+        /TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v2/GEN-SIM-RECO            
+        /TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v2/AODSIM                  
+        /WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/GEN-SIM-RECO        
+        /WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM              
+        /QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/GEN-SIM-RECO 
+        /QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM       
         """.split(),
-    "Dimuon_RECO_data": """
-        /Mu/Run2010A-Apr21ReReco-v1/RECO
-        /Mu/Run2010B-Apr21ReReco-v1/RECO
-        /Mu/Run2010A-ZMu-Apr21ReReco-v1/RAW-RECO
-        /Mu/Run2010B-ZMu-Apr21ReReco-v1/RAW-RECO
-        /SingleMu/Run2011A-ZMu-May10ReReco-v1/RAW-RECO
+    "Diphoton": """
+        /DoubleElectron/Run2011A-ZElectron-05Jul2011ReReco-ECAL-v1/RAW-RECO
+        /DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM
+        /TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v2/GEN-SIM-RECO    
+        /TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v2/AODSIM          
+        /WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/GEN-SIM-RECO
+        /WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM      
+        /DYToEE_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Summer11-PU_S4_START42_V11-v1/AODSIM  
         """.split(),
-    "Inclusive_AOD": """
-        /VBF_HToGG_M-120_7TeV-powheg-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM
-        /GluGluToHToGG_M-120_7TeV-powheg-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM
-        /G_Pt-15to3000_TuneZ2_Flat_7TeV_pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM
-        """.split(),
-    "Inclusive_RECO": """
-        /RelValSingleGammaPt10/CMSSW_4_2_2-MC_42_V11-v1/GEN-SIM-RECO
-        /RelValSingleGammaPt35/CMSSW_4_2_2-MC_42_V11-v1/GEN-SIM-RECO
-        """.split(),
-    "Dimuon_AOD": """
-        /DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM
-        /DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v1/AODSIM
-        /DYToMuMu_M-20_Tune4C_7TeV-pythia8/Summer11-PU_S3_START42_V11-v2/AODSIM
-        /WToMuNu_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v1/AODSIM
-        /WToMuNu_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM
-        """.split(),
-    "Dimuon_RECO": """
-        /DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/GEN-SIM-RECO
-        /DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v1/GEN-SIM-RECO
-        /DYToMuMu_M-20_Tune4C_7TeV-pythia8/Summer11-PU_S3_START42_V11-v2/GEN-SIM-RECO
-        /WToMuNu_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v1/GEN-SIM-RECO
-        /WToMuNu_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/GEN-SIM-RECO
+    "Inclusive": """
+        /G_Pt-80to120_TuneZ2_7TeV_pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM
+        /G_Pt-170to300_TuneZ2_7TeV_pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM
+        /G_Pt-15to3000_TuneZ2_Flat_7TeV_pythia6/Summer11-HCal_PU_S4_START42_V11-v1/AODSIM
         """.split(),
     }
 
+tierAndSourceMap = {
+    'AOD'         : 'AOD_data',
+    'RECO'        : 'RECO_data',
+    'RAW-RECO'    : 'RECO_data',
+    'AODSIM'      : 'AOD',
+    'GEN-SIM-RECO': 'RECO',
+}
+
+class SampleData:
+    pass
+
+sampleData = {}
+
 for label, datasets in samples.items():
     for dataset in datasets:
-        primaryName, processedName, tier = dataset.split("/")[1:]
-        print """
-###############################################################################
-[%s_%s]
-CMSSW.datasetpath = %s
-CMSSW.pycfg_params = options_load=options_%s.cfg
-USER.additional_input_files = options_%s.cfg
-USER.publish_data_name = %s_%s_%s-42X-v4""" % (
-            primaryName, label.replace("_data", ""),
-            dataset,
-            label,
-            label,
-            processedName, scheduler, label.replace("_data", ""),
+        data = SampleData()
+        data.label   = label
+        data.shortLabel = data.label.replace('_data', '')
+        data.dataset = dataset
+        data.primaryName, \
+            data.processedName, \
+            data.tier =   dataset.split("/")[1:]
+        data.label += "_" + tierAndSourceMap[data.tier]
+        data.isRealData = label[-4:] == "data"
+
+        name = '%s_%s_%s' % ( data.shortLabel,
+                              data.primaryName,
+                              data.processedName, )
+        if name in sampleData.keys():
+            print "WARNING! %s and %s share the same name `%s'" % (
+                sampleData[name].dataset,
+                dataset,
+                name,
             )
-        if label[-4:] == "data":
-            print """CMSSW.total_number_of_lumis = -1
-CMSSW.lumis_per_job = 100"""
         else:
-            print """CMSSW.total_number_of_events = -1
+            sampleData[name] = data
+
+for name, data in sampleData.items():
+    print """
+###############################################################################
+[{NAME}]
+CMSSW.datasetpath = {DATASET}
+CMSSW.pycfg_params = options_load=options_{LABEL}.cfg
+USER.additional_input_files = options_{LABEL}.cfg
+USER.publish_data_name = {PROCESSED_NAME}_{SCHEDULER}_{SHORT_LABEL}-42X-v4"""\
+    .format( NAME = name,
+             DATASET = data.dataset,
+             LABEL = data.label,
+             PROCESSED_NAME = data.processedName,
+             SCHEDULER = scheduler,
+             SHORT_LABEL = data.shortLabel )
+    if data.isRealData:
+        print """CMSSW.total_number_of_lumis = -1
+CMSSW.lumis_per_job = 100"""
+    else:
+        print """CMSSW.total_number_of_events = -1
 CMSSW.number_of_jobs = 100"""
+
