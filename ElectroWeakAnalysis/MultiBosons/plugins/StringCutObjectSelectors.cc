4c778cb93953db0a2c4e2e26f8cc8af8507af30e
veverka <>
2011-07-21 01:18:48 +0000

    Added a photon selectors needed for the Diphoton skim

--- ElectroWeakAnalysis/MultiBosons/plugins/StringCutObjectSelectors.cc
+++ ElectroWeakAnalysis/MultiBosons/plugins/StringCutObjectSelectors.cc
@@ -36,15 +36,15 @@
 // DEFINE_VIEW_PTR_SELECTOR( reco::GsfElectron, GsfElectronViewPtrSelector );
 // DEFINE_VIEW_REF_SELECTOR( reco::GsfElectron, GsfElectronViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( reco::GsfElectron, GsfElectronViewSelector    );
-// 
+//
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( reco::GsfElectron, LazyGsfElectronViewPtrSelector );
 // DEFINE_LAZY_VIEW_REF_SELECTOR( reco::GsfElectron, LazyGsfElectronViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( reco::GsfElectron, LazyGsfElectronViewSelector    );
-// 
+//
 // DEFINE_VIEW_PTR_SELECTOR( pat::Electron, PatElectronViewPtrSelector );
 // DEFINE_VIEW_REF_SELECTOR( pat::Electron, PatElectronViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( pat::Electron, PatElectronViewSelector    );
-// 
+//
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( pat::Electron, LazyPatElectronViewPtrSelector );
 // DEFINE_LAZY_VIEW_REF_SELECTOR( pat::Electron, LazyPatElectronViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( pat::Electron, LazyPatElectronViewSelector    );
@@ -52,17 +52,17 @@
 //-----------------------------------------------------------------------------
 /// Define muon selectors
 // DEFINE_VIEW_PTR_SELECTOR( reco::Muon, MuonViewPtrSelector );
-// DEFINE_VIEW_REF_SELECTOR( reco::Muon, MuonViewRefSelector );
+DEFINE_VIEW_REF_SELECTOR( reco::Muon, MuonViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( reco::Muon, MuonViewSelector    );
-// 
+//
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( reco::Muon, LazyMuonViewPtrSelector );
-// DEFINE_LAZY_VIEW_REF_SELECTOR( reco::Muon, LazyMuonViewRefSelector );
+DEFINE_LAZY_VIEW_REF_SELECTOR( reco::Muon, LazyMuonViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( reco::Muon, LazyMuonViewSelector    );
-// 
+//
 // DEFINE_VIEW_PTR_SELECTOR( pat::Muon, PatMuonViewPtrSelector );
 // DEFINE_VIEW_REF_SELECTOR( pat::Muon, PatMuonViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( pat::Muon, PatMuonViewSelector    );
-// 
+//
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( pat::Muon, LazyPatMuonViewPtrSelector );
 // DEFINE_LAZY_VIEW_REF_SELECTOR( pat::Muon, LazyPatMuonViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( pat::Muon, LazyPatMuonViewSelector    );
@@ -70,17 +70,17 @@
 //-----------------------------------------------------------------------------
 /// Define photon selectors
 // DEFINE_VIEW_PTR_SELECTOR( reco::Photon, PhotonViewPtrSelector );
-// DEFINE_VIEW_REF_SELECTOR( reco::Photon, PhotonViewRefSelector );
+DEFINE_VIEW_REF_SELECTOR( reco::Photon, PhotonViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( reco::Photon, PhotonViewSelector    );
 
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( reco::Photon, LazyPhotonViewPtrSelector );
-// DEFINE_LAZY_VIEW_REF_SELECTOR( reco::Photon, LazyPhotonViewRefSelector );
+DEFINE_LAZY_VIEW_REF_SELECTOR( reco::Photon, LazyPhotonViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( reco::Photon, LazyPhotonViewSelector    );
-// 
+//
 // DEFINE_VIEW_PTR_SELECTOR( pat::Photon, PatPhotonViewPtrSelector );
 // DEFINE_VIEW_REF_SELECTOR( pat::Photon, PatPhotonViewRefSelector );
 // DEFINE_VIEW_SELECTOR    ( pat::Photon, PatPhotonViewSelector    );
-// 
+//
 // DEFINE_LAZY_VIEW_PTR_SELECTOR( pat::Photon, LazyPatPhotonViewPtrSelector );
 // DEFINE_LAZY_VIEW_REF_SELECTOR( pat::Photon, LazyPatPhotonViewRefSelector );
 // DEFINE_LAZY_VIEW_SELECTOR    ( pat::Photon, LazyPatPhotonViewSelector    );
