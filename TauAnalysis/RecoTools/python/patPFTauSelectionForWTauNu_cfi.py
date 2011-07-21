9e77eb277284c7919a539f2025c7ed6c2ca38968
liis <>
2011-07-21 14:20:46 +0000

    require tau to come from the highest pt vertex

--- TauAnalysis/RecoTools/python/patPFTauSelectionForWTauNu_cfi.py
+++ TauAnalysis/RecoTools/python/patPFTauSelectionForWTauNu_cfi.py
@@ -5,6 +5,11 @@ from TauAnalysis.RecoTools.patPFTauSelection_cfi import *
 
 selectedPatTausForWTauNuEta21 = copy.deepcopy(selectedPatTausEta23)
 selectedPatTausForWTauNuPt20 = copy.deepcopy(selectedPatTausPt20)
+selectedPatTausForWTauNuTrkMatchVertex = cms.EDFilter("PATTauDzSelector",
+                                                      vertexSource = cms.InputTag('selectedPrimaryVertexHighestPtTrackSum'),
+                                                      dzMax = cms.double(0.2),
+                                                      filter = cms.bool(False)
+                                                      )
 selectedPatTausForWTauNuLeadTrk = copy.deepcopy(selectedPatTausLeadTrk)
 selectedPatTausForWTauNuLeadTrkPt = copy.deepcopy(selectedPatTausLeadTrkPt)
 selectedPatTausForWTauNuIso = copy.deepcopy(selectedPatTausTaNCdiscr)
