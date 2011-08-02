6dda6b4d0ff798ef3814031e0fefdb670847573a
liis <>
2011-08-02 12:13:49 +0000

    add jec

--- TauAnalysis/Configuration/test/runWtoTauNu_cfg.py
+++ TauAnalysis/Configuration/test/runWtoTauNu_cfg.py
@@ -1,18 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 import copy
 
-process = cms.Process('runWtoTauNu')
+process = cms.Process('runWtoTauNu1')
 
 process.load('Configuration/StandardSequences/Services_cff')
 process.load('FWCore/MessageService/MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 #load geometry
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
 process.load('Configuration/StandardSequences/MagneticField_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = cms.string('START39_V8::All')
+#process.GlobalTag.globaltag = cms.string('START42_V12::All')
+process.GlobalTag.globaltag = cms.string('GR_R_42_V18::All')
 
 # import particle data table, needed for print-out of generator level information
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -47,8 +48,6 @@ from TauAnalysis.Configuration.recoSampleDefinitionsWtoTauNu_cfi import *
 #process.add_( cms.Service("PrintLoadingPlugins") )
 #---------------------------------------------------------------------------------
 
-process.patTrigger.processName = 'REDIGI39X'
-process.patTriggerEvent.processName = "REDIGI39X"
 
 process.DQMStore = cms.Service("DQMStore")
 
@@ -57,20 +56,27 @@ process.saveWtoTauNuPlots = cms.EDAnalyzer("DQMSimpleFileSaver",
 )
 
 process.maxEvents = cms.untracked.PSet(            
-    input = cms.untracked.int32(-1)    
+    input = cms.untracked.int32(5000)    
 )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-#       'file:final_events_WtoTauNu_data_BTau_Run2010B_Nov4ReReco_RunPileup_127_1_nPN.root'
-#       'rfio:/castor/cern.ch/user/l/liis/CMSSW_38X/Histograms/Pileup/run6/final_events_WtoTauNu_Wtaunu_RunPileup_168_1_AXy.root'
-       ##       'rfio:/castor/cern.ch/user/l/liis/CMSSW_39X/PreselEvents/final_events_WtoTauNu_Wtaunu_Run02_144_1_oCQ.root'
-#       'rfio:/castor/cern.ch/user/l/liis/CMSSW_39X/TestAOD/aodSkim_1_1_LYl.root'
-       'rfio:/castor/cern.ch/user/l/liis/CMSSW_39X/TestAOD/aodSkim_Wtaunu_1_1_oVY.root'
-#       'rfio:/castor/cern.ch/user/l/liis/CMSSW_39X/TestAOD/aodSkim_Wtaunu_4_1_pLp.root'
+                            fileNames = cms.untracked.vstring(
+       #'file:data.root'
+       'file:/tmp/liis/RunData42Xv4/skim_WtoTauNu_data_RunData42Xv4.root'
        )
-    #skipBadFiles = cms.untracked.bool(True)
-)
+                            #skipBadFiles = cms.untracked.bool(True)
+                            )
+
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+#process.source = cms.Source("PoolSource",
+#                            fileNames = cms.untracked.vstring(
+#       pickRelValInputFiles( cmsswVersion  = 'CMSSW_4_2_0_pre8'
+#                             , relVal        = 'RelValTTbar'
+#                             , globalTag     = 'START42_V7'
+#                             , numberOfFiles = 1
+#                             )
+#       )
+#                            )
 
 #--------------------------------------------------------------------------------
 # define "hooks" for replacing configuration parameters
@@ -85,10 +91,8 @@ process.source = cms.Source("PoolSource",
 #--------------------------------------------------------------------------------
 #import utility function for switching pat::Tau input
 from PhysicsTools.PatAlgos.tools.tauTools import * 
-#switchToCaloTau(process)
-#switchToPFTauShrinkingCone(process)
-#switchToPFTauFixedCone(process)
-switchToPFTauHPSpTaNC(process)
+#switchToPFTauHPSpTaNC(process)
+switchToPFTauHPS(process)
 
 # disable preselection of pat::Taus
 # (disabled also in TauAnalysis/RecoTools/python/patPFTauConfig_cfi.py ,
@@ -98,6 +102,35 @@ process.cleanPatTaus.preselection = cms.string('')
 # import utility function for managing pat::Jets
 from PhysicsTools.PatAlgos.tools.jetTools import *
 
+isMC = "False"
+jec = [ 'L1Offset', 'L2Relative', 'L3Absolute']
+if not isMC:
+    jec.extend([ 'L2L3Residual' ])
+
+addJetCollection(process, cms.InputTag('ak5PFJets'),
+                                  'AK5', 'PF',
+                                  doJTA            = False,
+                                  doBTagging       = False,
+                                  jetCorrLabel     = ('AK5PF', cms.vstring(jec)),
+                                  doType1MET       = False,
+                                  genJetCollection = cms.InputTag("ak5GenJets"),
+                                  doJetID          = True,
+                                  jetIdLabel       = "ak5",
+                                  outputModule     = ''
+                                  )
+
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+
+# Turn-on the FastJet density calculation -----------------------
+#### FastJet corrections
+from RecoJets.JetProducers.kt4PFJets_cfi import *
+import RecoJets.JetProducers.kt4PFJets_cfi
+process.kt6PFJets = RecoJets.JetProducers.kt4PFJets_cfi.kt4PFJets.clone()
+process.kt6PFJets.rParam = cms.double(0.6)
+process.kt6PFJets.doRhoFastjet = cms.bool(True)
+process.kt6PFJets.Rho_EtaMax = cms.double(2.5)
+
 # uncomment to replace caloJets by pfJets
 switchJetCollection(process, jetCollection = cms.InputTag("ak5PFJets"), outputModule = '')
 #-----------------------------------------------------------------------------
@@ -105,7 +138,7 @@ switchJetCollection(process, jetCollection = cms.InputTag("ak5PFJets"), outputMo
 from TauAnalysis.Configuration.tools.metTools import *
 
 #use raw calo-MET as input
-process.patMETs.metSource = cms.InputTag('met')
+###process.patMETs.metSource = cms.InputTag('pfMET')
 
 # uncomment to add pfMET, set Boolean swich to true in order to apply type-1 corrections
 addPFMet(process, correct = False)
@@ -124,12 +157,12 @@ from TauAnalysis.Configuration.tools.changeCut import changeCut
 changeCut(process,"selectedPatTausForWTauNuEta21","abs(eta) < 2.3")
 changeCut(process,"selectedPatTausForWTauNuPt20","pt() > 30")
 changeCut(process,"selectedPatTausForWTauNuLeadTrk","tauID('decayModeFinding') > 0.5")
-changeCut(process,"selectedPatTausForWTauNuLeadTrkPt","leadPFChargedHadrCand().isNonnull() & leadPFChargedHadrCand().pt() > 15.")
 
+changeCut(process,"selectedPatTausForWTauNuLeadTrkPt","leadPFChargedHadrCand().isNonnull() & leadPFChargedHadrCand().pt() > 15.")
 changeCut(process,"selectedPatTausForWTauNuElectronVeto","tauID('againstElectronTight') > 0.5")
 changeCut(process,"selectedPatTausForWTauNuMuonVeto","tauID('againstMuonTight') > 0.5")
 changeCut(process,"selectedPatTausForWTauNuEmFraction","emFraction < 0.9")
-changeCut(process,"selectedPatTausForWTauNuIso","tauID('byHPSmedium') > 0.5")
+changeCut(process,"selectedPatTausForWTauNuIso","tauID('byMediumIsolation') > 0.5")
 
 #electron selection
 changeCut(process,"selectedPatElectronsTightId","electronID('eidLoose') > 0.5")
@@ -156,14 +189,27 @@ process.filterFinalEvents = cms.EDFilter("BoolEventFilter",
 
 process.o = cms.Path(process.filterFirstEvent + process.printEventContent)
 
+process.addJetCorr = cms.Sequence(
+        process.kt6PFJets
+        +process.patJetCorrFactorsAK5PF
+            +process.patJetPartonMatchAK5PF
+            +process.patJetGenJetMatchAK5PF
+            +process.patJetPartons
+             +process.patJetPartonAssociationAK5PF
+             +process.patJetFlavourAssociationAK5PF
+             +process.patJetsAK5PF
+ #       
+        )
+
 process.p = cms.Path( 
-    process.producePatTupleWtoTauNuSpecific
+    process.addJetCorr
+    +process.producePatTupleWtoTauNuSpecific
 #    +process.printGenParticleList # print-out of generator level particles
 #    +process.printEventContent    # dump of event content after PAT-tuple production
     +process.selectWtoTauNuEvents
     +process.analyzeWtoTauNuEvents
     +process.saveWtoTauNuPlots 
-#    +process.filterFinalEvents
+    +process.filterFinalEvents
 )
 
 
@@ -187,23 +233,23 @@ from TauAnalysis.Configuration.tools.factorizationTools import enableFactorizati
 #__#factorization#
 #--------------------------------------------------------------------------------
 # import utility function for estimation of systematic uncertainties
-from TauAnalysis.Configuration.tools.sysUncertaintyTools import enableSysUncertainties_runWtoTauNu
+from TauAnalysis.Configuration.tools.sysUncertaintyTools import disableSysUncertainties_runWtoTauNu
 #
 # define "hook" for keeping enabled/disabling estimation of systematic uncertainties
 # in case running jobs on the CERN batch system
 # (needs to be done after process.p has been defined)
 #__#systematics#
-if not hasattr(process, "isBatchMode"):
-       enableSysUncertainties_runWtoTauNu(process)
+#if not hasattr(process, "isBatchMode"):
+#       disableSysUncertainties_runWtoTauNu(process)
 
-#--------------------------------------------------------------------------------
-# disable event-dump output in order to reduce size of log-files
-#if hasattr(process, "disableEventDump"):
-#    process.analyzeWtoTauNuEvents.eventDumps = cms.VPSet()
 #--------------------------------------------------------------------------------
 # disable accessing generator level information if running on data
 from TauAnalysis.Configuration.tools.switchToData import switchToData
-#switchToData(process)
+switchToData(process)
+#--------------------------------------------------------------------------------    
+# disable event-dump output in order to reduce size of log-files
+if hasattr(process, "disableEventDump"):
+    process.analyzeWtoTauNuEvents.eventDumps = cms.VPSet()
 #--------------------------------------------------------------------------------
 process.producePatTupleAll = cms.Sequence(process.producePatTuple + process.producePatTupleWtoTauNuSpecific)
 
@@ -215,7 +261,7 @@ if not hasattr(process, "isBatchMode"):
     process.p.replace(process.producePatTupleWtoTauNuSpecific, process.producePatTuple+process.producePatTupleWtoTauNuSpecific)
 #--------------------------------------------------------------------------------
 ##replace reco->aod.
-from TauAnalysis.Configuration.tools.aodTools import *
+from TauAnalysis.Configuration.tools.switchToAOD import *
 switchToAOD(process)
 ##--------------------------------------------------------------------------------  
 
