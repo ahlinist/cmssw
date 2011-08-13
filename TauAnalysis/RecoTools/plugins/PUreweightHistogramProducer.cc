94e56f349d6f7259106c9ea137e712ad206aff4e
veelken <>
2011-08-13 07:47:49 +0000

    first check-in of EDAnalyzer module for creating histograms for simultaneous reweighting of in-time & out-out-time pile-up

---
+++ TauAnalysis/RecoTools/plugins/PUreweightHistogramProducer.cc
@@ -0,0 +1,67 @@
+#include "TauAnalysis/RecoTools/plugins/PUreweightHistogramProducer.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+PUreweightHistogramProducer::PUreweightHistogramProducer(const edm::ParameterSet& cfg)
+  : moduleLabel_(cfg.getParameter<std::string>("@module_label"))
+{
+  srcVertices_ = cfg.getParameter<edm::InputTag>("srcVertices");
+  srcPFNeutralRho_ = cfg.getParameter<edm::InputTag>("srcPFNeutralRho");
+  
+  if ( cfg.exists("srcWeight") ) srcWeight_ = cfg.getParameter<edm::InputTag>("srcWeight");
+}
+
+void PUreweightHistogramProducer::beginJob()
+{
+  if ( !edm::Service<DQMStore>().isAvailable() ) 
+    throw cms::Exception("PUreweightHistogramProducer::beginJob")
+      << "Failed to access dqmStore !!\n";
+
+  DQMStore& dqmStore = (*edm::Service<DQMStore>());
+  dqmStore.setCurrentFolder(moduleLabel_);
+
+  meVtxMultiplicity_ = 
+    dqmStore.book1D("VtxMultiplicity", "VtxMultiplicity", 20, -0.5, +19.5);
+  meVtxMultiplicity_->getTH1()->Sumw2();
+  mePFNeutralRho_ = 
+    dqmStore.book1D("PFNeutralRho", "PFNeutralRho", 50, 0., 20.);
+  mePFNeutralRho_->getTH1()->Sumw2();
+
+  mePFNeutralRhoVsVtxMultiplicity_ = 
+    dqmStore.book2D("PFNeutralRhoVsVtxMultiplicity", "PFNeutralRhoVsVtxMultiplicity", 20, -0.5, +19.5, 50, 0., 20.);
+  mePFNeutralRhoVsVtxMultiplicity_->getTH1()->Sumw2();
+}
+
+void PUreweightHistogramProducer::analyze(const edm::Event& evt, const edm::EventSetup& es)
+{
+  edm::Handle<reco::VertexCollection> vertices;
+  evt.getByLabel(srcVertices_, vertices);
+  size_t vtxMultiplicity = vertices->size();
+  
+  edm::Handle<double> pfNeutralRho_handle;
+  evt.getByLabel(srcPFNeutralRho_, pfNeutralRho_handle);
+  double pfNeutralRho = (*pfNeutralRho_handle);
+
+  double evtWeight = 1.0;
+  if ( srcWeight_.label() != "" ) {
+    edm::Handle<double> evtWeight_handle;
+    evt.getByLabel(srcWeight_, evtWeight_handle);
+    evtWeight = (*evtWeight_handle);
+  }
+
+  meVtxMultiplicity_->Fill(vtxMultiplicity, evtWeight);
+  mePFNeutralRho_->Fill(pfNeutralRho, evtWeight);
+
+  mePFNeutralRhoVsVtxMultiplicity_->Fill(vtxMultiplicity, pfNeutralRho, evtWeight);
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(PUreweightHistogramProducer);
+ 
---
+++ TauAnalysis/RecoTools/plugins/PUreweightHistogramProducer.h
@@ -0,0 +1,56 @@
+#ifndef TauAnalysis_Core_HistManagerBase_h
+#define TauAnalysis_Core_HistManagerBase_h
+
+/** \class PUreweightHistogramProducer
+ *
+ * Produce histogram for simultaneous reweighting of Monte Carlo simulated events
+ * to match in-time and out-of-time pile-up in Data
+ * ( cf. Alexei Raspereza's presentation in Higgs --> tau tau meeting on May 26th 2011:
+ *      https://indico.cern.ch/getFile.py/access?contribId=46&sessionId=0&resId=0&materialId=slides&confId=140191 )
+ * 
+ * \author Christian Veelken, UC Davis
+ *
+ * \version $Revision: 1.10 $
+ *
+ * $Id: HistManagerBase.h,v 1.10 2011/03/09 18:25:19 veelken Exp $
+ *
+ */
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+class PUreweightHistogramProducer : public edm::EDAnalyzer
+{
+ public:
+  // constructor 
+  explicit PUreweightHistogramProducer(const edm::ParameterSet&);
+  
+  // destructor
+  virtual ~PUreweightHistogramProducer() {}
+
+  void beginJob();
+  void analyze(const edm::Event&, const edm::EventSetup&);
+  void endJob() {}
+
+ private:
+
+  std::string moduleLabel_;
+
+  edm::InputTag srcVertices_;
+  edm::InputTag srcPFNeutralRho_;
+
+  edm::InputTag srcWeight_;
+
+  MonitorElement* meVtxMultiplicity_;
+  MonitorElement* mePFNeutralRho_;
+
+  MonitorElement* mePFNeutralRhoVsVtxMultiplicity_;
+};
+
+#endif  
+
