a6a56b947f8ae4847b9752faae9dd9fa57aef5e7
mkortela <>
2011-08-15 06:46:32 +0000

    Added muon isolation variables and muon-tau pair invariant mass to the ntuple.

--- ElectroWeakAnalysis/TauTriggerEfficiency/src/MuonAnalyzer.cc
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/MuonAnalyzer.cc
@@ -21,6 +21,7 @@ MuonAnalyzer::~MuonAnalyzer(){}
 void MuonAnalyzer::Setup(const edm::ParameterSet& iConfig,TTree *trigtree)
 {
 	MuonSource = iConfig.getParameter<edm::InputTag>("MuonSource");
+        muTauPairSource = iConfig.getParameter<edm::InputTag>("MuonTauPairSource");
 	ptMin      = iConfig.getParameter<double>("MuonPtMin");
 	etaMax     = iConfig.getParameter<double>("MuonEtaMax");
 
@@ -30,7 +31,13 @@ void MuonAnalyzer::Setup(const edm::ParameterSet& iConfig,TTree *trigtree)
   	muontree->Branch("MuonPt", &muonPt);
 	muontree->Branch("MuonEta",&muonEta);
 	muontree->Branch("MuonPhi",&muonPhi);
+        muontree->Branch("MuonIso03SumPt", &muonIso03SumPt);
+        muontree->Branch("MuonIso03EmEt", &muonIso03EmEt);
+        muontree->Branch("MuonIso03HadEt", &muonIso03HadEt);
 	muontree->Branch("NMuons",&nMuons, "NMuons/I");
+
+        muontree->Branch("MuonTauInvMass", &muTauInvMass);
+;
 }
 
 void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
@@ -39,6 +46,8 @@ void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	muonEta = -999;
 	muonPhi = -999;
 	nMuons  = 0;
+
+        muTauInvMass = 0;
 
 //
 	edm::Handle<reco::MuonCollection> muons;
@@ -53,9 +62,18 @@ void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 		  muonPt  = muons->at(i).pt();
 		  muonEta = muons->at(i).eta();
 		  muonPhi = muons->at(i).phi();
+                  muonIso03SumPt = muons->at(i).isolationR03().sumPt;
+                  muonIso03EmEt = muons->at(i).isolationR03().emEt;
+                  muonIso03HadEt = muons->at(i).isolationR03().hadEt;
 		}
 		nMuons++;
 	  }
 	}
-} 
+
+        edm::Handle<edm::View<reco::Candidate> > hcands;
+        iEvent.getByLabel(muTauPairSource, hcands);
+        if(hcands.isValid() && !hcands->empty()) {
+          muTauInvMass = hcands->at(0).mass();
+        }
+}
 
