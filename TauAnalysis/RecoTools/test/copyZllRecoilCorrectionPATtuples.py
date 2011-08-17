8a99f61404162d7fbb4e679c268d089c0ff0dc2b
veelken <>
2011-08-17 13:58:06 +0000

    first check-in of macro for determining 2d histogram for numRecoVertices vs. rhoNeutral pile-up reweighting, of python script to copy PAT-tuples for determining Z-recoil corrections and of module to produce collections of MET/diTaus with Z-recoil corrections applied

---
+++ TauAnalysis/RecoTools/test/copyZllRecoilCorrectionPATtuples.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import TauAnalysis.Configuration.tools.castor as castor
+import TauAnalysis.TauIdEfficiency.tools.castor_mirror2 as castor_mirror
+
+import subprocess
+import shlex
+
+version = "v1"
+
+# Get all the skim files from the castor directory
+sourceFilePath = "/castor/cern.ch/user/v/veelken/CMSSW_4_2_x/PATtuples/ZllRecoilCorrection/"
+
+source_files = [ file_info['path'] for file_info in castor.nslsl(sourceFilePath) ]
+
+targetFilePath = "/data2/veelken/CMSSW_4_2_x/PATtuples/ZllRecoilCorrection/%s/" % version
+
+samplesToCopy = [
+    # modify in case you want to submit jobs for some of the samples only...
+]
+
+files_to_copy = []
+
+for source_file in source_files:
+
+    if not (source_file.find("ZllRecoilCorrectionPATtuple") != -1 and source_file.find("%s" % version) != -1):
+	continue
+
+    isSampleToCopy = False
+    if len(samplesToCopy) == 0:
+        isSampleToCopy = True
+    for sampleToCopy in samplesToCopy:
+        if source_file.find(sampleToCopy) != -1:
+            isSampleToCopy = True
+    if not isSampleToCopy:
+        continue;
+
+    target_file = source_file.replace(sourceFilePath, targetFilePath)
+    
+    print("copying %s --> %s" % (source_file, target_file))
+    files_to_copy.append(source_file)
+
+castor_mirror.mirror_files(castor_mirror.needs_local_copy(files_to_copy, [ targetFilePath ]), [ targetFilePath ], 50)
