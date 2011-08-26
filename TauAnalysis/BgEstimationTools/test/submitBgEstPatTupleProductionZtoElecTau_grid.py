29dbfe25053ccc721f3f95a00cc8fda58405f708
lantonel <>
2011-08-26 15:09:43 +0000

    added AHtoElecTau samples

--- TauAnalysis/BgEstimationTools/test/submitBgEstPatTupleProductionZtoElecTau_grid.py
+++ TauAnalysis/BgEstimationTools/test/submitBgEstPatTupleProductionZtoElecTau_grid.py
@@ -2,6 +2,7 @@
 
 
 from TauAnalysis.Configuration.recoSampleDefinitionsZtoElecTau_grid_cfi import recoSampleDefinitionsZtoElecTau
+from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import recoSampleDefinitionsAHtoElecTau
 from TauAnalysis.Configuration.submitAnalysisToGrid import submitAnalysisToGrid
 from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getJobId
 
@@ -12,19 +13,6 @@ jobId = getJobId(channel)
 
 
 
-## samplesToAnalyze = [
-##     'data_TauPlusElec_Run2011A_PR_T3'
-##     'Ztautau_powheg_T3',
-##     'DYtautauM10to20_powheg_T3',
-##     'Zee_powheg_T3',
-##     'DYeeM10to20_pythia_T3',
-##     'PhotonPlusJets_Pt15to30_T3','PhotonPlusJets_Pt30to50_T3','PhotonPlusJets_Pt50to80_T3',
-##     'QCD_BCtoE_Pt20to30_T3','QCD_BCtoE_Pt30to80_T3','QCD_BCtoE_Pt80to170_T3',
-##     'QCD_EM_Pt20to30_T3','QCD_EM_Pt30to80_T3','QCD_EM_Pt80to170_T3',
-##     'TTplusJets_madgraph_T3',
-##     'WplusJets_madgraph_T3',
-##     'WW_T3','WZ_T3','ZZ_T3',
-## ]
 
 # Submit analysis jobs to the grid;
 # disable estimation of systematic uncertainties for all samples except Ztautau
