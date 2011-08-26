17a4f8815fb6a658cde5e4989312f10d949b1af4
lantonel <>
2011-08-26 14:28:02 +0000

    added AHtoElecTau samples

--- TauAnalysis/BgEstimationTools/test/harvestBgEstTemplatesZtoElecTau_grid.py
+++ TauAnalysis/BgEstimationTools/test/harvestBgEstTemplatesZtoElecTau_grid.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from TauAnalysis.Configuration.recoSampleDefinitionsZtoElecTau_grid_cfi import recoSampleDefinitionsZtoElecTau
+from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import recoSampleDefinitionsAHtoElecTau
+
 from TauAnalysis.Configuration.harvestAnalysisResults_grid import harvestAnalysisResults
 from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getHarvestingFilePath, getJobId, getTmpFilePath
 
@@ -11,6 +13,6 @@ jobId = getJobId(channel)
 tmpFilePath = getTmpFilePath(channel)
 
 
-harvestAnalysisResults(channel = channel, samples = recoSampleDefinitionsZtoElecTau,
+harvestAnalysisResults(channel = channel, samples = recoSampleDefinitionsAHtoElecTau,
                        inputFilePath = analysisFilePath, outputFilePath = harvestingFilePath, jobId = jobId, tmpFilePath = tmpFilePath, use_job_report = True,
                        useCastor = False)
