3dd6e0095e16609c72c12d70012d7b1ebca2e12f
jkolb <>
2011-08-31 18:31:44 +0000

    first commit for Jeff's tau ID branch

--- TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
+++ TauAnalysis/Configuration/test/submitAnalysisZtoMuTau_grid.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
 from TauAnalysis.Configuration.recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsZtoMuTau_7TeV
-from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsAHtoMuTau_7TeV
+#from TauAnalysis.Configuration.recoSampleDefinitionsAHtoMuTau_7TeV_grid_cfi import recoSampleDefinitionsAHtoMuTau_7TeV
 from TauAnalysis.Configuration.submitAnalysisToGrid import submitAnalysisToGrid
 from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getJobId
 
 channel = 'ZtoMuTau'
 configFile = 'runZtoMuTau_cfg.py'
-analysisFilePath = getAnalysisFilePath(channel)
+outputFilePath = getAnalysisFilePath(channel)
 jobId = getJobId(channel)
 
 samplesToAnalyze = [
     # modify in case you want to submit crab jobs for some of the samples only...    
+    'data_SingleMu_Run2011A_May10ReReco_skim',
+    #'Ztautau_embedded_part1_skim',
+    #'Ztautau_embedded_part2_skim',
+    #'DYtautauM10to20_pythia',
+    #'DYmumuM10to20_powheg',
+    #'qqZll',
+    #'Ztautau_powheg_skim',
+    #'Zmumu_powheg_skim',
+    #'PPmuXptGt20Mu15_skim',
+    #'WplusJets_madgraph_skim',
+    #'TTplusJets_madgraph_skim'    
 ]
 
-import copy
-samplesToAnalyze = copy.deepcopy(recoSampleDefinitionsZtoMuTau_7TeV['SAMPLES_TO_ANALYZE'])
+#import copy
+#samplesToAnalyze = copy.deepcopy(recoSampleDefinitionsZtoMuTau_7TeV['SAMPLES_TO_ANALYZE'])
 #samplesToAnalyze.extend([
 #    'A130', 'bbA130',
 #    'A200', 'bbA200',
@@ -27,5 +38,59 @@ samplesToAnalyze = copy.deepcopy(recoSampleDefinitionsZtoMuTau_7TeV['SAMPLES_TO_
 enableSystematics = False
 
 submitAnalysisToGrid(configFile = configFile, channel = 'ZtoMuTau',
-                     samples = recoSampleDefinitionsAHtoMuTau_7TeV, outputFilePath = analysisFilePath, jobId = jobId,
-                     samplesToAnalyze = samplesToAnalyze, disableSysUncertainties = not enableSystematics)
+                     samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                     samplesToAnalyze = samplesToAnalyze, disableSysUncertainties = not enableSystematics,
+                    samplesToSkip = ['WW','WZ','ZZ','DYmumuM10to20_powheg','DYtautauM10to20_pythia','qqZll'],
+                    outsideCERN = True,
+                    useCastor = False,
+                     create = True,
+                     submit = True)
+if samplesToAnalyze.count('WW') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = FilePath, jobId = jobId,
+                         samplesToAnalyze = ['WW'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+if samplesToAnalyze.count('WZ') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                         samplesToAnalyze = ['WZ'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+if samplesToAnalyze.count('ZZ') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                         samplesToAnalyze = ['ZZ'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+if samplesToAnalyze.count('DYmumuM10to20_powheg') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                         samplesToAnalyze = ['DYmumuM10to20_powheg'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+if samplesToAnalyze.count('DYtautauM10to20_pythia') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                         samplesToAnalyze = ['DYtautauM10to20_pythia'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+if samplesToAnalyze.count('qqZll') > 0 or len(samplesToAnalyze) == 0:
+    submitAnalysisToGrid(configFile = configFile, channel = channel,
+                         samples = recoSampleDefinitionsZtoMuTau_7TeV, outputFilePath = outputFilePath, jobId = jobId,
+                         samplesToAnalyze = ['qqZll'], samplesToSkip = None, disableSysUncertainties = True,
+                        outsideCERN = False,
+                        useCastor = False,
+                         create = True,
+                         submit = True)
+
