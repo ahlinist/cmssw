7e97ceb4aee83b8767360aa794549d4974c19ce7
mkortela <>
2011-09-08 06:43:28 +0000

    Moved to use PAT to have pat taus and muons to hold all the relevant discriminators, PF isolation values etc so that they wouldn't have to be read explicitly from the event (in the hope that this would ease the future maintenance). This also makes the use of generic selectors easier in the configuration.

--- ElectroWeakAnalysis/TauTriggerEfficiency/src/MuonAnalyzer.cc
+++ ElectroWeakAnalysis/TauTriggerEfficiency/src/MuonAnalyzer.cc
@@ -5,16 +5,16 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "DataFormats/Common/interface/ValueMap.h"
 #include "DataFormats/Common/interface/View.h"
-#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/Candidate/interface/CompositeCandidate.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 #include <TTree.h>
 #include <iostream>
 
 // Default constructor
-MuonAnalyzer::MuonAnalyzer():
-  muontree(0){}
+MuonAnalyzer::MuonAnalyzer(){}
 
 
 MuonAnalyzer::~MuonAnalyzer(){}
@@ -23,31 +23,27 @@ void MuonAnalyzer::Setup(const edm::ParameterSet& iConfig,TTree *trigtree)
 {
 	MuonSource = iConfig.getParameter<edm::InputTag>("MuonSource");
         muTauPairSource = iConfig.getParameter<edm::InputTag>("MuonTauPairSource");
-        pfIsoChargedSrc = iConfig.getParameter<edm::InputTag>("MuonPFIsoValueCharged");
-        pfIsoNeutralSrc = iConfig.getParameter<edm::InputTag>("MuonPFIsoValueNeutral");
-        pfIsoGammaSrc = iConfig.getParameter<edm::InputTag>("MuonPFIsoValueGamma");
-	ptMin      = iConfig.getParameter<double>("MuonPtMin");
-	etaMax     = iConfig.getParameter<double>("MuonEtaMax");
-
-  	muontree = trigtree;
 
   	// Setup branches
-  	muontree->Branch("MuonPt", &muonPt);
-	muontree->Branch("MuonEta",&muonEta);
-	muontree->Branch("MuonPhi",&muonPhi);
-        muontree->Branch("MuonIso03SumPt", &muonIso03SumPt);
-        muontree->Branch("MuonIso03EmEt", &muonIso03EmEt);
-        muontree->Branch("MuonIso03HadEt", &muonIso03HadEt);
-        muontree->Branch("MuonPFIsoChargedPt", &muonPFIsoCharged);
-        muontree->Branch("MuonPFIsoNeutralEt", &muonPFIsoNeutral);
-        muontree->Branch("MuonPFIsoGammaEt", &muonPFIsoGamma);
-	muontree->Branch("NMuons",&nMuons, "NMuons/I");
+  	trigtree->Branch("MuonPt", &muonPt);
+	trigtree->Branch("MuonEta",&muonEta);
+	trigtree->Branch("MuonPhi",&muonPhi);
+        trigtree->Branch("MuonIso03SumPt", &muonIso03SumPt);
+        trigtree->Branch("MuonIso03EmEt", &muonIso03EmEt);
+        trigtree->Branch("MuonIso03HadEt", &muonIso03HadEt);
+        trigtree->Branch("MuonPFIsoChargedPt", &muonPFIsoCharged);
+        trigtree->Branch("MuonPFIsoNeutralEt", &muonPFIsoNeutral);
+        trigtree->Branch("MuonPFIsoGammaEt", &muonPFIsoGamma);
+	trigtree->Branch("NMuons",&nMuons);
 
-        muontree->Branch("MuonTauInvMass", &muTauInvMass);
-;
+        trigtree->Branch("MuonTauInvMass", &muTauInvMass);
 }
 
-void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup, const reco::Candidate& tau) {
+  fill(iEvent, iSetup, tau.p4());
+}
+
+void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup, const LorentzVector& tau) {
 
 	muonPt  = -999;
 	muonEta = -999;
@@ -63,42 +59,51 @@ void MuonAnalyzer::fill(const edm::Event& iEvent, const edm::EventSetup& iSetup)
         muTauInvMass = 0;
 
 //
-	edm::Handle<edm::View<reco::Muon> > muons;
+	edm::Handle<edm::View<pat::Muon> > muons;
         iEvent.getByLabel(MuonSource, muons);
 
-        typedef edm::ValueMap<double> IsoMap;
-        edm::Handle<IsoMap> pfIsoCharged;
-        iEvent.getByLabel(pfIsoChargedSrc, pfIsoCharged);
-        edm::Handle<IsoMap> pfIsoNeutral;
-        iEvent.getByLabel(pfIsoNeutralSrc, pfIsoNeutral);
-        edm::Handle<IsoMap> pfIsoGamma;
-        iEvent.getByLabel(pfIsoGammaSrc, pfIsoGamma);
+        nMuons = muons->size();
 
-	if(muons.isValid()) {
-	  for(size_t i = 0; i < muons->size(); ++i){
-		if(muons->at(i).pt() < ptMin) continue;
-		if(fabs(muons->at(i).eta()) > etaMax) continue;
-		// add check that the muon is from the primary vertex?
-		if(muonPt < 0) { // assuming muons are pt ordered, saving the first
-		  muonPt  = muons->at(i).pt();
-		  muonEta = muons->at(i).eta();
-		  muonPhi = muons->at(i).phi();
-                  muonIso03SumPt = muons->at(i).isolationR03().sumPt;
-                  muonIso03EmEt = muons->at(i).isolationR03().emEt;
-                  muonIso03HadEt = muons->at(i).isolationR03().hadEt;
+        edm::Handle<edm::View<reco::CompositeCandidate> > hpairs;
+        iEvent.getByLabel(muTauPairSource, hpairs);
+        if(hpairs.isValid() && !hpairs->empty()) {
 
-                  muonPFIsoCharged = (*pfIsoCharged)[muons->refAt(i)];
-                  muonPFIsoNeutral = (*pfIsoNeutral)[muons->refAt(i)];
-                  muonPFIsoGamma = (*pfIsoGamma)[muons->refAt(i)];
-		}
-		nMuons++;
-	  }
-	}
+          const reco::CompositeCandidate *pair = 0;
+          const pat::Muon *muon = 0;
+          double minPt = 0;
+          for(size_t i=0; i<hpairs->size(); ++i) {
+            const reco::CompositeCandidate& muTau = hpairs->at(i);
+            if(muTau.numberOfDaughters() != 2)
+              throw cms::Exception("LogicError") << "MuonAnalyzer expected reco::CompositeCandidates with 2 daughters, got " << muTau.numberOfDaughters() << " from " << muTauPairSource.encode() << std::endl;
 
-        edm::Handle<edm::View<reco::Candidate> > hcands;
-        iEvent.getByLabel(muTauPairSource, hcands);
-        if(hcands.isValid() && !hcands->empty()) {
-          muTauInvMass = hcands->at(0).mass();
+            if(reco::deltaR(*(muTau.daughter(1)), tau) < 0.01 &&
+               muTau.daughter(0)->pt() > minPt) {
+              pair = &muTau;
+              muon = dynamic_cast<const pat::Muon *>(muTau.daughter(0));
+              if(!muon)
+                throw cms::Exception("LogicError") << "MuonAnalyzer expected reco::CompositeCandidates with pat::Muon as the first daughter!" << std::endl;
+              minPt = muon->pt();
+            }
+          }
+
+          // Found mu-tau pair matching to the given tau
+          if(muon) {
+            if(!pair)
+              throw cms::Exception("LogicError") << "MuonAnalyzer found a muon, but not the mu-tau pair. This needs some debugging." << std::endl;
+
+            muonPt  = muon->pt();
+            muonEta = muon->eta();
+            muonPhi = muon->phi();
+            muonIso03SumPt = muon->isolationR03().sumPt;
+            muonIso03EmEt = muon->isolationR03().emEt;
+            muonIso03HadEt = muon->isolationR03().hadEt;
+
+            muonPFIsoCharged = muon->userIsolation(pat::PfChargedHadronIso);
+            muonPFIsoNeutral = muon->userIsolation(pat::PfNeutralHadronIso);
+            muonPFIsoGamma = muon->userIsolation(pat::PfGammaIso);
+
+            muTauInvMass = pair->mass();
+          }
         }
 }
 
