923d2cdfb5ee3ea1aedfbe8d057d8db7dbe67f5e
wmtan <>
2011-09-25 12:23:40 +0000

    Fix Coverity errors

--- Utilities/bin/edmToFriendlyClassName.cc
+++ Utilities/bin/edmToFriendlyClassName.cc
@@ -2,25 +2,31 @@
 //
 // Package:     Utilities
 // Class  :     edmToFriendlyClassName
-// 
+//
 // Implementation:
 //     <Notes on implementation>
 //
 // Original Author:  Chris Jones
 //         Created:  Thu Oct  4 14:30:17 EDT 2007
-// $Id$
 //
 
-// system include files
-#include <iostream>
 // user include files
 #include "FWCore/Utilities/interface/FriendlyName.h"
 
+// system include files
+#include <iostream>
+#include <stdexcept>
+
 int
-main(int argc, char* argv[])
-{
-  for(int index=1; index < argc; ++index) {
-    std::cout <<edm::friendlyname::friendlyName(argv[index])<<std::endl;
+main(int argc, char* argv[]) {
+  try {
+    for(int index = 1; index < argc; ++index) {
+      std::cout << edm::friendlyname::friendlyName(argv[index]) << std::endl;
+    }
+  }
+  catch(std::runtime_error const& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
   }
   return 0;
 }
