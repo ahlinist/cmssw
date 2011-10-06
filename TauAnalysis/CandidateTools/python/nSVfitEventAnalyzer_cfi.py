1eea42cf00ff8f09d94cffd839c6731c48fd59a9
rwolf <>
2011-10-06 10:39:47 +0000

    generalized to any kind of lepton using edm::View

--- TauAnalysis/CandidateTools/python/nSVfitEventAnalyzer_cfi.py
+++ TauAnalysis/CandidateTools/python/nSVfitEventAnalyzer_cfi.py
@@ -5,8 +5,8 @@ import RecoMET.METProducers.METSigParams_cfi as met_config
 nSVfitEventAnalyzer = cms.EDAnalyzer(
     "NSVfitEventAnalyzer",
     met   = cms.InputTag("patMETs"),
-    muons = cms.InputTag("cleanPatMuons"),
-    elecs = cms.InputTag("cleanPatElectrons"),
+    leps1 = cms.InputTag("cleanPatMuons"),
+    leps2 = cms.InputTag("cleanPatElectrons"),
     metSignificance = cms.PSet(
         srcPFJets = cms.InputTag('ak5PFJets'),
         srcPFCandidates = cms.InputTag('particleFlow'),
