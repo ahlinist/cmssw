797fe1d34140bd1bc4ed9f8d3197e249e765b09d
veverka <>
2011-10-10 16:00:44 +0000

    added cluster-shape and gen-level variables

--- ElectroWeakAnalysis/MultiBosons/test/FsrAnalysis/r9Tree_cfg.py
+++ ElectroWeakAnalysis/MultiBosons/test/FsrAnalysis/r9Tree_cfg.py
@@ -1,46 +1,46 @@
 import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.VarParsing as VarParsing
-
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+## Default PAT process skeleton
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
 
 # setup 'analysis'  options
 options = VarParsing.VarParsing ('analysis')
 
 ## default options
-options.inputFiles = pickRelValInputFiles(
-    cmsswVersion  = 'CMSSW_4_1_3',
-    relVal        = 'RelValTTbar',
-    globalTag     = 'START311_V2',
-    numberOfFiles = 1
-)
-
-
+options.inputFiles = [("/store/user/apresyan/"
+    "SingleGammaFlatPt10To100_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_"
+    "noPU_noOOTPU_GEANT4_93p01/"
+    "SingleGammaFlatPt10To100_RAW2DIGI_L1Reco_RECO_GEANT_g93p01/"
+    "861ce875399c8b0396efbce750d4767e/"
+    "SingleGammaFlatPt10To100_RAW2DIGI_L1Reco_RECO_g93p01_9_1_Uoy.root")]
 options.outputFile = "r9Tree.root"
 
 ## get and parse the command line arguments
 options.parseArguments()
 
-process = cms.Process("TTREE")
+removeAllPATObjectsBut(process, ['Photons'])
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.maxEvents = cms.untracked.PSet(
   input = cms.untracked.int32(options.maxEvents)
 )
 
-process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring() + options.inputFiles
-)
+process.source.fileNames = options.inputFiles
+
+## Make the ttree on the fly, don't store edm
+del process.outpath
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string(options.outputFile)
 )
 
 process.load("ElectroWeakAnalysis.MultiBosons.FsrAnalysis.r9Tree_cfi")
+process.r9Tree.src = 'patPhotons'
 
-process.p = cms.Path(process.r9Tree)
+process.p = cms.Path(process.patDefaultSequence + process.r9Tree)
 
 ## Enable history and tab-completion for interactive inspection
 if __name__ == "__main__": import user
+
