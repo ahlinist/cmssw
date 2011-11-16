46147162f604ca8dbfe180cd697d0e4f7ba1ecec
bendavid <>
2011-11-16 17:32:29 +0000

    Add classes and modifications necessary to use GBR regression weights from db

---
+++ CondFormats/EgammaObjects/src/GBRForest.cxx
@@ -0,0 +1,50 @@
+
+
+
+#include "../interface/GBRForest.h"
+//#include <iostream>
+#include "TMVA/DecisionTree.h"
+#include "TMVA/MethodBDT.h"
+
+
+//ClassImp(GBRForest)
+
+
+//_______________________________________________________________________
+GBRForest::GBRForest() : 
+  fInitialResponse(0.)
+{
+
+}
+
+//_______________________________________________________________________
+GBRForest::~GBRForest() 
+{
+  for (UInt_t i=0; i<fTrees.size(); ++i) {
+    delete fTrees.at(i);
+  }
+}
+
+//_______________________________________________________________________
+GBRForest::GBRForest(const TMVA::MethodBDT *bdt) : 
+  TNamed("GBRForest","GBRForest"),
+  fInitialResponse(bdt->GetBoostWeights().front())
+{
+  
+  const std::vector<TMVA::DecisionTree*> &forest = bdt->GetForest();
+  for (std::vector<TMVA::DecisionTree*>::const_iterator it=forest.begin(); it!=forest.end(); ++it) {
+    GBRTree *tree = new GBRTree(*it);
+    fTrees.push_back(tree);
+  }
+}
+
+GBRForest::GBRForest(const GBRForest &other) :
+  fInitialResponse(other.fInitialResponse)
+{
+  for (std::vector<GBRTree*>::const_iterator it = other.fTrees.begin(); it!=other.fTrees.end(); ++it) {
+    fTrees.push_back(new GBRTree(**it));
+  }
+} 
+
+
+
