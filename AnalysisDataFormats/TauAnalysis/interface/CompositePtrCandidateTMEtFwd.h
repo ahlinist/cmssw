93a40190c707e836180a4053c30cbdece89b34a0
veelken <>
2011-11-17 14:26:59 +0000

    added template instantiations for e + MET and mu + MET

--- AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateTMEtFwd.h
+++ AnalysisDataFormats/TauAnalysis/interface/CompositePtrCandidateTMEtFwd.h
@@ -10,15 +10,23 @@
 #include <vector>
 
 /// collection of CompositeRefCandidateTMEt objects
+typedef std::vector<PATElecNuPair> PATElecNuPairCollection;
+typedef std::vector<PATMuNuPair> PATMuNuPairCollection;
 typedef std::vector<PATTauNuPair> PATTauNuPairCollection;
 
 /// persistent reference to a CompositeRefCandidateTMEt object
+typedef edm::Ref<PATElecNuPairCollection> PATElecNuPairRef;
+typedef edm::Ref<PATMuNuPairCollection> PATMuNuPairRef;
 typedef edm::Ref<PATTauNuPairCollection> PATTauNuPairRef;
 
 /// references to CompositeRefCandidateTMEt collection
+typedef edm::RefProd<PATElecNuPairCollection> PATElecNuPairRefProd;
+typedef edm::RefProd<PATMuNuPairCollection> PATMuNuPairRefProd;
 typedef edm::RefProd<PATTauNuPairCollection> PATTauNuPairRefProd;
 
 /// vector of references to CompositeRefCandidateTMEt objects all in the same collection
+typedef edm::RefVector<PATElecNuPairCollection> PATElecNuPairRefVector;
+typedef edm::RefVector<PATMuNuPairCollection> PATMuNuPairRefVector;
 typedef edm::RefVector<PATTauNuPairCollection> PATTauNuPairRefVector;
 
 #endif
