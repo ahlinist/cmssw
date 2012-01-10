5bff71406100091aab95d304cc30bc2b3383bee7
mikeh <>
2012-01-10 11:48:04 +0000

    initial version for CMSDAS 2012

--- PhysicsTools/Examples/test/CMSDAS_PileupStudy_cfg.py
+++ PhysicsTools/Examples/test/CMSDAS_PileupStudy_cfg.py
@@ -1,5 +1,5 @@
-# PYTHON configuration file for class: CMSDAS_PileupReweight.cc
-# Description:  Example of simple EDAnalyzer for Pileup Reweighting
+# PYTHON configuration file for class: CMSDAS_PileupStudy.cc
+# Description:  Example of simple EDAnalyzer for Pileup Studying
 # Author: Mike Hildreth
 # Date:  08 - January - 2012
 import FWCore.ParameterSet.Config as cms
@@ -17,7 +17,8 @@ process.source = cms.Source("PoolSource",
                              fileNames = cms.untracked.vstring(
     '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FEEBB1EF-B404-E111-BF75-00304867BF9A.root',
     '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FED957F3-A404-E111-93D0-003048678FE6.root'
-                                                              )
+#                   '/store/data/Run2011B/Tau/AOD/PromptReco-v1/000/175/886/3A9B3704-6BDD-E011-A2D1-BCAEC518FF52.root'
+                                         )
                             )          
 
 ##-------------------- Communicate with the DB -----------------------
@@ -28,12 +29,12 @@ from Configuration.AlCa.autoCond import autoCond
 process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
 
 #############   Correct Calo Jets on the fly #########
-process.PUReweight = cms.EDAnalyzer("CMSDAS_PileupReweight",
+process.PUStudy = cms.EDAnalyzer("CMSDAS_PileupStudy",
                                   vertexSrc = cms.InputTag("offlinePrimaryVertices")
 )
 
 #############   Path       ###########################
-process.p = cms.Path(process.PUReweight)
+process.p = cms.Path(process.PUStudy)
 
 
 #############   Format MessageLogger #################
@@ -42,5 +43,5 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 #############  This is how CMS handles output ROOT files #################
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string("PU_histos_reweight.root")
+    fileName = cms.string("PU_histos.root")
  )
