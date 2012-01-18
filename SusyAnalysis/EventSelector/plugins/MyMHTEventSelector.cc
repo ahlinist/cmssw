af965c55158adf64ea5525fa2e757cf501250fad
csander <>
2012-01-18 11:02:46 +0000

    formatting and adding WeightSelector

--- SusyAnalysis/EventSelector/plugins/MyMHTEventSelector.cc
+++ SusyAnalysis/EventSelector/plugins/MyMHTEventSelector.cc
@@ -6,105 +6,99 @@
 #include "DataFormats/PatCandidates/interface/JetCorrFactors.h"
 #include "DataFormats/JetReco/interface/CaloJet.h"
 
-
 //__________________________________________________________________________________________________
 MyMHTEventSelector::MyMHTEventSelector(const edm::ParameterSet& pset) :
-        SusyEventSelector(pset),
-        jetTag_(pset.getParameter<edm::InputTag> ("jetTag")),
-        minMHT_(pset.getParameter<double> ("minMHT")),
-        maxMHT_(pset.getParameter<double> ("maxMHT")),
-        maxMHTsig_(pset.getParameter<double> ("maxMHTsig")),
-        minPt_(pset.getParameter<double> ("minPt")),
-        maxEta_(pset.getParameter<double> ("maxEta")),
-        useJetID_(pset.getParameter<bool> ("useJetID")),
-rejectEvtJetID_(pset.getParameter<bool> ("rejectEvtJetID")){
+   SusyEventSelector(pset), jetTag_(pset.getParameter<edm::InputTag> ("jetTag")), minMHT_(pset.getParameter<double> (
+         "minMHT")), maxMHT_(pset.getParameter<double> ("maxMHT")),
+         maxMHTsig_(pset.getParameter<double> ("maxMHTsig")), minPt_(pset.getParameter<double> ("minPt")), maxEta_(
+               pset.getParameter<double> ("maxEta")), useJetID_(pset.getParameter<bool> ("useJetID")), rejectEvtJetID_(
+               pset.getParameter<bool> ("rejectEvtJetID")) {
 
-    //// Store computed HT
-    defineVariable("MHT");
-    defineVariable("MHTsignificance");
+   //// Store computed HT
+   defineVariable("MHT");
+   defineVariable("MHTsignificance");
 }
 
 //__________________________________________________________________________________________________
 bool MyMHTEventSelector::select(const edm::Event& event) const {
-    //// reset cached variables
-    math::XYZTLorentzVector MHT;
-    double HT = 0;
+   //// reset cached variables
+   math::XYZTLorentzVector MHT;
+   double HT = 0;
 
-    resetVariables();
+   resetVariables();
 
-    //// Get the jets
-    edm::Handle<edm::View<pat::Jet> > jetHandle;
-    event.getByLabel(jetTag_, jetHandle);
-    if (!jetHandle.isValid()) {
-        edm::LogWarning("MyMHTEventSelector") << "No Jet results for InputTag " << jetTag_;
-        return false;
-    }
+   //// Get the jets
+   edm::Handle<edm::View<pat::Jet> > jetHandle;
+   event.getByLabel(jetTag_, jetHandle);
+   if (!jetHandle.isValid()) {
+      edm::LogWarning("MyMHTEventSelector") << "No Jet results for InputTag " << jetTag_;
+      return false;
+   }
 
-    //// To be set true if one jet is found failing jetID
-    bool badJet = false;
-    JetIDSelectionFunctor jetIDLooseCalo( JetIDSelectionFunctor::PURE09, JetIDSelectionFunctor::LOOSE );
-    PFJetIDSelectionFunctor jetIDLoosePF( PFJetIDSelectionFunctor::FIRSTDATA, PFJetIDSelectionFunctor::LOOSE );
+   //// To be set true if one jet is found failing jetID
+   bool badJet = false;
+   JetIDSelectionFunctor jetIDLooseCalo(JetIDSelectionFunctor::PURE09, JetIDSelectionFunctor::LOOSE);
+   PFJetIDSelectionFunctor jetIDLoosePF(PFJetIDSelectionFunctor::FIRSTDATA, PFJetIDSelectionFunctor::LOOSE);
 
-    //// Sum over jet Ets (with cut on min. pt)
-    for( edm::View<pat::Jet>::const_iterator iJet = jetHandle->begin(); iJet != jetHandle->end(); ++iJet ){
+   //// Sum over jet Ets (with cut on min. pt)
+   for (edm::View<pat::Jet>::const_iterator iJet = jetHandle->begin(); iJet != jetHandle->end(); ++iJet) {
 
-        if (iJet->pt() > minPt_ && fabs(iJet->eta()) < maxEta_) {
+      if (iJet->pt() > minPt_ && fabs(iJet->eta()) < maxEta_) {
 
-            bool loose = true;
+         bool loose = true;
 
-            if (useJetID_){
-                if( iJet->isCaloJet() || iJet->isJPTJet() ){
-                    pat::strbitset ret = jetIDLooseCalo.getBitTemplate();
-                    ret.set(false);
-                    loose = jetIDLooseCalo( *iJet, ret );
-                }
-                else if ( iJet->isPFJet() ){
-		  /*
-                    pat::strbitset ret = jetIDLoosePF.getBitTemplate();
-                    ret.set(false);
-                    loose = jetIDLoosePF( *iJet, ret );
-		  */
-		  if(iJet->neutralHadronEnergyFraction() > 0.95 || iJet->photonEnergyFraction() > 0.95)
-		    loose = false;
-                }
+         if (useJetID_) {
+            if (iJet->isCaloJet() || iJet->isJPTJet()) {
+               pat::strbitset ret = jetIDLooseCalo.getBitTemplate();
+               ret.set(false);
+               loose = jetIDLooseCalo(*iJet, ret);
+            } else if (iJet->isPFJet()) {
+               /*
+                pat::strbitset ret = jetIDLoosePF.getBitTemplate();
+                ret.set(false);
+                loose = jetIDLoosePF( *iJet, ret );
+                */
+               if (iJet->neutralHadronEnergyFraction() > 0.95 || iJet->photonEnergyFraction() > 0.95)
+                  loose = false;
             }
+         }
 
-            if (!loose) {
-                //std::cout << "Failed JetID: " << iJet->pt() << ", " << iJet->eta() << ", " << iJet->phi() << ", "
-                //                              << iJet->emEnergyFraction() << ", " << iJet->jetID().n90Hits << ", "
-                //                              << iJet->jetID().fHPD << std::endl;
-                badJet = true;
-                continue;
-            }
+         if (!loose) {
+            //std::cout << "Failed JetID: " << iJet->pt() << ", " << iJet->eta() << ", " << iJet->phi() << ", "
+            //                              << iJet->emEnergyFraction() << ", " << iJet->jetID().n90Hits << ", "
+            //                              << iJet->jetID().fHPD << std::endl;
+            badJet = true;
+            continue;
+         }
 
-            math::XYZTLorentzVector p4(iJet->px(), iJet->py(), iJet->pz(), iJet->energy());//   iJet->correctedP4("abs");
-            MHT += p4;
-            HT += p4.pt();
-        }
-        //++iJet;
-    }
+         math::XYZTLorentzVector p4(iJet->px(), iJet->py(), iJet->pz(), iJet->energy());//   iJet->correctedP4("abs");
+         MHT += p4;
+         HT += p4.pt();
+      }
+      //++iJet;
+   }
 
-    float myMHT = MHT.pt();
-    //std::cout << myMHT << std::endl;
-    float myMHTsignificance = MHT.pt() / sqrt(HT);
-    //std::cout << "MHT = " << myMHT << ", HT = " << HT << ", MHT sig = " << myMHTsignificance << std::endl;
+   float myMHT = MHT.pt();
+   //std::cout << myMHT << std::endl;
+   float myMHTsignificance = MHT.pt() / sqrt(HT);
+   //std::cout << "MHT = " << myMHT << ", HT = " << HT << ", MHT sig = " << myMHTsignificance << std::endl;
 
-    setVariable("MHT", myMHT);
-    setVariable("MHTsignificance", myMHTsignificance);
+   setVariable("MHT", myMHT);
+   setVariable("MHTsignificance", myMHTsignificance);
 
-    //
-    // apply cut
-    //
-    bool result_maxsig = true;
-    if (maxMHTsig_ > 0)
-        result_maxsig = (myMHTsignificance < maxMHTsig_);
-    bool result_max = true;
-    if (maxMHT_ > 0)
-        result_max = (myMHT < maxMHT_);
-    bool result_min = true;
-    if (minMHT_ > 0)
-        result_min = (myMHT > minMHT_);
-    return (result_min && result_max && result_maxsig && !(rejectEvtJetID_ && badJet));
+   //
+   // apply cut
+   //
+   bool result_maxsig = true;
+   if (maxMHTsig_ > 0)
+      result_maxsig = (myMHTsignificance < maxMHTsig_);
+   bool result_max = true;
+   if (maxMHT_ > 0)
+      result_max = (myMHT < maxMHT_);
+   bool result_min = true;
+   if (minMHT_ > 0)
+      result_min = (myMHT > minMHT_);
+   return (result_min && result_max && result_maxsig && !(rejectEvtJetID_ && badJet));
 
 }
 
