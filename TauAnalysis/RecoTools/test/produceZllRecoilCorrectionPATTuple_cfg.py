138f02f113ed48a9d29c0b26d8c50250aa29f877
veelken <>
2012-01-22 10:16:45 +0000

    added SQLlite file for new JEC V12 correction factors

--- TauAnalysis/RecoTools/test/produceZllRecoilCorrectionPATTuple_cfg.py
+++ TauAnalysis/RecoTools/test/produceZllRecoilCorrectionPATTuple_cfg.py
@@ -54,6 +54,33 @@ else:
 #--------------------------------------------------------------------------------    
 
 #--------------------------------------------------------------------------------
+#
+# configure Jet Energy Corrections
+#
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.jec = cms.ESSource("PoolDBESSource",
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+    ),
+    timetype = cms.string('runnumber'),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V12_AK5PF'),
+            label  = cms.untracked.string('AK5PF')
+        ),
+        cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Jec11_V12_AK5Calo'),
+            label  = cms.untracked.string('AK5Calo')
+        )
+    ),
+    connect = cms.string('sqlite_fip:TauAnalysis/Configuration/data/Jec11_V12_20111220.db')
+)
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource', 'jec')
+#-------------------------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
 # compute neutral particle density for out-of-time pile-up reweighting
 
 process.load("TauAnalysis/RecoTools/recoVertexSelection_cff")
