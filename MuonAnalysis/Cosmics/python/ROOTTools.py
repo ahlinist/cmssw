537fb2ae16c6c1e1c886a8d0dd33dd039dafa3ac
tucker <>
2012-01-24 19:29:57 +0000

    differentiate_stat_box.

--- MuonAnalysis/Cosmics/python/ROOTTools.py
+++ MuonAnalysis/Cosmics/python/ROOTTools.py
@@ -42,6 +42,33 @@ def fit_gaussian(hist, factor=None, draw=False, likelihood=False, cache=[]):
         'sigma':    (fcn.GetParameter(2), fcn.GetParError(2))
         }
 
+def differentiate_stat_box(hist, movement=1, new_color=None):
+    """Move hist's stat box and change its line/text color. If
+    movement is just an int, that number specifies how many units to
+    move the box downward. If it is a 2-tuple of ints (m,n), the stat
+    box will be moved to the left m units and down n units. A unit is
+    the width or height of the stat box.
+
+    Call TCanvas::Update first (and use TH1::Draw('sames') if
+    appropriate) or else the stat box will not exist."""
+
+    s = hist.FindObject('stats')
+
+    if new_color is not None:
+        s.SetTextColor(new_color)
+        s.SetLineColor(new_color)
+
+    if type(movement) == int:
+        movement = (0,movement)
+    m,n = movement
+    
+    x1,x2 = s.GetX1NDC(), s.GetX2NDC()
+    y1,y2 = s.GetY1NDC(), s.GetY2NDC()
+    s.SetX1NDC(x1 - (x2-x1)*m)
+    s.SetX2NDC(x2 - (x2-x1)*m)
+    s.SetY1NDC(y1 - (y2-y1)*n)
+    s.SetY2NDC(y2 - (y2-y1)*n)
+    
 class plot_saver:
     i = 0
     
@@ -267,6 +294,7 @@ def ttree_iterator(tree, return_tree=False):
 __all__ = [
     'ROOT',
     'core_gaussian',
+    'differentiate_stat_box',
     'fit_gaussian',
     'plot_saver',
     'tdr_style',
