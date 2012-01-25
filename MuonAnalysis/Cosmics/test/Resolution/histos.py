b919c492d31914d551fd35eb3d36d8353b3ea7e8
tucker <>
2012-01-25 00:59:42 +0000

    Expose more flags/parameters to argparse and improve the advertisement.

--- MuonAnalysis/Cosmics/test/Resolution/histos.py
+++ MuonAnalysis/Cosmics/test/Resolution/histos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse, os, sys, FWCore.ParameterSet.Config as cms
+import argparse, os, pprint, FWCore.ParameterSet.Config as cms
 from bins import make_bins
 from runs import get_run_list
 
@@ -20,20 +20,42 @@ parser = argparse.ArgumentParser(prefix_chars='+', description='CosmicSplittingR
 parser.add_argument('input', nargs='+',
                     help='The filename for the input ROOT file containing the ntuple.')
 
+parser.add_argument('+input-directory', default='UTpickedTracks',
+                    help='In the input ROOT file, the directory the ntuple TTree "t" is in. Default is "%(default)s".')
 parser.add_argument('+output-fn',
                     help='Override the output filename. (Default is to write to the current directory a file with basename the same as the input, but ending in .histos.root.)')
-parser.add_argument('+run-type', metavar='TYPE', action='append', dest='run_types', default=['cosmics'], choices='cosmics collisions commissioning'.split(),
-                    help='For data, include runs of type %(metavar)s. (Choices are %(choices)s and may specify more than one; default is %(default)s.)')
+parser.add_argument('+run-type', action='append', dest='run_types', default=['cosmics'], choices='cosmics collisions commissioning'.split(),
+                    help='For data, include runs of type TYPE. (May be specified more than once; default is %(default)s.)')
+parser.add_argument('+require-rpc-good', action='store_true',
+                    help='In the run selection, require RPC subdetector marked as good in DQM.')
 parser.add_argument('+min-run', type=int, default=0,
                     help='For data, drop all runs below MIN_RUN.')
 parser.add_argument('+max-run', type=int, default=999999,
                     help='For data, drop all runs below MIN_RUN.')
 parser.add_argument('+is-mc', action='store_true',
-                    help='Specified input file is MC (data assumed by default).')
+                    help='Specified input file is MC. (Data assumed by default.)')
 parser.add_argument('+no-wrong-sample', action='store_false', dest='check_for_wrong_sample',
                     help='Do not check for "wrong" sample, i.e. use all MC events regardless of dataset id.')
-#JMTBAD the rest
+parser.add_argument('+copy-selected-events', action='store_true',
+                    help='Copy events passing the overall selection (in the good run list, numbers of track hits, etc.) to a new ntuple in the output file.')
+parser.add_argument('+min-pixel-layers', type=int, default=1,
+                    help='Require MIN_PIXEL_LAYERS pixel layers on every track (except standalone). Default is %(default)s.')
+parser.add_argument('+min-strip-layers', type=int, default=8,
+                    help='Require MIN_STRIP_LAYERS strip layers on every track (except standalone). Default is %(default)s.')
+parser.add_argument('+min-muon-hits', type=int, default=0,
+                    help='Require MIN_MUON_HITS muon hits on every track (except tracker-only). Default is %(default)s.')
+parser.add_argument('+muon-subdet', default='dtonly', choices='dtonly either'.split(),
+                    help='Which muon subdetectors to allow (and require DQM flags good for). Choices are %(choices)s; default is "%(default)s".')
+parser.add_argument('+require-tt25', action='store_true',
+                    help='Require technical trigger bit 25 to have fired in each event.')
+parser.add_argument('+require-not-tt25', action='store_true',
+                    help='Require technical trigger bit 25 not to have fired in each event.')
+parser.add_argument('+bin-by-run', action='store_true',
+                    help='Instead of binning by pT (default), bin by run number.')
 options = parser.parse_args()
+#JMTBAD groups
+
+################################################################################
 
 # Finalize the options after including any overrides, and do some
 # basic checks of consistency.
@@ -46,59 +68,58 @@ options.input_fn = options.input_fn[0]
 if not options.output_fn:
     options.output_fn = os.path.basename(options.input_fn).replace('.root', '.histos.root')
 
-#print options ; raise 1
+options.require_pixels = options.min_pixel_layers > 0
+
+if options.require_tt25 and options.require_not_tt25:
+    raise argparse.ArgumentError('cannot require_tt25 and require_not_tt25')
+if options.require_tt25 and not options.require_rpc_good:
+    print 'warning: require_tt25 and not require_rpc_good!'
 
 ################################################################################
 
-dt_or_csc = 'dt'
-require_pixels = True
-require_rpc_good = False
-require_tt25 = False
-require_not_tt25 = False
-min_pixel_layers = 1
-
-if require_tt25 and require_not_tt25:
-    raise ValueError('cannot require_tt25 and require_not_tt25')
-if require_tt25 and not require_rpc_good:
-    print 'warning: require_tt25 and not require_rpc_good!'
-
 run_list = []
 if not options.is_mc:
-    run_list = [r for r in get_run_list(options.run_types, dt_or_csc, require_pixels, require_rpc_good) if options.min_run <= r <= options.max_run]
+    run_list = [r for r in get_run_list(options.run_types, options.muon_subdet, options.require_pixels, options.require_rpc_good) if options.min_run <= r <= options.max_run]
 
 cfg = cms.PSet(
-    directory               = cms.string('UTpickedTracks'),
+    directory               = cms.string(options.input_directory),
     is_mc                   = cms.bool(options.is_mc),
     check_for_wrong_sample  = cms.bool(options.check_for_wrong_sample),
     filename                = cms.string(options.input_fn),
-    min_muon_hits           = cms.int32(0),
-    min_pixel_layers        = cms.int32(min_pixel_layers if require_pixels else 0),
-    min_strip_layers        = cms.int32(8),
+    min_muon_hits           = cms.int32(options.min_muon_hits),
+    min_pixel_layers        = cms.int32(options.min_pixel_layers),
+    min_strip_layers        = cms.int32(options.min_strip_layers),
     max_tpfms_station       = cms.int32(-1),
-    no_dt_allowed           = cms.bool(False),
-    no_csc_allowed          = cms.bool(dt_or_csc == 'dt'),
+    no_dt_allowed           = cms.bool(options.muon_subdet == 'csconly'),
+    no_csc_allowed          = cms.bool(options.muon_subdet == 'dtonly'),
     check_tksta_dphi        = cms.bool(False),
     use_unpropagated_values = cms.bool(True),
     pp_reco_mode            = cms.bool(False),
     force_run_list          = cms.vuint32(run_list),
     force_event_list        = cms.vuint32(),
-    require_tt25            = cms.bool(require_tt25),
-    require_not_tt25        = cms.bool(require_not_tt25),
+    require_tt25            = cms.bool(options.require_tt25),
+    require_not_tt25        = cms.bool(options.require_not_tt25),
     min_bfield              = cms.double(3.7),
-    copy_selected_events    = cms.bool(False),
+    copy_selected_events    = cms.bool(options.copy_selected_events),
     )
 
-bins = make_bins('pt')
-#bins = make_bins('run', run_list)
+if options.bin_by_run:
+    bins = make_bins('run', run_list)
+else:
+    bins = make_bins('pt')
 
 print 'configuring config:'
-for k,v in cfg.parameters_().items() + [('number of bins', len(bins)),
-                                        ('bins', bins),
-                                        ('run_types', options.run_types),
-                                        ('require_rpc_good', require_rpc_good),
-                                        ('output_file', options.output_fn),
-                                        ]:
-    print '%25s: %s' % (k, repr(v))
+
+z = cfg.parameters_().items() + [
+    ('bins, number of', len(bins)),
+    ('bins', bins),
+    ('run_types', options.run_types),
+    ('require_rpc_good', options.require_rpc_good),
+    ('output_file', options.output_fn),
+    ]
+z.sort(key=lambda x: x[0])
+for k,v in z:
+    print '%s: %s' % (k.ljust(25), pprint.pformat(v))
 
 ########################################################################################
 
