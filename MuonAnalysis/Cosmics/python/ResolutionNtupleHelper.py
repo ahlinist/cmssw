9f8ff47217d3d4137e0e33c55a84be84c1309a49
tucker <>
2012-01-29 19:17:04 +0000

    Loop only over "simple" tracks (the other tracks are just copies); min_muon_hits.

--- MuonAnalysis/Cosmics/python/ResolutionNtupleHelper.py
+++ MuonAnalysis/Cosmics/python/ResolutionNtupleHelper.py
@@ -21,18 +21,22 @@ n_tracks = len(track_nicks)
 for i,n in enumerate(track_nicks):
     exec 'tk_%s = %i' % (n.lower(), i)
 
-def make_cut_string(min_pixel_layers=1, min_strip_layers=8, no_csc_allowed=True, max_dxy=None, max_dz=None):
+n_simple_tracks = tk_dyt + 1
+
+def make_cut_string(min_pixel_layers=1, min_strip_layers=8, no_csc_allowed=True, max_dxy=None, max_dz=None, min_muon_hits=None):
     base_cut = []
     if no_csc_allowed:
         base_cut.append('!hit_csc')
     if min_pixel_layers > 0:
-        base_cut += ['pixel_layers[%i][%i] >= %i' % (i,j, min_pixel_layers) for j in xrange(n_track_pos) for i in xrange(n_tracks) if i != tk_stalone]
+        base_cut += ['pixel_layers[%i][%i] >= %i' % (i,j, min_pixel_layers) for j in xrange(n_track_pos) for i in xrange(n_simple_tracks) if i != tk_stalone]
     if min_strip_layers > 0:
-        base_cut += ['strip_layers[%i][%i] >= %i' % (i,j, min_strip_layers) for j in xrange(n_track_pos) for i in xrange(n_tracks) if i != tk_stalone]
+        base_cut += ['strip_layers[%i][%i] >= %i' % (i,j, min_strip_layers) for j in xrange(n_track_pos) for i in xrange(n_simple_tracks) if i != tk_stalone]
     if max_dxy:
-        base_cut += ['abs(unprop_dxy[%i][%i]) < %f' % (i,j, max_dxy) for j in xrange(n_track_pos) for i in xrange(n_tracks) if i != tk_stalone]
+        base_cut += ['abs(unprop_dxy[%i][%i]) < %f' % (i,j, max_dxy) for j in xrange(n_track_pos) for i in xrange(n_simple_tracks) if i != tk_stalone]
     if max_dz:
-        base_cut += ['abs(unprop_dz[%i][%i]) < %f' % (i,j, max_dz) for j in xrange(n_track_pos) for i in xrange(n_tracks) if i != tk_stalone]
+        base_cut += ['abs(unprop_dz[%i][%i]) < %f' % (i,j, max_dz) for j in xrange(n_track_pos) for i in xrange(n_simple_tracks) if i != tk_stalone]
+    if min_muon_hits:
+        base_cut += ['muon_hits[%i][%i] >= %i' % (i,j, min_muon_hits) for j in xrange(n_track_pos) for i in xrange(n_simple_tracks) if i != tk_tkonly]
+        
     #pprint(base_cut) ; raise 1
     return ' && '.join(base_cut)
-
