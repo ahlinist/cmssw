25026083da3161fcdd3c0b516a9325a3243b3af3
tucker <>
2012-01-30 23:43:56 +0000

    'alignment' -> 'alca'; allow tag to be a tuple of (tag, label) in kwargs.

--- MuonAnalysis/Cosmics/python/CMSSWTools.py
+++ MuonAnalysis/Cosmics/python/CMSSWTools.py
@@ -1,32 +1,36 @@
 import FWCore.ParameterSet.Config as cms
 
-def set_preferred_alignment(process, name, connect, **kwargs):
-    '''Function to select a set of alignment constants. Useful when doing
-    track re-reconstruction.
+def set_preferred_alca(process, name, connect, **kwargs):
+    '''Function to select a set of alca constants. Useful when doing
+    re-reconstruction.
 
     Example use:
 
-    extra_alignment = [('frontier://FrontierProd/CMS_COND_31X_FROM21X', {'CSCAlignmentRcd': 'CSCAlignmentRcd_CRAFT_PG-hardware-globalMuons_v3_offline'})]
-    for i, (connect, rcds) in enumerate(extra_alignment):
-        set_preferred_alignment(process, 'extraAlignment%i' % i, connect, **rcds)
+    extra_alca = [('frontier://FrontierProd/CMS_COND_31X_FROM21X', {'CSCAlignmentRcd': 'CSCAlignmentRcd_CRAFT_PG-hardware-globalMuons_v3_offline'})]
+    for i, (connect, rcds) in enumerate(extra_alca):
+        set_preferred_alca(process, 'extraAlca%i' % i, connect, **rcds)
     '''
 
     if len(kwargs) == 0:
         raise ValueError, 'must specify at least one record parameter'
     
     from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
-    alignment_source = cms.ESSource('PoolDBESSource',
+    alca_source = cms.ESSource('PoolDBESSource',
         CondDBSetup,
         connect = cms.string(connect),
         toGet = cms.VPSet()
     )
 
     for record, tag in kwargs.iteritems():
-        alignment_source.toGet.append(cms.PSet(record = cms.string(record), tag = cms.string(tag)))
+        if type(tag) == tuple:
+            tag, label = tag
+            alca_source.toGet.append(cms.PSet(record = cms.string(record), tag = cms.string(tag), label = cms.untracked.string(label)))
+        else:
+            alca_source.toGet.append(cms.PSet(record = cms.string(record), tag = cms.string(tag)))
 
-    setattr(process, name, alignment_source)
+    setattr(process, name, alca_source)
     process.prefer(name)
 
 __all__ = [
-    'set_preferred_alignment',
+    'set_preferred_alca',
     ]
