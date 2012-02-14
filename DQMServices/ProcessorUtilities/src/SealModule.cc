c8947cd11d74abdd4d137304fc0239ee41247811
smorovic <>
2012-02-14 21:32:11 +0000

    cleanup

--- DQMServices/ProcessorUtilities/src/SealModule.cc
+++ DQMServices/ProcessorUtilities/src/SealModule.cc
@@ -2,20 +2,13 @@
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "DQMServices/ProcessorUtilities/interface/MSService.h"
 #include "DQMServices/ProcessorUtilities/interface/InputControllerRegistry.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-//#include "DQMServices/ProcessorUtilities/interface/EventStreamHttpReader.h"
-//#include "DQMServices/ProcessorUtilities/interface/DQMHttpSource.h"
 
 using namespace edm::serviceregistry;
 using namespace dqmevf;
-//using edm::EventStreamHttpReader;
-//using edm::DQMHttpSource;
 
 typedef AllArgsMaker<MSService> MSServiceMaker;
 typedef ParameterSetMaker<InputControllerRegistry> InputControllerMaker;
 
 DEFINE_FWK_SERVICE_MAKER(MSService, MSServiceMaker);
 DEFINE_FWK_SERVICE_MAKER(InputControllerRegistry,InputControllerMaker);
-//DEFINE_FWK_INPUT_SOURCE(EventStreamHttpReader);
-//DEFINE_FWK_INPUT_SOURCE(DQMHttpSource);
 
