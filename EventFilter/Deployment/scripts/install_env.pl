3557a65d92f12e49a3ef8f72d4e7daf6c0f408c5
fwyzard <>
2012-02-25 16:45:35 +0000

    sigh

--- EventFilter/Deployment/scripts/install_env.pl
+++ EventFilter/Deployment/scripts/install_env.pl
@@ -11,7 +11,7 @@ $pid = $$;
 $envfile = "/tmp/cmsswenv$pid.tmp";
 print "creating emvironment file $envfile\n";
 system `scram runtime -sh > $envfile`;
-system `echo export PYTHONHOME="\$(scram tool tag python PYTHON_BASE)" >> $envfile`;
+system `echo export PYTHONHOME=\"\$(scram tool tag python PYTHON_BASE)\" >> $envfile`;
 system `cp $envfile /tmp/fwyzard/env`;
 print "creating library clones at $libdir\n";
 `copyFUlibs.pl $envfile $libdir`;
