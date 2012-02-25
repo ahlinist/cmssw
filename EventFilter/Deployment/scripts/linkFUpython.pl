4282b4c90b85a63564a9d5c013dcdc85feb18e0d
fwyzard <>
2012-02-25 16:04:04 +0000

    support dedicated PYTHONBASE and PYTHONPATH in online releases (CMSSW 5.1.x+)"

--- EventFilter/Deployment/scripts/linkFUpython.pl
+++ EventFilter/Deployment/scripts/linkFUpython.pl
@@ -7,17 +7,14 @@ open( INFILE, "$oldfile") || die "ERROR::could not open input file $oldfile";
 print "$INFILE\n";
 while( $record = <INFILE> )
 {
-
-    if($record =~ /PYTHONPATH=/){
-	print "found PYTHONPATH = $record\n";
+    if($record =~ /PYTHONBASE=/){
+	print "found PYTHONBASE = $record\n";
 	@tok1 = split(/=\"/, $record);
-#	print "@tok1\n";
-	#pop(@tok1);
-	#pop(@tok1);
 	print "this is tok1\n";
 	print "@tok1[1]\n";
 	mkdir ($destination);
-	opendir(LIBS,$destination);
+	mkdir ("$destination\/base");
+	opendir(LIBS,"$destination\/base");
 	@tokens = split(/\:/,@tok1[1]);
 	while($#tokens != -1)
 	{
@@ -33,22 +30,22 @@ while( $record = <INFILE> )
 		    my $ffname = @tokens[$#tokens].'/'.$fname;
 		    if(grep {(/\.py/ || /\.pyc/)} $fname)
 		    {
-			$foundlink = readlink "$destination\/$fname";
+			$foundlink = readlink "$destination\/base\/$fname";
 			print "replacing previous link $foundlink\n" if ($foundlink);
-			system `rm $destination\/$fname` if $foundlink;
+			system `rm $destination\/base\/$fname` if $foundlink;
 			print " linking $ffname\n";
-			symlink("$ffname","$destination\/$fname");
+			symlink("$ffname","$destination\/base\/$fname");
 		    }
 		    if(($fname ne '.') and ($fname ne "..") and -d $ffname)
 		    {
 			print "verifying directory $ffname\n";
-# make the dir if it does not exist
-			print $destination."/".$fname."\n";
-			$ddest = $destination."/".$fname;
+                        # make the dir if it does not exist
+			print $destination."/base/".$fname."\n";
+			$ddest = $destination."/base/".$fname;
 			print "check existence of $ddest\n";
 			mkdir ($ddest) unless -e $ddest;
 			print "checking subdirs of $fname\n";
-# go one level down and readdir
+                        # go one level down and readdir
 			opendir( SDIR, $ffname);
 			@subdirs = readdir(SDIR);
 			foreach $dname (@subdirs)
@@ -58,9 +55,9 @@ while( $record = <INFILE> )
 			    print "replacing previous link $foundlink\n" if ($foundlink);
 			    system `rm -r $ddest\/$dname` if $foundlink;
 			
-			    symlink("$ddname","$destination\/$fname\/$dname") if -d $ddname;
+			    symlink("$ddname","$destination\/base\/$fname\/$dname") if -d $ddname;
 			    print " linking directory $ddname\n" if -d $ddname; 
-			    symlink("$ddname","$destination\/$fname\/$dname") if (grep {(/\.py/ || /\.pyc/)} $dname);
+			    symlink("$ddname","$destination\/base\/$fname\/$dname") if (grep {(/\.py/ || /\.pyc/)} $dname);
 			    print " linking file $dname\n" if (grep {(/\.py/ || /\.pyc/)} $dname);
 			}
 		    }
@@ -71,4 +68,67 @@ while( $record = <INFILE> )
 	    
 	}
     }
+
+    if($record =~ /PYTHONPATH=/){
+	print "found PYTHONPATH = $record\n";
+	@tok1 = split(/=\"/, $record);
+	print "this is tok1\n";
+	print "@tok1[1]\n";
+	mkdir ($destination);
+	mkdir ("$destination\/path");
+	opendir(LIBS,"$destination\/path");
+	@tokens = split(/\:/,@tok1[1]);
+	while($#tokens != -1)
+	{
+	    if(@tokens[$#tokens] =~ /\$/)
+	    {}
+	    else
+	    {
+		print "token $#tokens @tokens[$#tokens]\n";
+		opendir( DIR, @tokens[$#tokens]);
+		@dots = readdir(DIR);
+		foreach $fname (@dots)
+		{
+		    my $ffname = @tokens[$#tokens].'/'.$fname;
+		    if(grep {(/\.py/ || /\.pyc/)} $fname)
+		    {
+			$foundlink = readlink "$destination\/path\/$fname";
+			print "replacing previous link $foundlink\n" if ($foundlink);
+			system `rm $destination\/path\/$fname` if $foundlink;
+			print " linking $ffname\n";
+			symlink("$ffname","$destination\/path\/$fname");
+		    }
+		    if(($fname ne '.') and ($fname ne "..") and -d $ffname)
+		    {
+			print "verifying directory $ffname\n";
+                        # make the dir if it does not exist
+			print $destination."/path/".$fname."\n";
+			$ddest = $destination."/path/".$fname;
+			print "check existence of $ddest\n";
+			mkdir ($ddest) unless -e $ddest;
+			print "checking subdirs of $fname\n";
+                        # go one level down and readdir
+			opendir( SDIR, $ffname);
+			@subdirs = readdir(SDIR);
+			foreach $dname (@subdirs)
+			{
+			    my $ddname = $ffname."/".$dname;
+			    $foundlink = readlink "$ddest\/$dname";
+			    print "replacing previous link $foundlink\n" if ($foundlink);
+			    system `rm -r $ddest\/$dname` if $foundlink;
+			
+			    symlink("$ddname","$destination\/path\/$fname\/$dname") if -d $ddname;
+			    print " linking directory $ddname\n" if -d $ddname; 
+			    symlink("$ddname","$destination\/path\/$fname\/$dname") if (grep {(/\.py/ || /\.pyc/)} $dname);
+			    print " linking file $dname\n" if (grep {(/\.py/ || /\.pyc/)} $dname);
+			}
+		    }
+		}
+		pop(@tokens);
+		
+	    }
+	    
+	}
+    }
+
 }
