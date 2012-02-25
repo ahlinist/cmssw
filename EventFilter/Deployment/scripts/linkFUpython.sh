9637a988cb3ecc1f10e274fc5a8f436dcc82ef6a
fwyzard <>
2012-02-25 17:12:02 +0000

    test...

--- EventFilter/Deployment/scripts/linkFUpython.sh
+++ EventFilter/Deployment/scripts/linkFUpython.sh
@@ -16,9 +16,15 @@ mkdir -p $TARGET/home $TARGET/path
 
 # traverse the PYTHONHOME defined in $ENVFILE in reverse order
 for DIR in $( unset PYTHONHOME; source $ENVFILE; echo $PYTHONHOME | tr : \\n | tac ); do
+    echo -e "\t************************************************"
+    echo -e "\tPYTHONHOME part: $DIR"
     cp -r -s -f $DIR/* $TARGET/home/
+    echo -e "\t************************************************"
 done
 # traverse the PYTHONPATH defined in $ENVFILE in reverse order
 for DIR in $( unset PYTHONPATH; source $ENVFILE; echo $PYTHONPATH | tr : \\n | tac ); do
+    echo -e "\t************************************************"
+    echo -e "\tPYTHONPATH part: $DIR"
     cp -r -s -f $DIR/* $TARGET/path/
+    echo -e "\t************************************************"
 done
