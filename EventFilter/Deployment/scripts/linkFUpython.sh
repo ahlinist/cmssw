2e2e512c36c20e1695ef519b49ca034dcf36007a
fwyzard <>
2012-02-25 17:33:17 +0000

    remove debug stuff

--- EventFilter/Deployment/scripts/linkFUpython.sh
+++ EventFilter/Deployment/scripts/linkFUpython.sh
@@ -4,27 +4,22 @@ ENVFILE=$1
 TARGET=$2
 
 [ -f "$ENVFILE" ] || {
-    echo "ERROR: file $ENVFILE does not exist or cannot be read" 1>&2
-    exit 1
+  echo "ERROR: file $ENVFILE does not exist or cannot be read" 1>&2
+  exit 1
 }
 grep -q "PYTHONPATH=" $ENVFILE || {
-    echo "ERROR: file $ENVFILE does not define PYTHONPATH" 1>&2
-    exit 1
+  echo "ERROR: file $ENVFILE does not define PYTHONPATH" 1>&2
+  exit 1
 }
 
 mkdir -p $TARGET/home $TARGET/path
 
 # traverse the PYTHONHOME defined in $ENVFILE in reverse order
 for DIR in $( unset PYTHONHOME; source $ENVFILE; echo $PYTHONHOME | tr : \\n | tac ); do
-    echo -e "\t************************************************" >> /tmp/fwyzard/log
-    echo -e "\tPYTHONHOME part: $DIR" >> /tmp/fwyzard/log
-    cp -r -s -f $DIR/* $TARGET/home/
-    echo -e "\t************************************************" >> /tmp/fwyzard/log
+  cp -r -s -f $DIR/* $TARGET/home/
 done
+
 # traverse the PYTHONPATH defined in $ENVFILE in reverse order
 for DIR in $( unset PYTHONPATH; source $ENVFILE; echo $PYTHONPATH | tr : \\n | tac ); do
-    echo -e "\t************************************************" >> /tmp/fwyzard/log
-    echo -e "\tPYTHONPATH part: $DIR" >> /tmp/fwyzard/log
-    cp -r -s -f $DIR/* $TARGET/path/
-    echo -e "\t************************************************" >> /tmp/fwyzard/log
+  cp -r -s -f $DIR/* $TARGET/path/
 done
