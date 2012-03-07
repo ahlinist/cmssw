90b6c5419d3fa1fe10e42745218d1e1674bddf48
veelken <>
2012-03-07 15:45:01 +0000

    first version of likelihood plugin taking mass + dPhi dependency of visPtCut effect into account

--- TauAnalysis/CandidateTools/interface/NSVfitSingleParticleLikelihood.h
+++ TauAnalysis/CandidateTools/interface/NSVfitSingleParticleLikelihood.h
@@ -8,9 +8,9 @@
  *
  * \author Christian Veelken, UC Davis
  *
- * \version $Revision: 1.6 $
+ * \version $Revision: 1.7 $
  *
- * $Id: NSVfitSingleParticleLikelihood.h,v 1.6 2012/02/28 09:44:08 veelken Exp $
+ * $Id: NSVfitSingleParticleLikelihood.h,v 1.7 2012/02/28 09:44:59 veelken Exp $
  *
  */
 
@@ -18,11 +18,14 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "FWCore/Utilities/interface/Exception.h"
+
 #include "TauAnalysis/CandidateTools/interface/NSVfitLikelihoodBase.h"
 
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesis.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesis.h"
 
-#include <TF1.h>
+#include <TFormula.h>
 #include <TString.h>
 
 #include <string>
@@ -40,27 +43,13 @@ class NSVfitSingleParticleLikelihood : public NSVfitLikelihoodBase
       cfg.getParameter<bool>("applyVisPtCutCorrection") : false;
     if ( applyVisPtCutCorrection_ ) {
       edm::ParameterSet cfgVisPtCutCorrection = cfg.getParameter<edm::ParameterSet>("visPtCutCorrection");
-      std::string formula = cfgVisPtCutCorrection.getParameter<std::string>("formula");
-      visPtCutCorr_xMin_ = cfgVisPtCutCorrection.getParameter<double>("xMin");
-      visPtCutCorr_xMax_ = cfgVisPtCutCorrection.getParameter<double>("xMax");
-      std::string name = Form("%s_visPtCutCorrFunction", pluginName_.data());
-      visPtCutCorrFunction_ = new TF1(name.data(), formula.data(), visPtCutCorr_xMin_, visPtCutCorr_xMax_);
-      int numParameter = visPtCutCorrFunction_->GetNpar();
-      for ( int iParameter = 0; iParameter < numParameter; ++iParameter ) {
-	std::string parName = Form("par%i", iParameter);
-	edm::ParameterSet cfgPar = cfgVisPtCutCorrection.getParameter<edm::ParameterSet>(parName);
-	std::string name = Form("%s_visPtCutCorrFunction_par%i", pluginName_.data(), iParameter);
-	visPtCutCorrParameters_.push_back(new massDepParameterType(iParameter, name, cfgPar));
-      }
+      std::string visPtCutCorrFunctionName = Form("%s_visPtCutCorrFunction", pluginName_.data());
+      visPtCutCorrFunction_ = new visPtCutCorrType_formula(visPtCutCorrFunctionName.data(), cfgVisPtCutCorrection);
     }
   }
   virtual ~NSVfitSingleParticleLikelihood() 
   {
     delete visPtCutCorrFunction_;
-    for ( std::vector<massDepParameterType*>::iterator it = visPtCutCorrParameters_.begin();
-	  it != visPtCutCorrParameters_.end(); ++it ) {
-      delete (*it);
-    }
   }
 
   virtual void beginCandidate(const NSVfitSingleParticleHypothesis*) {}
@@ -74,57 +63,120 @@ class NSVfitSingleParticleLikelihood : public NSVfitLikelihoodBase
   {
 //--- compute multiplicative correction to tau decay likelihood
 //    in order to account for effect of visible Pt cuts on x (= visible/tau lepton enery) distribution
-    double x = hypothesis->p4().energy()/hypothesis->p4_fitted().energy();
-    if ( x < 0. ) x = 0.;
-    if ( x > 1. ) x = 1.;
-    double mass = hypothesis->p4_fitted().mass();
-    if ( mass < visPtCutCorr_xMin_ ) mass = visPtCutCorr_xMin_;
-    if ( mass > visPtCutCorr_xMax_ ) mass = visPtCutCorr_xMax_;
-    for ( std::vector<massDepParameterType*>::const_iterator visPtCutCorrParameter = visPtCutCorrParameters_.begin();
-	  visPtCutCorrParameter != visPtCutCorrParameters_.end(); ++visPtCutCorrParameter ) {
-      visPtCutCorrFunction_->SetParameter((*visPtCutCorrParameter)->idx_, (*visPtCutCorrParameter)->evaluate(mass));
-    }
-    return visPtCutCorrFunction_->Eval(x);
+    return visPtCutCorrFunction_->eval(hypothesis);
   }
 
-  struct massDepParameterType
+  struct visPtCutCorrTypeBase
   {
-    massDepParameterType(int idx, const std::string& name, const edm::ParameterSet& cfg)
-      : idx_(idx),
-	function_(0)
+    visPtCutCorrTypeBase(const std::string& name, const edm::ParameterSet& cfg)
+      : function_(0)
     {
+      variable_ = cfg.getParameter<std::string>("variable");
       std::string formula = cfg.getParameter<std::string>("formula");
+      std::string functionName = Form("%s_formula", name.data());
+      function_ = new TFormula(functionName.data(), formula.data());
       xMin_ = cfg.getParameter<double>("xMin");
       xMax_ = cfg.getParameter<double>("xMax");
-      function_ = new TF1(name.data(), formula.data(), xMin_, xMax_);
-      int numParameter = function_->GetNpar();
-      for ( int iParameter = 0; iParameter < numParameter; ++iParameter ) {
-	std::string parName = Form("par%i", iParameter);
-	double parValue = cfg.getParameter<double>(parName);
-	function_->SetParameter(iParameter, parValue);
-      }
+      numParameter_ = function_->GetNpar();           
     }
-    ~massDepParameterType() 
-    { 
-      delete function_; 
-    }
-    double evaluate(double mass) const
+    virtual ~visPtCutCorrTypeBase() 
     {
-      double x = mass;
-      if ( x < xMin_ ) x = xMin_;
-      if ( x > xMax_ ) x = xMax_;
-      return function_->Eval(x);
+      delete function_;
     }
-    int idx_;
-    TF1* function_;
+
+    virtual double eval(const NSVfitSingleParticleHypothesis* hypothesis) const = 0;
+    
+    double get_x(const NSVfitSingleParticleHypothesis* hypothesis) const
+    {
+      double x = 0.;
+      if ( variable_ == "X"    ) {
+	x = hypothesis->p4().energy()/hypothesis->p4_fitted().energy();
+      } else if ( variable_ == "dPhi" ) {
+	const NSVfitResonanceHypothesis* resonance = hypothesis->mother();
+	assert(resonance);	
+	x = resonance->dPhiVis();
+      } else if ( variable_ == "mass" ) {
+	const NSVfitResonanceHypothesis* resonance = hypothesis->mother();
+	assert(resonance);	
+	x = resonance->p4_fitted().mass();
+      } else assert(0);
+      if      ( x < xMin_ ) x = xMin_;
+      else if ( x > xMax_ ) x = xMax_;
+      return x;
+    }
+    
+    std::string variable_;
+    TFormula* function_;
     double xMin_;
     double xMax_;
+    int numParameter_;
   };
 
-  mutable TF1* visPtCutCorrFunction_;
-  std::vector<massDepParameterType*> visPtCutCorrParameters_;
-  double visPtCutCorr_xMin_;
-  double visPtCutCorr_xMax_;
+  struct visPtCutCorrType_const : public visPtCutCorrTypeBase
+  {
+    visPtCutCorrType_const(const std::string& name, const edm::ParameterSet& cfg)
+      : visPtCutCorrTypeBase(name, cfg)
+    {      
+      edm::ParameterSet cfgPars = cfg.getParameter<edm::ParameterSet>("parameter");
+      parameter_.resize(numParameter_);
+      for ( int iParameter = 0; iParameter < numParameter_; ++iParameter ) {
+	parameter_[iParameter] = cfgPars.getParameter<double>(Form("par%i", iParameter));
+	function_->SetParameter(iParameter, parameter_[iParameter]);
+      }
+    }
+    ~visPtCutCorrType_const() 
+    {}
+
+    double eval(const NSVfitSingleParticleHypothesis* hypothesis) const
+    {
+      double x = get_x(hypothesis);
+      return function_->Eval(x);   
+    }
+
+    std::vector<double> parameter_;
+  };
+
+  struct visPtCutCorrType_formula : public visPtCutCorrTypeBase
+  {
+    visPtCutCorrType_formula(const std::string& name, const edm::ParameterSet& cfg)
+      : visPtCutCorrTypeBase(name, cfg)
+    {    
+      edm::ParameterSet cfgPars = cfg.getParameter<edm::ParameterSet>("parameter");
+      parameter_.resize(numParameter_);
+      for ( int iParameter = 0; iParameter < numParameter_; ++iParameter ) {
+	edm::ParameterSet cfgPar = cfgPars.getParameter<edm::ParameterSet>(Form("par%i", iParameter));
+	std::string parName = Form("%s_par%i", name.data(), iParameter);
+	// CV: determine type of parameters on next level
+	edm::ParameterSet cfgPar_next = cfgPar.getParameter<edm::ParameterSet>("parameter");
+	if ( cfgPar_next.existsAs<double>("par0") ) {
+	  parameter_[iParameter] = new visPtCutCorrType_const(parName, cfgPar);
+	} else if ( cfgPar_next.existsAs<edm::ParameterSet>("par0") ) {
+	  parameter_[iParameter] = new visPtCutCorrType_formula(parName, cfgPar);
+	} else throw cms::Exception("NSVfitSingleParticleLikelihood") 
+	    << "Failed to determine type of parameter needed by function = " << function_->GetName() << " !!\n";
+      }
+    }
+    ~visPtCutCorrType_formula() 
+    {
+      for ( std::vector<visPtCutCorrTypeBase*>::iterator it = parameter_.begin();
+	    it != parameter_.end(); ++it ) {
+	delete (*it);
+      }
+    }
+
+    double eval(const NSVfitSingleParticleHypothesis* hypothesis) const
+    {      
+      for ( int iParameter = 0; iParameter < numParameter_; ++iParameter ) {
+        function_->SetParameter(iParameter, parameter_[iParameter]->eval(hypothesis));
+      }
+      double x = get_x(hypothesis);
+      return function_->Eval(x);      
+    }
+
+    std::vector<visPtCutCorrTypeBase*> parameter_;
+  };
+
+  mutable visPtCutCorrType_formula* visPtCutCorrFunction_;
   bool applyVisPtCutCorrection_;
 };
 
