1ba08999fe1a045b6a70e2095eff6efc41ecad57
veelken <>
2012-03-13 15:28:34 +0000

    merging Lorenzo's changes with old cvs head

--- TauAnalysis/CandidateTools/plugins/NSVfitTauToLepLikelihoodMatrixElement.cc
+++ TauAnalysis/CandidateTools/plugins/NSVfitTauToLepLikelihoodMatrixElement.cc
@@ -15,7 +15,8 @@ template <typename T>
 NSVfitTauToLepLikelihoodMatrixElement<T>::NSVfitTauToLepLikelihoodMatrixElement(const edm::ParameterSet& cfg)
   : NSVfitSingleParticleLikelihood(cfg)
 {
-  if ( this->verbosity_ ) std::cout << "<NSVfitTauToLepLikelihoodMatrixElement::NSVfitTauToLepLikelihoodMatrixElement>:" << std::endl;
+  applySinThetaFactor_ = cfg.exists("applySinThetaFactor") ?
+    cfg.getParameter<bool>("applySinThetaFactor") : false;
 }
 
 template <typename T>
@@ -52,59 +53,49 @@ double NSVfitTauToLepLikelihoodMatrixElement<T>::operator()(const NSVfitSinglePa
 
   if ( this->verbosity_ ) std::cout << "<NSVfitTauToLepLikelihoodMatrixElement::operator()>:" << std::endl;
   
-  double chargedLepMass2 = square(hypothesis_T->p4().mass());         // electron/muon mass
-  double Emax = (tauLeptonMass2 + chargedLepMass2)/(2*tauLeptonMass); // formula (2.6)
-  double E = hypothesis_T->p4vis_rf().energy();                       // electron/muon energy (in tau lepton rest-frame)
-  double p = hypothesis_T->p4vis_rf().P();                            // electron/muon momentum (in tau lepton rest-frame)
-  double theta = hypothesis_T->decay_angle_rf();
-  double sinTheta = TMath::Sin(theta);
+  double decayAngle = hypothesis_T->decay_angle_rf();
   double nuMass = hypothesis_T->p4invis_rf().mass();
+  if ( nuMass < 0. ) nuMass = 0.; // CV: add protection against rounding errors when boosting between laboratory and rest frame
+  double nuMass2 = square(nuMass);
   double visEnFracX = hypothesis_T->visEnFracX();
 
   if ( this->verbosity_ ) {
-    std::cout << " chargedLepMass2 = " << chargedLepMass2 << std::endl;
-    std::cout << " Emax = " << Emax << std::endl;
-    std::cout << " E = " << E << std::endl;
-    std::cout << " p = " << p << std::endl;
-    std::cout << " theta = " << theta << std::endl;
+    std::cout << " decayAngle = " << decayAngle << std::endl;
     std::cout << " nuMass = " << nuMass << std::endl;
+    std::cout << " visEnFracX = " << visEnFracX << std::endl;
   }
 
-  /*
-  double prob = p*E*(3.*Emax - 2.*E - (chargedLepMass2/E))*sinTheta*(nuMass/tauLeptonMass); // formula (2.5)
-
+  // LB: normalize likelihood function such that 
+  //               1
+  //       integral  prob dX dMnunu = 1.
+  //               0
+  double prob = 1.;
+  if ( nuMass < TMath::Sqrt((1. - visEnFracX)*tauLeptonMass2) ) { // LB: physical solution
+    prob = (13./square(tauLeptonMass2))*cube(tauLeptonMass2 - nuMass2)*(tauLeptonMass2 + 2.*nuMass2)*nuMass;
+  } else {                                                        // LB: unphysical solution
+    prob = 1e-6;
+  }
+  if ( applySinThetaFactor_ ) prob *= (0.5*TMath::Sin(decayAngle));
+  
   if ( applyVisPtCutCorrection_ ) {
-    double probCorr = evaluateVisPtCutCorrection(hypothesis);
+    double probCorr = 1.;
+    if ( hypothesis_T->p4_fitted().pt() > visPtCutThreshold_ ) {
+      double xCut = visPtCutThreshold_/hypothesis_T->p4_fitted().pt();
+      probCorr = 1./(3. - 5.*xCut + 3.*cube(xCut) - fourth(xCut));
+    } else {
+      probCorr = 1e-6;
+    }
     if ( this->verbosity_ ) std::cout << "probCorr (lep) = " << probCorr << std::endl;
     prob *= probCorr;
   }
-  */
-
-  //hardcoded for the moment
-  double xcut       = 15./(hypothesis_T->p4_fitted()).Pt();
-  double ptCutsNorm = 3 - 5*xcut + 3*xcut*xcut*xcut - xcut*xcut*xcut*xcut;
-  double prob       = 52./4./tauLeptonMass2*tauLeptonMass2*(tauLeptonMass2-nuMass*nuMass)*(tauLeptonMass2-nuMass*nuMass)*(tauLeptonMass2-nuMass*nuMass)*(tauLeptonMass2+2*nuMass*nuMass)*nuMass;
-  
-  if(applyVisPtCutCorrection_){
-    prob /= ptCutsNorm;
-    if(visEnFracX<xcut || xcut>1) prob=1e-06;
-  }
-  
-  if( nuMass>TMath::Sqrt((1-visEnFracX)*tauLeptonMass2) || nuMass<0 ) prob = 1e-06;
-
 
   double nll = 0.;
   if ( prob > 0. ) {
     nll = -TMath::Log(prob);
   } else {
-    if ( prob < 0. ) 
-
-      std::cout << "xcut= " << xcut << ", " << "visEnFracX= " << visEnFracX << ", nuMass=" << nuMass << std::endl; 
-      edm::LogWarning ("NSVfitTauToLepLikelihoodMatrixElement::operator()")
-	<< " Unphysical solution: prob = " << prob << " --> returning very large negative number !!";
     nll = std::numeric_limits<float>::max();
   }
-
+  
   if ( this->verbosity_ ) std::cout << "--> nll = " << nll << std::endl;
 
   return nll;
