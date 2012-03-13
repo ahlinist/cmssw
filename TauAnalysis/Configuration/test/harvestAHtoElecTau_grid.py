5c5ac234ef647903856728fa30d5906e8c1db6c8
jkolb <>
2012-03-13 15:14:19 +0000

    just keeping CVS up to date with work area

--- TauAnalysis/Configuration/test/harvestAHtoElecTau_grid.py
+++ TauAnalysis/Configuration/test/harvestAHtoElecTau_grid.py
@@ -2,15 +2,17 @@
 
 from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import recoSampleDefinitionsAHtoElecTau
 from TauAnalysis.Configuration.harvestAnalysisResults_grid import harvestAnalysisResults
-from TauAnalysis.Configuration.userRegistry import getAnalysisFilePath, getHarvestingFilePath, getJobId, getTmpFilePath
+import TauAnalysis.Configuration.userRegistry as reg
 
 channel = 'AHtoElecTau'
-analysisFilePath = getAnalysisFilePath(channel)
-harvestingFilePath = getHarvestingFilePath(channel)
-jobId = getJobId(channel)
-tmpFilePath = getTmpFilePath(channel)
+reg.overrideJobId(channel,"Run41")
+analysisFilePath = reg.getAnalysisFilePath(channel)
+harvestingFilePath = reg.getHarvestingFilePath(channel)
+jobId = reg.getJobId(channel)
+tmpFilePath = reg.getTmpFilePath(channel)
 
 harvestAnalysisResults(channel = channel, samples = recoSampleDefinitionsAHtoElecTau,
                        inputFilePath = analysisFilePath, outputFilePath = harvestingFilePath, jobId = jobId,
                        tmpFilePath = tmpFilePath, ana_defs = None, plot_defs = None, plotters = None,
-                       use_job_report = True)
+                       use_job_report = True,
+                       useCastor = False)
--- TauAnalysis/Configuration/test/makeAHtoElecTauPlots_grid_cfg.py
+++ TauAnalysis/Configuration/test/makeAHtoElecTauPlots_grid_cfg.py
@@ -7,10 +7,9 @@ from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi \
         import recoSampleDefinitionsAHtoElecTau
 
 from TauAnalysis.Configuration.plotAHtoElecTau_drawJobs_cfi import \
-        drawJobConfigurator_AHtoElecTau_woBtag, plots_AHtoElecTau, \
-        drawJobConfigurator_AHtoElecTau_wBtag,\
-        drawJobConfigurator_AHtoElecTau_woBtagSS,\
-        drawJobConfigurator_AHtoElecTau_wBtagSS
+        drawJobConfiguratorDict,\
+        plots_AHtoElecTau,\
+        useSSdataForQCD
 
 from TauAnalysis.Configuration.makePlots2_grid import makePlots
 import TauAnalysis.Configuration.userRegistry as reg
@@ -23,11 +22,9 @@ process = cms.Process('makeAHtoElecTauPlots')
 
 channel = 'AHtoElecTau'
 
-#reg.overrideJobId(channel, '2010Dec23_lxbatch')
-#reg.overrideJobId(channel, 'Run33')
-#reg.overrideJobId(channel, 'Run37sysTanc')
-#reg.overrideJobId(channel, 'Run37sys')
-#reg.overrideJobId(channel, 'Run42OldTaNCfinal')
+categories = ['wBtag','woBtag','ZeroOneJets','VBF','Boosted']
+
+reg.overrideJobId(channel, 'Run41')
 
 inputFilePath = reg.getHarvestingFilePath(channel)
 jobId = reg.getJobId(channel)
@@ -35,23 +32,17 @@ jobId = reg.getJobId(channel)
 plotsDirectory = './plots'
 outputFileNameMaker = lambda channel: 'plots%s_all.root' % channel
 
-analyzer_draw_jobs = [
-    [ "ahElecTauAnalyzerOS_woBtag",
-     drawJobConfigurator_AHtoElecTau_woBtag,
-     "plotAHtoElecTauOS_woBtag_#PLOT#.png" ],
-    [ "ahElecTauAnalyzerOS_wBtag",
-     drawJobConfigurator_AHtoElecTau_wBtag,
-     "plotAHtoElecTauOS_wBtag_#PLOT#.png"  ],
+analyzer_draw_jobs = []
 
-    [ "ahElecTauAnalyzerSS_woBtag",
-     drawJobConfigurator_AHtoElecTau_woBtagSS,
-     "plotAHtoElecTauSS_woBtag_#PLOT#.png" ],
+for cat in categories:
+    for sign in ['OS','SS']:
+        analyzer_draw_jobs.append(
+            [ 'ahElecTauAnalyzer' + sign + '_' + cat,
+            drawJobConfiguratorDict[sign+'_'+cat],
+            'plotAHtoElecTauOS_' + cat + '_#PLOT#.png'])
 
-    [ "ahElecTauAnalyzerSS_wBtag",
-     drawJobConfigurator_AHtoElecTau_wBtagSS,
-     "plotAHtoElecTauSS_wBtag_#PLOT#.png"  ],
-]
 
+# attach FilterStatistics dump modules to process
 process.load("TauAnalysis.Configuration.dumpAHtoElecTau_grid_cff")
 
 makePlots(process, channel = channel,
@@ -63,7 +54,7 @@ makePlots(process, channel = channel,
 		  #skimStatFileMapper = reg.makeSkimStatFileMapper(channel),
 		  #skimFilterStatistic = 'evtSelDiMuPairZmumuHypothesisVetoByLooseIsolation',
           drawJobTemplate = plots_AHtoElecTau,
-          enableFactorizationFunction = enableFactorization_makeAHtoElecTauPlots_grid2,
+          #enableFactorizationFunction = enableFactorization_makeAHtoElecTauPlots_grid2,
           dqmDirectoryFilterStatistics = {
               'factorizationDisabled' : 'ahElecTauAnalyzerOS_woBtag/FilterStatistics',
               'factorizationEnabled' : 'ahElecTauAnalyzerOS_woBtag_factorizedWithElectronIsolation/FilterStatistics'
@@ -72,5 +63,70 @@ makePlots(process, channel = channel,
           outputFileNameMaker = outputFileNameMaker,
           plotsDirectory = plotsDirectory)
 
+
+bgYieldCorrections = {
+    'woBtag' : {
+        #  desired number / lumi-weighted number (sum if necessary) / processing inefficiency
+        'ZtautauEmbeddedSum' : 12232./12548, 
+        'ZtautauSum' : 12232./12548, 
+        'ZeeSum' : 6974./6104,
+        'WplusJets_madgraph_skim' : 2512./4903,
+        'VVsum' : 0.01/50.7,
+        'TTplusJets_madgraph_skim' : 58.8/30.2,
+        'qcdFromData' : 6536./8189 # desired QCD sum / SS data 
+    },
+    'wBtag' : {
+        #  desired number / lumi-weighted number (sum if necessary) / processing inefficiency
+        'ZtautauSum' : 151.2/132.6, 
+        'ZeeSum' : 56.3/46.1,
+        'WplusJets_madgraph_skim' : 42./65.5,
+        'VVsum' : 0.01/1.2,
+        'TTplusJets_madgraph_skim' : 78.3/62.2,
+        'qcdFromData' : 36./122 # desired QCD sum / SS data 
+    },
+    'VBF' : {
+        #  desired number / lumi-weighted number (sum if necessary) / processing inefficiency
+        'ZtautauSum' : 25.9/19.5, 
+        'ZeeSum' : 28.0/15.2,
+        'WplusJets_madgraph_skim' : 0.01/9.2,
+        'VVsum' : 0.01/0.2,
+        'TTplusJets_madgraph_skim' : 5.4/12.8,
+        'qcdFromData' : 8.47/37 # desired QCD sum / SS data 
+    },
+    'ZeroOneJets' : {
+        #  desired number / lumi-weighted number (sum if necessary) / processing inefficiency
+        'ZtautauEmbeddedSum' : 11310./11930, 
+        'ZtautauSum' : 11310./11930, 
+        'ZeeSum' : 6786./6014,
+        'WplusJets_madgraph_skim' : 1509./2839,
+        'VVsum' : 60./31,
+        'TTplusJets_madgraph_skim' : 0.01/43.6,
+        'qcdFromData' : 5455./7656.5 # desired QCD sum / SS data 
+    },
+    'Boosted' : {
+        #  desired number / lumi-weighted number (sum if necessary) / processing inefficiency
+        'ZtautauSum' : 67./65, 
+        'ZeeSum' : 4.4/8,
+        'WplusJets_madgraph_skim' : 0.01/25,
+        'TTplusJets_madgraph_skim' : 2.8/2.3,
+        'VVsum' : 1.1/0.6,
+        'qcdFromData' : 0.001/5 # desired QCD sum / SS data 
+    }
+}
+
+# use SS data events for QCD shape
+useSSdataForQCD(process, recoSampleDefinitionsAHtoElecTau, channel, categories, jobId, bgYieldCorrections)
+
+# load from previously-produced plots file
+process.loadFinalAHtoElecTauSamples = cms.EDAnalyzer("DQMFileLoader",
+    inputFilePath = cms.string('/afs/crc.nd.edu/group/NDCMS/data02/jkolb/TauResults/' + jobId),
+    all = cms.PSet(
+        inputFileNames = cms.vstring('plotsAHtoElecTau_all.root'),
+        dqmDirectory_store = cms.string('/'),
+        scaleFactor = cms.double(1.0)
+    )
+)
+#process.p = cms.Path(process.loadFinalAHtoElecTauSamples + process.plotAHtoElecTauSequence)
+
 # print-out all python configuration parameter information
-#print process.dumpPython()
+print process.dumpPython()
