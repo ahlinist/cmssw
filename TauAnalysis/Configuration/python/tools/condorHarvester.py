2b12b8fcc658604cff149b3ca855f726d0d20019
jkolb <>
2012-03-14 13:37:03 +0000

    Sean's updates

--- TauAnalysis/Configuration/python/tools/condorHarvester.py
+++ TauAnalysis/Configuration/python/tools/condorHarvester.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys,os,stat,subprocess,time
+import TauAnalysis.Configuration.userRegistry as reg
 
 if 'condor' not in os.listdir('.'):
     print 'Creating directory "condor"'
@@ -16,30 +17,31 @@ subFile = open(submitFileName,'w')
 jobFile = open(jobFileName,'w')
 
 subFile.write('universe = vanilla\n')
-subFile.write('log = condor/' + baseName + '.log\n')
-subFile.write('output = condor/' + baseName + '.out\n')
-subFile.write('executable = ' + jobFileName + '\n')
+subFile.write('log = '+os.getcwd()+'/condor/' + baseName + '.log\n')
+subFile.write('output = '+os.getcwd()+'/condor/' + baseName + '.out\n')
+subFile.write('executable = '+os.getcwd()+ '/' +jobFileName + '\n')
 subFile.write('getenv = true\n')
 subFile.write('Queue 1\n')
 subFile.write('\n')
 subFile.close()
 
 jobFile.write('#!/bin/sh\n')
-jobFile.write('cd ~/work/Run' + run + '\n')
-jobFile.write('TauAnalysis/Configuration/python/tools/genericHarvester.py ' + '%s' % ' '.join(sys.argv[1:]) +'\n')
+jobFile.write('$CMSSW_BASE/src/TauAnalysis/Configuration/python/tools/genericHarvester.py ' + '%s' % ' '.join(sys.argv[1:]) +'\n')
 jobFile.write('return 0')
 jobFile.close()
 
 os.system('chmod 777 ' + jobFileName)
 
 print 'Submitting ' + submitFileName + ' to condor\n'
-condorJobID =  subprocess.Popen(['condor_submit','./'+submitFileName],stdout=subprocess.PIPE).communicate()[0].split('\n')[2].split(' ')[-1].rstrip('.')
+submitOutput = subprocess.Popen(['condor_submit','./'+submitFileName],stdout=subprocess.PIPE).communicate()[0]
+print submitOutput
+condorJobID =  submitOutput.split('\n')[2].split(' ')[-1].rstrip('.')
 
 print 'Running as condor job ' + condorJobID + '\n'
 finished = False
 while not finished:
-    if condorJobID not in subprocess.Popen(['condor_q','jkolb'],stdout=subprocess.PIPE).communicate()[0]:
+    if condorJobID not in subprocess.Popen(['condor_q',os.environ['USER']],stdout=subprocess.PIPE).communicate()[0]:
         finished = True
-    time.sleep(30)
+    time.sleep(120)
 print submitFileName + ' has finished\n'
 
