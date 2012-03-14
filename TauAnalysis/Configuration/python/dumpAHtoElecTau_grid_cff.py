bccd8c1ef043dfc90be047610e1405e4adbafd83
jkolb <>
2012-03-14 14:50:17 +0000

    add all categories

--- TauAnalysis/Configuration/python/dumpAHtoElecTau_grid_cff.py
+++ TauAnalysis/Configuration/python/dumpAHtoElecTau_grid_cff.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+import copy
 
 from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import ALL_SAMPLES as all_samples
 from TauAnalysis.Configuration.recoSampleDefinitionsAHtoElecTau_grid_cfi import SAMPLES_TO_PRINT as samples_to_print
@@ -16,36 +17,52 @@ def sample_dqm_name(sample):
         return sample
 
 dumpAHtoElecTauOS_woBtag = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        **dict(
-            (sample, cms.string(
-                '/harvested/%s/ahElecTauAnalyzerOS_woBtag/FilterStatistics' 
-                % sample_dqm_name(sample))
-            ) for sample in samples_to_print)
-    ), 
-    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
-    #columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency", "indiv. Efficiency")
+        dqmDirectories = cms.PSet(
+            **dict(
+                (sample, cms.string(
+                    '/harvested/%s/ahElecTauAnalyzerOS_woBtag/FilterStatistics' 
+                    % sample_dqm_name(sample))
+                    ) for sample in samples_to_print)
+                ), 
+        columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
 )
-
-
 dumpAHtoElecTauOS_wBtag = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        **dict(
-            (sample, cms.string(
-                '/harvested/%s/ahElecTauAnalyzerOS_wBtag/FilterStatistics' 
-                % sample_dqm_name(sample))
-            ) for sample in samples_to_print)
-    ), 
-    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
+        dqmDirectories = cms.PSet(
+            **dict(
+                (sample, cms.string(
+                    '/harvested/%s/ahElecTauAnalyzerOS_wBtag/FilterStatistics' 
+                    % sample_dqm_name(sample))
+                    ) for sample in samples_to_print)
+                ), 
+        columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
 )
-
 dumpAHtoElecTauOS_VBF = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
-    dqmDirectories = cms.PSet(
-        **dict(
-            (sample, cms.string(
-                '/harvested/%s/ahElecTauAnalyzerOS_VBF/FilterStatistics' 
-                % sample_dqm_name(sample))
-            ) for sample in samples_to_print)
-    ), 
-    columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
+        dqmDirectories = cms.PSet(
+            **dict(
+                (sample, cms.string(
+                    '/harvested/%s/ahElecTauAnalyzerOS_VBF/FilterStatistics' 
+                    % sample_dqm_name(sample))
+                    ) for sample in samples_to_print)
+                ), 
+        columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
+)
+dumpAHtoElecTauOS_ZeroOneJets = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
+        dqmDirectories = cms.PSet(
+            **dict(
+                (sample, cms.string(
+                    '/harvested/%s/ahElecTauAnalyzerOS_ZeroOneJets/FilterStatistics' 
+                    % sample_dqm_name(sample))
+                    ) for sample in samples_to_print)
+                ), 
+        columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
+)
+dumpAHtoElecTauOS_Boosted = cms.EDAnalyzer("DQMDumpFilterStatisticsTables",
+        dqmDirectories = cms.PSet(
+            **dict(
+                (sample, cms.string(
+                    '/harvested/%s/ahElecTauAnalyzerOS_Boosted/FilterStatistics' 
+                    % sample_dqm_name(sample))
+                    ) for sample in samples_to_print)
+                ), 
+        columnsSummaryTable = cms.vstring("Passed", "cumul. Efficiency", "margin. Efficiency")
 )
