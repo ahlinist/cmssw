f93118760de07b0ecae401c4492c5135af2226fa
veelken <>
2012-03-14 09:53:38 +0000

    added plots

--- TauAnalysis/CandidateTools/plugins/NSVfitEventHypothesisAnalyzerT.cc
+++ TauAnalysis/CandidateTools/plugins/NSVfitEventHypothesisAnalyzerT.cc
@@ -6,20 +6,31 @@
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
+#include "DataFormats/METReco/interface/MET.h"
+#include "DataFormats/Common/interface/View.h"
+
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesisBase.h"
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesis.h"
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesisByIntegration.h"
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesis.h"
+#include "AnalysisDataFormats/TauAnalysis/interface/PFMEtSignCovMatrix.h"
+
+#include <TMatrixD.h>
+#include <TVectorD.h>
 
 template <typename T>
 NSVfitEventHypothesisAnalyzerT<T>::NSVfitEventHypothesisAnalyzerT(const edm::ParameterSet& cfg)
   : moduleLabel_(cfg.getParameter<std::string>("@module_label")),
-    plotsSVfitSolutionValid_(0),
-    plotsSVfitSolutionInvalid_(0),
     numEvents_processed_(0),
     numEventsWeighted_processed_(0.)
 {
-  src_ = cfg.getParameter<edm::InputTag>("src");
+  srcEventHypotheses_ = cfg.getParameter<edm::InputTag>("srcEventHypotheses");
+
+  srcGenLeg1_ = cfg.getParameter<edm::InputTag>("srcGenLeg1");
+  srcGenLeg2_ = cfg.getParameter<edm::InputTag>("srcGenLeg2");
+  srcGenMEt_ = cfg.getParameter<edm::InputTag>("srcGenMEt");
+
+  srcPFMEtCovMatrix_ = cfg.getParameter<edm::InputTag>("srcPFMEtCovMatrix");
 
   srcWeights_ = cfg.getParameter<vInputTag>("srcWeights");
 
@@ -29,7 +40,10 @@ NSVfitEventHypothesisAnalyzerT<T>::NSVfitEventHypothesisAnalyzerT(const edm::Par
 template <typename T>
 NSVfitEventHypothesisAnalyzerT<T>::~NSVfitEventHypothesisAnalyzerT()
 {
-// nothing to be done yet...
+  for ( typename std::vector<plotEntryType*>::iterator it = plotEntries_.begin();
+	it != plotEntries_.end(); ++it ) {
+    delete (*it);
+  }
 }
 
 template <typename T>
@@ -46,13 +60,17 @@ void NSVfitEventHypothesisAnalyzerT<T>::beginJob()
 
   svFitIsValidSolution_ = dqmStore.book1D("svFitIsValidSolution", "svFitIsValidSolution",  2, -0.5, 1.5);
 
-  std::string dqmDirectory_svFitSolutionValid = std::string(dqmDirectory_).append("/svFitSolutionValid");
-  plotsSVfitSolutionValid_ = new plotEntryType(dqmDirectory_svFitSolutionValid);
-  plotsSVfitSolutionValid_->bookHistograms(dqmStore);
-
-  std::string dqmDirectory_svFitSolutionInvalid = std::string(dqmDirectory_).append("/svFitSolutionInvalid");
-  plotsSVfitSolutionInvalid_ = new plotEntryType(dqmDirectory_svFitSolutionInvalid);
-  plotsSVfitSolutionInvalid_->bookHistograms(dqmStore);
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, -1., -1., +1));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, -1., -1., -1));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_,  0., 15.,  0));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, 15., 25.,  0));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, 25., 35.,  0));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, 35., 45.,  0));
+  plotEntries_.push_back(new plotEntryType(dqmDirectory_, 45., -1.,  0));
+  for ( typename std::vector<plotEntryType*>::iterator plotEntry = plotEntries_.begin();
+	plotEntry != plotEntries_.end(); ++plotEntry ) {
+    (*plotEntry)->bookHistograms(dqmStore);
+  }
 }
 
 namespace
@@ -61,14 +79,54 @@ namespace
   {
     return x*x;
   }
+
+  double compProjVecXY(const reco::Candidate::LorentzVector& p, const reco::Candidate::LorentzVector& dir)
+  {
+    double retVal = 0.;
+    if ( dir.pt() > 0. ) {
+      retVal = (p.px()*dir.px() + p.py()*dir.py())/dir.pt();
+    }
+    return retVal;
+  }
+
+  double compProjCovUncertaintyXY(const TMatrixD& cov, const reco::Candidate::LorentzVector& dir)
+  {
+    double retVal = 0.;
+    if ( dir.pt() > 0. ) {
+      retVal = TMath::Sqrt(dir.px()*(cov(0,0)*dir.px() + cov(0,1)*dir.py())
+			 + dir.py()*(cov(1,0)*dir.px() + cov(1,1)*dir.py()))/dir.pt();
+    }
+    return retVal;
+  }
 }
 
 template <typename T>
 void NSVfitEventHypothesisAnalyzerT<T>::analyze(const edm::Event& evt, const edm::EventSetup& es)
 {
   edm::Handle<NSVfitEventHypothesisCollection> svFitEventHypotheses;
-  evt.getByLabel(src_, svFitEventHypotheses);
+  evt.getByLabel(srcEventHypotheses_, svFitEventHypotheses);
   
+  typedef edm::View<reco::Candidate> CandidateView;
+  edm::Handle<CandidateView> genLeg1;
+  evt.getByLabel(srcGenLeg1_, genLeg1);
+  if ( !(genLeg1->size() == 1) ) return;
+  const reco::Candidate::LorentzVector& genLeg1P4 = genLeg1->front().p4();
+  edm::Handle<CandidateView> genLeg2;
+  evt.getByLabel(srcGenLeg2_, genLeg2);
+  if ( !(genLeg2->size() == 1) ) return;
+  const reco::Candidate::LorentzVector& genLeg2P4 = genLeg2->front().p4();
+
+  typedef edm::View<reco::MET> MEtView;
+  edm::Handle<MEtView> genMEt;
+  evt.getByLabel(srcGenMEt_, genMEt);
+  if ( !genMEt->size() == 1 ) 
+    throw cms::Exception("NSVfitEventHypothesisAnalyzer::analyze") 
+      << "Failed to find unique gen. MET object !!\n";
+  const reco::Candidate::LorentzVector& genMEtP4 = genMEt->front().p4();
+
+  edm::Handle<PFMEtSignCovMatrix> pfMEtSignCovMatrix;
+  evt.getByLabel(srcPFMEtCovMatrix_, pfMEtSignCovMatrix);
+
   double evtWeight = 1.0;
   for ( vInputTag::const_iterator srcWeight = srcWeights_.begin();
 	srcWeight != srcWeights_.end(); ++srcWeight ) {
@@ -118,15 +176,24 @@ void NSVfitEventHypothesisAnalyzerT<T>::analyze(const edm::Event& evt, const edm
       svFitMass_maxInterpol = (dynamic_cast<const NSVfitResonanceHypothesisByIntegration*>(svFitResonanceHypothesis))->mass_maxInterpol();
     }
 
-    if ( svFitIsValidSolution ) {
-      plotsSVfitSolutionValid_->fillHistograms(
-	svFitDaughter1P4, svFitDaughter2P4, svFitMass, svFitSigma, diTauPt, prodAngle_rf,
+    reco::Candidate::LorentzVector recMEtP4 = svFitEventHypothesis->met()->p4();
+    TVectorD covEigenValues(2);
+    pfMEtSignCovMatrix->EigenVectors(covEigenValues);
+    double metCov          = TMath::Power(square(covEigenValues(0)) + square(covEigenValues(1)), 0.25);
+    reco::Candidate::LorentzVector rec_minus_genMEtP4 = recMEtP4 - genMEtP4;
+    double metPull         = rec_minus_genMEtP4.pt()/compProjCovUncertaintyXY(*pfMEtSignCovMatrix, recMEtP4 - genMEtP4);
+    double metErrProjLeg1  = compProjVecXY(recMEtP4 - genMEtP4, svFitDaughter1P4);
+    double metPullProjLeg1 = metErrProjLeg1/compProjCovUncertaintyXY(*pfMEtSignCovMatrix, svFitDaughter1P4);
+    double metErrProjLeg2  = compProjVecXY(recMEtP4 - genMEtP4, svFitDaughter2P4);
+    double metPullProjLeg2 = metErrProjLeg2/compProjCovUncertaintyXY(*pfMEtSignCovMatrix, svFitDaughter2P4);
+    
+    for ( typename std::vector<plotEntryType*>::iterator plotEntry = plotEntries_.begin();
+	  plotEntry != plotEntries_.end(); ++plotEntry ) {
+      (*plotEntry)->fillHistograms(
+        svFitIsValidSolution,			     
+	genLeg1P4, svFitDaughter1P4, genLeg2P4, svFitDaughter2P4, svFitMass, svFitSigma, diTauPt, prodAngle_rf,
 	svFitMass_mean, svFitMass_median, svFitMass_maximum, svFitMass_maxInterpol, 
-	evtWeight);
-    } else {
-      plotsSVfitSolutionInvalid_->fillHistograms(
-        svFitDaughter1P4, svFitDaughter2P4, svFitMass, svFitSigma, diTauPt, prodAngle_rf,
-	svFitMass_mean, svFitMass_median, svFitMass_maximum, svFitMass_maxInterpol, 
+	metCov, rec_minus_genMEtP4, metPull, metErrProjLeg1, metPullProjLeg1, metErrProjLeg2, metPullProjLeg2, 
 	evtWeight);
     }
   }
@@ -142,6 +209,11 @@ void NSVfitEventHypothesisAnalyzerT<T>::endJob()
   //std::cout << " " << moduleLabel_ << ":"
   //	      << " num. Events processed = " << numEvents_processed_ << "," 
   //	      << " weighted = " << numEventsWeighted_processed_ << std::endl;
+
+  for ( typename std::vector<plotEntryType*>::iterator plotEntry = plotEntries_.begin();
+	plotEntry != plotEntries_.end(); ++plotEntry ) {
+    (*plotEntry)->finalizeHistograms();
+  }
 }
 
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitEventHypothesis.h"
