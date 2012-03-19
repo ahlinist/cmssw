e54545f9f105fdcbe7ff4bde9a2a62d89c3eb719
veelken <>
2012-03-19 09:14:53 +0000

    improved handling of polarization states

--- TauAnalysis/CandidateTools/plugins/NSVfitResonanceBuilder.cc
+++ TauAnalysis/CandidateTools/plugins/NSVfitResonanceBuilder.cc
@@ -37,47 +37,54 @@ NSVfitResonanceHypothesis* NSVfitResonanceBuilder::build(const inputParticleMap&
   resonance->numPolStates_ = numPolStates_;
 
 //--- set polarization status for daughters 
-  if ( numPolStates_ > 1 || polHandedness_[0] != NSVfitResonanceHypothesis::kPolUndefined ) {
-    if ( resonance->numDaughters() == 2 ) {
-      for ( size_t iDaughter = 0; iDaughter < resonance->numDaughters(); ++iDaughter ) {
-	NSVfitSingleParticleHypothesis* daughter = resonance->daughter(iDaughter);
-	daughter->polHandedness_.resize(numPolStates_);
-	daughter->polSign_.resize(numPolStates_);
-	daughter->numPolStates_ = numPolStates_;
-	for ( unsigned iPolState = 0; iPolState < numPolStates_; ++iPolState ) {
-	  int resonance_polHandedness = polHandedness_[iPolState];
-	  int daughter_polHandedness = -1;
-	  if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolUndefined ) {
-	    daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolUndefined;
-	  } else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolLR ) {
-	    if      ( iDaughter == 0 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
-	    else if ( iDaughter == 1 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
-	  } else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolRL ) {
-	    if      ( iDaughter == 0 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
-	    else if ( iDaughter == 1 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
-	  } else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolLL ) {
-	    daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
-	  } else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolRR ) {
-	    daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
-	  } 
-	  assert(daughter_polHandedness != -1);
-	  daughter->polHandedness_[iPolState] = daughter_polHandedness;
-	  int daughter_polSign = NSVfitResonanceHypothesis::kPolUndefined;
-	  // CV: left-handed  tau- and right-handed tau+ are assigned polarization -1,
-	  //     right-handed tau- and left-handed  tau+ are assigned polarization +1
-	  if ( daughter_polHandedness == NSVfitSingleParticleHypothesis::kPolL ) {
-	    if      ( daughter->particle()->charge() < -0.5 ) daughter_polSign = -1;
-	    else if ( daughter->particle()->charge() > +0.5 ) daughter_polSign = +1;
-	  } else if ( daughter_polHandedness == NSVfitSingleParticleHypothesis::kPolR ) {
-	    if      ( daughter->particle()->charge() < -0.5 ) daughter_polSign = +1;
-	    else if ( daughter->particle()->charge() > +0.5 ) daughter_polSign = -1;
-	  } 
-	  daughter->polSign_[iPolState] = daughter_polSign;
-	}
+  if ( resonance->numDaughters() == 2 ) {
+    for ( size_t iDaughter = 0; iDaughter < resonance->numDaughters(); ++iDaughter ) {
+      NSVfitSingleParticleHypothesis* daughter = resonance->daughter(iDaughter);
+      daughter->polHandedness_.resize(numPolStates_);
+      daughter->polSign_.resize(numPolStates_);
+      daughter->numPolStates_ = numPolStates_;
+      for ( unsigned iPolState = 0; iPolState < numPolStates_; ++iPolState ) {
+	int resonance_polHandedness = polHandedness_[iPolState];
+	int daughter_polHandedness = -1;
+	if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolUndefined ) {
+	  daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolUndefined;
+	} else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolLR ) {
+	  if      ( iDaughter == 0 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
+	  else if ( iDaughter == 1 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
+	} else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolRL ) {
+	  if      ( iDaughter == 0 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
+	  else if ( iDaughter == 1 ) daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
+	} else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolLL ) {
+	  daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolL;
+	} else if ( resonance_polHandedness == NSVfitResonanceHypothesis::kPolRR ) {
+	  daughter_polHandedness = NSVfitSingleParticleHypothesis::kPolR;
+	} 
+	assert(daughter_polHandedness != -1);
+	daughter->polHandedness_[iPolState] = daughter_polHandedness;
+	int daughter_polSign = 0;
+	// CV: left-handed  tau- and right-handed tau+ are assigned polarization -1,
+	//     right-handed tau- and left-handed  tau+ are assigned polarization +1
+	if ( daughter_polHandedness == NSVfitSingleParticleHypothesis::kPolL ) {
+	  if      ( daughter->particle()->charge() < -0.5 ) daughter_polSign = -1;
+	  else if ( daughter->particle()->charge() > +0.5 ) daughter_polSign = +1;
+	} else if ( daughter_polHandedness == NSVfitSingleParticleHypothesis::kPolR ) {
+	  if      ( daughter->particle()->charge() < -0.5 ) daughter_polSign = +1;
+	  else if ( daughter->particle()->charge() > +0.5 ) daughter_polSign = -1;
+	} 
+	daughter->polSign_[iPolState] = daughter_polSign;
       }
-    } else throw cms::Exception("NSVfitResonanceBuilder")
-	<< " Support for Polarization not implemented for case of " << resonance->numDaughters() << " daughters yet !!\n";
-  }
+    }
+  } else if ( numPolStates_ == 1 && polHandedness_[0] == NSVfitResonanceHypothesis::kPolUndefined ) {
+    for ( size_t iDaughter = 0; iDaughter < resonance->numDaughters(); ++iDaughter ) {
+      NSVfitSingleParticleHypothesis* daughter = resonance->daughter(iDaughter);
+      daughter->polHandedness_.resize(numPolStates_);
+      daughter->polHandedness_[0] = NSVfitSingleParticleHypothesis::kPolUndefined;
+      daughter->polSign_.resize(numPolStates_);
+      daughter->polSign_[0] = 0;
+      daughter->numPolStates_ = numPolStates_;
+    }
+  } else throw cms::Exception("NSVfitResonanceBuilder")
+      << " Support for Polarization not implemented for case of " << resonance->numDaughters() << " daughters yet !!\n";
 
   return resonance;
 }
