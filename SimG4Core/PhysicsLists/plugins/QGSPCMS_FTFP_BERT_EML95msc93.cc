def44b78f6505af5cda48675d43e7e814ed14376
civanch <>
2012-03-21 15:11:14 +0000

    added CMSUrbanMscModel92, 93, 95

--- SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT_EML95msc93.cc
+++ SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT_EML95msc93.cc
@@ -1,5 +1,5 @@
-#include "QGSPCMS_FTFP_BERT_EML95.hh"
-#include "SimG4Core/PhysicsLists/interface/CMSEmStandardPhysics95.h"
+#include "QGSPCMS_FTFP_BERT_EML95msc93.hh"
+#include "SimG4Core/PhysicsLists/interface/CMSEmStandardPhysics95msc93.h"
 #include "SimG4Core/PhysicsLists/interface/CMSMonopolePhysics.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
@@ -15,7 +15,7 @@
 
 #include <string>
 
-QGSPCMS_FTFP_BERT_EML95::QGSPCMS_FTFP_BERT_EML95(G4LogicalVolumeToDDLogicalPartMap& map,
+QGSPCMS_FTFP_BERT_EML95msc93::QGSPCMS_FTFP_BERT_EML95msc93(G4LogicalVolumeToDDLogicalPartMap& map,
 						 const HepPDT::ParticleDataTable * table_,
 						 sim::FieldBuilder *fieldBuilder_, 
 						 const edm::ParameterSet & p) : PhysicsList(map, table_, fieldBuilder_, p) {
@@ -28,14 +28,14 @@ QGSPCMS_FTFP_BERT_EML95::QGSPCMS_FTFP_BERT_EML95(G4LogicalVolumeToDDLogicalPartM
   bool tracking= p.getParameter<bool>("TrackingCut");
   std::string region = p.getParameter<std::string>("Region");
   edm::LogInfo("PhysicsList") << "You are using the simulation engine: "
-			      << "QGSP_FTFP_BERT_EML95 with Flags for EM Physics "
+			      << "QGSP_FTFP_BERT_EML95msc93 with Flags for EM Physics "
 			      << emPhys << ", for Hadronic Physics "
 			      << hadPhys << " and tracking cut " << tracking
                               << " with special region " << region;
 
   if (emPhys) {
     // EM Physics
-    RegisterPhysics( new CMSEmStandardPhysics95("standard EM EML95",ver,region));
+    RegisterPhysics( new CMSEmStandardPhysics95msc93("standard EM EML95msc93",ver,region));
 
     // Synchroton Radiation & GN Physics
     RegisterPhysics( new G4EmExtraPhysics("extra EM"));
