9f85c3b6eb504046f0805ce613680b39f7941fad
bianchi <>
2012-03-27 14:35:42 +0000

    added pol for 3prong

--- TauAnalysis/CandidateTools/plugins/NSVfitTauToHadLikelihoodMatrixElement.cc
+++ TauAnalysis/CandidateTools/plugins/NSVfitTauToHadLikelihoodMatrixElement.cc
@@ -22,7 +22,18 @@ NSVfitTauToHadLikelihoodMatrixElement::NSVfitTauToHadLikelihoodMatrixElement(con
     rhoTPlus_(0),
     rhoNormTPlus_(0),
     rhoTMinus_(0),
-    rhoNormTMinus_(0)
+    rhoNormTMinus_(0),
+    a1Lz_(0),
+    a1Tz_(0),
+    rhoyMinus_(0),
+    rhoyNormMinus_(0),
+    rhoyPlus_(0),
+    rhoyNormPlus_(0),
+    a1yMinus_(0),
+    a1yNormMinus_(0),
+    a1yPlus_(0),
+    a1yNormPlus_(0)
+
 {
   inputFileName_ = cfg.getParameter<edm::FileInPath>("VMshapeFileName");
   if ( !inputFileName_.isLocal() ) 
@@ -39,6 +50,27 @@ NSVfitTauToHadLikelihoodMatrixElement::NSVfitTauToHadLikelihoodMatrixElement(con
   rhoNormTPlus_  = dynamic_cast<TGraph*>(inputFile_->Get("gNormRhoTPlus"));
   rhoNormTMinus_ = dynamic_cast<TGraph*>(inputFile_->Get("gNormRhoTMinus"));
 
+  a1LPlus_      = dynamic_cast<TGraph*>(inputFile_->Get("gA1LPlus"));
+  a1LMinus_     = dynamic_cast<TGraph*>(inputFile_->Get("gA1LMinus"));
+  a1NormLPlus_  = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1LPlus"));
+  a1NormLMinus_ = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1LMinus"));
+  a1TPlus_      = dynamic_cast<TGraph*>(inputFile_->Get("gA1TPlus"));
+  a1TMinus_     = dynamic_cast<TGraph*>(inputFile_->Get("gA1TMinus"));
+  a1NormTPlus_  = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1TPlus"));
+  a1NormTMinus_ = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1TMinus"));
+
+  a1Lz_          = dynamic_cast<TGraph*>(inputFile_->Get("a1LzFraction"));
+  a1Tz_          = dynamic_cast<TGraph*>(inputFile_->Get("a1TzFraction"));
+  rhoyMinus_     = dynamic_cast<TGraph*>(inputFile_->Get("gRhoYMinus"));
+  rhoyNormMinus_ = dynamic_cast<TGraph*>(inputFile_->Get("gNormRhoYMinus"));
+  rhoyPlus_      = dynamic_cast<TGraph*>(inputFile_->Get("gRhoYPlus"));
+  rhoyNormPlus_  = dynamic_cast<TGraph*>(inputFile_->Get("gNormRhoYPlus"));
+  a1yMinus_      = dynamic_cast<TGraph*>(inputFile_->Get("gA1YMinus"));
+  a1yNormMinus_  = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1YMinus"));
+  a1yPlus_       = dynamic_cast<TGraph*>(inputFile_->Get("gA1YPlus"));
+  a1yNormPlus_   = dynamic_cast<TGraph*>(inputFile_->Get("gNormA1YPlus"));
+
+
   if ( !(rhoLPlus_      &&
 	 rhoLMinus_     &&
 	 rhoNormLPlus_  &&
@@ -46,7 +78,26 @@ NSVfitTauToHadLikelihoodMatrixElement::NSVfitTauToHadLikelihoodMatrixElement(con
 	 rhoTPlus_      &&
 	 rhoTMinus_     &&
 	 rhoNormTPlus_  &&
-	 rhoNormTMinus_) )
+	 rhoNormTMinus_ &&
+	 a1LPlus_      &&
+	 a1LMinus_     &&
+	 a1NormLPlus_  &&
+	 a1NormLMinus_ &&
+	 a1TPlus_      &&
+	 a1TMinus_     &&
+	 a1NormTPlus_  &&
+	 a1NormTMinus_ &&
+	 a1Lz_ &&
+	 a1Tz_ &&
+	 rhoyMinus_ &&
+	 rhoyNormMinus_ &&
+	 rhoyPlus_ &&
+	 rhoyNormPlus_ &&
+	 a1yMinus_ &&
+	 a1yNormMinus_ &&
+	 a1yPlus_ &&
+	 a1yNormPlus_) )
+    
     throw cms::Exception("NSVfitTauToHadLikelihoodMatrixElement") 
       << " Failed to load TGraph objects from File = " << inputFileName_ << " !!\n";
   
@@ -56,6 +107,9 @@ NSVfitTauToHadLikelihoodMatrixElement::NSVfitTauToHadLikelihoodMatrixElement(con
 
 NSVfitTauToHadLikelihoodMatrixElement::~NSVfitTauToHadLikelihoodMatrixElement()
 {
+
+  delete inputFile_;
+
   delete rhoLPlus_; 
   delete rhoNormLPlus_; 
   delete rhoLMinus_; 
@@ -64,7 +118,25 @@ NSVfitTauToHadLikelihoodMatrixElement::~NSVfitTauToHadLikelihoodMatrixElement()
   delete rhoNormTPlus_;
   delete rhoTMinus_; 
   delete rhoNormTMinus_;
-  delete inputFile_;
+  delete a1LPlus_; 
+  delete a1NormLPlus_; 
+  delete a1LMinus_; 
+  delete a1NormLMinus_; 
+  delete a1TPlus_; 
+  delete a1NormTPlus_;
+  delete a1TMinus_; 
+  delete a1NormTMinus_;
+  delete a1Lz_;
+  delete a1Tz_;
+  delete rhoyMinus_;
+  delete rhoyNormMinus_;
+  delete rhoyPlus_;
+  delete rhoyNormPlus_;
+  delete a1yMinus_;
+  delete a1yNormMinus_;
+  delete a1yPlus_;
+  delete a1yNormPlus_;
+
 }
 
 void NSVfitTauToHadLikelihoodMatrixElement::beginJob(NSVfitAlgorithmBase* algorithm)
@@ -87,6 +159,9 @@ double NSVfitTauToHadLikelihoodMatrixElement::operator()(const NSVfitSingleParti
 //             S. Raychaudhuri and D.P. Roy,
 //             Phys. Rev.  D52 (1995) 1556.           
 //
+
+  //polSign = -1;
+
   const NSVfitTauToHadHypothesis* hypothesis_T = dynamic_cast<const NSVfitTauToHadHypothesis*>(hypothesis);
   assert(hypothesis_T != 0);
 
@@ -128,12 +203,15 @@ double NSVfitTauToHadLikelihoodMatrixElement::operator()(const NSVfitSingleParti
       }
       prob *= probCorr;
     }
-  } else if ( tauDecayMode == reco::PFTau::kOneProng1PiZero ) { // tau- --> rho- nu --> pi- pi0 nu decay
+  } else if ( tauDecayMode == reco::PFTau::kOneProng1PiZero ||
+	      (tauDecayMode >= reco::PFTau::kOneProng2PiZero &&
+	       tauDecayMode <= reco::PFTau::kOneProngNPiZero)) { // tau- --> rho- nu --> pi- pi0 nu decay
+
     // compute fraction of rho meson energy carried by "distinguishable" (charged) pion
     double probLz = 3.*square(2.*z - 1.);
     double probTz = 6.*z*(1. - z);
-    double probLx = (polSign == +1) ? rhoLPlus_->Eval(z) : rhoLMinus_->Eval(z);
-    double probTx = (polSign == +1) ? rhoTPlus_->Eval(z) : rhoTMinus_->Eval(z);
+    double probLx = (polSign == +1) ? rhoLPlus_->Eval(visEnFracX) : rhoLMinus_->Eval(visEnFracX);
+    double probTx = (polSign == +1) ? rhoTPlus_->Eval(visEnFracX) : rhoTMinus_->Eval(visEnFracX);
 
     double xCut = ( applyVisPtCutCorrection_ ) ?
       visPtCutThreshold_/hypothesis_T->p4_fitted().pt() : 0.0; 
@@ -151,11 +229,53 @@ double NSVfitTauToHadLikelihoodMatrixElement::operator()(const NSVfitSingleParti
       mT = 1.e-3;
     }
     prob = (probLz*probLx + probTz*probTx)/(mL*probLz + mT*probTz);
-  } else if ( tauDecayMode >= reco::PFTau::kOneProng2PiZero ||
-	      tauDecayMode <= reco::PFTau::kOneProngNPiZero ) { // tau- --> a1- nu --> pi- pi0 pi0 nu decay
-    // decay mode not yet implemented
-  } else if ( tauDecayMode == reco::PFTau::kThreeProng0PiZero ) {
-    // decay mode not yet implemented
+  } 
+  else if ( tauDecayMode == reco::PFTau::kThreeProng0PiZero ) {
+    
+    // find the distinguishable pion
+    double distPionEnergy = 0.;
+    for(unsigned int k = 0 ; k<(tauJet->signalPFChargedHadrCands()).size(); k++){
+      reco::PFCandidateRef pion = (tauJet->signalPFChargedHadrCands()).at(k);
+      if( (pion->charge()*tauJet->charge()) > 0 ) distPionEnergy = pion->p();
+    }
+    double tauEnergy      = tauJet->p();
+    z     =  distPionEnergy/tauEnergy;
+
+    if ( z > 1. || z < 0. ) {
+      edm::LogWarning ("NSVfitTauToHadLikelihoodMatrixElement::operator()")
+	<< "Momentum oftau constituent exceeds tau-jet momentum !!" << std::endl; 
+      z = 0.5;
+    }
+
+    // compute fraction of rho meson energy carried by "distinguishable" (charged) pion
+    double probLz = a1Lz_->Eval(z);
+    double probTz = a1Tz_->Eval(z);
+    if( probLz<=0 && probTz<=0 ){
+      // if both prob are 0, then use f(x) instead of f(x|z)
+      probLz = 1.;
+      probTz = 1.;
+    }
+
+    double probLx = (polSign == +1) ? a1LPlus_->Eval(visEnFracX) : a1LMinus_->Eval(visEnFracX);
+    double probTx = (polSign == +1) ? a1TPlus_->Eval(visEnFracX) : a1TMinus_->Eval(visEnFracX);
+
+    double xCut = ( applyVisPtCutCorrection_ ) ?
+      visPtCutThreshold_/hypothesis_T->p4_fitted().pt() : 0.0; 
+
+    double mL = ( polSign == +1 ) ? 
+      (a1NormLPlus_->Eval(1.0)  - a1NormLPlus_->Eval(xCut)) : 
+      (a1NormLMinus_->Eval(1.0) - a1NormLMinus_->Eval(xCut));
+    double mT = ( polSign == +1 ) ? 
+      (a1NormTPlus_->Eval(1.0)  - a1NormTPlus_->Eval(xCut)) :
+      (a1NormTMinus_->Eval(1.0) - a1NormTMinus_->Eval(xCut));
+    if ( mL <= 0. && mT <= 0. ) {
+      edm::LogWarning ("NSVfitTauToHadLikelihoodMatrixElement::operator()")
+	<< "Vector meson mass computes to zero for all polarization hypotheses !!" << std::endl;
+      mL = 1.e-3;
+      mT = 1.e-3;
+    }
+    prob = (probLz*probLx + probTz*probTx)/(mL*probLz + mT*probTz);
+
   } else
     edm::LogError ("NSVfitTauToHadLikelihoodMatrixElement::operator()")
       << "Tau decay mode = " << tauDecayMode << " not supported yet !!" << std::endl;
