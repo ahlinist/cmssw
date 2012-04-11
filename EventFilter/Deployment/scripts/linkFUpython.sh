aec8d320f34773175326b924c63fb44dccfd3754
fwyzard <>
2012-04-11 12:12:04 +0000

    explicit threatment of all files, instead of relying on cp -sr, to work around RPM bugs in dealing with symlinks

--- EventFilter/Deployment/scripts/linkFUpython.sh
+++ EventFilter/Deployment/scripts/linkFUpython.sh
@@ -12,14 +12,47 @@ grep -q "PYTHONPATH=" $ENVFILE || {
   exit 1
 }
 
+# makeLinksTo BASE DEST
+function makeLinksTo() {
+  local BASE="$1"
+  local DEST="$2"
+
+  for X in $(find $BASE); do
+    T=$(echo $X | sed -e"s#^$BASE/#$DEST/#")
+    if [ -e $T ]; then
+      continue
+    fi
+
+    if [ -h $X ] && [ -f $X ]; then
+      # symlink to a regular file
+      ln -s $(readlink -f $X) $T
+    elif [ -h $X ] && [ -d $X ]; then
+      # symlinks to a directory
+      mkdir -p $T
+      makeLinksTo $(readlink -f $X) $T
+    elif [ -f $X ]; then
+      # regular file
+      ln -s $X $T
+    elif [ -d $X ]; then
+      # directory
+      mkdir $T
+    else
+      # unknown, make a symlink as default action
+      echo "Unexpected object at $X"
+      ln -s $X $T
+    fi
+  done
+
+}
+
 mkdir -p $TARGET/home $TARGET/path
 
-# traverse the PYTHONHOME defined in $ENVFILE in reverse order
-for DIR in $( unset PYTHONHOME; source $ENVFILE; echo $PYTHONHOME | tr : \\n | tac ); do
-  cp -r -s -f $DIR/* $TARGET/home/
+# traverse the PYTHONHOME defined in $ENVFILE
+for DIR in $( unset PYTHONHOME; source $ENVFILE; echo $PYTHONHOME | tr : \\n ); do
+  makeLinksTo $DIR $TARGET/home
 done
 
-# traverse the PYTHONPATH defined in $ENVFILE in reverse order
-for DIR in $( unset PYTHONPATH; source $ENVFILE; echo $PYTHONPATH | tr : \\n | tac ); do
-  cp -r -s -f $DIR/* $TARGET/path/
+# traverse the PYTHONPATH defined in $ENVFILE
+for DIR in $( unset PYTHONPATH; source $ENVFILE; echo $PYTHONPATH | tr : \\n ); do
+  makeLinksTo $DIR $TARGET/path
 done
