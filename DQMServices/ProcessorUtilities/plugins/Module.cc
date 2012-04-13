8ee295e2d4ef0b64c6498643ec1c21aa44902ea9
lilopera <>
2012-04-13 14:37:12 +0000

    Copied EventStreamHttpReader from EventFilter/StorageManager and renamed it DQMEventStreamHttpReader

---
+++ DQMServices/ProcessorUtilities/plugins/Module.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "DQMServices/ProcessorUtilities/interface/DQMEventStreamHttpReader.h"
+
+using edm::DQMEventStreamHttpReader;
+
+DEFINE_FWK_INPUT_SOURCE(DQMEventStreamHttpReader);
+
