41062a080a914f4e266bf8aadced55104166f1b0
veelken <>
2012-05-02 10:24:59 +0000

    added protection against negative visMass (due to rounding errors)

--- TauAnalysis/CandidateTools/plugins/NSVfitTauToLepLikelihoodMatrixElement.cc
+++ TauAnalysis/CandidateTools/plugins/NSVfitTauToLepLikelihoodMatrixElement.cc
@@ -50,8 +50,6 @@ double NSVfitTauToLepLikelihoodMatrixElement<T>::operator()(const NSVfitSinglePa
     dynamic_cast<const NSVfitTauToDaughtersHypothesisBaseT1T2<NSVfitTauDecayHypothesis, T>*>(hypothesis);
   assert(hypothesis_T != 0);
 
-  //if ( this->verbosity_ ) std::cout << "<NSVfitTauToLepLikelihoodMatrixElement::operator()>:" << std::endl;
-  
   double decayAngle = hypothesis_T->decay_angle_rf();
   double nuMass = hypothesis_T->p4invis_rf().mass();
   if ( nuMass < 0. ) nuMass = 0.; // CV: add protection against rounding errors when boosting between laboratory and rest frame
@@ -59,6 +57,7 @@ double NSVfitTauToLepLikelihoodMatrixElement<T>::operator()(const NSVfitSinglePa
   double visEnFracX = hypothesis_T->visEnFracX();
 
   //if ( this->verbosity_ ) {
+  //  std::cout << "<NSVfitTauToLepLikelihoodMatrixElement::operator()>:" << std::endl;
   //  std::cout << " decayAngle = " << decayAngle << std::endl;
   //  std::cout << " nuMass = " << nuMass << std::endl;
   //  std::cout << " visEnFracX = " << visEnFracX << std::endl;
@@ -73,12 +72,12 @@ double NSVfitTauToLepLikelihoodMatrixElement<T>::operator()(const NSVfitSinglePa
 
     if ( nuMass < TMath::Sqrt((1. - visEnFracX)*tauLeptonMass2) ) {
       prob = (nuMass/(4.*tauLeptonMass4))*((tauLeptonMass2 + 2.*nuMass2)*(tauLeptonMass2 - nuMass2) 
-					   + polSign*(tauLeptonMass2*(2.*visEnFracX - 1.) + nuMass2)*(-tauLeptonMass2 + 2.*nuMass2));
+				         + polSign*(tauLeptonMass2*(2.*visEnFracX - 1.) + nuMass2)*(-tauLeptonMass2 + 2.*nuMass2));
     }else{
       double nuMass_limit  = TMath::Sqrt((1. - visEnFracX)*tauLeptonMass2);
       double nuMass2_limit = square(nuMass_limit);
       prob = (nuMass_limit/(4.*tauLeptonMass4))*((tauLeptonMass2 + 2.*nuMass2_limit)*(tauLeptonMass2 - nuMass2_limit) 
-					   + polSign*(tauLeptonMass2*(2.*visEnFracX - 1.) + nuMass2_limit)*(-tauLeptonMass2 + 2.*nuMass2_limit));
+	    + polSign*(tauLeptonMass2*(2.*visEnFracX - 1.) + nuMass2_limit)*(-tauLeptonMass2 + 2.*nuMass2_limit));
       prob /= (1. + 1.e+6*square(nuMass - nuMass_limit));
     }
 
@@ -87,15 +86,12 @@ double NSVfitTauToLepLikelihoodMatrixElement<T>::operator()(const NSVfitSinglePa
       if ( hypothesis_T->p4_fitted().pt() > visPtCutThreshold_ ) {
 	double xCut = visPtCutThreshold_/hypothesis_T->p4_fitted().pt();
 	probCorr = 1./((0.5*(1. + polSign)*(1./3.)*(0.5 - xCut + cube(xCut) - 0.5*fourth(xCut))
-			+ 0.5*(1. - polSign)*(1./3.)*(0.75 - xCut + 0.25*fourth(xCut))));
+		      + 0.5*(1. - polSign)*(1./3.)*(0.75 - xCut + 0.25*fourth(xCut))));
       }
       //if ( this->verbosity_ ) std::cout << "probCorr (lep) = " << probCorr << std::endl;
       prob *= probCorr;
     }
-  } 
-
-
-  else {
+  } else {
     if ( nuMass < TMath::Sqrt((1. - visEnFracX)*tauLeptonMass2) ) { // LB: physical solution
       prob = (13./square(tauLeptonMass2))*(tauLeptonMass2 - nuMass2)*(tauLeptonMass2 + 2.*nuMass2)*nuMass;
     } else {                                                        // LB: unphysical solution
