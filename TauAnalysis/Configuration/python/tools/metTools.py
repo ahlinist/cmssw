01d43479b4cfc485b7aec1ce7dfeecd1198f0c58
veelken <>
2012-05-08 10:26:05 +0000

    added function to configure PAT PFMET production with latest JetMET recommendations (Type0 + Type1 + MET phi asymmetry corrections)

--- TauAnalysis/Configuration/python/tools/metTools.py
+++ TauAnalysis/Configuration/python/tools/metTools.py
@@ -19,6 +19,74 @@ def addPFMet(process, correct = False):
         process.makePatPFMETs = cms.Sequence(process.patPFMETs)
 
     process.makePatMETs += process.makePatPFMETs
+    
+    return process.makePatMETs
+
+def addCorrectedPFMet(process, isMC, doApplyType0corr, doApplySysShiftCorr, runPeriod, doSmearJets):
+
+    process.load("PhysicsTools.PatUtils.patPFMETCorrections_cff")
+    
+    process.makeCorrectedPatMETs = cms.Sequence()
+
+    if isMC:
+        import PhysicsTools.PatAlgos.tools.helpers as configtools
+        process.type0PFMEtCorrection.remove(process.type0PFMEtCorrectionPFCandToVertexAssociation)
+        process.makeCorrectedPatMETs += process.type0PFMEtCorrectionPFCandToVertexAssociation
+        configtools.cloneProcessingSnippet(process, process.producePatPFMETCorrections, "NoSmearing")
+        process.selectedPatJetsForMETtype1p2CorrNoSmearing.src = cms.InputTag('patJetsNotOverlappingWithLeptonsForMEtUncertainty')
+        process.selectedPatJetsForMETtype2CorrNoSmearing.src = process.selectedPatJetsForMETtype1p2CorrNoSmearing.src
+    else:
+        doSmearJets = False
+
+    sysShiftCorrParameter = None
+    if doApplySysShiftCorr:
+        process.load("JetMETCorrections.Type1MET.pfMETsysShiftCorrections_cfi")
+        if runPeriod == "2011RunA":
+            if isMC:
+                sysShiftCorrParameter = process.pfMEtSysShiftCorrParameters_2011runAvsNvtx_mc
+            else:
+                sysShiftCorrParameter = process.pfMEtSysShiftCorrParameters_2011runAvsNvtx_data
+        elif runPeriod == "2011RunB" or runPeriod == "2012RunA":
+            # CV: use corrections determined for 2011 run B data for 2012 run A data,
+            #     until corrections specific to 2012 run A data have been determined
+            if isMC:
+                sysShiftCorrParameter = process.pfMEtSysShiftCorrParameters_2011runBvsNvtx_mc
+            else:
+                sysShiftCorrParameter = process.pfMEtSysShiftCorrParameters_2011runBvsNvtx_data
+        else:
+            raise ValueError("Invalid runPeriod = %s !!" % runPeriod)
+
+    from PhysicsTools.PatUtils.tools.metUncertaintyTools import runMEtUncertainties
+    runMEtUncertainties(
+        process,
+        electronCollection = '',
+        photonCollection = '',
+        muonCollection = cms.InputTag('patMuons'),
+        tauCollection = '',
+        jetCollection = cms.InputTag('patJets'),
+        doSmearJets = doSmearJets,
+        doApplyType0corr = doApplyType0corr,
+        sysShiftCorrParameter = sysShiftCorrParameter,
+        doApplySysShiftCorr = doApplySysShiftCorr,
+        addToPatDefaultSequence = False
+    )
+
+    if isMC:
+        process.patPFMet.addGenMET = cms.bool(True)
+        process.patPFMetNoSmearing.addGenMET = cms.bool(True)
+        process.patPFJetMETtype1p2Corr.jetCorrLabel = cms.string("L3Absolute")
+        process.patPFJetMETtype1p2CorrNoSmearing.jetCorrLabel = cms.string("L3Absolute")
+    
+        process.makeCorrectedPatMETs += process.metUncertaintySequence
+        process.makeCorrectedPatMETs += process.producePatPFMETCorrectionsNoSmearing
+    else:
+        process.patPFMet.addGenMET = cms.bool(False)
+        process.patPFJetMETtype1p2Corr.jetCorrLabel = cms.string("L2L3Residual")
+    
+        process.makeCorrectedPatMETs += process.patJetsNotOverlappingWithLeptonsForMEtUncertainty
+        process.makeCorrectedPatMETs += process.producePatPFMETCorrections
+
+    return process.makeCorrectedPatMETs
 
 def addTCMet(process):
     process.layer1TCMETs = process.patMETs.clone()
