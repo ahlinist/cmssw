4a2650446222ea89acc31ad59fa15de3f756c621
slehti <>
2012-05-29 12:17:38 +0000

    Update

--- ElectroWeakAnalysis/TauTriggerEfficiency/test/checkoutTags.sh
+++ ElectroWeakAnalysis/TauTriggerEfficiency/test/checkoutTags.sh
@@ -18,6 +18,7 @@ fi
 
 # Tag list modification history
 
+# Skim
 #CMSSW_5_2_5 Skim 28.5.2012/S.Lehti:
 if [ "$1" == "skim" ]; then
   cvs co -r V01-04-17 RecoTauTag/RecoTau
@@ -29,10 +30,12 @@ if [ "$1" == "skim" ]; then
   cvs up -r 1.15      PhysicsTools/PatAlgos/python/recoLayer0/tauDiscriminators_cff.py
 fi
 
-
+# Analyzer
 #CMSSW_5_2_5_patch1 Analyzer 28.5.2012/S.Lehti:
+#CMSSW_5_2_5_patch1, update for V00-10-05 28.5.2012/S.Lehti
 if [ "$1" == "analyzer" ]; then
-  cvs co -r V13-05-07-01   FastSimulation/Configuration                     
-  cvs co -r V13-06-12      HLTrigger/Configuration                                 
+  cvs co -r V00-00-59      Configuration/AlCa                               
+  cvs co -r V01-27-31      Configuration/PyReleaseValidation                
+  cvs co -r V13-06-17      HLTrigger/Configuration
   checkdeps -a
 fi
