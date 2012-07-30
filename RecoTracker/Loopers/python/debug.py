d6fe8c6364a534b8a7ae51216f3469f712661d05
vlimant <>
2012-07-30 09:45:19 +0000

    more explanations of debugging

--- RecoTracker/Loopers/python/debug.py
+++ RecoTracker/Loopers/python/debug.py
@@ -10,14 +10,27 @@ def debug(process):
     process.load('logger_cfi')
     process.options.wantSummary = cms.untracked.bool(True)
 
-    process.MessageLogger._categoryCanTalk('Collect')
-    process.MessageLogger._categoryCanTalk('PeakFinder')
-    #process.MessageLogger._categoryCanTalk('Equilibrate')
+    ##### debugging from DataDumper.cc
+    #### category unique for collecting hits in histoset : Collect
+    #process.MessageLogger._categoryCanTalk('Collect')
+    #### category with alternative for finding the helix loopers
+    ### main category : PeakFinder
+    #process.MessageLogger._categoryCanTalk('PeakFinder')
+    ### sub-categories
+    ## for when a histoset is rejected or accepted
+    #process.MessageLogger._categoryCanTalk('CollectPeak')
+    ## single categories of algo step
     #process.MessageLogger._categoryCanTalk('Truncate')
+    #process.MessageLogger._categoryCanTalk('Equilibrate')
     #process.MessageLogger._categoryCanTalk('PhiInHelix')
     process.MessageLogger._categoryCanTalk('SlopeCheck')
-    #process.MessageLogger._categoryCanTalk('CollectPeak')
+    process.MessageLogger._categoryCanTalk('DoubleHits')
+    process.MessageLogger._categoryCanTalk('RadiusCheck')
 
+    ##### category for handling the looper set from Method.cc
+    process.MessageLogger._categoryCanTalk('LooperMethod')
+
+    
     process.MessageLogger._moduleCanTalk('loopersMask')
     process.TFileService = cms.Service("TFileService",
                                        fileName=cms.string(label+'_TF.root')
