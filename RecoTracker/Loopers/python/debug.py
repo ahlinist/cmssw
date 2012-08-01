4d38ae5381a1d0a5d40fcfb7a8626526b27cb699
vlimant <>
2012-08-01 11:35:33 +0000

    track flagging customise and new categories

--- RecoTracker/Loopers/python/debug.py
+++ RecoTracker/Loopers/python/debug.py
@@ -12,10 +12,10 @@ def debug(process):
 
     ##### debugging from DataDumper.cc
     #### category unique for collecting hits in histoset : Collect
-    process.MessageLogger._categoryCanTalk('Collect')
+    #process.MessageLogger._categoryCanTalk('Collect')
     #### category with alternative for finding the helix loopers
     ### main category : PeakFinder
-    process.MessageLogger._categoryCanTalk('PeakFinder')
+    #process.MessageLogger._categoryCanTalk('PeakFinder')
     ### sub-categories
     ## for when a histoset is rejected or accepted
     #process.MessageLogger._categoryCanTalk('CollectPeak')
@@ -26,7 +26,9 @@ def debug(process):
     #process.MessageLogger._categoryCanTalk('SlopeCheck')
     #process.MessageLogger._categoryCanTalk('DoubleHits')
     #process.MessageLogger._categoryCanTalk('RadiusCheck')
-    #process.MessageLogger._categoryCanTalk('Kinematics')
+    process.MessageLogger._categoryCanTalk('Kinematics')
+    process.MessageLogger._categoryCanTalk('Summary')
+    process.MessageLogger._categoryCanTalk('Plots')
 
     ##### category for handling the looper set from Method.cc
     process.MessageLogger._categoryCanTalk('LooperMethod')
@@ -39,4 +41,21 @@ def debug(process):
 
     process.MessageLogger._changeDestination(label)
     process.maxEvents.input = 1 # dont' flowed debug
+    return (process)
+
+def flagTracks(process):
+    process.load('SimTracker/TrackAssociation/TrackMCQuality_cfi')
+    process.generalTracksOldFlag = process.TrackMCQuality.clone(
+        label_tr = cms.InputTag('generalTracks','','RECO')
+        )
+    process.generalTracksNewFlag = process.TrackMCQuality.clone(
+        label_tr = cms.InputTag('generalTracks','','LOOPER')
+        )
+    process.validation+=process.generalTracksOldFlag
+    process.validation+=process.generalTracksNewFlag
+
+    for o in process.outputModules_():
+        om=process.outputModules_()[o]
+        om.outputCommands.append('keep *_generalTracksOldFlag_*_*')
+        om.outputCommands.append('keep *_generalTracksNewFlag_*_*')
     return (process)
