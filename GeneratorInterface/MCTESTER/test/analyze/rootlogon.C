c0ccfedc02d8862853e3a72468ddd26224e24a8f
inugent <>
2012-09-01 12:07:02 +0000

    patches to run in CMSSW

--- GeneratorInterface/MCTESTER/test/analyze/rootlogon.C
+++ GeneratorInterface/MCTESTER/test/analyze/rootlogon.C
@@ -3,9 +3,9 @@
   gSystem.Load("libHist.so");
   gSystem.Load("libGpad.so");
   
-  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libHEPEventMCTESTER.so");
-  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libHepMCEventMCTESTER.so");
-  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libInterfaceMCTESTER.so");
+  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libHEPEvent.so");
+  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libHepMCEvent.so");
+  gSystem.Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libMCTester.so");
   
   gROOT->SetStyle("Plain");
   
