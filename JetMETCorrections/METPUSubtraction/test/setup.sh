9f34ec59727e2d1ee202f24d823136026246c97e
pharris <>
2012-09-18 09:06:17 +0000

    adding setup script

---
+++ JetMETCorrections/METPUSubtraction/test/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#Setup script for MVA Met studies in CMSSW_5_3_3 
+#Point of this script is to get the root files
+#To start you must first
+# cvs co -r METPU_5_3_X_v0 JetMETCorrections/METPUSubtraction
+tag=METPU_5_3_X_v0
+
+cd ../../../
+cvs co -r HEAD -d pharrisTmp UserCode/pharris/MVAMet/data
+cp  -d pharrisTmp/*53*.root  JetMETCorrections/METPUSubtraction/data/
+rm -rf pharrisTmp
+
+cvs co -r $tag RecoJets/JetProducers
+cvs up -r CMSSW_5_3_3 RecoJets/JetProducers/src/JetSpecific.cc
+
+cvs co -r $tag                 DataFormats/METReco
+cvs co -r V01-05-06            RecoTauTag/RecoTau 
+cd -
