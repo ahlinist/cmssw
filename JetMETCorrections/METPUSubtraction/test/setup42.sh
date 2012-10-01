a1775e2657d3eaf0c078d2ec65f6302b060ac450
pharris <>
2012-10-01 15:59:49 +0000

    small fix

--- JetMETCorrections/METPUSubtraction/test/setup42.sh
+++ JetMETCorrections/METPUSubtraction/test/setup42.sh
@@ -4,12 +4,12 @@
 #Point of this script is to get the root files
 #To start you must first
 # cvs co -r METPU_5_3_X_v0 JetMETCorrections/METPUSubtraction
-tag=METPU_5_3_X_v2
+tag=METPU_5_3_X_v3
 
 cd ../../../
 cvs co -r HEAD -d pharrisTmp UserCode/pharris/MVAMet/data
-cp  -d pharrisTmp/*53.root                JetMETCorrections/METPUSubtraction/data/
-cp  -d pharrisTmp/*53_UnityResponse.root  JetMETCorrections/METPUSubtraction/data/
+cp  -d pharrisTmp/*42.root                JetMETCorrections/METPUSubtraction/data/
+cp  -d pharrisTmp/*42_UnityResponse.root  JetMETCorrections/METPUSubtraction/data/
 rm -rf pharrisTmp
 
 cvs co -r $tag RecoJets/JetProducers
