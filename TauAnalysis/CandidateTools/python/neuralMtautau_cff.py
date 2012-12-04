d4662ce948df53e2d63fbfd354dae81d6009f143
veelken <>
2012-12-04 14:33:17 +0000

    added suffix "ForNeuralMtautau" to collections of PFCandidates, PFJets, PFMET, PFMET cov. matrix produced by this config file, so as not to cause name clash with collections defined elsewhere

--- TauAnalysis/CandidateTools/python/neuralMtautau_cff.py
+++ TauAnalysis/CandidateTools/python/neuralMtautau_cff.py
@@ -5,7 +5,7 @@ neuralMtautauSequence = cms.Sequence()
 #--------------------------------------------------------------------------------
 # produce collections of jets not overlapping with leptons
 # and with (corrected) Pt > 20 GeV
-selectedPatJetsNotOverlappingWithLeptons = cms.EDFilter("PATJetAntiOverlapSelector",
+selectedPatJetsNotOverlappingWithLeptonsForNeuralMtautau = cms.EDFilter("PATJetAntiOverlapSelector",
     src = cms.InputTag('patJets'),
     # CV: set 'srcNotToBeFiltered' to collections of electrons, muons and taus passing analysis specific selection criteria
     srcNotToBeFiltered = cms.VInputTag(
@@ -16,80 +16,81 @@ selectedPatJetsNotOverlappingWithLeptons = cms.EDFilter("PATJetAntiOverlapSelect
     dRmin = cms.double(0.5),
     filter = cms.bool(False)      
 )
-neuralMtautauSequence += selectedPatJetsNotOverlappingWithLeptons
+neuralMtautauSequence += selectedPatJetsNotOverlappingWithLeptonsForNeuralMtautau
 
-selectedPatJetsPt20 = cms.EDFilter("PATJetSelector",
-    src = cms.InputTag('selectedPatJetsNotOverlappingWithLeptons'),                                   
+selectedPatJetsPt20ForNeuralMtautau = cms.EDFilter("PATJetSelector",
+    src = cms.InputTag('selectedPatJetsNotOverlappingWithLeptonsForNeuralMtautau'),                                   
     cut = cms.string('pt > 20.'), 
     filter = cms.bool(False)
 )
-neuralMtautauSequence += selectedPatJetsPt20
+neuralMtautauSequence += selectedPatJetsPt20ForNeuralMtautau
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
 # select ak5PFJets corresponding to selected pat::Jets
-ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20 = cms.EDFilter("PFJetAntiOverlapSelector",
+ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20ForNeuralMtautau = cms.EDFilter("PFJetAntiOverlapSelector",
     src = cms.InputTag('ak5PFJets'),
     srcNotToBeFiltered = cms.VInputTag('selectedPatJetsPt20'),
     dRmin = cms.double(1.e-1),
     invert = cms.bool(True),
     filter = cms.bool(False)                                                          
 )
-neuralMtautauSequence += ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20
+neuralMtautauSequence += ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20ForNeuralMtautau
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
 # select PFCandidates ("unclustered energy") not within jets
 from CommonTools.ParticleFlow.TopProjectors.pfNoJet_cfi import pfNoJet
-pfCandsNotInSelectedJets = pfNoJet.clone(
-    topCollection = cms.InputTag('ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20'),
+pfCandsNotInSelectedJetsForNeuralMtautau = pfNoJet.clone(
+    topCollection = cms.InputTag('ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20ForNeuralMtautau'),
     bottomCollection = cms.InputTag('particleFlow')
 )
-neuralMtautauSequence += pfCandsNotInSelectedJets
+neuralMtautauSequence += pfCandsNotInSelectedJetsForNeuralMtautau
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
 # produce genMET
 from RecoMET.Configuration.GenMETParticles_cff import *
-neuralMtautauSequence += genParticlesForMETAllVisible
+genParticlesForMETAllVisibleForNeuralMtautau = genParticlesForMETAllVisible.clone()
+neuralMtautauSequence += genParticlesForMETAllVisibleForNeuralMtautau
 
 from RecoMET.METProducers.genMetTrue_cfi import *
-genMetFromGenParticles = genMetTrue.clone(
-    src = cms.InputTag('genParticlesForMETAllVisible'),
+genMetFromGenParticlesForNeuralMtautau = genMetTrue.clone(
+    src = cms.InputTag('genParticlesForMETAllVisibleForNeuralMtautau'),
     alias = cms.string('genMetFromGenParticles')
 )
-neuralMtautauSequence += genMetFromGenParticles
+neuralMtautauSequence += genMetFromGenParticlesForNeuralMtautau
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
 # produce PFMET significance cov. matrix
 from RecoMET.METProducers.METSigParams_cfi import *
-pfMEtSignCovMatrix = cms.EDProducer("PFMEtSignCovMatrixProducer",
+pfMEtSignCovMatrixForNeuralMtautau = cms.EDProducer("PFMEtSignCovMatrixProducer",
     METSignificance_params,                     
     src = cms.VInputTag(
         'genMatchedElectrons',
         'genMatchedMuons',
         'genMatchedTaus',                                   
-        'ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20',
-        'pfCandsNotInSelectedJets'
+        'ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20ForNeuralMtautau',
+        'pfCandsNotInSelectedJetsForNeuralMtautau'
     )                                
 )
-neuralMtautauSequence += pfMEtSignCovMatrix
+neuralMtautauSequence += pfMEtSignCovMatrixForNeuralMtautau
 #--------------------------------------------------------------------------------
 
 from RecoMET.METProducers.METSigParams_cfi import *
 neuralMtautauNtupleProducer = cms.EDProducer("NeuralMtautauNtupleProducer",
     srcGenTauPair = cms.InputTag(''),
-    srcGenMEt = cms.InputTag('genMetFromGenParticles'), 
+    srcGenMEt = cms.InputTag('genMetFromGenParticlesForNeuralMtautau'), 
     srcGenTaus = cms.InputTag(''),                                         
     srcGenParticles = cms.InputTag('genParticles'),
                                              
-    srcRecPFJets = cms.InputTag('ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20'),
-    srcRecPFCandidatesNotWithinJets = cms.InputTag('pfCandsNotInSelectedJets'),
+    srcRecPFJets = cms.InputTag('ak5PFJetsNotOverlappingWithLeptonsCorrPtGt20ForNeuralMtautau'),
+    srcRecPFCandidatesNotWithinJets = cms.InputTag('pfCandsNotInSelectedJetsForNeuralMtautau'),
     srcRecLeg1 = cms.InputTag('genMatchedMuons'), # collection of reconstructed tau decay products (1st leg)
     srcRecLeg2 = cms.InputTag('genMatchedTaus'),  # collection of reconstructed tau decay products (2nd leg)
     srcRecMEt = cms.InputTag('patType1CorrectedPFMet'),
                                              
-    srcPFMEtCovMatrix = cms.InputTag('pfMEtSignCovMatrix')
+    srcPFMEtCovMatrix = cms.InputTag('pfMEtSignCovMatrixForNeuralMtautau')
 )                                         
 neuralMtautauSequence += neuralMtautauNtupleProducer
