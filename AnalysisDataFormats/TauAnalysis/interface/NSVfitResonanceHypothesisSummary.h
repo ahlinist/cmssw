2e5678733f5d789cab5154d0dea212933176ef06
veelken <>
2012-12-18 14:17:15 +0000

    added Pt, eta, phi values plus uncertainties

--- AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesisSummary.h
+++ AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesisSummary.h
@@ -27,15 +27,38 @@ class NSVfitResonanceHypothesisSummary
   double mass() const { return mass_; }
   double massErrUp() const { return massErrUp_; }
   double massErrDown() const { return massErrDown_; }
+  bool mass_isValid() const { return mass_isValid_; }
+  
+  /// mean and median plus -1 sigma and +1 sigma limits on 
+  /// reconstructed Pt, eta, phi
+  ///
+  /// NOTE: Pt, eta, phi will be valid only in case
+  ///       NSVfitAlgorithmByIntegration2 algorithm (Markov Chain integration) is used
+  ///  
+  double pt() const { return pt_; }
+  double ptErrUp() const { return ptErrUp_; }
+  double ptErrDown() const { return ptErrDown_; }
+  bool pt_isValid() const { return pt_isValid_; }
+  double eta() const { return eta_; }
+  double etaErrUp() const { return etaErrUp_; }
+  double etaErrDown() const { return etaErrDown_; }
+  bool eta_isValid() const { return eta_isValid_; }
+  double phi() const { return phi_; }
+  double phiErrUp() const { return phiErrUp_; }
+  double phiErrDown() const { return phiErrDown_; }
+  bool phi_isValid() const { return phi_isValid_; }
 
-  bool isValidSolution() const { return isValidSolution_; }
+  bool isValidSolution() const { return mass_isValid_; }
 
   virtual void print(std::ostream& stream) const
   {
     stream << "<NSVfitResonanceHypothesisSummary::print>:" << std::endl;
     stream << " name = " << name_ << std::endl;
-    stream << " mass = " << mass_ << " + " << massErrUp_ << " - " << massErrDown_ << std::endl;
-    stream << " isValidSolution = " << isValidSolution_ << std::endl;
+    if ( mass_isValid_ ) stream << " mass = " << mass_ << " + " << massErrUp_ << " - " << massErrDown_ << std::endl;
+    else stream << " mass: no valid solution";
+    if ( pt_isValid_   ) stream << " Pt   = " << pt_   << " + " << ptErrUp_   << " - " << ptErrDown_   << std::endl;
+    if ( eta_isValid_  ) stream << " eta  = " << eta_  << " + " << etaErrUp_  << " - " << etaErrDown_  << std::endl;
+    if ( phi_isValid_  ) stream << " phi  = " << phi_  << " + " << phiErrUp_  << " - " << phiErrDown_  << std::endl;
   }
 
  protected:
@@ -50,7 +73,22 @@ class NSVfitResonanceHypothesisSummary
   double massErrDown_;
 
   /// flag indicating that computed mass hypotheses are physically "valid" solutions
-  bool isValidSolution_;
+  bool mass_isValid_;
+
+  /// "best fit" reconstructed Pt, eta, phi 
+  /// plus -1 sigma and +1 sigma limits  
+  double pt_;
+  double ptErrUp_;
+  double ptErrDown_;
+  bool pt_isValid_;
+  double eta_;
+  double etaErrUp_;
+  double etaErrDown_;
+  bool eta_isValid_;
+  double phi_;
+  double phiErrUp_;
+  double phiErrDown_;
+  bool phi_isValid_;
 };
 
 #endif
--- AnalysisDataFormats/TauAnalysis/src/NSVfitResonanceHypothesis.cc
+++ AnalysisDataFormats/TauAnalysis/src/NSVfitResonanceHypothesis.cc
@@ -4,9 +4,23 @@ NSVfitResonanceHypothesis::NSVfitResonanceHypothesis(const NSVfitResonanceHypoth
   : NSVfitResonanceHypothesisBase(bluePrint),
     eventHyp_(bluePrint.eventHyp_),
     p4_(bluePrint.p4_),
-    dp4_(bluePrint.dp4_),
+    dp4_(bluePrint.dp4_),    
+    pt_(bluePrint.pt_),
+    ptErrUp_(bluePrint.ptErrUp_),
+    ptErrDown_(bluePrint.ptErrDown_),
+    pt_isValid_(bluePrint.pt_isValid_),    
+    eta_(bluePrint.eta_),
+    etaErrUp_(bluePrint.etaErrUp_),
+    etaErrDown_(bluePrint.etaErrDown_),
+    eta_isValid_(bluePrint.eta_isValid_),   
+    phi_(bluePrint.phi_),
+    phiErrUp_(bluePrint.phiErrUp_),
+    phiErrDown_(bluePrint.phiErrDown_),
+    phi_isValid_(bluePrint.phi_isValid_),
+    dPhiVis_(bluePrint.dPhiVis_),
     prod_angle_rf_(bluePrint.prod_angle_rf_),
     polHandedness_(bluePrint.polHandedness_),
+    polSign_(bluePrint.polSign_),
     numPolStates_(bluePrint.numPolStates_)
 {
   size_t  numDaughters = daughters_.size();
@@ -21,8 +35,22 @@ NSVfitResonanceHypothesis& NSVfitResonanceHypothesis::operator=(const NSVfitReso
   eventHyp_ = bluePrint.eventHyp_;
   p4_ = bluePrint.p4_;
   dp4_ = bluePrint.dp4_;
+  pt_ = bluePrint.pt_;
+  ptErrUp_ = bluePrint.ptErrUp_;
+  ptErrDown_ = bluePrint.ptErrDown_;
+  pt_isValid_ = bluePrint.pt_isValid_;
+  eta_ = bluePrint.eta_;
+  etaErrUp_ = bluePrint.etaErrUp_;
+  etaErrDown_ = bluePrint.etaErrDown_;
+  eta_isValid_ = bluePrint.eta_isValid_;
+  phi_ = bluePrint.phi_;
+  phiErrUp_ = bluePrint.phiErrUp_;
+  phiErrDown_ = bluePrint.phiErrDown_;
+  phi_isValid_ = bluePrint.phi_isValid_;
+  dPhiVis_ = bluePrint.dPhiVis_;
   prod_angle_rf_ = bluePrint.prod_angle_rf_;
   polHandedness_ = bluePrint.polHandedness_;
+  polSign_ = bluePrint.polSign_;
   numPolStates_ = bluePrint.numPolStates_;
   size_t  numDaughters = daughters_.size();
   for ( size_t iDaughter = 0; iDaughter < numDaughters; iDaughter++ ) {
