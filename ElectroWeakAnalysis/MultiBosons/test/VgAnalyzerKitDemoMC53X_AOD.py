e288ff9ddc0c7931f7f2c3b0a1def7130b5169fc
poter <>
2012-12-20 19:20:19 +0000

    Add ele MVA ID in 2012

--- ElectroWeakAnalysis/MultiBosons/test/VgAnalyzerKitDemoMC53X_AOD.py
+++ ElectroWeakAnalysis/MultiBosons/test/VgAnalyzerKitDemoMC53X_AOD.py
@@ -11,7 +11,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(100)
     )
 
 process.source = cms.Source("PoolSource",
