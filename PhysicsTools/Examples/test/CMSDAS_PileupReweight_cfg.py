1627fae950d54f55835c05c9a436a1bd8d142ebd
mikeh <>
2013-01-04 16:56:55 +0000

    update for 2012

--- PhysicsTools/Examples/test/CMSDAS_PileupReweight_cfg.py
+++ PhysicsTools/Examples/test/CMSDAS_PileupReweight_cfg.py
@@ -15,8 +15,10 @@ process.maxEvents = cms.untracked.PSet(
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
                              fileNames = cms.untracked.vstring(
-    '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FEEBB1EF-B404-E111-BF75-00304867BF9A.root',
-    '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FED957F3-A404-E111-93D0-003048678FE6.root'
+     '/store/mc/Summer12/DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph/AODSIM/PU_S7_START52_V9-v1/00000/FE8C611E-41F3-E111-A54B-001A928116D0.root',
+     '/store/mc/Summer12/DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph/AODSIM/PU_S7_START52_V9-v1/00000/FE1E2D03-4AF3-E111-8E54-002618943982.root'
+#    '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FEEBB1EF-B404-E111-BF75-00304867BF9A.root',
+#    '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6-START44_V5-v1/0000/FED957F3-A404-E111-93D0-003048678FE6.root'
                                                               )
                             )          
 
