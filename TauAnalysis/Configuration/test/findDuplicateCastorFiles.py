66fc112575523e98b96be4064634fd0d9d5f06df
veelken <>
2013-01-07 09:50:46 +0000

    improved comments

--- TauAnalysis/Configuration/test/findDuplicateCastorFiles.py
+++ TauAnalysis/Configuration/test/findDuplicateCastorFiles.py
@@ -19,11 +19,11 @@ import TauAnalysis.Configuration.tools.eos as eos
 # 
 # Examples:
 # 
-# ./findDuplicateCastorFiles.py channel duplicatefiles.list
+# ./findDuplicateCastorFiles.py channel badfiles.list
 #
 #   after checking badfiles.list is okay,
 #
-# cat duplicatefiles.list | xargs -n 1 rfrm
+# cat badfiles.list | xargs -n 1 rfrm
 #--------------------------------------------------------------------------------
 
 print("<findDuplicateCastorFiles>:")
