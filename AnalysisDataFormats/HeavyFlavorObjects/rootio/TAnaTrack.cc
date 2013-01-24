08940ff0f1d00b21138231c2db42475817425814
naegelic <>
2013-01-24 15:54:57 +0000

    Starting schema migration: 	- Added vectors for position and momentum where the track enters the muon chamber

--- AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaTrack.cc
+++ AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaTrack.cc
@@ -36,6 +36,7 @@ void TAnaTrack::clear() {
   fInt1 = fInt2 = fInt3 -99; 
   fDouble1 = fDouble2 = fDouble3 = -99;
   
+  fPosMuonChamber.SetXYZ(-1e30,-1e30,-1e30);
 }
 
 
@@ -67,6 +68,10 @@ void TAnaTrack::dump() {
 
   if (fMuID > -1) {
     cout << Form(" mid=%3x", fMuID);
+  }
+  
+  if (fPosMuonChamber.X() > -1e30 && fPosMuonChamber.Y() > -1e30 && fPosMuonChamber.Z() > -1e30) {
+	  cout << Form(" mupos=(%f,%f,%f)",fPosMuonChamber.X(),fPosMuonChamber.Y(),fPosMuonChamber.Z());
   }
     
   cout << endl;
