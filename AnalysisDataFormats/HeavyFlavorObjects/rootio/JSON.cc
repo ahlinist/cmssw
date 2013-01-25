b94c15051cdf619f52b5d5267bc128ec5d5649ec
dkotlins <>
2013-01-25 09:33:29 +0000

    fix the last line problem

--- AnalysisDataFormats/HeavyFlavorObjects/rootio/JSON.cc
+++ AnalysisDataFormats/HeavyFlavorObjects/rootio/JSON.cc
@@ -19,20 +19,24 @@ JSON::JSON(const char *fname, int verbose) {
   ifstream file(fname);  
   while (getline(file, line)) { 
     jsonFile.push_back(line); 
+    cout<<line<<endl;
   }
 
   string::size_type p1;
   string::size_type p2;
   string::size_type p3;
 
+  //cout<<" size "<<jsonFile.size()<<endl;
+
   for (unsigned int i = 0; i < jsonFile.size(); ++i) {
     p1 = jsonFile[i].find("\""); 
     p2 = jsonFile[i].find("\"", p1+1); 
     p3 = jsonFile[i].find("]],", p2+1); 
-    while (p1 != string::npos && p2 != string::npos && p3 != string::npos) {
+    if(p3 == string::npos ) p3 = jsonFile[i].find("]]}", p2+1); // take care of the last line  
+    while (p1 != string::npos && p2 != string::npos && p3 != string::npos ) {
       string sRun = jsonFile[i].substr(p1+1, p2-p1-1); 
       string sLs  = jsonFile[i].substr(p2+3, p3-p2-1); 
-      //      cout << "run: " << sRun << " -> LS: " << sLs << endl;
+      //cout << "run: " << sRun << " -> LS: " << sLs << " "<<i<<endl;
       int run = atoi(sRun.c_str());
       vector<string> sections = parseLS(sLs); 
 
@@ -41,7 +45,7 @@ JSON::JSON(const char *fname, int verbose) {
       for (unsigned int j = 0; j < sections.size(); ++j) {
 	lumisection = ls(sections[j]); 
 	runLS.push_back(lumisection); 
-	//	cout << "  " << sections[j] << " -> " << lumisection.first << " ... " << lumisection.second << endl;
+	//cout << "  " << sections[j] << " -> " << lumisection.first << " ... " << lumisection.second << endl;
       }
       fRunLsList.insert(make_pair(run, runLS)); 
      
