6607403a11329b07c0037bbbc2f774cba622963b
naegelic <>
2013-01-25 07:45:09 +0000

    schema migration. - include variables for entry of muon into muon system (muon track) - map with closeby tracks and doca

--- AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaMuon.hh
+++ AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaMuon.hh
@@ -9,6 +9,8 @@
 
 #include "TAnaTrack.hh"
 
+#include <map>
+
 class TAnaMuon : public TAnaTrack {
 
 public:
@@ -18,8 +20,9 @@ public:
   void dump();
 
   // ----------------------------------------------------------------------
-  TVector3  fInnerPlab, fOuterPlab, fGlobalPlab; 
+  TVector3  fInnerPlab, fOuterPlab, fGlobalPlab;
   TVector3  fPositionAtM1, fPositionAtM2;
+  TVector3  fMuonTrackPosAtM1,fMuonTrackPlabAtM1;
 
   // see AN2008_098 "Muon identification in CMS"
   double    fMuonChi2, fMuonZ, fMuonR;
@@ -28,7 +31,8 @@ public:
   int       fNmatchedStations;
   float     fTimeInOut, fTimeInOutE, fTimeOutIn, fTimeOutInE; 
   int       fTimeNdof;
-
+  
+  std::map<int,float> fNstTracks; // usage: (trackIx, doca)
 private:
 
   ClassDef(TAnaMuon,1)
