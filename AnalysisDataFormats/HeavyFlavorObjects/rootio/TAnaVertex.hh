e4260aaf7eebc67267c709607461dab2737a7ffc
ursl <>
2013-02-18 13:41:49 +0000

    remove unused track index from TAnaVertex (that information is contained in each track already)

--- AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaVertex.hh
+++ AnalysisDataFormats/HeavyFlavorObjects/rootio/TAnaVertex.hh
@@ -1,25 +1,17 @@
 #ifndef TANAVERTEX
 #define TANAVERTEX
 
-#define TANAVERTEX_MAXTRK 200
-
 #include "TObject.h"
 #include "TVector3.h"
 #include "TLorentzVector.h"
 #include "TMatrixD.h"
 
 class TAnaVertex: public TObject {
-
 public:
-
   TAnaVertex();
   virtual ~TAnaVertex();
   void clear();
   void dump();
-
-  //  TAnaVertex& operator=(const TAnaVertex&);
-
-  // ----------------------------------------------------------------------
   void setInfo(double chi2, double ndof, double prob, int isFake, int type) {
     fChi2 = chi2;
     fNdof = ndof;
@@ -28,15 +20,9 @@ public:
     fType = type;
   }
 
-  void           addTrack(int index);  
-  int            getNtracks()    {return fNtracks;}
-  int            getTrack(int n) {return fTracksIndex[n];}
-
   void setCovXX(Double_t *x);
   void getCovXX(TMatrixD& x);
 
-
-  // ----------------------------------------------------------------------
   float fChi2;
   float fNdof;
   float fProb;
@@ -50,16 +36,9 @@ public:
   TVector3 fPoint;
   TVector3 fSimPoint;
 
-
 private:
   float fCovXX[9];
-
-  int           fNtracks;
-  int           fTracksIndex[TANAVERTEX_MAXTRK];
-
   ClassDef(TAnaVertex,1)
-
 };
-
 
 #endif
